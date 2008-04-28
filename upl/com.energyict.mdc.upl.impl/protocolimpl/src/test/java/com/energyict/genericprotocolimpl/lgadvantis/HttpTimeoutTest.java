437aee00b5f424a223a332fe7cbfd21bd157bd83
swi <swi>
2008-04-28 15:09:30 +0000

    copied tests into protocolimpl project
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25232 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/genericprotocolimpl/lgadvantis/HttpTimeoutTest.java
@@ -0,0 +1,63 @@
+package com.energyict.genericprotocolimpl.lgadvantis;
+
+import com.energyict.mdw.core.MeteringWarehouse;
+import com.energyict.mdw.core.MeteringWarehouseFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class HttpTimeoutTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		MeteringWarehouse mw = (new MeteringWarehouseFactory()).getBatch(true);
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testHttpTimeout(){
+		try {
+			int msTimeout = 5000;
+			
+			HttpTimeoutHandler th = new HttpTimeoutHandler( msTimeout );
+			URL aUrl = new URL( "http", "10.0.0.88", 80, "/ReportAllValues", th );
+			
+			URLConnection conn = aUrl.openConnection();
+			
+			conn.setDoOutput(true);
+			OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+			wr.write("");
+			wr.flush();
+			System.out.println(new Date().toString());
+			InputStreamReader reader = new InputStreamReader(conn.getInputStream());
+			BufferedReader bReader = new BufferedReader(reader);
+			
+			StringBuffer sb = new StringBuffer();
+			String line;
+			
+			while ((line = bReader.readLine()) != null) {
+			    sb.append(line);
+			}
+			
+			wr.close();
+			bReader.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println(new Date().toString());
+			e.printStackTrace();
+		}
+
+	}
+	
+}
