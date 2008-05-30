5223d113d0d6f9ee828b431316e2fbf20d6488b7
Govanni Naessens <Govanni.Naessens@elster.com>
2008-05-30 09:39:47 +0000

    New Trimaran CJE Test
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25686 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/genericprotocolimpl/iskrap2lpc/ConcentratorTest.java
@@ -0,0 +1,103 @@
+package com.energyict.genericprotocolimpl.iskrap2lpc;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.rpc.ServiceException;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.energyict.cbo.BusinessException;
+import com.energyict.cpo.BusinessObject;
+import com.energyict.utils.Utilities;
+import com.energyict.mdw.core.CommunicationProfile;
+import com.energyict.mdw.core.CommunicationProtocol;
+import com.energyict.mdw.core.CommunicationScheduler;
+import com.energyict.mdw.core.MeteringWarehouse;
+import com.energyict.mdw.core.Rtu;
+import com.energyict.mdw.core.RtuType;
+import com.energyict.mdw.shadow.RtuShadow;
+import com.energyict.protocol.InvalidPropertyException;
+
+public class ConcentratorTest{
+
+	private final static String concentratorRtuType = "Iskra concentrator";
+	private final static String iskraGprsMeterRtuType = "Iskra Electricity Meter [ch4]";
+	
+	private CommunicationProfile commProfile;
+	private CommunicationScheduler scheduler;
+	private Concentrator iskraConcentrator;
+	private Rtu meter;
+	private Rtu concentrator;
+	
+	private RtuType conRtuType;
+	private RtuType meterRtuType;
+	private RtuShadow conShadow;
+	private RtuShadow meterShadow;
+	
+	private boolean notCorrect = false;
+	
+	@BeforeClass
+	public static void setUpOnce() {
+		Utilities.createEnvironment();
+		MeteringWarehouse.createBatchContext(false);
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		iskraConcentrator = new Concentrator();
+		iskraConcentrator.setTESTING(true);
+		conRtuType 		= iskraConcentrator.mw().getRtuTypeFactory().find(concentratorRtuType);
+		meterRtuType	= iskraConcentrator.mw().getRtuTypeFactory().find(iskraGprsMeterRtuType);
+		conShadow 		= conRtuType.newRtuShadow();
+		meterShadow 	= meterRtuType.newRtuShadow();
+		meterShadow.setName("testRtu");
+		meter 			= iskraConcentrator.mw().getRtuFactory().create(meterShadow);
+		concentrator 	= meter.getGateway();
+		if (concentrator != null){
+			Iterator scheduleIt = concentrator.getCommunicationSchedulers().iterator();
+			while((!notCorrect) && (scheduleIt.hasNext())){
+				scheduler = (CommunicationScheduler) scheduleIt.next();
+				notCorrect = scheduler.getCommunicationProfile().getReadDemandValues();
+			}
+			iskraConcentrator.communicationProfile = scheduler.getCommunicationProfile();
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if(meter != null)
+			meter.delete();
+	}
+	
+	@Ignore
+	public void importProfileTest() {
+		try {
+			if(meter==null)
+				fail();
+			XmlHandler xmlHandler = new XmlHandler(null, iskraConcentrator.getChannelMap(meter));
+			iskraConcentrator.importProfile(null, meter, xmlHandler);
+			assertEquals(true, true);
+			
+		} catch (InvalidPropertyException e) {
+			fail();
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			fail();
+			e.printStackTrace();
+		} catch (IOException e) {
+			fail();
+			e.printStackTrace();
+		} catch (BusinessException e) {
+			fail();
+			e.printStackTrace();
+		}
+	}
+}
--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/protocolimpl/edf/trimarancje/TrimaranTest.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/protocolimpl/edf/trimarancje/TrimaranTest.java
@@ -1,9 +1,11 @@
 package com.energyict.protocolimpl.edf.trimarancje;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.TimeZone;
 
@@ -11,7 +13,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.energyict.cbo.Unit;
 import com.energyict.cbo.Utils;
+import com.energyict.protocol.ChannelInfo;
 import com.energyict.protocol.UnsupportedException;
 import com.energyict.protocolimpl.edf.trimarancje.core.DataFactory;
 import com.energyict.protocolimpl.edf.trimarancje.core.DemandData;
@@ -20,7 +24,15 @@ public class TrimaranTest {
 	
 	private Trimaran cje;
 	private String trimaranProfile = "/offlineFiles/TrimaranProfile.bin";
-
+	private String[] profile = {"/offlineFiles/Profile1.bin", "/offlineFiles/Profile2.bin",
+								"/offlineFiles/Profile3.bin", "/offlineFiles/Profile4.bin",
+								"/offlineFiles/Profile5.bin", "/offlineFiles/Profile6.bin",
+								"/offlineFiles/Profile7.bin", "/offlineFiles/Profile8.bin",
+								"/offlineFiles/Profile9.bin", "/offlineFiles/Profile10.bin",
+								"/offlineFiles/Profile11.bin", "/offlineFiles/Profile12.bin",
+								"/offlineFiles/Profile13.bin", "/offlineFiles/Profile14.bin",
+								"/offlineFiles/Profile15.bin", "/offlineFiles/Profile16.bin",};
+	private String test = "/offlineFiles/Profile1.bin";
 	@Before
 	public void setUp() throws Exception {
 		cje = new Trimaran();
@@ -34,18 +46,57 @@ public class TrimaranTest {
 	@Test
 	public void profileTest(){
 		try {
-			cje.setTrimeranProfile(new TrimaranProfile(cje));
+			cje.setTrimaranProfile(new TrimaranProfile(cje));
 			cje.setDataFactory(new DataFactory(cje));
-			cje.getTrimeranProfile().setDemandData(new DemandData(cje.getDataFactory()));
+			cje.getTrimaranProfile().setDemandData(new DemandData(cje.getDataFactory()));
 	        File file = new File(Utils.class.getResource(trimaranProfile).getFile());
 	        FileInputStream fis = new FileInputStream(file);
 	        byte[] data=new byte[(int)file.length()];
 	        fis.read(data);
 	        fis.close();   
-	        cje.getTrimeranProfile().getDemandData().parse(data);
+	        cje.getTrimaranProfile().getDemandData().parse(data);
+	        cje.getTrimaranProfile().incrementPointer();
+	        
+	        /* have to check two profiles now */
+	        cje.getTrimaranProfile().setDemandData(new DemandData(cje.getDataFactory()));
+	        cje.getTrimaranProfile().getDemandData().parse(data);
+	        
 			cje.getProfileData(null, null, false);
 			// if we can get this far it should be OK ...
 		} catch (UnsupportedException e) {
+			fail();
+			e.printStackTrace();
+		} catch (IOException e) {
+			fail();
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	/**
+	 *  this test has to check the V2 of the Trimaran meters 
+	 */
+	public void profileTest2(){
+		cje.setTrimaranProfile(new TrimaranProfile(cje));
+		cje.setDataFactory(new DataFactory(cje));
+		try {
+			for(int i = 0; i < profile.length; i++){
+				cje.getTrimaranProfile().setDemandData(new DemandData(cje.getDataFactory()));
+				File file = new File(Utils.class.getResource(profile[i]).getFile());
+				FileInputStream fis;
+				fis = new FileInputStream(file);
+				byte[] data=new byte[(int)file.length()];
+				fis.read(data);
+				fis.close();   
+				cje.getTrimaranProfile().getDemandData().parse(data);
+				cje.getTrimaranProfile().incrementPointer();
+					
+			}
+			assertEquals(cje.getTrimaranProfile().getPointer(), 16);
+			cje.getTrimaranProfile().setPointer(1);
+			ChannelInfo ci = (ChannelInfo) cje.getTrimaranProfile().getProfileData().getChannelInfos().get(0);
+			assertEquals(ci.getUnit(), Unit.get("kW"));
+		} catch (FileNotFoundException e) {
 			fail();
 			e.printStackTrace();
 		} catch (IOException e) {
