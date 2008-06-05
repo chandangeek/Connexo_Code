a7b5da4e1c3b355db2d7d461948bc8af9c9b6c48
Govanni Naessens <Govanni.Naessens@elster.com>
2008-06-05 09:22:54 +0000

    Cause the 'remoteRun' from TeamCity doesn't add new classes, I commit these now and make the changes later.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25754 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/core/ParametersMinus1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p.core;
+
+/**
+ * @author gna
+ *
+ */
+public class ParametersMinus1 {
+
+	/**
+	 * 
+	 */
+	public ParametersMinus1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/core/ParametersPlus1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p.core;
+
+/**
+ * @author gna
+ *
+ */
+public class ParametersPlus1 {
+
+	/**
+	 * 
+	 */
+	public ParametersPlus1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
