c30d4a340d3bf3778a93afdb3c8d1157696a4d42
Govanni Naessens <Govanni.Naessens@elster.com>
2008-06-06 09:16:52 +0000

    Cause the 'remoteRun' from TeamCity doesn't add new classes, I commit these now and make the changes later.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25787 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/core/IndexProgReader.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p.core;
+
+/**
+ * @author gna
+ *
+ */
+public class IndexProgReader {
+
+	/**
+	 * 
+	 */
+	public IndexProgReader() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/core/Programmables.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p.core;
+
+/**
+ * @author gna
+ *
+ */
+public class Programmables {
+
+	/**
+	 * 
+	 */
+	public Programmables() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
