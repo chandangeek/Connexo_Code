efd922192d142d8b8852eb3bc2721fad932c0e61
Govanni Naessens <Govanni.Naessens@elster.com>
2008-06-06 09:05:45 +0000

    Some additional tests
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25774 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/utils/Utilities.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/utils/Utilities.java
@@ -1,13 +1,23 @@
 package com.energyict.utils;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import com.energyict.cbo.ApplicationException;
+import com.energyict.cbo.BusinessException;
 import com.energyict.cbo.Utils;
 import com.energyict.cpo.Environment;
+import com.energyict.mdw.core.CommunicationProtocol;
+import com.energyict.mdw.core.MeteringWarehouse;
+import com.energyict.mdw.core.Rtu;
+import com.energyict.mdw.core.RtuType;
+import com.energyict.mdw.shadow.CommunicationProtocolShadow;
+import com.energyict.mdw.shadow.RtuShadow;
+import com.energyict.mdw.shadow.RtuTypeShadow;
 
 public class Utilities {
+	
 	public static void createEnvironment() {
     	try {
     		Properties properties = new Properties();
@@ -16,5 +26,44 @@ public class Utilities {
 		} catch (IOException e) {
             throw new ApplicationException(e);
 		}
+    }
+	
+	public static CommunicationProtocol createCommunicationProtocol(String javaClassName) throws BusinessException, SQLException{
+		CommunicationProtocolShadow commProtShadow = new CommunicationProtocolShadow();
+		commProtShadow.setJavaClassName(javaClassName);
+		commProtShadow.setName(javaClassName);
+		return mw().getCommunicationProtocolFactory().create(commProtShadow);
+	}
+	
+	public static RtuType createRtuType(CommunicationProtocol commProtocol, String name, int channelCount) throws SQLException, BusinessException{
+		RtuTypeShadow rtuTypeShadow = new RtuTypeShadow();
+		rtuTypeShadow.setChannelCount(channelCount);
+		rtuTypeShadow.setName(name);
+//		rtuTypeShadow.setProtocolShadow(commProtocol.getShadow());
+		rtuTypeShadow.setProtocolId(commProtocol.getId());
+		RtuType rtuType = mw().getRtuTypeFactory().create(rtuTypeShadow);
+		return rtuType;
+	}
+	
+	public static Rtu createRtu(RtuType rtuType) throws SQLException, BusinessException{
+		RtuShadow rtuShadow = rtuType.newRtuShadow();
+		rtuShadow.markClean();
+		rtuShadow.setName(rtuType.getName());
+		rtuShadow.setSerialNumber("99999999");
+		Rtu rtu = mw().getRtuFactory().create(rtuShadow);
+		return rtu;
+	}
+	
+	public static Rtu createRtu(RtuType rtuType, String serial) throws SQLException, BusinessException{
+		final RtuShadow rtuShadow = rtuType.newRtuShadow();
+		rtuShadow.setName(rtuType.getName());
+		rtuShadow.setSerialNumber(serial);
+		Rtu rtu = mw().getRtuFactory().create(rtuShadow);
+		return rtu;
+	}
+	
+	
+    public static MeteringWarehouse mw() {
+        return MeteringWarehouse.getCurrent();
     }
 }
--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/utils/UtilitiesTest.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/utils/UtilitiesTest.java
@@ -19,7 +19,10 @@ import org.junit.Test;
 import com.energyict.cbo.BusinessException;
 import com.energyict.mdw.core.CommunicationProtocol;
 import com.energyict.mdw.core.MeteringWarehouse;
+import com.energyict.mdw.core.RtuType;
 import com.energyict.mdw.coreimpl.CommunicationProtocolImpl;
+import com.energyict.mdw.coreimpl.RtuImpl;
+import com.energyict.mdw.coreimpl.RtuTypeImpl;
 
 /**
  * @author gna
@@ -50,18 +53,24 @@ public class UtilitiesTest {
 	public void tearDown() throws Exception {
 		
 		// first delete the device
+		List result = Utilities.mw().getRtuFactory().findBySerialNumber("99999999");
+		if (result.size() > 0)
+			for(int i = 0; i < result.size(); i++)
+				((RtuImpl)result.get(0)).delete();
 		
 		// then the deviceType
+		result = Utilities.mw().getRtuTypeFactory().findByName(testRtu);
+		if (result.size() > 0)
+			for(int i = 0; i < result.size(); i++)
+				((RtuTypeImpl)result.get(0)).delete();
 		
 		// then the communication profile
-		
-		List result = Utilities.mw().getCommunicationProtocolFactory().findByName(javaClassName);
+		result = Utilities.mw().getCommunicationProtocolFactory().findByName(javaClassName);
 		if (result.size() > 0)
 			for(int i = 0; i < result.size(); i++)
 				((CommunicationProtocolImpl)result.get(0)).delete();
 	}
 	
-	@Ignore
 	@Test
 	public void createCommunicationProtocolTest(){
 		try {
@@ -83,13 +92,48 @@ public class UtilitiesTest {
 		}
 	}
 	
-	@Ignore
 	@Test
 	public void createRtuTypeTest(){
 		try {
 			CommunicationProtocol commProtocol = Utilities.createCommunicationProtocol(javaClassName);
 			
 			Utilities.createRtuType(commProtocol, testRtu, 6);
+			List result = Utilities.mw().getRtuTypeFactory().findByName(testRtu);
+			
+			if(result.size() == 1){
+				assertEquals(testRtu, ((RtuTypeImpl)result.get(0)).getShadow().getName());
+				assertEquals(javaClassName, ((RtuTypeImpl)result.get(0)).getShadow().getCommunicationProtocolShadow().getName());
+				assertEquals(6, ((RtuTypeImpl)result.get(0)).getShadow().getChannelCount());
+			}
+			else
+				fail();
+			
+		} catch (BusinessException e) {
+			fail();
+			e.printStackTrace();
+		} catch (SQLException e) {
+			fail();
+			e.printStackTrace();
+		}
+	}
+	@Ignore
+	@Test
+	public void createRtuTest(){
+		CommunicationProtocol commProtocol;
+		RtuType rtuType;
+		try {
+			commProtocol = Utilities.createCommunicationProtocol(javaClassName);
+			rtuType = Utilities.createRtuType(commProtocol, testRtu, 6);
+			
+			Utilities.createRtu(rtuType);
+			List result = Utilities.mw().getRtuFactory().findBySerialNumber("99999999");
+			
+			if(result.size() == 1){
+				assertEquals(testRtu, ((RtuImpl)result.get(0)).getShadow().getName());
+				assertEquals(6, ((RtuImpl)result.get(0)).getShadow().getChannelShadows().size());
+			}
+			else
+				fail();
 			
 		} catch (BusinessException e) {
 			fail();
