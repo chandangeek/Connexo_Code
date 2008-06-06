dba76ba70459538cc5af86fb2eec6f78376e0d16
Govanni Naessens <Govanni.Naessens@elster.com>
2008-06-06 11:11:57 +0000

    Because the 'remoteRun' from TeamCity doesn't add new classes, I commit these now and make the changes later.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25794 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/registermapping/RegisterFactory.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p.registermapping;
+
+/**
+ * @author gna
+ *
+ */
+public class RegisterFactory {
+
+	/**
+	 * 
+	 */
+	public RegisterFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/registermapping/RegisterNameFactory.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p.registermapping;
+
+/**
+ * @author gna
+ *
+ */
+public class RegisterNameFactory {
+
+	/**
+	 * 
+	 */
+	public RegisterNameFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
