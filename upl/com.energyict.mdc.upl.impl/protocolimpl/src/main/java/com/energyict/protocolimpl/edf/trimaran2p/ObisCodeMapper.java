1f4f2882e8898d81f53fd41b60b984e1c5c21ebd
Govanni Naessens <Govanni.Naessens@elster.com>
2008-06-06 11:04:58 +0000

    Because the 'remoteRun' from TeamCity doesn't add new classes, I commit these now and make the changes later.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@25793 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/edf/trimaran2p/ObisCodeMapper.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.edf.trimaran2p;
+
+/**
+ * @author gna
+ *
+ */
+public class ObisCodeMapper {
+
+	/**
+	 * 
+	 */
+	public ObisCodeMapper() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
