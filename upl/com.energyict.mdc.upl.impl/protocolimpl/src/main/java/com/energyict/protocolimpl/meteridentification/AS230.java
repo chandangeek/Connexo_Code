081ea9f2f42e89712285a48b89fa306fc7269d88
Koen Vanderschaeve <k.vanderschaeve@energyict.com>
2008-07-30 19:21:57 +0000

    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@26678 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/meteridentification/AS230.java
@@ -0,0 +1,48 @@
+/*
+ * LGZ.java
+ *
+ * Created on 15 april 2005, 11:53
+ */
+
+package com.energyict.protocolimpl.meteridentification;
+import java.io.*;
+import java.util.*;
+
+
+/**
+ *
+ * @author  Koen
+ */
+public class AS230 extends AbstractManufacturer {
+    
+    /** Creates a new instance of AS230 */
+    public AS230() {
+    }
+    
+    public String getManufacturer() throws IOException {
+        return "Elster AS230";
+    }
+    
+    public String getMeterProtocolClass() throws IOException {
+        
+        if ((getSignOnString()==null) || (getSignOnString().indexOf("\\2")>=0) || (getSignOnString().indexOf("\2")>=0))
+           return "com.energyict.protocolimpl.iec1107.abba230.ABBA230";
+        
+        throw new IOException("Unknown metertype for signonstring "+getSignOnString());
+    }
+    
+    public String[] getMeterSerialNumberRegisters() throws IOException {
+        if ((getSignOnString()==null) || (getSignOnString().indexOf("\\2")>=0) || (getSignOnString().indexOf("\2")>=0))
+           return new String[]{"0.0.0","00","0"};
+        
+        throw new IOException("Unknown metertype for signonstring "+getSignOnString());
+    }
+    
+    public String getMeterDescription() throws IOException {
+        if ((getSignOnString()==null) || (getSignOnString().indexOf("\\2")>=0) || (getSignOnString().indexOf("\2")>=0))
+           return "Elster AS230";
+        
+        throw new IOException("Unknown metertype for signonstring "+getSignOnString());
+    }
+    
+}
