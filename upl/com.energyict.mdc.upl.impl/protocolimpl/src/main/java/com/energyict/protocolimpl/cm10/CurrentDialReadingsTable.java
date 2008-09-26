94731f2092eeb2c79f95a49b86759db0ae02ede4
Isabelle Gheysens <Isabelle.Gheysens@elster.com>
2008-09-26 08:56:47 +0000

    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@28014 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/CurrentDialReadingsTable.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/CurrentDialReadingsTable.java
@@ -1,6 +1,8 @@
 package com.energyict.protocolimpl.cm10;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 import com.energyict.protocol.ProtocolUtils;
 
@@ -8,6 +10,7 @@ public class CurrentDialReadingsTable {
 	
 	private CM10 cm10Protocol;
 	private long dialReadings[] = new long[48];
+	private Calendar calendar = Calendar.getInstance(cm10Protocol.getTimeZone());
 	
 	public CurrentDialReadingsTable(CM10 cm10Protocol) {
 		this.cm10Protocol = cm10Protocol;
@@ -17,6 +20,14 @@ public class CurrentDialReadingsTable {
 		for (int i = 0; i < 48; i++) {
 			dialReadings[i] = ProtocolUtils.getLongLE(data, i * 3, 3);
 		}
+	}
+	
+	public long[] getValues() {
+		return dialReadings;
+	}
+	
+	public Date getToTime() {
+		return calendar.getTime();
 	}
 	
 	public String toString() {
--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/ObisCodeMapper.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/ObisCodeMapper.java
@@ -1,9 +1,15 @@
 package com.energyict.protocolimpl.cm10;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
 
+import com.energyict.cbo.BaseUnit;
+import com.energyict.cbo.Quantity;
+import com.energyict.cbo.Unit;
 import com.energyict.obis.ObisCode;
 
+import com.energyict.protocol.NoSuchRegisterException;
 import com.energyict.protocol.RegisterInfo;
 import com.energyict.protocol.RegisterValue;
 
@@ -27,7 +33,23 @@ public class ObisCodeMapper {
     }
 
     public RegisterValue getRegisterValue(ObisCode obisCode) throws IOException {
-    	return null;
+    	if( obisCode.getA()!=1 || 
+				obisCode.getC()!=82 ||
+				obisCode.getD()!=128 ||
+				obisCode.getE()!=0 ||
+				obisCode.getB()>=cm10Protocol.getNumberOfChannels() ||
+				obisCode.getB()<0)
+				throw new NoSuchRegisterException("ObisCode "+obisCode.toString()+" is not supported!");
+    	
+    	
+    	short[] channelVal;
+		RegisterValue reg;
+		CurrentDialReadingsTable currentDialReadingsTable = cm10Protocol.getCurrentDialReadingsTable();
+		long[] currentDialreadings = currentDialReadingsTable.getValues();
+		Date toTime = currentDialReadingsTable.getToTime();
+		Quantity quantity = new Quantity(new BigDecimal(currentDialreadings[obisCode.getB()]), Unit.get(BaseUnit.UNITLESS));
+		reg = new RegisterValue(obisCode, quantity, null, toTime);				
+		return reg;
     }
        
 
