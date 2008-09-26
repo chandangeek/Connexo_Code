c30ad9c0fa4f76abbc8f1149a33a2a44b2926fa0
Isabelle Gheysens <Isabelle.Gheysens@elster.com>
2008-09-26 08:46:23 +0000

    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@28010 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/MeterDemandsTable.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/MeterDemandsTable.java
@@ -36,10 +36,11 @@ public class MeterDemandsTable {
 			cal.add(Calendar.SECOND, intervalInSeconds);
 			IntervalData intervalData = new IntervalData(cal.getTime()); 
 			for (int j = 0; j < numberOfChannels; j++) {
-				BigDecimal value = new BigDecimal(ProtocolUtils.getLongLE(data, i, 2));
+				BigDecimal value = new BigDecimal(ProtocolUtils.getLongLE(data, i + (j * 2), 2));
 				intervalData.addValue(value);
 				cm10Protocol.getLogger().info("channel " + j + ": " + cal.getTime() + ": " + value);
 			}
+			profileData.addInterval(intervalData);
 			i = i + (numberOfChannels * 2); // 2 bytes per channel interval record
 		}
 	}
