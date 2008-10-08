31c9800e8514b33994d1a5ea23bb73ce050f0248
Koen Vanderschaeve <k.vanderschaeve@energyict.com>
2008-10-08 14:09:27 +0000

    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@28372 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/custom/DeviceCustomCosem.java
@@ -0,0 +1,55 @@
+package com.energyict.dlms.cosem.custom;
+
+import java.io.IOException;
+import java.util.Date;
+
+
+import com.energyict.dlms.*;
+import com.energyict.dlms.axrdencoding.*;
+import com.energyict.dlms.axrdencoding.OctetString;
+import com.energyict.dlms.client.CompoundDataBuilderConnection;
+import com.energyict.dlms.cosem.*;
+import com.energyict.obis.ObisCode;
+
+public class DeviceCustomCosem extends Data {
+
+	AbstractDataType dataType=null;
+
+	// in case of message null, only a integer8 (byte) is send in the data value
+	// in case of message, a struct is coded in the data value containing a integer8 (byte) and a octetstring with the message
+	
+	static final byte[] LN=new byte[]{0,0,96,111,0,0};
+	
+	public DeviceCustomCosem(AbstractDataType dataType) {
+		super(null,new ObjectReference(LN));
+		this.dataType=dataType;
+	}
+	
+	public DeviceCustomCosem() {
+		super(new CompoundDataBuilderConnection(),new ObjectReference(LN));
+	}
+	
+	public DeviceCustomCosem(ProtocolLink protocolLink) {
+		super(protocolLink,new ObjectReference(LN));
+    }
+    
+	static public ObisCode getObisCode() {
+		return ObisCode.fromByteArray(LN) ;
+	}
+	
+    protected int getClassId() {
+        return AbstractCosemObject.CLASSID_DATA;
+    }
+
+    public AbstractDataType getValueAttr() throws IOException {
+    	if (dataType == null)
+    		dataType = super.getValueAttr();
+    	return dataType;
+    }
+    
+    public void setValueAttr(AbstractDataType val) throws IOException {
+    	dataType = val;
+    	super.setValueAttr(dataType);
+    }
+    
+}
