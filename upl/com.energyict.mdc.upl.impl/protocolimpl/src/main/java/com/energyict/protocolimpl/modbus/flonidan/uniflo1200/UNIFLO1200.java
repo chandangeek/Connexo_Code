ae527db6df30ba0fbe4d37974aa969b2cbad01e2
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2008-12-04 14:27:08 +0000

    New meter protocol: UNIFLO 1200 modbus meter
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@29551 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/flonidan/uniflo1200/UNIFLO1200.java
@@ -0,0 +1,55 @@
+/**
+ * UNIFLO1200.java
+ * 
+ * Created on 4-dec-2008, 15:00:50 by jme
+ * 
+ */
+package com.energyict.protocolimpl.modbus.flonidan.uniflo1200;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import com.energyict.protocol.InvalidPropertyException;
+import com.energyict.protocol.MissingPropertyException;
+import com.energyict.protocol.discover.DiscoverResult;
+import com.energyict.protocol.discover.DiscoverTools;
+import com.energyict.protocolimpl.modbus.core.*;
+
+/**
+ * @author jme
+ *
+ */
+public class UNIFLO1200 extends Modbus {
+
+	protected void doTheConnect() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void doTheDisConnect() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected List doTheGetOptionalKeys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected void doTheValidateProperties(Properties properties)
+			throws MissingPropertyException, InvalidPropertyException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void initRegisterFactory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public DiscoverResult discover(DiscoverTools discoverTools) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
