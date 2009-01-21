4de681ac383d1181b29fc742b33a8634d3f1fe2c
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-01-21 14:49:27 +0000

    findByDailID
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@30447 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/edmi/mk10/MK10Push.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/edmi/mk10/MK10Push.java
@@ -165,18 +165,13 @@ public class MK10Push implements GenericProtocol {
 	
 	private Rtu findMatchingMeter(String serial) {
 		if (serial == null) return null;
-		List meterList = mw().getRtuFactory().findBySerialNumber(serial);
+		List meterList = mw().getRtuFactory().findByDialHomeId(serial);
 		
-		for (int i = 0; i < meterList.size(); i++) {
-			Rtu tempMeter = (Rtu) meterList.get(i);
-			if (tempMeter.getDialHomeId() != null){
-				if (tempMeter.getDialHomeId().trim().equalsIgnoreCase(serial.trim())) {
-					return (Rtu) meterList.get(i);
-				}
-			}
+		if (meterList.size() == 1) {
+			return (Rtu) meterList.get(0);
+		} else {
+			return null;
 		}
-		
-		return null;
 	}
 	
 	private void initMk10Protocol(Rtu rtu) throws IOException {
