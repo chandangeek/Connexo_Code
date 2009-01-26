8bac97ec5d8fff169226b125c15e9825243e3e87
Govanni Naessens <Govanni.Naessens@elster.com>
2009-01-26 16:29:22 +0000

    Extended the SingleActionScheduler object with all getters and setters for all attributes
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@30603 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SingleActionSchedule.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SingleActionSchedule.java
@@ -14,14 +14,17 @@ import com.energyict.dlms.axrdencoding.TypeEnum;
 /**
  * @author gna
  *
+ * Changes:
+ * GNA |26012009| Extended the object with all getters and setters for all attributes
+ * 				NOTE: check the executedScript, it is possible that you have to use a ScriptTable object instead of a Structure
  */
 public class SingleActionSchedule extends AbstractCosemObject{
 	
 	/** Attributes */
 //	private ScriptTable executedScript = null;
-	private Structure executedScript = null;
-	private TypeEnum type = null;
-	private Array executionTime = null;
+	private Structure executedScript = null;	// The Structure is a combination of the script_LogicalName and the script_Selector
+	private TypeEnum type = null;				// Let's you define wildcards in the datetime
+	private Array executionTime = null;	
 	
 	/** Attribute numbers */
 	static private final int ATTRB_EXECUTED_SCRIPT = 2;
@@ -29,7 +32,8 @@ public class SingleActionSchedule extends AbstractCosemObject{
 	static private final int ATTRB_EXECUTION_TIME = 4;
 	
 	/** Method invoke */
-
+	// none
+	
 	public SingleActionSchedule(ProtocolLink protocolLink,ObjectReference objectReference) {
 		super(protocolLink, objectReference);
 	}
@@ -37,7 +41,76 @@ public class SingleActionSchedule extends AbstractCosemObject{
 	protected int getClassId() {
 		return AbstractCosemObject.CLASSID_SINGLE_ACTION_SCHEDULE;
 	}
+	
+	/**
+	 * Get the latest retrieved executedScript
+	 * @return
+	 * @throws IOException
+	 */
+	public Structure getExecutedScript() throws IOException{
+		if(this.executedScript == null){
+			readExecutedScript(); // do a dummy read
+		}
+		return this.executedScript;
+	}
+	
+	/**
+	 * Read the executedScript from the device
+	 * @return
+	 * @throws IOException
+	 */
+	public Structure readExecutedScript() throws IOException{
+		this.executedScript = new Structure(getLNResponseData(ATTRB_EXECUTED_SCRIPT), 0, 0);
+		return this.executedScript;
+	}
+	
+	/**
+	 * Get the latest retrieved type
+	 * @return
+	 * @throws IOException
+	 */
+	public TypeEnum getType() throws IOException{
+		if(this.type == null){
+			readType();	// do a dummy read
+		}
+		return this.type;
+	}
+	
+	/**
+	 * Read the type from the device
+	 * @return
+	 * @throws IOException
+	 */
+	public TypeEnum readType() throws IOException{
+		this.type = new TypeEnum(getLNResponseData(ATTRB_TYPE), 0);
+		return this.type;
+	}
+	
+	/**
+	 * Write a type to the device
+	 * @param type
+	 * @throws IOException
+	 */
+	public void writeType(TypeEnum type) throws IOException{
+		write(ATTRB_TYPE, type.getBEREncodedByteArray());
+		this.type = type;
+	}
+	
+	/**
+	 * Write a specific scriptStructure to the device
+	 * @param script
+	 * @throws IOException
+	 */
+	public void writeExecutedScript(Structure script) throws IOException{
+		write(ATTRB_EXECUTED_SCRIPT, script.getBEREncodedByteArray());
+		this.executedScript = script;
+	}
 
+	/**
+	 * Reads the executionTime from the device
+	 * @return
+	 * @throws IOException
+	 */
 	public Array getExecutionTime() throws IOException {
 		if (executionTime == null){
 			executionTime = (Array)AXDRDecoder.decode(getLNResponseData(ATTRB_EXECUTION_TIME));
@@ -45,6 +118,11 @@ public class SingleActionSchedule extends AbstractCosemObject{
 		return executionTime;
 	}
 	
+	/**
+	 * Writes an ExecutionTime to the device
+	 * @param executionTime
+	 * @throws IOException
+	 */
     public void writeExecutionTime(Array executionTime) throws IOException {
         write(ATTRB_EXECUTION_TIME, executionTime.getBEREncodedByteArray());
         this.executionTime=executionTime;
