09496da483451dd58f8ea26d3da12b017eca4e7f
Govanni Naessens <Govanni.Naessens@elster.com>
2009-01-27 15:33:53 +0000

    Completed the Limiter Object with all attribute getters and setters
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@30624 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/Limiter.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/Limiter.java
@@ -10,7 +10,10 @@ import com.energyict.dlms.axrdencoding.AXDRDecoder;
 import com.energyict.dlms.axrdencoding.AbstractDataType;
 import com.energyict.dlms.axrdencoding.Array;
 import com.energyict.dlms.axrdencoding.BooleanObject;
+import com.energyict.dlms.axrdencoding.Integer8;
+import com.energyict.dlms.axrdencoding.OctetString;
 import com.energyict.dlms.axrdencoding.Structure;
+import com.energyict.dlms.axrdencoding.Unsigned16;
 import com.energyict.dlms.axrdencoding.Unsigned32;
 
 /**
@@ -22,11 +25,13 @@ import com.energyict.dlms.axrdencoding.Unsigned32;
 	 * 		- a long-unsigned(Unsigned16) class_id
 	 * 		- an octet-string(OctetString) logical_name 
 	 * 		- an integer(Integer8) attribute_index
+	 * (See class valueDefinitionType)
 
 	 * The emergencyProfile structure contains:
 	 * 		- a long-unsigned(Unsigned16) emergency_profile_id	
 	 *		- an octet-string(OctetString) emergency_activation_time	-> Defines date and time when the emergency_profile activated
 	 *		- a double-long-unsigned(Unsigned32) emergency_duration		-> Defines the duration in seconds, for which the emergency_profile is activated
+	 * (See class EmergencyProfile)
 			 
 	 * The emergencyProfile can be activated only if emergency_profile_id element of the emergencyProfile type matches one of the
 	 * elements on the emergencyProfileGroupIdList.
@@ -36,28 +41,30 @@ import com.energyict.dlms.axrdencoding.Unsigned32;
 	 * 		- an actionItem(Structure) action_over_threshold 	-> Defines the action when the value of the attribute monitored crosses the threshold in upwards
 	 * 															-> direction and remains over threshold for minimal overThresholdDuration time
 	 * 		- an actionItem(Structure) action_under_threshold	-> Defines the action when the value of the attribute monitored crosses the threshold in downwards
-	 * 															-> direction and remains under threshold for minimal underThresholdDuration time 
+	 * 															-> direction and remains under threshold for minimal underThresholdDuration time
+	 * (See class ActionType) 
 	 
 	 * The actionItem structure contains:	-> indicates a certain script table		
 	 * 		- an octet-string(OctetString) script_logical_name
 	 * 		- a long-unsigned(Unsigned16) script_selector
+	 * (See class ActionItem)
  */
 public class Limiter extends AbstractCosemObject{
 	
 	private int CLASSID = 71;
 	
 	/** Attributes */
-	private Structure monitoredValue = null;	// Defines an attribute of an object to be monitored. Only simple data types allowed.
+	private ValueDefinitionType monitoredValue = null;	// Defines an attribute of an object to be monitored. Only simple data types allowed.
 	private AbstractDataType thresholdActive = null;	// Provides the active threshold value to which the attribute monitored is compared.
 	private AbstractDataType thresholdNormal = null; // Provides the threshold value to which the attribute monitored is compared when in normal operation
 	private AbstractDataType thresholdEmergency = null; 	// Provides the threshold value to which the attribute monitored is compared when an emergency profile is active
 	private Unsigned32 minOverThresholdDuration = null;	// Defines the minimal over threshold duration in seconds required to execute the over threshold action
 	private Unsigned32 minUnderThresholdDuration = null;	// Defines the minimal under threshold duration in seconds required to execute the under threshold action
-	private Structure emergencyProfile = null; 	// An emergencyProfile is activated if the emergencyProfileId element matches one of the elements on the
+	private EmergencyProfile emergencyProfile = null; 	// An emergencyProfile is activated if the emergencyProfileId element matches one of the elements on the
 												// emergencyProfileGroupIdList, an time matches the emergencyActivationTime and emergencyDuration element.
 	private Array emergencyProfileGroupIdList = null;	// Defines a list of group id-s of the emergency profile
 	private BooleanObject emergencyProfileActive = null;	// Indicates that the emergencyProfile is active
-	private Structure actions = null;	// Defines the scripts to be executed when the monitored value crosses the threshold for minimal duration time
+	private ActionType actions = null;	// Defines the scripts to be executed when the monitored value crosses the threshold for minimal duration time
 	
 	/** Attribute numbers */
 	static private final int ATTRB_MONITORED_VALUE = 2;
@@ -87,9 +94,9 @@ public class Limiter extends AbstractCosemObject{
 	 * @return
 	 * @throws IOException
 	 */
-	public Structure readMonitoredValue() throws IOException{
+	public ValueDefinitionType readMonitoredValue() throws IOException{
 		try {
-			this.monitoredValue = new Structure(getLNResponseData(ATTRB_MONITORED_VALUE), 0, 0);
+			this.monitoredValue = new ValueDefinitionType(getLNResponseData(ATTRB_MONITORED_VALUE), 0, 0);
 			return this.monitoredValue;
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -102,7 +109,7 @@ public class Limiter extends AbstractCosemObject{
 	 * @return
 	 * @throws IOException
 	 */
-	public Structure getMonitoredValue() throws IOException{
+	public ValueDefinitionType getMonitoredValue() throws IOException{
 		if(this.monitoredValue == null) {
 			readMonitoredValue(); // do a dummy read
 		}
@@ -114,7 +121,7 @@ public class Limiter extends AbstractCosemObject{
 	 * @param monitoredValue
 	 * @throws IOException
 	 */
-	public void writeMonitoredValue(Structure monitoredValue) throws IOException{
+	public void writeMonitoredValue(ValueDefinitionType monitoredValue) throws IOException{
 		write(ATTRB_MONITORED_VALUE, monitoredValue.getBEREncodedByteArray());
 		this.monitoredValue = monitoredValue;
 	}
@@ -270,22 +277,271 @@ public class Limiter extends AbstractCosemObject{
 		}
 	}
 	
-	public Structure readEmergencyProfile() throws IOException{
+	/**
+	 * Retrieve the current emergencyProfile from the device
+	 * @return
+	 * @throws IOException
+	 */
+	public EmergencyProfile readEmergencyProfile() throws IOException{
 		try{
-			this.emergencyProfile = new Structure(getLNResponseData(ATTRB_EMERGENCY_PROFILE), 0, 0);
+			this.emergencyProfile = new EmergencyProfile(getLNResponseData(ATTRB_EMERGENCY_PROFILE), 0, 0);
 			return this.emergencyProfile;
 		} catch (IOException e){
 			e.printStackTrace();
-			throw new IOException("could not retrieve the emergencyProfile." + e.getMessage());
+			throw new IOException("Could not retrieve the emergencyProfile." + e.getMessage());
 		}
 	}
 	
-	public void writeEmergencyProfile(Structure emergencyProfile) throws IOException{
-//		try{
-//			
-//		} catch (IOException e){
-//			e.print
-//		}
+	/**
+	 * Write the given emergycyProfile structure to the device
+	 * @param emergencyProfile
+	 * @throws IOException
+	 */
+	public void writeEmergencyProfile(EmergencyProfile emergencyProfile) throws IOException{
+		try{
+			write(ATTRB_EMERGENCY_PROFILE, emergencyProfile.getBEREncodedByteArray());
+			this.emergencyProfile = emergencyProfile;
+		} catch (IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not write the emergencyProfile structure." + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Retrieve the current emergecyProfileGroupIdList from the device
+	 * @return
+	 * @throws IOException
+	 */
+	public Array readEmergencyProfileGroupIdList() throws IOException{
+		try{
+			this.emergencyProfileGroupIdList = new Array(getLNResponseData(ATTRB_EMERGENCY_PROFILE_GROUP_ID_LIST), 0, 0);
+			return this.emergencyProfileGroupIdList;
+		} catch (IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not retrieve the emergencyProfileGroupIdList array." + e.getMessage());
+		}
 	}
 
+	/**
+	 * Write the given emergencyProfileGroupId array to the device
+	 * @param emergencyProfileGroupIdList
+	 * @throws IOException
+	 */
+	public void writeEmergencyProfileGroupIdList(Array emergencyProfileGroupIdList) throws IOException {
+		try{
+			write(ATTRB_EMERGENCY_PROFILE_GROUP_ID_LIST, emergencyProfileGroupIdList.getBEREncodedByteArray());
+			this.emergencyProfileGroupIdList = emergencyProfileGroupIdList;
+		} catch (IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not write the emergencyProfileGroupIdList array." + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Retrieve the current state of the emergencyProfileActive boolean from the device
+	 * @return
+	 * @throws IOException
+	 */
+	public BooleanObject getEmergencyProfileActive() throws IOException{
+		try{
+			this.emergencyProfileActive = new BooleanObject(getLNResponseData(ATTRB_EMERGENCY_PROFILE_ACTIVE), 0);
+			return this.emergencyProfileActive;
+		} catch (IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not retrieve the emergencyProfileActive boolean." + e.getMessage());
+		}
+	}
+	
+	/**
+	 * @return true or false, depending on the state of the emergencyProfile
+	 * @throws IOException
+	 */
+	public boolean isEmergencyProfileActive() throws IOException{
+		return getEmergencyProfileActive().getState();
+	}
+	
+	/**
+	 * Write the given emergencyProfileActive boolean to the device
+	 * @param emergencyProfileActive
+	 * @throws IOException
+	 */
+	public void writeEmergencyProfileActive(BooleanObject emergencyProfileActive) throws IOException{
+		try{
+			write(ATTRB_EMERGENCY_PROFILE_ACTIVE, emergencyProfileActive.getBEREncodedByteArray());
+			this.emergencyProfileActive = emergencyProfileActive;
+		} catch (IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not write the current emergencyProfileActive booleanObject." + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Retrieve the actions from the device
+	 * @return
+	 * @throws IOException
+	 */
+	public ActionType readActions() throws IOException{
+		try{
+			this.actions = new ActionType(getLNResponseData(ATTRB_ACTIONS), 0, 0);
+			return this.actions;
+		} catch (IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not read the actions." + e.getMessage());
+		}
+	}
+	
+	/**
+	 * @return the latest retrieved actions attribute from the device
+	 * @throws IOExcepiton
+	 */
+	public ActionType getActions() throws IOException{
+		if(this.actions == null){
+			readActions(); 	// do a dummy read
+		}
+		return this.actions;
+	}
+	
+	/**
+	 * Write the given actions to the device
+	 * @param actions
+	 * @throws IOException
+	 */
+	public void writeActions(ActionType actions) throws IOException{
+		try{
+			write(ATTRB_ACTIONS, actions.getBEREncodedByteArray());
+			this.actions = actions;
+		} catch(IOException e){
+			e.printStackTrace();
+			throw new IOException("Could not write the actions to the device." + e.getMessage());
+		}
+	}
+	
+	public class ValueDefinitionType extends Structure{
+		static private final int ITEM_CLASS_ID = 0;
+		static private final int ITEM_LOGICAL_NAME = 0;
+		static private final int ITEM_ATTRIBUTE_INDEX = 0;
+		
+		public ValueDefinitionType(){
+			super();
+		}
+		
+		public ValueDefinitionType(byte[] berEncodedData, int offset, int level) throws IOException{
+			super(berEncodedData, offset, level);
+		}
+		
+		public Unsigned16 getClassId(){
+			return (Unsigned16) getDataType(ITEM_CLASS_ID);
+		}
+		
+		public OctetString getLogicalName(){
+			return (OctetString) getDataType(ITEM_LOGICAL_NAME);
+		}
+		
+		public Integer8 getAttributeIndex(){
+			return (Integer8) getDataType(ITEM_ATTRIBUTE_INDEX);
+		}
+	}
+	
+	public class EmergencyProfile extends Structure{
+		
+		static private final int ITEM_EMERGENCY_PROFILE_ID = 0;
+		static private final int ITEM_EMERGENCY_ACTIVATION_TIME = 1;
+		static private final int ITEM_EMERGENCY_DURATION = 2;
+		
+		public EmergencyProfile(){
+			super();
+		}
+		
+		public EmergencyProfile(byte[] berEncodedData, int offset, int level) throws IOException{
+			super(berEncodedData, offset, level);
+		}
+		
+		public Unsigned16 getEmergencyProfileId(){
+			return (Unsigned16) getDataType(ITEM_EMERGENCY_PROFILE_ID);
+		}
+		
+		public OctetString getEmergencyActivationTime(){
+			return (OctetString) getDataType(ITEM_EMERGENCY_ACTIVATION_TIME);
+		}
+		
+		public Unsigned32 getEmergencyDuration(){
+			return (Unsigned32) getDataType(ITEM_EMERGENCY_DURATION);
+		}
+	}
+	
+	public class ActionType extends Structure{
+		
+		static private final int ITEM_ACTION_OVER_THRESHOLD = 0;
+		static private final int ITEM_ACTION_UNDER_THRESHOLD = 1;
+		
+		public ActionType(){
+			super();
+		}
+		
+		public ActionType(ActionItem actionOverThreshold, ActionItem actionUnderThreshold){
+			super();
+			addDataType(actionOverThreshold);
+			addDataType(actionUnderThreshold);
+		}
+		
+		public ActionType(byte[] berEncodedData, int offset, int level) throws IOException{
+			super(berEncodedData, offset, level);
+		}
+		
+		public ActionItem getActionOverThreshold() throws IOException{
+			return  new ActionItem(getDataType(ITEM_ACTION_OVER_THRESHOLD).getBEREncodedByteArray(), 0, 0);
+		}
+		
+		public ActionItem getActionUnderThreshold() throws IOException{
+			return new ActionItem(getDataType(ITEM_ACTION_UNDER_THRESHOLD).getBEREncodedByteArray(), 0, 0);
+		}
+		
+	}
+	
+	public class ActionItem extends Structure{
+		
+		static private final int ITEM_LOGICAL_NAME = 0;
+		static private final int ITEM_SELECTOR = 1;
+		
+		public ActionItem(){
+			super();
+		}
+		
+		public ActionItem(OctetString scriptLogicalName, Unsigned16 scriptSelector){
+			super();
+			addDataType(scriptLogicalName);
+			addDataType(scriptSelector);
+		}
+		
+		public ActionItem(byte[] berEncodedData, int offset, int level) throws IOException{
+			super(berEncodedData, offset, level);
+		}
+		
+		public OctetString getScriptLogicalName(){
+			return (OctetString) getDataType(ITEM_LOGICAL_NAME);
+		}
+		
+		public Unsigned16 getScriptSelector(){
+			return (Unsigned16) getDataType(ITEM_SELECTOR);
+		}
+		
+	}
+
+	public static void main(String args[]){
+		try {
+			Limiter limiter = new Limiter(null ,null);
+			byte[] berEncodedData = new byte[]{2, 2, 2, 2, 9, 6, 1, 2, 3, 4, 5 ,6, 18, 0, 1, 2, 2, 9, 6, 6, 5, 4, 3, 2, 1, 18, 0, 2};
+			ActionType actionType = limiter.new ActionType(berEncodedData, 0, 0);
+			System.out.println(actionType);
+			System.out.println("ActionItem Over: " + actionType.getActionOverThreshold());
+			System.out.println("ActionItem Under: " + actionType.getActionUnderThreshold());
+			
+			System.out.println("ActionItem Over logicalName: " + actionType.getActionOverThreshold().getScriptLogicalName());
+			System.out.println("ActionItem Over selector: " + actionType.getActionOverThreshold().getScriptSelector());
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("To bad, try again.");
+		}
+	}
+	
 }
