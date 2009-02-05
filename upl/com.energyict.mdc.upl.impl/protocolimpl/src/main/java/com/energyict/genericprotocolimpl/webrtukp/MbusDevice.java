0c104668cc5f05a178e8122ffff09668a0b791fe
Govanni Naessens <Govanni.Naessens@elster.com>
2009-02-05 15:13:19 +0000

    Some import cleaning
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@30865 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/webrtukp/MbusDevice.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/webrtukp/MbusDevice.java
@@ -55,6 +55,7 @@ import com.energyict.protocolimpl.base.ProtocolChannelMap;
  * GNA |27012009| Instead of using the nodeAddress as channelnumber we search for the channelnumber by looking at the mbusSerialNumbers
  * GNA |28012009| Added the connect/disconnect messages. There is an option to enter an activationDate but there is no Object description for the
  * 					Mbus disconnect controller yet ...
+ * GNA |04022009| Mbus connect/disconnect can be applied with a scheduler. We use 0.x.24.6.0.255 as the ControlScheduler and 0.x.24.7.0.255 as ScriptTable
  */
 
 public class MbusDevice implements GenericProtocol, Messaging{
