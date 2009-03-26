3bc1249048c190ab56c84f01ec092a7dd6f38039
maven <maven@energyict.com>
2009-03-26 08:30:20 +0000

    Added new Schneider NSX modbus protocol
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@32077 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/schneider/compactnsx/CompactNSX.java
@@ -0,0 +1,158 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.modbus.schneider.compactnsx;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import com.energyict.obis.ObisCode;
+import com.energyict.protocol.InvalidPropertyException;
+import com.energyict.protocol.MissingPropertyException;
+import com.energyict.protocol.NoSuchRegisterException;
+import com.energyict.protocol.RegisterValue;
+import com.energyict.protocol.discover.DiscoverResult;
+import com.energyict.protocol.discover.DiscoverTools;
+import com.energyict.protocolimpl.modbus.core.Modbus;
+import com.energyict.protocolimpl.modbus.core.ModbusException;
+
+/**
+ * @author gna
+ *
+ */
+public class CompactNSX extends Modbus {
+
+	/**
+	 * 
+	 */
+	public CompactNSX() {
+	}
+
+	protected void doTheConnect() throws IOException {
+	}
+
+	protected void doTheDisConnect() throws IOException {
+	}
+
+	protected List doTheGetOptionalKeys() {
+        List result = new ArrayList();
+        return result;
+	}
+
+	protected void doTheValidateProperties(Properties properties)
+			throws MissingPropertyException, InvalidPropertyException {
+		 setInfoTypeInterframeTimeout(Integer.parseInt(properties.getProperty("InterframeTimeout", "25").trim()));
+		 setInfoTypePhysicalLayer(1);
+	}
+
+	protected void initRegisterFactory() {
+		setRegisterFactory(new RegisterFactory(this));
+	}
+
+	public DiscoverResult discover(DiscoverTools discoverTools) {
+		return null;
+	}
+	
+    public String getProtocolVersion() {
+        return "$Date$";
+    }
+    
+    public String getFirmwareVersion() throws IOException {
+    	return getRegisterFactory().getFunctionCodeFactory().getMandatoryReadDeviceIdentification().toString();
+    }
+    
+    public void setTime() throws IOException {
+    	byte[] time = perpareCurrentTime();
+    	byte[] regValues = prepareCommand(769, 18, 768, "0", time);
+    	getRegisterFactory().findRegister("Buffer").getWriteMultipleRegisters(regValues);
+    }
+
+	public Date getTime() throws IOException{
+    	byte[] regValues = prepareCommand(768, 10, 768, "0", null);
+    	getRegisterFactory().findRegister("Buffer").getWriteMultipleRegisters(regValues);
+    	long timeout = System.currentTimeMillis() + 3000;
+    	while(true){
+    		int status = (Integer)getRegisterFactory().findRegister("CommandStatus").objectValueWithParser("IntegerParser");
+    		if(status != 3){
+    			if(status == 0){
+    				break;
+    			} else {
+    				throw new IOException("Could not read time, errorCode: " + status);
+    			}
+    		}
+    	}
+    	return getRegisterFactory().findRegister("Date").dateValue();
+    }
+	
+    /*******************************************************************************************
+    R e g i s t e r P r o t o c o l  i n t e r f a c e 
+    *******************************************************************************************/
+   public RegisterValue readRegister(ObisCode obisCode) throws IOException {
+       try {
+           return new RegisterValue(obisCode,getRegisterFactory().findRegister(obisCode).quantityValue());
+       }
+       catch(ModbusException e) {
+           if ((e.getExceptionCode()==0x02) && (e.getFunctionErrorCode()==0x83))
+               throw new NoSuchRegisterException("ObisCode "+obisCode.toString()+" is not supported!");
+           else
+               throw e;
+       }
+   }
+    
+	/**
+	 * 
+	 * @param commandCode - Execution code (ex: 768 - readTime; 769 - setTime)
+	 * @param numbOfPars - Number of parameters to be set
+	 * @param destination - Destination
+	 * @param passWord - Password, contains 4 chars, default use 0000
+	 * @param data - data to be written if any
+	 * @return a composed byteArray
+	 */
+    private byte[] prepareCommand(int commandCode, int numbOfPars, int destination, String passWord, byte[] data){
+    	byte[] result = new byte[40];
+    	result[0] = (byte) ((commandCode>>8)&0xFF);
+    	result[1] = (byte) (commandCode&0xFF);
+    	result[2] = (byte) ((numbOfPars>>8)&0xFF);
+    	result[3] = (byte) (numbOfPars&0xFF);
+    	result[4] = (byte) ((destination>>8)&0xFF);
+    	result[5] = (byte) (destination&0xFF);
+    	result[6] = 0; result[7] = 0;
+    	if(passWord.equalsIgnoreCase("")){
+    		passWord = "0";
+    	}
+    	int pass = Integer.parseInt(passWord);
+    	result[8] = (byte)((pass>>24)&0xFF);
+    	result[9] = (byte)((pass>>16)&0xFF);
+    	result[10] = (byte)((pass>>8)&0xFF);
+    	result[11] = (byte)(pass&0xFF);
+    	if(data != null){
+    		System.arraycopy(data, 0, result, 12, data.length);
+    	}
+    	result[34] = (byte) 0x1F;
+    	result[35] = (byte) 0x53;
+    	result[36] = (byte) 0x1F;
+    	result[37] = (byte) 0x54;
+    	result[38] = (byte) 0x1F;
+    	result[39] = (byte) 0x55;
+    	return result;
+    }
+
+    private byte[] perpareCurrentTime() {
+    	byte[] time = new byte[8];
+    	Calendar cal = Calendar.getInstance(gettimeZone());
+    	time[0] = (byte) (cal.get(Calendar.MONTH) +1);
+    	time[1] = (byte) (cal.get(Calendar.DAY_OF_MONTH));
+    	time[2] = (byte) (cal.get(Calendar.YEAR)-2000);
+    	time[3] = (byte) (cal.get(Calendar.HOUR_OF_DAY));
+    	time[4] = (byte) (cal.get(Calendar.MINUTE));
+    	time[5] = (byte) (cal.get(Calendar.SECOND));
+    	time[6] = (byte) ((cal.get(Calendar.MILLISECOND)>>8)&0xFF);
+    	time[7] = (byte) ((cal.get(Calendar.MILLISECOND))&0xFF);
+    	return time;
+	}
+}
---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/schneider/compactnsx/RegisterFactory.java
@@ -0,0 +1,169 @@
+/**
+ * 
+ */
+package com.energyict.protocolimpl.modbus.schneider.compactnsx;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import com.energyict.cbo.Unit;
+import com.energyict.obis.ObisCode;
+import com.energyict.protocol.ProtocolUtils;
+import com.energyict.protocolimpl.modbus.core.AbstractRegister;
+import com.energyict.protocolimpl.modbus.core.AbstractRegisterFactory;
+import com.energyict.protocolimpl.modbus.core.HoldingRegister;
+import com.energyict.protocolimpl.modbus.core.Modbus;
+import com.energyict.protocolimpl.modbus.core.ModbusException;
+import com.energyict.protocolimpl.modbus.core.Parser;
+
+/**
+ * @author gna
+ *
+ */
+public class RegisterFactory extends AbstractRegisterFactory {
+
+	/**
+	 * @param modBus
+	 */
+	public RegisterFactory(Modbus modBus) {
+		super(modBus);
+	}
+
+	protected void init() {
+
+		setZeroBased(true);	// means that reg2read = reg-1
+		
+		// Voltage registers
+        getRegisters().add(new HoldingRegister(1000,1,ObisCode.fromString("1.1.32.7.0.255"))); // RMS Phase-to-Phase voltage V12
+        getRegisters().add(new HoldingRegister(1001,1,ObisCode.fromString("1.1.52.7.0.255"))); // RMS Phase-to-Phase voltage V23
+        getRegisters().add(new HoldingRegister(1002,1,ObisCode.fromString("1.1.72.7.0.255"))); // RMS Phase-to-Phase voltage V31
+        getRegisters().add(new HoldingRegister(1003,1,ObisCode.fromString("1.1.132.7.0.255"))); // RMS Phase-to-Neutral voltage V1N
+        getRegisters().add(new HoldingRegister(1004,1,ObisCode.fromString("1.1.152.7.0.255"))); // RMS Phase-to-Neutral voltage V2N
+        getRegisters().add(new HoldingRegister(1005,1,ObisCode.fromString("1.1.172.7.0.255"))); // RMS Phase-to-Neutral voltage V3N
+        getRegisters().add(new HoldingRegister(1006,1,ObisCode.fromString("1.1.12.4.0.255"))); // Total average Line-to-Line
+        getRegisters().add(new HoldingRegister(1007,1,ObisCode.fromString("1.1.112.4.0.255"))); // Total average Line-to-Neutral
+        getRegisters().add(new HoldingRegister(1145,1,ObisCode.fromString("1.1.12.6.0.255"))); // Maximum of V12, V23, V31
+        getRegisters().add(new HoldingRegister(1146,1,ObisCode.fromString("1.1.12.3.0.255"))); // Minimum of V12, V23, V31
+        
+        // Current registers
+        getRegisters().add(new HoldingRegister(1016,1,ObisCode.fromString("1.1.31.7.0.255"))); // RMS current on phase 1:L1
+        getRegisters().add(new HoldingRegister(1017,1,ObisCode.fromString("1.1.51.7.0.255"))); // RMS current on phase 2:L2
+        getRegisters().add(new HoldingRegister(1018,1,ObisCode.fromString("1.1.71.7.0.255"))); // RMS current on phase 3:L3
+        getRegisters().add(new HoldingRegister(1019,1,ObisCode.fromString("1.1.11.6.0.255"))); // Maximum I1, I2, I3
+        getRegisters().add(new HoldingRegister(1026,1,ObisCode.fromString("1.1.11.3.0.255"))); // Minimum I1, I2, I3
+        getRegisters().add(new HoldingRegister(1027,1,ObisCode.fromString("1.1.11.4.0.255"))); // Total average I1, I2, I3
+        
+        // Active power
+        getRegisters().add(new HoldingRegister(1034,1,ObisCode.fromString("1.1.21.7.0.255"),Unit.get("kW")).setParser("PowerSign")); // Active power Phase1
+        getRegisters().add(new HoldingRegister(1035,1,ObisCode.fromString("1.1.41.7.0.255"),Unit.get("kW")).setParser("PowerSign")); // Active power Phase2
+        getRegisters().add(new HoldingRegister(1036,1,ObisCode.fromString("1.1.61.7.0.255"),Unit.get("kW")).setParser("PowerSign")); // Active power Phase3
+        getRegisters().add(new HoldingRegister(1037,1,ObisCode.fromString("1.1.1.7.0.255"),Unit.get("kW")).setParser("PowerSign")); // Active power Total
+        
+        // Reactive power
+        getRegisters().add(new HoldingRegister(1038,1,ObisCode.fromString("1.1.23.7.0.255"),Unit.get("kvar")).setParser("PowerSign")); // Reactive power Phase1
+        getRegisters().add(new HoldingRegister(1039,1,ObisCode.fromString("1.1.43.7.0.255"),Unit.get("kvar")).setParser("PowerSign")); // Reactive power Phase2
+        getRegisters().add(new HoldingRegister(1040,1,ObisCode.fromString("1.1.63.7.0.255"),Unit.get("kvar")).setParser("PowerSign")); // Reactive power Phase3
+        getRegisters().add(new HoldingRegister(1041,1,ObisCode.fromString("1.1.3.7.0.255"),Unit.get("kvar")).setParser("PowerSign")); // Reactive power Total 
+        
+        // Apparent power
+        getRegisters().add(new HoldingRegister(1045,1,ObisCode.fromString("1.1.9.7.0.255"),Unit.get("VA"))); // Apparent power Total
+        
+        // Power Factors
+        getRegisters().add(new HoldingRegister(1046,1,ObisCode.fromString("1.1.33.7.0.255"),Unit.get("")).setParser("PowerFactorSign")); // powerfactor phase A
+        getRegisters().add(new HoldingRegister(1047,1,ObisCode.fromString("1.1.53.7.0.255"),Unit.get("")).setParser("PowerFactorSign")); // powerfactor phase B
+        getRegisters().add(new HoldingRegister(1048,1,ObisCode.fromString("1.1.73.7.0.255"),Unit.get("")).setParser("PowerFactorSign")); // powerfactor phase C
+        getRegisters().add(new HoldingRegister(1049,1,ObisCode.fromString("1.1.13.7.0.255"),Unit.get("")).setParser("PowerFactorSign")); // powerfactor phase B
+        
+        // Signers
+        getRegisters().add(new HoldingRegister(3316, 1, "PowerSign"));
+        getRegisters().add(new HoldingRegister(3318, 1, "PowerFactorSign"));
+        
+        // Clock
+        getRegisters().add(new HoldingRegister(8023,4, "Date"));
+        
+        // Buffer
+        getRegisters().add(new HoldingRegister(8000,20, "Buffer"));
+        getRegisters().add(new HoldingRegister(8021,1,"CommandStatus").setParser("IntegerParser"));
+        
+        
+	}
+	
+    protected void initParsers() {
+    	
+        // BigDecimal parser
+        getParserFactory().addBigDecimalParser(new Parser() {
+            public Object val(int[] values, AbstractRegister register) throws IOException {
+            	long val = 0;
+            	for(int i = 0; i < values.length; i++){
+            		val += values[i]<<(16*i);
+            	}
+                BigDecimal bd = new BigDecimal(""+val);
+                return bd;
+//                return bd.multiply(getModBus().getRegisterMultiplier(register.getReg()));
+            }
+        });
+    	
+    	getParserFactory().addParser("IntegerParser", new Parser() {
+    		public Object val(int[] values, AbstractRegister register) throws IOException {
+    			return values[0];
+    		}
+    	});
+    	
+        getParserFactory().addParser("PowerSign",new Parser() {
+            public Object val(int[] values, AbstractRegister register) throws IOException {
+            	long val = 0;
+            	for(int i = 0; i < values.length; i++){
+            		val += values[i]<<(16*i);
+            	}
+            	if((val != 0x8000) && (val != 0x80000000)){
+            		BigDecimal bd;
+            		if((Integer)getModBus().getRegisterFactory().findRegister("PowerSign").objectValueWithParser("IntegerParser") == 1){
+            			bd = new BigDecimal("-" + val);
+            		} else {
+            			bd = new BigDecimal("+" + val);
+            		}
+                    return bd.movePointRight(1);
+            	} else {
+            		throw new ModbusException("Not supported when Systemtype is 31 or 40",(short) 0,0x83,0x02);
+            	}
+            }
+        });
+        
+        getParserFactory().addParser("PowerFactorSign",new Parser() {
+            public Object val(int[] values, AbstractRegister register) throws IOException {
+            	long val = 0;
+            	for(int i = 0; i < values.length; i++){
+            		val += values[i]<<(16*i);
+            	}
+            	if((val != 0x8000) && (val != 0x80000000)){
+            		BigDecimal bd;
+            		if((Integer)getModBus().getRegisterFactory().findRegister("PowerFactorSign").objectValueWithParser("IntegerParser") == 1){
+            			bd = new BigDecimal("-" + val);
+            		} else {
+            			bd = new BigDecimal("+" + val);
+            		}
+                    return bd.movePointRight(2);
+            	} else {
+            		throw new ModbusException("Not supported when Systemtype is 31 or 40",(short) 0,0x83,0x02);
+            	}
+            }
+        });
+        
+        getParserFactory().addDateParser(new Parser() {
+            public Object val(int[] values, AbstractRegister register) {
+                Calendar cal = ProtocolUtils.getCleanCalendar(getModBus().getTimeZone());
+                cal.set(Calendar.MONTH, ((values[0]>>8)&0xFF) - 1);
+                cal.set(Calendar.DAY_OF_MONTH, (values[0])&0xFF);
+                cal.set(Calendar.YEAR, 2000 + ((values[1]>>8)&0xFF));
+                cal.set(Calendar.HOUR_OF_DAY, (values[1]&0xFF));
+                cal.set(Calendar.MINUTE, ((values[2]>>8)&0xFF));
+                cal.set(Calendar.SECOND, (values[2])&0xFF);
+                cal.set(Calendar.MILLISECOND, values[3]);
+                return cal.getTime();
+            }
+        });
+        
+    } 
+
+}
