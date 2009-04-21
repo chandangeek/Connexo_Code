9ad1b0c3835f3d6c5aeb43803a223d005cd04cdb
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-04-21 08:19:01 +0000

    New protocol: Kamstrup Unigas300 IEC1107 protocol
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@32640 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/kamstrup/unigas300/ObisCodeMapper.java
@@ -0,0 +1,72 @@
+/*
+ * ObisCodeMapper.java
+ *
+ * Created on 15 september 2006, 13:30
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package com.energyict.protocolimpl.iec1107.kamstrup.unigas300;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import com.energyict.cbo.Quantity;
+import com.energyict.cbo.Unit;
+import com.energyict.obis.ObisCode;
+import com.energyict.protocol.NoSuchRegisterException;
+import com.energyict.protocol.RegisterInfo;
+import com.energyict.protocol.RegisterValue;
+
+/**
+ *
+ * @author Koen
+ */
+public class ObisCodeMapper {
+    
+    Unigas300 unigas300;
+    
+    /** Creates a new instance of ObisCodeMapper */
+    public ObisCodeMapper(Unigas300 unigas300) {
+        this.unigas300=unigas300;
+    }
+    
+    
+    public String getRegisterInfo() {
+        StringBuffer strBuff = new StringBuffer();
+        RegisterMappingFactory rmf = new RegisterMappingFactory();
+        Iterator it = rmf.getRegisterMappings().iterator();
+        while(it.hasNext()) {
+            RegisterMapping rm = (RegisterMapping)it.next();
+            strBuff.append(rm.getObisCode()+", "+rm.getDescription()+", "+rm.getRegisterCode()+"\n");
+
+        }
+        return strBuff.toString();
+    }
+    
+    static public RegisterInfo getRegisterInfo(ObisCode obisCode) throws IOException {
+        RegisterMappingFactory rmf = new RegisterMappingFactory();
+        RegisterMapping rm = rmf.findRegisterMapping(obisCode);
+        return new RegisterInfo(rm.getDescription()+", "+rm.getRegisterCode());
+    }
+    
+    public RegisterValue getRegisterValue(ObisCode obisCode) throws IOException {
+        RegisterMappingFactory rmf = new RegisterMappingFactory();
+        String registerCode = rmf.findRegisterCode(obisCode);
+        Object o = unigas300.getKamstrupRegistry().getRegister(registerCode);
+        if (o instanceof Quantity) {
+            return new RegisterValue(obisCode,(Quantity)o);
+        }
+        else if (o instanceof Integer) {
+            return new RegisterValue(obisCode,new Quantity((Integer)o,Unit.get("")));
+        }
+        else if (o instanceof String) {
+            return new RegisterValue(obisCode,(String)o);
+        }
+        
+        throw new NoSuchRegisterException("ObisCode "+obisCode.toString()+" is not supported!");
+    }    
+    
+}
---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/kamstrup/unigas300/Unigas300Registry.java
@@ -0,0 +1,53 @@
+/*
+ * KamstrupRegister.java
+ *
+ * Created on 16 juni 2003, 16:35
+ */
+
+package com.energyict.protocolimpl.iec1107.kamstrup.unigas300;
+
+import com.energyict.protocolimpl.iec1107.ProtocolLink;
+import com.energyict.protocolimpl.iec1107.vdew.AbstractVDEWRegistry;
+import com.energyict.protocolimpl.iec1107.vdew.VDEWRegister;
+import com.energyict.protocolimpl.iec1107.vdew.VDEWRegisterDataParse;
+
+/**
+ *
+ * @author  Koen
+ */
+public class Unigas300Registry extends AbstractVDEWRegistry {
+    
+    /** Creates a new instance of KamstrupRegister */
+    public Unigas300Registry(ProtocolLink protocolLink) {
+        super(null,protocolLink);
+    }
+    
+    protected void initRegisters() {
+        registers.put("Converter Unconverted volume (Ch.1) Vm1", new VDEWRegister("1:13.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Converter Unconverted volume (Ch.2) Vm2", new VDEWRegister("2:13.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("converter error corrected volume (Ch.1) Vc", new VDEWRegister("13.1.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Converter converted volume (Ch.1) Vb", new VDEWRegister("23.2.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Measured, disturbed volume, Ve", new VDEWRegister("1:12.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Measured Temperature", new VDEWRegister("0:41.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Measured absolute pressure", new VDEWRegister("0:42.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Conversion factor", new VDEWRegister("0:52.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Correction factor", new VDEWRegister("0:51.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Compressibility factor", new VDEWRegister("0:53.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Actual normalised flow 5 minutes avg.", new VDEWRegister("1:43.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("Actual normalised flow 60 minutes avg.", new VDEWRegister("2:43.0.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        
+        registers.put("Unigas Error code", new VDEWRegister("97.97.0",VDEWRegisterDataParse.VDEW_QUANTITY,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        
+        registers.put("Time", new VDEWRegister("0.9.1",VDEWRegisterDataParse.VDEW_TIMESTRING,0, -1,null,VDEWRegister.WRITEABLE,VDEWRegister.NOT_CACHED));
+        registers.put("Date", new VDEWRegister("0.9.2",VDEWRegisterDataParse.VDEW_DATESTRING,0, -1,null,VDEWRegister.WRITEABLE,VDEWRegister.NOT_CACHED));
+        registers.put("TimeDate", new VDEWRegister("0.9.1 0.9.2",VDEWRegisterDataParse.VDEW_TIMEDATE,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.NOT_CACHED));
+        registers.put("1107 device address", new VDEWRegister("C.90.1",VDEWRegisterDataParse.VDEW_STRING,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("UNIGAS software revision number", new VDEWRegister("C.90.2",VDEWRegisterDataParse.VDEW_STRING,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        registers.put("CI software revision number", new VDEWRegister("C.90.3",VDEWRegisterDataParse.VDEW_STRING,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+        
+        registers.put("actual status bits", new VDEWRegister("C.5",VDEWRegisterDataParse.VDEW_STRING,0, -1,null,VDEWRegister.NOT_WRITEABLE,VDEWRegister.CACHED));
+    }
+    
+
+    
+}
