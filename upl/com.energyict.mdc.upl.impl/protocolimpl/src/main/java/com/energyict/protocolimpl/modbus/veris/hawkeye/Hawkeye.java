fce6ec827b6b74fda592a831a753735ec65b36bf
Koen Vanderschaeve <k.vanderschaeve@energyict.com>
2009-06-23 16:27:45 +0000

    changes & fixes
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@34257 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/veris/hawkeye/Hawkeye.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/veris/hawkeye/Hawkeye.java
@@ -148,7 +148,7 @@ while(count++<1) {
             Properties properties = new Properties();
             properties.setProperty("ProfileInterval", "60");
             //properties.setProperty(MeterProtocol.NODEID,"0");
-            properties.setProperty(MeterProtocol.ADDRESS,"2");
+            properties.setProperty(MeterProtocol.ADDRESS,"1");
             properties.setProperty("HalfDuplex", "-1");
             
 //            int ift;
