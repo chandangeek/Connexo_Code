e4e62695b1648208d57654d474238b9499dd0635
Govanni Naessens <Govanni.Naessens@elster.com>
2009-07-28 11:36:59 +0000

    Moved the failing Test to lower layer so extra(read difficult) setup is skipped.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@35346 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/test/java/com/energyict/dlms/aso/SecurityContextTest.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/test/java/com/energyict/dlms/aso/SecurityContextTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import org.junit.Test;
 
 import com.energyict.dlms.DLMSUtils;
+import com.energyict.dlms.mocks.MockDLMSConnection;
 import com.energyict.dlms.mocks.MockSecurityProvider;
 
 
@@ -63,4 +64,41 @@ public class SecurityContextTest {
 		}
 	}
 
+	@Test
+	public void logicalDataTransportEncryptionTest(){
+		try {
+			String globalKey = "000102030405060708090A0B0C0D0E0F";
+			String authenticationKey = "D0D1D2D3D4D5D6D7D8D9DADBDCDDDEDF";
+			String unSecuredRequest = "C0010000080000010000FF0200";							// first three bytes are redundant
+			String testDecryptA = "1E1001234567C0010000080000010000FF020006725D910F9221D263877516";
+			String testDecryptE = "122001234567411312FF935A47566827C467BC";
+			String testDecryptAE = "1E3001234567411312FF935A47566827C467BC7D825C3BE4A77C3FCC056B6B";
+			
+			MockSecurityProvider msp = new MockSecurityProvider();
+			msp.setAuthenticationKey(DLMSUtils.hexStringToByteArray(authenticationKey));
+			msp.setGlobalkey(DLMSUtils.hexStringToByteArray(globalKey));
+			
+			// Only authentication
+			SecurityContext sc = new SecurityContext(SecurityContext.SECURITYPOLICY_AUTHENTICATION, 0,0,"MMM0000012345678", msp);
+			sc.setFrameCounter(19088743); 		// this is '0x01234567'
+			byte[] cipheredResponse = sc.dataTransportEncryption(DLMSUtils.hexStringToByteArray(unSecuredRequest));
+			assertArrayEquals(DLMSUtils.hexStringToByteArray(testDecryptA), cipheredResponse);
+			
+			// Only encryption
+			sc = new SecurityContext(SecurityContext.SECURITYPOLICY_ENCRYPTION, 0,0,"MMM0000012345678", msp);
+			sc.setFrameCounter(19088743); 		// this is '0x01234567'
+			cipheredResponse = sc.dataTransportEncryption(DLMSUtils.hexStringToByteArray(unSecuredRequest));
+			assertArrayEquals(DLMSUtils.hexStringToByteArray(testDecryptE), cipheredResponse);
+			
+			// Both authentication and encryption
+			sc = new SecurityContext(SecurityContext.SECURITYPOLICY_BOTH, 0,0,"MMM0000012345678", msp);
+			sc.setFrameCounter(19088743); 		// this is '0x01234567'
+			cipheredResponse = sc.dataTransportEncryption(DLMSUtils.hexStringToByteArray(unSecuredRequest));
+			assertArrayEquals(DLMSUtils.hexStringToByteArray(testDecryptAE), cipheredResponse);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 }
