00a5f92a1513c2c87b15384d8f575ed518c2da22
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-08-19 12:55:24 +0000

    Changed protocoltester obiscode dialog to remove multiple registers in one click
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@35622 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/client/CosemAttributeDescriptor.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/client/CosemAttributeDescriptor.java
@@ -6,49 +6,53 @@ import com.energyict.obis.ObisCode;
 import com.energyict.protocol.ProtocolUtils;
 
 public class CosemAttributeDescriptor {
-	
+
 	private int classId;
 	private ObisCode obis;
 	private int attributeId;
 
-	
 	public CosemAttributeDescriptor(int classId, ObisCode obis, int attributeId) {
 		this.classId = classId;
 		this.obis = obis;
 		this.attributeId = attributeId;
 	}
-	
+
 	public CosemAttributeDescriptor(byte[] data, int offset) throws IOException {
-		classId = ProtocolUtils.getInt(data, offset, 2);
-		offset+=2;
-		obis = new ObisCode(ProtocolUtils.getInt(data, offset++,1),ProtocolUtils.getInt(data, offset++,1),ProtocolUtils.getInt(data, offset++,1),ProtocolUtils.getInt(data, offset++,1),ProtocolUtils.getInt(data, offset++,1),ProtocolUtils.getInt(data, offset++,1));
-		attributeId = ProtocolUtils.getInt(data, offset++,1);
+		int off = offset;
+		this.classId = ProtocolUtils.getInt(data, off, 2);
+		off+=2;
+		this.obis = new ObisCode(ProtocolUtils.getInt(data, off++,1),ProtocolUtils.getInt(data, off++,1),ProtocolUtils.getInt(data, off++,1),ProtocolUtils.getInt(data, off++,1),ProtocolUtils.getInt(data, off++,1),ProtocolUtils.getInt(data, off++,1));
+		this.attributeId = ProtocolUtils.getInt(data, off++,1);
 	}
-	
+
 	static public int size() {
 		return 9;
 	}
-//    public static void main(String[] args) {
-//        System.out.println(com.energyict.protocolimpl.base.ToStringBuilder.genCode(new CosemAttributeDescriptor()));
-//    }
-	
-    public String toString() {
-        // Generated code by ToStringBuilder
-        StringBuffer strBuff = new StringBuffer();
-        strBuff.append("CosemAttributeDescriptor: ");
-        strBuff.append(" attributeId="+getAttributeId());
-        strBuff.append(" classId="+getClassId());
-        strBuff.append(" obis="+getObis());
-        return strBuff.toString();
-    }	
+
+	@Override
+	public String toString() {
+		// Generated code by ToStringBuilder
+		StringBuffer strBuff = new StringBuffer();
+		strBuff.append("CosemAttributeDescriptor: ");
+		strBuff.append(" attributeId=");
+		strBuff.append(getAttributeId());
+		strBuff.append(" classId=");
+		strBuff.append(getClassId());
+		strBuff.append(" obis=");
+		strBuff.append(getObis());
+		return strBuff.toString();
+	}
+
 	public int getClassId() {
-		return classId;
+		return this.classId;
 	}
+
 	public ObisCode getObis() {
-		return obis;
+		return this.obis;
 	}
+
 	public int getAttributeId() {
-		return attributeId;
+		return this.attributeId;
 	}
-	
+
 }
