9d6108e26478b15f481b7dc0b1ecacaabd4949e4
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-08-26 13:57:02 +0000

    Added comments in RtuPlusServerHalfDuplexController and AS220DisplayController, Removed unused A1440MeterclassWriter
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@35783 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/base/RtuPlusServerHalfDuplexController.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/base/RtuPlusServerHalfDuplexController.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import com.energyict.dialer.core.HalfDuplexController;
 
 /**
+ * This class is used to replace the default HalfDuplexController, received from
+ * mdw during the init. On the RTU+ Server, the RS485 driver uses inverted logic
+ * to drive the RTS pin in halfduplex mode. This could be avoided to patch all
+ * the request2Receive() and request2Send() calls to the request2ReceiveRS485()
+ * and request2SendRS485() methods. This is what this class does.
  * @author jme
  * @since 26-aug-2009
  */
@@ -12,58 +17,103 @@ public class RtuPlusServerHalfDuplexController implements HalfDuplexController {
 
 	private HalfDuplexController innerHalfDuplexController;
 
+	/**
+	 * Default constructor for the RtuPlusServerHalfDuplexController
+	 * @param controller The HalfDuplexController to patch the methods to.
+	 */
 	public RtuPlusServerHalfDuplexController(HalfDuplexController controller) {
 		this.innerHalfDuplexController = controller;
 	}
 
+	/**
+	 * Getter for the current inner halfDuplexController
+	 * @return The current inner HalfDuplexController
+	 */
 	public HalfDuplexController getInnerHalfDuplexController() {
 		return this.innerHalfDuplexController;
 	}
 
+	/**
+	 * Setter to change the inner HalfDuplexController
+	 * @param innerHalfDuplexController
+	 */
 	public void setInnerHalfDuplexController(HalfDuplexController innerHalfDuplexController) {
 		this.innerHalfDuplexController = innerHalfDuplexController;
 	}
 
+	/**
+	 * This method is patched to the request2ReceiveRS485(int nrOfBytes) method of the inner {@link HalfDuplexController}
+	 */
 	public void request2Receive(int nrOfBytes) {
 		getInnerHalfDuplexController().request2ReceiveRS485(nrOfBytes);
 	}
 
+	/**
+	 * This method is patched to the request2SendRS485() method of the inner {@link HalfDuplexController}
+	 */
 	public void request2Send(int nrOfBytes) {
 		getInnerHalfDuplexController().request2SendRS485();
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void request2ReceiveRS485(int nrOfBytes) {
 		getInnerHalfDuplexController().request2ReceiveRS485(nrOfBytes);
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void request2ReceiveV25(int nrOfBytes) {
 		getInnerHalfDuplexController().request2SendV25(nrOfBytes);
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void request2SendRS485() {
 		getInnerHalfDuplexController().request2SendRS485();
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void request2SendV25(int nrOfBytes) {
 		getInnerHalfDuplexController().request2SendV25(nrOfBytes);
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void setDTR(boolean dtr) throws IOException {
 		getInnerHalfDuplexController().setDTR(dtr);
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void setDelay(long halfDuplexTXDelay) {
 		getInnerHalfDuplexController().setDelay(halfDuplexTXDelay);
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public void setRTS(boolean rts) throws IOException {
 		getInnerHalfDuplexController().setRTS(rts);
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public boolean sigCD() throws IOException {
 		return getInnerHalfDuplexController().sigCD();
 	}
 
+	/**
+	 * This method is patched to the same method of the inner {@link HalfDuplexController}
+	 */
 	public boolean sigCTS() throws IOException {
 		return getInnerHalfDuplexController().sigCTS();
 	}
--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/as220/AS220DisplayController.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/as220/AS220DisplayController.java
@@ -6,26 +6,26 @@ import java.util.logging.Logger;
 import com.energyict.protocol.ProtocolUtils;
 
 /**
+ * This class sends a message onto the display of the meter. This message has
+ * the highest priority. This means that all other messages are overwritten by
+ * this message in scrollmode.
  * @author jme
  * @since 25-aug-2009
  */
 public class AS220DisplayController {
 
-	private AS220 as220 = null;
-
 	private static final int OBIS_FIELD_LENGTH = 3;
 	private static final int VALUE_FIELD_LENGTH = 7;
+	private static final String CLEAR_MESSAGE = "000000000000000000000000000000000000";
 
+	private AS220 as220 = null;
+
+	/**
+	 * Constructor for the AS220DisplayController
+	 * @param as220Protocol The current AS220 protocol
+	 */
 	public AS220DisplayController(AS220 as220Protocol) {
 		this.as220 = as220Protocol;
-	}
-
-	private AS220 getAs220() {
-		return this.as220;
-	}
-
-	private Logger getLogger() {
-		return getAs220().getLogger();
 	}
 
 	/**
@@ -46,9 +46,14 @@ public class AS220DisplayController {
 	 */
 	public void clearDisplay() throws IOException {
 		getLogger().fine("Clearing message from device.");
-		getAs220().getAS220Registry().setRegister(AS220Registry.DISPLAY_MESSAGE_REGISTER, "00");
+		getAs220().getAS220Registry().setRegister(AS220Registry.DISPLAY_MESSAGE_REGISTER, CLEAR_MESSAGE);
 	}
 
+	/**
+	 * Get the ascii value in hex from a given string
+	 * @param displayMessage
+	 * @return the hex value of the string ascii code
+	 */
 	private static String toHexString(String displayMessage) {
 		String message = displayMessage != null ? displayMessage : "";
 		String output = "";
@@ -59,12 +64,25 @@ public class AS220DisplayController {
 		return output;
 	}
 
+	/**
+	 * Create a valid packet with a message to display on the LCD ready to write
+	 * to the device
+	 * @param displayMessage
+	 * @return The package, ready to send to the device
+	 */
 	private String getPacketFromString(String displayMessage) {
 		String message = formatMessageLength(cleanMessageValue(displayMessage));
 		message = toHexString(message);
 		return "010000" + message.substring(14, 20) + "000000" + message.substring(0, 14) + "0000";
 	}
 
+	/**
+	 * Changes the display message to a fixed length of 10. When the display
+	 * message is to long, the message is truncated at 10 characters. When the
+	 * message is to short, spaces are appended at the end of the message
+	 * @param displayMessage
+	 * @return The 10 chars long message
+	 */
 	private String formatMessageLength(String displayMessage) {
 		String message = displayMessage != null ? displayMessage : "";
 		if (message.length() > (OBIS_FIELD_LENGTH + VALUE_FIELD_LENGTH)) {
@@ -77,6 +95,11 @@ public class AS220DisplayController {
 		return message;
 	}
 
+	/**
+	 * Remove CR, LF and TABS from a string
+	 * @param attributeValue
+	 * @return The cleaned value of the string
+	 */
 	private String cleanMessageValue(String attributeValue) {
 		final char TAB = 0x09;
 		final char LF = 0x0A;
@@ -85,16 +108,38 @@ public class AS220DisplayController {
 		boolean isValidChar = true;
 		String returnValue = "";
 
-		for (int i = 0; i < attributeValue.length(); i ++) {
+		for (int i = 0; i < attributeValue.length(); i++) {
 			isValidChar = true;
-			if (attributeValue.charAt(i) == CR) { isValidChar = false; }
-			if (attributeValue.charAt(i) == LF) { isValidChar = false; }
-			if (attributeValue.charAt(i) == TAB) { isValidChar = false; }
+			if (attributeValue.charAt(i) == CR) {
+				isValidChar = false;
+			}
+			if (attributeValue.charAt(i) == LF) {
+				isValidChar = false;
+			}
+			if (attributeValue.charAt(i) == TAB) {
+				isValidChar = false;
+			}
 			if (isValidChar) {
 				returnValue += attributeValue.charAt(i);
 			}
 		}
 		return returnValue;
+	}
+
+	/**
+	 * Getter for the current AS220 protocol
+	 * @return The current AS220 protocol
+	 */
+	private AS220 getAs220() {
+		return this.as220;
+	}
+
+	/**
+	 * Getter for the logger of the AS220 protocol
+	 * @return The logger of the AS220 protocol
+	 */
+	private Logger getLogger() {
+		return getAs220().getLogger();
 	}
 
 }
