86d639ac99a8067ff54ee0c528fff777c225317b
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-09-03 08:55:57 +0000

    PPM Encryption
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@35927 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/ppm/Encryption.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/ppm/Encryption.java
@@ -4,64 +4,64 @@ import com.energyict.protocolimpl.base.Encryptor;
 
 public class Encryption implements Encryptor {
 
-	static char[] C245hgj = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-			'A', 'B', 'C', 'D', 'E', 'F'};
+	private static char[] C245HGJ = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
 	public Encryption() {
 	}
 
-	public int CAIMETHOD(String CA27, String CA28, char[] CA29) {
-		char[] CfG7821 = new char[9];
-		char[] K8057hwf = new char[9];
-		char[] LI6348 = new char[17];
-		char CFG873, CFG837;
+	private int caiMethod(String ca27, String ca28, char[] ca29) {
+		char[] cfG7821 = new char[9];
+		char[] k8057hwf = new char[9];
+		char[] li6348 = new char[17];
+		char cfg873;
+		char cfg837;
+
 		for (int i = 0; i < 8; i++) {
-			CfG7821[i] = CA27.charAt(i); 
+			cfG7821[i] = ca27.charAt(i);
 		}
-		CfG7821[8] = '\0';
+		cfG7821[8] = '\0';
 		for (int i = 0; i < 16; i += 2) {
-			CFG873 = CAIMETHODB(CA28.charAt(i));
-			CFG837 = CAIMETHODB(CA28.charAt(i + 1));
-			int iVal = (((int) CFG873) * 16) + ((int) CFG837);
-			K8057hwf[i / 2] = (char) iVal;
+			cfg873 = caiMethodB(ca28.charAt(i));
+			cfg837 = caiMethodB(ca28.charAt(i + 1));
+			int iVal = ((cfg873) * 16) + (cfg837);
+			k8057hwf[i / 2] = (char) iVal;
 		}
-		K8057hwf[8] = '\0';
+		k8057hwf[8] = '\0';
 		for (int i = 0; i < 8; i++) {
-			LI6348[i] += K8057hwf[i] ^ CfG7821[i];
+			li6348[i] += k8057hwf[i] ^ cfG7821[i];
 		}
-		LI6348[8] = '\0';
+		li6348[8] = '\0';
 		for (int i = 0; i < 8; i++) {
-			int C396uia;
-			C396uia = LI6348[i] + LI6348[i + 7];
-			C396uia = C396uia % 256;
-			LI6348[i + 8] = (char) C396uia;
-			CA29[i * 2] = C245hgj[C396uia / 16];
-			CA29[(i * 2) + 1] = C245hgj[C396uia % 16];
+			int c396uia;
+			c396uia = li6348[i] + li6348[i + 7];
+			c396uia = c396uia % 256;
+			li6348[i + 8] = (char) c396uia;
+			ca29[i * 2] = C245HGJ[c396uia / 16];
+			ca29[(i * 2) + 1] = C245HGJ[c396uia % 16];
 		}
-		//CA29[16] = '\0';
 		return 1;
 	}
-	private char CAIMETHODB(char C786fim) {
-		String C612ops = String.valueOf(C786fim);
-		C612ops = C612ops.toUpperCase();
-		C786fim = C612ops.charAt(0);
-		int C871ilg = 0;
+
+	private char caiMethodB(char c786fim) {
+		String c612ops = String.valueOf(c786fim);
+		c612ops = c612ops.toUpperCase();
+		c786fim = c612ops.charAt(0);
+		int c871ilg = 0;
 		boolean f613idv = false;
 		while (!f613idv) {
-			if (C245hgj[C871ilg] == C786fim) {
+			if (C245HGJ[c871ilg] == c786fim) {
 				f613idv = true;
 			} else {
-				C871ilg++;
+				c871ilg++;
 			}
 		}
-		return (char) C871ilg;
+		return (char) c871ilg;
 	}
-	
-    public String encrypt(String passWord, String key) {
-        char [] encryptedPassword = new char[16];
-    	CAIMETHOD(passWord, key, encryptedPassword );
-    	return new String( encryptedPassword );
-    }
-    
+
+	public String encrypt(String passWord, String key) {
+		char[] encryptedPassword = new char[16];
+		caiMethod(passWord, key, encryptedPassword);
+		return new String(encryptedPassword);
+	}
 
 }
