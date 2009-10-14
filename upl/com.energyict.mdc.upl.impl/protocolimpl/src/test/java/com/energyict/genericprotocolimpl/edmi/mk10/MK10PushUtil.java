3baaadad1668af15bbf31e1a8dd63507e94369a9
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-10-14 06:53:43 +0000

    Renamed MK10PushTest.java to MK10PushUtil.java and added @Ignore
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@36804 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/genericprotocolimpl/edmi/mk10/MK10PushUtil.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/genericprotocolimpl/edmi/mk10/MK10PushUtil.java
@@ -16,14 +16,17 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+import org.junit.Ignore;
+
 import com.energyict.protocol.ProtocolUtils;
 
 /**
  * @author jme
  * 
  */
-public class MK10PushTest {
+public class MK10PushUtil {
 
+	@Ignore
 	public static void main(String[] args) {
 		String hexString = "$8F$50$FF$E3$0C$77$9B$5F$45$30$39$30$31$33$34$36$00$33$35$36$31$38$37$30$33$30$30$30$32$35$31$38$00$4D$6B$31$30$5F$53$53$43$5F$30$31$34$35$00$31$30$58$58$00$31$2E$33$36$20$00$04$02$6D$40$31$31$61$66$2C$37$64$62$62$2C$38$2C$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$00$E4$37";
 		ByteArrayOutputStream bb = new ByteArrayOutputStream();
