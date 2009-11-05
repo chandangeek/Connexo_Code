d50044d42cdce0c637220609e324c58378251086
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-11-05 11:05:16 +0000

    Unittests
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@37340 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/protocolimpl/iec1107/vdew/VDEWRegisterDataParseTest.java
@@ -0,0 +1,18 @@
+package com.energyict.protocolimpl.iec1107.vdew;
+
+import org.junit.Test;
+
+import com.energyict.protocolimpl.utils.Utilities;
+
+public class VDEWRegisterDataParseTest {
+
+	public static final byte[] DATE = Utilities.getBytesFromHexString("$01$02$03$04$05$06$07$08$09$0A$0B$0C$0D");
+
+	@Test
+	public void testParseDateYYMMDD() {
+
+		//TODO: Add unittests
+
+	}
+
+}
