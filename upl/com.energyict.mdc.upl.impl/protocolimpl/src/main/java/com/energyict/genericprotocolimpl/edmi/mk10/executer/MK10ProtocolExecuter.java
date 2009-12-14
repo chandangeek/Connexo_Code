3868aff2420ae550aa41223bf7bc7e7ea8ed7db6
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-12-14 15:19:46 +0000

    Added MK10 stub for testing purposes.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@38247 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/edmi/mk10/executer/MK10ProtocolExecuter.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/edmi/mk10/executer/MK10ProtocolExecuter.java
@@ -1,8 +1,8 @@
 /**
  * MK10ProtocolExecuter.java
- * 
+ *
  * Created on 16-jan-2009, 09:16:11 by jme
- * 
+ *
  */
 package com.energyict.genericprotocolimpl.edmi.mk10.executer;
 
@@ -39,25 +39,24 @@ import com.energyict.protocolimpl.edmi.mk10.MK10;
  */
 public class MK10ProtocolExecuter {
 
-	private static final int DEBUG 					= 0;
+	private static final int		DEBUG					= 0;
+	public static final byte		STATUS_OK				= 0;
+	public static final byte		STATUS_PROTOCOLERROR	= 1;
+	public static final byte		STATUS_COMMERROR		= 2;
+	public static final String		NO_ERROR				= "no error";
 
-	public static final byte STATUS_OK 				= 0;
-	public static final byte STATUS_PROTOCOLERROR 	= 1;
-	public static final byte STATUS_COMMERROR 		= 2;
-	public static final String NO_ERROR 			= "no error";
+	private Rtu						meter					= null;
+	private MK10Push				mk10Push				= null;
+	private MK10					mk10Protocol			= new MK10();
+	private Properties				properties				= new Properties();
+	private List<AmrJournalEntry>	journal					= new ArrayList<AmrJournalEntry>();
 
-	private Rtu meter				= null;
-	private MK10Push mk10Push		= null;
-	private MK10 mk10Protocol		= new MK10();
-	private Properties properties	= new Properties();
-    private List journal 			= new ArrayList();
-    
-    private int completionCode				= AmrJournalEntry.CC_OK;
-    private String completionErrorString 	= "";
+	private int						completionCode			= AmrJournalEntry.CC_OK;
+	private String					completionErrorString	= "";
 
-    private MeterReadingData meterReadingData	= null;
-    private ProfileData meterProfileData		= null;
-    private MeterUsageData meterUsageData		= null;
+	private MeterReadingData		meterReadingData		= null;
+	private ProfileData				meterProfileData		= null;
+	private MeterUsageData			meterUsageData			= null;
 
 	/*
 	 * Constructors
@@ -66,11 +65,11 @@ public class MK10ProtocolExecuter {
 	public MK10ProtocolExecuter(MK10Push mk10Push) {
 		this.mk10Push = mk10Push;
 	}
-	
+
 	/*
 	 * Private getters, setters and methods
 	 */
-	
+
     private MeteringWarehouse mw() {
         return MeteringWarehouse.getCurrent();
     }
@@ -78,15 +77,15 @@ public class MK10ProtocolExecuter {
 	private MK10Push getMk10Push() {
 		return mk10Push;
 	}
-	
+
 	private Link getLink() {
 		return getMk10Push().getLink();
 	}
-	
+
 	/*
 	 * AMR and logging methods
 	 */
-	
+
 	private Logger getLogger() {
 		return getMk10Push().getLogger();
 	}
@@ -94,60 +93,83 @@ public class MK10ProtocolExecuter {
 	protected void log(Level level, String msg){
 		getLogger().log(level, msg + "\n");
 	}
-    
+
     protected void setAmrJournalOutOfBoundary(long timeDiff) {
         journal(new AmrJournalEntry(AmrJournalEntry.TIME_OUT_OF_BOUNDARY, "Time difference " + timeDiff + " ms is out of boundary."));
     }
-    
+
     public void setCompletionCodeConfiguration() {
         adjustCompletionCode(AmrJournalEntry.CC_CONFIGURATION);
     }
-    
+
     public void adjustCompletionCode(int cc) {
-    	if (DEBUG >= 1)	System.out.println(" ### adjustCompletionCode(), CC = " + cc);
-    	if (this.completionCode == AmrJournalEntry.CC_OK)
-            this.completionCode = cc;
+    	if (DEBUG >= 1) {
+			System.out.println(" ### adjustCompletionCode(), CC = " + cc);
+		}
+    	if (this.completionCode == AmrJournalEntry.CC_OK) {
+			this.completionCode = cc;
+		}
     }
-	
+
     public void setCompletionCodeTimeError(String msg) {
-        if (msg != null) completionErrorString = msg;
+        if (msg != null) {
+			completionErrorString = msg;
+		}
         adjustCompletionCode(AmrJournalEntry.CC_TIME_ERROR);
     }
 
-    
+
 	private void readMeterProperties() {
-		if (this.properties == null) this.properties = new Properties();
-		
+		if (this.properties == null) {
+			this.properties = new Properties();
+		}
+
 		CommunicationProtocol protocol = getMeter().getProtocol();
 		if (protocol != null) {
-			if (protocol.getProperties() != null) this.properties.putAll(protocol.getProperties());
+			if (protocol.getProperties() != null) {
+				this.properties.putAll(protocol.getProperties());
+			}
 		}
-		if (getMeter().getProperties() != null) this.properties.putAll(getMeter().getProperties());
-				
-		if (getMeter().getSerialNumber() != null) this.properties.put(MeterProtocol.SERIALNUMBER, getMeter().getSerialNumber());
-		if (getMeter().getNodeAddress() != null) this.properties.put(MeterProtocol.NODEID, getMeter().getNodeAddress());
-		if (getMeter().getDeviceId() != null) this.properties.put(MeterProtocol.ADDRESS, getMeter().getDeviceId());
-		if (getMeter().getPassword() != null) this.properties.put(MeterProtocol.PASSWORD, getMeter().getPassword());
-		if (String.valueOf(getMeter().getIntervalInSeconds()) != null) this.properties.put(MeterProtocol.PROFILEINTERVAL, String.valueOf(getMeter().getIntervalInSeconds()));
-		
-		if (DEBUG >= 2)	properties.list(System.out);
-		
+		if (getMeter().getProperties() != null) {
+			this.properties.putAll(getMeter().getProperties());
+		}
+
+		if (getMeter().getSerialNumber() != null) {
+			this.properties.put(MeterProtocol.SERIALNUMBER, getMeter().getSerialNumber());
+		}
+		if (getMeter().getNodeAddress() != null) {
+			this.properties.put(MeterProtocol.NODEID, getMeter().getNodeAddress());
+		}
+		if (getMeter().getDeviceId() != null) {
+			this.properties.put(MeterProtocol.ADDRESS, getMeter().getDeviceId());
+		}
+		if (getMeter().getPassword() != null) {
+			this.properties.put(MeterProtocol.PASSWORD, getMeter().getPassword());
+		}
+		if (String.valueOf(getMeter().getIntervalInSeconds()) != null) {
+			this.properties.put(MeterProtocol.PROFILEINTERVAL, String.valueOf(getMeter().getIntervalInSeconds()));
+		}
+
+		if (DEBUG >= 2) {
+			properties.list(System.out);
+		}
+
 	}
 
     private void doLogMeterDataCollection(MeterReadingData meterReadingData) {
     	if (meterReadingData == null) {
     		log(Level.INFO, "meterReadingData is null, probably meterreadings not supported by meter.");
     		return;
-    	} 
+    	}
 
     	List registerValues = meterReadingData.getRegisterValues();
     	Iterator it = registerValues.iterator();
-    	
+
     	while (it.hasNext()) {
     		RegisterValue each = (RegisterValue) it.next();
     		log(Level.INFO, each.toString());
     	}
-    	
+
     } // private void doLogMeterDataCollection(List readingsData)  throws ProtocolReaderException
 
     private void doLogMeterDataCollection(ProfileData profileData) {
@@ -176,12 +198,12 @@ public class MK10ProtocolExecuter {
         }
 
     } // private void doLogMeterDataCollection(ProfileData profileData)  throws ProtocolReaderException
-    
+
     protected void verifyMeterProfileInterval() throws IOException {
         // Verify profileinterval against the configuration
         int iProfileInterval = getMk10Protocol().getInfoTypeProfileInterval();
         int meterProfileInterval = getMk10Protocol().getProfileInterval();
-        
+
         try {
             if (meterProfileInterval != iProfileInterval) {
                 setCompletionCodeConfiguration();
@@ -191,7 +213,7 @@ public class MK10ProtocolExecuter {
         catch (UnsupportedException e) {
         }
     }
-	
+
 	/*
 	 * Public methods
 	 */
@@ -200,27 +222,31 @@ public class MK10ProtocolExecuter {
 
 		// Let the MK10 protocol know that it's been used as generic Push protocol
 		getMk10Protocol().setPushProtocol(true);
-		
+
 		// Read the properties from the Rtu and the Protocol in EIServer and apply them to the MK10Protocol.
 		readMeterProperties();
 		getMk10Protocol().setProperties(this.properties);
-		if (DEBUG >= 2) properties.list(System.out);
+		if (DEBUG >= 2) {
+			properties.list(System.out);
+		}
 
 		// Create new streams and pass them to the MK10 protocol
 		getMk10Protocol().init(
-				new MK10PushInputStream(getLink().getInputStream()), 
-				new MK10PushOutputStream(getLink().getOutputStream()),	 
-				getMeter().getTimeZone(), 
+				new MK10PushInputStream(getLink().getInputStream()),
+				new MK10PushOutputStream(getLink().getOutputStream()),
+				getMeter().getTimeZone(),
 				getLogger()
 		);
-		
+
 		// Try to connect to the meter ...
 		getMk10Protocol().connect();
-		
+
         // Before continuing with reading the meter's data, check some basic
         // things to be sure that we can continue...
-        if (getCommunicationProfile().getReadDemandValues()) verifyMeterProfileInterval();
-        
+        if (getCommunicationProfile().getReadDemandValues()) {
+			verifyMeterProfileInterval();
+		}
+
         MK10ClockExecuter clockExec = new MK10ClockExecuter(this);
         boolean markAsBadTime = clockExec.verifyMeterMaxTimeDiff(getCommunicationProfile().getCollectOutsideBoundary());
 
@@ -244,7 +270,7 @@ public class MK10ProtocolExecuter {
         String strVersion = getMk10Protocol().getProtocolVersion();
         log(Level.FINE, "MK10: protocol version: " + strVersion);
 
-        
+
         // Get the meter profile
     	MK10DemandValuesExecuter demandValuesExec = new MK10DemandValuesExecuter(this);
         if (getCommunicationProfile().getReadDemandValues()) {
@@ -257,27 +283,28 @@ public class MK10ProtocolExecuter {
         if (meterProfileData != null) {
             meterProfileData = demandValuesExec.validateProfileData(meterProfileData, demandValuesExec.getNow());
             meterProfileData.sort();
-            if (markAsBadTime)
-                meterProfileData.markIntervalsAsBadTime();
+            if (markAsBadTime) {
+				meterProfileData.markIntervalsAsBadTime();
+			}
         }
-        
+
         // Try to read or set the clock in the meter
 		long timeDiff = clockExec.verifyAndSetMeterTime(getMeter().getTimeZone());
         journal(new AmrJournalEntry(AmrJournalEntry.TIMEDIFF, timeDiff));
 
-		
-        
+
+
 		// Disconnect the meter ...
 		getMk10Protocol().disconnect();
 
         meterUsageData = new MeterUsageData(meterReadingData, meterProfileData);
-		
+
 		// Show data from meter in loggings
 		doLogMeterDataCollection(meterProfileData);
 		doLogMeterDataCollection(meterReadingData);
-		
+
 	}
-	
+
 	/*
 	 * Public getters and setters
 	 */
@@ -290,47 +317,51 @@ public class MK10ProtocolExecuter {
 					"CommunicationSchedulers found for Rtu: " + schedulerList.size()
 					);
 		}
-		
+
 		CommunicationScheduler cs = (CommunicationScheduler) schedulerList.get(0);
 		CommunicationProfile cp =  mw().getCommunicationProfileFactory().find(cs.getCommunicationProfileId());
-		if (cp == null) throw new BusinessException("No CommunicationProfile found for Rtu.");
-		
+		if (cp == null) {
+			throw new BusinessException("No CommunicationProfile found for Rtu.");
+		}
+
 		return cp;
 	}
-	
+
 	public Rtu getMeter() {
 		return meter;
 	}
-	
+
 	public void setMeter(Rtu meter) {
 		this.meter = meter;
 	}
-	
+
 	public MK10 getMk10Protocol() {
 		return mk10Protocol;
 	}
-	
+
 	public void addProperties(Properties properties) {
 		this.properties.putAll(properties);
 	}
 
-	public List getOptionalKeys() {
-		ArrayList list = new ArrayList();
+	@SuppressWarnings("unchecked")
+	public List<String> getOptionalKeys() {
+		List<String> list = new ArrayList<String>();
 		list.addAll(getMk10Protocol().getOptionalKeys());
 		return list;
 	}
 
-	public List getRequiredKeys() {
-		ArrayList list = new ArrayList(0);
+	@SuppressWarnings("unchecked")
+	public List<String> getRequiredKeys() {
+		List<String> list = new ArrayList<String>();
 		list.addAll(getMk10Protocol().getRequiredKeys());
 		return list;
 	}
 
-    public List getJournal() {
+    public List<AmrJournalEntry> getJournal() {
         return journal;
     }
 
-    protected void setJournal(List journal) {
+    protected void setJournal(List<AmrJournalEntry> journal) {
         this.journal = journal;
     }
 
@@ -341,25 +372,25 @@ public class MK10ProtocolExecuter {
     public Properties getProperties() {
 		return properties;
 	}
-    
+
     public ProfileData getMeterProfileData() {
 		return meterProfileData;
 	}
-    
+
     public MeterReadingData getMeterReadingData() {
 		return meterReadingData;
 	}
-    
+
     public int getCompletionCode() {
 		return completionCode;
 	}
-    
+
     public String getCompletionErrorString() {
 		return completionErrorString;
 	}
-    
+
     public MeterUsageData getMeterUsageData() {
 		return meterUsageData;
 	}
-    
+
 }
