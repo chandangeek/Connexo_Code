7c21b3872a35eff5346a8669228d90cebde9a8b2
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2009-12-23 14:50:55 +0000

    Applied code rules
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@38545 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/axrdencoding/AbstractDataType.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/axrdencoding/AbstractDataType.java
@@ -1,8 +1,6 @@
 /*
  * AbstractDataType.java
- *
  * Created on 17 oktober 2007, 14:19
- *
  * To change this template, choose Tools | Options and locate the template under
  * the Source Creation and Management node. Right-click the template and choose
  * Open. You can then make changes to the template in the Source Editor.
@@ -14,113 +12,131 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 /**
- *
  * @author kvds
- * 
  * changes:
- * jme	|04022009|	Added isUnsigned32() method.
- * 
+ * jme |04022009| Added isUnsigned32() method.
  */
 abstract public class AbstractDataType {
-    
-    abstract protected byte[] doGetBEREncodedByteArray() throws IOException;
-    abstract protected int size();
-    abstract public BigDecimal toBigDecimal( );
-    abstract public int intValue();
-    abstract public long longValue();
-       
-    
-    private int level;
-    
-    /** Creates a new instance of AbstractDataType */
-    public AbstractDataType() {
-        
-    } 
-    
-    public int getDecodedSize() {
-    	return size();
-    }
-    
-    public byte[] getBEREncodedByteArray() throws IOException {
-        return doGetBEREncodedByteArray();
-    }
-    
-    public Structure getStructure() {
-        return (Structure)this;
-    }
-    public Array getArray() {
-        return (Array)this;
-    }
-    public TypeEnum getTypeEnum() {
-        return (TypeEnum)this;
-    }
-    
-    public boolean isStructure() {
-        return this instanceof Structure;
-    }  
-    public boolean isUnsigned8() {
-        return this instanceof Unsigned8;
-    }  
-    public boolean isVisibleString() {
-        return this instanceof VisibleString;
-    }  
-    public boolean isOctetString() {
-        return this instanceof OctetString;
-    }  
-    public boolean isInteger32() {
-        return this instanceof Integer32;
-    }  
-    public boolean isNullData() {
-        return this instanceof NullData;
-    }  
-    public boolean isArray() {
-        return this instanceof Array;
-    }  
-    public boolean isUnsigned32() {
-    	return this instanceof Unsigned32;
-    }
-    
-    public VisibleString getVisibleString() {
-        return (VisibleString)this;
-    }
-    public OctetString getOctetString() {
-        return (OctetString)this;
-    }
-    public Integer8 getInteger8() {
-        return (Integer8)this;
-    }
-    public Integer16 getInteger16() {
-        return (Integer16)this;
-    }
-    public Integer64 getInteger64() {
-        return (Integer64)this;
-    }
-    public Integer32 getInteger32() {
-        return (Integer32)this;
-    }
-    public Unsigned8 getUnsigned8() {
-        return (Unsigned8)this;
-    }
-    public Unsigned16 getUnsigned16() {
-        return (Unsigned16)this;
-    }
-    public Unsigned32 getUnsigned32() {
-        return (Unsigned32)this;
-    }
-    public BitString getBitString() {
-        return (BitString)this;
-    }
-    public NullData getNullData() {
-        return (NullData)this;
-    }
-    
-    public int getLevel() {
-        return level;
-    }
 
-    public void setLevel(int level) {
-        this.level = level;
-    }
-            
-    
+	protected abstract byte[] doGetBEREncodedByteArray() throws IOException;
+
+	protected abstract int size();
+
+	public abstract BigDecimal toBigDecimal();
+
+	public abstract int intValue();
+
+	public abstract long longValue();
+
+	private int	level;
+
+	/** Creates a new instance of AbstractDataType */
+	public AbstractDataType() {
+
+	}
+
+	public int getDecodedSize() {
+		return size();
+	}
+
+	public byte[] getBEREncodedByteArray() throws IOException {
+		return doGetBEREncodedByteArray();
+	}
+
+	public Structure getStructure() {
+		return (Structure) this;
+	}
+
+	public Array getArray() {
+		return (Array) this;
+	}
+
+	public TypeEnum getTypeEnum() {
+		return (TypeEnum) this;
+	}
+
+	public boolean isStructure() {
+		return this instanceof Structure;
+	}
+
+	public boolean isUnsigned8() {
+		return this instanceof Unsigned8;
+	}
+
+	public boolean isVisibleString() {
+		return this instanceof VisibleString;
+	}
+
+	public boolean isOctetString() {
+		return this instanceof OctetString;
+	}
+
+	public boolean isInteger32() {
+		return this instanceof Integer32;
+	}
+
+	public boolean isNullData() {
+		return this instanceof NullData;
+	}
+
+	public boolean isArray() {
+		return this instanceof Array;
+	}
+
+	public boolean isUnsigned32() {
+		return this instanceof Unsigned32;
+	}
+
+	public VisibleString getVisibleString() {
+		return (VisibleString) this;
+	}
+
+	public OctetString getOctetString() {
+		return (OctetString) this;
+	}
+
+	public Integer8 getInteger8() {
+		return (Integer8) this;
+	}
+
+	public Integer16 getInteger16() {
+		return (Integer16) this;
+	}
+
+	public Integer64 getInteger64() {
+		return (Integer64) this;
+	}
+
+	public Integer32 getInteger32() {
+		return (Integer32) this;
+	}
+
+	public Unsigned8 getUnsigned8() {
+		return (Unsigned8) this;
+	}
+
+	public Unsigned16 getUnsigned16() {
+		return (Unsigned16) this;
+	}
+
+	public Unsigned32 getUnsigned32() {
+		return (Unsigned32) this;
+	}
+
+	public BitString getBitString() {
+		return (BitString) this;
+	}
+
+	public NullData getNullData() {
+		return (NullData) this;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
 }
--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/axrdencoding/OctetString.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/axrdencoding/OctetString.java
@@ -1,8 +1,6 @@
 /*
  * OctetString.java
- *
  * Created on 16 oktober 2007, 11:35
- *
  * To change this template, choose Tools | Options and locate the template under
  * the Source Creation and Management node. Right-click the template and choose
  * Open. You can then make changes to the template in the Source Editor.
@@ -18,166 +16,179 @@ import com.energyict.dlms.DLMSUtils;
 import com.energyict.protocol.ProtocolUtils;
 
 /**
- *
  * @author kvds
  * Changes:
- * GNA |26012009| Added and changed some functions to make fixed OctetStrings. Size is not returned in the encodedData
+ * GNA |26012009| Added and changed some functions to make fixed OctetStrings.
+ * Size is not returned in the encodedData
  */
 public class OctetString extends AbstractDataType {
-    
-    private byte[] octetStr;
-    int size;
-    private int offsetBegin,offsetEnd;
-    private boolean fixed;
-            
-    /** Creates a new instance of OctetString */
-    public OctetString(byte[] berEncodedData, int offset) throws IOException {
-        offsetBegin = offset;
-        if (berEncodedData[offset] != DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING)
-            throw new IOException("OctetString, invalid identifier "+berEncodedData[offset]);
-        offset++;
-        size = (int)DLMSUtils.getAXDRLength(berEncodedData,offset);
-        offset+=DLMSUtils.getAXDRLengthOffset(berEncodedData, offset);
-        setOctetStr(ProtocolUtils.getSubArray2(berEncodedData,offset, size));
-        offset+=size;
-        offsetEnd = offset;
-        this.fixed = false;
-    }
-    
-    /** Create a new instance of a fixed OctetString */
-    public OctetString(byte[] berEncodedData, int offset, boolean fixed) throws IOException {
-        offsetBegin = offset;
-        if (berEncodedData[offset] != DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING)
-            throw new IOException("OctetString, invalid identifier "+berEncodedData[offset]);
-        size = berEncodedData.length-1;
-        offset+=DLMSUtils.getAXDRLengthOffset(berEncodedData, offset);
-        setOctetStr(ProtocolUtils.getSubArray2(berEncodedData,offset, size));
-        offset+=size;
-        offsetEnd = offset;
-        this.fixed = fixed;
-    }
-    
-    public String toString() {
-        StringBuffer strBuffTab = new StringBuffer();
-        for (int i=0;i<getLevel();i++) 
-            strBuffTab.append("  ");
-        return strBuffTab.toString()+"OctetString="+ProtocolUtils.outputHexString(getOctetStr())+"\n";
-    }
-    
-    /**
-     * Create a variable length OctetString
-     * @param octetStr
-     */
-    public OctetString(byte[] octetStr) {
-        this(octetStr,octetStr.length,0);
-    }
-    /**
-     * It is possible to create a fixed length OctetString
-     * @param octetStr
-     * @param fixed a boolean to indicate whether it is fixed or not
-     */
-    public OctetString(byte[] octetStr, boolean fixed){
-    	this(octetStr, octetStr.length, (fixed?1:0));
-    }
-    
-    static public OctetString fromString(String string) {
-    	if (string==null)
-    		return new OctetString(new byte[]{});
-    	else
-    		return new OctetString(string.getBytes());
-    }
-    static public OctetString fromString(String string, int size) {
-        return new OctetString(string.getBytes(), size, 0);
-    }
-    static public OctetString fromByteArray(byte[] byteArray, int size) {
-        return new OctetString(byteArray, size, 0);
-    }
-    static public OctetString fromString(String string, int size, boolean fixed){
-    	return new OctetString(string.getBytes(), size, (fixed?1:0));
-    }
-    
-    private OctetString(byte[] octetStr, int size, int dummy) {
-        this.setOctetStr(octetStr);
-        this.size=size;
-        this.fixed = (dummy == 1);
-    }
-    
-    public String stringValue() {
-        return new String(getOctetStr());
-    }
-    
-    protected byte[] doGetBEREncodedByteArray() {
-    
-    	byte[] encodedLength;
-    	if(this.fixed){
-    		encodedLength = new byte[0];
-    	} else {
-    		encodedLength = DLMSUtils.getAXDRLengthEncoding(size);
-    	}
-        byte[] data = new byte[size+1+encodedLength.length];
-        data[0] = DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING;
-        for (int i=0;i<encodedLength.length;i++)
-            data[1+i]=encodedLength[i];
-        for (int i=0;i<(data.length-(1+encodedLength.length));i++) {
-            if (i < getOctetStr().length)
-                data[(1+encodedLength.length)+i] = getOctetStr()[i];
-            else
-                data[(1+encodedLength.length)+i] = 0;
-        }
-        return data;
-    }
-    
-    protected int size() {
-        return offsetEnd-offsetBegin;
-    }
-    
-    static public void main(String[]  artgs) {
-        try {
-           OctetString v = new OctetString(new byte[]{DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING,0x03,(byte)'A',(byte)'B',(byte)'C'}, 0);
-           System.out.println(v);
-           v = new OctetString(new byte[]{1,2,3,4,5,6},6, 0);
-           System.out.println(ProtocolUtils.outputHexString(v.getBEREncodedByteArray()));
-           v = new OctetString(new byte[]{1,2,3,4,5,6}, 6, 1);
-           System.out.println(ProtocolUtils.outputHexString(v.getBEREncodedByteArray()));
-           v = new OctetString(new byte[]{DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING,1,2,3,4,5,6}, 0, true);
-           System.out.println(ProtocolUtils.outputHexString(v.getBEREncodedByteArray()));
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-                
-    }
-    
-    public byte[] getContentBytes(){
-    	byte[] content = new byte[getOctetStr().length-2];
-    	System.arraycopy(getOctetStr(), fixed?1:2, content, 0, content.length);
-    	return content;
-    }
 
-    public byte[] getOctetStr() {
-        return octetStr;
-    }
+	private byte[]	octetStr;
+	private int		size;
+	private int		offsetBegin, offsetEnd;
+	private boolean	fixed;
 
-    public void setOctetStr(byte[] octetStr) {
-        this.octetStr = octetStr;
-    }
-    
-    public BigDecimal toBigDecimal() {
-        BigDecimal result = new BigDecimal( 0 );
-        for( int i = 0; i < octetStr.length; i ++ ) {
-            int idx = octetStr.length - i - 1;
-            BigDecimal temp = new BigDecimal( octetStr[idx] ).movePointRight(i);
-            result = result.add( temp );
-        
-        }
-        return result;
-    }
-    
-    public int intValue() {
-        return -1;
-    }
-    
-    public long longValue() {
-        return -1;
-    }        
+	/** Creates a new instance of OctetString */
+	public OctetString(byte[] berEncodedData, int offset) throws IOException {
+		int workingOffset = offset;
+		offsetBegin = workingOffset;
+		if (berEncodedData[workingOffset] != DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING) {
+			throw new IOException("OctetString, invalid identifier " + berEncodedData[workingOffset]);
+		}
+		workingOffset++;
+		size = (int) DLMSUtils.getAXDRLength(berEncodedData, workingOffset);
+		workingOffset += DLMSUtils.getAXDRLengthOffset(berEncodedData, workingOffset);
+		octetStr = ProtocolUtils.getSubArray2(berEncodedData, workingOffset, size);
+		workingOffset += size;
+		offsetEnd = workingOffset;
+		this.fixed = false;
+	}
+
+	/** Create a new instance of a fixed OctetString */
+	public OctetString(byte[] berEncodedData, int offset, boolean fixed) throws IOException {
+		offsetBegin = offset;
+		if (berEncodedData[offset] != DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING) {
+			throw new IOException("OctetString, invalid identifier " + berEncodedData[offset]);
+		}
+		size = berEncodedData.length - 1;
+		offset += DLMSUtils.getAXDRLengthOffset(berEncodedData, offset);
+		octetStr = ProtocolUtils.getSubArray2(berEncodedData, offset, size);
+		offset += size;
+		offsetEnd = offset;
+		this.fixed = fixed;
+	}
+
+	public String toString() {
+		StringBuffer strBuffTab = new StringBuffer();
+		for (int i = 0; i < getLevel(); i++) {
+			strBuffTab.append("  ");
+		}
+		return strBuffTab.toString() + "OctetString=" + ProtocolUtils.outputHexString(getOctetStr()) + "\n";
+	}
+
+	/**
+	 * Create a variable length OctetString
+	 *
+	 * @param octetStr
+	 */
+	public OctetString(byte[] octetStr) {
+		this(octetStr, octetStr.length, 0);
+	}
+
+	/**
+	 * It is possible to create a fixed length OctetString
+	 *
+	 * @param octetStr
+	 * @param fixed a boolean to indicate whether it is fixed or not
+	 */
+	public OctetString(byte[] octetStr, boolean fixed) {
+		this(octetStr, octetStr.length, (fixed ? 1 : 0));
+	}
+
+	public static OctetString fromString(String string) {
+		if (string == null) {
+			return new OctetString(new byte[] {});
+		} else {
+			return new OctetString(string.getBytes());
+		}
+	}
+
+	public static OctetString fromString(String string, int size) {
+		return new OctetString(string.getBytes(), size, 0);
+	}
+
+	public static OctetString fromByteArray(byte[] byteArray, int size) {
+		return new OctetString(byteArray, size, 0);
+	}
+
+	public static OctetString fromString(String string, int size, boolean fixed) {
+		return new OctetString(string.getBytes(), size, (fixed ? 1 : 0));
+	}
+
+	private OctetString(byte[] octetStr, int size, int dummy) {
+		this.setOctetStr(octetStr);
+		this.size = size;
+		this.fixed = (dummy == 1);
+	}
+
+	public String stringValue() {
+		return new String(getOctetStr());
+	}
+
+	protected byte[] doGetBEREncodedByteArray() {
+
+		byte[] encodedLength;
+		if (this.fixed) {
+			encodedLength = new byte[0];
+		} else {
+			encodedLength = DLMSUtils.getAXDRLengthEncoding(size);
+		}
+		byte[] data = new byte[size + 1 + encodedLength.length];
+		data[0] = DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING;
+		for (int i = 0; i < encodedLength.length; i++) {
+			data[1 + i] = encodedLength[i];
+		}
+		for (int i = 0; i < (data.length - (1 + encodedLength.length)); i++) {
+			if (i < getOctetStr().length) {
+				data[(1 + encodedLength.length) + i] = getOctetStr()[i];
+			} else {
+				data[(1 + encodedLength.length) + i] = 0;
+			}
+		}
+		return data;
+	}
+
+	protected int size() {
+		return offsetEnd - offsetBegin;
+	}
+
+	public byte[] getContentBytes() {
+		byte[] content = new byte[getOctetStr().length - 2];
+		System.arraycopy(getOctetStr(), fixed ? 1 : 2, content, 0, content.length);
+		return content;
+	}
+
+	public byte[] getOctetStr() {
+		return octetStr;
+	}
+
+	public void setOctetStr(byte[] octetStr) {
+		this.octetStr = octetStr.clone();
+	}
+
+	public BigDecimal toBigDecimal() {
+		BigDecimal result = BigDecimal.ZERO;
+		for (int i = 0; i < octetStr.length; i++) {
+			int idx = octetStr.length - i - 1;
+			BigDecimal temp = new BigDecimal(octetStr[idx]).movePointRight(i);
+			result = result.add(temp);
+
+		}
+		return result;
+	}
+
+	public int intValue() {
+		return -1;
+	}
+
+	public long longValue() {
+		return -1;
+	}
+
+//	public static void main(String[] artgs) {
+//		try {
+//			OctetString v = new OctetString(new byte[] { DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING, 0x03, (byte) 'A', (byte) 'B', (byte) 'C' }, 0);
+//			System.out.println(v);
+//			v = new OctetString(new byte[] { 1, 2, 3, 4, 5, 6 }, 6, 0);
+//			System.out.println(ProtocolUtils.outputHexString(v.getBEREncodedByteArray()));
+//			v = new OctetString(new byte[] { 1, 2, 3, 4, 5, 6 }, 6, 1);
+//			System.out.println(ProtocolUtils.outputHexString(v.getBEREncodedByteArray()));
+//			v = new OctetString(new byte[] { DLMSCOSEMGlobals.TYPEDESC_OCTET_STRING, 1, 2, 3, 4, 5, 6 }, 0, true);
+//			System.out.println(ProtocolUtils.outputHexString(v.getBEREncodedByteArray()));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+
 }
