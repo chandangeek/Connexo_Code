404503045c421e6a1c080fccf789532449af7e41
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-01-06 10:35:34 +0000

    Added LLCConnection for direct communication to the AM500 module of the Elster AS220 meter.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@38630 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/CosemPDUConnection.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/CosemPDUConnection.java
@@ -1,9 +1,12 @@
 package com.energyict.dlms;
 
-import com.energyict.dialer.connection.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-import com.energyict.dialer.connection.*;
+import com.energyict.dialer.connection.Connection;
+import com.energyict.dialer.connection.ConnectionException;
+import com.energyict.dialer.connection.HHUSignOn;
 import com.energyict.protocol.ProtocolUtils;
 
 /**
@@ -22,26 +25,26 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
     // TCPIP specific
     // Sequence numbering
     private boolean connected;
-    
+
     private int maxRetries;
     private int clientAddress;
     private int serverAddress;
     int timeout;
     private long forceDelay;
-    
+
     private int iskraWrapper = 0;
 
     private static final byte CLIENT_NO_SECURITY=0;
     private static final byte CLIENT_LOWLEVEL_SECURITY=1;
-    
+
     private InvokeIdAndPriority invokeIdAndPriority;
-    
+
     public CosemPDUConnection(InputStream inputStream,
                           OutputStream outputStream,
                           int timeout,
                           int forceDelay,
                           int maxRetries,
-                          int clientAddress, 
+                          int clientAddress,
                           int serverAddress) throws IOException {
         super(inputStream, outputStream);
         this.maxRetries = maxRetries;
@@ -51,17 +54,17 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
         this.forceDelay = forceDelay;
         connected=false;
         this.invokeIdAndPriority = new InvokeIdAndPriority();
-        
+
     } // public TCPIPConnection(...)
 
     public int getType() {
         return DLMSConnection.DLMS_CONNECTION_TCPIP;
     }
-    
+
     public void setSNRMType(int type) {
         // absorb...
     }
-    
+
 /**
  * Method that requests a MAC connection for the TCPIP layer. this request negotiates some parameters
  * for the buffersizes and windowsizes.
@@ -69,11 +72,13 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
  */
     public void connectMAC() throws IOException {
        if (connected==false) {
-          if (hhuSignOn != null) hhuSignOn.signOn("",meterId);
+          if (hhuSignOn != null) {
+			hhuSignOn.signOn("",meterId);
+		}
           connected=true;
        } // if (connected==false)
     } // public void connectMAC() throws IOException
-    
+
 /**
  * Method that requests a MAC disconnect for the TCPIP layer.
  * @exception TCPIPConnectionException
@@ -82,9 +87,9 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
        if (connected==true) {
            connected=false;
        } // if (connected==true)
-       
+
     } // public void disconnectMAC() throws IOException
-    
+
     private byte[] receiveData() throws IOException {
         byte[] data;
         long interFrameTimeout;
@@ -92,7 +97,9 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
         interFrameTimeout = System.currentTimeMillis() + timeout;
         while(true) {
             if ((data = readInArray()) != null) {
-               if (DEBUG>=2) ProtocolUtils.outputHexString(data);
+               if (DEBUG>=2) {
+				ProtocolUtils.outputHexString(data);
+			}
                byte[] dataWithLLC = new byte[data.length+3];
                System.arraycopy(data, 0, dataWithLLC, 3, data.length);
                return dataWithLLC;
@@ -100,16 +107,16 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
             if (((long) (System.currentTimeMillis() - interFrameTimeout)) > 0) {
                 throw new ConnectionException("receiveData() response timeout error",TIMEOUT_ERROR);
             }
-            
+
         } // while(true)
     } // private byte waitForTCPIPFrameStateMachine()
-    
-    
+
+
 	public void setIskraWrapper(int type) {
-		
+
 		/**
 		 * GN|260208|: Creation of this method to switch the client an server addresses
-		 * Should have been correct but Actaris probably switched this and Iskra came last ... 
+		 * Should have been correct but Actaris probably switched this and Iskra came last ...
 		 */
 		iskraWrapper = type;
 	}
@@ -122,19 +129,20 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
  */
     public byte[] sendRequest(byte[] data) throws IOException {
         int retry=0;
-        
+
         // strip the HDLC LLC header. This is because of the code inherited from  the days only HDLC existed...
         byte[] byteRequestBuffer = new byte[data.length-3];
         System.arraycopy(data, 3, byteRequestBuffer, 0, byteRequestBuffer.length);
-        
+
         while(true) {
             try {
                 sendOut(byteRequestBuffer);
                 return receiveData();
             }
             catch (ConnectionException e) {
-                if (retry++ >= maxRetries)
-                    throw new IOException("sendRequest, IOException, "+com.energyict.cbo.Utils.stack2string(e));
+                if (retry++ >= maxRetries) {
+					throw new IOException("sendRequest, IOException, "+com.energyict.cbo.Utils.stack2string(e));
+				}
             }
         }
 
@@ -148,18 +156,27 @@ public class CosemPDUConnection extends Connection implements DLMSConnection {
         this.meterId=meterId;
     }
     public HHUSignOn getHhuSignOn() {
-        return hhuSignOn;   
+        return hhuSignOn;
     }
-    
+
     /********************************************************************************************************
      * Invoke-Id-And-Priority byte setting
      ********************************************************************************************************/
-    
+
     public void setInvokeIdAndPriority(InvokeIdAndPriority iiap){
     	this.invokeIdAndPriority = iiap;
     }
-    
+
     public InvokeIdAndPriority getInvokeIdAndPriority(){
     	return this.invokeIdAndPriority;
     }
+
+    public int getTimeout() {
+		return timeout;
+	}
+
+    public int getMaxRetries() {
+		return maxRetries;
+	}
+
 } // public class TCPIPConnection
---
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/LLCConnection.java
@@ -0,0 +1,66 @@
+package com.energyict.dlms;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.energyict.dialer.connection.ConnectionException;
+
+/**
+ * @author jme
+ *
+ */
+public class LLCConnection extends CosemPDUConnection implements DLMSConnection {
+
+	public LLCConnection(InputStream inputStream, OutputStream outputStream, int timeout, int forceDelay, int maxRetries, int clientAddress, int serverAddress)
+			throws IOException {
+		super(inputStream, outputStream, timeout, forceDelay, maxRetries, clientAddress, serverAddress);
+	}
+
+	private byte[] receiveData() throws IOException {
+		byte[] data;
+		long interFrameTimeout;
+		copyEchoBuffer();
+		interFrameTimeout = System.currentTimeMillis() + getTimeout();
+		while (true) {
+			data = readInArray();
+			if (data != null) {
+				if (data[0] == (byte)0x90) {
+					return data;
+				} else {
+					while (readInArray() != null) {
+					}
+					throw new IOException("LLC packet should start with 0x90!");
+				}
+			}
+			if (((long) (System.currentTimeMillis() - interFrameTimeout)) > 0) {
+				throw new ConnectionException("receiveData() response timeout error", TIMEOUT_ERROR);
+			}
+		}
+	}
+
+	/**
+	 * Append the LLC header to the packet (Hard coded to 0x90, 0x01, 0x02)
+	 */
+	public byte[] sendRequest(byte[] data) throws IOException {
+		int retry = 0;
+
+		byte[] byteRequestBuffer = new byte[data.length];
+		System.arraycopy(data, 3, byteRequestBuffer, 3, data.length - 3);
+		byteRequestBuffer[0] = (byte) 0x90;
+		byteRequestBuffer[1] = (byte) 0x01;
+		byteRequestBuffer[2] = (byte) 0x02;
+
+		while (true) {
+			try {
+				sendOut(byteRequestBuffer);
+				return receiveData();
+			} catch (ConnectionException e) {
+				if (retry++ >= getMaxRetries()) {
+					throw new IOException("sendRequest, IOException, " + com.energyict.cbo.Utils.stack2string(e));
+				}
+			}
+		}
+	}
+
+}
