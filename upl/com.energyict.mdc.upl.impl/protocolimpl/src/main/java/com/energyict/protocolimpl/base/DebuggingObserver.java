ef16285124c66b47fb116701b6b4c0d89332bbc3
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-01-06 10:31:30 +0000

    Added comments
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@38627 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/base/DebuggingObserver.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/base/DebuggingObserver.java
@@ -6,38 +6,42 @@ import com.energyict.protocol.tools.InputStreamObserver;
 import com.energyict.protocol.tools.OutputStreamObserver;
 import com.energyict.protocolimpl.utils.ProtocolTools;
 
+/**
+ * Stream observer for debugging purposes. This observer formats the
+ * communication in a readable form, and writes it to the screen or to a file.
+ *
+ * @author jme
+ */
 public class DebuggingObserver implements InputStreamObserver, OutputStreamObserver {
 
-	private static final String	CRLF				= "\r\n";
+
 	private static final int	DIRECTION_UNKNOWN	= 0;
 	private static final int	DIRECTION_READING	= 1;
 	private static final int	DIRECTION_WRITING	= 2;
 
+	private static final String	CRLF				= "\r\n";
 	private static final long	TIME_DIFF			= 2000;
+
+	private final boolean		showCommunication;
+	private final String		fileName;
 
 	private int					lastDirection		= DIRECTION_UNKNOWN;
 	private long				lastAction			= 0L;
-	private boolean				showCommunication	= false;
-	private String				fileName			= null;
 
+	/**
+	 * Create a new {@link DebuggingObserver}.
+	 * @param fileName
+	 * @param showCommunication
+	 */
 	public DebuggingObserver(String fileName, boolean showCommunication) {
 		this.fileName = fileName;
 		this.showCommunication = showCommunication;
 		log(CRLF + CRLF + "Observer started " + new Date() + "[" + getClass().getCanonicalName() + "]" + CRLF);
 	}
 
-	public DebuggingObserver(String fileName) {
-		this(fileName, false);
-	}
-
-	public DebuggingObserver(boolean showCommunication) {
-		this(null, showCommunication);
-	}
-
-	public DebuggingObserver() {
-		this(null, false);
-	}
-
+	/* (non-Javadoc)
+	 * @see com.energyict.protocol.tools.InputStreamObserver#read(byte[])
+	 */
 	public void read(byte[] b) {
 		if (!isReading() || isLastActionLongTimeAgo()) {
 			log(CRLF);
@@ -48,12 +52,18 @@ public class DebuggingObserver implements InputStreamObserver, OutputStreamObser
 		setLastAction();
 	}
 
+	/* (non-Javadoc)
+	 * @see com.energyict.protocol.tools.InputStreamObserver#threw(java.lang.Throwable)
+	 */
 	public void threw(Throwable ex) {
 		setLastDirection(DIRECTION_UNKNOWN);
 		ex.printStackTrace();
 		setLastAction();
 	}
 
+	/* (non-Javadoc)
+	 * @see com.energyict.protocol.tools.OutputStreamObserver#wrote(byte[])
+	 */
 	public void wrote(byte[] b) {
 		if (!isWriting() || isLastActionLongTimeAgo()) {
 			log(CRLF);
@@ -72,11 +82,11 @@ public class DebuggingObserver implements InputStreamObserver, OutputStreamObser
 		return (lastDirection == DIRECTION_WRITING);
 	}
 
-	public void setLastDirection(int lastDirection) {
+	private void setLastDirection(int lastDirection) {
 		this.lastDirection = lastDirection;
 	}
 
-	public void setLastAction() {
+	private void setLastAction() {
 		this.lastAction = System.currentTimeMillis();
 	}
 
