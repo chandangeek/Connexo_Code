e243e4014280af7118c8e394552b623e856c4548
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-01-07 14:32:26 +0000

    Made AXDRProperties &  AXDRSerialCommunicationSettings more robust and added JUnit tests for both classes Renamed AXDRSerialCommunicationSettings (typo in classname)
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@38672 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/axrdencoding/util/AXDRProperties.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/axrdencoding/util/AXDRProperties.java
@@ -25,49 +25,54 @@ public final class AXDRProperties {
 	 */
 	public static Array encode(Properties properties) throws IOException {
 		Array array = new Array();
-
-		Enumeration e = properties.keys();
-		while (e.hasMoreElements()) {
-			Object key = e.nextElement();
-			String value = (String) properties.get(key);
-			Structure keyValue = new Structure();
-
-			if (key instanceof Integer) {
-				keyValue.addDataType(new Integer32(((Integer) key).intValue()));
-			} else if (key instanceof String) {
-				keyValue.addDataType(OctetString.fromString((String) key));
-			} else {
-				throw new IOException("Invalid key type for " + key.getClass().getName());
+		if (properties != null) {
+			Enumeration<Object> e = properties.keys();
+			while (e.hasMoreElements()) {
+				Object key = e.nextElement();
+				String value = (String) properties.get(key);
+				Structure keyValue = new Structure();
+				if (key instanceof Integer) {
+					keyValue.addDataType(new Integer32(((Integer) key).intValue()));
+				} else if (key instanceof String) {
+					keyValue.addDataType(OctetString.fromString((String) key));
+				} else {
+					throw new IOException("Invalid key type for " + key.getClass().getName());
+				}
+				keyValue.addDataType(OctetString.fromString(value));
+				array.addDataType(keyValue);
 			}
-
-			keyValue.addDataType(OctetString.fromString(value));
-			array.addDataType(keyValue);
 		}
-
 		return array;
 	}
 
 	/**
 	 * @param dataType
 	 * @return
+	 * @throws IOException
 	 */
-	public static Properties decode(AbstractDataType dataType) {
+	public static Properties decode(AbstractDataType dataType) throws IOException {
 		Properties properties = new Properties();
+		if ((dataType != null) && (dataType.isArray())) {
+			Array array = dataType.getArray();
+			for (int i = 0; i < array.nrOfDataTypes(); i++) {
+				Structure keyValue = array.getDataType(i).getStructure();
+				Object key = null;
+				if (keyValue.getDataType(0).isOctetString()) {
+					key = keyValue.getDataType(0).getOctetString().stringValue();
+				} else if (keyValue.getDataType(0).isInteger32()) {
+					key = Integer.valueOf(keyValue.getDataType(0).intValue());
+				} else {
+					throw new IOException("Invalid key type for " + keyValue.getDataType(0).getClass().getName());
+				}
 
-		Array array = dataType.getArray();
-		for (int i = 0; i < array.nrOfDataTypes(); i++) {
-			Structure keyValue = array.getDataType(i).getStructure();
-			Object key = null;
-			if (keyValue.getDataType(0).isOctetString()) {
-				key = keyValue.getDataType(0).getOctetString().stringValue();
-			} else if (keyValue.getDataType(0).isInteger32()) {
-				key = Integer.valueOf(keyValue.getDataType(0).intValue());
+				if (!keyValue.getDataType(1).isOctetString()) {
+					throw new IOException("Invalid value type for " + keyValue.getDataType(1).getClass().getName() + ". Should be OctetString");
+				}
+
+				String value = keyValue.getDataType(1).getOctetString().stringValue();
+				properties.put(key, value);
 			}
-
-			String value = keyValue.getDataType(1).getOctetString().stringValue();
-			properties.put(key, value);
 		}
-
 		return properties;
 	}
 
--- upl/com.energyict.mdc.upl.impl/dlms/src/test/java/com/energyict/dlms/axrdencoding/util/AXDRPropertiesTest.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/test/java/com/energyict/dlms/axrdencoding/util/AXDRPropertiesTest.java
@@ -3,10 +3,22 @@
  */
 package com.energyict.dlms.axrdencoding.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Ignore;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Test;
+
+import com.energyict.dlms.axrdencoding.Array;
+import com.energyict.dlms.axrdencoding.Integer16;
+import com.energyict.dlms.axrdencoding.Integer32;
+import com.energyict.dlms.axrdencoding.NullData;
+import com.energyict.dlms.axrdencoding.OctetString;
+import com.energyict.dlms.axrdencoding.Structure;
 
 /**
  * @author jme
@@ -14,22 +26,140 @@ import org.junit.Test;
  */
 public class AXDRPropertiesTest {
 
+	private static final String	STRING_KEY = "KEY1";
+	private static final Integer INTEGER_KEY	= new Integer(1);
+
+	private static final String	VALUE1	= "VALUE1";
+	private static final String	VALUE2	= "VALUE2";
+	private static final String	VALUE3	= "VALUE3";
+	private static final String	VALUE4	= "VALUE4";
+
+	private static final Properties	STRING_KEY_PROPERTIES	= new Properties();
+	private static final Properties	INTEGER_KEY_PROPERTIES	= new Properties();
+	private static final Properties	INVALID_KEY_PROPERTIES	= new Properties();
+	private static final Properties	EMPTY_KEY_PROPERTIES	= new Properties();
+
+	private static final Array		STRING_KEY_ARRAY		= new Array();
+	private static final Array		INTEGER_KEY_ARRAY		= new Array();
+	private static final Array		INVALID_KEY_ARRAY		= new Array();
+	private static final Array		INVALID_VALUE_ARRAY		= new Array();
+
+	static {
+		STRING_KEY_PROPERTIES.setProperty("1", VALUE1);
+		STRING_KEY_PROPERTIES.setProperty("2", VALUE2);
+
+		INTEGER_KEY_PROPERTIES.put(new Integer(1), VALUE1);
+		INTEGER_KEY_PROPERTIES.put(new Integer(2), VALUE2);
+		INTEGER_KEY_PROPERTIES.put(new Integer(3), VALUE3);
+
+		INVALID_KEY_PROPERTIES.put(new Long(1), VALUE1);
+		INVALID_KEY_PROPERTIES.put(new Long(2), VALUE2);
+		INVALID_KEY_PROPERTIES.put(new Long(3), VALUE3);
+		INVALID_KEY_PROPERTIES.put(new Long(4), VALUE4);
+
+		Structure invalidKey = new Structure();
+		invalidKey.addDataType(new Integer16(INTEGER_KEY));
+		invalidKey.addDataType(new OctetString(VALUE1.getBytes()));
+		INVALID_KEY_ARRAY.addDataType(invalidKey);
+
+		Structure invalidValue = new Structure();
+		invalidValue.addDataType(new OctetString(STRING_KEY.getBytes()));
+		invalidValue.addDataType(new Integer16(INTEGER_KEY));
+		INVALID_VALUE_ARRAY.addDataType(invalidValue);
+
+		Structure stringKey = new Structure();
+		stringKey.addDataType(new OctetString(STRING_KEY.getBytes()));
+		stringKey.addDataType(new OctetString(VALUE1.getBytes()));
+		STRING_KEY_ARRAY.addDataType(stringKey);
+
+		Structure integerKey = new Structure();
+		integerKey.addDataType(new Integer32(INTEGER_KEY));
+		integerKey.addDataType(new OctetString(VALUE2.getBytes()));
+		INTEGER_KEY_ARRAY.addDataType(integerKey);
+
+	}
+
 	/**
 	 * Test method for {@link com.energyict.dlms.axrdencoding.util.AXDRProperties#encode(java.util.Properties)}.
+	 * @throws IOException
 	 */
 	@Test
-	@Ignore
-	public final void testEncode() {
-		fail("Not yet implemented"); // TODO
+	public final void testEncode() throws IOException {
+		assertNotNull(AXDRProperties.encode(null));
+		assertNotNull(AXDRProperties.encode(STRING_KEY_PROPERTIES));
+		assertNotNull(AXDRProperties.encode(INTEGER_KEY_PROPERTIES));
+		assertNotNull(AXDRProperties.encode(EMPTY_KEY_PROPERTIES));
+
+		assertTrue(AXDRProperties.encode(null).isArray());
+		assertTrue(AXDRProperties.encode(STRING_KEY_PROPERTIES).isArray());
+		assertTrue(AXDRProperties.encode(INTEGER_KEY_PROPERTIES).isArray());
+		assertTrue(AXDRProperties.encode(EMPTY_KEY_PROPERTIES).isArray());
+
+		assertEquals(0, AXDRProperties.encode(null).getArray().nrOfDataTypes());
+		assertEquals(STRING_KEY_PROPERTIES.size(), AXDRProperties.encode(STRING_KEY_PROPERTIES).getArray().nrOfDataTypes());
+		assertEquals(INTEGER_KEY_PROPERTIES.size(), AXDRProperties.encode(INTEGER_KEY_PROPERTIES).getArray().nrOfDataTypes());
+		assertEquals(EMPTY_KEY_PROPERTIES.size(), AXDRProperties.encode(EMPTY_KEY_PROPERTIES).getArray().nrOfDataTypes());
+
+		for (int i = 0; i < STRING_KEY_PROPERTIES.size(); i++) {
+			assertTrue(AXDRProperties.encode(STRING_KEY_PROPERTIES).getArray().getDataType(i).isStructure());
+			assertTrue(AXDRProperties.encode(STRING_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(0).isOctetString());
+			assertTrue(AXDRProperties.encode(STRING_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(1).isOctetString());
+			String key = new String(AXDRProperties.encode(STRING_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(0).getOctetString().getOctetStr());
+			String value = new String(AXDRProperties.encode(STRING_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(1).getOctetString().getOctetStr());
+			assertEquals(STRING_KEY_PROPERTIES.get(key), value);
+		}
+
+		for (int i = 0; i < INTEGER_KEY_PROPERTIES.size(); i++) {
+			assertTrue(AXDRProperties.encode(INTEGER_KEY_PROPERTIES).getArray().getDataType(i).isStructure());
+			assertTrue(AXDRProperties.encode(INTEGER_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(0).isInteger32());
+			assertTrue(AXDRProperties.encode(INTEGER_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(1).isOctetString());
+			Integer key = new Integer(AXDRProperties.encode(INTEGER_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(0).getInteger32().getValue());
+			String value = new String(AXDRProperties.encode(INTEGER_KEY_PROPERTIES).getArray().getDataType(i).getStructure().getDataType(1).getOctetString().getOctetStr());
+			assertEquals(INTEGER_KEY_PROPERTIES.get(key), value);
+		}
+
+		try {
+			AXDRProperties.encode(INVALID_KEY_PROPERTIES);
+			fail("Expected IOException, but catched nothing.");
+		} catch (Exception e) {
+			assertTrue("Expected IOException, but catched another one: " + e.getClass(), e instanceof IOException);
+		}
+
 	}
 
 	/**
 	 * Test method for {@link com.energyict.dlms.axrdencoding.util.AXDRProperties#decode(com.energyict.dlms.axrdencoding.AbstractDataType)}.
+	 * @throws IOException
 	 */
 	@Test
-	@Ignore
-	public final void testDecode() {
-		fail("Not yet implemented"); // TODO
+	public final void testDecode() throws IOException {
+		assertNotNull(AXDRProperties.decode(null));
+		assertNotNull(AXDRProperties.decode(new NullData()));
+		assertNotNull(AXDRProperties.decode(AXDRProperties.encode(STRING_KEY_PROPERTIES)));
+		assertNotNull(AXDRProperties.decode(AXDRProperties.encode(INTEGER_KEY_PROPERTIES)));
+		assertNotNull(AXDRProperties.decode(AXDRProperties.encode(EMPTY_KEY_PROPERTIES)));
+
+		assertNotNull(AXDRProperties.decode(STRING_KEY_ARRAY));
+		assertNotNull(AXDRProperties.decode(INTEGER_KEY_ARRAY));
+		assertEquals(1, AXDRProperties.decode(STRING_KEY_ARRAY).size());
+		assertEquals(1, AXDRProperties.decode(INTEGER_KEY_ARRAY).size());
+		assertEquals(VALUE1, AXDRProperties.decode(STRING_KEY_ARRAY).get(STRING_KEY));
+		assertEquals(VALUE2, AXDRProperties.decode(INTEGER_KEY_ARRAY).get(INTEGER_KEY));
+
+		try {
+			AXDRProperties.decode(INVALID_KEY_ARRAY);
+			fail("Expected IOException, but catched nothing.");
+		} catch (Exception e) {
+			assertTrue("Expected IOException, but catched another one: " + e.getClass(), e instanceof IOException);
+		}
+
+		try {
+			AXDRProperties.decode(INVALID_VALUE_ARRAY);
+			fail("Expected IOException, but catched nothing.");
+		} catch (Exception e) {
+			assertTrue("Expected IOException, but catched another one: " + e.getClass(), e instanceof IOException);
+		}
+
 	}
 
 }
