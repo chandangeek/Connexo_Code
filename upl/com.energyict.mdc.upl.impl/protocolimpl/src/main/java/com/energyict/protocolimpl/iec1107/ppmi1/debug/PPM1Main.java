ec2e452cf9ef127811cd74310003a74a378423ba
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-01-15 10:46:33 +0000

    Added main class to test and debug the PPM1 protocol
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@38804 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/ppmi1/debug/PPM1Main.java
@@ -0,0 +1,129 @@
+/**
+ *
+ */
+package com.energyict.protocolimpl.iec1107.ppmi1.debug;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Properties;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.energyict.dialer.core.Dialer;
+import com.energyict.dialer.core.DialerFactory;
+import com.energyict.dialer.core.LinkException;
+import com.energyict.dialer.core.SerialCommunicationChannel;
+import com.energyict.protocol.MeterProtocol;
+import com.energyict.protocolimpl.base.DebuggingObserver;
+import com.energyict.protocolimpl.iec1107.ppmi1.PPM;
+import com.energyict.protocolimpl.utils.ProtocolTools;
+
+/**
+ * This class can be used to test the PPM1 protocol without the need of the
+ * CommServerJ, CommServerJOffline, EIServer or ProtocolTester. This class
+ * should only be used for debugging purposes.
+ *
+ * @author jme
+ */
+public class PPM1Main {
+
+	private static final Level	LOG_LEVEL				= Level.SEVERE;
+	private static final String	OBSERVER_FILENAME		= "c:\\logging\\PPM1Main\\communications.log";
+	private static final long	DELAY_BEFORE_DISCONNECT	= 100;
+
+	private static final TimeZone	DEFAULT_TIMEZONE		= TimeZone.getTimeZone("GMT+01");
+	private static final String		COMPORT					= "COM1";
+	private static final int		BAUDRATE				= 300;
+	private static final int		DATABITS				= SerialCommunicationChannel.DATABITS_7;
+	private static final int		PARITY					= SerialCommunicationChannel.PARITY_EVEN;
+	private static final int		STOPBITS				= SerialCommunicationChannel.STOPBITS_1;
+
+	private static PPM ppm = null;
+	private static Dialer dialer = null;
+	private static Logger logger = null;
+
+	public static PPM getPPM() {
+		if (ppm == null) {
+			ppm = new PPM();
+			log("Created new instance of " + ppm.getClass().getCanonicalName() + " [" + ppm.getProtocolVersion() + "]");
+		}
+		return ppm;
+	}
+
+	public static Dialer getDialer() {
+		if (dialer == null) {
+			dialer = DialerFactory.getOpticalDialer().newDialer();
+			dialer.setStreamObservers(new DebuggingObserver(OBSERVER_FILENAME, false));
+		}
+		return dialer;
+	}
+
+	public static Logger getLogger() {
+		 if (logger == null) {
+			 logger = Logger.getLogger(PPM1Main.class.getCanonicalName());
+			 logger.setLevel(LOG_LEVEL);
+		 }
+		 return logger;
+	}
+
+	private static Properties getProperties() {
+		Properties properties = new Properties();
+
+		properties.setProperty(MeterProtocol.MAXTIMEDIFF, "300");
+		properties.setProperty(MeterProtocol.MINTIMEDIFF, "1");
+		properties.setProperty(MeterProtocol.CORRECTTIME, "0");
+
+		properties.setProperty("Retries", "3");
+		properties.setProperty("Timeout", "5000");
+
+		properties.setProperty("OPUS", "0");
+
+		properties.setProperty(MeterProtocol.PROFILEINTERVAL, "1800");
+		properties.setProperty(MeterProtocol.PASSWORD, "--------");
+		properties.setProperty(MeterProtocol.SERIALNUMBER, "--------K9302433");
+
+		return properties;
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws LinkException
+	 */
+	public static void main(String[] args) throws IOException, LinkException {
+
+		getDialer().init(COMPORT);
+		getDialer().getSerialCommunicationChannel().setParams(BAUDRATE, DATABITS, PARITY, STOPBITS);
+		getDialer().connect();
+
+		try {
+			getPPM().setProperties(getProperties());
+			getPPM().init(getDialer().getInputStream(), getDialer().getOutputStream(), DEFAULT_TIMEZONE, getLogger());
+			getPPM().enableHHUSignOn(getDialer().getSerialCommunicationChannel());
+			getPPM().connect();
+
+			Calendar from = Calendar.getInstance();
+			Calendar to = Calendar.getInstance();
+
+			from.set(2009, Calendar.JANUARY, 1, 0, 0, 0);
+			to.set(2010, Calendar.JANUARY, 15, 0, 0, 0);
+			System.out.println(ProtocolTools.getProfileInfo(getPPM().getProfileData(from.getTime(), to.getTime(), false)));
+
+			System.out.println();
+
+
+		} finally {
+			ProtocolTools.delay(DELAY_BEFORE_DISCONNECT);
+			log("Done. Closing connections. \n");
+			getPPM().disconnect();
+			getDialer().disConnect();
+		}
+
+	}
+
+	private static void log(Object message) {
+		getLogger().log(Level.INFO, message == null ? "null" : message.toString());
+	}
+
+}
