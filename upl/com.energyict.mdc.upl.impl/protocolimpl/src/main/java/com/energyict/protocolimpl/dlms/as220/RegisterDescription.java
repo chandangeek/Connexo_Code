7f8d0df872aca3593c9d7282a946a589b3c0a7ea
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-04 23:29:45 +0000

    Updated RegisterDescription
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39203 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/RegisterDescription.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/RegisterDescription.java
@@ -18,6 +18,7 @@ public class RegisterDescription {
 	public static final Map<String, String> INFO = new HashMap<String, String>();
 	static {
 		INFO.put("0.0.26.0.0.255", "PLC S-FSK frequencies channel 1-6");
+		INFO.put("0.0.26.2.0.255", "PLC S-FSK MAC sync timeouts");
 	}
 
 	public static RegisterInfo getRegisterInfo(ObisCode obisCode) {
