0c16e12fe5eb1b8dfee930d547942ecee459eed9
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-04 08:57:07 +0000

    Added cache for SFSKPhyMacSetup for better performance
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39158 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKPhyMacSetup.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKPhyMacSetup.java
@@ -31,7 +31,7 @@ public class SFSKPhyMacSetup extends AbstractCosemObject {
 	private Unsigned16			macAddress							= null;
 	private Array				macGroupAddresses					= null;
 	private TypeEnum			repeater							= null;
-	private BooleanObject		repearterStatus						= null;
+	private BooleanObject		repeaterStatus						= null;
 	private Unsigned8			minDeltaCredit						= null;
 	private Unsigned16			initiatorMacAddress					= null;
 	private BooleanObject		synchronizationLocked				= null;
@@ -59,19 +59,17 @@ public class SFSKPhyMacSetup extends AbstractCosemObject {
 		super(protocolLink, objectReference);
 	}
 
-	public SFSKPhyMacSetup(ProtocolLink protocolLink) {
-		this(protocolLink, new ObjectReference(LN));
-	}
-
 	@Override
 	protected int getClassId() {
 		return DLMSClassId.S_FSK_PHY_MAC_SETUP.getClassId();
 	}
 
 	public TypeEnum getInitiatorElectricalPhase() {
-		try {
-			this.initiatorElectricalPhase = new TypeEnum(getResponseData(ATTRB_INITIATOR_ELECTRICAL_PHASE), 0);
-		} catch (IOException e) {}
+		if (initiatorElectricalPhase == null) {
+			try {
+				this.initiatorElectricalPhase = new TypeEnum(getResponseData(ATTRB_INITIATOR_ELECTRICAL_PHASE), 0);
+			} catch (IOException e) {}
+		}
 		return initiatorElectricalPhase;
 	}
 
@@ -83,30 +81,38 @@ public class SFSKPhyMacSetup extends AbstractCosemObject {
 	}
 
 	public Unsigned8 getMaxReceivingGain() {
-		try {
-			this.maxReceivingGain = new Unsigned8(getResponseData(ATTRB_MAX_RECEIVING_GAIN), 0);
-		} catch (IOException e) {}
+		if (maxReceivingGain == null) {
+			try {
+				this.maxReceivingGain = new Unsigned8(getResponseData(ATTRB_MAX_RECEIVING_GAIN), 0);
+			} catch (IOException e) {}
+		}
 		return maxReceivingGain;
 	}
 
 	public Unsigned8 getMaxTransmittingGain() {
-		try {
-			this.maxTransmittingGain = new Unsigned8(getResponseData(ATTRB_MAX_TRANSMITTING_GAIN), 0);
-		} catch (IOException e) {}
+		if (maxTransmittingGain == null) {
+			try {
+				this.maxTransmittingGain = new Unsigned8(getResponseData(ATTRB_MAX_TRANSMITTING_GAIN), 0);
+			} catch (IOException e) {}
+		}
 		return maxTransmittingGain;
 	}
 
 	public Unsigned8 getSearchInitiatorGain() {
-		try {
-			this.searchInitiatorGain = new Unsigned8(getResponseData(ATTRB_SEARCH_INITIATOR_GAIN), 0);
-		} catch (IOException e) {}
+		if (searchInitiatorGain == null) {
+			try {
+				this.searchInitiatorGain = new Unsigned8(getResponseData(ATTRB_SEARCH_INITIATOR_GAIN), 0);
+			} catch (IOException e) {}
+		}
 		return searchInitiatorGain;
 	}
 
 	public Array getFrequencies() {
-		try {
-			this.frequencies = new Array(getResponseData(ATTRB_FREQUENCIES), 0, 0);
-		} catch (IOException e) {}
+		if (frequencies == null) {
+			try {
+				this.frequencies = new Array(getResponseData(ATTRB_FREQUENCIES), 0, 0);
+			} catch (IOException e) {}
+		}
 		return frequencies;
 	}
 
@@ -118,24 +124,28 @@ public class SFSKPhyMacSetup extends AbstractCosemObject {
 	}
 
 	public Array getMacGroupAddresses() {
-		try {
-			this.macGroupAddresses = new Array(getResponseData(ATTRB_MAC_GROUP_ADDRESSES), 0, 0);
-		} catch (IOException e) {}
+		if (macGroupAddresses == null) {
+			try {
+				this.macGroupAddresses = new Array(getResponseData(ATTRB_MAC_GROUP_ADDRESSES), 0, 0);
+			} catch (IOException e) {}
+		}
 		return macGroupAddresses;
 	}
 
 	public TypeEnum getRepeater() {
-		try {
-			this.repeater = new TypeEnum(getResponseData(ATTRB_REPEATER), 0);
-		} catch (IOException e) {}
+		if (repeater == null) {
+			try {
+				this.repeater = new TypeEnum(getResponseData(ATTRB_REPEATER), 0);
+			} catch (IOException e) {}
+		}
 		return repeater;
 	}
 
-	public BooleanObject getRepearterStatus() {
+	public BooleanObject getRepeaterStatus() {
 		try {
-			this.repearterStatus = new BooleanObject(getResponseData(ATTRB_REPEATER_STATUS), 0);
+			this.repeaterStatus = new BooleanObject(getResponseData(ATTRB_REPEATER_STATUS), 0);
 		} catch (IOException e) {}
-		return repearterStatus;
+		return repeaterStatus;
 	}
 
 	public Unsigned8 getMinDeltaCredit() {
@@ -159,6 +169,16 @@ public class SFSKPhyMacSetup extends AbstractCosemObject {
 		return synchronizationLocked;
 	}
 
+	public void clearCache() {
+		initiatorElectricalPhase = null;
+		maxReceivingGain = null;
+		maxTransmittingGain = null;
+		searchInitiatorGain = null;
+		frequencies = null;
+		macGroupAddresses = null;
+		repeater = null;
+	}
+
 	@Override
 	public String toString() {
 		final String crlf = "\r\n";
@@ -173,7 +193,7 @@ public class SFSKPhyMacSetup extends AbstractCosemObject {
 		sb.append(" > macAddress = ").append(getMacAddress().getValue()).append(crlf);
 		sb.append(" > macGroupAddresses = ").append(getMacGroupAddresses().toString().replace("\n", " ").replace("\r", "")).append(crlf);
 		sb.append(" > repeater = ").append(getRepeater().getValue()).append(crlf);
-		sb.append(" > repearterStatus = ").append(getRepearterStatus().getState()).append(crlf);
+		sb.append(" > repearterStatus = ").append(getRepeaterStatus().getState()).append(crlf);
 		sb.append(" > minDeltaCredit = ").append(getMinDeltaCredit().getValue()).append(crlf);
 		sb.append(" > initiatorMacAddress = ").append(getInitiatorMacAddress().getValue()).append(crlf);
 		sb.append(" > synchronizationLocked = ").append(getSynchronizationLocked().getState()).append(crlf);
