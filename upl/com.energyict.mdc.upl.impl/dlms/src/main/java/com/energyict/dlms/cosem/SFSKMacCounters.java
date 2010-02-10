5db25ffcd83c20cfa969276933a21ae772183637
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-10 20:56:36 +0000

    Fixed build errors
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39320 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKMacCounters.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKMacCounters.java
@@ -2,6 +2,7 @@ package com.energyict.dlms.cosem;
 
 import com.energyict.dlms.ProtocolLink;
 import com.energyict.obis.ObisCode;
+import com.energyict.protocol.RegisterValue;
 
 public class SFSKMacCounters extends AbstractCosemObject {
 
@@ -16,6 +17,10 @@ public class SFSKMacCounters extends AbstractCosemObject {
 
 	public static ObisCode getObisCode() {
 		return ObisCode.fromString("0.0.26.3.0.255");
+	}
+
+	public RegisterValue asRegisterValue() {
+		return new RegisterValue(getObisCode());
 	}
 
 }
