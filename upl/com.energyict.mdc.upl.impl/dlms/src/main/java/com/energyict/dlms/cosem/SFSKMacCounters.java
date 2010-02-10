82a192613c9d475b01b211d780e8c23fabc553ff
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-10 20:35:42 +0000

    Added temporary SFSKMacCounters to fix build problems
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39319 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKMacCounters.java
@@ -0,0 +1,21 @@
+package com.energyict.dlms.cosem;
+
+import com.energyict.dlms.ProtocolLink;
+import com.energyict.obis.ObisCode;
+
+public class SFSKMacCounters extends AbstractCosemObject {
+
+	public SFSKMacCounters(ProtocolLink protocolLink, ObjectReference objectReference) {
+		super(protocolLink, objectReference);
+	}
+
+	@Override
+	protected int getClassId() {
+		return DLMSClassId.S_FSK_MAC_COUNTERS.getClassId();
+	}
+
+	public static ObisCode getObisCode() {
+		return ObisCode.fromString("0.0.26.3.0.255");
+	}
+
+}
