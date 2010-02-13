75aeebeac00e05532a2b1b4bf8c958c46c92ed90
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-13 09:11:07 +0000

    Added SFSKIec61334LLCSetup CosemObject
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39414 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKIec61334LLCSetup.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/cosem/SFSKIec61334LLCSetup.java
@@ -3,9 +3,12 @@
  */
 package com.energyict.dlms.cosem;
 
+import java.io.IOException;
+
 import com.energyict.dlms.ProtocolLink;
 import com.energyict.dlms.RegisterReadable;
-import com.energyict.dlms.axrdencoding.Unsigned16;
+import com.energyict.dlms.axrdencoding.Array;
+import com.energyict.dlms.axrdencoding.Unsigned8;
 import com.energyict.obis.ObisCode;
 import com.energyict.protocol.RegisterValue;
 
@@ -18,13 +21,12 @@ public class SFSKIec61334LLCSetup extends AbstractCosemObject implements Registe
 	private static final byte[]	LN	= ObisCode.fromString("0.0.26.5.0.255").getLN();
 
 	/** Attributes */
-	private Unsigned16			maxFrameLength			= null;
+	private Unsigned8			maxFrameLength			= null;
+	private Array				replyStatusList			= null;
 
 	/** Attribute numbers */
 	private static final int	ATTRB_MAX_FRAME_LENGTH	= 0x08;
-	private static final int	ATTRB_SYNC_CONFIRM_TIMEOUT		= 0x10;
-	private static final int	ATTRB_TIMEOUT_NOT_ADDRESSED		= 0x18;
-	private static final int	ATTRB_TIMEOUT_FRAME_NOT_OK		= 0x20;
+	private static final int	ATTRB_REPLY_STATUS_LIST	= 0x10;
 
 
 	public static ObisCode getObisCode() {
@@ -40,16 +42,35 @@ public class SFSKIec61334LLCSetup extends AbstractCosemObject implements Registe
 		super(protocolLink, objectReference);
 	}
 
+	public Unsigned8 getMaxFrameLength() {
+		try {
+			maxFrameLength = new Unsigned8(getResponseData(ATTRB_MAX_FRAME_LENGTH), 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return maxFrameLength;
+	}
+
+	public Array getReplyStatusList() {
+		try {
+			replyStatusList = new Array(getResponseData(ATTRB_REPLY_STATUS_LIST), 0, 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return replyStatusList;
+	}
 
 	@Override
 	public String toString() {
 		final String crlf = "\r\n";
 
-		Unsigned16 temp = null;
+		Unsigned8 maxFrameLength = getMaxFrameLength();
+		Array replyStatusList = getReplyStatusList();
 
 		StringBuffer sb = new StringBuffer();
 		sb.append("SFSKIec61334LLCSetup").append(crlf);
-		sb.append(" > temp = ").append(temp != null ? temp.getValue() : null).append(crlf);
+		sb.append(" > maxFrameLength = ").append(maxFrameLength != null ? maxFrameLength.getValue() : null).append(crlf);
+		sb.append(" > replyStatusList = ").append(replyStatusList != null ? replyStatusList : null).append(crlf);
 		return sb.toString();
 	}
 
