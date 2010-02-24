6832545b343c2373b1ffe0c9b65ac32f94421098
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-24 17:08:16 +0000

    Added new sfsk mapper
    COMMUNICATION-22
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39736 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/plc/SFSKSyncTimeoutsMapper.java
@@ -0,0 +1,76 @@
+/**
+ *
+ */
+package com.energyict.protocolimpl.dlms.as220.plc;
+
+import java.io.IOException;
+
+import com.energyict.dlms.cosem.SFSKSyncTimeouts;
+import com.energyict.dlms.cosem.attributes.SFSKSyncTimeoutsAttribute;
+import com.energyict.obis.ObisCode;
+import com.energyict.protocol.RegisterInfo;
+import com.energyict.protocol.RegisterValue;
+import com.energyict.protocolimpl.base.AbstractDLMSAttributeMapper;
+import com.energyict.protocolimpl.dlms.as220.AS220;
+
+/**
+ * @author jme
+ *
+ */
+public class SFSKSyncTimeoutsMapper extends AbstractDLMSAttributeMapper {
+
+	private final AS220	as220;
+	private SFSKSyncTimeouts sFSKSyncTimeouts = null;
+
+	public SFSKSyncTimeoutsMapper(ObisCode baseObisCode, AS220 as220) {
+		super(baseObisCode);
+		this.as220 = as220;
+	}
+
+	public int[] getSupportedAttributes() {
+		return new int[] {
+				SFSKSyncTimeoutsAttribute.SEARCH_INITIATOR_TIMEOUT.getAttributeNumber(),
+				SFSKSyncTimeoutsAttribute.SYNCHRONIZATION_CONFIRMATION_TIMEOUT.getAttributeNumber(),
+				SFSKSyncTimeoutsAttribute.TIME_OUT_NOT_ADDRESSED.getAttributeNumber(),
+				SFSKSyncTimeoutsAttribute.TIME_OUT_FRAME_NOT_OK.getAttributeNumber()
+		};
+	}
+
+	@Override
+	protected RegisterInfo doGetAttributeInfo(int attributeNr) {
+		SFSKSyncTimeoutsAttribute attribute = SFSKSyncTimeoutsAttribute.findByAttributeNumber(attributeNr);
+		if (attribute != null) {
+			return new RegisterInfo("SFSKActiveInitiator attribute " + attributeNr + ": " + attribute);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	protected RegisterValue doGetAttributeValue(int attributeNr) throws IOException {
+		return getsFSKSyncTimeouts().asRegisterValue(attributeNr);
+	}
+
+	/**
+	 * Getter for the {@link SFSKSyncTimeouts} dlms object
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	public SFSKSyncTimeouts getsFSKSyncTimeouts() throws IOException {
+		if (sFSKSyncTimeouts == null) {
+			sFSKSyncTimeouts = getAs220().getCosemObjectFactory().getSFSKSyncTimeouts(getBaseObjectObisCode());
+		}
+		return sFSKSyncTimeouts;
+	}
+
+	/**
+	 * Getter for the {@link AS220} protocol
+	 *
+	 * @return
+	 */
+	public AS220 getAs220() {
+		return as220;
+	}
+
+}
