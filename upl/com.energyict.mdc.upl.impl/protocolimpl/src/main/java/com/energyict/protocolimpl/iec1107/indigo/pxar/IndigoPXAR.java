10281633e87c3f79e5be60336c769ee4b9eff323
Govanni Naessens <Govanni.Naessens@elster.com>
2010-02-25 12:16:44 +0000

    COMMUNICATION-6 Fist start (merge from 8.3) for the PXAR protocol
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39761 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/indigo/pxar/IndigoPXAR.java
@@ -0,0 +1,89 @@
+package com.energyict.protocolimpl.iec1107.indigo.pxar;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import com.energyict.protocol.InvalidPropertyException;
+import com.energyict.protocol.MissingPropertyException;
+import com.energyict.protocolimpl.iec1107.AbstractIEC1107Protocol;
+
+/**
+ * 
+ * Protocol implementation for the Indigo PXA/R meter. The meter can be read with an Optical head or via RS232. 
+ * Both interfaces conform to IEC1107 mode C, with exception that the RS232 interface does not include baudrate switching.
+ * <br><br>
+ * There are two main versions of the PXA/R meter software, the latter version conforms to code of practice five.
+ * The differences between those two are as follows:
+ * <dd>- VTCT ration combinations are held in a different format<br>
+ * <dd>- Tariff registers contain a different number of decimal places<br>
+ * <dd>- The definition of which quantities are being stored in the interval data is different<br>
+ * <dd>- Code 5 meters can store a new interval quantity of "status flags"<br>
+ * <dd>- One of the non-code 5 rate dependent maximum demand registers has been re-defined in code 5
+ * meters as a continuous maximum demand register of different units to the main demand registers.<br>
+ * <dd>- Code 5 meters can be configured to provide password security read operations<br>
+ * <dd>- On code 5 meters an IEC1107 secondary protocol can be used which is quicker<br>
+ * <dd>- On code 5 meters it is possible to uniquely address a particular meter so that meters may share a modem<br>
+ * <br>
+ * Code 5 meters can be identified as such by:<br>
+ * <dd>a/ Their dial plate (not so useful for protocol development<br>
+ * <dd>b/ Their software version number (four BCD nibbles at address 0x0B00, 04.xx or 05.xx = code5, 02.xx = non-code5)<br>
+ * <dd>c/ Their meter type as held at 0x0FA8 in two BCD nibbles, and also passed in the IEC sing on ident message.
+ * (code 5 meter types have values 13 to 20, non-code 5 meter types are 04 to 11)<br>
+ * <dd>d/ IEC meters (ie. interval recording meters) have a manufacturer ident message beginning with "SLb" as opposed to 
+ * non-code 5 meters which begin "SLB". 
+ * <br>
+ * <b>Important Note:</b><br>
+ * If the meter is in the sign-on state, then most of the data is <i>not</i> updated.
+ * This means the date/time will be the same if you read it twice in the same session.
+ * 
+ * @author gna
+ * @since 5-feb-2010
+ *
+ */
+public class IndigoPXAR extends AbstractIEC1107Protocol {
+	
+	/**
+	 * Default constructor
+	 */
+	public IndigoPXAR(){
+		super();
+		//TODO if necessary, add additional information to the super() [Like an encryptor for security]
+	}
+	
+	/**
+	 * @return the ProtocolVerison 
+	 */
+    public String getProtocolVersion() {
+        return "$Date$";
+    }
+
+    /**
+     * Add additional functionality after a physical connect, like initializing some objects
+     */
+	@Override
+	protected void doConnect() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @return a List of Strings of optional keys
+	 */
+	@Override
+	protected List doGetOptionalKeys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Validate some protocol specific properties
+	 */
+	@Override
+	protected void doValidateProperties(Properties properties)
+			throws MissingPropertyException, InvalidPropertyException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
