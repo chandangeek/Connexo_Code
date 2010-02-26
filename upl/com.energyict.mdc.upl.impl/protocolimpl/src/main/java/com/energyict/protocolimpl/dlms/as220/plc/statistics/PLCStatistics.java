a49e76f51cf4fcd6789f15eb85ad5a85aa7f1b36
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-02-26 16:36:38 +0000

    Removed cummulative setter from plc statistics. AM500 is dealing allready with cummulative values in the profile COMMUNICATION-37
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@39805 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/plc/statistics/PLCStatistics.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/plc/statistics/PLCStatistics.java
@@ -89,13 +89,6 @@ public class PLCStatistics extends Array {
 		ChannelInfo fCor = new ChannelInfo(7, "Frames corrected", countUnit);
 		ChannelInfo fBad = new ChannelInfo(8, "Bad frames", countUnit);
 
-		crcOk.setCumulative();
-		crcNotOk.setCumulative();
-		fTx.setCumulative();
-		fRep.setCumulative();
-		fCor.setCumulative();
-		fBad.setCumulative();
-
 		List<ChannelInfo> channelInfos = new ArrayList<ChannelInfo>();
 		channelInfos.add(snr0);
 		channelInfos.add(snr1);
