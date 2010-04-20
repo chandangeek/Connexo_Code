2cb4a72b4d2d3c170bde2496f8ca3aa6fd25966c
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-04-20 11:21:34 +0000

    Added comments and extra helper methods
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@40840 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/utils/communicationdump/CommunicationDumpFile.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/utils/communicationdump/CommunicationDumpFile.java
@@ -1,26 +1,35 @@
 package com.energyict.protocolimpl.utils.communicationdump;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
+/**
+ * This class is a representation of a 'Full debug log file' from the CommServerJ 
+ */
 public class CommunicationDumpFile {
 
 	private List<CommunicationDumpEntry> entries = new ArrayList<CommunicationDumpEntry>();
 
+    /**
+     * Create a new CommunicationDumpFile from a given 'Full debug log file' from the CommServerJ
+     * @param fileName
+     */
 	public CommunicationDumpFile(String fileName) {
 		this(new File(fileName));
 	}
 
+    /**
+     * Create a new CommunicationDumpFile from a given 'Full debug log file' from the CommServerJ
+     * @param file
+     */
 	public CommunicationDumpFile(File file) {
 		readEntriesFromFile(file);
 	}
 
+    /**
+     * Open the given file, parse the data from it, and store it as a list of CommunicationDumpEntries
+     * @param file
+     */
 	private void readEntriesFromFile(File file) {
 		BufferedReader reader = null;
 		try {
@@ -53,11 +62,15 @@ public class CommunicationDumpFile {
 		}
 	}
 
+    /**
+     * Getter for the CommunicationDumpEntries 
+     * @return
+     */
 	public List<CommunicationDumpEntry> getEntries() {
 		return entries;
 	}
 
-	public String toString() {
+    public String toString() {
 		StringBuilder sb = new StringBuilder();
 		List entryList = getEntries();
 		for (Iterator iterator = entryList.iterator(); iterator.hasNext();) {
@@ -70,7 +83,11 @@ public class CommunicationDumpFile {
 		return sb.toString();
 	}
 
-	public String toStringAscii() {
+    /**
+     * Same as the toString() method, but the data is shown as readable ascii values where possible
+     * @return a human readable representation of the communicationDumpEntries
+     */
+    public String toStringAscii() {
 		StringBuilder sb = new StringBuilder();
 		List entryList = getEntries();
 		for (Iterator iterator = entryList.iterator(); iterator.hasNext();) {
@@ -83,7 +100,11 @@ public class CommunicationDumpFile {
 		return sb.toString();
 	}
 
-	public List<CommunicationDumpEntry> getRxEntries() {
+    /**
+     * Getter for the RX CommunicationDumpEntries
+     * @return
+     */
+    public List<CommunicationDumpEntry> getRxEntries() {
 		List<CommunicationDumpEntry> rxEntries = new ArrayList<CommunicationDumpEntry>();
 		for (CommunicationDumpEntry cde : entries) {
 			if (cde.isRx()) {
@@ -93,7 +114,11 @@ public class CommunicationDumpFile {
 		return rxEntries;
 	}
 
-	public List<CommunicationDumpEntry> getTxEntries() {
+    /**
+     * Getter for the TX CommunicationDumpEntries
+     * @return
+     */
+    public List<CommunicationDumpEntry> getTxEntries() {
 		List<CommunicationDumpEntry> txEntries = new ArrayList<CommunicationDumpEntry>();
 		for (CommunicationDumpEntry cde : entries) {
 			if (cde.isTx()) {
@@ -103,4 +128,54 @@ public class CommunicationDumpFile {
 		return txEntries;
 	}
 
+    /**
+     * Get an CommunicationDumpEntry by his sequence number
+     * @param sequence
+     * @return the CommunicationDumpEntry or null if this sequenceNumber does not exist
+     */
+    public CommunicationDumpEntry getCommunicationDumpEntry(int sequence) {
+        for (CommunicationDumpEntry entry : getEntries()) {
+            if (entry.getSequenceNumber() == sequence) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the CommunicationDumpEntry before a given CommunicationDumpEntry
+     * @param communicationDumpEntry
+     * @return
+     */
+    public CommunicationDumpEntry getPreviousEntry(CommunicationDumpEntry communicationDumpEntry) {
+        return getCommunicationDumpEntry(communicationDumpEntry.getSequenceNumber()-1);
+    }
+
+    /**
+     * Get the CommunicationDumpEntry after a given CommunicationDumpEntry
+     * @param communicationDumpEntry
+     * @return
+     */
+    public CommunicationDumpEntry getNextEntry(CommunicationDumpEntry communicationDumpEntry) {
+        return getCommunicationDumpEntry(communicationDumpEntry.getSequenceNumber()+1);
+    }
+
+    /**
+     * Get the next RX CommunicationDumpEntry before a given TX CommunicationDumpEntry
+     * @param communicationDumpEntry
+     * @return
+     */
+    public CommunicationDumpEntry getResponseOn(CommunicationDumpEntry communicationDumpEntry) {
+        CommunicationDumpEntry response = null;
+        if (communicationDumpEntry.isTx()) {
+            CommunicationDumpEntry nextEntry = getNextEntry(communicationDumpEntry);
+                if ((nextEntry != null) && (nextEntry.isTx())) {
+                    getResponseOn(nextEntry);
+                } else {
+                    response = nextEntry;
+                }
+        }
+        return response;
+    }
+    
 }
