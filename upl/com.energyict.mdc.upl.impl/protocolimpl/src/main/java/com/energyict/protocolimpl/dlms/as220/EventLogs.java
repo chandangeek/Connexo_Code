5309ea1ded84a2e17d443a58e6b6e2be6bb6aaa9
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-09-20 11:29:08 +0000

    COMMUNICATION-257 - AM500 protocol should check if logbook is available Made reading of logbooks more save (check if exists before reading)
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@43190 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/EventLogs.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/dlms/as220/EventLogs.java
@@ -12,55 +12,62 @@ import java.util.*;
  */
 public class EventLogs {
 
-	private static final ObisCode	STANDARD_EVENTLOG_OBISCODE	= ObisCode.fromString("0.0.99.98.0.255");
-    private static final ObisCode	DISCONNEC_EVENTLOG_OBISCODE	= ObisCode.fromString("0.0.99.98.2.255");
+    private static final ObisCode STANDARD_EVENTLOG_OBISCODE = ObisCode.fromString("0.0.99.98.0.255");
+    private static final ObisCode DISCONNEC_EVENTLOG_OBISCODE = ObisCode.fromString("0.0.99.98.2.255");
 
-	private final DLMSSNAS220 as220;
+    private final DLMSSNAS220 as220;
 
-	public EventLogs(DLMSSNAS220 as220) {
-		this.as220 = as220;
-	}
+    public EventLogs(DLMSSNAS220 as220) {
+        this.as220 = as220;
+    }
 
-	/**
-	 * Getter for the as220 protocol
-	 * @return
-	 */
-	private DLMSSNAS220 getAs220() {
-		return as220;
-	}
+    /**
+     * Getter for the as220 protocol
+     *
+     * @return
+     */
+    private DLMSSNAS220 getAs220() {
+        return as220;
+    }
 
-	/**
-	 * @param fromCalendar
-	 * @param toCalendar
-	 * @return
-	 * @throws IOException
-	 */
-	public List<MeterEvent> getEventLog(Calendar fromCalendar, Calendar toCalendar) throws IOException {
-		List<MeterEvent> meterEvents = new ArrayList<MeterEvent>();
-		meterEvents.addAll(readLogbook(STANDARD_EVENTLOG_OBISCODE, fromCalendar, toCalendar));
+    /**
+     * @param fromCalendar
+     * @param toCalendar
+     * @return
+     * @throws IOException
+     */
+    public List<MeterEvent> getEventLog(Calendar fromCalendar, Calendar toCalendar) {
+        List<MeterEvent> meterEvents = new ArrayList<MeterEvent>();
+        meterEvents.addAll(readLogbook(STANDARD_EVENTLOG_OBISCODE, fromCalendar, toCalendar));
         meterEvents.addAll(readLogbook(DISCONNEC_EVENTLOG_OBISCODE, fromCalendar, toCalendar));
-		return meterEvents;
-	}
+        return meterEvents;
+    }
 
-	/**
-	 * @param obisCode
-	 * @param fromCalendar
-	 * @param toCalendar
-	 * @return
-	 * @throws IOException
-	 */
-	private List<MeterEvent> readLogbook(ObisCode obisCode, Calendar fromCalendar, Calendar toCalendar) throws IOException {
-		DataContainer dc = getAs220().getCosemObjectFactory().getProfileGeneric(obisCode).getBuffer(fromCalendar, toCalendar);
-		List<MeterEvent> meterEvents = new ArrayList<MeterEvent>();
-		for (int i = 0; i < dc.getRoot().getNrOfElements(); i++) {
-			Date dateTime = dc.getRoot().getStructure(i).getOctetString(0).toDate(getAs220().getTimeZone());
-			int id = dc.getRoot().getStructure(i).getInteger(1);
-			MeterEvent meterEvent = EventNumber.toMeterEvent(id, dateTime);
-			if (meterEvent != null) {
-				meterEvents.add(meterEvent);
-			}
-		}
-		return meterEvents;
+    /**
+     * @param obisCode
+     * @param fromCalendar
+     * @param toCalendar
+     * @return
+     * @throws IOException
+     */
+    private List<MeterEvent> readLogbook(ObisCode obisCode, Calendar fromCalendar, Calendar toCalendar) {
+        List<MeterEvent> meterEvents = new ArrayList<MeterEvent>();
+        if (getAs220().getMeterConfig().isObisCodeInObjectList(obisCode)) {
+            try {
+                DataContainer dc = getAs220().getCosemObjectFactory().getProfileGeneric(obisCode).getBuffer(fromCalendar, toCalendar);
+                for (int i = 0; i < dc.getRoot().getNrOfElements(); i++) {
+                    Date dateTime = dc.getRoot().getStructure(i).getOctetString(0).toDate(getAs220().getTimeZone());
+                    int id = dc.getRoot().getStructure(i).getInteger(1);
+                    MeterEvent meterEvent = EventNumber.toMeterEvent(id, dateTime);
+                    if (meterEvent != null) {
+                        meterEvents.add(meterEvent);
+                    }
+                }
+            } catch (IOException e) {
+                // Absorb
+            }
+        }
+        return meterEvents;
 	}
 
 }
