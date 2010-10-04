1ce347673f5e12fe730d92169b922d7718e44dd4
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-10-04 13:06:26 +0000

    COMMUNICATION-269 - Implement encryption for CTR protocol Added empty encryption class
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@43505 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/encryption/CTREncryption.java
@@ -0,0 +1,20 @@
+package com.energyict.genericprotocolimpl.elster.ctr.encryption;
+
+import com.energyict.genericprotocolimpl.elster.ctr.frame.Frame;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 4-okt-2010
+ * Time: 14:04:03
+ */
+public class CTREncryption {
+
+    public Frame encryptFrame(Frame frame) {
+        return frame;
+    }
+
+    public Frame decryptFrame(Frame frame) {
+        return frame;
+    }
+
+}
