1f5b58b937af55e2ddc0ca810c887f0ff9e0d48b
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-10-04 13:06:45 +0000

    COMMUNICATION-258 Implement the CTR objects (parsers) Added frame interface
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@43506 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/frame/Frame.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/frame/Frame.java
@@ -1,6 +1,7 @@
 package com.energyict.genericprotocolimpl.elster.ctr.frame;
 
 import com.energyict.genericprotocolimpl.elster.ctr.common.Field;
+import com.energyict.genericprotocolimpl.elster.ctr.frame.field.*;
 
 /**
  * Copyrights EnergyICT
@@ -8,5 +9,37 @@ import com.energyict.genericprotocolimpl.elster.ctr.common.Field;
  * Time: 8:32:29
  */
 public interface Frame<T extends Frame> extends Field<T> {
+
+    public Address getAddress();
+
+    public void setAddress(Address address);
+
+    public Channel getChannel();
+
+    public void setChannel(Channel channel);
+
+    public Cpa getCpa();
+
+    public void setCpa(Cpa cpa);
+
+    public Crc getCrc();
+
+    public void setCrc(Crc crc);
+
+    public Data getData();
+
+    public void setData(Data data);
+
+    public FunctionCode getFunctionCode();
+
+    public void setFunctionCode(FunctionCode functionCode);
+
+    public Profi getProfi();
+
+    public void setProfi(Profi profi);
+
+    public StructureCode getStructureCode();
+
+    public void setStructureCode(StructureCode structureCode);
 
 }
