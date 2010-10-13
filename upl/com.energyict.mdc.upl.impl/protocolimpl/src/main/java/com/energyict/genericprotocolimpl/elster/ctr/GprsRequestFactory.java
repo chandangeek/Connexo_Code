1c97d4d23e6e50cc22e5f3b186fbb89d4fc5a94d
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-10-13 07:26:56 +0000

    COMMUNICATION-212 - Support the CTR protocol for use over GPRS Small code changes to gprs factory + comments
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@43774 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/GprsRequestFactory.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/GprsRequestFactory.java
@@ -26,28 +26,31 @@ public class GprsRequestFactory {
     private MTU155Properties properties;
     private Logger logger;
 
+    /**
+     *
+     * @param link
+     * @param logger
+     * @param properties
+     */
     public GprsRequestFactory(Link link, Logger logger, MTU155Properties properties) {
         this.connection = new SecureCtrConnection(link.getInputStream(), link.getOutputStream(), properties);
         this.logger = logger;
         this.properties = properties;
     }
 
-    public GPRSFrame getIdentificationRequest() {
-        GPRSFrame request = new GPRSFrame();
-        request.setAddress(getAddress());
-        request.getFunctionCode().setEncryptionStatus(EncryptionStatus.NO_ENCRYPTION);
-        request.getFunctionCode().setFunction(Function.IDENTIFICATION_REQUEST);
-        request.getProfi().setLongFrame(false);
-        request.getStructureCode().setStructureCode(StructureCode.IDENTIFICATION);
-        request.setData(new IdentificationRequestStructure());
-        request.setCpa(new Cpa(0x00));
-        return request;
-    }
-
+    /**
+     * Getter for the connection
+     * @return
+     */
     public CtrConnection getConnection() {
         return connection;
     }
 
+    /**
+     * Getter for the logger object.
+     * If there is no logger, create a new one
+     * @return
+     */
     public Logger getLogger() {
         if (this.logger == null) {
             this.logger = Logger.getLogger(getClass().getName());
@@ -55,6 +58,10 @@ public class GprsRequestFactory {
         return logger;
     }
 
+    /**
+     * Getter for the protocol properties
+     * @return
+     */
     public MTU155Properties getProperties() {
         if (properties == null) {
             this.properties = new MTU155Properties();
@@ -62,17 +69,13 @@ public class GprsRequestFactory {
         return properties;
     }
 
-    public List<AbstractCTRObject> queryRegisters(AttributeType attributeType, CTRObjectID... objectId) {
-        List<AbstractCTRObject> registerList = new ArrayList<AbstractCTRObject>();
-
-        // request
-
-        for (CTRObjectID objectID : objectId) {
-
-        }
-        return registerList;
+    /**
+     * Create a new address, with a value from the protocolProperties
+     * @return
+     */
+    public Address getAddress() {
+        return new Address(getProperties().getAddress());
     }
-
 
     /**
      * @return
@@ -87,7 +90,37 @@ public class GprsRequestFactory {
         }
     }
 
-    public Address getAddress() {
-        return new Address(getProperties().getAddress());
+    /**
+     *
+     * @return
+     */
+    private GPRSFrame getIdentificationRequest() {
+        GPRSFrame request = new GPRSFrame();
+        request.setAddress(getAddress());
+        request.getFunctionCode().setEncryptionStatus(EncryptionStatus.NO_ENCRYPTION);
+        request.getFunctionCode().setFunction(Function.IDENTIFICATION_REQUEST);
+        request.getProfi().setLongFrame(false);
+        request.getStructureCode().setStructureCode(StructureCode.IDENTIFICATION);
+        request.setData(new IdentificationRequestStructure());
+        request.setCpa(new Cpa(0x00));
+        return request;
     }
+
+    /**
+     * 
+     * @param attributeType
+     * @param objectId
+     * @return
+     */
+    public List<AbstractCTRObject> queryRegisters(AttributeType attributeType, CTRObjectID... objectId) {
+        List<AbstractCTRObject> registerList = new ArrayList<AbstractCTRObject>();
+
+        // request
+
+        for (CTRObjectID objectID : objectId) {
+
+        }
+        return registerList;
+    }
+
 }
