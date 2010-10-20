3616b03f7ffc96a3b6a52f44b9a37f9337d54d68
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2010-10-20 15:06:51 +0000

    COMMUNICATION-212 - Support the CTR protocol for use over GPRS Added NACK reasons
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@43962 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/structure/field/NackReason.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/ctr/structure/field/NackReason.java
@@ -33,4 +33,37 @@ public class NackReason extends AbstractField<NackReason> {
         return this;
     }
 
+    public String getDescription() {
+        String reason = "[" + getReason() + "] ";
+        switch (getReason()) {
+            case 0x040: reason += "Generic Can be used as generic NACK in cases where there are no specific codes."; break;
+            case 0x041: reason += "Access denied If the password has not been recognized."; break;
+            case 0x042: reason += "Function not implemented The function has not been implemented in the application."; break;
+            case 0x043: reason += "Response to the Write. An object or one of its attributes is not supported by the application."; break;
+            case 0x044: reason += "Data structure not implemented. The data structure is not supported by the application."; break;
+            case 0x045: reason += "Response to the Query (Overflow). More data items have been requested than the permitted number."; break;
+            case 0x046: reason += "Incorrect data field. An error in the data field has been detected for the command received."; break;
+            case 0x047: reason += "Function not allowed. The requested function is not allowed because privileges are not held."; break;
+            case 0x048: reason += "Function $ - Down Loader Downloader cannot be enabled due to inconsistency between parameters."; break;
+            case 0x049: reason += "Non-availability. The Client process is temporarily busy and cannot execute the command."; break;
+            case 0x04A: reason += "Response to the Write. A write operation was not executed due to a session error."; break;
+            case 0x04B: reason += "Response to the Execute Function. The execute function specified by Obj.id cannot be executed for reasons other than privileges."; break;
+            case 0x04C: reason += "Encryption error CPA irregularity detected after decryption."; break;
+            case 0x04D: reason += "Function D-Down Loading One DL segment not received correctly."; break;
+            case 0x04E: reason += "Response to Write The Write cannot be accepted because the date of validity has expired."; break;
+            case 0x04F: reason += "Response to The Execute cannot be accepted."; break;
+            case 0x050: reason += "Response to Write or Execute. The Write or Execute cannot be executed because the condition is ‘under maintenance’."; break;
+            default: reason += "Unknown NACK reason code."; break;
+        }
+        return reason;
+    }
+
+    public int getReason() {
+        return reason;
+    }
+
+    @Override
+    public String toString() {
+        return getDescription();
+    }
 }
