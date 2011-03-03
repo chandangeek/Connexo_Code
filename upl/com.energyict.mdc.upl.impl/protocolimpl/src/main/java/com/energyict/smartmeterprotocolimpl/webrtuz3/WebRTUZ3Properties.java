dd2b3eeed34b1a58104f8b689286639e96115963
Govanni Naessens <Govanni.Naessens@elster.com>
2011-03-03 07:48:33 +0000

    COMMUNICATION-218 - Implement profiles for the Z3 and make the WebRtuZ3 protocol more generic
    A draft with some Z3 registerSupport.
    
    Some protocolTester changes have been done to make it compatible ...
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@46929 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/smartmeterprotocolimpl/webrtuz3/WebRTUZ3Properties.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/smartmeterprotocolimpl/webrtuz3/WebRTUZ3Properties.java
@@ -1,7 +1,8 @@
 package com.energyict.smartmeterprotocolimpl.webrtuz3;
 
 import com.energyict.dlms.DLMSReference;
-import com.energyict.dlms.common.DlmsProtocolProperties;
+import com.energyict.protocolimpl.base.ProtocolProperty;
+import com.energyict.protocolimpl.dlms.common.DlmsProtocolProperties;
 import com.energyict.protocol.InvalidPropertyException;
 import com.energyict.protocol.MissingPropertyException;
 
@@ -15,13 +16,29 @@ import java.util.List;
  */
 public class WebRTUZ3Properties extends DlmsProtocolProperties {
 
+    private static final String MaxReceivePduSize = "4096";
+    private static final String DefaultZ3BulkRequesSupport = "1";
+
     public List<String> getOptionalKeys() {
         List<String> optional = new ArrayList<String>();
+        optional.add(DlmsProtocolProperties.ADDRESSING_MODE);
+        optional.add(DlmsProtocolProperties.CLIENT_MAC_ADDRESS);
+        optional.add(DlmsProtocolProperties.SERVER_MAC_ADDRESS);
+        optional.add(DlmsProtocolProperties.CONNECTION);
+        optional.add(DlmsProtocolProperties.SERVER_MAC_ADDRESS);
+        optional.add(DlmsProtocolProperties.FORCED_DELAY);
+        optional.add(DlmsProtocolProperties.DELAY_AFTER_ERROR);
+        optional.add(DlmsProtocolProperties.INFORMATION_FIELD_SIZE);
+        optional.add(DlmsProtocolProperties.MAX_REC_PDU_SIZE);
+        optional.add(DlmsProtocolProperties.RETRIES);
+        optional.add(DlmsProtocolProperties.ROUND_TRIP_CORRECTION);
+        optional.add(DlmsProtocolProperties.BULK_REQUEST);  //TODO needed this one for our connection setup ...
         return optional;
     }
 
     public List<String> getRequiredKeys() {
         List<String> required = new ArrayList<String>();
+        required.add(DlmsProtocolProperties.SECURITY_LEVEL);
         return required;
     }
 
@@ -30,8 +47,16 @@ public class WebRTUZ3Properties extends DlmsProtocolProperties {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(new WebRTUZ3Properties().toString());
+    @ProtocolProperty
+    @Override
+    public int getMaxRecPDUSize() {
+        return getIntProperty(MAX_REC_PDU_SIZE, MaxReceivePduSize);
+    }
+
+    @ProtocolProperty
+    @Override
+    public boolean isBulkRequest() {
+        return getBooleanProperty(BULK_REQUEST, DefaultZ3BulkRequesSupport);
     }
 
     @Override
