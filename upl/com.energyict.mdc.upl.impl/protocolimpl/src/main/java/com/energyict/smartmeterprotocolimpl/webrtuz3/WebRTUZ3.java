17689e707fb56ceeee3445ea3e1c85460ca0fd2c
Govanni Naessens <Govanni.Naessens@elster.com>
2011-03-03 07:46:57 +0000

    COMMUNICATION-218 - Implement profiles for the Z3 and make the WebRtuZ3 protocol more generic
    A draft with some Z3 registerSupport.
    
    Some protocolTester changes have been done to make it compatible ...
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@46923 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/smartmeterprotocolimpl/webrtuz3/WebRTUZ3.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/smartmeterprotocolimpl/webrtuz3/WebRTUZ3.java
@@ -1,19 +1,42 @@
 package com.energyict.smartmeterprotocolimpl.webrtuz3;
 
-import com.energyict.dlms.common.AbstractSmartDlmsProtocol;
 import com.energyict.protocol.*;
+import com.energyict.protocolimpl.base.CachedMeterTime;
+import com.energyict.protocolimpl.dlms.common.AbstractSmartDlmsProtocol;
+import com.energyict.smartmeterprotocolimpl.common.SimpleMeter;
+import com.energyict.smartmeterprotocolimpl.webrtuz3.composedobjects.ComposedMeterInfo;
+import com.energyict.smartmeterprotocolimpl.webrtuz3.profiles.EMeterEventProfile;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
 
 /**
  * Copyrights EnergyICT
  * Date: 7-feb-2011
  * Time: 14:15:14
  */
-public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
+public class WebRTUZ3 extends AbstractSmartDlmsProtocol implements SimpleMeter {
 
+    /**
+     * Contains properties related to the WebRTUZ3 protocol
+     */
     private WebRTUZ3Properties properties;
+
+    /**
+     * Contains information about the meter (serialNumber, firwmareVersion, ...)
+     */
+    private ComposedMeterInfo meterInfo;
+
+    /**
+     * The Factory containing all the Register related information/functionality
+     */
+    private WebRTUZ3RegisterFactory registerFactory;
+
+    /**
+     * Contains the actual meterTime
+     */
+    private CachedMeterTime cachedMeterTime;
 
     @Override
     protected WebRTUZ3Properties getProperties() {
@@ -21,6 +44,14 @@ public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
             properties = new WebRTUZ3Properties();
         }
         return properties;
+    }
+
+    /**
+     * Initialization method right after we are connected to the physical device.
+     */
+    @Override
+    protected void initAfterConnect() {
+        //TODO read the slaveDevices and create a list of serialNumbers mapped to ObisCode-B-channels
     }
 
     /**
@@ -34,23 +65,83 @@ public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
     }
 
     /**
-     * Get the current system time of the WebRTUZ3
+     * Get the firmware version of the meter
      *
-     * @return
-     * @throws IOException
+     * @return the version of the meter firmware
+     * @throws java.io.IOException Thrown in case of an exception
+     * @throws com.energyict.protocol.UnsupportedException
+     *                             Thrown if method is not supported
      */
-    public Date getTime() throws IOException {
-        // TODO: Implement the setTime method
-        return new Date();
+    public String getFirmwareVersion() throws IOException {
+        try {
+            StringBuilder firmware = new StringBuilder();
+            firmware.append(getMeterInfo().getFirmwareVersion());
+            String rfFirmware = getRFFirmwareVersion();
+            if (!rfFirmware.equalsIgnoreCase("")) {
+                firmware.append(" - RF-FirmwareVersion : ");
+                firmware.append(rfFirmware);
+            }
+            return firmware.toString();
+        } catch (IOException e) {
+            String message = "Could not fetch the firmwareVersion. " + e.getMessage();
+            getLogger().finest(message);
+            return "UnKnown version";
+        }
     }
 
     /**
-     * Set the device time of the WebRTUZ3 to the current commserver time
+     * Read the Z3/R2 RF-Firmwareversion
      *
-     * @throws IOException
+     * @return the firmwareversion, if it's not available then return an empty string
      */
-    public void setTime() throws IOException {
-        // TODO: Implement the setTime method
+    private String getRFFirmwareVersion() {
+        try {
+            return getMeterInfo().getRFFirmwareVersion();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    /**
+     * Get the SerialNumber of the device
+     *
+     * @return the serialNumber of the device
+     * @throws java.io.IOException thrown in case of an exception
+     */
+    public String getMeterSerialNumber() throws IOException {
+        try {
+            return getMeterInfo().getSerialNr();
+        } catch (IOException e) {
+            String message = "Could not retrieve the serialnumber of the meter. " + e.getMessage();
+            getLogger().finest(message);
+            throw new IOException(message);
+        }
+    }
+
+    /**
+     * 'Lazy' getter for the {@link #meterInfo}
+     *
+     * @return the {@link #meterInfo}
+     */
+    public ComposedMeterInfo getMeterInfo() {
+        if (meterInfo == null) {
+            meterInfo = new ComposedMeterInfo(getDlmsSession(), supportsBulkRequests());
+        }
+        return meterInfo;
+    }
+
+    @Override
+    public int requestConfigurationChanges() throws IOException {
+        return getMeterInfo().getConfigurationChanges();
+    }
+
+    /**
+     * Tests if the Rtu wants to use the bulkRequests
+     *
+     * @return true if the Rtu wants to use BulkRequests, false otherwise
+     */
+    public boolean supportsBulkRequests() {
+        return getProperties().isBulkRequest();
     }
 
     /**
@@ -66,6 +157,32 @@ public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
     }
 
     /**
+     * @return the current device time
+     * @throws java.io.IOException <br>
+     */
+    @Override
+    public Date getTime() throws IOException {
+        if (this.cachedMeterTime == null) {
+            this.cachedMeterTime = new CachedMeterTime(super.getTime());
+        }
+        return this.cachedMeterTime.getTime();
+    }
+
+    /**
+     * <p>
+     * sets the device time to the current system time.
+     * </p>
+     *
+     * @param newMeterTime the time to set in the meter
+     * @throws java.io.IOException Thrown in case of an exception
+     */
+    @Override
+    public void setTime(Date newMeterTime) throws IOException {
+        this.cachedMeterTime = null;    // we clear the cachedMeterTime again
+        super.setTime(newMeterTime);
+    }
+
+    /**
      * Fetch a whole list of registers. We should combine as much as possible values in
      * one request/response to the meter to save time and bandwidth
      *
@@ -74,9 +191,7 @@ public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
      * @throws IOException
      */
     public List<RegisterValue> readRegisters(List<Register> registers) throws IOException {
-        List<RegisterValue> registerValues = new ArrayList<RegisterValue>();
-        // TODO: Implement the translateRegister method
-        return registerValues;
+        return getRegisterFactory().readRegisters(registers);
     }
 
     /**
@@ -85,10 +200,12 @@ public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
      * @param lastLogbookDate The last event that's already in EIServer
      * @return A list of meterEvents
      */
-    public List<MeterEvent> getMeterEvents(Date lastLogbookDate) {
-        List<MeterEvent> meterEvents = new ArrayList<MeterEvent>();
-        // TODO: Implement the meter events
-        return meterEvents;
+    public List<MeterEvent> getMeterEvents(Date lastLogbookDate) throws IOException {
+        EMeterEventProfile eventProfile = new EMeterEventProfile(this, getDlmsSession());
+
+        //TODO need the slaveMeterEvents!!!!!!
+
+        return eventProfile.getEvents(lastLogbookDate);
     }
 
     /**
@@ -116,42 +233,28 @@ public class WebRTUZ3 extends AbstractSmartDlmsProtocol {
         return profileData;
     }
 
-    /**
-     * <p></p>
-     *
-     * @param name Register name. Devices supporting OBIS codes,
-     *             should use the OBIS code as register name
-     *             </p>
-     * @return the value for the specified register
-     *         </p><p>
-     * @throws java.io.IOException <br>
-     * @throws com.energyict.protocol.UnsupportedException
-     *                             if the device does not support this operation
-     * @throws com.energyict.protocol.NoSuchRegisterException
-     *                             if the device does not support the specified register
-     */
-    public String getRegister(String name) throws IOException, UnsupportedException, NoSuchRegisterException {
-        //TODO check if you can put these in the AbstractSmartDlmsProtocol
-        return null;  //TODO implement proper functionality.
+    public WebRTUZ3RegisterFactory getRegisterFactory() {
+        if (this.registerFactory == null) {
+            this.registerFactory = new WebRTUZ3RegisterFactory(this);
+        }
+        return this.registerFactory;
     }
 
     /**
-     * <p>
-     * sets the specified register to value
-     * </p>
+     * The serialNumber of the meter
      *
-     * @param name  Register name. Devices supporting OBIS codes,
-     *              should use the OBIS code as register name
-     * @param value to set the register.
-     *              </p>
-     * @throws java.io.IOException <br>
-     * @throws com.energyict.protocol.UnsupportedException
-     *                             if the device does not support this operation
-     * @throws com.energyict.protocol.NoSuchRegisterException
-     *                             if the device does not support the specified register
+     * @return the serialNumber of the meter
      */
-    public void setRegister(String name, String value) throws IOException, NoSuchRegisterException, UnsupportedException {
-        //TODO check if you can put these in the AbstractSmartDlmsProtocol
-        //TODO implement proper functionality.
+    public String getSerialNumber() {
+        return getProperties().getSerialNumber();
+    }
+
+    /**
+     * Get the physical address of the Meter. Mostly this will be an index of the meterList
+     *
+     * @return the physical Address of the Meter.
+     */
+    public int getPhysicalAddress() {
+        return 0; // the 'Master' has physicalAddress 0
     }
 }
