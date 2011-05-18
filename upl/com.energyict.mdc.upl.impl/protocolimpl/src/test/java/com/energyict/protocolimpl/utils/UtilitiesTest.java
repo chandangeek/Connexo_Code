fd09e42403808cfdd32a75930059fe1431c85da6
Bart Vansegbroeck <Bart.Vansegbroeck@elster.com>
2011-05-18 07:28:46 +0000

    Updated test layout
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@48717 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/protocolimpl/utils/UtilitiesTest.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/protocolimpl/utils/UtilitiesTest.java
@@ -6,15 +6,22 @@ package com.energyict.protocolimpl.utils;
 
 import com.energyict.cbo.BusinessException;
 import com.energyict.cbo.TimeDuration;
-import com.energyict.mdw.core.*;
-import com.energyict.mdw.coreimpl.*;
-import org.junit.*;
+import com.energyict.mdw.core.CommunicationProtocol;
+import com.energyict.mdw.core.MeteringWarehouse;
+import com.energyict.mdw.core.Rtu;
+import com.energyict.mdw.core.RtuType;
+import com.energyict.mdw.coreimpl.CommunicationProtocolImpl;
+import com.energyict.mdw.coreimpl.RtuImpl;
+import com.energyict.mdw.coreimpl.RtuTypeImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 /**
  * @author gna
@@ -31,16 +38,10 @@ public class UtilitiesTest {
 		MeteringWarehouse.createBatchContext(false);
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
 
@@ -48,7 +49,7 @@ public class UtilitiesTest {
 		List result = Utilities.mw().getRtuFactory().findByName("99999999");
 		if (result.size() > 0) {
 			for(int i = 0; i < result.size(); i++) {
-				((Rtu)result.get(0)).delete();
+				((Rtu)result.get(0)).delete(); // TODO This looks wrong
 			}
 		}
 
@@ -56,7 +57,7 @@ public class UtilitiesTest {
 		result = Utilities.mw().getRtuTypeFactory().findByName(testRtu);
 		if (result.size() > 0) {
 			for(int i = 0; i < result.size(); i++) {
-				((RtuTypeImpl)result.get(0)).delete();
+				((RtuTypeImpl)result.get(0)).delete(); // TODO This looks wrong
 			}
 		}
 
@@ -64,136 +65,80 @@ public class UtilitiesTest {
 		result = Utilities.mw().getCommunicationProtocolFactory().findByName(javaClassName);
 		if (result.size() > 0) {
 			for(int i = 0; i < result.size(); i++) {
-				((CommunicationProtocolImpl)result.get(0)).delete();
+				((CommunicationProtocolImpl)result.get(0)).delete(); // TODO This looks wrong
 			}
 		}
 	}
 
 	@Test
-	public void createCommunicationProtocolTest(){
-		try {
-			Utilities.createCommunicationProtocol(javaClassName);
-			List result = Utilities.mw().getCommunicationProtocolFactory().findByName(javaClassName);
+	public void createCommunicationProtocolTest() throws BusinessException, SQLException {
+        Utilities.createCommunicationProtocol(javaClassName);
+        List result = Utilities.mw().getCommunicationProtocolFactory().findByName(javaClassName);
 
-			if(result.size() == 1){
-				assertEquals(javaClassName, ((CommunicationProtocolImpl)result.get(0)).getShadow().getJavaClassName());
-			} else {
-				fail();
-			}
-
-		} catch (BusinessException e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		} catch (SQLException e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
+		assertEquals(1, result.size());
+	    assertEquals(javaClassName, ((CommunicationProtocolImpl)result.get(0)).getShadow().getJavaClassName());
 	}
 
 	@Test
-	public void createRtuTypeTest(){
-		try {
-			CommunicationProtocol commProtocol = Utilities.createCommunicationProtocol(javaClassName);
+	public void createRtuTypeTest() throws BusinessException, SQLException {
+        CommunicationProtocol commProtocol = Utilities.createCommunicationProtocol(javaClassName);
 
-			Utilities.createRtuType(commProtocol, testRtu, 6);
-			List result = Utilities.mw().getRtuTypeFactory().findByName(testRtu);
+        Utilities.createRtuType(commProtocol, testRtu, 6);
+        List result = Utilities.mw().getRtuTypeFactory().findByName(testRtu);
 
-			if(result.size() == 1){
-				assertEquals(testRtu, ((RtuTypeImpl)result.get(0)).getShadow().getName());
-				assertEquals(javaClassName, ((RtuTypeImpl)result.get(0)).getShadow().getCommunicationProtocolShadow().getName());
-				assertEquals(6, ((RtuTypeImpl)result.get(0)).getShadow().getChannelCount());
-			} else {
-				fail();
-			}
-		} catch (BusinessException e) {
-			e.printStackTrace();
-			fail();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			fail();
-		}
+        assertEquals(1, result.size());
+        assertEquals(testRtu, ((RtuTypeImpl)result.get(0)).getShadow().getName());
+        assertEquals(javaClassName, ((RtuTypeImpl)result.get(0)).getShadow().getCommunicationProtocolShadow().getName());
+        assertEquals(6, ((RtuTypeImpl)result.get(0)).getShadow().getChannelCount());
 	}
 
 	@Test
-	public void createRtuTest(){
+	public void createRtuTest() throws BusinessException, SQLException {
 		CommunicationProtocol commProtocol = null;
 		RtuType rtuType;
-		try {
+        List result = Utilities.mw().getCommunicationProtocolFactory().findAll();
+        for(int i = 0; i < result.size(); i++){
+            if(((CommunicationProtocol)result.get(i)).getJavaClassName().equalsIgnoreCase(javaClassName)){
+                commProtocol = (CommunicationProtocol)result.get(i);
+                break;
+            }
+        }
+        if(commProtocol == null) {
+            commProtocol = Utilities.createCommunicationProtocol(javaClassName);
+        }
+        rtuType = Utilities.createRtuType(commProtocol, testRtu, 6);
 
-			List result = Utilities.mw().getCommunicationProtocolFactory().findAll();
-			for(int i = 0; i < result.size(); i++){
-				if(((CommunicationProtocol)result.get(i)).getJavaClassName().equalsIgnoreCase(javaClassName)){
-					commProtocol = (CommunicationProtocol)result.get(i);
-					break;
-				}
-			}
-			if(commProtocol == null) {
-				commProtocol = Utilities.createCommunicationProtocol(javaClassName);
-			}
-			rtuType = Utilities.createRtuType(commProtocol, testRtu, 6);
+        Utilities.createRtu(rtuType);
+        result = Utilities.mw().getRtuFactory().findBySerialNumber("99999999");
 
-			Utilities.createRtu(rtuType);
-			result = Utilities.mw().getRtuFactory().findBySerialNumber("99999999");
-
-			if(result.size() == 1){
-				assertEquals("99999999", ((RtuImpl)result.get(0)).getShadow().getName());
-				assertEquals(6, ((RtuImpl)result.get(0)).getShadow().getChannelShadows().size());
-			} else {
-				fail();
-			}
-
-		} catch (BusinessException e) {
-			e.printStackTrace();
-			fail();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			fail();
-		}
+        assertEquals(1, result.size());
+        assertEquals("99999999", ((RtuImpl)result.get(0)).getShadow().getName());
+        assertEquals(6, ((RtuImpl)result.get(0)).getShadow().getChannelShadows().size());
 	}
 
 	@Test
-	public void addChannelTest(){
+	public void addChannelTest() throws BusinessException, SQLException {
 		CommunicationProtocol commProtocol;
 		RtuType rtuType;
 		Rtu rtu = null;
-		try {
-			commProtocol = Utilities.createCommunicationProtocol(javaClassName);
-			rtuType = Utilities.createRtuType(commProtocol, testRtu, 6);
+        commProtocol = Utilities.createCommunicationProtocol(javaClassName);
+        rtuType = Utilities.createRtuType(commProtocol, testRtu, 6);
 
-			Utilities.createRtu(rtuType);
-			List result = Utilities.mw().getRtuFactory().findBySerialNumber("99999999");
+        Utilities.createRtu(rtuType);
+        List result = Utilities.mw().getRtuFactory().findBySerialNumber("99999999");
 
-			if(result.size() == 1){
-				rtu = (Rtu)result.get(0);
-			} else {
-				fail();
-			}
+        assertEquals(1, result.size());
+        rtu = (Rtu)result.get(0);
 
-			rtu = Utilities.addChannel(rtu, TimeDuration.SECONDS, 7);
-			rtu = Utilities.addChannel(rtu, TimeDuration.DAYS, 8);
-			rtu = Utilities.addChannel(rtu, TimeDuration.MONTHS, 9);
-			rtu = Utilities.addChannel(rtu, TimeDuration.MINUTES, 10);
+        rtu = Utilities.addChannel(rtu, TimeDuration.SECONDS, 7);
+        rtu = Utilities.addChannel(rtu, TimeDuration.DAYS, 8);
+        rtu = Utilities.addChannel(rtu, TimeDuration.MONTHS, 9);
+        rtu = Utilities.addChannel(rtu, TimeDuration.MINUTES, 10);
 
-			assertEquals(10, rtu.getShadow().getChannelShadows().size());
-			if(!Utilities.getChannelWithProfileIndex(rtu, 7).getInterval().equals(new TimeDuration(1, TimeDuration.SECONDS))) {
-				fail();
-			}
-			if(!Utilities.getChannelWithProfileIndex(rtu, 8).getInterval().equals(new TimeDuration(1, TimeDuration.DAYS))) {
-				fail();
-			}
-			if(!Utilities.getChannelWithProfileIndex(rtu, 9).getInterval().equals(new TimeDuration(1, TimeDuration.MONTHS))) {
-				fail();
-			}
-			if(!Utilities.getChannelWithProfileIndex(rtu, 10).getInterval().equals(new TimeDuration(1, TimeDuration.MINUTES))) {
-				fail();
-			}
-
-		} catch (BusinessException e) {
-			e.printStackTrace();
-			fail();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			fail();
-		}
+        assertEquals(10, rtu.getShadow().getChannelShadows().size());
+        assertEquals(new TimeDuration(1, TimeDuration.SECONDS), Utilities.getChannelWithProfileIndex(rtu, 7).getInterval());
+        assertEquals(new TimeDuration(1, TimeDuration.DAYS), Utilities.getChannelWithProfileIndex(rtu, 8).getInterval());
+        assertEquals(new TimeDuration(1, TimeDuration.MONTHS), Utilities.getChannelWithProfileIndex(rtu, 9).getInterval());
+        assertEquals(new TimeDuration(1, TimeDuration.MINUTES), Utilities.getChannelWithProfileIndex(rtu, 10).getInterval());
 	}
 }
