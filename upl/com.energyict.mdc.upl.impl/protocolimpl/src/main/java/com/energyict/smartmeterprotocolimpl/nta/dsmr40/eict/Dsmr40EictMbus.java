06e5a69e3674fe9d0953716efe16a6c439b5725c
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2011-07-19 08:19:16 +0000

    COMMUNICATION-553 - Create a new DSMR 4.0 protocol. Added temporary EICT dsmr 4.0 protocol, until we know the exact manufacturer device names. Added package structure for each manufacturer.
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@50287 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/smartmeterprotocolimpl/nta/dsmr40/eict/Dsmr40EictMbus.java
@@ -0,0 +1,17 @@
+package com.energyict.smartmeterprotocolimpl.nta.dsmr40.eict;
+
+import com.energyict.smartmeterprotocolimpl.nta.dsmr23.eict.MbusDevice;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 18/07/11
+ * Time: 17:22
+ */
+public class Dsmr40EictMbus extends MbusDevice {
+
+    @Override
+    public String getVersion() {
+        return "$Date$";
+    }
+
+}
