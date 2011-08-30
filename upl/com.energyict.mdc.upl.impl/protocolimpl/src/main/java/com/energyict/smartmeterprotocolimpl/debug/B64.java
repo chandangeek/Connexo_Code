4e104a3a75bb6844925b29fc61e8245dcffedcdd
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2011-08-30 08:04:49 +0000

    Added small class to B64 encode a file
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@51057 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/smartmeterprotocolimpl/debug/B64.java
@@ -0,0 +1,22 @@
+package com.energyict.smartmeterprotocolimpl.debug;
+
+import com.energyict.protocolimpl.utils.ProtocolTools;
+import sun.misc.BASE64Encoder;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 25/08/11
+ * Time: 15:43
+ */
+public class B64 {
+
+    private static final String BIN_FW_FILE = "C:\\original_firmware_file.bin";
+    private static final String B64_FW_FILE = "C:\\base64_encoded_firmware_file_to_create.bin";
+
+    public static void main(String[] args) {
+        byte[] rawFirmware = ProtocolTools.readBytesFromFile(BIN_FW_FILE);
+        String encoded = new BASE64Encoder().encode(rawFirmware);
+        ProtocolTools.writeStringToFile(B64_FW_FILE, encoded, false);
+    }
+
+}
