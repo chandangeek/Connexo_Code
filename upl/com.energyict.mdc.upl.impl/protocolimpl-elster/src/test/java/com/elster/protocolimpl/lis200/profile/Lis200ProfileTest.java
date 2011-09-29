9f91644000b2c7db0d0632d0ac7ed1cd08183733
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2011-09-29 16:16:50 +0000

    COMMUNICATION-595 - Merge elster protocols in to energyict protocols project Ignore test
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@52160 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl-elster/src/test/java/com/elster/protocolimpl/lis200/profile/Lis200ProfileTest.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-elster/src/test/java/com/elster/protocolimpl/lis200/profile/Lis200ProfileTest.java
@@ -99,8 +99,8 @@ public class Lis200ProfileTest implements ProtocolLink {
         assertEquals(result.toString(), compareData);
     }
 
-    @Test
     @Ignore
+    @Test
     public void testEk260processing() throws IOException {
 
         System.out.println("testEk260processing()");
