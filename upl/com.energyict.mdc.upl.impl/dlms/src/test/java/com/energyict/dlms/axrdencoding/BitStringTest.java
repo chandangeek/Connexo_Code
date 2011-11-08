0cb9744cb5f06c6f0a8c778d1774db765217cf36
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2011-11-08 10:53:30 +0000

    UKHUB-29 Some event log filters not available in the AM110R Added JUnit test for this issue. Sadly, the complete build-up of the bitstring class assumes that a bitstring is a long value, and that doesn't map very well on large bitstrings :(
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@53401 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/dlms/src/test/java/com/energyict/dlms/axrdencoding/BitStringTest.java
@@ -0,0 +1,28 @@
+package com.energyict.dlms.axrdencoding;
+
+import com.energyict.protocolimpl.utils.ProtocolTools;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 8/11/11
+ * Time: 11:26
+ */
+public class BitStringTest {
+
+    @Test
+    public void testLargeBitString() {
+        byte[] rawValue = ProtocolTools.getBytesFromHexString("$04$82$02$00$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF$FF");
+        try {
+            BitString bitString = new BitString(rawValue, 0);
+            assertNotNull(bitString);
+            assertTrue(bitString.isBitString());
+            assertEquals(rawValue.length, bitString.size());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+}
