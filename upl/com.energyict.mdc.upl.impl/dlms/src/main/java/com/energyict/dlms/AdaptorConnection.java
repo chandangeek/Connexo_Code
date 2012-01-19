15f875074752eccdee430beb37ad7fa6d782bbf9
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2012-01-19 09:51:45 +0000

    Port from 8.9
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@55680 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/AdaptorConnection.java
+++ upl/com.energyict.mdc.upl.impl/dlms/src/main/java/com/energyict/dlms/AdaptorConnection.java
@@ -2,93 +2,70 @@ package com.energyict.dlms;
 
 import com.energyict.dialer.connection.HHUSignOn;
 import com.energyict.dlms.aso.ApplicationServiceObject;
-import com.energyict.protocol.ProtocolUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class AdaptorConnection implements DLMSConnection {
 
-	final int DEBUG=0;
-	ByteArrayOutputStream baos = null;
-	private InvokeIdAndPriority invokeIdAndPriority = new InvokeIdAndPriority();
+    ByteArrayOutputStream baos = null;
+    private InvokeIdAndPriority invokeIdAndPriority = new InvokeIdAndPriority();
 
-	public AdaptorConnection() throws IOException {
-		// TODO Auto-generated constructor stub
-	}
-
-	public void connectMAC() throws IOException, DLMSConnectionException {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void disconnectMAC() throws IOException, DLMSConnectionException {
-		// TODO Auto-generated method stub
-
-	}
-
-	public HHUSignOn getHhuSignOn() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int getType() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public byte[] sendRequest(byte[] byteRequestBuffer) throws IOException {
-		if (DEBUG>=1) {
-			System.out.println(ProtocolUtils.outputHexString(byteRequestBuffer));
-		}
-		if (baos == null) {
-			baos = new ByteArrayOutputStream();
-		}
-		baos.write(byteRequestBuffer, 3, byteRequestBuffer.length-3); // skip HDLS LLC
-		return null;
-	}
-
-	public void reset() {
-		baos.reset();
-	}
-
-	public byte[] getCompoundData() {
-		byte[] data = null;
-		data = baos.toByteArray();
-//		baos=null;
-		return data;
-	}
-
-	public void setHHUSignOn(HHUSignOn hhuSignOn, String meterId) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void setIskraWrapper(int type) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void setSNRMType(int type) {
-		// TODO Auto-generated method stub
-
-	}
-
-    /********************************************************************************************************
-     * Invoke-Id-And-Priority byte setting
-     ********************************************************************************************************/
-
-    public void setInvokeIdAndPriority(InvokeIdAndPriority iiap){
-    	this.invokeIdAndPriority = iiap;
+    public byte[] sendRequest(byte[] byteRequestBuffer) throws IOException {
+        if (baos == null) {
+            baos = new ByteArrayOutputStream();
+        }
+        baos.write(byteRequestBuffer, 3, byteRequestBuffer.length - 3); // skip HDLS LLC
+        return null;
     }
 
-    public InvokeIdAndPriority getInvokeIdAndPriority(){
-    	return this.invokeIdAndPriority;
+    public void reset() {
+        baos.reset();
     }
 
-	public int getMaxRetries() {
-		return 0;
-	}
+    public byte[] getCompoundData() {
+        return baos.toByteArray();
+    }
+
+    public void connectMAC() throws IOException, DLMSConnectionException {
+        // Nothing to do
+    }
+
+    public void disconnectMAC() throws IOException, DLMSConnectionException {
+        // Nothing to do
+    }
+
+    public HHUSignOn getHhuSignOn() {
+        return null;
+    }
+
+    public int getType() {
+        return 0;
+    }
+
+    public void setHHUSignOn(HHUSignOn hhuSignOn, String meterId) {
+        // Nothing to do
+    }
+
+    public void setIskraWrapper(int type) {
+        // Nothing to do
+    }
+
+    public void setSNRMType(int type) {
+        // Nothing to do
+    }
+
+    public void setInvokeIdAndPriority(InvokeIdAndPriority iiap) {
+        this.invokeIdAndPriority = iiap;
+    }
+
+    public InvokeIdAndPriority getInvokeIdAndPriority() {
+        return this.invokeIdAndPriority;
+    }
+
+    public int getMaxRetries() {
+        return 0;
+    }
 
     public ApplicationServiceObject getApplicationServiceObject() {
         return null;
