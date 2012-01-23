8aed5c2400a185346825bdd372de0c5d1c82eee4
Jeroen Meulemeester <Jeroen.Meulemeester@elster.com>
2012-01-23 16:21:05 +0000

    COMMUNICATION-695 - EK280 Inbound protocol: does not support Device channel indexes on the Device Prototype
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@55800 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl-elster/src/main/java/com/elster/genericprotocolimpl/dlms/ek280/executors/RegisterTaskExecutor.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-elster/src/main/java/com/elster/genericprotocolimpl/dlms/ek280/executors/RegisterTaskExecutor.java
@@ -1,9 +1,11 @@
 package com.elster.genericprotocolimpl.dlms.ek280.executors;
 
 import com.energyict.mdw.amr.RtuRegister;
+import com.energyict.mdw.amr.RtuRegisterSpec;
 import com.energyict.obis.ObisCode;
 import com.energyict.protocol.NoSuchRegisterException;
 import com.energyict.protocol.RegisterValue;
+import com.energyict.protocolimpl.utils.ProtocolTools;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,13 +25,18 @@ public class RegisterTaskExecutor extends AbstractExecutor<RegisterTaskExecutor.
     public void execute(RegisterTask registerTask) throws IOException {
         List<RtuRegister> registers = registerTask.getRegisters();
         for (RtuRegister register : registers) {
-            ObisCode obis = register.getRtuRegisterSpec().getDeviceObisCode();
+            RtuRegisterSpec registerSpec = register.getRtuRegisterSpec();
+            ObisCode obis = registerSpec.getDeviceObisCode();
+            ObisCode obisToRead;
             if (obis == null) {
                 obis = register.getRegisterMapping().getObisCode();
+                obisToRead = ProtocolTools.setObisCodeField(obis, 1, (byte) (registerSpec.getDeviceChannelIndex() & 0x0FF));
+            } else {
+                obisToRead = ObisCode.fromByteArray(obis.getLN());
             }
             try {
-                RegisterValue registerValue = getDlmsProtocol().readRegister(obis);
-                getStoreObject().add(register, registerValue);
+                RegisterValue registerValue = getDlmsProtocol().readRegister(obisToRead);
+                getStoreObject().add(register, ProtocolTools.setRegisterValueObisCode(registerValue, obis));
             } catch (NoSuchRegisterException e) {
                 getMeterAmrLogging().logRegisterFailure(e, obis);
             }
