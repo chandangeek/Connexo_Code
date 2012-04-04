8310c38999d58059599d70fb984c01a665d20613
Stijn Vanhoorelbeke <Stijn.Vanhoorelbeke@elster.com>
2012-04-04 15:14:52 +0000

    Reuse the existing CodeTableXml
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@58529 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/AM100R/Apollo/messages/ApolloActivityCalendarController.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/genericprotocolimpl/elster/AM100R/Apollo/messages/ApolloActivityCalendarController.java
@@ -9,7 +9,7 @@ import com.energyict.genericprotocolimpl.common.ParseUtils;
 import com.energyict.genericprotocolimpl.elster.AM100R.Apollo.ApolloMeter;
 import com.energyict.protocolimpl.base.ActivityCalendarController;
 import com.energyict.protocolimpl.dlms.as220.emeter.AS220Messaging;
-import com.energyict.protocolimpl.dlms.as220.parsing.CodeTableXml;
+import com.energyict.protocolimpl.messages.codetableparsing.CodeTableXml;
 import com.energyict.protocolimpl.utils.ProtocolTools;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -220,7 +220,7 @@ public class ApolloActivityCalendarController implements ActivityCalendarControl
     }
 
     /**
-     * Construct a map of shifted dayProfile ID's. We are reusing the {@link com.energyict.protocolimpl.dlms.as220.parsing.CodeTableXml}, but this results
+     * Construct a map of shifted dayProfile ID's. We are reusing the {@link com.energyict.protocolimpl.messages.codetableparsing.CodeTableXml}, but this results
      * in dayIds with values of 0, which the ApolloMeter does not allow ...
      * This method will most likely result in a shift of all the dayProfileId's
      *
