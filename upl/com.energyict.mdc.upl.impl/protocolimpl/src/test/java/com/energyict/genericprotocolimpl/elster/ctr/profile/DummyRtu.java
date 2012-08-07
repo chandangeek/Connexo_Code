981af639df56c151d9de18e41f57a0d9c84e6441
Govanni Naessens <Govanni.Naessens@elster.com>
2012-08-07 12:20:23 +0000

    Compliant with new api
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@62888 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/genericprotocolimpl/elster/ctr/profile/DummyRtu.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/test/java/com/energyict/genericprotocolimpl/elster/ctr/profile/DummyRtu.java
@@ -10,9 +10,12 @@ import com.energyict.dynamicattributes.AttributeType;
 import com.energyict.dynamicattributes.BusinessObjectType;
 import com.energyict.interval.DateTime;
 import com.energyict.mdc.protocol.DeviceProtocolPluggableClass;
+import com.energyict.mdc.shadow.tasks.ComTaskShadow;
 import com.energyict.mdc.shadow.tasks.ProtocolTypeUsageShadow;
+import com.energyict.mdc.shadow.tasks.RecurringConnectionTaskShadow;
 import com.energyict.mdc.tasks.ComTask;
 import com.energyict.mdc.tasks.ProtocolTypeUsage;
+import com.energyict.mdc.tasks.RecurringConnectionTask;
 import com.energyict.mdw.amr.RtuRegister;
 import com.energyict.mdw.core.*;
 import com.energyict.mdw.offline.OfflineRtu;
@@ -116,8 +119,23 @@ public class DummyRtu implements Rtu {
     }
 
     @Override
+    public RecurringConnectionTask createRecurringConnectionTask(RecurringConnectionTaskShadow shadow) throws BusinessException, SQLException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public RecurringConnectionTask getRecurringConnectionTask() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
     public List<ComTask> getComTasks() {
         return new ArrayList<ComTask>(0);
+    }
+
+    @Override
+    public ComTask createComTask(ComTaskShadow shadow) throws BusinessException, SQLException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public List<CommunicationScheduler> getCommunicationSchedulers() {
