9740793e5a4e33017d4efc512c6507f33c528acd
Karel Haeck <k.haeck@energyict.com>
2013-04-30 17:07:35 +0200

    added events and more declarative services

--- copl/com.elster.jupiter.http.whiteboard/src/com/elster/jupiter/http/whiteboard/impl/WhiteBoard.java
+++ copl/com.elster.jupiter.http.whiteboard/src/com/elster/jupiter/http/whiteboard/impl/WhiteBoard.java
@@ -1,74 +1,48 @@
 package com.elster.jupiter.http.whiteboard.impl;
 
-import org.osgi.framework.*;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
+import java.util.HashMap;
+
+import org.osgi.service.component.annotations.*;
 import org.osgi.service.http.*;
-import org.osgi.util.tracker.*;
+
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
 
 import com.elster.jupiter.http.whiteboard.HttpResource;
 
 @Component(name = "com.elster.jupiter.http.whiteboard")
 public class WhiteBoard {
 	private volatile HttpService httpService;
-	private volatile ServiceTracker<HttpResource, HttpResource> resourceTracker;
+	private volatile EventAdmin eventAdmin;
 	
 	public WhiteBoard() {
 	}
-		
-	void addResource(HttpResource resource) {
+	
+	@Reference
+	public void setHttpService(HttpService httpService) {
+		this.httpService = httpService;
+	}
+	
+	@Reference
+	public void setEventAdmin(EventAdmin eventAdmin) {
+		this.eventAdmin = eventAdmin;
+	}
+	
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE , policy = ReferencePolicy.DYNAMIC)
+	public void addResource(HttpResource resource) {
 		HttpContext httpContext = new HttpContextImpl(resource.getResolver());
 		try {
 			httpService.registerResources(resource.getAlias(),resource.getLocalName(), httpContext);
+			Event event = new Event("com/elster/jupiter/http/whiteboard", new HashMap<String,Object>());
+			eventAdmin.postEvent(event);
 		} catch (NamespaceException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 	}
 	
-	void removeResource(HttpResource resource) {		
+	public void removeResource(HttpResource resource) {		
 		httpService.unregister(resource.getAlias());
 	}
 
-	@Reference
-	public void setHttpService(HttpService httpService) {
-		this.httpService = httpService;
-	}
-	
-	@Activate
-	public void activate(BundleContext context) {
-		this.resourceTracker = new ServiceTracker<>(context, HttpResource.class, new ResourceTrackerCustomizer(context));
-		this.resourceTracker.open();
-	}
-
-	@Deactivate
-	public void deActivate() {
-		this.resourceTracker.close();
-	}
-	
-	class ResourceTrackerCustomizer implements ServiceTrackerCustomizer<HttpResource,HttpResource> {
-		private final BundleContext bundleContext;
-		
-		public ResourceTrackerCustomizer(BundleContext bundleContext) {
-			this.bundleContext = bundleContext;
-		}
-
-		@Override
-		public HttpResource addingService(ServiceReference<HttpResource> reference) {
-			HttpResource resource = bundleContext.getService(reference);
-			addResource(resource);
-			return resource;
-		}
-
-		@Override
-		public void modifiedService(ServiceReference<HttpResource> reference, HttpResource service) {						
-		}
-
-		@Override
-		public void removedService(ServiceReference<HttpResource> reference,HttpResource service) {
-			removeResource(service);
-		}
-	}
 }
