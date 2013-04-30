5975b183ed52c68e7a72faee347449a85db78f38
Karel Haeck <k.haeck@energyict.com>
2013-04-30 17:05:27 +0200

    changed to declarative services

--- copl/com.elster.jupiter.bootstrap/src/com/elster/jupiter/bootstrap/impl/BootstrapServiceImpl.java
+++ copl/com.elster.jupiter.bootstrap/src/com/elster/jupiter/bootstrap/impl/BootstrapServiceImpl.java
@@ -1,20 +1,48 @@
 package com.elster.jupiter.bootstrap.impl;
 
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
 
 import com.elster.jupiter.bootstrap.BootstrapService;
 
-class BootstrapServiceImpl implements BootstrapService {
+@Component (name = "com.elster.jupiter.bootstrap")
+public class BootstrapServiceImpl implements BootstrapService {
 
-	private final DataSource dataSource;
+	private volatile DataSource dataSource;
 	
-	public BootstrapServiceImpl(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public BootstrapServiceImpl() {		
 	}
 	
 	@Override
 	public DataSource getDataSource() {
 		return dataSource;
 	}
+	
+	@Activate 
+	public void activate() throws SQLException {
+		try {
+			this.dataSource =  createDataSource();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			throw ex;
+		}
+	}
 
+	private DataSource createDataSource() throws SQLException {
+		OracleDataSource source = new OracleDataSource();
+		source.setDriverType("thin");
+		source.setServerName("localhost");
+		source.setPortNumber(1521);
+		source.setDatabaseName("eiserver");
+		source.setUser("kore");
+		source.setPassword("kore");	
+		source.setConnectionCachingEnabled(true);
+		return source;
+	}
 }
