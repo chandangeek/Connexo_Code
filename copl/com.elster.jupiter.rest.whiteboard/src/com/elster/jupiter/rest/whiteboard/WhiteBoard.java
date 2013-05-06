a1d91f1b713310d3898b8a5896ddac069832f2c1
Karel Haeck <k.haeck@energyict.com>
2013-05-06 08:24:33 +0200

    switched to declarative services

--- copl/com.elster.jupiter.rest.whiteboard/src/com/elster/jupiter/rest/whiteboard/WhiteBoard.java
+++ copl/com.elster.jupiter.rest.whiteboard/src/com/elster/jupiter/rest/whiteboard/WhiteBoard.java
@@ -8,64 +8,33 @@ import com.sun.jersey.api.core.ApplicationAdapter;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 import com.sun.jersey.spi.container.ResourceFilterFactory;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.http.HttpContext;
-import org.osgi.service.http.HttpService;
-import org.osgi.service.http.NamespaceException;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+import org.osgi.service.component.annotations.*;
+import org.osgi.service.http.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.ws.rs.core.Application;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 @Component (name = "com.elster.jupiter.rest.whiteboard" , service = {})
 public class WhiteBoard implements ServiceLocator {
 
 	private final HttpContext httpContext;
-	private volatile BundleContext bundleContext;
     private volatile HttpService httpService;
     private volatile UserService userService;
     private volatile ThreadPrincipalService threadPrincipalService;
-    private volatile ServiceTracker<Application, Application> resourceTracker;
+
 
     public WhiteBoard() {
         this.httpContext = new HttpContextImpl();            
     }
 
-    private void open(final long delay) {
-        // some jersey code is dependent on bundle activation,
-        // but does not register any services that can be tracked
-        // to avoid complex bundle management, just delay the white board activation
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(delay);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                open();
-            }
-        };
-        new Thread(runnable).start();
-    }
-
-    private void open() {
-    	if (bundleContext != null) {
-    		resourceTracker = new ServiceTracker<>(bundleContext, Application.class,  new ApplicationTrackerCustomizer(bundleContext));
-    		resourceTracker.open();
-    	}
-    }
-
-    void addResource(String alias, Application application) {
+    @Reference(name = "ZResource" , cardinality=ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    void addResource(Application application, Map<String,Object> properties) {
+    	String alias = (String) properties.get("alias");
         ResourceConfig secureConfig = new ApplicationAdapter(application);
         List<Class<? extends ContainerRequestFilter>> requestFilters = new ArrayList<>();
         requestFilters.add(RoleFilter.class);
@@ -74,7 +43,7 @@ public class WhiteBoard implements ServiceLocator {
         resourceFilterFactories.add(RolesAllowedResourceFilterFactory.class);
         resourceFilterFactories.add(ResourceDebuggingFilterFactory.class);
         secureConfig.getProperties().put(ResourceConfig.PROPERTY_RESOURCE_FILTER_FACTORIES, resourceFilterFactories);
-        com.sun.jersey.spi.container.servlet.ServletContainer container = new com.sun.jersey.spi.container.servlet.ServletContainer(secureConfig);
+        ServletContainer container = new ServletContainer(secureConfig);
         HttpServlet wrapper = new ServletWrapper(container);
         try {
             httpService.registerServlet(alias, wrapper, null, httpContext);
@@ -84,8 +53,8 @@ public class WhiteBoard implements ServiceLocator {
         }
     }
 
-    void removeResource(String alias) {
-        httpService.unregister(alias);
+    void removeResource(Application application,Map<String,Object> properties) {
+        httpService.unregister((String) properties.get("alias"));
     }
 
     @Reference
@@ -94,21 +63,15 @@ public class WhiteBoard implements ServiceLocator {
     }
     
     @Activate
-    public void activate(BundleContext context) {    	
-    	this.bundleContext = context;
-    	Bus.setServiceLocator(this);
-    	open(1000L);
+    public void activate() {    	
+    	Bus.setServiceLocator(this);    	
     }
     
     @Deactivate
-    public void deActivate() {
-    	this.bundleContext = null;
-    	Bus.setServiceLocator(null);
-    	if (resourceTracker != null) {
-    		resourceTracker.close();
-    		resourceTracker = null;
-    	}    	
+    public void deActivate() {    	
+    	Bus.setServiceLocator(null);    	 	
     }
+    
     @Override
 	public UserService getUserService() {
 		return userService;
@@ -129,43 +92,4 @@ public class WhiteBoard implements ServiceLocator {
 		this.threadPrincipalService = threadPrincipalService;
 	}
 	
-    private class ApplicationTrackerCustomizer implements ServiceTrackerCustomizer<Application, Application> {
-    	private final BundleContext bundleContext;
-    	
-    	ApplicationTrackerCustomizer(BundleContext context) {
-    		this.bundleContext = context;
-		}
-
-        private String getAlias(ServiceReference<Application> reference) {
-            return (String) reference.getProperty("alias");
-        }
-
-        @Override
-        public Application addingService(ServiceReference<Application> reference) {
-            String alias = getAlias(reference);
-            if (alias == null) {
-                System.out.println("No alias defined for " + reference);
-                return null;
-            } else {
-                Application application = bundleContext.getService(reference);
-                addResource(alias, application);
-                return application;
-            }
-        }
-
-        @Override
-        public void modifiedService(ServiceReference<Application> reference, Application service) {
-        }
-
-        @Override
-        public void removedService(ServiceReference<Application> reference, Application service) {
-            String alias = getAlias(reference);
-            if (alias != null) {
-                removeResource(alias);
-            }
-        }
-
-
-    }
-    
 }
