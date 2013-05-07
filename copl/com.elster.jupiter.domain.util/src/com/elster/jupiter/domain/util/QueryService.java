518d116b82d6fdc144e0684e5bcfaf246ad82825
Karel Haeck <k.haeck@energyict.com>
2013-05-07 13:48:58 +0200

    added isCurrent condition

--- copl/com.elster.jupiter.domain.util/src/com/elster/jupiter/domain/util/QueryService.java
+++ copl/com.elster.jupiter.domain.util/src/com/elster/jupiter/domain/util/QueryService.java
@@ -1,7 +1,9 @@
 package com.elster.jupiter.domain.util;
 
+import com.elster.jupiter.conditions.Condition;
 import com.elster.jupiter.orm.QueryExecutor;
 
 public interface QueryService {
 	<T> Query<T> wrap(QueryExecutor<T> queryExecutor);
+	Condition isCurrent(String name);
 }
--- copl/com.elster.jupiter.domain.util/src/com/elster/jupiter/domain/util/impl/QueryServiceImpl.java
+++ copl/com.elster.jupiter.domain.util/src/com/elster/jupiter/domain/util/impl/QueryServiceImpl.java
@@ -2,6 +2,8 @@ package com.elster.jupiter.domain.util.impl;
 
 import org.osgi.service.component.annotations.Component;
 
+import com.elster.jupiter.conditions.Condition;
+import com.elster.jupiter.conditions.Operator;
 import com.elster.jupiter.domain.util.Query;
 import com.elster.jupiter.domain.util.QueryService;
 import com.elster.jupiter.orm.QueryExecutor;
@@ -12,6 +14,14 @@ public class QueryServiceImpl implements QueryService {
 	@Override
 	public <T> Query<T> wrap(QueryExecutor<T> queryExecutor) {
 		return new QueryImpl<>(queryExecutor);
+	}
+
+	@Override
+	public Condition isCurrent(String name) {
+		long now = System.currentTimeMillis();
+		Condition condition = Operator.LESSTHANOREQUAL.compare( name + ".start" , now);
+		condition.and(Operator.GREATERTHAN.compare(name + ".stop", now));
+		return condition;
 	}
 
 }
