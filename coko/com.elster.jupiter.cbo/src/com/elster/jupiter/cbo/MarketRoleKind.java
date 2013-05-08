16e27917179b11ac433b006a73284200bc9183b6
Karel Haeck <k.haeck@energyict.com>
2013-05-08 09:05:59 +0200

    added 2 cim compounds

---
+++ coko/com.elster.jupiter.cbo/src/com/elster/jupiter/cbo/MarketRoleKind.java
@@ -0,0 +1,69 @@
+package com.elster.jupiter.cbo;
+
+public enum MarketRoleKind {
+	ENERGYSERVICECONSUMER("Energy Service Consumer"),
+	GENERATOROWNER ("Generator owner"),
+	GENERATOROPERATOR ("Generator Operator"),
+	TRANSMISSIONSERVICEPROVIDER ("Transmission Service Provider"),
+	TRANSMISSIONOWNER("Transmission Onwer"),
+	TRANSMISSIONOPERATOR("Transmission Operator"),
+	DISTRIBUTIONPROVIDER("Distribution Provider"),
+	LOADSERVINGENTITY("Load Serving Entity"),
+	PURCHASINGSELLINGENTITIY("Purchasing Selling Entity"),
+	COMPETTITIVERETAILER("Competitive Retailer"),
+	RELIABILILIYAUTHORITY("Reliability Authority"),
+	PLANNINGAUTHORITY("Planning Authority"),
+	BALANCINGAUTHORITY("Balancing Authority"),
+	INTERCHANGEAUTHORITY("Interchange Authority"),
+	TRANSMISSIONPLANNNER("Transmission Planner"),
+	RESOURCEPLANNER("Resource Planner"),
+	STANDARDSDEVELOPER("Standards Developer"),
+	COMPLIANCEMONITOR("Compliance Monitor"),
+	BALANCERESPONSIBLEPARTY("Balance Responsible Party"),
+	BALCANCESUPPLIER("Balance Supplier"),
+	BILLINGAGENT("Billing Agent"),
+	BLOCKENERGYTRADER("Block Energy Trader"),
+	CAPACITYCOORDINATOR("Capacity Coordinator"),
+	CAPACITYTRADER("Capacity Trader"),
+	CONSUMER("Consumer"),
+	CONSUMPTIONRESPONSIBLEPARTY("Consumption Responsible Party"),
+	CONTROLAREAOPERATOR("Control Area Operator"),
+	CONTROLBLOCKOPERATOR("Control Block Operator"),
+	COORDINATIONCENTEROPERATOR("Coordination Center Operator"),
+	GRIDACCESSPROVIDER("Grid Access Provider"),
+	GRIDOPERATOR("Grid operator"),
+	IMBALANCESETTLEMENTRESPONSIBLE("Imbalance Settlement Responsible"),
+	INTERCONNECTIONTRADERESPONSIBLE("Interconnection Trade Responsible"),
+	MARKETINFORMATIONAGGREGATOR("Market Information Aggregator"),
+	MARKETOPERATOR("Market Operator"),
+	METERADMINISTRATOR("Meter Administrator"),
+	METEROPERATOR("Meter Operator"),
+	METEREDDATACOLLECTOR("Meter Data Collector"),
+	METEREDDATARESPONSIBLE("Meter Data Responsible"),
+	METEREDDATAGGREGATOR("Metered Data Aggregator"),
+	METERINGPOINTADMINISTRATOR("Metering Point Administrator"),
+	MOLRESPONSIBLE("MOL Responsbile"),
+	NOMINAITIONVALIDATOR("Nomination Validator"),
+	PARTYCONNECTEDTOTHEGRID("Party Connected to the Grid"),
+	PRODUCER("Producer"),
+	PRODUCTIONRESPONSIBLEPARTY("Production Responsible Party"),
+	RECONCILIATIONACCOUNTABLE("Reconciliation Accountable"),
+	RECONCILIATIONRESPONSIBLE("Reconciliation Responsible"),
+	RESERVEALLOCATOR("Reserver Allocator"),
+	RESOURCEPROVIDER("Resource Provider"),
+	SCHEDULINGCOORDINATOR("Scheduling Coordinator"),
+	SYSTEMOPERATOR("System Operator"),
+	TRADERESPONSIBLEPARTY("Trade Responsible Party"),
+	TRANSMISSIONCAPACITYALLOCATOR("Transmission Capacity Allocator");
+		
+	final private String displayName;
+	
+	private MarketRoleKind(String displayName) {
+		this.displayName = displayName;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+
+}
---
+++ coko/com.elster.jupiter.cbo/src/com/elster/jupiter/cbo/PostalAddress.java
@@ -0,0 +1,75 @@
+package com.elster.jupiter.cbo;
+
+public final class PostalAddress implements Cloneable {
+	private String postalCode;
+	private String poBox;	
+	private StreetDetail streetDetail;
+	private TownDetail townDetail;
+	
+	public PostalAddress(String postalCode, String poBox , StreetDetail streetDetail , TownDetail townDetail) {
+		this.postalCode = postalCode;
+		this.poBox = poBox;
+		this.streetDetail = streetDetail;
+		this.townDetail = townDetail;		
+	}
+	
+	public PostalAddress(StreetDetail streetDetail , TownDetail townDetail ) {
+		this(null,null,streetDetail,townDetail);
+	}
+	
+	public PostalAddress() {
+		this(new StreetDetail(),new TownDetail());
+	}
+
+	public StreetDetail getStreetDetail() {
+		return streetDetail;
+	}
+
+	public void setStreetDetail(StreetDetail streetDetail) {
+		this.streetDetail = streetDetail;
+	}
+
+	public TownDetail getTownDetail() {
+		return townDetail;
+	}
+
+	public void setTownDetail(TownDetail townDetail) {
+		this.townDetail = townDetail;
+	}
+	
+	public PostalAddress copy() {
+		try {
+			PostalAddress result = (PostalAddress) this.clone();
+			result.streetDetail = this.streetDetail.copy();
+			result.townDetail = this.townDetail.copy();			
+			return result;
+		} catch (CloneNotSupportedException ex) {
+			throw new UnsupportedOperationException(ex);
+		}
+	}
+	
+	public boolean isEmpty() {
+		return postalCode == null && poBox == null && townDetail.isEmpty() && streetDetail.isEmpty();
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPoBox() {
+		return poBox;
+	}
+
+	public void setPoBox(String poBox) {
+		this.poBox = poBox;
+	}
+	
+	@Override
+	public String toString() {
+		return "" + postalCode + " " + poBox + " " + getStreetDetail() + " " + getTownDetail();
+	}
+}
