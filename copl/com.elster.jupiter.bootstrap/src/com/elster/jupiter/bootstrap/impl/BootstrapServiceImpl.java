39c8e726d77968ffb42e7db0a1695055d7618f17
Karel Haeck <k.haeck@energyict.com>
2013-05-28 13:50:50 +0200

    Added maxLimit and maxStatements properties

--- copl/com.elster.jupiter.bootstrap/src/com/elster/jupiter/bootstrap/impl/BootstrapServiceImpl.java
+++ copl/com.elster.jupiter.bootstrap/src/com/elster/jupiter/bootstrap/impl/BootstrapServiceImpl.java
@@ -2,6 +2,7 @@ package com.elster.jupiter.bootstrap.impl;
 
 import com.elster.jupiter.bootstrap.BootstrapService;
 import com.elster.jupiter.bootstrap.PropertyNotFoundException;
+
 import oracle.jdbc.pool.OracleDataSource;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
@@ -9,6 +10,7 @@ import org.osgi.service.component.annotations.Component;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @Component (name = "com.elster.jupiter.bootstrap")
 public class BootstrapServiceImpl implements BootstrapService {
@@ -16,15 +18,21 @@ public class BootstrapServiceImpl implements BootstrapService {
     private static final String JDBC_DRIVER_URL = "com.elster.jupiter.datasource.jdbcurl";
     private static final String JDBC_USER = "com.elster.jupiter.datasource.jdbcuser";
     private static final String JDBC_PASSWORD = "com.elster.jupiter.datasource.jdbcpassword";
+    private static final String JDBC_POOLMAXLIMIT = "com.elster.jupiter.datasource.pool.maxlimit";
+    private static final String JDBC_POOLMAXSTATEMENTS = "com.elster.jupiter.datasource.pool.maxstatements";
 
     private String jdbcUrl;
     private String jdbcUser;
     private String jdbcPassword;
+    private int maxLimit;
+    private int maxStatementsLimit;
 
     public void activate(ComponentContext context) {
         jdbcUrl = getRequiredProperty(context.getBundleContext(), JDBC_DRIVER_URL);
         jdbcUser = getRequiredProperty(context.getBundleContext(), JDBC_USER);
         jdbcPassword = getRequiredProperty(context.getBundleContext(), JDBC_PASSWORD);
+        maxLimit = getOptionalIntProperty(context.getBundleContext(), JDBC_POOLMAXLIMIT,50);
+        maxStatementsLimit = getOptionalIntProperty(context.getBundleContext(), JDBC_POOLMAXSTATEMENTS,50);
     }
 
     @Override
@@ -32,12 +40,22 @@ public class BootstrapServiceImpl implements BootstrapService {
         return createDataSourceFromProperties();
     }
 
-    private DataSource createDataSourceFromProperties() throws SQLException {
+    // tried to switch to UCP , but ran into OSGI related class loading problems.
+    // for now , we will live with deprecated methods
+    @SuppressWarnings("deprecation")
+	private DataSource createDataSourceFromProperties() throws SQLException {
         OracleDataSource source = new OracleDataSource();
         source.setURL(jdbcUrl);
         source.setUser(jdbcUser);
         source.setPassword(jdbcPassword);
         source.setConnectionCachingEnabled(true);
+        Properties connectionCacheProps = new Properties();
+        connectionCacheProps.put("MaxLimit",maxLimit);
+        connectionCacheProps.put("MaxStatementsLimit",maxStatementsLimit);        
+        source.setConnectionCacheProperties(connectionCacheProps);
+        // for now , no need to set connection properties , but possible interesting keys are
+        // defaultRowPrefetch
+        // oracle.jdbc.FreeMemoryOnEnterImplicitCache
         return source;
     }
 
@@ -49,4 +67,15 @@ public class BootstrapServiceImpl implements BootstrapService {
         return value;
     }
 
+    private int getOptionalIntProperty(BundleContext context, String property , int defaultValue) {
+        String stringValue = context.getProperty(property);
+        if (stringValue == null) {
+            return defaultValue;
+        }
+        try {
+        	return Integer.valueOf(stringValue);
+        } catch (NumberFormatException ex) {
+        	return defaultValue;
+        }
+    }
 }
