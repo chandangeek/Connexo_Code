aa236a0fb3592b92eb4d897a1b09ffbbfdad7129
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-06-05 13:20:17 +0200

    Enable terminating a PartyRole of a Party.

--- coko/com.elster.jupiter.ids/src/main/java/com/elster/jupiter/ids/impl/IdsServiceImpl.java
+++ coko/com.elster.jupiter.ids/src/main/java/com/elster/jupiter/ids/impl/IdsServiceImpl.java
@@ -10,69 +10,67 @@ import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.OrmService;
 import com.elster.jupiter.orm.callback.InstallService;
 
-@Component (name="com.elster.jupiter.ids", service={IdsService.class,InstallService.class}, property="name="+Bus.COMPONENTNAME)
+@Component(name = "com.elster.jupiter.ids", service = {IdsService.class, InstallService.class}, property = "name=" + Bus.COMPONENTNAME)
 public class IdsServiceImpl implements IdsService, InstallService, ServiceLocator {
-	private volatile OrmClient ormClient;
+
+    private volatile OrmClient ormClient;
     private volatile Clock clock;
-	
-	@Override
-	public Optional<Vault> getVault(String component, long id) {
-		return getOrmClient().getVaultFactory().get(component,id);
-	}
 
-	@Override
-	public Optional<RecordSpec> getRecordSpec(String component, long id) {
-		return getOrmClient().getRecordSpecFactory().get(component,id);
-	}
-
-	@Override
-	public Optional<TimeSeries> getTimeSeries(long id) {
-		return getOrmClient().getTimeSeriesFactory().get(id);
-	}
-	
-	@Override
-	public TimeSeriesDataStorer createStorer(boolean overrules) {
-		return new TimeSeriesDataStorerImpl(overrules);
-	}
-	
-	@Override
-	public void install() {
-		new InstallerImpl().install(true,true,true);
-	}
-	
-	@Override
-	public OrmClient getOrmClient() {
-		return ormClient;
-	}
-
-	@Override
-	public Vault newVault(String component, long id, String name, int slotCount , boolean regular) {
-		return new VaultImpl(component,id,name,slotCount,regular);
-	}
-
-	@Override
-	public RecordSpec newRecordSpec(String component, long id,String name) {
-		return new RecordSpecImpl(component, id, name);
-	}
-
-    @Reference 
-    public void setOrmService(OrmService ormService) {
-    	DataModel dataModel = ormService.getDataModel(Bus.COMPONENTNAME);
-    	if (dataModel == null) {
-    		dataModel = ormService.newDataModel(Bus.COMPONENTNAME, "TimeSeries Data Store");
-    		for (TableSpecs spec : TableSpecs.values()) {
-    			spec.addTo(dataModel);			
-    		}	
-    	}
-    	ormClient = new OrmClientImpl(dataModel);    		
+    @Override
+    public Optional<Vault> getVault(String component, long id) {
+        return getOrmClient().getVaultFactory().get(component, id);
     }
-    
+
+    @Override
+    public Optional<RecordSpec> getRecordSpec(String component, long id) {
+        return getOrmClient().getRecordSpecFactory().get(component, id);
+    }
+
+    @Override
+    public Optional<TimeSeries> getTimeSeries(long id) {
+        return getOrmClient().getTimeSeriesFactory().get(id);
+    }
+
+    @Override
+    public TimeSeriesDataStorer createStorer(boolean overrules) {
+        return new TimeSeriesDataStorerImpl(overrules);
+    }
+
+    @Override
+    public void install() {
+        new InstallerImpl().install(true, true, true);
+    }
+
+    @Override
+    public OrmClient getOrmClient() {
+        return ormClient;
+    }
+
+    @Override
+    public Vault newVault(String component, long id, String name, int slotCount, boolean regular) {
+        return new VaultImpl(component, id, name, slotCount, regular);
+    }
+
+    @Override
+    public RecordSpec newRecordSpec(String component, long id, String name) {
+        return new RecordSpecImpl(component, id, name);
+    }
+
+    @Reference
+    public void setOrmService(OrmService ormService) {
+        DataModel dataModel = ormService.newDataModel(Bus.COMPONENTNAME, "TimeSeries Data Store");
+        for (TableSpecs spec : TableSpecs.values()) {
+            spec.addTo(dataModel);
+        }
+        ormClient = new OrmClientImpl(dataModel);
+    }
+
     public void activate(ComponentContext context) {
-    	Bus.setServiceLocator(this);
+        Bus.setServiceLocator(this);
     }
 
     public void deactivate(ComponentContext context) {
-    	Bus.setServiceLocator(null);
+        Bus.setServiceLocator(null);
     }
 
     public Clock getClock() {
