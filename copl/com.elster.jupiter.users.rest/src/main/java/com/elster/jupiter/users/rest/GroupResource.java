86aee4e003c5272c04302e94303f7d98b7c7f230
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-06-12 09:04:33 +0200

    Grant/revoke privileges

--- copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/GroupResource.java
+++ copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/GroupResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -60,6 +61,17 @@ public class GroupResource {
         infos.total = queryParameters.determineTotal(list.size());
         return infos;
     }
+
+    @PUT
+    @Path("/{id}/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public GroupInfos updateGroup(GroupInfo info, @PathParam("id") long id) {
+        info.id = id;
+        Bus.getTransactionService().execute(new UpdateGroupTransaction(info));
+        return getGroup(info.id);
+    }
+
 
     private RestQuery<Group> getGroupRestQuery() {
         Query<Group> query = Bus.getUserService().getGroupQuery();
