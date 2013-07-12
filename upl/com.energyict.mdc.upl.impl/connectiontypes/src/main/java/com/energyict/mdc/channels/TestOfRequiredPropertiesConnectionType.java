08d85b755fd753114dd3a07022e020075533568c
Pasquien Dorny <Pasquien.Dorny@elster.com>
2013-07-12 12:19:39 +0000

    EISERVERSG-2120: Testing typedproperties of all kind in ConnectionType
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@75273 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/connectiontypes/src/main/java/com/energyict/mdc/channels/TestOfRequiredPropertiesConnectionType.java
@@ -0,0 +1,85 @@
+package com.energyict.mdc.channels;
+
+import com.energyict.cpo.PropertySpec;
+import com.energyict.cpo.PropertySpecFactory;
+import com.energyict.mdc.ports.ComPort;
+import com.energyict.mdc.ports.ComPortType;
+import com.energyict.mdc.protocol.*;
+import com.energyict.mdc.tasks.ConnectionTaskProperty;
+import com.energyict.mdc.tasks.ConnectionTypeImpl;
+
+import java.util.*;
+
+@SuppressWarnings("unused") // Pluggable
+public class TestOfRequiredPropertiesConnectionType extends ConnectionTypeImpl {
+
+    @Override
+    public boolean allowsSimultaneousConnections() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsComWindow() {
+        return false;
+    }
+
+    @Override
+    public Set<ComPortType> getSupportedComPortTypes() {
+        return EnumSet.allOf(ComPortType.class);
+    }
+
+    @Override
+    public ComChannel connect(ComPort comPort, List<ConnectionTaskProperty> properties) throws ConnectionException {
+        return new VoidComChannel();
+    }
+
+    @Override
+    public PropertySpec getPropertySpec(String name) {
+        return null;
+    }
+
+    @Override
+    public boolean isRequiredProperty(String name) {
+        return false;
+    }
+
+    @Override
+    public String getVersion() {
+        return "$Date$";
+    }
+
+    @Override
+    public List<PropertySpec> getRequiredProperties() {
+        List<PropertySpec> specs = new ArrayList<>();
+        specs.add(PropertySpecFactory.bigDecimalPropertySpec("BigDecimal"));
+        specs.add(PropertySpecFactory.booleanPropertySpec("Boolean"));
+        specs.add(PropertySpecFactory.booleanPropertySpecWithoutThreeState("NoThreeStateBoolean"));
+     //   specs.add(PropertySpecFactory.customCoordinatesPropertySpec("CustomCoordinates"));
+        specs.add(PropertySpecFactory.dateTimePropertySpec("DateTime"));
+        specs.add(PropertySpecFactory.datePropertySpec("Date"));
+     //   specs.add(PropertySpecFactory.ean13PropertySpec("Ean13"));
+     //   specs.add(PropertySpecFactory.ean18PropertySpec("Ean18"));
+     //   specs.add(PropertySpecFactory.encryptedStringPropertySpec("EncryptedString"));
+//        specs.add(PropertySpecFactory.hexStringPropertySpec("HexString"));
+        specs.add(PropertySpecFactory.largeStringPropertySpec("LargeString"));
+        specs.add(PropertySpecFactory.obisCodePropertySpec("ObisCode"));
+        specs.add(PropertySpecFactory.passwordPropertySpec("Password"));
+     //   specs.add(PropertySpecFactory.spatialCoordinatesPropertySpec("SpatialCoordinates"));
+        specs.add(PropertySpecFactory.stringPropertySpec("String"));
+        specs.add(PropertySpecFactory.timeDurationPropertySpec("TimeDuration"));
+        specs.add(PropertySpecFactory.timeOfDayPropertySpec("TimeOfDay"));
+        //references
+        specs.add(PropertySpecFactory.codeTableReferencePropertySpec("CodeTable"));
+        specs.add(PropertySpecFactory.loadProfilePropertySpec("LoadProfile"));
+        specs.add(PropertySpecFactory.loadProfileTypePropertySpecByList("LoadProfileType"));
+        specs.add(PropertySpecFactory.lookupPropertySpec("Lookup"));
+        specs.add(PropertySpecFactory.timeZoneInUseReferencePropertySpec("TimeZoneInUse"));
+        specs.add(PropertySpecFactory.userFileReferencePropertySpec("UserFile"));
+        return specs;
+    }
+
+    @Override
+    public List<PropertySpec> getOptionalProperties() {
+        return Collections.emptyList();
+    }
+}
