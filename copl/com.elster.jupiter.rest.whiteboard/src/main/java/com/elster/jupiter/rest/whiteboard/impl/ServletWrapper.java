e5660a4803f61d14fd68acccdb91a51031d3294f
Karel Haeck <k.haeck@energyict.com>
2013-08-01 14:22:40 +0200

    added module and action

--- copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/ServletWrapper.java
+++ copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/ServletWrapper.java
@@ -31,12 +31,28 @@ public class ServletWrapper extends HttpServlet  {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		request = new RequestWrapper(request);
 		ThreadPrincipalService service = Bus.getThreadPrincipalService();
-		service.set(request.getUserPrincipal());
+		service.set(request.getUserPrincipal(),getModule(request),request.getMethod());
 		try {
 			servlet.service(request,response);
 		} finally {
 			service.clear();
 		}
+	}
+	
+	// returns the first two parts 
+	private String getModule(HttpServletRequest request) {
+		String result = request.getServletPath();
+		String path = request.getPathInfo();
+		if (path == null) {
+			return result;
+		} else {
+			String[] parts = request.getPathInfo().split("/",3);
+			if (parts.length == 0) {
+				return result;
+			} else {
+				return result + "/" + parts[1];
+			}
+		}				
 	}
 
 }
