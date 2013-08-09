5f8729b6ca9bdbf3c60084c4f41b274c4577db81
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-08-09 08:49:58 +0200

    Events implementation

---
+++ copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/TransactionRequired.java
@@ -0,0 +1,15 @@
+package com.elster.jupiter.orm;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Method annotation that documents it as needing to be called from within a transaction.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TransactionRequired {
+}
