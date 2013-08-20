40020ed7cb7aafd18ff59f40bb966e138790d654
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-08-20 13:40:24 +0200

    Exception documantation

--- copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/FileIOException.java
+++ copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/FileIOException.java
@@ -4,6 +4,9 @@ import com.elster.jupiter.util.exception.BaseException;
 
 import java.io.IOException;
 
+/**
+ * RuntimeException to wrap IOException that occur when reading/writing files.
+ */
 public class FileIOException extends BaseException {
 
     public FileIOException(IOException cause) {
