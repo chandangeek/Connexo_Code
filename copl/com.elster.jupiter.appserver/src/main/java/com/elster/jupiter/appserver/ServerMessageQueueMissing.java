7cb4da5cca74c58fbdd4a57a0cf88d18ff1f92b9
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-08-22 16:06:14 +0200

    Adding unit tests

--- copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/ServerMessageQueueMissing.java
+++ copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/ServerMessageQueueMissing.java
@@ -11,5 +11,6 @@ public class ServerMessageQueueMissing extends BaseException {
 
     public ServerMessageQueueMissing(String destinationName) {
         super(ExceptionTypes.SERVER_MESSAGE_QUEUE_MISSING, MessageFormat.format("Server's message queue with name ''{0}'' not found", destinationName));
+        set("destination", destinationName);
     }
 }
