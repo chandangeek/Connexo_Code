ec4c5ceee34d67e9a867cac48e994643b0cf74dc
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-08-27 14:24:30 +0200

    Adding unit tests

--- copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/domain/util/Query.java
+++ copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/domain/util/Query.java
@@ -1,19 +1,19 @@
 package com.elster.jupiter.domain.util;
 
-import java.util.List;
-
 import com.elster.jupiter.util.conditions.Condition;
 import com.elster.jupiter.util.conditions.Subquery;
+
+import java.util.List;
 
 public interface Query<T> {
 	void setLazy(String... includes);
 	void setEager(String... excludes);
-	List<T> select(Condition condition, String ... orderBy);	
-	List<T> select(Condition condition,int from , int to , String ... orderBy);
+    List<T> select(Condition condition, String ... orderBy);
+    List<T> select(Condition condition,int from , int to , String ... orderBy);
 	T get(Object ... key);
 	boolean hasField(String fieldName);
 	Object convert(String fieldName , String value);
 	Subquery asSubquery(Condition condition, String ... fieldNames);
-	List<String> getQueryFieldNames();
+    List<String> getQueryFieldNames();
 	Class<?> getType(String fieldName);
 }
