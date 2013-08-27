2d03fb791c617809929395436ed13cf465ff43c4
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-08-27 14:24:28 +0200

    Adding unit tests

--- copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/RestQuery.java
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/RestQuery.java
@@ -5,7 +5,7 @@ import com.elster.jupiter.util.conditions.Condition;
 import java.util.List;
 
 public interface RestQuery<T> {
-	List<T> select(QueryParameters queryParameters);
+    List<T> select(QueryParameters queryParameters);
 
     List<T> select(QueryParameters queryParameters, Condition condition);
 }
