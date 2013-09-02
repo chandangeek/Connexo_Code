e975a8263ca1d1331a8a052e15e5ad52344721bc
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-09-02 09:08:58 +0200

    FileImport unit tests

--- copl/com.elster.jupiter.messaging/src/main/java/com/elster/jupiter/messaging/subscriber/MessageHandler.java
+++ copl/com.elster.jupiter.messaging/src/main/java/com/elster/jupiter/messaging/subscriber/MessageHandler.java
@@ -2,9 +2,7 @@ package com.elster.jupiter.messaging.subscriber;
 
 import com.elster.jupiter.messaging.Message;
 
-import java.sql.SQLException;
-
 public interface MessageHandler {
 
-    void process(Message message) throws SQLException;
+    void process(Message message);
 }
