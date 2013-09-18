7d37983f5defe6477fc3eb807711f91dbaf7445d
Karel Haeck <k.haeck@energyict.com>
2013-09-18 14:58:23 +0200

    Added comments

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/sql/SqlFragment.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/sql/SqlFragment.java
@@ -3,9 +3,24 @@ package com.elster.jupiter.util.sql;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/*
+ * SqlFragment encapsulates both the sql fragment, 
+ * as the related the PreparedStatement setXXX methods
+ */
 public interface SqlFragment {
-
+	/**
+	 * bind values to host variables, using PreparedStatement setXXX methods.
+	 * Will be called from SqlBuilder.prepare
+	 * @param statement
+	 * @param position start position
+	 * @return position + number of bound variables
+	 * @throws SQLException
+	 */
 	int bind(PreparedStatement statement , int position) throws SQLException;
 
+	/**
+	 * Will be called from SqlBuilder.add.
+	 * @return the fragment's sql text, which typically contains ? place holders for bound variables  
+	 */
     String getText();
 }
