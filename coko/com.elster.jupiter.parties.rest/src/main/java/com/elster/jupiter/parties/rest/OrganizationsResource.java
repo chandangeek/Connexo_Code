f4c63b3b7a3694d15333311f625c2e291b310f96
Karel Haeck <k.haeck@energyict.com>
2013-09-19 15:27:45 +0200

    moved rest interface to /api space

--- coko/com.elster.jupiter.parties.rest/src/main/java/com/elster/jupiter/parties/rest/OrganizationsResource.java
+++ coko/com.elster.jupiter.parties.rest/src/main/java/com/elster/jupiter/parties/rest/OrganizationsResource.java
@@ -24,7 +24,7 @@ import javax.ws.rs.core.UriInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("/prt/organizations")
+@Path("/organizations")
 public class OrganizationsResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
--- coko/com.elster.jupiter.parties.rest/src/main/java/com/elster/jupiter/parties/rest/RolesResource.java
+++ coko/com.elster.jupiter.parties.rest/src/main/java/com/elster/jupiter/parties/rest/RolesResource.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
-@Path("/prt/roles")
+@Path("/roles")
 public class RolesResource {
 
     @POST
