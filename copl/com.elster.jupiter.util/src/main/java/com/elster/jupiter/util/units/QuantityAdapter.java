93147a56d8f3d482afc070373be0d583dc666210
Karel Haeck <k.haeck@energyict.com>
2013-09-26 11:22:16 +0200

    Prevent creation of illegal Quantities

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/units/QuantityAdapter.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/units/QuantityAdapter.java
@@ -6,13 +6,16 @@ public final class QuantityAdapter extends XmlAdapter<AdaptedQuantity,Quantity> 
 
 	@Override
 	public Quantity unmarshal(AdaptedQuantity v) throws Exception {
+		if (v.value == null ||  v.unit == null) {
+			return null;
+		}
 		Unit unit = Unit.get(v.unit);
 		return unit.amount(v.value,v.multiplier);
 	}
 
 	@Override
 	public AdaptedQuantity marshal(Quantity v) throws Exception {		
-		return v == null ? null : new AdaptedQuantity(v);
+		return v == null ? null : AdaptedQuantity.from(v);
 	}
 
 }
