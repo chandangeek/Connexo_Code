1fb93dbf70efafef4fd82bc50dd1915287c51593
Kristof Hennebel <Kristof.Hennebel@elster.com>
2013-10-24 08:30:40 +0000

    COMMUNICATION-944 - CLONE - [ComServer&Protocol] Device Messages - adaptor
    Added some general message entry creators that are reusable
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@78695 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/messages/convertor/messageentrycreators/general/OneTagMessageEntry.java
@@ -0,0 +1,29 @@
+package com.energyict.protocolimplv2.messages.convertor.messageentrycreators.general;
+
+import com.energyict.mdw.offline.OfflineDeviceMessage;
+import com.energyict.protocol.MessageEntry;
+import com.energyict.protocol.messaging.*;
+import com.energyict.protocolimplv2.messages.convertor.MessageEntryCreator;
+
+/**
+ * Creates XML: <tag/>
+ *
+ * Copyrights EnergyICT
+ * Date: 24/10/13
+ * Time: 10:23
+ * Author: khe
+ */
+public class OneTagMessageEntry implements MessageEntryCreator {
+
+    private final String tag;
+
+    public OneTagMessageEntry(String tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public MessageEntry createMessageEntry(Messaging messagingProtocol, OfflineDeviceMessage offlineDeviceMessage) {
+        MessageTag messageTag = new MessageTag(tag);
+        return new MessageEntry(messagingProtocol.writeTag(messageTag), offlineDeviceMessage.getTrackingId());
+    }
+}
---
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/messages/convertor/messageentrycreators/general/SimpleTagMessageEntry.java
@@ -0,0 +1,30 @@
+package com.energyict.protocolimplv2.messages.convertor.messageentrycreators.general;
+
+import com.energyict.mdw.offline.OfflineDeviceMessage;
+import com.energyict.protocol.MessageEntry;
+import com.energyict.protocol.messaging.*;
+import com.energyict.protocolimplv2.messages.convertor.MessageEntryCreator;
+
+/**
+ * Creates XML: <tag> </tag>
+ *
+ * Copyrights EnergyICT
+ * Date: 24/10/13
+ * Time: 10:23
+ * Author: khe
+ */
+public class SimpleTagMessageEntry implements MessageEntryCreator {
+
+    private final String tag;
+
+    public SimpleTagMessageEntry(String tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public MessageEntry createMessageEntry(Messaging messagingProtocol, OfflineDeviceMessage offlineDeviceMessage) {
+        MessageTag messageTag = new MessageTag(tag);
+        messageTag.add(new MessageValue(" "));
+        return new MessageEntry(messagingProtocol.writeTag(messageTag), offlineDeviceMessage.getTrackingId());
+    }
+}
