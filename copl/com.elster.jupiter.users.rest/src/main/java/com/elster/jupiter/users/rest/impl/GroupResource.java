5a85f78708a7cd96f465a7af87f7d33a97d142ba
Karel Haeck <k.haeck@energyict.com>
2013-10-28 09:44:42 +0100

    eclipse warning

--- copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/impl/GroupResource.java
+++ copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/impl/GroupResource.java
@@ -18,7 +18,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.List;
 
 @Path("/groups")
 public class GroupResource {
