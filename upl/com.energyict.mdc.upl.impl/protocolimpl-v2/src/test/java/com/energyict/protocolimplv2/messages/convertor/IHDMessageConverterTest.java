2f996d84be4713d159eec2b69a575e44c342db82
Stijn Vanhoorelbeke <Stijn.Vanhoorelbeke@elster.com>
2013-10-28 09:31:28 +0000

    COMMUNICATION-944 - [ComServer&Protocol] Device Messages - adaptor Message converter for IHD protocol
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@78788 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/test/java/com/energyict/protocolimplv2/messages/convertor/IHDMessageConverterTest.java
@@ -0,0 +1,71 @@
+package com.energyict.protocolimplv2.messages.convertor;
+
+import com.energyict.cpo.PropertySpec;
+import com.energyict.mdc.messages.LegacyMessageConverter;
+import com.energyict.mdw.core.UserFile;
+import com.energyict.mdw.offline.OfflineDeviceMessage;
+import com.energyict.protocol.MessageEntry;
+import com.energyict.protocol.messaging.Messaging;
+import com.energyict.protocolimplv2.messages.DeviceMessageConstants;
+import com.energyict.protocolimplv2.messages.FirmwareDeviceMessage;
+import com.energyict.smartmeterprotocolimpl.eict.ukhub.UkHub;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test that creates OfflineDeviceMessages (the attributes are all filled with dummy values) and converts them to the legacy XML message,
+ * testing the {@link IHDMessageConverter} component.
+ *
+ * @author sva
+ * @since 28/10/13 - 10:26
+ */
+public class IHDMessageConverterTest extends AbstractMessageConverterTest {
+
+    @Test
+    public void testMessageConversion() {
+        MessageEntry messageEntry;
+        OfflineDeviceMessage offlineDeviceMessage;
+
+        offlineDeviceMessage = createMessage(FirmwareDeviceMessage.UPGRADE_FIRMWARE_WITH_USER_FILE);
+        messageEntry = getMessageConverter().toMessageEntry(offlineDeviceMessage);
+        assertEquals("<FirmwareUpgrade UserFileID=\"1\"> </FirmwareUpgrade>", messageEntry.getContent());
+
+        offlineDeviceMessage = createMessage(FirmwareDeviceMessage.UPGRADE_FIRMWARE_WITH_USER_FILE_AND_ACTIVATE);
+        messageEntry = getMessageConverter().toMessageEntry(offlineDeviceMessage);
+        assertEquals("<FirmwareUpgrade UserFileID=\"1\" Activation_date=\"28/10/2013 10:00:00\"> </FirmwareUpgrade>", messageEntry.getContent());
+    }
+
+    @Override
+    protected Messaging getMessagingProtocol() {
+        return new UkHub();
+    }
+
+    @Override
+    LegacyMessageConverter doGetMessageConverter() {
+        return new UkHubMessageConverter();
+    }
+
+    @Override
+    protected Object getPropertySpecValue(PropertySpec propertySpec) {
+        try {
+            switch (propertySpec.getName()) {
+                case DeviceMessageConstants.firmwareUpdateUserFileAttributeName:
+                    UserFile mockedUserFile = mock(UserFile.class);
+                    when(mockedUserFile.getId()).thenReturn(1);
+                    return mockedUserFile;
+                case DeviceMessageConstants.firmwareUpdateActivationDateAttributeName:
+                    return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse("28/10/2013 10:00:00");
+                default:
+                    return "";
+            }
+        } catch (ParseException e) {
+            return "";
+        }
+    }
+}
