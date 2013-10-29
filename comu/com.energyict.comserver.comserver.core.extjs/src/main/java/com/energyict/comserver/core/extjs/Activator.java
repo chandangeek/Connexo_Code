b3f9f31e12979abda8087a8a4394e631f36e1195
Govanni Naessens <g.naessens@energyict.com>
2013-10-29 13:40:26 +0100

    Cleaned up the activator for now

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/Activator.java
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/Activator.java
@@ -1,10 +1,7 @@
 package com.energyict.comserver.core.extjs;
 
-import com.elster.jupiter.http.whiteboard.BundleResolver;
-import com.elster.jupiter.http.whiteboard.HttpResource;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 
 /**
  * Copyrights EnergyICT
@@ -13,18 +10,18 @@ import org.osgi.framework.ServiceRegistration;
  */
 public class Activator implements BundleActivator {
 
-    private volatile ServiceRegistration<HttpResource> registration;
+//    private volatile ServiceRegistration<HttpResource> registration;
 
     public void start(BundleContext bundleContext) throws Exception {
-        String alias = "/cms";
-        HttpResource resource = new HttpResource(alias, "/js/cms" , new BundleResolver(bundleContext));
-        // Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
-        //HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/meteringextjs/js/mtr" , new FileResolver());
-        registration = bundleContext.registerService(HttpResource.class, resource , null);
+//        String alias = "/cms";
+//        HttpResource resource = new HttpResource(alias, "/js/cms" , new BundleResolver(bundleContext));
+//        Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
+//        HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/meteringextjs/js/mtr" , new FileResolver());
+//        registration = bundleContext.registerService(HttpResource.class, resource , null);
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
-        registration.unregister();
+//        registration.unregister();
     }
 
 }
