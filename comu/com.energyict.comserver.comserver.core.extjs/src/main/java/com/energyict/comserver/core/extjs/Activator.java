03988594aefcd7dbafbf04309e584bc7797910ea
Govanni Naessens <g.naessens@energyict.com>
2013-10-29 16:36:21 +0100

    Fixed the comserver-core-extjs stuff. Needed to import the http.whiteboard, the parent prevented this (maybe we should update the parent instead)

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/Activator.java
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/Activator.java
@@ -1,7 +1,10 @@
 package com.energyict.comserver.core.extjs;
 
+import com.elster.jupiter.http.whiteboard.BundleResolver;
+import com.elster.jupiter.http.whiteboard.HttpResource;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * Copyrights EnergyICT
@@ -10,18 +13,18 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
 
-//    private volatile ServiceRegistration<HttpResource> registration;
+    private volatile ServiceRegistration<HttpResource> registration;
 
     public void start(BundleContext bundleContext) throws Exception {
-//        String alias = "/cms";
-//        HttpResource resource = new HttpResource(alias, "/js/cms" , new BundleResolver(bundleContext));
+        String alias = "/cms";
+        HttpResource resource = new HttpResource(alias, "/js/cms" , new BundleResolver(bundleContext));
 //        Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
 //        HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/meteringextjs/js/mtr" , new FileResolver());
-//        registration = bundleContext.registerService(HttpResource.class, resource , null);
+        registration = bundleContext.registerService(HttpResource.class, resource , null);
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
-//        registration.unregister();
+        registration.unregister();
     }
 
 }
