aa872e40fda8f2fb0637a75012ddaa1db9870fc1
Bart Vansegbroeck <b.vansegbroeck@energyict.com>
2013-11-06 09:44:12 +0100

    Added ComServersInfo wrapper

--- comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/OnlineComServerInfo.java
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/OnlineComServerInfo.java
@@ -3,18 +3,23 @@ package com.energyict.mdc.rest.impl;
 import com.energyict.mdc.servers.OnlineComServer;
 
 public class OnlineComServerInfo extends ComServerInfo{
-    private String queryAPIPostUri;
-    private boolean usesDefaultQueryAPIPostUri;
-    private String eventRegistrationUri;
-    private boolean usesDefaultEventRegistrationUri;
-    private int storeTaskQueueSize;
-    private int numberOfStoreTaskThreads;
-    private int storeTaskThreadPriority;
+    public String queryAPIPostUri;
+    public boolean usesDefaultQueryAPIPostUri;
+    public String eventRegistrationUri;
+    public boolean usesDefaultEventRegistrationUri;
+    public int storeTaskQueueSize;
+    public int numberOfStoreTaskThreads;
+    public int storeTaskThreadPriority;
 
     public OnlineComServerInfo(OnlineComServer onlineComServer) {
         super("OnlineComServer");
         this.queryAPIPostUri = onlineComServer.getQueryApiPostUri();
+        this.usesDefaultQueryAPIPostUri = onlineComServer.usesDefaultQueryApiPostUri();
+        this.eventRegistrationUri = onlineComServer.getEventRegistrationUri();
+        this.usesDefaultEventRegistrationUri = onlineComServer.usesDefaultEventRegistrationUri();
+        this.storeTaskQueueSize = onlineComServer.getStoreTaskQueueSize();
+        this.numberOfStoreTaskThreads = onlineComServer.getNumberOfStoreTaskThreads();
+        this.storeTaskThreadPriority = onlineComServer.getStoreTaskThreadPriority();
     }
-
 
 }
--- comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/TimeDurationInfo.java
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/TimeDurationInfo.java
@@ -3,8 +3,8 @@ package com.energyict.mdc.rest.impl;
 import com.energyict.cbo.TimeDuration;
 
 public class TimeDurationInfo {
-    private int count;
-    private String timeUnit;
+    public int count;
+    public String timeUnit;
 
     public TimeDurationInfo() {
     }
