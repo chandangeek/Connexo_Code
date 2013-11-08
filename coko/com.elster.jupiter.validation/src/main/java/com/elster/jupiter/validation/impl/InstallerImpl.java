f9caafd32baa3e659b81b7c216774fb67f61e186
Isabelle  Gheysens <i.gheysens@energyict.com>
2013-11-08 16:19:24 +0100

    ValidationRuleSet classes added

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/InstallerImpl.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/InstallerImpl.java
@@ -2,9 +2,6 @@ package com.elster.jupiter.validation.impl;
 
 public class InstallerImpl {
 
-    private static final int SLOT_COUNT = 8;
-    private static final int MONTHS_PER_YEAR = 12;
-
     public void install(boolean executeDdl, boolean updateOrm) {
         try {
             Bus.getOrmClient().install(executeDdl, updateOrm);
