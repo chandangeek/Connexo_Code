0f70b86f84ae0edb79c4c5fb2e2c6c9577e5e2eb
Bart Vansegbroeck <b.vansegbroeck@energyict.com>
2013-11-12 09:45:35 +0100

    ComServers are now ordered

--- comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/ComServersInfo.java
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/ComServersInfo.java
@@ -1,7 +1,7 @@
 package com.energyict.mdc.rest.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -10,5 +10,5 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class ComServersInfo {
     @XmlElement
     @XmlElementWrapper(name = "ComServers")
-    public Collection<? super ComServerInfo> comServers = new HashSet<>();
+    public Collection<? super ComServerInfo> comServers = new ArrayList<>();
 }
