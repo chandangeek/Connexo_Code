0a35438b256c4242991536e23a8cf5ed9d1df44a
Govanni Naessens <g.naessens@energyict.com>
2013-11-18 13:38:09 +0100

    Introduced the abstract layer in between the PluggableClassServices

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocols/mdc/services/DeviceProtocolServiceImpl.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocols/mdc/services/DeviceProtocolServiceImpl.java
@@ -4,21 +4,13 @@ import com.energyict.cbo.BusinessException;
 import com.energyict.comserver.adapters.meterprotocol.MeterProtocolAdapter;
 import com.energyict.comserver.adapters.smartmeterprotocol.SmartMeterProtocolAdapter;
 import com.energyict.comserver.exceptions.CodingException;
-import com.energyict.cpo.Environment;
 import com.energyict.mdc.protocol.DeviceProtocol;
 import com.energyict.mdc.services.DeviceProtocolService;
-import com.energyict.mdw.core.MeteringWarehouse;
 import com.energyict.mdw.core.Pluggable;
 import com.energyict.mdw.core.PluggableClass;
-import com.energyict.mdw.shadow.PluggableClassShadow;
 import com.energyict.protocol.MeterProtocol;
 import com.energyict.protocol.SmartMeterProtocol;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-
-import java.sql.SQLException;
 
 /**
  * Copyrights EnergyICT
@@ -26,34 +18,7 @@ import java.sql.SQLException;
  * Time: 11:03
  */
 @Component(name = "com.energyict.mdc.service.deviceprotocols", service = DeviceProtocolService.class, immediate = true)
-public class DeviceProtocolServiceImpl implements DeviceProtocolService {
-
-    @Activate
-    public void activate(BundleContext context) {
-        MeteringWarehouse.createBatchContext(true);
-    }
-
-    @Deactivate
-    public void deactivate(){
-        Environment.getDefault().closeConnection();
-    }
-
-    public PluggableClass create(PluggableClassShadow shadow) throws BusinessException, SQLException {
-        return MeteringWarehouse.getCurrent().getPluggableClassFactory().create(shadow);
-    }
-
-    public PluggableClass update(int id, PluggableClassShadow shadow) throws BusinessException, SQLException {
-        PluggableClass pluggableClass = MeteringWarehouse.getCurrent().getPluggableClassFactory().find(id);
-        if (pluggableClass != null) {
-            pluggableClass.update(shadow);
-        }
-        return pluggableClass;
-    }
-
-    public void delete(int id) throws BusinessException, SQLException {
-        PluggableClass pluggableClass = MeteringWarehouse.getCurrent().getPluggableClassFactory().find(id);
-        pluggableClass.delete();
-    }
+public class DeviceProtocolServiceImpl extends AbstractPluggableClassServiceImpl implements DeviceProtocolService {
 
     @Override
     public DeviceProtocol createDeviceProtocolFor(PluggableClass pluggableClass) {
