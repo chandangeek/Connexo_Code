2f3d71de9c8fd991bb7f37cea84e061db70f2851
Isabelle  Gheysens <i.gheysens@energyict.com>
2013-11-21 16:28:29 +0100

    rules added

--- coko/com.elster.jupiter.validation.rest/src/main/java/com.elster.jupiter.validation.rest.impl/ValidationResource.java
+++ coko/com.elster.jupiter.validation.rest/src/main/java/com.elster.jupiter.validation.rest.impl/ValidationResource.java
@@ -1,11 +1,13 @@
 package com.elster.jupiter.validation.rest.impl;
 
+import com.elster.jupiter.validation.ValidationAction;
 import com.elster.jupiter.validation.ValidationRuleSet;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -18,6 +20,19 @@ public class ValidationResource {
         List<ValidationRuleSet> list = Bus.getValidationService().getValidationRuleSets();
         ValidationRuleSetInfos infos = new ValidationRuleSetInfos(list);
         infos.total = list.size();
+        return infos;
+    }
+
+    @GET
+    @Path("/actions")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ValidationActionInfos getAvailableValidationActions(@Context UriInfo uriInfo) {
+        ValidationActionInfos infos = new ValidationActionInfos();
+        ValidationAction[] actions = ValidationAction.values();
+        for (ValidationAction action : actions) {
+            infos.add(action);
+        }
+        infos.total = actions.length;
         return infos;
     }
 	
