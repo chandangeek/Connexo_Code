7049d4d9b82d71cea5865e61c5a50d2c945b8717
Isabelle  Gheysens <i.gheysens@energyict.com>
2013-11-21 14:50:04 +0100

    rules added

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/ValidationAction.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/ValidationAction.java
@@ -1,5 +1,29 @@
 package com.elster.jupiter.validation;
 
 public enum ValidationAction {
-    WARN_ONLY, FAIL // TODO check with Karel for completeness
-}
+    WARN_ONLY ("warnOnly"), FAIL ("fail");  // TODO check with Karel for completeness
+
+    private final String value;
+
+    private ValidationAction(String value) {
+        this.value = value;
+    }
+
+    public static ValidationAction get(int id) {
+        return values()[id-1];
+    }
+
+    public int getId() {
+        return ordinal() + 1;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    }
