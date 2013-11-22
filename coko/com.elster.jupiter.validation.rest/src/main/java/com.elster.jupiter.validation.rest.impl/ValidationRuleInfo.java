ffcbb1b6d8e4f5a4480a1681425c961cef62bfa9
Isabelle  Gheysens <i.gheysens@energyict.com>
2013-11-22 16:10:33 +0100

    adding properties

--- coko/com.elster.jupiter.validation.rest/src/main/java/com.elster.jupiter.validation.rest.impl/ValidationRuleInfo.java
+++ coko/com.elster.jupiter.validation.rest/src/main/java/com.elster.jupiter.validation.rest.impl/ValidationRuleInfo.java
@@ -2,6 +2,10 @@ package com.elster.jupiter.validation.rest.impl;
 
 import com.elster.jupiter.validation.ValidationAction;
 import com.elster.jupiter.validation.ValidationRule;
+import com.elster.jupiter.validation.ValidationRuleProperties;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ValidationRuleInfo {
 
@@ -10,12 +14,16 @@ public class ValidationRuleInfo {
     public ValidationAction action;
     public String implementation; //validator name
     public int position;
+    public List<ValidationRulePropertyInfo> properties = new ArrayList<ValidationRulePropertyInfo>();
 
     public ValidationRuleInfo(ValidationRule validationRule) {
         id = validationRule.getId();
         active = validationRule.isActive();
         action = validationRule.getAction();
         implementation = validationRule.getImplementation();
+        for (ValidationRuleProperties property : validationRule.getProperties()) {
+            properties.add(new ValidationRulePropertyInfo(property));
+        }
     }
 
     public ValidationRuleInfo() {
---
+++ coko/com.elster.jupiter.validation.rest/src/main/java/com.elster.jupiter.validation.rest.impl/ValidationRulePropertyInfo.java
@@ -0,0 +1,27 @@
+package com.elster.jupiter.validation.rest.impl;
+
+import com.elster.jupiter.util.units.Quantity;
+import com.elster.jupiter.util.units.Unit;
+import com.elster.jupiter.validation.ValidationRuleProperties;
+
+import java.math.BigDecimal;
+
+public class ValidationRulePropertyInfo {
+
+    public String name;
+    public BigDecimal value;
+    public Unit unit = Unit.WATT_HOUR;
+    public int multiplier = 1;
+
+
+    public ValidationRulePropertyInfo(ValidationRuleProperties validationRuleProperties) {
+        name = validationRuleProperties.getName();
+        Quantity qty = validationRuleProperties.getValue();
+        value = qty.getValue();
+        unit = qty.getUnit();
+        multiplier = qty.getMultiplier();
+    }
+
+    public ValidationRulePropertyInfo() {
+    }
+}
