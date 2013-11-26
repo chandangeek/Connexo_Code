91a3ed9a959a8262c052a7172deeb4428a5a6fba
kha <karel.haeck@elster.com>
2013-11-26 09:58:15 +0100

    Added getMinutes to TimeAttribute

--- coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/TimeAttribute.java
+++ coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/TimeAttribute.java
@@ -1,59 +1,62 @@
 package com.elster.jupiter.cbo;
 
 public enum TimeAttribute {
-	NOTAPPLICABLE (0,"NotApplicable"),
-	MINUTE10 (1,"10-minute"),
-	MINUTE15 (2,"15-minute"),
-	MINUTE1 (3,"1-minute"),
-	HOUR24 (4,"24-hour"),
-	MINUTE30 (5,"30-minute"),
-	MINUTE5 (6,"5-minute"),
-	MINUTE60 (7,"60-minute"),
-	BILLINGPERIOD (8,"BillingPeriod"),
-	BLOCK (9,"block"),
-	MINUTE2 (10,"2-minute"),
-	DAILYSHIFTED (11,"Daily"),
-	INSTANTANEOUS (12,"Instantaneous"),
-	MONTHLYSHIFTED (13,"Monthly"),
-	MINUTE3 (14,"3-minute"),
-	PRESENT (15,"Present"),
-	PREVIOUS (16,"Previous"),
-	MINUTE20 (31,"20-minute"),
-	FIXEDBLOCK60MIN(50,"60-Minute Fixed Block"),
-	FIXEDBLOCK30MIN(51,"30-Minute Fixed Block"),
-	FIXEDBLOCK20MIN(52,"20-Minute Fixed Block"),
-	FIXEDBLOCK15MIN(53,"15-Minute Fixed Block"),
-	FIXEDBLOCK10MIN(53,"10-Minute Fixed Block"),
-	FIXEDBLOCK5MIN(54,"5-Minute Fixed Block"),
-	FIXEDBLOCK1MIN(55,"1-Minute Fixed Block"),
-	ROLLING60_30(57,"Rolling Block 60-Minute with 30-Minute Sub-intervals"),
-	ROLLING60_20(58,"Rolling Block 60-Minute with 20-Minute Sub-intervals"),
-	ROLLING60_15(59,"Rolling Block 60-Minute with 15-Minute Sub-intervals"),
-	ROLLING60_12(60,"Rolling Block 60-Minute with 12-Minute Sub-intervals"),
-	ROLLING60_10(61,"Rolling Block 60-Minute with 10-Minute Sub-intervals"),
-	ROLLING60_6(62,"Rolling Block 60-Minute with 6-Minute Sub-intervals"),
-	ROLLING60_5(63,"Rolling Block 60-Minute with 5-Minute Sub-intervals"),
-	ROLLING60_4(64,"Rolling Block 60-Minute with 4-Minute Sub-intervals"),
-	ROLLING30_15(65,"Rolling Block 30-Minute with 15-Minute Sub-intervals"),
-	ROLLING30_10(66,"Rolling Block 30-Minute with 10-Minute Sub-intervals"),
-	ROLLING30_6(67,"Rolling Block 30-Minute with 6-Minute Sub-intervals"),
-	ROLLING30_5(68,"Rolling Block 30-Minute with 5-Minute Sub-intervals"),
-	ROLLING30_3(69,"Rolling Block 30-Minute with 3-Minute Sub-intervals"),
-	ROLLING30_2(70,"Rolling Block 30-Minute with 2-Minute Sub-intervals"),
-	ROLLING15_5(71,"Rolling Block 15-Minute with 5-Minute Sub-intervals"),
-	ROLLING15_3(72,"Rolling Block 15-Minute with 3-Minute Sub-intervals"),
-	ROLLING15_1(73,"Rolling Block 15-Minute with 1-Minute Sub-intervals"),
-	ROLLING10_5(74,"Rolling Block 10-Minute with 5-Minute Sub-intervals"),
-	ROLLING10_2(75,"Rolling Block 10-Minute with 2-Minute Sub-intervals"),
-	ROLLING10_1(76,"Rolling Block 10-Minute with 2-Minute Sub-intervals"),
-	ROLLING5_1(77,"Rolling Block 5-Minute with 1-Minute Sub-intervals");
+	NOTAPPLICABLE (0,"NotApplicable",0),
+	MINUTE10 (1,"10-minute",10),
+	MINUTE15 (2,"15-minute",15),
+	MINUTE1 (3,"1-minute",1),
+	HOUR24 (4,"24-hour",60*24),
+	MINUTE30 (5,"30-minute",30),
+	MINUTE5 (6,"5-minute",5),
+	MINUTE60 (7,"60-minute",60),
+	BILLINGPERIOD (8,"BillingPeriod",0),
+	BLOCK (9,"block",0),
+	MINUTE2 (10,"2-minute",2),
+	MINUTE3 (14,"3-minute",3),
+	PRESENT (15,"Present",0),
+	PREVIOUS (16,"Previous",0),
+	MINUTE20 (31,"20-minute",20),
+	FIXEDBLOCK60MIN(50,"60-Minute Fixed Block",60),
+	FIXEDBLOCK30MIN(51,"30-Minute Fixed Block",30),
+	FIXEDBLOCK20MIN(52,"20-Minute Fixed Block",20),
+	FIXEDBLOCK15MIN(53,"15-Minute Fixed Block",15),
+	FIXEDBLOCK10MIN(53,"10-Minute Fixed Block",10),
+	FIXEDBLOCK5MIN(54,"5-Minute Fixed Block",5),
+	FIXEDBLOCK1MIN(55,"1-Minute Fixed Block",1),
+	ROLLING60_30(57,"Rolling Block 60-Minute with 30-Minute Sub-intervals",60),
+	ROLLING60_20(58,"Rolling Block 60-Minute with 20-Minute Sub-intervals",60),
+	ROLLING60_15(59,"Rolling Block 60-Minute with 15-Minute Sub-intervals",60),
+	ROLLING60_12(60,"Rolling Block 60-Minute with 12-Minute Sub-intervals",60),
+	ROLLING60_10(61,"Rolling Block 60-Minute with 10-Minute Sub-intervals",60),
+	ROLLING60_6(62,"Rolling Block 60-Minute with 6-Minute Sub-intervals",60),
+	ROLLING60_5(63,"Rolling Block 60-Minute with 5-Minute Sub-intervals",60),
+	ROLLING60_4(64,"Rolling Block 60-Minute with 4-Minute Sub-intervals",60),
+	ROLLING30_15(65,"Rolling Block 30-Minute with 15-Minute Sub-intervals",30),
+	ROLLING30_10(66,"Rolling Block 30-Minute with 10-Minute Sub-intervals",30),
+	ROLLING30_6(67,"Rolling Block 30-Minute with 6-Minute Sub-intervals",30),
+	ROLLING30_5(68,"Rolling Block 30-Minute with 5-Minute Sub-intervals",30),
+	ROLLING30_3(69,"Rolling Block 30-Minute with 3-Minute Sub-intervals",30),
+	ROLLING30_2(70,"Rolling Block 30-Minute with 2-Minute Sub-intervals",30),
+	ROLLING15_5(71,"Rolling Block 15-Minute with 5-Minute Sub-intervals",15),
+	ROLLING15_3(72,"Rolling Block 15-Minute with 3-Minute Sub-intervals",15),
+	ROLLING15_1(73,"Rolling Block 15-Minute with 1-Minute Sub-intervals",15),
+	ROLLING10_5(74,"Rolling Block 10-Minute with 5-Minute Sub-intervals",10),
+	ROLLING10_2(75,"Rolling Block 10-Minute with 2-Minute Sub-intervals",10),
+	ROLLING10_1(76,"Rolling Block 10-Minute with 2-Minute Sub-intervals",10),
+	ROLLING5_1(77,"Rolling Block 5-Minute with 1-Minute Sub-intervals",5);
 	
 	private final int id;
 	private final String description;
+	private final int minutes;
 	
-	TimeAttribute(int id, String description)  {
+	TimeAttribute(int id, String description,int minutes)  {
 		this.id = id;
 		this.description = description;		
+		this.minutes = minutes;
+	}
+	
+	public int getMinutes() {
+		return minutes;
 	}
 			
 	public static TimeAttribute get(int id) {
