ba1b082e67c8ce9ee9f72819c0ed6d6a0ab2258b
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-11-27 08:35:31 +0100

    InMemoryPersistence Test

---
+++ copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/persistence/H2BootStrapService.java
@@ -0,0 +1,27 @@
+package com.elster.jupiter.devtools.tests.persistence;
+
+
+import org.h2.jdbcx.JdbcDataSource;
+
+import javax.sql.DataSource;
+import java.text.MessageFormat;
+
+public class H2BootStrapService /*implements BootstrapService */{
+
+    private static final String JDBC_URL_PATTERN = "jdbc:h2:mem:{0};MVCC=TRUE;lock_timeout={1}";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+    private static final String DATABASE_NAME_BASE = "DB";
+
+    //@Override
+    public DataSource createDataSource() {
+        String jdbcUrl = MessageFormat.format(JDBC_URL_PATTERN, DATABASE_NAME_BASE, 5000L);
+        JdbcDataSource source = new JdbcDataSource();
+        source.setURL(jdbcUrl);
+        source.setUser(USER);
+        source.setPassword(PASSWORD);
+        return source;
+    }
+
+
+}
