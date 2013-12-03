77944959cb380408a330c77759f896fe100718fb
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-12-03 10:54:05 +0100

    Split bootstrap bundle into two : API and implementation Provaide alternative H2 in memory implementation of bootstrap API

---
+++ copl/com.elster.jupiter.bootstrap.oracle/src/main/java/com/elster/jupiter/bootstrap/oracle/impl/DecoratedDataSource.java
@@ -0,0 +1,53 @@
+package com.elster.jupiter.bootstrap.oracle.impl;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+public class DecoratedDataSource implements DataSource {
+
+    private final DataSource decorated;
+
+    public DecoratedDataSource(DataSource decorated) {
+        this.decorated = decorated;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return new DecoratedConnection(decorated.getConnection());
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        decorated.setLogWriter(out);
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return decorated.getParentLogger();
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return decorated.getLoginTimeout();
+    }
+
+    public Connection getConnection(String username, String password) throws SQLException {
+        return new DecoratedConnection(decorated.getConnection(username, password));
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return decorated.getLogWriter();
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return decorated.isWrapperFor(iface);
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+        decorated.setLoginTimeout(seconds);
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return decorated.unwrap(iface);
+    }
+}
