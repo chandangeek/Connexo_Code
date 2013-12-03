39e101a6af6eaa36b73ee2f3eb611d2c5308ecc3
kha <karel.haeck@elster.com>
2013-12-03 20:34:38 +0100

    moved h2 db life cycle management to bootstrap.h2

--- copl/com.elster.jupiter.transaction/src/main/java/com/elster/jupiter/transaction/impl/TransactionModule.java
+++ copl/com.elster.jupiter.transaction/src/main/java/com/elster/jupiter/transaction/impl/TransactionModule.java
@@ -6,18 +6,9 @@ import com.elster.jupiter.security.thread.ThreadPrincipalService;
 import com.elster.jupiter.transaction.TransactionService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import com.google.inject.TypeLiteral;
-import com.google.inject.spi.InjectionListener;
-import com.google.inject.spi.TypeEncounter;
-import com.google.inject.spi.TypeListener;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class TransactionModule extends AbstractModule {
-
-    private Connection lifeLineConnection;
 
     @Override
     protected void configure() {
@@ -27,34 +18,5 @@ public class TransactionModule extends AbstractModule {
 
         bind(TransactionService.class).to(TransactionServiceImpl.class).in(Scopes.SINGLETON);
         bind(DataSource.class).to(TransactionalDataSource.class).in(Scopes.SINGLETON);
-
-        bindListener(new ServiceMatcher(DataSource.class), new TypeListener() {
-            @Override
-            public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
-                encounter.register(new InjectionListener<I>() {
-                    @Override
-                    public void afterInjection(I injectee) {
-                    	if (lifeLineConnection == null) {
-                    		try {
-                    			lifeLineConnection = ((DataSource) injectee).getConnection();
-                    			} catch (SQLException e) {
-                    				throw new RuntimeException(e);
-                    			}
-                        }
-                    }
-                });
-            }
-        });
-
-    }
-
-    public void closeLifeLineConnection() {
-        if (lifeLineConnection != null) {
-            try {
-                lifeLineConnection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
