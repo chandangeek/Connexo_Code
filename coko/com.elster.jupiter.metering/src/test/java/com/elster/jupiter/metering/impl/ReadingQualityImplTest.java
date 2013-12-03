35acd74db0db86d1b62456c0d14c19f86513a583
kha <karel.haeck@elster.com>
2013-12-03 19:26:34 +0100

    in memory testing

--- coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/ReadingQualityImplTest.java
+++ coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/ReadingQualityImplTest.java
@@ -1,6 +1,6 @@
 package com.elster.jupiter.metering.impl;
 
-import com.elster.jupiter.bootstrap.impl.InMemoryBootstrapModule;
+import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
 import com.elster.jupiter.domain.util.impl.DomainUtilModule;
 import com.elster.jupiter.events.impl.EventsModule;
 import com.elster.jupiter.ids.impl.IdsModule;
@@ -23,15 +23,18 @@ import com.elster.jupiter.orm.impl.OrmModule;
 import com.elster.jupiter.parties.impl.PartyModule;
 import com.elster.jupiter.pubsub.impl.PubSubModule;
 import com.elster.jupiter.security.thread.impl.ThreadSecurityModule;
+import com.elster.jupiter.transaction.Transaction;
 import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.transaction.VoidTransaction;
 import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.util.UtilModule;
+import com.elster.jupiter.util.conditions.Condition;
 import com.google.common.base.Optional;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+
 import org.joda.time.DateMidnight;
 import org.junit.After;
 import org.junit.Before;
@@ -49,6 +52,8 @@ import java.security.Principal;
 import java.sql.SQLException;
 import java.util.Date;
 
+import javax.sql.DataSource;
+
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -59,15 +64,12 @@ public class ReadingQualityImplTest {
 
     @Mock
     private LogService logService;
-
     @Mock
     private BundleContext bundleContext;
     @Mock
     private UserService userService;
     @Mock
     private Principal principal;
-    @Mock
-    private EventAdmin eventAdmin;
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private MessageService messageService;
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
@@ -81,33 +83,40 @@ public class ReadingQualityImplTest {
 
         @Override
         protected void configure() {
-            bind(LogService.class).toInstance(logService);
             bind(MessageService.class).toInstance(messageService);
             bind(UserService.class).toInstance(userService);
-            bind(BundleContext.class).toInstance(bundleContext);
-            bind(EventAdmin.class).toInstance(eventAdmin);
+            bind(BundleContext.class).toInstance(bundleContext);           
         }
     }
 
     @Before
     public void setUp() throws SQLException {
         transactionModule = new TransactionModule();
-        injector = Guice.createInjector(new MockModule(), new InMemoryBootstrapModule(), new IdsModule(), new MeteringModule(), new PartyModule(), new EventsModule(), new DomainUtilModule(), new OrmModule(new OrmModule.TransactionInjection() {
-            @Override
-            public void execute(final Runnable runnable) {
-                injector.getInstance(TransactionService.class).execute(new VoidTransaction() {
-                    @Override
-                    protected void doPerform() {
-                        runnable.run();
-                    }
-                });
-            }
-        }), new UtilModule(), new ThreadSecurityModule(principal), new PubSubModule(), transactionModule, new OrmCacheModule());
+        injector = Guice.createInjector(
+        			new MockModule(), 
+        			new InMemoryBootstrapModule(), 
+        			new IdsModule(), 
+        			new MeteringModule(), 
+        			new PartyModule(), 
+        			new EventsModule(), 
+        			new DomainUtilModule(), 
+        			new OrmModule(),
+        			new UtilModule(), 
+        			new ThreadSecurityModule(principal), 
+        			new PubSubModule(logService), 
+        			transactionModule, 
+        			new OrmCacheModule());
         when(messageService.getQueueTableSpec(anyString())).thenReturn(Optional.of(queueTableSpec));
         when(messageService.getDestinationSpec(anyString())).thenReturn(Optional.of(destinationSpec));
         when(principal.getName()).thenReturn("Test");
-        injector.getInstance(MeteringService.class);
-        injector.getInstance(MeteringService.class);
+        injector.getInstance(DataSource.class);
+        injector.getInstance(TransactionService.class).execute(new Transaction<Void>() {
+			@Override
+			public Void perform() {
+				injector.getInstance(MeteringService.class);
+				return null;
+			}
+		});
     }
 
     @After
@@ -141,16 +150,12 @@ public class ReadingQualityImplTest {
         ServiceCategory serviceCategory = meteringService.getServiceCategory(ServiceKind.ELECTRICITY).get();
         UsagePoint usagePoint = serviceCategory.newUsagePoint("mrID");
         usagePoint.save();
-
         ReadingType readingType = meteringService.getReadingType("0.0.2.4.1.1.12.0.0.0.0.0.0.0.0.3.72.0").get();
-
         MeterActivation meterActivation = usagePoint.activate(date);
         Channel channel = meterActivation.createChannel(readingType);
-
         ReadingStorer regularStorer = meteringService.createNonOverrulingStorer();
         regularStorer.addIntervalReading(channel, date, 0L, BigDecimal.valueOf(561561, 2));
         regularStorer.execute();
-
         BaseReadingRecord reading = channel.getReading(date).get();
         ReadingQuality readingQuality = channel.createReadingQuality(new ReadingQualityType("6.1"), reading);
         readingQuality.save();
