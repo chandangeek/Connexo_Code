d8b16fc9d9edaee90b0d7477c5167fb9f653b6ba
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-12-04 16:50:23 +0100

    Regression testing : Validation Test : switch from fest-assert to AssertJ

--- coko/com.elster.jupiter.metering.rest/src/test/java/com/elster/jupiter/metering/rest/impl/CreateUsagePointTransactionTest.java
+++ coko/com.elster.jupiter.metering.rest/src/test/java/com/elster/jupiter/metering/rest/impl/CreateUsagePointTransactionTest.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
