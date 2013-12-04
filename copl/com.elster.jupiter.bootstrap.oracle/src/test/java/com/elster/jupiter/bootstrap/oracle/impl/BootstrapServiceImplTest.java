f38c1a8d5980ed449c72395ae62ca9e18671cf98
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-12-04 16:50:30 +0100

    Regression testing : Validation Test : switch from fest-assert to AssertJ

--- copl/com.elster.jupiter.bootstrap.oracle/src/test/java/com/elster/jupiter/bootstrap/oracle/impl/BootstrapServiceImplTest.java
+++ copl/com.elster.jupiter.bootstrap.oracle/src/test/java/com/elster/jupiter/bootstrap/oracle/impl/BootstrapServiceImplTest.java
@@ -9,7 +9,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.osgi.framework.BundleContext;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
