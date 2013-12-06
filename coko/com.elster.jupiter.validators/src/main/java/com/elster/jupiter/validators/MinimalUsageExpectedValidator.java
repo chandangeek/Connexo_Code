dcd5ff622924d33ba440f5aa7ce6099ef817ea2e
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-12-06 16:46:27 +0100

    Regression testing : Refactor Validation : looping readings into ValidationRule

--- coko/com.elster.jupiter.validators/src/main/java/com/elster/jupiter/validators/MinimalUsageExpectedValidator.java
+++ coko/com.elster.jupiter.validators/src/main/java/com/elster/jupiter/validators/MinimalUsageExpectedValidator.java
@@ -1,11 +1,15 @@
 package com.elster.jupiter.validators;
 
 import com.elster.jupiter.metering.Channel;
+import com.elster.jupiter.metering.IntervalReadingRecord;
+import com.elster.jupiter.metering.ReadingQualityType;
+import com.elster.jupiter.metering.ReadingRecord;
 import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.util.time.Interval;
 import com.elster.jupiter.util.units.Quantity;
-import com.elster.jupiter.validation.ValidationStats;
+import com.elster.jupiter.validation.ValidationResult;
 import com.elster.jupiter.validation.Validator;
+import com.google.common.base.Optional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +33,24 @@ public class MinimalUsageExpectedValidator implements Validator {
     }
 
     @Override
-    public String getReadingQualityTypeCode() {
+    public Optional<ReadingQualityType> getReadingQualityTypeCode() {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
-    public ValidationStats validate(Channel channel, ReadingType readingType, Interval interval) {
+    public void init(Channel channel, ReadingType readingType, Interval interval) {
+        //TODO automatically generated method body, provide implementation.
+
+    }
+
+    @Override
+    public ValidationResult validate(IntervalReadingRecord intervalReadingRecord) {
+        //TODO automatically generated method body, provide implementation.
+        return null;
+    }
+
+    @Override
+    public ValidationResult validate(ReadingRecord readingRecord) {
         //TODO automatically generated method body, provide implementation.
         return null;
     }
