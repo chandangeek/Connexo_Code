46013111d04bdae734717be2c00a48e412bfda92
kha <karel.haeck@elster.com>
2013-12-10 10:34:32 +0100

    moved ReadingTypeCodeBuilder to cbo

---
+++ coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/ReadingTypeCodeBuilder.java
@@ -0,0 +1,144 @@
+package com.elster.jupiter.cbo;
+
+import java.util.Currency;
+
+public final class ReadingTypeCodeBuilder {
+	private MacroPeriod macroPeriod = MacroPeriod.NOTAPPLICABLE;
+	private Aggregate aggregate = Aggregate.NOTAPPLICABLE;
+	private TimeAttribute measuringPeriod = TimeAttribute.NOTAPPLICABLE;
+	private Accumulation accumulation = Accumulation.NOTAPPLICABLE;
+	private FlowDirection flowDirection = FlowDirection.NOTAPPLICABLE;
+	private Commodity commodity = Commodity.NOTAPPLICABLE;
+	private MeasurementKind measurementKind = MeasurementKind.NOTAPPLICABLE;
+	private RationalNumber interharmonic = RationalNumber.NOTAPPLICABLE;
+	private RationalNumber argument = RationalNumber.NOTAPPLICABLE;
+	private int tou = 0;
+	private int cpp = 0; 
+	private int consumptionTier = 0;
+	private Phase phases = Phase.NOTAPPLICABLE;
+	private MetricMultiplier multiplier = MetricMultiplier.ZERO;
+	private ReadingTypeUnit unit = ReadingTypeUnit.NOTAPPLICABLE;
+	private Currency currency = ReadingTypeCodeBuilder.getCurrency(0);
+	
+	private ReadingTypeCodeBuilder() {
+	}
+	
+	public static Currency getCurrency(int isoCode) {
+		if (isoCode == 0) {
+			isoCode = 999;
+		} 
+		for (Currency each : Currency.getAvailableCurrencies()) {
+			if (each.getNumericCode() == isoCode) {
+				return each;
+			}
+		}
+		throw new IllegalArgumentException("Invalid currency code " + isoCode);		
+	}
+	
+	public static int getCurrencyId(Currency currency) {
+		int result = currency.getNumericCode();
+		return result == 999 ? 0 : result;
+	}
+	
+	public static ReadingTypeCodeBuilder of(Commodity commodity) {
+		return new ReadingTypeCodeBuilder().commodity(commodity);
+	}
+	
+	public ReadingTypeCodeBuilder period(MacroPeriod macroPeriod) {
+		this.macroPeriod = macroPeriod;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder aggregate(Aggregate aggregate) {
+		this.aggregate = aggregate;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder period(TimeAttribute timeAttribute) {
+		this.measuringPeriod = timeAttribute;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder accumulate(Accumulation accumulation) {
+		this.accumulation = accumulation;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder flow(FlowDirection flowDirection) {
+		this.flowDirection = flowDirection;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder commodity(Commodity commodity) {
+		this.commodity = commodity;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder measure(MeasurementKind meaurementKind) {
+		this.measurementKind = meaurementKind;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder harmonic(int numerator , int denominator) {
+		this.interharmonic = new RationalNumber(numerator, denominator);
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder argument(int numerator , int denominator) {
+		this.argument = new RationalNumber(numerator, denominator);
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder tou(int tou) {
+		this.tou = tou;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder cpp(int cpp) {
+		this.cpp = cpp;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder tier(int consumptionTier) {
+		this.consumptionTier = consumptionTier;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder phase(Phase phases) {
+		this.phases = phases;
+		return this;
+	}
+	
+	public ReadingTypeCodeBuilder in (MetricMultiplier multiplier , ReadingTypeUnit unit) {
+		this.multiplier = multiplier;
+		this.unit = unit;
+		return this;		
+	}
+	
+	public ReadingTypeCodeBuilder in (ReadingTypeUnit unit) {
+		return in(MetricMultiplier.ZERO,unit);
+	}
+	
+	public String code() {
+		return 
+			"" +
+			macroPeriod.getId() + "." + 
+			aggregate.getId() + "." + 
+			measuringPeriod.getId() + "." + 
+			accumulation.getId() + "." + 
+			flowDirection.getId() + "." +
+			commodity.getId() + "." + 
+			measurementKind.getId() + "." + 
+			interharmonic.getNumerator() + "." + 
+			interharmonic.getDenominator() + "." + 
+			argument.getNumerator() + "." + 
+			argument.getDenominator() + "." + 
+			tou + "." + 
+			cpp + "." + 
+			consumptionTier + "." + 
+			phases.getId() + "." + 
+			multiplier.getId() + "." + 
+			unit.getId() + "." + 
+			ReadingTypeCodeBuilder.getCurrencyId(currency);
+	}
+}
