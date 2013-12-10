353664c4ffc3fb4a98743b3eec874243452022cd
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2013-12-10 09:59:35 +0100

    Change BaseReadingRecord interface

--- coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/ReadingInfo.java
+++ coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/ReadingInfo.java
@@ -1,8 +1,10 @@
 package com.elster.jupiter.metering.rest.impl;
 
 import com.elster.jupiter.metering.BaseReadingRecord;
+import com.elster.jupiter.util.units.Quantity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadingInfo {
@@ -14,6 +16,10 @@ public class ReadingInfo {
     public ReadingInfo(BaseReadingRecord reading) {
         this.timeStamp = reading.getTimeStamp().getTime();
         this.recordTime = reading.getReportedDateTime() == null ? null : reading.getReportedDateTime().getTime();
-        values = reading.getValues();
+        List<Quantity> quantities = reading.getQuantities();
+        values = new ArrayList<>(quantities.size());
+        for (Quantity quantity : quantities) {
+            values.add(quantity.getValue());
+        }
     }
 }
