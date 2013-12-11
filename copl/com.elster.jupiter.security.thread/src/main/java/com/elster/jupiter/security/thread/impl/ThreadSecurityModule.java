ffa67a3a6619714dea1158b646c18bab8cac8168
kha <karel.haeck@elster.com>
2013-12-11 10:55:23 +0100

    Simplified test setup

--- copl/com.elster.jupiter.security.thread/src/main/java/com/elster/jupiter/security/thread/impl/ThreadSecurityModule.java
+++ copl/com.elster.jupiter.security.thread/src/main/java/com/elster/jupiter/security/thread/impl/ThreadSecurityModule.java
@@ -9,28 +9,20 @@ import com.google.inject.spi.TypeEncounter;
 import com.google.inject.spi.TypeListener;
 
 import java.security.Principal;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ThreadSecurityModule extends AbstractModule {
 
-    private final Principal principal;
+    private final ThreadPrincipalService service;
 
     public ThreadSecurityModule(Principal principal) {
-        this.principal = principal;
+        this.service = new ThreadPrincipalServiceImpl();
+        service.set(principal);
     }
 
     @Override
     protected void configure() {
-        bind(ThreadPrincipalService.class).to(ThreadPrincipalServiceImpl.class).in(Scopes.SINGLETON);
-        bindListener(new ServiceMatcher(ThreadPrincipalService.class), new TypeListener() {
-            @Override
-            public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
-                encounter.register(new InjectionListener<I>() {
-                    @Override
-                    public void afterInjection(I injectee) {
-                        ((ThreadPrincipalService) injectee).set(principal);
-                    }
-                });
-            }
-        });
+        bind(ThreadPrincipalService.class).toInstance(service);
     }
 }
