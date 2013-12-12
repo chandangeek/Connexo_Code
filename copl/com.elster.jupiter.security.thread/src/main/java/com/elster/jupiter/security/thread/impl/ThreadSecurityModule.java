8737def72f74c444364e51329ec9650c486f18a1
kha <karel.haeck@elster.com>
2013-12-12 11:27:50 +0100

    Simplified h2 test setup

--- copl/com.elster.jupiter.security.thread/src/main/java/com/elster/jupiter/security/thread/impl/ThreadSecurityModule.java
+++ copl/com.elster.jupiter.security.thread/src/main/java/com/elster/jupiter/security/thread/impl/ThreadSecurityModule.java
@@ -1,21 +1,23 @@
 package com.elster.jupiter.security.thread.impl;
 
+import java.security.Principal;
+
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
-import com.google.inject.TypeLiteral;
-import com.google.inject.spi.InjectionListener;
-import com.google.inject.spi.TypeEncounter;
-import com.google.inject.spi.TypeListener;
-
-import java.security.Principal;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class ThreadSecurityModule extends AbstractModule {
 
     private final ThreadPrincipalService service;
 
+    public ThreadSecurityModule() {
+    	this(new Principal() {	
+			@Override
+			public String getName() {
+				return "Test";
+			}
+		});
+    }
+    
     public ThreadSecurityModule(Principal principal) {
         this.service = new ThreadPrincipalServiceImpl();
         service.set(principal);
