160fb086b67998e43a2716b8c3b84d57d1d68957
sla <s.labis@energyict.com>
2013-12-19 10:02:40 +0100

    nullpointer when asking list of properties

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/eict/rtuplusserver/g3/RtuPlusServer.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/eict/rtuplusserver/g3/RtuPlusServer.java
@@ -1,23 +1,15 @@
 package com.energyict.protocolimplv2.eict.rtuplusserver.g3;
 
-import com.energyict.mdc.protocol.api.dynamic.PropertySpec;
 import com.energyict.dlms.common.AbstractDlmsProtocol;
 import com.energyict.dlms.common.DlmsProtocolProperties;
+import com.energyict.mdc.protocol.api.*;
+import com.energyict.mdc.protocol.api.device.data.*;
 import com.energyict.mdc.protocol.api.device.messages.DeviceMessageSpec;
-import com.energyict.mdc.protocol.api.ConnectionType;
-import com.energyict.mdc.protocol.api.DeviceProtocolCapabilities;
-import com.energyict.mdc.protocol.api.DeviceProtocolDialect;
-import com.energyict.mdc.protocol.api.LoadProfileReader;
-import com.energyict.mdc.protocol.api.LogBookReader;
-import com.energyict.mdc.protocol.api.device.data.CollectedLoadProfile;
-import com.energyict.mdc.protocol.api.device.data.CollectedLoadProfileConfiguration;
-import com.energyict.mdc.protocol.api.device.data.CollectedLogBook;
-import com.energyict.mdc.protocol.api.device.data.CollectedMessageList;
-import com.energyict.mdc.protocol.api.device.data.CollectedRegister;
-import com.energyict.mdc.protocol.api.device.data.CollectedTopology;
-import com.energyict.mdc.protocol.api.device.offline.OfflineRegister;
 import com.energyict.mdc.protocol.api.device.offline.OfflineDeviceMessage;
+import com.energyict.mdc.protocol.api.device.offline.OfflineRegister;
+import com.energyict.mdc.protocol.api.dynamic.PropertySpec;
 import com.energyict.protocolimplv2.dialects.NoParamsDeviceProtocolDialect;
+import com.energyict.protocolimplv2.nta.dsmr23.Dsmr23Properties;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -37,7 +29,7 @@ public class RtuPlusServer extends AbstractDlmsProtocol {
 
     @Override
     protected DlmsProtocolProperties getProtocolProperties() {
-        return null;  // nothing to do yet
+        return new Dsmr23Properties();
     }
 
     @Override
