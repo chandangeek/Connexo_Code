2aaa1601b6b460bc0ec5a05a3163d797c31f0ece
sla <s.labis@energyict.com>
2013-12-19 09:36:33 +0100

    fieldcontainer, no : in labels

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/FieldContainerOverride.js
@@ -0,0 +1,14 @@
+Ext.define('Uni.override.FieldContainerOverride', {
+    override: 'Ext.form.FieldContainer',
+
+    /**
+     * Changes the default value ':'.
+     */
+    labelSeparator: '',
+
+    /**
+     * Changes the default value 'qtip'.
+     */
+    msgTarget: 'side'
+
+});
