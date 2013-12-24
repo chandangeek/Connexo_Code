3076503242f8e63b406c3df92080cd9877787319
kha <karel.haeck@elster.com>
2013-12-24 16:54:38 +0100

    added ValidationResolverProvider to Guice test setup module

--- copl/com.elster.jupiter.bootstrap.oracle/src/main/java/com/elster/jupiter/bootstrap/oracle/impl/OracleBootstrapModule.java
+++ copl/com.elster.jupiter.bootstrap.oracle/src/main/java/com/elster/jupiter/bootstrap/oracle/impl/OracleBootstrapModule.java
@@ -1,6 +1,9 @@
 package com.elster.jupiter.bootstrap.oracle.impl;
 
+import javax.validation.ValidationProviderResolver;
+
 import com.elster.jupiter.bootstrap.BootstrapService;
+import com.elster.jupiter.bootstrap.validation.impl.ProviderResolverService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
@@ -9,5 +12,6 @@ public class OracleBootstrapModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(BootstrapService.class).to(BootstrapServiceImpl.class).in(Scopes.SINGLETON);
+        bind(ValidationProviderResolver.class).to(ProviderResolverService.class).in(Scopes.SINGLETON);
     }
 }
