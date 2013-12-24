508fae16b2170d99e2f26a43e4b9e2704226f7ce
kha <karel.haeck@elster.com>
2013-12-24 09:13:50 +0100

    instantiating Hibernate Validator in OSGI environment

--- copl/com.elster.jupiter.bootstrap/src/main/java/com/elster/jupiter/bootstrap/validation/impl/ValidationFactoryProvider.java
+++ copl/com.elster.jupiter.bootstrap/src/main/java/com/elster/jupiter/bootstrap/validation/impl/ValidationFactoryProvider.java
@@ -1,5 +1,7 @@
 package com.elster.jupiter.bootstrap.validation.impl;
 
+import java.util.List;
+
 import org.hibernate.validator.HibernateValidator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -7,13 +9,21 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 
+import com.google.common.collect.ImmutableList;
+
 import javax.validation.Validation;
+import javax.validation.ValidationProviderResolver;
 import javax.validation.ValidatorFactory;
+import javax.validation.spi.ValidationProvider;
 
 @Component(name="com.elster.jupiter.bootstrap.validation.impl")
 public class ValidationFactoryProvider {
 	
-	ServiceRegistration<ValidatorFactory> registration;
+	private ServiceRegistration<ValidatorFactory> registration;
+	
+	public ValidationFactoryProvider() {
+		
+	}
 	
 	@Activate
 	public void activate(BundleContext context) {
@@ -26,8 +36,17 @@ public class ValidationFactoryProvider {
 	}
 	
 	private ValidatorFactory getValidatorFactory() {
-		return Validation.byProvider( HibernateValidator.class ).configure().buildValidatorFactory();		
-	}
-
+		return Validation.byDefaultProvider()
+	        .providerResolver( new Resolver() )
+	        .configure()
+	        .buildValidatorFactory();
+    }
+    
+    static class Resolver implements ValidationProviderResolver {
+        @Override
+        public List<ValidationProvider<?>> getValidationProviders() {
+            return ImmutableList.<ValidationProvider<?>>of(new HibernateValidator());
+        }
+    }
 	
 }
