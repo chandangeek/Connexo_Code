6dfb0fa0a6187c75fcbc46b449b6f06e5c2717ea
kha <karel.haeck@elster.com>
2014-01-02 17:00:28 +0100

    Utility class to find senders of a method, constructor

---
+++ copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/test/analysis/MethodInvocationCheckerTest.java
@@ -0,0 +1,37 @@
+package com.elster.jupiter.devtools.test.analysis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.google.common.reflect.ClassPath;
+
+public class MethodInvocationCheckerTest {
+	
+	@Test
+	public void testMethod() throws IOException, NoSuchMethodException, SecurityException {
+		Set<ClassPath.ClassInfo> classInfos = ClassPath.from(this.getClass().getClassLoader()).getTopLevelClassesRecursive("com.elster");
+		assertThat(classInfos).isNotEmpty();
+		boolean found = false;
+		for (ClassPath.ClassInfo classInfo : classInfos) {
+			found |= MethodInvocationChecker.invokes(classInfo,MethodInvocationChecker.class.getMethod("invokes",ClassPath.ClassInfo.class,Method.class));
+		}
+		assertThat(found).isTrue();
+	}
+
+	@Test
+	public void testConstructor() throws IOException, NoSuchMethodException, SecurityException {
+		new MethodInvocationCheckerTest(); 
+		Set<ClassPath.ClassInfo> classInfos = ClassPath.from(this.getClass().getClassLoader()).getTopLevelClassesRecursive("com.elster");
+		assertThat(classInfos).isNotEmpty();
+		boolean found = false;
+		for (ClassPath.ClassInfo classInfo : classInfos) {
+			found |= MethodInvocationChecker.invokes(classInfo,MethodInvocationCheckerTest.class.getConstructor());
+		}
+		assertThat(found).isTrue();
+	}
+}
