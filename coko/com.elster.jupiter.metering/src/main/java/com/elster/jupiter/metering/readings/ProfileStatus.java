86106b386862a0a163b9527cfb27e179ca903e7a
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2014-01-17 12:39:16 +0100

    Fix import, simplify equals() on ProfileStatus

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/readings/ProfileStatus.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/readings/ProfileStatus.java
@@ -1,6 +1,6 @@
 package com.elster.jupiter.metering.readings;
 
-import com.sun.org.apache.xalan.internal.utils.Objects;
+import java.util.Objects;
 
 public final class ProfileStatus {
 	private final long bits;
@@ -42,11 +42,7 @@ public final class ProfileStatus {
 	
 	@Override
 	public boolean equals(Object other) {
-		if (other instanceof ProfileStatus) {
-			return bits == ((ProfileStatus) other).bits;
-		} else {
-			return false;
-		}
+        return other instanceof ProfileStatus && bits == ((ProfileStatus) other).bits;
 	}
 	
 	@Override
