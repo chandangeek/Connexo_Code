0a23fa16e0bc2d20a99f5911459f66a03b130d26
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2014-01-20 11:58:57 +0100

    JP-674 - Localization of dynamic data Convert Metering to use NlsService

--- copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/nls/impl/NlsModule.java
+++ copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/nls/impl/NlsModule.java
@@ -4,11 +4,10 @@ import com.elster.jupiter.nls.NlsService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
-class NlsModule extends AbstractModule {
+public class NlsModule extends AbstractModule {
 
     @Override
     protected void configure() {
         bind(NlsService.class).to(NlsServiceImpl.class).in(Scopes.SINGLETON);
-
     }
 }
