70d780d1fffba6ce61b73b078c8da5a99a0612e4
Bart Vansegbroeck <b.vansegbroeck@energyict.com>
2014-01-20 08:40:29 +0100

    JP-683: Fixed test

--- comu/com.energyict.mdc.engine.config/src/test/java/com/energyict/mdc/channels/serial/BaudrateValueTest.java
+++ comu/com.energyict.mdc.engine.config/src/test/java/com/energyict/mdc/channels/serial/BaudrateValueTest.java
@@ -1,5 +1,6 @@
 package com.energyict.mdc.channels.serial;
 
+import com.energyict.mdc.protocol.api.channels.serial.BaudrateValue;
 import java.math.BigDecimal;
 import java.util.EnumSet;
 import java.util.Set;
