77eb0d9fdd5fb8603ca66641fa40f4cfbe3f432f
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2014-01-20 09:48:15 +0100

    JP-674 - Localization of dynamic data Convert Appserver to use NlsService

--- copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/MessageSeeds.java
+++ copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/MessageSeeds.java
@@ -12,9 +12,9 @@ public enum MessageSeeds implements MessageSeed {
 
     SERVER_MESSAGE_QUEUE_MISSING(1001, "server.messagequeue.missing", "Server's message queue with name ''{0}'' not found", Level.SEVERE),
     APPSERVER_NAME_UNKNOWN(1002, "appserver.name.unknown", "AppServer with name ''{0}'' is unknown", Level.SEVERE),
-    APPSERVER_STARTED_ANONYMOUSLY(1003, "appserver.started.anonymously", "AppServer started anonymously.", Level.WARNING),
-    THREAD_UNCAUGHT_EXCEPTION(1004, "thread.uncaught.exception", "Uncaught exception occurred on thread {0}", Level.SEVERE),
-    MESSAGEHANDLER_FAILED(1005, "messagehandler.failed", "Message handler failed", Level.SEVERE);
+    APPSERVER_STARTED_ANONYMOUSLY(2001, "appserver.started.anonymously", "AppServer started anonymously.", Level.WARNING),
+    THREAD_UNCAUGHT_EXCEPTION(2002, "thread.uncaught.exception", "Uncaught exception occurred on thread {0}", Level.SEVERE),
+    MESSAGEHANDLER_FAILED(2003, "messagehandler.failed", "Message handler failed", Level.SEVERE);
 
     private final int number;
     private final String key;
--- copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/UnknownAppServerNameException.java
+++ copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/UnknownAppServerNameException.java
@@ -1,6 +1,5 @@
 package com.elster.jupiter.appserver;
 
-import com.elster.jupiter.nls.NlsMessageFormat;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.util.exception.BaseException;
 
@@ -12,8 +11,7 @@ public class UnknownAppServerNameException extends BaseException {
     }
 
     private static String buildMessage(String appServerName, Thesaurus thesaurus) {
-        NlsMessageFormat format = thesaurus.getFormat(MessageSeeds.APPSERVER_NAME_UNKNOWN);
-        return format.format(appServerName);
+        return thesaurus.getFormat(MessageSeeds.APPSERVER_NAME_UNKNOWN).format(appServerName);
     }
 
 }
