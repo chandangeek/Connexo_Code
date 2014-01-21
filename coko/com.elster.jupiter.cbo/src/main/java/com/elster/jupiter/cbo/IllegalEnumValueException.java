981201280172644ce5a961deb7f4d4ebd6812b6f
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2014-01-21 15:57:38 +0100

    JP-674 - Localization of dynamic data Streamline Exception translations

--- coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/IllegalEnumValueException.java
+++ coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/IllegalEnumValueException.java
@@ -2,12 +2,10 @@ package com.elster.jupiter.cbo;
 
 import com.elster.jupiter.util.exception.BaseException;
 
-import java.text.MessageFormat;
-
 public class IllegalEnumValueException extends BaseException {
 
     public IllegalEnumValueException(Class<?> enumClass, int value) {
-        super(ExceptionTypes.ILLEGAL_ENUM_VALUE, MessageFormat.format("{0} is not a value matching an instance of enum {1}", value, enumClass.getName()));
+        super(MessageSeeds.ILLEGAL_ENUM_VALUE, value, enumClass.getName());
         set("enumClass", enumClass);
         set("value", value);
     }
---
+++ coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/MessageSeeds.java
@@ -0,0 +1,42 @@
+package com.elster.jupiter.cbo;
+
+import com.elster.jupiter.util.exception.MessageSeed;
+
+import java.util.logging.Level;
+
+public enum MessageSeeds implements MessageSeed {
+    ILLEGAL_ENUM_VALUE(1001, "{0} is not a value matching an instance of enum {1}");
+
+    private final int number;
+    private final String defaultFormat;
+
+    MessageSeeds(int number, String defaultFormat) {
+        this.number = number;
+        this.defaultFormat = defaultFormat;
+    }
+
+    @Override
+    public String getModule() {
+        return "CBO";
+    }
+
+    @Override
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getDefaultFormat() {
+        return defaultFormat;
+    }
+
+    @Override
+    public Level getLevel() {
+        return Level.SEVERE;
+    }
+}
