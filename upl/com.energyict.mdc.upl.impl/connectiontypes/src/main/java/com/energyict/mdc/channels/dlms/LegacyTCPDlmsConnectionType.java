b8d0414aeb74537f97d11ab6158a830ac994e26e
Bruno Ballekens <Bruno.Ballekens@elster.com>
2014-01-23 08:36:54 +0000

    EISERVERSG-2998 - Reading of only slave comtasks in a master slave setup fails Added functionality to provide the ConnectionTask properties to the Meter/SmartMeter/Device-Protocol. Added two DLMS ConnectionTypes which can make proper use of this functionality to read only the Slave devices Backport from rev 78314 on 9.1.0-BETA branch
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@80797 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/connectiontypes/src/main/java/com/energyict/mdc/channels/dlms/LegacyTCPDlmsConnectionType.java
@@ -0,0 +1,98 @@
+package com.energyict.mdc.channels.dlms;
+
+import com.energyict.cpo.PropertySpec;
+import com.energyict.cpo.PropertySpecFactory;
+import com.energyict.mdc.channels.ip.socket.OutboundTcpIpConnectionType;
+import com.energyict.mdc.ports.ComPort;
+import com.energyict.mdc.ports.ComPortType;
+import com.energyict.mdc.protocol.ComChannel;
+import com.energyict.mdc.protocol.ConnectionException;
+import com.energyict.mdc.tasks.ConnectionTaskProperty;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 10/10/13
+ * Time: 14:48
+ */
+public class LegacyTCPDlmsConnectionType extends DlmsConnectionType {
+
+    public LegacyTCPDlmsConnectionType() {
+        super(new OutboundTcpIpConnectionType());
+    }
+
+    @Override
+    public boolean allowsSimultaneousConnections() {
+        return getActualConnectionType().allowsSimultaneousConnections();
+    }
+
+    @Override
+    public boolean supportsComWindow() {
+        return getActualConnectionType().supportsComWindow();
+    }
+
+    @Override
+    public Set<ComPortType> getSupportedComPortTypes() {
+        return getActualConnectionType().getSupportedComPortTypes();
+    }
+
+    @Override
+    public ComChannel connect(ComPort comPort, List<ConnectionTaskProperty> properties) throws ConnectionException {
+        return getActualConnectionType().connect(comPort, properties);
+    }
+
+    @Override
+    public PropertySpec getPropertySpec(String name) {
+        switch (name) {
+            case PROPERTY_NAME_ADDRESSING_MODE:
+                return this.getAddressingModePropertySpec();
+            case PROPERTY_NAME_CONNECTION:
+                return this.getConnectionPropertySpec();
+            case PROPERTY_NAME_SERVER_MAC_ADDRESS:
+                return this.getServerMacAddress();
+            case PROPERTY_NAME_SERVER_LOWER_MAC_ADDRESS:
+                return this.getServerLowerMacAddress();
+            case PROPERTY_NAME_SERVER_UPPER_MAC_ADDRESS:
+                return this.getServerUpperMacAddress();
+            default:
+                return getActualConnectionType().getPropertySpec(name);
+        }
+    }
+
+    @Override
+    public boolean isRequiredProperty(String name) {
+        return getActualConnectionType().isRequiredProperty(name);
+    }
+
+    @Override
+    public String getVersion() {
+        return "$Date$";
+    }
+
+    @Override
+    public List<PropertySpec> getRequiredProperties() {
+        return getActualConnectionType().getRequiredProperties();
+    }
+
+    @Override
+    public List<PropertySpec> getOptionalProperties() {
+        List<PropertySpec> optionalProperties = getActualConnectionType().getOptionalProperties();
+        optionalProperties.add(getAddressingModePropertySpec());
+        optionalProperties.add(getConnectionPropertySpec());
+        optionalProperties.add(getServerMacAddress());
+        optionalProperties.add(getServerLowerMacAddress());
+        optionalProperties.add(getServerUpperMacAddress());
+        return optionalProperties;
+    }
+
+    PropertySpec getServerUpperMacAddress() {
+        return PropertySpecFactory.bigDecimalPropertySpec(PROPERTY_NAME_SERVER_UPPER_MAC_ADDRESS, new BigDecimal(1));
+    }
+
+    PropertySpec getServerLowerMacAddress() {
+        return PropertySpecFactory.bigDecimalPropertySpec(PROPERTY_NAME_SERVER_LOWER_MAC_ADDRESS, new BigDecimal(0));
+    }
+}
