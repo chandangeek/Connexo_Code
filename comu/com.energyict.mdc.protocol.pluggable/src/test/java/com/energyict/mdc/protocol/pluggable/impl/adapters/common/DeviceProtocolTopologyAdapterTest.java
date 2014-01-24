b9f780f72584bdb4b7cb468341baf0b05ba72b27
Rudi Vankeirsbilck <rudi@objectsmith.be>
2014-01-24 17:01:15 +0100

    JP-682: Revision of persistence of pluggable classes - Fix unit tests

--- comu/com.energyict.mdc.protocol.pluggable/src/test/java/com/energyict/mdc/protocol/pluggable/impl/adapters/common/DeviceProtocolTopologyAdapterTest.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/test/java/com/energyict/mdc/protocol/pluggable/impl/adapters/common/DeviceProtocolTopologyAdapterTest.java
@@ -1,11 +1,13 @@
 package com.energyict.mdc.protocol.pluggable.impl.adapters.common;
 
+import com.energyict.mdc.common.ApplicationContext;
 import com.energyict.mdc.common.Environment;
 import com.energyict.mdc.common.UserEnvironment;
 import com.energyict.mdc.issues.Bus;
 import com.energyict.mdc.issues.IssueService;
 import com.energyict.mdc.issues.Problem;
 import com.energyict.mdc.protocol.api.device.Device;
+import com.energyict.mdc.protocol.api.device.data.CollectedDataFactory;
 import com.energyict.mdc.protocol.api.device.data.CollectedTopology;
 import com.energyict.mdc.protocol.api.device.data.ResultType;
 import com.energyict.mdc.protocol.api.inbound.DeviceIdentifier;
@@ -16,9 +18,12 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -39,7 +44,17 @@ public class DeviceProtocolTopologyAdapterTest {
     @Mock
     private Environment environment;
     @Mock
+    private ApplicationContext applicationContext;
+    @Mock
     private IssueService issueService;
+    @Mock
+    private CollectedDataFactory collectedDataFactory;
+    @Mock
+    private CollectedTopology collectedTopology;
+    @Mock
+    private Device device;
+    @Mock
+    private DeviceIdentifier deviceIdentifier;
 
     @BeforeClass
     public static void initializeUserEnvironment () {
@@ -61,11 +76,25 @@ public class DeviceProtocolTopologyAdapterTest {
     public void initializeEnvironment () {
         Environment.DEFAULT.set(this.environment);
         when(this.environment.getTranslation("devicetopologynotsupported")).thenReturn("devicetopologynotsupported");
+
+        when(this.environment.getApplicationContext()).thenReturn(this.applicationContext);
+        when(this.applicationContext.getModulesImplementing(CollectedDataFactory.class)).thenReturn(Arrays.asList(this.collectedDataFactory));
     }
 
     @After
     public void cleanupEnvironment () {
         Environment.DEFAULT.set(null);
+    }
+
+    @Before
+    public void initializeCollecteData () {
+        when(this.collectedDataFactory.createCollectedTopology(getDeviceIdentifier())).thenReturn(this.collectedTopology);
+    }
+
+    @Before
+    public void initializeDevice () {
+        when(this.device.getId()).thenReturn(DEVICE_ID);
+        when(this.deviceIdentifier.findDevice()).thenReturn(this.device);
     }
 
     @Before
@@ -82,19 +111,16 @@ public class DeviceProtocolTopologyAdapterTest {
     @Test
     public void getUnsupportedCollectedTopology(){
         DeviceProtocolTopologyAdapter deviceProtocolTopologyAdapter = new DeviceProtocolTopologyAdapter();
-        deviceProtocolTopologyAdapter.setDeviceIdentifier(getDeviceIdentifier());
+        DeviceIdentifier expectedDeviceIdentifier = getDeviceIdentifier();
+        deviceProtocolTopologyAdapter.setDeviceIdentifier(expectedDeviceIdentifier);
         CollectedTopology deviceTopology = deviceProtocolTopologyAdapter.getDeviceTopology();
-        assertEquals("Device topology should be unsupported", ResultType.NotSupported, deviceTopology.getResultType());
+
+        // Asserts
+        verify(this.collectedTopology).setFailureInformation(eq(ResultType.NotSupported), eq(this.device), anyString());
         verify(this.issueService).newProblem(anyString(), anyString(), anyVararg());
-        DeviceIdentifier deviceIdentifier = deviceTopology.getDeviceIdentifier();
-        assertEquals("TopologyId should be equal to " + DEVICE_ID, DEVICE_ID, deviceIdentifier.findDevice().getId());
     }
 
     private DeviceIdentifier getDeviceIdentifier(){
-        DeviceIdentifier deviceIdentifier = mock(DeviceIdentifier.class);
-        Device device = mock(Device.class);
-        when(device.getId()).thenReturn(DEVICE_ID);
-        when(deviceIdentifier.findDevice()).thenReturn(device);
         return deviceIdentifier;
     }
 
