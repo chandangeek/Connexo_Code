f37efda0f8e35c54425ee86a4a0b07b688d07434
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-01-30 15:31:06 +0100

    JP-531: Added REST test

--- comu/com.energyict.mdc.device.config.rest/src/test/java/com/energyict/mdc/device/configuration/rest/impl/DeviceTypeResourceTest.java
+++ comu/com.energyict.mdc.device.config.rest/src/test/java/com/energyict/mdc/device/configuration/rest/impl/DeviceTypeResourceTest.java
@@ -1,6 +1,12 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
+import com.energyict.mdc.protocol.api.DeviceFunction;
+import com.energyict.mdc.protocol.api.DeviceProtocol;
+import com.energyict.mdc.protocol.api.DeviceProtocolCapabilities;
+import com.energyict.mdc.protocol.api.DeviceProtocolPluggableClass;
 import com.energyict.mdc.services.DeviceConfigurationService;
+import com.energyict.mdw.core.DeviceType;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import javax.ws.rs.core.Application;
@@ -17,6 +23,7 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 public class DeviceTypeResourceTest extends JerseyTest {
 
@@ -63,51 +70,51 @@ public class DeviceTypeResourceTest extends JerseyTest {
         assertThat((List)map.get("deviceTypes")).isEmpty();
     }
 
-//    @Test
-//    public void testGetSingleDeviceTypeList() throws Exception {
-//        int NUMBER_OF_CONFIGS = 4;
-//        int NUMBER_OF_LOADPROFILES = 6;
-//        int NUMBER_OF_REGISTERS = 8;
-//        int NUMBER_OF_LOGBOOKS = 10;
-//
-//        DeviceType deviceType = mock(DeviceType.class);
-//        when(deviceType.getName()).thenReturn("unique name");
-//        List configsList = mock(List.class);
-//        when(configsList.size()).thenReturn(NUMBER_OF_CONFIGS);
-//        List loadProfileList = mock(List.class);
-//        when(loadProfileList.size()).thenReturn(NUMBER_OF_LOADPROFILES);
-//        List registerList = mock(List.class);
-//        when(registerList.size()).thenReturn(NUMBER_OF_REGISTERS);
-//        List logBooksList = mock(List.class);
-//        when(logBooksList.size()).thenReturn(NUMBER_OF_LOGBOOKS);
-//
-//        DeviceProtocolPluggableClass deviceProtocolPluggableClass = mock(DeviceProtocolPluggableClass.class);
-//        DeviceProtocol deviceProtocol = mock(DeviceProtocol.class);
-//        when(deviceProtocol.getDeviceProtocolCapabilities()).thenReturn(Arrays.asList(DeviceProtocolCapabilities.PROTOCOL_MASTER));
-//        when(deviceProtocol.getDeviceFunction()).thenReturn(DeviceFunction.GATEWAY);
-//        when(deviceProtocolPluggableClass.getDeviceProtocol()).thenReturn(deviceProtocol);
-//        when(deviceProtocolPluggableClass.getName()).thenReturn("device protocol name");
-//        when(deviceType.getConfigurations()).thenReturn(configsList);
-//        when(deviceType.getLoadProfileTypes()).thenReturn(loadProfileList);
-//        when(deviceType.getLogBookTypes()).thenReturn(logBooksList);
-//        when(deviceType.getRegisterMappings()).thenReturn(registerList);
-//        when(deviceType.getDeviceProtocolPluggableClass()).thenReturn(deviceProtocolPluggableClass);
-//
-//        when(deviceConfigurationService.findAllDeviceTypes()).thenReturn(Arrays.asList(deviceType));
-//
-//        Map<String, Object> map = target("/devicetypes/").request().get(Map.class);
-//        assertThat(map.get("total")).describedAs("JSon representation of a field, JavaScript impact if it changed").isEqualTo(1);
-//        assertThat((List)map.get("deviceTypes")).hasSize(1).describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        Map jsonDeviceType = (Map) ((List) map.get("deviceTypes")).get(0);
-//        assertThat(jsonDeviceType.get("logBookCount")).isEqualTo(NUMBER_OF_LOGBOOKS).describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceType.get("registerCount")).isEqualTo(NUMBER_OF_REGISTERS).describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceType.get("deviceConfigurationCount")).isEqualTo(NUMBER_OF_CONFIGS).describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceType.get("loadProfileCount")).isEqualTo(NUMBER_OF_LOADPROFILES).describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceType.get("name")).isEqualTo("unique name").describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        Map<String, Object> jsonDeviceProtocol = (Map<String, Object>) jsonDeviceType.get("communicationProtocol");
-//        assertThat(jsonDeviceProtocol).isNotNull().describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceProtocol.get("name")).isEqualTo("device protocol name").describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceProtocol.get("serviceKind")).isNotNull().describedAs("JSon representation of a field, JavaScript impact if it changed");
-//        assertThat(jsonDeviceProtocol.get("deviceFunction")).isNotNull().describedAs("JSon representation of a field, JavaScript impact if it changed");
-//    }
+    @Test
+    public void testGetSingleDeviceTypeList() throws Exception {
+        int NUMBER_OF_CONFIGS = 4;
+        int NUMBER_OF_LOADPROFILES = 6;
+        int NUMBER_OF_REGISTERS = 8;
+        int NUMBER_OF_LOGBOOKS = 10;
+
+        DeviceType deviceType = mock(DeviceType.class);
+        when(deviceType.getName()).thenReturn("unique name");
+        List configsList = mock(List.class);
+        when(configsList.size()).thenReturn(NUMBER_OF_CONFIGS);
+        List loadProfileList = mock(List.class);
+        when(loadProfileList.size()).thenReturn(NUMBER_OF_LOADPROFILES);
+        List registerList = mock(List.class);
+        when(registerList.size()).thenReturn(NUMBER_OF_REGISTERS);
+        List logBooksList = mock(List.class);
+        when(logBooksList.size()).thenReturn(NUMBER_OF_LOGBOOKS);
+
+        DeviceProtocolPluggableClass deviceProtocolPluggableClass = mock(DeviceProtocolPluggableClass.class);
+        DeviceProtocol deviceProtocol = mock(DeviceProtocol.class);
+        when(deviceProtocol.getDeviceProtocolCapabilities()).thenReturn(Arrays.asList(DeviceProtocolCapabilities.PROTOCOL_MASTER));
+        when(deviceProtocol.getDeviceFunction()).thenReturn(DeviceFunction.GATEWAY);
+        when(deviceProtocolPluggableClass.getDeviceProtocol()).thenReturn(deviceProtocol);
+        when(deviceProtocolPluggableClass.getName()).thenReturn("device protocol name");
+        when(deviceType.getConfigurations()).thenReturn(configsList);
+        when(deviceType.getLoadProfileTypes()).thenReturn(loadProfileList);
+        when(deviceType.getLogBookTypes()).thenReturn(logBooksList);
+        when(deviceType.getRegisterMappings()).thenReturn(registerList);
+        when(deviceType.getDeviceProtocolPluggableClass()).thenReturn(deviceProtocolPluggableClass);
+
+        when(deviceConfigurationService.findAllDeviceTypes()).thenReturn(Arrays.asList(deviceType));
+
+        Map<String, Object> map = target("/devicetypes/").request().get(Map.class);
+        assertThat(map.get("total")).describedAs("JSon representation of a field, JavaScript impact if it changed").isEqualTo(1);
+        assertThat((List)map.get("deviceTypes")).hasSize(1).describedAs("JSon representation of a field, JavaScript impact if it changed");
+        Map jsonDeviceType = (Map) ((List) map.get("deviceTypes")).get(0);
+        assertThat(jsonDeviceType.get("logBookCount")).isEqualTo(NUMBER_OF_LOGBOOKS).describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceType.get("registerCount")).isEqualTo(NUMBER_OF_REGISTERS).describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceType.get("deviceConfigurationCount")).isEqualTo(NUMBER_OF_CONFIGS).describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceType.get("loadProfileCount")).isEqualTo(NUMBER_OF_LOADPROFILES).describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceType.get("name")).isEqualTo("unique name").describedAs("JSon representation of a field, JavaScript impact if it changed");
+        Map<String, Object> jsonDeviceProtocol = (Map<String, Object>) jsonDeviceType.get("communicationProtocol");
+        assertThat(jsonDeviceProtocol).isNotNull().describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceProtocol.get("name")).isEqualTo("device protocol name").describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceProtocol.get("serviceKind")).isNotNull().describedAs("JSon representation of a field, JavaScript impact if it changed");
+        assertThat(jsonDeviceProtocol.get("deviceFunction")).isNotNull().describedAs("JSon representation of a field, JavaScript impact if it changed");
+    }
 }
