e812c3aeb74b98adcec88ade85b78d115d1e23a7
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-01-31 15:57:32 +0100

    JP-531: Added Finder support for legacy factories ...

---
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/services/FactoryFinder.java
@@ -0,0 +1,38 @@
+package com.energyict.mdc.common.services;
+
+import java.util.List;
+
+/**
+ * The Kore Finder backported for use by legacy factories
+ * @param <T>
+ */
+public class FactoryFinder<T> implements Finder<T> {
+
+    private final Callback<T> callback;
+
+    public FactoryFinder(Callback<T> callback) {
+        this.callback = callback;
+    }
+
+    public static <T> Finder<T> of(Callback<T> callback) {
+        return new FactoryFinder<T>(callback);
+    }
+    @Override
+    public Finder<T> paged(Integer start, Integer pageSize) {
+        return this;
+    }
+
+    @Override
+    public Finder<T> sorted(String sortColumn, SortOrder sortOrder) {
+        return this;
+    }
+
+    @Override
+    public List<T> find() {
+        return callback.getDataFromFactory();
+    }
+
+    public interface Callback<T> {
+        public List<T> getDataFromFactory();
+    }
+}
