168b0eb640e499e126ad3699c2c6e54d58931d19
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-02-06 09:23:46 +0100

    JP-725 (device type details): Added robustness

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/DeviceTypeInfo.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/DeviceTypeInfo.java
@@ -1,8 +1,11 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
 import com.energyict.mdc.protocol.api.DeviceFunction;
+import com.energyict.mdc.protocol.api.DeviceProtocol;
 import com.energyict.mdc.protocol.api.DeviceProtocolCapabilities;
+import com.energyict.mdc.protocol.api.DeviceProtocolPluggableClass;
 import com.energyict.mdw.core.DeviceType;
+import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonUnwrapped;
@@ -36,8 +39,17 @@ public class DeviceTypeInfo {
         this.registerCount=deviceType.getRegisterMappings().size();
         this.logBookCount=deviceType.getLogBookTypes().size();
         this.deviceConfigurationCount=deviceType.getConfigurations().size();
-        this.isDirectlyAddressable=deviceType.getDeviceProtocolPluggableClass().getDeviceProtocol().getDeviceProtocolCapabilities().contains(DeviceProtocolCapabilities.PROTOCOL_SESSION);
-        this.canBeGateway=deviceType.getDeviceProtocolPluggableClass().getDeviceProtocol().getDeviceFunction().equals(DeviceFunction.GATEWAY);
-        this.deviceProtocolInfo=new DeviceProtocolInfo(deviceType.getDeviceProtocolPluggableClass());
+        DeviceProtocolPluggableClass deviceProtocolPluggableClass = deviceType.getDeviceProtocolPluggableClass();
+        if (deviceProtocolPluggableClass!=null) {
+            this.deviceProtocolInfo=new DeviceProtocolInfo(deviceProtocolPluggableClass);
+            DeviceProtocol deviceProtocol = deviceProtocolPluggableClass.getDeviceProtocol();
+            if (deviceProtocol!=null) {
+                this.canBeGateway= DeviceFunction.GATEWAY.equals(deviceProtocol.getDeviceFunction());
+                List<DeviceProtocolCapabilities> deviceProtocolCapabilities = deviceProtocol.getDeviceProtocolCapabilities();
+                if (deviceProtocolCapabilities!=null) {
+                    this.isDirectlyAddressable= deviceProtocolCapabilities.contains(DeviceProtocolCapabilities.PROTOCOL_SESSION);
+                }
+            }
+        }
     }
 }
