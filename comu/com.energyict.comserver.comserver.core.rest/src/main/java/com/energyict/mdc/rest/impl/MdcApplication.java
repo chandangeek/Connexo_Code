0f876aa27c72a83f995a190968f938fbdf17cc99
Dieter Vyncke <dieter.vyncke@elster.com>
2014-02-11 09:08:39 +0100

    JP-840 - create/edit device types

--- comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/MdcApplication.java
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/MdcApplication.java
@@ -19,7 +19,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
-@Component(name = "com.elster.mdc.rest", service = Application.class, immediate = true, property = {"alias=/mdc"})
+@Component(name = "com.energyict.mdc.rest", service = Application.class, immediate = true, property = {"alias=/mdc"})
 public class MdcApplication extends Application {
 
     private static final Logger LOGGER = Logger.getLogger(MdcApplication.class.getSimpleName());
