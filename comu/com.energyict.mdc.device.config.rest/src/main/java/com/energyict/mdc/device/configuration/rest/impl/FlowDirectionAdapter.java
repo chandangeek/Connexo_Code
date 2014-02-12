c250ce0496bc05e9bb3b52ce4896a7b66868bfcc
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-02-12 11:44:04 +0100

    JP-940: layout improvements

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/FlowDirectionAdapter.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/FlowDirectionAdapter.java
@@ -16,18 +16,18 @@ public class FlowDirectionAdapter extends MapBasedXmlAdapter<FlowDirection> {
         register("Lagging", FlowDirection.LAGGING);
         register("Leading", FlowDirection.LEADING);
         register("Net", FlowDirection.NET);
-        register("Q1", FlowDirection.Q1);
-        register("Q1 minus Q4", FlowDirection.Q1MINUSQ4);
-        register("Q1 plus Q2", FlowDirection.Q1PLUSQ2);
-        register("Q1 plus Q3", FlowDirection.Q1PLUSQ3);
-        register("Q1 plus Q4", FlowDirection.Q1PLUQQ4);
-        register("Q2", FlowDirection.Q2);
-        register("Q2 minus Q3", FlowDirection.Q2MINUSQ3);
-        register("Q2 plus Q3", FlowDirection.Q2PLUSQ3);
-        register("Q2 plus Q4", FlowDirection.Q2PLUSQ4);
-        register("Q3", FlowDirection.Q3);
-        register("Q3 minus Q2", FlowDirection.Q3MINUSQ2);
-        register("Q3 plus Q4", FlowDirection.Q3PLUSQ4);
-        register("Q4", FlowDirection.Q4);
+        register("Q1 only", FlowDirection.Q1);
+        register("Q1 - Q4", FlowDirection.Q1MINUSQ4);
+        register("Q1 + Q2", FlowDirection.Q1PLUSQ2);
+        register("Q1 + Q3", FlowDirection.Q1PLUSQ3);
+        register("Q1 + Q4", FlowDirection.Q1PLUQQ4);
+        register("Q2 only", FlowDirection.Q2);
+        register("Q2 - Q3", FlowDirection.Q2MINUSQ3);
+        register("Q2 + Q3", FlowDirection.Q2PLUSQ3);
+        register("Q2 + Q4", FlowDirection.Q2PLUSQ4);
+        register("Q3 only", FlowDirection.Q3);
+        register("Q3 - Q2", FlowDirection.Q3MINUSQ2);
+        register("Q3 + Q4", FlowDirection.Q3PLUSQ4);
+        register("Q4 only", FlowDirection.Q4);
     }
 }
