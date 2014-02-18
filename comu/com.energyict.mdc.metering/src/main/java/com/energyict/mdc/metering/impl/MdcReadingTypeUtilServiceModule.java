3da19887c718185e2a1c76ac99ea4ff6c6d51d84
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-02-18 14:00:42 +0100

    JP-792: Create integration testing module

---
+++ comu/com.energyict.mdc.metering/src/main/java/com/energyict/mdc/metering/impl/MdcReadingTypeUtilServiceModule.java
@@ -0,0 +1,17 @@
+package com.energyict.mdc.metering.impl;
+
+import com.energyict.mdc.metering.MdcReadingTypeUtilService;
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+
+/**
+ * Module to resolve dependencies during integration testing
+ */
+public class MdcReadingTypeUtilServiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(MdcReadingTypeUtilService.class).to(MdcReadingTypeUtilServiceImpl.class).in(Scopes.SINGLETON);
+    }
+
+}
