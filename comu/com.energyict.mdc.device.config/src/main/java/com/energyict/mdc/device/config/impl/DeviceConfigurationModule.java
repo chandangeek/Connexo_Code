2384d3c73aeee1b0779d868527f0a3c74c71b970
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-02-18 14:04:07 +0100

    JP-792: Add dependency

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceConfigurationModule.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceConfigurationModule.java
@@ -1,14 +1,15 @@
 package com.energyict.mdc.device.config.impl;
 
 import com.elster.jupiter.events.EventService;
-import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.orm.OrmService;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
+import com.energyict.mdc.metering.MdcReadingTypeUtilService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
 /**
- * Insert your comments here.
+ * Module intended for use by integration tests
  *
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2014-01-30 (15:49)
@@ -17,9 +18,10 @@ public class DeviceConfigurationModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        requireBinding(DataModel.class);
         requireBinding(OrmService.class);
         requireBinding(EventService.class);
+        requireBinding(MeteringService.class);
+        requireBinding(MdcReadingTypeUtilService.class);
 
         bind(DeviceConfigurationService.class).to(DeviceConfigurationServiceImpl.class).in(Scopes.SINGLETON);
     }
