8a500d421a37bd1ef11ec9a75e1231ae9a928de2
borunova <borunova@mera.ru>
2014-02-19 14:46:58 +0100

    Bug JP-1010 improvement

--- coko/com.elster.jupiter.issue/src/main/java/com/elster/jupiter/issue/IssueStatus.java
+++ coko/com.elster.jupiter.issue/src/main/java/com/elster/jupiter/issue/IssueStatus.java
@@ -1,19 +1,22 @@
 package com.elster.jupiter.issue;
 
-import com.google.common.base.CaseFormat;
-
 /**
  This enumeration provides list of available statuses for issue type
  */
 public enum IssueStatus {
-    OPEN,
-    CLOSED,
-    POSTPONED,
-    IN_PROGRESS,
-    REJECTED;
+    OPEN ("Open"),
+    CLOSED ("Closed"),
+    POSTPONED ("Postponded"),
+    IN_PROGRESS ("In progress"),
+    REJECTED ("Rejected");
+
+    private final String readableName;
+
+    IssueStatus(String name) {
+        this.readableName = name;
+    }
 
     public String toString() {
-        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, this.name());
-
+        return this.readableName;
     }
 }
