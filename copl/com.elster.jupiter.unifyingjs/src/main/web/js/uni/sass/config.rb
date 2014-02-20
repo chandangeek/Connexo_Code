2350fc1fe7671b7ff21044c03d1b240eafb1ac17
Levi Van Zele <Levi.VanZele@elster.com>
2014-02-20 15:05:35 +0100

    [JP-661] Removed obsolete configuration.

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/sass/config.rb
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/sass/config.rb
@@ -3,9 +3,6 @@ cur_dir = File.dirname(__FILE__)
 # Get the directory that this configuration file exists in
 dir = File.dirname(__FILE__)
 
-# Load ExtJS themes
-#load File.join(dir, '..', '..', '..', '..', '..', '..', '..', '..', 'com.elster.jupiter.extjs', 'src', 'main', 'web', 'js', 'ext', 'packages', 'ext-theme-neptune')
-
 # Compass configurations
 sass_path = dir
 css_path = File.join(dir, '..', 'resources', 'css')
