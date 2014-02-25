3a5844b9939159faea8e1dc1c875a04c0272afd2
Isabelle  Gheysens <isabelle.gheysens@elster.com>
2014-02-25 12:06:39 +0100

    reset

--- coko/com.elster.jupiter.config.extjs/src/main/java/com/elster/jupiter/config/extjs/Activator.java
+++ coko/com.elster.jupiter.config.extjs/src/main/java/com/elster/jupiter/config/extjs/Activator.java
@@ -9,18 +9,18 @@ import com.elster.jupiter.http.whiteboard.*;
 
 public class Activator implements BundleActivator {
 
-	private volatile ServiceRegistration<HttpResource> registration;
-	
-	public void start(BundleContext bundleContext) throws Exception {
-		String alias = "/cfg";
-		//HttpResource resource = new HttpResource(alias, "/js/cfg" , new BundleResolver(bundleContext),new DefaultStartPage("configuration"));
-		// Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
-		HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/com.elster.jupiter.config.extjs/src/main/web/js/cfg" , new FileResolver());
-		registration = bundleContext.registerService(HttpResource.class, resource , null);
-	}
-	
-	public void stop(BundleContext bundleContext) throws Exception {
-		registration.unregister();
-	}
+    private volatile ServiceRegistration<HttpResource> registration;
+
+    public void start(BundleContext bundleContext) throws Exception {
+        String alias = "/cfg";
+        HttpResource resource = new HttpResource(alias, "/js/cfg" , new BundleResolver(bundleContext),new DefaultStartPage("configuration"));
+        // Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
+        //HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/meteringextjs/js/mtr" , new FileResolver());
+        registration = bundleContext.registerService(HttpResource.class, resource , null);
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        registration.unregister();
+    }
 
 }
