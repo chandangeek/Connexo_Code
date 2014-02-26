aaf6c71d07182ef75ad4f7ab70bf7de7db266778
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-02-26 14:17:04 +0100

    JP-919: added create and update (POST and PUT) for register type

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/RegisterTypeResource.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/RegisterTypeResource.java
@@ -1,9 +1,13 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.ReadingType;
 import com.energyict.mdc.common.rest.PagedInfoList;
 import com.energyict.mdc.common.rest.QueryParameters;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
+import com.energyict.mdc.device.config.ProductSpec;
 import com.energyict.mdc.device.config.RegisterMapping;
+import com.google.common.base.Optional;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -11,6 +15,8 @@ import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,10 +28,12 @@ import javax.ws.rs.core.Response;
 public class RegisterTypeResource {
 
     private final DeviceConfigurationService deviceConfigurationService;
+    private final MeteringService meteringService;
 
     @Inject
-    public RegisterTypeResource(DeviceConfigurationService deviceConfigurationService) {
+    public RegisterTypeResource(DeviceConfigurationService deviceConfigurationService, MeteringService meteringService) {
         this.deviceConfigurationService = deviceConfigurationService;
+        this.meteringService = meteringService;
     }
 
     @GET
@@ -53,6 +61,46 @@ public class RegisterTypeResource {
         RegisterMapping registerMapping = findRegisterTypeOrThrowException(id);
         registerMapping.delete();
         return Response.ok().build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public RegisterMappingInfo createRegisterMapping(RegisterMappingInfo registerMappingInfo) {
+        Optional<ReadingType> readingType = findReadingTypeOrThrowException(registerMappingInfo);
+        ProductSpec productSpecByReadingType = findProductSpecOrThrowException(registerMappingInfo, readingType.get());
+
+        return new RegisterMappingInfo(deviceConfigurationService.newRegisterMapping(registerMappingInfo.name, registerMappingInfo.obisCode, productSpecByReadingType));
+    }
+
+    private ProductSpec findProductSpecOrThrowException(RegisterMappingInfo registerMappingInfo, ReadingType readingType) {
+        ProductSpec productSpecByReadingType = deviceConfigurationService.findProductSpecByReadingType(readingType);
+        if (productSpecByReadingType==null) {
+            throw new WebApplicationException("No product spec for reading type " + registerMappingInfo.readingTypeInfo.mrid, Response.Status.BAD_REQUEST);
+        }
+        return productSpecByReadingType;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public RegisterMappingInfo updateRegisterMapping(@PathParam("id") Long id, RegisterMappingInfo registerMappingInfo) {
+        RegisterMapping registerMapping = findRegisterTypeOrThrowException(id);
+        registerMappingInfo.writeTo(registerMapping);
+        Optional<ReadingType> readingType = findReadingTypeOrThrowException(registerMappingInfo);
+        ProductSpec productSpecByReadingType = findProductSpecOrThrowException(registerMappingInfo, readingType.get());
+        registerMapping.setProductSpec(productSpecByReadingType);
+        registerMapping.save();
+        return new RegisterMappingInfo(registerMapping);
+    }
+
+    private Optional<ReadingType> findReadingTypeOrThrowException(RegisterMappingInfo registerMappingInfo) {
+        Optional<ReadingType> readingType = meteringService.getReadingType(registerMappingInfo.readingTypeInfo.mrid);
+        if (!readingType.isPresent()) {
+            throw new WebApplicationException("No reading type " + registerMappingInfo.readingTypeInfo.mrid, Response.Status.BAD_REQUEST);
+        }
+        return readingType;
     }
 
     private RegisterMapping findRegisterTypeOrThrowException(Long id) {
