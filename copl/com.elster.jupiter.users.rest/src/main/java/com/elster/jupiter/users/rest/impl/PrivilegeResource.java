60b95729685b32748d7b75c21518d9d6458d8beb
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2014-02-27 16:06:00 +0100

    Revert "Added REST methods for listing permissions on group/user"
    This reverts commit 0447247f347a71a777fc932fab45c1b46b030099.

--- copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/impl/PrivilegeResource.java
+++ copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/impl/PrivilegeResource.java
@@ -1,19 +1,15 @@
 package com.elster.jupiter.users.rest.impl;
 
 import com.elster.jupiter.transaction.TransactionService;
-import com.elster.jupiter.users.Group;
-import com.elster.jupiter.users.User;
 import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.users.rest.PrivilegeInfos;
 import com.elster.jupiter.users.rest.actions.AddPrivilegeToGroupTransaction;
 import com.elster.jupiter.users.rest.actions.DeletePrivilegeFromGroupTransaction;
-import com.google.common.base.Optional;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 @Path("/privileges")
@@ -54,33 +50,6 @@ public class PrivilegeResource {
         return result;
     }
 
-    @GET
-    @Path("/groups/{idgroup}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public PrivilegeInfos getPrivilegesForGroup(@PathParam("idgroup") long idGroup) {
-        PrivilegeInfos result = new PrivilegeInfos();
-
-        Optional<Group> foundGroup = userService.getGroup(idGroup);
-        if(!foundGroup.isPresent()){
-            throw new WebApplicationException(Response.Status.NOT_FOUND);
-        }
-
-        result.addAll(foundGroup.get().getPrivileges());
-        return result;
-    }
-
-    @GET
-    @Path("/users/{iduser}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public PrivilegeInfos getPrivilegesForUser(@PathParam("iduser") long idUser) {
-        PrivilegeInfos result = new PrivilegeInfos();
-
-        Optional<User> foundUser = userService.getUser(idUser);
-        if(!foundUser.isPresent()){
-            throw new WebApplicationException(Response.Status.NOT_FOUND);
-        }
-
-        result.addAll(foundUser.get().getPrivileges());
-        return result;
-    }
+    //TODO: add GET all privileges for group
+    //TODO: add GET all privileges for user
 }
