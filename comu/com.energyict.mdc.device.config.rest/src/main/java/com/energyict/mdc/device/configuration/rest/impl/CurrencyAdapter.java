70b81e27d9086fdb1535c9b8005c98c884539624
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-03-03 13:47:41 +0100

    JP-919: Fixed adapter to handle null and empty string

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/CurrencyAdapter.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/CurrencyAdapter.java
@@ -1,5 +1,6 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
+import com.elster.jupiter.util.Checks;
 import java.util.Currency;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
@@ -7,11 +8,17 @@ public class CurrencyAdapter extends XmlAdapter<String, Currency>{
 
     @Override
     public Currency unmarshal(String jsonValue) throws Exception {
+        if (Checks.is(jsonValue).emptyOrOnlyWhiteSpace()) {
+            return null;
+        }
         return Currency.getInstance(jsonValue);
     }
 
     @Override
     public String marshal(Currency currency) throws Exception {
+        if (currency==null) {
+            return null;
+        }
         return currency.toString();
     }
 }
