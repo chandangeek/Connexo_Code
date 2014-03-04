ab3dadc181ade6a408c338f8c0dee92baa550aa6
Sergey <ssentyur@mera.ru>
2014-03-04 15:51:24 +0400

    "Issue assignment rules" navigation & filter panels add

---
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/datacollection/issueassignmentrules/Filter.js
@@ -0,0 +1,9 @@
+Ext.define('Mtr.view.workspace.datacollection.issueassignmentrules.Filter', {
+    extend: 'Ext.container.Container',
+    alias: 'widget.issues-assignment-rules-filter',
+    items: [
+        {
+            html: '<span>Filter</span>'
+        }
+    ]
+});
--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/datacollection/issueassignmentrules/Overview.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/datacollection/issueassignmentrules/Overview.js
@@ -2,18 +2,48 @@ Ext.define('Mtr.view.workspace.datacollection.issueassignmentrules.Overview', {
     extend: 'Ext.container.Container',
     requires: [
         'Uni.view.breadcrumb.Trail',
+        'Mtr.view.workspace.datacollection.issueassignmentrules.Navigation',
+        'Mtr.view.workspace.datacollection.issueassignmentrules.Filter',
         'Mtr.view.workspace.datacollection.issueassignmentrules.List'
     ],
     alias: 'widget.issue-assignment-rules-overview',
+    layout: 'border',
     overflowY: 'auto',
 
     items: [
         {
             xtype: 'breadcrumbTrail',
+            region: 'north',
             padding: 6
         },
         {
-            xtype: 'issues-assignment-rules-list'
+            xtype: 'container',
+            region: 'west',
+            width: 170,
+            items: [
+                {
+                    xtype: 'issues-assignment-rules-navigation'
+                },
+                {
+                    xtype: 'issues-assignment-rules-filter'
+                }
+            ]
+        },
+        {
+            xtype: 'container',
+            region: 'center',
+            cls: 'content-wrapper',
+            style: {
+                backgroundColor: '#fff'
+            },
+            items: [
+                {
+                    html: '<h1>Issue assignment rules</h1>'
+                },
+                {
+                    xtype: 'issues-assignment-rules-list'
+                }
+            ]
         }
     ]
 });
