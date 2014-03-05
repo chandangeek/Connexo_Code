1c1d831ef51ab679c40136b3fa5dacfdec1a3d48
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-03-05 07:46:58 +0100

    JP-1073: Created exception to trap JSON mapping errors (unfinished)

---
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/UnmappableEnumValueException.java
@@ -0,0 +1,17 @@
+package com.energyict.mdc.common.rest;
+
+/**
+ * Thrown by JSon adapters when they run into a string value that can't be converted to an enum
+ */
+public class UnmappableEnumValueException extends RuntimeException {
+    private String fieldName;
+
+    public UnmappableEnumValueException(String fieldName, String message) {
+        super(message);
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+}
