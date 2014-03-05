86a07f8aeb08e733e69c7d1ab42c497a656bc62c
Dieter Vyncke <dieter.vyncke@elster.com>
2014-03-05 15:31:47 +0100

    JP-1027 - Merge existing MDC and Kore apps

--- coko/com.elster.jupiter.config.extjs/src/main/java/com/elster/jupiter/config/extjs/Activator.java
+++ coko/com.elster.jupiter.config.extjs/src/main/java/com/elster/jupiter/config/extjs/Activator.java
@@ -13,8 +13,8 @@ public class Activator implements BundleActivator {
 
     public void start(BundleContext bundleContext) throws Exception {
         String alias = "/cfg";
-//        HttpResource resource = new HttpResource(alias, "/js/cfg" , new BundleResolver(bundleContext),new DefaultStartPage("Cfg","","/index.html","Cfg.controller.Main"));
-        HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/jupiter-all/com.elster.jupiter.config.extjs/src/main/web/js/cfg" , new FileResolver(),new DefaultStartPage("Cfg","","/index.html","Cfg.controller.Main"));
+        HttpResource resource = new HttpResource(alias, "/js/cfg" , new BundleResolver(bundleContext),new DefaultStartPage("Cfg","","/index.html","Cfg.controller.Main"));
+//        HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/jupiter-all/com.elster.jupiter.config.extjs/src/main/web/js/cfg" , new FileResolver(),new DefaultStartPage("Cfg","","/index.html","Cfg.controller.Main"));
 //        HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/comserver-all/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc" , new FileResolver(),new DefaultStartPage("Mdc","","/index.html","Mdc.controller.Main"));
 
         // Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
