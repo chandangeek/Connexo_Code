47cce76cf3d911ade7c66378697295678c6a4d4f
Sergey <ssentyur@mera.ru>
2014-03-07 17:05:20 +0400

    Unit tests add

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app-test/specs/data_collection.issues.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app-test/specs/data_collection.issues.js
@@ -1,16 +1,70 @@
-describe('Data collection issues', function () {
-    var ctlr = null;
+window.location.href = '#/workspace/datacollection/issues';
 
-    beforeEach(function(){
-        if (!ctlr) {
-            ctlr = Mtr.getApplication().getController('Issues');
-        }
+describe('Data collection issues', function () {
+    var delay = 5000,
+        issuesController,
+        issuesStore;
+
+    beforeEach(function () {
+        issuesController = Mtr.getApplication().getController('Issues');
+        issuesStore = issuesController.getStore('Mtr.store.Issues');
+
+        expect(issuesStore).toBeTruthy();
+
+        waitsFor(function () {
+                return !issuesStore.isLoading();
+            },
+            'loading issues store',
+            delay
+        );
     });
 
-    it("tracks the number of calls", function() {
-        spyOn(ctlr, 'bulkChangeIssues');
-        ctlr.bulkChangeIssues();
-        ctlr.bulkChangeIssues();
-        return expect(ctlr.bulkChangeIssues.calls.length).toEqual(2);
+    it('has loaded issue detail', function () {
+        var grid = issuesController.getIssuesList().getView(),
+            record = issuesStore.getAt(0),
+            itemPanel = issuesController.getItemPanel(),
+            flag;
+
+        runs(function () {
+            itemPanel.on('change', function () {
+                flag = true;
+            });
+            issuesController.loadGridItemDetail(grid, record);
+        });
+
+        waitsFor(function () {
+                return flag;
+            },
+            'loading issue detail',
+            delay
+        );
+
+        runs(function () {
+            expect(itemPanel.down('toolbar')).not.toBeUndefined();
+        });
+    });
+
+    it('has not loaded issue detail', function () {
+        var itemPanel = issuesController.getItemPanel();
+
+        issuesController.showDefaultItems();
+
+        expect(itemPanel.down('toolbar')).toBeNull();
+    });
+
+    it('has clicked issues grid action icon', function () {
+        var actionCells = Ext.query('[id^=issues-list] .x-grid-cell-inner-action-col'),
+            grid = issuesController.getIssuesList().getView(),
+            firstActionCell = actionCells[0],
+            firstActionCellEll = Ext.get(firstActionCell),
+            menuBtn;
+
+        firstActionCell.click();
+        menuBtn = firstActionCellEll.next();
+
+        expect(firstActionCellEll.getHeight()).toEqual(0);
+        expect(firstActionCellEll.isVisible()).toBeFalsy();
+        expect(menuBtn).not.toBeNull();
+        expect(menuBtn.isVisible()).toBeTruthy();
     });
 });
