91ce609c056bd300c072c556d70f31580f9f2fb0
Isabelle  Gheysens <isabelle.gheysens@elster.com>
2014-03-11 10:01:35 +0100

    JP-1138 - Sorting in grid is case sensitve

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/ValidationRuleSetImpl.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/ValidationRuleSetImpl.java
@@ -197,7 +197,7 @@ public final class ValidationRuleSetImpl implements IValidationRuleSet {
     public List<IValidationRule> getRules(int start, int limit) {
         return Collections.unmodifiableList(
                 dataModel.query(IValidationRule.class).select(
-                        Operator.EQUAL.compare("ruleSetId", this.id), new Order[] {Order.ascending("name")}, false, new String[]{}, start + 1, start + limit));
+                        Operator.EQUAL.compare("ruleSetId", this.id), new Order[] {Order.ascending("upper(name)")}, false, new String[]{}, start + 1, start + limit));
     }
 
     private List<IValidationRule> doGetRules() {
@@ -208,7 +208,7 @@ public final class ValidationRuleSetImpl implements IValidationRuleSet {
     }
 
     private List<IValidationRule> loadRules() {
-        return ruleFactory().find("ruleSet", this, Order.ascending("name"));
+        return ruleFactory().find("ruleSet", this, Order.ascending("upper(name)"));
     }
 
     @Override
