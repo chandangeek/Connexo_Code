3d64d3c27b4569afdcedc1edbab5823fcd102760
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-03-12 16:00:46 +0100

    JP-1430: Introduce RegisterConfigInfo

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/RegisterConfigInfo.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/RegisterConfigInfo.java
@@ -1,14 +1,32 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
+import com.energyict.mdc.common.ObisCode;
 import com.energyict.mdc.device.config.RegisterSpec;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 public class RegisterConfigInfo {
 
     @JsonProperty("id")
     public long id;
+    @JsonProperty("name")
+    public String name;
+    @JsonProperty("readingType")
+    public ReadingTypeInfo readingTypeInfo;
+    @JsonProperty("obisCode")
+    @XmlJavaTypeAdapter(ObisCodeAdapter.class)
+    public ObisCode obisCode;
+    @JsonProperty("obisCodeDescription")
+    public String obisCodeDescription;
+
+    public RegisterConfigInfo() {
+    }
 
     public RegisterConfigInfo(RegisterSpec registerSpec) {
         this.id = registerSpec.getId();
+        this.name = registerSpec.getRegisterMapping().getName();
+        this.readingTypeInfo = new ReadingTypeInfo(registerSpec.getRegisterMapping().getReadingType());
+        this.obisCode = registerSpec.getObisCode();
+        this.obisCodeDescription = registerSpec.getObisCode().getDescription();
     }
 }
