44db181a1adda9bd03a116aefe9fbb11e3a9ae5a
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-03-12 13:50:35 +0100

    JP-1425: Added more isLinkedBy... fields

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceConfigurationServiceImpl.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceConfigurationServiceImpl.java
@@ -195,8 +195,18 @@ public class DeviceConfigurationServiceImpl implements ServerDeviceConfiguration
     }
 
     @Override
-    public List<RegisterSpec> findRegisterSpecsByDeviceTypeAndRegisterMapping(DeviceType deviceType, RegisterMapping registerMapping) {
-        Condition condition = where("deviceConfig.deviceType").isEqualTo(deviceType).and(where("registerMapping").isEqualTo(registerMapping));
+    public List<RegisterSpec> findActiveRegisterSpecsByDeviceTypeAndRegisterMapping(DeviceType deviceType, RegisterMapping registerMapping) {
+        Condition condition = where("deviceConfig.deviceType").isEqualTo(deviceType).
+                and(where("registerMapping").isEqualTo(registerMapping)).
+                and(where("deviceConfig.active").isEqualTo(Boolean.TRUE));
+        return this.getDataModel().query(RegisterSpec.class, DeviceConfiguration.class).select(condition);
+    }
+
+    @Override
+    public List<RegisterSpec> findInactiveRegisterSpecsByDeviceTypeAndRegisterMapping(DeviceType deviceType, RegisterMapping registerMapping) {
+        Condition condition = where("deviceConfig.deviceType").isEqualTo(deviceType).
+                and(where("registerMapping").isEqualTo(registerMapping)).
+                and(where("deviceConfig.active").isEqualTo(Boolean.FALSE));
         return this.getDataModel().query(RegisterSpec.class, DeviceConfiguration.class).select(condition);
     }
 
