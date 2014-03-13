cb75cbf520243f9bd027f222a3a45ce57d1652c2
Isabelle  Gheysens <isabelle.gheysens@elster.com>
2014-03-13 15:58:44 +0100

    JP-1251 - Upgrade all existing screens to use submenu (Kore)

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/navigation/SubMenu.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/navigation/SubMenu.js
@@ -1,5 +1,52 @@
 /**
  * @class Uni.view.navigation.SubMenu
+ *
+ * Common submenu that supports adding buttons and toggling.
+ *
+ * Styling will also be applied automatically to anything that is in the component.
+ *
+ * How and where content is show after clicking a button in the submenu is not fixed.
+ * Switching between panels can easily be done using a card layout.
+ *
+ * # Example usage
+ *
+ *     @example
+ *       side: [
+ *        {
+ *            xtype: 'navigationSubMenu',
+ *            itemId: 'myMenu'
+ *        }
+ *        ],
+ *        ...
+ *        initComponent: function () {
+ *               ...
+ *               this.initMenu();
+ *               ...
+ *           },
+ *
+ *        initMenu: function () {
+ *               var me = this;
+ *               var menu = this.getMenuCmp();
+ *
+ *               var button1 = menu.add({
+ *                   text: '...',
+ *                   pressed: true,
+ *                   href: '...',
+ *                   ...
+ *               });
+ *
+ *               var button2 = menu.add({
+ *                   text: '...',
+ *                   pressed: false,
+ *                   itemId: 'rulesLink',
+ *                   href: '...',
+ *                   ...
+ *               });
+ *               ...
+ *
+ *               getMenuCmp: function () {
+ *                   return this.down('#myMenu');
+ *               },
  */
 Ext.define('Uni.view.navigation.SubMenu', {
     extend: 'Ext.container.Container',
