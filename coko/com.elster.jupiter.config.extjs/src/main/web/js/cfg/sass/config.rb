b222bcb3d46fef145b900e8bb5b4e8cae085192f
Bruno Ballekens <bruno.ballekens@elster.com>
2014-03-14 15:29:43 +0100

    Minification of Javascript - use of UnifiyngJS package

---
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/sass/config.rb
@@ -0,0 +1,21 @@
+# Get the directory that this configuration file exists in
+dir = File.dirname(__FILE__)
+
+
+# Compass configurations
+sass_path = dir
+css_path = File.join(dir, '..', 'resources', 'css')
+fonts_path = File.join(dir, '..', 'resources', 'fonts')
+images_dir = File.join(dir, '..', 'resources', 'images')
+
+# Require any additional compass plugins here.
+output_style = :compressed
+environment = :production
+
+
+module Sass::Script::Functions
+  def get_resource_dir()
+    dir = ENV.fetch("resource.dir", '../../')
+    Sass::Script::String.new(dir)
+  end
+end
