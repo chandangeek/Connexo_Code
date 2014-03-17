15de6b4ff87e34ffbd039c51b6dd5a8cd6563f04
Tom De Greyt <T.DeGreyt@EnergyICT.com>
2014-03-17 13:32:04 +0100

    Answers enumeration.

---
+++ copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/Answers.java
@@ -0,0 +1,19 @@
+package com.elster.jupiter.devtools.tests;
+
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+public enum Answers implements Answer<Object> {
+    RETURNS_SELF;
+
+    public Object answer(InvocationOnMock invocation) throws Throwable {
+        Object mock = invocation.getMock();
+        if( invocation.getMethod().getReturnType().isInstance( mock )){
+            return mock;
+        }
+        else{
+            return Mockito.RETURNS_DEFAULTS.answer(invocation);
+        }
+    }
+}
