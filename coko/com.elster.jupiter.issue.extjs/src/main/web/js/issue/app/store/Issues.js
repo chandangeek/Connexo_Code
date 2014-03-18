80e5d61d78f91d7a12e2c6221f3a0504d0093fa6
Artem Puchenkin <apuchenk@mera.ru>
2014-03-18 15:22:08 +0400

    hotfix

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/store/Issues.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/store/Issues.js
@@ -1,53 +1,17 @@
 Ext.define('Isu.store.Issues', {
     extend: 'Ext.data.Store',
     requires: [
-        'Ext.data.proxy.Rest'
+        'Ext.data.proxy.Rest',
+        'Isu.component.filter.store.Filterable'
+    ],
+
+    mixins: [
+        'Isu.component.filter.store.Filterable'
     ],
 
     model: 'Isu.model.Issues',
     pageSize: 10,
     autoLoad: false,
-    proxyFilter: null,
-
-    /**
-     * @param filter Isu.component.filter.model.Filter
-     */
-    setProxyFilter: function(filter) {
-        if (!filter instanceof Isu.component.filter.model.Filter) {
-            //<debug>
-            Ext.Error.raise('!filter instanceof Isu.component.filter.model.Filter');
-            //</debug>
-        }
-
-        this.proxyFilter = filter;
-        this.load();
-        this.fireEvent('updateProxyFilter', this.proxyFilter);
-    },
-
-    /*
-     * @returns {Isu.component.filter.model.Filter}
-     */
-    getProxyFilter: function() {
-        return this.proxyFilter;
-    },
-
-    removeProxyFilter: function(key, id) {
-        if (!key) {
-            this.proxyFilter = null;
-        } else {
-            if (id) {
-                var store = this.proxyFilter[key]();
-                var rec = store.getById(id);
-                if (rec) {
-                    store.remove(rec);
-                }
-            } else if (!_.isUndefined(this.proxyFilter.data[key])){
-                delete this.proxyFilter.data[key];
-            }
-        }
-        this.load();
-        this.fireEvent('updateProxyFilter', this.proxyFilter);
-    },
 
     listeners: {
         "beforeLoad": function() {
@@ -55,7 +19,7 @@ Ext.define('Isu.store.Issues', {
 
             // replace filter extra params with new ones
             if (this.proxyFilter) {
-                var data = this.proxyFilter.getPlainData();
+                var data = this.getFilterParams();
                 extraParams = _.omit(extraParams, this.proxyFilter.getFields());
                 Ext.merge(extraParams, data);
             }
