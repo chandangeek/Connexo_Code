10e65f2990124e4e3d69703c6ac7b2b1b30dc4f4
Sergey <ssentyur@mera.ru>
2014-03-20 16:36:18 +0400

    JP-1643 "There are no comments yet on this issue" text is displayed if add a comment": bug fix

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/issues/component/TagButton.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/issues/component/TagButton.js
@@ -1,6 +1,5 @@
 Ext.define('Isu.view.workspace.issues.component.TagButton', {
     extend: 'Ext.button.Split',
     arrowCls: ' isu-icon-cancel isu-button-close isu-icon-white',
-    iconAlign: 'right',
     alias: 'widget.button-tag'
 });
