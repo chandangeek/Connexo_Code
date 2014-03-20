c9041cb3a1074f5f8fe6b35432a6f8e72bf81907
Sergey <ssentyur@mera.ru>
2014-03-20 16:17:08 +0400

    JP-1532 "In filter summary , filters must be deleted by clicking "x" - close icon": bug fix

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/issues/component/TagButton.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/workspace/issues/component/TagButton.js
@@ -1,6 +1,6 @@
 Ext.define('Isu.view.workspace.issues.component.TagButton', {
-    extend: 'Ext.button.Button',
-    iconCls: 'isu-icon-white isu-icon-cancel',
+    extend: 'Ext.button.Split',
+    arrowCls: ' isu-icon-cancel isu-button-close isu-icon-white',
     iconAlign: 'right',
     alias: 'widget.button-tag'
 });
