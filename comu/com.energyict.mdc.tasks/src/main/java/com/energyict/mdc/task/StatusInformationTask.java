fc81b57add27c48cfbc5f870b38b36d179800f41
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-03-21 14:04:39 +0100

    JP-1638: Moved StatusInformationTask

---
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/task/StatusInformationTask.java
@@ -0,0 +1,12 @@
+package com.energyict.mdc.task;
+
+import com.energyict.mdc.protocol.api.device.Device;
+
+/**
+ * Models the {@link com.energyict.mdc.task.ProtocolTask} to update the Status information of a {@link Device}.
+ *
+ * @author gna
+ * @since 19/04/12 - 16:17
+ */
+public interface StatusInformationTask extends ProtocolTask {
+}
