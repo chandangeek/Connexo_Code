8260527d1f9a7c38d27d9cf034172ed135fc169a
sla <silvie.labis@elster.com>
2014-03-24 09:16:35 +0100

    JP-1662 Unable to navigate to second page of grid

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/services/ListPager.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/services/ListPager.java
@@ -42,8 +42,11 @@ public class ListPager<T> {
             if (start>=elements.size()) {
                 return Collections.emptyList();
             }
-            int limit = Math.min(elements.size()-this.start, this.start+this.pageSize+1);// +1 for the 'there is another page'-indicator
-            return elements.subList(this.start, limit + this.start);
+            int toIndex = this.start + this.pageSize + 1;
+            if (toIndex > elements.size()) {
+                toIndex = elements.size();
+            }
+            return elements.subList(this.start, toIndex);
         } else {
             return elements;
         }
