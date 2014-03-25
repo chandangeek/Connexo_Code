e7e90f5f5ebe17b95a600d9a6adc0b2cfc2a39fd
Artem Puchenkin <apuchenkin@gmail.com>
2014-03-25 19:03:58 +0400

    JP-1731 integration

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/controller/Main.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/controller/Main.js
@@ -9,9 +9,24 @@ Ext.define('Isu.controller.Main', {
     ],
 
     controllers: [
-        'Uni.controller.Navigation',
-        'Uni.controller.Configuration',
-        'Isu.controller.history.Workspace'
+        'Isu.controller.Main',
+        'Isu.controller.Workspace',
+        'Isu.controller.DataCollection',
+        'Isu.controller.Issues',
+        'Isu.controller.AssignIssues',
+        'Isu.controller.CloseIssues',
+        'Isu.controller.BulkChangeIssues',
+        'Isu.controller.MessageWindow',
+        'Isu.controller.IssueAssignmentRules',
+        'Isu.controller.IssueCreationRules',
+        'Isu.controller.Licensing',
+        'Isu.controller.AddLicense',
+        'Isu.controller.history.Workspace',
+        'Isu.controller.IssueFilter',
+        'Isu.controller.IssueDetail',
+        'Isu.controller.history.Administration',
+        'Isu.controller.Administration',
+        'Isu.controller.AdministrationDataCollection'
     ],
 
     config: {
@@ -31,10 +46,28 @@ Ext.define('Isu.controller.Main', {
     ],
 
     init: function () {
-        console.log('Isu.controller.Main - init');
+        this.initMenu();
         this.initNavigation();
         this.initDefaultHistoryToken();
         this.getApplication().on('changecontentevent', this.showContent, this);
+    },
+
+    initMenu: function () {
+        var menuItem = Ext.create('Uni.model.MenuItem', {
+            text: 'Workspace',
+            href: '#/workspace',
+            glyph: 'xe01e@icomoon'
+        });
+
+        Uni.store.MenuItems.add(menuItem);
+
+        var menuItem = Ext.create('Uni.model.MenuItem', {
+            text: 'Administration',
+            href: '#/administration',
+            glyph: 'xe011@icomoon'
+        });
+
+        Uni.store.MenuItems.add(menuItem);
     },
 
     initNavigation: function () {
