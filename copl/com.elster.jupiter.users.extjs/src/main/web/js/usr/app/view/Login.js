e092aa7ac1ae0e291f91eff0e65bdb7d7c5da56b
adrianlupan <adrianlupan@elster.com>
2014-03-26 16:27:56 +0200

    changed login form style

--- copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/app/view/Login.js
+++ copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/app/view/Login.js
@@ -11,6 +11,7 @@ Ext.define('Usr.view.Login', {
     ],
 
     id: 'usm_elster_login',
+    cls:'uni-content-container uni-content-container-default',
     layout: {
         type: 'vbox',
         align: 'center',
@@ -23,93 +24,99 @@ Ext.define('Usr.view.Login', {
         Ext.applyIf(me, {
             items: [
                 {
-                    xtype: 'form',
-                    height: 250,
-                    width: 400,
-                    itemId: 'contentPanel',
-                    title: 'Welcome to Jupiter',
-                    bodyPadding: 10,
-                    layout: {
-                        type: 'hbox',
-                        align: 'stretch'
-                    },
-                    items: [
-                        {
-                            xtype: 'container',
-                            height: 131,
-                            maxWidth: 150,
-                            width: 342,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch',
-                                pack: 'center',
-                                padding: 20
-                            },
-                            items: [
-                                {
-                                    xtype: 'image',
-                                    src: 'resources/images/user_icon.jpg',
-                                    height: 125,
-                                    width: 125
-                                }
-                            ]
+                xtype: 'form',
+                height: 250,
+                width: 400,
+                border: 1,
+                style: {
+                    borderColor: 'lightgray',
+                    borderStyle: 'solid'
+                },
+                itemId: 'contentPanel',
+                title: 'Welcome to Jupiter',
+                bodyPadding: 10,
+                layout: {
+                    type: 'hbox',
+                    align: 'stretch'
+                },
+                items: [
+                    {
+                        xtype: 'container',
+                        height: 131,
+                        maxWidth: 150,
+                        width: 342,
+                        layout: {
+                            type: 'vbox',
+                            align: 'stretch',
+                            pack: 'center',
+                            padding: 20
                         },
-                        {
-                            xtype: 'container',
-                            flex: 3,
-                            maxWidth: 280,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch',
-                                pack: 'center',
-                                padding: 20
+                        items: [
+                            {
+                                xtype: 'image',
+                                src: 'resources/images/user_icon.jpg',
+                                height: 115,
+                                width: 115
+                            }
+                        ]
+                    },
+                    {
+                        xtype: 'container',
+                        flex: 3,
+                        maxWidth: 280,
+                        layout: {
+                            type: 'vbox',
+                            align: 'stretch',
+                            pack: 'center',
+                            padding: 20
+                        },
+                        items: [
+                            {
+                                xtype: 'label',
+                                text: 'User name'
                             },
-                            items: [
-                                {
-                                    xtype: 'label',
-                                    text: 'User name'
+                            {
+                                xtype: 'textfield',
+                                fieldLabel: '',
+                                name: 'username',
+                                itemId: 'username',
+                                allowBlank: true
+                            },
+                            {
+                                xtype: 'label',
+                                margins: '10 0 0 0',
+                                text: 'Password'
+                            },
+                            {
+                                xtype: 'textfield',
+                                fieldLabel: '',
+                                name: 'password',
+                                itemId: 'password',
+                                inputType: 'password',
+                                allowBlank: true
+                            },
+                            {
+                                xtype: 'button',
+                                itemId: 'loginButton',
+                                listeners: {
+                                    click: function(){
+                                        var username = this.up('container').down('#username').getValue();
+                                        var password = this.up('container').down('#password').getValue();
+                                        this.fireEvent('signin', this, username, password);
+                                    }
                                 },
-                                {
-                                    xtype: 'textfield',
-                                    fieldLabel: '',
-                                    name: 'username',
-                                    itemId: 'username',
-                                    allowBlank: true
-                                },
-                                {
-                                    xtype: 'label',
-                                    margins: '10 0 0 0',
-                                    text: 'Password'
-                                },
-                                {
-                                    xtype: 'textfield',
-                                    fieldLabel: '',
-                                    name: 'password',
-                                    itemId: 'password',
-                                    inputType: 'password',
-                                    allowBlank: true
-                                },
-                                {
-                                    xtype: 'button',
-                                    itemId: 'loginButton',
-                                    listeners: {
-                                        click: function(){
-                                            var username = this.up('container').down('#username').getValue();
-                                            var password = this.up('container').down('#password').getValue();
-                                            this.fireEvent('signin', this, username, password);
-                                        }
-                                    },
-                                    formBind: true,
-                                    margin: '20 0 0 0 ',
-                                    maxWidth: 80,
-                                    text: 'Login'
+                                formBind: true,
+                                margin: '20 0 0 0 ',
+                                maxWidth: 80,
+                                text: 'Login'
 
-                                }
-                            ]
-                        }
-                    ]
+                            }
+                        ]
+                    }
+                ]
                 }
             ]
+
         });
 
         me.callParent(arguments);
