3bea2184eaaf08d9ae940c3a7b777b5e72af2e9f
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-03-26 10:32:02 +0100

    Added test to cover incomplete second page

--- comu/com.energyict.mdc.common/src/test/java/com/energyict/mdc/common/rest/ListFinderTest.java
+++ comu/com.energyict.mdc.common/src/test/java/com/energyict/mdc/common/rest/ListFinderTest.java
@@ -47,6 +47,16 @@ public class ListFinderTest {
     }
 
     @Test
+    public void testGetIncompleteSecondPage() throws Exception {
+        List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12);
+        QueryParameters queryParameters = mock(QueryParameters.class);
+        when(queryParameters.getStart()).thenReturn(10);
+        when(queryParameters.getLimit()).thenReturn(10);
+        List<Integer> found = ListPager.of(ints, new NullComparator()).from(queryParameters).find();
+        assertThat(found).containsExactly(11,12);
+    }
+
+    @Test
     public void testGetOutOfBoundsPage() throws Exception {
         List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7);
         QueryParameters queryParameters = mock(QueryParameters.class);
