0b03f488e1a8fb3c1238e392aa9970044443b056
Isabelle  Gheysens <isabelle.gheysens@elster.com>
2014-03-27 16:17:57 +0100

    removed empty grid container for demo

--- coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/app/view/validation/RuleBrowse.js
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/app/view/validation/RuleBrowse.js
@@ -7,8 +7,7 @@ Ext.define('Cfg.view.validation.RuleBrowse', {
     requires: [
         'Cfg.view.validation.RuleList',
         'Cfg.view.validation.RulePreview',
-        'Uni.view.breadcrumb.Trail',
-        'Uni.view.container.EmptyGridContainer'
+        'Uni.view.breadcrumb.Trail'
     ],
 
     ruleSetId: null,
@@ -41,7 +40,7 @@ Ext.define('Cfg.view.validation.RuleBrowse', {
                 },
                 {
                     xtype: 'component',
-                    height: 50
+                    height : 50
                 },
                 {
                     xtype: 'rulePreview'
@@ -54,48 +53,9 @@ Ext.define('Cfg.view.validation.RuleBrowse', {
         this.callParent(arguments);
         this.down('#validationruleListContainer').add(
             {
-                xtype: 'emptygridcontainer',
-                grid: {
-                    xtype: 'validationruleList',
-                    ruleSetId: this.ruleSetId
-                },
-                emptyComponent: {
-                    xtype: 'container',
-                    layout: 'vbox',
-                    items: [
-                        {
-                            xtype: 'component',
-                            html: '<h3>No rules found</h3>'
-                        },
-                        {
-                            xtype: 'component',
-                            html: '<p>Sorry there are no rules configured. This could be because:</p>' +
-                                '<ul>' +
-                                '<li>No rules have been created yet</li>' +
-                                '<li>The filter is too narrow</li>' +
-                                '</ul>'
-                        },
-                        {
-                            xtype: 'component',
-                            html: '<p>Possible steps:</p>'
-                        },
-                        {
-                            xtype: 'container',
-                            layout: 'hbox',
-                            items: [
-                                {
-                                    xtype: 'button',
-                                    text: Uni.I18n.translate('validation.addRule', 'CFG', 'Add rule'),
-                                    itemId: 'addRuleLink',
-                                    href: '#administration/validation/addRule/' + this.ruleSetId,
-                                    hrefTarget: '_self'
-                                }
-                            ]
-                        }
-                    ]
-                }
+                xtype: 'validationruleList',
+                ruleSetId: this.ruleSetId
             }
         );
     }
 });
-
