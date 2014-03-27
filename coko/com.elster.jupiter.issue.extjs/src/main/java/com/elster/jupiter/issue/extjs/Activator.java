ab83b6f7d0e6fadfd03732b05dd14155b872c3c2
Artem Puchenkin <apuchenkin@gmail.com>
2014-03-27 17:50:36 +0400

    paths and deps

--- coko/com.elster.jupiter.issue.extjs/src/main/java/com/elster/jupiter/issue/extjs/Activator.java
+++ coko/com.elster.jupiter.issue.extjs/src/main/java/com/elster/jupiter/issue/extjs/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.ServiceRegistration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.HashMap;
 
 public class Activator implements BundleActivator {
 	public static final String HTTP_RESOURCE_ALIAS = "/issue";
@@ -16,8 +16,20 @@ public class Activator implements BundleActivator {
 	
 	public void start(BundleContext bundleContext) throws Exception {
 		List<Script> scripts = new ArrayList<>();
+		Map<String, String> dependencies = new HashMap<>();
 		
-		DefaultStartPage issueStartPage = new DefaultStartPage("Isu", "", "/index.html", "Isu.controller.Main", scripts, Arrays.asList("ISU"));
+        dependencies.put("Ext.ux.Rixo", "/resources/js/Ext/ux/Rixo");
+		
+		DefaultStartPage issueStartPage = new DefaultStartPage(
+			"Isu", 
+			"",
+			"/index.html", 
+			"Isu.controller.Main", 
+			scripts, 
+			Arrays.asList("ISU")
+			Arrays.asList("/resources/css/isu.css"),
+            dependencies
+		);
         HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/issue" , new BundleResolver(bundleContext), issueStartPage);
         registration = bundleContext.registerService(HttpResource.class, resource , null);
 	}
