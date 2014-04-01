177c7ba27426c933e323d0a43dcc2b8fe340bf76
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-01 10:00:30 +0200

    JP-1266: Created specialized LocalizedFieldException for FieldValidation

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/exceptions/NoSuchProtocolException.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/exceptions/NoSuchProtocolException.java
@@ -1,6 +1,6 @@
 package com.energyict.mdc.device.config.exceptions;
 
-import com.elster.jupiter.nls.LocalizedException;
+import com.elster.jupiter.nls.LocalizedFieldValidationException;
 import com.elster.jupiter.nls.Thesaurus;
 
 /**
@@ -8,10 +8,9 @@ import com.elster.jupiter.nls.Thesaurus;
  * Date: 31/03/14
  * Time: 13:14
  */
-public class NoSuchProtocolException extends LocalizedException {
+public class NoSuchProtocolException extends LocalizedFieldValidationException {
     public NoSuchProtocolException(Thesaurus thesaurus, String name, String fieldName) {
-        super(thesaurus, MessageSeeds.PROTOCOL_INVALID_NAME, name);
+        super(thesaurus, MessageSeeds.PROTOCOL_INVALID_NAME, fieldName, name);
         this.set("name", name);
-        this.setViolatingProperty(fieldName);
     }
 }
