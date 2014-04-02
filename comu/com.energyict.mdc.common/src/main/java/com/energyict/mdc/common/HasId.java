9442f173e440de5c02d7f6f3b69387a4993d5347
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-02 15:08:11 +0200

    JP-1638: added tests for RegistersTask

---
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/HasId.java
@@ -0,0 +1,12 @@
+package com.energyict.mdc.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bvn
+ * Date: 4/2/14
+ * Time: 9:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface HasId {
+    public long getId();
+}
