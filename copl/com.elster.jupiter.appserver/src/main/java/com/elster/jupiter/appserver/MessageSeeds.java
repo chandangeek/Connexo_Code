bf9e3c9e9c6820df5512bcaa4a3938d5860cfa4f
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-07 09:54:49 +0200

    JP-1133: Replaced all single quotes in MessageSeeds with escaped double quotes.

--- copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/MessageSeeds.java
+++ copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/MessageSeeds.java
@@ -4,14 +4,13 @@ package com.elster.jupiter.appserver;
 import com.elster.jupiter.nls.NlsMessageFormat;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.util.exception.MessageSeed;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public enum MessageSeeds implements MessageSeed {
 
-    SERVER_MESSAGE_QUEUE_MISSING(1001, "server.messagequeue.missing", "Server's message queue with name ''{0}'' not found", Level.SEVERE),
-    APPSERVER_NAME_UNKNOWN(1002, "appserver.name.unknown", "AppServer with name ''{0}'' is unknown", Level.SEVERE),
+    SERVER_MESSAGE_QUEUE_MISSING(1001, "server.messagequeue.missing", "Server's message queue with name \"{0}\" not found", Level.SEVERE),
+    APPSERVER_NAME_UNKNOWN(1002, "appserver.name.unknown", "AppServer with name \"{0}\" is unknown", Level.SEVERE),
     APPSERVER_STARTED_ANONYMOUSLY(2001, "appserver.started.anonymously", "AppServer started anonymously.", Level.WARNING),
     THREAD_UNCAUGHT_EXCEPTION(2002, "thread.uncaught.exception", "Uncaught exception occurred on thread {0}", Level.SEVERE),
     MESSAGEHANDLER_FAILED(2003, "messagehandler.failed", "Message handler failed", Level.SEVERE);
