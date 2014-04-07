53e2bbc32b76e6df55244db2e5fa3686f4ed3053
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-07 09:54:49 +0200

    JP-1133: Replaced all single quotes in MessageSeeds with escaped double quotes.

--- copl/com.elster.jupiter.events/src/main/java/com/elster/jupiter/events/MessageSeeds.java
+++ copl/com.elster.jupiter.events/src/main/java/com/elster/jupiter/events/MessageSeeds.java
@@ -3,13 +3,12 @@ package com.elster.jupiter.events;
 import com.elster.jupiter.nls.NlsMessageFormat;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.util.exception.MessageSeed;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public enum MessageSeeds implements MessageSeed {
     CORRUPT_ACCESSPATH(1001, "accesspath.corrupt", "Access path corrupt.", Level.SEVERE),
-    INVALID_PROPERTY_TYPE(1002, "property.type.invalid", "Access path ''{0}'' on bean ''{1}'' was of type {2} while type {3} was expected.", Level.SEVERE),
+    INVALID_PROPERTY_TYPE(1002, "property.type.invalid", "Access path \"{0}\" on bean \"{1}\" was of type {2} while type {3} was expected.", Level.SEVERE),
     NO_SUCH_TOPIC(1003, "topic.doesnotexist", "Topic {0} does not exist.", Level.SEVERE);
 
     private final int number;
