eda212fd04c1bba904774cac724b38e67a33c0b4
Dieter Vyncke <dieter.vyncke@elster.com>
2014-04-08 13:43:12 +0200

    JP-1995 - Used register types should not appear in list on create page

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/store/AvailableRegisterTypesForDeviceConfiguration.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/store/AvailableRegisterTypesForDeviceConfiguration.js
@@ -1,10 +1,11 @@
-Ext.define('Mdc.store.AvailableRegisterTypes', {
+Ext.define('Mdc.store.AvailableRegisterTypesForDeviceConfiguration', {
     extend: 'Ext.data.Store',
     requires: [
         'Mdc.model.RegisterType'
     ],
+//    remoteFilter: true,
     model: 'Mdc.model.RegisterType',
-    storeId: 'AvailableRegisterTypes',
+    storeId: 'AvailableRegisterTypesForDeviceConfiguration',
    // buffered: true,
    // leadingBufferZone: 50,
    // pageSize: 20,
