ab64a10ac7bf3284365bd69f71e38c07c888fd0d
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-08 13:00:38 +0200

    JP-1995: renamed methods in ExtjsFilter

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/ResourceHelper.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/ResourceHelper.java
@@ -1,8 +1,13 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
+import com.energyict.mdc.common.HasId;
 import com.energyict.mdc.device.config.DeviceConfiguration;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.config.DeviceType;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
@@ -33,4 +38,54 @@ public class ResourceHelper {
         throw new WebApplicationException("No such device configuration for the device type", Response.status(Response.Status.NOT_FOUND).entity("No such device configuration for the device type").build());
     }
 
+    public Set<Long> asIds(Collection<? extends HasId> collection) {
+        HashSet<Long> ids = new HashSet<>();
+        if (collection!=null) {
+            for (HasId hasId : collection) {
+                ids.add(hasId.getId());
+            }
+        }
+        return ids;
+    }
+
+    public <T extends HasId> Difference<T> getDifference(Collection<T> existingObjects, Collection<T> desiredObjects) {
+        DifferenceImpl<T> difference = new DifferenceImpl<>();
+        Set<Long> existingObjectIds = asIds(existingObjects);
+        difference.toBeCreated = new HashSet<>(desiredObjects);
+        for (T desiredObject : desiredObjects) {
+            if (existingObjectIds.contains(desiredObject.getId())) {
+                difference.toBeCreated.remove(desiredObject);
+            }
+        }
+
+        Set<Long> desiredObjectIds = asIds(desiredObjects);
+        difference.toBeDeleted = new HashSet<>(existingObjects);
+        for (T existingObject : existingObjects) {
+            if (desiredObjectIds.contains(existingObject.getId())) {
+                difference.toBeDeleted.remove(existingObject);
+            }
+        }
+
+        return difference;
+    }
+
+    static interface Difference<T extends HasId> {
+        public Set<T> toBeCreated();
+        public Set<T> toBeDeleted();
+    }
+
+    class DifferenceImpl<T extends HasId> implements Difference<T> {
+        Set<T> toBeCreated = new HashSet<>();
+        Set<T> toBeDeleted = new HashSet<>();
+
+        @Override
+        public Set<T> toBeCreated() {
+            return Collections.unmodifiableSet(toBeCreated);
+        }
+
+        @Override
+        public Set<T> toBeDeleted() {
+            return Collections.unmodifiableSet(toBeDeleted);
+        }
+    }
 }
