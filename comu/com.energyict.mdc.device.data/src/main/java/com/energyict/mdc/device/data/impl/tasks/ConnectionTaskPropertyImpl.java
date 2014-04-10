efeca8e027f18fb3259fbf14867edfd85fcc7fa7
govanni <govanni.naessens@elster.com>
2014-04-10 15:42:35 +0200

    JP-1122 - Revise persistence Device ... Removed the PluggableClassUsagePropertyImpl. Pulled down the methods to the objects in the respective bundles.

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/tasks/ConnectionTaskPropertyImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/tasks/ConnectionTaskPropertyImpl.java
@@ -3,8 +3,6 @@ package com.energyict.mdc.device.data.impl.tasks;
 import com.elster.jupiter.util.time.Interval;
 import com.energyict.mdc.device.data.tasks.ConnectionTaskProperty;
 import com.energyict.mdc.dynamic.relation.Relation;
-import com.energyict.mdc.pluggable.impl.PluggableClassUsagePropertyImpl;
-import com.energyict.mdc.protocol.api.ConnectionType;
 import com.energyict.mdc.pluggable.PluggableClass;
 
 /**
@@ -13,18 +11,86 @@ import com.energyict.mdc.pluggable.PluggableClass;
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2012-06-04 (09:05)
  */
-public class ConnectionTaskPropertyImpl extends PluggableClassUsagePropertyImpl<ConnectionType> implements ConnectionTaskProperty {
+public class ConnectionTaskPropertyImpl implements ConnectionTaskProperty {
+
+    private PluggableClass pluggableClass;
+    private String name;
+    private Object value;
+    private Interval activePeriod;
+    private boolean inherited;
 
     public ConnectionTaskPropertyImpl (String name) {
-        super(name);
+        this.name = name;
     }
 
     public ConnectionTaskPropertyImpl (Relation relation, String name, PluggableClass pluggableClass) {
-        super(relation, name, pluggableClass);
+        this(name, relation.get(name), relation.getPeriod(), pluggableClass, false);
     }
 
     public ConnectionTaskPropertyImpl (String name, Object value, Interval activePeriod, PluggableClass pluggableClass) {
-        super(name, value, activePeriod, pluggableClass, true);
+        this(name, value, activePeriod, pluggableClass, true);
     }
 
+    public ConnectionTaskPropertyImpl(String name, Object value, Interval activePeriod, PluggableClass pluggableClass, boolean inherited) {
+        this(name);
+        this.value = value;
+        this.activePeriod = activePeriod;
+        this.pluggableClass = pluggableClass;
+        this.inherited = inherited;
+    }
+
+    @Override
+    public PluggableClass getPluggableClass () {
+        return this.pluggableClass;
+    }
+
+    @Override
+    public String getName () {
+        return this.name;
+    }
+
+    @Override
+    public Object getValue () {
+        return this.value;
+    }
+
+    public void setValue (Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean isInherited () {
+        return inherited;
+    }
+
+    @Override
+    public Interval getActivePeriod () {
+        return this.activePeriod;
+    }
+
+    public void setActivePeriod (Interval activePeriod) {
+        this.activePeriod = activePeriod;
+    }
+
+    @Override
+    public String toString () {
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.getClass().getSimpleName());
+        builder.append('(');
+        builder.append(this.name);
+        builder.append(':');
+        builder.append(String.valueOf(this.value));
+        if (this.inherited) {
+            builder.append(", inherited");
+        }
+        if ((this.activePeriod.getStart() == null) && (this.activePeriod.getEnd() == null)) {
+            builder.append(", always active");
+        }
+        else {
+            builder.append(", active ");
+            builder.append(String.valueOf(this.activePeriod));
+        }
+        builder.append(')');
+        return builder.toString();
+    }
 }
