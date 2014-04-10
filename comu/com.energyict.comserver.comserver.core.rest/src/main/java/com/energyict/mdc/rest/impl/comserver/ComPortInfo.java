9ad8d4aecf17f62fd9c1d856fcaea216dff96f54
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-10 16:04:35 +0200

    JP-2183: remove extraneous JsonProperty annotations

--- comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/comserver/ComPortInfo.java
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/comserver/ComPortInfo.java
@@ -16,82 +16,49 @@ import java.util.List;
 import java.util.Map;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import org.codehaus.jackson.annotate.JsonProperty;
 
 @XmlRootElement
 public abstract class ComPortInfo<T extends ComPort, B extends ComPort.Builder<B,T>> {
 
     public long id;
-    @JsonProperty("direction")
     public String direction;
-    @JsonProperty("name")
     public String name;
-    @JsonProperty("description")
     public String description;
-    @JsonProperty("active")
     public boolean active;
-    @JsonProperty("bound")
     public boolean bound;
-    @JsonProperty("comPortType")
     @XmlJavaTypeAdapter(ComPortTypeAdapter.class)
     public ComPortType comPortType;
-    @JsonProperty("comServer_id")
     public long comServer_id;
-    @JsonProperty("numberOfSimultaneousConnections")
     public int numberOfSimultaneousConnections;
     public Date modificationDate;
-    @JsonProperty("ringCount")
     public Integer ringCount;
-    @JsonProperty("maximumNumberOfDialErrors")
     public Integer maximumNumberOfDialErrors;
-    @JsonProperty("connectTimeout")
     public TimeDurationInfo connectTimeout;
-    @JsonProperty("delayAfterConnect")
     public TimeDurationInfo delayAfterConnect;
-    @JsonProperty("delayBeforeSend")
     public TimeDurationInfo delayBeforeSend;
-    @JsonProperty("atCommandTimeout")
     public TimeDurationInfo atCommandTimeout;
-    @JsonProperty("atCommandTry")
     public BigDecimal atCommandTry;
-    @JsonProperty("modemInitStrings")
     public List<Map<String, String>> modemInitStrings;
-    @JsonProperty("addressSelector")
     public String addressSelector;
-    @JsonProperty("postDialCommands")
     public String postDialCommands;
-    @JsonProperty("baudrate")
     @XmlJavaTypeAdapter(BaudrateAdapter.class)
     public BaudrateValue baudrate;
-    @JsonProperty("nrOfDataBits")
     @XmlJavaTypeAdapter(NrOfDataBitsAdapter.class)
     public NrOfDataBits nrOfDataBits;
-    @JsonProperty("nrOfStopBits")
     @XmlJavaTypeAdapter(NrOfStopBitsAdapter.class)
     public NrOfStopBits nrOfStopBits;
-    @JsonProperty("flowControl")
     @XmlJavaTypeAdapter(value = FlowControlAdapter.class)
     public FlowControl flowControl;
-    @JsonProperty("parity")
     @XmlJavaTypeAdapter(ParitiesAdapter.class)
     public Parities parity;
-    @JsonProperty("comPortPool_id")
     public Long comPortPool_id;
-    @JsonProperty("portNumber")
     public Integer portNumber;
-    @JsonProperty("bufferSize")
     public Integer bufferSize;
-    @JsonProperty("useHttps")
     public Boolean useHttps;
-    @JsonProperty("keyStoreFilePath")
     public String keyStoreFilePath;
-    @JsonProperty("trustStoreFilePath")
     public String trustStoreFilePath;
-    @JsonProperty("keyStorePassword")
     public String keyStorePassword;
-    @JsonProperty("trustStorePassword")
     public String trustStorePassword;
-    @JsonProperty("contextPath")
     public String contextPath;
 
     public ComPortInfo() {
