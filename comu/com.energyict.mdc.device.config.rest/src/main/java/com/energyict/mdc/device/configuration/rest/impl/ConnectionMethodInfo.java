8c65206f824bd4c56dc6dcd24d406b8e9998e4c8
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-11 15:35:05 +0200

    JP-2183: Moved ConnectionMethodResource to device.config.rest

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/ConnectionMethodInfo.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/ConnectionMethodInfo.java
@@ -1,26 +1,49 @@
 package com.energyict.mdc.device.configuration.rest.impl;
 
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.energyict.mdc.common.TypedProperties;
+import com.energyict.mdc.device.config.PartialConnectionTask;
+import com.energyict.mdc.device.config.PartialInboundConnectionTask;
+import com.energyict.mdc.device.config.PartialOutboundConnectionTask;
+import com.energyict.mdc.dynamic.PropertySpec;
+import com.energyict.mdc.pluggable.rest.MdcPropertyUtils;
+import com.energyict.mdc.pluggable.rest.PropertyInfo;
+import com.google.common.base.Joiner;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.UriInfo;
 
 /**
- * Copyrights EnergyICT
- * Date: 1/04/14
- * Time: 10:43
+ * This Info element represents the PartialConnectionTask in the domain model
  */
 public class ConnectionMethodInfo {
-    @JsonProperty("id")
     public long id;
-    @JsonProperty("name")
-    public String name;
-
+    public String direction;
+    public String connectionType;
+    public List<PropertyInfo> propertyInfos;
 
     public ConnectionMethodInfo() {
     }
 
-    public static ConnectionMethodInfo from(long id, String name) {
+    public static ConnectionMethodInfo from(PartialConnectionTask partialConnectionTask, UriInfo uriInfo) {
         ConnectionMethodInfo connectionMethodInfo = new ConnectionMethodInfo();
-        connectionMethodInfo.id = id;
-        connectionMethodInfo.name = name;
+        connectionMethodInfo.id=partialConnectionTask.getId();
+        connectionMethodInfo.direction=determineDirection(partialConnectionTask);
+        connectionMethodInfo.connectionType= Joiner.on(',').join(partialConnectionTask.getConnectionType().getSupportedComPortTypes());
+        List<PropertySpec> propertySpecs = partialConnectionTask.getConnectionType().getPropertySpecs();
+        TypedProperties typedProperties = partialConnectionTask.getTypedProperties();
+        List<PropertyInfo> propertyInfoList = new ArrayList<>();
+        MdcPropertyUtils.convertPropertySpecsToPropertyInfos(uriInfo, propertySpecs, typedProperties, propertyInfoList);
+        connectionMethodInfo.propertyInfos= propertyInfoList;
         return connectionMethodInfo;
+    }
+
+    private static String determineDirection(PartialConnectionTask partialConnectionTask) {
+        if (partialConnectionTask instanceof PartialInboundConnectionTask) {
+            return "Inbound";
+        } else if (partialConnectionTask instanceof PartialOutboundConnectionTask) {
+            return "Outbound";
+        } else {
+            return "Scheduled";
+        }
     }
 }
--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/ConnectionMethodResource.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/ConnectionMethodResource.java
@@ -2,19 +2,23 @@ package com.energyict.mdc.device.configuration.rest.impl;
 
 import com.energyict.mdc.common.rest.PagedInfoList;
 import com.energyict.mdc.common.rest.QueryParameters;
+import com.energyict.mdc.common.services.ListPager;
 import com.energyict.mdc.device.config.DeviceConfiguration;
-import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.config.DeviceType;
-
+import com.energyict.mdc.device.config.PartialConnectionTask;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Copyrights EnergyICT
@@ -22,32 +26,37 @@ import java.util.List;
  * Time: 8:59
  */
 public class ConnectionMethodResource {
-    private final DeviceConfigurationService deviceConfigurationService;
     private final ResourceHelper resourceHelper;
 
     @Inject
-    public ConnectionMethodResource(DeviceConfigurationService deviceConfigurationService, ResourceHelper resourceHelper) {
-        this.deviceConfigurationService = deviceConfigurationService;
+    public ConnectionMethodResource(ResourceHelper resourceHelper) {
         this.resourceHelper = resourceHelper;
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public PagedInfoList getConnectionMethods(@PathParam("deviceTypeId") long deviceTypeId, @PathParam("deviceConfigurationId") long deviceConfigurationId, @BeanParam QueryParameters queryParameters) {
+    public PagedInfoList getConnectionMethods(@PathParam("deviceTypeId") long deviceTypeId, @PathParam("deviceConfigurationId") long deviceConfigurationId, @BeanParam QueryParameters queryParameters, @Context UriInfo uriInfo) {
         DeviceType deviceType = resourceHelper.findDeviceTypeByIdOrThrowException(deviceTypeId);
         DeviceConfiguration deviceConfiguration = resourceHelper.findDeviceConfigurationForDeviceTypeOrThrowException(deviceType, deviceConfigurationId);
-//        List<Connection> pagedRegisterSpecs = ListPager.of(deviceConfiguration.getRegisterSpecs(), new RegisterConfigurationComparator()).from(queryParameters).find();
         List<ConnectionMethodInfo> connectionMethodInfos = new ArrayList<>();
-        connectionMethodInfos.add(ConnectionMethodInfo.from(1, "aaaaaaaaa"));
-        connectionMethodInfos.add(ConnectionMethodInfo.from(2, "bbbbbbbbb"));
-//                RegisterConfigInfo.from(pagedRegisterSpecs);
-        return PagedInfoList.asJson("connectionmethods", connectionMethodInfos, queryParameters);
+        for (PartialConnectionTask partialConnectionTask : deviceConfiguration.getPartialConnectionTasks()) {
+            connectionMethodInfos.add(ConnectionMethodInfo.from(partialConnectionTask, uriInfo));
+        }
+        List<ConnectionMethodInfo> pagedConnectionMethodInfos = ListPager.of(connectionMethodInfos).from(queryParameters).find();
+        return PagedInfoList.asJson("connectionMethods", pagedConnectionMethodInfos, queryParameters);
     }
 
     @GET
     @Path("/{connectionMethodId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ConnectionMethodInfo getConnectionMethods(@PathParam("deviceTypeId") long deviceTypeId, @PathParam("deviceConfigurationId") long deviceConfigurationId, @PathParam("connectionMethodId") long connectionMethodId) {
-        return ConnectionMethodInfo.from(1, "aaaaaaaaa");
+    public ConnectionMethodInfo getConnectionMethods(@PathParam("deviceTypeId") long deviceTypeId, @PathParam("deviceConfigurationId") long deviceConfigurationId, @PathParam("connectionMethodId") long connectionMethodId, @Context UriInfo uriInfo) {
+        DeviceType deviceType = resourceHelper.findDeviceTypeByIdOrThrowException(deviceTypeId);
+        DeviceConfiguration deviceConfiguration = resourceHelper.findDeviceConfigurationForDeviceTypeOrThrowException(deviceType, deviceConfigurationId);
+        for (PartialConnectionTask partialConnectionTask : deviceConfiguration.getPartialConnectionTasks()) {
+            if (partialConnectionTask.getId()==connectionMethodId) {
+                return ConnectionMethodInfo.from(partialConnectionTask, uriInfo);
+            }
+        }
+        throw new WebApplicationException(Response.Status.NOT_FOUND);
     }
 }
