7c4739c7eb27d72533cf2a3fdd2063a9ec177895
Kristof Hennebel <Kristof.Hennebel@elster.com>
2014-04-16 09:35:33 +0000

    COMMUNICATION-999 - Migrate EIWebPlus servlet from 8.X to 9.1
    Added the EIWebPlus connection type that is basically a placeholder used for the concentrator device in EIServer.
    It has one property "ipAddress" that is filled in by the EIWebPlus servlet itself with the IP address of the posting concentrator.
    
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@82627 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/connectiontypes/src/main/java/com/energyict/mdc/channels/inbound/EIWebPlusConnectionType.java
@@ -0,0 +1,100 @@
+package com.energyict.mdc.channels.inbound;
+
+import com.energyict.cpo.*;
+import com.energyict.mdc.ports.ComPort;
+import com.energyict.mdc.ports.ComPortType;
+import com.energyict.mdc.protocol.ComChannel;
+import com.energyict.mdc.protocol.ConnectionException;
+import com.energyict.mdc.tasks.ConnectionTaskProperty;
+import com.energyict.mdc.tasks.ConnectionType;
+
+import java.util.*;
+
+/**
+ * Specific ConnectionType used for the EIWeb plus Protocol
+ * <p/>
+ * Copyrights EnergyICT
+ * Date: 13/12/12
+ * Time: 15:46
+ */
+public class EIWebPlusConnectionType implements ConnectionType {
+
+    private TypedProperties properties = TypedProperties.empty();
+
+    public static final String IP_ADDRESS_PROPERTY_NAME = "ipAddress";
+
+    private PropertySpec ipAddressPropertySpec() {
+        return PropertySpecFactory.stringPropertySpec(IP_ADDRESS_PROPERTY_NAME);
+    }
+
+    protected TypedProperties getAllProperties() {
+        return this.properties;
+    }
+
+    protected Object getProperty(String propertyName) {
+        return this.getAllProperties().getProperty(propertyName);
+    }
+
+    public String ipAddressValue() {
+        return (String) this.getProperty(IP_ADDRESS_PROPERTY_NAME);
+    }
+
+    @Override
+    public boolean allowsSimultaneousConnections() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsComWindow() {
+        return true;
+    }
+
+    @Override
+    public Set<ComPortType> getSupportedComPortTypes() {
+        return EnumSet.of(ComPortType.TCP);
+    }
+
+    @Override
+    public ComChannel connect(ComPort comPort, List<ConnectionTaskProperty> properties) throws ConnectionException {
+        throw new UnsupportedOperationException("Calling connect is not allowed on an EIWebPlusConnectionType");
+    }
+
+    @Override
+    public void disconnect(ComChannel comChannel) throws ConnectionException {
+    }
+
+    @Override
+    public PropertySpec getPropertySpec(String name) {
+        switch (name) {
+            case IP_ADDRESS_PROPERTY_NAME:
+                return this.ipAddressPropertySpec();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public boolean isRequiredProperty(String name) {
+        return false;
+    }
+
+    @Override
+    public String getVersion() {
+        return "$Date$";
+    }
+
+    @Override
+    public void addProperties(TypedProperties properties) {
+        this.properties = TypedProperties.copyOf(properties);
+    }
+
+    @Override
+    public List<PropertySpec> getRequiredProperties() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<PropertySpec> getOptionalProperties() {
+        return Arrays.asList(this.ipAddressPropertySpec());
+    }
+}
