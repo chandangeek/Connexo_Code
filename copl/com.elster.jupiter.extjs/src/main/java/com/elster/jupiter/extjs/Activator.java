64203d805169de82074421b1250a4802f2e6584c
Artem Puchenkin <apuchenkin@gmail.com>
2014-04-16 15:03:09 +0400

    JP-2313 Branding of data collection issue page

--- copl/com.elster.jupiter.extjs/src/main/java/com/elster/jupiter/extjs/Activator.java
+++ copl/com.elster.jupiter.extjs/src/main/java/com/elster/jupiter/extjs/Activator.java
@@ -1,15 +1,41 @@
 package com.elster.jupiter.extjs;
 
 import org.osgi.framework.*;
+
 import com.elster.jupiter.http.whiteboard.*;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Activator implements BundleActivator {
-
-	private volatile ServiceRegistration<HttpResource> registration;
+    public static final String HTTP_RESOURCE_ALIAS = "/ext";
+    private volatile ServiceRegistration<HttpResource> registration;
 	
 	public void start(BundleContext bundleContext) throws Exception {
-		HttpResource resource = new HttpResource("/ext", "/js/ext" , new BundleResolver(bundleContext));
-		registration = bundleContext.registerService(HttpResource.class, resource , null);
+	        List<Script> scripts = new ArrayList<>();
+    		Map<String, String> dependencies = new HashMap<>();
+
+            dependencies.put("Skyline", "/packages/uni-theme-skyline/src");
+
+    		DefaultStartPage startPage = new DefaultStartPage(
+    			"Ext",
+    			"",
+    			"",
+    			"",
+    			scripts,
+    			null,
+    			null,
+                dependencies
+    		);
+
+		HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/ext" , new BundleResolver(bundleContext), startPage);
+		registration = bundleContext.registerService(HttpResource.class, resource, null);
 	}
 	
 	public void stop(BundleContext bundleContext) throws Exception {
