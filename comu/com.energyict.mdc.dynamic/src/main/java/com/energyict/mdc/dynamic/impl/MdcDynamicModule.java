b31d8fbb7e59f0941138fd4bc2ca39a7db7bd5f2
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-17 14:57:01 +0200

    JP-2187: Fixed setting Scheduling specific properties

--- comu/com.energyict.mdc.dynamic/src/main/java/com/energyict/mdc/dynamic/impl/MdcDynamicModule.java
+++ comu/com.energyict.mdc.dynamic/src/main/java/com/energyict/mdc/dynamic/impl/MdcDynamicModule.java
@@ -1,17 +1,11 @@
 package com.energyict.mdc.dynamic.impl;
 
 import com.elster.jupiter.orm.OrmService;
-import com.elster.jupiter.security.thread.ThreadPrincipalService;
-import com.elster.jupiter.transaction.TransactionService;
-import com.energyict.mdc.common.Environment;
-import com.energyict.mdc.common.impl.EnvironmentImpl;
 import com.energyict.mdc.dynamic.PropertySpecService;
 import com.energyict.mdc.dynamic.relation.RelationService;
 import com.energyict.mdc.dynamic.relation.impl.RelationServiceImpl;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-
-import javax.sql.DataSource;
 
 /**
  * Insert your comments here.
