1a53dc39cc3c2801d5bea78c90105749df9aae27
Artem Puchenkin <apuchenkin@gmail.com>
2014-04-17 16:46:34 +0400

    JP-2313 Branding of data collection issue page. Issue details page refactoring

--- copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/view/Table.js
+++ copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/view/Table.js
@@ -5,20 +5,20 @@ Ext.define('Skyline.view.Table', {
 
     // todo: refactor this
     listeners: {
-        refresh: function () {
-            var body = this.getEl().parent('.x-grid-body');
-            body.update(body.getHTML() + this.scrollbarTpl);
-            if (body.down('.x-grid-view')) {
-                body.down('.x-grid-view').addCls('viewport');
-
-                this.getEl().down('.x-grid-table').addCls('overview');
-
-                var $scrollbar = body.dom,
-                    scrollbar  = tinyscrollbar($scrollbar, {
-                        trackSize: body.down('.track').dom.offsetHeight
-                    });
-            }
-
-        }
+//        refresh: function () {
+//            var body = this.getEl().parent('.x-grid-body');
+//            body.update(body.getHTML() + this.scrollbarTpl);
+//            if (body.down('.x-grid-view')) {
+//                body.down('.x-grid-view').addCls('viewport');
+//
+//                this.getEl().down('.x-grid-table').addCls('overview');
+//
+//                var $scrollbar = body.dom,
+//                    scrollbar  = tinyscrollbar($scrollbar, {
+//                        trackSize: body.down('.track').dom.offsetHeight
+//                    });
+//            }
+//
+//        }
     }
 });
