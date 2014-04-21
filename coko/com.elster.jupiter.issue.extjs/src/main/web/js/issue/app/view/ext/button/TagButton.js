c33d4dece2e27dbb1cb03259e7830ee8882ae04d
Kuryukin Andrey <akuryuk@mail.ru>
2014-04-21 17:53:32 +0400

    JP-2428 Defined new paddings

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/ext/button/TagButton.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/ext/button/TagButton.js
@@ -1,21 +1,22 @@
 Ext.define('Isu.view.ext.button.TagButton', {
     extend: 'Ext.button.Split',
     alias: 'widget.tag-button',
-    width: 150,
     split: true,
     menu: {},
-//    ui: 'tag',
+    ui: 'tag',
     arrowCls: null,
     afterRender: function () {
         var me = this,
             btnEl = this.getEl(),
             baseSpan = btnEl.first(),
+            textSpan = baseSpan.first().first(),
             closeIcon = baseSpan.createChild({
                 tag: 'span',
                 cls: 'x-btn-tag-right'
             }),
-            icon = baseSpan.getById(closeIcon.id);
-        icon.on('click', function(){
+            closeIconEl = baseSpan.getById(closeIcon.id);
+        textSpan.addCls('x-btn-tag-text');
+        closeIconEl.on('click', function(){
             me.destroy();
         });
         this.callParent(arguments)
