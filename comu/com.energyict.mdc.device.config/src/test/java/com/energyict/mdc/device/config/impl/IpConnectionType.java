f385133fea45ead13588d5ea6dc7c73263d2c201
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-04-23 09:52:12 +0200

    Added tests for validating properties

---
+++ comu/com.energyict.mdc.device.config/src/test/java/com/energyict/mdc/device/config/impl/IpConnectionType.java
@@ -0,0 +1,110 @@
+package com.energyict.mdc.device.config.impl;
+
+import com.energyict.mdc.common.TypedProperties;
+import com.energyict.mdc.dynamic.OptionalPropertySpecFactory;
+import com.energyict.mdc.dynamic.PropertySpec;
+import com.energyict.mdc.dynamic.PropertySpecService;
+import com.energyict.mdc.dynamic.RequiredPropertySpecFactory;
+import com.energyict.mdc.protocol.api.ComChannel;
+import com.energyict.mdc.protocol.api.ComPortType;
+import com.energyict.mdc.protocol.api.ConnectionException;
+import com.energyict.mdc.protocol.api.dynamic.ConnectionProperty;
+import com.energyict.protocols.mdc.protocoltasks.ServerConnectionType;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Models a {@link com.energyict.mdc.protocol.api.ConnectionType} for TCP/IP taht does not support
+ * multiple connections and that is designed for unit testing purposes only.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2012-08-17 (11:19)
+ */
+public class IpConnectionType implements ServerConnectionType {
+
+    public static final String IP_ADDRESS_PROPERTY_NAME = "ipAddress";
+    public static final String PORT_PROPERTY_NAME = "port";
+    private static final int HASH_CODE = 35809; // Random prime number
+
+    public IpConnectionType() {
+        super();
+    }
+
+    @Override
+    public void setPropertySpecService(PropertySpecService propertySpecService) {
+
+    }
+
+    @Override
+    public boolean allowsSimultaneousConnections () {
+        return true;
+    }
+
+    @Override
+    public boolean supportsComWindow () {
+        return false;
+    }
+
+    @Override
+    public Set<ComPortType> getSupportedComPortTypes () {
+        return EnumSet.of(ComPortType.TCP, ComPortType.UDP);
+    }
+
+    private PropertySpec ipAddressPropertySpec () {
+        return RequiredPropertySpecFactory.newInstance().stringPropertySpec(IP_ADDRESS_PROPERTY_NAME);
+    }
+
+    private PropertySpec portNumberPropertySpec () {
+        return OptionalPropertySpecFactory.newInstance().bigDecimalPropertySpec(PORT_PROPERTY_NAME);
+    }
+
+    @Override
+    public List<PropertySpec> getPropertySpecs() {
+        return Arrays.asList(this.ipAddressPropertySpec(), this.portNumberPropertySpec());
+    }
+
+    @Override
+    public PropertySpec getPropertySpec (String name) {
+        switch (name) {
+            case IP_ADDRESS_PROPERTY_NAME:
+                return this.ipAddressPropertySpec();
+            case PORT_PROPERTY_NAME:
+                return this.portNumberPropertySpec();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public String getVersion () {
+        return "For Unit Testing purposes only";
+    }
+
+    @Override
+    public void copyProperties(TypedProperties properties) {
+        // Do not need this as it is for unit testing purposes only
+    }
+
+    @Override
+    public ComChannel connect(List<ConnectionProperty> properties) throws ConnectionException {
+        return null;
+    }
+
+    @Override
+    public void disconnect(ComChannel comChannel) throws ConnectionException {
+    }
+
+    @Override
+    public int hashCode () {
+        return HASH_CODE;
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        return obj instanceof IpConnectionType || super.equals(obj);
+    }
+
+}
--- comu/com.energyict.mdc.device.config/src/test/java/com/energyict/mdc/device/config/impl/PartialOutboundConnectiontaskCrudIT.java
+++ comu/com.energyict.mdc.device.config/src/test/java/com/energyict/mdc/device/config/impl/PartialOutboundConnectiontaskCrudIT.java
@@ -14,6 +14,7 @@ import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.impl.NlsModule;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.OrmService;
+import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.orm.impl.OrmModule;
 import com.elster.jupiter.parties.impl.PartyModule;
 import com.elster.jupiter.pubsub.impl.PubSubModule;
@@ -24,11 +25,14 @@ import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.users.impl.UserModule;
 import com.elster.jupiter.util.UtilModule;
 import com.energyict.mdc.common.ApplicationContext;
+import com.energyict.mdc.common.CanFindByLongPrimaryKey;
 import com.energyict.mdc.common.ComWindow;
 import com.energyict.mdc.common.Environment;
+import com.energyict.mdc.common.FactoryIds;
 import com.energyict.mdc.common.IdBusinessObjectFactory;
 import com.energyict.mdc.common.TimeDuration;
 import com.energyict.mdc.common.Translator;
+import com.energyict.mdc.common.TypedProperties;
 import com.energyict.mdc.common.impl.MdcCommonModule;
 import com.energyict.mdc.device.config.ConnectionStrategy;
 import com.energyict.mdc.device.config.DeviceConfiguration;
@@ -74,7 +78,10 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
 
+import java.math.BigDecimal;
 import java.security.Principal;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.guava.api.Assertions.assertThat;
@@ -125,6 +132,7 @@ public class PartialOutboundConnectiontaskCrudIT {
     private IdBusinessObjectFactory businessObjectFactory;
 
     private class MockModule extends AbstractModule {
+
         @Override
         protected void configure() {
             bind(EventAdmin.class).toInstance(eventAdmin);
@@ -186,6 +194,7 @@ public class PartialOutboundConnectiontaskCrudIT {
         Environment environment = injector.getInstance(Environment.class);
         environment.put(InMemoryPersistence.JUPITER_BOOTSTRAP_MODULE_COMPONENT_NAME, bootstrapModule, true);
         environment.setApplicationContext(applicationContext);
+        createOracleAliases();
     }
 
     @Before
@@ -199,6 +208,9 @@ public class PartialOutboundConnectiontaskCrudIT {
         when(deviceProtocolPluggableClass.getDeviceProtocol()).thenReturn(deviceProtocol);
 
         initializeDatabase(false, false);
+
+        Environment.DEFAULT.get().registerFinder(new ConnectionMethodFinder());
+
         protocolPluggableService = injector.getInstance(ProtocolPluggableService.class);
         engineModelService = injector.getInstance(EngineModelService.class);
 
@@ -274,7 +286,7 @@ public class PartialOutboundConnectiontaskCrudIT {
 
     }
 
-//            partialInboundConnectionTask.setName("Changed");
+    //            partialInboundConnectionTask.setName("Changed");
     @Test
     public void testUpdate() {
 
@@ -512,8 +524,144 @@ public class PartialOutboundConnectiontaskCrudIT {
         }
     }
 
+    @Test
+    @ExpectedConstraintViolation(messageId = '{' + MessageSeeds.Constants.PARTIAL_CONNECTION_TASK_PROPERTY_HAS_NO_SPEC_KEY + '}')
+    public void testCreateWithNonSpeccedProperty() {
+
+        PartialScheduledConnectionTaskImpl outboundConnectionTask;
+        DeviceConfiguration deviceConfiguration;
+        try (TransactionContext context = transactionService.getContext()) {
+            connectionTypePluggableClass = protocolPluggableService.newConnectionTypePluggableClass("IPConnectionType", IpConnectionType.class.getName());
+            connectionTypePluggableClass.save();
+            DeviceType deviceType = deviceConfigurationService.newDeviceType("MyType", deviceProtocolPluggableClass);
+            deviceType.save();
+
+            deviceConfiguration = deviceType.newConfiguration("Normal").add();
+            deviceConfiguration.save();
+
+            outboundConnectionTask = deviceConfiguration.newPartialScheduledConnectionTask("MyOutbound", connectionTypePluggableClass, TimeDuration.seconds(60), ConnectionStrategy.MINIMIZE_CONNECTIONS)
+                    .comPortPool(outboundComPortPool)
+                    .comWindow(COM_WINDOW)
+                    .nextExecutionSpec().temporalExpression(TimeDuration.days(1), TimeDuration.minutes(90)).set()
+                    .addProperty("NONSPECCED", 5)
+                    .asDefault(true).build();
+            deviceConfiguration.save();
+
+            context.commit();
+        }
+
+
+    }
+
+    @Test
+    @ExpectedConstraintViolation(messageId = '{' + MessageSeeds.Constants.PARTIAL_CONNECTION_TASK_PROPERTY_VALUE_OF_WRONG_TYPE_KEY + '}')
+    public void testCreateWithWrongValueForProperty() {
+
+        PartialScheduledConnectionTaskImpl outboundConnectionTask;
+        DeviceConfiguration deviceConfiguration;
+        try (TransactionContext context = transactionService.getContext()) {
+            connectionTypePluggableClass = protocolPluggableService.newConnectionTypePluggableClass("IPConnectionType", IpConnectionType.class.getName());
+            connectionTypePluggableClass.save();
+            DeviceType deviceType = deviceConfigurationService.newDeviceType("MyType", deviceProtocolPluggableClass);
+            deviceType.save();
+
+            deviceConfiguration = deviceType.newConfiguration("Normal").add();
+            deviceConfiguration.save();
+
+            outboundConnectionTask = deviceConfiguration.newPartialScheduledConnectionTask("MyOutbound", connectionTypePluggableClass, TimeDuration.seconds(60), ConnectionStrategy.MINIMIZE_CONNECTIONS)
+                    .comPortPool(outboundComPortPool)
+                    .comWindow(COM_WINDOW)
+                    .nextExecutionSpec().temporalExpression(TimeDuration.days(1), TimeDuration.minutes(90)).set()
+                    .addProperty(IpConnectionType.IP_ADDRESS_PROPERTY_NAME, BigDecimal.valueOf(140024, 2))
+                    .asDefault(true).build();
+            deviceConfiguration.save();
+
+            context.commit();
+        }
+
+
+    }
+
+    @Test
+    public void testCreateWithCorrectProperty() {
+
+        PartialScheduledConnectionTaskImpl outboundConnectionTask;
+        DeviceConfiguration deviceConfiguration;
+        try (TransactionContext context = transactionService.getContext()) {
+            connectionTypePluggableClass = protocolPluggableService.newConnectionTypePluggableClass("IPConnectionType", IpConnectionType.class.getName());
+            connectionTypePluggableClass.save();
+            DeviceType deviceType = deviceConfigurationService.newDeviceType("MyType", deviceProtocolPluggableClass);
+            deviceType.save();
+
+            deviceConfiguration = deviceType.newConfiguration("Normal").add();
+            deviceConfiguration.save();
+
+            outboundConnectionTask = deviceConfiguration.newPartialScheduledConnectionTask("MyOutbound", connectionTypePluggableClass, TimeDuration.seconds(60), ConnectionStrategy.MINIMIZE_CONNECTIONS)
+                    .comPortPool(outboundComPortPool)
+                    .comWindow(COM_WINDOW)
+                    .nextExecutionSpec().temporalExpression(TimeDuration.days(1), TimeDuration.minutes(90)).set()
+                    .addProperty(IpConnectionType.IP_ADDRESS_PROPERTY_NAME, "127.0.0.1")
+                    .asDefault(true).build();
+            deviceConfiguration.save();
+
+            context.commit();
+        }
+        Optional<PartialConnectionTask> found = deviceConfigurationService.getPartialConnectionTask(outboundConnectionTask.getId());
+        assertThat(found).isPresent();
+
+        PartialConnectionTask partialConnectionTask = found.get();
+
+        TypedProperties typedProperties = partialConnectionTask.getTypedProperties();
+        assertThat(typedProperties.hasValueFor(IpConnectionType.IP_ADDRESS_PROPERTY_NAME)).isTrue();
+        assertThat(typedProperties.getProperty(IpConnectionType.IP_ADDRESS_PROPERTY_NAME)).isEqualTo("127.0.0.1");
+
+    }
+
 
     public interface MyDeviceProtocolPluggableClass extends DeviceProtocolPluggableClass {
+
     }
+
+    public class ConnectionMethodFinder implements CanFindByLongPrimaryKey {
+
+        @Override
+        public FactoryIds registrationKey() {
+            return FactoryIds.CONNECTION_METHOD;
+        }
+
+        @Override
+        public Class valueDomain() {
+            return null;
+        }
+
+        @Override
+        public Optional findByPrimaryKey(long id) {
+            return null;
+        }
+    }
+
+    private static void createOracleAliases() {
+        try {
+            try (PreparedStatement preparedStatement = Environment.DEFAULT.get().getConnection().prepareStatement(
+                    "CREATE VIEW IF NOT EXISTS USER_TABLES AS select table_name from INFORMATION_SCHEMA.TABLES where table_schema = 'PUBLIC'"
+            )) {
+                preparedStatement.execute();
+            }
+            try (PreparedStatement preparedStatement = Environment.DEFAULT.get().getConnection().prepareStatement(
+                    "CREATE VIEW IF NOT EXISTS USER_IND_COLUMNS AS select index_name, table_name, column_name, ordinal_position AS column_position from INFORMATION_SCHEMA.INDEXES where table_schema = 'PUBLIC'"
+            )) {
+                preparedStatement.execute();
+            }
+            try (PreparedStatement preparedStatement = Environment.DEFAULT.get().getConnection().prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS USER_SEQUENCES ( SEQUENCE_NAME VARCHAR2 (30) NOT NULL, MIN_VALUE NUMBER, MAX_VALUE NUMBER, INCREMENT_BY NUMBER NOT NULL, CYCLE_FLAG VARCHAR2 (1), ORDER_FLAG VARCHAR2 (1), CACHE_SIZE NUMBER NOT NULL, LAST_NUMBER NUMBER NOT NULL)"
+            )) {
+                preparedStatement.execute();
+            }
+            Environment.DEFAULT.get().closeConnection();
+        } catch (SQLException e) {
+            throw new UnderlyingSQLFailedException(e);
+        }
+    }
+
 
 }
