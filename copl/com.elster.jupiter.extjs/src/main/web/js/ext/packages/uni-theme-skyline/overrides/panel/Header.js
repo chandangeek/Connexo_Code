ec87a68b0f339a32fabb52c6b1f0bbb263e93811
Artem Puchenkin <apuchenkin@gmail.com>
2014-04-23 21:01:19 +0400

    Panel header

---
+++ copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/panel/Header.js
@@ -0,0 +1,47 @@
+Ext.define('Skyline.panel.Header', {
+    override: 'Ext.panel.Header',
+
+    headingTpl: [
+        // unselectable="on" is required for Opera, other browsers inherit unselectability from the header
+        '<span id="{id}-textEl" class="{headerCls}-text {cls}-text {cls}-text-{ui}" unselectable="on"',
+        '<tpl if="headerRole">',
+        ' role="{headerRole}"',
+        '</tpl>',
+        '>{title}</span>',
+        '<span id="{id}-subTextEl" class="{headerCls}-text-sub {cls}-text-sub {cls}-text-sub-{ui}" unselectable="on"',
+        '<tpl if="headerRole">',
+        ' role="{headerRole}"',
+        '</tpl>',
+        '>{subtitle}</span>'
+    ],
+
+    initComponent: function() {
+        var me = this;
+
+        this.callParent(arguments);
+        me.titleCmp.childEls.push("subTextEl");
+    },
+
+    /**
+     * Sets the subtitle of the header.
+     * @param {String} subtitle The title to be set
+     */
+    setSubTitle: function(subtitle) {
+        var me = this,
+            titleCmp = me.titleCmp;
+
+        me.subtitle = subtitle;
+
+        if (titleCmp.rendered) {
+            titleCmp.subTextEl.update(me.subtitle || '&#160;');
+            titleCmp.updateLayout();
+        } else {
+            me.titleCmp.on({
+                render: function() {
+                    me.setSubTitle(subtitle);
+                },
+                single: true
+            });
+        }
+    }
+});
