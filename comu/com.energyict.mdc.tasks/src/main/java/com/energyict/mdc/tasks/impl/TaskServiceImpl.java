a394d92906833d5e25c9093fcc50572db13cc064
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-04-25 14:01:13 +0200

    Fixing dependencies

--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/TaskServiceImpl.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/TaskServiceImpl.java
@@ -7,7 +7,6 @@ import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.OrmService;
 import com.elster.jupiter.orm.callback.InstallService;
-import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.tasks.BasicCheckTask;
 import com.energyict.mdc.tasks.ClockTask;
 import com.energyict.mdc.tasks.ComTask;
