a7bc6bbd4120279aed87c91299b86718e9eac3dc
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-04-29 09:40:00 +0200

    JP-1331: next planned date is empty in case of PAUZED schedule

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/ComScheduleInfo.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/ComScheduleInfo.java
@@ -1,6 +1,7 @@
 package com.energyict.mdc.scheduling.rest.impl;
 
 import com.energyict.mdc.scheduling.model.ComSchedule;
+import com.energyict.mdc.scheduling.model.SchedulingStatus;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -20,7 +21,7 @@ public class ComScheduleInfo {
         comScheduleInfo.id = comSchedule.getId();
         comScheduleInfo.name = comSchedule.getName();
         comScheduleInfo.temporalExpression = TemporalExpressionInfo.from(comSchedule.getTemporalExpression());
-        comScheduleInfo.plannedDate = comSchedule.getNextTimestamp(Calendar.getInstance());
+        comScheduleInfo.plannedDate = SchedulingStatus.PAUSED.equals(comSchedule.getSchedulingStatus())?null:comSchedule.getNextTimestamp(Calendar.getInstance());
         comScheduleInfo.isInUse = inUse;
         return comScheduleInfo;
     }
