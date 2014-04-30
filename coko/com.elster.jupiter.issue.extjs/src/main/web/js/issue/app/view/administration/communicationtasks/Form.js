7834fc12ea75e3226d4870a77099c600a3f7ddf2
Sergey <ssentyur@mera.ru>
2014-04-30 16:57:52 +0400

    "commandsString" field of "CommunicationTasks" model add

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/communicationtasks/Form.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/communicationtasks/Form.js
@@ -18,7 +18,7 @@ Ext.define('Isu.view.administration.communicationtasks.Form', {
         {
             xtype: 'displayfield',
             fieldLabel: 'Commands',
-            name: 'commands'
+            name: 'commandsString'
         }
     ]
 });
