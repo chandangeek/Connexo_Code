2e31b9885b70a25aa1264dc00c3d6553982418ad
Sergey <ssentyur@mera.ru>
2014-05-05 16:55:34 +0400

    "Create communication task" view add

---
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/communicationtasks/Edit.js
@@ -0,0 +1,12 @@
+Ext.define('Isu.view.administration.communicationtasks.Edit', {
+    extend: 'Uni.view.container.ContentContainer',
+    requires: [
+
+    ],
+    alias: 'widget.communication-tasks-edit',
+    content: [
+        {
+            title: 'Create communication task'
+        }
+    ]
+});
