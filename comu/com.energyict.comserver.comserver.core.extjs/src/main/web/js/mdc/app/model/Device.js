3138f873cbe2fc6464c5efcdd731ef0418e7ac65
sla <silvie.labis@elster.com>
2014-05-06 16:23:51 +0200

    JP-2598 Develop View meter attributes on landing page

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/model/Device.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/model/Device.js
@@ -5,7 +5,7 @@ Ext.define('Mdc.model.Device', {
     ],
     fields: [
         {name: 'id',type:'number',useNull:true},
-        {name: 'mRId', type: 'string', useNull: true},
+        {name: 'mRID', type: 'string', useNull: true},
         {name: 'serialNumber', type: 'string', useNull: true},
         {name: 'deviceTypeId', type: 'number', useNull: true},
         {name: 'deviceTypeName', type: 'string', useNull: true},
