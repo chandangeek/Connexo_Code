e93659679255a96e53c6f6342c630f982f3435e9
Bruno Ballekens <bruno.ballekens@elster.com>
2014-05-07 13:53:43 +0200

    Added workaround for JPR-38 and JPR-32 : Minify and integrated apps don't work together

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/uni-all.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/uni-all.js
@@ -1,11 +1,11 @@
 /**
  * Adds the Unifying JS config to the Ext JS loader.
- * When changing, also change uni-all.js
+ * When changing, also change the uni-dev.js
  */
 (function () {
     Ext.Loader.setConfig({
         enabled: true,
-        disableCaching: true,
+        disableCaching: false,
         paths: {
             'Ext.ux.window': '../uni/packages/Ext.ux.window.Notification',
             'Uni': '../uni/src'
