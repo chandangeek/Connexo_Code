7265256c6c53bb9f395f6640d8b09a3fd04c1451
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-05-09 08:05:55 +0200

    JP-2013: Fixed returning exclusive list of available comTasks for a comSchedule

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/DeviceConfigurationService.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/DeviceConfigurationService.java
@@ -187,6 +187,13 @@ public interface DeviceConfigurationService {
      */
     public Optional<ComTaskEnablement> findComTaskEnablement (ComTask comTask, DeviceConfiguration deviceConfiguration);
 
+    /**
+     * Return a list of ComTasks that are legal for assignment to the ComSchedule. A ComTask can be assigned to the comSchedule IF all devices
+     * already linked to the schedule are enabled for the ComTask.
+     * This list will include ComTasks already linked to the ComSchedule!
+     * @param comSchedule
+     * @return List of ComTasks, including ComTasks already linked to the schedule.
+     */
     public List<ComTask> findAvailableComTasks(ComSchedule comSchedule);
 
 }
