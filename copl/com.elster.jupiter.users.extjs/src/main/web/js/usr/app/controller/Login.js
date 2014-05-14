6aed1633b7764b66dd3fefdade7cb3ff4f9b0361
Artem Puchenkin <apuchenkin@gmail.com>
2014-05-14 18:44:39 +0400

    JP-2590 Branding of Login

--- copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/app/controller/Login.js
+++ copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/app/controller/Login.js
@@ -1,40 +1,45 @@
-Ext.require('Uni.Loader');
-Ext.require('Ext.Viewport');
-Ext.require('Usr.controller.Base64');
-
-Ext.Loader.setConfig({
-    enabled: true,
-    disableCaching: true, // For debug only.
-    paths: {
-        'Uni' : '../uni/src'
-    }
-});
-
 Ext.define('Usr.controller.Login', {
     extend: 'Ext.app.Controller',
+
+    requires: [
+        'Usr.controller.Base64'
+    ],
 
     views: [
         'Usr.view.Login'
     ],
 
+    refs: [
+        {
+            ref: 'loginViewport',
+            selector: 'login'
+        },
+        {
+            ref: 'loginForm',
+            selector: 'login #login-form'
+        }
+    ],
+
     init: function(application) {
         this.control({
-            "login #loginButton": {
-                signin: this.signinuser
+            "login #login-form [action=login]": {
+                click: this.signinuser
             }
         });
     },
 
     showOverview: function() {
         var widget = Ext.widget("login");
-        this.getApplication().fireEvent('changecontentevent', widget);
     },
 
-    signinuser : function(button, username, password)    {
-        Ext.Ajax.suspendEvent('requestexception');
+    signinuser : function()    {
+        var me = this,
+            form = this.getLoginForm(),
+            data = form.getValues();
+//        Ext.Ajax.suspendEvent('requestexception');
         this.hideLoginError();
 
-        var unencodedToken = username + ":" + password;
+        var unencodedToken = data.username + ":" + data.password;
         var encodedToken = "Basic " + Usr.controller.Base64.encode(unencodedToken);
 
         //var widget =  Ext.getCmp('usm_elster_login').down('#contentPanel');
@@ -50,42 +55,42 @@ Ext.define('Usr.controller.Login', {
             },
             scope: this,
             success: function(response, opt){
-                //widget.setLoading(false);
-                myMask.hide();
-                this.loginOK(button);
+                me.loginOK();
             },
             failure: function(response, opt){
-                //widget.setLoading(false);
+                me.loginNOK();
+            },
+            callback: function() {
                 myMask.hide();
-                this.loginNOK();
             }
         });
     },
 
-    loginOK : function(button){
-        Ext.Ajax.resumeEvent('requestexception');
+    loginOK : function(){
+//        Ext.Ajax.resumeEvent('requestexception');
 
-        var loader = Ext.create('Uni.Loader');
-        loader.initI18n(['USM']);
-
-        Uni.I18n.currencyFormatKey = 'mtr.playground.i18n.currencyformat';
-        Uni.I18n.decimalSeparatorKey = 'mtr.playground.i18n.decimalseparator';
-        Uni.I18n.thousandsSeparatorKey = 'mtr.playground.i18n.thousandsseparator';
-
-        var me=this;
-        loader.onReady(function () {
-            Ext.getCmp('usm_elster_login').destroy();
-            me.getApplication().destroy();
-
-            Ext.application({
-                name: 'Usr',
-
-                extend: 'Usr.Application',
-
-                autoCreateViewport: true
-            });
-
-        });
+//        var loader = Ext.create('Uni.Loader');
+//        loader.initI18n(['USM']);
+//
+//        Uni.I18n.currencyFormatKey = 'mtr.playground.i18n.currencyformat';
+//        Uni.I18n.decimalSeparatorKey = 'mtr.playground.i18n.decimalseparator';
+//        Uni.I18n.thousandsSeparatorKey = 'mtr.playground.i18n.thousandsseparator';
+//
+//        var me=this;
+//        loader.onReady(function () {
+//            Ext.getCmp('usm_elster_login').destroy();
+//            me.getApplication().destroy();
+//
+//            Ext.application({
+//                name: 'Usr',
+//
+//                extend: 'Usr.Application',
+//
+//                autoCreateViewport: true
+//            });
+//
+//        });
+        this.getLoginViewport().destroy();
     },
 
     loginNOK : function(){
@@ -93,30 +98,13 @@ Ext.define('Usr.controller.Login', {
     },
 
     hideLoginError: function () {
-        var widget =  Ext.getCmp('usm_elster_login').down('#contentPanel'),
-            errorLabel = widget.down('#errorLabel');
-
-        widget.setHeight(250);
-        widget.down('#errorIcon').setHeight(0);
-
-        errorLabel.setHeight(0);
-
-        widget.down('#errorContainer').hide();
-        widget.doLayout();
+        var errorLabel =  this.getLoginForm().down('#errorLabel');
+        errorLabel.hide();
     },
 
     showLoginError: function () {
-        var widget =  Ext.getCmp('usm_elster_login').down('#contentPanel'),
-            errorLabel = widget.down('#errorLabel');
-
-        widget.setHeight(300);
-        widget.down('#errorIcon').setHeight(50);
-
-        errorLabel.setHeight(50);
+        var errorLabel =  this.getLoginForm().down('#errorLabel');
         errorLabel.setValue('Failed to log in. Please contact your administrator if the problem persists.');
-
-        widget.down('#errorContainer').show();
-        widget.doLayout();
+        errorLabel.show();
     }
-
 });
