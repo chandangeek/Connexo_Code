da83e20aea1faa759aabcfe2a349e65ec5ef7262
Levi Van Zele <Levi.VanZele@elster.com>
2014-05-22 16:39:44 +0200

    Changed the buttons on the register type and group forms.

--- copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/build/uni-theme-skyline-debug.js
+++ copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/build/uni-theme-skyline-debug.js
@@ -487,10 +487,41 @@ Ext.define('ExtThemeNeptune.container.ButtonGroup', {
 
 Ext.define('Skyline.form.Labelable', {
     override: 'Ext.form.Labelable',
-    labelAlign: 'right',
     labelPad: 15,
     msgTarget: 'under',
-    blankText: 'This is a required field'
+    blankText: 'This is a required field',
+
+    /**
+     * Required property, set when a field is required.
+     */
+    required: false,
+
+    /**
+     * Changes the default value ':'.
+     */
+    labelSeparator: '',
+
+    /**
+     * Changes the default value 'left'.
+     */
+    labelAlign: 'right',
+
+    /**
+     * @inheritDoc Ext.form.Labelable#getLabelCls
+     *
+     * Adds an extra required class when needed
+     *
+     * @returns {string}
+     */
+    getLabelCls: function () {
+        var labelCls = this.labelCls;
+
+        if (this.required) {
+            labelCls += ' ' + 'uni-form-item-label-required';
+        }
+
+        return labelCls;
+    }
 });
 
 
