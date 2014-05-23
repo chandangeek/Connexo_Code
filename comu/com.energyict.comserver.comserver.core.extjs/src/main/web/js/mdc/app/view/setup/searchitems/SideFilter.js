0edc8abf3e6adf86cc05c91218128b2239ee0304
Artem Puchenkin <apuchenkin@gmail.com>
2014-05-23 16:34:20 +0400

    [JP-2593] - Branding of meter landing page (cherry picked from commit a27443c)

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/searchitems/SideFilter.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/searchitems/SideFilter.js
@@ -1,72 +1,50 @@
 Ext.define('Mdc.view.setup.searchitems.SideFilter', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.search-side-filter',
-    itemId: 'sideFilter',
     cls: 'filter-form',
     width: 250,
+    title: Uni.I18n.translate('searchItems.searchFor', 'MDC', 'Search for devices'),
     ui: 'medium',
     requires: [
         'Uni.component.filter.view.Filter',
-        'Mdc.store.DeviceTypes',
-        'Mdc.store.DeviceConfigurations'
+        'Mdc.store.DeviceTypes'
     ],
     items: [
         {
-        xtype: 'filter-form',
-        layout: {
-            type: 'vbox',
-            align: 'stretch'
-        },
-        items: [
-                    {
-                        xtype: 'textfield',
-                        name: 'mrid',
-                        itemId: 'mrid',
-                        fieldLabel: Uni.I18n.translate('searchItems.mrid', 'MDC', 'MRID'),
-                        labelAlign: 'top'
-                    },
-                    {
-                        xtype: 'textfield',
-                        name: 'sn',
-                        itemId: 'sn',
-                        fieldLabel: Uni.I18n.translate('searchItems.serialNumber', 'MDC', 'Serial number'),
-                        labelAlign: 'top'
-                    },
-                    {
-                        xtype: 'combobox',
-                        name: 'type',
-                        itemId: 'type',
-                        store: 'DeviceTypes',
-                        fieldLabel: Uni.I18n.translate('searchItems.type', 'MDC', 'Type'),
-                        displayField: 'name',
-                        valueField: 'id',
-                        forceSelection: false,
-                        editable: false,
-                        emptyText: ' ',
-                        labelAlign: 'top',
-                        allowBlank:true,
-                        listeners: {
-                            select: function (comp) {
-                                if (comp.getValue() == "-1")
-                                    comp.setValue(null);
-                            }
-                        }
-                    }
+            xtype: 'filter-form',
+            ui: 'filter',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            defaults: {
+                labelAlign: 'top'
+            },
+            items: [
+                {
+                    xtype: 'textfield',
+                    name: 'mrid',
+                    itemId: 'mrid',
+                    fieldLabel: Uni.I18n.translate('searchItems.mrid', 'MDC', 'MRID')
+                },
+                {
+                    xtype: 'textfield',
+                    name: 'sn',
+                    itemId: 'sn',
+                    fieldLabel: Uni.I18n.translate('searchItems.serialNumber', 'MDC', 'Serial number')
                 },
                 {
                     xtype: 'combobox',
                     name: 'type',
-                    itemId: 'configuration',
-                    store: 'DeviceConfigurations',
-                    queryMode: 'local',
-                    fieldLabel: Uni.I18n.translate('searchItems.configuration', 'MDC', 'Configuration'),
+                    itemId: 'type',
+                    store: 'DeviceTypes',
+                    fieldLabel: Uni.I18n.translate('searchItems.type', 'MDC', 'Type'),
                     displayField: 'name',
-                    hidden: true,
                     valueField: 'id',
                     forceSelection: false,
                     editable: false,
-                    labelAlign: 'top',
-                    allowBlank:true,
+                    emptyText: ' ',
+                    allowBlank: true,
                     listeners: {
                         select: function (comp) {
                             if (comp.getValue() == "-1")
@@ -74,23 +52,20 @@ Ext.define('Mdc.view.setup.searchitems.SideFilter', {
                         }
                     }
                 }
+            ],
+
+            buttons: [
+                {
+                    text: Uni.I18n.translate('searchItems.clearAll', 'MDC', 'Search'),
+                    itemId: 'searchAllItems',
+                    action: 'applyfilter'
+                },
+                {
+                    text: Uni.I18n.translate('searchItems.clearAll', 'MDC', 'Clear all'),
+                    itemId: 'clearAllItems',
+                    action: 'clearfilter'
+                }
             ]
         }
-    ],
-    buttons: [
-        {
-            text: Uni.I18n.translate('searchItems.clearAll', 'MDC', 'Search'),
-            itemId: 'searchAllItems',
-            action: 'applyfilter'
-        },
-        {
-            text: Uni.I18n.translate('searchItems.clearAll', 'MDC', 'Clear all'),
-            itemId: 'clearAllItems',
-            action: 'clearfilter'
-        }
-    ],
-    clearComboConfiguration: function(cmbConfig){
-        cmbConfig.setValue(null);
-        cmbConfig.setVisible(false);
-    }
+    ]
 });
