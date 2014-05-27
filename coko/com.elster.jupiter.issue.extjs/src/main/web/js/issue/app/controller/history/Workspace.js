cf9b535fe3e9788cb2d64412c7385610fcd01ca7
Artem Puchenkin <apuchenkin@gmail.com>
2014-05-27 18:31:32 +0400

    Router component

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/controller/history/Workspace.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/controller/history/Workspace.js
@@ -2,36 +2,60 @@ Ext.define('Isu.controller.history.Workspace', {
     extend: 'Uni.controller.history.Converter',
 
     rootToken: 'workspace',
+    previousPath: '',
     currentPath: null,
 
-    init: function () {
-        var me = this;
+    routeConfig: {
+        workspace : {
+            title: 'Workspace',
+            route: 'workspace',
+            disabled: true,
+            items: {
+                datacollection: {
+                    title: 'Data collection',
+                    route: 'datacollection',
+                    controller: 'Isu.controller.DataCollectionOverview',
+                    items: {
+                        issues: {
+                            title : 'View Issues',
+                            route: 'issues',
+                            controller: 'Isu.controller.Issues',
+                            items: {
+                                view: {
+                                    title: 'issue details',
+                                    route: '{id}',
+                                    controller: 'Isu.controller.IssueDetail'
+                                },
+                                edit: {
+                                    title: 'Issue Edit',
+                                    route: '{id}/addcomment',
+                                    controller: 'Isu.controller.IssueDetail'
+                                },
+                                assign: {
+                                    title: 'Issue Assign',
+                                    route: '{id}/assign',
+                                    controller: 'Isu.controller.AssignIssues'
+                                },
+                                close: {
+                                    title: 'Issue Close',
+                                    route: '{id}/close',
+                                    controller: 'Isu.controller.CloseIssues'
+                                }
+                            }
+                        },
+                        bulk: {
+                            title : 'Bulk Changes',
+                            route: 'bulkaction',
+                            controller: 'Isu.controller.BulkChangeIssues'
+                        }
+                    }
+                }
+            }
+        }
+    },
 
-        crossroads.addRoute('workspace/datacollection', function () {
-            me.getController('Isu.controller.DataCollectionOverview').showOverview();
-        });
-        crossroads.addRoute('workspace/datacollection/issues', function () {
-            me.getController('Isu.controller.Issues').showOverview();
-        });
-        crossroads.addRoute('workspace/datacollection/issuesbulkaction', function () {
-            me.getController('Isu.controller.BulkChangeIssues').showOverview();
-        });
-        crossroads.addRoute('workspace/datacollection/issues/{id}', function (id) {
-            me.getController('Isu.controller.IssueDetail').showOverview(id);
-        });
-        crossroads.addRoute('workspace/datacollection/issues/{id}/addcomment', function (id) {
-            me.getController('Isu.controller.IssueDetail').showOverview(id, true);
-        });
-        crossroads.addRoute('workspace/datacollection/bulkaction', function () {
-            me.getController('Isu.controller.BulkChangeIssues').showOverview();
-        });
-        crossroads.addRoute('workspace/datacollection/issues/{id}/assign', function (id) {
-            me.getController('Isu.controller.AssignIssues').showOverview(id);
-        });
-        crossroads.addRoute('workspace/datacollection/issues/{id}/close', function (id) {
-            me.getController('Isu.controller.CloseIssues').showOverview(id);
-        });
-
-        this.callParent(arguments);
+    init :function() {
+        var router = this.getController('Uni.controller.history.Router');
+        router.addConfig(this.routeConfig);
     }
 });
