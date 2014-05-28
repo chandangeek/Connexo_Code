3a06f80f1b528c807bfab95d405dec365e74e8ab
dvy <dieter.vyncke@elster.com>
2014-05-28 14:48:08 +0200

    Fix for required stars on forms

--- copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/form/field/FieldContainer.js
+++ copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/form/field/FieldContainer.js
@@ -1,20 +1,13 @@
-Ext.define('Skyline.form.field.Base', {
-    override: "Ext.form.field.Base",
-    labelAlign: 'right',
-    labelPad: 15,
-    msgTarget: 'under',
-    blankText: 'This is a required field',
+Ext.define('Skyline.form.field.FieldContainer', {
+    override: "Ext.form.FieldContainer",
     getLabelCls: function () {
         var labelCls = this.labelCls;
         if (this.required) {
             labelCls += ' ' + 'uni-form-item-label-required';
         }
-
         return labelCls;
     },
     initComponent: function() {
         this.callParent(arguments);
     }
 });
-
-
