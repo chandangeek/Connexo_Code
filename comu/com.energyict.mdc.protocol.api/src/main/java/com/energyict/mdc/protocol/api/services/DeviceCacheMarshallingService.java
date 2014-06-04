dc1934e2ba140513f51754e8c9bb5870411a5367
govanni <govanni.naessens@elster.com>
2014-06-04 16:38:31 +0200

    jp-2707 - JSONized the DeviceCache object. Each DeviceCache should make sure it is JSONizable ...

---
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/services/DeviceCacheMarshallingService.java
@@ -0,0 +1,22 @@
+package com.energyict.mdc.protocol.api.services;
+
+import java.io.Serializable;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 6/3/14
+ * Time: 5:31 PM
+ */
+public interface DeviceCacheMarshallingService {
+
+    /**
+     * Load the jsoned Cache object for the given string.
+     * The unmarshalling should happen in the bundle which 'knows' the cache objects.
+     *
+     * @param jsonCache the json representation of the cache
+     * @return the unmarshalled object
+     */
+    Object unMarshallCache(String jsonCache);
+
+    String marshall(Object legacyCache);
+}
