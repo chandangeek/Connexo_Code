aca35c01865b07ab642e04b2e11b7b3c8a0afe69
Isabelle  Gheysens <isabelle.gheysens@elster.com>
2014-06-06 11:20:29 +0200

    updated to detailicon

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/ext/button/ItemAction.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/ext/button/ItemAction.js
@@ -1,7 +1,7 @@
 Ext.define('Isu.view.ext.button.ItemAction', {
     extend: 'Ext.button.Button',
     text: 'Actions',
-    iconCls: 'x-uni-action-iconA',
+    iconCls: 'x-uni-action-iconD',
 
     alias: 'widget.item-action',
     menuAlign: 'tr-br?',
