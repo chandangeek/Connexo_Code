4e040c2bc174819cd70eb07450b135a58786a575
Dmitry Vikhrev <vikhrev@mera.ru>
2014-06-07 18:17:52 +0200

    JP-3307: Use Preview container and confirmation messages in Security settings according to RI

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/securitysettings/SecuritySettingEdit.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Mdc.view.setup.securitysettings.SecuritySettingEdit', {
-    extend: 'Uni.view.container.ContentContainer',
-    alias: 'widget.securitySettingEdit',
-    itemId: 'securitySettingEdit'
-
-});
