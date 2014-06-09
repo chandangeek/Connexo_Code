25ba29fb6280a6bbc6d67a74073406bc55c1807a
Maksim Barinov <mbarinov@mera.ru>
2014-06-09 12:00:15 +0400

    Add preview container to Issue Assignment Rules

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/datacollection/issueassignmentrules/List.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/datacollection/issueassignmentrules/List.js
@@ -1,63 +1,45 @@
 Ext.define('Isu.view.administration.datacollection.issueassignmentrules.List', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.grid.Panel',
     requires: [
         'Ext.layout.container.Column',
         'Ext.grid.column.Template',
         'Ext.grid.column.Action'
     ],
     alias: 'widget.issues-assignment-rules-list',
-    border: false,
-    items: [
-        {
-            name: 'empty-text',
-            border: false,
-            hidden: true,
-            items: [
-                {
-                    html: '<h3>No rules found</h3>'
-                },
-                {
-                    xtype: 'label',
-                    text: 'There are no rules have been created yet'
-                }
-            ]
+    store: 'Isu.store.AssignmentRules',
+    height: 285,
+
+    columns: {
+        defaults: {
+            sortable: false,
+            menuDisabled: true
         },
-        {
-            xtype: 'grid',
-            store: 'Isu.store.AssignmentRules',
-            height: 285,
-            columns: {
-                defaults: {
-                    sortable: false,
-                    menuDisabled: true
-                },
-                items: [
-                    {
-                        header: 'Description',
-                        dataIndex: 'description',
-                        tdCls: 'isu-grid-description',
-                        flex: 1
-                    },
-                    {
-                        header: 'Assign to',
-                        xtype: 'templatecolumn',
-                        tpl: '<tpl if="assignee.type"><span class="isu-icon-{assignee.type} isu-assignee-type-icon"></span></tpl> {assignee.name}',
-                        flex: 1
-                    },
-                    {
-                        xtype: 'uni-actioncolumn',
-                        items: 'Isu.view.administration.datacollection.issueassignmentrules.ActionMenu'
-                    }
-                ]
+        items: [
+            {
+                header: 'Description',
+                dataIndex: 'description',
+                tdCls: 'isu-grid-description',
+                flex: 1
             },
-            dockedItems: [
-                {
-                    xtype: 'toolbar',
-                    dock: 'top'
-                }
-            ]
+            {
+                header: 'Assign to',
+                xtype: 'templatecolumn',
+                tpl: '<tpl if="assignee.type"><span class="isu-icon-{assignee.type} isu-assignee-type-icon"></span></tpl> {assignee.name}',
+                flex: 1
+            },
+            {
+                xtype: 'uni-actioncolumn',
+                items: 'Isu.view.administration.datacollection.issueassignmentrules.ActionMenu'
+            }
+        ]
+    },
+    dockedItems: [
+        {
+            xtype: 'toolbar',
+            dock: 'top'
         }
     ],
+
 
     initComponent: function () {
         var self = this,
@@ -65,7 +47,7 @@ Ext.define('Isu.view.administration.datacollection.issueassignmentrules.List', {
 
         self.callParent(arguments);
 
-        store = this.down('grid').getStore();
+        store = self.getStore();
 
         self.onStoreLoad(store);
 
@@ -84,38 +66,15 @@ Ext.define('Isu.view.administration.datacollection.issueassignmentrules.List', {
 
         if (storeTotal) {
             this.setTotal(storeTotal);
-            this.hideEmptyText();
-        } else {
-            this.showEmptyText();
-        }
-    },
-
-    showEmptyText: function () {
-        var grid = this.down('grid'),
-            emtyText = this.down('panel[name=empty-text]');
-
-        if (grid && emtyText) {
-            grid.hide();
-            emtyText.show();
-        }
-    },
-
-    hideEmptyText: function () {
-        var grid = this.down('grid'),
-            emtyText = this.down('panel[name=empty-text]');
-
-        if (grid && emtyText) {
-            grid.show();
-            emtyText.hide();
         }
     },
 
     setTotal: function (total) {
-        var grid = this.down('grid'),
+        var self = this,
             gridTop;
 
-        if (grid) {
-            gridTop = grid.getDockedItems('toolbar[dock="top"]')[0];
+        if (self) {
+            gridTop = self.getDockedItems('toolbar[dock="top"]')[0];
             gridTop.removeAll();
             gridTop.add({
                 xtype: 'component',
--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/datacollection/issueassignmentrules/Overview.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/app/view/administration/datacollection/issueassignmentrules/Overview.js
@@ -2,25 +2,51 @@ Ext.define('Isu.view.administration.datacollection.issueassignmentrules.Overview
     extend: 'Uni.view.container.ContentContainer',
     requires: [
         'Uni.view.navigation.SubMenu',
-        'Isu.view.administration.datacollection.issueassignmentrules.List'
+        'Isu.view.administration.datacollection.issueassignmentrules.List',
+        'Uni.view.container.PreviewContainer'
     ],
     alias: 'widget.issue-assignment-rules-overview',
-
-    side: [
-        {
-            itemId: 'sideMenu',
-            xtype: 'navigationSubMenu'
-        }
-    ],
 
     content: {
         itemId: 'title',
         xtype: 'panel',
         ui: 'large',
-        title: 'Issue assignment rules',
+        title: Uni.I18n.translate('issue.administration.assignment', 'ISU', 'Issue assignment rules'),
         items: {
             itemId: 'issues-rules-list',
-            xtype: 'issues-assignment-rules-list'
+            xtype: 'preview-container',
+            grid: {
+                xtype: 'issues-assignment-rules-list'
+            },
+            emptyComponent: {
+                xtype: 'container',
+                layout: {
+                    type: 'hbox',
+                    align: 'left'
+                },
+                minHeight: 20,
+                items: [
+                    {
+                        xtype: 'image',
+                        margin: '0 10 0 0',
+                        src: '../ext/packages/uni-theme-skyline/build/resources/images/shared/icon-info-small.png',
+                        height: 20,
+                        width: 20
+                    },
+                    {
+                        xtype: 'container',
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: '<b>' + Uni.I18n.translate('issueAssignment.empty.title', 'ISU', 'No issue assignment rules found') + '</b><br>' +
+                                    Uni.I18n.translate('issueAssignment.empty.detail', 'ISU', 'There are no issue assignment rules. This could be because:') + '<lv><li>&nbsp&nbsp' +
+                                    Uni.I18n.translate('issueAssignment.empty.list.item', 'ISU', 'No issue assignment rules have been defined yet.')
+                            }
+                        ]
+                    }
+                ]
+            },
+            previewComponent: null
         }
-    },
+    }
 });
