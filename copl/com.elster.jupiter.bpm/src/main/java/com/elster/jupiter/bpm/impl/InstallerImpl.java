b26d6d3195fd4ef6aea3ec138b29a96a54167b53
Bruno Ballekens <bruno.ballekens@elster.com>
2014-06-12 14:35:05 +0200

    only create DestinationSpec and SubsciptionSpec on install

--- copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/InstallerImpl.java
+++ copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/InstallerImpl.java
@@ -5,7 +5,6 @@ import com.elster.jupiter.bpm.BpmService;
 import com.elster.jupiter.messaging.DestinationSpec;
 import com.elster.jupiter.messaging.MessageService;
 import com.elster.jupiter.messaging.QueueTableSpec;
-import com.elster.jupiter.messaging.SubscriberSpec;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,16 +15,15 @@ public class InstallerImpl {
     private static final Logger LOGGER = Logger.getLogger(InstallerImpl.class.getName());
 
     public void install(MessageService messageService, AppService appService) {
-        createBPMQueue(messageService, appService);
+        createBPMQueue(messageService);
     }
 
-    private void createBPMQueue(MessageService messageService, AppService appService) {
+    private void createBPMQueue(MessageService messageService) {
         try {
             QueueTableSpec defaultQueueTableSpec = messageService.getQueueTableSpec("MSG_RAWQUEUETABLE").get();
             DestinationSpec destinationSpec = defaultQueueTableSpec.createDestinationSpec(BpmService.BPM_QUEUE_DEST, DEFAULT_RETRY_DELAY_IN_SECONDS);
             destinationSpec.activate();
-            SubscriberSpec subscriberSpec = destinationSpec.subscribe(BpmService.BPM_QUEUE_SUBSC);
-            appService.getAppServer().get().createSubscriberExecutionSpec(subscriberSpec, 1);
+            destinationSpec.subscribe(BpmService.BPM_QUEUE_SUBSC);
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
