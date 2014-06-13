bc643df7b2a693349af4eedd3a9cf142817afe20
Sergey <ssentyur@mera.ru>
2014-06-13 11:31:46 +0400

    hotfix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/controller/setup/Comtasks.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/controller/setup/Comtasks.js
@@ -262,7 +262,7 @@ Ext.define('Mdc.controller.setup.Comtasks', {
             jsonData: sendingData,
             success: function () {
                 window.location.href = '#/administration/communicationtasks';
-                this.getApplication().fireEvent('acknowledge', 'Successfully created');
+                self.getApplication().fireEvent('acknowledge', 'Successfully created');
                 self.commands = [];
             },
             failure: function (response) {
