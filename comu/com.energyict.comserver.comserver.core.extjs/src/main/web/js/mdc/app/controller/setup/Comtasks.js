4e7656d13fa4e645230f9afcd48c56b594801bca
Sergey <ssentyur@mera.ru>
2014-06-13 11:17:22 +0400

    hotfix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/controller/setup/Comtasks.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/controller/setup/Comtasks.js
@@ -134,7 +134,7 @@ Ext.define('Mdc.controller.setup.Comtasks', {
                         url: '/api/cts/comtasks/' + id,
                         method: 'DELETE',
                         success: function () {
-                            this.getApplication().fireEvent('acknowledge', 'Successfully removed');
+                            self.getApplication().fireEvent('acknowledge', 'Successfully removed');
                             self.store.loadPage(1, {
                                 callback: function () {
                                     grid.getSelectionModel().select(0);
