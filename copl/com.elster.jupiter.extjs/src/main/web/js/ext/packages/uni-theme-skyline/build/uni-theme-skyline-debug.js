e423fd6449ed4e3b546bcb0dabb3923e8c076d5c
Levi Van Zele <Levi.VanZele@elster.com>
2014-06-18 09:18:13 +0200

    Added an overflow Y override on the grid panels, cause it is used in (almost?) all grids.

--- copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/build/uni-theme-skyline-debug.js
+++ copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/build/uni-theme-skyline-debug.js
@@ -716,14 +716,13 @@ Ext.define('ExtThemeNeptune.form.field.HtmlEditor', {
 
 Ext.define('Skyline.grid.Panel', {
     override: 'Ext.grid.Panel',
-//    border: false,
-//    frame: true,
     bodyBorder: true,
     enableColumnHide: false,
     enableColumnMove: false,
     enableColumnResize: false,
     sortableColumns: false,
     collapsible: false,
+    overflowY: 'auto',
     selModel: {
         mode: 'SINGLE'
     }
--- copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/grid/Panel.js
+++ copl/com.elster.jupiter.extjs/src/main/web/js/ext/packages/uni-theme-skyline/overrides/grid/Panel.js
@@ -1,13 +1,12 @@
 Ext.define('Skyline.grid.Panel', {
     override: 'Ext.grid.Panel',
-//    border: false,
-//    frame: true,
     bodyBorder: true,
     enableColumnHide: false,
     enableColumnMove: false,
     enableColumnResize: false,
     sortableColumns: false,
     collapsible: false,
+    overflowY: 'auto',
     selModel: {
         mode: 'SINGLE'
     }
