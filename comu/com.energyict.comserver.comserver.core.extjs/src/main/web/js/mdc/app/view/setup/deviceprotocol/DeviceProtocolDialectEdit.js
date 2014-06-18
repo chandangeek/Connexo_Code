ba86f84c3f6d73d3bd9fa3e0255a12529db43499
sla <silvie.labis@elster.com>
2014-06-18 11:56:32 +0200

    JP-3476 Develop View protocol dialects on device

---
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/deviceprotocol/DeviceProtocolDialectEdit.js
@@ -0,0 +1,134 @@
+Ext.define('Mdc.view.setup.deviceprotocol.DeviceProtocolDialectEdit', {
+    extend: 'Uni.view.container.ContentContainer',
+    alias: 'widget.deviceProtocolDialectEdit',
+    itemId: 'deviceProtocolDialectEdit',
+    edit: false,
+    mRID: null,
+
+    isEdit: function () {
+        return this.edit;
+    },
+
+    setEdit: function (edit, returnLink) {
+        if (edit) {
+            this.edit = edit;
+            this.down('#addEditButton').setText(Uni.I18n.translate('general.save', 'MDC', 'Save'));
+            this.down('#addEditButton').action = 'editDeviceProtocolDialect';
+        } else {
+            this.edit = edit;
+            this.down('#addEditButton').setText(Uni.I18n.translate('general.add', 'MDC', 'Add'));
+            this.down('#addEditButton').action = 'addDeviceProtocolDialect';
+        }
+        this.down('#cancelLink').href = returnLink;
+    },
+
+    initComponent: function () {
+        var me = this;
+        this.content = [
+            {
+                xtype: 'container',
+                layout: {
+                    type: 'vbox'
+                },
+
+                items: [
+                    {
+                        xtype: 'component',
+                        html: '',
+                        itemId: 'deviceProtocolDialectEditAddTitle'
+                    },
+                    {
+                        xtype: 'form',
+                        border: false,
+                        itemId: 'deviceProtocolDialectEditForm',
+                        layout: {
+                            type: 'vbox',
+                            align: 'stretch'
+                        },
+                        defaults: {
+                            labelWidth: 250
+                        },
+                        items: [
+                        ]
+                    },
+                    {
+                        xtype: 'form',
+                        border: false,
+                        itemId: 'editProtocolDialectsDetailsTitle',
+                        hidden: true,
+                        layout: {
+                            type: 'vbox',
+                            align: 'stretch'
+                        },
+                        defaults: {
+                            labelWidth: 250
+                        },
+                        items: [
+                            {
+                                xtype: 'displayfield',
+                                fieldLabel: '<h3>' + Uni.I18n.translate('protocolDialect.protocolDialectDetails', 'MDC', 'Protocol dialect details') + '</h3>',
+                                text: ''
+                            }
+                        ]
+                    },
+                    {
+                        xtype: 'propertyEdit',
+                        width: '100%'
+                    },
+                    {
+                        xtype: 'form',
+                        border: false,
+                        itemId: 'deviceProtocolDialectEditButtonsForm',
+                        layout: {
+                            type: 'vbox',
+                            align: 'stretch'
+                        },
+                        width: '100%',
+                        defaults: {
+                            labelWidth: 250
+                        },
+                        items: [
+                            {
+                                xtype: 'fieldcontainer',
+                                ui: 'actions',
+                                fieldLabel: '&nbsp',
+                                items: [
+                                    {
+                                        text: Uni.I18n.translate('general.save', 'MDC', 'Save'),
+                                        xtype: 'button',
+                                        ui: 'action',
+                                        action: 'createAction',
+                                        itemId: 'addEditButton'
+                                    },
+                                    {
+                                        text: Uni.I18n.translate('general.cancel', 'MDC', 'Cancel'),
+                                        xtype: 'button',
+                                        ui: 'link',
+                                        itemId: 'cancelLink',
+                                        href: '#/devices/' + this.mRID + '/'
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        ;
+        this.callParent(arguments);
+        if (this.isEdit()) {
+            this.down('#addEditButton').setText(Uni.I18n.translate('general.save', 'MDC', 'Save'));
+            this.down('#addEditButton').action = 'editDeviceProtocolDialect';
+        } else {
+            this.down('#addEditButton').setText(Uni.I18n.translate('general.add', 'MDC', 'Add'));
+            this.down('#addEditButton').action = 'createDeviceProtocolDialect';
+        }
+        this.down('#cancelLink').href = this.returnLink;
+
+    }
+
+})
+;
+
+
+
