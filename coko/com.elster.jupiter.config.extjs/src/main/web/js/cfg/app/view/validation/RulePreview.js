bc695534d44bdb29d2a3e486ac4676d60ce4e7e9
Levi Van Zele <Levi.VanZele@elster.com>
2014-06-23 14:28:42 +0200

    [JP-3436] Made the validation rule sets visible on the device configuration page.

--- coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/app/view/validation/RulePreview.js
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/app/view/validation/RulePreview.js
@@ -44,9 +44,7 @@ Ext.define('Cfg.view.validation.RulePreview', {
                     html: '<H5>' + Uni.I18n.translate('validation.selectRule', 'MDC', 'Select a rule to see its details') + '</H5>'
                 }
             ]
-
         },
-
         {
             xtype: 'form',
             border: false,
@@ -55,43 +53,36 @@ Ext.define('Cfg.view.validation.RulePreview', {
                 type: 'vbox'
             },
 
+            defaults: {
+                xtype: 'displayfield',
+                labelWidth: 250
+            },
 
             items: [
                 {
-                    xtype: 'displayfield',
                     name: 'name',
-                    fieldLabel: Uni.I18n.translate('validation.name', 'CFG', 'Name'),
-                    labelAlign: 'right',
-                    labelWidth: 250
+                    fieldLabel: Uni.I18n.translate('validation.name', 'CFG', 'Name')
                 },
                 {
-                    xtype: 'displayfield',
                     name: 'displayName',
-                    fieldLabel: Uni.I18n.translate('validation.Rule', 'CFG', 'Rule'),
-                    labelAlign: 'right',
-                    labelWidth: 250
+                    fieldLabel: Uni.I18n.translate('validation.Rule', 'CFG', 'Rule')
                 },
                 {
-                    xtype: 'displayfield',
                     name: 'active',
                     fieldLabel: Uni.I18n.translate('validation.active', 'CFG', 'Active'),
-                    labelAlign: 'right',
-                    labelWidth: 250,
                     renderer: function (value) {
                         if (value) {
-                            return Uni.I18n.translate('general.yes', 'CFG', 'Yes')
+                            return Uni.I18n.translate('general.yes', 'CFG', 'Yes');
                         } else {
-                            return Uni.I18n.translate('general.no', 'CFG', 'No')
+                            return Uni.I18n.translate('general.no', 'CFG', 'No');
                         }
                     }
                 },
-
                 {
                     xtype: 'container',
                     itemId: 'readingTypesArea',
                     items: []
                 },
-                ,
                 {
                     xtype: 'container',
                     margin: '5 0 0 0',
