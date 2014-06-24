5eaf24af1f50513942b3bc11ee0f2a9a60f0ba16
Bruno Ballekens <bruno.ballekens@elster.com>
2014-06-24 16:16:47 +0200

    JP-3616 - Develop Validate data on store - better handle exceptions during installation

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/InstallerImpl.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/InstallerImpl.java
@@ -1,15 +1,22 @@
 package com.elster.jupiter.validation.impl;
 
 import com.elster.jupiter.events.EventService;
-import com.elster.jupiter.nls.*;
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsKey;
+import com.elster.jupiter.nls.SimpleNlsKey;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.Translation;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.validation.ValidationService;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class InstallerImpl {
+    private static final Logger LOGGER = Logger.getLogger(InstallerImpl.class.getName());
 
     private final DataModel dataModel;
     private final EventService eventService;
@@ -22,24 +29,36 @@ public class InstallerImpl {
     }
 
     public void install(boolean executeDdl, boolean updateOrm) {
-        dataModel.install(executeDdl, updateOrm);
+        try {
+            dataModel.install(executeDdl, updateOrm);
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Could not install datamodel : " + ex.getMessage(), ex);
+        }
         setTranslations();
         createEventTypes();
     }
 
     private void createEventTypes() {
         for (EventType eventType : EventType.values()) {
-            eventType.install(eventService);
+            try {
+                eventType.install(eventService);
+            } catch (Exception ex) {
+                LOGGER.log(Level.SEVERE, "Could not install eventType '" + eventType.name() + "': " + ex.getMessage(), ex);
+            }
         }
     }
 
-    private void setTranslations(){
+    private void setTranslations() {
         List<Translation> translations = new ArrayList<>(MessageSeeds.values().length);
         for (MessageSeeds messageSeed : MessageSeeds.values()) {
             SimpleNlsKey nlsKey = SimpleNlsKey.key(ValidationService.COMPONENTNAME, Layer.DOMAIN, messageSeed.getKey()).defaultMessage(messageSeed.getDefaultFormat());
             translations.add(toTranslation(nlsKey, Locale.ENGLISH, messageSeed.getDefaultFormat()));
         }
-        thesaurus.addTranslations(translations);
+        try {
+            thesaurus.addTranslations(translations);
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Could not install translations : " + ex.getMessage(), ex);
+        }
     }
 
     private Translation toTranslation(final SimpleNlsKey nlsKey, final Locale locale, final String translation) {
