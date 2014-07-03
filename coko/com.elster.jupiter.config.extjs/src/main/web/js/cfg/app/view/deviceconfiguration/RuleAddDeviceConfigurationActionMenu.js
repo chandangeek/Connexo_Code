2804de013b7f6e83b647d0fd78f5e90c914e975f
Maksim Barinov <mbarinov@mera.ru>
2014-07-03 17:27:18 +0400

    Third commit

---
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/app/view/deviceconfiguration/RuleAddDeviceConfigurationActionMenu.js
@@ -0,0 +1,12 @@
+Ext.define('Cfg.view.deviceconfiguration.RuleAddDeviceConfigurationActionMenu', {
+    extend: 'Ext.menu.Menu',
+    alias: 'widget.rule-add-device-configuration-action-menu',
+    plain: true,
+    border: false,
+    itemId: 'add-device-configuration-action-menu',
+    shadow: false,
+    items: [
+
+    ]
+});
+
