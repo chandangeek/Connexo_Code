203fd36d4c5f950a9ec2744a40904862d7e82044
Sentyurin Sergey <ssentyur@mera.ru>
2014-07-03 18:09:25 +0400

    Bug fixing: JP-3955 The left menu is required

---
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/comportpool/SubMenu.js
@@ -0,0 +1,35 @@
+Ext.define('Mdc.view.setup.comportpool.SubMenu', {
+    extend: 'Uni.view.navigation.SubMenu',
+    alias: 'widget.comportpoolsubmenu',
+    ui: 'side-menu',
+    items: [
+        {
+            text: Uni.I18n.translate('general.overview', 'MDC', 'Overview'),
+            itemId: 'comPortPoolOverviewLink',
+            href: '#/administration/comportpools/{0}/overview',
+            hrefTarget: '_self'
+        },
+        {
+            text: Uni.I18n.translate('comserver.preview.communicationPortsLabel', 'MDC', 'Communication ports'),
+            itemId: 'comPortPoolComPortsLink',
+            href: '#/administration/comportpools/{0}/comports',
+            hrefTarget: '_self'
+        }
+    ],
+
+    setServer: function (model) {
+        var id = model.getId(),
+            name = model.get('name'),
+            currentHash = window.location.hash;
+
+        this.setTitle(name);
+
+        Ext.Array.each(this.query('menuitem'), function (item) {
+            var href = item.href,
+                formatHref = Ext.String.format(href, id);
+
+            item.setHref(formatHref);
+            (currentHash == formatHref) && item.addCls('current');
+        });
+    }
+});
