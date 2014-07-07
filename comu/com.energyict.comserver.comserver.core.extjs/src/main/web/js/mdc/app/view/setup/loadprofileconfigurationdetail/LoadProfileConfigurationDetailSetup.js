7262c34b2e40747e158f98492c013aa717654a41
Levi Van Zele <Levi.VanZele@elster.com>
2014-07-07 15:29:03 +0200

    Removed some of the unnecessary component layers.

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/loadprofileconfigurationdetail/LoadProfileConfigurationDetailSetup.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/loadprofileconfigurationdetail/LoadProfileConfigurationDetailSetup.js
@@ -1,105 +1,98 @@
 Ext.define('Mdc.view.setup.loadprofileconfiguration.LoadProfileConfigurationDetailSetup', {
-    extend: 'Uni.view.container.ContentContainer',
-    alias: 'widget.loadProfileConfigurationDetailSetup',
+    extend: 'Ext.container.Container',
+    xtype: 'loadProfileConfigurationDetailSetup',
+
     intervalStore: null,
     deviceTypeId: null,
     deviceConfigId: null,
     loadProfileConfigurationId: null,
     autoDestroy: false,
+    padding: '0 10 0 10',
 
-    content: [
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+
+    items: [
+        {
+            xtype: 'component',
+            margins: '10 10 10 10',
+            itemId: 'loadProfileConfigurationDetailTitle'
+        },
         {
             xtype: 'container',
-            cls: 'content-container',
+            itemId: 'loadProfileConfigurationDetailInfoContainer'
+        },
+        {
+            xtype: 'component',
+            margins: '10 10 10 10',
+            itemId: 'loadProfileConfigurationDetailChannelConfigurationTitle'
+        },
+        {
+            xtype: 'container',
+            itemId: 'loadProfileConfigurationDetailDockedItems'
+        },
+        {
+            xtype: 'container',
+            itemId: 'emptyPanel',
+            hidden: true,
             layout: {
-                type: 'vbox',
-                align: 'stretch'
+                type: 'hbox',
+                align: 'left'
             },
-            padding: '0 10 0 10',
+            minHeight: 20,
             items: [
                 {
-                    xtype: 'component',
-                    margins: '10 10 10 10',
-                    itemId: 'loadProfileConfigurationDetailTitle'
+                    xtype: 'image',
+                    margin: '0 10 0 0',
+                    src: '../ext/packages/uni-theme-skyline/build/resources/images/shared/icon-info-small.png',
+                    height: 20,
+                    width: 20
                 },
                 {
                     xtype: 'container',
-                    itemId: 'loadProfileConfigurationDetailInfoContainer'
-                },
-                {
-                    xtype: 'component',
-                    margins: '10 10 10 10',
-                    itemId: 'loadProfileConfigurationDetailChannelConfigurationTitle'
-                },
-                {
-                    xtype: 'container',
-                    itemId: 'loadProfileConfigurationDetailDockedItems'
-                },
-                {
-                    xtype: 'container',
-                    itemId: 'emptyPanel',
-                    hidden: true,
-                    layout: {
-                        type: 'hbox',
-                        align: 'left'
-                    },
-                    minHeight: 20,
                     items: [
                         {
-                            xtype: 'image',
-                            margin: '0 10 0 0',
-                            src: '../ext/packages/uni-theme-skyline/build/resources/images/shared/icon-info-small.png',
-                            height: 20,
-                            width: 20
+                            xtype: 'component',
+                            html: '<b>' + Uni.I18n.translate('channelConfigurations.empty.title', 'MDC', 'No channel configurations found') + '</b><br>' +
+                                Uni.I18n.translate('channelConfigurations.empty.detail', 'MDC', 'There are no channel configurations. This could be because:') + '<ul><li>' +
+                                Uni.I18n.translate('channelConfigurations.empty.list.item1', 'MDC', 'No channel configurations have been defined yet.') + '</li><li>' +
+                                Uni.I18n.translate('channelConfigurations.empty.list.item2', 'MDC', 'No channel configurations comply to the filter.') + '</li></ul><br>' +
+                                Uni.I18n.translate('channelConfigurations.empty.steps', 'MDC', 'Possible steps:')
                         },
                         {
                             xtype: 'container',
-                            items: [
-                                {
-                                    xtype: 'component',
-                                    html: '<b>' + Uni.I18n.translate('channelConfigurations.empty.title', 'MDC', 'No channel configurations found') + '</b><br>' +
-                                        Uni.I18n.translate('channelConfigurations.empty.detail', 'MDC', 'There are no channel configurations. This could be because:') + '<lv><li>' +
-                                        Uni.I18n.translate('channelConfigurations.empty.list.item1', 'MDC', 'No channel configurations have been defined yet.') + '</li><li>' +
-                                        Uni.I18n.translate('channelConfigurations.empty.list.item2', 'MDC', 'No channel configurations comply to the filter.') + '</li></lv><br>' +
-                                        Uni.I18n.translate('channelConfigurations.empty.steps', 'MDC', 'Possible steps:')
-                                },
-                                {
-                                    xtype: 'container',
-                                    itemId: 'addchannelconfigurationcontainer'
-                                }
-                            ]
+                            itemId: 'addchannelconfigurationcontainer'
                         }
                     ]
-                },
-//                {
-//                    xtype: 'container',
-//                    itemId: 'loadProfileConfigurationEmptyListContainer'
-//                },
-                {
-                    xtype: 'container',
-                    itemId: 'loadProfileConfigurationDetailChannelGridContainer'
-                },
-                {
-                    xtype: 'menuseparator',
-                    itemId: 'separator'
-                },
-                {
-                    xtype: 'container',
-                    itemId: 'loadProfileConfigurationDetailChannelPreviewContainer'
                 }
             ]
+        },
+        {
+            xtype: 'container',
+            itemId: 'loadProfileConfigurationDetailChannelGridContainer'
+        },
+        {
+            xtype: 'menuseparator',
+            itemId: 'separator'
+        },
+        {
+            xtype: 'container',
+            itemId: 'loadProfileConfigurationDetailChannelPreviewContainer'
         }
     ],
 
-
     initComponent: function () {
         this.callParent(arguments);
+
         this.down('#loadProfileConfigurationDetailInfoContainer').add(
             {
                 xtype: 'loadProfileConfigurationDetailInfo',
                 intervalStore: this.intervalStore
             }
         );
+
         this.down('#loadProfileConfigurationDetailDockedItems').add(
             {
                 xtype: 'loadProfileConfigurationDetailDockedItems',
@@ -108,6 +101,7 @@ Ext.define('Mdc.view.setup.loadprofileconfiguration.LoadProfileConfigurationDeta
                 loadProfileConfigurationId: this.loadProfileConfigurationId
             }
         );
+
         this.down('#addchannelconfigurationcontainer').add(
             {
                 xtype: 'button',
@@ -118,6 +112,7 @@ Ext.define('Mdc.view.setup.loadprofileconfiguration.LoadProfileConfigurationDeta
                 href: '#/administration/devicetypes/' + this.deviceTypeId + '/deviceconfigurations/' + this.deviceConfigId + '/loadprofiles/' + this.loadProfileConfigurationId + '/channels/add'
             }
         );
+
         this.down('#loadProfileConfigurationDetailChannelGridContainer').add(
             {
                 xtype: 'loadProfileConfigurationDetailChannelGrid',
@@ -125,6 +120,7 @@ Ext.define('Mdc.view.setup.loadprofileconfiguration.LoadProfileConfigurationDeta
                 deleteActionName: 'deleteloadprofileconfigurationdetailchannel'
             }
         );
+
         this.down('#loadProfileConfigurationDetailChannelPreviewContainer').add(
             {
                 xtype: 'loadProfileConfigurationDetailChannelPreview',
