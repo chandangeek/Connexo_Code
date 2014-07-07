2169188964771506586e8cc9a2ba065f570699b0
akuryuk <akuryuk@mera.ru>
2014-07-07 13:57:46 +0400

    Form Error Message

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/util/FormErrorMessage.js
@@ -0,0 +1,50 @@
+Ext.define('Uni.util.FormErrorMessage', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.uni-form-error-message',
+    ui: 'form-error-framed',
+    text: null,
+    defaultText: 'There are errors on this page that require your attention.',
+    layout: {
+        type: 'hbox',
+        align: 'middle'
+    },
+    errorIcon: null,
+    defaultErrorIcon: 'x-uni-form-error-msg-icon',
+    margin: '7 0 32 0',
+    beforeRender: function () {
+        var me = this;
+        if (!me.text) {
+            me.text = me.defaultText;
+        }
+        if (!me.errorIcon) {
+            me.errorIcon = me.defaultErrorIcon
+        }
+        me.renew();
+        me.callParent(arguments)
+    },
+
+    renew: function () {
+        var me = this;
+        me.removeAll(true);
+        me.add([
+            {
+                xtype: 'box',
+                height: 22,
+                width: 26,
+                cls: me.errorIcon
+            },
+            {
+                ui: 'form-error',
+                name: 'errormsgpanel',
+                html: me.text
+            }
+        ]);
+    },
+
+    setText: function (text) {
+        var me = this;
+        me.text = text;
+        me.renew();
+    }
+
+});
