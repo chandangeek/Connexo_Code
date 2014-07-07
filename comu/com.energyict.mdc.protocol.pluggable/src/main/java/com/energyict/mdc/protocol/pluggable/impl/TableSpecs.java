227bc06f9f40a94608ed1002209a77f55c9b7847
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-07-07 14:42:47 +0200

    Introduce constants for often used field lengths in TableSpecs

--- comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/TableSpecs.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/TableSpecs.java
@@ -6,6 +6,8 @@ import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.Table;
 import com.energyict.mdc.protocol.pluggable.impl.adapters.common.*;
 
+import static com.elster.jupiter.orm.Table.*;
+
 /**
  * Models the database tables that hold the data of the {@link PluggableClassRelationAttributeTypeUsage}s.
  *
@@ -35,8 +37,8 @@ public enum TableSpecs {
             Table<SecuritySupportAdapterMapping> table = dataModel.addTable(name(), SecuritySupportAdapterMapping.class);
             table.map(SecuritySupportAdapterMappingImpl.class);
             table.cache();
-            Column deviceProtocolClassNameColumn = table.column("deviceprotocoljavaclassname").type("varchar2(255)").notNull().map("deviceProtocolJavaClassName").add();
-            Column securitySupportClassNameColumn = table.column("securitysupportclassname").type("varchar2(255)").notNull().map("securitySupportJavaClassName").add();
+            Column deviceProtocolClassNameColumn = table.column("deviceprotocoljavaclassname").varChar(SHORT_DESCRIPTION_LENGTH).notNull().map("deviceProtocolJavaClassName").add();
+            Column securitySupportClassNameColumn = table.column("securitysupportclassname").varChar(SHORT_DESCRIPTION_LENGTH).notNull().map("securitySupportJavaClassName").add();
             table.primaryKey("PK_PPC_SECSUPPORTADAPTRMAPPING").on(deviceProtocolClassNameColumn, securitySupportClassNameColumn).add();
         }
     },
@@ -47,8 +49,8 @@ public enum TableSpecs {
             Table<MessageAdapterMapping> table = dataModel.addTable(name(), MessageAdapterMapping.class);
             table.map(MessageAdapterMappingImpl.class);
             table.cache();
-            Column deviceProtocolClassNameColumn = table.column("deviceprotocoljavaclassname").type("varchar2(255)").notNull().map("deviceProtocolJavaClassName").add();
-            Column securitySupportClassNameColumn = table.column("messageadapterjavaclassname").type("varchar2(255)").notNull().map("messageAdapterJavaClassName").add();
+            Column deviceProtocolClassNameColumn = table.column("deviceprotocoljavaclassname").varChar(SHORT_DESCRIPTION_LENGTH).notNull().map("deviceProtocolJavaClassName").add();
+            Column securitySupportClassNameColumn = table.column("messageadapterjavaclassname").varChar(SHORT_DESCRIPTION_LENGTH).notNull().map("messageAdapterJavaClassName").add();
             table.primaryKey("PK_PPC_MSGADAPTERMAPPING").on(deviceProtocolClassNameColumn, securitySupportClassNameColumn).add();
         }
     },
@@ -59,7 +61,7 @@ public enum TableSpecs {
             Table<DeviceCapabilityMapping> table = dataModel.addTable(name(), DeviceCapabilityMapping.class);
             table.map(DeviceCapabilityAdapterMappingImpl.class);
             table.cache();
-            Column deviceProtocolClassNameColumn = table.column("deviceprotocoljavaclassname").type("varchar2(255)").notNull().map("deviceProtocolJavaClassName").add();
+            Column deviceProtocolClassNameColumn = table.column("deviceprotocoljavaclassname").varChar(SHORT_DESCRIPTION_LENGTH).notNull().map("deviceProtocolJavaClassName").add();
             Column capabilitiesColumn = table.column("deviceprotocolcapabilities").number().notNull().map("deviceProtocolCapabilities").conversion(ColumnConversion.NUMBER2INT).add();
             table.primaryKey("PK_PPC_CAPADAPTERMAPPING").on(deviceProtocolClassNameColumn, capabilitiesColumn).add();
         }
