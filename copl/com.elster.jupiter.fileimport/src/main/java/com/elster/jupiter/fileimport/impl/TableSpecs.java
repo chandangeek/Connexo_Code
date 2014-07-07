0d46e15bf9058bdbf4bad910de116764e8438849
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-07-07 14:42:50 +0200

    Introduce constants for often used field lengths in TableSpecs

--- copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/impl/TableSpecs.java
+++ copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/impl/TableSpecs.java
@@ -8,6 +8,7 @@ import com.elster.jupiter.orm.DeleteRule;
 import com.elster.jupiter.orm.Table;
 
 import static com.elster.jupiter.orm.ColumnConversion.*;
+import static com.elster.jupiter.orm.Table.*;
 
 enum TableSpecs {
 
@@ -16,12 +17,12 @@ enum TableSpecs {
         void describeTable(Table table) {
             table.map(ImportScheduleImpl.class);
             Column idColumn = table.addAutoIdColumn();
-            table.column("DESTINATION").type("varchar2(80)").notNull().map("destinationName").add();
-            table.column("CRONSTRING").type("varchar2(80)").notNull().map("cronString").add();
-            table.column("IMPORTDIR").type("varchar2(80)").notNull().conversion(CHAR2FILE).map("importDirectory").add();
-            table.column("INPROCESSDIR").type("varchar2(80)").notNull().conversion(CHAR2FILE).map("inProcessDirectory").add();
-            table.column("SUCCESSDIR").type("varchar2(80)").notNull().conversion(CHAR2FILE).map("successDirectory").add();
-            table.column("FAILDIR").type("varchar2(80)").notNull().conversion(CHAR2FILE).map("failureDirectory").add();
+            table.column("DESTINATION").varChar(NAME_LENGTH).notNull().map("destinationName").add();
+            table.column("CRONSTRING").varChar(NAME_LENGTH).notNull().map("cronString").add();
+            table.column("IMPORTDIR").varChar(NAME_LENGTH).notNull().conversion(CHAR2FILE).map("importDirectory").add();
+            table.column("INPROCESSDIR").varChar(NAME_LENGTH).notNull().conversion(CHAR2FILE).map("inProcessDirectory").add();
+            table.column("SUCCESSDIR").varChar(NAME_LENGTH).notNull().conversion(CHAR2FILE).map("successDirectory").add();
+            table.column("FAILDIR").varChar(NAME_LENGTH).notNull().conversion(CHAR2FILE).map("failureDirectory").add();
             table.primaryKey("FIM_PK_IMPORT_SCHEDULE").on(idColumn).add();
         }
 
@@ -32,7 +33,7 @@ enum TableSpecs {
             table.map(FileImportImpl.class);
             Column idColumn = table.addAutoIdColumn();
             Column importScheduleColumn = table.column("IMPORTSCHEDULE").type("number").notNull().conversion(NUMBER2LONG).map("importScheduleId").add();
-            table.column("FILENAME").type("varchar2(80)").notNull().conversion(CHAR2FILE).map("file").add();
+            table.column("FILENAME").varChar(NAME_LENGTH).notNull().conversion(CHAR2FILE).map("file").add();
             table.column("STATE").type("number").notNull().conversion(NUMBER2ENUM).map("state").add();
             table.primaryKey("FIM_PK_FILE_IMPORT").on(idColumn).add();
             table.foreignKey("FIM_FKFILEIMPORT_SCHEDULE").references(FIM_IMPORT_SCHEDULE.name()).onDelete(DeleteRule.CASCADE).map("importSchedule").on(importScheduleColumn).add();
