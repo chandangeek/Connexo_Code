ea174eb95b8bbe894ebea67141ed5b6ef272e552
Artem Puchenkin <apuchenkin@gmail.com>
2014-07-08 17:21:35 +0400

    [JP-3817] Too much white space an the end of the infinite scrolling grid

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/register/RegisterMappingAddGrid.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/register/RegisterMappingAddGrid.js
@@ -26,6 +26,88 @@ Ext.define('Mdc.view.setup.register.RegisterMappingAddGrid', {
     },
     height: 400,
     store: 'AvailableRegisterTypes',
+
+    columns: [
+        {
+            header: Uni.I18n.translate('registerMappings.name', 'MDC', 'Name'),
+            dataIndex: 'name',
+            flex: 3,
+            sortable: false,
+            fixed: true,
+            hideable: false
+        },
+        {
+            xtype: 'actioncolumn',
+            renderer: function (value, metaData, record) {
+                return '<div style="float:left; font-size: 13px; line-height: 1em;">'
+                    + record.getReadingType().get('mrid') + '&nbsp' + '&nbsp'
+                    + '</div>'
+            },
+            header: Uni.I18n.translate('registerMappings.readingType', 'MDC', 'Reading type'),
+            flex: 2,
+            items: [
+                {
+                    icon: '../mdc/resources/images/info.png',
+                    iconCls: 'uni-info-icon',
+                    tooltip: Uni.I18n.translate('readingType.tooltip','MDC','Reading type info'),
+                    handler: function (grid, rowIndex, colIndex, item, e, record, row) {
+                        //var record = grid.getStore().getAt(rowIndex);
+                        this.fireEvent('showReadingTypeInfo', record);
+                    }
+                }
+            ],
+            tdCls: 'view',
+            sortable: false,
+            fixed: true,
+            hideable: false
+        },
+        {
+            header: Uni.I18n.translate('registerMappings.obisCode', 'MDC', 'OBIS code'),
+            dataIndex: 'obisCode',
+            flex: 1,
+            sortable: false,
+            fixed: true,
+            hideable: false
+        },
+        {
+            header: Uni.I18n.translate('registerMappings.type', 'MDC', 'Type'),
+            renderer: function (value, metaData, record) {
+                return '<div style="float:left; font-size: 13px; line-height: 1em;">'
+                    + record.getReadingType().get('measurementKind')
+                    + '</div>'
+            },
+            flex: 1,
+            sortable: false,
+            fixed: true,
+            hideable: false
+        }
+    ],
+
+    tbar: {
+        xtype: 'pagingtoolbartop',
+        store: this.store,
+        itemId: 'pagingt',
+        displayMsg: this.nbrOfSelectedItems + ' ' + Uni.I18n.translatePlural('registerMappingsAdd.pagingtoolbartop.displayMsg', this.nbrOfSelectedItems, 'MDC', ' register types selected'),
+        items: [
+            {
+                xtype: 'component',
+                flex: 1
+            },
+            {
+                xtype: 'button',
+                text: Uni.I18n.translate('registerMappings.manageRegisterTypes', 'MDC', 'Manage register types'),
+                ui: 'link',
+                listeners: {
+                    click: {
+                        fn: function () {
+                            window.location.href = '#/administration/registertypes';
+                        }
+                    }
+                }
+            }
+        ]
+    },
+
     initComponent: function () {
         var me = this;
         var store = Ext.data.StoreManager.lookup('AvailableRegisterTypes');
@@ -33,88 +115,6 @@ Ext.define('Mdc.view.setup.register.RegisterMappingAddGrid', {
             property:'available',
             value:true
         }]));
-        this.columns = [
-            {
-                header: Uni.I18n.translate('registerMappings.name', 'MDC', 'Name'),
-                dataIndex: 'name',
-                flex: 3,
-                sortable: false,
-                fixed: true,
-                hideable: false
-            },
-            {
-                xtype: 'actioncolumn',
-                renderer: function (value, metaData, record) {
-                    return '<div style="float:left; font-size: 13px; line-height: 1em;">'
-                        + record.getReadingType().get('mrid') + '&nbsp' + '&nbsp'
-                        + '</div>'
-                },
-                header: Uni.I18n.translate('registerMappings.readingType', 'MDC', 'Reading type'),
-                flex: 2,
-                items: [
-                    {
-                        icon: '../mdc/resources/images/info.png',
-                        iconCls: 'uni-info-icon',
-                        tooltip: Uni.I18n.translate('readingType.tooltip','MDC','Reading type info'),
-                        handler: function (grid, rowIndex, colIndex, item, e, record, row) {
-                            //var record = grid.getStore().getAt(rowIndex);
-                            this.fireEvent('showReadingTypeInfo', record);
-                        }
-                    }
-                ],
-                tdCls: 'view',
-                sortable: false,
-                fixed: true,
-                hideable: false
-            },
-            {
-                header: Uni.I18n.translate('registerMappings.obisCode', 'MDC', 'OBIS code'),
-                dataIndex: 'obisCode',
-                flex: 1,
-                sortable: false,
-                fixed: true,
-                hideable: false
-            },
-            {
-                header: Uni.I18n.translate('registerMappings.type', 'MDC', 'Type'),
-                renderer: function (value, metaData, record) {
-                    return '<div style="float:left; font-size: 13px; line-height: 1em;">'
-                        + record.getReadingType().get('measurementKind')
-                        + '</div>'
-                },
-                flex: 1,
-                sortable: false,
-                fixed: true,
-                hideable: false
-            }
-        ];
-        this.dockedItems = [
-            {
-                xtype: 'pagingtoolbartop',
-                store: this.store,
-                dock: 'top',
-                itemId: 'pagingt',
-                displayMsg: this.nbrOfSelectedItems + ' ' + Uni.I18n.translatePlural('registerMappingsAdd.pagingtoolbartop.displayMsg', this.nbrOfSelectedItems, 'MDC', ' register types selected'),
-                items: [
-                    {
-                        xtype: 'component',
-                        flex: 1
-                    },
-                    {
-                        xtype: 'button',
-                        text: Uni.I18n.translate('registerMappings.manageRegisterTypes', 'MDC', 'Manage register types'),
-                        ui: 'link',
-                        listeners: {
-                            click: {
-                                fn: function () {
-                                    window.location.href = '#/administration/registertypes';
-                                }
-                            }
-                        }
-                    }
-                ]
-            }
-        ];
 
         this.callParent();
     }
