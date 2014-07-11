23dcb50d22a465c6a7741ca1603ebfceae1964d2
Dragos Sinca <dragos.sinca@elster.com>
2014-07-11 11:44:20 +0300

    Added details to the empty grid

--- copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/app/view/instance/Browse.js
+++ copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/app/view/instance/Browse.js
@@ -25,8 +25,33 @@ Ext.define('Bpm.view.instance.Browse', {
                         xtype: 'instanceList'
                     },
                     emptyComponent: {
-                        xtype: 'component',
-                        html: 'There are no processes'
+                        xtype: 'container',
+                        layout: {
+                            type: 'hbox',
+                            align: 'left'
+                        },
+                        minHeight: 20,
+                        items: [
+                            {
+                                xtype: 'image',
+                                margin: '0 10 0 0',
+                                src: "../mdc/resources/images/information.png",
+                                height: 20,
+                                width: 20
+                            },
+                            {
+                                xtype: 'container',
+                                items: [
+                                    {
+                                        xtype: 'component',
+                                        html: '<b>' + Uni.I18n.translate('bpm.instance.empty.title', 'BPM', 'No processes found') + '</b><br>' +
+                                            Uni.I18n.translate('bpm.instance.empty.detail', 'BPM', 'There are no processes. This could be because:') + '<lv><li>&nbsp&nbsp' +
+                                            Uni.I18n.translate('bpm.instance.empty.list.item1', 'BPM', 'BPM engine cannot be reached.') + '<lv><li>&nbsp&nbsp' +
+                                            Uni.I18n.translate('bpm.instance.empty.list.item2', 'BPM', 'No processes have been started yet.')
+                                    }
+                                ]
+                            }
+                        ]
                     },
                     previewComponent: {
                         xtype: 'instanceDetails'
