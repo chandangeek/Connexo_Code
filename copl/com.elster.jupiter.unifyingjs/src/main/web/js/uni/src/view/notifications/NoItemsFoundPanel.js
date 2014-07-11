3ed2991db9a2b1b17d2baad515130452a99641f1
Levi Van Zele <Levi.VanZele@elster.com>
2014-07-11 09:52:35 +0200

    [JP-975] Wrote extra documentation on how to use the component correctly.

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/notifications/NoItemsFoundPanel.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/notifications/NoItemsFoundPanel.js
@@ -1,18 +1,93 @@
 /**
  * @class Uni.view.notifications.NoItemsFoundPanel
+ *
+ * The no items found panel is primarily meant to be shown as a configuration for
+ * {@link Uni.view.container.PreviewContainer#emptyComponent empty components} in a
+ * {@link Uni.view.container.PreviewContainer}. It can also be used independently for whichever
+ * use-case needs it.
+ *
+ *     @example
+ *     xtype: 'preview-container',
+ *     grid: {
+ *         xtype: 'my-favorite-grid',
+ *     },
+ *     emptyComponent: {
+ *         xtype: 'no-items-found-panel',
+ *         title: 'No favorite items found',
+ *         reasons: [
+ *             'No favorite items have been defined yet.',
+ *             'No favorite items comply to the filter.'
+ *         ],
+ *         stepItems: [
+ *             {
+ *                 text: 'Add item',
+ *                 action: 'addItem'
+ *             }
+ *         ]
+ *     },
+ *     previewComponent: {
+ *         xtype: 'my-favorite-preview'
+ *     }
  */
 Ext.define('Uni.view.notifications.NoItemsFoundPanel', {
     extend: 'Ext.container.Container',
     xtype: 'no-items-found-panel',
 
+    /**
+     * @cfg {String}
+     *
+     * Title to be shown on the panel.
+     */
     title: Uni.I18n.translate('notifications.NoItemsFoundPanel.title', 'UNI', 'No items found'),
 
+    /**
+     * @cfg {String}
+     *
+     * Text shown above the reasons.
+     */
     reasonsText: Uni.I18n.translate('notifications.NoItemsFoundPanel.reasonsText', 'UNI', 'This could be because:'),
 
+    /**
+     * @cfg {String[]/String}
+     *
+     * An array of reasons formatted as string. A single string value is also
+     * supported. If no reasons are given, the reasons section of the panel is
+     * not shown to the user.
+     *
+     *     @example
+     *     reasons = [
+     *         'No items have been defined yet.',
+     *         'No items comply to the filter.'
+     *     ]
+     */
     reasons: [],
 
+    /**
+     * @cfg {String}
+     *
+     * Text shown above the step components.
+     */
     stepsText: Uni.I18n.translate('notifications.NoItemsFoundPanel.stepsText', 'UNI', 'Possible steps:'),
 
+    /**
+     * @cfg {Object[]/Object}
+     *
+     * Configuration objects for the items that need to be added for possible
+     * steps to take if there are no items found. By default an item configuration
+     * is assumed to be a button, but any component configuration is possible.
+     *
+     *     @example
+     *     stepItems = [
+     *         {
+     *             text: 'Add item',
+     *             action: 'addItem'
+     *         },
+     *         {
+     *             text: 'Import item',
+     *             action: 'importItem'
+     *         }
+     *     ]
+     */
     stepItems: [],
 
     layout: {
@@ -43,6 +118,7 @@ Ext.define('Uni.view.notifications.NoItemsFoundPanel', {
 
         if (Ext.isArray(me.reasons) || Ext.isString(me.reasons)) {
             var formattedReasons = me.formatReasons(me.reasons);
+
             wrapper.add({
                 xtype: 'component',
                 html: formattedReasons
