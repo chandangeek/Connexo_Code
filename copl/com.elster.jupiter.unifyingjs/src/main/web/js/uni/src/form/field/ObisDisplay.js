67778c3ae3f5e430f6a0ad369c101fa087c3ccda
Artem Puchenkin <apuchenkin@gmail.com>
2014-07-15 11:59:40 +0400

    [JP-4106] fixes

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/form/field/ObisDisplay.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/form/field/ObisDisplay.js
@@ -3,8 +3,7 @@
  */
 Ext.define('Uni.form.field.ObisDisplay', {
     extend: 'Ext.form.field.Display',
-    alias: 'widget.obis-displayfield',
-    itemId: 'obis-displayfield',
+    xtype: 'obis-displayfield',
     name: 'obisCode',
     cls: 'obisCode',
     fieldLabel: Uni.I18n.translate('obis.label', 'UNI', 'OBIS code'),
