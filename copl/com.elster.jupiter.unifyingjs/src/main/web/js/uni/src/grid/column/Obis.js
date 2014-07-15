24c4e0ad874dcb46135d2eaedebf8a8c48e8dba2
Artem Puchenkin <apuchenkin@gmail.com>
2014-07-15 15:17:05 +0400

    itemId removed

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/column/Obis.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/column/Obis.js
@@ -4,7 +4,6 @@
 Ext.define('Uni.grid.column.Obis', {
     extend: 'Ext.grid.column.Column',
     alias: 'widget.obis-column',
-    itemId: 'obis-column',
     header: Uni.I18n.translate('obis.label', 'UNI', 'OBIS code'),
     width: 140,
     align: 'left'
