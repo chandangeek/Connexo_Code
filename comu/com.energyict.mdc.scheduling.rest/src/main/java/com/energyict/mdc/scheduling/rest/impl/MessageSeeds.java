cfa9cdd301c97cb89878f3798cd0b4570d0635ed
bvn <Bart.Vansegbroeck@elster.com>
2014-07-17 10:51:15 +0200

    JP-2025: removed component name prefix in message seed keys

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/MessageSeeds.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/MessageSeeds.java
@@ -1,12 +1,11 @@
 package com.energyict.mdc.scheduling.rest.impl;
 
 import com.elster.jupiter.util.exception.MessageSeed;
-import com.energyict.mdc.scheduling.SchedulingService;
 import java.util.logging.Level;
 
 public enum MessageSeeds implements MessageSeed {
-    INVALID_VALUE(1, "SCR.InvalidValue", "Invalid value"),
-    CAN_NOT_BE_EMPTY(2, "SCR.CanNotBeEmpty", "Field can not be empty"),
+    INVALID_VALUE(1, "InvalidValue", "Invalid value"),
+    CAN_NOT_BE_EMPTY(2, "CanNotBeEmpty", "Field can not be empty"),
     ;
 
     public static final String COMPONENT_NAME = "SCR";
@@ -18,17 +17,9 @@ public enum MessageSeeds implements MessageSeed {
 
     private MessageSeeds(int number, String key, String format) {
         this.number = number;
-        this.key = stripComponentNameIfPresent(key);
+        this.key = key;
         this.format = format;
         this.level = Level.SEVERE;
-    }
-
-    private String stripComponentNameIfPresent(String key) {
-        if (key.startsWith(SchedulingService.COMPONENT_NAME+".")) {
-            return key.substring(SchedulingService.COMPONENT_NAME.length()+1);
-        } else {
-            return key;
-        }
     }
 
     @Override
