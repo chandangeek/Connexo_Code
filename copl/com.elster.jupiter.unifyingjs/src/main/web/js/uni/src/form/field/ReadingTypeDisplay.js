2ee9ad79ebf2e90fa7314ae6408c6070ec69b4b4
Artem Puchenkin <apuchenkin@gmail.com>
2014-07-18 10:57:30 +0400

    [JP-4107] Create common Reading type component
    Squashed commit of the following:
    
    commit 6ce7eccec2a0bbfa72b8450df3e49c559a6bf3de
    Author: Artem Puchenkin <apuchenkin@gmail.com>
    Date:   Fri Jul 18 10:52:11 2014 +0400
    
        [JP-4107] Create common Reading type component
    
    commit c0080abb075ddc7812b6fdb5ea51f1f750605ee4
    Merge: bb857e1 2a37513
    Author: Artem Puchenkin <apuchenkin@gmail.com>
    Date:   Fri Jul 18 09:35:37 2014 +0400
    
        Merge remote-tracking branch 'origin/master' into JP-4107
    
    commit bb857e1e6c6961eae12833566db91f34b1953254
    Author: Artem Puchenkin <apuchenkin@gmail.com>
    Date:   Thu Jul 17 18:03:39 2014 +0400
    
        [JP-4107] Create common Reading type component
    
    commit 0c65788b46a5389888a10639c5b5cef78c327e95
    Author: Artem Puchenkin <apuchenkin@gmail.com>
    Date:   Wed Jul 16 15:34:39 2014 +0400
    
        ReadingType Init

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/form/field/ReadingTypeDisplay.js
@@ -0,0 +1,48 @@
+/**
+ * @class Uni.form.field.ObisDisplay
+ */
+Ext.define('Uni.form.field.ReadingTypeDisplay', {
+    extend: 'Ext.form.field.Display',
+    xtype: 'reading-type-displayfield',
+    name: 'readingType',
+    fieldLabel: Uni.I18n.translate('readingType.label', 'UNI', 'Reading type'),
+    emptyText: '-',
+
+    requires: [
+        'Ext.button.Button'
+    ],
+
+    deferredRenderer: function (value, field) {
+        var me = this;
+
+        new Ext.button.Button({
+            renderTo: field.getEl().down('.x-form-display-field'),
+            tooltip: Uni.I18n.translate('readingType.tooltip', 'UNI', 'Reading type info'),
+            iconCls: 'icon-info-small',
+            cls: 'uni-btn-transparent',
+            style: {
+                display: 'inline-block',
+                "text-decoration": 'none !important'
+            },
+            handler: function() {
+                me.handler(value);
+            }
+        });
+    },
+
+    handler: function (value) {
+        var widget = Ext.widget('readingTypeDetails');
+
+        widget.down('form').getForm().setValues(value);
+        widget.show();
+    },
+
+    renderer: function (value, field) {
+        if (!value) {
+            return this.emptyText;
+        }
+
+        Ext.defer(this.deferredRenderer, 1, this, [value, field]);
+        return '<span style="display: inline-block; width: 230px; float: left;">' + value.mrid + '</span>';
+    }
+});
---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/column/ReadingType.js
@@ -0,0 +1,33 @@
+/**
+ * @class Uni.grid.column.ReadingType
+ */
+Ext.define('Uni.grid.column.ReadingType', {
+    extend: 'Ext.grid.column.Column',
+    xtype: 'reading-type-column',
+    header: Uni.I18n.translate('readingType.label', 'UNI', 'Reading type'),
+    minWidth: 280,
+    align: 'left',
+
+    requires: [
+        'Ext.panel.Tool',
+        'Ext.util.Point',
+        'Uni.view.window.ReadingTypeDetails',
+        'Uni.form.field.ReadingTypeDisplay'
+    ],
+
+    deferredRenderer: function (value, record, view) {
+        var me = this;
+        var cmp = view.getCell(record, me).down('.x-grid-cell-inner');
+        var field = new Uni.form.field.ReadingTypeDisplay({
+            fieldLabel: false
+        });
+        cmp.setHTML('');
+        field.setValue(value);
+        field.render(cmp);
+    },
+
+    renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
+        var me = metaData.column;
+        Ext.defer(me.deferredRenderer, 10, me, [value, record, view]);
+    }
+});
