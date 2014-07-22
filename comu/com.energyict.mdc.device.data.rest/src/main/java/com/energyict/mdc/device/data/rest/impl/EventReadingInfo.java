223edebbebc28462118dc43186c59141470f35e4
Dmitry Vikhrev <vikhrev@mera.ru>
2014-07-22 15:06:01 +0200

    JP-4272 View text register report

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/EventReadingInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/EventReadingInfo.java
@@ -1,15 +1,20 @@
 package com.energyict.mdc.device.data.rest.impl;
 
 import com.elster.jupiter.util.time.Interval;
+import com.elster.jupiter.util.units.Unit;
 import com.energyict.mdc.device.data.EventReading;
 import com.energyict.mdc.device.data.Register;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.math.BigDecimal;
 
 public class EventReadingInfo extends ReadingInfo<EventReading> {
     @JsonProperty("value")
     public BigDecimal value;
+    @JsonProperty("unitOfMeasure")
+    @XmlJavaTypeAdapter(ReadingUnitAdapter.class)
+    public Unit unitOfMeasure;
     @JsonProperty("interval")
     public IntervalInfo interval;
 
@@ -18,6 +23,7 @@ public class EventReadingInfo extends ReadingInfo<EventReading> {
     public EventReadingInfo(EventReading reading, Register register) {
         super(reading, register);
         this.value = reading.getQuantity().getValue();
+        this.unitOfMeasure = reading.getQuantity().getUnit();
         if(reading.getInterval().isPresent()) {
             this.interval = new IntervalInfo(reading.getInterval().get());
         }
--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/NumericalReadingInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/NumericalReadingInfo.java
@@ -1,9 +1,11 @@
 package com.energyict.mdc.device.data.rest.impl;
 
+import com.elster.jupiter.util.units.Unit;
 import com.energyict.mdc.device.data.NumericalReading;
 import com.energyict.mdc.device.data.Register;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.math.BigDecimal;
 
 public class NumericalReadingInfo extends ReadingInfo<NumericalReading> {
@@ -11,6 +13,9 @@ public class NumericalReadingInfo extends ReadingInfo<NumericalReading> {
     public BigDecimal value;
     @JsonProperty("rawValue")
     public BigDecimal rawValue;
+    @JsonProperty("unitOfMeasure")
+    @XmlJavaTypeAdapter(ReadingUnitAdapter.class)
+    public Unit unitOfMeasure;
 
     public NumericalReadingInfo() {}
 
@@ -18,5 +23,6 @@ public class NumericalReadingInfo extends ReadingInfo<NumericalReading> {
         super(reading, register);
         this.value = reading.getQuantity().getValue();
         this.rawValue = reading.getQuantity().getValue();
+        this.unitOfMeasure = reading.getQuantity().getUnit();
     }
 }
