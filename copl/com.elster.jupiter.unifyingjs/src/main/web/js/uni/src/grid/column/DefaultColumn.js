8c23aceff3fe817001b50ddd6f6ec462a9ec7a87
sla <silvie.labis@elster.com>
2014-07-22 15:24:47 +0200

    highlight default column icon

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/column/DefaultColumn.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/column/DefaultColumn.js
@@ -4,14 +4,15 @@
 Ext.define('Uni.grid.column.DefaultColumn', {
     extend: 'Ext.grid.column.Column',
     xtype: 'default-column',
-    header: Uni.I18n.translate('obis.label', 'UNI', 'OBIS code'),
+    header: Uni.I18n.translate('general.default', 'UNI', 'Default'),
     minWidth: 120,
-    align: 'left',
+    align: 'center',
 
     renderer: function (value, metadata) {
         if (value === true) {
-            metadata.style = "padding: 6px 16px 6px 16px;";
-            return '<img src="../ext/packages/uni-theme-skyline/resources/images/grid/defaultItem.png">';
+           //TODO change this code so when hovering of selecting the row, the hightlighted icon is shown.
+            // icon that should be shown in that case: defaultItemHighlight.png
+            return '<div style="background-image: url(../ext/packages/uni-theme-skyline/resources/images/grid/defaultItem.png); height: 16px; width: 16px; "> </div>';
         } else {
             return '';
         }
