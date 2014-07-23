83a24e5ae7e2f522ebc70f028426c8215f6503d7
Zheltukhin Alexey <azhelt@mera.ru>
2014-07-23 17:48:14 +0400

    remove unused code for previous commit

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/model/EventReadingInterval.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Mdc.model.EventReadingInterval', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'start', type:'date', dateFormat: 'time', useNull: true},
-        {name: 'end', type:'date', dateFormat: 'time', useNull: true}
-    ]
-});
--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/deviceregisterdata/MainSetup.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/deviceregisterdata/MainSetup.js
@@ -6,34 +6,6 @@ Ext.define('Mdc.view.setup.deviceregisterdata.MainSetup', {
     mRID: null,
     registerId: null,
 
-    content: [
-        {
-            xtype: 'panel',
-            ui: 'large',
-            title: Uni.I18n.translate('device.registerData.title', 'MDC', 'Register data'),
-            items: [
-                {
-                    xtype: 'preview-container',
-                    grid: {
-                        xtype: 'devicevalueregisterreportgrid'
-                    },
-                    emptyComponent: {
-                        xtype: 'no-items-found-panel',
-                        title: Uni.I18n.translate('device.registerData.title', 'MDC', 'No readings found'),
-                        reasons: [
-                            Uni.I18n.translate('device.registerData.list.item1', 'MDC', 'No readings have been defined yet.'),
-                            Uni.I18n.translate('device.registerData.list.item2', 'MDC', 'No readings comply to the filter.')
-                        ],
-                        stepItems: []
-                    },
-                    previewComponent: {
-                        xtype: 'devicevalueregisterreportpreview'
-                    }
-                }
-            ]
-        }
-    ],
-
     initComponent: function () {
         var me = this;
 
