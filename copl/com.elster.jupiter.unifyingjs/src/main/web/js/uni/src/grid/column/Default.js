e2077e060992f1cb6637468784b1ceb31345528e
Levi Van Zele <Levi.VanZele@elster.com>
2014-07-24 16:54:48 +0200

    [JP-4487] Added a highlighted icon to the default column.

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/column/Default.js
@@ -0,0 +1,20 @@
+/**
+ * @class Uni.grid.column.Default
+ */
+Ext.define('Uni.grid.column.Default', {
+    extend: 'Ext.grid.column.Column',
+    xtype: 'uni-default-column',
+
+    header: Uni.I18n.translate('general.default', 'UNI', 'Default'),
+    minWidth: 120,
+    align: 'left',
+
+    renderer: function (value, metadata) {
+        if (value) {
+            return '<div class="' + Uni.About.baseCssPrefix + 'default-column-icon'
+                + ' default">&nbsp;</div>';
+        } else {
+            return '';
+        }
+    }
+});
