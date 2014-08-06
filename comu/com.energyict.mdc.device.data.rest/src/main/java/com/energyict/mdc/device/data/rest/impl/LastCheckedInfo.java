317fa61ca7ef577031d07f3d3539b91779089061
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-08-06 16:11:18 +0200

    JP-4730 - Start date (format) error when reactivating validation Back-end fixes (front end fixes still needed)

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/LastCheckedInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/LastCheckedInfo.java
@@ -1,5 +1,8 @@
 package com.energyict.mdc.device.data.rest.impl;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class LastCheckedInfo {
 
     public Long lastChecked;
