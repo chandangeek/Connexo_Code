a92cba9052a10b421c9b925932138ceab574afce
Mera_DmitryK <dkorchem@mera.ru>
2014-08-14 13:09:20 +0400

    Fix for zero and false values passed to PropertyHydrator

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/packages/property/src/form/PropertyHydrator.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/packages/property/src/form/PropertyHydrator.js
@@ -2,16 +2,25 @@ Ext.define('Uni.property.form.PropertyHydrator', {
     extract: function(record) {
         return record.getData(true);
     },
-
+    falseAndZeroChecker: function(value) {
+        if (value.toString() == "false") {
+            return false;
+        }
+        if (value.toString() == "0"){
+           return 0;
+        }
+        return value || null
+    },
     hydrate: function(data, record) {
         var values = data;
+        var me = this;
         if (typeof record === 'undefined' || !record.properties()) {
             return false;
         }
         record.properties().each(function (property) {
             if (property.get('isInheritedOrDefaultValue') === true) {
                 if (property.get('required') === true && property.get('hasDefaultValue')) {
-                    var value = values[property.get('key')] || null;
+                    var value = me.falseAndZeroChecker(values[property.get('key')]);
                     propertyValue = Ext.create('Uni.property.model.PropertyValue');
                     property.setPropertyValue(propertyValue);
                     propertyValue.set('value', value);
@@ -19,15 +28,15 @@ Ext.define('Uni.property.form.PropertyHydrator', {
                     property.setPropertyValue(null);
                 }
             } else {
-                var value = values[property.get('key')] || null;
+                var value = me.falseAndZeroChecker(values[property.get('key')]);
                 if (!property.raw['propertyValueInfo']) {
                     propertyValue = Ext.create('Uni.property.model.PropertyValue');
                     property.setPropertyValue(propertyValue);
                 }
-
                 var propertyValue = property.getPropertyValue();
                 propertyValue.set('value', value);
             }
+
         });
     }
 });
