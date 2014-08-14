02473dd24676f3ded11e872540446cb4b728a584
Lobanov Dmitry <dlobanov@mera.ru>
2014-08-14 12:40:05 +0400

    JP-695 JP-2935

---
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/properties/ListValueInfo.java
@@ -0,0 +1,15 @@
+package com.elster.jupiter.rest.util.properties;
+
+public class ListValueInfo {
+    
+    public String id;
+    public String name;
+    
+    public ListValueInfo(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public ListValueInfo() {
+    }
+}
