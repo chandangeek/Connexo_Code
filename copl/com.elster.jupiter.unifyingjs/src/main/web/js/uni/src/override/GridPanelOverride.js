bb6f260760fd2e2789c9e0f82004d7cdc60273e3
Sentyurin Sergey <ssentyur@mera.ru>
2014-08-18 13:46:18 +0400

    action column disappearance fix

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/GridPanelOverride.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/GridPanelOverride.js
@@ -3,10 +3,6 @@
  */
 Ext.define('Uni.override.GridPanelOverride', {
     override: 'Ext.grid.Panel',
-    requires: [
-        'Uni.grid.plugin.ShowConditionalToolTip'
-    ],
-    plugins: ['showConditionalToolTip'],
 
     /**
      * Do not select row when column is of type actioncolumn.
