e6a7828567d101c6bf7ff04b372a722a49c92a54
Levi Van Zele <Levi.VanZele@elster.com>
2014-08-19 15:05:40 +0200

    [JP-4726] Minor translation changes.

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/store/DaysOfWeek.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/store/DaysOfWeek.js
@@ -18,37 +18,37 @@ Ext.define('Mdc.store.DaysOfWeek', {
         {
             id: 1,
             key: 'monday',
-            translation: Uni.I18n.translate('schedulefield.monday', 'UNI', 'monday')
+            translation: Uni.I18n.translate('schedulefield.monday', 'UNI', 'Monday')
         },
         {
             id: 2,
             key: 'tuesday',
-            translation: Uni.I18n.translate('schedulefield.tuesday', 'UNI', 'tuesday')
+            translation: Uni.I18n.translate('schedulefield.tuesday', 'UNI', 'Tuesday')
         },
         {
             id: 3,
             key: 'wednesday',
-            translation: Uni.I18n.translate('schedulefield.wednesday', 'UNI', 'wednesday')
+            translation: Uni.I18n.translate('schedulefield.wednesday', 'UNI', 'Wednesday')
         },
         {
             id: 4,
             key: 'thursday',
-            translation: Uni.I18n.translate('schedulefield.thursday', 'UNI', 'thursday')
+            translation: Uni.I18n.translate('schedulefield.thursday', 'UNI', 'Thursday')
         },
         {
             id: 5,
             key: 'friday',
-            translation: Uni.I18n.translate('schedulefield.friday', 'UNI', 'friday')
+            translation: Uni.I18n.translate('schedulefield.friday', 'UNI', 'Friday')
         },
         {
             id: 6,
             key: 'saturday',
-            translation: Uni.I18n.translate('schedulefield.saturday', 'UNI', 'saturday')
+            translation: Uni.I18n.translate('schedulefield.saturday', 'UNI', 'Saturday')
         },
         {
             id: 7,
             key: 'sunday',
-            translation: Uni.I18n.translate('schedulefield.sunday', 'UNI', 'sunday')
+            translation: Uni.I18n.translate('schedulefield.sunday', 'UNI', 'Sunday')
         }
     ]
 });
