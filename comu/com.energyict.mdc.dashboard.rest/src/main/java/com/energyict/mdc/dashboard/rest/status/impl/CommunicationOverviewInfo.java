c12b71e004885f3fb6ea45f4aa4e4b2a45b1230e
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-08-26 13:16:04 +0200

    JP-4344: Added overview per current state

---
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/CommunicationOverviewInfo.java
@@ -0,0 +1,121 @@
+package com.energyict.mdc.dashboard.rest.status.impl;
+
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.HasName;
+import com.energyict.mdc.common.HasId;
+import com.energyict.mdc.common.rest.MapBasedXmlAdapter;
+import com.energyict.mdc.dashboard.ComPortPoolBreakdown;
+import com.energyict.mdc.dashboard.ComSessionSuccessIndicatorOverview;
+import com.energyict.mdc.dashboard.ConnectionTypeBreakdown;
+import com.energyict.mdc.dashboard.Counter;
+import com.energyict.mdc.dashboard.DashboardCounters;
+import com.energyict.mdc.dashboard.DeviceTypeBreakdown;
+import com.energyict.mdc.dashboard.TaskStatusBreakdownCounter;
+import com.energyict.mdc.dashboard.TaskStatusBreakdownCounters;
+import com.energyict.mdc.dashboard.TaskStatusOverview;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ * This JSON representation holds the entire communication overview
+ * @link http://confluence.eict.vpdc/display/JUP/Communications
+ */
+public class CommunicationOverviewInfo {
+
+    public static final Comparator<TaskCounterInfo> TASK_COUNTER_INFO_COMPARATOR = new Comparator<TaskCounterInfo>() {
+        @Override
+        public int compare(TaskCounterInfo o1, TaskCounterInfo o2) {
+            return -Long.valueOf(o1.count).compareTo(o2.count);
+        }
+    };
+
+    public static final Comparator<TaskBreakdownInfo> TASK_BREAKDOWN_INFO_COMPARATOR = new Comparator<TaskBreakdownInfo>() {
+        @Override
+        public int compare(TaskBreakdownInfo o1, TaskBreakdownInfo o2) {
+            return -Long.valueOf(o1.failedCount).compareTo(o2.failedCount);
+        }
+    };
+
+    @JsonIgnore
+    private Thesaurus thesaurus;
+    @JsonIgnore
+    private static final TaskStatusAdapter taskStatusAdapter = new TaskStatusAdapter();
+    @JsonIgnore
+    private static final SuccessIndicatorAdapter successIndicatorAdapter = new SuccessIndicatorAdapter();
+
+//    public ConnectionSummaryInfo connectionSummary;
+
+    public List<TaskSummaryInfo> overviews;
+//    public List<BreakdownSummaryInfo> breakdowns;
+
+    public CommunicationOverviewInfo() {
+    }
+
+    public <H extends HasName & HasId> CommunicationOverviewInfo(
+            ConnectionSummaryData connectionSummaryData,
+            TaskStatusOverview taskStatusOverview,
+            ComSessionSuccessIndicatorOverview comSessionSuccessIndicatorOverview,
+            ComPortPoolBreakdown comPortPoolBreakdown,
+            ConnectionTypeBreakdown connectionTypeBreakdown,
+            DeviceTypeBreakdown deviceTypeBreakdown,
+            Thesaurus thesaurus) throws Exception {
+        this.thesaurus = thesaurus;
+
+
+        overviews=new ArrayList<>(2);
+        overviews.add(createOverview(thesaurus.getString(MessageSeeds.PER_CURRENT_STATE.getKey(), MessageSeeds.PER_CURRENT_STATE.getDefaultFormat()), taskStatusOverview, FilterOption.state, taskStatusAdapter)); // JP-4278
+        overviews.add(createOverview(thesaurus.getString(MessageSeeds.PER_LATEST_RESULT.getKey(), MessageSeeds.PER_LATEST_RESULT.getDefaultFormat()), comSessionSuccessIndicatorOverview, FilterOption.latestResult, successIndicatorAdapter)); // JP-4280
+
+        sortAllOverviews();
+
+    }
+
+
+    private void sortAllOverviews() {
+        for (TaskSummaryInfo overview : overviews) {
+            Collections.sort(overview.counters, TASK_COUNTER_INFO_COMPARATOR);
+        }
+    }
+
+    private <C extends HasName & HasId> BreakdownSummaryInfo createBreakdown(String name, TaskStatusBreakdownCounters<C> breakdownCounters, BreakdownOption alias) {
+        BreakdownSummaryInfo info = new BreakdownSummaryInfo();
+        info.displayName=name;
+        info.alias=alias;
+        info.total=breakdownCounters.getTotalCount();
+        info.totalSuccessCount=breakdownCounters.getTotalSuccessCount();
+        info.totalPendingCount=breakdownCounters.getTotalPendingCount();
+        info.totalFailedCount =breakdownCounters.getTotalFailedCount();
+        info.counters=new ArrayList<>();
+        for (TaskStatusBreakdownCounter<C> counter : breakdownCounters) {
+            TaskBreakdownInfo taskBreakdownInfo = new TaskBreakdownInfo();
+            taskBreakdownInfo.id=counter.getCountTarget().getId();
+            taskBreakdownInfo.displayName=counter.getCountTarget().getName();
+            taskBreakdownInfo.successCount=counter.getSuccessCount();
+            taskBreakdownInfo.pendingCount=counter.getPendingCount();
+            taskBreakdownInfo.failedCount=counter.getFailedCount();
+            info.counters.add(taskBreakdownInfo);
+        }
+
+        return info;
+    }
+
+    private <C> TaskSummaryInfo createOverview(String overviewBreakdownName, DashboardCounters<C> dashboardCounters, FilterOption alias, MapBasedXmlAdapter<C> adapter) throws Exception {
+        TaskSummaryInfo info = new TaskSummaryInfo();
+        info.displayName =overviewBreakdownName;
+        info.alias=alias;
+        info.counters=new ArrayList<>();
+        for (Counter<C> taskStatusCounter : dashboardCounters) {
+            TaskCounterInfo taskCounterInfo = new TaskCounterInfo();
+            taskCounterInfo.id=adapter.marshal(taskStatusCounter.getCountTarget());
+            taskCounterInfo.displayName=thesaurus.getString(adapter.marshal(taskStatusCounter.getCountTarget()), adapter.marshal(taskStatusCounter.getCountTarget()));
+            taskCounterInfo.count=taskStatusCounter.getCount();
+            info.counters.add(taskCounterInfo);
+        }
+
+        return info;
+    }
+}
+
---
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/CommunicationOverviewResource.java
@@ -0,0 +1,39 @@
+package com.energyict.mdc.dashboard.rest.status.impl;
+
+import com.elster.jupiter.nls.Thesaurus;
+import com.energyict.mdc.dashboard.ComCommandCompletionCodeOverview;
+import com.energyict.mdc.dashboard.DashboardService;
+import com.energyict.mdc.dashboard.TaskStatusOverview;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by bvn on 7/29/14.
+ */
+@Path("/communicationoverview")
+public class CommunicationOverviewResource {
+
+    private final Thesaurus thesaurus;
+    private final DashboardService dashboardService;
+
+    @Inject
+    public CommunicationOverviewResource(Thesaurus thesaurus, DashboardService dashboardService) {
+        this.thesaurus = thesaurus;
+        this.dashboardService = dashboardService;
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public CommunicationOverviewInfo getCommunicationOverview() throws Exception {
+        TaskStatusOverview taskStatusOverview = dashboardService.getCommunicationTaskStatusOverview();
+        ComCommandCompletionCodeOverview comSessionSuccessIndicatorOverview = dashboardService.getCommunicationTaskCompletionResultOverview();
+
+        return new CommunicationOverviewInfo(null, taskStatusOverview, null, null, null, null, thesaurus);
+    }
+
+}
