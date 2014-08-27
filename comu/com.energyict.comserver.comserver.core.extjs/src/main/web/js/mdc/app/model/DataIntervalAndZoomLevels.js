80805b29286e36cc5e05f5d5d8615f8cd82bdb8d
Sentyurin Sergey <ssentyur@mera.ru>
2014-08-27 14:38:25 +0400

    JP-137 code review marks

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/model/DataIntervalAndZoomLevels.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/model/DataIntervalAndZoomLevels.js
@@ -13,32 +13,8 @@ Ext.define('Mdc.model.DataIntervalAndZoomLevels', {
     idProperty: 'interval',
 
     getIntervalStart: function (intervalEnd) {
-        var all = this.get('all'),
-            intervalEndDate = new Date(intervalEnd),
-            intervalEndDateTime = intervalEndDate.getTime(),
-            intervalStart;
+        var all = this.get('all');
 
-        switch (all.timeUnit) {
-            case 'minutes':
-                intervalStart = intervalEndDateTime - all.count * 60000;
-                break;
-            case 'hours':
-                intervalStart = intervalEndDateTime - all.count * 1440000;
-                break;
-            case 'days':
-                intervalStart = intervalEndDateTime - all.count * 86400000;
-                break;
-            case 'weeks':
-                intervalStart = intervalEndDateTime - all.count * 604800000;
-                break;
-            case 'months':
-                intervalStart = intervalEndDate.setMonth(intervalEndDate.getMonth() - all.count);
-                break;
-            case 'years':
-                intervalStart = intervalEndDate.setFullYear(intervalEndDate.getFullYear() - all.count);
-                break;
-        }
-
-        return intervalStart;
+        return moment(intervalEnd).subtract(all.timeUnit, all.count).valueOf();
     }
 });
