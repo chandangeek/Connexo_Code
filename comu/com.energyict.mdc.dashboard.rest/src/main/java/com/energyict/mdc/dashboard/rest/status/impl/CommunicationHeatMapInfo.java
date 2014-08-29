94210f5d99ea808944a2f0a4428a90a9a8dbad97
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-08-29 14:16:28 +0200

    JP-4344: Added communication heatmap resource

---
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/CommunicationHeatMapInfo.java
@@ -0,0 +1,55 @@
+package com.energyict.mdc.dashboard.rest.status.impl;
+
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.HasName;
+import com.energyict.mdc.common.HasId;
+import com.energyict.mdc.dashboard.ComCommandCompletionCodeOverview;
+import com.energyict.mdc.dashboard.CommunicationTaskHeatMap;
+import com.energyict.mdc.dashboard.CommunicationTaskHeatMapRow;
+import com.energyict.mdc.dashboard.Counter;
+import com.energyict.mdc.device.data.tasks.history.CompletionCode;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bvn on 8/19/14.
+ */
+public class CommunicationHeatMapInfo {
+
+
+    private static final CompletionCodeAdapter completionCodeAdapter = new CompletionCodeAdapter();
+
+    public List<HeatMapRowInfo> heatMap;
+
+    public <H extends HasName & HasId> CommunicationHeatMapInfo(CommunicationTaskHeatMap heatMap, Thesaurus thesaurus)
+            throws Exception {
+        this.heatMap=new ArrayList<>();
+        this.heatMap.addAll(createHeatMap(heatMap, thesaurus));
+    }
+
+    private <H extends HasName & HasId> List<HeatMapRowInfo> createHeatMap(CommunicationTaskHeatMap heatMap, Thesaurus thesaurus) throws Exception {
+        List<HeatMapRowInfo> heatMapInfoList = new ArrayList<>();
+        if (heatMap!=null) {
+            for (CommunicationTaskHeatMapRow row : heatMap) {
+                HeatMapRowInfo heatMapRowInfo = new HeatMapRowInfo();
+                heatMapRowInfo.displayValue = row.getTarget().getName(); // CPP name, device type name, ...
+                heatMapRowInfo.id = row.getTarget().getId(); // ID of the object
+                heatMapRowInfo.data = new ArrayList<>();
+                for (ComCommandCompletionCodeOverview counters : row) {
+                    for (Counter<CompletionCode> completionCodeCounter : counters) {
+                        TaskCounterInfo taskCounterInfo = new TaskCounterInfo();
+                        taskCounterInfo.id = completionCodeAdapter.marshal(completionCodeCounter.getCountTarget());
+                        taskCounterInfo.displayName = thesaurus.getString(completionCodeAdapter.marshal(completionCodeCounter.getCountTarget()), null);
+                        taskCounterInfo.count = completionCodeCounter.getCount();
+                        heatMapRowInfo.data.add(taskCounterInfo);
+                    }
+                }
+                heatMapInfoList.add(heatMapRowInfo);
+            }
+        }
+        return heatMapInfoList;
+    }
+
+
+
+}
--- comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/CommunicationHeatMapResource.java
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/CommunicationHeatMapResource.java
@@ -1,15 +1,11 @@
 package com.energyict.mdc.dashboard.rest.status.impl;
 
 import com.elster.jupiter.nls.Thesaurus;
-import com.energyict.mdc.common.rest.JsonQueryFilter;
 import com.energyict.mdc.dashboard.DashboardService;
 import javax.inject.Inject;
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 @Path("/connectionheatmap")
 public class CommunicationHeatMapResource {
@@ -26,23 +22,13 @@ public class CommunicationHeatMapResource {
     /**
      * Generates data in heatmap format according to the requested breakdown
      *
-     * @param jsonQueryFilter QueryFilter for breakdown attribute (mandatory)
      * @return HeatMap data
      * @throws Exception
      */
     @GET
-    @Consumes("application/json")
     @Produces("application/json")
-    public ConnectionHeatMapInfo getConnectionHeatMap(@BeanParam JsonQueryFilter jsonQueryFilter) throws Exception {
-        if (!jsonQueryFilter.getFilterProperties().containsKey("breakdown")) {
-            Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        BreakdownOption breakdown = jsonQueryFilter.getProperty("breakdown", new BreakdownOptionAdapter());
-
-        return new ConnectionHeatMapInfo(
-                breakdown==BreakdownOption.comPortPool?dashboardService.getConnectionsComPortPoolHeatMap():(breakdown==BreakdownOption.connectionType?dashboardService.getConnectionTypeHeatMap():dashboardService.getConnectionsDeviceTypeHeatMap()),
-                breakdown,
-                thesaurus);
+    public CommunicationHeatMapInfo getConnectionHeatMap() throws Exception {
+        return new CommunicationHeatMapInfo(dashboardService.getCommunicationTasksHeatMap(),thesaurus);
     }
 
 }
