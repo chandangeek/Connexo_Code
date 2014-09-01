959c07eaaf630aa104e6bd574e71a0ed51af2bea
Vitaliy Nishukov <nishukov@mera.ru>
2014-09-01 13:11:53 +0400

    CleanUp

--- comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/app/model/ConnectionCurrentState.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Dsh.model.ConnectionCurrentState', {
-    extend: 'Ext.data.Model',
-    fields: [
-        { name: 'name', type: 'string' },
-        { name: 'localizedValue', type: 'string' }
-    ]
-});
-
