e85a90225c33993888da3afd2d8c0ba671fd7e39
Sentyurin Sergey <ssentyur@mera.ru>
2014-09-01 16:14:28 +0400

    JP-4946 View logbook data in a table

---
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/app/view/setup/devicelogbooks/DataTableView.js
@@ -0,0 +1,26 @@
+Ext.define('Mdc.view.setup.devicelogbooks.DataTableView', {
+    extend: 'Uni.view.container.PreviewContainer',
+    alias: 'widget.deviceLogbookDataTableView',
+    itemId: 'deviceLogbookDataTableView',
+    requires: [
+        'Uni.view.notifications.NoItemsFoundPanel',
+        'Mdc.view.setup.devicelogbooks.DataGrid',
+        'Mdc.view.setup.devicelogbooks.DataPreview'
+    ],
+
+    grid: {
+        xtype: 'deviceLogbookDataGrid'
+    },
+
+    emptyComponent: {
+        xtype: 'no-items-found-panel',
+        title: Uni.I18n.translate('devicelogbooks.dataTableView.empty.title', 'MDC', 'No events found'),
+        reasons: [
+            Uni.I18n.translate('devicelogbooks.dataTableView.empty.list.item1', 'MDC', 'No events have been defined yet.')
+        ]
+    },
+
+    previewComponent: {
+        xtype: 'deviceLogbookDataPreview'
+    }
+});
