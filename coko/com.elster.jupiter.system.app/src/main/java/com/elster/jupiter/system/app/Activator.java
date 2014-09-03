7c534be499aa95bc1350e0b804b554b2108b9ac6
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-03 13:11:14 +0200

    [JP-4062] Renamed Pulse to System Admin.

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
@@ -11,13 +11,13 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-    public static final String HTTP_RESOURCE_ALIAS = "/pulse";
+    public static final String HTTP_RESOURCE_ALIAS = "/systemadmin";
 
     private volatile ServiceRegistration<HttpResource> registration;
 
     public void start(BundleContext bundleContext) throws Exception {
-        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/system", new BundleResolver(bundleContext), new DefaultStartPage("Pulse App"));
-//        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.system.app/src/main/web/js/system", new FileResolver(), new DefaultStartPage("Pulse App"));
+        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/system", new BundleResolver(bundleContext), new DefaultStartPage("System Admin App"));
+//        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.system.app/src/main/web/js/system", new FileResolver(), new DefaultStartPage("System Admin App"));
         registration = bundleContext.registerService(HttpResource.class, resource, null);
     }
 
