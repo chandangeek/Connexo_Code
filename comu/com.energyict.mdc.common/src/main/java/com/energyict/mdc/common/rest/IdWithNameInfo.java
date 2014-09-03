d596ecc80a982e509e3191cb3201220c14917ea7
Lobanov Dmitry <dlobanov@mera.ru>
2014-09-03 12:43:12 +0400

    JP-3729: Move IdWithNameInfo from dashboard.rest to share with device.data

---
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/IdWithNameInfo.java
@@ -0,0 +1,17 @@
+package com.energyict.mdc.common.rest;
+
+/**
+ * Created by bvn on 8/12/14.
+ */
+public class IdWithNameInfo {
+    public Object id;
+    public String name;
+
+    public IdWithNameInfo() {
+    }
+
+    public IdWithNameInfo(Object id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
