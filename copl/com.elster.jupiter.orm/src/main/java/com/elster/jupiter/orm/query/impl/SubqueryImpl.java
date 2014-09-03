92d15b9120a7cb62473d777461346a13f5312688
kha <karel.haeck@elster.com>
2014-09-03 10:50:46 +0200

    improved subquery support

---
+++ copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/query/impl/SubqueryImpl.java
@@ -0,0 +1,19 @@
+package com.elster.jupiter.orm.query.impl;
+
+import com.elster.jupiter.util.conditions.Subquery;
+import com.elster.jupiter.util.sql.SqlFragment;
+
+public class SubqueryImpl implements Subquery {
+	
+	private final SqlFragment fragment;
+	
+	SubqueryImpl(SqlFragment fragment) {
+		this.fragment = fragment;
+	}
+
+	@Override
+	public SqlFragment toFragment() {
+		return fragment;
+	}
+	
+}
