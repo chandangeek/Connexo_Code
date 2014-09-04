9cd7eb95db75e9afb9918b33c633a53c67f74b77
Vitaliy Nishukov <nishukov@mera.ru>
2014-09-04 12:35:37 +0400

    Communication servers widget fix

--- comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/app/store/CommunicationServerInfos.js
+++ comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/app/store/CommunicationServerInfos.js
@@ -1,9 +1,7 @@
 Ext.define('Dsh.store.CommunicationServerInfos', {
     extend: 'Ext.data.Store',
     storeId: 'CommunicationServerInfos',
-    requires: [
-        'Dsh.model.CommunicationServerInfo'
-    ],
+    requires: ['Dsh.model.CommunicationServerInfo'],
     model: 'Dsh.model.CommunicationServerInfo',
-    pageSize: 3
+    sorters: [{ property: 'running', direction: 'ASC' }]
 });
