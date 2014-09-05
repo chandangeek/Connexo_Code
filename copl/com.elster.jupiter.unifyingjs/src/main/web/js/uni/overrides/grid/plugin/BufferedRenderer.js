4846456499effeeaba3adc357fdb05b4350c097b
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-05 16:19:04 +0200

    [JP-4062] Modifications so that the Isu bundle (and some leftovers of Cfg bundle) could be used as a package.

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/overrides/grid/plugin/BufferedRenderer.js
@@ -0,0 +1,20 @@
+Ext.define('Uni.override.grid.plugin.BufferedRenderer', {
+    override: 'Ext.grid.plugin.BufferedRenderer',
+    rowHeight: 29, // comes from skyline theme
+
+    init: function(grid) {
+        this.callParent(arguments);
+
+        // grid height calculated before the toolbar is on layouts, it causes the bug: JP-3817
+        grid.on('boxready', function() {
+            grid.view.refresh();
+        })
+    },
+
+    bindStore: function(store) {
+        var me = this;
+        me.trailingBufferZone = 0;
+        me.leadingBufferZone = store.pageSize;
+        this.callParent(arguments);
+    }
+});
