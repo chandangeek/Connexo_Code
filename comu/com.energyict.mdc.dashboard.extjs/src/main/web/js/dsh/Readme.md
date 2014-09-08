ad0dc067681207006588956e1e6cfce53bda0509
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-08 15:59:54 +0200

    [JP-4062] Modifications so that the Dsh bundle could be used as a package.

--- comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/Readme.md
+++
@@ -1,1 +0,0 @@
-Bundle for dashboard
---
+++ comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/src/view/Connections.js
@@ -0,0 +1,58 @@
+Ext.define('Dsh.view.Connections', {
+    extend: 'Uni.view.container.ContentContainer',
+    alias: 'widget.connections-details',
+    itemId: 'connectionsdetails',
+    overflowY: 'auto',
+
+    requires: [
+        'Dsh.view.widget.ConnectionsList',
+        'Dsh.view.widget.PreviewConnection',
+        'Dsh.view.widget.SideFilter'
+    ],
+
+    content: [
+        {
+            xtype: 'panel',
+            ui: 'large',
+            title: Uni.I18n.translate('workspace.dataCommunication.connections.title', 'DSH', 'Connections')
+        },
+        {
+            xtype: 'preview-container',
+            grid: {
+                xtype: 'connections-list',
+                itemId: 'connectionsdetails'
+            },
+            emptyComponent: {
+                xtype: 'no-items-found-panel',
+                title: Uni.I18n.translate('workspace.dataCommunication.connections.empty.title', 'DSH', 'No connections found'),
+                reasons: [
+                    Uni.I18n.translate('workspace.dataCommunication.connections.empty.list.item1', 'DSH', 'No connections in the system.'),
+                    Uni.I18n.translate('workspace.dataCommunication.connections.empty.list.item2', 'DSH', 'No connections found due to applied filters.')
+                ]
+            },
+            previewComponent: {
+                xtype: 'preview_connection',
+                itemId: 'connectionpreview'
+            }
+        },
+        {
+            ui: 'medium',
+            itemId: 'comtaskstitlepanel',
+            title: ''
+        },
+        {
+            xtype: 'container',
+            itemId: 'communicationcontainer'
+        }
+    ],
+
+    side: [
+        {
+            xtype: 'dsh-side-filter'
+        }
+    ],
+
+    initComponent: function () {
+        this.callParent(arguments);
+    }
+});
