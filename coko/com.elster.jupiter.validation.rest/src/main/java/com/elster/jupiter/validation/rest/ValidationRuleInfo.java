6bbf3de5e39cc4f7709250995585c50b991214e4
borunova <borunova@mera.ru>
2014-09-10 16:57:49 +0400

    JP-4674

--- coko/com.elster.jupiter.validation.rest/src/main/java/com/elster/jupiter/validation/rest/ValidationRuleInfo.java
+++ coko/com.elster.jupiter.validation.rest/src/main/java/com/elster/jupiter/validation/rest/ValidationRuleInfo.java
@@ -6,8 +6,7 @@ import com.elster.jupiter.rest.util.properties.PropertyInfo;
 import com.elster.jupiter.validation.DataValidationStatus;
 import com.elster.jupiter.validation.ValidationRule;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ValidationRuleInfo {
 
@@ -44,14 +43,63 @@ public class ValidationRuleInfo {
         return infos;
     }
 
-    public static List<ValidationRuleInfo> from(DataValidationStatus dataValidationStatus) {
-        List<ValidationRuleInfo> validationRuleInfos = new ArrayList<>();
+    public static Set<ValidationRuleInfo> from(DataValidationStatus dataValidationStatus) {
+        Set<ValidationRuleInfo> validationRuleInfos = new LinkedHashSet<>();
         for (ValidationRule validationRule : dataValidationStatus.getOffendedRules()) {
-            validationRuleInfos.add(new ValidationRuleInfo(validationRule));
+            if(validationRule.isObsolete()) {
+                validationRuleInfos.add(getObsoleteRuleInfo());
+            } else {
+                validationRuleInfos.add(new ValidationRuleInfo(validationRule));
+            }
         }
         return validationRuleInfos;
     }
 
+    public static Map<ValidationRuleInfo, Long> from(Map<ValidationRule, Long> suspectReasonMap) {
+        Map<ValidationRuleInfo, Long> suspectReasonInfoMap = new HashMap<>();
+        for(Map.Entry<ValidationRule, Long> entry : suspectReasonMap.entrySet()) {
+            if(entry.getKey().isObsolete()) {
+                putObsoleteRuleInfo(suspectReasonInfoMap, entry);
+            } else {
+                suspectReasonInfoMap.put(new ValidationRuleInfo(entry.getKey()), entry.getValue());
+            }
+        }
+        return suspectReasonInfoMap;
+    }
+
     public ValidationRuleInfo() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        return id == ((ValidationRuleInfo) o).id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    private static void putObsoleteRuleInfo(Map<ValidationRuleInfo, Long> suspectReasonInfoMap, Map.Entry<ValidationRule, Long> entry) {
+        ValidationRuleInfo validationRuleInfo = getObsoleteRuleInfo();
+        if(suspectReasonInfoMap.containsKey(validationRuleInfo)) {
+            Long number = suspectReasonInfoMap.get(validationRuleInfo);
+            suspectReasonInfoMap.put(validationRuleInfo, ++number);
+        } else {
+            suspectReasonInfoMap.put(validationRuleInfo, 1L);
+        }
+    }
+
+    private static ValidationRuleInfo getObsoleteRuleInfo() {
+        ValidationRuleInfo validationRuleInfo = new ValidationRuleInfo();
+        validationRuleInfo.id = 0;
+        validationRuleInfo.name = "removed rule";
+        return validationRuleInfo;
     }
 }
