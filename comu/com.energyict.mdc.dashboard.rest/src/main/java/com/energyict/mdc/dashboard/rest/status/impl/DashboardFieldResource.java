cefd79a33630fa827225a70a09bf1e79fa2a15dd
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-09-10 09:39:31 +0200

    Added convenience field resource for comboboxes in connection filtering

--- comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/DashboardFieldResource.java
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/DashboardFieldResource.java
@@ -3,14 +3,26 @@ package com.energyict.mdc.dashboard.rest.status.impl;
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
 import com.energyict.mdc.common.rest.FieldResource;
+import com.energyict.mdc.common.rest.IdWithNameInfo;
 import com.energyict.mdc.dashboard.rest.DashboardApplication;
+import com.energyict.mdc.device.config.DeviceConfigurationService;
+import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.data.rest.TaskStatusAdapter;
+import com.energyict.mdc.engine.model.ComPortPool;
+import com.energyict.mdc.engine.model.EngineModelService;
 import com.energyict.mdc.engine.model.security.Privileges;
+import com.energyict.mdc.protocol.pluggable.ConnectionTypePluggableClass;
+import com.energyict.mdc.protocol.pluggable.ProtocolPluggableService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  * Why the wrapped return value? JavaScript people didn't want to see a naked JSON list, had to be
@@ -21,9 +33,17 @@ import javax.ws.rs.Produces;
 public class DashboardFieldResource extends FieldResource {
 
 
+    private static final BreakdownOptionAdapter BREAKDOWN_OPTION_ADAPTER = new BreakdownOptionAdapter();
+    private final DeviceConfigurationService deviceConfigurationService;
+    private final EngineModelService engineModelService;
+    private final ProtocolPluggableService protocolPluggableService;
+
     @Inject
-    public DashboardFieldResource(NlsService nlsService) {
+    public DashboardFieldResource(NlsService nlsService, DeviceConfigurationService deviceConfigurationService, EngineModelService engineModelService, ProtocolPluggableService protocolPluggableService) {
         super(nlsService.getThesaurus(DashboardApplication.COMPONENT_NAME, Layer.REST));
+        this.deviceConfigurationService = deviceConfigurationService;
+        this.engineModelService = engineModelService;
+        this.protocolPluggableService = protocolPluggableService;
     }
 
     @GET
@@ -31,7 +51,7 @@ public class DashboardFieldResource extends FieldResource {
     @Produces("application/json")
     @RolesAllowed(Privileges.VIEW_COMSERVER)
     public Object getBreakdownValues() {
-        return asJsonArrayObjectWithTranslation("breakdowns", "breakdown", new BreakdownOptionAdapter().getClientSideValues());
+        return asJsonArrayObjectWithTranslation("breakdowns", "breakdown", BREAKDOWN_OPTION_ADAPTER.getClientSideValues());
     }
 
     @GET
@@ -70,6 +90,47 @@ public class DashboardFieldResource extends FieldResource {
     @Path("/completioncodes")
     @Produces("application/json")
     public Object getCompletionCodes() {
-        return asJsonArrayObjectWithTranslation("completionCodes", "completionCode", new CompletionCodeAdapter().getClientSideValues());    }
+        return asJsonArrayObjectWithTranslation("completionCodes", "completionCode", new CompletionCodeAdapter().getClientSideValues());
+    }
+
+    @GET
+    @Path("/devicetypes")
+    @Produces("application/json")
+    public Object getDeviceTypes() {
+        Map<String, List<IdWithNameInfo>> map = new HashMap<>();
+        List<IdWithNameInfo> infos = new ArrayList<>();
+        map.put("deviceTypes", infos);
+        for (DeviceType deviceType : deviceConfigurationService.findAllDeviceTypes().find()) {
+            infos.add(new IdWithNameInfo(deviceType));
+        }
+        return map;
+    }
+
+    @GET
+    @Path("/comportpools")
+    @Produces("application/json")
+    public Object getComPortPools() {
+        Map<String, List<IdWithNameInfo>> map = new HashMap<>();
+        List<IdWithNameInfo> infos = new ArrayList<>();
+        map.put("comPortPools", infos);
+        for (ComPortPool comPortPool : engineModelService.findAllComPortPools()) {
+            infos.add(new IdWithNameInfo(comPortPool));
+        }
+        return map;
+    }
+
+    @GET
+    @Path("/connectiontypepluggableclasses")
+    @Produces("application/json")
+    @RolesAllowed(Privileges.VIEW_COMSERVER)
+    public Object getConnectionTypeValues() {
+        List<IdWithNameInfo> names = new ArrayList<>();
+        for (ConnectionTypePluggableClass connectionTypePluggableClass : protocolPluggableService.findAllConnectionTypePluggableClasses()) {
+            names.add(new IdWithNameInfo(connectionTypePluggableClass.getId(), connectionTypePluggableClass.getName()));
+        }
+        Map<String, List<IdWithNameInfo>> map = new HashMap<>();
+        map.put("connectiontypepluggableclasses", names);
+        return Response.ok(map).build();
+    }
 
 }
