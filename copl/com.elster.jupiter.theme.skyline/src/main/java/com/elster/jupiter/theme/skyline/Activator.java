f939f35e81c2c8ae64757505c81f0be85f6208af
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-12 16:25:20 +0200

    [JP-4062] Changed the Skyline theme configuration.

--- copl/com.elster.jupiter.theme.skyline/src/main/java/com/elster/jupiter/theme/skyline/Activator.java
+++ copl/com.elster.jupiter.theme.skyline/src/main/java/com/elster/jupiter/theme/skyline/Activator.java
@@ -1,22 +1,22 @@
 package com.elster.jupiter.theme.skyline;
 
-import com.elster.jupiter.http.whiteboard.DefaultStartPage;
 import com.elster.jupiter.http.whiteboard.BundleResolver;
 import com.elster.jupiter.http.whiteboard.FileResolver;
 import com.elster.jupiter.http.whiteboard.HttpResource;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 
-public class Activator {
+public class Activator implements BundleActivator {
 
     private volatile ServiceRegistration<HttpResource> registration;
 
     public void start(BundleContext bundleContext) throws Exception {
         String alias = "/skyline";
 
-//        HttpResource resource = new HttpResource(alias, "/js/skyline", new BundleResolver(bundleContext), new DefaultStartPage("Skyline Theme"));
-        HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.theme.skyline/src/main/web/js/skyline", new FileResolver(), new DefaultStartPage("Skyline Theme"));
+        HttpResource resource = new HttpResource(alias, "/js/skyline", new BundleResolver(bundleContext));
+//        HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.theme.skyline/src/main/web/js/skyline", new FileResolver());
 
         registration = bundleContext.registerService(HttpResource.class, resource, null);
     }
--- copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/sass/config.rb
+++ copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/sass/config.rb
@@ -1,17 +1,9 @@
-require '../../ext-theme-base/sass/utils.rb'
-
-# sass_path: the directory your Sass files are in. THIS file should also be in the Sass folder
-sass_path = File.dirname(__FILE__)
 cur_dir = File.dirname(__FILE__)
-css_path = File.join(sass_path, "..", "build", "resources")
+output_style = :compressed
 
-$ext_path = File.join("..", "..", "..")
-
-# We need to load in the Ext4 themes folder, which includes all it's default styling, images, variables and mixins
-#load File.join(File.dirname(__FILE__), $ext_path, 'resources', 'themes')
-#load File.join(File.dirname(__FILE__), $ext_path, 'resources', 'ext-theme-neptune')
-
-#Compass config variable
-relative_assets = true
-
-output_style = :nested
+module Sass::Script::Functions
+  def get_resource_dir()
+    dir = ENV.fetch("resource_dir", '../../')
+    Sass::Script::String.new(dir)
+  end
+end
