f564d1d06488c2dcc5fc53b5050e85299b81a761
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-09-15 11:13:15 +0200

    JP-5438: Added GET method

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/security/Privileges.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/security/Privileges.java
@@ -22,4 +22,20 @@ public interface Privileges {
     String UPDATE_REGISTER_CONFIG = "privilege.update.registerConfiguration";
     String DELETE_REGISTER_CONFIG = "privilege.delete.registerConfiguration";
     String VIEW_REGISTER_CONFIG = "privilege.view.registerConfiguration";
+
+    String VIEW_DEVICE_SECURITY_PROPERTIES_1 = "view.device.security.properties.level1";
+    String VIEW_DEVICE_SECURITY_PROPERTIES_2 = "view.device.security.properties.level2";
+    String VIEW_DEVICE_SECURITY_PROPERTIES_3 = "view.device.security.properties.level3";
+    String VIEW_DEVICE_SECURITY_PROPERTIES_4 = "view.device.security.properties.level4";
+
+    String EDIT_DEVICE_SECURITY_PROPERTIES_1 = "edit.device.security.properties.level1";
+    String EDIT_DEVICE_SECURITY_PROPERTIES_2 = "edit.device.security.properties.level2";
+    String EDIT_DEVICE_SECURITY_PROPERTIES_3 = "edit.device.security.properties.level3";
+    String EDIT_DEVICE_SECURITY_PROPERTIES_4 = "edit.device.security.properties.level4";
+
+    String EXECUTE_COM_TASK_1 = "execute.com.task.level1";
+    String EXECUTE_COM_TASK_2 = "execute.com.task.level2";
+    String EXECUTE_COM_TASK_3 = "execute.com.task.level3";
+    String EXECUTE_COM_TASK_4 = "execute.com.task.level4";
+
 }
