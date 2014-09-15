bb76d22d7f1ac1f3509893819f1f69b7b802c25e
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-09-15 17:40:13 +0200

    JP-5374 add filtering

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/streams/Predicates.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/streams/Predicates.java
@@ -1,13 +1,19 @@
 package com.elster.jupiter.util.streams;
 
+import java.util.function.Predicate;
+
 /**
  * Created by tgr on 10/09/2014.
  */
 public enum Predicates {
     ;
 
-    public static <T> java.util.function.Predicate<T> notNull() {
+    public static <T> Predicate<T> notNull() {
         return r -> r != null;
+    }
+
+    public static <T> Predicate<T> isNull() {
+        return r -> r == null;
     }
 
 }
