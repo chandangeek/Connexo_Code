30d01a2d0092008ff8f51bc4f1f7c686f01a13e7
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-17 16:01:56 +0200

    [JP-4062] Changed the login mechanism to redirect to the correct page. Also made sure the correct default history token is used upon logging in.

--- comu/com.energyict.mdc.app/src/main/web/js/mdc/app/controller/Main.js
+++ comu/com.energyict.mdc.app/src/main/web/js/mdc/app/controller/Main.js
@@ -5,6 +5,7 @@ Ext.define('MdcApp.controller.Main', {
     extend: 'Uni.controller.AppController',
 
     applicationTitle: 'Connexo Multi Sense',
+    defaultToken: '#/workspace',
 
     controllers: [
         'Cfg.controller.Main',
