5bb5c6076a4fe9741e0a5fa3b6e3e15cb16fbaf8
Isabelle  Gheysens <isabelle.gheysens@elster.com>
2014-09-17 10:07:52 +0200

    JP-4734 - Develop View validation status of channel data

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/deviceloadprofilechannels/DataPreview.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/deviceloadprofilechannels/DataPreview.js
@@ -1,54 +1,67 @@
-Ext.define('Mdc.view.setup.deviceloadprofilechannels.DataPreview', {
+Ext.define('Mdc.view.setup.deviceloadprofilechannels.numerical.DataPreview', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.deviceLoadProfileChannelDataPreview',
     itemId: 'deviceLoadProfileChannelDataPreview',
     requires: [
         'Mdc.view.setup.deviceloadprofilechannels.DataActionMenu',
-        'Uni.form.field.IntervalFlagsDisplay'
+        'Uni.form.field.IntervalFlagsDisplay',
+        'Mdc.view.setup.deviceloadprofilechannels.ValidationPreview'
     ],
-    layout: 'fit',
+    title: '',
     frame: true,
-
-    channelRecord: null,
 
     tools: [
         {
             xtype: 'button',
             text: Uni.I18n.translate('general.actions', 'MDC', 'Actions'),
-            itemId: 'actionButton',
-            iconCls: 'x-uni-action-iconD',
-            menu: {
-                xtype: 'deviceLoadProfileChannelDataActionMenu'
-            }
+            iconCls: 'x-uni-action-iconD'
         }
     ],
 
-    initComponent: function () {
-        var me = this,
-            readingType = me.channelRecord.get('cimReadingType'),
-            measurementType = me.channelRecord.get('unitOfMeasure_formatted'),
-            accumulationBehavior;
+    items: {
+        xtype: 'form',
+        defaults: {
+            xtype: 'container',
+            layout: 'form'
+        },
+        items: [
+            {
+                xtype:'fieldcontainer',
+                fieldLabel: Uni.I18n.translate('deviceregisterconfiguration.general', 'MDC', 'General'),
+                labelAlign: 'top',
+                layout: 'vbox',
+                defaults: {
+                    xtype: 'displayfield',
+                    labelWidth: 200
+                },
+                items: [
 
-        me.items = {
-            xtype: 'form',
-            itemId: 'deviceLoadProfileChannelDataPreviewForm',
-            defaults: {
-                xtype: 'displayfield',
-                labelWidth: 200
+                ]
+            },
+            {
+                xtype: 'deviceloadprofilechannelspreview-validation'
             }
-        };
+        ]
+    },
 
-
-        me.items.items = [
+    initComponent: function () {
+        var me = this;
+        //me.items.items[0].removeAll();
+        me.items.items[0].items.push(
             {
                 fieldLabel: Uni.I18n.translate('deviceloadprofiles.interval', 'MDC', 'Interval'),
                 name: 'interval_formatted'
-            },
+            }
+        );
+        me.items.items[0].items.push(
             {
                 fieldLabel: Uni.I18n.translate('deviceloadprofiles.readingTime', 'MDC', 'Reading time'),
                 name: 'readingTime_formatted'
             }
-        ];
+        );
+        var readingType = me.channelRecord.get('cimReadingType'),
+            measurementType = me.channelRecord.get('unitOfMeasure_formatted'),
+            accumulationBehavior;
 
         //Getting 4th magic number of a reading type to understand if it holds cumulative values or not
         if (readingType) {
@@ -57,22 +70,23 @@ Ext.define('Mdc.view.setup.deviceloadprofilechannels.DataPreview', {
 
         // 1 means cumulative
         if (accumulationBehavior && accumulationBehavior == 1) {
-            me.items.items.push({
-                fieldLabel: Uni.I18n.translate('deviceloadprofiles.channels.cumulativeValue', 'MDC', 'Cumulative value'),
+            me.items.items[0].items.push({
+                fieldLabel: Uni.I18n.translate('deviceloadprofiles.channels.delta', 'MDC', 'Delta'),
                 name: 'value',
                 renderer: function (value, metaData, record) {
                     return value ? Uni.I18n.formatNumber(value, 'MDC', 3) + ' ' + measurementType : '';
 
                 }
             }, {
-                fieldLabel: Uni.I18n.translate('deviceloadprofiles.channels.delta', 'MDC', 'Delta'),
+
+                fieldLabel: Uni.I18n.translate('deviceloadprofiles.channels.cumulativeValue', 'MDC', 'Cumulative value'),
                 name: 'delta',
                 renderer: function (value, metaData, record) {
                     return value ? Uni.I18n.formatNumber(value, 'MDC', 3) + ' ' + measurementType: '';
                 }
             });
         } else {
-            me.items.items.push(
+            me.items.items[0].items.push(
                 {
                     fieldLabel: Uni.I18n.translate('deviceloadprofiles.channels.value', 'MDC', 'Value'),
                     name: 'value',
@@ -82,7 +96,7 @@ Ext.define('Mdc.view.setup.deviceloadprofilechannels.DataPreview', {
                 });
         }
 
-        me.items.items.push(
+        me.items.items[0].items.push(
             {
                 fieldLabel: Uni.I18n.translate('deviceloadprofiles.multiplier', 'MDC', 'Multiplier'),
                 name: 'multiplier'
