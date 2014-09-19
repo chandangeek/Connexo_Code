8076ecd5ce1c0c1d5ccbc113fdb2915f7b544e17
adrianlupan <adrianlupan@elster.com>
2014-09-19 10:06:00 +0300

    Implemented the new list of privileges

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/security/Privileges.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/security/Privileges.java
@@ -2,9 +2,6 @@ package com.elster.jupiter.validation.security;
 
 public interface Privileges {
 
-    String CREATE_VALIDATION_RULE = "privilege.create.validationRule";
-    String UPDATE_VALIDATION_RULE = "privilege.update.validationRule";
-    String DELETE_VALIDATION_RULE = "privilege.delete.validationRule";
-    String VIEW_VALIDATION_RULE = "privilege.view.validationRule";
-    String ACTIVATE_VALIDATION_RULE = "privilege.view.activateRule";
+    String ADMINISTRATE_VALIDATION_CONFIGURATION = "privilege.administrate.validationConfiguration";
+    String VIEW_VALIDATION_CONFIGURATION = "privilege.view.validationConfiguration";
 }
