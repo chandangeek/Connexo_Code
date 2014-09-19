d417d686208f351048f1d14e6feed9c4fc1a626e
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-09-19 15:50:43 +0200

    JP-5386 - [BE] Develop Validate data on manual trigger for load profile

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceConfigValidationRuleSetResolver.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceConfigValidationRuleSetResolver.java
@@ -2,10 +2,8 @@ package com.energyict.mdc.device.data.impl;
 
 import com.elster.jupiter.metering.KnownAmrSystem;
 import com.elster.jupiter.metering.MeterActivation;
-import com.elster.jupiter.util.time.Interval;
 import com.elster.jupiter.validation.ValidationRuleSet;
 import com.elster.jupiter.validation.ValidationRuleSetResolver;
-import com.energyict.mdc.device.config.DeviceConfiguration;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.DeviceDataService;
@@ -49,10 +47,7 @@ public class DeviceConfigValidationRuleSetResolver implements ValidationRuleSetR
 
     @Override
     public boolean isValidationRuleSetInUse(ValidationRuleSet ruleset) {
-        if(!deviceConfigurationService.findDeviceConfigurationsForValidationRuleSet(ruleset.getId()).isEmpty()) {
-                return true;
-        }
-        return false;
+        return !deviceConfigurationService.findDeviceConfigurationsForValidationRuleSet(ruleset.getId()).isEmpty();
     }
 
     private boolean hasMdcMeter(MeterActivation meterActivation) {
