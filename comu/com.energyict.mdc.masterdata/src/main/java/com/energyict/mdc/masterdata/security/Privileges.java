6a57b4bf0dc543b98fef2caabfc799221ac2c1be
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-09-22 09:56:01 +0200

    Cleaned up some code

---
+++ comu/com.energyict.mdc.masterdata/src/main/java/com/energyict/mdc/masterdata/security/Privileges.java
@@ -0,0 +1,8 @@
+package com.energyict.mdc.masterdata.security;
+
+/**
+ * Created by bvn on 9/22/14.
+ */
+public interface Privileges {
+    public static final String VIEW_LOAD_PROFILE_TYPE = "x";
+}
