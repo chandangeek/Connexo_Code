310623b18202191df3c4064c3e3127b985462f10
Bruno Ballekens <bruno.ballekens@elster.com>
2014-09-23 12:29:56 +0200

    JP-4062: Apps and bundles strategy: Registeration of apps JP-5246: Rest call to get list of apps

--- comu/com.energyict.mdc.app/src/main/java/com/energyict/mdc/app/Activator.java
+++ comu/com.energyict.mdc.app/src/main/java/com/energyict/mdc/app/Activator.java
@@ -1,7 +1,7 @@
 package com.energyict.mdc.app;
 
+import com.elster.jupiter.http.whiteboard.App;
 import com.elster.jupiter.http.whiteboard.BundleResolver;
-import com.elster.jupiter.http.whiteboard.FileResolver;
 import com.elster.jupiter.http.whiteboard.DefaultStartPage;
 import com.elster.jupiter.http.whiteboard.HttpResource;
 import org.osgi.framework.BundleActivator;
@@ -13,12 +13,13 @@ public class Activator implements BundleActivator {
 
     public static final String HTTP_RESOURCE_ALIAS = "/multisense";
 
-    private volatile ServiceRegistration<HttpResource> registration;
+    private volatile ServiceRegistration<App> registration;
 
     public void start(BundleContext bundleContext) throws Exception {
         HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/mdc", new BundleResolver(bundleContext), new DefaultStartPage("Multi Sense App"));
 //        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.energyict.mdc.app/src/main/web/js/mdc", new FileResolver(), new DefaultStartPage("Multi Sense App"));
-        registration = bundleContext.registerService(HttpResource.class, resource, null);
+        App mdcApp = new App("Connexo Multi Sense", "connexo", HTTP_RESOURCE_ALIAS, resource);
+        registration = bundleContext.registerService(App.class, mdcApp, null);
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
