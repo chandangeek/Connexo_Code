73d1df5fc28f05060b4e4d93814937374bae4fc8
Bruno Ballekens <bruno.ballekens@elster.com>
2014-09-23 12:29:55 +0200

    JP-4062: Apps and bundles strategy: Registeration of apps JP-5246: Rest call to get list of apps

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
@@ -1,7 +1,7 @@
 package com.elster.jupiter.system.app;
 
+import com.elster.jupiter.http.whiteboard.App;
 import com.elster.jupiter.http.whiteboard.BundleResolver;
-import com.elster.jupiter.http.whiteboard.FileResolver;
 import com.elster.jupiter.http.whiteboard.DefaultStartPage;
 import com.elster.jupiter.http.whiteboard.HttpResource;
 import org.osgi.framework.BundleActivator;
@@ -13,12 +13,14 @@ public class Activator implements BundleActivator {
 
     public static final String HTTP_RESOURCE_ALIAS = "/systemadmin";
 
-    private volatile ServiceRegistration<HttpResource> registration;
+    private volatile ServiceRegistration<App> registration;
 
     public void start(BundleContext bundleContext) throws Exception {
         HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/system", new BundleResolver(bundleContext), new DefaultStartPage("System Admin App"));
 //        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.system.app/src/main/web/js/system", new FileResolver(), new DefaultStartPage("System Admin App"));
-        registration = bundleContext.registerService(HttpResource.class, resource, null);
+        App systemAdminApp = new App("Connexo System Admin", "connexo", HTTP_RESOURCE_ALIAS, resource);
+        Class<App> appClass = App.class;
+        registration = bundleContext.registerService(appClass, systemAdminApp, null);
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
