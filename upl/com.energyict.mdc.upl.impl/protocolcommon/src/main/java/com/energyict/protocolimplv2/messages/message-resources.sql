887963a4b60af83f4a90818ef8842815490f2d6b
Kristof Hennebel <Kristof.Hennebel@elster.com>
2014-09-25 11:49:16 +0000

    EISERVERSG-3336 - Port offline comserver from 8.11 to 9.1
    Some minor tweaks
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@85401 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/messages/message-resources.sql
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/messages/message-resources.sql
@@ -27,3 +27,4 @@ insert into nlsstrings (key, english, usage, fullclient, webclient, module, mod_
 insert into nlsstrings (key, english, usage, fullclient, webclient, module, mod_date) values ('AlarmConfigurationMessage.destinationAddress', 'Destination address', 'L', 'Y', 'N', 'MDW', sysdate);
 insert into nlsstrings (key, english, usage, fullclient, webclient, module, mod_date) values ('AlarmConfigurationMessage.messageType', 'Message type', 'L', 'Y', 'N', 'MDW', sysdate);
 insert into nlsstrings (key, english, usage, fullclient, webclient, module, mod_date) values ('SecurityMessage.CHANGE_HLS_SECRET_PASSWORD', 'Change HLS secret', 'L', 'Y', 'N', 'MDW', sysdate);
+insert into nlsstrings (key, english, usage, fullclient, webclient, module, mod_date) values ('reading', 'Reading...', 'L', 'Y', 'N', 'MDW', sysdate);
