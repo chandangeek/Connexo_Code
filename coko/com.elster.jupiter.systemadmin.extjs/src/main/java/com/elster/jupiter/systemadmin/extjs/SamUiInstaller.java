794bfe86627820d4b0c6ac7da5b10e34b8390e6e
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-30 13:14:36 +0200

    [JP-5612] Added a common activator and translation installer.

---
+++ coko/com.elster.jupiter.systemadmin.extjs/src/main/java/com/elster/jupiter/systemadmin/extjs/SamUiInstaller.java
@@ -0,0 +1,17 @@
+package com.elster.jupiter.systemadmin.extjs;
+
+import com.elster.jupiter.install.impl.UiInstallService;
+import com.elster.jupiter.orm.callback.InstallService;
+import org.osgi.service.component.annotations.Component;
+
+@Component(name = "com.elster.jupiter.systemadmin.extjs", service = {InstallService.class},
+        property = {"name=" + SamUiInstaller.COMPONENT_NAME}, immediate = true)
+public class SamUiInstaller extends UiInstallService {
+
+    public static final String COMPONENT_NAME = "SAM";
+
+    public SamUiInstaller() {
+        super(COMPONENT_NAME, new Activator());
+    }
+
+}
