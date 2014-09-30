224814778f7968292e8526740b516689c4cf4a4f
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-30 13:14:25 +0200

    [JP-5612] Added a common activator and translation installer.

--- coko/com.elster.jupiter.config.extjs/src/main/java/com/elster/jupiter/config/extjs/Activator.java
+++ coko/com.elster.jupiter.config.extjs/src/main/java/com/elster/jupiter/config/extjs/Activator.java
@@ -1,29 +1,16 @@
 package com.elster.jupiter.config.extjs;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
+import com.elster.jupiter.http.whiteboard.impl.HttpActivator;
 
-import com.elster.jupiter.http.whiteboard.*;
+public class Activator extends HttpActivator {
 
+    public static final String HTTP_RESOURCE_ALIAS = "/cfg";
+    public static final String HTTP_RESOURCE_LOCAL_NAME = "/js/cfg";
 
-public class Activator implements BundleActivator {
-
-    private volatile ServiceRegistration<HttpResource> registration;
-
-    public void start(BundleContext bundleContext) throws Exception {
-        String alias = "/cfg";
-        HttpResource resource = new HttpResource(alias, "/js/cfg" , new BundleResolver(bundleContext),new DefaultStartPage("Cfg","","/index.html","Cfg.controller.Main"));
-//        HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/jupiter-all/com.elster.jupiter.config.extjs/src/main/web/js/cfg" , new FileResolver(),new DefaultStartPage("Cfg","","/index.html","Cfg.controller.Main"));
-//        HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/comserver-all/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc" , new FileResolver(),new DefaultStartPage("Mdc","","/index.html","Mdc.controller.Main"));
-
-        // Comment above and uncomment next line for file based javascript serving, changing second argument as appropriate
-        //HttpResource resource = new HttpResource(alias, "/home/lvz/Documents/Workspace/Jupiter/meteringextjs/js/mtr" , new FileResolver());
-        registration = bundleContext.registerService(HttpResource.class, resource , null);
-    }
-
-    public void stop(BundleContext bundleContext) throws Exception {
-        registration.unregister();
+    public Activator() {
+        super(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME);
+        // EXAMPLE: Below is how to enable development mode.
+//        super(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.config.extjs/src/main/web/js/cfg", true);
     }
 
 }
