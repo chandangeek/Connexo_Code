4911f7cb85839120736d35edd6a82b9d497894bc
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-30 16:05:46 +0200

    [JP-5612] Refactoring and additions of the activators to the bnd files.

--- coko/com.elster.jupiter.issue.extjs/src/main/java/com/elster/jupiter/issue/extjs/Activator.java
+++ coko/com.elster.jupiter.issue.extjs/src/main/java/com/elster/jupiter/issue/extjs/Activator.java
@@ -1,6 +1,6 @@
 package com.elster.jupiter.issue.extjs;
 
-import com.elster.jupiter.http.whiteboard.impl.HttpActivator;
+import com.elster.jupiter.http.whiteboard.HttpActivator;
 
 public class Activator extends HttpActivator {
 
