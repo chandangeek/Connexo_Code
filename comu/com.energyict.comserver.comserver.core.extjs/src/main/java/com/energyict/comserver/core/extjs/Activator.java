9877319c36daf68195077ea4ca946362e77f4541
Levi Van Zele <Levi.VanZele@elster.com>
2014-09-30 13:14:32 +0200

    [JP-5612] Added a common activator and translation installer.

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/Activator.java
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/Activator.java
@@ -1,36 +1,16 @@
 package com.energyict.comserver.core.extjs;
 
-import com.elster.jupiter.http.whiteboard.BundleResolver;
-import com.elster.jupiter.http.whiteboard.DefaultStartPage;
-import com.elster.jupiter.http.whiteboard.HttpResource;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
+import com.elster.jupiter.http.whiteboard.impl.HttpActivator;
 
-import java.util.Arrays;
+public class Activator extends HttpActivator {
 
-/**
- * Copyrights EnergyICT
- * Date: 28/10/13
- * Time: 10:18
- */
-public class Activator implements BundleActivator {
+    public static final String HTTP_RESOURCE_ALIAS = "/mdc";
+    public static final String HTTP_RESOURCE_LOCAL_NAME = "/js/mdc";
 
-    private volatile ServiceRegistration<HttpResource> registration;
-
-    public void start(BundleContext bundleContext) throws Exception {
-        String alias = "/mdc";
-//        List<Script> scripts = new ArrayList<>();
-//        scripts.add(new Script("jquery","/bla/bla/bla"));
-//        DefaultStartPage mdc = new DefaultStartPage("Mdc", "", "/index.html", "Mdc.controller.Main",scripts);
-        DefaultStartPage mdc = new DefaultStartPage("Mdc", "", "/index.html", "Mdc.controller.Main",null,Arrays.asList("MDC"));
-        HttpResource resource = new HttpResource(alias, "/js/mdc" , new BundleResolver(bundleContext), mdc);
-//        HttpResource resource = new HttpResource(alias, "C:/jupiterrepo/comserver-all/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc" , new FileResolver(), mdc);
-        registration = bundleContext.registerService(HttpResource.class, resource , null);
-    }
-
-    public void stop(BundleContext bundleContext) throws Exception {
-        registration.unregister();
+    public Activator() {
+        super(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME);
+        // EXAMPLE: Below is how to enable development mode.
+//        super(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc", true);
     }
 
 }
--- comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/MdcUiInstaller.java
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/java/com/energyict/comserver/core/extjs/MdcUiInstaller.java
@@ -1,118 +1,17 @@
 package com.energyict.comserver.core.extjs;
 
-import com.elster.jupiter.nls.Layer;
-import com.elster.jupiter.nls.NlsKey;
-import com.elster.jupiter.nls.NlsService;
-import com.elster.jupiter.nls.SimpleNlsKey;
-import com.elster.jupiter.nls.Thesaurus;
-import com.elster.jupiter.nls.Translation;
-import com.elster.jupiter.orm.UnderlyingSQLFailedException;
+import com.elster.jupiter.install.impl.UiInstallService;
 import com.elster.jupiter.orm.callback.InstallService;
-import javax.inject.Inject;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
+@Component(name = "com.energyict.comserver.core.extjs", service = {InstallService.class},
+        property = {"name=" + MdcUiInstaller.COMPONENT_NAME}, immediate = true)
+public class MdcUiInstaller extends UiInstallService {
 
-/**
- * Copyrights EnergyICT
- * Date: 10/02/14
- * Time: 10:15
- */
-@Component(name="com.energyict.mdc.ui",service={InstallService.class},property = {"name=MDC"}, immediate = true)
-public class MdcUiInstaller implements InstallService {
-
-    public static String COMPONENTNAME = "MDC";
-
-    private volatile Thesaurus thesaurus;
-    private volatile Activator activator;
+    public static final String COMPONENT_NAME = "MDC";
 
     public MdcUiInstaller() {
+        super(COMPONENT_NAME, new Activator());
     }
 
-    @Activate
-    public void activate(BundleContext context) {
-        try {
-            activator = new Activator();
-            activator.start(context);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Deactivate
-    public void deactivate() {
-        if (activator != null) {
-            try {
-                activator.stop(null);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    @Inject
-    public MdcUiInstaller(Thesaurus thesaurus) {
-        this.thesaurus = thesaurus;
-    }
-
-    @Reference
-    public void setThesaurus(NlsService nlsService){
-        this.thesaurus = nlsService.getThesaurus("MDC", Layer.REST);
-    }
-
-    @Override
-    public void install() {
-        createTranslations();
-    }
-
-    private void createTranslations() {
-        Properties prop = new Properties();
-        InputStream input = null;
-        List<Translation> translations = new ArrayList<>();
-        try {
-            input = this.getClass().getClassLoader().getResourceAsStream("i18n.properties");
-            prop.load(input);
-            for (Map.Entry<Object, Object> translationProp : prop.entrySet()) {
-                SimpleNlsKey nlsKey = SimpleNlsKey.key(COMPONENTNAME, Layer.REST, (String)translationProp.getKey()).defaultMessage((String)translationProp.getValue());
-                translations.add(toTranslation(nlsKey, Locale.ENGLISH,(String)translationProp.getValue()));
-                translations.add(toTranslation(nlsKey, new Locale("stars"),("************************************************************************************************************************************************" +
-                        "****************************************************************************************************************************************************************************************************" +
-                        "******************************************************************************************************************************************************************************************************").substring(0,translationProp.getValue().toString().length())));
-            }
-            thesaurus.addTranslations(translations);
-
-        } catch (UnderlyingSQLFailedException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Translation toTranslation(final SimpleNlsKey nlsKey, final Locale locale, final String translation) {
-        return new Translation() {
-            @Override
-            public NlsKey getNlsKey() {
-                return nlsKey;
-            }
-
-            @Override
-            public Locale getLocale() {
-                return locale;
-            }
-
-            @Override
-            public String getTranslation() {
-                return translation;
-            }
-        };
-    }
 }
