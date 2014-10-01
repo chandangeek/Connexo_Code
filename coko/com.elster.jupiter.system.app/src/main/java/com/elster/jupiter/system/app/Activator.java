c814e9c0eac0f8b7ed1faa9d1439427eba53a963
Levi Van Zele <Levi.VanZele@elster.com>
2014-10-01 09:48:18 +0200

    [JP-5612] Refactored the remaining activators to the new common activator.

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
@@ -1,30 +1,16 @@
 package com.elster.jupiter.system.app;
 
-import com.elster.jupiter.http.whiteboard.App;
-import com.elster.jupiter.http.whiteboard.BundleResolver;
-import com.elster.jupiter.http.whiteboard.DefaultStartPage;
-import com.elster.jupiter.http.whiteboard.HttpResource;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
+import com.elster.jupiter.http.whiteboard.HttpActivator;
 
-
-public class Activator implements BundleActivator {
+public class Activator extends HttpActivator {
 
     public static final String HTTP_RESOURCE_ALIAS = "/systemadmin";
+    public static final String HTTP_RESOURCE_LOCAL_NAME = "/js/system";
 
-    private volatile ServiceRegistration<App> registration;
-
-    public void start(BundleContext bundleContext) throws Exception {
-        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/system", new BundleResolver(bundleContext), new DefaultStartPage("System Admin App"));
-//        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.system.app/src/main/web/js/system", new FileResolver(), new DefaultStartPage("System Admin App"));
-        App systemAdminApp = new App("Connexo System Admin", "connexo", HTTP_RESOURCE_ALIAS, resource);
-        Class<App> appClass = App.class;
-        registration = bundleContext.registerService(appClass, systemAdminApp, null);
-    }
-
-    public void stop(BundleContext bundleContext) throws Exception {
-        registration.unregister();
+    public Activator() {
+        super(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME);
+        // EXAMPLE: Below is how to enable development mode.
+//        super(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.system.app/src/main/web/js/system", true);
     }
 
 }
