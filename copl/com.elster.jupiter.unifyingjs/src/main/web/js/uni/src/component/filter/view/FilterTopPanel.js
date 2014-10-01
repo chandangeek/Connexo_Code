f0d9f61eafd092aaf9c9fa60323a855b0843c045
Maksim Barinov <mbarinov@mera.ru>
2014-10-01 17:14:39 +0400

    Add usual button to FilterTopPanel

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/component/filter/view/FilterTopPanel.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/component/filter/view/FilterTopPanel.js
@@ -5,13 +5,13 @@ Ext.define('Uni.component.filter.view.FilterTopPanel', {
     extend: 'Uni.view.panel.FilterToolbar',
     alias: 'widget.filter-top-panel',
     title: 'Filters',
-    setFilter: function (key, name, value) {
+    setFilter: function (key, name, value, hideIcon) {
         var me = this,
             btnsContainer = me.getContainer(),
             btn = btnsContainer.down('button[name=' + key + ']');
         if (!_.isEmpty(btn)) {
             btn.setText(name + ': ' + value);
-        } else {
+        } else if (!hideIcon) {
             btnsContainer.add(Ext.create('Uni.view.button.TagButton', {
                 text: name + ': ' + value,
                 name: key,
@@ -20,6 +20,12 @@ Ext.define('Uni.component.filter.view.FilterTopPanel', {
                         me.fireEvent('removeFilter', key);
                     }
                 }
+            }));
+        } else {
+            btnsContainer.add(Ext.create('Ext.button.Button', {
+                text: name + ': ' + value,
+                name: key,
+                ui: 'tag'
             }));
         }
         this.updateContainer(this.getContainer());
