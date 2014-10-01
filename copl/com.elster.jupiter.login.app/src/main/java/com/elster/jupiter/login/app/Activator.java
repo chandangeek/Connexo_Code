7db0a21397f288039d914687aa976925d89f25f4
Levi Van Zele <Levi.VanZele@elster.com>
2014-10-01 09:48:18 +0200

    [JP-5612] Refactored the remaining activators to the new common activator.

--- copl/com.elster.jupiter.login.app/src/main/java/com/elster/jupiter/login/app/Activator.java
+++ copl/com.elster.jupiter.login.app/src/main/java/com/elster/jupiter/login/app/Activator.java
@@ -1,27 +1,16 @@
 package com.elster.jupiter.login.app;
 
-import com.elster.jupiter.http.whiteboard.BundleResolver;
-import com.elster.jupiter.http.whiteboard.FileResolver;
-import com.elster.jupiter.http.whiteboard.DefaultStartPage;
-import com.elster.jupiter.http.whiteboard.HttpResource;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
+import com.elster.jupiter.http.whiteboard.HttpActivator;
 
-public class Activator implements BundleActivator {
+public class Activator extends HttpActivator {
 
     public static final String HTTP_RESOURCE_ALIAS = "/login";
+    public static final String HTTP_RESOURCE_LOCAL_NAME = "/js/login";
 
-    private volatile ServiceRegistration<HttpResource> registration;
-
-    public void start(BundleContext bundleContext) throws Exception {
-        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/js/login", new BundleResolver(bundleContext), new DefaultStartPage("Login App"));
-//        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.login.app/src/main/web/js/login", new FileResolver(), new DefaultStartPage("Login App"));
-        registration = bundleContext.registerService(HttpResource.class, resource, null);
-    }
-
-    public void stop(BundleContext bundleContext) throws Exception {
-        registration.unregister();
+    public Activator() {
+        super(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME);
+        // EXAMPLE: Below is how to enable development mode.
+//        super(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.login.app/src/main/web/js/login", true);
     }
 
 }
