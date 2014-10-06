8585beb541234588634487b805240c36526a73d5
Bruno Ballekens <bruno.ballekens@elster.com>
2014-10-06 18:12:05 +0200

    JP-5883: Changes to installer code to enable automatic testing

--- coko/com.elster.jupiter.issue/src/main/java/com/elster/jupiter/issue/impl/service/InstallServiceImpl.java
+++ coko/com.elster.jupiter.issue/src/main/java/com/elster/jupiter/issue/impl/service/InstallServiceImpl.java
@@ -3,7 +3,11 @@ package com.elster.jupiter.issue.impl.service;
 import com.elster.jupiter.issue.impl.module.Installer;
 import com.elster.jupiter.issue.impl.tasks.IssueOverdueHandlerFactory;
 import com.elster.jupiter.issue.security.Privileges;
-import com.elster.jupiter.issue.share.service.*;
+import com.elster.jupiter.issue.share.service.IssueActionService;
+import com.elster.jupiter.issue.share.service.IssueAssignmentService;
+import com.elster.jupiter.issue.share.service.IssueCreationService;
+import com.elster.jupiter.issue.share.service.IssueMappingService;
+import com.elster.jupiter.issue.share.service.IssueService;
 import com.elster.jupiter.messaging.DestinationSpec;
 import com.elster.jupiter.messaging.MessageService;
 import com.elster.jupiter.metering.MeteringService;
@@ -15,7 +19,6 @@ import com.elster.jupiter.orm.callback.InstallService;
 import com.elster.jupiter.tasks.RecurrentTask;
 import com.elster.jupiter.tasks.RecurrentTaskBuilder;
 import com.elster.jupiter.tasks.TaskService;
-import com.elster.jupiter.users.Privilege;
 import com.elster.jupiter.users.UserService;
 import com.google.inject.AbstractModule;
 import org.osgi.service.component.annotations.Activate;
@@ -24,6 +27,8 @@ import org.osgi.service.component.annotations.Reference;
 
 import javax.inject.Inject;
 import javax.validation.MessageInterpolator;
+import java.util.Arrays;
+import java.util.List;
 
 @Component(name = "com.elster.jupiter.issue.install", service = InstallService.class, property = "name=" + IssueService.COMPONENT_NAME, immediate = true)
 public class InstallServiceImpl implements InstallService {
@@ -33,7 +38,7 @@ public class InstallServiceImpl implements InstallService {
 
     private volatile DataModel dataModel;
     private volatile Thesaurus thesaurus;
-    private volatile  NlsService nlsService;
+    private volatile NlsService nlsService;
     private volatile MessageService messageService;
     private volatile MeteringService meteringService;
     private volatile UserService userService;
@@ -45,7 +50,8 @@ public class InstallServiceImpl implements InstallService {
     private volatile IssueCreationService issueCreationService;
     private volatile IssueActionService issueActionService;
 
-    public InstallServiceImpl(){}
+    public InstallServiceImpl() {
+    }
 
     @Inject
     public InstallServiceImpl(
@@ -80,7 +86,7 @@ public class InstallServiceImpl implements InstallService {
     }
 
     @Activate
-    public final void activate(){
+    public final void activate() {
         dataModel.register(new AbstractModule() {
             @Override
             protected void configure() {
@@ -100,6 +106,7 @@ public class InstallServiceImpl implements InstallService {
             }
         });
     }
+
     @Override
     public final void install() {
         createPrivileges();
@@ -108,17 +115,22 @@ public class InstallServiceImpl implements InstallService {
         createIssueOverdueTask();
     }
 
+    @Override
+    public List<String> getPrerequisiteModules() {
+        return Arrays.asList("USR", "TSK", "MSG", "ORM", "NLS", "MTR");
+    }
+
     private void createPrivileges() {
-        this.userService.createResourceWithPrivileges("MDC", "issue.issues", "issue.issues.description",  new String[] {Privileges.VIEW_ISSUE, Privileges.COMMENT_ISSUE, Privileges.CLOSE_ISSUE, Privileges.ASSIGN_ISSUE, Privileges.ACTION_ISSUE});
-        this.userService.createResourceWithPrivileges("MDC", "issueConfiguration.issueConfigurations", "issueConfiguration.issueConfigurations.description", new String[] {Privileges.VIEW_CREATION_RULE, Privileges.ADMINISTRATE_CREATION_RULE, Privileges.VIEW_ASSIGNMENT_RULE});
+        this.userService.createResourceWithPrivileges("MDC", "issue.issues", "issue.issues.description", new String[]{Privileges.VIEW_ISSUE, Privileges.COMMENT_ISSUE, Privileges.CLOSE_ISSUE, Privileges.ASSIGN_ISSUE, Privileges.ACTION_ISSUE});
+        this.userService.createResourceWithPrivileges("MDC", "issueConfiguration.issueConfigurations", "issueConfiguration.issueConfigurations.description", new String[]{Privileges.VIEW_CREATION_RULE, Privileges.ADMINISTRATE_CREATION_RULE, Privileges.VIEW_ASSIGNMENT_RULE});
     }
 
     private void assignPrivilegesToDefaultRoles() {
-        this.userService.grantGroupWithPrivilege(UserService.DEFAULT_METER_EXPERT_ROLE, new String[] {Privileges.VIEW_CREATION_RULE, Privileges.ADMINISTRATE_CREATION_RULE, Privileges.VIEW_ASSIGNMENT_RULE, Privileges.VIEW_ISSUE, Privileges.COMMENT_ISSUE, Privileges.CLOSE_ISSUE, Privileges.ASSIGN_ISSUE, Privileges.ACTION_ISSUE});
-        this.userService.grantGroupWithPrivilege(UserService.DEFAULT_METER_OPERATOR_ROLE, new String[] {Privileges.VIEW_ISSUE, Privileges.COMMENT_ISSUE, Privileges.CLOSE_ISSUE, Privileges.ASSIGN_ISSUE, Privileges.ACTION_ISSUE});
+        this.userService.grantGroupWithPrivilege(UserService.DEFAULT_METER_EXPERT_ROLE, new String[]{Privileges.VIEW_CREATION_RULE, Privileges.ADMINISTRATE_CREATION_RULE, Privileges.VIEW_ASSIGNMENT_RULE, Privileges.VIEW_ISSUE, Privileges.COMMENT_ISSUE, Privileges.CLOSE_ISSUE, Privileges.ASSIGN_ISSUE, Privileges.ACTION_ISSUE});
+        this.userService.grantGroupWithPrivilege(UserService.DEFAULT_METER_OPERATOR_ROLE, new String[]{Privileges.VIEW_ISSUE, Privileges.COMMENT_ISSUE, Privileges.CLOSE_ISSUE, Privileges.ASSIGN_ISSUE, Privileges.ACTION_ISSUE});
     }
 
-    private void createIssueOverdueTask(){
+    private void createIssueOverdueTask() {
         DestinationSpec destination = messageService.getQueueTableSpec("MSG_RAWTOPICTABLE").get()
                 .createDestinationSpec(IssueOverdueHandlerFactory.ISSUE_OVERDUE_TASK_DESTINATION, ISSUE_OVERDUE_TASK_RETRY_DELAY);
         destination.activate();
@@ -138,35 +150,43 @@ public class InstallServiceImpl implements InstallService {
     public final void setMessageService(MessageService messageService) {
         this.messageService = messageService;
     }
+
     @Reference
     public final void setMeteringService(MeteringService meteringService) {
         this.meteringService = meteringService;
     }
+
     @Reference
     public final void setUserService(UserService userService) {
         this.userService = userService;
     }
+
     @Reference
     public final void setIssueService(IssueService issueService) {
         this.issueService = issueService;
     }
+
     @Reference
     public final void setIssueAssignmentService(IssueAssignmentService issueAssignmentService) {
         this.issueAssignmentService = issueAssignmentService;
     }
+
     @Reference
     public final void setIssueCreationService(IssueCreationService issueCreationService) {
         this.issueCreationService = issueCreationService;
     }
+
     @Reference
     public final void setIssueMappingService(IssueMappingService issueMappingService) {
         dataModel = IssueMappingServiceImpl.class.cast(issueMappingService).getDataModel();
     }
+
     @Reference
     public final void setNlsService(NlsService nlsService) {
         this.nlsService = nlsService;
         this.thesaurus = nlsService.getThesaurus(IssueService.COMPONENT_NAME, Layer.DOMAIN);
     }
+
     @Reference
     public final void setTaskService(TaskService taskService) {
         this.taskService = taskService;
