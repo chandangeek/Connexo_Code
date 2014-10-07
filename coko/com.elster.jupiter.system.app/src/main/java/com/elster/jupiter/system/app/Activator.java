6a3634c8fbc3f5b63b649401e6e13a5e0d93383c
Bruno Ballekens <bruno.ballekens@elster.com>
2014-10-07 13:26:41 +0200

    JP-5883: Changes to installer code to enable automatic testing Clean up

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/Activator.java
@@ -1,13 +1,14 @@
 package com.elster.jupiter.system.app;
 
 import com.elster.jupiter.http.whiteboard.App;
+import com.elster.jupiter.http.whiteboard.BundleResolver;
 import com.elster.jupiter.http.whiteboard.DefaultStartPage;
-import com.elster.jupiter.http.whiteboard.HttpActivator;
 import com.elster.jupiter.http.whiteboard.HttpResource;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-public class Activator extends HttpActivator {
+public class Activator implements BundleActivator {
 
     public static final String HTTP_RESOURCE_ALIAS = "/systemadmin";
     public static final String HTTP_RESOURCE_LOCAL_NAME = "/js/system";
@@ -18,14 +19,11 @@ public class Activator extends HttpActivator {
     private volatile ServiceRegistration<App> registration;
 
     public Activator() {
-        super(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME);
-        // EXAMPLE: Below is how to enable development mode.
-//        super(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.system.app/src/main/web/js/system", true);
     }
 
     @Override
     public void start(BundleContext context) throws Exception {
-        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME, createResolver(context), new DefaultStartPage(APP_NAME));
+        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME, new BundleResolver(context), new DefaultStartPage(APP_NAME));
         App app = new App(APP_NAME, APP_ICON, HTTP_RESOURCE_ALIAS, resource);
 
         registration = context.registerService(App.class, app, null);
