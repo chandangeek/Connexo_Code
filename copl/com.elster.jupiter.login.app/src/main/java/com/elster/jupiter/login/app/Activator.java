4cb6d1c9fb4ff7a759b74b11db41fe2db4f1667d
Bruno Ballekens <bruno.ballekens@elster.com>
2014-10-07 13:26:37 +0200

    JP-5883: Changes to installer code to enable automatic testing Clean up

--- copl/com.elster.jupiter.login.app/src/main/java/com/elster/jupiter/login/app/Activator.java
+++ copl/com.elster.jupiter.login.app/src/main/java/com/elster/jupiter/login/app/Activator.java
@@ -1,16 +1,29 @@
 package com.elster.jupiter.login.app;
 
-import com.elster.jupiter.http.whiteboard.HttpActivator;
+import com.elster.jupiter.http.whiteboard.BundleResolver;
+import com.elster.jupiter.http.whiteboard.HttpResource;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
-public class Activator extends HttpActivator {
+public class Activator implements BundleActivator {
 
     public static final String HTTP_RESOURCE_ALIAS = "/login";
     public static final String HTTP_RESOURCE_LOCAL_NAME = "/js/login";
 
+    private volatile ServiceRegistration<HttpResource> registration;
+
     public Activator() {
-        super(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME);
-        // EXAMPLE: Below is how to enable development mode.
-//        super(HTTP_RESOURCE_ALIAS, "/home/lvz/Documents/Workspace/Jupiter/com.elster.jupiter.login.app/src/main/web/js/login", true);
     }
 
+    @Override
+    public void start(BundleContext context) throws Exception {
+        HttpResource resource = new HttpResource(HTTP_RESOURCE_ALIAS, HTTP_RESOURCE_LOCAL_NAME, new BundleResolver(context));
+        registration = context.registerService(HttpResource.class, resource, null);
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        registration.unregister();
+    }
 }
