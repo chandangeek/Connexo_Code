0a948e3f2829311edc043e43f802fdc1889cb262
Maksim Barinov <mbarinov@mera.ru>
2014-10-08 16:59:00 +0400

    JP-5383

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceLoadProfileChannels.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceLoadProfileChannels.js
@@ -18,6 +18,10 @@ Ext.define('Mdc.controller.setup.DeviceLoadProfileChannels', {
 
     refs: [
         {
+            ref: 'page',
+            selector: 'deviceLoadProfileChannelsSetup'
+        },
+        {
             ref: 'preview',
             selector: 'deviceLoadProfileChannelsSetup #deviceLoadProfileChannelsPreview'
         }
@@ -29,6 +33,9 @@ Ext.define('Mdc.controller.setup.DeviceLoadProfileChannels', {
                 select: this.showPreview
             },
             '#deviceLoadProfileChannelsActionMenu': {
+                click: this.chooseAction
+            },
+            '#channelActionMenu': {
                 click: this.chooseAction
             }
         });
@@ -69,20 +76,25 @@ Ext.define('Mdc.controller.setup.DeviceLoadProfileChannels', {
                     }
                 });
             };
-
+        me.mRID = mRID;
+        me.loadProfileId = loadProfileId;
         timeUnitsStore.getCount() ? showPage() : timeUnitsStore.on('load', showPage, me, {single: true});
     },
 
     showPreview: function (selectionModel, record) {
         var preview = this.getPreview();
-
+        if (!record.data.validationInfo.validationActive) {
+            preview.down('#validateNowChannel').hide();
+            Ext.ComponentQuery.query('#channelActionMenu #validateNowChannel')[0].hide();
+        }
         preview.setTitle(record.get('name'));
         preview.down('#deviceLoadProfileChannelsPreviewForm').loadRecord(record);
         preview.down('#deviceLoadProfileChannelsActionMenu').record = record;
     },
 
     chooseAction: function (menu, item) {
-        var router = this.getController('Uni.controller.history.Router'),
+        var me = this,
+            router = this.getController('Uni.controller.history.Router'),
             route;
 
         switch (item.action) {
@@ -92,11 +104,117 @@ Ext.define('Mdc.controller.setup.DeviceLoadProfileChannels', {
             case 'viewDetails':
                 route = 'devices/device/loadprofiles/loadprofile/channels/channel';
                 break;
+            case 'validateNow':
+                me.showValidateNowMessage(menu.record);
+                break;
         }
 
         route && (route = router.getRoute(route));
         route && route.forward({
             channelId: menu.record.getId()
         });
+    },
+
+    showValidateNowMessage: function (record) {
+        var me = this,
+            confirmationWindow = Ext.create('Uni.view.window.Confirmation', {
+                itemId: 'validateNowChannelConfirmationWindow',
+                confirmText: Uni.I18n.translate('general.validate', 'MDC', 'Validate'),
+                confirmation: function () {
+                    me.activateDataValidation(record, this);
+                }
+            });
+        Ext.Ajax.request({
+            url: '../../api/ddr/devices/' + me.mRID + '/validationrulesets/validationstatus',
+            timeout: 30000000,
+            method: 'GET',
+            success: function (response) {
+                var res = Ext.JSON.decode(response.responseText);
+                if (res.hasValidation) {
+                    if (res.lastChecked) {
+                        me.dataValidationLastChecked = new Date(res.lastChecked);
+                    } else {
+                        me.dataValidationLastChecked = new Date();
+                    }
+                    confirmationWindow.add(me.getValidationContent());
+                    confirmationWindow.show({
+                        title: Uni.I18n.translatePlural('deviceloadprofiles.channels.validateNow', record.get('name'), 'MDC', 'Validate data of channel {0}?'),
+                        msg: ''
+                    });
+                } else {
+                    var title = Uni.I18n.translatePlural('deviceloadprofiles.channels.validateNow.error', record.get('name'), 'MDC', 'Failed to validate data of channel {0}'),
+                        message = Uni.I18n.translate('deviceloadprofiles.channels.noData', 'MDC', 'There is currently no data for this channel'),
+                        config = {
+                            icon: Ext.MessageBox.WARNING
+                        };
+                    me.getApplication().getController('Uni.controller.Error').showError(title, message, config);
+                }
+            }
+        });
+        confirmationWindow.on('close', function () {
+            this.destroy();
+        });
+    },
+
+    getValidationContent: function () {
+        var me = this;
+        return Ext.create('Ext.container.Container', {
+            defaults: {
+                labelAlign: 'left',
+                labelStyle: 'font-weight: normal; padding-left: 50px'
+            },
+            items: [
+                {
+                    xtype: 'datefield',
+                    itemId: 'validateChannelFromDate',
+                    editable: false,
+                    showToday: false,
+                    value: me.dataValidationLastChecked,
+                    fieldLabel: Uni.I18n.translate('deviceloadprofiles.validateNow.item1', 'MDC', 'The data of load profile will be validated starting from'),
+                    labelWidth: 375,
+                    labelPad: 0.5
+                },
+                {
+                    xtype: 'panel',
+                    itemId: 'validateChannelDateErrors',
+                    hidden: true,
+                    bodyStyle: {
+                        color: '#eb5642',
+                        padding: '0 0 15px 65px'
+                    },
+                    html: ''
+                },
+                {
+                    xtype: 'displayfield',
+                    value: '',
+                    padding: '0 0 -10 0',
+                    fieldLabel: Uni.I18n.translate('deviceloadprofiles.validateNow.item2', 'MDC', 'Note: The date displayed by default is the last checked (the moment when the last interval was checked in the validation process).'),
+                    labelWidth: 500
+                }
+            ]
+        });
+    },
+
+    activateDataValidation: function (record, confWindow) {
+        var me = this;
+        if (confWindow.down('#validateChannelFromDate').getValue() > me.dataValidationLastChecked) {
+            confWindow.down('#validateChannelDateErrors').update(Uni.I18n.translate('deviceloadprofiles.activation.error', 'MDC', 'The date should be before or equal to the default date.'));
+            confWindow.down('#validateChannelDateErrors').setVisible(true);
+        } else {
+            Ext.Ajax.request({
+                url: '../../api/ddr/devices/' + me.mRID + '/loadprofiles/' + me.loadProfileId + '/channels/' + record.get('id') + '/validate',
+                method: 'PUT',
+                jsonData: {
+                    lastChecked: confWindow.down('#validateChannelFromDate').getValue().getTime()
+                },
+                success: function () {
+                    confWindow.removeAll(true);
+                    confWindow.destroy();
+                    me.getApplication().fireEvent('acknowledge',
+                        Uni.I18n.translatePlural('deviceloadprofiles.channels.activation.completed', record.get('name'), 'MDC', 'Data validation on channel {0} was completed successfully'));
+                    Ext.ComponentQuery.query('#deviceLoadProfileChannelsGrid')[0].fireEvent('select', Ext.ComponentQuery.query('#deviceLoadProfileChannelsGrid')[0].getSelectionModel(), record);
+                }
+            });
+        }
     }
 });
