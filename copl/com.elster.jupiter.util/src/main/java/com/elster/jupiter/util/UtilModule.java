f0dcc6398077b33a9523d5a224f2d812ccff3ef6
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-10-13 14:53:09 +0200

    JP-5830 Implement test for persisting a RecurrentTask

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/UtilModule.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/UtilModule.java
@@ -2,6 +2,8 @@ package com.elster.jupiter.util;
 
 import com.elster.jupiter.util.beans.BeanService;
 import com.elster.jupiter.util.beans.impl.BeanServiceImpl;
+import com.elster.jupiter.util.cron.CronExpressionParser;
+import com.elster.jupiter.util.cron.impl.DefaultCronExpressionParser;
 import com.elster.jupiter.util.json.JsonService;
 import com.elster.jupiter.util.json.impl.JsonServiceImpl;
 import com.elster.jupiter.util.time.Clock;
@@ -11,10 +13,21 @@ import com.google.inject.Scopes;
 
 public class UtilModule extends AbstractModule {
 
+    public UtilModule(Clock clock) {
+        this.clock = clock;
+    }
+
+    public UtilModule() {
+
+    }
+
+    private Clock clock = new DefaultClock();
+
     @Override
     protected void configure() {
         bind(JsonService.class).to(JsonServiceImpl.class).in(Scopes.SINGLETON);
         bind(BeanService.class).to(BeanServiceImpl.class).in(Scopes.SINGLETON);
-        bind(Clock.class).to(DefaultClock.class).in(Scopes.SINGLETON);
+        bind(Clock.class).toInstance(clock);
+        bind(CronExpressionParser.class).toInstance(new DefaultCronExpressionParser());
     }
 }
