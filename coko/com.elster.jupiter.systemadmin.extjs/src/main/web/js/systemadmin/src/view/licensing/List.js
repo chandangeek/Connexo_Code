4ef23b072957868e3107fecbb8f71290708bf44a
adrianlupan <adrianlupan@elster.com>
2014-10-13 13:39:19 +0300

    Reactivate 'hidden' property of upload buttons

--- coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/licensing/List.js
+++ coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/licensing/List.js
@@ -35,9 +35,8 @@ Ext.define('Sam.view.licensing.List', {
                             text: 'Upload licenses',
                             action: 'uploadlicenses',
                             hrefTarget: '',
-                            href: '#/administration/licensing/upload'
-                            //TODO: uncomment when the privileges are loaded before
-                            //hidden: Uni.Auth.hasNoPrivilege('privilege.upload.license')
+                            href: '#/administration/licensing/upload',
+                            hidden: Uni.Auth.hasNoPrivilege('privilege.upload.license')
                         }
                     ]
                 }
@@ -93,9 +92,8 @@ Ext.define('Sam.view.licensing.List', {
                             text: 'Upload licenses',
                             action: 'uploadlicenses',
                             hrefTarget: '',
-                            href: '#/administration/licensing/upload'
-                            //TODO: uncomment when the privileges are loaded before
-                            //hidden: Uni.Auth.hasNoPrivilege('privilege.upload.license')
+                            href: '#/administration/licensing/upload',
+                            hidden: Uni.Auth.hasNoPrivilege('privilege.upload.license')
                         }
                     ]
                 }
