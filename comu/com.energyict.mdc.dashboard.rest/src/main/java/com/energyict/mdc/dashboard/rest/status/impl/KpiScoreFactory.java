c0a4b17c8615e1b612c4c198591d2a185cabe5ae
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-10-17 08:36:40 +0200

    JP-6130: KPIs now working

--- comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/KpiScoreFactory.java
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/KpiScoreFactory.java
@@ -4,6 +4,7 @@ import com.elster.jupiter.util.time.Interval;
 import com.energyict.mdc.common.rest.ExceptionFactory;
 import com.energyict.mdc.device.data.kpi.DataCollectionKpi;
 import com.energyict.mdc.device.data.kpi.DataCollectionKpiScore;
+import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.DayOfWeek;
 import java.time.Duration;
@@ -14,6 +15,7 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.TemporalAmount;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.inject.Inject;
 
@@ -52,8 +54,9 @@ public class KpiScoreFactory {
             Instant endTimeIndex = Instant.ofEpochMilli(intervalByPeriod.getEnd().getTime());
 
             int kpiScoreIndex=0;
+            kpiScores.add(new SentinelKpiScore());
             while (timeIndex.isBefore(endTimeIndex)) {
-                kpiInfo.time.add(timeIndex.getEpochSecond());
+                kpiInfo.time.add(Date.from(timeIndex).getTime());
                 DataCollectionKpiScore kpiScore = kpiScores.get(kpiScoreIndex);
                 if (kpiScore.getTimestamp().toInstant().equals(timeIndex)) {
                     success.data.add(kpiScore.getSuccess());
@@ -75,28 +78,74 @@ public class KpiScoreFactory {
 
     private Interval getIntervalByPeriod(TemporalAmount temporalAmount) {
         LocalDate startDay=null;
+        LocalDate endDay=null;
         if (temporalAmount.getUnits().contains(ChronoUnit.SECONDS)) {
             if (temporalAmount.get(ChronoUnit.SECONDS) == Duration.ofMinutes(5).getSeconds()
                     || temporalAmount.get(ChronoUnit.SECONDS) == Duration.ofMinutes(15).getSeconds()
                     || temporalAmount.get(ChronoUnit.SECONDS) == Duration.ofMinutes(30).getSeconds()) {
                 startDay = LocalDate.now(clock);
+                endDay = LocalDate.now(clock);
             } else if (temporalAmount.get(ChronoUnit.SECONDS) == Duration.ofHours(1).getSeconds()) {
                 startDay = LocalDate.now(clock).with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+                endDay = LocalDate.now(clock).with(TemporalAdjusters.previous(DayOfWeek.SUNDAY));
             } else if (temporalAmount.get(ChronoUnit.SECONDS) == Duration.ofDays(1).getSeconds()) {
                 startDay = LocalDate.now(clock).with(TemporalAdjusters.firstDayOfMonth());
+                endDay = LocalDate.now(clock).with(TemporalAdjusters.lastDayOfMonth());
             }
         } else {
             if (temporalAmount.get(ChronoUnit.DAYS) == 1) {
                 startDay = LocalDate.now(clock).with(TemporalAdjusters.firstDayOfMonth());
+                endDay = LocalDate.now(clock).with(TemporalAdjusters.lastDayOfMonth());
             } else if (temporalAmount.get(ChronoUnit.MONTHS) == 1) {
                 startDay = LocalDate.now(clock).with(TemporalAdjusters.firstDayOfYear());
+                endDay = LocalDate.now(clock).with(TemporalAdjusters.lastDayOfYear());
             }
         }
         if (startDay==null) {
             throw exceptionFactory.newException(MessageSeeds.UNSUPPORTED_KPI_PERIOD);
         }
 
-        return Interval.of(startDay.atStartOfDay().toInstant(ZoneOffset.UTC), Instant.now(clock));
+        return Interval.of(startDay.atStartOfDay().toInstant(ZoneOffset.UTC), endDay.plusDays(1).atStartOfDay().toInstant(ZoneOffset.UTC));
     }
 
+    /**
+     * Adding this sentinel at the end of array makes sure we never proceed down the kpiScore list (null object)
+     */
+    private class SentinelKpiScore implements DataCollectionKpiScore {
+
+        @Override
+        public Date getTimestamp() {
+            return Date.from(Instant.EPOCH);
+        }
+
+        @Override
+        public BigDecimal getTarget() {
+            return null;
+        }
+
+        @Override
+        public boolean meetsTarget() {
+            return false;
+        }
+
+        @Override
+        public BigDecimal getSuccess() {
+            return null;
+        }
+
+        @Override
+        public BigDecimal getOngoing() {
+            return null;
+        }
+
+        @Override
+        public BigDecimal getFailed() {
+            return null;
+        }
+
+        @Override
+        public int compareTo(DataCollectionKpiScore o) {
+            return 0;
+        }
+    }
 }
--- comu/com.energyict.mdc.dashboard.rest/src/test/java/com/energyict/mdc/dashboard/rest/status/impl/ConnectionOverviewResourceTest.java
+++ comu/com.energyict.mdc.dashboard.rest/src/test/java/com/energyict/mdc/dashboard/rest/status/impl/ConnectionOverviewResourceTest.java
@@ -136,8 +136,33 @@ public class ConnectionOverviewResourceTest extends DashboardApplicationJerseyTe
         assertThat(connectionOverviewInfo.breakdowns.get(0).counters).isSortedAccordingTo(taskBreakdownInfoComparator);
         assertThat(connectionOverviewInfo.breakdowns.get(1).counters).isSortedAccordingTo(taskBreakdownInfoComparator);
         assertThat(connectionOverviewInfo.breakdowns.get(2).counters).isSortedAccordingTo(taskBreakdownInfoComparator);
+    }
+
+    @Test
+    public void testGetOverviewWithKpisWithDeviceGroup() throws UnsupportedEncodingException {
+        int deviceGroupId = 123;
+
+        TaskStatusOverview taskStatusOverview = createConnectionStatusOverview();
+        QueryEndDeviceGroup endDeviceGroup = mock(QueryEndDeviceGroup.class);
+        when(dashboardService.getConnectionTaskStatusOverview(endDeviceGroup)).thenReturn(taskStatusOverview);
+        ComSessionSuccessIndicatorOverview comSessionSuccessIndicatorOverview = createComTaskCompletionOverview();
+        when(dashboardService.getComSessionSuccessIndicatorOverview(endDeviceGroup)).thenReturn(comSessionSuccessIndicatorOverview);
+        ComPortPoolBreakdown comPortPoolBreakdown = createComPortPoolBreakdown();
+        when(dashboardService.getComPortPoolBreakdown(endDeviceGroup)).thenReturn(comPortPoolBreakdown);
+        ConnectionTypeBreakdown connectionStatusBreakdown = createConnectionTypeBreakdown();
+        when(dashboardService.getConnectionTypeBreakdown(endDeviceGroup)).thenReturn(connectionStatusBreakdown);
+        DeviceTypeBreakdown deviceTypeBreakdown=createDeviceTypeBreakdown();
+        when(dashboardService.getConnectionTasksDeviceTypeBreakdown(endDeviceGroup)).thenReturn(deviceTypeBreakdown);
+        DataCollectionKpi dataCollectionKpi = mockDataCollectionKpi();
+        when(dataCollectionKpiService.findDataCollectionKpi(endDeviceGroup)).thenReturn(Optional.of(dataCollectionKpi));
+        when(meteringGroupsService.findQueryEndDeviceGroup(deviceGroupId)).thenReturn(com.google.common.base.Optional.of(endDeviceGroup));
+        when(endDeviceGroup.getId()).thenReturn((long) deviceGroupId);
+        when(endDeviceGroup.getName()).thenReturn("South region");
+
+        ConnectionOverviewInfo connectionOverviewInfo = target("/connectionoverview").queryParam("filter", ExtjsFilter.filter("deviceGroup", (long) deviceGroupId)).request().get(ConnectionOverviewInfo.class);
+
         assertThat(connectionOverviewInfo.kpi).isNotNull();
-        assertThat(connectionOverviewInfo.kpi.time).hasSize(1440); // all day
+        assertThat(connectionOverviewInfo.kpi.time).hasSize(96); // all day
         assertThat(connectionOverviewInfo.kpi.series.get(0).name).isEqualTo("Success");
         assertThat(connectionOverviewInfo.kpi.series.get(1).name).isEqualTo("Ongoing");
         assertThat(connectionOverviewInfo.kpi.series.get(2).name).isEqualTo("Failed");
@@ -145,6 +170,17 @@ public class ConnectionOverviewResourceTest extends DashboardApplicationJerseyTe
         assertThat(connectionOverviewInfo.deviceGroup.id).isEqualTo(123);
         assertThat(connectionOverviewInfo.deviceGroup.name).isEqualTo("South region");
         assertThat(connectionOverviewInfo.deviceGroup.alias).isEqualTo("deviceGroups");
+
+        // Assert the first KPI (alignment between different lists on index)
+        assertThat(connectionOverviewInfo.kpi.time.get(56)).isEqualTo(Date.from(LocalDateTime.of(2014,10,1,14, 0, 0).toInstant(ZoneOffset.UTC)).getTime());
+        assertThat(connectionOverviewInfo.kpi.series.get(0).name).isEqualTo("Success");
+        assertThat(connectionOverviewInfo.kpi.series.get(0).data.get(56)).isEqualTo(BigDecimal.valueOf(10L));
+        assertThat(connectionOverviewInfo.kpi.series.get(1).name).isEqualTo("Ongoing");
+        assertThat(connectionOverviewInfo.kpi.series.get(1).data.get(56)).isEqualTo(BigDecimal.valueOf(80L));
+        assertThat(connectionOverviewInfo.kpi.series.get(2).name).isEqualTo("Failed");
+        assertThat(connectionOverviewInfo.kpi.series.get(2).data.get(56)).isEqualTo(BigDecimal.valueOf(10L));
+        assertThat(connectionOverviewInfo.kpi.series.get(3).name).isEqualTo("Target");
+        assertThat(connectionOverviewInfo.kpi.series.get(3).data.get(56)).isEqualTo(BigDecimal.valueOf(100L));
     }
 
     private DataCollectionKpi mockDataCollectionKpi() {
