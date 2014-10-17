c33a917a04fd92f856d2d78e083259a43dc107c8
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-10-17 11:25:28 +0200

    JP-6234: Com port pool field now correctly filled in

--- comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/ConnectionTaskInfoFactory.java
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/ConnectionTaskInfoFactory.java
@@ -58,8 +58,8 @@ public class ConnectionTaskInfoFactory {
             info.endDateTime = Date.from(comSession.getStopDate().with(ChronoField.MILLI_OF_SECOND, 0));
             info.duration=new TimeDurationInfo(Duration.ofMillis(info.endDateTime.getTime()-info.startDateTime.getTime()).getSeconds());   // JP-6022
             info.comPort = new IdWithNameInfo(comSession.getComPort());
-            info.comPortPool = new IdWithNameInfo(connectionTask.getComPortPool());
             info.comServer = new IdWithNameInfo(comSession.getComPort().getComServer());
+            info.comPortPool = new IdWithNameInfo(connectionTask.getComPortPool());
         }
 
         info.direction=thesaurus.getString(connectionTask.getConnectionType().getDirection().name(),connectionTask.getConnectionType().getDirection().name());
