e9021fb8c2e11127f962ad8cb06486572ff710fa
kha <karel.haeck@elster.com>
2014-10-18 12:24:20 +0200

    removed unused java.util.Date import

--- coko/com.elster.jupiter.gogo/src/main/java/com/elster/jupiter/gogo/MeteringCommands.java
+++ coko/com.elster.jupiter.gogo/src/main/java/com/elster/jupiter/gogo/MeteringCommands.java
@@ -30,7 +30,6 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
