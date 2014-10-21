0cfe7a1e2e3332081f63d5e18f9952ecc1f22476
adrianlupan <adrianlupan@elster.com>
2014-10-21 10:46:11 +0300

    Added specific privileges to application

--- comu/com.energyict.mdc.app/src/main/web/js/mdc/app/controller/Main.js
+++ comu/com.energyict.mdc.app/src/main/web/js/mdc/app/controller/Main.js
@@ -7,7 +7,13 @@ Ext.define('MdcApp.controller.Main', {
     applicationTitle: 'Connexo MultiSense',
     defaultToken: '#/workspace',
     searchEnabled: Uni.Auth.hasAnyPrivilege(['privilege.administrate.device','privilege.view.device']),
-
+    privileges: ['privilege.view.issue','privilege.comment.issue','privilege.close.issue','privilege.assign.issue','privilege.action.issue',
+        'privilege.view.creationRule','privilege.administrate.creationRule','privilege.view.assignmentRule','privilege.administrate.validationConfiguration',
+        'privilege.view.validationConfiguration','privilege.administrate.schedule','privilege.view.schedule','privilege.administrate.communicationInfrastructure',
+        'privilege.view.communicationInfrastructure','privilege.administrate.protocol','privilege.view.protocol','privilege.administrate.deviceConfiguration',
+        'privilege.view.deviceConfiguration','privilege.administrate.device','privilege.view.device','privilege.view.validateDevice','privilege.view.scheduleDevice',
+        'privilege.import.inventoryManagement','privilege.revoke.inventoryManagement','privilege.create.inventoryManagement','privilege.administrate.deviceSecurity',
+        'privilege.view.deviceSecurity'],
     controllers: [
         'Cfg.controller.Main',
         'Mdc.controller.Main',
