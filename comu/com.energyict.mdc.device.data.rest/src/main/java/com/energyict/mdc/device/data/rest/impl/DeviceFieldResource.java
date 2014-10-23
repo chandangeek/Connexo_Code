b61ccad99e455e2022d8f205a279ff3ee4801d61
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-10-23 13:05:09 +0200

    JP-6327: Added generic device category field resource: missing user visibility filter

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceFieldResource.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceFieldResource.java
@@ -1,21 +1,27 @@
 package com.energyict.mdc.device.data.rest.impl;
 
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsService;
+import com.energyict.mdc.common.rest.FieldResource;
+import com.energyict.mdc.common.rest.IdWithNameInfo;
+import com.energyict.mdc.protocol.api.device.messages.DeviceMessageService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.elster.jupiter.nls.Layer;
-import com.elster.jupiter.nls.NlsService;
-import com.energyict.mdc.common.rest.FieldResource;
+import static java.util.stream.Collectors.toList;
 
 @Path("/field")
 public class DeviceFieldResource extends FieldResource {
-    
+
+    private final DeviceMessageService deviceMessageService;
+
     @Inject
-    public DeviceFieldResource(NlsService nlsService) {
+    public DeviceFieldResource(NlsService nlsService, DeviceMessageService deviceMessageService) {
         super(nlsService.getThesaurus(DeviceApplication.COMPONENT_NAME, Layer.REST));
+        this.deviceMessageService = deviceMessageService;
     }
     
     @GET
@@ -38,4 +44,13 @@ public class DeviceFieldResource extends FieldResource {
     public Object getAllEndDeviceEventOrActions() {
         return asJsonArrayObjectWithTranslation("eventOrActions", "eventOrAction", new EndDeviceEventOrActionAdapter().getClientSideValues());
     }
+
+    @GET
+    @Path("/commandcategories")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object getAllCommandCategories() {
+        return asJsonArrayObjectWithTranslation("commandCategories", "commandCategory", deviceMessageService.allCategories().stream().map(c->new IdWithNameInfo(c.getId(),c.getName())).collect(toList()));
+    }
+
+
 }
