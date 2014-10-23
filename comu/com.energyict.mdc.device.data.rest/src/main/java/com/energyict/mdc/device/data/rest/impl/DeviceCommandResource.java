dc9437394ebbe288f01e51edc82cf8ca4203d45d
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-10-23 08:57:32 +0200

    JP-6327: Inter

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceCommandResource.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceCommandResource.java
@@ -2,7 +2,12 @@ package com.energyict.mdc.device.data.rest.impl;
 
 import com.energyict.mdc.common.rest.PagedInfoList;
 import com.energyict.mdc.common.rest.QueryParameters;
+import com.energyict.mdc.device.config.ComTaskEnablement;
+import com.energyict.mdc.device.data.CommunicationTaskService;
 import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.data.DeviceService;
+import com.energyict.mdc.device.data.tasks.ComTaskExecution;
+import com.energyict.mdc.tasks.ComTask;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.BeanParam;
@@ -29,7 +34,22 @@ public class DeviceCommandResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public PagedInfoList getDeviceCommands(@PathParam("mRID") String mrid, @BeanParam QueryParameters queryParameters) {
+
+
+
         Device device = resourceHelper.findDeviceByMrIdOrThrowException(mrid);
+
+        List<ComTask> comTasks = device.getDeviceConfiguration().
+                getComTaskEnablements().stream().
+                map(ComTaskEnablement::getComTask).
+
+//                map(ComTask::getProtocolTasks).
+//                filter(task -> task instanceof MessagesTask).
+//                map(task -> ((MessagesTask)task).getDeviceMessageCategories()).
+//                ;
+//
+        device.getComTaskExecutions().stream().map(ComTaskExecution::)
+
         List<DeviceMessageInfo> infos = device.getMessages().stream().
                 sorted((c1, c2) -> c2.getReleaseDate().compareTo(c1.getReleaseDate())).
                 map(deviceMessageInfoFactory::asInfo).
