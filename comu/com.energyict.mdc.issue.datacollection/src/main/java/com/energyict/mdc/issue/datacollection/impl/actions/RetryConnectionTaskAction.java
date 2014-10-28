97c20c999ba8d19908348df1eec16e03d89cd929
Lobanov Dmitry <dlobanov@mera.ru>
2014-10-28 10:57:14 +0300

    Fail action result

--- comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/actions/RetryConnectionTaskAction.java
+++ comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/actions/RetryConnectionTaskAction.java
@@ -47,7 +47,7 @@ public class RetryConnectionTaskAction extends AbstractIssueAction {
     public <T extends Issue> boolean isApplicable(T issue) {
         if (issue != null && issue instanceof IssueDataCollection){
             IssueDataCollection dcIssue = (IssueDataCollection) issue;
-            if (!dcIssue.getStatus().isHistorical() && dcIssue.getConnectionTask().isPresent()){
+            if (!dcIssue.getStatus().isHistorical() && dcIssue.getConnectionTask().isPresent() && !dcIssue.getCommunicationTask().isPresent()){
                 ConnectionTask task = dcIssue.getConnectionTask().get();
                 return task instanceof ScheduledConnectionTask
                         && task.getConnectionType().getDirection() == ConnectionType.Direction.OUTBOUND;
