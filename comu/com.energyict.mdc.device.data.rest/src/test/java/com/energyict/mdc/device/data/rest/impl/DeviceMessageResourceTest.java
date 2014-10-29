91d6c52c8f9658af5aa4a96f102d947e88c16bd2
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-10-29 15:59:50 +0100

    JP-6724 : Added test

--- comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/DeviceMessageResourceTest.java
+++ comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/DeviceMessageResourceTest.java
@@ -376,6 +376,10 @@ public class DeviceMessageResourceTest extends DeviceDataRestApplicationJerseyTe
         assertThat(jsonModel.<String>get("$[0].name")).isEqualTo("Contactor");
         assertThat(jsonModel.<Integer>get("$[0].id")).isEqualTo(1);
         assertThat(jsonModel.<List>get("$[0].deviceMessageSpecs")).hasSize(2); // 2 device messages
+        assertThat(jsonModel.<List>get("$[0].deviceMessageSpecs[0].propertySpecs")).hasSize(1);
+        assertThat(jsonModel.<String>get("$[0].deviceMessageSpecs[0].propertySpecs[0].key")).isEqualTo("ContactorDeviceMessage.digitalOutput");
+        assertThat(jsonModel.<String>get("$[0].deviceMessageSpecs[0].propertySpecs[0].propertyTypeInfo.simplePropertyType")).isEqualTo("NUMBER");
+        assertThat(jsonModel.<Boolean>get("$[0].deviceMessageSpecs[0].propertySpecs[0].required")).isEqualTo(true);
     }
 
     @Test
