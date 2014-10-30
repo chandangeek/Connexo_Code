704a6ee272535f77860705012aef05972eee2bbc
kha <karel.haeck@elster.com>
2014-10-30 18:00:23 +0100

    ChannelValidationContainer

---
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/ChannelValidationContainer.java
@@ -0,0 +1,43 @@
+package com.elster.jupiter.validation.impl;
+
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import com.elster.jupiter.validation.ChannelValidation;
+
+public class ChannelValidationContainer {
+
+	private final List<IChannelValidation> channelValidations;
+	
+	private ChannelValidationContainer(List<IChannelValidation> channelValidations) {
+		this.channelValidations = channelValidations;
+	}
+	
+	static ChannelValidationContainer of(List<IChannelValidation> channelValidations) {
+		return new ChannelValidationContainer(channelValidations);
+	}
+	
+	void updateLastChecked(Instant date) {
+		 channelValidations.stream()
+         	.filter(IChannelValidation::hasActiveRules)
+         	.forEach(cv -> {
+         		cv.updateLastChecked(date);
+         		cv.getMeterActivationValidation().save();
+         	});
+	}
+	
+	boolean isValidationActive() {
+    	return channelValidations.stream().anyMatch(ChannelValidation::hasActiveRules);
+    }
+	
+	Optional<Instant> getLastChecked() {
+		return channelValidations.stream()
+			.map(IChannelValidation::getLastChecked)
+			.filter(Objects::nonNull)
+			.min(Comparator.naturalOrder());
+	}
+	
+}
