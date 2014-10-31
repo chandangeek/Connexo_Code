f7acd504f01931978ebb115c9116fd76006c1b90
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2014-10-31 13:22:26 +0100

    JP-788: Use ProtocolPluggableService as a facade for DeviceCacheMarshallingService

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocols/mdc/services/impl/DeviceCacheMarshallingServiceImpl.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocols/mdc/services/impl/DeviceCacheMarshallingServiceImpl.java
@@ -1,6 +1,9 @@
 package com.energyict.protocols.mdc.services.impl;
 
+import com.energyict.mdc.protocol.api.services.DeviceCacheMarshallingException;
 import com.energyict.mdc.protocol.api.services.DeviceCacheMarshallingService;
+import com.energyict.mdc.protocol.api.services.NotAppropriateDeviceCacheMarshallingTargetException;
+
 import org.osgi.service.component.annotations.Component;
 
 import javax.xml.bind.JAXBContext;
@@ -9,6 +12,7 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,37 +31,54 @@ public class DeviceCacheMarshallingServiceImpl implements DeviceCacheMarshalling
     private static final int JSON_PAYLOAD_INDEX = 1;
 
     @Override
-    public Object unMarshallCache(String jsonCache) {
-        String[] cacheElements = jsonCache.split(REGEX);
+    public Optional<Object> unMarshallCache(String marshalledCache) {
+        String[] cacheElements = marshalledCache.split(REGEX);
         if (cacheElements.length == NUMBER_OF_ELEMENTS_IN_JSON_CACHE) {
+            String className = cacheElements[CLASS_NAME_INDEX];
+            String jsonPayload = cacheElements[JSON_PAYLOAD_INDEX];
             try {
-                JAXBContext jc = JAXBContext.newInstance(Class.forName(cacheElements[CLASS_NAME_INDEX]));
+                JAXBContext jc = JAXBContext.newInstance(Class.forName(className));
                 Unmarshaller unmarshaller = jc.createUnmarshaller();
-                return unmarshaller.unmarshal(new StringReader(cacheElements[JSON_PAYLOAD_INDEX]));
-            } catch (JAXBException | ClassNotFoundException e) {
+                return Optional.of(unmarshaller.unmarshal(new StringReader(jsonPayload)));
+            }
+            catch (JAXBException e) {
                 // if some unMarshalling exception occurs, then log it and shove it under the rug
-                LOGGER.log(Level.WARNING, "An error occurred during the UnMarshalling of the DeviceProtocolCache: " + e.getMessage(), e);
+                LOGGER.log(Level.SEVERE, "An error occurred during the UnMarshalling of the DeviceProtocolCache: " + e.getMessage(), e);
+                throw new DeviceCacheMarshallingException("Marshalling error in JAXBContext for class " + className, e);
+            }
+            catch (ClassNotFoundException e) {
+                // Not for one of my classes
+                throw new NotAppropriateDeviceCacheMarshallingTargetException();
             }
         }
-        return null;
+        else {
+            throw new NotAppropriateDeviceCacheMarshallingTargetException();
+        }
     }
 
     @Override
-    public String marshall(Object legacyCache) {
-        StringWriter stringWriter = new StringWriter();
-        if (legacyCache != null) {
-            try {
-                Class<?> cacheClass = legacyCache.getClass();
-                stringWriter.append(cacheClass.getName()).append(REGEX);
-                JAXBContext jc = JAXBContext.newInstance(cacheClass);
-                Marshaller marshaller = jc.createMarshaller();
-                marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-                marshaller.marshal(legacyCache, stringWriter);
-            } catch (JAXBException e) {
-                // if some marshalling exception occurs, then log it and shove it under the rug
-                LOGGER.log(Level.WARNING, "An error occurred during the Marshalling of the DeviceProtocolCache: " + e.getMessage(), e);
-            }
+    public String marshall(Object cache) {
+        Class<?> cacheClass = cache.getClass();
+        String className = cacheClass.getName();
+        try {
+            Class.forName(className);   // Try to load the class to check if the cache is created/managed by this bundle
+            StringWriter stringWriter = new StringWriter();
+            stringWriter.append(className).append(REGEX);
+            JAXBContext jc = JAXBContext.newInstance(cacheClass);
+            Marshaller marshaller = jc.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(cache, stringWriter);
+            return stringWriter.toString();
         }
-        return stringWriter.toString();
+        catch (JAXBException e) {
+            // if some marshalling exception occurs, then log it and shove it under the rug
+            LOGGER.log(Level.SEVERE, "An error occurred during the Marshalling of the DeviceProtocolCache: " + e.getMessage(), e);
+            throw new DeviceCacheMarshallingException("Marshalling error in JAXBContext for class " + className, e);
+        }
+        catch (ClassNotFoundException e) {
+            // Not for one of my classes
+            throw new NotAppropriateDeviceCacheMarshallingTargetException();
+        }
     }
+
 }
