a7d8c4e2f9bd0b57c6f7ed5d0deb4b28fab4d199
Bruno Ballekens <bruno.ballekens@elster.com>
2014-11-04 11:59:26 +0100

    JP-2073: Run data export task. First commit

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/time/Interval.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/time/Interval.java
@@ -1,37 +1,36 @@
 package com.elster.jupiter.util.time;
 
+import com.google.common.collect.BoundType;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Range;
 
-import java.util.Date;
 import java.time.Clock;
 import java.time.Instant;
+import java.util.Date;
 import java.util.Objects;
 
 /**
- * 
  * Interval represents a Date range that may be finite or infinite at either end.
  * All instance creation methods that accept a Date instance allow null as value, and will interpret this to mean the Interval is infinite at the approriate bound.
- * 
  */
 public final class Interval {
-	private static final long ETERNITY = 1_000_000_000_000_000_000L;
-	private static final Interval SINCE_EPOCH = new Interval(0,ETERNITY);
+    private static final long ETERNITY = 1_000_000_000_000_000_000L;
+    private static final Interval SINCE_EPOCH = new Interval(0, ETERNITY);
 
     private final long start;
-	private final long end;
+    private final long end;
 
     /**
      * @param start Date instance marking the start of the interval, or null to signify the interval is infinite at the start
-     * @param end Date instance marking the end of the interval, or null to signify the interval is infinite at the end
+     * @param end   Date instance marking the end of the interval, or null to signify the interval is infinite at the end
      */
-	private Interval(Instant start, Instant end) {
-		this.start = getStartValue(start);
-		this.end = getEndValue(end);
+    private Interval(Instant start, Instant end) {
+        this.start = getStartValue(start);
+        this.end = getEndValue(end);
         if (this.start > this.end) {
             throw new IllegalArgumentException("Start cannot be later than end.");
         }
-	} 
+    }
 
     private Interval(long start, long end) {
         this.start = start;
@@ -43,31 +42,33 @@ public final class Interval {
 
     @Deprecated
     public Interval(Date start, Date end) {
-    	this.start = start == null ? -ETERNITY : getStartValue(start.toInstant());
-    	this.end = end == null ? ETERNITY : getEndValue(end.toInstant());
+        this.start = start == null ? -ETERNITY : getStartValue(start.toInstant());
+        this.end = end == null ? ETERNITY : getEndValue(end.toInstant());
     }
-    
+
     public static Interval of(Instant start, Instant end) {
-    	return new Interval(start,end);
+        return new Interval(start, end);
     }
-    
+
     public static Interval of(Range<Instant> range) {
-    	long start = range.hasLowerBound() ? range.lowerEndpoint().toEpochMilli() : -ETERNITY;
-    	long end = range.hasUpperBound() ? range.upperEndpoint().toEpochMilli() : ETERNITY;
-    	return new Interval(start,end);
+        long start = range.hasLowerBound() ? range.lowerEndpoint().toEpochMilli() : -ETERNITY;
+        long end = range.hasUpperBound() ? range.upperEndpoint().toEpochMilli() : ETERNITY;
+        return new Interval(start, end);
     }
-    
+
     /**
      * Static factory method to create an infinite Interval that starts at the given Date.
+     *
      * @param start
      * @return
      */
     public static Interval startAt(Instant start) {
         return new Interval(start, null);
     }
-    
+
     /**
      * Static factory method to create an infinite Interval that ends at the given Date.
+     *
      * @param end
      * @return
      */
@@ -76,69 +77,70 @@ public final class Interval {
     }
 
     public static Interval sinceEpoch() {
-    	return SINCE_EPOCH;
+        return SINCE_EPOCH;
     }
-    
-	public Instant getEnd() {
-		return end == ETERNITY ? null : Instant.ofEpochMilli(end);
-	}
 
-	public Instant getStart() {
-		return start == -ETERNITY ? null : Instant.ofEpochMilli(start);
-	}
+    public Instant getEnd() {
+        return end == ETERNITY ? null : Instant.ofEpochMilli(end);
+    }
 
-    /** 
+    public Instant getStart() {
+        return start == -ETERNITY ? null : Instant.ofEpochMilli(start);
+    }
+
+    /**
      * As this method is used for validity check, the CLOSED_OPEN endpoint behavior is used.
+     *
      * @param clock
      * @return true if the current time according to the given Clock is contained in this Interval.
      */
-	public boolean isCurrent(Clock clock) {
-		long now = clock.instant().toEpochMilli();
-		return contains(now,EndpointBehavior.CLOSED_OPEN);
-	}
-	
-	public boolean isEffective(Interval interval) {
-		return this.overlaps(interval);
-	}
-	
-	public Range<Instant> toOpenClosedRange() {
-		return (start == -ETERNITY) ?
-			(end == ETERNITY ? Range.all() : Range.lessThan(Instant.ofEpochMilli(end))) :
-			(end == ETERNITY ? 
-					Range.atLeast(Instant.ofEpochMilli(start)) :
-					Range.openClosed(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
-	}
-	
-	public Range<Instant> toClosedOpenRange() {
-		return (start == -ETERNITY) ?
-			(end == ETERNITY ? Range.all() : Range.lessThan(Instant.ofEpochMilli(end))) :
-			(end == ETERNITY ? 
-					Range.atLeast(Instant.ofEpochMilli(start)) :
-					Range.closedOpen(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
-	}
-	
-	public Range<Instant> toClosedRange() {
-		return (start == -ETERNITY) ?
-			(end == ETERNITY ? Range.all() : Range.atMost(Instant.ofEpochMilli(end))) :
-			(end == ETERNITY ? 
-					Range.atLeast(Instant.ofEpochMilli(start)) :
-					Range.closed(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
-	}
-	
-	public Range<Instant> toOpenRange() {
-		return (start == -ETERNITY) ?
-			(end == ETERNITY ? Range.all() : Range.lessThan(Instant.ofEpochMilli(end))) :
-			(end == ETERNITY ? 
-					Range.greaterThan(Instant.ofEpochMilli(start)) :
-					Range.open(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
-	}
+    public boolean isCurrent(Clock clock) {
+        long now = clock.instant().toEpochMilli();
+        return contains(now, EndpointBehavior.CLOSED_OPEN);
+    }
+
+    public boolean isEffective(Interval interval) {
+        return this.overlaps(interval);
+    }
+
+    public Range<Instant> toOpenClosedRange() {
+        return (start == -ETERNITY) ?
+                (end == ETERNITY ? Range.all() : Range.lessThan(Instant.ofEpochMilli(end))) :
+                (end == ETERNITY ?
+                        Range.atLeast(Instant.ofEpochMilli(start)) :
+                        Range.openClosed(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
+    }
+
+    public Range<Instant> toClosedOpenRange() {
+        return (start == -ETERNITY) ?
+                (end == ETERNITY ? Range.all() : Range.lessThan(Instant.ofEpochMilli(end))) :
+                (end == ETERNITY ?
+                        Range.atLeast(Instant.ofEpochMilli(start)) :
+                        Range.closedOpen(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
+    }
+
+    public Range<Instant> toClosedRange() {
+        return (start == -ETERNITY) ?
+                (end == ETERNITY ? Range.all() : Range.atMost(Instant.ofEpochMilli(end))) :
+                (end == ETERNITY ?
+                        Range.atLeast(Instant.ofEpochMilli(start)) :
+                        Range.closed(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
+    }
+
+    public Range<Instant> toOpenRange() {
+        return (start == -ETERNITY) ?
+                (end == ETERNITY ? Range.all() : Range.lessThan(Instant.ofEpochMilli(end))) :
+                (end == ETERNITY ?
+                        Range.greaterThan(Instant.ofEpochMilli(start)) :
+                        Range.open(Instant.ofEpochMilli(start), Instant.ofEpochMilli(this.end)));
+    }
 
     public Range<Instant> toRange(EndpointBehavior endpointBehavior) {
         return endpointBehavior.toRange(this);
     }
 
-    private boolean contains(long when,EndpointBehavior behavior) {
-    	return behavior.contains(this,when);
+    private boolean contains(long when, EndpointBehavior behavior) {
+        return behavior.contains(this, when);
     }
 
     /**
@@ -148,36 +150,36 @@ public final class Interval {
     public boolean overlaps(Interval other) {
         return other.end > start && end > other.start;
     }
-	
-	@SuppressWarnings("unused")
-	private Interval() {
-		start = 0;
-		end = 0;
-	}
-	
-	private long getEndValue(Instant endInstant) {
-		if (endInstant == null) {
-			return ETERNITY;
-		} else {
-			if (endInstant.toEpochMilli() >= ETERNITY) {
-				throw new IllegalArgumentException("End instant too late");
-			} else {
-				return endInstant.toEpochMilli();
-			}
-		}
-	}
-	
-	private long getStartValue(Instant startInstant) {
-		if (startInstant == null) {
-			return -ETERNITY;
-		} else {
-			if (startInstant.toEpochMilli() <= -ETERNITY) {
-				throw new IllegalArgumentException("Start instant too early");
-			} else {
-				return startInstant.toEpochMilli();
-			}
-		}
-	}
+
+    @SuppressWarnings("unused")
+    private Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    private long getEndValue(Instant endInstant) {
+        if (endInstant == null) {
+            return ETERNITY;
+        } else {
+            if (endInstant.toEpochMilli() >= ETERNITY) {
+                throw new IllegalArgumentException("End instant too late");
+            } else {
+                return endInstant.toEpochMilli();
+            }
+        }
+    }
+
+    private long getStartValue(Instant startInstant) {
+        if (startInstant == null) {
+            return -ETERNITY;
+        } else {
+            if (startInstant.toEpochMilli() <= -ETERNITY) {
+                throw new IllegalArgumentException("Start instant too early");
+            } else {
+                return startInstant.toEpochMilli();
+            }
+        }
+    }
 
     @Override
     public String toString() {
@@ -191,16 +193,16 @@ public final class Interval {
      * @param date
      * @return true if the given instance is contained within this Date range.
      */
-    public boolean contains(Instant date , EndpointBehavior behavior) {
-        return contains(Objects.requireNonNull(date).toEpochMilli() , behavior);
+    public boolean contains(Instant date, EndpointBehavior behavior) {
+        return contains(Objects.requireNonNull(date).toEpochMilli(), behavior);
     }
-    
+
     public Interval withEnd(Instant instant) {
-    	return new Interval(start, getEndValue(instant));
+        return new Interval(start, getEndValue(instant));
     }
-    
+
     public Interval withStart(Instant instant) {
-    	return new Interval(getStartValue(instant), end);
+        return new Interval(getStartValue(instant), end);
     }
 
     public Interval intersection(Interval interval) {
@@ -230,7 +232,7 @@ public final class Interval {
 
     public boolean startsBefore(Instant testDate) {
         if (getStart() == null) {
-            return  testDate != null;
+            return testDate != null;
         }
         return testDate != null && getStart().isBefore(testDate);
     }
@@ -241,7 +243,6 @@ public final class Interval {
         }
         return testDate != null && getEnd().isAfter(testDate);
     }
-
 
 
     /**
@@ -259,8 +260,7 @@ public final class Interval {
      * Determines whether the two given {@link Interval}s abut. Two {@link Interval}s abut if the to value of one, equals the
      * from of the other.
      *
-     * @param second
-     *            the second
+     * @param second the second
      * @return true if both {@link Interval}s abut, false otherwise.
      */
     public boolean abuts(Interval second) {
@@ -286,8 +286,7 @@ public final class Interval {
      * Determines whether the first {@link Interval} abuts the second at the first's from. (i.e. the first starts at the point
      * where the second one ends).
      *
-     * @param second
-     *            the second
+     * @param second the second
      * @return true if the first {@link Interval} abuts the second at the first's from, false otherwise.
      */
     private boolean fromAbuts(Interval second) {
@@ -298,8 +297,7 @@ public final class Interval {
      * Determines whether the first {@link Interval} abuts the second at the first's to. (i.e. the first ends at the point where
      * the second one starts).
      *
-     * @param second
-     *            the second
+     * @param second the second
      * @return true if the first {@link Interval} abuts the second at the first's to, false otherwise.
      */
     private boolean toAbuts(Interval second) {
@@ -347,61 +345,77 @@ public final class Interval {
         }
         return new Interval(Ordering.natural().min(start, date.toEpochMilli()), Ordering.natural().max(end, date.toEpochMilli()));
     }
-    
+
     public long dbStart() {
-    	return start;
+        return start;
     }
-    
+
     public long dbEnd() {
-    	return end;
+        return end;
     }
 
     public enum EndpointBehavior {
-    	CLOSED_OPEN {
-    		boolean contains(Interval interval , long when) {
-    			return when >= interval.start && when < interval.end;
-    		}
+        CLOSED_OPEN {
+            boolean contains(Interval interval, long when) {
+                return when >= interval.start && when < interval.end;
+            }
 
             @Override
             public Range<Instant> toRange(Interval interval) {
                 return interval.toClosedOpenRange();
             }
         },
-    	OPEN_CLOSED {
-    		boolean contains(Interval interval , long when) {
-    			return when > interval.start && when <= interval.end;
-    		}
+        OPEN_CLOSED {
+            boolean contains(Interval interval, long when) {
+                return when > interval.start && when <= interval.end;
+            }
 
             @Override
             public Range<Instant> toRange(Interval interval) {
                 return interval.toOpenClosedRange();
             }
-		},
-    	CLOSED_CLOSED {
-    		boolean contains(Interval interval , long when) {
-    			return when >= interval.start && when <= interval.end;
-    		}
+        },
+        CLOSED_CLOSED {
+            boolean contains(Interval interval, long when) {
+                return when >= interval.start && when <= interval.end;
+            }
 
 
             @Override
             public Range<Instant> toRange(Interval interval) {
                 return interval.toClosedRange();
             }
-		},
-    	OPEN_OPEN {
-    		boolean contains(Interval interval , long when) {
-    			return when > interval.start && when < interval.end;
-    		}
+        },
+        OPEN_OPEN {
+            boolean contains(Interval interval, long when) {
+                return when > interval.start && when < interval.end;
+            }
 
             @Override
             public Range<Instant> toRange(Interval interval) {
                 return interval.toOpenRange();
             }
-		};
-    	
-    	abstract boolean contains(Interval interval , long when);
+        };
+
+        abstract boolean contains(Interval interval, long when);
 
         public abstract Range<Instant> toRange(Interval interval);
+
+        public static EndpointBehavior fromRange(Range range) {
+            if (BoundType.CLOSED.equals(range.lowerBoundType())) {
+                if (BoundType.CLOSED.equals(range.upperBoundType())) {
+                    return CLOSED_CLOSED;
+                } else {
+                    return CLOSED_OPEN;
+                }
+            } else {
+                if (BoundType.CLOSED.equals(range.upperBoundType())) {
+                    return OPEN_CLOSED;
+                } else {
+                    return OPEN_OPEN;
+                }
+            }
+        }
     }
- 
- }
+
+}
