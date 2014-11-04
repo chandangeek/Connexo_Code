fc26cf12778f58de6702d6eccd9ac278637500c7
Bruno Ballekens <bruno.ballekens@elster.com>
2014-11-04 15:13:30 +0100

    JP-6496: Add relative period name validation error

--- copl/com.elster.jupiter.time.rest/src/main/java/com/elster/jupiter/time/rest/TimeApplication.java
+++ copl/com.elster.jupiter.time.rest/src/main/java/com/elster/jupiter/time/rest/TimeApplication.java
@@ -12,7 +12,6 @@ import com.elster.jupiter.rest.util.LocalizedExceptionMapper;
 import com.elster.jupiter.rest.util.LocalizedFieldValidationExceptionMapper;
 import com.elster.jupiter.rest.util.RestQueryService;
 import com.elster.jupiter.time.TimeService;
-import com.elster.jupiter.time.rest.impl.ExceptionEventLogger;
 import com.elster.jupiter.time.rest.impl.RelativePeriodResource;
 import com.elster.jupiter.time.rest.impl.i18n.TranslationInstaller;
 import com.elster.jupiter.transaction.TransactionService;
@@ -45,7 +44,6 @@ public class TimeApplication extends Application implements InstallService {
     @Override
     public Set<Class<?>> getClasses() {
         return ImmutableSet.of(
-                ExceptionEventLogger.class,
                 ConstraintViolationExceptionMapper.class,
                 LocalizedFieldValidationExceptionMapper.class,
                 JsonMappingExceptionMapper.class,
