a93e926a98a7426445d83861ad575dda5e119878
dvy <dieter.vyncke@elster.com>
2014-11-06 15:49:16 +0100

    connection -> connection method

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicecommunicationtask/DeviceCommunicationTaskPreview.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicecommunicationtask/DeviceCommunicationTaskPreview.js
@@ -92,7 +92,7 @@ Ext.define('Mdc.view.setup.devicecommunicationtask.DeviceCommunicationTaskPrevie
                                         if(value!==''){
                                             if(!this.up('form').getRecord().data.connectionDefinedOnDevice){
                                                 this.inputAttrTpl = " data-qtip='This is my quick tip!' ";
-                                                return '<tpl data-qtip=\''+ Uni.I18n.translate('deviceCommunicationTask.connectionNotDefinedOnDevice', 'MDC', 'This connection is not defined on the device yet') + '\'><img src="../sky/build/resources/images/shared/bullet-red.png" class="ct-result ct-failure"><span style="position: relative; top: -3px; left: 4px">' + value + '</span></tpl>'
+                                                return '<tpl data-qtip=\''+ Uni.I18n.translate('deviceCommunicationTask.connectionNotDefinedOnDevice', 'MDC', 'This connection method is not defined on the device yet') + '\'><img src="../sky/build/resources/images/shared/bullet-red.png" class="ct-result ct-failure"><span style="position: relative; top: -3px; left: 4px">' + value + '</span></tpl>'
                                             } else {
                                                 return value;
                                             }
