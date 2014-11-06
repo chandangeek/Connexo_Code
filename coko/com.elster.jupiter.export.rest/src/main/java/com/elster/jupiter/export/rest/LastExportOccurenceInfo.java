802571608edb7682c6ad60a91e7ab4b0f2441a2a
borunova <borunova@mera.ru>
2014-11-06 21:47:01 +0300

    Add export task status translations and export task delete functionality

--- coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/LastExportOccurenceInfo.java
+++ coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/LastExportOccurenceInfo.java
@@ -2,6 +2,7 @@ package com.elster.jupiter.export.rest;
 
 import com.elster.jupiter.export.DataExportOccurrence;
 import com.elster.jupiter.export.DataExportStatus;
+import com.elster.jupiter.nls.Thesaurus;
 
 import java.time.Instant;
 
@@ -11,15 +12,15 @@ import java.time.Instant;
  * Time: 14:38
  */
 public class LastExportOccurenceInfo {
-    public DataExportStatus status;
+    public String status;
     public long lastRun;
     public Long startedOn;
     public Long finishedOn;
     public Long duration;
 
-    public LastExportOccurenceInfo(DataExportOccurrence dataExportOccurrence) {
+    public LastExportOccurenceInfo(DataExportOccurrence dataExportOccurrence, Thesaurus thesaurus) {
 
-        status = dataExportOccurrence.getStatus();
+        status = getName(dataExportOccurrence.getStatus(), thesaurus);
         lastRun = dataExportOccurrence.getTriggerTime().toEpochMilli();
         startedOn = toLong(dataExportOccurrence.getStartDate());
         finishedOn = dataExportOccurrence.getEndDate().map(this::toLong).orElse(null);
@@ -35,5 +36,9 @@ public class LastExportOccurenceInfo {
 
     private Long toLong(Instant instant) {
         return instant == null ? null : instant.toEpochMilli();
+    }
+
+    private static String getName(DataExportStatus status, Thesaurus thesaurus) {
+        return thesaurus.getStringBeyondComponent(status.toString(), status.toString());
     }
 }
