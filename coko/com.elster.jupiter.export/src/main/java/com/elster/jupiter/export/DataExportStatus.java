78de8736985d494301b5d06618cf9b2da93daa51
borunova <borunova@mera.ru>
2014-11-06 21:33:25 +0300

    Add export task status translations and export task delete functionality

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataExportStatus.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataExportStatus.java
@@ -1,5 +1,20 @@
 package com.elster.jupiter.export;
 
 public enum DataExportStatus {
-    BUSY, SUCCESS, WARNING, FAILED
+    BUSY("Busy"),
+    SUCCESS("Success"),
+    WARNING("Warning"),
+    FAILED("Failed"),
+    NOT_PERFORMED("Not performed yet");
+
+    private String name;
+
+    DataExportStatus(String name) {
+       this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
 }
--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/ReadingTypeDataExportTaskImpl.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/ReadingTypeDataExportTaskImpl.java
@@ -8,6 +8,7 @@ import com.elster.jupiter.export.ValidatedDataOption;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.metering.groups.EndDeviceGroup;
+import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.Table;
 import com.elster.jupiter.orm.associations.IsPresent;
@@ -184,6 +185,11 @@ class ReadingTypeDataExportTaskImpl implements IReadingTypeDataExportTask {
         } else {
             Save.UPDATE.save(dataModel, this);
         }
+    }
+
+    @Override
+    public void delete() {
+        dataModel.remove(this);
     }
 
     @Override
