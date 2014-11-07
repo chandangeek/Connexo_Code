c96fd2e55675a3991dee31b4aafff5808675565a
adrianlupan <adrianlupan@elster.com>
2014-11-07 13:34:23 +0200

    Added privileges: Validate manual, Fine tune validation

--- comu/com.energyict.mdc.app/src/main/web/js/mdc/app/controller/Main.js
+++ comu/com.energyict.mdc.app/src/main/web/js/mdc/app/controller/Main.js
@@ -11,7 +11,7 @@ Ext.define('MdcApp.controller.Main', {
         'privilege.view.creationRule','privilege.administrate.creationRule','privilege.view.assignmentRule','privilege.administrate.validationConfiguration',
         'privilege.view.validationConfiguration','privilege.administrate.schedule','privilege.view.schedule','privilege.administrate.communicationInfrastructure',
         'privilege.view.communicationInfrastructure','privilege.administrate.protocol','privilege.view.protocol','privilege.administrate.deviceConfiguration',
-        'privilege.view.deviceConfiguration','privilege.administrate.device','privilege.view.device','privilege.view.validateDevice','privilege.view.scheduleDevice',
+        'privilege.view.deviceConfiguration','privilege.administrate.device','privilege.view.device','privilege.view.validateManual','privilege.view.fineTuneValidationConfiguration','privilege.view.scheduleDevice',
         'privilege.import.inventoryManagement','privilege.revoke.inventoryManagement','privilege.create.inventoryManagement','privilege.administrate.deviceSecurity',
         'privilege.view.deviceSecurity'],
     controllers: [
