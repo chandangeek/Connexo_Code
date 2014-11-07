2193ec0ebb399e4d6bfbd0c86452b9e6cdb3e5ef
adrianlupan <adrianlupan@elster.com>
2014-11-07 13:27:33 +0200

    Added privileges: Validate manual, Fine tune validation

--- copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/impl/AppResource.java
+++ copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/impl/AppResource.java
@@ -27,7 +27,7 @@ public class AppResource {
                                         "privilege.view.creationRule","privilege.administrate.creationRule","privilege.view.assignmentRule","privilege.administrate.validationConfiguration",
                                         "privilege.view.validationConfiguration","privilege.administrate.schedule","privilege.view.schedule","privilege.administrate.communicationInfrastructure",
                                         "privilege.view.communicationInfrastructure","privilege.administrate.protocol","privilege.view.protocol","privilege.administrate.deviceConfiguration",
-                                        "privilege.view.deviceConfiguration","privilege.administrate.device","privilege.view.device","privilege.view.validateDevice","privilege.view.scheduleDevice",
+                                        "privilege.view.deviceConfiguration","privilege.administrate.device","privilege.view.device","privilege.view.validateManual","privilege.view.fineTuneValidationConfiguration","privilege.view.scheduleDevice",
                                         "privilege.import.inventoryManagement","privilege.revoke.inventoryManagement","privilege.create.inventoryManagement","privilege.administrate.deviceSecurity",
                                         "privilege.view.deviceSecurity"};
     private static final String[] BPM_CONSOLE_PRIVILEGES = {"privilege.view.bpm"};
