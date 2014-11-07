02810f2f47a52d6ab16acb8b10a7326e53a5e228
dvy <dieter.vyncke@elster.com>
2014-11-07 08:52:37 +0100

    JP-6431

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/ComServerComPortsEdit.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/ComServerComPortsEdit.js
@@ -256,6 +256,7 @@ Ext.define('Mdc.controller.setup.ComServerComPortsEdit', {
             }
             record.set('modemInitStrings', modemInit);
             ids && (record.set('outboundComPortPoolIds', ids));
+            record.set('modificationDate',null);
             record.save({
                 callback: function (records, operation, success) {
                     if (success) {
