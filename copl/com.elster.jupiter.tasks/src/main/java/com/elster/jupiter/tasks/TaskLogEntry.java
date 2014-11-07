92d104e1e4ad6ca7a7b6ab45a66511d13d389a7a
Bruno Ballekens <bruno.ballekens@elster.com>
2014-11-07 13:18:48 +0100

    JP-2073: Run data export task Api to get everything

--- copl/com.elster.jupiter.tasks/src/main/java/com/elster/jupiter/tasks/TaskLogEntry.java
+++ copl/com.elster.jupiter.tasks/src/main/java/com/elster/jupiter/tasks/TaskLogEntry.java
@@ -1,19 +1,12 @@
 package com.elster.jupiter.tasks;
 
-import java.time.Instant;
-import java.util.logging.Level;
+import com.elster.jupiter.util.logging.LogEntry;
 
 /**
  * Copyrights EnergyICT
  * Date: 5/11/2014
  * Time: 10:38
  */
-public interface TaskLogEntry {
+public interface TaskLogEntry extends LogEntry {
     TaskOccurrence getTaskOccurrence();
-
-    Instant getTimeStamp();
-
-    String getMessage();
-
-    Level getLevel();
 }
