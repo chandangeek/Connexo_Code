b84cc10530d6a17e5a2006a0691783f1664d06e3
Lobanov Dmitry <dlobanov@mera.ru>
2014-11-12 22:44:07 +0300

    BigDecimaAsStringAdapter should check null or empty string when unmarshall

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/BigDecimalAsStringAdapter.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/BigDecimalAsStringAdapter.java
@@ -2,12 +2,18 @@ package com.energyict.mdc.device.data.rest;
 
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import com.elster.jupiter.util.Checks;
+
 import java.math.BigDecimal;
 
 public class BigDecimalAsStringAdapter extends XmlAdapter<String, BigDecimal>{
 
     @Override
     public BigDecimal unmarshal(String v) throws Exception {
+        if (Checks.is(v).emptyOrOnlyWhiteSpace()) {
+            return null;
+        }
         return new BigDecimal(v);
     }
 
