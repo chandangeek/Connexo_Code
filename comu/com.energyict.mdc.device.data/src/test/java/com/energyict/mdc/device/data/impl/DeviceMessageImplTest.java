1b276c0d075d10699b7bfc7b80a3111634dbffa4
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2014-11-17 10:07:28 +0100

    JP-788: merge with JP-4438

--- comu/com.energyict.mdc.device.data/src/test/java/com/energyict/mdc/device/data/impl/DeviceMessageImplTest.java
+++ comu/com.energyict.mdc.device.data/src/test/java/com/energyict/mdc/device/data/impl/DeviceMessageImplTest.java
@@ -12,7 +12,7 @@ import com.energyict.mdc.device.config.security.Privileges;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.exceptions.MessageSeeds;
 import com.energyict.mdc.dynamic.PropertySpecService;
-import com.energyict.mdc.protocol.api.ComChannel;
+import com.energyict.mdc.io.ComChannel;
 import com.energyict.mdc.protocol.api.ConnectionType;
 import com.energyict.mdc.protocol.api.DeviceFunction;
 import com.energyict.mdc.protocol.api.DeviceProtocol;
