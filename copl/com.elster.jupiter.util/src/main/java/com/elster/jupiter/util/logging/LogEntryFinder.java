3fa0d15c9cfd6410b7c30a8d051e5787323fa69b
borunova <borunova@mera.ru>
2014-11-18 19:51:52 +0300

    JP-6644 review remarks

---
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/logging/LogEntryFinder.java
@@ -0,0 +1,16 @@
+package com.elster.jupiter.util.logging;
+
+import com.elster.jupiter.util.conditions.Condition;
+
+import java.util.List;
+
+public interface LogEntryFinder {
+
+    public LogEntryFinder with (Condition condition);
+
+    public LogEntryFinder setStart (Integer start);
+
+    public LogEntryFinder setLimit (Integer limit);
+
+    public List<? extends LogEntry> find();
+}
