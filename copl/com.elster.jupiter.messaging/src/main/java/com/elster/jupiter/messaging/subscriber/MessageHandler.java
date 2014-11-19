2c752ec1d5e7bdefdd639f322fb849de3fdfe997
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-11-19 13:07:44 +0100

    JP-6344 - Develop Run data export task

--- copl/com.elster.jupiter.messaging/src/main/java/com/elster/jupiter/messaging/subscriber/MessageHandler.java
+++ copl/com.elster.jupiter.messaging/src/main/java/com/elster/jupiter/messaging/subscriber/MessageHandler.java
@@ -5,4 +5,10 @@ import com.elster.jupiter.messaging.Message;
 public interface MessageHandler {
 
     void process(Message message);
+
+    /**
+     * the message handler may do additional processing here that falls outside the Transaction that dequeued the message
+     */
+    default void onMessageDelete() {
+    }
 }
