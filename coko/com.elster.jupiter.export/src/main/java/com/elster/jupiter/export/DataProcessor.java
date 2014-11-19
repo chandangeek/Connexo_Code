9f572a9ba6bdbf8bc242a5a759c0588c4411be70
borunova <borunova@mera.ru>
2014-11-19 13:30:39 +0300

    Remove unnecessary imports

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataProcessor.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataProcessor.java
@@ -1,10 +1,8 @@
 package com.elster.jupiter.export;
 
 import com.elster.jupiter.metering.readings.MeterReading;
-import com.elster.jupiter.properties.PropertySpec;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.logging.Logger;
 
 public interface DataProcessor {
