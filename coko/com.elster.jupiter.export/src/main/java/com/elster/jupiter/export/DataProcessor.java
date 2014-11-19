ffda9c8e5cdd29346c2e162be87ad9594ac627cb
borunova <borunova@mera.ru>
2014-11-19 13:24:40 +0300

    JP-6343 Export data to CSV

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataProcessor.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataProcessor.java
@@ -1,10 +1,23 @@
 package com.elster.jupiter.export;
 
+import com.elster.jupiter.metering.readings.MeterReading;
 import com.elster.jupiter.properties.PropertySpec;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.logging.Logger;
 
 public interface DataProcessor {
 
-    List<PropertySpec<?>> getPropertySpecs();
+    void startExport(DataExportOccurrence dataExportOccurrence, Logger logger);
+
+    void startItem(ReadingTypeDataExportItem item);
+
+    Instant processData(MeterReading data);
+
+    Instant processUpdatedData(MeterReading updatedData);
+
+    void endItem(ReadingTypeDataExportItem item);
+
+    void endExport();
 }
