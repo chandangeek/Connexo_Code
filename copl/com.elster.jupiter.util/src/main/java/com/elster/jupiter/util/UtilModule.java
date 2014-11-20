9b1a49304a22a9dfcee635f124a565946b05c48b
kha <karel.haeck@elster.com>
2014-11-20 09:55:24 +0100

    Added CronExpressionParser to UtilModule

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/UtilModule.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/UtilModule.java
@@ -4,6 +4,8 @@ import java.time.Clock;
 
 import com.elster.jupiter.util.beans.BeanService;
 import com.elster.jupiter.util.beans.impl.BeanServiceImpl;
+import com.elster.jupiter.util.cron.CronExpressionParser;
+import com.elster.jupiter.util.cron.impl.DefaultCronExpressionParser;
 import com.elster.jupiter.util.json.JsonService;
 import com.elster.jupiter.util.json.impl.JsonServiceImpl;
 import com.google.inject.AbstractModule;
@@ -25,6 +27,7 @@ public class UtilModule extends AbstractModule {
     protected void configure() {
         bind(JsonService.class).to(JsonServiceImpl.class).in(Scopes.SINGLETON);
         bind(BeanService.class).to(BeanServiceImpl.class).in(Scopes.SINGLETON);
-        bind(Clock.class).toInstance(clock);
+        bind(CronExpressionParser.class).to(DefaultCronExpressionParser.class).in(Scopes.SINGLETON);
+        bind(Clock.class).toInstance(clock);        
     }
 }
