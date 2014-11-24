12d9b9c657981a8127be7e020c077347b7370c11
Anton Suslennikov <antsusl@mera.ru>
2014-11-24 15:45:47 +0300

    JP-6920 JsonQueryFilter moving

---
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/JsonQueryFilter.java
@@ -0,0 +1,153 @@
+package com.elster.jupiter.rest.util;
+
+import com.elster.jupiter.nls.LocalizedFieldValidationException;
+import com.elster.jupiter.rest.util.impl.MessageSeeds;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import javax.ws.rs.QueryParam;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.io.ByteArrayInputStream;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+@XmlRootElement
+public class JsonQueryFilter {
+    private static final String PROPERTY = "property";
+    private static final String VALUE = "value";
+    private static final Function<JsonNode, String> AS_STRING = node -> node != null ? node.getTextValue() : null;
+    private static final Function<JsonNode, String> AS_JSON_STRING = node -> node != null && !node.isNull() ? node.toString() : null;
+    private static final Function<JsonNode, Integer> AS_INT = node -> {
+        if (node != null){
+            Number number = node.getNumberValue();
+            if (number != null){
+                return number.intValue();
+            }
+        }
+        return null;
+    };
+    private static final Function<JsonNode, Long> AS_LONG = node -> {
+        if (node != null){
+            Number number = node.getNumberValue();
+            if (number != null){
+                return number.longValue();
+            }
+        }
+        return null;
+    };
+    private static final Function<JsonNode, Boolean> AS_BOOLEAN = node -> node != null ? node.asBoolean() : null;
+    private static final Function<JsonNode, Instant> AS_INSTANT = node -> {
+        if (node != null){
+            Number number = node.getNumberValue();
+            if (number != null){
+                return Instant.ofEpochMilli(number.longValue());
+            }
+        }
+        return null;
+    };
+
+    private final Map<String, JsonNode> filterProperties = new HashMap<>();
+
+    public JsonQueryFilter(@QueryParam("filter") String source) {
+        try {
+            if (source != null) {
+                JsonNode node = new ObjectMapper().readValue(new ByteArrayInputStream(source.getBytes()), JsonNode.class);
+                if (node != null && node.isArray()) {
+                    for (JsonNode singleFilter : node) {
+                        filterProperties.put(singleFilter.get(PROPERTY).getTextValue(), singleFilter.get(VALUE));
+                    }
+                }
+            }
+        } catch (Exception ex){
+            throw new LocalizedFieldValidationException(MessageSeeds.INVALID_VALUE, "filter");
+        }
+    }
+
+    private <T> T unmarshalValue(String name, String value, XmlAdapter<String, T> adapter){
+        try {
+            return adapter.unmarshal(value);
+        } catch (Exception e) {
+            throw new LocalizedFieldValidationException(MessageSeeds.INVALID_VALUE, name);
+        }
+    }
+
+    private Map<String, JsonNode> getFilterProperties() {
+        return filterProperties;
+    }
+
+    public <T> T getProperty(String name, Function<JsonNode, T> mapper){
+        return mapper.apply(getFilterProperties().get(name));
+    }
+
+    public String getComplexProperty(String name){
+        return getProperty(name, AS_JSON_STRING);
+    }
+
+    public <T> T getProperty(String name, XmlAdapter<String, T> adapter) {
+        return unmarshalValue(name, getString(name), adapter);
+    }
+
+    public String getString(String name){
+        return getProperty(name, AS_STRING);
+    }
+
+    public Integer getInteger(String name){
+        return getProperty(name, AS_INT);
+    }
+
+    public Long getLong(String name) {
+        return getProperty(name, AS_LONG);
+    }
+
+    public Instant getInstant(String name) {
+        return getProperty(name, AS_INSTANT);
+    }
+
+    public Boolean getBoolean(String name) {
+        return getProperty(name, AS_BOOLEAN);
+    }
+
+    public <T> List<T> getPropertyList(String name, Function<JsonNode, T> mapper){
+        JsonNode node = getFilterProperties().get(name);
+        List<T> values = new ArrayList<>();
+        if (node != null && node.isArray()){
+            for (JsonNode value : node) {
+                values.add(mapper.apply(value));
+            }
+        }
+        return values;
+    }
+
+    public List<String> getPropertyList(String name){
+        return getPropertyList(name, AS_JSON_STRING);
+    }
+
+    public <T> List<T> getPropertyList(String name, XmlAdapter<String, T> adapter) {
+        return getPropertyList(name, AS_STRING.andThen(s -> unmarshalValue(name, s, adapter)));
+    }
+
+    public List<String> getStringList(String name){
+        return getPropertyList(name, AS_STRING);
+    }
+
+    public List<Integer> getIntegerList(String name){
+        return getPropertyList(name, AS_INT);
+    }
+
+    public List<Long> getLongList(String name){
+        return getPropertyList(name, AS_LONG);
+    }
+
+    public List<Instant> getInstantList(String name){
+        return getPropertyList(name, AS_INSTANT);
+    }
+
+    public List<Boolean> getBooleanList(String name){
+        return getPropertyList(name, AS_BOOLEAN);
+    }
+}
