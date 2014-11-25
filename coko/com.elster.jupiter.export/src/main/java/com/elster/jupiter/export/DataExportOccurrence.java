bfb789d7f404ae76d936d8102ee22eb1030ca5fe
Bruno Ballekens <bruno.ballekens@elster.com>
2014-11-25 12:01:43 +0100

    JP-6814 - Adaptations for TaskOccurence

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataExportOccurrence.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/DataExportOccurrence.java
@@ -12,7 +12,7 @@ public interface DataExportOccurrence {
 
     ReadingTypeDataExportTask getTask();
 
-    Instant getStartDate();
+    Optional<Instant> getStartDate();
 
     Optional<Instant> getEndDate();
 
