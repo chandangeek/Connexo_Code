8ae9e562e15fca9907df59baa0027245f4a874c9
kha <karel.haeck@elster.com>
2014-11-27 18:01:37 +0100

    jersey upgrade

--- coko/com.elster.jupiter.parties.rest/src/main/java/com/elster/jupiter/parties/rest/impl/PartyRepresentationInfo.java
+++ coko/com.elster.jupiter.parties.rest/src/main/java/com/elster/jupiter/parties/rest/impl/PartyRepresentationInfo.java
@@ -1,20 +1,16 @@
 package com.elster.jupiter.parties.rest.impl;
 
 import com.elster.jupiter.parties.PartyRepresentation;
-import com.elster.jupiter.rest.util.JsonInstantAdapter;
 import com.elster.jupiter.users.rest.UserInfo;
 
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import java.time.Instant;
 
 @XmlRootElement
 public class PartyRepresentationInfo {
     public long partyId;
-    @XmlJavaTypeAdapter(JsonInstantAdapter.class)
     public Instant start;
-    @XmlJavaTypeAdapter(JsonInstantAdapter.class)
     public Instant end;
     public UserInfo userInfo;
 
