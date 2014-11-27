4095fa086725f44207cdd17b26178615921c618b
borunova <borunova@mera.ru>
2014-11-27 15:59:25 +0300

    Revert "JP-6993 Hide updated data file properties"
    This reverts commit 6f7e6b5a4e6712b2b4d99dcad4185c7fb875c1be.

--- coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/tasks/PropertyForm.js
+++ coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/tasks/PropertyForm.js
@@ -56,46 +56,44 @@ Ext.define('Dxp.view.tasks.PropertyForm', {
                     xtype: 'fieldcontainer',
                     itemId: 'group-fieldcontainer'
                 };
-            if (groupName !== "fileFormat.updatedData") {
-                if (namesArray.length > 1) {
-                    fieldContainer.fieldLabel = Uni.I18n.translate(namesArray[1], 'DES', namesArray[1]);
-                    fieldContainer.items = groupItems;
-                    Ext.Array.each(groupItems, function (groupItem) {
-                        groupItem.setWidth(600);
-                        groupItem.labelAlign = 'left';
-                    })
+            if (namesArray.length > 1) {
+                fieldContainer.fieldLabel = Uni.I18n.translate(namesArray[1], 'DES', namesArray[1]);
+                fieldContainer.items = groupItems;
+                Ext.Array.each(groupItems, function (groupItem) {
+                    groupItem.setWidth(600);
+                    groupItem.labelAlign = 'left';
+                })
+            } else {
+                if (!me.addEditPage) {
+                    me.add({
+                        xtype: 'displayfield',
+                        renderer: function () {
+                            return '<b>' + Uni.I18n.translate(namesArray[0], 'DES', namesArray[0]) + '</b>'
+                        }
+                    });
                 } else {
-                    if (!me.addEditPage) {
-                        me.add({
-                            xtype: 'displayfield',
-                            renderer: function () {
-                                return '<b>' + Uni.I18n.translate(namesArray[0], 'DES', namesArray[0]) + '</b>'
-                            }
-                        });
-                    } else {
-                        me.add({
-                            title: Uni.I18n.translate(namesArray[0], 'DES', namesArray[0]),
-                            ui: 'medium'
-                        });
-                    }
+                    me.add({
+                        title: Uni.I18n.translate(namesArray[0], 'DES', namesArray[0]),
+                        ui: 'medium'
+                    });
                 }
-                me.add(fieldContainer.items ? fieldContainer : groupItems);
             }
+            me.add(fieldContainer.items ? fieldContainer : groupItems);
         });
         this.initialised = true;
     },
 
-    updateRecord: function () {
+    updateRecord: function() {
         var me = this,
             raw = me.getFieldValues(),
             values = {},
             key,
             field;
-        _.each(raw.properties || [], function (firstValue, firstKey) {
+        _.each(raw.properties || [], function(firstValue, firstKey){
             if (typeof firstValue == 'object') {
-                _.each(firstValue, function (secondValue, secondKey) {
+                _.each(firstValue, function(secondValue, secondKey){
                     if (typeof secondValue == 'object') {
-                        _.each(secondValue, function (thirdValue, thirdKey) {
+                        _.each(secondValue, function(thirdValue, thirdKey){
                             key = firstKey + '.' + secondKey + '.' + thirdKey;
                             field = me.getPropertyField(key);
                             values[key] = field.getValue(thirdValue);
@@ -114,7 +112,7 @@ Ext.define('Dxp.view.tasks.PropertyForm', {
         this.getForm().hydrator.hydrate(values, me.getRecord());
     },
 
-    getPropertyField: function (key) {
+    getPropertyField: function(key) {
         var me = this;
         if (key.split('.').length === 3) {
             return me.down('#group-fieldcontainer').getComponent(key);
