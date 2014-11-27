876f46a63c8e8e865fd37d7a657e23ea88cf977e
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-11-27 15:09:59 +0100

    JP-6732 - Data export task: trigger property is not displayed

--- copl/com.elster.jupiter.time.rest/src/main/java/com/elster/jupiter/time/rest/impl/i18n/TranslationInstaller.java
+++ copl/com.elster.jupiter.time.rest/src/main/java/com/elster/jupiter/time/rest/impl/i18n/TranslationInstaller.java
@@ -4,6 +4,7 @@ import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.SimpleNlsKey;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.nls.Translation;
+import com.elster.jupiter.time.rest.impl.TimeApplication;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +28,7 @@ public class TranslationInstaller {
             List<Translation> translations = new ArrayList<>(MessageSeeds.values().length);
             for (MessageSeeds messageSeed : MessageSeeds.values()) {
                 SimpleNlsKey nlsKey = SimpleNlsKey
-                        .key(thesaurus.getComponent(), Layer.REST, messageSeed.getKey())
+                        .key(TimeApplication.COMPONENT_NAME, Layer.REST, messageSeed.getKey())
                         .defaultMessage(messageSeed.getDefaultFormat());
                 translations.add(toTranslation(nlsKey, Locale.ENGLISH, messageSeed.getDefaultFormat()));
             }
