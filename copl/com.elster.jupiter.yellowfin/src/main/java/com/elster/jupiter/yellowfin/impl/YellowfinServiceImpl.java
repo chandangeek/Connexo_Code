61d41b2e6b7fbcf9494c72c6ae753499ff6e37c9
kha <karel.haeck@elster.com>
2014-11-28 17:36:35 +0100

    Revert "clean out"
    This reverts commit 2abb2f24d48504a04f2b7754b6094c66a9ffe3b6.

--- copl/com.elster.jupiter.yellowfin/src/main/java/com/elster/jupiter/yellowfin/impl/YellowfinServiceImpl.java
+++ copl/com.elster.jupiter.yellowfin/src/main/java/com/elster/jupiter/yellowfin/impl/YellowfinServiceImpl.java
@@ -29,19 +29,128 @@ public class YellowfinServiceImpl implements YellowfinService {
     private String yellowfinWebServiceUser;
     private String yellowfinWebServicePassword;
 
-    
+    @Activate
+    public void activate(BundleContext context) {
+        String url = context.getProperty(YELLOWFIN_URL);
+        if (url == null || !url.startsWith("http://")) {
+            url = DEFAULT_YELLOWFIN_URL;
+        }
+
+        yellowfinWebServiceUser = context.getProperty(YELLOWFIN_WEBSERVICES_USER);
+        yellowfinWebServicePassword = context.getProperty(YELLOWFIN_WEBSERVICES_PASSWORD);
+
+        yellowfinUrl = url;
+        String parts[] = url.substring("http://".length()).split(":");
+        yellowfinHost = parts[0];
+        yellowfinPort = Integer.parseInt(parts[1]);
+
+    }
+
     @Override
     public String getYellowfinUrl(){
         return yellowfinUrl;
     }
 
     @Override
-    public boolean importContent(String filePath) {;
-    	return false;
+    public boolean importContent(String filePath) {
+
+        FileInputStream inputStream = null;
+        try {
+            File file = new File(filePath);
+            inputStream = new FileInputStream(file);
+            byte[] data = new byte[(int)file.length()];
+            inputStream.read(data);
+
+            AdministrationServiceResponse rs = null;
+            AdministrationServiceRequest rsr = new AdministrationServiceRequest();
+            AdministrationServiceService ts = new AdministrationServiceServiceLocator(yellowfinHost, yellowfinPort, "/services/AdministrationService", false);
+            AdministrationServiceSoapBindingStub rssbs = null;
+            try {
+                rssbs = (AdministrationServiceSoapBindingStub) ts.getAdministrationService();
+            } catch (ServiceException e) {
+                e.printStackTrace();
+            }
+
+            rsr.setLoginId(yellowfinWebServiceUser);
+            rsr.setPassword(yellowfinWebServicePassword);
+            //rsr.setPassword(null);
+            rsr.setOrgId(new Integer(1));
+            rsr.setFunction("IMPORTCONTENT");
+            rsr.setParameters( new String[] { Base64.encodeBytes(data) } );
+
+            if (rssbs != null) {
+                try {
+                    rs = rssbs.remoteAdministrationCall(rsr);
+                    if (rs != null){
+                        if("SUCCESS".equals(rs.getStatusCode()) ) {
+                            return true;
+                        }
+                    }
+
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+            catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+        return false;
     }
 
     @Override
     public String login(String username)  {
+
+        AdministrationServiceResponse rs = null;
+        AdministrationServiceRequest rsr = new AdministrationServiceRequest();
+        AdministrationServiceService ts = new AdministrationServiceServiceLocator(yellowfinHost, yellowfinPort, "/services/AdministrationService", false);
+        AdministrationServiceSoapBindingStub rssbs = null;
+        try {
+            rssbs = (AdministrationServiceSoapBindingStub) ts.getAdministrationService();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        rsr.setLoginId(yellowfinWebServiceUser);
+        rsr.setPassword(yellowfinWebServicePassword);
+        //rsr.setPassword(null);
+        rsr.setOrgId(new Integer(1));
+        rsr.setFunction("LOGINUSERNOPASSWORD");
+        AdministrationPerson ap = new AdministrationPerson();
+        ap.setUserId(username);
+        //ap.setPassword("admin");
+        rsr.setPerson(ap);
+
+        if (rssbs != null) {
+            try {
+                rs = rssbs.remoteAdministrationCall(rsr);
+                if (rs != null){
+                    if("SUCCESS".equals(rs.getStatusCode()) ) {
+                        return rs.getLoginSessionId();
+                    }
+                    if(rs.getErrorCode() == 9) { // license breach
+                        return "LICENSE_BREACH";
+                    }
+                }
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
         return null;
     }
 
@@ -49,7 +158,78 @@ public class YellowfinServiceImpl implements YellowfinService {
 
     @Override
     public boolean logout(String username)  {
+        AdministrationServiceResponse rs = null;
+        AdministrationServiceRequest rsr = new AdministrationServiceRequest();
+        AdministrationServiceService ts = new AdministrationServiceServiceLocator(yellowfinHost, yellowfinPort, "/services/AdministrationService", false);
+        AdministrationServiceSoapBindingStub rssbs = null;
+        try {
+            rssbs = (AdministrationServiceSoapBindingStub) ts.getAdministrationService();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        rsr.setLoginId(yellowfinWebServiceUser);
+        rsr.setPassword(yellowfinWebServicePassword);
+        rsr.setOrgId(new Integer(1));
+        rsr.setFunction("LOGOUTUSER");
+        AdministrationPerson ap = new AdministrationPerson();
+        ap.setUserId(username);
+        //ap.setPassword("admin");
+        rsr.setPerson(ap);
+
+// This is the Session ID
+        //rsr.setLoginSessionId(sessionId);
+        if (rssbs != null) {
+            try {
+                rs = rssbs.remoteAdministrationCall(rsr);
+                if (rs != null && "SUCCESS".equals(rs.getStatusCode()) ) {
+                    return true;
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
         return false;
+    }
+
+    public String reloadLicense(String username)  {
+
+        AdministrationServiceResponse rs = null;
+        AdministrationServiceRequest rsr = new AdministrationServiceRequest();
+        AdministrationServiceService ts = new AdministrationServiceServiceLocator(yellowfinHost, yellowfinPort, "/services/AdministrationService", false);
+        AdministrationServiceSoapBindingStub rssbs = null;
+        try {
+            rssbs = (AdministrationServiceSoapBindingStub) ts.getAdministrationService();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        rsr.setLoginId(yellowfinWebServiceUser);
+        rsr.setPassword(yellowfinWebServicePassword);
+        //rsr.setPassword(null);
+        rsr.setOrgId(new Integer(1));
+        rsr.setFunction("RELOADLICENCE");
+
+
+        if (rssbs != null) {
+            try {
+                rs = rssbs.remoteAdministrationCall(rsr);
+                if (rs != null){
+                    if("SUCCESS".equals(rs.getStatusCode()) ) {
+                        return rs.getSessionId();
+                    }
+                    if(rs.getErrorCode() == 9) { // license breach
+                        return "LICENSE_BREACH";
+                    }
+                }
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 
 }
