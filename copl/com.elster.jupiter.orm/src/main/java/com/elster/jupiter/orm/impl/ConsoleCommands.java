dc1ca4abdf3a2f803f5f68fcf2fbf0202c344339
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-12-01 08:44:02 +0100

    Adding some console commands

--- copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/impl/ConsoleCommands.java
+++ copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/impl/ConsoleCommands.java
@@ -9,14 +9,23 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
+import javax.sql.DataSource;
+import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.Collection;
 
-@Component(name = "com.elster.jupiter.orm.console", service = ConsoleCommands.class, property = {"osgi.command.scope=orm", "osgi.command.function=ddl"}, immediate = true)
+@Component(name = "com.elster.jupiter.orm.console", service = ConsoleCommands.class, property = {"osgi.command.scope=orm", "osgi.command.function=ddl", "osgi.command.function=executeQuery"}, immediate = true)
 public class ConsoleCommands {
 
     private volatile BundleContext context;
+    private volatile DataSource dataSource;
 
     @Activate
     public void activate(BundleContext context) {
@@ -26,6 +35,11 @@ public class ConsoleCommands {
     @Deactivate
     public void deactivate() {
 
+    }
+
+    @Reference
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void ddl(String... componentName) {
@@ -72,5 +86,77 @@ public class ConsoleCommands {
                 System.out.println(s);
             }
         }
+    }
+
+    public void executeQuery(String query) {
+        try (Connection connection = dataSource.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement(query)) {
+                ResultSet resultSet = statement.executeQuery();
+                new ResultSetPrinter(System.out).print(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ResultSetPrinter {
+
+        private final PrintStream out;
+
+        public ResultSetPrinter() {
+            super();
+            out = System.out;
+        }
+
+        public ResultSetPrinter(PrintStream out) {
+            super();
+            this.out = out;
+        }
+
+        public void print (ResultSet resultSet) {
+            try {
+                this.print(resultSet, resultSet.getMetaData());
+            }
+            catch (SQLException e) {
+                e.printStackTrace(System.err);
+            }
+        }
+
+        private void print (ResultSet resultSet, ResultSetMetaData metaData) throws SQLException {
+            this.printHeader(metaData);
+            this.printRows(resultSet, metaData);
+        }
+
+        private void printHeader (ResultSetMetaData metaData) throws SQLException {
+            int numberOfColumns = metaData.getColumnCount();
+            for (int i = 0; i < numberOfColumns; i++) {
+                out.print(metaData.getColumnLabel(i + 1));
+                if (i < numberOfColumns - 1) {
+                    out.print("\t");
+                }
+            }
+            out.println();
+        }
+
+        private void printRows (ResultSet resultSet, ResultSetMetaData metaData) throws SQLException {
+            int numberOfRows = 0;
+            while (resultSet.next()) {
+                this.printRow(resultSet, metaData);
+                numberOfRows++;
+                out.println();
+            }
+            out.println("numberOfRows = " + numberOfRows);
+        }
+
+        private void printRow (ResultSet resultSet, ResultSetMetaData metaData) throws SQLException {
+            int numberOfColumns = metaData.getColumnCount();
+            for (int i = 0; i < numberOfColumns; i++) {
+                out.print(resultSet.getObject(i + 1));
+                if (i < numberOfColumns - 1) {
+                    out.print("\t");
+                }
+            }
+        }
+
     }
 }
