195e83fd2f67294373bc6eeaba36db417756b8d6
Anton Suslennikov <antsusl@mera.ru>
2014-12-02 15:17:42 +0300

    JP-7112 View events on device

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/LogBookDataInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/LogBookDataInfo.java
@@ -1,11 +1,11 @@
 package com.energyict.mdc.device.data.rest.impl;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import com.elster.jupiter.metering.events.EndDeviceEventRecord;
 import com.elster.jupiter.nls.Thesaurus;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogBookDataInfo {
 
@@ -15,23 +15,23 @@ public class LogBookDataInfo {
     public int eventLogId;
     public Date readingDate;
     public String message;
+    public long logBookId;
 
     public static List<LogBookDataInfo> from(List<EndDeviceEventRecord> endDeviceEventRecords, Thesaurus thesaurus) {
-        List<LogBookDataInfo> infos = new ArrayList<>(endDeviceEventRecords.size());
-        for(EndDeviceEventRecord record : endDeviceEventRecords) {
-            LogBookDataInfo info = new LogBookDataInfo();
-
-            info.eventDate = Date.from(record.getCreatedDateTime());
-            info.eventType = EndDeviceEventTypeInfo.from(record.getEventType(), thesaurus);
-            info.deviceCode = record.getDeviceEventType();
-            info.eventLogId = record.getLogBookPosition();
-            info.readingDate = Date.from(record.getModTime() != null ? record.getModTime() : record.getCreateTime());
-            info.message = record.getDescription();
-
-            infos.add(info);
-        }
-
-        return infos;
+        return endDeviceEventRecords.stream().map(r -> from(r, thesaurus)).collect(Collectors.toList());
     }
 
+    public static LogBookDataInfo from(EndDeviceEventRecord record, Thesaurus thesaurus){
+        LogBookDataInfo info = new LogBookDataInfo();
+
+        info.logBookId = record.getLogBookId();
+        info.eventDate = Date.from(record.getCreatedDateTime());
+        info.eventType = EndDeviceEventTypeInfo.from(record.getEventType(), thesaurus);
+        info.deviceCode = record.getDeviceEventType();
+        info.eventLogId = record.getLogBookPosition();
+        info.readingDate = Date.from(record.getModTime() != null ? record.getModTime() : record.getCreateTime());
+        info.message = record.getDescription();
+
+        return info;
+    }
 }
--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/LogBookResource.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/LogBookResource.java
@@ -4,6 +4,8 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import javax.inject.Inject;
 import javax.ws.rs.BeanParam;
@@ -68,15 +70,27 @@ public class LogBookResource {
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     @Path("{lbid}/data")
-    public Response getLogBookData(@PathParam("mRID") String mrid, @PathParam("lbid") long logBookId, @BeanParam JsonQueryFilter jsonQueryFilter, @BeanParam QueryParameters queryParameters)
-    {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLogBookDataForSpecificLogbook(@PathParam("mRID") String mrid, @PathParam("lbid") long logBookId, @BeanParam JsonQueryFilter jsonQueryFilter, @BeanParam QueryParameters queryParameters){
+        return this.getLogBookData(mrid, (d, f) -> {
+            LogBook logBook = findLogBookOrThrowException(d, logBookId);
+            return logBook.getEndDeviceEventsByFilter(f);
+        }, jsonQueryFilter, queryParameters);
+    }
+
+    @GET
+    @Path("/data")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLogBookDataForDevice(@PathParam("mRID") String mrid, @BeanParam JsonQueryFilter jsonQueryFilter, @BeanParam QueryParameters queryParameters){
+        return this.getLogBookData(mrid, (d, f) -> d.getDeviceEventsByFilter(f), jsonQueryFilter, queryParameters);
+    }
+
+    private Response getLogBookData(String mrid, BiFunction<Device, EndDeviceEventRecordFilterSpecification, List<EndDeviceEventRecord>> eventProvider, JsonQueryFilter jsonQueryFilter, QueryParameters queryParameters){
         Device device = resourceHelper.findDeviceByMrIdOrThrowException(mrid);
-        LogBook logBook = findLogBookOrThrowException(device, logBookId);
         try {
             EndDeviceEventRecordFilterSpecification filter = buildFilterFromJsonQuery(jsonQueryFilter);
-            List<EndDeviceEventRecord> endDeviceEvents = logBook.getEndDeviceEventsByFilter(filter);
+            List<EndDeviceEventRecord> endDeviceEvents = eventProvider.apply(device, filter);
             List<EndDeviceEventRecord> pagedEndDeviceEvents = ListPager.of(endDeviceEvents).from(queryParameters).find();
             return Response.ok(PagedInfoList.asJson("data", LogBookDataInfo.from(pagedEndDeviceEvents, thesaurus), queryParameters)).build();
         } catch (IllegalArgumentException | IllegalEnumValueException e) {
