dd7d9a45872ded6595744aad4f5f302433690406
Artem Puchenkin <apuchenkin@gmail.com>
2014-12-02 20:32:04 +0300

    [JP-6849]  Develop Flag device on meter landing page

---
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/model/DeviceLabel.js
@@ -0,0 +1,26 @@
+Ext.define('Mdc.model.DeviceLabel', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {
+            name: 'id', mapping: function (data) {return data.category.id;}, persist: false
+        },
+        {name: 'category', type: 'auto'},
+        {name: 'comment', type: 'string', useNull: true},
+        {name: 'creationDate', dateFormat: 'time', type: 'date'}
+    ],
+
+    proxy: {
+        type: 'rest',
+        urlTpl: '/api/ddr/devices/{mRID}/devicelabels',
+        reader: {
+            type: 'json',
+            root: 'deviceLabels',
+            totalProperty: 'total'
+        },
+
+        setUrl: function (mRID) {
+            this.url = this.urlTpl.replace('{mRID}', mRID);
+        }
+    }
+});
+
