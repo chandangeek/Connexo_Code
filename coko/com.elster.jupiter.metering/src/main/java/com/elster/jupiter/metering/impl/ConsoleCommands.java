0cc039269749a17aef05696014c46abe1b7c29cf
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-12-03 17:03:33 +0100

    JP-7151 - Develop Edit device group

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/ConsoleCommands.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/ConsoleCommands.java
@@ -1,18 +1,25 @@
 package com.elster.jupiter.metering.impl;
 
 import com.elster.jupiter.cbo.IdentifiedObject;
+import com.elster.jupiter.metering.AmrSystem;
+import com.elster.jupiter.metering.Meter;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.Table;
+import com.elster.jupiter.security.thread.ThreadPrincipalService;
+import com.elster.jupiter.transaction.TransactionContext;
+import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.util.conditions.Condition;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-@Component(name = "com.elster.jupiter.metering.console", service = ConsoleCommands.class, property = {"osgi.command.scope=metering", "osgi.command.function=printDdl", "osgi.command.function=meters", "osgi.command.function=readingTypes"}, immediate = true)
+@Component(name = "com.elster.jupiter.metering.console", service = ConsoleCommands.class, property = {"osgi.command.scope=metering", "osgi.command.function=printDdl", "osgi.command.function=meters", "osgi.command.function=readingTypes", "osgi.command.function=createMeter"}, immediate = true)
 public class ConsoleCommands {
 
     private volatile MeteringService meteringService;
     private volatile DataModel dataModel;
+    private volatile TransactionService transactionService;
+    private volatile ThreadPrincipalService threadPrincipalService;
 
     public void printDdl() {
         try {
@@ -32,6 +39,18 @@ public class ConsoleCommands {
                 .forEach(System.out::println);
     }
 
+    public void createMeter(long amrSystemId, String amrid, String mrId) {
+        threadPrincipalService.set(() -> "Console");
+        try (TransactionContext context = transactionService.getContext()) {
+            AmrSystem amrSystem = meteringService.findAmrSystem(amrSystemId).orElseThrow(() -> new IllegalArgumentException("amr System not found"));
+            Meter meter = amrSystem.newMeter(amrid, mrId);
+            meter.save();
+            context.commit();
+        } finally {
+            threadPrincipalService.clear();
+        }
+    }
+
     public void readingTypes() {
         meteringService.getAvailableReadingTypes().stream()
                 .map(IdentifiedObject::getMRID)
@@ -44,4 +63,13 @@ public class ConsoleCommands {
         this.dataModel = ((MeteringServiceImpl) meteringService).getDataModel();
     }
 
+    @Reference
+    public void setTransactionService(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @Reference
+    public void setThreadPrincipalService(ThreadPrincipalService threadPrincipalService) {
+        this.threadPrincipalService = threadPrincipalService;
+    }
 }
