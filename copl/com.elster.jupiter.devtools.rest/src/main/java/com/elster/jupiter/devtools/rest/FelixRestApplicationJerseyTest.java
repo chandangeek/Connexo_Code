204396faa69e28fbcb69752f600f80fe0978c32f
Bruno Ballekens <bruno.ballekens@elster.com>
2014-12-03 13:08:46 +0100

    JP-6864 - Use rest.util JsonQueryfilter instead of mdc.common JsonQueryfilter everywhere JP-7162 - Add channel configuration: unit of measure rest call fails

--- copl/com.elster.jupiter.devtools.rest/src/main/java/com/elster/jupiter/devtools/rest/FelixRestApplicationJerseyTest.java
+++ copl/com.elster.jupiter.devtools.rest/src/main/java/com/elster/jupiter/devtools/rest/FelixRestApplicationJerseyTest.java
@@ -5,7 +5,7 @@ import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.transaction.TransactionContext;
 import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.util.exception.MessageSeed;
-import javax.ws.rs.core.Application;
+import com.google.common.reflect.ClassPath;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
@@ -13,9 +13,22 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
+import org.junit.Assert;
+import org.junit.Test;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
@@ -24,48 +37,41 @@ import static org.mockito.Mockito.when;
 /**
  * This can serve as base test class for all our REST applications.
  * It makes setup of the test framework very easy
- *
+ * <p>
  * ONLY FOR TESTING PURPOSES -- NO PRODUCTION USAGE ALLOWED
  *
- *
-
-
-
-                              @@@@@@@@@;
-                            @@@@@@@@@@@@@:
-                           @@@@@@@@@@@@@@@#
-                          @@@@@@@@@@@@@@@@@
-                          @@@@@@@@@@@@@@@@@#
-                         .@@@@@@@@@@@@@@@@@@
-                         :@@@@@@@@@@@@@@@@@@
-                         `@@@@@@@@@@@@@@@@@@
-                          @@@   '@@@@   @@@,
-                          @@'    @@@     @@
-                          @@+    @@@     @@
-                           @@   @@@@@   @@
-                           @@@@@@@ @@@@@@@
-                           :@@@@@   @@@@@@
-                            @@@@@`, @@@@@
-                               @@@@@@@`
-                             .@ @@@@@#'+
-                              @+      @
-                              @@@#''@@@
-                                @@@@@;
-                                 @@@`        #
-                       @@#                  @@@
-                      ,@@@@                @@@@
-                      @@@@@@@#          @@@@@@@`
-                      @@@;@@@@@@,    #@@@@@:@@@@
-                             @@@@@@@@@@@
-                               ,@@@@@;
-                               @@@@@@@;
-                             @@@@.  @@@@`
-                           @@@@'      @@@@
-                      ,@@@@@@@         +@@@@@@
-                      @@@@@@+            @@@@@@@
-                      @@@@@@             @@@@@@`
-                        +@@;              @@@
-                          ;                @`
+ * @@@@@@@@@;
+ * @@@@@@@@@@@@@:
+ * @@@@@@@@@@@@@@@#
+ * @@@@@@@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@@# .@@@@@@@@@@@@@@@@@@
+ * :@@@@@@@@@@@@@@@@@@
+ * `@@@@@@@@@@@@@@@@@@
+ * @@@ '@@@@   @@@,
+ * @@' @@@     @@
+ * @@+ @@@     @@
+ * @@ @@@@@   @@
+ * @@@@@@@ @@@@@@@
+ * :@@@@@   @@@@@@
+ * @@@@@`, @@@@@
+ * @@@@@@@` .@ @@@@@#'+
+ * @+ @
+ * @@@#''@@@
+ * @@@@@;
+ * @@@` #
+ * @@# @@@
+ * ,@@@@                @@@@
+ * @@@@@@@# @@@@@@@`
+ * @@@;@@@@@@, #@@@@@:@@@@
+ * @@@@@@@@@@@ ,@@@@@;
+ * @@@@@@@;
+ * @@@@. @@@@`
+ * @@@@' @@@@
+ * ,@@@@@@@         +@@@@@@
+ * @@@@@@+ @@@@@@@
+ * @@@@@@ @@@@@@`
+ * +@@;              @@@
+ * ;                @`
  */
 public abstract class FelixRestApplicationJerseyTest extends JerseyTest {
 
@@ -78,7 +84,7 @@ public abstract class FelixRestApplicationJerseyTest extends JerseyTest {
     @Mock
     public TransactionContext transactionContext;
 
-    public void setupMocks () {
+    public void setupMocks() {
         when(nlsService.getThesaurus(anyString(), anyObject())).thenReturn(thesaurus);
         when(thesaurus.getString(anyString(), anyString())).thenAnswer(invocationOnMock -> {
             for (MessageSeed messageSeeds : getMessageSeeds()) {
@@ -88,7 +94,7 @@ public abstract class FelixRestApplicationJerseyTest extends JerseyTest {
             }
             return (String) invocationOnMock.getArguments()[1];
         });
-        when(thesaurus.getFormat(Matchers.<MessageSeed>anyObject())).thenAnswer(invocation->new SimpleNlsMessageFormat((MessageSeed)invocation.getArguments()[0]));
+        when(thesaurus.getFormat(Matchers.<MessageSeed>anyObject())).thenAnswer(invocation -> new SimpleNlsMessageFormat((MessageSeed) invocation.getArguments()[0]));
         when(transactionService.getContext()).thenReturn(transactionContext);
     }
 
@@ -113,6 +119,7 @@ public abstract class FelixRestApplicationJerseyTest extends JerseyTest {
 
     /**
      * Set up your bundle's application, set all required services (usually with @Reference annotation on the setter) and return
+     *
      * @return
      */
     protected abstract Application getApplication();
@@ -124,5 +131,39 @@ public abstract class FelixRestApplicationJerseyTest extends JerseyTest {
         config.register(MultiPartFeature.class);
         super.configureClient(config);
     }
+
+    @Test
+    public void checkRestMethods() throws IOException {
+        ClassPath classPath = ClassPath.from(this.getClass().getClassLoader());
+        String packageName = this.getClass().getPackage().getName();
+        if (packageName.endsWith(".impl")) {
+            packageName = packageName.substring(0, packageName.lastIndexOf(".impl"));
+        }
+        StringBuilder builder = new StringBuilder();
+        for (ClassPath.ClassInfo classInfo : classPath.getTopLevelClassesRecursive(packageName)) {
+            Class<?> loadedClass = classInfo.load();
+            for (Method method : loadedClass.getMethods()) {
+                if (isRestMethod(method)) {
+                    if (!method.isAnnotationPresent(Produces.class)) {
+                        builder.append("REST method \'").append(method.getName()).append("\' of class \'").append(classInfo.getName()).append("\' does not have the @Produce annotation");
+                        builder.append("\n");
+                    }
+                }
+            }
+        }
+        if (builder.length() > 0) {
+            Assert.fail(builder.toString());
+        }
+    }
+
+    private boolean isRestMethod(Method method) {
+        return method.isAnnotationPresent(GET.class) ||
+                method.isAnnotationPresent(PUT.class) ||
+                method.isAnnotationPresent(POST.class) ||
+                method.isAnnotationPresent(DELETE.class) ||
+                method.isAnnotationPresent(HEAD.class) ||
+                method.isAnnotationPresent(OPTIONS.class);
+    }
+
 
 }
