4205c5d1f889e43fed61e7b20d3fd179b85fcf58
Dragos Sinca <dragos.sinca@elster.com>
2014-12-04 16:15:50 +0200

    Main apps need to be hanled differently

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/MasterSchedulingApplication.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/MasterSchedulingApplication.java
@@ -116,7 +116,7 @@ public class MasterSchedulingApplication extends Application implements InstallS
         this.clock = clock;
     }
 
-    @Reference(target="(com.elster.jupiter.license.application.key=" + APP_KEY  + ")")
+    @Reference(target="(com.elster.jupiter.license.rest.key=" + APP_KEY  + ")")
     public void setLicense(License license) {
         this.license = license;
     }
