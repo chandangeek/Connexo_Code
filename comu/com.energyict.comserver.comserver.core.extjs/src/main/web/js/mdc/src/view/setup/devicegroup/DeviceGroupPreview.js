b4c0d5f99f7819ad97ef854cb41662c7a8c6730c
igh <isabelle.gheysens@elster.com>
2014-12-05 09:12:31 +0100

    device group details

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicegroup/DeviceGroupPreview.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicegroup/DeviceGroupPreview.js
@@ -5,6 +5,19 @@ Ext.define('Mdc.view.setup.devicegroup.DeviceGroupPreview', {
     requires: [
         'Mdc.view.setup.devicegroup.DeviceGroupActionMenu'
     ],
+
+    tools: [
+        {
+            xtype: 'button',
+            text: Uni.I18n.translate('general.actions', 'MDC', Uni.I18n.translate('general.actions', 'MDC', 'Actions')),
+            hidden: Uni.Auth.hasNoPrivilege('privilege.administrate.deviceGroup'),
+            iconCls: 'x-uni-action-iconD',
+            menu: {
+                xtype: 'device-group-action-menu'
+            }
+        }
+    ],
+
     items: {
         xtype: 'form',
         border: false,
