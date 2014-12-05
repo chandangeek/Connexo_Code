a942124cfb93f6d1b58e1f8bd4a5b7b1577bf4b6
Sentyurin Sergey <ssentyur@mera.ru>
2014-12-05 16:48:02 +0300

    JP-7263 Integrate the next/previous navigation common component into a logbook details view

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicelogbooks/TabbedDeviceLogBookView.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicelogbooks/TabbedDeviceLogBookView.js
@@ -2,25 +2,46 @@ Ext.define('Mdc.view.setup.devicelogbooks.TabbedDeviceLogBookView', {
     extend: 'Uni.view.container.ContentContainer',
     alias: 'widget.tabbedDeviceLogBookView',
     itemId: 'tabbedDeviceLogBookView',
-    content: [
-        {
-            xtype: 'tabpanel',
-            itemId: 'logBookTabPanel',
-            ui: 'large',
-            items: [
-                {
-                    title: 'Specifications',
-                    itemId: 'logBook-specifications'
-                },
-                {
-                    title: 'Data',
-                    itemId: 'logBook-data'
-                }
-            ]
-        }
+    requires: [
+        'Uni.view.toolbar.PreviousNextNavigation'
     ],
     initComponent: function () {
         var me = this;
+        me.content = [
+            {
+                xtype: 'tabpanel',
+                itemId: 'logBookTabPanel',
+                ui: 'large',
+                items: [
+                    {
+                        title: 'Specifications',
+                        itemId: 'logBook-specifications'
+                    },
+                    {
+                        title: 'Data',
+                        itemId: 'logBook-data'
+                    }
+                ],
+                listeners: {
+                    afterrender: function(panel){
+                        var bar = panel.tabBar;
+                        bar.insert(2,[
+                            {
+                                xtype: 'tbfill'
+                            },
+                            {
+                                xtype: 'previous-next-navigation-toolbar',
+                                itemId: 'tabbed-device-logbook-view-previous-next-navigation-toolbar',
+                                store: 'Mdc.store.LogbooksOfDevice',
+                                router: me.router,
+                                routerIdArgument: 'logbookId',
+                                itemsName: '<a href="' + me.router.getRoute('devices/device/logbooks').buildUrl() + '">' + Uni.I18n.translate('devicelogbooks.title', 'MDC', 'Logbooks').toLowerCase() + '</a>'
+                            }
+                        ]);
+                    }
+                }
+            }
+        ];
         me.side = [
             {
                 xtype: 'panel',
