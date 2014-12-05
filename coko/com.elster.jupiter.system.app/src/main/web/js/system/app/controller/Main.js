4036ce3bdda53c333912f0e800f940a626109db5
adrianlupan <adrianlupan@elster.com>
2014-12-05 10:58:48 +0200

    Added data purge privileges

--- coko/com.elster.jupiter.system.app/src/main/web/js/system/app/controller/Main.js
+++ coko/com.elster.jupiter.system.app/src/main/web/js/system/app/controller/Main.js
@@ -8,7 +8,7 @@ Ext.define('SystemApp.controller.Main', {
     defaultToken: '#/administration',
     searchEnabled: false,
     privileges: ['privilege.administrate.userAndRole','privilege.view.userAndRole','privilege.view.license','privilege.upload.license',
-                'privilege.administrate.period','privilege.view.period',
+                'privilege.administrate.period','privilege.view.period','privilege.administrate.dataPurge','privilege.view.dataPurge',
                 'privilege.administrate.dataExportTask','privilege.view.dataExportTask','privilege.update.dataExportTask','privilege.update.schedule.dataExportTask','privilege.run.dataExportTask'],
     controllers: [
         'Usr.controller.Main',
