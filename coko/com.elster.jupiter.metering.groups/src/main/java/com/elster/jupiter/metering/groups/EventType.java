5b5de75b6a2c4f7bb3124b4b143a8ada0d162e82
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-12-08 15:49:21 +0100

    JP-6940: Added REST call, moved EventType to metering.group bundle

---
+++ coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/EventType.java
@@ -0,0 +1,47 @@
+package com.elster.jupiter.metering.groups;
+
+import com.elster.jupiter.events.EventService;
+import com.elster.jupiter.events.EventTypeBuilder;
+import com.elster.jupiter.events.ValueType;
+import com.elster.jupiter.orm.TransactionRequired;
+
+public enum EventType {
+    ENDDEVICEGROUP_VALIDATE_DELETED("enddevicegroup/VALIDATE_DELETE");
+
+    private static final String NAMESPACE = "com/elster/jupiter/metering/groups/";
+    private final String topic;
+    private boolean hasMRID;
+
+    EventType(String topic) {
+        this.topic = topic;
+    }
+
+    EventType(String topic, boolean mRID) {
+        this.topic = topic;
+        this.hasMRID = mRID;
+    }
+
+    public String topic() {
+        return NAMESPACE + topic;
+    }
+
+    @TransactionRequired
+    public void install(EventService eventService) {
+        EventTypeBuilder builder = eventService.buildEventTypeWithTopic(topic())
+                .name(name())
+                .component(MeteringGroupsService.COMPONENTNAME)
+                .category("Crud")
+                .scope("System")
+                .shouldPublish()
+                .withProperty("id", ValueType.LONG, "id")
+                .withProperty("version", ValueType.LONG, "version")
+                .withProperty("MRID", ValueType.STRING, "MRID");
+        addCustomProperties(builder).create().save();
+    }
+
+    EventTypeBuilder addCustomProperties(EventTypeBuilder eventTypeBuilder) {
+        return eventTypeBuilder;
+    }
+
+
+}
