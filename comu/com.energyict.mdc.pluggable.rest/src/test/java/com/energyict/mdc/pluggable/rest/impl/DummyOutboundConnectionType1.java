f75491199b1e53b1364435e28ce5c59032f1e28c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2014-12-09 15:57:19 +0100

    JP-7233: Fix all remaining compile errors, next step is to run all tests ;-)

---
+++ comu/com.energyict.mdc.pluggable.rest/src/test/java/com/energyict/mdc/pluggable/rest/impl/DummyOutboundConnectionType1.java
@@ -0,0 +1,16 @@
+package com.energyict.mdc.pluggable.rest.impl;
+
+import com.energyict.mdc.protocol.api.ConnectionType;
+
+/**
+ * Insert your comments here.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2014-12-09 (15:15)
+ */
+public class DummyOutboundConnectionType1 extends DummyConnectionType {
+    @Override
+    public Direction getDirection() {
+        return Direction.OUTBOUND;
+    }
+}
---
+++ comu/com.energyict.mdc.pluggable.rest/src/test/java/com/energyict/mdc/pluggable/rest/impl/DummyOutboundConnectionType2.java
@@ -0,0 +1,14 @@
+package com.energyict.mdc.pluggable.rest.impl;
+
+/**
+ * Insert your comments here.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2014-12-09 (15:15)
+ */
+public class DummyOutboundConnectionType2 extends DummyConnectionType {
+    @Override
+    public Direction getDirection() {
+        return Direction.OUTBOUND;
+    }
+}
