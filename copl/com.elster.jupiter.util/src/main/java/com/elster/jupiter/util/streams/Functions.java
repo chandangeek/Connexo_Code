5d855f1860603e7fe0974b08eabecb6a66115d59
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-12-09 10:02:49 +0100

    Add Optional to Stream Function

---
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/streams/Functions.java
@@ -0,0 +1,21 @@
+package com.elster.jupiter.util.streams;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public enum Functions {
+    ;
+
+    /**
+     * This Function converts an Optional to a Stream. This is especially handy to convert a Stream of Optionals of T to a Stream of T.
+     * Chances are that Optional will be getting a new method toStream() or will implement Stream in a future version of java, which will make this function obsolete.
+     *
+     * @param <T>
+     * @return a function that converts an Optional to a Stream
+     */
+    public static <T> Function<Optional<T>, Stream<T>> asStream() {
+        return op -> op.map(Stream::of).orElseGet(Stream::empty);
+    }
+
+}
