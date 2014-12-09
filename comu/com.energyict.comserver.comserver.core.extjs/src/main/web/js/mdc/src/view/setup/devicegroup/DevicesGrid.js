3987b5716b5cba6c4465981ad257b1c35b7ff42e
igh <isabelle.gheysens@elster.com>
2014-12-09 16:41:04 +0100

    JP-6885 - View device group details

---
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicegroup/DevicesGrid.js
@@ -0,0 +1,88 @@
+Ext.define('Mdc.view.setup.devicegroup.DevicesGrid', {
+    extend: 'Ext.grid.Panel',
+    xtype: 'devicegroup-devices-grid',
+    overflowY: 'auto',
+
+    requires: [
+        'Mdc.store.Devices',
+        'Uni.view.toolbar.PagingTop',
+        'Uni.view.toolbar.PagingBottom',
+        'Mdc.store.DevicesOfDeviceGroup'
+    ],
+
+    selModel: {
+        mode: 'SINGLE'
+    },
+
+    groupId: null,
+
+    store: 'Mdc.store.DevicesOfDeviceGroup',
+
+    bottomToolbarHidden: true,
+
+    initComponent: function () {
+        var me = this;
+
+        me.store = Ext.getStore(me.store) || Ext.create(me.store);
+        me.store.proxy.extraParams = {
+            id: me.groupId
+        };
+
+        me.columns = [
+            {
+                header: Uni.I18n.translate('searchItems.mrid', 'MDC', 'MRID'),
+                dataIndex: 'mRID',
+                sortable: false,
+                hideable: false,
+                renderer: function (value, b, record) {
+                    return '<a href="#/devices/' + record.get('mRID') + '">' + value + '</a>';
+                },
+                fixed: true,
+                flex: 3
+            },
+            {
+                header: Uni.I18n.translate('searchItems.serialNumber', 'MDC', 'Serial number'),
+                dataIndex: 'serialNumber',
+                sortable: false,
+                hideable: false,
+                fixed: true,
+                flex: 3
+            },
+            {
+                header: Uni.I18n.translate('searchItems.type', 'MDC', 'Type'),
+                dataIndex: 'deviceTypeName',
+                sortable: false,
+                hideable: false,
+                fixed: true,
+                flex: 3
+            },
+            {
+                header: Uni.I18n.translate('searchItems.configuration', 'MDC', 'Configuration'),
+                dataIndex: 'deviceConfigurationName',
+                sortable: false,
+                hideable: false,
+                fixed: true,
+                flex: 3
+            }
+        ];
+
+        me.dockedItems = [
+            {
+                xtype: 'pagingtoolbartop',
+                store: me.store,
+                dock: 'top',
+                displayMsg: Uni.I18n.translate('devices.pagingtoolbartop.displayMsg', 'MDC', '{0} - {1} of {2} devices'),
+                displayMoreMsg: Uni.I18n.translate('devices.pagingtoolbartop.displayMoreMsg', 'MDC', '{0} - {1} of more than {2} devices'),
+                emptyMsg: Uni.I18n.translate('devices.pagingtoolbartop.emptyMsg', 'MDC', 'There are no devices to display')
+            },
+            {
+                xtype: 'pagingtoolbarbottom',
+                store: me.store,
+                dock: 'bottom',
+                itemsPerPageMsg: Uni.I18n.translate('devices.pagingtoolbarbottom.itemsPerPage', 'MDC', 'Devices per page')
+            }
+        ];
+
+        me.callParent();
+    }
+});
