bf1ce6debd6982005f455dbd3c910c0d9b66765f
Anton Suslennikov <antsusl@mera.ru>
2014-12-11 20:04:41 +0300

    JP-7394Cannot create register validation rules

--- coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/MeteringApplication.java
+++ coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/MeteringApplication.java
@@ -1,6 +1,13 @@
 package com.elster.jupiter.metering.rest.impl;
 
+import com.elster.jupiter.cbo.TimeAttribute;
 import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.SimpleTranslationKey;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.TranslationKey;
+import com.elster.jupiter.nls.TranslationKeyProvider;
 import com.elster.jupiter.rest.util.BinderProvider;
 import com.elster.jupiter.rest.util.RestQueryService;
 import com.elster.jupiter.transaction.TransactionService;
@@ -12,17 +19,24 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
+import javax.validation.MessageInterpolator;
 import javax.ws.rs.core.Application;
 import java.time.Clock;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-@Component(name = "com.elster.jupiter.metering.rest" , service=Application.class , immediate = true , property = {"alias=/mtr", "app=SYS", "name=" + MeteringApplication.COMPONENT_NAME} )
-public class MeteringApplication extends Application implements BinderProvider {
+@Component(name = "com.elster.jupiter.metering.rest" , service={Application.class, TranslationKeyProvider.class}, immediate = true , property = {"alias=/mtr", "app=SYS", "name=" + MeteringApplication.COMPONENT_NAME} )
+public class MeteringApplication extends Application implements TranslationKeyProvider {
     public static final String COMPONENT_NAME = "MTR";
 
     private volatile MeteringService meteringService;
     private volatile TransactionService transactionService;
     private volatile RestQueryService restQueryService;
+    private volatile Thesaurus thesaurus;
     private volatile Clock clock;
 
     public Set<Class<?>> getClasses() {
@@ -53,6 +67,11 @@ public class MeteringApplication extends Application implements BinderProvider {
         this.clock = clock;
     }
 
+    @Reference
+    public void setNlsService(NlsService nlsService) {
+        this.thesaurus = nlsService.getThesaurus(getComponentName(), getLayer());
+    }
+
     @Activate
     public void activate() {
     }
@@ -62,15 +81,42 @@ public class MeteringApplication extends Application implements BinderProvider {
     }
 
     @Override
-    public Binder getBinder() {
-        return new AbstractBinder() {
-            @Override
-            protected void configure() {
-                bind(restQueryService).to(RestQueryService.class);
-                bind(transactionService).to(TransactionService.class);
-                bind(meteringService).to(MeteringService.class);
-                bind(clock).to(Clock.class);
-            }
+    public Set<Object> getSingletons() {
+        Set<Object> hashSet = new HashSet<>();
+        hashSet.addAll(super.getSingletons());
+        hashSet.add(new HK2Binder());
+        return Collections.unmodifiableSet(hashSet);
+    }
+
+    @Override
+    public String getComponentName() {
+        return COMPONENT_NAME;
+    }
+
+    @Override
+    public Layer getLayer() {
+        return Layer.REST;
+    }
+
+    @Override
+    public List<TranslationKey> getKeys() {
+        List<TranslationKey> keys = new ArrayList<>(Arrays.asList(MessageSeeds.values()));
+        for (int i = 1; i < TimeAttribute.values().length; i++){
+            TimeAttribute ta = TimeAttribute.values()[i];
+            keys.add(new SimpleTranslationKey(MessageSeeds.Keys.TIME_ATTRIBUTE_KEY_PREFIX + ta.getId(), ta.getDescription()));
+        }
+        return keys;
+    }
+
+    class HK2Binder extends AbstractBinder {
+        @Override
+        protected void configure() {
+            bind(restQueryService).to(RestQueryService.class);
+            bind(transactionService).to(TransactionService.class);
+            bind(meteringService).to(MeteringService.class);
+            bind(clock).to(Clock.class);
+            bind(thesaurus).to(Thesaurus.class);
+            bind(thesaurus).to(MessageInterpolator.class);
         };
     }
 }
