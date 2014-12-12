3a136a7c66545a856c2553d741007f5bf8134e59
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-12-12 10:46:27 +0100

    Add init dependency

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/DataExportServiceImpl.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/DataExportServiceImpl.java
@@ -146,7 +146,7 @@ public class DataExportServiceImpl implements IDataExportService, InstallService
 
     @Override
     public List<String> getPrerequisiteModules() {
-        return Arrays.asList(OrmService.COMPONENTNAME, TimeService.COMPONENT_NAME, MeteringService.COMPONENTNAME, TaskService.COMPONENTNAME, MeteringGroupsService.COMPONENTNAME, MessageService.COMPONENTNAME, NlsService.COMPONENTNAME);
+        return Arrays.asList(OrmService.COMPONENTNAME, TimeService.COMPONENT_NAME, MeteringService.COMPONENTNAME, TaskService.COMPONENTNAME, MeteringGroupsService.COMPONENTNAME, MessageService.COMPONENTNAME, NlsService.COMPONENTNAME, AppService.COMPONENT_NAME);
     }
 
     @Override
