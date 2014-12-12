dd6a3fd1a802c800e073f422ebe2f927ac1e896c
adrianlupan <adrianlupan@elster.com>
2014-12-12 11:28:17 +0200

    Revoked privileges check in GUI

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/Auth.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/Auth.js
@@ -11,12 +11,14 @@ Ext.define('Uni.Auth', {
     ],
 
     hasPrivilege: function (privilege) {
-        for (var i = 0; i < Ldr.store.Privileges.getCount(); i++) {
-            if (privilege === Ldr.store.Privileges.getAt(i).get('name')) {
-                return true;
-            }
-        }
-        return false;
+        return true;
+//        TODO: uncomment this when privileges are changed in front-end
+//        for (var i = 0; i < Ldr.store.Privileges.getCount(); i++) {
+//            if (privilege === Ldr.store.Privileges.getAt(i).get('name')) {
+//                return true;
+//            }
+//        }
+//        return false;
     },
 
     hasNoPrivilege: function (privilege) {
@@ -24,25 +26,27 @@ Ext.define('Uni.Auth', {
     },
 
     hasAnyPrivilege: function (privileges) {
-        var result = false;
-        if (Ext.isArray(privileges)) {
-            for (var i = 0; i < privileges.length; i++) {
-                var privilege = privileges[i];
-                if (Ext.isArray(privilege)) {
-                    result = false;
-                    for (var j = 0; j < privilege.length; j++) {
-                        result = result || this.hasPrivilege(privilege[j]);
-                    }
-                    if (!result) {
-                        return result;
-                    }
-                } else {
-                    if (this.hasPrivilege(privilege)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return result;
+        return true;
+//        TODO: uncomment this when privileges are changed in front-end
+//        var result = false;
+//        if (Ext.isArray(privileges)) {
+//            for (var i = 0; i < privileges.length; i++) {
+//                var privilege = privileges[i];
+//                if (Ext.isArray(privilege)) {
+//                    result = false;
+//                    for (var j = 0; j < privilege.length; j++) {
+//                        result = result || this.hasPrivilege(privilege[j]);
+//                    }
+//                    if (!result) {
+//                        return result;
+//                    }
+//                } else {
+//                    if (this.hasPrivilege(privilege)) {
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//        return result;
     }
 });
