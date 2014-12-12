a368958853589e8f09788c97217f6f9a12b72db2
adrianlupan <adrianlupan@elster.com>
2014-12-12 12:53:31 +0200

    Changed validation privileges

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/security/Privileges.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/security/Privileges.java
@@ -4,4 +4,10 @@ public interface Privileges {
 
     String ADMINISTRATE_VALIDATION_CONFIGURATION = "privilege.administrate.validationConfiguration";
     String VIEW_VALIDATION_CONFIGURATION = "privilege.view.validationConfiguration";
+
+    String VALIDATE_MANUAL = "privilege.view.validateManual";
+
+    String FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE = "privilege.view.fineTuneValidationConfiguration.onDevice";
+    String FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE_CONFIGURATION = "privilege.view.fineTuneValidationConfiguration.onDeviceConfiguration";
+
 }
