fcf09e71bfd0670ceea95296ece37f8e1eb118f3
Lobanov Dmitry <dlobanov@mera.ru>
2014-12-12 19:14:24 +0300

    Intro

---
+++ comu/com.energyict.mdc.app.help/src/main/web/whxdata/toc19.js
@@ -0,0 +1,1 @@
+gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><data  src=\"toc.js\"  name=\"Get data valid\" url=\"000017_EN_Connexo_1.0_User_guide/000017_UMNL_Get_data_valid/Get_data_valid.htm\"><book  src=\"toc20.js\"  name=\"Edit data\" url=\"000017_EN_Connexo_1.0_User_guide/000017_UMNL_Get_data_valid/Edit_data.htm\" /></data>";
