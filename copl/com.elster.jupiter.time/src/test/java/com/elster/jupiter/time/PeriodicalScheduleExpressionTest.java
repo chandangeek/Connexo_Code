c7c29ffcb52a7fddc379db7ceae472cd49753903
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-12-12 10:22:38 +0100

    JP-7332 - Data export task scheduling

--- copl/com.elster.jupiter.time/src/test/java/com/elster/jupiter/time/PeriodicalScheduleExpressionTest.java
+++ copl/com.elster.jupiter.time/src/test/java/com/elster/jupiter/time/PeriodicalScheduleExpressionTest.java
@@ -1,46 +1,119 @@
 package com.elster.jupiter.time;
 
-import com.elster.jupiter.util.time.ScheduleExpression;
+import com.elster.jupiter.util.Pair;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.time.DayOfWeek;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Optional;
 
+import static com.elster.jupiter.devtools.tests.assertions.JupiterAssertions.assertThat;
 import static java.util.Arrays.asList;
 
 @RunWith(Parameterized.class)
 public class PeriodicalScheduleExpressionTest {
 
-    @Test
-    public void test() {
-        List<PeriodicalScheduleExpression> expressions =
-                asList(
-                        PeriodicalScheduleExpression.every(2).months().at(4, 0, 0, 0).build(),
-                        PeriodicalScheduleExpression.every(3).years().atLastDayOfMonth(2, 14, 15, 22).build(),
-                        PeriodicalScheduleExpression.every(4).weeks().at(DayOfWeek.TUESDAY, 0, 0, 0).build(),
-                        PeriodicalScheduleExpression.every(5).days().at(16, 12, 11).build(),
+    @Parameterized.Parameters
+    public static List<Object[]> arguments() {
+        return asList(
+            new Object[] {
+                    PeriodicalScheduleExpression.every(2).months().at(4, 0, 0, 0).build(),
+                    "P[2,MONTH,0,0,0,4]",
+                    asList(
+                            Pair.of(time(2014, 12, 2, 14, 32), time(2014, 12, 4, 0, 0, 0)),
+                            Pair.of(time(2014, 12, 4, 14, 32), time(2015, 2, 4, 0, 0, 0)),
+                            Pair.of(time(2014, 12, 8, 14, 32), time(2015, 2, 4, 0, 0, 0))
+                    )
+            },
+            new Object[] {
+                    PeriodicalScheduleExpression.every(3).years().atLastDayOfMonth(2, 14, 15, 22).build(),
+                    "P[3,YEAR,22,15,14,LAST,2]",
+                    asList(
+                            Pair.of(time(2012, 4, 18, 15, 12), time(2015, 2, 28, 14, 15, 22)),
+                            Pair.of(time(2012, 1, 18, 15, 12), time(2012, 2, 29, 14, 15, 22)),
+                            Pair.of(time(2011, 1, 18, 15, 12), time(2011, 2, 28, 14, 15, 22))
+                    )
+            },
+            new Object[] {
+                    PeriodicalScheduleExpression.every(3).years().at(2, 3, 14, 15, 22).build(),
+                    "P[3,YEAR,22,15,14,3,2]",
+                    asList(
+                            Pair.of(time(2012, 4, 18, 15, 12), time(2015, 2, 3, 14, 15, 22)),
+                            Pair.of(time(2012, 1, 18, 15, 12), time(2012, 2, 3, 14, 15, 22)),
+                            Pair.of(time(2011, 1, 18, 15, 12), time(2011, 2, 3, 14, 15, 22))
+                    )
+            },
+            new Object[] {
+                    PeriodicalScheduleExpression.every(4).weeks().at(DayOfWeek.TUESDAY, 0, 0, 0).build(),
+                    "P[4,WEEK,0,0,0,TUESDAY]",
+                    asList(
+                            Pair.of(time(2014, 12, 8, 15, 12), time(2014, 12, 9, 0, 0, 0)),
+                            Pair.of(time(2014, 12, 9, 0, 0), time(2015, 1, 6, 0, 0, 0)),
+                            Pair.of(time(2014, 12, 9, 0, 1), time(2015, 1, 6, 0, 0, 0))
+                    )
+            },
+            new Object[] {
+                    PeriodicalScheduleExpression.every(5).days().at(16, 12, 11).build(),
+                    "P[5,DAY,11,12,16]",
+                    asList(
+                            Pair.of(time(1984, 5, 22, 15, 12), time(1984, 5, 22, 16, 12, 11)),
+                            Pair.of(time(1984, 5, 22, 17, 12), time(1984, 5, 27, 16, 12, 11)),
+                            Pair.of(time(2012, 2, 27, 17, 1), time(2012, 3, 3, 16, 12, 11))
+                    )
+            },
+                new Object[] {
                         PeriodicalScheduleExpression.every(6).hours().at(10, 0).build(),
-                        PeriodicalScheduleExpression.every(7).minutes().at(23).build()
-                );
+                        "P[6,HOUR,0,10]",
+                        asList(
+                                Pair.of(time(1994, 6, 30, 15, 12), time(1994, 6, 30, 21, 10, 0)),
+                                Pair.of(time(1994, 6, 30, 15, 9), time(1994, 6, 30, 15, 10, 0)),
+                                Pair.of(time(1994, 6, 30, 21, 18), time(1994, 7, 1, 3, 10, 0))
+                        )
+                },
+                new Object[] {
+                        PeriodicalScheduleExpression.every(7).minutes().at(23).build(),
+                        "P[7,MINUTE,23]",
+                        asList(
+                                Pair.of(time(1994, 6, 30, 15, 12, 24), time(1994, 6, 30, 15, 19, 23)),
+                                Pair.of(time(1994, 6, 30, 15, 12, 20), time(1994, 6, 30, 15, 12, 23))
+                        )
+            }
 
-        expressions.stream().map(ScheduleExpression::encoded).forEach(System.out::println);
-
-        ZonedDateTime time = ZonedDateTime.now();
-
-        expressions.stream()
-                .map(sch -> sch.nextOccurrence(time))
-                .forEach(System.out::println);
-
-        TemporalExpression temporalExpression = new TemporalExpression(new TimeDuration(3, TimeDuration.TimeUnit.YEARS), new TimeDuration(0, TimeDuration.TimeUnit.SECONDS));
-
-        Optional<ZonedDateTime> threeYears = temporalExpression.nextOccurrence(temporalExpression.nextOccurrence(time).get());
-
-        System.out.println(threeYears);
+        );
     }
 
+    private PeriodicalScheduleExpression expression;
+    private String stringForm;
+    private List<Pair<ZonedDateTime, ZonedDateTime>> nextOccurrencePairs;
+
+    public PeriodicalScheduleExpressionTest(PeriodicalScheduleExpression expression, String stringForm, List<Pair<ZonedDateTime, ZonedDateTime>> nextOccurrencePairs) {
+        this.expression = expression;
+        this.stringForm = stringForm;
+        this.nextOccurrencePairs = nextOccurrencePairs;
+    }
+
+    @Test
+    public void testEncoded() {
+        assertThat(expression.encoded()).isEqualTo(stringForm);
+    }
+
+    @Test
+    public void testNextOccurrence() {
+        for (Pair<ZonedDateTime, ZonedDateTime> pair : nextOccurrencePairs) {
+            assertThat(expression.nextOccurrence(pair.getFirst())).contains(pair.getLast());
+        }
+    }
+
+
+    private static ZonedDateTime time(int year, int month, int day, int hour, int minute) {
+        return ZonedDateTime.of(year, month, day, hour, minute, 44, 654897321, ZoneId.systemDefault());
+    }
+
+    private static ZonedDateTime time(int year, int month, int day, int hour, int minute, int second) {
+        return ZonedDateTime.of(year, month, day, hour, minute, second, 0, ZoneId.systemDefault());
+    }
 
 }
