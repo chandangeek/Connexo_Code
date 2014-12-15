438930a123335840c2f3d6a54e01e07b12033af6
Artem Puchenkin <apuchenkin@gmail.com>
2014-12-15 13:13:09 +0300

    [JP-7185] View connections on device landing page

---
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/model/DeviceConnection.js
@@ -0,0 +1,42 @@
+Ext.define('Mdc.model.DeviceConnection', {
+    extend: 'Ext.data.Model',
+
+    fields: [
+        {name: 'id', type: 'number'},
+        {name: 'currentState', type: 'auto'},
+        {name: 'latestStatus', type: 'auto'},
+        {name: 'latestResult', type: 'auto'},
+        {name: 'taskCount', type: 'auto'},
+        {name: 'startDateTime', type: 'date', dateFormat: 'time'},
+        {name: 'endDateTime', type: 'date', dateFormat: 'time'},
+        {name: 'duration', type: 'auto'},
+        {name: 'comPort', type: 'auto'},
+        {name: 'comPortPool', type: 'auto'},
+        {name: 'direction', type: 'string'},
+        {name: 'connectionType', type: 'string'},
+        {name: 'comServer', type: 'auto'},
+        {name: 'connectionMethod', type: 'auto'},
+        {name: 'window', type: 'string'},
+        {name: 'connectionStrategy', type: 'auto'},
+        {name: 'nextExecution', type: 'date', dateFormat: 'time'},
+        {name: 'comSessionId', type: 'number'},
+
+        // mapped fields
+        {name: 'isDefault', type: 'boolean', mapping: function (data) {return data.connectionMethod.isDefault;}, persist: false}
+    ],
+
+    proxy: {
+        type: 'rest',
+        urlTpl: '/api/ddr/devices/{mRID}/connections',
+        reader: {
+            type: 'json',
+            root: 'connections',
+            totalProperty: 'total'
+        },
+
+        setUrl: function (mRID) {
+            this.url = this.urlTpl.replace('{mRID}', mRID);
+        }
+    }
+});
+
