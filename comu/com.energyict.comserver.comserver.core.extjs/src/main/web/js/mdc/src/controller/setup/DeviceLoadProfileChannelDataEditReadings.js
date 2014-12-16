8bf6d817d8e930a48a0da2105f9e99b00c6ed066
akuryuk <akuryuk@mera.ru>
2014-12-16 12:08:31 +0300

    [HotFix] Edit readings + getProxy of undefined Fixes

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceLoadProfileChannelDataEditReadings.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceLoadProfileChannelDataEditReadings.js
@@ -137,7 +137,7 @@ Ext.define('Mdc.controller.setup.DeviceLoadProfileChannelDataEditReadings', {
         if (changedData.length) {
             page.setLoading(Uni.I18n.translate('general.saving', 'MDC', 'Saving...'));
             Ext.Ajax.request({
-                url: Ext.String.format('/api/ddr/devices/{0}/channels/{2}/data', router.arguments.mRID, router.arguments.channelId),
+                url: Ext.String.format('/api/ddr/devices/{0}/channels/{1}/data', router.arguments.mRID, router.arguments.channelId),
                 method: 'PUT',
                 jsonData: Ext.encode(changedData),
                 timeout: 300000,
