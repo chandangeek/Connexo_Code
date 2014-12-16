61fc7f182a25d4d39d0b62b4da4323c01c7b5e59
Dragos Sinca <dragos.sinca@elster.com>
2014-12-16 19:32:57 +0200

    Added transactional support

--- coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinApplication.java
+++ coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinApplication.java
@@ -2,6 +2,8 @@ package com.elster.jupiter.yellowfin.rest.impl;
 
 import com.elster.jupiter.license.License;
 import com.elster.jupiter.rest.util.BinderProvider;
+import com.elster.jupiter.transaction.Transaction;
+import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.yellowfin.YellowfinService;
 import com.elster.jupiter.yellowfin.groups.YellowfinGroupsService;
 import com.google.common.collect.ImmutableSet;
@@ -21,6 +23,7 @@ public class YellowfinApplication extends Application implements BinderProvider{
     public static final String APP_KEY = "YFN";
     public static final String COMPONENT_NAME = "YFN";
 
+    private volatile TransactionService transactionService;
     private volatile YellowfinService yellowfinService;
     private volatile YellowfinGroupsService yellowfinGroupsService;
     private volatile License license;
@@ -31,6 +34,11 @@ public class YellowfinApplication extends Application implements BinderProvider{
     @Override
     public Set<Class<?>> getClasses() {
         return ImmutableSet.of(YellowfinResource.class, YellowfinDeviceGroupsResource.class);
+    }
+
+    @Reference
+    public void setTransactionService(TransactionService transactionService) {
+        this.transactionService = transactionService;
     }
 
     @Reference
@@ -61,6 +69,7 @@ public class YellowfinApplication extends Application implements BinderProvider{
         return new AbstractBinder() {
             @Override
             protected void configure() {
+                bind(transactionService).to(TransactionService.class);
                 bind(yellowfinService).to(YellowfinService.class);
                 bind(yellowfinGroupsService).to(YellowfinGroupsService.class);
             }
--- coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinDeviceGroupsResource.java
+++ coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinDeviceGroupsResource.java
@@ -1,25 +1,32 @@
 package com.elster.jupiter.yellowfin.rest.impl;
 
+import com.elster.jupiter.transaction.TransactionContext;
+import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.yellowfin.groups.YellowfinGroupsService;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
 
 @Path("/devicegroups")
 public class YellowfinDeviceGroupsResource {
 
-    private YellowfinGroupsService yellowfinGroupsService;
+    private final YellowfinGroupsService yellowfinGroupsService;
+    private final TransactionService transactionService;
 
     @Inject
-    private YellowfinDeviceGroupsResource(YellowfinGroupsService yellowfinGroupsService){
+    private YellowfinDeviceGroupsResource(YellowfinGroupsService yellowfinGroupsService, TransactionService transactionService){
         this.yellowfinGroupsService = yellowfinGroupsService;
+        this.transactionService = transactionService;
     }
 
     @POST
     @Path("/dynamic/{groupname}")
-    public void cacheDynamicGroup(@QueryParam("groupname") String groupname) {
-        yellowfinGroupsService.cacheDeviceGroup(groupname);
+    public void cacheDynamicGroup(@PathParam("groupname") String groupname) {
+        try(TransactionContext context = transactionService.getContext()){
+            yellowfinGroupsService.cacheDeviceGroup(groupname);
+            context.commit();
+        }
     }
 }
