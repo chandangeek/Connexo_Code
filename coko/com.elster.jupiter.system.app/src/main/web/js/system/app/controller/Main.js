69b57875065928b7c69f0ac4620f4a53f3b4ca9d
Sentyurin Sergey <ssentyur@mera.ru>
2014-12-17 16:46:51 +0300

    JP-7514 Consult online Admin help

--- coko/com.elster.jupiter.system.app/src/main/web/js/system/app/controller/Main.js
+++ coko/com.elster.jupiter.system.app/src/main/web/js/system/app/controller/Main.js
@@ -7,6 +7,7 @@ Ext.define('SystemApp.controller.Main', {
     applicationTitle: 'Connexo Admin',
     defaultToken: '#/administration',
     searchEnabled: false,
+    onlineHelpEnabled: true,
     privileges: ['privilege.administrate.userAndRole','privilege.view.userAndRole','privilege.view.license','privilege.upload.license',
                 'privilege.administrate.period','privilege.view.period','privilege.administrate.dataPurge','privilege.view.dataPurge',
                 'privilege.administrate.dataExportTask','privilege.view.dataExportTask','privilege.update.dataExportTask','privilege.update.schedule.dataExportTask','privilege.run.dataExportTask'],
