79cfd24ef865b1db328e7257130061bec83af4ed
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-12-17 16:12:21 +0100

    JP-7439: Adding POST for DataCollectionKpi

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/TimeDurationInfo.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/TimeDurationInfo.java
@@ -1,7 +1,7 @@
 package com.energyict.mdc.common.rest;
 
 import com.elster.jupiter.time.TimeDuration;
-
+import java.time.temporal.TemporalAmount;
 import java.util.Optional;
 
 public class TimeDurationInfo {
@@ -45,6 +45,11 @@ public class TimeDurationInfo {
 
     public TimeDuration asTimeDuration() {
         return new TimeDuration(this.count+" "+this.timeUnit);
+    }
+
+    public TemporalAmount asTemporalAmount() {
+        return asTimeDuration().getTimeUnit().getTemporalUnit().getDuration();
+
     }
 
 }
