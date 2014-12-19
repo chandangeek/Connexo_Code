6e6c9eaa9cf8a33192484375dd55d1820b8d94a3
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2014-12-19 08:56:15 +0100

    JP-7439: Adding tests for TimeDuration conversion to TemporalAmount

--- copl/com.elster.jupiter.time/src/test/java/com/elster/jupiter/time/TimeDurationTest.java
+++ copl/com.elster.jupiter.time/src/test/java/com/elster/jupiter/time/TimeDurationTest.java
@@ -1,12 +1,14 @@
 package com.elster.jupiter.time;
 
-import org.junit.Test;
-
+import java.time.Duration;
+import java.time.Period;
 import java.util.Calendar;
 import java.util.Date;
+import org.junit.Test;
 
 import static com.elster.jupiter.util.Checks.is;
 import static junit.framework.Assert.assertEquals;
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class TimeDurationTest {
@@ -50,6 +52,55 @@ public class TimeDurationTest {
         duration = new TimeDuration(15, TimeDuration.TimeUnit.SECONDS);
         duration.truncate(cal);
         assertTime(cal.getTime(), 12, 31, 15);
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForMinutes() throws Exception {
+        TimeDuration td = new TimeDuration(5, TimeDuration.TimeUnit.MINUTES);
+        assertThat(td.asTemporalAmount()).isEqualTo(Duration.ofMinutes(5));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForSeconds() throws Exception {
+        TimeDuration td = new TimeDuration(10, TimeDuration.TimeUnit.SECONDS);
+        assertThat(td.asTemporalAmount()).isEqualTo(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForYears() throws Exception {
+        TimeDuration td = new TimeDuration("3 years");
+        assertThat(td.asTemporalAmount()).isEqualTo(Period.ofYears(3));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForDays() throws Exception {
+        TimeDuration td = new TimeDuration("9 days");
+        assertThat(td.asTemporalAmount()).isEqualTo(Period.ofDays(9));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForMillis() throws Exception {
+        TimeDuration td = new TimeDuration("310 milliseconds");
+        assertThat(td.asTemporalAmount()).isEqualTo(Duration.ofMillis(310));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForHours() throws Exception {
+        TimeDuration td = new TimeDuration("5 hours");
+        assertThat(td.asTemporalAmount()).isEqualTo(Duration.ofHours(5));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountForWeeks() throws Exception {
+        TimeDuration td = new TimeDuration("1 weeks");
+        assertThat(td.asTemporalAmount()).isEqualTo(Period.ofWeeks(1));
+    }
+
+    @Test
+    public void testConversionToTemporalAmountFor2Weeks() throws Exception {
+        TimeDuration td = new TimeDuration("2 weeks");
+        assertThat(td.asTemporalAmount()).isEqualTo(Period.ofWeeks(2));
+        assertThat(td.asTemporalAmount()).isEqualTo(Period.ofDays(14));
     }
 
     private static void assertTime(Date testDate, int hour, int minute, int second) {
