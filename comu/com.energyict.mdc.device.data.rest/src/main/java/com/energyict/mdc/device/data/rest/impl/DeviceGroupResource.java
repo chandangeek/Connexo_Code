16d8293cd98bb14ade57d8975a61e835c39ed68f
igh <isabelle.gheysens@elster.com>
2014-12-22 08:55:21 +0100

    JP-7666 - Edit device group: static device group membership erased when editing

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceGroupResource.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceGroupResource.java
@@ -125,7 +125,7 @@ public class DeviceGroupResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @RolesAllowed({Privileges.ADMINISTRATE_DEVICE_GROUP, Privileges.ADMINISTRATE_DEVICE_ENUMERATED_GROUP, Privileges.VIEW_DEVICE_GROUP_DETAIL})
+    // not protected by privileges yet because a combobox containing al the groups needs to be shown when creating an export task
     public PagedInfoList getDeviceGroups(@BeanParam QueryParameters queryParameters, @QueryParam("type") String typeName, @Context UriInfo uriInfo) {
 
         com.elster.jupiter.rest.util.QueryParameters koreQueryParameters =
