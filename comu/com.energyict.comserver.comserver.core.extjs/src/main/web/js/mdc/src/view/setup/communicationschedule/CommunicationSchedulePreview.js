5fba59d2d6e3be9b0c50b6aa6b6308e2a0c05ff6
Sentyurin Sergey <ssentyur@mera.ru>
2014-12-23 14:18:21 +0300

    JP-7714: View shered schedules; Add communication schedule; Edit communication schedule

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/communicationschedule/CommunicationSchedulePreview.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/communicationschedule/CommunicationSchedulePreview.js
@@ -100,12 +100,8 @@ Ext.define('Mdc.view.setup.communicationschedule.CommunicationSchedulePreview', 
                                     xtype: 'displayfield',
                                     name: 'plannedDate',
                                     fieldLabel: Uni.I18n.translate('communicationschedule.plannedDate', 'MDC', 'Planned date'),
-                                    renderer: function(value){
-                                        if(value!==null){
-                                            return new Date(value).toLocaleString();
-                                        } else {
-                                            return '';
-                                        }
+                                    renderer: function (value) {
+                                        return value ? Uni.DateTime.formatDateTimeLong(value) : '';
                                     }
                                 }
 
