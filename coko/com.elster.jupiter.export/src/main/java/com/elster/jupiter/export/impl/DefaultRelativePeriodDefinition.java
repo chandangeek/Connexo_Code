8fd2a7151d8f01c9763f9b68e07d217ce077ae7b
Tom De Greyt <Tom.DeGreyt@elster.com>
2014-12-24 10:48:56 +0100

    JP-7721 - This month is not correctly configured

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/DefaultRelativePeriodDefinition.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/DefaultRelativePeriodDefinition.java
@@ -11,17 +11,16 @@ import java.time.temporal.WeekFields;
 import java.util.List;
 import java.util.Locale;
 
-import static com.elster.jupiter.time.RelativeDate.NOW;
 import static com.elster.jupiter.time.RelativeField.*;
 
 public enum DefaultRelativePeriodDefinition {
     LAST_7_DAYS("Last 7 days", atMidnight(DAY.minus(7)), atMidnight()),
     PREVIOUS_MONTH("Previous month", atMidnight(MONTH.minus(1), DAY.equalTo(1)), atMidnight(MONTH.minus(0), DAY.equalTo(1))),
-    THIS_MONTH("This month", atMidnight(MONTH.minus(0), DAY.equalTo(1)), NOW),
+    THIS_MONTH("This month", atMidnight(MONTH.minus(0), DAY.equalTo(1)), atMidnight(DAY.plus(1))),
     PREVIOUS_WEEK("Previous week", onFirstDayOfWeek(atMidnight(WEEK.minus(1))), onFirstDayOfWeek(atMidnight(WEEK.minus(0)))),
-    THIS_WEEK("This week", onFirstDayOfWeek(atMidnight(WEEK.minus(0))), NOW),
-    YESTERDAY("Yesterday", atMidnight(DAY.minus(1)), atMidnight()),
-    TODAY("Today", atMidnight(DAY.minus(0)), NOW);
+    THIS_WEEK("This week", onFirstDayOfWeek(atMidnight(WEEK.minus(0))), atMidnight(DAY.plus(1))),
+    YESTERDAY("Yesterday", atMidnight(DAY.minus(1)), atMidnight(DAY.minus(0))),
+    TODAY("Today", atMidnight(DAY.minus(0)), atMidnight(DAY.plus(1)));
 
     private final String name;
     private final RelativeDate from;
