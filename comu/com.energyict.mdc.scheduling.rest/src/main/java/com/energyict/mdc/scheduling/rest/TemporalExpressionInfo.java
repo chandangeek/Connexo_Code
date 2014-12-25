ffa0b17637c462291ec9904b98077d3fbbcd9b46
Anton Suslennikov <antsusl@mera.ru>
2014-12-25 15:00:37 +0300

    JP-7439 View data collection KPIs (merge)

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/TemporalExpressionInfo.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/TemporalExpressionInfo.java
@@ -1,7 +1,14 @@
 package com.energyict.mdc.scheduling.rest;
 
-import com.energyict.mdc.common.rest.TimeDurationInfo;
 import com.elster.jupiter.time.TemporalExpression;
+import com.energyict.mdc.common.rest.TimeDurationInfo;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
+import java.util.function.Function;
 
 public class TemporalExpressionInfo {
     public TimeDurationInfo every;
@@ -17,6 +24,48 @@ public class TemporalExpressionInfo {
         info.offset=new TimeDurationInfo(temporalExpression.getOffset().getSeconds());
         info.lastDay=temporalExpression.isLastDay();
         return info;
+    }
+
+    /**
+     * This method wraps the passed time value in the REST info object {@link TemporalExpressionInfo}.
+     * @param temporalAmount time which will be wrapped, should have a single-unit value, i.e
+     * time value '1 day 30 min' will be converted to '1 day' (the biggest dimension will be used)
+     * @return the info wrapper which can be send to front-end
+     */
+    public static TemporalExpressionInfo from(TemporalAmount temporalAmount) {
+        TemporalExpressionInfo info = new TemporalExpressionInfo();
+        if (temporalAmount instanceof Duration){
+            /* Special case for Duration class, because it supports only two units: seconds and nanos */
+            info.every = new TimeDurationInfo();
+            Duration duration = (Duration) temporalAmount;
+            fromDuration(info.every, duration, Duration::toDays, ChronoUnit.DAYS);
+            fromDuration(info.every, duration, Duration::toHours, ChronoUnit.HOURS);
+            fromDuration(info.every, duration, Duration::toMinutes, ChronoUnit.MINUTES);
+        } else {
+            info.every = fromTemporalAmount(temporalAmount);
+        }
+        return info;
+    }
+
+    private static void fromDuration(TimeDurationInfo info, Duration duration, Function<Duration, Long> conv, TemporalUnit unit){
+        if (info != null && info.count == 0){
+            info.count = conv.apply(duration);
+            info.timeUnit = unit.toString().toLowerCase();
+        }
+    }
+
+    private static void fromTemporalAmount(TimeDurationInfo info, TemporalAmount temporalAmount, TemporalUnit unit){
+        if (info != null && info.count == 0){
+            info.count = temporalAmount.get(unit);
+            info.timeUnit = unit.toString().toLowerCase();
+        }
+    }
+    private static TimeDurationInfo fromTemporalAmount(TemporalAmount temporalAmount){
+        TimeDurationInfo durationInfo = new TimeDurationInfo();
+        for (TemporalUnit unit : temporalAmount.getUnits()) {
+            fromTemporalAmount(durationInfo, temporalAmount, unit);
+        }
+        return durationInfo;
     }
 
     public TemporalExpression asTemporalExpression() {
