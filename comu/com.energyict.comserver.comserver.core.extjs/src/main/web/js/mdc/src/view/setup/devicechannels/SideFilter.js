acd6193ca6d3d8e036b94432c14e01429eff41f4
Sentyurin Sergey <ssentyur@mera.ru>
2014-12-26 11:20:26 +0300

    datefield format fix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicechannels/SideFilter.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicechannels/SideFilter.js
@@ -29,10 +29,7 @@ Ext.define('Mdc.view.setup.devicechannels.SideFilter', {
                         name: 'intervalStart',
                         fieldLabel: Uni.I18n.translate('deviceloadprofiles.filter.from', 'MDC', 'From'),
                         labelAlign: 'top',
-                        labelStyle: 'font-weight: normal',
-                        dateConfig: {
-                            format: Uni.util.Preferences.lookup(Uni.DateTime.dateShortKey, Uni.DateTime.dateShortDefault)
-                        }
+                        labelStyle: 'font-weight: normal'
                     }
                 ]
             },
