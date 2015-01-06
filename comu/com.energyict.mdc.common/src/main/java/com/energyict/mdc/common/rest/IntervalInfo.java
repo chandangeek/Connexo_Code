96626cf4b4a2e78c1c3f2bc0514dc4fc99289cc8
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-01-06 10:14:10 +0100

    MDC hackathon: replace java.util.Date and Interval with java.time.Instant and google.Range

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/IntervalInfo.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/IntervalInfo.java
@@ -1,7 +1,9 @@
 package com.energyict.mdc.common.rest;
 
-import com.elster.jupiter.util.time.Interval;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Range;
+
+import java.time.Instant;
 
 /**
  * Created by bvn on 8/6/14.
@@ -15,10 +17,29 @@ public class IntervalInfo {
     public IntervalInfo() {
     }
 
-    public static IntervalInfo from(Interval interval) {
+    public static IntervalInfo from(Range<Instant> range) {
         IntervalInfo info = new IntervalInfo();
-        info.start = interval.getStart().toEpochMilli();
-        info.end = interval.getEnd().toEpochMilli();
+        info.start = rangeStart(range);
+        info.end = rangeEnd(range);
         return info;
     }
+
+    private static Long rangeStart(Range<Instant> range) {
+        if (range.hasLowerBound()) {
+            return range.lowerEndpoint().toEpochMilli();
+        }
+        else {
+            return null;
+        }
+    }
+
+    private static Long rangeEnd(Range<Instant> range) {
+        if (range.hasUpperBound()) {
+            return range.upperEndpoint().toEpochMilli();
+        }
+        else {
+            return null;
+        }
+    }
+
 }
