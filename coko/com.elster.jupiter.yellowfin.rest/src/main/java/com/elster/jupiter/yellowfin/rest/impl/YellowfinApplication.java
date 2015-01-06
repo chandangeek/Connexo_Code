46a48805662a8d40baadff3e5d86ae2295df524f
David Nitescu <david.nitescu@elster.com>
2015-01-06 16:31:45 +0200

    yellowfin rest updated

--- coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinApplication.java
+++ coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinApplication.java
@@ -2,7 +2,10 @@ package com.elster.jupiter.yellowfin.rest.impl;
 
 import com.elster.jupiter.license.License;
 import com.elster.jupiter.rest.util.BinderProvider;
+import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.yellowfin.YellowfinService;
+import com.elster.jupiter.yellowfin.groups.YellowfinGroupsService;
+import com.energyict.mdc.device.data.DeviceService;
 import com.google.common.collect.ImmutableSet;
 import org.glassfish.hk2.utilities.Binder;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
@@ -20,7 +23,11 @@ public class YellowfinApplication extends Application implements BinderProvider{
 
     private final Set<Class<?>> classes = new HashSet<>();
 
+    private volatile DeviceService deviceService;
+    private volatile TransactionService transactionService;
+
     private volatile YellowfinService yellowfinService;
+    private volatile YellowfinGroupsService yellowfinGroupsService;
     private volatile License license;
 
     public YellowfinApplication() {
@@ -28,13 +35,28 @@ public class YellowfinApplication extends Application implements BinderProvider{
 
     @Override
     public Set<Class<?>> getClasses() {
+        return ImmutableSet.of(YellowfinResource.class, YellowfinDeviceGroupsResource.class, YellowfinReportInfoResource.class);
+    }
 
-        return ImmutableSet.of(YellowfinResource.class, YellowfinDeviceGroupsResource.class);
+
+    @Reference
+    public void setDeviceService(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @Reference
+    public void setTransactionService(TransactionService transactionService) {
+        this.transactionService = transactionService;
     }
 
     @Reference
     public void setYellowfinService(YellowfinService yellowfinService) {
         this.yellowfinService = yellowfinService;
+    }
+
+    @Reference
+    public void setYellowfinGroupsService(YellowfinGroupsService yellowfinGroupsService) {
+        this.yellowfinGroupsService = yellowfinGroupsService;
     }
 
     @Reference(target="(com.elster.jupiter.license.rest.key=" + APP_KEY  + ")")
@@ -48,6 +70,9 @@ public class YellowfinApplication extends Application implements BinderProvider{
             @Override
             protected void configure() {
                 bind(yellowfinService).to(YellowfinService.class);
+                bind(yellowfinGroupsService).to(YellowfinGroupsService.class);
+                bind(deviceService).to(DeviceService.class);
+                bind(transactionService).to(TransactionService.class);
             }
         };
     }
