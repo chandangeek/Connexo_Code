66fe08d41004f400a6097394bb8b62f45b5242bf
Bruno Ballekens <bruno.ballekens@elster.com>
2015-01-07 12:11:53 +0100

    Fix cyclic dependency with http whiteboard

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/SysAppService.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/SysAppService.java
@@ -1,11 +1,8 @@
 package com.elster.jupiter.system.app;
 
-import java.util.List;
-
 public interface SysAppService {
 
     String COMPONENTNAME = "SYSAPP";
     String APPLICATION_KEY = "SYS";
 
-    List<String> getAvailablePrivileges();
 }
