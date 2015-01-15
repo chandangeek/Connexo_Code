11fe5cdfd4a0fcc4b7e21ccf0d76fbe61cfeca34
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-01-15 17:04:56 +0100

    JP-7561

--- comu/com.energyict.mdc.device.config.rest/src/test/java/com/energyict/mdc/device/configuration/rest/impl/ProtocolPropertiesResourceTest.java
+++ comu/com.energyict.mdc.device.config.rest/src/test/java/com/energyict/mdc/device/configuration/rest/impl/ProtocolPropertiesResourceTest.java
@@ -11,6 +11,8 @@ import com.energyict.mdc.device.config.DeviceProtocolConfigurationProperties;
 import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.configuration.rest.ProtocolInfo;
 import com.energyict.mdc.pluggable.rest.impl.properties.SimplePropertyType;
+import com.energyict.mdc.protocol.api.DeviceProtocolProperty;
+
 import com.jayway.jsonpath.JsonModel;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,7 +36,7 @@ public class ProtocolPropertiesResourceTest extends BaseLoadProfileTest {
         String response = target("/devicetypes/11/deviceconfigurations/12/protocols/1").request().get(String.class);
         JsonModel jsonModel = JsonModel.create(response);
         assertThat(jsonModel.<Integer>get("$.id")).isEqualTo(7);
-        assertThat(jsonModel.<String>get("$.properties[0].key")).isEqualTo("callHomeId");
+        assertThat(jsonModel.<String>get("$.properties[0].key")).isEqualTo(DeviceProtocolProperty.callHomeId.name());
         assertThat(jsonModel.<String>get("$.properties[0].propertyValueInfo.value")).isEqualTo("0x7");
     }
 
@@ -43,7 +45,7 @@ public class ProtocolPropertiesResourceTest extends BaseLoadProfileTest {
         DeviceProtocolConfigurationProperties properties = mock(DeviceProtocolConfigurationProperties.class);
         DeviceConfiguration deviceConfiguration = mockDeviceConfiguration(properties);
         PropertyInfo propertyInfo = new PropertyInfo();
-        propertyInfo.key="callHomeId";
+        propertyInfo.key=DeviceProtocolProperty.callHomeId.name();
         propertyInfo.propertyValueInfo = new PropertyValueInfo<>("0x99", null, null, true);
         propertyInfo.propertyTypeInfo = new PropertyTypeInfo();
         propertyInfo.propertyTypeInfo.simplePropertyType= SimplePropertyType.TEXT;
@@ -53,17 +55,17 @@ public class ProtocolPropertiesResourceTest extends BaseLoadProfileTest {
 
         Response response = target("/devicetypes/11/deviceconfigurations/12/protocols/7").request().put(Entity.json(protocolInfo));
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-        verify(properties).setProperty("callHomeId", "0x99");
+        verify(properties).setProperty(DeviceProtocolProperty.callHomeId.name(), "0x99");
         verify(deviceConfiguration).save();
     }
 
     private DeviceConfiguration mockDeviceConfiguration(DeviceProtocolConfigurationProperties properties) {
-        BasicPropertySpec propertySpec = new BasicPropertySpec("callHomeId", new StringFactory());
+        BasicPropertySpec propertySpec = new BasicPropertySpec(DeviceProtocolProperty.callHomeId.name(), new StringFactory());
         DeviceType deviceType = mockDeviceType("device", 11, Arrays.asList(propertySpec));
         DeviceConfiguration deviceConfiguration = mockDeviceConfiguration("config", 12);
         when(properties.getDeviceConfiguration()).thenReturn(deviceConfiguration);
         TypedProperties typedProperties = TypedProperties.empty();
-        typedProperties.setProperty("callHomeId", "0x7");
+        typedProperties.setProperty(DeviceProtocolProperty.callHomeId.name(), "0x7");
         when(properties.getTypedProperties()).thenReturn(typedProperties);
         when(properties.getPropertySpecs()).thenReturn(Arrays.asList(propertySpec));
         when(deviceConfiguration.getDeviceProtocolProperties()).thenReturn(properties);
