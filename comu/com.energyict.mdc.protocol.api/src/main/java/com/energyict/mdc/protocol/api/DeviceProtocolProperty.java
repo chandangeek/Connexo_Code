63a0d0ba61335678b309aebd1a159579c2b525eb
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-01-15 17:04:57 +0100

    JP-7561

---
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/DeviceProtocolProperty.java
@@ -0,0 +1,18 @@
+package com.energyict.mdc.protocol.api;
+
+/**
+ * Provides the names of commonly used properties
+ * of {@link DeviceProtocol}s.
+ * Note that those properties could be general,
+ * related to a protocol dialect or related to
+ * the connection that is established
+ * to talk to the actual Device.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-01-15 (16:23)
+ */
+public enum DeviceProtocolProperty {
+
+    callHomeId, deviceTimeZone, phoneNumber;
+
+}
