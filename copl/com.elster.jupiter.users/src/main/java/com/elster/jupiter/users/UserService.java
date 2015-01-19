ec5d1a705af57e25d6de6ad1ee2431c0d58bf166
adrianlupan <adrianlupan@elster.com>
2015-01-19 15:41:03 +0200

    [JP-8062] Batch executor user has not all privileges, so execution of some tasks will fail

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/UserService.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/UserService.java
@@ -11,6 +11,8 @@ public interface UserService {
 
     String DEFAULT_ADMIN_ROLE = "Administrators";
     String DEFAULT_ADMIN_ROLE_DESCRIPTION = "Administrative privileges";
+    String BATCH_EXECUTOR_ROLE = "Batch executors";
+    String BATCH_EXECUTOR_ROLE_DESCRIPTION = "Batch executors privileges";
 
     User createUser(String name, String description);
 
