ceb7cd5c1811d03a362b59ce001636572411452b
Bruno Ballekens <bruno.ballekens@elster.com>
2015-01-20 10:49:05 +0100

    Made sure that all InstallServices are NOT license dependant

--- comu/com.energyict.mdc.issue.datacollection.rest/src/main/java/com/energyict/mdc/issue/datacollection/rest/IssueDataCollectionApplication.java
+++ comu/com.energyict.mdc.issue.datacollection.rest/src/main/java/com/energyict/mdc/issue/datacollection/rest/IssueDataCollectionApplication.java
@@ -10,37 +10,31 @@ import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.nls.TranslationKey;
 import com.elster.jupiter.nls.TranslationKeyProvider;
-import com.elster.jupiter.orm.callback.InstallService;
-import com.elster.jupiter.rest.util.BinderProvider;
 import com.elster.jupiter.rest.util.ConstraintViolationExceptionMapper;
 import com.elster.jupiter.rest.util.ConstraintViolationInfo;
 import com.elster.jupiter.rest.util.LocalizedExceptionMapper;
 import com.elster.jupiter.rest.util.RestQueryService;
 import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.users.UserService;
-import com.energyict.mdc.device.data.ConnectionTaskService;
 import com.energyict.mdc.device.data.DeviceService;
 import com.energyict.mdc.issue.datacollection.IssueDataCollectionService;
 import com.energyict.mdc.issue.datacollection.rest.i18n.MessageSeeds;
 import com.energyict.mdc.issue.datacollection.rest.resource.IssueResource;
 import com.energyict.mdc.issue.datacollection.rest.response.DataCollectionIssueInfoFactory;
 import com.google.common.collect.ImmutableSet;
-
-import org.glassfish.hk2.utilities.Binder;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.ws.rs.core.Application;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Component(name = "com.energyict.mdc.issue.datacollection.rest", service = {Application.class, InstallService.class, TranslationKeyProvider.class}, immediate = true, property = {"alias=/idc", "app=MDC", "name=" + IssueDataCollectionApplication.ISSUE_DATACOLLECTION_REST_COMPONENT})
-public class IssueDataCollectionApplication extends Application implements InstallService, TranslationKeyProvider {
+@Component(name = "com.energyict.mdc.issue.datacollection.rest", service = {Application.class, TranslationKeyProvider.class}, immediate = true, property = {"alias=/idc", "app=MDC", "name=" + IssueDataCollectionApplication.ISSUE_DATACOLLECTION_REST_COMPONENT})
+public class IssueDataCollectionApplication extends Application implements TranslationKeyProvider {
     public static final String APP_KEY = "MDC";
     public static final String ISSUE_DATACOLLECTION_REST_COMPONENT = "IDR";
 
@@ -105,25 +99,15 @@ public class IssueDataCollectionApplication extends Application implements Insta
         this.nlsService = nlsService;
         this.thesaurus = nlsService.getThesaurus(IssueDataCollectionService.COMPONENT_NAME, Layer.REST);
     }
-    
+
     @Reference
     public void setDeviceService(DeviceService deviceService) {
         this.deviceService = deviceService;
     }
 
-    @Reference(target="(com.elster.jupiter.license.rest.key=" + APP_KEY  + ")")
+    @Reference(target = "(com.elster.jupiter.license.rest.key=" + APP_KEY + ")")
     public void setLicense(License license) {
         this.license = license;
-    }
-
-    @Override
-    public void install() {
-        // do nothing
-    }
-
-    @Override
-    public List<String> getPrerequisiteModules() {
-        return Arrays.asList("NLS", "IDC", "ISR");
     }
 
     @Override
@@ -140,7 +124,7 @@ public class IssueDataCollectionApplication extends Application implements Insta
     public List<TranslationKey> getKeys() {
         return Arrays.asList(MessageSeeds.values());
     }
-    
+
     @Override
     public Set<Object> getSingletons() {
         Set<Object> hashSet = new HashSet<>();
@@ -148,7 +132,7 @@ public class IssueDataCollectionApplication extends Application implements Insta
         hashSet.add(new HK2Binder());
         return Collections.unmodifiableSet(hashSet);
     }
-    
+
     class HK2Binder extends AbstractBinder {
 
         @Override
