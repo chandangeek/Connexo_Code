0f14c8fed9ffec7dff651c350e9403cadfa39d07
Lobanov Dmitry <dlobanov@mera.ru>
2015-01-21 14:37:32 +0300

    JP-7911

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/ComScheduleInfo.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/ComScheduleInfo.java
@@ -1,17 +1,20 @@
 package com.energyict.mdc.scheduling.rest.impl;
 
+import java.time.Instant;
+import java.util.List;
+
 import com.energyict.mdc.scheduling.model.ComSchedule;
 import com.energyict.mdc.scheduling.rest.ComTaskInfo;
 import com.energyict.mdc.scheduling.rest.TemporalExpressionInfo;
-
-import java.time.Instant;
-import java.util.List;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.NullifyingDeserializer;
 
 public class ComScheduleInfo {
 
     public long id;
     public String name;
     public TemporalExpressionInfo temporalExpression;
+    @JsonDeserialize(using = NullifyingDeserializer.class)
     public Instant plannedDate;
     public boolean isInUse;
     public List<ComTaskInfo> comTaskUsages;
@@ -27,7 +30,7 @@ public class ComScheduleInfo {
         comScheduleInfo.name = comSchedule.getName();
         comScheduleInfo.temporalExpression = TemporalExpressionInfo.from(comSchedule.getTemporalExpression());
         comScheduleInfo.plannedDate = comSchedule.getPlannedDate().orElse(null);
-        comScheduleInfo.startDate = comSchedule.getStartDate()==null?null: comSchedule.getStartDate();
+        comScheduleInfo.startDate = comSchedule.getStartDate() == null ? null : comSchedule.getStartDate();
         comScheduleInfo.isInUse = inUse;
         comScheduleInfo.comTaskUsages = ComTaskInfo.from(comSchedule.getComTasks());
         comScheduleInfo.mRID = comSchedule.getmRID();
