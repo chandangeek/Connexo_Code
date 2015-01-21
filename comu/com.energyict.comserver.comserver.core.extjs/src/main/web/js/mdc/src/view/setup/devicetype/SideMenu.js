f3759774c00372cfbf442f6f51c41ce8f159567f
akuryuk <akuryuk@mera.ru>
2015-01-21 14:20:35 +0300

    [JP-8126] Set order of left menu items on device type

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicetype/SideMenu.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicetype/SideMenu.js
@@ -10,24 +10,34 @@ Ext.define('Mdc.view.setup.devicetype.SideMenu', {
                 href: '#/administration/devicetypes/' + this.deviceTypeId
             },
             {
-                text: Uni.I18n.translate('devicetypemenu.registerTypes', 'MDC', 'Register types'),
-                itemId: 'registerConfigsLink',
-                href: '#/administration/devicetypes/' + this.deviceTypeId + '/registertypes'
+                title: Uni.I18n.translate('devicetypemenu.datasources', 'MDC', 'Data sources'),
+                items: [
+                    {
+                        text: Uni.I18n.translate('devicetypemenu.loadProfiles', 'MDC', 'Load profiles'),
+                        itemId: 'loadProfilesLink',
+                        href: '#/administration/devicetypes/' + this.deviceTypeId + '/loadprofiles'
+                    },
+                    {
+                        text: Uni.I18n.translate('devicetype.sidemenu.logbookType', 'MDC', 'Logbook types'),
+                        itemId: 'logbooksLink',
+                        href: '#/administration/devicetypes/' + this.deviceTypeId + '/logbooktypes'
+                    },
+                    {
+                        text: Uni.I18n.translate('devicetypemenu.registerTypes', 'MDC', 'Register types'),
+                        itemId: 'registerConfigsLink',
+                        href: '#/administration/devicetypes/' + this.deviceTypeId + '/registertypes'
+                    }
+                ]
             },
             {
-                text: Uni.I18n.translate('devicetypemenu.loadProfiles', 'MDC', 'Load profiles'),
-                itemId: 'loadProfilesLink',
-                href: '#/administration/devicetypes/' + this.deviceTypeId + '/loadprofiles'
-            },
-            {
-                text: Uni.I18n.translate('devicetype.sidemenu.logbookType', 'MDC', 'Logbook types'),
-                itemId: 'logbooksLink',
-                href: '#/administration/devicetypes/' + this.deviceTypeId + '/logbooktypes'
-            },
-            {
-                text: Uni.I18n.translate('devicetypemenu.configurations', 'MDC', 'Configurations'),
-                itemId: 'configurationsLink',
-                href: '#/administration/devicetypes/' + this.deviceTypeId + '/deviceconfigurations'
+                title: Uni.I18n.translate('devicetypemenu.configurations', 'MDC', 'Configurations'),
+                items: [
+                    {
+                        text: Uni.I18n.translate('devicetypemenu.configurations', 'MDC', 'Configurations'),
+                        itemId: 'configurationsLink',
+                        href: '#/administration/devicetypes/' + this.deviceTypeId + '/deviceconfigurations'
+                    }
+                ]
             }
         ];
         this.callParent(this);
