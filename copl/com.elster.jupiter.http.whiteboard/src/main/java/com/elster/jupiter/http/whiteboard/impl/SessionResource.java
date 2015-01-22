0614aaf4cd87096691f3f1f96f1160a6629a15d5
Lucian Timar <lucian.timar@elster.com>
2015-01-22 12:38:10 +0200

    JP7859 - Session expires in Connexo while working on the Yellowfin designer

--- copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/impl/SessionResource.java
+++ copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/impl/SessionResource.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/session")
@@ -16,6 +17,13 @@ public class SessionResource {
     @Produces(MediaType.APPLICATION_JSON)
     public SessionTimeOutInfo getSessionTimeOut() {
         return new SessionTimeOutInfo(whiteBoard.getSessionTimeout());
+    }
+
+    @GET
+    @Path("/timeoutp")
+    @Produces("application/x-javascript")
+    public String getSessionTimeOutP(@QueryParam("callback") String callback) {
+        return String.format("%s({sessionTimeout:%d})",callback, whiteBoard.getSessionTimeout());
     }
 
 }
