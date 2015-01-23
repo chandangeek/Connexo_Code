78787fbae9ea9f77073446bc6a5662d76c3d1d94
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-01-23 13:17:23 +0100

    Fix usage of EventServiceImpl outside of the owning bundle.

--- comu/com.energyict.mdc.tasks/src/test/java/com/energyict/mdc/tasks/MockModule.java
+++ comu/com.energyict.mdc.tasks/src/test/java/com/energyict/mdc/tasks/MockModule.java
@@ -1,8 +1,6 @@
 package com.energyict.mdc.tasks;
 
-import com.elster.jupiter.events.EventService;
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
 
@@ -23,7 +21,6 @@ public class MockModule extends AbstractModule {
     protected void configure() {
         bind(BundleContext.class).toInstance(bundleContext);
         bind(EventAdmin.class).toInstance(eventAdmin);
-        bind(EventService.class).to(SpyEventService.class).in(Scopes.SINGLETON);
     }
 
 }
