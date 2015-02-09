0399fcac06e9f723f5c9296d43fcdd8c5c7850f6
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-02-09 13:49:08 +0100

    JP-4548: Fixed support for regex searching

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/StandardParametersBean.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/StandardParametersBean.java
@@ -1,102 +1,28 @@
 package com.energyict.mdc.device.data.rest.impl;
 
-import javax.ws.rs.WebApplicationException;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class StandardParametersBean {
 
     private UriInfo uriInfo;
-    private boolean wasRegExp;
-    private boolean wasMultiValued;
 
     public StandardParametersBean(@Context UriInfo uriInfo) {
         this.uriInfo = uriInfo;
     }
 
-    public List<String> get(String key) {
-        return getQueryParameters().get(key);
-    }
-
-    /**
-     * Checks that the last call to {@link #getFirst(String)}
-     * returned a value that is actually a regular expression.
-     *
-     * @return A flag that indicates if the last value returned by getFirst was a regular expression
-     */
-    public boolean wasRegExp() {
-        return wasRegExp;
-    }
-
-    /**
-     * Checks that the last call to {@link #getFirst(String)}
-     * returned a value that is actually a comman separated list of values.
-     *
-     * @return A flag that indicates if the last value returned by getFirst is a comma separated list of values
-     */
-    public boolean wasMultiValued() {
-        return wasMultiValued;
-    }
-
-    public boolean containsKey(String key) {
-        return getQueryParameters().containsKey(key);
-    }
-
     public String getFirst(String key){
-        wasRegExp = false;
-        wasMultiValued = false;
-        List<String> values = get(key);
+        List<String> values = getQueryParameters().get(key);
         if (values != null && !values.isEmpty()) {
-            String value = values.get(0);
-            return processedValue(value);
+            return values.get(0);
         }
         return null;
     }
 
-    private String processedValue(String value) {
-        if (value.contains("*")) {
-            value = value.replaceAll("\\*","%");
-            wasRegExp = true;
-        }
-        if (value.contains("?")) {
-            value = value.replaceAll("\\?","_");
-            wasRegExp = true;
-        }
-        if (value.contains("%")) {
-            wasRegExp = true;
-        }
-        if (value.contains(",")) {
-            wasMultiValued = true;
-        }
-        return value;
-    }
-
-    public List<Long> getLong(String key) {
-        List<String> original = get(key);
-        List<Long> resultList = new ArrayList<>();
-        if (original != null) {
-            for (String param : original){
-                try {
-                    resultList.add(Long.parseLong(param));
-                } catch (NumberFormatException ex) {
-                    throw new WebApplicationException(Response.Status.BAD_REQUEST);
-                }
-            }
-        }
-        return resultList;
-    }
-
     public MultivaluedMap<String, String> getQueryParameters(){
-        return getUriInfo().getQueryParameters();
-    }
-
-    public UriInfo getUriInfo() {
-        return uriInfo;
+        return uriInfo.getQueryParameters();
     }
 
 }
