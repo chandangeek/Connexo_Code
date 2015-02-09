552ba22c20911b95c47863febbf5e142a2e419b3
igh <isabelle.gheysens@elster.com>
2015-02-09 12:31:30 +0100

    JP-7841 - Application server: provide display names for the message services

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/Installer.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/Installer.java
@@ -1,5 +1,12 @@
 package com.energyict.mdc.device.data.impl;
 
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsKey;
+import com.elster.jupiter.nls.SimpleNlsKey;
+import com.elster.jupiter.nls.SimpleTranslation;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.Translation;
+import com.energyict.mdc.device.data.DeviceDataServices;
 import com.energyict.mdc.device.data.impl.events.ComTaskEnablementConnectionMessageHandlerFactory;
 import com.energyict.mdc.device.data.impl.events.ComTaskEnablementPriorityMessageHandlerFactory;
 import com.energyict.mdc.device.data.impl.events.ComTaskEnablementStatusMessageHandlerFactory;
@@ -13,7 +20,10 @@ import com.elster.jupiter.messaging.QueueTableSpec;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.users.UserService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,6 +38,8 @@ public class Installer {
 
     public static final String COMSCHEDULE_RECALCULATOR_MESSAGING_NAME = "COMSCHED_RECALCULATOR";
     public static final String COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_NAME = "COMSCHED_BATCH_OBSOLETE";
+    public static final String COMSCHEDULE_RECALCULATOR_MESSAGING_DISPLAYNAME = "Recalculate communication schedules";
+    public static final String COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_DISPLAYNAME = "Handle obsolete communication schedules";
     private static final int DEFAULT_RETRY_DELAY_IN_SECONDS = 60;
     private static final int KPI_CALCULATOR_TASK_RETRY_DELAY = 60;
 
@@ -36,16 +48,19 @@ public class Installer {
     private final MessageService messageService;
     private final UserService userService;
     private final Logger logger = Logger.getLogger(Installer.class.getName());
+    private final Thesaurus thesaurus;
 
-    public Installer(DataModel dataModel, EventService eventService, MessageService messageService, UserService userService) {
+    public Installer(DataModel dataModel, EventService eventService, MessageService messageService, UserService userService, Thesaurus thesaurus) {
         super();
         this.dataModel = dataModel;
         this.eventService = eventService;
         this.messageService = messageService;
         this.userService = userService;
+        this.thesaurus = thesaurus;
     }
 
     public void install(boolean executeDdl) {
+        addTranslations();
         try {
             this.dataModel.install(executeDdl, true);
         } catch (Exception e) {
@@ -61,6 +76,12 @@ public class Installer {
         this.addJupiterEventSubscribers();
         this.createMasterData();
         this.createKpiCalculatorDestination();
+    }
+
+    private void addTranslations() {
+        addTranslation(DeviceDataServices.COMPONENT_NAME, DataCollectionKpiCalculatorHandlerFactory.TASK_SUBSCRIBER, DataCollectionKpiCalculatorHandlerFactory.TASK_SUBSCRIBER_DISPLAYNAME);
+        addTranslation(DeviceDataServices.COMPONENT_NAME, COMSCHEDULE_RECALCULATOR_MESSAGING_NAME, COMSCHEDULE_RECALCULATOR_MESSAGING_DISPLAYNAME);
+        addTranslation(DeviceDataServices.COMPONENT_NAME, COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_NAME, COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_DISPLAYNAME);
     }
 
     private void addJupiterEventSubscribers() {
@@ -85,6 +106,14 @@ public class Installer {
                                 KPI_CALCULATOR_TASK_RETRY_DELAY);
         destination.activate();
         destination.subscribe(DataCollectionKpiCalculatorHandlerFactory.TASK_SUBSCRIBER);
+    }
+
+    private void addTranslation(String componentName, String subscriberName, String subscriberDisplayName) {
+        NlsKey statusKey = SimpleNlsKey.key(componentName, Layer.DOMAIN, subscriberName);
+        Translation statusTranslation = SimpleTranslation.translation(statusKey, Locale.ENGLISH, subscriberDisplayName);
+        List<Translation> translations = new ArrayList<>();
+        translations.add(statusTranslation);
+        thesaurus.addTranslations(translations);
     }
 
     private void createPrivileges() {
