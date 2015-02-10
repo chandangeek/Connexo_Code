ca620374c6e6e0b31bcaa29ad0500d412ae96128
Artem Puchenkin <apuchenkin@gmail.com>
2015-02-10 15:42:35 +0300

    [JP-8424] Broken layout on communication protocols page

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/property/view/property/Period.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/property/view/property/Period.js
@@ -85,10 +85,13 @@ Ext.define('Uni.property.view.property.Period', {
     updateResetButton: function () {
         var me = this,
             button = me.getResetButton(),
-            countValue = me.getField().getValue(),
-            timeUnitValue = me.getComboField().getValue();
+            countValue,
+            timeUnitValue;
 
         if (me.isEdit) {
+            countValue = me.getField().getValue();
+            timeUnitValue = me.getComboField().getValue();
+
             if (!me.getProperty().get('isInheritedOrDefaultValue')
                 && typeof countValue !== 'undefined' && countValue !== null
                 && typeof timeUnitValue !== 'undefined' && timeUnitValue !== null
