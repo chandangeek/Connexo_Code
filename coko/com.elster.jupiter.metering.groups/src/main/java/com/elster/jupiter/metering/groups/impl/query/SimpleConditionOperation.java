4e446a3bfeb7918e16f47aba9c9cb5f908fc11b2
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-02-12 16:29:52 +0100

    JP-7660: added decompilation of LIKE regex. Failed to add test coverage

--- coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/impl/query/SimpleConditionOperation.java
+++ coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/impl/query/SimpleConditionOperation.java
@@ -5,6 +5,7 @@ import com.elster.jupiter.util.conditions.Condition;
 import com.elster.jupiter.util.conditions.Operator;
 import com.google.common.collect.ImmutableSet;
 
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.Set;
@@ -58,6 +59,34 @@ public class SimpleConditionOperation extends ConditionOperation {
     }
 
     public Object[] getValues() {
-        return values;
+        if (operator.equals(Operator.EQUAL.getSymbol())) {
+            return values;
+        } if (operator.equals(Operator.LIKE.getSymbol())) {
+            return Arrays.asList(values).stream().map(v->fromOracleSql((String)v)).collect(Collectors.toList()).toArray();
+        } else {
+            throw new UnsupportedOperationException("Only EQUAL and LIKE operator are supported.");
+        }
     }
+
+    /**
+     * Translate the LIKE string value into our supported wildcard regex. This method inverses Where.toOracleSql()
+     */
+    private String fromOracleSql(String value) {
+        // escape sql our wildcards
+        for (String keyword: Arrays.asList("*", "?")) {
+            value=value.replace(keyword,"\\"+keyword);
+        }
+        // transform un-escaped LIKE operators % and _ to our wildcards
+        value=value.replaceAll("([^\\\\]|^)\\%", "$1*");
+        value=value.replaceAll("([^\\\\]|^)\\_", "$1?");
+
+        // transform escaped LIKE operators % and _ to their unescaped wildcard literal
+        // We need to search for double escape: it was doubled in the little loop on top of this method
+        value=value.replaceAll("\\\\\\\\\\%", "%");
+        value=value.replaceAll("\\\\\\\\\\_", "_");
+
+        return value;
+    }
+
+
 }
