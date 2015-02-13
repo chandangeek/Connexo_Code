ae0bb5d638dc084b71c61f554e30d97657666040
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-02-13 09:37:30 +0100

    JP-8535: Fixed paging in case no parameters are present

---
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/PagedInfoList.java
@@ -0,0 +1,96 @@
+package com.elster.jupiter.rest.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * generic helper class to json-serialize a list of info-objects into a json format that is understood by our ExtJS paging component.
+ */
+@JsonSerialize(using = PagedInfoList.Serializer.class)
+public class PagedInfoList {
+
+    private final String jsonListName;
+    private boolean couldHaveNextPage;
+    private List<?> infos = new ArrayList<>();
+    private QueryParameters queryParameters;
+    private Integer totalCount;
+
+    public int getTotal() {
+        if (totalCount != null) {
+            return totalCount;
+        }
+        int total = infos.size();
+        total+=queryParameters.getStart();
+        if (couldHaveNextPage) {
+            total++;
+        }
+        return total;
+    }
+
+    public List<?> getInfos() {
+        return ImmutableList.copyOf(infos);
+    }
+
+    private PagedInfoList(String jsonListName, List<?> infos, QueryParameters queryParameters, Integer totalCount) {
+        this.queryParameters = queryParameters;
+        this.jsonListName = jsonListName;
+        this.infos = infos;
+        couldHaveNextPage=queryParameters.getLimit()!=-1 && infos.size()>queryParameters.getLimit();
+        if (couldHaveNextPage) {
+            this.infos=infos.subList(0,queryParameters.getLimit());
+        }
+        this.totalCount = totalCount;
+    }
+
+    private PagedInfoList(String jsonListName, List<?> infos, QueryParameters queryParameters) {
+        this(jsonListName, infos, queryParameters, null);
+    }
+
+    /**
+     * Create a Json serialized object for paged search results.
+     * E.g.
+     *    ("deviceTypes", {deviceTypeInfo1, deviceTypeInfo2}, queryParameters}
+     *    with queryParameters,limit=2 (TWO)
+     *    returning 2 results when 2 were asked implicates a full page and the the field 'total' is increased by 1 to indicate there could be a next page.
+     *
+     * will end up serialized into the following JSON
+     *
+     *   {
+     *       "total":3,
+     *       "deviceTypes":[{"name":"...",...},{"name":"...",...}]
+     *   }
+     * @param jsonListName The name of the list property in JSON
+     * @param infos The search results to assign to the list property
+     * @param queryParameters The original query parameters used for building the list that is being returned. This is required as it is used to determine
+     *                        if the returned 'page' was full, if so, total is incremented by 1 to indicate to ExtJS there could be a next page.
+     * @return A map that will be correctly serialized as JSON paging object, understood by ExtJS
+     */
+    public static PagedInfoList asJson(String jsonListName, List<?> infos, QueryParameters queryParameters) {
+        return new PagedInfoList(jsonListName, infos, queryParameters);
+    }
+
+    public static PagedInfoList asJson(String jsonListName, List<?> infos, QueryParameters queryParameters, Integer totalCount) {
+        return new PagedInfoList(jsonListName, infos, queryParameters, totalCount);
+    }
+
+    public static class Serializer extends JsonSerializer<PagedInfoList> {
+
+        @Override
+        public void serialize(PagedInfoList value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeStartObject();
+            jgen.writeNumberField("total", value.getTotal());
+            jgen.writeArrayFieldStart(value.jsonListName);
+            for (Object info : value.infos) {
+                jgen.writeObject(info);
+            }
+            jgen.writeEndArray();
+            jgen.writeEndObject();
+        }
+    }
+}
---
+++ copl/com.elster.jupiter.rest.util/src/test/java/com/elster/jupiter/rest/util/PagedInfoListTest.java
@@ -0,0 +1,86 @@
+package com.elster.jupiter.rest.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.core.MultivaluedHashMap;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PagedInfoListTest {
+
+    @Test
+    public void testObjectMapperSerializesTypeInformation() throws Exception {
+        BogusInfo deviceTypeInfo1 = new BogusInfo();
+        deviceTypeInfo1.name = "new 1";
+        deviceTypeInfo1.canBeDirectlyAddressed = true;
+        deviceTypeInfo1.canBeGateway = false;
+        deviceTypeInfo1.deviceConfigurationCount = 4;
+        deviceTypeInfo1.loadProfileCount = 5;
+        deviceTypeInfo1.logBookCount = 6;
+        deviceTypeInfo1.registerCount = 7;
+        BogusInfo deviceTypeInfo2 = new BogusInfo();
+        deviceTypeInfo2.name = "new 2";
+        deviceTypeInfo2.canBeDirectlyAddressed = true;
+        deviceTypeInfo2.canBeGateway = false;
+        deviceTypeInfo2.deviceConfigurationCount = 4;
+        deviceTypeInfo2.loadProfileCount = 5;
+        deviceTypeInfo2.logBookCount = 6;
+        deviceTypeInfo2.registerCount = 7;
+        BogusInfo deviceTypeInfo3 = new BogusInfo();
+        deviceTypeInfo3.name = "new 3";
+        deviceTypeInfo3.canBeDirectlyAddressed = true;
+        deviceTypeInfo3.canBeGateway = false;
+        deviceTypeInfo3.deviceConfigurationCount = 4;
+        deviceTypeInfo3.loadProfileCount = 5;
+        deviceTypeInfo3.logBookCount = 6;
+        deviceTypeInfo3.registerCount = 7;
+        ObjectMapper objectMapper = new ObjectMapper();
+        QueryParameters queryParameters = createQueryParameters(0, 2);
+        String response = objectMapper.writeValueAsString(PagedInfoList.asJson("deviceTypes", Arrays.asList(deviceTypeInfo1, deviceTypeInfo2, deviceTypeInfo3), queryParameters));
+        assertThat(response).contains("\"deviceTypes\":[{");
+        assertThat(response).contains("\"total\":3");
+        assertThat(response).doesNotContain("\"new 3\"");
+    }
+
+    @Test
+    public void testHasNextPage() throws Exception {
+        QueryParameters queryParameters = createQueryParameters(80, 5);
+        List<Object> infos = Arrays.asList(new Object(), new Object(), new Object(), new Object(), new Object(), new Object()); // 6 objects
+
+        PagedInfoList list = PagedInfoList.asJson("list", infos, queryParameters);
+        assertThat(list.getTotal()).isEqualTo(86);
+        assertThat(list.getInfos()).hasSize(5);
+    }
+
+    @Test
+    public void testHasNoNextPage() throws Exception {
+        QueryParameters queryParameters = createQueryParameters(80, 5);
+        List<Object> infos = Arrays.asList(new Object(), new Object(), new Object(), new Object(), new Object()); // 5 objects
+
+        PagedInfoList list = PagedInfoList.asJson("list", infos, queryParameters);
+        assertThat(list.getTotal()).isEqualTo(85);
+        assertThat(list.getInfos()).hasSize(5);
+    }
+
+    @Test
+    public void testNextPage() throws Exception {
+        QueryParameters queryParameters = createQueryParameters(10,10);
+
+        List<Object> infos = Arrays.asList(new Object(), new Object(), new Object(), new Object(), new Object()
+                , new Object(), new Object(), new Object(), new Object(), new Object(), new Object()); // 11 objects
+
+        PagedInfoList list = PagedInfoList.asJson("list", infos, queryParameters);
+        assertThat(list.getTotal()).isEqualTo(21);
+        assertThat(list.getInfos()).hasSize(10);
+
+    }
+
+    private QueryParameters createQueryParameters(int start, int limit) {
+        MultivaluedHashMap<String, String> multivaluedHashMap = new MultivaluedHashMap<>();
+        multivaluedHashMap.put("start", Arrays.asList(""+start));
+        multivaluedHashMap.put("limit", Arrays.asList(""+limit));
+        return QueryParameters.wrap(multivaluedHashMap);
+    }
+}
