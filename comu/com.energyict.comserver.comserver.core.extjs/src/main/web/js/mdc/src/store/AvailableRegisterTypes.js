5939d756263fcfdebc1d5c8b7f7d794288ea12da
Levi Van Zele <Levi.VanZele@elster.com>
2015-02-13 14:30:17 +0100

    [JP-8508] Added infinite scrolling to the available register types page of device types.

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/store/AvailableRegisterTypes.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/store/AvailableRegisterTypes.js
@@ -3,13 +3,14 @@ Ext.define('Mdc.store.AvailableRegisterTypes', {
     requires: [
         'Mdc.model.RegisterType'
     ],
+
     model: 'Mdc.model.RegisterType',
     storeId: 'AvailableRegisterTypes',
+
+    buffered: true,
+
     proxy: {
         type: 'rest',
-        pageParam: undefined,
-        startParam: undefined,
-        limitParam: undefined,
         url: '../../api/dtc/devicetypes/{deviceType}/registertypes',
         reader: {
             type: 'json',
