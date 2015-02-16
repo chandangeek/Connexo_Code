70c9d5fd2ae2441d6892dee09f1f723b76e93bcd
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-02-16 12:03:58 +0100

    Added test

---
+++ comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/BigDecimalAdapterTest.java
@@ -0,0 +1,21 @@
+package com.energyict.mdc.device.data.rest.impl;
+
+import com.energyict.mdc.device.data.rest.BigDecimalAsStringAdapter;
+import java.math.BigDecimal;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by bvn on 2/16/15.
+ */
+public class BigDecimalAdapterTest {
+
+    @Test
+    public void testMarshal() throws Exception {
+        BigDecimalAsStringAdapter adapter = new BigDecimalAsStringAdapter();
+        assertThat(adapter.marshal(BigDecimal.valueOf(0.34343435))).isEqualTo("0.34343435");
+
+    }
+
+}
