d2fc2c451252569c3ad2b035c0902322244593a6
Levi Van Zele <Levi.VanZele@elster.com>
2015-02-24 15:33:18 +0100

    [JP-8688] Temporary test commit for production build.

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/grid/plugin/BufferedRenderer.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/grid/plugin/BufferedRenderer.js
@@ -1,6 +1,7 @@
 Ext.define('Uni.override.grid.plugin.BufferedRenderer', {
     override: 'Ext.grid.plugin.BufferedRenderer',
     rowHeight: 29, // comes from skyline theme
+
     init: function (grid) {
         this.callParent(arguments);
         grid.view.cls = 'uni-infinite-scrolling-grid-view';
@@ -9,38 +10,49 @@ Ext.define('Uni.override.grid.plugin.BufferedRenderer', {
             grid.view.refresh();
         });
     },
+
     bindStore: function (store) {
         var me = this;
-        me.trailingBufferZone = 0; // No idea why this needs to be 0, without it some grids don't work.
-        me.leadingBufferZone = store.pageSize;
-        me.callParent(arguments);
-    },
 
-    onViewScroll: function(e, t) {
-        var me = this,
-            store = me.store,
-            totalCount = (store.buffered ? store.getTotalCount() : store.getCount()),
-            vscrollDistance,
-            scrollDirection,
-            scrollTop = me.scrollTop = me.view.el.dom.scrollTop,
-            scrollHandled = false;
-        if (me.ignoreNextScrollEvent) {
-            me.ignoreNextScrollEvent = false;
-            return;
-        }
-        if (!(me.disabled || totalCount < me.viewSize)) {
-            vscrollDistance = scrollTop - me.position;
-            scrollDirection = vscrollDistance >= 0 ? 1 : -1;
-            if (Math.abs(vscrollDistance) >= 20 || (scrollDirection !== me.lastScrollDirection)) {
-                me.lastScrollDirection = scrollDirection;
-                me.handleViewScroll(me.lastScrollDirection);
-                scrollHandled = true;
-            }
-        }
-        if (!scrollHandled) {
-            if (me.lockingPartner && me.lockingPartner.scrollTop !== scrollTop) {
-                me.lockingPartner.view.el.dom.scrollTop = scrollTop;
-            }
-        }
+        // TODO Check below!
+        // TEMPORARY Disabling some things to get the workaround retested!
+
+        //me.trailingBufferZone = 0; // No idea why this needs to be 0, without it some grids don't work.
+        //me.leadingBufferZone = store.pageSize;
+
+        me.callParent(arguments);
     }
+
+    //,
+    //
+    //onViewScroll: function (e, t) {
+    //    var me = this,
+    //        store = me.store,
+    //        totalCount = (store.buffered ? store.getTotalCount() : store.getCount()),
+    //        vscrollDistance,
+    //        scrollDirection,
+    //        scrollTop = me.scrollTop = me.view.el.dom.scrollTop,
+    //        scrollHandled = false;
+    //
+    //    if (me.ignoreNextScrollEvent) {
+    //        me.ignoreNextScrollEvent = false;
+    //        return;
+    //    }
+    //
+    //    if (!(me.disabled || totalCount < me.viewSize)) {
+    //        vscrollDistance = scrollTop - me.position;
+    //        scrollDirection = vscrollDistance >= 0 ? 1 : -1;
+    //        if (Math.abs(vscrollDistance) >= 20 || (scrollDirection !== me.lastScrollDirection)) {
+    //            me.lastScrollDirection = scrollDirection;
+    //            me.handleViewScroll(me.lastScrollDirection);
+    //            scrollHandled = true;
+    //        }
+    //    }
+    //
+    //    if (!scrollHandled) {
+    //        if (me.lockingPartner && me.lockingPartner.scrollTop !== scrollTop) {
+    //            me.lockingPartner.view.el.dom.scrollTop = scrollTop;
+    //        }
+    //    }
+    //}
 });
