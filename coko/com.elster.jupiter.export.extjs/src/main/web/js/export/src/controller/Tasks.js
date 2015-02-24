0e510757e4f08b285820c4f493497767e34c5aec
Maksim Barinov <mbarinov@mera.ru>
2015-02-24 12:51:33 +0300

    Fix for JP-8682

--- coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/controller/Tasks.js
+++ coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/controller/Tasks.js
@@ -1005,6 +1005,7 @@ Ext.define('Dxp.controller.Tasks', {
 
         previewContainer = {
             xtype: 'preview-container',
+            selectByDefault: false,
             grid: {
                 itemId: 'addReadingTypesGrid',
                 xtype: 'AddReadingTypesToTaskBulk',
