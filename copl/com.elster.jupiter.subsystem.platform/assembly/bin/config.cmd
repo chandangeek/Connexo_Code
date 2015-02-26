c93d9088134c774bfce46a3c0d40f6d7bbac6699
kbe <kristof.boeykens@elster.com>
2015-02-26 15:32:29 +0100

    Update installation scripts for Connexo/Facts/Flow

---
+++ copl/com.elster.jupiter.subsystem.platform/assembly/bin/config.cmd
@@ -0,0 +1,21 @@
+set CONNEXO_HTTP_PORT=8080
+set TOMCAT_HTTP_PORT=8081
+
+set jdbcUrl=jdbc:oracle:thin:@doraps003.eict.vpdc:7163:ACCJUPITER
+set dbUserName=ACCJUPITER_kbe
+set dbPassword=zorro
+set CONNEXO_SERVICE=yes
+
+set FACTS_DB_HOST=doraps003.eict.vpdc
+set FACTS_DB_PORT=7163
+set FACTS_DB_NAME=ACCJUPITER
+set FACTS_DBUSER=ACCJUPITER_kbefacts
+set FACTS_DBPASSWORD=zorro
+
+set FLOW_JDBC_URL=jdbc:oracle:thin:@doraps003.eict.vpdc:7163:ACCJUPITER
+set FLOW_DB_USER=ACCJUPITER_kbeflow
+set FLOW_DB_PASSWORD=zorro
+set SMTP_HOST=localhost
+set SMTP_PORT=25
+set SMTP_USER=
+set SMTP_PASSWORD=
