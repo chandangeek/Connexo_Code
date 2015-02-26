396940556411fa17c426f282f3ea04b5a44a62af
Levi Van Zele <Levi.VanZele@elster.com>
2015-02-26 10:20:26 +0100

    [JP-8688] Removed the commented out code after testing it works.

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/grid/plugin/BufferedRenderer.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/grid/plugin/BufferedRenderer.js
@@ -9,50 +9,5 @@ Ext.define('Uni.override.grid.plugin.BufferedRenderer', {
         grid.on('boxready', function () {
             grid.view.refresh();
         });
-    },
-
-    bindStore: function (store) {
-        var me = this;
-
-        // TODO Check below!
-        // TEMPORARY Disabling some things to get the workaround retested!
-
-        //me.trailingBufferZone = 0; // No idea why this needs to be 0, without it some grids don't work.
-        //me.leadingBufferZone = store.pageSize;
-
-        me.callParent(arguments);
     }
-
-    //,
-    //
-    //onViewScroll: function (e, t) {
-    //    var me = this,
-    //        store = me.store,
-    //        totalCount = (store.buffered ? store.getTotalCount() : store.getCount()),
-    //        vscrollDistance,
-    //        scrollDirection,
-    //        scrollTop = me.scrollTop = me.view.el.dom.scrollTop,
-    //        scrollHandled = false;
-    //
-    //    if (me.ignoreNextScrollEvent) {
-    //        me.ignoreNextScrollEvent = false;
-    //        return;
-    //    }
-    //
-    //    if (!(me.disabled || totalCount < me.viewSize)) {
-    //        vscrollDistance = scrollTop - me.position;
-    //        scrollDirection = vscrollDistance >= 0 ? 1 : -1;
-    //        if (Math.abs(vscrollDistance) >= 20 || (scrollDirection !== me.lastScrollDirection)) {
-    //            me.lastScrollDirection = scrollDirection;
-    //            me.handleViewScroll(me.lastScrollDirection);
-    //            scrollHandled = true;
-    //        }
-    //    }
-    //
-    //    if (!scrollHandled) {
-    //        if (me.lockingPartner && me.lockingPartner.scrollTop !== scrollTop) {
-    //            me.lockingPartner.view.el.dom.scrollTop = scrollTop;
-    //        }
-    //    }
-    //}
 });
