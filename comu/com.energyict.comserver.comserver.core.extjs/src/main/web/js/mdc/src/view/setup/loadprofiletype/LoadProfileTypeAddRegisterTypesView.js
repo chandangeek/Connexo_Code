b388ee0f91b4865997cf8fb259c64a35f6042bff
Vitaliy Nishukov <nishukov@mera.ru>
2015-02-27 15:30:34 +0300

    JP-8695 FIX

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/loadprofiletype/LoadProfileTypeAddRegisterTypesView.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/loadprofiletype/LoadProfileTypeAddRegisterTypesView.js
@@ -2,26 +2,19 @@ Ext.define('Mdc.view.setup.loadprofiletype.LoadProfileTypeAddRegisterTypesView',
     extend: 'Uni.view.container.ContentContainer',
     alias: 'widget.loadProfileTypeAddRegisterTypesView',
     itemId: 'loadProfileTypeAddRegisterTypesView',
+    title: Uni.I18n.translate('setup.loadprofiletype.LoadProfileTypeAddRegisterTypesView.title', 'MDC', 'Add register types'),
+    ui: 'large',
 
     content: [
         {
             xtype: 'panel',
-            ui: 'large',
-            title: Uni.I18n.translate('setup.loadprofiletype.LoadProfileTypeAddRegisterTypesView.title', 'MDC', 'Add register types'),
             layout: {
                 type: 'vbox',
                 align: 'stretch'
             },
-            items: [
-                {
-                    xtype: 'loadProfileTypeAddRegisterTypesGrid'
-                }
-            ]
+            items: {
+                xtype: 'loadProfileTypeAddRegisterTypesGrid'
+            }
         }
-    ],
-
-    initComponent: function () {
-        this.callParent(arguments);
-    }
+    ]
 });
-
