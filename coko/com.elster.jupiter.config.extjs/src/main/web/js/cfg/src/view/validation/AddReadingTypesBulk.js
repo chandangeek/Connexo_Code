18bc2662fbfd7c85e9054ad4517aecc203ac401f
Levi Van Zele <Levi.VanZele@elster.com>
2015-02-27 11:59:04 +0100

    Removing comments and making sure the first row is not selected whenever the grid is loaded for the first time for adding register types to a device type.

--- coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/src/view/validation/AddReadingTypesBulk.js
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/src/view/validation/AddReadingTypesBulk.js
@@ -22,52 +22,11 @@ Ext.define('Cfg.view.validation.AddReadingTypesBulk', {
 
     bottomToolbarHidden: true,
 
-    //allLabel: Uni.I18n.translate('validation.allReadingTypes', 'CFG', 'All reading types'),
-    //allDescription: Uni.I18n.translate(
-    //    'general.selectAllItemsRelatedToAppliedFilter',
-    //    'MDC',
-    //    'Select all items related to applied filters'
-    //),
-
-    //selectedLabel: Uni.I18n.translate('validation.selectedReadingTypes', 'CFG', 'Selected reading types'),
-    //selectedDescription: Uni.I18n.translate(
-    //    'general.selectItemsInTable',
-    //    'MDC',
-    //    'Select items in table'
-    //),
-
     columns: [
         {
             xtype: 'reading-type-column',
             dataIndex: 'readingType',
             flex: 1
         }
-    ]//,
-
-    /*onBulkSelectionChange: function () {
-        var me = this,
-            selection = me.view.getSelectionModel().getSelection();
-
-        me.up('#addReadingTypesToRuleSetup').down('#buttonsContainer button[name=add]').setDisabled(!me.isAllSelected() && selection.length === 0);
-    }//,
-
-    /*onChangeSelectionGroupType: function (radiogroup, value) {
-        var me = this;
-        if (me.getView()) {
-            var selection = me.getView().getSelectionModel().getSelection();
-
-            me.up('#addReadingTypesToRuleSetup').down('#buttonsContainer button[name=add]').setDisabled(!me.isAllSelected() && selection.length === 0);
-            if (!me.isAllSelected()) {
-                me.setGridVisible(true);
-                me.getView().getSelectionModel().deselectAll();
-                me.getView().getSelectionModel().select(0);
-            } else {
-                me.setGridVisible(false);
-            }
-
-
-        }
-    }*/
-
-
+    ]
 });
