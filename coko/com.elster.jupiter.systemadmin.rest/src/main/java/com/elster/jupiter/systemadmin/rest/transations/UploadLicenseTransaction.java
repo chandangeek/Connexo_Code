9e591368523dd19ec43ed0c0a410b05d3885ca21
borunova <borunova@mera.ru>
2015-02-28 23:13:02 +0300

    remove unused imports

--- coko/com.elster.jupiter.systemadmin.rest/src/main/java/com/elster/jupiter/systemadmin/rest/transations/UploadLicenseTransaction.java
+++ coko/com.elster.jupiter.systemadmin.rest/src/main/java/com/elster/jupiter/systemadmin/rest/transations/UploadLicenseTransaction.java
@@ -1,21 +1,16 @@
 package com.elster.jupiter.systemadmin.rest.transations;
 
-import com.elster.jupiter.license.InvalidLicenseException;
 import com.elster.jupiter.license.LicenseService;
 import com.elster.jupiter.nls.Layer;
-import com.elster.jupiter.nls.LocalizedFieldValidationException;
 import com.elster.jupiter.nls.NlsService;
-import com.elster.jupiter.rest.util.ConstraintViolationInfo;
 import com.elster.jupiter.systemadmin.rest.resource.BaseResource;
 import com.elster.jupiter.systemadmin.rest.response.ActionInfo;
-import com.elster.jupiter.systemadmin.rest.response.RootEntity;
 import com.elster.jupiter.transaction.Transaction;
 import com.elster.jupiter.util.json.JsonService;
 
 import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 import java.security.SignedObject;
 import java.util.LinkedHashSet;
 import java.util.Set;
