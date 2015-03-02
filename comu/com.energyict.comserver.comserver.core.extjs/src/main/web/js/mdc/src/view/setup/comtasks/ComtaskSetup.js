a594246d4e03371ddbe5b654a8957d95fbce12cb
Vitaliy Nishukov <nishukov@mera.ru>
2015-03-02 13:36:34 +0300

    JP-8429 FIX

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/comtasks/ComtaskSetup.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/comtasks/ComtaskSetup.js
@@ -25,8 +25,7 @@ Ext.define('Mdc.view.setup.comtasks.ComtaskSetup', {
                         xtype: 'no-items-found-panel',
                         title: Uni.I18n.translate('comtask.empty.title', 'MDC', 'No communication tasks found'),
                         reasons: [
-                            Uni.I18n.translate('comtask.empty.list.item1', 'MDC', 'No communication tasks have been added yet.'),
-                            Uni.I18n.translate('comtask.empty.list.item2', 'MDC', 'No communication tasks comply to the filter.')
+                            Uni.I18n.translate('comtask.empty.list.item1', 'MDC', 'No communication tasks have been added yet.')
                         ],
                         stepItems: [
                             {
