6ac7a3a5cbfa545a304ad9909ec51dc9710da8e3
Maksim Barinov <mbarinov@mera.ru>
2015-03-03 13:58:59 +0300

    Fix for JP-8744

--- copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/config.rb
+++ copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/config.rb
@@ -1,2 +1,2 @@
-require '../../../../../../../com.elster.jupiter.extjs/src/main/web/js/ext/packages/ext-theme-base/sass/utils.rb'
-require '../sass/config.rb'
+require '..\..\..\..\..\..\..\com.elster.jupiter.extjs\src\main\web\js\ext\packages\ext-theme-base\sass\utils.rb'
+require '..\sass\config.rb'
