42f9f778ac9bf68362962867f2de88c0e798ba7c
Maksim Barinov <mbarinov@mera.ru>
2015-03-03 17:04:49 +0300

    Reverted fix for JP-8747

--- comu/com.energyict.mdc.issue.datacollection.extjs/src/main/web/js/idc/src/view/workspace/issues/bulk/IssuesSelectionGrid.js
+++ comu/com.energyict.mdc.issue.datacollection.extjs/src/main/web/js/idc/src/view/workspace/issues/bulk/IssuesSelectionGrid.js
@@ -14,7 +14,7 @@ Ext.define('Idc.view.workspace.issues.bulk.IssuesSelectionGrid', {
     },
 
     allLabel: Uni.I18n.translate('workspace.issues.bulk.IssuesSelectionGrid.allLabel', 'MDC', 'All issues'),
-    allDescription: Uni.I18n.translate('workspace.issues.bulk.allDescription', 'MDC', 'Select all issues'),
+    allDescription: Uni.I18n.translate('workspace.issues.bulk.IssuesSelectionGrid.allDescription', 'MDC', 'Select all issues (related to filters and grouping on the issues screen)'),
 
     selectedLabel: Uni.I18n.translate('workspace.issues.bulk.IssuesSelectionGrid.selectedLabel', 'MDC', 'Selected issues'),
     selectedDescription: Uni.I18n.translate('workspace.issues.bulk.IssuesSelectionGrid.selectedDescription', 'MDC', 'Select issues in table'),
