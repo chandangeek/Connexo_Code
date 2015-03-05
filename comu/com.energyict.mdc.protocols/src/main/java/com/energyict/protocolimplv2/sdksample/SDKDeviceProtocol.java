51d4df3e791a147c6e44e85326e58a6f93b257d6
Govanni Naessens <govanni.naessens@elster.com>
2015-03-05 13:47:06 +0100

    COMU-8 Added an additional firmware message to the SDKDeviceProtocol

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/sdksample/SDKDeviceProtocol.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/sdksample/SDKDeviceProtocol.java
@@ -270,7 +270,8 @@ public class SDKDeviceProtocol implements DeviceProtocol {
                 DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_AND_ACTIVATE_DATE,
                 DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_LATER,
                 DeviceMessageId.FIRMWARE_UPGRADE_ACTIVATE,
-                DeviceMessageId.FIRMWARE_UPGRADE_URL_AND_ACTIVATE_DATE);
+                DeviceMessageId.FIRMWARE_UPGRADE_URL_AND_ACTIVATE_DATE,
+                DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_IMMEDIATE);
     }
 
     @Override
