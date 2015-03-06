b8f923a9874da5732dae66b40e1139514d14d70b
Kristof Hennebel <Kristof.Hennebel@elster.com>
2015-03-06 08:27:16 +0000

    COMMUNICATION-1162 - Migrate Crypto protocols to 9.1
    Added placeholder for crypto securitysupport
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@89024 0d108963-8a47-0410-809b-ebd9fb3b3407

---
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/security/CryptoDlmsSecuritySupport.java
@@ -0,0 +1,11 @@
+package com.energyict.protocolimplv2.security;
+
+/**
+ * Copyrights EnergyICT
+ *
+ * @author khe
+ * @since 5/03/2015 - 11:49
+ */
+public class CryptoDlmsSecuritySupport extends DlmsSecuritySupport {
+    //TODO extend this to support the crypto functionality
+}
