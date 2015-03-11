0e96c4986294d5fab0d1f5466e61eb1ca69490b7
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-03-11 13:38:38 +0100

    Add support for integration tests in dependent modules

---
+++ copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/impl/FinateStateMachineModule.java
@@ -0,0 +1,30 @@
+package com.elster.jupiter.fsm.impl;
+
+import com.elster.jupiter.events.EventService;
+import com.elster.jupiter.fsm.FinateStateMachineService;
+import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.orm.OrmService;
+import com.elster.jupiter.transaction.TransactionService;
+import com.elster.jupiter.users.UserService;
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+
+/**
+ * Module intended for use by integration tests.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-03-11 (12:04)
+ */
+public class FinateStateMachineModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        requireBinding(OrmService.class);
+        requireBinding(NlsService.class);
+        requireBinding(UserService.class);
+        requireBinding(EventService.class);
+        requireBinding(TransactionService.class);
+        bind(FinateStateMachineService.class).to(FinateStateMachineServiceImpl.class).in(Scopes.SINGLETON);
+    }
+
+}
