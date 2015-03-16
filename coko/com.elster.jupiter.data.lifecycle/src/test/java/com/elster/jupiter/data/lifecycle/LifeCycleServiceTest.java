c0728bc97560d6a955493c7c0a0fadac2e71099c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-03-16 17:30:03 +0100

    Fix issues caused by change in the metering bundle, largely due to the fact that metering data model now has a dependency to the fsm bundle (foreign key constraint)

--- coko/com.elster.jupiter.data.lifecycle/src/test/java/com/elster/jupiter/data/lifecycle/LifeCycleServiceTest.java
+++ coko/com.elster.jupiter.data.lifecycle/src/test/java/com/elster/jupiter/data/lifecycle/LifeCycleServiceTest.java
@@ -13,6 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import com.elster.jupiter.fsm.FiniteStateMachineService;
+import com.elster.jupiter.fsm.impl.FiniteStateMachineModule;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
 
@@ -40,43 +43,45 @@ import com.google.inject.Injector;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LifeCycleServiceTest {
-	
+
 	private InMemoryBootstrapModule inMemoryBootstrapModule = new InMemoryBootstrapModule();
 	private LifeCycleService lifeCycleService;
-	private Injector injector; 
+	private Injector injector;
 
 	private static class MockModule extends AbstractModule {
 		@Override
-        protected void configure() {       
+        protected void configure() {
             bind(BundleContext.class).toInstance(mock(BundleContext.class));
             bind(EventAdmin.class).toInstance(mock(EventAdmin.class));
         }
     }
-	    
+
     private static final boolean printSql = false;
 
 	@Before
 	public void setUp() throws SQLException {
 		injector = Guice.createInjector(
-				new MockModule(), 
-    			inMemoryBootstrapModule,      		
+				new MockModule(),
+    			inMemoryBootstrapModule,
     			new OrmModule(),
-    			new DomainUtilModule(), 
+    			new DomainUtilModule(),
     			new NlsModule(),
     			new UserModule(),
-    			new UtilModule(), 
-    			new ThreadSecurityModule(), 
-    			new PubSubModule(), 
+    			new UtilModule(),
+    			new ThreadSecurityModule(),
+    			new PubSubModule(),
     			new TransactionModule(printSql),
     			new InMemoryMessagingModule(),
-    			new TaskModule(), 
+    			new TaskModule(),
     			new IdsModule(),
     			new EventsModule(),
     			new PartyModule(),
+                new FiniteStateMachineModule(),
     			new MeteringModule(false),
     			new DataLifeCycleModule()
 		);
 		try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext() ) {
+			injector.getInstance(FiniteStateMachineService.class);
 			lifeCycleService = injector.getInstance(LifeCycleService.class);
 			ctx.commit();
 		}
@@ -90,19 +95,19 @@ public class LifeCycleServiceTest {
 	@Test
     public void test()  {
 		Instant afterInstall = Instant.now();
-		List<LifeCycleCategory> categories = lifeCycleService.getCategories();		
+		List<LifeCycleCategory> categories = lifeCycleService.getCategories();
 		assertThat(categories).hasSize(LifeCycleCategoryKind.values().length);
 		for (LifeCycleCategory category : categories) {
-			assertThat(categories.indexOf(category)).isEqualTo(	category.getKind().ordinal());		
+			assertThat(categories.indexOf(category)).isEqualTo(	category.getKind().ordinal());
 		}
 		lifeCycleService.getTask();
 		injector.getInstance(TransactionService.class).execute(() -> { lifeCycleService.runNow(); return null; });
-		Period period = categories.get(0).getRetention();		
+		Period period = categories.get(0).getRetention();
 		try {
 			Thread.sleep(10);
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
-		}	
+		}
 		injector.getInstance(TransactionService.class).execute(() -> { categories.get(0).setRetentionDays(100); return null;});
 		assertThat(lifeCycleService.getCategories().get(0).getRetention()).isEqualTo(Period.ofDays(100));
 		assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).get(0).getRetention()).isEqualTo(period);
