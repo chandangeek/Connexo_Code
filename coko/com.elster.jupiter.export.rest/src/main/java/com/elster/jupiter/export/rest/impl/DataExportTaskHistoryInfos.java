1f99ef51c338516fcb8441fbfb0ba5043ede64ec
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-03-18 08:38:01 +0100

    Fix history population in rest layer : trigger time can be before creation of the task.

--- coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/DataExportTaskHistoryInfos.java
+++ coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/DataExportTaskHistoryInfos.java
@@ -27,7 +27,7 @@ public class DataExportTaskHistoryInfos {
         return result;
     }
 
-    public DataExportTaskHistoryInfo add(History<? extends ReadingTypeDataExportTask> history, DataExportOccurrence occurrence, Thesaurus thesaurus, TimeService timeService) {
+    public DataExportTaskHistoryInfo add(History<ReadingTypeDataExportTask> history, DataExportOccurrence occurrence, Thesaurus thesaurus, TimeService timeService) {
         DataExportTaskHistoryInfo result = new DataExportTaskHistoryInfo(history, occurrence, thesaurus, timeService);
         data.add(result);
         total++;
@@ -35,7 +35,7 @@ public class DataExportTaskHistoryInfos {
     }
 
     private void addAll(ReadingTypeDataExportTask task, Iterable<? extends DataExportOccurrence> occurrences, Thesaurus thesaurus, TimeService timeService) {
-        History<? extends ReadingTypeDataExportTask> history = task.getHistory();
+        History<ReadingTypeDataExportTask> history = task.getHistory();
         for (DataExportOccurrence each : occurrences) {
             add(history, each, thesaurus, timeService);
         }
