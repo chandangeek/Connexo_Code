a0767f770e017ef02c1e3c82aa54fd7956aab2df
Bruno Ballekens <bruno.ballekens@elster.com>
2015-03-20 17:01:56 +0100

    JP-8819: Fixed copy-paste error

--- coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/MacroPeriod.java
+++ coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/MacroPeriod.java
@@ -2,21 +2,21 @@ package com.elster.jupiter.cbo;
 
 public enum MacroPeriod {
 	NOTAPPLICABLE (0,"NotApplicable"),
-	BILLINGPERIOD (8,"10-minute"),
-	DAILY(11,"Daily"),
-	MONTHLY(13,"Monthly"),
+    BILLINGPERIOD(8, "Billing Period"),
+    DAILY(11, "Daily"),
+    MONTHLY(13,"Monthly"),
 	SEASONAL (22,"Season"),
 	WEEKLYS (24,"Weekly"),
 	SPECIFIEDPERIOD (32,"Specified Period");
-	
+
 	private final int id;
 	private final String description;
-	
+
 	MacroPeriod(int id, String description)  {
 		this.id = id;
-		this.description = description;		
+		this.description = description;
 	}
-			
+
 	public static MacroPeriod get(int id) {
 		for (MacroPeriod each : values()) {
 			if (each.getId() == id) {
@@ -25,17 +25,17 @@ public enum MacroPeriod {
 		}
 		throw new IllegalEnumValueException(MacroPeriod.class, id);
 	}
-	
+
 
 	public String getDescription() {
 		return description;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "TimeAttribute " + id + " : " + description;
 	}
-	
+
 	public boolean isApplicable() {
 		return id != 0;
 	}
