4b46c020c7da4fa092e59e1b16470a7f512f5da8
Sentyurin Sergey <ssentyur@mera.ru>
2015-03-24 17:38:41 +0300

    COMU-272 Validation of 'Device group' field on 'Add data collection KPI is not complete; add itemId 'cmb-display-range' for Selenium tests; small code refactoring

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/datacollectionkpis/Edit.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/datacollectionkpis/Edit.js
@@ -29,7 +29,7 @@ Ext.define('Mdc.view.setup.datacollectionkpis.Edit', {
                 },
                 {
                     xtype: 'combobox',
-                    name: 'devicegroup',
+                    name: 'deviceGroup',
                     emptyText: Uni.I18n.translate('datacollectionkpis.selectDeviceGroup', 'MDC', 'Select a device group'),
                     itemId: 'cmb-device-group',
                     fieldLabel: Uni.I18n.translate('datacollectionkpis.deviceGroup', 'MDC', 'Device group'),
@@ -68,7 +68,7 @@ Ext.define('Mdc.view.setup.datacollectionkpis.Edit', {
                     xtype: 'combobox',
                     name: 'displayRange',
                     emptyText: Uni.I18n.translate('datacollectionkpis.selectDisplayRange', 'MDC', 'Select a display range'),
-                    itemId: 'displayRangeCombo',
+                    itemId: 'cmb-display-range',
                     fieldLabel: Uni.I18n.translate('datacollectionkpis.displayRange', 'MDC', 'Display range'),
                     store: 'Mdc.store.DataCollectionKpiRange',
                     queryMode: 'local',
@@ -130,7 +130,7 @@ Ext.define('Mdc.view.setup.datacollectionkpis.Edit', {
                     displayRange = record.get('displayRange'),
                     connectionTarget = record.get('connectionTarget'),
                     communicationTarget = record.get('communicationTarget'),
-                    deviceGroupCombo = me.down('[name=devicegroup]');
+                    deviceGroupCombo = me.down('[name=deviceGroup]');
 
                 me.getForm().loadRecord(record);
                 if (deviceGroup) {
