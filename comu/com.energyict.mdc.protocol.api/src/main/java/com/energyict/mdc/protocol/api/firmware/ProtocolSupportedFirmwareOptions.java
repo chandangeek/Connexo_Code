af42a9b003ea9a2c50e85ec1d23c628634d72500
borunova <borunova@mera.ru>
2015-03-31 14:41:08 +0300

    Add firmware upgrade options

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/firmware/ProtocolSupportedFirmwareOptions.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/firmware/ProtocolSupportedFirmwareOptions.java
@@ -1,12 +1,39 @@
 package com.energyict.mdc.protocol.api.firmware;
 
+import java.util.Optional;
+
 /**
  * Provides a plain summary of all FirmwareUpgrade options which a DeviceProtocol can support
  */
 public enum ProtocolSupportedFirmwareOptions {
 
-    UPLOAD_FIRMWARE_AND_ACTIVATE_LATER,
-    UPLOAD_FIRMWARE_AND_ACTIVATE_IMMEDIATE,
-    UPLOAD_FIRMWARE_AND_ACTIVATE_WITH_DATE
+    UPLOAD_FIRMWARE_AND_ACTIVATE_LATER("install"),
+    UPLOAD_FIRMWARE_AND_ACTIVATE_IMMEDIATE("activate"),
+    UPLOAD_FIRMWARE_AND_ACTIVATE_WITH_DATE("activateOnDate");
 
+    private String id;
+
+    ProtocolSupportedFirmwareOptions(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static Optional<ProtocolSupportedFirmwareOptions> from(String id) {
+        Optional<ProtocolSupportedFirmwareOptions> optional = Optional.empty();
+        switch (id) {
+            case "install":
+                optional = Optional.of(UPLOAD_FIRMWARE_AND_ACTIVATE_LATER);
+                break;
+            case "activate":
+                optional = Optional.of(UPLOAD_FIRMWARE_AND_ACTIVATE_IMMEDIATE);
+                break;
+            case "activateOnDate":
+                optional = Optional.of(UPLOAD_FIRMWARE_AND_ACTIVATE_WITH_DATE);
+                break;
+        }
+        return optional;
+    }
 }
