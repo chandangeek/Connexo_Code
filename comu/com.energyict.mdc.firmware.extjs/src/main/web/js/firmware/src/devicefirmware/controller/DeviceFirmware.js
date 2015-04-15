fff198c5ff84b9efb1f54e7386a42f52ab6cf72d
Zheltukhin Alexey <azhelt@mera.ru>
2015-04-15 18:04:19 +0300

    merge 575

--- comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/devicefirmware/controller/DeviceFirmware.js
+++ comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/devicefirmware/controller/DeviceFirmware.js
@@ -12,6 +12,11 @@ Ext.define('Fwc.devicefirmware.controller.DeviceFirmware', {
         'Mdc.model.Device'
     ],
 
+    models: [
+        'Mdc.model.Device',
+        'Fwc.devicefirmware.model.FirmwareMessageSpec'
+    ],
+
     stores: [
         'Fwc.devicefirmware.store.Firmwares',
         'Fwc.devicefirmware.store.FirmwareActions'
@@ -20,7 +25,8 @@ Ext.define('Fwc.devicefirmware.controller.DeviceFirmware', {
     refs: [
         {ref: 'container', selector: 'viewport > #contentPanel'},
         {ref: 'setupPage', selector: 'device-firmware-setup'},
-        {ref: 'uploadPage', selector: 'device-firmware-upload'}
+        {ref: 'uploadPage', selector: 'device-firmware-upload'},
+        {ref: 'uploadForm', selector: 'device-firmware-upload-form'}
     ],
 
     init: function () {
@@ -120,35 +126,80 @@ Ext.define('Fwc.devicefirmware.controller.DeviceFirmware', {
     showDeviceFirmwareUpload: function (mRID) {
         var me = this,
             router = this.getController('Uni.controller.history.Router'),
-            activateOption = router.queryParams.activate,
-            title;
+            messageSpecModel = me.getModel('Fwc.devicefirmware.model.FirmwareMessageSpec'),
+            activateOption = router.queryParams.activate;
 
-        switch (activateOption) {
-            case 'dont':
-                title = Uni.I18n.translate('deviceFirmware.upload', 'FWC', 'Upload firmware');
-                break;
-            case 'now':
-                title = Uni.I18n.translate('deviceFirmware.uploadActivate', 'FWC', 'Upload firmware and activate');
-                break;
-            case 'inDate':
-                title = Uni.I18n.translate('deviceFirmware.uploadActivateInDate', 'FWC', 'Upload firmware with activation date');
-                break;
-            default:
-                router.getRoute('devices/device/firmware').forward();
-                break;
-        }
+        messageSpecModel.getProxy().setUrl(mRID);
 
-        Ext.ModelManager.getModel('Mdc.model.Device').load(mRID, {
-            success: function (device) {
-                me.getApplication().fireEvent('loadDevice', device);
-                me.getApplication().fireEvent('changecontentevent', 'device-firmware-upload', {
-                    device: device,
-                    title: title,
-                    router: router
-                });
-            }
+        var record = Ext.create(messageSpecModel, {
+            id: "activateOnDate",
+            displayValue: "Upload firmware with activation date"
         });
 
+//        record.properties().add([
+//            {
+//                key: "FirmwareDeviceMessage.upgrade.firwareversion",
+//                name: "Firmware version",
+//                propertyValueInfo: { },
+//                propertyTypeInfo: {
+//                    simplePropertyType: "UNKNOWN",
+//                    predefinedPropertyValuesInfo: {
+//                        possibleValues: [
+//                            {
+//                                "id": 1, "name": "ASP03.01.03-12359"
+//                            },
+//                            {
+//                                "id": 2, "name": "ASP03.01.04-4532"
+//                            }
+//                        ],
+//                        selectionMode: "COMBOBOX",
+//                        exhaustive: true
+//                    }
+//                },
+//                required: true
+//            },
+//            {
+//                key: "FirmwareDeviceMessage.upgrade.activationdate",
+//                name: "Activation date",
+//                propertyValueInfo: { },
+//                propertyTypeInfo: {
+//                    simplePropertyType: "CLOCK"
+//                },
+//                required: true
+//            }
+//        ]);
 
+
+//        console.log(record);
+//
+//        switch (activateOption) {
+//            case 'dont':
+//                title = Uni.I18n.translate('deviceFirmware.upload', 'FWC', 'Upload firmware');
+//                break;
+//            case 'now':
+//                title = Uni.I18n.translate('deviceFirmware.uploadActivate', 'FWC', 'Upload firmware and activate');
+//                break;
+//            case 'inDate':
+//                title = Uni.I18n.translate('deviceFirmware.uploadActivateInDate', 'FWC', 'Upload firmware with activation date');
+//                break;
+//            default:
+//                router.getRoute('devices/device/firmware').forward();
+//                break;
+//        }
+
+        me.getModel('Mdc.model.Device').load(mRID, {
+            success: function (device) {
+                //        messageSpecModel.load(activateOption, {
+//            success: function (record) {
+                var widget = Ext.widget('device-firmware-upload', {device: device, title: record.get('displayValue'), router: router});
+                me.getApplication().fireEvent('loadDevice', device);
+                me.getApplication().fireEvent('changecontentevent', widget);
+                widget.down('property-form').loadRecord(record);
+
+
+                //            }
+//        });
+            }
+        });
     }
 });
