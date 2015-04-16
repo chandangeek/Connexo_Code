f556c948f240b4ecb287604bf931839781c1dc2a
gna <govanni.naessens@elster.com>
2015-04-16 08:49:52 +0200

    COMU-618 Updated the MessageConvertor, the FirmwareMessage was pointing to the wrong DeviceMessageId

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/messages/convertor/AS220DLMSMessageConverter.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/messages/convertor/AS220DLMSMessageConverter.java
@@ -149,7 +149,7 @@ public class AS220DLMSMessageConverter extends AbstractMessageConverter {
         registry.put(DeviceMessageId.PLC_CONFIGURATION_SET_SFSK_REPEATER, new MultipleAttributeMessageEntry("SetSFSKRepeater", "REPEATER"));
 
         registry.put(DeviceMessageId.MBUS_SETUP_DECOMMISSION_ALL, new SimpleTagMessageEntry("DecommissionAll"));
-        registry.put(DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_LATER, new FirmwareUdateWithUserFileMessageEntry(firmwareUpdateFileAttributeName));
+        registry.put(DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_IMMEDIATE, new FirmwareUdateWithUserFileMessageEntry(firmwareUpdateFileAttributeName));
         return registry;
     }
 
