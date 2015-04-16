8ee95b0ac953a4600b0706f4bbdab240fa842a85
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-04-16 16:33:26 +0200

    Update javadoc to reflect changes that were applied

--- comu/com.energyict.mdc.device.lifecycle/src/main/java/com/energyict/mdc/device/lifecycle/impl/micro/checks/SecurityPropertiesAreValid.java
+++ comu/com.energyict.mdc.device.lifecycle/src/main/java/com/energyict/mdc/device/lifecycle/impl/micro/checks/SecurityPropertiesAreValid.java
@@ -1,5 +1,6 @@
 package com.energyict.mdc.device.lifecycle.impl.micro.checks;
 
+import com.energyict.mdc.device.config.ComTaskEnablement;
 import com.energyict.mdc.device.config.SecurityPropertySet;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.lifecycle.DeviceLifeCycleActionViolation;
@@ -14,9 +15,10 @@ import java.util.Optional;
 /**
  * Provides an implementation for the {@link ServerMicroCheck} interface
  * that checks that a Device has valid security properties
- * for all {@link SecurityPropertySet}s that are defined
- * at the configuration level.
- * @see Device#securityPropertiesAreValid() 
+ * for all {@link SecurityPropertySet}s that are used at the
+ * configuration level when communication tasks are being enabled.
+ * @see ComTaskEnablement#getSecurityPropertySet()
+ * @see Device#securityPropertiesAreValid()
  *
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2015-04-15 (09:48)
