4a10c5756856db27cea4b02e8a1c9eebdb55904c
Artem Puchenkin <apuchenkin@gmail.com>
2015-04-16 14:07:28 +0300

    [COMU-365] View failed firmware upgrade on a device

--- comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/devicefirmware/view/FirmwareForm.js
+++ comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/devicefirmware/view/FirmwareForm.js
@@ -39,11 +39,30 @@ Ext.define('Fwc.devicefirmware.view.FirmwareForm', {
             xtype: 'toolbar',
             dock: 'top',
             border: true,
+            layout: 'vbox',
             items: [
                 {
                     xtype: 'uni-form-error-message',
-                    itemId: 'form-errors',
-                    hidden: true
+                    padding: 10,
+                    itemId: 'form-error',
+                    hidden: true,
+                    layout: {
+                        type: 'hbox',
+                        defaultMargins: '5 10 5 5'
+                    },
+                    buttonAlign: 'left',
+                    buttons: [{
+                        margin: '0 0 0 46',
+                        text: Uni.I18n.translate('device.firmware.failed.retry', 'MDC', 'Retry'),
+                        ui: 'action',
+                        action: 'retry',
+                        itemId: 'retryBtn'
+                    }, {
+                        text: Uni.I18n.translate('device.firmware.failed.log', 'MDC', 'View log'),
+                        ui: 'link',
+                        action: 'viewLog',
+                        itemId: 'logBtn'
+                    }]
                 },
                 {
                     xtype: 'uni-form-error-message',
@@ -96,18 +115,30 @@ Ext.define('Fwc.devicefirmware.view.FirmwareForm', {
 
     initComponent: function () {
         var me = this,
-            formInfo;
+            formInfo,
+            formError;
+
         me.callParent(arguments);
         me.loadRecord(me.record.getActiveVersion());
         me.setTitle(me.record.get('type'));
-        formInfo = me.down('#form-info');
+        formInfo    = me.down('#form-info');
+        formError   = me.down('#form-error');
 
-        var pendingVersion = me.record.getAssociatedData().activeVersion;
+        var pendingVersion = me.record.getAssociatedData().activeVersion; //todo: replace on pendingVersion
+        var failedVersion  = me.record.getAssociatedData().activeVersion; //todo: replace on failedVerion
+
         if (pendingVersion) {
             formInfo.show();
-            formInfo.setText(Uni.I18n.translate('device.firmware.field.status.deprecated.tooltip', 'FWC', 'Firmware version {0} will be uploaded to device on {1}', [
+            formInfo.setText(Uni.I18n.translate('device.firmware.deprecated.message', 'FWC', 'Firmware version {0} will be uploaded to device on {1}', [
                 pendingVersion.firmwareVersion,
                 Uni.DateTime.formatDateTimeShort(pendingVersion.plannedDate)
+            ]));
+        }
+
+        if (failedVersion) {
+            formError.show();
+            formError.setText(Uni.I18n.translate('device.firmware.failed.message', 'FWC', 'Failed to upload version {0} to the device', [
+                pendingVersion.firmwareVersion
             ]));
         }
     }
