6dd2d2f901e6a01230074efeaa36ada6d6e47f5e
Anton Suslennikov <antsusl@mera.ru>
2015-04-17 11:38:08 +0300

    Revert ResourceFiel back

--- comu/com.energyict.mdc.firmware.rest/src/main/java/com/energyict/mdc/firmware/rest/impl/FirmwareFieldResource.java
+++ comu/com.energyict.mdc.firmware.rest/src/main/java/com/energyict/mdc/firmware/rest/impl/FirmwareFieldResource.java
@@ -9,13 +9,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.*;
 
 @Path("/field")
 public class FirmwareFieldResource extends FieldResource {
+    private final Thesaurus thesaurus;
 
     @Inject
     public FirmwareFieldResource(Thesaurus thesaurus) {
-        super(thesaurus);
+        this.thesaurus = thesaurus;
     }
 
     @GET
@@ -32,5 +34,19 @@ public class FirmwareFieldResource extends FieldResource {
     @RolesAllowed({com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_TYPE, com.energyict.mdc.device.config.security.Privileges.ADMINISTRATE_DEVICE_TYPE})
     public Object getFirmwareTypes() {
         return asJsonArrayObjectWithTranslation("firmwareTypes", "id", new FirmwareTypeFieldAdapter().getClientSideValues());
+    }
+
+    @Override
+    protected <T> Map<String, Object> asJsonArrayObjectWithTranslation(String fieldName, String valueName, Collection<T> values) {
+        Map<String, Object> map = new HashMap<>();
+        List<Map<String, Object>> list = new ArrayList<>();
+        map.put(fieldName, list);
+        for (final T value : values) {
+            Map<String, Object> subMap = new HashMap<>();
+            subMap.put(valueName, value);
+            subMap.put("displayValue", thesaurus.getString(value.toString(), value.toString()));
+            list.add(subMap);
+        }
+        return map;
     }
 }
