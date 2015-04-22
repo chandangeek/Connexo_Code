805a4fee61e156383fdc63da8b062794f73f0856
Geert <gde@geertd.eict.local>
2015-04-22 15:34:19 +0200

    COMU-713 - Demo script: Create G3 devices Added the creation of the G3 gateway device itself

--- comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/commands/CreateG3GatewayCommand.java
+++ comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/commands/CreateG3GatewayCommand.java
@@ -1,18 +1,26 @@
 package com.elster.jupiter.demo.impl.commands;
 
 import com.elster.jupiter.demo.impl.Builders;
+import com.elster.jupiter.demo.impl.UnableToCreate;
+import com.elster.jupiter.demo.impl.builders.DeviceBuilder;
 import com.elster.jupiter.demo.impl.templates.ComTaskTpl;
 import com.elster.jupiter.demo.impl.templates.DeviceTypeTpl;
 import com.elster.jupiter.demo.impl.templates.OutboundTCPComPortPoolTpl;
 import com.elster.jupiter.time.TimeDuration;
+import com.energyict.mdc.common.TypedProperties;
+import com.energyict.mdc.device.config.ComTaskEnablement;
 import com.energyict.mdc.device.config.ConnectionStrategy;
 import com.energyict.mdc.device.config.DeviceConfiguration;
 import com.energyict.mdc.device.config.DeviceSecurityUserAction;
 import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.config.GatewayType;
+import com.energyict.mdc.device.config.PartialScheduledConnectionTask;
 import com.energyict.mdc.device.config.SecurityPropertySet;
+import com.energyict.mdc.device.data.ConnectionTaskService;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.DeviceService;
+import com.energyict.mdc.device.data.tasks.ConnectionTask;
+import com.energyict.mdc.device.data.tasks.ScheduledConnectionTask;
 import com.energyict.mdc.protocol.api.device.messages.DlmsAuthenticationLevelMessageValues;
 import com.energyict.mdc.protocol.api.device.messages.DlmsEncryptionLevelMessageValues;
 import com.energyict.mdc.protocol.pluggable.ConnectionTypePluggableClass;
@@ -20,6 +28,8 @@ import com.energyict.mdc.protocol.pluggable.ProtocolPluggableService;
 import com.energyict.mdc.tasks.ComTask;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -34,14 +44,19 @@ public class CreateG3GatewayCommand {
 
     private final DeviceService deviceService;
     private final ProtocolPluggableService protocolPluggableService;
+    private final ConnectionTaskService connectionTaskService;
     private ConnectionTypePluggableClass requiredPluggableClass;
+    private final Provider<DeviceBuilder> deviceBuilderProvider;
 
     private Map<ComTaskTpl, ComTask> comTasks;
 
     @Inject
-    public CreateG3GatewayCommand(DeviceService deviceService, ProtocolPluggableService protocolPluggableService) {
+    public CreateG3GatewayCommand(DeviceService deviceService, ProtocolPluggableService protocolPluggableService,
+                                  ConnectionTaskService connectionTaskService, Provider<DeviceBuilder> deviceBuilderProvider) {
         this.deviceService = deviceService;
         this.protocolPluggableService = protocolPluggableService;
+        this.connectionTaskService = connectionTaskService;
+        this.deviceBuilderProvider = deviceBuilderProvider;
     }
 
     public void run() {
@@ -52,16 +67,24 @@ public class CreateG3GatewayCommand {
             return;
         }
         Optional<ConnectionTypePluggableClass> pluggableClass = protocolPluggableService.findConnectionTypePluggableClassByName(REQUIRED_PLUGGABLE_CLASS_NAME);
-        if (pluggableClass.isPresent()) {
+        if (!pluggableClass.isPresent()) {
             System.out.println("Nothing was created since the required pluggable class '" + REQUIRED_PLUGGABLE_CLASS_NAME + "' couldn't be found!");
             return;
         }
+
+        // 2. Find or create required objects
         requiredPluggableClass = pluggableClass.get();
-        // 2. Create the device type
+        findOrCreateRequiredObjects();
+
+        // 3. Create the device type
         DeviceType g3DeviceType = Builders.from(DeviceTypeTpl.RTU_Plus_G3).get();
-        // 3. Create the configuration
+
+        // 4. Create the configuration
         DeviceConfiguration configuration = g3DeviceType.getConfigurations().stream().filter(dc -> DEVICE_CONFIG_NAME.equals(dc.getName())).findFirst()
             .orElseGet(() -> createG3DeviceConfiguration(g3DeviceType, DEVICE_CONFIG_NAME));
+
+        // 5. Create the gateway device
+        createG3GatewayDevice(GATEWAY_MRID, configuration);
     }
 
     private DeviceConfiguration createG3DeviceConfiguration(DeviceType g3DeviceType, String deviceConfigName) {
@@ -70,14 +93,6 @@ public class CreateG3GatewayCommand {
             .isDirectlyAddressable(true)
             .gatewayType(GatewayType.LOCAL_AREA_NETWORK);
         DeviceConfiguration configuration = configBuilder.add();
-
-        findOrCreateRequiredObjects();
-//        Map<String, String> channels = new HashMap<>(4);
-//        channels.put(RegisterTypeTpl.DELTA_A_PLUS_ALL_PHASES.getMrid(), "0.1.128.0.0.255");
-//        channels.put(RegisterTypeTpl.DELTA_A_MINUS_ALL_PHASES.getMrid(), "0.2.128.0.0.255");
-//        channels.put(RegisterTypeTpl.DELRA_REACTIVE_ENERGY_PLUS.getMrid(), "0.3.128.0.0.255");
-//        channels.put(RegisterTypeTpl.DELRA_REACTIVE_ENERGY_MINUS.getMrid(), "0.4.128.0.0.255");
-//        addChannelsOnLoadProfileToDeviceConfiguration(configuration, channels);
 
         configuration
             .newPartialScheduledConnectionTask(
@@ -88,7 +103,8 @@ public class CreateG3GatewayCommand {
             .comPortPool(Builders.from(OutboundTCPComPortPoolTpl.ORANGE).get())
             .asDefault(true).build();
 
-        SecurityPropertySet securityPropertySet = configuration.createSecurityPropertySet(SECURITY_PROPERTY_SET_NAME)
+        configuration
+            .createSecurityPropertySet(SECURITY_PROPERTY_SET_NAME)
             .authenticationLevel(DlmsAuthenticationLevelMessageValues.HIGH_LEVEL_GMAC.getValue())
             .encryptionLevel(DlmsEncryptionLevelMessageValues.NO_ENCRYPTION.getValue())
             .addUserAction(DeviceSecurityUserAction.VIEWDEVICESECURITYPROPERTIES1)
@@ -97,8 +113,7 @@ public class CreateG3GatewayCommand {
             .addUserAction(DeviceSecurityUserAction.EDITDEVICESECURITYPROPERTIES2)
             .build();
 
-        addComTasksToDeviceConfiguration(configuration,
-            ComTaskTpl.TOPOLOGY_UPDATE/*, ComTaskTpl.READ_LOAD_PROFILE_DATA, ComTaskTpl.READ_LOG_BOOK_DATA, ComTaskTpl.READ_REGISTER_DATA*/);
+        addComTasksToDeviceConfiguration(configuration, ComTaskTpl.TOPOLOGY_UPDATE);
         configuration.activate();
         configuration.save();
         return configuration;
@@ -107,15 +122,11 @@ public class CreateG3GatewayCommand {
     private void findOrCreateRequiredObjects() {
         comTasks = new HashMap<>();
         findOrCreateComTask(ComTaskTpl.TOPOLOGY_UPDATE);
-//        findOrCreateComTask(ComTaskTpl.READ_LOAD_PROFILE_DATA);
-//        findOrCreateComTask(ComTaskTpl.READ_LOG_BOOK_DATA);
-//        findOrCreateComTask(ComTaskTpl.READ_REGISTER_DATA);
     }
 
     private ComTask findOrCreateComTask(ComTaskTpl comTaskTpl) {
         return comTasks.put(comTaskTpl, Builders.from(comTaskTpl).get());
     }
-
 
     private void addComTasksToDeviceConfiguration(DeviceConfiguration configuration, ComTaskTpl... names) {
         if (names == null) {
@@ -133,4 +144,51 @@ public class CreateG3GatewayCommand {
         }
     }
 
+    private void createG3GatewayDevice(String mrid, DeviceConfiguration configuration) {
+        Device device = deviceBuilderProvider.get()
+            .withMrid(mrid)
+            .withSerialNumber(mrid)
+            .withDeviceConfiguration(configuration)
+            .withYearOfCertification(2015)
+            .get();
+        addConnectionTasksToDevice(device);
+        addSecurityPropertiesToDevice(device);
+        addComTaskToDevice(device, ComTaskTpl.TOPOLOGY_UPDATE);
+        device.setProtocolProperty("Short_MAC_address", new BigDecimal(0));
+        device.save();
+    }
+
+    private void addConnectionTasksToDevice(Device device) {
+        DeviceConfiguration configuration = device.getDeviceConfiguration();
+        PartialScheduledConnectionTask connectionTask = configuration.getPartialOutboundConnectionTasks().get(0);
+        ScheduledConnectionTask deviceConnectionTask = device.getScheduledConnectionTaskBuilder(connectionTask)
+            .setComPortPool(Builders.from(OutboundTCPComPortPoolTpl.ORANGE).get())
+            .setConnectionStrategy(ConnectionStrategy.AS_SOON_AS_POSSIBLE)
+            .setNextExecutionSpecsFrom(null)
+            .setConnectionTaskLifecycleStatus(ConnectionTask.ConnectionTaskLifecycleStatus.ACTIVE)
+            .setProperty("host", "10.0.0.135")
+            .setProperty("portNumber", new BigDecimal(4059))
+            .setSimultaneousConnectionsAllowed(false)
+            .add();
+        connectionTaskService.setDefaultConnectionTask(deviceConnectionTask);
+    }
+
+    private void addComTaskToDevice(Device device, ComTaskTpl comTask) {
+        DeviceConfiguration configuration = device.getDeviceConfiguration();
+        ComTaskEnablement taskEnablement = configuration.getComTaskEnablementFor(comTasks.get(comTask)).get();
+        device.newManuallyScheduledComTaskExecution(taskEnablement, null).add();
+    }
+
+    private void addSecurityPropertiesToDevice(Device device) {
+        DeviceConfiguration configuration = device.getDeviceConfiguration();
+        SecurityPropertySet securityPropertySet = configuration.getSecurityPropertySets().stream()
+            .filter(sps -> SECURITY_PROPERTY_SET_NAME.equals(sps.getName())).findFirst().orElseThrow(() -> new UnableToCreate(""));
+        TypedProperties typedProperties = TypedProperties.empty();
+        typedProperties.setProperty("ClientMacAddress", "1");
+        securityPropertySet.getPropertySpecs().stream().filter(ps -> ps.getName().equals("AuthenticationKey")).findFirst().ifPresent(
+            ps -> typedProperties.setProperty(ps.getName(), ps.getValueFactory().fromStringValue("00112233445566778899AABBCCDDEEFF")));
+        securityPropertySet.getPropertySpecs().stream().filter(ps -> ps.getName().equals("EncryptionKey")).findFirst().ifPresent(
+            ps -> typedProperties.setProperty(ps.getName(), ps.getValueFactory().fromStringValue("11223344556677889900AABBCCDDEEFF")));
+        device.setSecurityProperties(securityPropertySet, typedProperties);
+    }
 }
