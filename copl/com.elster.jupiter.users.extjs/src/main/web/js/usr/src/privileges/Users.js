faf962bb024032819768a4284d3ee41b50746b33
Lucian Timar <lucian.timar@elster.com>
2015-04-23 15:30:29 +0300

    privileges refactoring

--- copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/src/privileges/Users.js
+++ copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/src/privileges/Users.js
@@ -10,7 +10,7 @@ Ext.define('Usr.privileges.Users', {
     singleton: true,
     view : ['privilege.administrate.userAndRole', 'privilege.view.userAndRole'],
     admin : ['privilege.administrate.userAndRole'],
-    any: function() {
+    all: function() {
         return Ext.Array.merge(Usr.privileges.Users.view,
             Usr.privileges.Users.admin);
     },
