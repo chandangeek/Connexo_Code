480ebe4258d4f31aa571803a03715381a1b8111c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-04-23 11:19:22 +0200

    Fix failures after faulty merge conflict resolution during merge master into branch

--- comu/com.energyict.mdc.favorites/src/test/java/com/energyict/mdc/favorites/impl/FavoritesServiceImplTest.java
+++ comu/com.energyict.mdc.favorites/src/test/java/com/energyict/mdc/favorites/impl/FavoritesServiceImplTest.java
@@ -142,7 +142,6 @@ public class FavoritesServiceImplTest {
 
         @Override
         protected void configure() {
-            bind(com.elster.jupiter.issue.share.service.IssueService.class).toInstance(mock(com.elster.jupiter.issue.share.service.IssueService.class));
             bind(Clock.class).toInstance(Clock.systemDefaultZone());
             bind(JsonService.class).toInstance(new JsonServiceImpl());
             bind(BeanService.class).toInstance(new BeanServiceImpl());
