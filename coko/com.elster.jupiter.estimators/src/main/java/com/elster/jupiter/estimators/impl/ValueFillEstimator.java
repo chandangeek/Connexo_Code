5f122c7539bc55d377fcca7c5474cccb87f638ca
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-04-27 15:59:33 +0200

    COPL-417 - Property validation for 'value fill' rule

--- coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/ValueFillEstimator.java
+++ coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/ValueFillEstimator.java
@@ -3,16 +3,23 @@ package com.elster.jupiter.estimators.impl;
 import com.elster.jupiter.estimation.Estimatable;
 import com.elster.jupiter.estimation.EstimationBlock;
 import com.elster.jupiter.estimation.EstimationResult;
+import com.elster.jupiter.estimation.EstimationRuleProperties;
+import com.elster.jupiter.estimators.MessageSeeds;
+import com.elster.jupiter.nls.LocalizedFieldValidationException;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.properties.PropertySpec;
 import com.elster.jupiter.properties.PropertySpecService;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,7 +32,7 @@ public class ValueFillEstimator extends AbstractEstimator {
     private static final BigDecimal MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS_DEFAULT_VALUE = BigDecimal.valueOf(10);
 
     static final String FILL_VALUE = "valuefill.fillValue";
-    private static final BigDecimal DEFAULT_FILL_VALUE = BigDecimal.valueOf(0);
+    private static final BigDecimal DEFAULT_FILL_VALUE = BigDecimal.ZERO;
 
     private BigDecimal numberOfConsecutiveSuspects;
     private BigDecimal fillValue;
@@ -68,7 +75,7 @@ public class ValueFillEstimator extends AbstractEstimator {
 
     public void estimate(EstimationBlock estimationBlock, List<EstimationBlock> remain, List<EstimationBlock> estimated) {
         if (isEstimatable(estimationBlock)) {
-            estimationBlock.estimatables().forEach(estimatable -> estimate(estimatable));
+            estimationBlock.estimatables().forEach(this::estimate);
 
             estimated.add(estimationBlock);
         } else {
@@ -101,5 +108,32 @@ public class ValueFillEstimator extends AbstractEstimator {
                 FILL_VALUE, true, DEFAULT_FILL_VALUE));
         return builder.build();
     }
+
+    @Override
+    public void validateProperties(List<EstimationRuleProperties> estimatorProperties) {
+        ImmutableMap.Builder<String, Consumer<EstimationRuleProperties>> builder = ImmutableMap.builder();
+        builder.put(MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, property -> {
+            BigDecimal value = (BigDecimal) property.getValue();
+            if (hasFractionalPart(value)) {
+                throw new LocalizedFieldValidationException(MessageSeeds.INVALID_NUMBER_OF_CONSECUTIVE_SUSPECTS_SHOULD_BE_INTEGER_VALUE, "properties." + MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS);
+            }
+            if (value.intValue() < 1) {
+                throw new LocalizedFieldValidationException(MessageSeeds.INVALID_NUMBER_OF_CONSECUTIVE_SUSPECTS, "properties." + MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS);
+            }
+        });
+
+        ImmutableMap<String, Consumer<EstimationRuleProperties>> propertyValidations = builder.build();
+
+        estimatorProperties.forEach(property -> {
+            Optional.ofNullable(propertyValidations.get(property.getName()))
+                    .ifPresent(validator -> validator.accept(property));
+        });
+    }
+
+    private boolean hasFractionalPart(BigDecimal value) {
+        return value.setScale(0, RoundingMode.DOWN).compareTo(value) != 0;
+    }
+
+
 }
 
--- coko/com.elster.jupiter.estimators/src/test/java/com/elster/jupiter/estimators/impl/ValueFillEstimatorTest.java
+++ coko/com.elster.jupiter.estimators/src/test/java/com/elster/jupiter/estimators/impl/ValueFillEstimatorTest.java
@@ -6,10 +6,13 @@ import com.elster.jupiter.devtools.tests.rules.Using;
 import com.elster.jupiter.estimation.Estimatable;
 import com.elster.jupiter.estimation.EstimationBlock;
 import com.elster.jupiter.estimation.EstimationResult;
+import com.elster.jupiter.estimation.EstimationRule;
+import com.elster.jupiter.estimation.EstimationRuleProperties;
 import com.elster.jupiter.estimation.Estimator;
 import com.elster.jupiter.metering.Channel;
 import com.elster.jupiter.metering.ReadingQualityType;
 import com.elster.jupiter.metering.ReadingType;
+import com.elster.jupiter.nls.LocalizedFieldValidationException;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.properties.PropertySpecService;
 import org.junit.Rule;
@@ -21,10 +24,12 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.elster.jupiter.devtools.tests.assertions.JupiterAssertions.assertThat;
+import static com.elster.jupiter.estimators.impl.ValueFillEstimator.MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -52,7 +57,7 @@ public class ValueFillEstimatorTest {
 
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(ValueFillEstimator.FILL_VALUE, new BigDecimal(5));
-        properties.put(ValueFillEstimator.MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, new BigDecimal(10));
+        properties.put(MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, new BigDecimal(10));
 
         Estimator estimator = new ValueFillEstimator(thesaurus, propertySpecService, properties);
         estimator.init();
@@ -66,5 +71,70 @@ public class ValueFillEstimatorTest {
         verify(estimatable1).setEstimation(new BigDecimal(5));
         verify(estimatable2).setEstimation(new BigDecimal(5));
     }
+
+    @Test(expected = LocalizedFieldValidationException.class)
+    public void testInvalidPropertiesWhenConsecutiveIsFractional() {
+        EstimationRuleProperties property = estimationRuleProperty(MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, BigDecimal.valueOf(11, 1));
+
+        Estimator estimator = new ValueFillEstimator(thesaurus, propertySpecService);
+
+        estimator.validateProperties(Collections.singletonList(property));
+    }
+
+    @Test
+    public void testValidPropertiesWhenConsecutiveIsFractionalYetZeroAsFractionalPart() {
+        EstimationRuleProperties property = estimationRuleProperty(MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, BigDecimal.valueOf(10, 1));
+
+        Estimator estimator = new ValueFillEstimator(thesaurus, propertySpecService);
+
+        estimator.validateProperties(Collections.singletonList(property));
+    }
+
+    @Test(expected = LocalizedFieldValidationException.class)
+    public void testInvalidPropertiesWhenConsecutiveIsZero() {
+        EstimationRuleProperties property = estimationRuleProperty(MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, BigDecimal.ZERO);
+
+        Estimator estimator = new ValueFillEstimator(thesaurus, propertySpecService);
+
+        estimator.validateProperties(Collections.singletonList(property));
+    }
+
+    @Test(expected = LocalizedFieldValidationException.class)
+    public void testInvalidPropertiesWhenConsecutiveIsNegative() {
+        EstimationRuleProperties property = estimationRuleProperty(MAX_NUMBER_OF_CONSECUTIVE_SUSPECTS, BigDecimal.valueOf(-1));
+
+        Estimator estimator = new ValueFillEstimator(thesaurus, propertySpecService);
+
+        estimator.validateProperties(Collections.singletonList(property));
+    }
+
+    private EstimationRuleProperties estimationRuleProperty(final String name, final Object value) {
+        return new EstimationRuleProperties() {
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public String getDisplayName() {
+                return name;
+            }
+
+            @Override
+            public Object getValue() {
+                return value;
+            }
+
+            @Override
+            public void setValue(Object value) {
+            }
+
+            @Override
+            public EstimationRule getRule() {
+                return mock(EstimationRule.class);
+            }
+        };
+    }
+
 
 }
