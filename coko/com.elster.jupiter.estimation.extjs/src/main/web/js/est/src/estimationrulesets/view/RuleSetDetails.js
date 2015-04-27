420969cf20824d7be5f56b30b3ae70e2c100b22b
akuryuk <akuryuk@mera.ru>
2015-04-27 16:24:35 +0300

    [HotFix] Add/ edit rule sets

--- coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationrulesets/view/RuleSetDetails.js
+++ coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationrulesets/view/RuleSetDetails.js
@@ -14,59 +14,43 @@ Ext.define('Est.estimationrulesets.view.RuleSetDetails', {
         var me = this;
         me.content = [
             {
-                xtype: 'container',
-                layout: 'hbox',
+                xtype: 'form',
+                title: Uni.I18n.translate('estimationrulesets.details.title', 'EST', 'Overview'),
+                ui: 'large',
+                itemId: 'rule-set-form',
+                defaults: {
+                    xtype: 'displayfield',
+                    labelWidth: 150
+                },
                 items: [
                     {
-                        xtype: 'panel',
-                        ui: 'large',
-                        flex: 1,
-                        title: Uni.I18n.translate('estimation.ruleSetDetail.title', 'EST', 'Overvirew'),
-                        items: [
-                            {
-                                xtype: 'form',
-                                itemId: 'rule-set-form',
-                                defaults: {
-                                    xtype: 'displayfield',
-                                    labelWidth: 150
-                                },
-                                items: [
-                                    {
-                                        name: 'name',
-                                        fieldLabel: Uni.I18n.translate('general.name', 'EST', 'Name')
-                                    },
-                                    {
-                                        name: 'description',
-                                        fieldLabel: Uni.I18n.translate('general.description', 'EST', 'Description')
-                                    },
-                                    {
-                                        name: 'numberOfActiveRules',
-                                        fieldLabel: Uni.I18n.translate('estimationrulesets.activeRules', 'EST', 'Active rules')
-                                    },
-                                    {
-                                        name: 'numberOfInactiveRules',
-                                        fieldLabel: Uni.I18n.translate('estimationrulesets.inactiveRules', 'EST', 'Inactive rules')
-                                    }
-                                ]
-                            }
-                        ]
+                        name: 'name',
+                        fieldLabel: Uni.I18n.translate('estimationrulesets.details.name', 'EST', 'Name')
                     },
                     {
-                        xtype: 'container',
-                        width: 100,
-                        items: Uni.Auth.hasAnyPrivilege(['privilege.administrate.EstimationConfiguration']) && {
-                            xtype: 'button',
-                            iconCls: 'x-uni-action-iconD',
-                            itemId: 'action-button',
-                            text: Uni.I18n.translate('general.actions', 'EST', 'Actions'),
-                            menu: {
-                                xtype: 'estimation-rule-sets-action-menu',
-                                record: me.record
-                            }
-                        }
+                        name: 'description',
+                        fieldLabel: Uni.I18n.translate('estimationrulesets.details.description', 'EST', 'Description')
+                    },
+                    {
+                        name: 'numberOfActiveRules',
+                        fieldLabel: Uni.I18n.translate('estimationrulesets.details.activeRules', 'EST', 'Active rules')
+                    },
+                    {
+                        name: 'numberOfInactiveRules',
+                        fieldLabel: Uni.I18n.translate('estimationrulesets.details.inactiveRules', 'EST', 'Inactive rules')
                     }
-                ]
+                ],
+                tools: [{
+                    xtype: 'button',
+                    iconCls: 'x-uni-action-iconD',
+                    itemId: 'action-button',
+                    text: Uni.I18n.translate('estimationrulesets.details.action', 'EST', 'Actions'),
+                    menu: {
+                        xtype: 'estimation-rule-sets-action-menu'
+                    }
+                }]
             }
+
         ];
         me.side = {
             xtype: 'panel',
--- coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationrulesets/view/RuleSetEdit.js
+++ coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationrulesets/view/RuleSetEdit.js
@@ -2,72 +2,59 @@ Ext.define('Est.estimationrulesets.view.RuleSetEdit', {
     extend: 'Uni.view.container.ContentContainer',
     alias: 'widget.rule-set-edit',
     itemId: 'rule-set-edit',
-    record: null,
     initComponent: function () {
-        var me = this,
-            title;
-
-        if (me.record) {
-            title = Uni.I18n.translate('estimationrulesets.edit.title', 'EST', "Edit '{ruleSet}'").replace('{ruleSet}', me.record.get('name'))
-        } else {
-            title = Uni.I18n.translate('estimationrulesets.add.title', 'EST', 'Add estimation rule set')
-        }
-
+        var me = this;
         me.content = [
             {
-                xtype: 'panel',
+                xtype: 'form',
                 ui: 'large',
-                title: title,
-                items: {
-                    xtype: 'form',
-                    itemId: 'rule-set-edit-form',
-                    layout: {
-                        type: 'vbox',
-                        align: 'stretch'
+                itemId: 'rule-set-edit-form',
+                layout: {
+                    type: 'vbox',
+                    align: 'stretch'
+                },
+                defaults: {
+                    labelWidth: 150,
+                    maxWidth: 600,
+                    allowBlank: false
+                },
+                items: [
+                    {
+                        itemId: 'form-errors',
+                        xtype: 'uni-form-error-message',
+                        name: 'form-errors',
+                        hidden: true,
+                        width: 600
                     },
-                    defaults: {
-                        labelWidth: 150,
-                        maxWidth: 600,
-                        allowBlank: false
+                    {
+                        xtype: 'textfield',
+                        fieldLabel: Uni.I18n.translate('estimationrulesets.add.name', 'EST', 'Name'),
+                        name: 'name',
+                        itemId: 'name',
+                        required: true
                     },
-                    items: [
-                        {
-                            itemId: 'form-errors',
-                            xtype: 'uni-form-error-message',
-                            name: 'form-errors',
-                            hidden: true,
-                            width: 600
-                        },
-                        {
-                            xtype: 'textfield',
-                            fieldLabel: Uni.I18n.translate('general.name', 'EST', 'Name'),
-                            name: 'name',
-                            itemId: 'name',
-                            required: true
-                        },
-                        {
-                            xtype: 'textareafield',
-                            fieldLabel: Uni.I18n.translate('general.description', 'EST', 'Description'),
-                            name: 'description',
-                            itemId: 'description',
-                            minHeight: 100
-                        }
-                    ],
-                    buttons: [
-                        {
-                            text: me.record ?
-                                Uni.I18n.translate('general.save', 'EST', 'Save') :
-                                Uni.I18n.translate('general.add', 'EST', 'Add'),
-                            ui: 'action',
-                            itemId: 'save-button'
-                        },
-                        {
-                            text: Uni.I18n.translate('general.cancel', 'EST', 'Cancel'),
-                            ui: 'link',
-                            itemId: 'cancel-button'
-                        }
-                    ]
-                }
+                    {
+                        xtype: 'textareafield',
+                        fieldLabel: Uni.I18n.translate('estimationrulesets.add.description', 'EST', 'Description'),
+                        name: 'description',
+                        itemId: 'description',
+                        minHeight: 100
+                    }
+                ],
+                buttons: [
+                    {
+                        text: me.record ?
+                            Uni.I18n.translate('general.save', 'EST', 'Save') :
+                            Uni.I18n.translate('general.add', 'EST', 'Add'),
+                        ui: 'action',
+                        itemId: 'save-button'
+                    },
+                    {
+                        text: Uni.I18n.translate('general.cancel', 'EST', 'Cancel'),
+                        ui: 'link',
+                        itemId: 'cancel-button'
+                    }
+                ]
             }
         ];
         this.callParent(arguments);
