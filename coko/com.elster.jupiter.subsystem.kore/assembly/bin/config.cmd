00ddcae9d7f2628a2ad5ccc6f87326dd0a852fe2
kbe <kristof.boeykens@elster.com>
2015-04-28 15:28:59 +0200

    Add checking of port usage in installation script + return version of script with "--version" option + add hostname in link to facts/flow

--- coko/com.elster.jupiter.subsystem.kore/assembly/bin/config.cmd
+++ coko/com.elster.jupiter.subsystem.kore/assembly/bin/config.cmd
@@ -1,3 +1,4 @@
+set HOST_NAME=localhost
 set CONNEXO_HTTP_PORT=8080
 set TOMCAT_HTTP_PORT=8081
 set SERVICE_VERSION=
@@ -8,19 +9,19 @@ set INSTALL_FLOW=yes
 set INSTALL_WSO2IS=
 
 set jdbcUrl=jdbc:oracle:thin:@doraps003.eict.vpdc:7163:ACCJUPITER
-set dbUserName=ACCJUPITER_xxx
+set dbUserName=xxx_test
 set dbPassword=zorro
 set CONNEXO_SERVICE=yes
 
 set FACTS_DB_HOST=doraps003.eict.vpdc
 set FACTS_DB_PORT=7163
 set FACTS_DB_NAME=ACCJUPITER
-set FACTS_DBUSER=ACCJUPITER_xxxfacts
+set FACTS_DBUSER=xxx_test_FACTS
 set FACTS_DBPASSWORD=zorro
 set FACTS_LICENSE=
 
 set FLOW_JDBC_URL=jdbc:oracle:thin:@doraps003.eict.vpdc:7163:ACCJUPITER
-set FLOW_DB_USER=ACCJUPITER_xxxflow
+set FLOW_DB_USER=xxx_test_FLOW
 set FLOW_DB_PASSWORD=zorro
 set SMTP_HOST=localhost
 set SMTP_PORT=25
