79c22af5b87bb4154d43e9e0d12c69dc8539d107
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-05-04 10:56:26 +0200

    Added

---
+++ copl/com.elster.jupiter.properties/src/main/java/com/elster/jupiter/properties/InstantFactory.java
@@ -0,0 +1,72 @@
+package com.elster.jupiter.properties;
+
+import com.elster.jupiter.util.Checks;
+
+import java.sql.SQLException;
+import java.time.Instant;
+
+/**
+ * Provides an implementation for the {@link ValueFactory} interface
+ * that supports {@link Instant}s and persists them as the number
+ * of millis since Jan 1st, 1970.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-04-24 (15:44)
+ */
+public class InstantFactory extends AbstractValueFactory<Instant> {
+
+    @Override
+    public Class<Instant> getValueType() {
+        return Instant.class;
+    }
+
+    @Override
+    public String getDatabaseTypeName () {
+        return "number";
+    }
+
+    @Override
+    public int getJdbcType() {
+        return java.sql.Types.INTEGER;
+    }
+
+    @Override
+    public Instant valueFromDatabase (Object object) throws SQLException {
+        return this.valueFromDatabase((Number) object);
+    }
+
+    private Instant valueFromDatabase (Number number) {
+        if (number != null) {
+            return Instant.ofEpochMilli(number.longValue());
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public Object valueToDatabase(Instant instant) {
+        return instant.toEpochMilli();
+    }
+
+    @Override
+    public Instant fromStringValue(String stringValue) {
+        if (Checks.is(stringValue).emptyOrOnlyWhiteSpace()) {
+            return null;
+        }
+        else {
+            return this.valueFromDatabase(new Long(stringValue));
+        }
+    }
+
+    @Override
+    public String toStringValue(Instant object) {
+        if (object != null) {
+            return this.valueToDatabase(object).toString();
+        }
+        else {
+            return "";
+        }
+    }
+
+}
