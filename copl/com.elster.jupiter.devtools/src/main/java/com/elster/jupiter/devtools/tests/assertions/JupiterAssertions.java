8fae46ec5ceac74ae0f0f0c15b35012fdd8cc111
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-05-05 10:08:56 +0200

    changes for assertj 3.0.0

--- copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/assertions/JupiterAssertions.java
+++ copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/assertions/JupiterAssertions.java
@@ -3,13 +3,7 @@ package com.elster.jupiter.devtools.tests.assertions;
 import com.elster.jupiter.devtools.tests.fakes.LogRecorder;
 import org.assertj.core.api.Assertions;
 
-import java.util.Optional;
-
 public class JupiterAssertions extends Assertions {
-
-    public static <T> OptionalAssert<T> assertThat(Optional<T> optional) {
-        return new OptionalAssert<>(optional);
-    }
 
     public static LogRecorderAssert assertThat(LogRecorder logRecorder) {
         return new LogRecorderAssert(logRecorder);
