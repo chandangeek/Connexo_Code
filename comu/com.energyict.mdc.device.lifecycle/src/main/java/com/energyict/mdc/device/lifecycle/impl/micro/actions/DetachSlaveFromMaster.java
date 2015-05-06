cdb68c4c2e56bc09f53194b01c39bf2c405cdf33
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-05-06 15:47:32 +0200

    COMU-500: Provide an implementation for the action that clears the physical gateway on a slave device

---
+++ comu/com.energyict.mdc.device.lifecycle/src/main/java/com/energyict/mdc/device/lifecycle/impl/micro/actions/DetachSlaveFromMaster.java
@@ -0,0 +1,55 @@
+package com.energyict.mdc.device.lifecycle.impl.micro.actions;
+
+import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.lifecycle.ExecutableActionProperty;
+import com.energyict.mdc.device.lifecycle.impl.ServerMicroAction;
+import com.energyict.mdc.device.topology.TopologyService;
+
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.PropertySpecService;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static com.energyict.mdc.device.lifecycle.impl.micro.actions.MicroActionPropertySupport.getOptionalEffectiveTimestamp;
+import static com.energyict.mdc.device.lifecycle.impl.micro.actions.MicroActionPropertySupport.optionalEffectiveTimestamp;
+
+/**
+ * Provides an implementation for the {@link ServerMicroAction} interface
+ * that will detach a slave Device from it physical master on the effective
+ * timestamp of the transition.
+ * @see {@link com.energyict.mdc.device.lifecycle.config.MicroAction#DETACH_SLAVE_FROM_MASTER}
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-05-06 (15:11)
+ */
+public class DetachSlaveFromMaster implements ServerMicroAction {
+
+    private final TopologyService topologyService;
+
+    public DetachSlaveFromMaster(TopologyService topologyService) {
+        super();
+        this.topologyService = topologyService;
+    }
+
+    @Override
+    public List<PropertySpec> getPropertySpecs(PropertySpecService propertySpecService) {
+        return Arrays.asList(optionalEffectiveTimestamp(propertySpecService));
+    }
+
+    @Override
+    public void execute(Device device, List<ExecutableActionProperty> properties) {
+        if (device.isLogicalSlave()) {
+            Optional<Instant> effectiveTimestamp = getOptionalEffectiveTimestamp(properties);
+            if (effectiveTimestamp.isPresent()) {
+                this.topologyService.clearPhysicalGateway(device, effectiveTimestamp.get());
+            }
+            else {
+                this.topologyService.clearPhysicalGateway(device);
+            }
+        }
+    }
+
+}
---
+++ comu/com.energyict.mdc.device.lifecycle/src/test/java/com/energyict/mdc/device/lifecycle/impl/micro/actions/DetachSlaveFromMasterTest.java
@@ -0,0 +1,131 @@
+package com.energyict.mdc.device.lifecycle.impl.micro.actions;
+
+import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.lifecycle.DeviceLifeCycleService;
+import com.energyict.mdc.device.lifecycle.ExecutableActionProperty;
+import com.energyict.mdc.device.topology.TopologyService;
+
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.PropertySpecService;
+import com.elster.jupiter.properties.ValueFactory;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests the {@link DetachSlaveFromMaster} component.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-05-06 (15:35)
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DetachSlaveFromMasterTest {
+
+    @Mock
+    private PropertySpecService propertySpecService;
+    @Mock
+    private TopologyService topologyService;
+    @Mock
+    private Device gateway;
+    @Mock
+    private Device slaveDevice;
+
+    @Before
+    public void initializeMocks() {
+        when(this.gateway.isLogicalSlave()).thenReturn(false);
+        when(this.slaveDevice.isLogicalSlave()).thenReturn(true);
+    }
+
+    @Test
+    public void testGetPropertySpecsDelegatesToPropertySpecService() {
+        DetachSlaveFromMaster microAction = this.getTestInstance();
+
+        // Business method
+        List<PropertySpec> propertySpecs = microAction.getPropertySpecs(this.propertySpecService);
+
+        // Asserts
+        int numberOfSpecs = propertySpecs.size();
+        verify(this.propertySpecService, times(numberOfSpecs))
+                .basicPropertySpec(
+                        anyString(),
+                        anyBoolean(),
+                        any(ValueFactory.class));
+    }
+
+    @Test
+    public void executeWithEffectiveTimestampClearsPhysicalGateway() {
+        Instant now = Instant.ofEpochSecond(97L);
+        DetachSlaveFromMaster microAction = this.getTestInstance();
+        ExecutableActionProperty property = mock(ExecutableActionProperty.class);
+        PropertySpec propertySpec = mock(PropertySpec.class);
+        when(propertySpec.getName()).thenReturn(DeviceLifeCycleService.MicroActionPropertyName.EFFECTIVE_TIMESTAMP.key());
+        when(property.getPropertySpec()).thenReturn(propertySpec);
+        when(property.getValue()).thenReturn(now);
+
+        // Business method
+        microAction.execute(this.slaveDevice, Arrays.asList(property));
+
+        // Asserts
+        verify(this.topologyService).clearPhysicalGateway(this.slaveDevice, now);
+    }
+
+    @Test
+    public void executeWithoutEffectiveTimeClearsPhysicalGateway() {
+        DetachSlaveFromMaster microAction = this.getTestInstance();
+
+        // Business method
+        microAction.execute(this.slaveDevice, Collections.emptyList());
+
+        // Asserts
+        verify(this.topologyService).clearPhysicalGateway(this.slaveDevice);
+    }
+
+    @Test
+    public void executeWithEffectiveTimestampDoesNotClearForGatewayDevice() {
+        Instant now = Instant.ofEpochSecond(97L);
+        DetachSlaveFromMaster microAction = this.getTestInstance();
+        ExecutableActionProperty property = mock(ExecutableActionProperty.class);
+        PropertySpec propertySpec = mock(PropertySpec.class);
+        when(propertySpec.getName()).thenReturn(DeviceLifeCycleService.MicroActionPropertyName.EFFECTIVE_TIMESTAMP.key());
+        when(property.getPropertySpec()).thenReturn(propertySpec);
+        when(property.getValue()).thenReturn(now);
+
+        // Business method
+        microAction.execute(this.gateway, Arrays.asList(property));
+
+        // Asserts
+        verify(this.topologyService, never()).clearPhysicalGateway(any(Device.class), any(Instant.class));
+    }
+
+    @Test
+    public void executeWithoutEffectiveTimestampDoesNotClearForGatewayDevice() {
+        DetachSlaveFromMaster microAction = this.getTestInstance();
+
+        // Business method
+        microAction.execute(this.gateway, Collections.emptyList());
+
+        // Asserts
+        verify(this.topologyService, never()).clearPhysicalGateway(any(Device.class));
+    }
+
+    private DetachSlaveFromMaster getTestInstance() {
+        return new DetachSlaveFromMaster(this.topologyService);
+    }
+
+}
