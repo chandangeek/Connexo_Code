7b5cb8564c86987adec23b94929c5aca12b69b4d
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-05-06 08:50:30 +0200

    COMU-380: QueryParameters now returning Optionals

--- comu/com.energyict.mdc.masterdata/src/test/java/com/energyict/mdc/masterdata/impl/MasterDataServiceImplTest.java
+++ comu/com.energyict.mdc.masterdata/src/test/java/com/energyict/mdc/masterdata/impl/MasterDataServiceImplTest.java
@@ -1,8 +1,9 @@
 package com.energyict.mdc.masterdata.impl;
 
-import com.energyict.mdc.common.rest.QueryParameters;
+import com.energyict.mdc.common.rest.JsonQueryParameters;
 import com.energyict.mdc.masterdata.RegisterType;
 import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -23,9 +24,9 @@ public class MasterDataServiceImplTest extends PersistenceTest {
 
     @Test
     public void testGetAllRegisterTypesSupportsPaging() throws Exception {
-        QueryParameters queryParameters = mock(QueryParameters.class);
-        when(queryParameters.getLimit()).thenReturn(10);
-        when(queryParameters.getStart()).thenReturn(10);
+        JsonQueryParameters queryParameters = mock(JsonQueryParameters.class);
+        when(queryParameters.getLimit()).thenReturn(Optional.of(10));
+        when(queryParameters.getStart()).thenReturn(Optional.of(10));
         List<RegisterType> registerTypes = inMemoryPersistence.getMasterDataService().findAllRegisterTypes().from(queryParameters).find();
         assertThat(registerTypes).hasSize(11);
     }
