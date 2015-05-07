ffdb82d1824e46a4d6ca664086002960b0c24ca4
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-05-07 15:18:34 +0200

    COMU-832: Moving default mappers to Whiteboard

--- coko/com.elster.jupiter.estimation.rest/src/main/java/com/elster/jupiter/estimation/rest/impl/EstimationApplication.java
+++ coko/com.elster.jupiter.estimation.rest/src/main/java/com/elster/jupiter/estimation/rest/impl/EstimationApplication.java
@@ -10,18 +10,16 @@ import com.elster.jupiter.rest.util.BinderProvider;
 import com.elster.jupiter.rest.util.ConstraintViolationExceptionMapper;
 import com.elster.jupiter.rest.util.ConstraintViolationInfo;
 import com.elster.jupiter.rest.util.LocalizedExceptionMapper;
-import com.elster.jupiter.rest.util.LocalizedFieldValidationExceptionMapper;
 import com.elster.jupiter.rest.util.RestQueryService;
 import com.elster.jupiter.time.TimeService;
 import com.elster.jupiter.transaction.TransactionService;
 import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
 import org.glassfish.hk2.utilities.Binder;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-
-import javax.ws.rs.core.Application;
-import java.util.Set;
 
 @Component(name = "com.elster.jupiter.estimation.rest", service = Application.class, immediate = true, property = {"alias=/est", "app=SYS", "name=" + EstimationApplication.COMPONENT_NAME})
 public class EstimationApplication extends Application implements BinderProvider {
@@ -40,8 +38,6 @@ public class EstimationApplication extends Application implements BinderProvider
     public Set<Class<?>> getClasses() {
         return ImmutableSet.<Class<?>>of(
                 EstimationResource.class,
-                LocalizedExceptionMapper.class,
-                LocalizedFieldValidationExceptionMapper.class,
                 ConstraintViolationExceptionMapper.class);
     }
 
