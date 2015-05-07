24c23240d4e3fb1004276cdf4395d0ed758ae458
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-05-07 15:34:36 +0200

    COMU-832: Moved ConstraintViolationExceptionMapper to Whiteboard

--- coko/com.elster.jupiter.appserver.rest/src/main/java/com/elster/jupiter/appserver/rest/impl/AppServerApplication.java
+++ coko/com.elster.jupiter.appserver.rest/src/main/java/com/elster/jupiter/appserver/rest/impl/AppServerApplication.java
@@ -6,7 +6,6 @@ import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.callback.InstallService;
-import com.elster.jupiter.rest.util.ConstraintViolationExceptionMapper;
 import com.elster.jupiter.rest.util.ConstraintViolationInfo;
 import com.elster.jupiter.rest.util.RestQueryService;
 import com.elster.jupiter.transaction.TransactionService;
@@ -39,7 +38,6 @@ public class AppServerApplication extends Application implements InstallService 
 
     public Set<Class<?>> getClasses() {
         return ImmutableSet.<Class<?>>of(
-                ConstraintViolationExceptionMapper.class,
                 AppServerResource.class);
     }
     @Reference
