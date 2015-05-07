c44c8c080b5426ebbb05c730062cdb5255e6fc12
dvy <dieter.vyncke@elster.com>
2015-05-07 11:11:23 +0200

    COPL-495 - Export screen to CSV for channel data, register data and load profile data

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/form/field/IntervalFlagsDisplay.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/form/field/IntervalFlagsDisplay.js
@@ -10,17 +10,22 @@ Ext.define('Uni.form.field.IntervalFlagsDisplay', {
 
     renderer: function (value) {
         var result,
-            tooltip = '';
+            tooltip = '',
+            flags = '<span style="display:none">(';
+
 
         if (Ext.isArray(value) && value.length) {
             result = '<span style="display: inline-block; width: 25px; float: left;">' + value.length + '</span>';
             Ext.Array.each(value, function (value, index) {
                 index++;
                 tooltip += Uni.I18n.translate('intervalFlags.Flag', 'UNI', 'Flag') + ' ' + index + ': ' + value + '<br>';
+                flags += value + '/';
             });
-            result += '<span class="uni-icon-info-small" style="display: inline-block; width: 16px; height: 16px; float: left;" data-qtip="' + Ext.htmlEncode(tooltip) + '"></span>';
+            flags = flags.slice(0,-1);
+            flags += ')</span>';
+            result = result + flags + '<span class="uni-icon-info-small" style="display: inline-block; width: 16px; height: 16px; float: left;" data-qtip="' + Ext.htmlEncode(tooltip) + '"></span>';
         }
-
+        console.log(result);
         return result || this.emptyText;
     }
 });
