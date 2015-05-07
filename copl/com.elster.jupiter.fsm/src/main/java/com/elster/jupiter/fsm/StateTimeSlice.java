96cce8305e8a0b5474d22f3cf3d8fe08a924ec3b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-05-07 11:08:44 +0200

    COMU-501: add support to get history of device status

---
+++ copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/StateTimeSlice.java
@@ -0,0 +1,24 @@
+package com.elster.jupiter.fsm;
+
+import com.google.common.collect.Range;
+
+import java.time.Instant;
+
+/**
+ * Models a slice of the {@link StateTimeline}
+ * and specifies the {@link State} of an Object
+ * during a period in time.
+ * The period in time is of type "closed-open"
+ * or to use the conventions of the Range class:
+ * {@code [a..b)} or in mathematical writing {@code {x | a <= x < b}}
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-05-07 (08:36)
+ */
+public interface StateTimeSlice {
+
+    public Range<Instant> getPeriod();
+
+    public State getState();
+
+}
---
+++ copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/StateTimeline.java
@@ -0,0 +1,23 @@
+package com.elster.jupiter.fsm;
+
+import java.util.List;
+
+/**
+ * Models timeline of the state changes of a statefull object.
+ * A StateTimeline is effectively a collection of {@link StateTimeSlice slices}.
+ * The timeline should not have any gaps because an object is
+ * always in some state until it is being deleted.
+ * Therefore, each slice abuts with the previous.
+ * In other words:
+ * <code>[t1, t2), [t2, t3), ... [tn-1, tn)</code>
+ * is an example of the timeline where the end
+ * of each slice is the start of the next.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-05-07 (08:31)
+ */
+public interface StateTimeline {
+
+    public List<StateTimeSlice> getSlices();
+
+}
