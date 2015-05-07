31c7e89b13e883715f352b360a33d88e6cc6ab78
Vitaliy Nishukov <nishukov@mera.ru>
2015-05-07 11:09:19 +0300

    Add schedule to EstimationTaskHistory

--- coko/com.elster.jupiter.estimation.rest/src/main/java/com/elster/jupiter/estimation/rest/impl/EstimationTaskHistoryInfo.java
+++ coko/com.elster.jupiter.estimation.rest/src/main/java/com/elster/jupiter/estimation/rest/impl/EstimationTaskHistoryInfo.java
@@ -5,8 +5,14 @@ import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.History;
 import com.elster.jupiter.tasks.TaskOccurrence;
 import com.elster.jupiter.tasks.TaskStatus;
+import com.elster.jupiter.time.PeriodicalScheduleExpression;
+import com.elster.jupiter.time.TemporalExpression;
+import com.elster.jupiter.time.rest.PeriodicalExpressionInfo;
+import com.elster.jupiter.util.time.Never;
+import com.elster.jupiter.util.time.ScheduleExpression;
 
 import java.time.Instant;
+import java.util.Optional;
 
 public class EstimationTaskHistoryInfo {
 
@@ -41,18 +47,17 @@ public class EstimationTaskHistoryInfo {
                         .orElse(estimationTask));
         task = new EstimationTaskInfo();
         task.populate(version, thesaurus);
-
-//        Optional<ScheduleExpression> foundSchedule = version.getScheduleExpression(taskOccurrence.getTriggerTime());
-//        if (!foundSchedule.isPresent() || Never.NEVER.equals(foundSchedule.get())) {
-//            task.schedule = null;
-//        } else if (foundSchedule.isPresent()) {
-//            ScheduleExpression scheduleExpression = foundSchedule.get();
-//            if (scheduleExpression instanceof TemporalExpression) {
-//                task.schedule = new PeriodicalExpressionInfo((TemporalExpression) scheduleExpression);
-//            } else {
-//                task.schedule = PeriodicalExpressionInfo.from((PeriodicalScheduleExpression) scheduleExpression);
-//            }
-//        }
+        Optional<ScheduleExpression> foundSchedule = version.getScheduleExpression(taskOccurrence.getTriggerTime());
+        if (!foundSchedule.isPresent() || Never.NEVER.equals(foundSchedule.get())) {
+            task.schedule = null;
+        } else if (foundSchedule.isPresent()) {
+            ScheduleExpression scheduleExpression = foundSchedule.get();
+            if (scheduleExpression instanceof TemporalExpression) {
+                task.schedule = new PeriodicalExpressionInfo((TemporalExpression) scheduleExpression);
+            } else {
+                task.schedule = PeriodicalExpressionInfo.from((PeriodicalScheduleExpression) scheduleExpression);
+            }
+        }
     }
 
     private static String getName(TaskStatus status, Thesaurus thesaurus) {
@@ -83,61 +88,4 @@ public class EstimationTaskHistoryInfo {
             this.statusPrefix = statusTranslation;
         }
     }
-
-//    private String getScheduledTriggerDescription(TaskOccurrence estimationTaskOccurrence, Thesaurus thesaurus, TimeService timeService) {
-//        ScheduleExpression scheduleExpression = estimationTaskOccurrence.getTask().getScheduleExpression();
-//        if (Never.NEVER.equals(scheduleExpression)) {
-//            return null;
-//        }
-//        if (scheduleExpression instanceof PeriodicalScheduleExpression) {
-//            return fromPeriodicalScheduleExpression((PeriodicalScheduleExpression) scheduleExpression, timeService);
-//        }
-//        if (scheduleExpression instanceof TemporalExpression) {
-//            return fromTemporalExpression((TemporalExpression) scheduleExpression, thesaurus);
-//        }
-//        return scheduleExpression.toString();
-//    }
-
-//    private String fromPeriodicalScheduleExpression(PeriodicalScheduleExpression scheduleExpression, TimeService timeService) {
-//        return timeService.toLocalizedString(scheduleExpression);
-//    }
-//
-//    private String fromTemporalExpression(TemporalExpression scheduleExpression, Thesaurus thesaurus) {
-//        TimeDuration every = scheduleExpression.getEvery();
-//        int count = every.getCount();
-//        TimeDuration.TimeUnit unit = every.getTimeUnit();
-//        String everyTranslation = thesaurus.getString("every", "every");
-//
-//        String unitTranslation = unit.getDescription();
-//        if (unit.equals(TimeDuration.TimeUnit.DAYS)) {
-//            if (count == 1) {
-//                unitTranslation = thesaurus.getString("day", "day");
-//            } else {
-//                unitTranslation = thesaurus.getString("multipleDays", "days");
-//            }
-//        } else if (unit.equals(TimeDuration.TimeUnit.WEEKS)) {
-//            if (count == 1) {
-//                unitTranslation = thesaurus.getString("week", "week");
-//            } else {
-//                unitTranslation = thesaurus.getString("multipleWeeks", "weeks");
-//            }
-//        } else if (unit.equals(TimeDuration.TimeUnit.MONTHS)) {
-//            if (count == 1) {
-//                unitTranslation = thesaurus.getString("month", "month");
-//            } else {
-//                unitTranslation = thesaurus.getString("multipleMonths", "months");
-//            }
-//        } else if (unit.equals(TimeDuration.TimeUnit.YEARS)) {
-//            if (count == 1) {
-//                unitTranslation = thesaurus.getString("year", "year");
-//            } else {
-//                unitTranslation = thesaurus.getString("multipleYears", "years");
-//            }
-//        }
-//        if (count == 1) {
-//            return everyTranslation + " " + unitTranslation;
-//        } else {
-//            return everyTranslation + " " + count + " " + unitTranslation;
-//        }
-//    }
 }
