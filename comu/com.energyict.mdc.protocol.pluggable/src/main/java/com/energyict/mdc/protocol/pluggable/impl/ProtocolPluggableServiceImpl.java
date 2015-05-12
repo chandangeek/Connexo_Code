a9162cd40087c03eeeb43b136a53bced709f1070
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-05-12 14:27:22 +0200

    COMU-501: set CIM device life cycle dates in response to device state change events

--- comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/ProtocolPluggableServiceImpl.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/ProtocolPluggableServiceImpl.java
@@ -618,6 +618,7 @@ public class ProtocolPluggableServiceImpl implements ProtocolPluggableService, I
         this.meteringService = meteringService;
     }
 
+    @Override
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
     public void addDeviceProtocolService(DeviceProtocolService deviceProtocolService) {
         this.deviceProtocolServices.add(deviceProtocolService);
@@ -653,6 +654,7 @@ public class ProtocolPluggableServiceImpl implements ProtocolPluggableService, I
         this.deviceProtocolSecurityServices.remove(deviceProtocolSecurityService);
     }
 
+    @Override
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
     public void addLicensedProtocolService(LicensedProtocolService licensedProtocolService) {
         this.licensedProtocolServices.add(licensedProtocolService);
@@ -663,6 +665,7 @@ public class ProtocolPluggableServiceImpl implements ProtocolPluggableService, I
         this.licensedProtocolServices.remove(licensedProtocolService);
     }
 
+    @Override
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
     public void addInboundDeviceProtocolService(InboundDeviceProtocolService inboundDeviceProtocolService) {
         this.inboundDeviceProtocolServices.add(inboundDeviceProtocolService);
@@ -705,6 +708,7 @@ public class ProtocolPluggableServiceImpl implements ProtocolPluggableService, I
         return connectionTypeServices;
     }
 
+    @Override
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
     public void addConnectionTypeService(ConnectionTypeService connectionTypeService) {
         this.connectionTypeServices.add(connectionTypeService);
