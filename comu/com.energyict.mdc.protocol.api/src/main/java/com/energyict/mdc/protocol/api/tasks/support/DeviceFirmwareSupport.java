b4fa025fefa0915fcb78842dc41edf115fab6e5b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-05-15 11:45:22 +0200

    Fix compile errors after merge device-life-cycle into master

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/tasks/support/DeviceFirmwareSupport.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/tasks/support/DeviceFirmwareSupport.java
@@ -3,13 +3,10 @@ package com.energyict.mdc.protocol.api.tasks.support;
 import com.energyict.mdc.protocol.api.device.data.CollectedFirmwareVersion;
 
 /**
- * Defines functionality related to Firmware Version information
+ * Defines functionality related to Firmware Version information.
  */
 public interface DeviceFirmwareSupport {
 
-    /**
-     * @return the collected FirmwareVersion
-     */
     CollectedFirmwareVersion getFirmwareVersions();
 
 }
