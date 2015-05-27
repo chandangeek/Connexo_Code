45c9e0c6d27b17e8db5bb66b5b41f1fb801fd096
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-05-27 09:56:53 +0200

    Added paging restriction

--- copl/com.elster.jupiter.devtools.rest/src/main/java/com/elster/jupiter/devtools/rest/ObjectMapperProvider.java
+++ copl/com.elster.jupiter.devtools.rest/src/main/java/com/elster/jupiter/devtools/rest/ObjectMapperProvider.java
@@ -1,11 +1,6 @@
 package com.elster.jupiter.devtools.rest;
 
-import java.io.IOException;
-import java.time.Instant;
-
-import javax.ws.rs.ext.ContextResolver;
-
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -20,6 +15,9 @@ import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import java.io.IOException;
+import java.time.Instant;
+import javax.ws.rs.ext.ContextResolver;
 
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
 	
@@ -30,7 +28,7 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
 		AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
 	    AnnotationIntrospector secondary = new JaxbAnnotationIntrospector(mapper.getTypeFactory());
 	    AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
-		//mapper.setSerializationInclusion(Include.NON_NULL);
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		mapper.setAnnotationIntrospector(pair);
 		mapper.registerModule(new SimpleModule("Instant", new Version(1,0,0,null, "com.elster.jupiter", "devtools.rest"))
 			.addSerializer(new InstantSerializer())
