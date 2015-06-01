c929e7077df9dfd5a610cbff1065ff4cc246f0df
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-06-01 14:15:45 +0200

    COMU-1004: Add support to group properties, determine if they are sticky or not, determine their selection mode (single or multi).

--- copl/com.elster.jupiter.search/src/main/java/com/elster/jupiter/search/SearchableProperty.java
+++ copl/com.elster.jupiter.search/src/main/java/com/elster/jupiter/search/SearchableProperty.java
@@ -1,9 +1,11 @@
 package com.elster.jupiter.search;
 
+import aQute.bnd.annotation.ProviderType;
 import com.elster.jupiter.properties.PropertySpec;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Models a property of a {@link SearchDomain domain class}
@@ -25,15 +27,71 @@ import java.util.Objects;
  * it is not possible for the UI to compile a
  * list of "subclasses".
  * </p>
+ * <p>
+ * Currently, the fact that a {@link PropertySpec} can be
+ * "required" or "optional" is not.
+ * A property's visibility is either "sticky" or "removable".
+ * Sticky properties are intended to be always visible on the
+ * search screen while "removable" ones may be removed by the
+ * user when he no longer wants to search against that property.
+ * </p>
+ * <p>
+ * A SearchableProperty can be part of a group and all
+ * properties of the same group are expected to be rendered
+ * together in the UI to indicate to the user that they are
+ * somehow related. A group could e.g. be used to indicate
+ * that the properties in the group actually belong to
+ * a related object of the actual SearchDomain.
+ * </p>
  *
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2015-05-26 (14:34)
  */
+@ProviderType
 public interface SearchableProperty {
+
+    /**
+     * Models the visibility of a SearchableProperty.
+     */
+    public enum Visibility {
+        /**
+         * The SearchableProperty sticks on the screen
+         * and is therefor always visible.
+         */
+        STICKY,
+
+        /**
+         * The SearchableProperty is not necessarily
+         * always visible, it can be removed by the
+         * user when no longer necessary.
+         */
+        REMOVABLE;
+    }
+
+    /**
+     * Determines selection mode of the SearchableProperty.
+     */
+    public enum SelectionMode {
+        /**
+         * Supports only a single value to be specified/selected at the same time.
+         */
+        SINGLE,
+
+        /**
+         * Supports multiple values to be specified/selected at the same time.
+         */
+        MULTI;
+    }
 
     public SearchDomain getDomain();
 
+    public Optional<SearchablePropertyGroup> getGroup();
+
     public PropertySpec getSpecification();
+
+    public Visibility getVisibility();
+
+    public SelectionMode getSelectionMode();
 
     /**
      * Tests if the {@link PropertySpec specification}
---
+++ copl/com.elster.jupiter.search/src/main/java/com/elster/jupiter/search/SearchablePropertyGroup.java
@@ -0,0 +1,20 @@
+package com.elster.jupiter.search;
+
+/**
+ * Groups {@link SearchableProperty SearchableProperties}
+ * to indicate to the UI that these properties are all
+ * linked and should somehow be rendered as a group.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-06-01 (13:52)
+ */
+public interface SearchablePropertyGroup {
+
+    /**
+     * Gets a unique identifier for this SearchablePropertyGroup.
+     *
+     * @return The unique identifier
+     */
+    public String getId();
+
+}
