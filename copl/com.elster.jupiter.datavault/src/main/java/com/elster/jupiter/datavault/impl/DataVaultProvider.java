ef697675a8c60f8eb8edf787bae68aee1d1523ca
Bruno Ballekens <bruno.ballekens@elster.com>
2015-06-01 12:45:10 +0200

    Performance improvements: caching of some master data (cherry picked from commit ca09c55)

--- copl/com.elster.jupiter.datavault/src/main/java/com/elster/jupiter/datavault/impl/DataVaultProvider.java
+++ copl/com.elster.jupiter.datavault/src/main/java/com/elster/jupiter/datavault/impl/DataVaultProvider.java
@@ -2,12 +2,14 @@ package com.elster.jupiter.datavault.impl;
 
 import com.elster.jupiter.datavault.DataVault;
 import com.elster.jupiter.orm.DataModel;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import javax.inject.Inject;
-import javax.inject.Provider;
+import java.util.Optional;
 
 /**
  * Created by bvn on 11/7/14.
@@ -16,6 +18,7 @@ public class DataVaultProvider implements Provider<DataVault> {
 
     private final DataModel dataModel;
     private final ExceptionFactory exceptionFactory;
+    private Optional<OrmKeyStoreImpl> ormKeyStore = Optional.empty();
 
     @Inject
     public DataVaultProvider(DataModel dataModel, ExceptionFactory exceptionFactory) {
@@ -25,20 +28,30 @@ public class DataVaultProvider implements Provider<DataVault> {
 
     @Override
     public DataVault get() {
-        List<OrmKeyStoreImpl> ormKeyStores = dataModel.mapper(OrmKeyStoreImpl.class).find();
         KeyStoreDataVault dataVault = dataModel.getInstance(KeyStoreDataVault.class);
-        if (ormKeyStores.size()>1) {
-            throw exceptionFactory.newException(MessageSeeds.AMBIGUOUS_KEYSTORE);
+        getOrmKeyStore().ifPresent(keyStore -> this.initDataVault(dataVault, keyStore));
+        return dataVault;
+    }
+
+    private void initDataVault(KeyStoreDataVault dataVault, OrmKeyStoreImpl keyStore) {
+        try (InputStream inputStream = new ByteArrayInputStream(keyStore.getKeyStoreBytes())) {
+            dataVault.readKeyStore(inputStream);
+        } catch (IOException e) {
+            throw exceptionFactory.newException(MessageSeeds.KEYSTORE_CREATION_FAILED, keyStore.getId());
+        }
+    }
+
+    private synchronized Optional<OrmKeyStoreImpl> getOrmKeyStore() {
+        if (!ormKeyStore.isPresent()) {
+            List<OrmKeyStoreImpl> ormKeyStores = dataModel.mapper(OrmKeyStoreImpl.class).find();
+            if (ormKeyStores.size() > 1) {
+                throw exceptionFactory.newException(MessageSeeds.AMBIGUOUS_KEYSTORE);
 //        } else if (ormKeyStores.isEmpty()) {
 //            throw exceptionFactory.newException(MessageSeeds.NO_KEYSTORE);
-        } else if (ormKeyStores.size()==1) {
-            try (InputStream inputStream = new ByteArrayInputStream(ormKeyStores.get(0).getKeyStoreBytes())) {
-                dataVault.readKeyStore(inputStream);
-            } catch (IOException e) {
-                throw exceptionFactory.newException(MessageSeeds.KEYSTORE_CREATION_FAILED, ormKeyStores.get(0).getId());
+            } else if (ormKeyStores.size() == 1) {
+                ormKeyStore = Optional.of(ormKeyStores.get(0));
             }
         }
-
-        return dataVault;
+        return ormKeyStore;
     }
 }
