43a5e1661907d28bbab2a843f867c89ec18343b2
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-06-04 11:39:08 +0200

    COMU-1004: Added InfoService

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/DeviceInfoFactory.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/DeviceInfoFactory.java
@@ -1,39 +1,39 @@
 package com.energyict.mdc.device.data.rest;
 
 import com.elster.jupiter.issue.share.service.IssueService;
-import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.rest.util.InfoFactory;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.imp.DeviceImportService;
 import com.energyict.mdc.device.data.rest.impl.DeviceInfo;
 import com.energyict.mdc.device.data.rest.impl.DeviceTopologyInfo;
 import com.energyict.mdc.device.topology.TopologyService;
-
-import javax.inject.Inject;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.inject.Inject;
+import org.osgi.service.component.annotations.Component;
 
-public class DeviceInfoFactory {
 
+@Component(name="device.info.factory", service = { InfoFactory.class, Device.class }, immediate = true)
+public class DeviceInfoFactory implements InfoFactory<Device> {
     private final Thesaurus thesaurus;
     private final  DeviceImportService deviceImportService;
     private final TopologyService topologyService;
     private final IssueService issueService;
-    private final MeteringService meteringService;
 
     @Inject
-    public DeviceInfoFactory(Thesaurus thesaurus, DeviceImportService deviceImportService, TopologyService topologyService, IssueService issueService, MeteringService meteringService) {
+    public DeviceInfoFactory(Thesaurus thesaurus, DeviceImportService deviceImportService, TopologyService topologyService, IssueService issueService) {
         this.thesaurus = thesaurus;
         this.deviceImportService = deviceImportService;
         this.topologyService = topologyService;
         this.issueService = issueService;
-        this.meteringService = meteringService;
     }
 
     public List<DeviceInfo> from(List<Device> devices) {
         return devices.stream().map(this::from).collect(Collectors.toList());
     }
 
+    @Override
     public DeviceInfo from(Device device){
         return DeviceInfo.from(device);
     }
@@ -42,4 +42,17 @@ public class DeviceInfoFactory {
         return DeviceInfo.from(device, slaveDevices, deviceImportService, topologyService, issueService, thesaurus);
     }
 
+//    @Override
+//    public Object from(Device domainObject) {
+//        if (Device.class.isAssignableFrom(domainObject.getClass())) {
+//            return this.from((Device)domainObject);
+//        } else {
+//            throw new IllegalArgumentException("Can only convert Devices");
+//        }
+//    }
+
+    @Override
+    public Class<Device> getDomainClass() {
+        return Device.class;
+    }
 }
