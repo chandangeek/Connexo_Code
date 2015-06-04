b1de5dcb315ec83627ba74a801059f8ec884e540
Lucian Timar <lucian.timar@elster.com>
2015-06-04 15:38:42 +0300

    add status default names

--- copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/Status.java
+++ copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/Status.java
@@ -1,9 +1,24 @@
 package com.elster.jupiter.fileimport;
 
 /**
- * Enumeration of the states of a FileImport.
- * A FileImport starts out as new. When moved to processing becomes PROCESSING and upon completion becomes either SUCCESS or FAILURE, depending on the success of processing.
+ * Enumeration of the states of a FileImportOccurrence.
+ * A FileImportOccurrence starts out as new. When moved to processing becomes PROCESSING and upon completion becomes either SUCCESS or FAILURE, depending on the success of processing.
  */
 public enum Status {
-    NEW, PROCESSING, SUCCESS, SUCCESS_WITH_FAILURES, FAILURE
+    NEW("New"),
+    PROCESSING("Busy"),
+    SUCCESS("Success"),
+    SUCCESS_WITH_FAILURES("Success with failures"),
+    FAILURE("Failure");
+
+    private String name;
+
+    Status(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
