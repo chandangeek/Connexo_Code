db6364453c8ca0ed0c645406d86f30c8c8829db0
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-06-04 14:08:01 +0200

    COMU-1004: Fixed InfoFactoryService

--- copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/InfoFactoryWhiteBoard.java
+++ copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/InfoFactoryWhiteBoard.java
@@ -1,11 +1,9 @@
 package com.elster.jupiter.rest.whiteboard.impl;
 
 import com.elster.jupiter.rest.util.InfoFactory;
-import com.elster.jupiter.rest.util.InfoService;
-import java.util.ArrayList;
+import com.elster.jupiter.rest.util.InfoFactoryService;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
@@ -15,31 +13,30 @@ import org.osgi.service.component.annotations.ReferencePolicy;
  * Created by bvn on 6/4/15.
  */
 
-@Component(name = "com.elster.jupiter.info.whiteboard.implementation" , immediate = true , service = {InfoService.class}, property = "name=INFO" )
-public class InfoFactoryWhiteBoard implements InfoService {
+/**
+ * Registers which InfoFactory can be used to convert which domain object. Used by the dynamicSearchResource
+ */
+@Component(name = "com.elster.jupiter.info.whiteboard.implementation" , immediate = true , service = {InfoFactoryService.class} )
+public class InfoFactoryWhiteBoard implements InfoFactoryService {
 
-    private final List<InfoFactory<?>> factories = new ArrayList<>();
+    private final List<InfoFactory> factories = new CopyOnWriteArrayList<>();
 
     @Reference(name="Z-Order",cardinality= ReferenceCardinality.MULTIPLE,policy= ReferencePolicy.DYNAMIC)
-    public void addFactory(InfoFactory infoFactory, Map<String,Object> properties) {
+    public void addFactory(InfoFactory infoFactory) {
         factories.add(infoFactory);
     }
 
-    private String getDomainClass(Map<String, Object> properties) {
-        return (String) Optional.ofNullable(properties.get("domainClass")).orElseThrow(() -> new IllegalStateException("Expected class property"));
-    }
-
-    public void removeFactory(InfoFactory infoFactory,Map<String,Object> properties) {
+    public void removeFactory(InfoFactory infoFactory) {
         factories.removeIf(fac -> infoFactory.getDomainClass().equals(fac.getDomainClass()));
     }
 
 
     @Override
     public Object from(Object domainObject) {
-        InfoFactory infoFactory = factories.stream().
+        return factories.stream().
                 filter(fac -> fac.getDomainClass().isAssignableFrom(domainObject.getClass())).
                 findFirst().
-                orElseThrow(() -> new IllegalStateException("No registered factory for " + domainObject.getClass()));
-        return infoFactory.from(infoFactory.getDomainClass().cast(domainObject));
+                orElseThrow(() -> new IllegalStateException("No registered factory for " + domainObject.getClass())).
+                from(domainObject);
     }
 }
