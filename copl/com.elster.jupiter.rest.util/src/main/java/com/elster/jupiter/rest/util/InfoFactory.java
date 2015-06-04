ea66c1861d97982d60988ad4745d20d8c550dcc4
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-06-04 14:08:01 +0200

    COMU-1004: Fixed InfoFactoryService

--- copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/InfoFactory.java
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/InfoFactory.java
@@ -1,16 +1,19 @@
 package com.elster.jupiter.rest.util;
 
 /**
- * Created by bvn on 6/4/15.
+ * Allows creation of an Info object based on a domain object.
  */
 public interface InfoFactory<T> {
 
     /**
      * Converts a DomainObject to an Info object
      * @param domainObject The domain object (DeviceImpl, ComServerImpl, FirmwareVersion, ....
-     * @return Some Info type
+     * @return Some Info object
      */
     public Object from(T domainObject);
 
+    /**
+     * Base-class any domain object for this factory should obey IS-A relation to.
+     */
     public Class<T> getDomainClass();
 }
--- copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/InfoFactoryService.java
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/InfoFactoryService.java
@@ -3,7 +3,7 @@ package com.elster.jupiter.rest.util;
 /**
  * Created by bvn on 6/4/15.
  */
-public interface InfoService {
+public interface InfoFactoryService {
 
     public Object from(Object domainObject);
 }
