36bf16bf2ae97a3a28555996fe28d08b320980e3
gna <govanni.naessens@elster.com>
2015-06-08 10:46:59 +0200

    Added the proper getPrerequisiteModules DLD and DLC

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceConfigurationServiceImpl.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceConfigurationServiceImpl.java
@@ -454,7 +454,7 @@ public class DeviceConfigurationServiceImpl implements ServerDeviceConfiguration
 
     @Override
     public List<String> getPrerequisiteModules() {
-        return Arrays.asList("ORM", "EVT", "NLS", "USR", "MDS", "CPC", "MDC", "SCH", "CTS", "VAL", "EST");
+        return Arrays.asList("ORM", "EVT", "NLS", "USR", "MDS", "CPC", "MDC", "SCH", "CTS", "VAL", "EST", "DLD", "DLC");
     }
 
     @Reference
