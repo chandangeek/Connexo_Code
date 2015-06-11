bfd3fcc3629e3e15d3ffa353196b9137abf4f9ec
Pasquien Dorny <pasquien.dorny@elster.com>
2015-06-11 10:33:24 +0200

    COMU-1065:State 'In Stock' should be 'In stock'

--- comu/com.energyict.mdc.device.lifecycle.config.rest/src/main/java/com/energyict/mdc/device/lifecycle/config/rest/info/StateTransitionEventTypeInfo.java
+++ comu/com.energyict.mdc.device.lifecycle.config.rest/src/main/java/com/energyict/mdc/device/lifecycle/config/rest/info/StateTransitionEventTypeInfo.java
@@ -1,8 +1,5 @@
 package com.energyict.mdc.device.lifecycle.config.rest.info;
 
-import com.elster.jupiter.fsm.StateTransitionEventType;
-import com.elster.jupiter.nls.Thesaurus;
-
 public class StateTransitionEventTypeInfo {
 
     public String symbol;
