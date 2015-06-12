71659918d8c054d3935aaebbf449cd8e9b35d526
Geert <geert.delmeiren@elster.com>
2015-06-12 11:05:49 +0200

    COMU-623 - Cancel firmware campaign Before cancelling the campaign show a message

--- comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/firmwarecampaigns/controller/Overview.js
+++ comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/firmwarecampaigns/controller/Overview.js
@@ -55,25 +55,43 @@ Ext.define('Fwc.firmwarecampaigns.controller.Overview', {
     },
 
     chooseAction: function (menu, item) {
-        var me = this,
-            record = menu.record,
-            form = this.getPreview().down('form'),
-            store = this.getStore('Fwc.firmwarecampaigns.store.FirmwareCampaigns');
-
         switch (item.action) {
-            case 'cancelCampaign':
-                store.getProxy().url = '/api/fwc/campaigns/' + record.id;
-                record.set('status', {id: "CANCELLED", localizedValue: "Cancelled"});
-                record.save({
-                    callback: function (model, operation) {
-                        store.getProxy().url = '/api/fwc/campaigns/';
-                        if (operation.success) {
-                            form.loadRecord(model);
-                            me.getApplication().fireEvent('acknowledge', Uni.I18n.translate('firmware.campaigns.cancelled', 'FWC', 'Firmware campaign cancelled'));
-                        }
-                    }
-                });
+            case 'cancelCampaign': this.onCancelCampaign(menu.record);
                 break;
         }
+    },
+
+    onCancelCampaign : function(record) {
+        var me = this,
+            confirmationWindow = Ext.create('Uni.view.window.Confirmation', {
+                confirmText: Uni.I18n.translate('firmware.campaigns.cancelCampaign', 'FWC', 'Cancel campaign'),
+                confirmation: function () {
+                    me.doCancelCampaign(record);
+                }
+            });
+
+        confirmationWindow.show({
+            msg: Uni.I18n.translate('firmware.campaigns.cancel.msg', 'FWC', 'Cancel the firmware upload for all scheduled devices. Ongoing uploads will not be terminated and successful uploads will not be reversed.'),
+            title: Ext.String.format(
+                Uni.I18n.translate('firmware.campaigns.cancel.title', 'FWC', "Cancel firmware campaign '{0}'?"),
+                record.get('name'))
+        });
+    },
+
+    doCancelCampaign : function(record) {
+        var me = this,
+            form = this.getPreview().down('form'),
+            store = this.getStore('Fwc.firmwarecampaigns.store.FirmwareCampaigns');
+        store.getProxy().url = '/api/fwc/campaigns/' + record.id;
+        record.set('status', {id: "CANCELLED", localizedValue: "Cancelled"});
+        record.save({
+            callback: function (model, operation) {
+                store.getProxy().url = '/api/fwc/campaigns/';
+                if (operation.success) {
+                    form.loadRecord(model);
+                    me.getApplication().fireEvent('acknowledge', Uni.I18n.translate('firmware.campaigns.cancelled', 'FWC', 'Firmware campaign cancelled'));
+                }
+            }
+        });
     }
 });
