f4885f6ef0520b6ba1f31f3fb55cd06111308345
gna <govanni.naessens@elster.com>
2015-06-25 13:36:03 +0200

    Added two gogo commands to calculate either the MicroAction or MicroCheck bit based on the given action/check name. You can provide multiple actions (or checks) to make a combination of more stuff to happen at once .

--- comu/com.energyict.mdc.gogo/src/main/java/com/energyict/mdc/gogo/DeviceLifeCycleCommands.java
+++ comu/com.energyict.mdc.gogo/src/main/java/com/energyict/mdc/gogo/DeviceLifeCycleCommands.java
@@ -1,12 +1,5 @@
 package com.energyict.mdc.gogo;
 
-import com.energyict.mdc.device.data.Device;
-import com.energyict.mdc.device.data.DeviceService;
-import com.energyict.mdc.device.lifecycle.DeviceLifeCycleService;
-import com.energyict.mdc.device.lifecycle.ExecutableAction;
-import com.energyict.mdc.device.lifecycle.ExecutableActionProperty;
-import com.energyict.mdc.device.lifecycle.config.AuthorizedTransitionAction;
-
 import com.elster.jupiter.fsm.CustomStateTransitionEventType;
 import com.elster.jupiter.fsm.FiniteStateMachineService;
 import com.elster.jupiter.fsm.State;
@@ -19,6 +12,14 @@ import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.util.streams.DecoratedStream;
 import com.elster.jupiter.util.time.DefaultDateTimeFormatters;
+import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.data.DeviceService;
+import com.energyict.mdc.device.lifecycle.DeviceLifeCycleService;
+import com.energyict.mdc.device.lifecycle.ExecutableAction;
+import com.energyict.mdc.device.lifecycle.ExecutableActionProperty;
+import com.energyict.mdc.device.lifecycle.config.AuthorizedTransitionAction;
+import com.energyict.mdc.device.lifecycle.config.MicroAction;
+import com.energyict.mdc.device.lifecycle.config.MicroCheck;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -29,9 +30,11 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Provides gogo commands that support the device life cycle.
@@ -45,7 +48,9 @@ import java.util.stream.Collectors;
                 "osgi.command.function=triggerEvent",
                 "osgi.command.function=triggerAction",
                 "osgi.command.function=currentState",
-                "osgi.command.function=historicalState"},
+                "osgi.command.function=historicalState",
+                "osgi.command.function=printMicroActionBitFor",
+                "osgi.command.function=printMicroCheckBitFor"},
         immediate = true)
 @SuppressWarnings("unused")
 public class DeviceLifeCycleCommands {
@@ -109,8 +114,7 @@ public class DeviceLifeCycleCommands {
         Optional<StateTransitionEventType> eventType = this.finiteStateMachineService.findStateTransitionEventTypeBySymbol(symbolicEventName);
         if (eventType.isPresent()) {
             this.triggerEvent(eventType.get(), mRID);
-        }
-        else {
+        } else {
             System.out.println("State transition event type " + symbolicEventName + " does not exist");
         }
     }
@@ -118,8 +122,7 @@ public class DeviceLifeCycleCommands {
     private void triggerEvent(StateTransitionEventType eventType, String mRID) {
         if (eventType instanceof CustomStateTransitionEventType) {
             this.triggerEvent((CustomStateTransitionEventType) eventType, mRID);
-        }
-        else {
+        } else {
             System.out.println("Only custom state transition event types are supported as I don't know how to publish standard ones");
         }
     }
@@ -127,9 +130,8 @@ public class DeviceLifeCycleCommands {
     private void triggerEvent(CustomStateTransitionEventType eventType, String mRID) {
         Optional<Device> device = this.deviceService.findByUniqueMrid(mRID);
         if (device.isPresent()) {
-            this.triggerEvent(eventType,  device.get());
-        }
-        else {
+            this.triggerEvent(eventType, device.get());
+        } else {
             System.out.println("Device with mRID " + mRID + " does not exist");
         }
     }
@@ -151,8 +153,7 @@ public class DeviceLifeCycleCommands {
         Optional<StateTransitionEventType> eventType = this.finiteStateMachineService.findStateTransitionEventTypeBySymbol(symbolicEventName);
         if (eventType.isPresent()) {
             this.triggerAction(eventType.get(), mRID, this.parseEffectiveTimestamp(effectiveTimestamp));
-        }
-        else {
+        } else {
             System.out.println("State transition event type " + symbolicEventName + " does not exist");
         }
     }
@@ -161,15 +162,13 @@ public class DeviceLifeCycleCommands {
         try {
             if (effectiveTimestamp == null) {
                 return this.clock.instant();
-            }
-            else {
+            } else {
                 return LocalDate
                         .from(DateTimeFormatter.ISO_LOCAL_DATE.parse(effectiveTimestamp))
                         .atStartOfDay(ZoneId.systemDefault())
                         .toInstant();
             }
-        }
-        catch (DateTimeParseException e) {
+        } catch (DateTimeParseException e) {
             System.out.println("Please respect the following format for the effective timestamp: " + DateTimeFormatter.ISO_LOCAL_DATE.toFormat().toString());
             throw e;
         }
@@ -178,9 +177,8 @@ public class DeviceLifeCycleCommands {
     private void triggerAction(StateTransitionEventType eventType, String mRID, Instant effectiveTimestamp) {
         Optional<Device> device = this.deviceService.findByUniqueMrid(mRID);
         if (device.isPresent()) {
-            this.triggerAction(eventType,  device.get(), effectiveTimestamp);
-        }
-        else {
+            this.triggerAction(eventType, device.get(), effectiveTimestamp);
+        } else {
             System.out.println("Device with mRID " + mRID + " does not exist");
         }
     }
@@ -190,8 +188,7 @@ public class DeviceLifeCycleCommands {
             Optional<ExecutableAction> executableAction = this.deviceLifeCycleService.getExecutableActions(device, eventType);
             if (executableAction.isPresent()) {
                 this.execute(executableAction.get(), device, effectiveTimestamp);
-            }
-            else {
+            } else {
                 System.out.println("Current state of device with mRID " + device.getmRID() + " does not support the event type");
             }
             return null;
@@ -202,11 +199,11 @@ public class DeviceLifeCycleCommands {
         AuthorizedTransitionAction authorizedTransitionAction = (AuthorizedTransitionAction) action.getAction();
         List<ExecutableActionProperty> properties =
                 DecoratedStream
-                    .decorate(authorizedTransitionAction.getActions().stream())
-                    .flatMap(ma -> this.deviceLifeCycleService.getPropertySpecsFor(ma).stream())
-                    .distinct(PropertySpec::getName)
-                    .map(ps -> this.toExecutableActionProperty(ps, effectiveTimestamp))
-                    .collect(Collectors.toList());
+                        .decorate(authorizedTransitionAction.getActions().stream())
+                        .flatMap(ma -> this.deviceLifeCycleService.getPropertySpecsFor(ma).stream())
+                        .distinct(PropertySpec::getName)
+                        .map(ps -> this.toExecutableActionProperty(ps, effectiveTimestamp))
+                        .collect(Collectors.toList());
         action.execute(properties);
     }
 
@@ -214,15 +211,12 @@ public class DeviceLifeCycleCommands {
         try {
             if (DeviceLifeCycleService.MicroActionPropertyName.EFFECTIVE_TIMESTAMP.key().equals(propertySpec.getName())) {
                 return this.deviceLifeCycleService.toExecutableActionProperty(effectiveTimestamp, propertySpec);
-            }
-            else if (DeviceLifeCycleService.MicroActionPropertyName.LAST_CHECKED.key().equals(propertySpec.getName())) {
+            } else if (DeviceLifeCycleService.MicroActionPropertyName.LAST_CHECKED.key().equals(propertySpec.getName())) {
                 return this.deviceLifeCycleService.toExecutableActionProperty(effectiveTimestamp, propertySpec);
-            }
-            else {
+            } else {
                 throw new IllegalArgumentException("Unknown or unsupported PropertySpec: " + propertySpec.getName() + " that requires value type: " + propertySpec.getValueFactory().getValueType());
             }
-        }
-        catch (InvalidValueException e) {
+        } catch (InvalidValueException e) {
             e.printStackTrace(System.err);
             throw new RuntimeException(e);
         }
@@ -233,8 +227,7 @@ public class DeviceLifeCycleCommands {
         Optional<Device> device = this.deviceService.findByUniqueMrid(mRID);
         if (device.isPresent()) {
             this.currentState(device.get());
-        }
-        else {
+        } else {
             System.out.println("Device with mRID " + mRID + " does not exist");
         }
     }
@@ -249,8 +242,7 @@ public class DeviceLifeCycleCommands {
         Optional<Device> device = this.deviceService.findByUniqueMrid(mRID);
         if (device.isPresent()) {
             this.historicalState(device.get(), when);
-        }
-        else {
+        } else {
             System.out.println("Device with mRID " + mRID + " does not exist");
         }
     }
@@ -261,14 +253,50 @@ public class DeviceLifeCycleCommands {
             Optional<State> state = device.getState(historical);
             if (state.isPresent()) {
                 System.out.println("State of device (at: " + when + "): " + state.get().getName());
-            }
-            else {
+            } else {
                 System.out.println("Device did not exist yet at the specified timestamp: " + when);
             }
-        }
-        catch (DateTimeParseException e) {
+        } catch (DateTimeParseException e) {
             System.out.println("Invalid date time format, was expecting yyyy-MM-dd HH:mm:ss but got '" + when + "'");
             e.printStackTrace();
+        }
+    }
+
+    public void printMicroActionBitFor(String... providedMicroActions) {
+        boolean allIsOk = true;
+        List<MicroAction> microActions = new ArrayList<>();
+        for (String providedMicroAction : providedMicroActions) {
+            Optional<MicroAction> any = Stream.of(MicroAction.values()).filter(microAction -> microAction.name().equalsIgnoreCase(providedMicroAction)).findAny();
+            if (any.isPresent()) {
+                microActions.add(any.get());
+            } else {
+                System.out.println("ERROR ERROR -> No MicroAction found for '" + providedMicroAction);
+                allIsOk = false;
+            }
+        }
+        if (allIsOk) {
+            System.out.println("Your action bit is " + new Double(microActions.stream().mapToDouble(microAction -> Math.pow(2, microAction.ordinal())).sum()).longValue());
+        } else {
+            System.out.println("Your action bit is not calculated as you provided incorrect MicroActions.");
+        }
+    }
+
+    public void printMicroCheckBitFor(String... providedMicroChecks) {
+        boolean allIsOk = true;
+        List<MicroCheck> microChecks = new ArrayList<>();
+        for (String providedMicroCheck : providedMicroChecks) {
+            Optional<MicroCheck> any = Stream.of(MicroCheck.values()).filter(microCheck -> microCheck.name().equalsIgnoreCase(providedMicroCheck)).findAny();
+            if (any.isPresent()) {
+                microChecks.add(any.get());
+            } else {
+                System.out.println("ERROR ERROR -> No MicroCheck found for '" + providedMicroCheck);
+                allIsOk = false;
+            }
+        }
+        if (allIsOk) {
+            System.out.println("Your check bit is " + new Double(microChecks.stream().mapToDouble(microCheck -> Math.pow(2, microCheck.ordinal())).sum()).longValue());
+        } else {
+            System.out.println("Your check bit is not calculated as you provided incorrect MicroChecks.");
         }
     }
 
@@ -286,6 +314,7 @@ public class DeviceLifeCycleCommands {
         System.out.println("       #revoked");
         System.out.println("currentState <device mRID>");
         System.out.println("historicalState <device mRID> <yyyy-MM-dd HH:mm:ss>");
+        System.out.println("printMicroActionBitFor [<microAction1>, <microAction2>, ...]");
     }
 
     private DateTimeFormatter dateTimeFormatter() {
