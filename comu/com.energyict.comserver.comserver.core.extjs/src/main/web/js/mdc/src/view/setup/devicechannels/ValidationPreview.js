decd62d5dae90cabd59bfbe2912329c76bd7f649
Zheltukhin Alexey <azhelt@mera.ru>
2015-07-01 16:09:23 +0300

    fast privilege fix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicechannels/ValidationPreview.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicechannels/ValidationPreview.js
@@ -100,7 +100,7 @@ Ext.define('Mdc.view.setup.devicechannels.ValidationPreview', {
                             str += '<span style="word-wrap: break-word; display: inline-block; width: 800px">' + rule.name + ' ' + Uni.I18n.translate('device.registerData.removedRule', 'MDC', '(removed rule)') + prop + '</span>' + '<br>';
                         } else {
                             str = '<span style="word-wrap: break-word; display: inline-block; width: 800px">';
-                            if (Cfg.privileges.Validation.canViewOrAdminstrate()) {
+                            if (Cfg.privileges.Validation.canViewOrAdministrate()) {
                                 str += '<a href="#/administration/validation/rulesets/' + rule.ruleSetVersion.id + '/rules/' + rule.id + '">' + rule.name + '</a>';
                             } else {
                                 str += rule.name;
