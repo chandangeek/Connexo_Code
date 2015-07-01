729fbac3f67e478db556d181be69fe3989df5433
Kristof Hennebel <Kristof.Hennebel@elster.com>
2015-07-01 09:11:28 +0000

    COMMUNICATION-1240 - Create concentrator protocol for the G3 RTU3
    Added JUnit test for the crontab converter
    
    git-svn-id: http://source.eict.vpdc/svn/labo/protocols/trunk@91642 0d108963-8a47-0410-809b-ebd9fb3b3407

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtuplusserver/rtu3/messages/RTU3Messaging.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtuplusserver/rtu3/messages/RTU3Messaging.java
@@ -278,7 +278,7 @@ public class RTU3Messaging extends AbstractMessageExecutor implements DeviceMess
                     //Don't add a schedule if one already exists that has exactly the same name (e.g. 'every day at 00:00')
                     if (!scheduleAlreadyExists(allMasterData.getSchedules(), comTaskExecution.getNextExecutionSpecs())) {
                         final NextExecutionSpecs nextExecutionSpecs = comTaskExecution.getNextExecutionSpecs();
-                        final RTU3Schedule rtu3Schedule = new RTU3Schedule(getScheduleId(nextExecutionSpecs), getScheduleName(nextExecutionSpecs), convertToCrontabStyle(nextExecutionSpecs));
+                        final RTU3Schedule rtu3Schedule = new RTU3Schedule(getScheduleId(nextExecutionSpecs), getScheduleName(nextExecutionSpecs), CronTabStyleConverter.convert(nextExecutionSpecs));
                         allMasterData.getSchedules().add(rtu3Schedule);
                     }
                 }
@@ -454,50 +454,6 @@ public class RTU3Messaging extends AbstractMessageExecutor implements DeviceMess
             }
         }
         return new ArrayList<>(loadProfileObisCodes);
-    }
-
-    private String convertToCrontabStyle(NextExecutionSpecs nextExecutionSpecs) {
-        if (nextExecutionSpecs == null) {
-            return "";
-        } else if (nextExecutionSpecs.getTemporalExpression() != null) {
-
-            final TimeDuration every = nextExecutionSpecs.getTemporalExpression().getEvery();
-            final NextExecutionSpecsShadow.Offset offset = nextExecutionSpecs.getShadow().getOffsetInDaysHoursMinutes();
-
-            switch (every.getTimeUnitCode()) {
-                case TimeDuration.MONTHS:
-                    return "0 " + offset.getMinutes() + " " + offset.getHours() + " " + getDayOfMonth(offset) + " */" + every.getCount() + " *";  //E.g. '0 0 6 16 */3 *' means 'every 3 months, on day 16, at 06:00:00'
-                case TimeDuration.WEEKS:
-                    return "0 " + offset.getMinutes() + " " + offset.getHours() + " */" + (every.getCount() * 7) + " * " + getDayOfWeek(offset);  //E.g. '0 0 6 */14 * 1' means 'every 2 weeks, on monday, at 06:00:00'
-                case TimeDuration.DAYS:
-                    return "0 " + offset.getMinutes() + " " + offset.getHours() + " */" + every.getCount() + " * *";  //E.g. '0 0 6 */3 * *' means 'every 3 days, at 06:00:00'
-                case TimeDuration.HOURS:
-                    return "0 " + offset.getMinutes() + " */" + every.getCount() + " * * *";  //E.g. '0 3 */2 * * *' means 'every 2 hours, at 00:03:00'
-                case TimeDuration.MINUTES:
-                    return "0 */" + every.getCount() + " * * * *";  //E.g. '0 */5 * * * *' means 'every 5 minutes'
-                default:
-                    return "";
-            }
-        } else if (nextExecutionSpecs.getDialCalendar() != null) {
-            return "";    //TODO add support to convert dial calendar to crontab style
-        } else {
-            return "";
-        }
-    }
-
-    /**
-     * EIServer day is 0 based, return 1-based
-     */
-    private int getDayOfMonth(NextExecutionSpecsShadow.Offset offset) {
-        return offset.getDays() + 1;
-    }
-
-    /**
-     * offset.getDays: 0 = monday, 1 = tuesday,... 6 = sunday
-     * returns: 1 for monday, 2 for tuesday, ... 7 for sunday
-     */
-    private int getDayOfWeek(NextExecutionSpecsShadow.Offset offset) {
-        return offset.getDays() + 1;
     }
 
     private RTU3ClientType getClientType(Device device, ComTaskExecution comTaskExecution) {
---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/test/java/com/energyict/protocolimplv2/eict/rtuplusserver/rtu3/messages/CronTabStyleConverterTest.java
@@ -0,0 +1,44 @@
+package com.energyict.protocolimplv2.eict.rtuplusserver.rtu3.messages;
+
+import com.energyict.cbo.TemporalExpression;
+import com.energyict.cbo.TimeDuration;
+import com.energyict.mdc.tasks.NextExecutionSpecs;
+import com.energyict.mdc.tasks.NextExecutionSpecsImpl;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+/**
+ * Copyrights EnergyICT
+ *
+ * @author khe
+ * @since 1/07/2015 - 10:40
+ */
+public class CronTabStyleConverterTest {
+
+    @Test
+    public void test() {
+        NextExecutionSpecs nextExecutionSpecs = spy(new NextExecutionSpecsImpl(0));
+        when(nextExecutionSpecs.getTemporalExpression()).thenReturn(new TemporalExpression(new TimeDuration(5, TimeDuration.MINUTES), new TimeDuration(0)));
+        String convert = CronTabStyleConverter.convert(nextExecutionSpecs);
+        assertEquals("0 */5 * * * *", convert);
+
+        when(nextExecutionSpecs.getTemporalExpression()).thenReturn(new TemporalExpression(new TimeDuration(2, TimeDuration.HOURS), new TimeDuration(3, TimeDuration.MINUTES)));
+        convert = CronTabStyleConverter.convert(nextExecutionSpecs);
+        assertEquals("0 3 */2 * * *", convert);
+
+        when(nextExecutionSpecs.getTemporalExpression()).thenReturn(new TemporalExpression(new TimeDuration(2, TimeDuration.DAYS), new TimeDuration(6, TimeDuration.HOURS)));
+        convert = CronTabStyleConverter.convert(nextExecutionSpecs);
+        assertEquals("0 0 6 */2 * *", convert);
+
+        when(nextExecutionSpecs.getTemporalExpression()).thenReturn(new TemporalExpression(new TimeDuration(2, TimeDuration.WEEKS), new TimeDuration(6, TimeDuration.HOURS)));
+        convert = CronTabStyleConverter.convert(nextExecutionSpecs);
+        assertEquals("0 0 6 */14 * 1", convert);
+
+        when(nextExecutionSpecs.getTemporalExpression()).thenReturn(new TemporalExpression(new TimeDuration(3, TimeDuration.MONTHS), new TimeDuration(15, TimeDuration.DAYS)));
+        convert = CronTabStyleConverter.convert(nextExecutionSpecs);
+        assertEquals("0 0 0 16 */3 *", convert);
+    }
+}
