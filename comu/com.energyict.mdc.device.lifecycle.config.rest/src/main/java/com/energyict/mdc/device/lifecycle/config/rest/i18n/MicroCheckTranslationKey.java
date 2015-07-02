136ea86004d2cfeb07e7e2478970f50bc6450d6e
Anton Suslennikov <antsusl@mera.ru>
2015-07-02 21:45:20 +0300

    COMU-1289 Translate actions and checks

--- comu/com.energyict.mdc.device.lifecycle.config.rest/src/main/java/com/energyict/mdc/device/lifecycle/config/rest/i18n/MicroCheckTranslationKey.java
+++ comu/com.energyict.mdc.device.lifecycle.config.rest/src/main/java/com/energyict/mdc/device/lifecycle/config/rest/i18n/MicroCheckTranslationKey.java
@@ -25,6 +25,8 @@ public enum MicroCheckTranslationKey implements TranslationKey {
     MICRO_CHECK_NAME_LINKED_WITH_USAGE_POINT(Keys.MICRO_CHECK_NAME_TRANSLATE_KEY + MicroCheck.LINKED_WITH_USAGE_POINT, "Connected to usage point"),
     MICRO_CHECK_NAME_ALL_ISSUES_AND_ALARMS_ARE_CLOSED(Keys.MICRO_CHECK_NAME_TRANSLATE_KEY + MicroCheck.ALL_ISSUES_AND_ALARMS_ARE_CLOSED, "All issues closed"),
     MICRO_CHECK_NAME_MANDATORY_COMMUNICATION_ATTRIBUTES_AVAILABLE(Keys.MICRO_CHECK_NAME_TRANSLATE_KEY + MicroActionAndCheckInfoFactory.CONSOLIDATED_MICRO_CHECKS_KEY, "Mandatory communication attributes available"),
+    MICRO_CHECK_NAME_AT_LEAST_ONE_ACTIVE_CONNECTION_AVAILABLE(Keys.MICRO_CHECK_NAME_TRANSLATE_KEY + MicroCheck.AT_LEAST_ONE_ACTIVE_CONNECTION_AVAILABLE, "At least one active connection available"),
+    MICRO_CHECK_NAME_ALL_DATA_VALIDATED(Keys.MICRO_CHECK_NAME_TRANSLATE_KEY + MicroCheck.ALL_DATA_VALIDATED, "All data validated"),
 
     MICRO_CHECK_DESCRIPTION_DEFAULT_CONNECTION_AVAILABLE(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.DEFAULT_CONNECTION_AVAILABLE, "Check if a default connection is available on the device."),
     MICRO_CHECK_DESCRIPTION_AT_LEAST_ONE_MANUALLY_SCHEDULED_COMMUNICATION_TASK_AVAILABLE(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.AT_LEAST_ONE_SCHEDULED_COMMUNICATION_TASK_AVAILABLE, "Check if at least one communication task has been scheduled."),
@@ -34,7 +36,10 @@ public enum MicroCheckTranslationKey implements TranslationKey {
     MICRO_CHECK_DESCRIPTION_SLAVE_DEVICE_HAS_GATEWAY(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.SLAVE_DEVICE_HAS_GATEWAY, "If this device is a slave, check if the device has been linked to a master device."),
     MICRO_CHECK_DESCRIPTION_LINKED_WITH_USAGE_POINT(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.LINKED_WITH_USAGE_POINT, "Check if this device is connected to a usage point."),
     MICRO_CHECK_DESCRIPTION_ALL_ISSUES_AND_ALARMS_ARE_CLOSED(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.ALL_ISSUES_AND_ALARMS_ARE_CLOSED, "Check if all the issues on this device are closed."),
-    MICRO_CHECK_DESCRIPTION_MANDATORY_COMMUNICATION_ATTRIBUTES_AVAILABLE(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroActionAndCheckInfoFactory.CONSOLIDATED_MICRO_CHECKS_KEY, "Check if the mandatory communication attributes are available on the device: protocol dialect attributes, security setting attributes, connection attributes, general attributes.");
+    MICRO_CHECK_DESCRIPTION_MANDATORY_COMMUNICATION_ATTRIBUTES_AVAILABLE(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroActionAndCheckInfoFactory.CONSOLIDATED_MICRO_CHECKS_KEY, "Check if the mandatory communication attributes are available on the device: protocol dialect attributes, security setting attributes, connection attributes, general attributes."),
+    MICRO_CHECK_DESCRIPTION_AT_LEAST_ONE_ACTIVE_CONNECTION_AVAILABLE(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.AT_LEAST_ONE_ACTIVE_CONNECTION_AVAILABLE, "Check if at least one connection is available on the device with the status: \"Active\"."),
+    MICRO_CHECK_DESCRIPTION_ALL_DATA_VALIDATED(Keys.MICRO_CHECK_DESCRIPTION_TRANSLATE_KEY + MicroCheck.ALL_DATA_VALIDATED, "Check if all the collected data is validated."),
+    ;
 
     private final String key;
     private final String defaultFormat;
