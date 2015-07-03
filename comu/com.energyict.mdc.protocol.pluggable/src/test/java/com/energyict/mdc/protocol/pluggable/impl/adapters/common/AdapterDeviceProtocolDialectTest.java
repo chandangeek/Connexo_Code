4e364cb2fccfe55bc27631d130469356ce6ea01f
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-07-03 13:04:09 +0200

    Remove dependency on BpmService from device life cycle configuration

--- comu/com.energyict.mdc.protocol.pluggable/src/test/java/com/energyict/mdc/protocol/pluggable/impl/adapters/common/AdapterDeviceProtocolDialectTest.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/test/java/com/energyict/mdc/protocol/pluggable/impl/adapters/common/AdapterDeviceProtocolDialectTest.java
@@ -12,7 +12,6 @@ import com.energyict.mdc.protocol.pluggable.impl.ProtocolPluggableServiceImpl;
 import com.energyict.mdc.protocol.pluggable.mocks.MockMeterProtocol;
 
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
-import com.elster.jupiter.bpm.BpmService;
 import com.elster.jupiter.datavault.impl.DataVaultModule;
 import com.elster.jupiter.domain.util.impl.DomainUtilModule;
 import com.elster.jupiter.events.impl.EventsModule;
@@ -38,10 +37,10 @@ import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.users.impl.UserModule;
 import com.elster.jupiter.util.UtilModule;
+import com.elster.jupiter.util.streams.Predicates;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Provider;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
 
@@ -50,6 +49,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.*;
 import org.junit.runner.*;
@@ -77,8 +77,6 @@ public class AdapterDeviceProtocolDialectTest {
 
     @Mock
     private BundleContext bundleContext;
-    @Mock
-    private BpmService bpmService;
     @Mock
     private Principal principal;
     @Mock
@@ -137,7 +135,7 @@ public class AdapterDeviceProtocolDialectTest {
     @Test
     public void testDialectName () {
         MockMeterProtocol mockDeviceProtocol = new MockMeterProtocol();
-        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<PropertySpec>());
+        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<>());
 
         assertThat(dialect.getDeviceProtocolDialectName()).isEqualTo("MockMeterPro316065908");
     }
@@ -145,7 +143,7 @@ public class AdapterDeviceProtocolDialectTest {
     @Test
     public void getRequiredKeysTest () {
         MockMeterProtocol mockDeviceProtocol = new MockMeterProtocol();
-        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<PropertySpec>());
+        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<>());
 
         assertThat(getRequiredPropertiesFromSet(dialect.getPropertySpecs())).containsOnly(getRequiredPropertySpec());
     }
@@ -153,7 +151,7 @@ public class AdapterDeviceProtocolDialectTest {
     @Test
     public void getOptionalKeysTest () {
         MockMeterProtocol mockDeviceProtocol = new MockMeterProtocol();
-        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<PropertySpec>());
+        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<>());
 
         assertThat(getOptionalPropertiesFromSet(dialect.getPropertySpecs())).containsOnly(getOptionalPropertySpec());
     }
@@ -161,7 +159,7 @@ public class AdapterDeviceProtocolDialectTest {
     @Test
     public void getPropertySpecTest () {
         MockMeterProtocol mockDeviceProtocol = new MockMeterProtocol();
-        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<PropertySpec>());
+        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<>());
 
         assertThat(dialect.getPropertySpec(REQUIRED_PROPERTY_NAME)).isEqualTo(getRequiredPropertySpec());
         assertThat(dialect.getPropertySpec(OPTIONAL_PROPERTY_NAME)).isEqualTo(getOptionalPropertySpec());
@@ -170,7 +168,7 @@ public class AdapterDeviceProtocolDialectTest {
     @Test
     public void testWithAdditionalProperties () {
         MockMeterProtocol mockDeviceProtocol = new MockMeterProtocol();
-        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<PropertySpec>());
+        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<>());
 
         assertThat(dialect.getPropertySpecs()).containsOnly(this.getPropertySpecs());
     }
@@ -187,7 +185,7 @@ public class AdapterDeviceProtocolDialectTest {
     @Test
     public void testWithOnlyAdditionalProperties () {
         MeterProtocol mockDeviceProtocol = mock(MeterProtocol.class);
-        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<PropertySpec>());
+        AdapterDeviceProtocolDialect dialect = new AdapterDeviceProtocolDialect(propertySpecService, protocolPluggableService, mockDeviceProtocol, new ArrayList<>());
 
         assertThat(dialect.getPropertySpecs()).isEmpty();
     }
@@ -202,23 +200,17 @@ public class AdapterDeviceProtocolDialectTest {
     }
 
     private List<PropertySpec> getOptionalPropertiesFromSet(List<PropertySpec> propertySpecs) {
-        List<PropertySpec> requiredProperties = new ArrayList<>();
-        for (PropertySpec propertySpec : propertySpecs) {
-            if(!propertySpec.isRequired()){
-                requiredProperties.add(propertySpec);
-            }
-        }
-        return requiredProperties;
+        return propertySpecs
+                .stream()
+                .filter(Predicates.not(PropertySpec::isRequired))
+                .collect(Collectors.toList());
     }
 
     private List<PropertySpec> getRequiredPropertiesFromSet(List<PropertySpec> propertySpecs) {
-        List<PropertySpec> requiredProperties = new ArrayList<>();
-        for (PropertySpec propertySpec : propertySpecs) {
-            if(propertySpec.isRequired()){
-                requiredProperties.add(propertySpec);
-            }
-        }
-        return requiredProperties;
+        return propertySpecs
+                .stream()
+                .filter(PropertySpec::isRequired)
+                .collect(Collectors.toList());
     }
 
     private PropertySpec[] getPropertySpecs () {
@@ -249,7 +241,6 @@ public class AdapterDeviceProtocolDialectTest {
         protected void configure() {
             bind(EventAdmin.class).toInstance(eventAdmin);
             bind(BundleContext.class).toInstance(bundleContext);
-            bind(BpmService.class).toInstance(bpmService);
             bind(LicenseService.class).toInstance(licenseService);
             bind(DataModel.class).toProvider(() -> dataModel);
         }
