5e93e4e1e615e70439477d580b16cbae48610fe3
Geert <geert.delmeiren@elster.com>
2015-07-03 12:57:09 +0200

    Fix a bug

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/container/PreviewContainer.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/container/PreviewContainer.js
@@ -239,7 +239,7 @@ Ext.define('Uni.view.container.PreviewContainer', {
     },
 
     onLoad: function (store, records) {
-        this.updateOnChange(records.length === 0);
+        this.updateOnChange(records==null || records.length === 0);
     },
 
     onChange: function(store) {
