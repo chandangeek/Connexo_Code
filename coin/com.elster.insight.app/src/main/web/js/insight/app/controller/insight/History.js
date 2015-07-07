26f4357c066069b69e8c4b97aeded01211160404
Kurt Kikendall <kurt.kikendall@elster.com>
2015-07-07 16:15:28 -0400

    Fix breadcrumb issue.

--- coin/com.elster.insight.app/src/main/web/js/insight/app/controller/insight/History.js
+++ coin/com.elster.insight.app/src/main/web/js/insight/app/controller/insight/History.js
@@ -10,6 +10,7 @@ Ext.define('InsightApp.controller.insight.History', {
             disabled: true,
             items: {
                 usagepoints: {
+                	title: Uni.I18n.translate('general.usagepoints', 'INS', 'Usage Points'),
                     route: 'usagepoints',
                     controller: 'InsightApp.controller.insight.UsagePoint',
                     action: 'test'
