61f486576465bec916fd6f63d3c1f3886d158b25
Mihai Andreescu <mihai.andreescu@elster.com>
2015-07-08 09:57:53 +0300

    Finished 'reusing privileges through app'.

--- comu/com.energyict.mdc.app/src/main/java/com/energyict/mdc/app/impl/MdcAppInstaller.java
+++ comu/com.energyict.mdc.app/src/main/java/com/energyict/mdc/app/impl/MdcAppInstaller.java
@@ -5,7 +5,6 @@ import com.elster.jupiter.users.Privilege;
 import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.util.HasName;
 import com.energyict.mdc.app.MdcAppService;
-import com.energyict.mdc.device.lifecycle.config.Privileges;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -23,27 +22,6 @@ import java.util.stream.Collectors;
 @Component(name = "com.energyict.mdc.app.install", service = {InstallService.class}, property = "name=" + MdcAppService.COMPONENTNAME, immediate = true)
 @SuppressWarnings("unused")
 public class MdcAppInstaller implements InstallService {
-
-    public static final String PRIVILEGE_VIEW_REPORTS = "privilege.view.reports";
-
-    public static final String PRIVILEGE_RUN_EXPORT = "privilege.run.dataExportTask";
-    public static final String PRIVILEGE_VIEW_EXPORT = "privilege.view.dataExportTask";
-
-    public static final String PRIVILEGE_VIEW_COMMUNICATION = "privilege.view.communicationAdministration";
-    public static final String PRIVILEGE_VIEW_DEVICE = "privilege.view.device";
-    public static final String PRIVILEGE_OPERATE_DEVICECOMMUNICATION = "privilege.operate.deviceCommunication";
-    public static final String PRIVILEGE_ADMINISTRATE_DEVICEDATA = "privilege.administrate.deviceData";
-    public static final String PRIVILEGE_VIEW_DEVICETYPE = "privilege.view.deviceType";
-    public static final String PRIVILEGE_ACTION_ISSUE_ACTION = "privilege.action.issue";
-    public static final String PRIVILEGE_ASSIGN_ISSUE = "privilege.assign.issue";
-    public static final String PRIVILEGE_CLOSE_ISSUE = "privilege.close.issue";
-    public static final String PRIVILEGE_COMMENT_ISSUE = "privilege.comment.issue";
-    public static final String PRIVILEGE_VIEW_ISSUE = "privilege.view.issue";
-    public static final String PRIVILEGE_VIEW_MASTER_DATA = "privilege.view.masterData";
-    public static final String PRIVILEGE_VIEW_VALIDATION = "privilege.view.validationConfiguration";
-    public static final String VIEW_MDC_IMPORT_SERVICES = "privilege.view.mdc.importServices";
-    public static final String VIEW_FWC_CAMPAIGNS = "privilege.view.firmware.campaign";
-
 
     private final Logger logger = Logger.getLogger(MdcAppInstaller.class.getName());
     private volatile UserService userService;
@@ -76,16 +54,13 @@ public class MdcAppInstaller implements InstallService {
 
     private void assignPrivilegesToDefaultRoles() {
         List<Privilege> availablePrivileges =  getApplicationPrivileges();
-        String[] privilegesWithoutReports = availablePrivileges.stream().map(HasName::getName).filter(p -> !p.equals(PRIVILEGE_VIEW_REPORTS)).toArray(String[]::new);
+        String[] privilegesWithoutReports = availablePrivileges.stream().map(HasName::getName).filter(p -> !p.equals(com.elster.jupiter.yellowfin.security.Privileges.VIEW_REPORTS)).toArray(String[]::new);
         String[] privilegesMeterOperator = getPrivilegesMeterOperator();
 
         userService.grantGroupWithPrivilege(MdcAppService.Roles.METER_OPERATOR.value(), MdcAppService.APPLICATION_KEY, privilegesMeterOperator);
         userService.grantGroupWithPrivilege(MdcAppService.Roles.METER_EXPERT.value(), MdcAppService.APPLICATION_KEY, privilegesWithoutReports);
-        userService.grantGroupWithPrivilege(MdcAppService.Roles.METER_EXPERT.value(), MdcAppService.APPLICATION_KEY, this.deviceLifeCycleAdministrationPrivileges());
-        userService.grantGroupWithPrivilege(MdcAppService.Roles.METER_EXPERT.value(), MdcAppService.APPLICATION_KEY, this.allDeviceLifeCycleActionPrivileges());
-        userService.grantGroupWithPrivilege(MdcAppService.Roles.METER_OPERATOR.value(), MdcAppService.APPLICATION_KEY, this.meterOperatorDeviceLifeCycleActionPrivileges());
         userService.grantGroupWithPrivilege(UserService.BATCH_EXECUTOR_ROLE, MdcAppService.APPLICATION_KEY, privilegesWithoutReports);
-        userService.grantGroupWithPrivilege(MdcAppService.Roles.REPORT_VIEWER.value(), MdcAppService.APPLICATION_KEY, availablePrivileges.stream().map(HasName::getName).filter(p -> p.equals(PRIVILEGE_VIEW_REPORTS)).toArray(String[]::new));
+        userService.grantGroupWithPrivilege(MdcAppService.Roles.REPORT_VIEWER.value(), MdcAppService.APPLICATION_KEY, availablePrivileges.stream().map(HasName::getName).filter(p -> p.equals(com.elster.jupiter.yellowfin.security.Privileges.VIEW_REPORTS)).toArray(String[]::new));
         //TODO: workaround: attached Meter expert to user admin !!! to remove this line when the user can be created/added to system
         userService.getUser(1).ifPresent(u -> u.join(userService.getGroups().stream().filter(e -> e.getName().equals(MdcAppService.Roles.METER_EXPERT.value())).findFirst().get()));
         //TODO: workaround: attached Report viewer to user admin !!! to remove this line when the user can be created/added to system
@@ -98,31 +73,24 @@ public class MdcAppInstaller implements InstallService {
 
     private String[] getPrivilegesMeterOperator(){
         return new String[] {
-                PRIVILEGE_RUN_EXPORT, PRIVILEGE_VIEW_EXPORT,
-                PRIVILEGE_VIEW_COMMUNICATION, PRIVILEGE_VIEW_DEVICE,
-                PRIVILEGE_OPERATE_DEVICECOMMUNICATION, PRIVILEGE_ADMINISTRATE_DEVICEDATA, PRIVILEGE_VIEW_DEVICETYPE,
-                PRIVILEGE_ACTION_ISSUE_ACTION, PRIVILEGE_ASSIGN_ISSUE, PRIVILEGE_CLOSE_ISSUE, PRIVILEGE_COMMENT_ISSUE, PRIVILEGE_VIEW_ISSUE,
-                PRIVILEGE_VIEW_MASTER_DATA, PRIVILEGE_VIEW_VALIDATION, VIEW_MDC_IMPORT_SERVICES, VIEW_FWC_CAMPAIGNS
+                com.elster.jupiter.export.security.Privileges.RUN_DATA_EXPORT_TASK,
+                com.elster.jupiter.export.security.Privileges.VIEW_DATA_EXPORT_TASK,
+                com.energyict.mdc.engine.config.security.Privileges.VIEW_COMMUNICATION_ADMINISTRATION,
+                com.energyict.mdc.device.data.security.Privileges.VIEW_DEVICE,
+                com.energyict.mdc.device.data.security.Privileges.OPERATE_DEVICE_COMMUNICATION,
+                com.energyict.mdc.device.data.security.Privileges.ADMINISTRATE_DEVICE_DATA,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_TYPE,
+                com.elster.jupiter.issue.security.Privileges.ACTION_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.ASSIGN_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.CLOSE_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.COMMENT_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.VIEW_ISSUE,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_MASTER_DATA,
+                com.elster.jupiter.validation.security.Privileges.VIEW_VALIDATION_CONFIGURATION,
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_1,
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_2,
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_3
         };
     }
-
-    private String[] deviceLifeCycleAdministrationPrivileges(){
-        return new String[] {Privileges.VIEW_DEVICE_LIFE_CYCLE, Privileges.CONFIGURE_DEVICE_LIFE_CYCLE};
-    }
-
-    private String[] allDeviceLifeCycleActionPrivileges() {
-        return new String[]{
-                Privileges.INITIATE_ACTION_1,
-                Privileges.INITIATE_ACTION_2,
-                Privileges.INITIATE_ACTION_3,
-                Privileges.INITIATE_ACTION_4};
-    }
-
-    private String[] meterOperatorDeviceLifeCycleActionPrivileges() {
-        return new String[]{
-                Privileges.INITIATE_ACTION_1,
-                Privileges.INITIATE_ACTION_2,
-                Privileges.INITIATE_ACTION_3};
-    }
-
+    
 }
