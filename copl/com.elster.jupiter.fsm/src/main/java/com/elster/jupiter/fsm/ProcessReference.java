65434d24b20555bc9092e6536b7594585d79b0b1
Pasquien Dorny <pasquien.dorny@elster.com>
2015-07-10 13:49:59 +0200

    COMU-1264: Configure processes on state

--- copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/ProcessReference.java
+++ copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/ProcessReference.java
@@ -11,6 +11,6 @@ import aQute.bnd.annotation.ProviderType;
 @ProviderType
 public interface ProcessReference {
 
-    public StateChangeBusinessProcess getStateChangeBusinessProcess();
+    StateChangeBusinessProcess getStateChangeBusinessProcess();
 
 }
