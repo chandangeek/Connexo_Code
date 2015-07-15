4692523e97c726b65d4f059e6d32d6290da10539
Lobanov Dmitry <dlobanov@mera.ru>
2015-07-15 08:27:07 +0300

    COMU-1330

--- coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/ReadingTypeFieldResource.java
+++ coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/ReadingTypeFieldResource.java
@@ -7,8 +7,10 @@ import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.metering.rest.ReadingTypeInfo;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.rest.util.JsonQueryFilter;
-import com.elster.jupiter.rest.util.ListPager;
+import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.elster.jupiter.rest.util.KorePagedInfoList;
+import com.elster.jupiter.rest.util.ListPager;
+import com.elster.jupiter.rest.util.PagedInfoList;
 import com.elster.jupiter.rest.util.QueryParameters;
 import com.elster.jupiter.util.streams.DecoratedStream;
 
@@ -40,7 +42,7 @@ public class ReadingTypeFieldResource {
 
     @GET
     @Path("/unitsofmeasure")
-    @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
     public Response getUnitsOfMeasure(@BeanParam QueryParameters queryParameters) {
         return Response.ok(KorePagedInfoList.asJson(
                 "unitsOfMeasure",
@@ -55,16 +57,16 @@ public class ReadingTypeFieldResource {
 
     @GET
     @Path("/intervals")
-    @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
     public IntervalFieldInfos getIntervals() {
         List<ReadingType> readingTypes = meteringService.getAvailableReadingTypes();
 
         Set<TimeAttribute> timeAttributes = readingTypes.stream()
-            .map(ReadingType::getMeasuringPeriod)
-            .collect(Collectors.<TimeAttribute>toSet());
+                .map(ReadingType::getMeasuringPeriod)
+                .collect(Collectors.<TimeAttribute>toSet());
         Set<MacroPeriod> macroPeriods = readingTypes.stream()
-            .map(ReadingType::getMacroPeriod)
-            .collect(Collectors.<MacroPeriod>toSet());
+                .map(ReadingType::getMacroPeriod)
+                .collect(Collectors.<MacroPeriod>toSet());
 
         IntervalFieldInfos intervalFieldInfos = new IntervalFieldInfos();
         intervalFieldInfos.from(timeAttributes, macroPeriods, thesaurus);
@@ -73,48 +75,63 @@ public class ReadingTypeFieldResource {
 
     @GET
     @Path("/timeofuse")
-    @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
     public TimeOfUseFieldInfos getTou() {
         return new TimeOfUseFieldInfos();
     }
 
     @GET
     @Path("/readingtypes")
-    @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
-    public Response getReadingTypes(@BeanParam JsonQueryFilter queryFilter, @BeanParam QueryParameters queryParameters) {
-        List<ReadingType> readingTypes = meteringService.getAvailableReadingTypes();
-        final Predicate<ReadingType> filter = getReadingTypeFilterPredicate(queryFilter);
-        readingTypes = readingTypes.stream().filter(filter).sorted((c1,c2)->(c1.getFullAliasName().compareToIgnoreCase(c2.getFullAliasName()))).collect(Collectors.<ReadingType>toList());
-        List<ReadingTypeInfo> pagedReadingTypes = ListPager.of(readingTypes).from(queryParameters).find().stream().map(ReadingTypeInfo::new).collect(toList());
-        return Response.ok(KorePagedInfoList.asJson("readingTypes", pagedReadingTypes, queryParameters, readingTypes.size())).build();
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    public PagedInfoList getReadingTypes(@BeanParam JsonQueryFilter queryFilter, @BeanParam JsonQueryParameters queryParameters) {
+        List<ReadingType> readingTypes;
+        if (queryFilter.hasProperty("equidistant")) {
+            readingTypes = queryFilter.getBoolean("equidistant")
+                    ? meteringService.getAvailableEquidistantReadingTypes()
+                    : meteringService.getAvailableNonEquidistantReadingTypes();
+        } else {
+            readingTypes = meteringService.getAvailableReadingTypes();
+        }
+        Predicate<ReadingType> filter = getReadingTypeFilterPredicate(queryFilter);
+        readingTypes = readingTypes.stream()
+                .filter(filter)
+                .sorted((c1, c2) -> (c1.getFullAliasName().compareToIgnoreCase(c2.getFullAliasName())))
+                .collect(Collectors.toList());
+        List<ReadingTypeInfo> pagedReadingTypes = ListPager.of(readingTypes)
+                .paged(queryParameters.getStart().orElse(null), queryParameters.getLimit().orElse(null))
+                .find()
+                .stream()
+                .map(ReadingTypeInfo::new)
+                .collect(toList());
+        return PagedInfoList.fromPagedList("readingTypes", pagedReadingTypes, queryParameters);
     }
 
     private Predicate<ReadingType> getReadingTypeFilterPredicate(JsonQueryFilter queryFilter) {
-        Predicate<ReadingType> filter = rt->true; // unfiltered
+        Predicate<ReadingType> filter = rt -> true; // unfiltered
         if (queryFilter.hasFilters()) {
             if (queryFilter.hasProperty("mRID")) {
-                filter=filter.and(rt->rt.getMRID().toLowerCase().contains(queryFilter.getString("mRID").toLowerCase()));
+                filter = filter.and(rt -> rt.getMRID().toLowerCase().contains(queryFilter.getString("mRID").toLowerCase()));
             }
             if (queryFilter.hasProperty("name")) {
-                filter=filter.and(rt -> rt.getFullAliasName().toLowerCase().contains(queryFilter.getString("name").toLowerCase()));
+                filter = filter.and(rt -> rt.getFullAliasName().toLowerCase().contains(queryFilter.getString("name").toLowerCase()));
             }
             if (queryFilter.hasProperty("tou")) {
-                filter=filter.and(rt->rt.getTou() == queryFilter.getInteger("tou"));
+                filter = filter.and(rt -> rt.getTou() == queryFilter.getInteger("tou"));
             }
             if (queryFilter.hasProperty("unitOfMeasure")) {
-                filter=filter.and(rt->rt.getUnit().getId() == queryFilter.getLong("unitOfMeasure"));
+                filter = filter.and(rt -> rt.getUnit().getId() == queryFilter.getLong("unitOfMeasure"));
             }
             if (queryFilter.hasProperty("time")) {
-                filter=filter.and(rt->rt.getMeasuringPeriod().getId() == queryFilter.getInteger("time"));
+                filter = filter.and(rt -> rt.getMeasuringPeriod().getId() == queryFilter.getInteger("time"));
             }
             if (queryFilter.hasProperty("macro")) {
                 filter = filter.and(rt -> rt.getMacroPeriod().getId() == queryFilter.getInteger("macro"));
             }
             if (queryFilter.hasProperty("multiplier")) {
-                filter=filter.and(rt->rt.getMultiplier().getMultiplier()==queryFilter.getInteger("multiplier"));
+                filter = filter.and(rt -> rt.getMultiplier().getMultiplier() == queryFilter.getInteger("multiplier"));
             }
             if (queryFilter.hasProperty("selectedReadings")) {
-                filter=filter.and(rt->!queryFilter.getStringList("selectedReadings").contains(rt.getMRID().toLowerCase()));
+                filter = filter.and(rt -> !queryFilter.getStringList("selectedReadings").contains(rt.getMRID().toLowerCase()));
             }
         }
         return filter;
