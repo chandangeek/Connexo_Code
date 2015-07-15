3bc3f8e53422d49d22b12cb2327365c0a2d31291
Mihai Andreescu <mihai.andreescu@elster.com>
2015-07-15 11:31:30 +0300

    Reusing of MDC privileges.

--- comu/com.energyict.mdc.app/src/main/java/com/energyict/mdc/app/impl/MdcAppPrivileges.java
+++ comu/com.energyict.mdc.app/src/main/java/com/energyict/mdc/app/impl/MdcAppPrivileges.java
@@ -1,88 +1,126 @@
 package com.energyict.mdc.app.impl;
 
+import com.energyict.mdc.device.data.security.Privileges;
+
 import java.util.Arrays;
 import java.util.List;
 
 class MdcAppPrivileges {
 
-    //export
-    static String ADMINISTRATE_DATA_EXPORT_TASK = "privilege.administrate.dataExportTask";
-    static String VIEW_DATA_EXPORT_TASK = "privilege.view.dataExportTask";
-    static String UPDATE_DATA_EXPORT_TASK = "privilege.update.dataExportTask";
-    static String UPDATE_SCHEDULE_DATA_EXPORT_TASK = "privilege.update.schedule.dataExportTask";
-    static String RUN_DATA_EXPORT_TASK = "privilege.run.dataExportTask";
-
-    //import
-    static String VIEW_IMPORT_SERVICES = "privilege.view.importServices";
-
-    //validation
-    static String ADMINISTRATE_VALIDATION_CONFIGURATION = "privilege.administrate.validationConfiguration";
-    static String VIEW_VALIDATION_CONFIGURATION = "privilege.view.validationConfiguration";
-
-    static String VALIDATE_MANUAL = "privilege.view.validateManual";
-
-    static String FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE = "privilege.view.fineTuneValidationConfiguration.onDevice";
-    static String FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE_CONFIGURATION = "privilege.view.fineTuneValidationConfiguration.onDeviceConfiguration";
-
-    //yellowfin
-    static String VIEW_REPORTS = "privilege.view.reports";
-
-
-
-   static final String PRIVILEGE_VIEW_REPORTS = "privilege.view.reports";
-
-   static final String PRIVILEGE_RUN_EXPORT = "privilege.run.dataExportTask";
-   static final String PRIVILEGE_VIEW_EXPORT = "privilege.view.dataExportTask";
-
-   static final String PRIVILEGE_VIEW_COMMUNICATION = "privilege.view.communicationAdministration";
-   static final String PRIVILEGE_VIEW_DEVICE = "privilege.view.device";
-   static final String PRIVILEGE_OPERATE_DEVICECOMMUNICATION = "privilege.operate.deviceCommunication";
-   static final String PRIVILEGE_ADMINISTRATE_DEVICEDATA = "privilege.administrate.deviceData";
-   static final String PRIVILEGE_VIEW_DEVICETYPE = "privilege.view.deviceType";
-   static final String PRIVILEGE_ACTION_ISSUE_ACTION = "privilege.action.issue";
-   static final String PRIVILEGE_ASSIGN_ISSUE = "privilege.assign.issue";
-   static final String PRIVILEGE_CLOSE_ISSUE = "privilege.close.issue";
-   static final String PRIVILEGE_COMMENT_ISSUE = "privilege.comment.issue";
-   static final String PRIVILEGE_VIEW_ISSUE = "privilege.view.issue";
-   static final String PRIVILEGE_VIEW_MASTER_DATA = "privilege.view.masterData";
-   static final String PRIVILEGE_VIEW_VALIDATION = "privilege.view.validationConfiguration";
-   static final String VIEW_MDC_IMPORT_SERVICES = "privilege.view.mdc.importServices";
-   static final String VIEW_FWC_CAMPAIGNS = "privilege.view.firmware.campaign";
-
-
-
     static List<String> getApplicationPrivileges(){
-        return Arrays.asList(ADMINISTRATE_DATA_EXPORT_TASK,
-                VIEW_DATA_EXPORT_TASK, UPDATE_DATA_EXPORT_TASK, UPDATE_SCHEDULE_DATA_EXPORT_TASK,
-                RUN_DATA_EXPORT_TASK,
-                VIEW_IMPORT_SERVICES,
+        return Arrays.asList(
 
-                ADMINISTRATE_VALIDATION_CONFIGURATION, VIEW_VALIDATION_CONFIGURATION,
-                VALIDATE_MANUAL,FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE,
-                FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE_CONFIGURATION,
-                VIEW_REPORTS,
+                //validation
+                com.elster.jupiter.validation.security.Privileges.ADMINISTRATE_VALIDATION_CONFIGURATION,
+                com.elster.jupiter.validation.security.Privileges.VIEW_VALIDATION_CONFIGURATION,
+                com.elster.jupiter.validation.security.Privileges.VALIDATE_MANUAL,
+                com.elster.jupiter.validation.security.Privileges.FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE,
+                com.elster.jupiter.validation.security.Privileges.FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE_CONFIGURATION,
 
-                PRIVILEGE_VIEW_REPORTS,
+                //issue
+                com.elster.jupiter.issue.security.Privileges.ACTION_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.ASSIGN_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.CLOSE_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.COMMENT_ISSUE,
+                com.elster.jupiter.issue.security.Privileges.VIEW_ISSUE,
 
-                PRIVILEGE_RUN_EXPORT,
-                PRIVILEGE_VIEW_EXPORT,
+                //issue configuration
+                com.elster.jupiter.issue.security.Privileges.ADMINISTRATE_CREATION_RULE,
+                com.elster.jupiter.issue.security.Privileges.VIEW_ASSIGNMENT_RULE,
+                com.elster.jupiter.issue.security.Privileges.VIEW_CREATION_RULE,
 
-                PRIVILEGE_VIEW_COMMUNICATION,
-                PRIVILEGE_VIEW_DEVICE,
-                PRIVILEGE_OPERATE_DEVICECOMMUNICATION,
-                PRIVILEGE_ADMINISTRATE_DEVICEDATA,
-                PRIVILEGE_VIEW_DEVICETYPE,
-                PRIVILEGE_ACTION_ISSUE_ACTION,
-                PRIVILEGE_ASSIGN_ISSUE,
-                PRIVILEGE_CLOSE_ISSUE,
-                PRIVILEGE_COMMENT_ISSUE,
-                PRIVILEGE_VIEW_ISSUE,
-                PRIVILEGE_VIEW_MASTER_DATA,
-                PRIVILEGE_VIEW_VALIDATION,
-                VIEW_MDC_IMPORT_SERVICES,
-                VIEW_FWC_CAMPAIGNS
+                //yellowfin reports
+                com.elster.jupiter.yellowfin.security.Privileges.VIEW_REPORTS,
 
+                //mdc.engine.config
+                com.energyict.mdc.engine.config.security.Privileges.ADMINISTRATE_COMMUNICATION_ADMINISTRATION,
+                com.energyict.mdc.engine.config.security.Privileges.VIEW_COMMUNICATION_ADMINISTRATION,
+                com.energyict.mdc.engine.config.security.Privileges.VIEW_COMMUNICATION_ADMINISTRATION_INTERNAL,
 
+                //mdc.device.data
+                com.energyict.mdc.device.data.security.Privileges.ADD_DEVICE,
+                com.energyict.mdc.device.data.security.Privileges.REMOVE_DEVICE,
+                com.energyict.mdc.device.data.security.Privileges.VIEW_DEVICE,
+
+                //mdc.device.data - Device communication
+                com.energyict.mdc.device.data.security.Privileges.ADMINISTRATE_DEVICE_COMMUNICATION,
+                com.energyict.mdc.device.data.security.Privileges.OPERATE_DEVICE_COMMUNICATION,
+
+                //mdc.device.data - Device data
+                com.energyict.mdc.device.data.security.Privileges.ADMINISTRATE_DEVICE_ATTRIBUTE,
+                com.energyict.mdc.device.data.security.Privileges.ADMINISTRATE_DEVICE_DATA,
+
+                //mdc.device.data - Devices group
+                com.energyict.mdc.device.data.security.Privileges.ADMINISTRATE_DEVICE_GROUP,
+                com.energyict.mdc.device.data.security.Privileges.ADMINISTRATE_DEVICE_ENUMERATED_GROUP,
+                com.energyict.mdc.device.data.security.Privileges.VIEW_DEVICE_GROUP_DETAIL,
+
+                //mdc.device.data - Device data
+                com.energyict.mdc.device.data.security.Privileges.IMPORT_INVENTORY_MANAGEMENT,
+                com.energyict.mdc.device.data.security.Privileges.REVOKE_INVENTORY_MANAGEMENT,
+
+                //mdc.device.config
+
+                com.energyict.mdc.device.config.security.Privileges.EXECUTE_DEVICE_MESSAGE_1,
+                com.energyict.mdc.device.config.security.Privileges.EXECUTE_DEVICE_MESSAGE_2,
+                com.energyict.mdc.device.config.security.Privileges.EXECUTE_DEVICE_MESSAGE_3,
+                com.energyict.mdc.device.config.security.Privileges.EXECUTE_DEVICE_MESSAGE_4,
+
+                //mdc.device.config - Device security
+                com.energyict.mdc.device.config.security.Privileges.EDIT_DEVICE_SECURITY_PROPERTIES_1,
+                com.energyict.mdc.device.config.security.Privileges.EDIT_DEVICE_SECURITY_PROPERTIES_2,
+                com.energyict.mdc.device.config.security.Privileges.EDIT_DEVICE_SECURITY_PROPERTIES_3,
+                com.energyict.mdc.device.config.security.Privileges.EDIT_DEVICE_SECURITY_PROPERTIES_4,
+
+                com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_SECURITY_PROPERTIES_1,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_SECURITY_PROPERTIES_2,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_SECURITY_PROPERTIES_3,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_SECURITY_PROPERTIES_4,
+
+                //mdc.device.config - Device type
+                com.energyict.mdc.device.config.security.Privileges.ADMINISTRATE_DEVICE_TYPE,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_DEVICE_TYPE,
+
+                //mdc.device.config - Master data
+                com.energyict.mdc.device.config.security.Privileges.ADMINISTRATE_MASTER_DATA,
+                com.energyict.mdc.device.config.security.Privileges.VIEW_MASTER_DATA,
+
+                //estimation
+                com.elster.jupiter.estimation.security.Privileges.Constants.ADMINISTRATE_ESTIMATION_CONFIGURATION,
+                com.elster.jupiter.estimation.security.Privileges.Constants.ADMINISTRATE_ESTIMATION_TASK,
+                com.elster.jupiter.estimation.security.Privileges.Constants.RUN_ESTIMATION_TASK,
+                com.elster.jupiter.estimation.security.Privileges.Constants.UPDATE_ESTIMATION_CONFIGURATION,
+                com.elster.jupiter.estimation.security.Privileges.Constants.UPDATE_SCHEDULE_ESTIMATION_TASK,
+                com.elster.jupiter.estimation.security.Privileges.Constants.VIEW_ESTIMATION_CONFIGURATION,
+                com.elster.jupiter.estimation.security.Privileges.Constants.VIEW_ESTIMATION_TASK,
+                com.elster.jupiter.estimation.security.Privileges.Constants.FINE_TUNE_ESTIMATION_CONFIGURATION_ON_DEVICE,
+                com.elster.jupiter.estimation.security.Privileges.Constants.FINE_TUNE_ESTIMATION_CONFIGURATION_ON_DEVICE_CONFIGURATION,
+
+                //import
+                com.elster.jupiter.fileimport.security.Privileges.VIEW_IMPORT_SERVICES,
+
+                //mdc.firmware
+                com.energyict.mdc.firmware.security.Privileges.ADMINISTRATE_FIRMWARE_CAMPAIGN,
+                com.energyict.mdc.firmware.security.Privileges.VIEW_FIRMWARE_CAMPAIGN,
+
+                //mdc.device.lifecycle
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_1,
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_2,
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_3,
+                com.energyict.mdc.device.lifecycle.config.Privileges.INITIATE_ACTION_4,
+
+                com.energyict.mdc.device.lifecycle.config.Privileges.CONFIGURE_DEVICE_LIFE_CYCLE,
+                com.energyict.mdc.device.lifecycle.config.Privileges.VIEW_DEVICE_LIFE_CYCLE,
+
+                //com.elster.jupiter.metering.security - usage points
+                com.elster.jupiter.metering.security.Privileges.ADMIN_ANY,
+                com.elster.jupiter.metering.security.Privileges.ADMIN_OWN,
+                com.elster.jupiter.metering.security.Privileges.BROWSE_ANY,
+                com.elster.jupiter.metering.security.Privileges.BROWSE_OWN,
+
+                //mdc.scheduling.security
+                com.energyict.mdc.scheduling.security.Privileges.ADMINISTRATE_SHARED_COMMUNICATION_SCHEDULE,
+                com.energyict.mdc.scheduling.security.Privileges.VIEW_SHARED_COMMUNICATION_SCHEDULE
 
 
         );
