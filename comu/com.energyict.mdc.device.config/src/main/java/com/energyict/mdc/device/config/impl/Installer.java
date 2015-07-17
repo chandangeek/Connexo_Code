9b27cb0dcb5f794cc16f414a813e0cc2adb4d866
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-07-17 17:17:04 +0200

    Cleanup

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/Installer.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/Installer.java
@@ -1,23 +1,16 @@
 package com.energyict.mdc.device.config.impl;
 
-import com.energyict.mdc.device.config.DeviceMessageUserAction;
-import com.energyict.mdc.device.config.DeviceSecurityUserAction;
 import com.energyict.mdc.device.config.events.EventType;
-import com.energyict.mdc.device.config.security.Privileges;
 
 import com.elster.jupiter.events.EventService;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.users.UserService;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static java.util.stream.Collectors.toList;
-
 /**
- * Represents the Installer for the DeviceConfiguration module
+ * Represents the Installer for the DeviceConfiguration module.
  *
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2014-01-30 (15:42)
@@ -45,8 +38,6 @@ public class Installer {
         }
         createEventTypes();
     }
-
-
 
     private void createEventTypes() {
         for (EventType eventType : EventType.values()) {
