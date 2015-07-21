2fee726e8d3f7a658285b313bf9a6ef683c1fabd
Anton Suslennikov <antsusl@mera.ru>
2015-07-21 15:29:24 +0300

    Fix component name for privileges

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/DevicePrivileges.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/DevicePrivileges.java
@@ -50,7 +50,7 @@ public final class DevicePrivileges {
             @Override
             List<String> getPrivileges(User user) {
                 List<String> privileges = new ArrayList<>();
-                if (user != null && user.hasPrivilege(DeviceDataServices.COMPONENT_NAME, Privileges.ADMINISTER_DECOMMISSIONED_DEVICE_DATA)) {
+                if (user != null && user.hasPrivilege("MDC", Privileges.ADMINISTER_DECOMMISSIONED_DEVICE_DATA)) {
                     privileges.add(DEVICES_ACTIONS_DATA_EDIT);
                 }
                 return privileges;
