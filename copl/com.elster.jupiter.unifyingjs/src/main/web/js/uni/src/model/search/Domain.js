3e54b2470655d90f0be31433bc3c4faad2da2cad
Artem Puchenkin <apuchenkin@gmail.com>
2015-07-24 16:43:40 +0300

    [COMU-1345] Secondary search criteria zone

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/model/search/Domain.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/model/search/Domain.js
@@ -4,63 +4,67 @@
 Ext.define('Uni.model.search.Domain', {
     extend: 'Ext.data.Model',
 
+    removeDomain: function (url) {
+        return url.replace(/http:\/\/.*:\d+(\/.*)/, '$1');
+    },
+
     fields: [
         {name: 'id', type: 'string'},
         {name: 'displayValue', type: 'string'},
         {
             name: 'selfHref', type: 'auto', convert: function (v, record) {
-            var linkParams = record.raw.link,
-                result = undefined;
+                var linkParams = record.raw.link,
+                    result = undefined;
 
-            if (Ext.isArray(linkParams)) {
-                linkParams.forEach(function (linkParam) {
-                    if (linkParam.params.rel === 'self') {
-                        result = linkParam.href;
-                        return false;
-                    }
-                    return true;
-                })
+                if (Ext.isArray(linkParams)) {
+                    linkParams.forEach(function (linkParam) {
+                        if (linkParam.params.rel === 'self') {
+                            result = record.removeDomain(linkParam.href);
+                            return false;
+                        }
+                        return true;
+                    })
+                }
+
+                return result;
             }
-
-            return result;
-        }
         }
         ,
         {
             name: 'glossaryHref', type: 'auto', convert: function (v, record) {
-            var linkParams = record.raw.link,
-                result = undefined;
+                var linkParams = record.raw.link,
+                    result = undefined;
 
-            if (Ext.isArray(linkParams)) {
-                linkParams.forEach(function (linkParam) {
-                    if (linkParam.params.rel === 'glossary') {
-                        result = linkParam.href;
-                        return false;
-                    }
-                    return true;
-                })
+                if (Ext.isArray(linkParams)) {
+                    linkParams.forEach(function (linkParam) {
+                        if (linkParam.params.rel === 'glossary') {
+                            result = record.removeDomain(linkParam.href);
+                            return false;
+                        }
+                        return true;
+                    })
+                }
+
+                return result;
             }
-
-            return result;
-        }
         },
         {
             name: 'describedByHref', type: 'auto', convert: function (v, record) {
-            var linkParams = record.raw.link,
-                result = undefined;
+                var linkParams = record.raw.link,
+                    result = undefined;
 
-            if (Ext.isArray(linkParams)) {
-                linkParams.forEach(function (linkParam) {
-                    if (linkParam.params.rel === 'describedby') {
-                        result = linkParam.href;
-                        return false;
-                    }
-                    return true;
-                })
+                if (Ext.isArray(linkParams)) {
+                    linkParams.forEach(function (linkParam) {
+                        if (linkParam.params.rel === 'describedby') {
+                            result = record.removeDomain(linkParam.href);
+                            return false;
+                        }
+                        return true;
+                    })
+                }
+
+                return result;
             }
-
-            return result;
-        }
         }
     ]
 });
