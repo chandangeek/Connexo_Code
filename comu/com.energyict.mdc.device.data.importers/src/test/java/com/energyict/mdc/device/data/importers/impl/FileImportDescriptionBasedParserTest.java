c2a8e6b1e1a04d66cc0445d707a84cc9f94cd245
Anton Suslennikov <antsusl@mera.ru>
2015-07-27 13:57:13 +0300

    Fix errors

--- comu/com.energyict.mdc.device.data.importers/src/test/java/com/energyict/mdc/device/data/importers/impl/FileImportDescriptionBasedParserTest.java
+++ comu/com.energyict.mdc.device.data.importers/src/test/java/com/energyict/mdc/device/data/importers/impl/FileImportDescriptionBasedParserTest.java
@@ -135,7 +135,50 @@ public class FileImportDescriptionBasedParserTest {
 
         importer.process(importOccurrence);
 
+        verify(importOccurrence).markSuccess(TranslationKeys.IMPORT_RESULT_SUCCESS.getTranslated(thesaurus, 1));
         verify(record, times(3)).addReading(Matchers.startsWith("r"));
         verify(record, times(3)).addValue(Matchers.startsWith("v"));
+    }
+
+
+    @Test
+    public void testProcessRepetitiveColumnsWithoutDescriptionSupport() throws Exception {
+        String csv = "Device MRID;Reading1;Value1;Reading2;Value2;Reading3;Value3\n"
+                + "SPE001;r1;v1;r2;v2;r3;v3";
+        FileImportOccurrence importOccurrence = mockFileImportOccurrence(csv);
+        RepetitiveRecord record = spy(new RepetitiveRecord());
+        FileImportParser<FileImportRecord> parser = new FileImportDescriptionBasedParser(new FileImportDescription<RepetitiveRecord>(){
+            @Override
+            public RepetitiveRecord getFileImportRecord() {
+                return record;
+            }
+
+            @Override
+            public List<FileImportField<?>> getFields(RepetitiveRecord record) {
+                List<FileImportField<?>> fields = new ArrayList<>();
+                LiteralStringParser stringParser = new LiteralStringParser();
+                fields.add(CommonField.withParser(stringParser)
+                        .withConsumer(record::setDeviceMRID)
+                        .markMandatory()
+                        .build());
+                fields.add(CommonField.withParser(stringParser)
+                        .withConsumer(record::addReading)
+                        .markMandatory()
+                        .build());
+                fields.add(CommonField.withParser(stringParser)
+                        .withConsumer(record::addValue)
+                        .markMandatory()
+                        .build());
+                return fields;
+            }
+        });
+        FileImportProcessor<FileImportRecord> processor = mock(FileImportProcessor.class);
+        DeviceDataCsvImporter<FileImportRecord> importer = DeviceDataCsvImporter.withParser(parser).withProcessor(processor).withDelimiter(';').build(context);
+
+        importer.process(importOccurrence);
+
+        verify(importOccurrence).markSuccess(TranslationKeys.IMPORT_RESULT_SUCCESS.getTranslated(thesaurus, 1));
+        verify(record, times(1)).addReading(Matchers.startsWith("r"));
+        verify(record, times(1)).addValue(Matchers.startsWith("v"));
     }
 }
