0d9fd518a79dc7be084b98c5b35ea13b53bd5ae6
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-07-27 15:25:09 +0200

    Added annotation to enfore functional

--- comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/utils/PropertyCopier.java
+++ comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/utils/PropertyCopier.java
@@ -6,6 +6,7 @@ import javax.ws.rs.core.UriInfo;
  * Functional interface used to copy a single property from a domain object to an info object. UriInfo is passed along should there be a need to create links
  * Created by bvn on 5/13/15.
  */
+@FunctionalInterface
 public interface PropertyCopier<D,S> {
     public void copy(D d, S s, UriInfo uriInfo);
 }
