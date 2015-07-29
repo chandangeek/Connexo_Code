7f0621e95e2fae69c7e9ff8638ce924d07fcfb5d
prikazch <prikazch@mera.ru>
2015-07-29 18:10:12 +0300

    [COMU-1415] FE - Use a number range

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/NumberLine.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/NumberLine.js
@@ -1,56 +1,74 @@
 Ext.define('Uni.view.search.field.NumberLine', {
-    extend: 'Ext.container.Container',
+    extend: 'Ext.panel.Panel',
     xtype: 'uni-view-search-field-number-line',
-    margin: '5px 5px 3px 5px',
-    width: '450',
+    width: '455',
+    layout: 'hbox',
     items: [
         {
-            xtype: 'container',
-            layout: 'hbox',
-            items: [
-                {
-                    xtype: 'combo',
-                    value: '>',
-                    disabled: true,
-                    width: 55,
-                    margin: '1px 30px 0px 5px'
-                },
-                {
-                    xtype: 'numberfield',
-                    margin: '1px 5px 0px 0px',
-                    value: 0,
-                    //hideTrigger: true,
-                    listeners: {
-                        change: function() {
-                            me = this;
-                            if (me.value === 0) {
-                                me.up('container').down('button').disable(true)
-                            } else {
-                                me.up('container').down('button').enable(true)
-                                me.up('button').setText( me.up('button').defaultText + '*')
-                            }
+            xtype: 'combo',
+            disabled: true,
+            width: 55,
+            margin: '1px 30px 1px 5px'
+        },
+        {
+            xtype: 'numberfield',
+            margin: '1px 5px 1px 0px',
+            value: 0,
+            width: 180,
+            listeners: {
+                change: function () {
+                    me = this;
+                    if (me.value === 0) {
+                        me.up('container').down('button').disable(true)
+                    } else {
+                        me.up('container').down('button').enable(true)
+                        me.up('button').setText(me.up('button').defaultText + '*')
+                    }
 
-                        }
-                    }
-                },
-                {
-                    xtype: 'button',
-                    itemId: 'filter-clear',
-                    ui: 'plain',
-                    tooltip: 'Clear filter',
-                    iconCls: ' icon-close4',
-                    margin: '0px 0px 0px 5px',
-                    padding: 6,
-                    disabled: true,
-                    style: {
-                        fontSize: '16px'
-                    },
-                    handler: function () {
-                        var me = this;
-                        me.up('menu').focus().down('numberfield').reset();
-                    }
                 }
-            ]
+            }
         }
-    ]
+
+    ],
+    rbar: {
+        width: 30,
+        items: [
+            {
+                xtype: 'button',
+                itemId: 'filter-clear',
+                ui: 'plain',
+                tooltip: 'Clear filter',
+                iconCls: ' icon-close4',
+                margin: '0px 0px 5px 6px',
+                disabled: true,
+                hidden: true,
+                style: {
+                    fontSize: '16px'
+                },
+                handler: function () {
+                    var me = this;
+                    me.up('menu').focus().down('numberfield').reset();
+                }
+            }
+        ]
+    }
+
+    ,
+    listeners: {
+        render: function (c) {
+            var button = c.down('#filter-clear');
+            c.el.on('mouseover', function () {
+                button.setVisible(true);
+            });
+            c.el.on('mouseout', function () {
+                button.setVisible(false);
+            });
+        }
+
+    },
+    initComponent: function () {
+        me = this;
+        me.callParent(arguments);
+        me.down('combo').setValue(this.operator);
+    }
 });
--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/NumberRange.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/NumberRange.js
@@ -1,136 +1,28 @@
 Ext.define('Uni.view.search.field.NumberRange', {
-    extend: 'Ext.button.Button',
+    extend: 'Ext.panel.Panel',
     alias: 'widget.uni-view-search-field-number-range',
     xtype: 'uni-view-search-field-number-range',
-    text: Uni.I18n.translate('search.overview.lastReadingDate.emptyText', 'UNI', 'Interval'),
-    defaultText: Uni.I18n.translate('search.overview.lastReadingDate.emptyText', 'UNI', 'Interval'),
-    arrowAlign: 'right',
-    menuAlign: 'tr-br',
     requires: [
         'Uni.view.search.field.NumberLine'
     ],
-    layout: 'hbox',
-    style: {
-        'background-color': '#71adc7'
-    },
-    mixins: [
-        'Ext.util.Bindable'
-    ],
-
-    clearAllHandler: function () {
-        var me = this;
-        var items = me.down('menu').items.items;
-        Ext.each(items, function (item) {
-            if (item.down('numberfield')) item.down('numberfield').reset();
-        });
-        me.setText( me.defaultText)
-    },
-    addRangeHandler: function () {
-        var me = this;
-        me.down('menu').add({
-            xtype: 'container',
-            margin: '5px 0px 0px 0px',
-            items: [
-                {
-                    xtype: 'uni-view-search-field-number-line',
-                    operator: '>'
-                }
-            ]
-        });
-        me.down('menu').add({
-            xtype: 'container',
-            margin: '0px 0px 0px 0px',
-            items: [
-                {
-                    xtype: 'uni-view-search-field-number-line',
-                    operator: '<'
-                }
-            ]
-        });
-    },
+    layout: 'vbox',
+    items:
+        [
+            {
+                xtype: 'uni-view-search-field-number-line',
+                default: true,
+                operator: '>',
+                margin: '0 0 3 0'
+            },
+            {
+                xtype: 'uni-view-search-field-number-line',
+                default: true,
+                operator: '<',
+                margin: '0 0 3 0'
+            }
+        ],
 
     initComponent: function () {
-        var me = this;
-        me.menu = {
-            plain: true,
-            style: {
-                overflow: 'visible'
-            },
-            cls: 'x-menu-body-custom',
-            minWidth: 273,
-            items: [
-                {
-                    xtype: 'uni-view-search-field-number-line',
-                    default: true,
-                    operator: '='
-                },
-                {
-                    xtype: 'menuseparator',
-                    default: true
-                },
-                {
-                    xtype: 'uni-view-search-field-number-line',
-                    default: true,
-                    operator: '>'
-                },
-                {
-                    xtype: 'uni-view-search-field-number-line',
-                    default: true,
-                    operator: '<'
-                }
-            ],
-            listeners: {
-                hide: function (menu) {
-                    Ext.each(menu.items.items, function (item) {
-                        if (item && !item.default && item.down('numberfield').getValue() == 0) {
-                            menu.remove(item);
-                        }
-                    });
-                }
-            },
-            dockedItems: [
-                {
-                    xtype: 'toolbar',
-                    background: '#71adc7',
-                    dock: 'bottom',
-                    items: [
-                        {
-                            flex: 2,
-                            cls: 'x-spacers'
-                        },
-                        {
-                            xtype: 'button',
-                            text: Uni.I18n.translate('general.clearAll', 'UNI', 'Clear all'),
-                            align: 'right',
-                            handler: function () {
-                                me.clearAllHandler();
-                            }
-                        },
-                        {
-                            xtype: 'button',
-                            ui: 'action',
-                            text: Uni.I18n.translate('general.addRange', 'UNI', 'Add number range'),
-                            action: 'addrange',
-                            margin: '0 0 0 0',
-                            handler: function () {
-                                me.addRangeHandler();
-                            }
-                        },
-                        {
-                            flex: 1.35,
-                            cls: 'x-spacers'
-                        }
-                    ]
-                }
-            ]
-        };
-
-
         this.callParent(arguments);
-        Ext.suspendLayouts();
-        this.menu.items.items[0].down('combo').setValue(this.menu.items.items[0].operator);
-        this.menu.items.items[2].down('combo').setValue(this.menu.items.items[2].operator);
-        this.menu.items.items[3].down('combo').setValue(this.menu.items.items[3].operator);
-        Ext.resumeLayouts(true);
     }
 });
