9fecde470c8126320a96a6d51bd0dcc003c664f2
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-07-30 17:32:57 +0200

    Fix build errors after (manual) merge master into branch

--- comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/ConnectionResource.java
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/ConnectionResource.java
@@ -1,19 +1,10 @@
 package com.energyict.mdc.dashboard.rest.status.impl;
 
-import com.elster.jupiter.appserver.AppServer;
-import com.elster.jupiter.appserver.AppService;
-import com.elster.jupiter.messaging.DestinationSpec;
-import com.elster.jupiter.messaging.MessageService;
-import com.elster.jupiter.metering.groups.MeteringGroupsService;
-import com.elster.jupiter.rest.util.JsonQueryFilter;
-import com.elster.jupiter.util.json.JsonService;
-import com.elster.jupiter.util.time.Interval;
-import com.elster.jupiter.rest.util.ExceptionFactory;
+import com.energyict.mdc.common.TypedProperties;
 import com.energyict.mdc.common.rest.IdWithNameInfo;
-import com.elster.jupiter.rest.util.PagedInfoList;
-import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.data.ConnectionTaskService;
+import com.energyict.mdc.device.data.FilterFactory;
 import com.energyict.mdc.device.data.QueueMessage;
 import com.energyict.mdc.device.data.rest.ComSessionSuccessIndicatorAdapter;
 import com.energyict.mdc.device.data.rest.ConnectionTaskSuccessIndicatorAdapter;
@@ -23,25 +14,36 @@ import com.energyict.mdc.device.data.tasks.ComTaskExecution;
 import com.energyict.mdc.device.data.tasks.ConnectionTask;
 import com.energyict.mdc.device.data.tasks.ConnectionTaskFilterSpecification;
 import com.energyict.mdc.device.data.tasks.ConnectionTaskFilterSpecificationMessage;
-import com.energyict.mdc.device.data.tasks.ConnectionTaskQueueMessage;
-import com.energyict.mdc.device.data.tasks.ItemizeConnectionFilterQueueMessage;
+import com.energyict.mdc.device.data.tasks.ItemizeConnectionFilterRescheduleQueueMessage;
+import com.energyict.mdc.device.data.tasks.ItemizeConnectionFilterUpdatePropertiesQueueMessage;
+import com.energyict.mdc.device.data.tasks.RescheduleConnectionTaskQueueMessage;
 import com.energyict.mdc.device.data.tasks.ScheduledConnectionTask;
 import com.energyict.mdc.device.data.tasks.TaskStatus;
+import com.energyict.mdc.device.data.tasks.UpdateConnectionTaskPropertiesQueueMessage;
 import com.energyict.mdc.device.data.tasks.history.ComSession;
 import com.energyict.mdc.device.data.tasks.history.ComTaskExecutionSession;
 import com.energyict.mdc.engine.config.ComPortPool;
 import com.energyict.mdc.engine.config.EngineConfigurationService;
+import com.energyict.mdc.pluggable.rest.MdcPropertyUtils;
 import com.energyict.mdc.protocol.pluggable.ConnectionTypePluggableClass;
 import com.energyict.mdc.protocol.pluggable.ProtocolPluggableService;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
+import com.elster.jupiter.appserver.AppServer;
+import com.elster.jupiter.appserver.AppService;
+import com.elster.jupiter.messaging.DestinationSpec;
+import com.elster.jupiter.messaging.MessageService;
+import com.elster.jupiter.metering.groups.MeteringGroupsService;
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.rest.util.ExceptionFactory;
+import com.elster.jupiter.rest.util.JsonQueryFilter;
+import com.elster.jupiter.rest.util.JsonQueryParameters;
+import com.elster.jupiter.rest.util.PagedInfoList;
+import com.elster.jupiter.rest.util.properties.PropertyInfo;
+import com.elster.jupiter.util.json.JsonService;
+import com.elster.jupiter.util.time.Interval;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.BeanParam;
@@ -51,10 +53,21 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.elster.jupiter.util.streams.Functions.asStream;
 import static java.util.stream.Collectors.toSet;
@@ -77,9 +90,11 @@ public class ConnectionResource {
     private final MessageService messageService;
     private final JsonService jsonService;
     private final AppService appService;
+    private final MdcPropertyUtils mdcPropertyUtils;
+    private final FilterFactory filterFactory;
 
     @Inject
-    public ConnectionResource(ConnectionTaskService connectionTaskService, EngineConfigurationService engineConfigurationService, ProtocolPluggableService protocolPluggableService, DeviceConfigurationService deviceConfigurationService, ConnectionTaskInfoFactory connectionTaskInfoFactory, ExceptionFactory exceptionFactory, MeteringGroupsService meteringGroupsService, ComTaskExecutionSessionInfoFactory comTaskExecutionSessionInfoFactory, MessageService messageService, JsonService jsonService, AppService appService) {
+    public ConnectionResource(ConnectionTaskService connectionTaskService, EngineConfigurationService engineConfigurationService, ProtocolPluggableService protocolPluggableService, DeviceConfigurationService deviceConfigurationService, ConnectionTaskInfoFactory connectionTaskInfoFactory, ExceptionFactory exceptionFactory, MeteringGroupsService meteringGroupsService, ComTaskExecutionSessionInfoFactory comTaskExecutionSessionInfoFactory, MessageService messageService, JsonService jsonService, AppService appService, MdcPropertyUtils mdcPropertyUtils, FilterFactory filterFactory) {
         super();
         this.connectionTaskService = connectionTaskService;
         this.engineConfigurationService = engineConfigurationService;
@@ -92,6 +107,8 @@ public class ConnectionResource {
         this.messageService = messageService;
         this.jsonService = jsonService;
         this.appService = appService;
+        this.mdcPropertyUtils = mdcPropertyUtils;
+        this.filterFactory = filterFactory;
     }
 
     @GET
@@ -282,7 +299,7 @@ public class ConnectionResource {
         if (connectionsBulkRequestInfo !=null && connectionsBulkRequestInfo.filter!=null) {
             Optional<DestinationSpec> destinationSpec = messageService.getDestinationSpec(ConnectionTaskService.FILTER_ITEMIZER_QUEUE_DESTINATION);
             if (destinationSpec.isPresent()) {
-                return processMessagePost(new ItemizeConnectionFilterQueueMessage(substituteRestToDomainEnums(connectionsBulkRequestInfo.filter), "scheduleNow"), destinationSpec.get());
+                return processMessagePost(new ItemizeConnectionFilterRescheduleQueueMessage(substituteRestToDomainEnums(connectionsBulkRequestInfo.filter), "scheduleNow"), destinationSpec.get());
             } else {
                 throw exceptionFactory.newException(MessageSeeds.NO_SUCH_MESSAGE_QUEUE);
             }
@@ -290,13 +307,154 @@ public class ConnectionResource {
         } else if (connectionsBulkRequestInfo !=null && connectionsBulkRequestInfo.connections!=null) {
             Optional<DestinationSpec> destinationSpec = messageService.getDestinationSpec(ConnectionTaskService.CONNECTION_RESCHEDULER_QUEUE_DESTINATION);
             if (destinationSpec.isPresent()) {
-                connectionsBulkRequestInfo.connections.stream().forEach(c -> processMessagePost(new ConnectionTaskQueueMessage(c, "scheduleNow"), destinationSpec.get()));
+                connectionsBulkRequestInfo.connections.stream().forEach(c -> processMessagePost(new RescheduleConnectionTaskQueueMessage(c, "scheduleNow"), destinationSpec.get()));
                 return Response.status(Response.Status.OK).build();
             } else {
                 throw exceptionFactory.newException(MessageSeeds.NO_SUCH_MESSAGE_QUEUE);
             }
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    @GET
+    @Path("/properties")
+    @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed({Privileges.VIEW_DEVICE, Privileges.OPERATE_DEVICE_COMMUNICATION, Privileges.ADMINISTRATE_DEVICE_COMMUNICATION})
+    public Response getCommonConnectionTaskProperties(@QueryParam("filter") String filterQueryParam, @QueryParam("connections") String connectionsQueryParam) throws Exception {
+        ConnectionTypePluggableClass connectionType = getConnectionTypePluggableClassFromQueryParameters(filterQueryParam, connectionsQueryParam);
+        List<PropertySpec> propertySpecs = connectionType.getPropertySpecs();
+        TypedProperties typedProperties = connectionType.getProperties(propertySpecs);
+        PropertiesBulkRequestInfo info = new PropertiesBulkRequestInfo();
+        info.properties=mdcPropertyUtils.convertPropertySpecsToPropertyInfos(propertySpecs, typedProperties);
+        return Response.ok(info).build();
+    }
+
+    @PUT
+    @Path("/properties")
+    @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed({Privileges.VIEW_DEVICE, Privileges.OPERATE_DEVICE_COMMUNICATION, Privileges.ADMINISTRATE_DEVICE_COMMUNICATION})
+    public Response updateCommonConnectionTaskProperties(PropertiesBulkRequestInfo propertiesBulkRequestInfo) throws Exception {
+        if (!verifyAppServerExists(ConnectionTaskService.FILTER_ITEMIZER_PROPERTIES_QUEUE_DESTINATION) || !verifyAppServerExists(ConnectionTaskService.CONNECTION_PROP_UPDATER_QUEUE_DESTINATION)) {
+            throw exceptionFactory.newException(MessageSeeds.NO_APPSERVER);
+        }
+        if (propertiesBulkRequestInfo!=null && propertiesBulkRequestInfo.properties!=null && !propertiesBulkRequestInfo.properties.isEmpty()) {
+            // we do the call below merely as validation for uniqueness of ConnectionTypePluggableClass
+            getConnectionTypePluggableClassFromQueryParameters(propertiesBulkRequestInfo.filter, propertiesBulkRequestInfo.connections);
+            Map<String, String> properties = convertPropertyInfosToMap(propertiesBulkRequestInfo.properties);
+
+            if (propertiesBulkRequestInfo.filter != null) {
+                Optional<DestinationSpec> destinationSpec = messageService.getDestinationSpec(ConnectionTaskService.FILTER_ITEMIZER_PROPERTIES_QUEUE_DESTINATION);
+                if (destinationSpec.isPresent()) {
+                    return processMessagePost(new ItemizeConnectionFilterUpdatePropertiesQueueMessage(substituteRestToDomainEnums(propertiesBulkRequestInfo.filter), properties), destinationSpec.get());
+                } else {
+                    throw exceptionFactory.newException(MessageSeeds.NO_SUCH_MESSAGE_QUEUE);
+                }
+
+            } else if (propertiesBulkRequestInfo.connections != null) {
+                Optional<DestinationSpec> destinationSpec = messageService.getDestinationSpec(ConnectionTaskService.CONNECTION_PROP_UPDATER_QUEUE_DESTINATION);
+                if (destinationSpec.isPresent()) {
+                    propertiesBulkRequestInfo.connections.stream().forEach(c -> processMessagePost(new UpdateConnectionTaskPropertiesQueueMessage(c, properties), destinationSpec.get()));
+                    return Response.status(Response.Status.OK).build();
+                } else {
+                    throw exceptionFactory.newException(MessageSeeds.NO_SUCH_MESSAGE_QUEUE);
+                }
+            }
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    private Map<String, String> convertPropertyInfosToMap(List<PropertyInfo> propertyInfos) {
+        Map<String, String> properties = new HashMap<>();
+        propertyInfos.stream().forEach(info -> {
+            Object value = info.getPropertyValueInfo().getValue();
+            properties.put(info.key, value==null?null:jsonService.serialize(value));
+        });
+        return properties;
+    }
+
+    /**
+     * Gets the ConnectionTypePluggableClass from either filter or list of connections.
+     *
+     * @param filterQueryParam Describes the filter accepted by the method
+     * @param connectionsQueryParam Describes a list of (long) ids
+     * @return unique ConnectionTypePluggableClass, or exception if not unique
+     * @throws IOException if Jackson was unable to parse the Json filter or id list
+     * @throws RuntimeException if the distills ConnectionTypePluggableClass was not unique
+     *
+     */
+    private ConnectionTypePluggableClass getConnectionTypePluggableClassFromQueryParameters(String filterQueryParam, String connectionsQueryParam) throws Exception {
+        List<ConnectionTypePluggableClass> connectionTypePluggableClasses = new ArrayList<>();
+        if (filterQueryParam!=null) {
+            ConnectionTaskFilterSpecification filter = buildFilterFromJsonQuery(new JsonQueryFilter(filterQueryParam));
+            connectionTypePluggableClasses.addAll(connectionTaskService.findConnectionTypeByFilter(filter));
+        } else if (connectionsQueryParam!=null) {
+            connectionTypePluggableClasses.addAll(getPluggableClassesFromConnectionQueryString(connectionsQueryParam));
+        }
+
+        validateConnectionTypeSelection(connectionTypePluggableClasses);
+        return connectionTypePluggableClasses.get(0);
+    }
+
+    /**
+     * Gets the ConnectionTypePluggableClass from either filter or list of connections.
+     * @param filterMessage Describes the filter accepted by the method
+     * @param connections Describes a list of (long) ids
+     * @return unique ConnectionTypePluggableClass, or exception if not unique
+     * @throws IOException if Jackson was unable to parse the Json filter or id list
+     * @throws RuntimeException if the distilles ConnectionTypePluggableClass was not unique
+     *
+     */
+    private ConnectionTypePluggableClass getConnectionTypePluggableClassFromQueryParameters(ConnectionTaskFilterSpecificationMessage filterMessage, List<Long> connections) throws Exception {
+        List<ConnectionTypePluggableClass> connectionTypePluggableClasses = new ArrayList<>();
+        if (filterMessage!=null) {
+            ConnectionTaskFilterSpecification filter = filterFactory.buildFilterFromMessage(filterMessage);
+            connectionTypePluggableClasses.addAll(connectionTaskService.findConnectionTypeByFilter(filter));
+        } else if (connections!=null) {
+            connectionTypePluggableClasses.addAll(getPluggableClassesFromConnectionTaskIdList(connections));
+        }
+
+        validateConnectionTypeSelection(connectionTypePluggableClasses);
+        return connectionTypePluggableClasses.get(0);
+    }
+
+    private void validateConnectionTypeSelection(List<ConnectionTypePluggableClass> connectionTypePluggableClasses) {
+        if (connectionTypePluggableClasses.size()>1) {
+            throw exceptionFactory.newException(MessageSeeds.CONNECTION_TASK_NOT_UNIQUE);
+        }
+        if (connectionTypePluggableClasses.isEmpty()) {
+            throw exceptionFactory.newException(MessageSeeds.ONE_CONNECTION_TYPE_REQUIRED);
+        }
+    }
+
+    private List<ConnectionTypePluggableClass> getPluggableClassesFromConnectionQueryString(String connectionsQueryParam) throws IOException {
+        List<ConnectionTypePluggableClass> connectionTypePluggableClasses = new ArrayList<>();
+        JsonNode node = new ObjectMapper().readValue(connectionsQueryParam, JsonNode.class);
+        if (node != null && node.isArray()) {
+            List<Long> connectionTaskIds = new ArrayList<>();
+            for (JsonNode singleFilter : node) {
+                connectionTaskIds.add(singleFilter.asLong());
+            }
+            connectionTypePluggableClasses.addAll(getPluggableClassesFromConnectionTaskIdList(connectionTaskIds));
+        }
+        return connectionTypePluggableClasses;
+    }
+
+    private List<ConnectionTypePluggableClass> getPluggableClassesFromConnectionTaskIdList(List<Long> connections) throws IOException {
+        List<ConnectionTypePluggableClass> connectionTypePluggableClasses = new ArrayList<>();
+        if (connections != null) {
+            List<String> javaClassNames = new ArrayList<>();
+            for (Long connectionTaskId : connections) {
+                connectionTaskService.
+                        findConnectionTask(connectionTaskId).
+                        ifPresent((connectionTask) -> javaClassNames.add(connectionTask.getPluggableClass().getJavaClassName()));
+            }
+            protocolPluggableService.findAllConnectionTypePluggableClasses().stream().
+                    filter(pluggableClass->javaClassNames.contains(pluggableClass.getJavaClassName())).
+                    forEach(connectionTypePluggableClasses::add);
+        }
+        return connectionTypePluggableClasses;
     }
 
     private boolean verifyAppServerExists(String destinationName) {
@@ -313,7 +471,7 @@ public class ConnectionResource {
     private Response processMessagePost(QueueMessage message, DestinationSpec destinationSpec) {
         String json = jsonService.serialize(message);
         destinationSpec.message(json).send();
-        return Response.ok().entity("{\"success\":\"true\"}").build();
+        return Response.accepted().entity("{\"success\":\"true\"}").build();
     }
 
 }
