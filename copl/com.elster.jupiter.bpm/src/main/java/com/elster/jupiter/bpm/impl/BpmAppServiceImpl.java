c78476f4cd7b50d1b284fe48181cbef316377a94
Mihai Andreescu <mihai.andreescu@ro.elster.com>
2015-08-04 13:33:49 +0300

    COMU-1565 - Add flow in privileges application grid.

--- copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/BpmAppServiceImpl.java
+++ copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/BpmAppServiceImpl.java
@@ -2,9 +2,11 @@ package com.elster.jupiter.bpm.impl;
 
 import com.elster.jupiter.bpm.BpmAppService;
 import com.elster.jupiter.bpm.BpmService;
+import com.elster.jupiter.bpm.security.Privileges;
 import com.elster.jupiter.http.whiteboard.App;
 import com.elster.jupiter.license.License;
 import com.elster.jupiter.orm.callback.InstallService;
+import com.elster.jupiter.users.ApplicationPrivilegesProvider;
 import com.elster.jupiter.users.UserService;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -14,17 +16,20 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
 
 @Component(
         name = "com.elster.jupiter.bpm.app",
-        service = {BpmAppService.class},
+        service = {BpmAppService.class, ApplicationPrivilegesProvider.class},
         immediate = true
 )
-public class BpmAppServiceImpl implements BpmAppService {
+public class BpmAppServiceImpl implements BpmAppService , ApplicationPrivilegesProvider {
 
     private volatile ServiceRegistration<App> registration;
     private volatile BpmService bpmService;
     private volatile License license;
+    public static final String APP_KEY = "BPM";
 
     public BpmAppServiceImpl() {
     }
@@ -37,7 +42,7 @@ public class BpmAppServiceImpl implements BpmAppService {
 
     @Activate
     public final void activate(BundleContext context) {
-        App app = new App("BPM", "Flow", "connexo", bpmService.getBpmServer().getUrl(), user -> user.getPrivileges(bpmService.COMPONENTNAME).stream().anyMatch(p -> "privilege.design.bpm".equals(p.getName())));
+        App app = new App(APPLICATION_KEY, "Flow", "connexo", bpmService.getBpmServer().getUrl(), user -> user.getPrivileges(bpmService.COMPONENTNAME).stream().anyMatch(p -> "privilege.design.bpm".equals(p.getName())));
         registration = context.registerService(App.class, app, null);
     }
 
@@ -54,5 +59,18 @@ public class BpmAppServiceImpl implements BpmAppService {
     @Reference(target = "(com.elster.jupiter.license.application.key=" + APPLICATION_KEY + ")")
     public void setLicense(License license) {
         this.license = license;
+    }
+
+    @Override
+    public List<String> getApplicationPrivileges() {
+        return Arrays.asList(
+                Privileges.VIEW_BPM,
+                Privileges.DESIGN_BPM
+        );
+    }
+
+    @Override
+    public String getApplicationName() {
+        return APP_KEY;
     }
 }
