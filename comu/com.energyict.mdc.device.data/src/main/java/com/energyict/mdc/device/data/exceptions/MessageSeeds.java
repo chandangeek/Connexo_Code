c0c9703610c3f8f8fd9718762a304ecc663f999b
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-08-14 15:24:17 +0200

    Fixed MessageSeeds

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/exceptions/MessageSeeds.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/exceptions/MessageSeeds.java
@@ -109,7 +109,7 @@ public enum MessageSeeds implements MessageSeed, TranslationKey {
     USER_IS_NOT_ALLOWED_TO_EDIT_SECURITY_PROPERTIES(2075, Keys.USER_IS_NOT_ALLOWED_TO_EDIT_SECURITY_PROPERTIES, "You are not allowed to edit the security properties", Level.SEVERE),
     NO_SUCH_COM_SCHEDULE(2076, "NoSuchComSchedule" , "No communication schedule with id {0}", Level.SEVERE),
     NO_SUCH_DEVICE(2077, "NoSuchDevice" , "No device with mRID {0}", Level.SEVERE),
-    NO_METER_ACTIVATION_AT(2076, Keys.NO_METER_ACTIVATION_AT, "There is no meter activation at {0}", Level.SEVERE),    ;
+    NO_METER_ACTIVATION_AT(2078, Keys.NO_METER_ACTIVATION_AT, "There is no meter activation at {0}", Level.SEVERE),    ;
 
     private final int number;
     private final String key;
