b364b9fc5311d1011cace2cb5fd22d8fea6914ca
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-08-14 15:13:21 +0200

    Fixed MessageSeeds

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/MessageSeeds.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/MessageSeeds.java
@@ -208,8 +208,9 @@ public enum MessageSeeds implements MessageSeed, TranslationKey {
     CIM_DATE_SHOULD_BE_AFTER_X(238, "CIMDateShouldBeAfterX" , "This date should be greater than (or equal) ''{0}''"),
     NO_APPSERVER(239, "NoAppServer", "There is no active application server that can handle this request"),
     NO_SUCH_MESSAGE_QUEUE(240, "NoSuchMessageQueue", "Unable to queue command: no message queue was found"),
-    BAD_ACTION(241, "BadAction", "Expected action to be either 'add' or 'remove'"), NO_SUCH_COM_SCHEDULE(242, "NoSuchSchedule", "No communication schedule with id {0}"),
-    CANT_ADD_READINGS_FOR_STATE(242, "cantAddReadingForState", "The state of the device at {0} does not allow adding readings."),
+    BAD_ACTION(241, "BadAction", "Expected action to be either 'add' or 'remove'"),
+    NO_SUCH_COM_SCHEDULE(242, "NoSuchSchedule", "No communication schedule with id {0}"),
+    CANT_ADD_READINGS_FOR_STATE(243, "cantAddReadingForState", "The state of the device at {0} does not allow adding readings."),
     ;
     private final int number;
     private final String key;
