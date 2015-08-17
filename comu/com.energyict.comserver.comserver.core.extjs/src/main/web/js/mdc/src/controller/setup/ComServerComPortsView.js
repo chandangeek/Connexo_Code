37c40e9a194e466347f7cbe07cdc6a8a7def1e7f
dvy <dieter.vyncke@elster.com>
2015-08-17 13:48:50 +0200

    COMU-1708 - Spelling and other mistakes in error message when activating inbound CP withoud CPP

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/ComServerComPortsView.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/ComServerComPortsView.js
@@ -234,12 +234,12 @@ Ext.define('Mdc.controller.setup.ComServerComPortsView', {
                             me.getApplication().fireEvent('acknowledge', Uni.I18n.translate('comPortOnComServer.changeState.msg', 'MDC', 'Communication port ' + ' ' + msg));
                         },
                         failure: function (response) {
-                            var title = Uni.I18n.translate('comServerComPorts.activation.failure', 'MDC', 'Failed to activate') + " " + record.get('name'),
+                            var title = Uni.I18n.translate('comServerComPorts.activation.failure', 'MDC', 'Failed to activate') + " '" + record.get('name') + "'",
                                 errorsArray = Ext.JSON.decode(response.responseText).errors,
                                 message = '';
 
                             Ext.Array.each(errorsArray, function (obj) {
-                                message += obj.msg + '.</br>'
+                                message += obj.msg + '.'
                             });
 
                             me.getApplication().getController('Uni.controller.Error').showError(title, message);
