50aae6d7f51869c0ce0999f2359dd326bca6cc3a
Artem Puchenkin <apuchenkin@gmail.com>
2015-08-20 15:28:58 +0300

    [COMU-1776] Visualization of data is too slow

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/plugin/ShowConditionalToolTip.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/plugin/ShowConditionalToolTip.js
@@ -24,29 +24,47 @@ Ext.define('Uni.grid.plugin.ShowConditionalToolTip', {
      */
     setTooltip: function (grid) {
         var gridPanel = grid.up('gridpanel');
-        Ext.Array.each(gridPanel.columns, function (column) {
-            var header = Ext.get(gridPanel.getEl().query('#' + column.id + '-titleEl')[0]);
+        if (gridPanel.rendered && !grid.isHidden()) {
+            Ext.suspendLayouts();
+            Ext.Array.each(gridPanel.columns, function (column) {
+                if (!column.isHidden()) {
+                    var header = column.getEl().down('.' + Ext.baseCSSPrefix + 'column-header-inner');
 
-            if (column.text && (header.getWidth(true) < header.getTextWidth())) {
-                header.set({'data-qtip': column.text});
-            } else {
-                header.set({'data-qtip': undefined});
-            }
-
-            if (column.$className === 'Ext.grid.column.Column' || column.$className === 'Ext.grid.column.Date' || column.$className === 'Ext.grid.column.Template'|| column.$className === 'Uni.grid.column.Duration') {
-                Ext.Array.each(grid.getEl().query('.x-grid-cell-headerId-' + (column.itemId || column.id)), function (item) {
-                    var cell = Ext.get(item),
-                        inner = cell.down('.x-grid-cell-inner'),
-                        text = inner ? Ext.util.Format.stripTags(inner.getHTML()) : false,
-                        tooltip = cell.getAttribute('data-qtip');
-
-                    if (text && (cell.getWidth(true) < cell.getTextWidth())) {
-                        cell.set({'data-qtip': tooltip || text});
+                    if (column.text && (header.getWidth(true) < header.getTextWidth())) {
+                        header.set({'data-qtip': column.text});
                     } else {
-                        cell.set({'data-qtip': (tooltip !== text ? tooltip : null) || undefined});
+                        header.set({'data-qtip': undefined});
                     }
-                });
-            }
-        });
+
+                    if (   column.$className === 'Ext.grid.column.Column'
+                        || column.$className === 'Ext.grid.column.Date'
+                        || column.$className === 'Ext.grid.column.Template'
+                        || column.$className === 'Uni.grid.column.Duration') {
+
+                        var first = grid.getEl().down(grid.getCellInnerSelector(column));
+                        var width = first && first.getWidth(true);
+                        if (width) {
+                            Ext.each(grid.getEl().query(grid.getCellSelector(column)), function (el) {
+                                var cell = Ext.get(el),
+                                    inner = cell.down('.' + Ext.baseCSSPrefix + 'grid-cell-inner');
+
+                                if (inner) {
+                                    var text = Ext.util.Format.stripTags(inner.getHTML()),
+                                        tooltip = cell.getAttribute('data-qtip');
+
+                                    if (text && (width < cell.getTextWidth())) {
+                                        cell.set({'data-qtip': tooltip || text});
+                                    } else {
+                                        cell.set({'data-qtip': (tooltip !== text ? tooltip : null) || undefined});
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+
+            Ext.resumeLayouts(true);
+        }
     }
 });
