f4a85c667e64f6f108837c126c3e083a98c27d31
Geert <geert.delmeiren@elster.com>
2015-08-21 11:05:16 +0200

    COMU-411 - The name of a device configuration is incorrect in a message

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/exceptions/MessageSeeds.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/exceptions/MessageSeeds.java
@@ -64,7 +64,7 @@ public enum MessageSeeds implements MessageSeed, TranslationKey {
     LOAD_PROFILE_SPEC_CANNOT_ADD_TO_ACTIVE_CONFIGURATION(8003, "loadProfileSpec.active.configuration", "You can not add a load profile configuration to an active device configuration", SEVERE),
     LOAD_PROFILE_SPEC_CANNOT_CHANGE_DEVICE_CONFIG(8004, "loadProfileSpec.change.configuration", "You can not change the device configuration of an existing load profile configuration", SEVERE),
     LOAD_PROFILE_SPEC_CANNOT_CHANGE_LOAD_PROFILE_TYPE(8005, "loadProfileSpec.change.loadProfileType", "You can not change the load profile type of an existing load profile configuration", SEVERE),
-    LOAD_PROFILE_SPEC_CANNOT_DELETE_FROM_ACTIVE_CONFIG(8006, "loadProfileSpec.cannot.delete.active.config", "You can not remove a load profile configuration \"{0}\" from an active device configuration \"{1}\"", SEVERE),
+    LOAD_PROFILE_SPEC_CANNOT_DELETE_FROM_ACTIVE_CONFIG(8006, "loadProfileSpec.cannot.delete.active.config", "It is not allowed to remove a load profile configuration from an active device configuration", SEVERE),
     LOAD_PROFILE_SPEC_CANNOT_DELETE_STILL_LINKED_CHANNEL_SPECS(8007, "loadProfileSpec.cannot.delete.linked.channel.specs", "Cannot remove load profile configuration because there are still channel specs linked", SEVERE),
     LOAD_PROFILE_SPEC_LOAD_PROFILE_TYPE_IS_REQUIRED(8008, Keys.LOAD_PROFILE_SPEC_LOAD_PROFILE_TYPE_IS_REQUIRED, "The load profile type of a load profile configuration is required", SEVERE),
     LOGBOOK_SPEC_DEVICE_CONFIG_IS_REQUIRED(9001, "logBookSpec.deviceConfig.required", "The device configuration of a logbook configuration is required", SEVERE),
