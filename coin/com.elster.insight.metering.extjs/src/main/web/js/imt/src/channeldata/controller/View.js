c58ed5370f9b69b91a4fa621fb66e79ab65823bc
Kurt Kikendall <kurt.kikendall@elster.com>
2015-08-25 10:59:00 -0400

    COIN-233 Work on screen navigation further.  Had problem where breadcrumb failed to update with MRID of UP when going to saved URL which pointed to channels list page.

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/channeldata/controller/View.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/channeldata/controller/View.js
@@ -4,6 +4,9 @@ Ext.define('Imt.channeldata.controller.View', {
         'Imt.channeldata.store.Channel',
         'Imt.channeldata.view.Setup'
     ],
+    models: [
+        'Imt.usagepointmanagement.model.UsagePoint'
+    ],
     stores: [
         'Imt.channeldata.store.Channel'
     ],
@@ -18,11 +21,18 @@ Ext.define('Imt.channeldata.controller.View', {
 
     showUsagePointChannels: function (mRID) {
         var me = this,
-            router = this.getController('Uni.controller.history.Router'),
+            router = me.getController('Uni.controller.history.Router'),
+            // TODO: Why does me.getModel() NOT work here?
+            usagePoint = Ext.create('Imt.usagepointmanagement.model.UsagePoint'),
             pageMainContent = Ext.ComponentQuery.query('viewport > #contentPanel')[0];
        
         pageMainContent.setLoading(true);
         var widget = Ext.widget('channel-list-setup', {router: router, mRID: mRID});
+        // TODO: Should we be loading a full Usage Point model from the back-end just so that
+        // the event can contain the mRID which is used by History.js to change the link
+        // name in the breadcrumb?  For now, just create empty model and set this one field.
+        usagePoint.set('mRID', mRID);
+        me.getApplication().fireEvent('usagePointLoaded', usagePoint);
         me.getApplication().fireEvent('changecontentevent', widget);
         me.getOverviewLink().setText(mRID);
         pageMainContent.setLoading(false);
