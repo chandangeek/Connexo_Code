1892d131c33193803b4144eb1931a84f1cdf808d
Geert <geert.delmeiren@elster.com>
2015-08-25 15:48:02 +0200

    COMU-1905 - View readings of empty register results in NullPointerException

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/deviceregisterdata/text/Setup.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/deviceregisterdata/text/Setup.js
@@ -12,32 +12,40 @@ Ext.define('Mdc.view.setup.deviceregisterdata.text.Setup', {
                 ui: 'large',
                 items: [
                     {
-                        xtype: 'preview-container',
-                        grid: {
-                            xtype: 'deviceregisterreportgrid-text',
-                            mRID: me.mRID,
-                            registerId: me.registerId
-                        },
-                        emptyComponent: {
-                            xtype: 'no-items-found-panel',
-                            itemId: 'ctr-no-register-data',
-                            title: Uni.I18n.translate('device.registerData.noData', 'MDC', 'No readings found'),
-                            reasons: [
-                                Uni.I18n.translate('device.registerData.list.item1', 'MDC', 'No readings have been defined yet.'),
-                                Uni.I18n.translate('device.registerData.list.item2', 'MDC', 'No readings comply to the filter.')
-                            ],
-                            stepItems: [
-                                {
-                                    text:  Uni.I18n.translate('general.addReading','MDC','Add reading'),
-                                    privileges: Mdc.privileges.Device.administrateDeviceData,
-                                    href: '#/devices/' + encodeURIComponent(me.mRID) + '/registers/' + me.registerId + '/data/add',
-                                    dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.deviceDataEditActions
+                        xtype: 'mdc-registers-topfilter',
+                        itemId: 'deviceregistersdatafilterpanel'
+                    },
+                    {
+                        items: [
+                            {
+                                xtype: 'preview-container',
+                                grid: {
+                                    xtype: 'deviceregisterreportgrid-text',
+                                    mRID: me.mRID,
+                                    registerId: me.registerId
+                                },
+                                emptyComponent: {
+                                    xtype: 'no-items-found-panel',
+                                    itemId: 'ctr-no-register-data',
+                                    title: Uni.I18n.translate('device.registerData.noData', 'MDC', 'No readings found'),
+                                    reasons: [
+                                        Uni.I18n.translate('device.registerData.list.item1', 'MDC', 'No readings have been defined yet.'),
+                                        Uni.I18n.translate('device.registerData.list.item2', 'MDC', 'No readings comply to the filter.')
+                                    ],
+                                    stepItems: [
+                                        {
+                                            text: Uni.I18n.translate('general.addReading', 'MDC', 'Add reading'),
+                                            privileges: Mdc.privileges.Device.administrateDeviceData,
+                                            href: '#/devices/' + encodeURIComponent(me.mRID) + '/registers/' + me.registerId + '/data/add',
+                                            dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.deviceDataEditActions
+                                        }
+                                    ]
+                                },
+                                previewComponent: {
+                                    xtype: 'deviceregisterreportpreview-text'
                                 }
-                            ]
-                        },
-                        previewComponent: {
-                            xtype: 'deviceregisterreportpreview-text'
-                        }
+                            }
+                        ]
                     }
                 ]
             }
