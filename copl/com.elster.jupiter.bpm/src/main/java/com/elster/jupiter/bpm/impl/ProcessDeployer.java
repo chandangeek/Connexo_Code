93900f26d97f3d4c7fc15d40a4b0c844916b29bf
Dragos Iancu <(dragos.iancu@elster.com)>
2015-08-27 10:00:56 +0300

    COPL-444

--- copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/ProcessDeployer.java
+++ copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/ProcessDeployer.java
@@ -1,6 +1,7 @@
 package com.elster.jupiter.bpm.impl;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
@@ -9,6 +10,10 @@ import java.util.Base64;
  * Main class to deploy the predefined processes during Connexo install
  */
 public class ProcessDeployer {
+
+    private static final String defaultRepoPayload="{\"name\":\"connexoRepo\",\"description\":\"repository for the new Connexo project\",\"userName\":null,\"password\":null,\"requestType\":\"new\",\"gitURL\":null}";
+    private static final String defaultOrgUnitPayload="{\"name\":\"connexoGroup\",\"description\":\"default Connexo organizational unit\",\"owner\":\"rootUsr\",\"repositories\":[\"connexoRepo\"]}";
+
     public static void main(String args[]) {
         if (args.length < 3) {
             System.out.println("Incorrect syntax. The following parameters are required:");
@@ -19,31 +24,51 @@ public class ProcessDeployer {
         }
 
         String authString = "Basic " + new String(Base64.getEncoder().encode((args[1] + ":" + args[2]).getBytes()));
-
+        createRepository(args[0], authString, defaultRepoPayload);
+        createOrganizationalUnit(args[0], authString,defaultOrgUnitPayload);
         deployProcess("org.jbpm:notifyuser:1.0:defaultKieBase:defaultKieSession", args[0], authString);
         deployProcess("org.jbpm:sendsomeone:1.0:defaultKieBase:defaultKieSession", args[0], authString);
     }
 
-    private static void deployProcess(String deploymentId, String arg, String authString) {
-        String url = arg + "/rest/deployment/" + deploymentId + "/deploy?strategy=SINGLETON";
-        doPost(url, authString);
+    private static void createRepository(String arg, String authString, String payload){
+       String url = arg + "/rest/repositories/";
+        doPost(url, authString, payload);
     }
 
-    private static void doPost(String url, String authString) {
-        HttpURLConnection httpConnection = null;
-        try {
+    private static void createOrganizationalUnit(String arg, String authString, String payload){
+        String url = arg + "/rest/organizationalunits/";
+        doPost(url, authString, payload);
+    }
 
-            URL targetUrl = new URL(url);
-            httpConnection = (HttpURLConnection) targetUrl.openConnection();
+    private static void deployProcess(String deploymentId, String arg, String authString) {
+        String url = arg + "/rest/deployment/" + deploymentId + "/deploy?strategy=SINGLETON";
+        doPost(url, authString, null);
+    }
+
+    private static void doPost(String url, String authString, String payload) {
+        HttpURLConnection httpConnection = null;
+        try{
+
+        URL targetUrl = new URL(url);
+        httpConnection = (HttpURLConnection)targetUrl.openConnection();
             httpConnection.setDoOutput(true);
             httpConnection.setRequestMethod("POST");
             httpConnection.setRequestProperty("Authorization", authString);
-            httpConnection.setRequestProperty("Content-Type", "text/plain;charset=UTF-8");
+
+            if(payload!=null && payload.length()!= 0) {
+                httpConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                OutputStreamWriter outWriter = new OutputStreamWriter(httpConnection.getOutputStream(),"UTF-8");
+                outWriter.write(payload);
+                outWriter.close();
+            }else{
+                httpConnection.setRequestProperty("Content-Type", "text/plain;charset=UTF-8");
+                }
 
             if (httpConnection.getResponseCode() != 202) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + httpConnection.getResponseCode());
-            }
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + httpConnection.getResponseCode());
+                }
+
         } catch (IOException e) {
             throw new RuntimeException(e.getStackTrace().toString());
         } finally {
