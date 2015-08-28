21a77bba146da6ae1f100e7c1fef06c4d3ca2843
dvy <dieter.vyncke@elster.com>
2015-08-28 10:56:14 +0200

    I18n properties files sorting

--- coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/src/store/DaysWeeksMonths.js
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/src/store/DaysWeeksMonths.js
@@ -3,9 +3,9 @@ Ext.define('Cfg.store.DaysWeeksMonths', {
     model: 'Cfg.model.DayWeekMonth',
 
     data: [
-        {name: 'days', displayValue: 'day(s)'},
-        {name: 'weeks', displayValue: 'week(s)'},
-        {name: 'months', displayValue: 'month(s)'},
-        {name: 'years', displayValue: 'year(s)'}
+        {name: 'days', displayValue: Uni.I18n.translate('period.days','CFG','day(s)')},
+        {name: 'weeks', displayValue: Uni.I18n.translate('period.weeks','CFG','week(s)')},
+        {name: 'months', displayValue: Uni.I18n.translate('period.months','CFG','month(s)')},
+        {name: 'years', displayValue: Uni.I18n.translate('perios.years','CFG','year(s)')}
     ]
 });
