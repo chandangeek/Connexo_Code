4ab50c45c80ae30c222a425742375fe4f79f4d14
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-08-31 13:51:41 +0200

    Beautify code

--- comu/com.energyict.mdc.gogo/src/main/java/com/energyict/mdc/gogo/ProtocolGogo.java
+++ comu/com.energyict.mdc.gogo/src/main/java/com/energyict/mdc/gogo/ProtocolGogo.java
@@ -1,10 +1,5 @@
 package com.energyict.mdc.gogo;
 
-import com.elster.jupiter.security.thread.ThreadPrincipalService;
-import com.elster.jupiter.transaction.TransactionService;
-import com.elster.jupiter.transaction.VoidTransaction;
-import com.elster.jupiter.users.User;
-import com.elster.jupiter.users.UserService;
 import com.energyict.mdc.device.config.DeviceConfiguration;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.config.DeviceType;
@@ -12,6 +7,12 @@ import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.DeviceService;
 import com.energyict.mdc.protocol.api.DeviceProtocolPluggableClass;
 import com.energyict.mdc.protocol.pluggable.ProtocolPluggableService;
+
+import com.elster.jupiter.security.thread.ThreadPrincipalService;
+import com.elster.jupiter.transaction.TransactionService;
+import com.elster.jupiter.transaction.VoidTransaction;
+import com.elster.jupiter.users.User;
+import com.elster.jupiter.users.UserService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -79,89 +80,126 @@ public class ProtocolGogo {
 
     public void cleanUpProtocol(String communicationProtocol) {
         Optional<DeviceProtocolPluggableClass> deviceProtocolPluggableClassByName = protocolPluggableService.findDeviceProtocolPluggableClassByName(communicationProtocol.toUpperCase());
-
-        deviceProtocolPluggableClassByName.map(deviceProtocolPluggableClass -> {
-            List<DeviceType> deviceTypesWithDeviceProtocol = deviceConfigurationService.findDeviceTypesWithDeviceProtocol(deviceProtocolPluggableClass);
-            List<DeviceConfiguration> deviceConfigurations = deviceTypesWithDeviceProtocol.stream().flatMap(deviceType -> deviceType.getConfigurations().stream()).collect(Collectors.toList());
-            List<Device> devices = deviceConfigurations.stream().flatMap(deviceConfiguration -> this.deviceService.findDevicesByDeviceConfiguration(deviceConfiguration).stream()).collect(Collectors.toList());
-
-            System.out.println("Are you really really sure you want to delete:");
-            System.out.println("    - " + deviceTypesWithDeviceProtocol.size() + " deviceTypes");
-            System.out.println("    - " + deviceConfigurations.size() + " deviceConfigurations");
-            System.out.println("    - " + devices.size() + " devices");
-            System.out.println("    - the '" + communicationProtocol + "' protocol");
-            System.out.println("??? (Y/n)");
+        if (deviceProtocolPluggableClassByName.isPresent()) {
+            DeviceProtocolPluggableClass deviceProtocolPluggableClass = deviceProtocolPluggableClassByName.get();
+            CleanupTransaction cleanupTransaction = new CleanupTransaction(communicationProtocol, deviceProtocolPluggableClass);
             try {
-                String response = readInput();
-                boolean proceed = analyseResponse(response);
+                boolean proceed = cleanupTransaction.getConfirmationFromUser();
                 if (proceed) {
-                    transactionService.execute(new VoidTransaction() {
-                                                   @Override
-                                                   protected void doPerform() {
-                                                       boolean success = setupThreadPrinciple();
-                                                       if(success){
-                                                           System.out.println("Here we go ... (this can take a while)");
-                                                           System.out.print("Deleting devices ");
-                                                           devices.stream().forEach(device -> {
-                                                               device.delete();
-                                                               System.out.print(".");
-                                                           });
-                                                           System.out.println(" Done!");
-                                                           System.out.print("Deleting deviceconfigurations ");
-                                                           deviceConfigurations.stream().forEach(deviceConfiguration -> {
-                                                               deviceConfiguration.deactivate();
-                                                               System.out.print(".");
-                                                           });
-                                                           System.out.println(" Done!");
-                                                           System.out.print("Deleting devicetypes ");
-                                                           deviceTypesWithDeviceProtocol.stream().forEach(deviceType -> {
-                                                               deviceType.delete();
-                                                               System.out.print(".");
-                                                           });
-                                                           System.out.println(" Done!");
-                                                           System.out.print("Deleting the protocol ... ");
-                                                           deviceProtocolPluggableClass.delete();
-                                                           System.out.println("Completely finished");
-                                                           System.out.println("Please note that at the first restart your protocol will be auto-loaded again!");
-                                                       } else {
-                                                           System.out.println("Could not set a proper user, action will not be executed, nothing will be deleted!");
-                                                       }
-                                                   }
-                                               }
-                    );
-
-                } else {
+                    this.transactionService.execute(cleanupTransaction);
+                }
+                else {
                     System.out.println("I'm glad you changed your mind, will not delete anything ...");
                 }
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
-            return true;
-        }).
-                orElseGet(() -> {
-                    System.out.println("Could not find a protocol pluggable class with the name " + communicationProtocol);
-                    return true;
-                });
-    }
-
-    private boolean setupThreadPrinciple() {
-        Optional<User> admin = this.userService.findUser("admin");
-        if(admin.isPresent()){
-            this.threadPrincipalService.set(admin.get());
-            return true;
-        } else {
-            System.out.println("Not possible to set the user, action will fail");
-            return false;
+        }
+        else {
+            System.out.println("Could not find a protocol pluggable class with the name " + communicationProtocol);
         }
     }
 
-    private boolean analyseResponse(String response) {
-        String[] ok = {"", "y", "yes"};
-        return Stream.of(ok).filter(option -> option.equalsIgnoreCase(response)).findFirst().isPresent();
+    private class CleanupTransaction extends VoidTransaction {
+        private final String communicationProtocol;
+        private final DeviceProtocolPluggableClass deviceProtocolPluggableClass;
+        private final List<DeviceType> deviceTypesWithDeviceProtocol;
+        private final List<DeviceConfiguration> deviceConfigurations;
+        private final List<Device> devices;
+
+        private CleanupTransaction(String communicationProtocol, DeviceProtocolPluggableClass deviceProtocolPluggableClass) {
+            super();
+            this.communicationProtocol = communicationProtocol;
+            this.deviceProtocolPluggableClass = deviceProtocolPluggableClass;
+            this.deviceTypesWithDeviceProtocol = deviceConfigurationService.findDeviceTypesWithDeviceProtocol(deviceProtocolPluggableClass);
+            this.deviceConfigurations =
+                    this.deviceTypesWithDeviceProtocol
+                            .stream()
+                            .flatMap(deviceType -> deviceType.getConfigurations().stream())
+                            .collect(Collectors.toList());
+            this.devices =
+                    this.deviceConfigurations
+                            .stream()
+                            .flatMap(this::findDevicesByConfiguration)
+                            .collect(Collectors.toList());
+        }
+
+        private Stream<Device> findDevicesByConfiguration(DeviceConfiguration deviceConfiguration) {
+            return deviceService.findDevicesByDeviceConfiguration(deviceConfiguration).stream();
+        }
+
+        boolean getConfirmationFromUser() throws IOException {
+            System.out.println("Are you really really sure you want to delete:");
+            System.out.println("    - " + this.deviceTypesWithDeviceProtocol.size() + " deviceTypes");
+            System.out.println("    - " + this.deviceConfigurations.size() + " deviceConfigurations");
+            System.out.println("    - " + this.devices.size() + " devices");
+            System.out.println("    - the '" + this.communicationProtocol + "' protocol");
+            System.out.println("??? (Y/n)");
+            return this.analyseResponse(this.readInput());
+        }
+
+        private String readInput() throws IOException {
+            return new BufferedReader(new InputStreamReader(System.in)).readLine();
+        }
+
+        private boolean analyseResponse(String response) {
+            String[] ok = {"", "y", "yes"};
+            return Stream.of(ok).filter(option -> option.equalsIgnoreCase(response)).findFirst().isPresent();
+        }
+
+        @Override
+        protected void doPerform() {
+            boolean success = this.setupThreadPrinciple();
+            if (success) {
+                System.out.println("Here we go ... (this can take a while)");
+                System.out.print("Deleting devices");
+                this.devices.stream().forEach(this::delete);
+                this.logDone();
+                System.out.print("Deactivating device configurations");
+                this.deviceConfigurations.stream().forEach(this::deactivate);
+                this.logDone();
+                System.out.print("Deleting device types");
+                this.deviceTypesWithDeviceProtocol.stream().forEach(this::delete);
+                this.logDone();
+                System.out.print("Deleting the protocol...");
+                this.deviceProtocolPluggableClass.delete();
+                System.out.println("Completely finished");
+                System.out.println("Please note that at the first restart your protocol will be recreated by the 'out of the box' activation process for protocol classes!");
+            } else {
+                System.out.println("Could not set a proper user, action will not be executed, nothing will be deleted!");
+            }
+        }
+
+        private boolean setupThreadPrinciple() {
+            Optional<User> admin = userService.findUser("admin");
+            if (admin.isPresent()) {
+                threadPrincipalService.set(admin.get());
+                return true;
+            } else {
+                System.out.println("Not possible to set the user, action will fail");
+                return false;
+            }
+        }
+
+        private void logDone() {
+            System.out.println(" Done!");
+        }
+
+        private void delete(Device device) {
+            device.delete();
+            System.out.print(".");
+        }
+
+        private void deactivate(DeviceConfiguration configuration) {
+            configuration.deactivate();
+            System.out.print(".");
+        }
+
+        private void delete(DeviceType deviceType) {
+            deviceType.delete();
+            System.out.print(".");
+        }
     }
 
-    private String readInput() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        return br.readLine();
-    }
 }
