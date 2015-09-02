4c6275a1e83dd1fabe1aca942779b4701a2374ad
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-02 10:11:13 +0200

    Cleanup

--- comu/com.energyict.mdc.device.data.validation.rest/src/main/java/com/energyict/mdc/device/data/validation/rest/impl/ValidationResultsResource.java
+++ comu/com.energyict.mdc.device.data.validation.rest/src/main/java/com/energyict/mdc/device/data/validation/rest/impl/ValidationResultsResource.java
@@ -1,14 +1,18 @@
 package com.energyict.mdc.device.data.validation.rest.impl;
 
+import com.energyict.mdc.device.data.validation.DeviceDataValidationService;
+
 import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.elster.jupiter.rest.util.PagedInfoList;
 import com.elster.jupiter.validation.security.Privileges;
-import com.energyict.mdc.device.data.validation.DeviceDataValidationService;
-import com.energyict.mdc.device.data.validation.ValidationOverview;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -30,13 +34,13 @@ public class ValidationResultsResource {
     @Path("/devicegroups/{id}")
     @RolesAllowed({Privileges.VIEW_VALIDATION_CONFIGURATION, Privileges.VALIDATE_MANUAL, Privileges.ADMINISTRATE_VALIDATION_CONFIGURATION, Privileges.FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE, Privileges.FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE_CONFIGURATION})
     public PagedInfoList getValidationResultsPerDeviceGroup(@Context UriInfo uriInf, @BeanParam JsonQueryParameters queryParameters, @PathParam("id") Long groupId) {
-
-        List<ValidationOverview> list = deviceDataValidationService.getValidationResultsOfDeviceGroup(groupId, queryParameters.getStart(), queryParameters.getLimit());
-
-        List<ValidationSummaryInfo> data = list.stream().map(each -> new ValidationSummaryInfo(each)).collect(Collectors.toList());
+        List<ValidationSummaryInfo> data =
+                deviceDataValidationService
+                        .getValidationResultsOfDeviceGroup(groupId, queryParameters.getStart(), queryParameters.getLimit())
+                        .stream()
+                        .map(ValidationSummaryInfo::new)
+                        .collect(Collectors.toList());
         return PagedInfoList.fromPagedList("summary", data, queryParameters);
-
     }
-
 
 }
