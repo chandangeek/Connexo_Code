43d63c27f25c3cf7ae7017f76d5d07dbd71ac5ac
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-02 10:11:38 +0200

    Cleanup

--- comu/com.energyict.mdc.metering/src/main/java/com/energyict/mdc/metering/impl/MdcReadingTypeUtilServiceImpl.java
+++ comu/com.energyict.mdc.metering/src/main/java/com/energyict/mdc/metering/impl/MdcReadingTypeUtilServiceImpl.java
@@ -1,18 +1,16 @@
 package com.energyict.mdc.metering.impl;
 
+import com.energyict.mdc.common.ObisCode;
+import com.energyict.mdc.common.Unit;
+import com.energyict.mdc.metering.MdcReadingTypeUtilService;
+import com.energyict.mdc.metering.ReadingTypeInformation;
+
 import com.elster.jupiter.cbo.MacroPeriod;
 import com.elster.jupiter.cbo.ReadingTypeCodeBuilder;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.time.TimeDuration;
-import com.energyict.mdc.common.ObisCode;
-import com.energyict.mdc.common.Unit;
-import com.energyict.mdc.metering.MdcReadingTypeUtilService;
-import com.energyict.mdc.metering.ReadingTypeInformation;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.inject.Inject;
@@ -20,6 +18,8 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 /**
+ * Provides an implementation for the {@link MdcReadingTypeUtilService} interface.
+ *
  * Copyrights EnergyICT
  * Date: 17/02/14
  * Time: 13:58
@@ -40,17 +40,11 @@ public class MdcReadingTypeUtilServiceImpl implements MdcReadingTypeUtilService 
 
     @Inject
     public MdcReadingTypeUtilServiceImpl(MeteringService meteringService) {
-        this.meteringService = meteringService;
+        this();
+        this.setMeteringService(meteringService);
     }
 
-    @Activate
-    public void activate(BundleContext context) {
-    }
-
-    @Deactivate
-    public void deactivate() {
-    }
-
+    @SuppressWarnings("unused")
     public void readingTypeHelp(){
         logger.info("- Use getReadingTypeInformation(readingType) to print the ObisCode/Unit and interval of a given 18 digit ReadingType.");
         logger.info("- The getReadingTypeFrom takes two or three arguments:");
@@ -58,6 +52,7 @@ public class MdcReadingTypeUtilServiceImpl implements MdcReadingTypeUtilService 
         logger.info("\t- Obiscode, Unit and Interval in seconds.");
     }
 
+    @SuppressWarnings("unused")
     public void getReadingTypeInformation(String readingType){
         ReadingTypeInformation readingTypeInformation = getReadingTypeInformationFor(readingType);
         logger.info("ObisCode : " + readingTypeInformation.getObisCode());
@@ -67,7 +62,7 @@ public class MdcReadingTypeUtilServiceImpl implements MdcReadingTypeUtilService 
 
     public void getReadingTypeFrom(String... arguments){
         try {
-            if(arguments.length == 2){
+            if (arguments.length == 2) {
                 logger.info("ReadingType : " + getReadingTypeMridFrom(ObisCode.fromString(arguments[0]), Unit.get(arguments[1])));
             } else if (arguments.length == 3){
                 logger.info("ReadingType : " + getReadingTypeFrom(ObisCode.fromString(arguments[0]), Unit.get(arguments[1]), new TimeDuration(Integer.valueOf(arguments[2]))));
@@ -75,7 +70,7 @@ public class MdcReadingTypeUtilServiceImpl implements MdcReadingTypeUtilService 
                 logger.info("Sorry, you provided an incorrect amount of arguments.");
             }
         } catch (Exception e) {
-            System.err.println(e);
+            e.printStackTrace(System.err);
         }
     }
 
@@ -119,9 +114,9 @@ public class MdcReadingTypeUtilServiceImpl implements MdcReadingTypeUtilService 
     private ReadingTypeCodeBuilder getReadingTypeCodeBuilderWithInterval(ReadingType readingType, Optional<TimeDuration> interval, ObisCode registerObisCode) {
         ReadingTypeCodeBuilder readingTypeCodeBuilder = copyReadingTypeFields(readingType);
 
-        if(interval.isPresent() && interval.get().equals(TimeDuration.days(1))){
+        if (interval.isPresent() && interval.get().equals(TimeDuration.days(1))) {
             readingTypeCodeBuilder.period(MacroPeriod.DAILY);
-        } else if(interval.isPresent() && interval.get().equals(TimeDuration.months(1))){
+        } else if (interval.isPresent() && interval.get().equals(TimeDuration.months(1))) {
             readingTypeCodeBuilder.period(MacroPeriod.MONTHLY);
         } else {
             readingTypeCodeBuilder.period(MeasuringPeriodMapping.getMeasuringPeriodFor(registerObisCode, interval.orElse(null)));
