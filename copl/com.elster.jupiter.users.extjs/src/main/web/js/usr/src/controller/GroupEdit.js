41600a218c33a950b9720088b55ca4f38c418f1e
dvy <dieter.vyncke@elster.com>
2015-09-03 14:10:38 +0200

    translation fixes - parameterization

--- copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/src/controller/GroupEdit.js
+++ copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/src/controller/GroupEdit.js
@@ -87,7 +87,7 @@ Ext.define('Usr.controller.GroupEdit', {
         me.mode = 'edit';
         Ext.ModelManager.getModel('Usr.model.Group').load(groupId, {
             success: function (group) {
-                me.showOverview(group, Uni.I18n.translate('general.edit', 'USR', 'Edit') + ' \'' + group.get('name') + '\'');
+                me.showOverview(group, Uni.I18n.translate('general.editx', 'USR', "Edit '{0}'",[group.get('name')]));
             }
         });
     },
