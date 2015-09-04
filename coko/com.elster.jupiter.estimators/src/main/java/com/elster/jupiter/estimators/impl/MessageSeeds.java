0843843f16e1aae74163dc23915994e097dd1d7b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-04 17:57:05 +0200

    COPL-1048: refactor MessageSeeds and TranslationKeys

--- coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/MessageSeeds.java
+++ coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/MessageSeeds.java
@@ -2,20 +2,19 @@ package com.elster.jupiter.estimators.impl;
 
 import com.elster.jupiter.nls.NlsMessageFormat;
 import com.elster.jupiter.nls.Thesaurus;
-import com.elster.jupiter.nls.TranslationKey;
 import com.elster.jupiter.util.exception.MessageSeed;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public enum MessageSeeds implements MessageSeed, TranslationKey {
-    NO_SUCH_ESTIMATOR(1001, "estimator.doesnotexist", "Estimator {0} does not exist.", Level.SEVERE),
-    MISSING_PROPERTY(1002, "property.missing", "Required property with key ''{0}'' was not found.", Level.SEVERE),
-    INVALID_NUMBER_OF_SAMPLES(1003, "property.error.maxNumberOfSamples", "Maximum cannot be smaller than minimum", Level.SEVERE),
-    INVALID_NUMBER_OF_CONSECUTIVE_SUSPECTS(1004, "property.error.invalidMaxNumberOfConsecutiveSuspects", "This value should be positive", Level.SEVERE),
-    INVALID_ADVANCE_READINGTYPE(1005, "property.error.advanceReadingsSettings", "The reading type should be cumulative", Level.SEVERE),
-    INVALID_NUMBER_OF_CONSECUTIVE_SUSPECTS_SHOULD_BE_INTEGER_VALUE(1006, "property.error.maxNumberOfConsecutiveSuspectsShouldBeInteger", "This value should be positive", Level.SEVERE),
-    INVALID_ADVANCE_READINGTYPE_NONE_NOT_ALLOWED(1007, "property.error.advanceReadingsSettings.not.none", "None is not allowed", Level.SEVERE);
+public enum MessageSeeds implements MessageSeed {
+    NO_SUCH_ESTIMATOR(1001, "estimator.doesnotexist", "Estimator {0} does not exist."),
+    MISSING_PROPERTY(1002, "property.missing", "Required property with key ''{0}'' was not found."),
+    INVALID_NUMBER_OF_SAMPLES(1003, "property.error.maxNumberOfSamples", "Maximum cannot be smaller than minimum"),
+    INVALID_NUMBER_OF_CONSECUTIVE_SUSPECTS(1004, "property.error.invalidMaxNumberOfConsecutiveSuspects", "This value should be positive"),
+    INVALID_ADVANCE_READINGTYPE(1005, "property.error.advanceReadingsSettings", "The reading type should be cumulative"),
+    INVALID_NUMBER_OF_CONSECUTIVE_SUSPECTS_SHOULD_BE_INTEGER_VALUE(1006, "property.error.maxNumberOfConsecutiveSuspectsShouldBeInteger", "This value should be positive"),
+    INVALID_ADVANCE_READINGTYPE_NONE_NOT_ALLOWED(1007, "property.error.advanceReadingsSettings.not.none", "None is not allowed");
 
     public static final String COMPONENT_NAME = "ESR";
 
@@ -23,6 +22,10 @@ public enum MessageSeeds implements MessageSeed, TranslationKey {
     private final String key;
     private final String defaultFormat;
     private final Level level;
+
+    MessageSeeds(int number, String key, String defaultFormat) {
+        this(number, key, defaultFormat, Level.SEVERE);
+    }
 
     MessageSeeds(int number, String key, String defaultFormat, Level level) {
         this.number = number;
--- coko/com.elster.jupiter.estimators/src/test/java/com/elster/jupiter/estimators/impl/DefaultEstimatorFactoryTest.java
+++ coko/com.elster.jupiter.estimators/src/test/java/com/elster/jupiter/estimators/impl/DefaultEstimatorFactoryTest.java
@@ -3,13 +3,15 @@ package com.elster.jupiter.estimators.impl;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.properties.PropertySpecService;
+import com.elster.jupiter.time.TimeService;
 import com.elster.jupiter.validation.ValidationService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.*;
+import org.junit.runner.*;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static com.elster.jupiter.devtools.tests.assertions.JupiterAssertions.*;
+import static com.elster.jupiter.devtools.tests.assertions.JupiterAssertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultEstimatorFactoryTest {
@@ -22,16 +24,17 @@ public class DefaultEstimatorFactoryTest {
     private ValidationService validationService;
     @Mock
     private MeteringService meteringService;
+    @Mock
+    private TimeService timeService;
 
     @Test
     public void testCreatePowerGapFill() {
 
-        DefaultEstimatorFactory defaultEstimatorFactory = new DefaultEstimatorFactory(nlsService, propertySpecService, validationService, meteringService);
+        DefaultEstimatorFactory defaultEstimatorFactory = new DefaultEstimatorFactory(nlsService, propertySpecService, validationService, meteringService, timeService);
 
         assertThat(defaultEstimatorFactory.available()).contains(DefaultEstimatorFactory.POWER_GAP_FILL_ESTIMATOR);
 
         assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.POWER_GAP_FILL_ESTIMATOR)).isInstanceOf(PowerGapFill.class);
     }
-
 
 }
