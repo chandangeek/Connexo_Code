8e64e6f65005eeec9581c2fe60aa779436858d0c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-04 17:57:03 +0200

    COPL-1048: refactor MessageSeeds and TranslationKeys

--- comu/com.energyict.mdc.device.data.validation.rest/src/test/java/com/energyict/mdc/device/data/validation/rest/impl/DeviceDataValidationRestApplicationJerseyTest.java
+++ comu/com.energyict.mdc.device.data.validation.rest/src/test/java/com/energyict/mdc/device/data/validation/rest/impl/DeviceDataValidationRestApplicationJerseyTest.java
@@ -1,13 +1,13 @@
 package com.energyict.mdc.device.data.validation.rest.impl;
 
-import com.elster.jupiter.cbo.MessageSeeds;
-import com.elster.jupiter.devtools.rest.FelixRestApplicationJerseyTest;
-import com.elster.jupiter.util.exception.MessageSeed;
 import com.energyict.mdc.device.data.validation.DeviceDataValidationService;
-import org.junit.Before;
-import org.mockito.Mock;
+
+import com.elster.jupiter.devtools.rest.FelixRestApplicationJerseyTest;
 
 import javax.ws.rs.core.Application;
+
+import org.junit.*;
+import org.mockito.Mock;
 
 public class DeviceDataValidationRestApplicationJerseyTest extends FelixRestApplicationJerseyTest {
 
@@ -16,13 +16,6 @@ public class DeviceDataValidationRestApplicationJerseyTest extends FelixRestAppl
 
     @Before
     public void setup() {
-        //when(taskService.findComTask(anyLong())).thenReturn(Optional.empty());
-        //when(taskService.findComTask(firmwareComTaskId)).thenReturn(Optional.of(firmwareComTask));
-    }
-
-    @Override
-    protected MessageSeed[] getMessageSeeds() {
-        return MessageSeeds.values();
     }
 
     @Override
