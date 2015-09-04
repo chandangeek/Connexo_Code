abe48e7b5f28883ba6f41d2071483ab5a174b12f
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-04 17:57:09 +0200

    COPL-1048: refactor MessageSeeds and TranslationKeys

--- comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/MdcApplication.java
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/MdcApplication.java
@@ -1,13 +1,5 @@
 package com.energyict.mdc.rest.impl;
 
-import com.elster.jupiter.license.License;
-import com.elster.jupiter.nls.Layer;
-import com.elster.jupiter.nls.NlsService;
-import com.elster.jupiter.nls.Thesaurus;
-import com.elster.jupiter.nls.TranslationKey;
-import com.elster.jupiter.nls.TranslationKeyProvider;
-import com.elster.jupiter.rest.util.ConstraintViolationInfo;
-import com.elster.jupiter.transaction.TransactionService;
 import com.energyict.mdc.common.rest.TransactionWrapper;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.engine.config.EngineConfigurationService;
@@ -17,17 +9,27 @@ import com.energyict.mdc.rest.impl.comserver.ComPortPoolResource;
 import com.energyict.mdc.rest.impl.comserver.ComPortResource;
 import com.energyict.mdc.rest.impl.comserver.ComServerComPortResource;
 import com.energyict.mdc.rest.impl.comserver.ComServerResource;
-import com.energyict.mdc.rest.impl.comserver.MessageSeeds;
+import com.energyict.mdc.rest.impl.comserver.TimeDurationUnitTranslationKeys;
+
+import com.elster.jupiter.license.License;
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.TranslationKey;
+import com.elster.jupiter.nls.TranslationKeyProvider;
+import com.elster.jupiter.rest.util.ConstraintViolationInfo;
+import com.elster.jupiter.transaction.TransactionService;
 import com.google.common.collect.ImmutableSet;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.ws.rs.core.Application;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import javax.ws.rs.core.Application;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 @Component(name = "com.energyict.mdc.rest", service = {Application.class, TranslationKeyProvider.class}, immediate = true, property = {"alias=/mdc", "app=MDC", "name=" + MdcApplication.COMPONENT_NAME})
 public class MdcApplication extends Application implements TranslationKeyProvider {
@@ -90,7 +92,7 @@ public class MdcApplication extends Application implements TranslationKeyProvide
 
     @Override
     public List<TranslationKey> getKeys() {
-        return Arrays.asList(MessageSeeds.values());
+        return Arrays.asList(TimeDurationUnitTranslationKeys.values());
     }
 
     @Reference
---
+++ comu/com.energyict.comserver.comserver.core.rest/src/main/java/com/energyict/mdc/rest/impl/comserver/TimeDurationUnitTranslationKeys.java
@@ -0,0 +1,36 @@
+package com.energyict.mdc.rest.impl.comserver;
+
+import com.elster.jupiter.nls.TranslationKey;
+import com.elster.jupiter.time.TimeDuration;
+
+public enum TimeDurationUnitTranslationKeys implements TranslationKey {
+
+    MILLISECONDS(TimeDuration.TimeUnit.MILLISECONDS.getDescription(), "millisecond(s)"),
+    SECONDS(TimeDuration.TimeUnit.SECONDS.getDescription(), "second(s)"),
+    MINUTES(TimeDuration.TimeUnit.MINUTES.getDescription(), "minute(s)"),
+    HOURS(TimeDuration.TimeUnit.HOURS.getDescription(), "hour(s)"),
+    DAYS(TimeDuration.TimeUnit.DAYS.getDescription(), "day(s)"),
+    WEEKS(TimeDuration.TimeUnit.WEEKS.getDescription(), "week(s)"),
+    MONTHS(TimeDuration.TimeUnit.MONTHS.getDescription(), "months(s)"),
+    YEARS(TimeDuration.TimeUnit.YEARS.getDescription(), "year(s)"),
+    ;
+
+    private final String key;
+    private final String format;
+
+    TimeDurationUnitTranslationKeys(String key, String format) {
+        this.key = key;
+        this.format = format;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String getDefaultFormat() {
+        return format;
+    }
+
+}
