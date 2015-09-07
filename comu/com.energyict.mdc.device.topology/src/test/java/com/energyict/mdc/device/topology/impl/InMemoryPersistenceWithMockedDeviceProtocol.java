3ba0eb53a3d96de98f0db907aad65b6225335ede
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-07 16:58:15 +0200

    COPL-1048: Fix unit test issues after merge branch into master

--- comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/InMemoryPersistenceWithMockedDeviceProtocol.java
+++ comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/InMemoryPersistenceWithMockedDeviceProtocol.java
@@ -1,6 +1,5 @@
 package com.energyict.mdc.device.topology.impl;
 
-import com.elster.jupiter.nls.Thesaurus;
 import com.energyict.mdc.common.TypedProperties;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.config.impl.DeviceConfigurationModule;
@@ -62,6 +61,7 @@ import com.elster.jupiter.metering.groups.MeteringGroupsService;
 import com.elster.jupiter.metering.groups.impl.MeteringGroupsModule;
 import com.elster.jupiter.metering.impl.MeteringModule;
 import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.nls.impl.NlsModule;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.OrmService;
@@ -156,8 +156,7 @@ public class InMemoryPersistenceWithMockedDeviceProtocol {
                 new UserModule(),
                 new IdsModule(),
                 new MeteringModule(
-//                        ,"0.0.0.0.0.41.92.0.0.0.0.0.0.0.0.0.114.0"
-                        "0.0.0.1.1.1.12.0.0.0.0.0.0.0.0.0.72.0"
+                         "0.0.0.1.1.1.12.0.0.0.0.0.0.0.0.0.72.0"
                         ,"0.0.0.1.1.1.12.0.0.0.0.0.0.0.0.3.72.0"
                         ,"0.0.0.1.1.7.58.0.0.0.0.0.0.0.0.0.167.0"
                         ,"0.0.0.1.1.7.58.0.0.0.0.0.0.0.0.0.42.0"
@@ -179,6 +178,8 @@ public class InMemoryPersistenceWithMockedDeviceProtocol {
                         ,"0.0.0.9.19.1.12.0.0.0.0.1.0.0.0.3.72.0"
                         ,"0.0.0.9.19.1.12.0.0.0.0.2.0.0.0.0.72.0"
                         ,"0.0.0.9.19.1.12.0.0.0.0.2.0.0.0.3.72.0"
+                        ,"0.0.2.1.1.1.12.0.0.0.0.0.0.0.0.3.72.0"
+                        ,"0.0.2.1.19.1.12.0.0.0.0.0.0.0.0.3.72.0"
                         ,"0.2.0.6.0.7.58.0.0.0.0.0.0.0.0.0.107.0"
                         ,"0.2.0.6.0.7.58.0.0.0.0.0.0.0.0.0.125.0"
                         ,"0.2.0.6.0.7.58.0.0.0.0.0.0.0.0.0.126.0"
