9dc6f381889f0a54a3188e0f90a7d73d5965f6c7
Sentyurin Sergey <ssentyur@mera.ru>
2015-09-09 11:58:24 +0300

    translation fix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/deviceattributes/DeviceAttributesEditForm.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/deviceattributes/DeviceAttributesEditForm.js
@@ -100,7 +100,7 @@ Ext.define('Mdc.view.setup.deviceattributes.DeviceAttributesEditForm', {
                                     if (store.getTotalCount() > store.getCount()) {
                                         el.appendChild({
                                             tag: 'li',
-                                            html: Uni.I18n.translate('issues.limitNotification', 'ISU', 'Keep typing to narrow down'),
+                                            html: Uni.I18n.translate('issues.limitNotification', 'MDC', 'Keep typing to narrow down'),
                                             cls: Ext.baseCSSPrefix + 'boundlist-item combo-limit-notification'
                                         });
                                     }
