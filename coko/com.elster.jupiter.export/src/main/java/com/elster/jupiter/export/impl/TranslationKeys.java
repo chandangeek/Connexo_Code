8a0f88b565be7cc5f0299ea47f61038e97eb9069
igh <isabelle.gheysens@elster.com>
2015-09-09 11:25:22 +0200

    new export categories

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/TranslationKeys.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/TranslationKeys.java
@@ -5,6 +5,8 @@ import com.elster.jupiter.nls.TranslationKey;
 
 public enum TranslationKeys implements TranslationKey {
     RELATIVE_PERIOD_CATEGORY(Installer.RELATIVE_PERIOD_CATEGORY, "Data Export"),
+    RELATIVE_PERIOD_UPDATEWINDOW_CATEGORY(Installer.RELATIVE_PERIOD_UPDATEWINDOW_CATEGORY, "Update window"),
+    RELATIVE_PERIOD_UPDATETIMEFRAME_CATEGORY(Installer.RELATIVE_PERIOD_UPDATETIMEFRAME_CATEGORY, "Update timeframe"),
     STANDARD_DATA_SELECTOR_FACTORY(StandardDataSelectorFactory.class.getName(), DataExportService.STANDARD_DATA_SELECTOR),
     SUBSCRIBER_NAME(Installer.SUBSCRIBER_NAME, DataExportServiceImpl.SUBSCRIBER_DISPLAYNAME),
     ;
