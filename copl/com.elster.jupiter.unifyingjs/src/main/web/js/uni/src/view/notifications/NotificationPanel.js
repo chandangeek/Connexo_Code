1cadaeb76165b9466b2a542828edb218ee41f51a
Zheltukhin Alexey <azhelt@mera.ru>
2015-09-22 16:43:27 +0300

    another build fix

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/notifications/NotificationPanel.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/notifications/NotificationPanel.js
@@ -20,12 +20,12 @@
  *                message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
  *                additionalItems: [
  *                   {
- *                      text: Uni.I18n.translate('general.add', 'MDC', 'Add'),
+ *                      text: Uni.I18n.translate('general.add', 'UNI', 'Add'),
  *                      xtype: 'button',
  *                      ui: 'action'
  *                   },
  *                   {
- *                      text: Uni.I18n.translate('general.cancel', 'MDC', 'Cancel'),
+ *                      text: Uni.I18n.translate('general.cancel', 'UNI', 'Cancel'),
  *                      xtype: 'button',
  *                      ui: 'link'
  *                    }
