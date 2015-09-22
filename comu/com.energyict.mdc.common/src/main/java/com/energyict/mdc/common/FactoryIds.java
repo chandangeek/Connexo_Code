f0aebb15dbd7e28100164182e1dc24f0ed2e8d42
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-22 14:03:32 +0200

    Code review for device search: support state

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/FactoryIds.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/FactoryIds.java
@@ -15,7 +15,7 @@ public enum FactoryIds {
     LOOKUP(Constants.LOOKUP_FACTORY_ID),
     DEVICE_TYPE(Constants.DEVICE_TYPE_FACTORY_ID),
     DEVICE_CONFIGURATION(Constants.DEVICE_CONFIGURATION_FACTORY_ID),
-    DEVICE_STATE(Constants.DEVICE_STATE_FACTORY_ID),
+    FINITE_STATE(Constants.FINITE_STATE_FACTORY_ID),
     REGISTER(Constants.REGISTER_FACTORY_ID),
     RELATION_TYPE(Constants.RELATION_TYPE_FACTORY_ID),
     TIMEZONE_IN_USE(Constants.TIMEZONE_IN_USE_FACTORYID),
@@ -54,7 +54,6 @@ public enum FactoryIds {
         public static final int LOOKUP_FACTORY_ID = 111;
         public static final int DEVICE_TYPE_FACTORY_ID = 122;
         public static final int DEVICE_CONFIGURATION_FACTORY_ID = 202;
-        public static final int DEVICE_STATE_FACTORY_ID = 203;
         public static final int REGISTER_FACTORY_ID = 140;
         public static final int LOADPROFILE_TYPE_FACTORYID = 182;
         public static final int LOADPROFILE_FACTORYID = 185;
@@ -64,5 +63,6 @@ public enum FactoryIds {
         public static final int SECURITY_SET_FACTORYID = 5012;
         public static final int LOGBOOK_FACTORYID = 206;
         public static final int FIRMWARE_FACTORYID = 10000;
+        public static final int FINITE_STATE_FACTORY_ID = 10001;
     }
 }
