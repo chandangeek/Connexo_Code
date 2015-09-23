817b4c1aba2a8ce345ddbc4fe3b674b2a4f77868
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-09-23 08:55:55 +0200

    Fixing issues after merge master into branch + issues introduced after getName and getDisplayName on PropertySpec

--- comu/com.energyict.mdc.issue.datavalidation/src/test/java/com/energyict/mdc/issue/datavalidation/impl/PersistenceIntegrationTest.java
+++ comu/com.energyict.mdc.issue.datavalidation/src/test/java/com/energyict/mdc/issue/datavalidation/impl/PersistenceIntegrationTest.java
@@ -1,18 +1,18 @@
 package com.energyict.mdc.issue.datavalidation.impl;
 
-import java.sql.SQLException;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import com.energyict.mdc.issue.datavalidation.IssueDataValidationService;
 
 import com.elster.jupiter.devtools.persistence.test.rules.TransactionalRule;
+import com.elster.jupiter.fsm.FiniteStateMachineService;
 import com.elster.jupiter.transaction.TransactionContext;
 import com.elster.jupiter.transaction.TransactionService;
-import com.energyict.mdc.issue.datavalidation.IssueDataValidationService;
+
+import java.sql.SQLException;
+
+import org.junit.*;
+import org.junit.rules.*;
+import org.junit.runner.*;
+import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public abstract class PersistenceIntegrationTest {
@@ -28,6 +28,7 @@ public abstract class PersistenceIntegrationTest {
         inMemoryPersistence.initializeDatabase("PersistenceIntegrationTest.mdc.issue.datavalidation", false);
 
         try (TransactionContext ctx = inMemoryPersistence.getTransactionService().getContext()) {
+            inMemoryPersistence.getService(FiniteStateMachineService.class);
             inMemoryPersistence.getService(IssueDataValidationService.class);
             ctx.commit();
         }
