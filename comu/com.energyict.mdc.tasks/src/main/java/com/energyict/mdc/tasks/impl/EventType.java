5d2b5cbb724bfe10f4b3f2a19145a381eac44182
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-09-24 14:49:44 +0200

    apiconsistency : com.elster.jupiter.events

--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/EventType.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/EventType.java
@@ -35,7 +35,7 @@ public enum EventType {
                 .category("Crud")
                 .scope("System")
                 .withProperty("id", ValueType.LONG, "id");
-        this.addCustomProperties(builder).create().save();
+        this.addCustomProperties(builder).create();
     }
 
     @TransactionRequired
