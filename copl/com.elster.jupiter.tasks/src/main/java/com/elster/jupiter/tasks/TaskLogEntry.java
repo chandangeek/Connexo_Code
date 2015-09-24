9f9c647176086ee2d8fdd5a61246b449a0a81a1e
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-09-24 14:54:22 +0200

    apiconsistency : com.elster.jupiter.tasks

--- copl/com.elster.jupiter.tasks/src/main/java/com/elster/jupiter/tasks/TaskLogEntry.java
+++ copl/com.elster.jupiter.tasks/src/main/java/com/elster/jupiter/tasks/TaskLogEntry.java
@@ -2,11 +2,6 @@ package com.elster.jupiter.tasks;
 
 import com.elster.jupiter.util.logging.LogEntry;
 
-/**
- * Copyrights EnergyICT
- * Date: 5/11/2014
- * Time: 10:38
- */
 public interface TaskLogEntry extends LogEntry {
     TaskOccurrence getTaskOccurrence();
 }
