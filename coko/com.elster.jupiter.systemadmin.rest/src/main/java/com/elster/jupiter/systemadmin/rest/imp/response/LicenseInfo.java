b6badf5d8fcc24f4328226fa485a1f802b23dc35
dvy <dieter.vyncke@elster.com>
2015-10-01 13:08:05 +0200

    COMU-2285 - Untranslated strings in detailed multisense license menu

--- coko/com.elster.jupiter.systemadmin.rest/src/main/java/com/elster/jupiter/systemadmin/rest/imp/response/LicenseInfo.java
+++ coko/com.elster.jupiter.systemadmin.rest/src/main/java/com/elster/jupiter/systemadmin/rest/imp/response/LicenseInfo.java
@@ -1,10 +1,13 @@
 package com.elster.jupiter.systemadmin.rest.imp.response;
 
 import com.elster.jupiter.license.License;
+import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.systemadmin.rest.imp.LicensingApplication;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LicenseInfo extends LicenseShortInfo {
 
@@ -12,7 +15,7 @@ public class LicenseInfo extends LicenseShortInfo {
     private int graceperiod;
     private String type;
     private String description;
-    private Set<Map.Entry<Object, Object>> content;
+    private Set<Map.Entry<Object, Object>> content = new HashSet<>();
 
     public LicenseInfo() {
     }
@@ -23,12 +26,20 @@ public class LicenseInfo extends LicenseShortInfo {
         this.description = license.getDescription();
         this.validfrom = license.getActivation().toEpochMilli();
         this.graceperiod = license.getGracePeriodInDays();
-        this.content = license.getLicensedValues().entrySet();
+        Thesaurus thesaurus = nlsService.getThesaurus(LicensingApplication.COMPONENT_NAME, Layer.REST);
+        content = license.getLicensedValues().entrySet()
+                .stream()
+                .map(property -> new AbstractMap.SimpleEntry<Object, Object>(
+                        thesaurus.getString(property.getKey().toString(), property.getKey().toString()),
+                        property.getValue()
+                ))
+                .collect(Collectors.toCollection(HashSet::new));
     }
 
     public Set<Map.Entry<Object, Object>> getContent() {
         return this.content;
     }
+
     public Long getExpires() {
         return expires;
     }
