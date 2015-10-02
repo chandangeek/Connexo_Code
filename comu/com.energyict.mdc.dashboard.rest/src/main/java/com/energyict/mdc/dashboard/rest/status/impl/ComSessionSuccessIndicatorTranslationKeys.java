454facea492d7a7d7d29da7885b9eab4b86aaa79
Maksim Barinov <mbarinov@mera.ru>
2015-10-02 11:27:05 +0300

    COMU-2217

--- comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/ComSessionSuccessIndicatorTranslationKeys.java
+++ comu/com.energyict.mdc.dashboard.rest/src/main/java/com/energyict/mdc/dashboard/rest/status/impl/ComSessionSuccessIndicatorTranslationKeys.java
@@ -29,7 +29,7 @@ public enum ComSessionSuccessIndicatorTranslationKeys implements TranslationKey 
 
     @Override
     public String getKey() {
-        return ComSession.SuccessIndicator.class.getSimpleName() + "." + this.successIndicator.name();
+        return this.successIndicator.name();
     }
 
     @Override
