7589e7a3f69d73f6f8b36ae6e624eed2b8da3cda
Anton Fomchenko <antfom@mera.ru>
2015-10-02 17:06:02 +0300

    COMU-2188 added enum value factory

---
+++ copl/com.elster.jupiter.properties/src/main/java/com/elster/jupiter/properties/EnumFactory.java
@@ -0,0 +1,52 @@
+package com.elster.jupiter.properties;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by antfom on 03.08.2015.
+ */
+public class EnumFactory extends AbstractValueFactory<Enum> {
+    private final Class<? extends Enum> enumClass;
+
+    public EnumFactory(Class<? extends Enum> enumClass) {
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    public Enum fromStringValue(String stringValue) {
+        return Stream.of(enumClass.getEnumConstants())
+                .filter(e -> stringValue.equalsIgnoreCase(e.name()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public String toStringValue(Enum object) {
+        return object.toString();
+    }
+
+    @Override
+    public Class<Enum> getValueType() {
+        return Enum.class;
+    }
+
+    @Override
+    public String getDatabaseTypeName() {
+        return "varchar2(40)";
+    }
+
+    @Override
+    public int getJdbcType() {
+        return java.sql.Types.VARCHAR;
+    }
+
+    @Override
+    public Enum valueFromDatabase(Object object) {
+        return (Enum) object;
+    }
+
+    @Override
+    public Object valueToDatabase(Enum object) {
+        return object;
+    }
+}
