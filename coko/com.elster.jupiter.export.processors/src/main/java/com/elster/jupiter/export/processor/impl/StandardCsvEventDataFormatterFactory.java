9f081d4e28e21709c9c7efbaf9878d359b3ab835
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-10-05 14:50:15 +0200

    COPL-1036 - Add device events data exporter task

--- coko/com.elster.jupiter.export.processors/src/main/java/com/elster/jupiter/export/processor/impl/StandardCsvEventDataFormatterFactory.java
+++ coko/com.elster.jupiter.export.processors/src/main/java/com/elster/jupiter/export/processor/impl/StandardCsvEventDataFormatterFactory.java
@@ -23,7 +23,7 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 @Component(name = "com.elster.jupiter.export.processor.StandardCsvEventDataFormatterFactory",
-        property = {DataExportService.DATA_TYPE_PROPERTY + "=" + DataExportService.STANDARD_READING_DATA_TYPE},
+        property = {DataExportService.DATA_TYPE_PROPERTY + "=" + DataExportService.STANDARD_EVENT_DATA_TYPE},
         service = DataFormatterFactory.class, immediate = true)
 public class StandardCsvEventDataFormatterFactory implements DataFormatterFactory {
 
