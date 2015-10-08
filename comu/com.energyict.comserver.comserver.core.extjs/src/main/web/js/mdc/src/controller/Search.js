2163d0f7ad371f61541c2990eea90348fe51dda8
Kirill Koshelev <kirkosh@mera.ru>
2015-10-08 12:36:25 +0300

    UP mRID fix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/Search.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/Search.js
@@ -5,7 +5,7 @@ Ext.define('Mdc.controller.Search', {
     extend: 'Uni.controller.Search',
 
     init: function () {
-        var me = this,
+        var me = this, url,
             router = this.getController('Uni.controller.history.Router');
 
         me.callParent(arguments);
@@ -14,7 +14,11 @@ Ext.define('Mdc.controller.Search', {
             var column = columns[cid];
             if (column) {
                 column.renderer = function (value) {
-                    var url = router.getRoute('devices/device').buildUrl({mRID: value});
+                    if (me.searchDomain.get('id') == 'com.elster.jupiter.metering.UsagePoint') {
+                        url = router.getRoute('usagepoints/usagepoint').buildUrl({usagePointMRID: value});
+                    } else {
+                        url = router.getRoute('devices/device').buildUrl({mRID: value});
+                    }
                     return '<a href="{0}">{1}</a>'.replace('{0}', url).replace('{1}', Ext.String.htmlEncode(value));
                 }
             }
