03e6cad9dcdf2cedfab7992cd123003c3e7fc724
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-10-08 11:52:14 +0200

    Added support for @Untransactional annotation

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/TransactionWrapper.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/TransactionWrapper.java
@@ -3,10 +3,23 @@ package com.energyict.mdc.common.rest;
 import com.elster.jupiter.transaction.TransactionContext;
 import com.elster.jupiter.transaction.TransactionService;
 import javax.inject.Inject;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.StreamingOutput;
+
 import org.glassfish.jersey.server.monitoring.ApplicationEvent;
 import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
 import org.glassfish.jersey.server.monitoring.RequestEvent;
 import org.glassfish.jersey.server.monitoring.RequestEventListener;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * This listener automatically closes DB connection after EVERY REST call
@@ -37,18 +50,20 @@ public class TransactionWrapper implements ApplicationEventListener {
         @Override
         public void onEvent(final RequestEvent event) {
             switch (event.getType()) {
-            case RESOURCE_METHOD_START:
-                contextThreadLocal.set(transactionService.getContext());
-                break;
-            case FINISHED:
-                TransactionContext context = contextThreadLocal.get();
-                if (context!=null) { // context will be null if METHOD was never started, e.g. in case of 404
-                    if (event.isSuccess()) {
-                        context.commit();
+                case REQUEST_MATCHED:
+                    if (!event.getUriInfo().getMatchedResourceMethod().getInvocable().getHandlingMethod().isAnnotationPresent(Untransactional.class)) {
+                        contextThreadLocal.set(transactionService.getContext());
                     }
-                    context.close(); // will rollback if called without commit()
-                }
-                break;
+                    break;
+                case FINISHED:
+                    TransactionContext context = contextThreadLocal.get();
+                    if (context!=null) { // context will be null if METHOD was never started, e.g. in case of 404
+                        if (event.isSuccess()) {
+                            context.commit();
+                        }
+                        context.close(); // will rollback if called without commit()
+                    }
+                    break;
             }
         }
     }
