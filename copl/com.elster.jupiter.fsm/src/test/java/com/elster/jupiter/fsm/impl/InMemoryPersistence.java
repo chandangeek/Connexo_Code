d5b37ca7e5e9d1d88e464064598482e0650e6168
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-10-13 11:07:02 +0200

    Fixed missing dep

--- copl/com.elster.jupiter.fsm/src/test/java/com/elster/jupiter/fsm/impl/InMemoryPersistence.java
+++ copl/com.elster.jupiter.fsm/src/test/java/com/elster/jupiter/fsm/impl/InMemoryPersistence.java
@@ -1,6 +1,7 @@
 package com.elster.jupiter.fsm.impl;
 
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
+import com.elster.jupiter.datavault.impl.DataVaultModule;
 import com.elster.jupiter.domain.util.impl.DomainUtilModule;
 import com.elster.jupiter.events.EventService;
 import com.elster.jupiter.events.impl.EventsModule;
@@ -77,7 +78,8 @@ public class InMemoryPersistence {
                 new UserModule(),
                 new UtilModule(),
                 new DomainUtilModule(),
-                new NlsModule()
+                new NlsModule(),
+                new DataVaultModule()
         );
     }
 
