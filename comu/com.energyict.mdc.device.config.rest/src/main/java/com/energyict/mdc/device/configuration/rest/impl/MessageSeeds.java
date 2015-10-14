5d1e51222edb3b48d806df944d0fc3d5f316ea0a
Geert <geert.delmeiren@elster.com>
2015-10-14 14:48:01 +0200

    Fixed some errors in translations

--- comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/MessageSeeds.java
+++ comu/com.energyict.mdc.device.config.rest/src/main/java/com/energyict/mdc/device/configuration/rest/impl/MessageSeeds.java
@@ -9,7 +9,7 @@ import java.util.logging.Level;
 public enum MessageSeeds implements MessageSeed {
 
     FIELD_IS_REQUIRED(1, "RequiredField", "This field is required"),
-    PROTOCOL_INVALID_NAME(2, "deviceType.no.such.protocol", "A protocol with name '{0}' does not exist"),
+    PROTOCOL_INVALID_NAME(2, "deviceType.no.such.protocol", "A protocol with name ''{0}'' does not exist"),
     NO_LOGBOOK_TYPE_ID_FOR_ADDING(3, "NoLogBookTypeIdForAdding", "User should specify ids of LogBook Type for adding"),
     NO_LOGBOOK_TYPE_FOUND(4, "NoLogBookTypeFound", "No LogBook type with id {0}"),
     NO_LOGBOOK_SPEC_FOUND(5, "NoLogBookSpecFound", "No LogBook configuration with id {0}"),
@@ -17,12 +17,12 @@ public enum MessageSeeds implements MessageSeed {
     NO_LOAD_PROFILE_TYPE_FOUND(7, "NoLoadProfileTypeFound", "No Load Profile type with id {0}"),
     NO_CHANNEL_SPEC_FOUND(9, "NoChannelSpecFound", "No channel specification with id {0}"),
     INVALID_REFERENCE_TO_REGISTER_TYPE(10, "NoSuchRegisterType", "Register type could not be found"),
-    DUPLICATE_OBISCODE(11, "DuplicateObisCode", "A register mapping with obis code '{0}', unit '{1}' and time of use '{2}' already exists"),
+    DUPLICATE_OBISCODE(11, "DuplicateObisCode", "A register mapping with obis code ''{0}'', unit ''{1}'' and time of use ''{2}'' already exists"),
     NO_SUCH_DEVICE(14, "noSuchDevice", "No device with MRID {0}"),
     DEVICE_DOES_NOT_MATCH_CONFIG(15, "deviceDoesNotMatchConfig", "Device does not match device configuration"),
     NO_SUCH_CONNECTION_TASK(16, "NoSuchConnectionTask", "No such connection task"),
     NO_DEVICECONFIG_ID_FOR_ADDING(17, "NoDeviceConfigurationIdForAdding", "User should specify ids of Device Configuration for adding"),
-    CONNECTION_TYPE_UNKNOWN(18, "NoSuchConnectionType", "No connection type pluggable class could be found for '{0}'"),
+    CONNECTION_TYPE_UNKNOWN(18, "NoSuchConnectionType", "No connection type pluggable class could be found for ''{0}''"),
     NO_VALIDATIONRULESET_ID_FOR_ADDING(20, "NoValidationRuleSetIdForAdding", "User should specify ids of Validation Ruleset for adding"),
     EXECUTE_COM_TASK_LEVEL1(21, Privileges.EXECUTE_COM_TASK_1, "Execute com task (level 1)"),
     EXECUTE_COM_TASK_LEVEL2(22, Privileges.EXECUTE_COM_TASK_2, "Execute com task (level 2)"),
