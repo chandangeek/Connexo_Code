3d34e236ab78312996f11135cae72b05cfa01448
govanni <govanni.naessens@elster.com>
2015-10-16 16:36:33 +0200

    Hackaton, update to Final instead of initial creation

--- comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/commands/SetupFirmwareManagementCommand.java
+++ comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/commands/SetupFirmwareManagementCommand.java
@@ -48,11 +48,11 @@ public class SetupFirmwareManagementCommand {
     private void setUpDeviceTypeForFirmwareManagement(DeviceType deviceType){
         if (!isExcluded(deviceType)) {
             FirmwareVersion v1 = firmwareService.getFirmwareVersionByVersionAndType(FIRMWARE_VERSION_V1, FirmwareType.METER, deviceType)
-                                .orElseGet(() -> firmwareService.newFirmwareVersion(deviceType, FIRMWARE_VERSION_V1, FirmwareStatus.TEST, FirmwareType.METER).create());
+                                .orElseGet(() -> firmwareService.newFirmwareVersion(deviceType, FIRMWARE_VERSION_V1, FirmwareStatus.GHOST, FirmwareType.METER).create());
             setFirmwareBytes(v1, getClass().getClassLoader().getResourceAsStream(FIRMWARE_VERSION_V1+".firm"));
 
             FirmwareVersion v2 = firmwareService.getFirmwareVersionByVersionAndType(FIRMWARE_VERSION_V2, FirmwareType.METER, deviceType)
-                    .orElseGet(() -> firmwareService.newFirmwareVersion(deviceType, FIRMWARE_VERSION_V2, FirmwareStatus.TEST, FirmwareType.METER).create());
+                    .orElseGet(() -> firmwareService.newFirmwareVersion(deviceType, FIRMWARE_VERSION_V2, FirmwareStatus.GHOST, FirmwareType.METER).create());
             setFirmwareBytes(v2, getClass().getClassLoader().getResourceAsStream(FIRMWARE_VERSION_V2+".firm"));
 
             if (firmwareService.getAllowedFirmwareManagementOptionsFor(deviceType).isEmpty()) {
