779b32885cc9447545a211a23816ca0d5e26aa38
Anton Suslennikov <antsusl@mera.ru>
2015-10-20 17:21:25 +0300

    feature/COMU-211-concurrency-check

--- copl/com.elster.jupiter.time.extjs/src/main/web/js/time/src/model/RelativePeriod.js
+++ copl/com.elster.jupiter.time.extjs/src/main/web/js/time/src/model/RelativePeriod.js
@@ -1,5 +1,5 @@
 Ext.define('Tme.model.RelativePeriod', {
-    extend: 'Ext.data.Model',
+    extend: 'Uni.model.Version',
     fields: [
         {name: 'id', type: 'int', useNull: true},
         {name: 'name', type: 'string', useNull: true},
