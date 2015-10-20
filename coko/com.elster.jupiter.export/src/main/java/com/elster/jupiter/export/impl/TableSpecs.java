5e507484f6701a7f7c0e002ae256eaa829bf2a42
igh <isabelle.gheysens@elster.com>
2015-10-20 11:21:16 +0200

    COPL-1243 - Destination failure messages (logging) do not match design

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/TableSpecs.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/TableSpecs.java
@@ -149,7 +149,7 @@ enum TableSpecs {
             table.addIntervalColumns("exportedDataInterval");
             table.column("INTERVALENDPTBEHAVIOUR").number().conversion(ColumnConversion.NUMBER2ENUM).map("exportedDataBoundaryType").add();
             table.column("STATUS").number().conversion(ColumnConversion.NUMBER2ENUM).map("status").add();
-            table.column("MESSAGE").varChar(Table.SHORT_DESCRIPTION_LENGTH).map("failureReason").add();
+            table.column("MESSAGE").varChar(Table.DESCRIPTION_LENGTH).map("failureReason").add();
             table.column("SUMMARY").type("CLOB").conversion(CLOB2STRING).map("summary").add();
 
             table.primaryKey("DES_PK_EXPOCC").on(taskOccurrence).add();
