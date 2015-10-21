c0296d57065627008799af0ca6d62cb7445a4609
Jeff Smith <Jeffrey.Smith@elster.com>
2015-10-21 14:36:52 -0400

    COIN-288 expose usage point query getter needed for validation rest layer

--- coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/MeteringGroupsService.java
+++ coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/MeteringGroupsService.java
@@ -62,6 +62,8 @@ public interface MeteringGroupsService {
 
     Optional<EndDeviceQueryProvider> pollEndDeviceQueryProvider(String name, Duration duration) throws InterruptedException;
 
+    Query<UsagePointGroup> getUsagePointGroupQuery();
+
     //Finder<EndDeviceGroup> findAllEndDeviceGroups();
 
 }
