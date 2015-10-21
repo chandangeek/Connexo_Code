4fa98e457ee964c42c487655650aa416dadacae7
Kirill Koshelev <kirkosh@mera.ru>
2015-10-21 12:21:12 +0300

    added itemID for search-criteria-sub-menu

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/SearchCriteriaSelector.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/SearchCriteriaSelector.js
@@ -87,6 +87,7 @@ Ext.define('Uni.view.search.field.SearchCriteriaSelector', {
                         text: items[0].criteria.get('group').displayValue,
                         value: group.name,
                         menu: {
+                            itemId: 'search-criteria-sub-menu',
                             items: items
                         }
                     })
