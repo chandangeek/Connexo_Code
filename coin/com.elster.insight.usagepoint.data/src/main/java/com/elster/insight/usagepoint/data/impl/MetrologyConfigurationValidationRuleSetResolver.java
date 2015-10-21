80bb56e3c57b5a3a94d638411ae64ebfb31e724f
Jeff Smith <Jeffrey.Smith@elster.com>
2015-10-21 15:27:02 -0400

    COIN-242 update task to work on usage point groups therefore need resolver in insight for this

---
+++ coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/insight/usagepoint/data/impl/MetrologyConfigurationValidationRuleSetResolver.java
@@ -0,0 +1,46 @@
+package com.elster.insight.usagepoint.data.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.elster.insight.usagepoint.config.UsagePointConfigurationService;
+import com.elster.jupiter.metering.MeterActivation;
+import com.elster.jupiter.validation.ValidationRuleSet;
+import com.elster.jupiter.validation.ValidationRuleSetResolver;
+
+/**
+ * Copyrights EnergyICT
+ * Date: 24/06/2014
+ * Time: 17:56
+ */
+@Component(name = "com.elster.insight.udagepoint.data.validationruleSetResolver", service = ValidationRuleSetResolver.class)
+public class MetrologyConfigurationValidationRuleSetResolver implements ValidationRuleSetResolver {
+
+    private volatile UsagePointConfigurationService usagePointConfigurationService;
+
+    @Reference
+    public void setUsagePointConfigurationService(UsagePointConfigurationService usagePointConfigurationService) {
+        this.usagePointConfigurationService = usagePointConfigurationService;
+    }
+
+    @Override
+    public List<ValidationRuleSet> resolve(MeterActivation meterActivation) {
+        if (!meterActivation.getUsagePoint().isPresent()) {
+            return usagePointConfigurationService.findMetrologyConfigurationForUsagePoint(meterActivation.getUsagePoint().get())
+            .map(metrologyConfiguration -> metrologyConfiguration.getValidationRuleSets())
+            .orElse(Collections.emptyList());
+        }
+        else {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public boolean isValidationRuleSetInUse(ValidationRuleSet ruleset) {
+        return !usagePointConfigurationService.findDeviceConfigurationsForValidationRuleSet(ruleset.getId()).isEmpty();
+    }
+
+}
