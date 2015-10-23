551493ed7911f6dccb436befb3e0c181418920e1
Kurt Kikendall <kurt.kikendall@elster.com>
2015-10-23 09:17:35 -0400

    COIN-293 Added required module dependency on ValidationService.

--- coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/UsagePointConfigModule.java
+++ coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/UsagePointConfigModule.java
@@ -8,6 +8,7 @@ import com.elster.jupiter.events.EventService;
 import com.elster.jupiter.orm.OrmService;
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
 import com.elster.jupiter.users.UserService;
+import com.elster.jupiter.validation.ValidationService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
@@ -16,6 +17,7 @@ public class UsagePointConfigModule extends AbstractModule {
     @Override
     protected void configure() {
         requireBinding(Clock.class);
+        requireBinding(ValidationService.class);
         requireBinding(OrmService.class);
         requireBinding(QueryService.class);
         requireBinding(UserService.class);
