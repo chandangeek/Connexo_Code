a7adfc1cfa413afa62beb489919679c5820543f1
Kurt Kikendall <kurt.kikendall@elster.com>
2015-10-27 14:13:56 -0400

    COIN-296 Add gogo commands to facilitate more testing.

--- coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/ConsoleCommands.java
+++ coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/ConsoleCommands.java
@@ -1,14 +1,36 @@
 package com.elster.insight.usagepoint.config.impl;
 
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+import org.apache.felix.service.command.Descriptor;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import com.elster.insight.usagepoint.config.MetrologyConfiguration;
 import com.elster.insight.usagepoint.config.UsagePointConfigurationService;
+import com.elster.jupiter.cbo.PhaseCode;
+import com.elster.jupiter.metering.AmiBillingReadyKind;
+import com.elster.jupiter.metering.AmrSystem;
+import com.elster.jupiter.metering.Meter;
+import com.elster.jupiter.metering.MeterBuilder;
 import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.ServiceCategory;
+import com.elster.jupiter.metering.ServiceKind;
 import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.metering.UsagePointBuilder;
+import com.elster.jupiter.metering.UsagePointConnectedKind;
+import com.elster.jupiter.metering.readings.IntervalReading;
+import com.elster.jupiter.metering.readings.MeterReading;
+import com.elster.jupiter.metering.readings.beans.IntervalBlockImpl;
+import com.elster.jupiter.metering.readings.beans.IntervalReadingImpl;
+import com.elster.jupiter.metering.readings.beans.MeterReadingImpl;
+import com.elster.jupiter.metering.readings.beans.ReadingImpl;
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
+import com.elster.jupiter.transaction.TransactionContext;
 import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.transaction.VoidTransaction;
 import com.elster.jupiter.validation.ValidationRuleSet;
@@ -23,7 +45,12 @@ import com.elster.jupiter.validation.ValidationService;
                 "osgi.command.function=metrologyConfigurations",
                 "osgi.command.function=linkUsagePointToMetrologyConfiguration",
                 "osgi.command.function=createValidationRuleSet",
-                "osgi.command.function=assignValRuleSetToMetrologyConfig"}, immediate = true)
+                "osgi.command.function=assignValRuleSetToMetrologyConfig",
+                "osgi.command.function=createMeter",
+                "osgi.command.function=createUsagePoint",
+                "osgi.command.function=saveRegister",
+                "osgi.command.function=saveLP",
+                "osgi.command.function=getLpReadings"}, immediate = true)
 public class ConsoleCommands {
 
     private volatile UsagePointConfigurationService usagePointConfigurationService;
@@ -134,6 +161,115 @@ public class ConsoleCommands {
         }
     }
     
+    @Descriptor("Create a meter")
+    public void createMeter(@Descriptor("System id (usually 2)") long amrSystemId, @Descriptor("EA_MS Meter ID") String amrid, @Descriptor("MRID") String mrId) {
+        threadPrincipalService.set(() -> "Console");
+        try (TransactionContext context = transactionService.getContext()) {
+            AmrSystem amrSystem = meteringService.findAmrSystem(amrSystemId).orElseThrow(() -> new IllegalArgumentException("amr System not found"));
+            Meter meter = amrSystem.newMeter(amrid).setName(amrid).setMRID(mrId).create();
+            meter.update();
+            context.commit();
+            System.out.println("Meter " + amrid + " created with ID: " + meter.getId());
+        } finally {
+            threadPrincipalService.clear();
+        }
+    }
+
+    @Descriptor("Create a usage point")
+    public void createUsagePoint(@Descriptor("System id (usually 2)") long amrSystemId, @Descriptor("Meter ID") String mrId, @Descriptor("UsagePoint ID") String upId, @Descriptor("Name") String name) {
+        threadPrincipalService.set(() -> "Console");
+        try (TransactionContext context = transactionService.getContext()) {
+            AmrSystem amrSystem = meteringService.findAmrSystem(amrSystemId).orElseThrow(() -> new IllegalArgumentException("amr System not found"));
+            Meter meter = amrSystem.findMeter(mrId).orElseThrow(() -> new IllegalArgumentException("Usage Point not created : Meter not found " + mrId));
+            ServiceCategory category = meteringService.getServiceCategory(ServiceKind.ELECTRICITY).orElseThrow(() -> new IllegalArgumentException("Could not get service"));
+            UsagePointBuilder builder = category.newUsagePoint(upId);
+            UsagePoint up = builder.withName(name).withIsSdp(true).withIsVirtual(false).create();
+            up.newElectricityDetailBuilder(Instant.now()).
+                    withAmiBillingReady(AmiBillingReadyKind.BILLINGAPPROVED).
+                    withConnectionState(UsagePointConnectedKind.CONNECTED).
+                    withGrounded(true).
+                    withPhaseCode(PhaseCode.UNKNOWN).build();
+            meter.activate(up, Instant.parse("2014-01-01T08:00:00Z"));
+            meter.update();
+            up.update();
+            context.commit();
+            System.out.println("Usage point " + up.getId() + " created with name: " + name);
+        } finally {
+            threadPrincipalService.clear();
+        }
+    }
+
+    @Descriptor("Save a register reading")
+    public void saveRegister(@Descriptor("System id (usually 2)") long amrSystemId,
+            @Descriptor("EA_MS Meter ID") String amrid,
+            @Descriptor("timestamp (2015-05-14T10:15:30Z)") String timestamp,
+            @Descriptor("CIM code") String cim,
+            @Descriptor("Value") double value) {
+        threadPrincipalService.set(() -> "Console");
+        try (TransactionContext context = transactionService.getContext()) {
+            AmrSystem amrSystem = meteringService.findAmrSystem(amrSystemId).orElseThrow(() -> new IllegalArgumentException("amr System not found"));
+            Meter meter = amrSystem.findMeter(amrid).orElseThrow(() -> new IllegalArgumentException("Meter not found " + amrid));
+            meter.store(createReading(cim, BigDecimal.valueOf(value), Instant.parse(timestamp)));
+            context.commit();
+            System.out.println("Save register for ID: " + meter.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPrincipalService.clear();
+        }
+    }
+
+    @Descriptor("Save an LP readings")
+    public void saveLP(@Descriptor("System id (usually 2)") long amrSystemId,
+            @Descriptor("EA_MS Meter ID") String amrid,
+            @Descriptor("timestamp (2015-05-14T10:15:30Z)") String timestamp,
+            @Descriptor("CIM code") String cim,
+            @Descriptor("Interval in minutes") int minutes,
+            @Descriptor("Comma separated Values") String values) {
+        threadPrincipalService.set(() -> "Console");
+        try (TransactionContext context = transactionService.getContext()) {
+            AmrSystem amrSystem = meteringService.findAmrSystem(amrSystemId).orElseThrow(() -> new IllegalArgumentException("amr System not found"));
+            Meter meter = amrSystem.findMeter(amrid).orElseThrow(() -> new IllegalArgumentException("Meter not found " + amrid));
+            meter.store(createLPReading(cim, values, Instant.parse(timestamp), minutes));
+            context.commit();
+            System.out.println("Save LP for ID: " + meter.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPrincipalService.clear();
+        }
+    }
+
+    private MeterReading createReading(String cim, BigDecimal value, Instant timeStamp) {
+        ReadingImpl reading = ReadingImpl.of(cim, value, timeStamp);
+        MeterReading results = MeterReadingImpl.of(reading);
+        return results;
+    }
+
+    private MeterReading createLPReading(String cim, String values, Instant timeStamp, int minutes) {
+        IntervalBlockImpl block = IntervalBlockImpl.of(cim);
+        for (IntervalReading reading : getLpReadings(values, timeStamp, minutes)) {
+            block.addIntervalReading(reading);
+        }
+        MeterReadingImpl results = MeterReadingImpl.newInstance();
+        results.addIntervalBlock(block);
+        return results;
+    }
+
+    private ArrayList<IntervalReading> getLpReadings(String values, Instant timeStamp, int minutes) {
+        ArrayList<IntervalReading> results = new ArrayList<>();
+        for (String value : values.split(",")) {
+            try {
+                results.add(IntervalReadingImpl.of(timeStamp, new BigDecimal(value)));
+            } catch (Exception e) {
+                System.out.println("Illegal value : " + value);
+                throw e;
+            }
+            timeStamp = timeStamp.plus(minutes, ChronoUnit.MINUTES);
+        }
+        return results;
+    }
+
     @Reference
     public void setUsagePointConfigurationService(UsagePointConfigurationService usagePointConfigurationService) {
         this.usagePointConfigurationService = usagePointConfigurationService;
