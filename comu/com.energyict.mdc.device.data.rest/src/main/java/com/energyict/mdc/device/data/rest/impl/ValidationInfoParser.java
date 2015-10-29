0b2664d0e4ded16a512d2bc9fe83250b93c5b654
prikazch <prikazch@mera.ru>
2015-10-29 11:09:12 +0300

    [COMU-2395] The validation results page of a device still has the filter below the device menu

---
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/ValidationInfoParser.java
@@ -0,0 +1,27 @@
+package com.energyict.mdc.device.data.rest.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class ValidationInfoParser {
+
+    public ValidationLoadProfilePeriodInfo parseFromNode(JsonNode jsonNode) {
+        ValidationLoadProfilePeriodInfo info = new ValidationLoadProfilePeriodInfo();
+        convertJsonNodeToInfo(info, jsonNode);
+        return info;
+    }
+
+    private void convertJsonNodeToInfo(ValidationLoadProfilePeriodInfo info, JsonNode singleFilter) {
+        JsonNode property = singleFilter.get("id");
+        if (property != null && property.numberValue() != null) {
+            info.id = property.numberValue().longValue();
+        }
+        property = singleFilter.get("intervalStart");
+        if (property != null && property.numberValue() != null) {
+            info.startInterval = property.numberValue().longValue();
+        }
+        property = singleFilter.get("intervalEnd");
+        if (property != null && property.numberValue() != null) {
+            info.endInterval = property.numberValue().longValue();
+        }
+    }
+}
