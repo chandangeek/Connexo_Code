87a1936b5cd8754688e43291078442bc902e3781
Geert <geert.delmeiren@elster.com>
2015-11-02 09:28:14 +0100

    Fixed a typo in the display name "Conneco Facts search" -> "Connexo Facts search"

--- coko/com.elster.jupiter.yellowfin.groups/src/main/java/com/elster/jupiter/yellowfin/groups/YellowfinGroupsService.java
+++ coko/com.elster.jupiter.yellowfin.groups/src/main/java/com/elster/jupiter/yellowfin/groups/YellowfinGroupsService.java
@@ -1,8 +1,6 @@
 package com.elster.jupiter.yellowfin.groups;
 
 import com.elster.jupiter.nls.Thesaurus;
-import com.elster.jupiter.tasks.RecurrentTask;
-import com.elster.jupiter.tasks.TaskOccurrence;
 import com.elster.jupiter.yellowfin.groups.impl.DynamicDeviceGroupImpl;
 
 import java.util.List;
@@ -12,7 +10,7 @@ public interface YellowfinGroupsService {
     String COMPONENTNAME = "YFG";
     String ADHOC_SEARCH_LIFE_CYCLE_QUEUE_DEST = "AdHocSearch";
     String ADHOC_SEARCH_LIFE_CYCLE_QUEUE_TASK = "AdHoc Search";
-    String ADHOC_SEARCH_LIFE_CYCLE_QUEUE_DEST_DISPLAYNAME = "Conneco Facts search";
+    String ADHOC_SEARCH_LIFE_CYCLE_QUEUE_DEST_DISPLAYNAME = "Connexo Facts search";
 
     Thesaurus thesaurus();
 
