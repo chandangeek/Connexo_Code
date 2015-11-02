d9fac1fa67bca3910aca7091b7e896d27033a260
Geert <geert.delmeiren@elster.com>
2015-11-02 13:27:55 +0100

    COPL-1329 - First data formatter in list is always selected when editing a data export task Fix for the blank page mentioned in the comment of Matthieu posted at 2015-10-20 (10:56)

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/ContextOverride.js
@@ -0,0 +1,35 @@
+Ext.define('Uni.override.ContextOverride', {
+    override: 'Ext.layout.Context',
+
+    // Workaround for the ExtJS bug that produces the error "Uncaught TypeError: Cannot read property 'isItemBoxParent' of undefined"
+    // cf. https://www.sencha.com/forum/showthread.php?291412-Error-after-upgrade-to-ExtJS-4.2.3
+
+    queueInvalidate: function(item, options) {
+        var me = this,
+            comp, item, invalidQueue,
+            filteredQueue = [],
+            i, queueLength;
+
+
+        // Call the parent method we are overriding
+        me.callParent(arguments);
+
+        // MJN Change: we want to ignore Load Masks with no ownerLayout setting
+        invalidQueue = me.invalidQueue;
+        queueLength = invalidQueue.length;
+
+
+        for (i = 0; i < queueLength; i++) {
+            comp = invalidQueue[i];
+            item = comp.item;
+            if (item && item.id.indexOf("loadmask-") > -1 && !item.ownerLayout) {
+                //apex.debug("ignoring " + item.$className + " from layout refresh");
+            } else {
+                filteredQueue.push(comp);
+            }
+        }
+        // Set the invalidQueue to our filtered queue to excldue loadMasks that cause exceptions
+        me.invalidQueue = filteredQueue;
+    }
+
+});
