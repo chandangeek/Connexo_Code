c058fb0db2eb152a4c3ce5b963138d45b8652345
Kurt Kikendall <kurt.kikendall@elster.com>
2015-11-03 09:42:29 -0500

    Work on code review JP-480.  Add ProviderType annotation.

--- coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/MetrologyConfiguration.java
+++ coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/MetrologyConfiguration.java
@@ -3,8 +3,11 @@ package com.elster.insight.usagepoint.config;
 import java.time.Instant;
 import java.util.List;
 
+import aQute.bnd.annotation.ProviderType;
+
 import com.elster.jupiter.validation.ValidationRuleSet;
 
+@ProviderType
 public interface MetrologyConfiguration {
     long getId();
 
