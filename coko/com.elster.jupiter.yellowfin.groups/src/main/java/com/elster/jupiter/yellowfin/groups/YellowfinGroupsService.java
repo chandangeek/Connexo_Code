f552e7e0fc55b1196c62d6ace8c34f11c0fbf70a
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-11-03 17:03:04 +0100

    Incremented packageinfo and added appropriate types to interface

--- coko/com.elster.jupiter.yellowfin.groups/src/main/java/com/elster/jupiter/yellowfin/groups/YellowfinGroupsService.java
+++ coko/com.elster.jupiter.yellowfin.groups/src/main/java/com/elster/jupiter/yellowfin/groups/YellowfinGroupsService.java
@@ -1,11 +1,13 @@
 package com.elster.jupiter.yellowfin.groups;
 
+import aQute.bnd.annotation.ProviderType;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.yellowfin.groups.impl.DynamicDeviceGroupImpl;
 
 import java.util.List;
 import java.util.Optional;
 
+@ProviderType
 public interface YellowfinGroupsService {
     String COMPONENTNAME = "YFG";
     String ADHOC_SEARCH_LIFE_CYCLE_QUEUE_DEST = "AdHocSearch";
