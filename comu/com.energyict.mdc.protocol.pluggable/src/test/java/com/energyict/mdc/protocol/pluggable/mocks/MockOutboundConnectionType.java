9e3222f43684273c0234ac1224900964b841efb2
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-11-06 09:37:54 +0100

    Delegae getPropertySpecs to the CustomPropertySet if any (add @Obsolete on classes that still need an implementation for getCustomPropertySet to make sure that I do not forget to delete the method later)

--- comu/com.energyict.mdc.protocol.pluggable/src/test/java/com/energyict/mdc/protocol/pluggable/mocks/MockOutboundConnectionType.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/test/java/com/energyict/mdc/protocol/pluggable/mocks/MockOutboundConnectionType.java
@@ -9,7 +9,6 @@ import com.energyict.mdc.protocol.api.dynamic.ConnectionProperty;
 
 import com.elster.jupiter.cps.CustomPropertySet;
 import com.elster.jupiter.cps.PersistentDomainExtension;
-import com.elster.jupiter.properties.PropertySpec;
 
 import java.util.Collections;
 import java.util.List;
@@ -64,11 +63,6 @@ public class MockOutboundConnectionType implements ConnectionType {
     @Override
     public Optional<CustomPropertySet<ConnectionType, ? extends PersistentDomainExtension<ConnectionType>>> getCustomPropertySet() {
         return Optional.empty();
-    }
-
-    @Override
-    public List<PropertySpec> getPropertySpecs() {
-        return Collections.emptyList();
     }
 
 }
