817a1e1b6b96c29ac8b0329b73f82a1cd007db58
Kurt Kikendall <kurt.kikendall@elster.com>
2015-11-11 13:47:18 -0500

    Rework router and menu setup.

--- coin/com.elster.insight.app/src/main/web/js/insight/app/controller/Main.js
+++ coin/com.elster.insight.app/src/main/web/js/insight/app/controller/Main.js
@@ -7,8 +7,7 @@ Ext.define('InsightApp.controller.Main', {
     requires: [
         'Cfg.controller.Main',
         'Uni.controller.Navigation',
-        'Imt.controller.Main',
-        'InsightApp.controller.History'
+        'Imt.controller.Main'
     ],
 
     applicationTitle: 'Connexo Insight',
@@ -21,14 +20,10 @@ Ext.define('InsightApp.controller.Main', {
 
     controllers: [
         'Cfg.controller.Main',
-        'Imt.controller.Main',
-        'InsightApp.controller.History'
+        'Imt.controller.Main'
     ],
     init: function () {
-        // Init historians
-        var hist = this.getController('InsightApp.controller.History');
         var router = this.getController('Uni.controller.history.Router');
-        this.initMenu();
         
         // default route redirect
         router.initRoute('default', {
@@ -38,48 +33,4 @@ Ext.define('InsightApp.controller.Main', {
 
         this.callParent(arguments);
     },
-
-    initMenu: function () {
-        var menuItem = Ext.create('Uni.model.MenuItem', {
-            text: Uni.I18n.translate('general.label.usagepoints', 'INS', 'Usage points'),
-            href: 'usagepoints',
-            portal: 'usagepoints',
-            glyph: 'devices'
-        });
-
-        Uni.store.MenuItems.add(menuItem);
-
-        var portalItem1 = Ext.create('Uni.model.PortalItem', {
-            title: Uni.I18n.translate('general.label.administration', 'INS', 'Administration'),
-            portal: 'usagepoints',
-            items: [
-                {
-                    text: Uni.I18n.translate('general.label.usagepoint.add', 'INS', 'Add usage point'),
-                    href: '#/usagepoints/add',
-                    itemId: 'add-usagepoints'
-                },
-            ]
-        });
-
-        Uni.store.PortalItems.add(
-            portalItem1
-        );
-        
-        var portalItem2 = Ext.create('Uni.model.PortalItem', {
-            title: Uni.I18n.translate('general.label.metrologyconfiguration', 'INS', 'Metrology configuration'),
-            portal: 'administration',
-            items: [
-                {
-                    text: Uni.I18n.translate('general.label.metrologyconfiguration', 'INS', 'Metrology configuration'),
-                    href: '#/administration/metrologyconfiguration',
-                    itemId: 'overview-metrologyconfiguration'
-                },
-            ]
-        });
-
-
-        Uni.store.PortalItems.add(
-            portalItem2
-        );
-    }
 });
