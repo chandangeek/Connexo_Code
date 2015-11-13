6a16122d99e9a8ecc1e51328e15fefc3e80e99c2
Maksim Barinov <mbarinov@mera.ru>
2015-11-13 17:19:22 +0300

    [COMU-2203] View components list

--- comu/com.elster.jupiter.subsystem.mdc/src/main/java/com/elster/jupiter/subsystem/mdc/impl/MdcSubsystemActivator.java
+++ comu/com.elster.jupiter.subsystem.mdc/src/main/java/com/elster/jupiter/subsystem/mdc/impl/MdcSubsystemActivator.java
@@ -1,14 +1,26 @@
 package com.elster.jupiter.subsystem.mdc.impl;
 
 import com.elster.jupiter.license.License;
+import com.elster.jupiter.system.Component;
+import com.elster.jupiter.system.PomSaxParser;
 import com.elster.jupiter.system.SubsystemService;
 import com.elster.jupiter.system.beans.SubsystemImpl;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.xml.sax.SAXException;
 
-@Component(name = "com.elster.jupiter.subsystem.mdc", immediate = true)
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+@org.osgi.service.component.annotations.Component(name = "com.elster.jupiter.subsystem.mdc", immediate = true)
 public class MdcSubsystemActivator {
 
     public static final String MULTISENSE_ID = "MultiSense";
@@ -17,13 +29,28 @@ public class MdcSubsystemActivator {
 
     private volatile License license;
     private volatile SubsystemService subsystemService;
-
-    public MdcSubsystemActivator() {
-    }
+    private Logger logger = Logger.getLogger(MdcSubsystemActivator.class.getName());
 
     @Activate
     public void activate(BundleContext context) {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        PomSaxParser saxp = new PomSaxParser();
+        try {
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(context.getBundle().getResource("META-INF/maven/com.elster.jupiter.subsystem/mdc/pom.xml").toURI().toString(), saxp);
+            parser.parse(context.getBundle().findEntries("", "mdc.bom*.pom", false).nextElement().toURI().toString(), saxp);
+        } catch (SAXException e) {
+            logger.log(Level.SEVERE, "SAXException");
+        } catch (ParserConfigurationException e) {
+            logger.log(Level.SEVERE, "ParserConfigurationException");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "IOException");
+        } catch (URISyntaxException e) {
+            logger.log(Level.SEVERE, "URISyntaxException");
+        }
         SubsystemImpl subsystem = new SubsystemImpl(MULTISENSE_ID, MULTISENSE_NAME, context.getBundle().getVersion().toString());
+        List<Component> dependenciesList = saxp.getDependencies().values().stream().map(dependency -> saxp.createComponent(dependency, subsystem)).collect(Collectors.toList());
+        subsystem.addComponents(dependenciesList);
         this.subsystemService.registerSubsystem(subsystem);
     }
 
