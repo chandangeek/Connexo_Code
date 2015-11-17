2250f13dfc1311cf737b7267f4c1c4c4c4b80325
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-11-17 09:55:51 +0100

    Fix cyclic dependency

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/cron/parser/DescriptionTypeEnum.java
+++
@@ -1,9 +0,0 @@
-package com.elster.jupiter.util.cron.parser;
-
-/**
- * @author grhodes
- * @since 10 Dec 2012 11:40:03
- */
-public enum DescriptionTypeEnum {
-    FULL, TIMEOFDAY, SECONDS, MINUTES, HOURS, DAYOFWEEK, MONTH, DAYOFMONTH, YEAR
-}
