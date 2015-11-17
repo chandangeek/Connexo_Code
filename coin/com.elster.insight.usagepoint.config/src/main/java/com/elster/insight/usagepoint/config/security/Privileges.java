ababf21d4b712b8b2e1d9d20a88dbc66177ed6f6
git <surya.pampari@elster.com>
2015-11-17 16:34:31 -0500

    COIN-261 Implement privileges

---
+++ coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/security/Privileges.java
@@ -0,0 +1,52 @@
+package com.elster.insight.usagepoint.config.security;
+
+/**
+ * Not an enum, otherwise it won't work for RolesAllowed.
+ */
+/*public interface Privileges {
+
+}*/
+
+import com.elster.jupiter.nls.TranslationKey;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum Privileges implements TranslationKey {
+
+	ADMIN_ANY_METROLOGY_CONFIG(Constants.ADMIN_ANY_METROLOGY_CONFIG, "Administrate any metrology configuration"),
+	BROWSE_ANY_METROLOGY_CONFIG(Constants.BROWSE_ANY_METROLOGY_CONFIG, "Browse any metrology configuration");
+
+
+	private final String key;
+	private final String description;
+
+	Privileges(String key, String description) {
+		this.key = key;
+		this.description = description;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	@Override
+	public String getDefaultFormat() {
+		return getDescription();
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public static String[] keys() {
+		return Arrays.stream(Privileges.values())
+				.map(Privileges::getKey)
+				.collect(Collectors.toList())
+				.toArray(new String[Privileges.values().length]);
+	}
+
+	public interface Constants {
+        String ADMIN_ANY_METROLOGY_CONFIG = "UCR_ADMIN_ANY_METROLOGY_CONFIG";
+        String BROWSE_ANY_METROLOGY_CONFIG = "UCR_BROWSE_ANY_METROLOGY_CONFIG";
+	}
+}
