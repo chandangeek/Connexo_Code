10389c8eef82bc083e5aa69e8dfbf8acee09d9ab
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-11-18 11:20:05 +0100

    Move test classes to src/test directory

---
+++ copl/com.elster.jupiter.devtools/src/test/java/com/elster/jupiter/devtools/test/analysis/SlocMetricTest.java
@@ -0,0 +1,25 @@
+package com.elster.jupiter.devtools.test.analysis;
+
+import com.google.common.reflect.ClassPath;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.junit.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Ignore
+public class SlocMetricTest {
+
+	@Test
+	public void testSloc() throws IOException {
+		Set<ClassPath.ClassInfo> classInfos = ClassPath.from(this.getClass().getClassLoader()).getTopLevelClassesRecursive("com.elster");
+		int sloc = 0;
+		for (ClassPath.ClassInfo classInfo : classInfos) {
+			sloc += SlocMetric.sloc(classInfo.getName());
+		}
+		assertThat(sloc).isGreaterThan(10);
+	}
+
+}
