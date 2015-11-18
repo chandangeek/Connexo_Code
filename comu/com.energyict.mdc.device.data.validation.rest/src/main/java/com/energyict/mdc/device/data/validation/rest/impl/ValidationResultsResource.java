20bf9f7e55dbcfb63e64decd75952b778b3e5c4e
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2015-11-18 11:30:49 +0100

    Moved TransactionWrapper to rest.util and now auto-loaded by REST whiteboard

--- comu/com.energyict.mdc.device.data.validation.rest/src/main/java/com/energyict/mdc/device/data/validation/rest/impl/ValidationResultsResource.java
+++ comu/com.energyict.mdc.device.data.validation.rest/src/main/java/com/energyict/mdc/device/data/validation/rest/impl/ValidationResultsResource.java
@@ -1,6 +1,6 @@
 package com.energyict.mdc.device.data.validation.rest.impl;
 
-import com.energyict.mdc.common.rest.Transactional;
+import com.elster.jupiter.rest.util.Transactional;
 import com.energyict.mdc.device.data.validation.DeviceDataValidationService;
 
 import com.elster.jupiter.rest.util.JsonQueryParameters;
