6981e7eca6b24abbf337aee8f28b2659d33372a9
Lobanov Dmitry <dlobanov@mera.ru>
2015-11-19 10:28:39 +0300

    View components list

--- comu/com.elster.jupiter.subsystem.mdc/src/main/java/com/elster/jupiter/subsystem/mdc/impl/MdcSubsystemActivator.java
+++ comu/com.elster.jupiter.subsystem.mdc/src/main/java/com/elster/jupiter/subsystem/mdc/impl/MdcSubsystemActivator.java
@@ -1,24 +1,27 @@
 package com.elster.jupiter.subsystem.mdc.impl;
 
 import com.elster.jupiter.license.License;
+import com.elster.jupiter.system.BundleType;
 import com.elster.jupiter.system.Component;
-import com.elster.jupiter.system.PomSaxParser;
+import com.elster.jupiter.system.utils.DependenciesParser;
+import com.elster.jupiter.system.utils.SubsystemModel;
 import com.elster.jupiter.system.SubsystemService;
+import com.elster.jupiter.system.beans.ComponentImpl;
 import com.elster.jupiter.system.beans.SubsystemImpl;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 @org.osgi.service.component.annotations.Component(name = "com.elster.jupiter.subsystem.mdc", immediate = true)
 public class MdcSubsystemActivator {
@@ -27,31 +30,33 @@ public class MdcSubsystemActivator {
     public static final String MULTISENSE_NAME = "Connexo MultiSense";
     public static final String APPLICATION_KEY = "MDC";
 
-    private volatile License license;
+    private static final Logger LOGGER = Logger.getLogger(MdcSubsystemActivator.class.getName());
+    private static final String ROOT = "";
+
     private volatile SubsystemService subsystemService;
-    private Logger logger = Logger.getLogger(MdcSubsystemActivator.class.getName());
+
+    private SubsystemImpl subsystem;
 
     @Activate
     public void activate(BundleContext context) {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        PomSaxParser saxp = new PomSaxParser();
-        try {
-            SAXParser parser = factory.newSAXParser();
-            parser.parse(context.getBundle().getResource("META-INF/maven/com.elster.jupiter.subsystem/mdc/pom.xml").toURI().toString(), saxp);
-            parser.parse(context.getBundle().findEntries("", "mdc.bom*.pom", false).nextElement().toURI().toString(), saxp);
-        } catch (SAXException e) {
-            logger.log(Level.SEVERE, "SAXException");
-        } catch (ParserConfigurationException e) {
-            logger.log(Level.SEVERE, "ParserConfigurationException");
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "IOException");
-        } catch (URISyntaxException e) {
-            logger.log(Level.SEVERE, "URISyntaxException");
-        }
-        SubsystemImpl subsystem = new SubsystemImpl(MULTISENSE_ID, MULTISENSE_NAME, context.getBundle().getVersion().toString());
-        List<Component> dependenciesList = saxp.getDependencies().values().stream().map(dependency -> saxp.createComponent(dependency, subsystem)).collect(Collectors.toList());
-        subsystem.addComponents(dependenciesList);
+        SubsystemModel model = new SubsystemModel();
+        DependenciesParser parser = new DependenciesParser(model);
+
+        loadProperties(parser, findBundleResource(context, ROOT, "third-party-bundles.properties"));
+        parse(parser, findBundleResource(context, "META-INF/maven/com.elster.jupiter.subsystem/mdc", "pom.xml"));
+        parse(parser, findBundleResource(context, ROOT, "mdc.bom*.pom"));
+        model.addDependency(buildSelfComponent(context));
+
+        List<Component> components = model.mergeDependencies();
+
+        subsystem = new SubsystemImpl(MULTISENSE_ID, MULTISENSE_NAME, context.getBundle().getVersion().toString());
+        subsystem.addComponents(components);
         this.subsystemService.registerSubsystem(subsystem);
+    }
+
+    @Deactivate
+    public void deactivate(BundleContext context) {
+        this.subsystemService.unregisterSubsystem(subsystem);
     }
 
     @Reference
@@ -61,6 +66,41 @@ public class MdcSubsystemActivator {
 
     @Reference(target = "(com.elster.jupiter.license.application.key=" + APPLICATION_KEY + ")")
     public void setLicense(License license) {
-        this.license = license;
+    }
+
+    // We need to add this component manually because the mdc subsystem bundle can't have itself in the list of dependencies
+    private ComponentImpl buildSelfComponent(BundleContext context) {
+        ComponentImpl self = new ComponentImpl();
+        self.setSymbolicName(context.getBundle().getSymbolicName());
+        self.setVersion(context.getBundle().getVersion().toString());
+        self.setBundleType(BundleType.APPLICATION_SPECIFIC);
+        return self;
+    }
+
+    private void loadProperties(DependenciesParser parser, URL url) {
+        try {
+            parser.loadThirdPartyBundlesProperties(url);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Unable to load properties file [url=" + url.toString() + "]", e);
+        }
+    }
+
+    private void parse(DependenciesParser parser, URL url) {
+        if (url != null) {
+            try {
+                parser.parse(url.toURI().toString());
+            } catch (URISyntaxException | ParserConfigurationException | SAXException | IOException e) {
+                LOGGER.log(Level.SEVERE, "Unable to parse file [url=" + url.toString() + "]", e);
+            }
+        }
+    }
+
+    private URL findBundleResource(BundleContext context, String path, String filePattern) {
+        Enumeration<URL> entries = context.getBundle().findEntries(path, filePattern, false);
+        if (entries.hasMoreElements()) {
+            return entries.nextElement();
+        }
+        LOGGER.log(Level.SEVERE, "Unable to find resource [path=" + path + ", filePattern=" + filePattern + "]");
+        return null;
     }
 }
