d60f92093ad24dc286a8345db1a9416c733d63d5
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-11-20 09:54:00 +0100

    Support target application on SearchDomain

---
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/search/SearchDomains.java
@@ -0,0 +1,21 @@
+package com.energyict.mdc.common.search;
+
+/**
+ * Provides common constants that relate to the search domains
+ * that are supported by the MultiSense application bundles.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2015-11-20 (09:02)
+ */
+public enum SearchDomains {
+
+    DEVICE;
+
+    /**
+     * The String that should be used by all search domains
+     * that can only be within MultiSense and should
+     * therefore not appear in UI components of other applications.
+     */
+    public static final String SEARCH_DOMAIN_APPLICATION_KEY = "COMU";
+
+}
