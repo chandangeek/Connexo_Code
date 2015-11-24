01dbc30d27752c667a587bb78144239195d5380a
Lobanov Dmitry <dlobanov@mera.ru>
2015-11-24 13:21:39 +0300

    COMU-2515

--- comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/attributes/connection/ConnectionAttributesImportProcessor.java
+++ comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/attributes/connection/ConnectionAttributesImportProcessor.java
@@ -19,10 +19,12 @@ import com.energyict.mdc.device.data.importers.impl.properties.SupportedNumberFo
 import com.energyict.mdc.device.data.tasks.ConnectionTask;
 import com.energyict.mdc.device.data.tasks.InboundConnectionTask;
 import com.energyict.mdc.device.data.tasks.ScheduledConnectionTask;
+import com.energyict.mdc.protocol.api.ConnectionType;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
 public class ConnectionAttributesImportProcessor implements FileImportProcessor<ConnectionAttributesImportRecord> {
@@ -30,6 +32,7 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
     private final DeviceDataImporterContext context;
     private final PropertiesConverterConfig propertiesConverterConfig;
 
+    private boolean isFirstRow = true;
     private String connectionMethod;
 
     ConnectionAttributesImportProcessor(DeviceDataImporterContext context, SupportedNumberFormat numberFormat) {
@@ -45,10 +48,9 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
         Optional<ConnectionTask<?, ?>> connectionTask = device.getConnectionTasks().stream()
                 .filter(task -> task.getName().equals(data.getConnectionMethod())).findFirst();
         if (connectionTask.isPresent()) {
+            validateConnectionMethodProperties(data, connectionTask.get().getConnectionType(), logger);
             ConnectionTask task = connectionTask.get();
-            data.getConnectionAttributes().forEach((key, value) -> {
-                task.setProperty(key, parseStringToValue(task.getConnectionType().getPropertySpec(key), value, data));
-            });
+            setConnectionAttributes(data, task.getConnectionType(), task::setProperty);
             task.save();
             logMissingPropertiesIfIncomplete(task, data, logger);
         } else {
@@ -56,8 +58,10 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
                     .stream().filter(task -> task.getName().equals(data.getConnectionMethod()))
                     .findFirst()
                     .orElseThrow(() -> new ProcessorException(MessageSeeds.NO_CONNECTION_METHOD_ON_DEVICE, data.getLineNumber(), data.getConnectionMethod()));
+            validateConnectionMethodProperties(data, partialConnectionTask.getConnectionType(), logger);
             createConnectionTaskOnDevice(device, partialConnectionTask, data, logger);
         }
+        isFirstRow = false;
     }
 
     private void logMissingPropertiesIfIncomplete(ConnectionTask<?, ?> connectionTask, ConnectionAttributesImportRecord data, FileImportLogger logger) {
@@ -82,6 +86,16 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
         }
     }
 
+    private void validateConnectionMethodProperties(ConnectionAttributesImportRecord data, ConnectionType connectionType, FileImportLogger logger) {
+        if (isFirstRow) {
+            String unknownAttributes = data.getConnectionAttributes().keySet().stream()
+                    .filter(key -> connectionType.getPropertySpec(key) == null).collect(Collectors.joining(", "));
+            if (!unknownAttributes.isEmpty()) {
+                logger.warning(MessageSeeds.UNKNOWN_CONNECTION_ATTRIBUTE, data.getConnectionMethod(), unknownAttributes);
+            }
+        }
+    }
+
     private Object parseStringToValue(PropertySpec propertySpec, String value, ConnectionAttributesImportRecord data) {
         ValueFactory<?> valueFactory = propertySpec.getValueFactory();
         Optional<DynamicPropertyConverter> propertyParser = DynamicPropertyConverter.of(valueFactory.getClass());
@@ -91,8 +105,7 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
                 value = propertyParser.get().configure(propertiesConverterConfig).convert(value);
             }
             parsedValue = valueFactory.fromStringValue(value);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             String expectedFormat = propertyParser.isPresent() ? propertyParser.get().getExpectedFormat(context.getThesaurus()) : valueFactory.getValueType().getName();
             throw new ProcessorException(MessageSeeds.LINE_FORMAT_ERROR, data.getLineNumber(), propertySpec.getName(), expectedFormat);
         }
@@ -115,7 +128,7 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
     private void addInboundConnectionTaskToDevice(Device device, PartialInboundConnectionTask partialConnectionTask, ConnectionAttributesImportRecord data, FileImportLogger logger) throws ProcessorException {
         Device.InboundConnectionTaskBuilder inboundConnectionTaskBuilder = device.getInboundConnectionTaskBuilder(partialConnectionTask);
         try {
-            data.getConnectionAttributes().forEach((key, value) -> inboundConnectionTaskBuilder.setProperty(key, parseStringToValue(partialConnectionTask.getConnectionType().getPropertySpec(key), value, data)));
+            setConnectionAttributes(data, partialConnectionTask.getConnectionType(), inboundConnectionTaskBuilder::setProperty);
             inboundConnectionTaskBuilder.setConnectionTaskLifecycleStatus(ConnectionTask.ConnectionTaskLifecycleStatus.ACTIVE).add();
         } catch (Exception e) {
             try {
@@ -131,7 +144,7 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
 
     private void addScheduledConnectionTaskToDevice(Device device, PartialOutboundConnectionTask partialConnectionTask, ConnectionAttributesImportRecord data, FileImportLogger logger) throws ProcessorException {
         Device.ScheduledConnectionTaskBuilder scheduledConnectionTaskBuilder = device.getScheduledConnectionTaskBuilder(partialConnectionTask);
-        data.getConnectionAttributes().forEach((key, value) -> scheduledConnectionTaskBuilder.setProperty(key, parseStringToValue(partialConnectionTask.getConnectionType().getPropertySpec(key), value, data)));
+        setConnectionAttributes(data, partialConnectionTask.getConnectionType(), scheduledConnectionTaskBuilder::setProperty);
         try {
             scheduledConnectionTaskBuilder.setConnectionTaskLifecycleStatus(ConnectionTask.ConnectionTaskLifecycleStatus.ACTIVE).add();
         } catch (Exception e) {
@@ -148,5 +161,14 @@ public class ConnectionAttributesImportProcessor implements FileImportProcessor<
 
     private String buildConstraintViolationCause(ConstraintViolationException exception) {
         return exception.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining("; "));
+    }
+
+    private void setConnectionAttributes(ConnectionAttributesImportRecord data, ConnectionType connectionType, BiConsumer<String, Object> setter) {
+        connectionType.getPropertySpecs().stream()
+                .filter(propertySpec -> data.getConnectionAttributes().containsKey(propertySpec.getName()))
+                .forEach(propertySpec -> {
+                    String name = propertySpec.getName();
+                    setter.accept(name, parseStringToValue(propertySpec, data.getConnectionAttributes().get(name), data));
+                });
     }
 }
