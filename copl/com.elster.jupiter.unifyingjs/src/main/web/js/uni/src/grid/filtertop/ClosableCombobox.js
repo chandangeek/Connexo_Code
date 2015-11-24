0ff99ebf22cb4289ea0f98c1c80371cb0969d090
akuryuk <akuryuk@mera.ru>
2015-11-24 16:08:38 +0300

    [COPL-32] Reading types management
    Squashed commit of the following:
    
    commit 1d8448279509faf04f03cb006bf1bd9ba9b4b4dc
    Merge: efe78c8 0ffa342
    Author: akuryuk <akuryuk@mera.ru>
    Date:   Tue Nov 24 15:57:11 2015 +0300
    
        Merge branch 'master' into COPL-1332
    
        Conflicts:
        	src/main/web/js/uni/src/grid/FilterPanelTop.js
    
    commit efe78c89f794bfec5df3d20c0f7bfdfd5aeedbb9
    Merge: 0cf0331 01c1669
    Author: akuryuk <akuryuk@mera.ru>
    Date:   Sat Oct 24 15:05:47 2015 +0300
    
        Merge branch 'master' into COPL-1332
    
    commit 0cf0331f1040e6a636cd89ed64990ba58ff62b13
    Author: akuryuk <akuryuk@mera.ru>
    Date:   Sat Oct 24 15:05:15 2015 +0300
    
        [COPL-1332] Pre-final version
    
    commit 451b7f33e5e35ef6427c243457ae8db6849074f0
    Author: akuryuk <akuryuk@mera.ru>
    Date:   Wed Oct 21 19:19:33 2015 +0300
    
        [COPL-1332] ClosableCombobox + FilterPanelTop improvement

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/grid/filtertop/ClosableCombobox.js
@@ -0,0 +1,86 @@
+Ext.define('Uni.grid.filtertop.ClosableCombobox', {
+//    extend: 'Uni.grid.filtertop.ComboBox',
+    extend: 'Ext.container.Container',
+    xtype: 'uni-closable-combobox',
+
+    required: [
+        'Uni.grid.filtertop.ComboBox'
+    ],
+    mixins: [
+        'Uni.grid.filtertop.Base'
+    ],
+
+    layout: {
+        type: 'hbox'
+    },
+    initComponent: function () {
+        var me = this;
+        me.store = me.store ? Ext.getStore(me.store) || Ext.create(me.store) : Ext.create('Ext.data.Store');
+        me.combobox = Ext.create('Uni.grid.filtertop.ComboBox',{
+            store: me.store,
+            emptyText: me.emptyText,
+            queryMode: me.queryMode,
+            valueField: me.valueField,
+            displayField: me.displayField,
+            forceSelection: me.forceSelection,
+            multiSelect: me.multiSelect,
+            listeners: {
+                filterupdate: function() {
+                    me.fireFilterUpdateEvent()
+                }
+            }
+        });
+        me.items = [
+            me.combobox,
+            {
+                xtype: 'button',
+                margin: '0 0 0 2',
+                iconCls: ' icon-close2',
+                action: 'remove',
+                listeners: {
+                    click: function () {
+                        me.removeHandler.apply(me, arguments)
+                    }
+                }
+            }
+        ];
+        me.callParent(arguments);
+    },
+
+    fireFilterUpdateEvent: function () {
+        this.fireEvent('filterupdate');
+    },
+
+    setFilterValue: function (data) {
+        return this.combobox.setFilterValue(data)
+    },
+
+    getFilterValue: function () {
+        if (this.combobox.getValue) {
+            return this.combobox.getValue();
+        }
+    },
+
+    resetValue: function () {
+        if (this.combobox.reset) {
+            this.combobox.reset();
+        }
+    },
+
+    isArrayOfNumerics: function (array) {
+        return this.combobox.isArrayOfNumerics(array)
+    },
+
+    getParamValue: function () {
+        return this.combobox.getParamValue()
+    },
+
+    createStoreFromOptions: function () {
+        return this.combobox.createStoreFromOptions()
+    },
+
+    getValueForDisplayValue: function (displayValue) {
+        return this.combobox.getValueForDisplayValue(displayValue)
+    }
+});
+
