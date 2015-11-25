6e45e285dc4b79e89285b57953b3446e2dc7101c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-11-25 15:35:40 +0100

    Cleanup leftovers of connection and security properties

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/sqlbuilder/ConnectionTypePropertyJoinType.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/sqlbuilder/ConnectionTypePropertyJoinType.java
@@ -4,7 +4,6 @@ import com.elster.jupiter.cps.CustomPropertySet;
 import com.elster.jupiter.cps.PersistenceSupport;
 import com.elster.jupiter.util.sql.SqlBuilder;
 import com.energyict.mdc.protocol.pluggable.ConnectionTypePluggableClass;
-import com.energyict.mdc.protocol.pluggable.ConnectionTypePropertyRelationAttributeTypeNames;
 
 public class ConnectionTypePropertyJoinType implements JoinType {
     private final ConnectionTypePluggableClass pluggableClass;
@@ -19,7 +18,7 @@ public class ConnectionTypePropertyJoinType implements JoinType {
         sqlBuilder.append(" join ");
         sqlBuilder.append(this.getDynamicAttributeTableName());
         sqlBuilder.append(" props on props.");
-        sqlBuilder.append(ConnectionTypePropertyRelationAttributeTypeNames.CONNECTION_TASK_ATTRIBUTE_NAME);
+        sqlBuilder.append(this.getConnectionTaskColumnName());
         sqlBuilder.append(" = ct.id AND props.TODATE is NULL");
     }
 
@@ -29,6 +28,15 @@ public class ConnectionTypePropertyJoinType implements JoinType {
                 .getCustomPropertySet()
                 .map(CustomPropertySet::getPersistenceSupport)
                 .map(PersistenceSupport::tableName)
+                .orElseThrow(() -> new IllegalStateException("Cannot search for connection task properties because connection type does not have properties"));
+    }
+
+    protected String getConnectionTaskColumnName() {
+        return this.pluggableClass
+                .getConnectionType()
+                .getCustomPropertySet()
+                .map(CustomPropertySet::getPersistenceSupport)
+                .map(PersistenceSupport::domainColumnName)
                 .orElseThrow(() -> new IllegalStateException("Cannot search for connection task properties because connection type does not have properties"));
     }
 
