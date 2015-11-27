80fec7aa1f35b462ac1cfb95040790cc61e3d332
khe <kristof.hennebel@elster.com>
2015-11-27 17:05:58 +0100

    EISERVERSG-4183 - Prioritize and queue custom actions that follow on certain inbound notifications
     - PSK providing is no longer done in the inbound protocol itself. Instead, the inbound protocol instances (running on several concurrent threads) will delegate the task to a singleton PSK providing instance that knows the state and the active association to the beacon(s)

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/mdc/protocol/inbound/g3/PushEventNotification.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/mdc/protocol/inbound/g3/PushEventNotification.java
@@ -107,7 +107,7 @@ public class PushEventNotification implements BinaryInboundDeviceProtocol {
 
     @Override
     public String getVersion() {
-        return "$Date: 2015-11-27 17:04:49 +0100 (Fri, 27 Nov 2015)$";
+        return "$Date: 2015-11-27 17:05:58 +0100 (Fri, 27 Nov 2015)$";
     }
 
     @Override
