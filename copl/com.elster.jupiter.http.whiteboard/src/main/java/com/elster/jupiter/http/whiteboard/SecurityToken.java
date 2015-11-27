947dcae6eb436c63cdfebc1daf970b3646c054eb
Dragos Iancu <dragos.iancu@elster.com>
2015-11-27 11:36:43 +0200

    COPL - 805 - HMAC replaced with RSA for token creation and verification

--- copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/SecurityToken.java
+++ copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/SecurityToken.java
@@ -5,8 +5,8 @@ import com.elster.jupiter.users.Group;
 import com.elster.jupiter.users.User;
 import com.elster.jupiter.users.UserService;
 import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 
@@ -14,12 +14,22 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
 import java.text.ParseException;
 import java.util.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 
 public class SecurityToken {
 
-    private static final String SHARED_SECRET = "{my greatest secret tokenaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa}";
     private final  static long MAX_COUNT = WhiteBoard.getTokenRefreshMaxCnt();
     private final static int TIMEOUT = WhiteBoard.getTimeout();
     private final static int TOKEN_EXPTIME = WhiteBoard.getTokenExpTime();
@@ -29,51 +39,66 @@ public class SecurityToken {
     }
 
     public static String createToken(User user) {
-
-        // Create HMAC signer
-        JWSSigner signer = new MACSigner(SHARED_SECRET.getBytes());
-
-        List<Group> userGroups = user.getGroups();
-        List<RoleClaimInfo> roles = new ArrayList<>();
-
-        for (Group group : userGroups){
-            roles.add(new RoleClaimInfo(group.getId(),group.getName()));
-        }
-        
-        // Prepare JWT with claims set
-        JWTClaimsSet claimsSet = new JWTClaimsSet();
-        claimsSet.setCustomClaim("username",user.getName());
-        claimsSet.setSubject(Long.toString(user.getId()));
-        claimsSet.setCustomClaim("roles",roles);
-        claimsSet.setIssuer("Elster Connexo");
-        claimsSet.setJWTID("token" + (++COUNT));
-        claimsSet.setIssueTime(new Date());
-        claimsSet.setExpirationTime(new Date(System.currentTimeMillis() + TOKEN_EXPTIME*1000));
-        claimsSet.setCustomClaim("cnt", COUNT);
-
-        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
-
-        // Apply the HMAC protection
         try {
+            KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+            keyGenerator.initialize(1024);
+            KeyPair keyPair = keyGenerator.genKeyPair();
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+            JWSSigner signer = new RSASSASigner(privateKey);
+
+            List<Group> userGroups = user.getGroups();
+            List<RoleClaimInfo> roles = new ArrayList<>();
+
+            for (Group group : userGroups) {
+                roles.add(new RoleClaimInfo(group.getId(), group.getName()));
+            }
+
+           String pk =  publicKey.getModulus().toString() + " " +
+                    publicKey.getPublicExponent().toString();
+            JWTClaimsSet claimsSet = new JWTClaimsSet();
+            claimsSet.setCustomClaim("username", user.getName());
+            claimsSet.setSubject(Long.toString(user.getId()));
+            claimsSet.setCustomClaim("roles", roles);
+            claimsSet.setIssuer("Elster Connexo");
+            claimsSet.setJWTID("token" + (++COUNT));
+            claimsSet.setIssueTime(new Date());
+            claimsSet.setExpirationTime(new Date(System.currentTimeMillis() + TOKEN_EXPTIME * 1000));
+            claimsSet.setCustomClaim("cnt", COUNT);
+            claimsSet.setCustomClaim("publicKey", pk);
+
+            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.RS256), claimsSet);
+
             signedJWT.sign(signer);
+            return signedJWT.serialize();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
         } catch (JOSEException e) {
             e.printStackTrace();
             return null;
         }
-        // Serialize to compact form, produces something like
-        return signedJWT.serialize();
-    }
 
+    }
 
     public static Optional<User> verifyToken(String token, HttpServletRequest request, HttpServletResponse response, UserService userService) {
         try {
+
             SignedJWT signedJWT = SignedJWT.parse(token);
-            JWSVerifier verifier = new MACVerifier(SHARED_SECRET);
+            String publicKey = String.valueOf(signedJWT.getJWTClaimsSet().getCustomClaim("publicKey"));
             String issuer = signedJWT.getJWTClaimsSet().getIssuer();
             Date issueTime = signedJWT.getJWTClaimsSet().getIssueTime();
             Date expirationTime = signedJWT.getJWTClaimsSet().getExpirationTime();
             long count = (Long)signedJWT.getJWTClaimsSet().getCustomClaim("cnt");
             long tokenNumericTermination = Long.parseLong(signedJWT.getJWTClaimsSet().getJWTID().split("[a-z]")[5]);
+            BigInteger modulus = new BigInteger(publicKey.split(" ")[0]);
+            BigInteger exp = new BigInteger(publicKey.split(" ")[1]);
+            RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus,exp);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            RSAPublicKey pubKey = (RSAPublicKey) keyFactory.generatePublic(keySpec);
+
+            JWSVerifier verifier = new RSASSAVerifier(pubKey);
 
             if (signedJWT.verify(verifier) && issuer.equals("Elster Connexo") &&
                     issueTime.before(expirationTime) && count == tokenNumericTermination) {
@@ -95,6 +120,10 @@ public class SecurityToken {
         } catch (ParseException e) {
             e.printStackTrace();
         } catch (JOSEException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
             e.printStackTrace();
         }
 
