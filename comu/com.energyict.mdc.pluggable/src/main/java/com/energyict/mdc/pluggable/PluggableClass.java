f1fe01834359b1e71312fa0547743d77f9328bb2
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-11-30 16:12:09 +0100

    RelationService is a gonner!

--- comu/com.energyict.mdc.pluggable/src/main/java/com/energyict/mdc/pluggable/PluggableClass.java
+++ comu/com.energyict.mdc.pluggable/src/main/java/com/energyict/mdc/pluggable/PluggableClass.java
@@ -1,9 +1,9 @@
 package com.energyict.mdc.pluggable;
 
-import com.elster.jupiter.util.HasId;
-import com.energyict.mdc.common.TypedProperties;
 import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.util.HasId;
 import com.elster.jupiter.util.HasName;
+import com.energyict.mdc.common.TypedProperties;
 
 import java.time.Instant;
 import java.util.List;
@@ -16,35 +16,35 @@ import java.util.List;
  */
 public interface PluggableClass extends HasId, HasName {
 
-    public void setName(String name);
+    void setName(String name);
 
     /**
      * Returns the type of Pluggable that is implemented by the java class.
      *
      * @return The PluggableClassType
      */
-    public PluggableClassType getPluggableClassType ();
+    PluggableClassType getPluggableClassType();
 
     /**
      * Returns the name of the java implementation class.
      *
      * @return the java class name
      */
-    public String getJavaClassName ();
+    String getJavaClassName();
 
     /**
      * Gets the Date on which this PluggableClass was created or last modified.
      *
      * @return The Date on which this PluggableClass was created or last modified
      */
-    public Instant getModificationDate();
+    Instant getModificationDate();
 
     /**
      * Returns the dynamic properties of this PluggableClass.
      *
      * @return The TypedProperties
      */
-    public TypedProperties getProperties(List<PropertySpec> propertySpecs);
+    TypedProperties getProperties(List<PropertySpec> propertySpecs);
 
     /**
      * Sets the value of a single property of this PluggableClass.
@@ -52,22 +52,22 @@ public interface PluggableClass extends HasId, HasName {
      * @param propertySpec The PropertySpec
      * @param value The value of the property
      */
-    public void setProperty(PropertySpec propertySpec, Object value);
+    void setProperty(PropertySpec propertySpec, Object value);
 
     /**
      * Removes the value of a single property of this PluggableClass.
      *
      * @param propertySpec The PropertySpec
      */
-    public void removeProperty(PropertySpec propertySpec);
+    void removeProperty(PropertySpec propertySpec);
 
-    public void save();
+    void save();
 
     /**
      * Deletes this PluggableClass.
      *
      */
-    public void delete();
+    void delete();
 
     long getEntityVersion();
 }
