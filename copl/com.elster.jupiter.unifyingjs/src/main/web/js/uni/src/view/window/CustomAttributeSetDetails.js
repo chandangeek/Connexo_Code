557af4f91e17271959c1737d263772c80048e251
Zheltukhin Alexey <azhelt@mera.ru>
2015-12-01 11:38:01 +0300

    COMU-2726 Administrate CAS pop up has wrong (or no) design

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/window/CustomAttributeSetDetails.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/window/CustomAttributeSetDetails.js
@@ -1,29 +1,23 @@
 Ext.define('Uni.view.window.CustomAttributeSetDetails', {
-    extend: 'Ext.window.Window',
+    extend: 'Uni.view.window.Notification',
     xtype: 'custom-attribute-set-details',
-    closable: true,
-    width: 600,
-    constrain: true,
-    autoShow: true,
-    modal: true,
-    layout: 'fit',
-    closeAction: 'destroy',
-    floating: true,
     record: null,
 
     requires: [
+        'Uni.view.window.Notification',
         'Uni.view.form.CustomAttributeSetDetails'
     ],
 
     items: {
-        xtype: 'custom-attribute-set-details-form'
+        xtype: 'custom-attribute-set-details-form',
+        showDefaultTitle: false
     },
 
     initComponent: function () {
         var me = this;
 
         me.callParent(arguments);
-        me.down('custom-attribute-set-details-form').setTitle(Uni.I18n.translate('customattributeset.customTitle', 'UNI', '\'{0}\' attribute set details', [encodeURIComponent(me.record.name)]));
+        me.setFormTitle(Uni.I18n.translate('customattributeset.customTitle', 'UNI', '\'{0}\' attribute set details', [encodeURIComponent(me.record.name)]));
         me.down('custom-attribute-set-details-form').loadCustomRecord(me.record);
     }
 });
