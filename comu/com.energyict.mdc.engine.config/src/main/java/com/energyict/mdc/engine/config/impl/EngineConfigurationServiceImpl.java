e3a6d8ac55db0910cc5a74786bf0a6e6b6a84908
Pasquien Dorny <pasquien.dorny@elster.com>
2015-12-02 14:36:32 +0100

    COMU-2481: Active flag of ComPortPool is not used

--- comu/com.energyict.mdc.engine.config/src/main/java/com/energyict/mdc/engine/config/impl/EngineConfigurationServiceImpl.java
+++ comu/com.energyict.mdc.engine.config/src/main/java/com/energyict/mdc/engine/config/impl/EngineConfigurationServiceImpl.java
@@ -58,7 +58,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.elster.jupiter.util.conditions.Where.where;
