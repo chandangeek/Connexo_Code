01184cf00739f43d52150c86407e4c8bf7cd3037
Tom De Greyt <Tom.DeGreyt@elster.com>
2015-12-03 15:27:10 +0100

    COPL-1166 - Model changes for device multiplier

--- copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/FakeBuilder.java
+++ copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/FakeBuilder.java
@@ -1,11 +1,10 @@
 package com.elster.jupiter.devtools.tests;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import org.mockito.Mockito;
+import org.mockito.internal.stubbing.defaultanswers.ReturnsEmptyValues;
+import org.mockito.invocation.InvocationOnMock;
+
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 
 public class FakeBuilder {
 
@@ -21,19 +20,17 @@ public class FakeBuilder {
         final Class<?>[] allInterfaces = new Class<?>[subInterfaces.length + 1];
         allInterfaces[0] = builderInterface;
         System.arraycopy(subInterfaces, 0, allInterfaces, 1, subInterfaces.length);
-        final AtomicReference<T> proxyInstance = new AtomicReference<>();
-        proxyInstance.set((T) Proxy.newProxyInstance(builderInterface.getClassLoader(), allInterfaces, new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                if (Arrays.stream(allInterfaces).anyMatch(aClass -> aClass.isAssignableFrom(method.getReturnType()))) {
-                    return proxyInstance.get();
-                }
-
-                return buildResultGetter.get();
-            }
-            private Supplier<Object> buildResultGetter = () -> build;
-        }));
-        return proxyInstance.get();
+        return Mockito.mock(builderInterface, Mockito.withSettings().defaultAnswer(
+                new ReturnsEmptyValues() {
+                    @Override
+                    public Object answer(InvocationOnMock invocation) {
+                        if (Arrays.stream(allInterfaces).anyMatch(aClass -> aClass.isAssignableFrom(invocation.getMethod().getReturnType()))) {
+                            return invocation.getMock();
+                        } else {
+                            return build;
+                        }
+                    }
+                }));
     }
 
 }
