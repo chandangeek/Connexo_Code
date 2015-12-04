954d9cfc064ef48a73bf105bded57646517e02ad
Maksim Barinov <mbarinov@mera.ru>
2015-12-04 11:52:11 +0300

    Quick fix for COMU-2657

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicechannels/DataGrid.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicechannels/DataGrid.js
@@ -132,12 +132,14 @@ Ext.define('Mdc.view.setup.devicechannels.DataGrid', {
                         xtype: 'button',
                         itemId: 'save-changes-button',
                         text: Uni.I18n.translate('general.saveChanges', 'MDC', 'Save changes'),
+                        privileges: Mdc.privileges.Device.administrateDeviceData,
                         disabled: true
                     },
                     {
                         xtype: 'button',
                         itemId: 'undo-button',
                         text: Uni.I18n.translate('general.undo', 'MDC', 'Undo'),
+                        privileges: Mdc.privileges.Device.administrateDeviceData,
                         disabled: true
                     },
                     {
