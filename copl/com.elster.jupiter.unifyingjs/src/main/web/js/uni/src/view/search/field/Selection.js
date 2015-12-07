ea139bc361da281f07df3d7f163d7fa6532c568e
Artem Puchenkin <apuchenkin@gmail.com>
2015-12-07 15:04:35 +0300

    [COMU-2815] [Search] Problem with the option "Select all displayed values"

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/Selection.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/Selection.js
@@ -327,6 +327,8 @@ Ext.define('Uni.view.search.field.Selection', {
         model.deselectAll(true);
         model.select(me.getStoreRecords(), true, true);
         model.updateHeaderState();
+
+        me.down('#select-all').setDisabled(!me.getStore().count());
         me.down('#filter-selected').setValue(
                 me.store.getCount() === model.getCount()
             &&  me.store.getCount() === me.selection.getCount()
