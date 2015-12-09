9433554aea62fa8723f2d568a49e793819c59459
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-12-09 21:58:30 +0100

    Attempt to move all value factory implementation classes to private packages for future extensibility

--- comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/impl/properties/SimplePropertyType.java
+++ comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/impl/properties/SimplePropertyType.java
@@ -1,32 +1,30 @@
 package com.energyict.mdc.pluggable.rest.impl.properties;
 
 import com.elster.jupiter.metering.ReadingType;
-import com.elster.jupiter.properties.BigDecimalFactory;
-import com.elster.jupiter.properties.BooleanFactory;
-import com.elster.jupiter.properties.InstantFactory;
 import com.elster.jupiter.properties.StringFactory;
-import com.elster.jupiter.properties.ThreeStateFactory;
 import com.elster.jupiter.properties.ValueFactory;
 import com.elster.jupiter.rest.util.properties.PropertyType;
+import com.elster.jupiter.time.TimeDuration;
+import com.energyict.mdc.common.HexString;
+import com.energyict.mdc.common.ObisCode;
+import com.energyict.mdc.common.Password;
+import com.energyict.mdc.common.TimeOfDay;
+import com.energyict.mdc.common.ean.Ean13;
+import com.energyict.mdc.common.ean.Ean18;
 import com.energyict.mdc.device.data.LoadProfile;
 import com.energyict.mdc.device.data.LogBook;
 import com.energyict.mdc.device.data.Register;
-import com.energyict.mdc.dynamic.DateAndTimeFactory;
-import com.energyict.mdc.dynamic.DateFactory;
-import com.energyict.mdc.dynamic.Ean13Factory;
-import com.energyict.mdc.dynamic.Ean18Factory;
-import com.energyict.mdc.dynamic.EncryptedStringFactory;
-import com.energyict.mdc.dynamic.HexStringFactory;
 import com.energyict.mdc.dynamic.LargeStringFactory;
-import com.energyict.mdc.dynamic.ObisCodeValueFactory;
-import com.energyict.mdc.dynamic.PasswordFactory;
-import com.energyict.mdc.dynamic.TimeDurationValueFactory;
-import com.energyict.mdc.dynamic.TimeOfDayFactory;
 import com.energyict.mdc.firmware.FirmwareVersion;
 import com.energyict.mdc.masterdata.LoadProfileType;
 import com.energyict.mdc.protocol.api.UserFile;
 import com.energyict.mdc.protocol.api.codetables.Code;
 import com.energyict.mdc.protocol.api.timezones.TimeZoneInUse;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Date;
 
 /**
  * Represents simple types which a property can have
@@ -37,23 +35,21 @@ import com.energyict.mdc.protocol.api.timezones.TimeZoneInUse;
  */
 public enum SimplePropertyType implements PropertyType {
     UNKNOWN(Void.class),
-    PASSWORD(PasswordFactory.class),
-    HEXSTRING(HexStringFactory.class),
-    NUMBER(BigDecimalFactory.class),
-    NULLABLE_BOOLEAN(ThreeStateFactory.class),
-    BOOLEAN(BooleanFactory.class),
-    TIMEDURATION(TimeDurationValueFactory.class),
-    TIMEOFDAY(TimeOfDayFactory.class),
-    CLOCK(DateAndTimeFactory.class),
+    PASSWORD(Password.class),
+    HEXSTRING(HexString.class),
+    NUMBER(BigDecimal.class),
+    BOOLEAN(Boolean.class),
+    TIMEDURATION(TimeDuration.class),
+    TIMEOFDAY(TimeOfDay.class),
+    CLOCK(Clock.class),
     CODETABLE(Code.class, true),
     TIMEZONEINUSE(TimeZoneInUse.class, true),
     USERFILEREFERENCE(UserFile.class, true),
-    EAN13(Ean13Factory.class),
-    EAN18(Ean18Factory.class),
-    DATE(DateFactory.class),
+    EAN13(Ean13.class),
+    EAN18(Ean18.class),
+    DATE(Date.class),
     TEXTAREA(LargeStringFactory.class),
-    ENCRYPTED_STRING(EncryptedStringFactory.class),
-    OBISCODE(ObisCodeValueFactory.class),
+    OBISCODE(ObisCode.class),
     READINGTYPE(ReadingType.class),
     LOADPROFILETYPE(LoadProfileType.class, true),
     LOADPROFILE(LoadProfile.class, true),
@@ -61,19 +57,19 @@ public enum SimplePropertyType implements PropertyType {
     REGISTER(Register.class, true),
     TEXT(StringFactory.class),
     FIRMWAREVERSION(FirmwareVersion.class, true),
-    TIMESTAMP(InstantFactory.class),
+    TIMESTAMP(Instant.class),
     ;
 
-    private Class domainClass;
+    private Class discriminatorClass;
     private boolean isReference;
 
     SimplePropertyType(Class valueFactoryClass) {
         this(valueFactoryClass, false);
     }
 
-    SimplePropertyType(Class domainClass, boolean reference) {
+    SimplePropertyType(Class discriminatorClass, boolean reference) {
         this.isReference = reference;
-        this.domainClass = domainClass;
+        this.discriminatorClass = discriminatorClass;
     }
 
     public boolean isReference() {
@@ -81,7 +77,12 @@ public enum SimplePropertyType implements PropertyType {
     }
 
     private boolean matches (ValueFactory valueFactory) {
-        return this.domainClass.isAssignableFrom(valueFactory.getValueType());
+        if (ValueFactory.class.isAssignableFrom(this.discriminatorClass)) {
+            return this.discriminatorClass.isAssignableFrom(valueFactory.getClass());
+        }
+        else {
+            return this.discriminatorClass.isAssignableFrom(valueFactory.getValueType());
+        }
     }
 
     public static SimplePropertyType getTypeFrom(ValueFactory valueFactory) {
