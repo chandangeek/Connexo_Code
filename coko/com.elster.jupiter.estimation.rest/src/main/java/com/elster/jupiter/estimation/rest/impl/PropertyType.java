927a85e53b2f4d28e0c114c6d1abade5f50581f0
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-12-10 20:39:09 +0100

    Cleanup of old methods that have been redirected to new API approach

--- coko/com.elster.jupiter.estimation.rest/src/main/java/com/elster/jupiter/estimation/rest/impl/PropertyType.java
+++ coko/com.elster.jupiter.estimation.rest/src/main/java/com/elster/jupiter/estimation/rest/impl/PropertyType.java
@@ -6,10 +6,10 @@ import com.elster.jupiter.properties.BigDecimalFactory;
 import com.elster.jupiter.properties.BooleanFactory;
 import com.elster.jupiter.properties.ListValueFactory;
 import com.elster.jupiter.properties.LongFactory;
-import com.elster.jupiter.properties.RelativePeriodFactory;
 import com.elster.jupiter.properties.StringFactory;
 import com.elster.jupiter.properties.ThreeStateFactory;
 import com.elster.jupiter.properties.ValueFactory;
+import com.elster.jupiter.properties.impl.RelativePeriodFactory;
 
 public enum PropertyType implements com.elster.jupiter.rest.util.properties.PropertyType {
     UNKNOWN(Void.class),
