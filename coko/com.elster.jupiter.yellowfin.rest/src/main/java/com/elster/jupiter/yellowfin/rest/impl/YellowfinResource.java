2f9fa0619bcfd157fd31016cf33298332895616d
Dragos Sinca <dragos.sinca@elster.com>
2015-12-10 11:02:55 +0200

    Add support for retrieving url without logging in

--- coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinResource.java
+++ coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/YellowfinResource.java
@@ -8,10 +8,7 @@ import com.elster.jupiter.yellowfin.security.Privileges;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,6 +25,17 @@ public class YellowfinResource {
 	private YellowfinResource(YellowfinService yellowfinService, Thesaurus thesaurus){
 		this.yellowfinService = yellowfinService;
 		this.errorMessage = thesaurus.getString("error.facts.unavailable", "Connexo Facts is not available.");
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+	@Path("/url")
+	@RolesAllowed({Privileges.Constants.VIEW_REPORTS,Privileges.Constants.DESIGN_REPORTS})
+	public YellowfinInfo getUrl() {
+		YellowfinInfo info = new YellowfinInfo();
+		info.token = "";
+		info.url = yellowfinService.getYellowfinUrl();
+		return info;
 	}
 
 
