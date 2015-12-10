24e4a5aab306a71126c9683d4a459ceff2f5da7b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-12-10 09:49:49 +0100

    Fix unit test failures

--- copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/FakeBuilder.java
+++ copl/com.elster.jupiter.devtools/src/main/java/com/elster/jupiter/devtools/tests/FakeBuilder.java
@@ -1,5 +1,6 @@
 package com.elster.jupiter.devtools.tests;
 
+import org.mockito.MockSettings;
 import org.mockito.Mockito;
 import org.mockito.internal.stubbing.defaultanswers.ReturnsEmptyValues;
 import org.mockito.invocation.InvocationOnMock;
@@ -12,7 +13,11 @@ public class FakeBuilder {
         final Class<?>[] allInterfaces = new Class<?>[subInterfaces.length + 1];
         allInterfaces[0] = builderInterface;
         System.arraycopy(subInterfaces, 0, allInterfaces, 1, subInterfaces.length);
-        return Mockito.mock(builderInterface, Mockito.withSettings().defaultAnswer(
+        MockSettings mockSettings = Mockito.withSettings();
+        if (subInterfaces.length > 0) {
+            mockSettings.extraInterfaces(subInterfaces);
+        }
+        mockSettings.defaultAnswer(
                 new ReturnsEmptyValues() {
                     @Override
                     public Object answer(InvocationOnMock invocation) {
@@ -22,7 +27,8 @@ public class FakeBuilder {
                             return build;
                         }
                     }
-                }));
+                });
+        return Mockito.mock(builderInterface, mockSettings);
     }
 
 }
