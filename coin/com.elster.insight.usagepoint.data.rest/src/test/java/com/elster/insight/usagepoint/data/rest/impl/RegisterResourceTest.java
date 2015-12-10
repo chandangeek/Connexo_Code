8d502b97e0fca5b63a309a08c3f2a371a18687c0
Jeff Smith <Jeffrey.Smith@elster.com>
2015-12-10 14:59:57 -0500

    Add tests for register resource

--- coin/com.elster.insight.usagepoint.data.rest/src/test/java/com/elster/insight/usagepoint/data/rest/impl/RegisterResourceTest.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/test/java/com/elster/insight/usagepoint/data/rest/impl/RegisterResourceTest.java
@@ -4,25 +4,32 @@ package com.elster.insight.usagepoint.data.rest.impl;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doReturn;
 
-
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Currency;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import com.elster.insight.common.rest.IntervalInfo;
 import com.elster.insight.usagepoint.data.UsagePointValidation;
 import com.elster.jupiter.cbo.Accumulation;
 import com.elster.jupiter.cbo.Aggregate;
@@ -35,15 +42,20 @@ import com.elster.jupiter.cbo.Phase;
 import com.elster.jupiter.cbo.RationalNumber;
 import com.elster.jupiter.cbo.ReadingTypeUnit;
 import com.elster.jupiter.cbo.TimeAttribute;
+import com.elster.jupiter.devtools.ExtjsFilter;
 import com.elster.jupiter.metering.BaseReadingRecord;
 import com.elster.jupiter.metering.Channel;
 import com.elster.jupiter.metering.Meter;
 import com.elster.jupiter.metering.MeterActivation;
+import com.elster.jupiter.metering.ReadingQualityRecord;
+import com.elster.jupiter.metering.ReadingQualityType;
 import com.elster.jupiter.metering.ReadingRecord;
 import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.metering.UsagePoint;
 import com.elster.jupiter.util.Ranges;
+import com.elster.jupiter.validation.DataValidationStatus;
 import com.elster.jupiter.validation.ValidationEvaluator;
+import com.elster.jupiter.validation.ValidationResult;
 import com.google.common.collect.Range;
 import com.jayway.jsonpath.JsonModel;
 
@@ -52,6 +64,7 @@ public class RegisterResourceTest extends UsagePointDataRestApplicationJerseyTes
     public static final String REGISTER_MRID1 = "0.0.0.1.1.1.12.0.0.0.0.0.0.0.0.3.72.0";
 
     public static final Instant LAST_READING = Instant.ofEpochMilli(1410786196000L);
+    public static final Date LAST_CHECKED = new Date(1409570229000L);
     private static long intervalStart = 1410774630000L;
     private static long intervalEnd = 1410828630000L;
 
@@ -93,6 +106,7 @@ public class RegisterResourceTest extends UsagePointDataRestApplicationJerseyTes
 
         Range<Instant> interval = Ranges.openClosed(Instant.ofEpochMilli(intervalStart), Instant.ofEpochMilli(intervalEnd));
         when(channel.getReadings(interval)).thenReturn(asList(readingRecord, readingRecord2, readingRecord3, readingRecord4));
+        when(channel.getReading(Instant.ofEpochMilli(intervalEnd))).thenReturn(Optional.of(readingRecord));
         List myList = asList(readingRecord, readingRecord2, readingRecord3, readingRecord4);
         when(usagePoint.getReadings(interval, readingType)).thenReturn(myList);
 
@@ -109,13 +123,16 @@ public class RegisterResourceTest extends UsagePointDataRestApplicationJerseyTes
         when(readingRecord4.getTimeStamp()).thenReturn(LAST_READING);
         when(readingRecord4.getReadingType()).thenReturn(readingType);
         
+        when(channel.getMeterActivation()).thenReturn(meterActivation);
+        doReturn(Arrays.asList(meterActivation)).when(meter).getMeterActivations();
+        
         when(usagePoint.getMeter(any())).thenReturn(Optional.of(meter));
         when(clock.instant()).thenReturn(Instant.now());
         when(channel.getMeterActivation()).thenReturn(meterActivation);
         Range<Instant> intervalToNow = Ranges.openClosed(Instant.ofEpochMilli(intervalStart), Instant.now());
         when(meterActivation.getRange()).thenReturn(intervalToNow);
 //        when(evaluator.getValidationStatus(channel, myList, channel.getMeterActivation().getRange().intersection(interval))).thenReturn(new ArrayList());
-        when(evaluator.getValidationStatus(eq(channel), any(), any(Range.class))).thenReturn(new ArrayList());
+//        when(evaluator.getValidationStatus(eq(channel), any(), any(Range.class))).thenReturn(new ArrayList());
         when(validationService.getEvaluator()).thenReturn(evaluator);
         when(validationService.getEvaluator(eq(meter), any(Range.class))).thenReturn(evaluator);
         when(evaluator.isValidationEnabled(channel)).thenReturn(false);
@@ -125,7 +142,7 @@ public class RegisterResourceTest extends UsagePointDataRestApplicationJerseyTes
     }
 
     @Test
-    public void testChannelData() {
+    public void testRegisterData() {
         String filter = URLEncoder.encode("[{\"property\":\"intervalStart\",\"value\":1410774630000},{\"property\":\"intervalEnd\",\"value\":1410828630000}]");
 
         String json = target("usagepoints/1/registers/" + REGISTER_MRID1 + "/data")
@@ -155,5 +172,96 @@ public class RegisterResourceTest extends UsagePointDataRestApplicationJerseyTes
         assertThat(jsonModel.<String> get("$.readingType.mRID")).isEqualTo(REGISTER_MRID1);
         assertThat(jsonModel.<Number> get("$.lastValueTimestamp")).isEqualTo(LAST_READING.toEpochMilli());
     }
+    
+    @Test
+    public void testPutRegisterData() {
+        
+        when(channel.getReading(Instant.ofEpochMilli(intervalEnd+900000))).thenReturn(Optional.of(lastReading));
+        
+        RegisterDataInfo registerDataInfo = new RegisterDataInfo();
+        registerDataInfo.value = BigDecimal.TEN;
+        registerDataInfo.interval = new IntervalInfo();
+        registerDataInfo.readingTime = Instant.ofEpochMilli(intervalEnd);
+
+        Response response = target("usagepoints/1/registers/" + REGISTER_MRID1 + "/data/" + intervalEnd).request().put(Entity.json(registerDataInfo));
+        verify(channel).editReadings(any());
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    public void testChannelDataFiltered() throws UnsupportedEncodingException {
+        when(evaluator.getValidationResult(any())).thenReturn(ValidationResult.VALID);
+//        when(deviceValidation.getValidationResult(any())).thenReturn(ValidationResult.VALID);
+
+        String filter = ExtjsFilter.filter().property("intervalStart", 1410774630000L).property("intervalEnd", 1410828630000L).property("suspect", "suspect").create();
+        String json = target("usagepoints/1/registers/" + REGISTER_MRID1 + "/data")
+                .queryParam("filter", filter)
+                .request().get(String.class);
+
+        JsonModel jsonModel = JsonModel.create(json);
+
+        assertThat(jsonModel.<List<?>>get("$.data")).isEmpty();
+    }
+
+    @Test
+    public void testChannelDataFilteredMatches() throws UnsupportedEncodingException {
+        when(evaluator.getValidationResult(any())).thenReturn(ValidationResult.SUSPECT);
+        ReadingQualityType readingQualitySuspect = new ReadingQualityType("3.5.258");
+        DataValidationStatus statusForSuspect = mockDataValidationStatus(readingQualitySuspect, false);
+        when(evaluator.getValidationStatus(eq(channel), any(), any())).thenReturn(Arrays.asList(statusForSuspect));       
+        String filter = ExtjsFilter.filter().property("intervalStart", 1410774630000L).property("intervalEnd", 1410828630000L).property("suspect","suspect").create();
+        String json = target("usagepoints/1/registers/" + REGISTER_MRID1 + "/data")
+                .queryParam("filter", filter)
+                .request().get(String.class);
+
+        JsonModel jsonModel = JsonModel.create(json);
+
+        assertThat(jsonModel.<List<?>>get("$.data")).hasSize(4);
+    }
+    
+    private DataValidationStatus mockDataValidationStatus(ReadingQualityType readingQualityType, boolean isBulk) {
+        DataValidationStatus status = mock(DataValidationStatus.class);
+        ReadingQualityRecord readingQualityRecord = mock(ReadingQualityRecord.class);
+        when(readingQualityRecord.getType()).thenReturn(readingQualityType);
+        List<? extends ReadingQualityRecord> readingQualities = Arrays.asList(readingQualityRecord);
+        if (isBulk) {
+            doReturn(readingQualities).when(status).getBulkReadingQualities();
+        } else {
+            doReturn(readingQualities).when(status).getReadingQualities();
+        }
+        return status;
+    }
+
+    @Test
+    public void testValidate() {
+        when(meteringService.findAndLockUsagePointByIdAndVersion(usagePoint.getId(), usagePoint.getVersion())).thenReturn(Optional.of(usagePoint));
+        RegisterTriggerValidationInfo info = new RegisterTriggerValidationInfo();
+        info.version = channel.getVersion();
+        info.id = REGISTER_MRID1;
+
+        Response response = target("usagepoints/1/registers/" + REGISTER_MRID1 + "/validate")
+                .request()
+                .put(Entity.json(info));
+
+        assertThat(response.getEntity()).isNotNull();
+        verify(validationService).validate(eq(meterActivation), any());
+    }
+//
+    @Test
+    public void testValidateWithDate() {
+        when(meteringService.findAndLockUsagePointByIdAndVersion(usagePoint.getId(), usagePoint.getVersion())).thenReturn(Optional.of(usagePoint));
+        UsagePointTriggerValidationInfo info = new UsagePointTriggerValidationInfo();
+        info.version = channel.getVersion();
+        info.id = REGISTER_MRID1;
+        info.lastChecked = LAST_CHECKED.getTime();
+        Response response = target("usagepoints/1/channels/" + REGISTER_MRID1 + "/validate")
+                .request()
+                .put(Entity.json(info));
+
+        assertThat(response.getEntity()).isNotNull();
+        verify(validationService).updateLastChecked(channel, LAST_CHECKED.toInstant());
+        verify(validationService).validate(eq(meterActivation), any());
+    }
+
 
 }
