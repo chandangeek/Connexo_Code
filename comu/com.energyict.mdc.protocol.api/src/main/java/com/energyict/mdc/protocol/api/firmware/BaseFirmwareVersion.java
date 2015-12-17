e837e236b1e24c5dd4b2de0e421b706d1367727b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2015-12-17 10:53:44 +0100

    Use BeanService to get type information for the primarykey and use Column to get value for the primary key

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/firmware/BaseFirmwareVersion.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/firmware/BaseFirmwareVersion.java
@@ -1,10 +1,12 @@
 package com.energyict.mdc.protocol.api.firmware;
 
+import com.elster.jupiter.util.HasId;
+
 /**
  * Insert your comments here.
  *
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2015-12-11 (11:00)
  */
-public interface BaseFirmwareVersion {
+public interface BaseFirmwareVersion extends HasId {
 }
