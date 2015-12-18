33ad0c04e825d5ae4b933bceaddff2919a59c900
David Nitescu <david.nitescu@elster.com>
2015-12-18 10:21:25 +0200

    COMU-2810 - 'question mark arrow' icon shown in stead of 'hand' in report wizard

--- coko/com.elster.jupiter.yellowfin.extjs/src/main/web/js/yfn/src/view/generatereport/Step2.js
+++ coko/com.elster.jupiter.yellowfin.extjs/src/main/web/js/yfn/src/view/generatereport/Step2.js
@@ -53,33 +53,20 @@ Ext.define('Yfn.view.generatereport.Step2', {
                             text: Uni.I18n.translate('generatereport.wizard.mandatoryFilters', 'YFN', 'Mandatory filters')
                         },
                         {
-                            xtype: 'box',
-                            itemId: 'report-mandatory-filters-info',
-                            cls: 'uni-info-icon',
-                            autoEl: {
-                                tag: 'img',
-                                src: '../sky/build/resources/images/shared/icon-info-small.png',
-                                width: 16,
-                                height:16
-                            },
-                            margin: '6 0 0 10',
-                            style: {
-                                cursor: 'help'
-                            },
-                            listeners: {
-                                afterrender: function (component) {
-                                    new Ext.ToolTip({
-                                        target: component.getEl(),
-                                        html: Uni.I18n.translate('generatereport.wizard.mandatoryFiltersTooltip',
-                                            'YFN',
-                                            'Reports allow you to look up a lot of data. A mandatory filter is a minimum ' +
-                                            'required selection to help narrow down the results, so to avoid performance ' +
-                                            'issues when generation the report')
-
-                                    });
-                                }
-                            }
+                            xtype: 'button',
+                            tooltip: Uni.I18n.translate('generatereport.wizard.mandatoryFiltersTooltip',
+                                'YFN',
+                                'Reports allow you to look up a lot of data. A mandatory filter is a minimum ' +
+                                'required selection to help narrow down the results, so to avoid performance ' +
+                                'issues when generation the report'),
+                            iconCls: 'uni-icon-info-small',
+                            ui: 'blank',
+                            shadow: false,
+                            margin: '5 0 0 5',
+                            width: 16,
+                            tabIndex: -1
                         }
+
                     ]
                 },
                 {
@@ -102,30 +89,16 @@ Ext.define('Yfn.view.generatereport.Step2', {
                             text: Uni.I18n.translate('generatereport.wizard.optionalFilters', 'YFN', 'In report filters')
                         },
                         {
-                            xtype: 'box',
-                            itemId: 'report-mandatory-filters-info',
-                            cls: 'uni-info-icon',
-                            autoEl: {
-                                tag: 'img',
-                                src: '../sky/build/resources/images/shared/icon-info-small.png',
-                                width: 16,
-                                height:16
-                            },
-                            margin: '6 0 0 10',
-                            style: {
-                                cursor: 'help'
-                            },
-                            listeners: {
-                                afterrender: function (component) {
-                                    new Ext.ToolTip({
-                                        target: component.getEl(),
-                                        html: Uni.I18n.translate('generatereport.wizard.optionalFiltersTooltip',
-                                            'YFN',
-                                            'Filtering on the data that is in the report data set')
-
-                                    });
-                                }
-                            }
+                            xtype: 'button',
+                            tooltip:  Uni.I18n.translate('generatereport.wizard.optionalFiltersTooltip',
+                                'YFN',
+                                'Filtering on the data that is in the report data set'),
+                            iconCls: 'uni-icon-info-small',
+                            ui: 'blank',
+                            shadow: false,
+                            margin: '5 0 0 5',
+                            width: 16,
+                            tabIndex: -1
                         }
                     ]
                 },
