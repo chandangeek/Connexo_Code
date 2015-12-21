d8ac0a473623f451cc5a346dc78e0919265517c1
Anton Fomchenko <antfom@mera.ru>
2015-12-21 16:54:14 +0300

    COMU-2879 added privileges check

--- comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/impl/CodeTableResource.java
+++ comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/impl/CodeTableResource.java
@@ -1,8 +1,10 @@
 package com.energyict.mdc.pluggable.rest.impl;
 
 import com.elster.jupiter.rest.util.Transactional;
+import com.energyict.mdc.device.config.security.Privileges;
 import com.energyict.mdc.protocol.api.codetables.CodeFactory;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -28,6 +30,11 @@ public class CodeTableResource {
 
     @GET @Transactional
     @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @RolesAllowed({Privileges.Constants.ADMINISTRATE_MASTER_DATA, Privileges.Constants.VIEW_MASTER_DATA,
+            com.energyict.mdc.engine.config.security.Privileges.Constants.VIEW_COMMUNICATION_ADMINISTRATION,
+            com.energyict.mdc.engine.config.security.Privileges.Constants.ADMINISTRATE_COMMUNICATION_ADMINISTRATION,
+            com.energyict.mdc.device.config.security.Privileges.Constants.VIEW_DEVICE_TYPE,
+            com.energyict.mdc.device.config.security.Privileges.Constants.ADMINISTRATE_DEVICE_TYPE})
     public CodeTableInfos getCodeTablePropertyContext(@Context UriInfo uriInfo){
         CodeTableInfos timeZoneInUseInfos = new CodeTableInfos();
         this.codeFactory
--- comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/impl/LicensedProtocolResource.java
+++ comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/impl/LicensedProtocolResource.java
@@ -1,9 +1,11 @@
 package com.energyict.mdc.pluggable.rest.impl;
 
 import com.elster.jupiter.rest.util.Transactional;
+import com.energyict.mdc.device.config.security.Privileges;
 import com.energyict.mdc.protocol.api.LicensedProtocol;
 import com.energyict.mdc.protocol.pluggable.ProtocolPluggableService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -26,6 +28,11 @@ public class LicensedProtocolResource {
 
     @GET @Transactional
     @Produces(MediaType.APPLICATION_JSON+"; charset=UTF-8")
+    @RolesAllowed({Privileges.Constants.ADMINISTRATE_MASTER_DATA, Privileges.Constants.VIEW_MASTER_DATA,
+            com.energyict.mdc.engine.config.security.Privileges.Constants.VIEW_COMMUNICATION_ADMINISTRATION,
+            com.energyict.mdc.engine.config.security.Privileges.Constants.ADMINISTRATE_COMMUNICATION_ADMINISTRATION,
+            com.energyict.mdc.device.config.security.Privileges.Constants.VIEW_DEVICE_TYPE,
+            com.energyict.mdc.device.config.security.Privileges.Constants.ADMINISTRATE_DEVICE_TYPE})
     public LicensedProtocolsInfo getLicensedProtocolInfos() {
         LicensedProtocolsInfo licensedProtocolsInfo = new LicensedProtocolsInfo();
         for (LicensedProtocol licensedProtocol : this.protocolPluggableService.getAllLicensedProtocols()) {
