1e6326821106fbb7d32183fc00ec1019653f0103
govanni <govanni.naessens@elster.com>
2015-12-22 15:17:30 +0100

    feature/COMU-1532-change-device-configuration-on provided functionality to execute the search in the eventHandler

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DevicesForConfigChangeSearchFactory.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DevicesForConfigChangeSearchFactory.java
@@ -6,14 +6,38 @@ import com.elster.jupiter.search.SearchService;
 import com.elster.jupiter.search.SearchableProperty;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.DevicesForConfigChangeSearch;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import javax.inject.Inject;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 
 /**
  * Factory which allows easy creation of a DevicesForConfigChangeSearch object based on a JsonQueryFilter
  */
 public class DevicesForConfigChangeSearchFactory {
+
+    private static final Function<JsonNode, String> AS_CRITERIA = node -> {
+        if (node != null) {
+            return node.findValue("criteria").asText();
+        }
+        return null;
+    };
+    private static final Function<JsonNode, List<String>> AS_LIST_CRITERIA = node -> {
+        if (node != null) {
+            JsonNode criteria = node.findValue("criteria");
+            if(criteria.isArray()){
+                List<String> values = new ArrayList<>();
+                for (JsonNode aCriteria : criteria) {
+                    values.add(aCriteria.asText());
+                }
+                return values;
+            } else {
+                return Collections.singletonList(criteria.asText());
+            }
+        }
+        return null;
+    };
 
     private SearchService searchService;
 
@@ -30,11 +54,11 @@ public class DevicesForConfigChangeSearchFactory {
                     filter(p -> filter.hasProperty(p.getName())).
                     forEach(searchableProperty -> {
                         if (searchableProperty.getSelectionMode() == SearchableProperty.SelectionMode.MULTI) {
-                            devicesForConfigChangeSearch.searchItems.add(new DevicesForConfigChangeSearch.DeviceSearchItem(searchableProperty.getName(), DevicesForConfigChangeSearch.Operator.IN, filter.getStringList(searchableProperty.getName())));
+                            devicesForConfigChangeSearch.searchItems.add(new DevicesForConfigChangeSearch.DeviceSearchItem(searchableProperty.getName(), DevicesForConfigChangeSearch.Operator.IN, filter.getProperty(searchableProperty.getName(), AS_LIST_CRITERIA)));
                         } else if (searchableProperty.getSpecification().getValueFactory().getValueType().equals(String.class)) {
-                            devicesForConfigChangeSearch.searchItems.add(new DevicesForConfigChangeSearch.DeviceSearchItem(searchableProperty.getName(), DevicesForConfigChangeSearch.Operator.LIKE, filter.getString(searchableProperty.getName())));
+                            devicesForConfigChangeSearch.searchItems.add(new DevicesForConfigChangeSearch.DeviceSearchItem(searchableProperty.getName(), DevicesForConfigChangeSearch.Operator.LIKE, filter.getProperty(searchableProperty.getName(), AS_CRITERIA)));
                         } else {
-                            devicesForConfigChangeSearch.searchItems.add(new DevicesForConfigChangeSearch.DeviceSearchItem(searchableProperty.getName(), DevicesForConfigChangeSearch.Operator.EQUALS, filter.getString(searchableProperty.getName())));
+                            devicesForConfigChangeSearch.searchItems.add(new DevicesForConfigChangeSearch.DeviceSearchItem(searchableProperty.getName(), DevicesForConfigChangeSearch.Operator.EQUALS, filter.getProperty(searchableProperty.getName(), AS_CRITERIA)));
                         }
                     });
         }
