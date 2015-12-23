3f91e6c7233227001179f0b1520988ce4ddf3808
Kurt Kikendall <kurt.kikendall@elster.com>
2015-12-23 09:47:20 -0500

    JP-525 review changes.  Remove date format.

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/channeldata/model/ChannelDataFilter.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/channeldata/model/ChannelDataFilter.js
@@ -8,7 +8,7 @@ Ext.define('Imt.channeldata.model.ChannelDataFilter', {
         root: 'filter'
     },
     fields: [
-        {name: 'intervalStart', type: 'date', dateFormat: 'Y-m-dTH:i:s'},
+        {name: 'intervalStart', type: 'date'},
         {name: 'duration'},
         {name: 'onlySuspect'},
         {name: 'onlyNonSuspect'}
