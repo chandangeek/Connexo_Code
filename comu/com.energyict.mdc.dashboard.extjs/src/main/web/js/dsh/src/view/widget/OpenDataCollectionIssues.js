2bdad4440fee42c7f9dbe3afedae7052a1bb5ffe
TVN <thomas.vandebuerie@elster.com>
2015-12-29 14:09:00 +0100

    COMU-2665 - Multisense dashboard: text not filled in in flagged devices block

--- comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/src/view/widget/OpenDataCollectionIssues.js
+++ comu/com.energyict.mdc.dashboard.extjs/src/main/web/js/dsh/src/view/widget/OpenDataCollectionIssues.js
@@ -10,94 +10,96 @@ Ext.define('Dsh.view.widget.OpenDataCollectionIssues', {
     },
 
     setRecord: function (record) {
-        var me = this,
-            elm = me.down('#issues-dataview'),
-            countContainer = me.down('#open-data-collection-issues-count-container'),
-            dockedLinksContainer = me.down('#open-data-collection-issues-docked-links'),
-            assigned = record.getAssignedToMeIssues(),
-            unassigned = record.getUnassignedIssues(),
-            store = assigned.topMyIssues(),
-            issuesCount = store.getCount();
+        var me = this;
+        if(me.rendered) {
+            var elm = me.down('#issues-dataview'),
+                countContainer = me.down('#open-data-collection-issues-count-container'),
+                dockedLinksContainer = me.down('#open-data-collection-issues-docked-links'),
+                assigned = record.getAssignedToMeIssues(),
+                unassigned = record.getUnassignedIssues(),
+                store = assigned.topMyIssues(),
+                issuesCount = store.getCount();
 
-        var title = '<h3>' + Ext.String.format(
-            Uni.I18n.translate('overview.widget.openDataCollectionIssues.header', 'DSH','Open data collection issues ({0})'),
-            assigned.get('total'))
-            + '</h3>';
-        me.setTitle(title);
+            var title = '<h3>' + Ext.String.format(
+                    Uni.I18n.translate('overview.widget.openDataCollectionIssues.header', 'DSH', 'Open data collection issues ({0})'),
+                    assigned.get('total'))
+                + '</h3>';
+            me.setTitle(title);
 
-        store.each(function (item) {
-            var dueDate = item.get('dueDate');
-            item.set('href', me.router.getRoute('workspace/datacollectionissues/view').buildUrl({issueId: item.get('id')}));
+            store.each(function (item) {
+                var dueDate = item.get('dueDate');
+                item.set('href', me.router.getRoute('workspace/datacollectionissues/view').buildUrl({issueId: item.get('id')}));
 
-            if (dueDate) {
-                if (moment().isAfter(moment(dueDate))) {
-                    item.set('tooltip', Uni.I18n.translate('overview.widget.openDataCollectionIssues.overdue', 'DSH', 'Overdue'));
-                    item.set('icon', '/apps/dsh/resources/images/widget/blocked.png');
-                } else {
-                    if(moment().endOf('day').isAfter(moment(dueDate))) {
-                        item.set('tooltip', Uni.I18n.translate('overview.widget.openDataCollectionIssues.dueToday', 'DSH', 'Due today'));
+                if (dueDate) {
+                    if (moment().isAfter(moment(dueDate))) {
+                        item.set('tooltip', Uni.I18n.translate('overview.widget.openDataCollectionIssues.overdue', 'DSH', 'Overdue'));
                         item.set('icon', '/apps/dsh/resources/images/widget/blocked.png');
                     } else {
-                        if (moment().add(1, 'day').endOf('day').isAfter(moment(dueDate))) {
-                            item.set('tooltip', Uni.I18n.translate('overview.widget.openDataCollectionIssues.dueTomorrow', 'DSH', 'Due tomorrow'));
-                            item.set('icon', '/apps/dsh/resources/images/widget/inactive.png');
+                        if (moment().endOf('day').isAfter(moment(dueDate))) {
+                            item.set('tooltip', Uni.I18n.translate('overview.widget.openDataCollectionIssues.dueToday', 'DSH', 'Due today'));
+                            item.set('icon', '/apps/dsh/resources/images/widget/blocked.png');
+                        } else {
+                            if (moment().add(1, 'day').endOf('day').isAfter(moment(dueDate))) {
+                                item.set('tooltip', Uni.I18n.translate('overview.widget.openDataCollectionIssues.dueTomorrow', 'DSH', 'Due tomorrow'));
+                                item.set('icon', '/apps/dsh/resources/images/widget/inactive.png');
+                            }
                         }
                     }
                 }
-            }
-        });
-
-        elm.bindStore(store);
-
-        Ext.suspendLayouts();
-
-        countContainer.removeAll();
-        dockedLinksContainer.removeAll();
-
-        if (issuesCount === 0) {
-            countContainer.add({
-                xtype: 'label',
-                text: Uni.I18n.translate('operator.dashboard.issuesEmptyMsg', 'DSH', 'No open issues assigned to you.')
             });
-        }
-        if (issuesCount) {
-            countContainer.add({
-                xtype: 'container',
-                html: Ext.String.format(Uni.I18n.translate('overview.widget.openDataCollectionIssues.topIssues', 'DSH', 'Top {0} most urgent issues assigned to me'), issuesCount)
-            });
-        }
 
-        var assignedFilter = {
-            assignee: assigned.get('filter').assigneeId + ':' + assigned.get('filter').assigneeType,
-            status: 'status.open',
-            groupingType: 'none',
-            sort: ['dueDate', 'modTime']
-        };
+            elm.bindStore(store);
 
-        var unassignedFilter = {
-            assignee: unassigned.get('filter').assigneeId + ':' + unassigned.get('filter').assigneeType,
-            status: 'status.open',
-            groupingType: 'none',
-            sort: ['dueDate', 'modTime']
-        };
+            Ext.suspendLayouts();
 
-        dockedLinksContainer.add([
-            {
-                xtype: 'button',
-                itemId: 'lnk-assigned-issues-link',
-                text: Ext.String.format(Uni.I18n.translate('overview.widget.openDataCollectionIssues.assignedToMe', 'DSH', 'Assigned to me ({0})'), assigned.get('total')),
-                ui: 'link',
-                href: me.router.getRoute('workspace/datacollectionissues').buildUrl(null, assignedFilter)
-            },
-            {
-                xtype: 'button',
-                itemId: 'lnk-unassigned-issues-link',
-                text: Ext.String.format(Uni.I18n.translate('general.unassignedCounter', 'DSH', 'Unassigned ({0})'), unassigned.get('total')),
-                ui: 'link',
-                href: me.router.getRoute('workspace/datacollectionissues').buildUrl(null, unassignedFilter)
+            countContainer.removeAll();
+            dockedLinksContainer.removeAll();
+
+            if (issuesCount === 0) {
+                countContainer.add({
+                    xtype: 'label',
+                    text: Uni.I18n.translate('operator.dashboard.issuesEmptyMsg', 'DSH', 'No open issues assigned to you.')
+                });
             }
-        ]);
-        Ext.resumeLayouts(true);
+            if (issuesCount) {
+                countContainer.add({
+                    xtype: 'container',
+                    html: Ext.String.format(Uni.I18n.translate('overview.widget.openDataCollectionIssues.topIssues', 'DSH', 'Top {0} most urgent issues assigned to me'), issuesCount)
+                });
+            }
+
+            var assignedFilter = {
+                assignee: assigned.get('filter').assigneeId + ':' + assigned.get('filter').assigneeType,
+                status: 'status.open',
+                groupingType: 'none',
+                sort: ['dueDate', 'modTime']
+            };
+
+            var unassignedFilter = {
+                assignee: unassigned.get('filter').assigneeId + ':' + unassigned.get('filter').assigneeType,
+                status: 'status.open',
+                groupingType: 'none',
+                sort: ['dueDate', 'modTime']
+            };
+
+            dockedLinksContainer.add([
+                {
+                    xtype: 'button',
+                    itemId: 'lnk-assigned-issues-link',
+                    text: Ext.String.format(Uni.I18n.translate('overview.widget.openDataCollectionIssues.assignedToMe', 'DSH', 'Assigned to me ({0})'), assigned.get('total')),
+                    ui: 'link',
+                    href: me.router.getRoute('workspace/datacollectionissues').buildUrl(null, assignedFilter)
+                },
+                {
+                    xtype: 'button',
+                    itemId: 'lnk-unassigned-issues-link',
+                    text: Ext.String.format(Uni.I18n.translate('general.unassignedCounter', 'DSH', 'Unassigned ({0})'), unassigned.get('total')),
+                    ui: 'link',
+                    href: me.router.getRoute('workspace/datacollectionissues').buildUrl(null, unassignedFilter)
+                }
+            ]);
+            Ext.resumeLayouts(true);
+        }
     },
 
     tbar: {
