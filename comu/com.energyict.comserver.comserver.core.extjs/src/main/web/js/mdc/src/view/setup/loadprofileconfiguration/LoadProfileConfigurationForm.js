35903762588fe5b2cf54103bf8380bf00b169e28
TVN <thomas.vandebuerie@elster.com>
2015-12-29 11:02:42 +0100

    COPL-1510 - Fix automated tests

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/loadprofileconfiguration/LoadProfileConfigurationForm.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/loadprofileconfiguration/LoadProfileConfigurationForm.js
@@ -48,7 +48,7 @@ Ext.define('Mdc.view.setup.loadprofileconfiguration.LoadProfileConfigurationForm
                         store: 'Mdc.store.LoadProfileConfigurationsOnDeviceConfigurationAvailable',
                         required: true,
                         allowBlank: false,
-                        forceSelection: true,
+                        forceSelection: !me.edit,
                         //fieldLabel: 'Load profile type',
                         fieldLabel: Uni.I18n.translate('general.loadProfileType', 'MDC', 'Load profile type'),
                         emptyText: Uni.I18n.translate('loadprofileconfiguration.selectLoadProfileType','MDC','Select a load profile type'),
