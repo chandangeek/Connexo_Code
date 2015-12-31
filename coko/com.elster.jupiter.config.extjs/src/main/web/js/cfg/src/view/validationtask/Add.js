82a1f92c64a6c0c525fa887654d61271feb15d08
TVN <thomas.vandebuerie@elster.com>
2015-12-31 14:44:32 +0100

    COMU-2927 - Strange validation message when editing validation task

--- coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/src/view/validationtask/Add.js
+++ coko/com.elster.jupiter.config.extjs/src/main/web/js/cfg/src/view/validationtask/Add.js
@@ -167,7 +167,7 @@ Ext.define('Cfg.view.validationtask.Add', {
                                 xtype: 'displayfield',
                                 itemId: 'no-usagepoint',
                                 hidden: true,
-                                value: '<div style="color: #FF0000">' + Uni.I18n.translate('validationTasks.general.noUsagePointGroup', 'CFG', 'No usage point group defined yet.') + '</div>',
+                                value: '<div style="color: #EB5642">' + Uni.I18n.translate('validationTasks.general.noUsagePointGroup', 'CFG', 'No usage point group defined yet.') + '</div>',
                                 htmlEncode: false,
                                 labelwidth: 500,
                                 width: 235
