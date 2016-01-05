8ec2a2b5917f0874aa1f2b205bbd95bd084fcb09
TVN <thomas.vandebuerie@elster.com>
2016-01-05 11:18:31 +0100

    COPL-1510 - Fix automated tests

--- copl/com.elster.jupiter.fileimport.extjs/src/main/web/js/fileimport/src/view/importservices/AddImportService.js
+++ copl/com.elster.jupiter.fileimport.extjs/src/main/web/js/fileimport/src/view/importservices/AddImportService.js
@@ -56,7 +56,7 @@ Ext.define('Fim.view.importservices.AddImportService', {
                         enforceMaxLength: true,
                         listeners: {
                             afterrender: function (field) {
-                                field.focus(false, 500);
+                                field.focus(false, 200);
                             }
                         }
                     },
