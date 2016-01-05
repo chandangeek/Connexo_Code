c66da2fdfc2bbd6d8061e50b7de799f24855ba84
Geert <geert.delmeiren@elster.com>
2016-01-05 11:26:49 +0100

    Small layout correction

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/logbooktype/LogbookTypeEdit.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/logbooktype/LogbookTypeEdit.js
@@ -62,7 +62,6 @@ Ext.define('Mdc.view.setup.logbooktype.LogbookTypeEdit', {
                                         itemId: 'editLogbookTypeNameField',
                                         maxLength: 80,
                                         enforceMaxLength: true,
-                                        width: 600,
                                         listeners: {
                                             afterrender: function (field) {
                                                 field.focus(false, 500);
