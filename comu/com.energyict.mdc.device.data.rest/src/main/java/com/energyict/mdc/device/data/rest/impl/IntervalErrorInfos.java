ee1e0ebfaafc7665e6e5a169e723dcedc59fff36
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-01-07 15:34:16 +0100

    Fix unit test failure due to non unique message seeds + fix typo or bizarre English in enum value names

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/IntervalErrorInfos.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/IntervalErrorInfos.java
@@ -1,8 +1,5 @@
 package com.energyict.mdc.device.data.rest.impl;
 
-import com.elster.jupiter.util.exception.MessageSeed;
-
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +8,8 @@ public class IntervalErrorInfos {
     public List<IntervalErrorInfo> errors = new ArrayList<>();
 
     public IntervalErrorInfos() {
-        errors.add(new IntervalErrorInfo("startTime", MessageSeeds.INTERVAL_START_EXCEEDS_END.getDefaultFormat()));
-        errors.add(new IntervalErrorInfo("endTime", MessageSeeds.INTERVAL_END_BENEATH_START.getDefaultFormat()));
+        errors.add(new IntervalErrorInfo("startTime", MessageSeeds.INTERVAL_START_AFTER_END.getDefaultFormat()));
+        errors.add(new IntervalErrorInfo("endTime", MessageSeeds.INTERVAL_END_BEFORE_START.getDefaultFormat()));
     }
 
     public class IntervalErrorInfo {
