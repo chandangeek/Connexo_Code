53147b94e2916f2d1e23e847b825c0f8984ed34d
govanni <govanni.naessens@elster.com>
2016-01-13 09:57:39 +0100

    feature/COPL-1166 Fixed tests after merge into master

--- comu/com.energyict.mdc.device.lifecycle/src/test/java/com/energyict/mdc/device/lifecycle/impl/micro/actions/SetMultiplierTest.java
+++ comu/com.energyict.mdc.device.lifecycle/src/test/java/com/energyict/mdc/device/lifecycle/impl/micro/actions/SetMultiplierTest.java
@@ -1,31 +1,26 @@
 package com.energyict.mdc.device.lifecycle.impl.micro.actions;
 
 import com.elster.jupiter.nls.Thesaurus;
-import com.elster.jupiter.properties.BoundedBigDecimalPropertySpecImpl;
 import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.PropertySpecBuilderWizard;
 import com.elster.jupiter.properties.PropertySpecService;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.lifecycle.DeviceLifeCycleService;
 import com.energyict.mdc.device.lifecycle.ExecutableActionProperty;
-import com.energyict.mdc.device.lifecycle.impl.DeviceLifeCyclePropertySupport;
+import com.energyict.mdc.device.lifecycle.impl.micro.i18n.MicroActionTranslationKey;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Copyrights EnergyICT
@@ -45,24 +40,6 @@ public class SetMultiplierTest {
     public SetMultiplier getTestInstance() {
         return new SetMultiplier(thesaurus);
     }
-
-    @Test
-    public void testGetPropertySpecsDelegatesToPropertySpecService() {
-        SetMultiplier setMultiplier = getTestInstance();
-
-        when(propertySpecService.boundedDecimalPropertySpec(any(String.class), any(Boolean.class), any(BigDecimal.class), any(BigDecimal.class)))
-                .thenAnswer(invocationOnMock -> new BoundedBigDecimalPropertySpecImpl(
-                        ((String) invocationOnMock.getArguments()[0]),
-                        ((BigDecimal) invocationOnMock.getArguments()[2]),
-                        ((BigDecimal) invocationOnMock.getArguments()[3])));
-
-        // Business method
-        List<PropertySpec> propertySpecs = setMultiplier.getPropertySpecs(this.propertySpecService);
-
-        assertThat(propertySpecs).hasSize(1);
-        assertThat(propertySpecs.get(0).getName()).isEqualToIgnoringCase(DeviceLifeCycleService.MicroActionPropertyName.MULTIPLIER.key());
-    }
-
 
     @Test
     public void setMultiplierTest() {
