661d50946029d7e19046cd100250b31ae8aef271
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-01-13 14:50:03 +0100

    COPL-1507

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceProtocolConfigurationProperty.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/DeviceProtocolConfigurationProperty.java
@@ -1,12 +1,15 @@
 package com.energyict.mdc.device.config.impl;
 
-import com.energyict.mdc.device.config.DeviceConfiguration;
-
 import com.elster.jupiter.domain.util.Save;
 import com.elster.jupiter.orm.associations.Reference;
 import com.elster.jupiter.orm.associations.ValueReference;
+import com.elster.jupiter.properties.InvalidValueException;
+import com.elster.jupiter.properties.PropertySpec;
+import com.energyict.mdc.device.config.DeviceConfiguration;
 
+import javax.validation.ConstraintValidatorContext;
 import javax.validation.constraints.Size;
+import java.text.MessageFormat;
 import java.time.Instant;
 
 /**
@@ -16,6 +19,7 @@ import java.time.Instant;
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2014-11-24 (09:32)
  */
+@IsValidProperty
 class DeviceProtocolConfigurationProperty {
 
     private Reference<DeviceConfiguration> deviceConfiguration = ValueReference.absent();
@@ -50,4 +54,31 @@ class DeviceProtocolConfigurationProperty {
         return this;
     }
 
+    boolean validate(ConstraintValidatorContext context) {
+        PropertySpec propertySpec = getPropertySpec();
+        try {
+            Object actualValue = value;
+            try {
+                actualValue = propertySpec.getValueFactory().fromStringValue(value);
+            } catch (Exception e) {
+                // if conversion fails, validation will fail
+            }
+            return propertySpec.validateValue(actualValue);
+        } catch (InvalidValueException e) {
+            context.buildConstraintViolationWithTemplate(MessageFormat.format(e.getDefaultPattern(), e.getArguments()))
+                    .addPropertyNode("properties").addPropertyNode(propertySpec.getName()).addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+            return false;
+        }
+    }
+
+    private PropertySpec getPropertySpec() {
+        return deviceConfiguration.get()
+                .getDeviceProtocolProperties()
+                .getPropertySpecs()
+                .stream()
+                .filter(spec -> spec.getName().equals(name))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
---
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/IsValidPropertiesValidator.java
@@ -0,0 +1,15 @@
+package com.energyict.mdc.device.config.impl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class IsValidPropertiesValidator implements ConstraintValidator<IsValidProperty, DeviceProtocolConfigurationProperty> {
+    @Override
+    public void initialize(IsValidProperty constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(DeviceProtocolConfigurationProperty value, ConstraintValidatorContext context) {
+        return value.validate(context);
+    }
+}
