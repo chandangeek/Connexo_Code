8497d11d4b96fffc6244bfe728eee355870ffdef
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-01-14 09:42:52 +0100

    Fix COPL-1605

--- comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/templates/BasicDataCollectionRuleTemplate.java
+++ comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/templates/BasicDataCollectionRuleTemplate.java
@@ -8,6 +8,7 @@ import com.elster.jupiter.issue.share.entity.OpenIssue;
 import com.elster.jupiter.issue.share.service.IssueService;
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.properties.PropertySelectionMode;
 import com.elster.jupiter.properties.PropertySpec;
 import com.energyict.mdc.dynamic.PropertySpecService;
 import com.energyict.mdc.issue.datacollection.IssueDataCollectionService;
@@ -133,7 +134,7 @@ public class BasicDataCollectionRuleTemplate extends AbstractDataCollectionTempl
                 .fromThesaurus(this.getThesaurus())
                 .markRequired()
                 .addValues(eventTypes.getEventTypes())
-                .markExhaustive()
+                .markExhaustive(PropertySelectionMode.COMBOBOX)
                 .finish());
         builder.add(propertySpecService
                 .booleanSpec()
