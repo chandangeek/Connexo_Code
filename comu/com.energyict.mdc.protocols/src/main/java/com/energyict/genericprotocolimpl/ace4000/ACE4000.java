b6e5d4797cf699c20480f31df57521930c8e2d41
govanni <govanni.naessens@elster.com>
2016-01-15 16:23:54 +0100

    COMU-3212 - Removed unused files - Added the protocols.pro file - Updated the pom file so protosol can be obfuscated

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/genericprotocolimpl/ace4000/ACE4000.java
+++
@@ -1,9 +0,0 @@
-package com.energyict.genericprotocolimpl.ace4000;
-
-/**
- * @author gna & khe
- * @deprecated generic protocols are no longer supported, a proper DeviceProtocol has been created to replace this
- */
-public class ACE4000 {
-
-}
