b6b77623f4c89c531521f46720a3abd244855b0b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-01-15 15:17:39 +0100

    Fix COMU-3276

--- copl/com.elster.jupiter.fileimport.rest/src/main/java/com/elster/jupiter/fileimport/rest/impl/PropertyType.java
+++ copl/com.elster.jupiter.fileimport.rest/src/main/java/com/elster/jupiter/fileimport/rest/impl/PropertyType.java
@@ -2,26 +2,30 @@ package com.elster.jupiter.fileimport.rest.impl;
 
 import com.elster.jupiter.properties.BigDecimalFactory;
 import com.elster.jupiter.properties.BooleanFactory;
+import com.elster.jupiter.properties.HasIdAndName;
+import com.elster.jupiter.properties.ListValueFactory;
 import com.elster.jupiter.properties.StringFactory;
 import com.elster.jupiter.properties.ThreeStateFactory;
 import com.elster.jupiter.properties.ValueFactory;
 
 public enum PropertyType implements com.elster.jupiter.rest.util.properties.PropertyType {
-    UNKNOWN(Void.class),
-    NUMBER(BigDecimalFactory.class),
-    NULLABLE_BOOLEAN(ThreeStateFactory.class),
-    BOOLEAN(BooleanFactory.class),
-    TEXT(StringFactory.class),
+    UNKNOWN(new NeverMatch()),
+    NUMBER(new ClassBasedValueFactoryMatcher(BigDecimalFactory.class)),
+    NULLABLE_BOOLEAN(new ClassBasedValueFactoryMatcher(ThreeStateFactory.class)),
+    BOOLEAN(new ClassBasedValueFactoryMatcher(BooleanFactory.class)),
+    TEXT(new ClassBasedValueFactoryMatcher(StringFactory.class)),
+    LISTVALUE(new ClassBasedValueFactoryMatcher(ListValueFactory.class)),
+    IDWITHNAME(new DomainClassValueFactoryMatcher(HasIdAndName.class)),
     ;
 
-    private Class valueFactoryClass;
+    private final ValueFactoryMachter matcher;
 
-    PropertyType(Class valueFactoryClass) {
-        this.valueFactoryClass = valueFactoryClass;
+    PropertyType(ValueFactoryMachter matcher) {
+        this.matcher = matcher;
     }
 
     private boolean matches(ValueFactory valueFactory) {
-        return this.valueFactoryClass.isAssignableFrom(valueFactory.getClass());
+        return this.matcher.matches(valueFactory);
     }
 
     public static PropertyType getTypeFrom(ValueFactory valueFactory) {
@@ -32,4 +36,46 @@ public enum PropertyType implements com.elster.jupiter.rest.util.properties.Prop
         }
         return UNKNOWN;
     }
+
+    private interface ValueFactoryMachter {
+        boolean matches(ValueFactory valueFactory);
+    }
+
+    private static class NeverMatch implements ValueFactoryMachter {
+        @Override
+        public boolean matches(ValueFactory valueFactory) {
+            return false;
+        }
+    }
+
+    private static class ClassBasedValueFactoryMatcher implements ValueFactoryMachter {
+        private final Class valueFactoryClass;
+
+        private ClassBasedValueFactoryMatcher(Class valueFactoryClass) {
+            super();
+            this.valueFactoryClass = valueFactoryClass;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean matches(ValueFactory valueFactory) {
+            return this.valueFactoryClass.isAssignableFrom(valueFactory.getClass());
+        }
+    }
+
+    private static class DomainClassValueFactoryMatcher implements ValueFactoryMachter {
+        private final Class domainClass;
+
+        private DomainClassValueFactoryMatcher(Class domainClass) {
+            super();
+            this.domainClass = domainClass;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean matches(ValueFactory valueFactory) {
+            return this.domainClass.isAssignableFrom(valueFactory.getValueType());
+        }
+    }
+
 }
