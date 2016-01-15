e994fc9ace422dde75add46184b0d7cc91b582b7
David Nitescu <david.nitescu@elster.com>
2016-01-15 13:15:55 +0200

    COMU-3217 - Bounded bigdecimal TypedProperty doesn't work

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicegeneralattributes/Edit.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/view/setup/devicegeneralattributes/Edit.js
@@ -4,61 +4,69 @@ Ext.define('Mdc.view.setup.devicegeneralattributes.Edit', {
     itemId: 'deviceGeneralAttributesEdit',
 
     requires: [
+        'Uni.util.FormErrorMessage',
         'Mdc.view.setup.device.DeviceMenu',
         'Uni.property.form.Property'
     ],
 
-    content: [
-        {
-            xtype: 'panel',
-            ui: 'large',
-            title: Uni.I18n.translate('deviceconfiguration.generalAttributes.edit', 'MDC', 'Edit general attributes'),
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
-            items: [
-                {
-                    xtype: 'property-form',
-                    width: '100%'
-                }
-            ],
-            dockedItems: {
-                xtype: 'container',
-                dock: 'bottom',
-                margin: '20 0 0 265',
-                layout: {
-                    type: 'hbox',
-                    align: 'stretch'
-                },
-
-                items: [
-                    {
-                        xtype: 'button',
-                        itemId: 'deviceGeneralAttributesSaveBtn',
-
-                        ui: 'action',
-                        text: Uni.I18n.translate('general.save', 'MDC', 'Save')
-                    },
-                    {
-                        xtype: 'button',
-                        text: Uni.I18n.translate('general.restoreToDefaultSettings', 'MDC', 'Restore to default settings'),
-                        icon: '../sky/build/resources/images/form/restore.png',
-                        itemId: 'deviceGeneralAttributesRestoreDefaultBtn'
-                    },
-                    {
-                        xtype: 'button',
-                        ui: 'link',
-                        itemId: 'deviceGeneralAttributesCancelBtn',
-                        text: Uni.I18n.translate('general.cancel', 'MDC', 'Cancel')
-                    }
-                ]
-            }
-        }
-    ],
-
     initComponent: function () {
         var me = this;
+
+        me.content = [
+            {
+                xtype: 'form',
+                ui: 'large',
+                title: Uni.I18n.translate('deviceconfiguration.generalAttributes.edit', 'MDC', 'Edit general attributes'),
+                layout: {
+                    type: 'vbox',
+                    align: 'left'
+                },
+                items: [
+                    {
+                        itemId: 'form-errors',
+                        xtype: 'uni-form-error-message',
+                        width: 550,
+                        margin: '0 0 10 0',
+                        hidden: true
+                    },
+                    {
+                        xtype: 'property-form',
+                        width: '100%'
+                    }
+                ],
+                dockedItems: {
+                    xtype: 'container',
+                    dock: 'bottom',
+                    margin: '20 0 0 265',
+                    layout: {
+                        type: 'hbox',
+                        align: 'stretch'
+                    },
+
+                    items: [
+                        {
+                            xtype: 'button',
+                            itemId: 'deviceGeneralAttributesSaveBtn',
+
+                            ui: 'action',
+                            text: Uni.I18n.translate('general.save', 'MDC', 'Save')
+                        },
+                        {
+                            xtype: 'button',
+                            text: Uni.I18n.translate('general.restoreToDefaultSettings', 'MDC', 'Restore to default settings'),
+                            icon: '../sky/build/resources/images/form/restore.png',
+                            itemId: 'deviceGeneralAttributesRestoreDefaultBtn'
+                        },
+                        {
+                            xtype: 'button',
+                            ui: 'link',
+                            itemId: 'deviceGeneralAttributesCancelBtn',
+                            text: Uni.I18n.translate('general.cancel', 'MDC', 'Cancel')
+                        }
+                    ]
+                }
+            }
+        ];
 
         me.side = [
             {
