32a9bd199fec7da0b4988003a5feaf24bc441db6
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-01-18 14:41:17 +0100

    COMU-3292 - Installation importer: Action set multiplier is not defined on the lifecycle transition while it is (set)

--- comu/com.energyict.mdc.device.lifecycle/src/main/java/com/energyict/mdc/device/lifecycle/impl/micro/i18n/MicroActionTranslationKey.java
+++ comu/com.energyict.mdc.device.lifecycle/src/main/java/com/energyict/mdc/device/lifecycle/impl/micro/i18n/MicroActionTranslationKey.java
@@ -51,7 +51,7 @@ public enum MicroActionTranslationKey implements TranslationKey {
     MICRO_ACTION_DESCRIPTION_FORCE_VALIDATION_AND_ESTIMATION(Keys.MICRO_ACTION_DESCRIPTION_TRANSLATE_KEY + MicroAction.FORCE_VALIDATION_AND_ESTIMATION, "Force the validation and the estimation to resolve all reading quality issues."),
     MICRO_ACTION_DESCRIPTION_ENABLE_ESTIMATION(Keys.MICRO_ACTION_DESCRIPTION_TRANSLATE_KEY + MicroAction.ENABLE_ESTIMATION, "Activate the data estimation on this device. This auto change is effective immediately."),
     MICRO_ACTION_DESCRIPTION_DISABLE_ESTIMATION(Keys.MICRO_ACTION_DESCRIPTION_TRANSLATE_KEY + MicroAction.DISABLE_ESTIMATION, "Stop the data estimation on this device."),
-    MICRO_ACTION_DESCRIPTION_SET_MULTIPLIER(Keys.MICRO_ACTION_DESCRIPTION_TRANSLATE_KEY + MicroAction.SET_MULTIPLIER, "Set the multiplier on the device. The collected data can be multiplied by this value if it is configured on the respective readingtype."),
+    MICRO_ACTION_DESCRIPTION_SET_MULTIPLIER(DeviceLifeCycleService.MicroActionPropertyName.MULTIPLIER.key(), "Set the multiplier on the device. The collected data can be multiplied by this value if it is configured on the respective readingtype."),
     ;
 
     private final String key;
