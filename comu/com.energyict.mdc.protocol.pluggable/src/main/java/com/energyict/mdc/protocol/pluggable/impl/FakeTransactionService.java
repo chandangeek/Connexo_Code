07bff43c71f314f2709c273defc2dc15b9ab1f3f
Bruno Ballekens <bruno.ballekens@elster.com>
2016-01-19 10:37:23 +0100

    COMU-3212: fixed compilation errrors

--- comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/FakeTransactionService.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/FakeTransactionService.java
@@ -38,4 +38,8 @@ public class FakeTransactionService implements TransactionService {
         return null;
     }
 
+    @Override
+    public boolean isInTransaction() {
+        return this.actualTransactionService.isInTransaction();
+    }
 }
