1d989b9722381f3397e466506496bb4fa99658a9
Lobanov Dmitry <dlobanov@mera.ru>
2016-01-19 13:58:02 +0300

    bugfix/COMU-3320

--- comu/com.energyict.mdc.masterdata.rest/src/main/java/com/energyict/mdc/masterdata/rest/RegisterTypeInfo.java
+++ comu/com.energyict.mdc.masterdata.rest/src/main/java/com/energyict/mdc/masterdata/rest/RegisterTypeInfo.java
@@ -7,11 +7,12 @@ import com.energyict.mdc.common.ObisCode;
 import com.energyict.mdc.common.rest.ObisCodeAdapter;
 import com.energyict.mdc.masterdata.ChannelType;
 import com.energyict.mdc.masterdata.MeasurementType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.NullifyingDeserializer;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -25,6 +26,7 @@ public class RegisterTypeInfo {
     public Boolean isLinkedByActiveRegisterConfig; // This property makes no sense if the register mapping was retrieved outside the scope of a device type. It will be null.
     public Boolean isLinkedByInactiveRegisterConfig; // This property makes no sense if the register mapping was retrieved outside the scope of a device type. It will be null.
     public ReadingTypeInfo readingType;
+    @JsonDeserialize(using = NullifyingDeserializer.class)
     public ReadingTypeInfo calculatedReadingType;
     public long version;
     public VersionInfo<Long> parent;
