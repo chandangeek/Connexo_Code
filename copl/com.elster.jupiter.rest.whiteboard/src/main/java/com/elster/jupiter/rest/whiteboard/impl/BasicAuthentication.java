33d3b6fab020c3a7d021fbaf296e52edb9b62111
Dragos Sinca <dragos.sinca@elster.com>
2016-01-20 18:31:24 +0200

    COPL-1611

--- copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/BasicAuthentication.java
+++ copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/BasicAuthentication.java
@@ -26,47 +26,62 @@ public class BasicAuthentication implements Authentication {
     @Override
     public boolean handleSecurity(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String authentication = request.getHeader("Authorization");
-        Optional<User> user = Optional.empty();
-
-        boolean refreshCookie = false;
         if (authentication != null && authentication.startsWith("Basic ")) {
-            refreshCookie = true;
-
-            user = userService.authenticateBase64(authentication.split(" ")[1]);
-            SecurityToken.getInstance().removeCookie(request, response);
+            return doBasicAuthentication(request, response, authentication.split(" ")[1]);
+        } else if (authentication != null && authentication.startsWith("Bearer ")) {
+            return doBearerAuthorization(request, response, authentication);
         } else {
-            Optional<Cookie> xsrf = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("X-CONNEXO-TOKEN")).findFirst();
-            if (xsrf.isPresent()) {
-                if (authentication != null && authentication.startsWith("Bearer ")) {
-                    if (!SecurityToken.getInstance().doComparison(xsrf.get(), authentication.substring(authentication.lastIndexOf(" ") + 1))) {
-                        return deny(request, response);
-                    }
-                }
-                user = SecurityToken.getInstance().verifyToken(xsrf.get().getValue(), request, response, userService);
-            } else if (authentication != null && authentication.startsWith("Bearer ")) {
-                user = SecurityToken.getInstance().verifyToken(authentication.substring(authentication.lastIndexOf(" ") + 1), request, response, userService);
-            }
+            return doCookieAuthorization(request, response);
         }
-        return (user.isPresent() && !user.get().getPrivileges().isEmpty()) ? allow(request, response, user.get(), refreshCookie) : deny(request, response);
     }
 
-    private boolean allow(HttpServletRequest request, HttpServletResponse response, User user, boolean refreshCookie) {
+    private boolean doCookieAuthorization(HttpServletRequest request, HttpServletResponse response) {
+        Optional<User> user = Optional.empty();
+        Optional<Cookie> xsrf = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("X-CONNEXO-TOKEN")).findFirst();
+        if (xsrf.isPresent()) {
+            user = SecurityToken.getInstance().verifyToken(xsrf.get().getValue(), request, response, userService);
+        }
+
+        return isAuthenticated(user) ? allow(request, response, user.get()) : deny(request, response);
+    }
+
+    private boolean doBearerAuthorization(HttpServletRequest request, HttpServletResponse response, String authentication) {
+        Optional<Cookie> xsrf = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("X-CONNEXO-TOKEN")).findFirst();
+        if (xsrf.isPresent() && !SecurityToken.getInstance().doComparison(xsrf.get(), authentication.substring(authentication.lastIndexOf(" ") + 1))) {
+           return deny(request, response);
+        }
+
+        Optional<User> user = SecurityToken.getInstance().verifyToken(authentication.substring(authentication.lastIndexOf(" ") + 1), request, response, userService);
+        return isAuthenticated(user) ? allow(request, response, user.get()) : deny(request, response);
+    }
+
+    private boolean doBasicAuthentication(HttpServletRequest request, HttpServletResponse response, String authentication) {
+        Optional<User> user = userService.authenticateBase64(authentication);
+        SecurityToken.getInstance().removeCookie(request, response);
+        if(isAuthenticated(user)){
+            SecurityToken.getInstance().createToken(request, response, user.get(), 0);
+            return allow(request, response, user.get());
+        }
+        else {
+            return deny(request, response);
+        }
+    }
+
+    private boolean isAuthenticated(Optional<User> user) {
+        return user.isPresent() && !user.get().getPrivileges().isEmpty();
+    }
+
+    private boolean allow(HttpServletRequest request, HttpServletResponse response, User user) {
         request.setAttribute(HttpContext.AUTHENTICATION_TYPE, HttpServletRequest.BASIC_AUTH);
         request.setAttribute(WhiteBoardConfiguration.USERPRINCIPAL, user);
         request.setAttribute(HttpContext.REMOTE_USER, user.getName());
-        Optional<Cookie> xsrf = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("X-CONNEXO-TOKEN")).findFirst();
-        if (!xsrf.isPresent() || refreshCookie) {
-            SecurityToken.getInstance().createToken(request, response, user, 0);
-        }
         userService.addLoggedInUser(user);
         return true;
     }
 
     private boolean deny(HttpServletRequest request, HttpServletResponse response) {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        Optional<Cookie> xsrf = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("X-CONNEXO-TOKEN")).findFirst();
-        if (xsrf.isPresent())
-            SecurityToken.getInstance().removeCookie(request, response);
+        SecurityToken.getInstance().removeCookie(request, response);
         SecurityToken.getInstance().invalidateSession(request);
         return false;
     }
