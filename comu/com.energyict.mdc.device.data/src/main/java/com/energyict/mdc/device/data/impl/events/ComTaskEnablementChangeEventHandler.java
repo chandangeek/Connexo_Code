1105511c8c3468277699f1bbdf8ed1196c7f5733
Anton Suslennikov <antsusl@mera.ru>
2016-01-22 15:21:51 +0300

    COMU-3195 Fill in attributes for security settings added later than the device, gives 400 Bad Request

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/events/ComTaskEnablementChangeEventHandler.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/events/ComTaskEnablementChangeEventHandler.java
@@ -3,12 +3,23 @@ package com.energyict.mdc.device.data.impl.events;
 import com.elster.jupiter.events.LocalEvent;
 import com.elster.jupiter.events.TopicHandler;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.conditions.Condition;
 import com.energyict.mdc.device.config.ComTaskEnablement;
+import com.energyict.mdc.device.config.DeviceConfiguration;
+import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.data.DeviceFields;
 import com.energyict.mdc.device.data.impl.DeviceDataModelService;
-import com.energyict.mdc.device.data.tasks.ComTaskExecutionFilterSpecification;
+import com.energyict.mdc.device.data.tasks.ComTaskExecution;
+import com.energyict.mdc.device.data.tasks.ComTaskExecutionFields;
 import com.energyict.mdc.scheduling.SchedulingService;
+import com.energyict.mdc.scheduling.model.ComSchedule;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.elster.jupiter.util.conditions.Where.where;
 
 @Component(name = "com.energyict.mdc.device.data.comtaskenablement.eventhandler", service = TopicHandler.class, immediate = true)
 public class ComTaskEnablementChangeEventHandler implements TopicHandler {
@@ -34,15 +45,29 @@ public class ComTaskEnablementChangeEventHandler implements TopicHandler {
     }
 
     private void handleOnActiveDeviceConfig(ComTaskEnablement comTaskEnablement) {
-        ComTaskExecutionFilterSpecification filter = new ComTaskExecutionFilterSpecification();
-        this.schedulingService.findAllSchedules().stream()
+        List<ComSchedule> affectedComSchedules = this.schedulingService.findAllSchedules().stream()
                 .filter(comSchedule -> comSchedule.containsComTask(comTaskEnablement.getComTask()))
                 .filter(comSchedule -> comSchedule.getComTasks().size() > 1)
-                .forEach(filter.comSchedules::add);
-        if (!filter.comSchedules.isEmpty()
-                && !this.deviceDataModelService.communicationTaskService().findComTaskExecutionsByFilter(filter, 0, 1).isEmpty()) {
+                .collect(Collectors.toList());
+        if (!affectedComSchedules.isEmpty() && !hasNoUsageOnDevices(comTaskEnablement, affectedComSchedules)) {
             throw new VetoComTaskEnablementChangeException(getThesaurus(), comTaskEnablement);
         }
+    }
+
+    private boolean hasNoUsageOnDevices(ComTaskEnablement comTaskEnablement, List<ComSchedule> affectedComSchedules) {
+        return this.deviceDataModelService
+                .dataModel()
+                .query(ComTaskExecution.class, Device.class, DeviceConfiguration.class)
+                .select(conditionForDeviceConfig(comTaskEnablement).and(conditionForComSchedules(affectedComSchedules)), null, false, null, 1, 1)
+                .isEmpty();
+    }
+
+    private Condition conditionForDeviceConfig(ComTaskEnablement comTaskEnablement) {
+        return where(ComTaskExecutionFields.DEVICE.fieldName() + "." + DeviceFields.DEVICECONFIGURATION.fieldName()).isEqualTo(comTaskEnablement.getDeviceConfiguration());
+    }
+
+    private Condition conditionForComSchedules(List<ComSchedule> affectedComSchedules) {
+        return where(ComTaskExecutionFields.COM_SCHEDULE.fieldName()).in(affectedComSchedules);
     }
 
     private Thesaurus getThesaurus() {
