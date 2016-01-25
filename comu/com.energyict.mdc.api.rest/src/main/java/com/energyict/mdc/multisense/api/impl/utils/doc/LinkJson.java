16cc94c389f89bebfa84229638dff40ea3918e70
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-01-25 10:22:49 +0100

    COMU-3272: Improved link comment

--- comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/utils/doc/LinkJson.java
+++ comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/utils/doc/LinkJson.java
@@ -4,17 +4,24 @@ package com.energyict.mdc.multisense.api.impl.utils.doc;
  * Interface used to instruct Miredot how to generate doc for javax.ws.rs.core.Link
  */
 public class LinkJson {
-    public Self self;
     /**
-     * HTTP link to described element
+     * Parameters describing the nature of the link
+     */
+    public Params params;
+    /**
+     * HTTP link to linked entity
      */
     public String href;
 
-    private class Self {
+    private class Params {
         /**
+         * Describes the nature of the link between the main entity and the entity in this link.
          * Cfr https://tools.ietf.org/html/rfc5988#section-6.2.2
          */
         public Relation rel;
+        /**
+         * General name of the entity that is linked to. This name can be used for display purposes and should be localized.
+         */
         public String title;
     }
 
