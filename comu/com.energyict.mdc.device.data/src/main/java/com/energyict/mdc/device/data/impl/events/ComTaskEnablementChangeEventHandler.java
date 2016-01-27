bbf15d211cfbeb7ed3ada8f45881c89ce1d5cbf2
sva <stijn.vanhoorelbeke@elster.com>
2016-01-27 11:09:42 +0100

    COMU-3300 Inbound communication tasks having 'Always execute for inbound' not picked up When creating a new ComTaskEnablement or updating an existing one, events will be published. The event handler which picks up these events will create a new message (in order to allow handling in background). MessageHandler ComTaskEnablementChangeMessageHandler will handle these messages, by first searching for all impacted devices and then - if the ComTaskExecution was marked as default one -  (in parallel) ComTaskExecution objects will be created.

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/events/ComTaskEnablementChangeEventHandler.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/events/ComTaskEnablementChangeEventHandler.java
@@ -2,13 +2,30 @@ package com.energyict.mdc.device.data.impl.events;
 
 import com.elster.jupiter.events.LocalEvent;
 import com.elster.jupiter.events.TopicHandler;
+import com.elster.jupiter.messaging.DestinationSpec;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.conditions.Condition;
 import com.energyict.mdc.device.config.ComTaskEnablement;
+import com.energyict.mdc.device.config.DeviceConfiguration;
+import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.data.DeviceFields;
+import com.energyict.mdc.device.data.ItemizeComTaskEnablementQueueMessage;
+import com.energyict.mdc.device.data.exceptions.NoDestinationSpecFound;
 import com.energyict.mdc.device.data.impl.DeviceDataModelService;
-import com.energyict.mdc.device.data.tasks.ComTaskExecutionFilterSpecification;
+import com.energyict.mdc.device.data.tasks.ComTaskExecution;
+import com.energyict.mdc.device.data.tasks.ComTaskExecutionFields;
 import com.energyict.mdc.scheduling.SchedulingService;
+import com.energyict.mdc.scheduling.model.ComSchedule;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.event.EventConstants;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.elster.jupiter.util.conditions.Where.where;
 
 @Component(name = "com.energyict.mdc.device.data.comtaskenablement.eventhandler", service = TopicHandler.class, immediate = true)
 public class ComTaskEnablementChangeEventHandler implements TopicHandler {
