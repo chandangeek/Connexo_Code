189087bf8bdb14a65fb35c67c7a407626fcf5882
Dragos Sinca <dragos.sinca@elster.com>
2016-01-27 13:04:19 +0200

    Clean up Tomcat sessions

--- copa/com.elster.jupiter.sso.connexo/src/main/java/com/elster/partners/connexo/filters/generic/ConnexoAuthenticationSSOFilter.java
+++ copa/com.elster.jupiter.sso.connexo/src/main/java/com/elster/partners/connexo/filters/generic/ConnexoAuthenticationSSOFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -41,8 +42,13 @@ public class ConnexoAuthenticationSSOFilter extends ConnexoAbstractSSOFilter {
                 updateToken(response, null, 0); // clear out token
             }
 
+            HttpSession session = request.getSession();
+            if(session != null){
+                session.invalidate();
+            }
+
             if(shouldUnauthorize(request)){
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
             else {
                 redirectToLogin(request, response);
