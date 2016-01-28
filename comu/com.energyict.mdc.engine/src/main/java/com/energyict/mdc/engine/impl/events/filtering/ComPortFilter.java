0a5db93e8b64166350a87f9c430f31817f2b65d1
sva <stijn.vanhoorelbeke@elster.com>
2016-01-28 15:42:35 +0100

    CXO-131 [Comserver monitor] ComPort related events are not shown in the monitor Addapted the filter to compare the ids instead of using java object compare

--- comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/events/filtering/ComPortFilter.java
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/events/filtering/ComPortFilter.java
@@ -1,8 +1,8 @@
 package com.energyict.mdc.engine.impl.events.filtering;
 
+import com.energyict.mdc.engine.config.ComPort;
 import com.energyict.mdc.engine.events.ComPortRelatedEvent;
 import com.energyict.mdc.engine.events.ComServerEvent;
-import com.energyict.mdc.engine.config.ComPort;
 
 import java.util.List;
 
@@ -35,7 +35,7 @@ public class ComPortFilter implements EventFilterCriterion {
     public boolean matches (ComServerEvent event) {
         if (event.isComPortRelated()) {
             ComPortRelatedEvent comPortEvent = (ComPortRelatedEvent) event;
-            return !this.comPorts.contains(comPortEvent.getComPort());
+            return !this.comPorts.stream().anyMatch(comPort -> comPort.getId() == comPortEvent.getComPort().getId());
         }
         else {
             return false;
