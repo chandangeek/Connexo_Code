3243b9d00b7a86766b7910b7522c7ef3d8af3ae5
sva <stijn.vanhoorelbeke@elster.com>
2016-02-02 14:26:28 +0100

    CXO-272 Rework general attribute 'Timezone' of device protocol + Included the list of possible values in the the timezonePropertySpec + Extended PredefinedPropertyValuesInfo with extra flag 'editable' - if set, then the ComboBox should be editable (~ the user should be allowed to type text in order to narrow down the list)

--- coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/PropertyUtils.java
+++ coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/PropertyUtils.java
@@ -85,7 +85,7 @@ public class PropertyUtils {
 
         PropertySelectionMode selectionMode = possibleValues.getSelectionMode();
 
-        return new PredefinedPropertyValuesInfo<>(possibleObjects, selectionMode, propertySpec.getPossibleValues().isExhaustive());
+        return new PredefinedPropertyValuesInfo<>(possibleObjects, selectionMode, propertySpec.getPossibleValues().isExhaustive(), propertySpec.getPossibleValues().isEditable());
     }
 
     public Object findPropertyValue(PropertySpec propertySpec, List<PropertyInfo> propertyInfos) {
