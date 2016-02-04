2939288cf785606cf9804b2b8e534ccabfbaadfe
Albert Versin <albertv@rimroe.elster-group.com>
2016-02-04 13:49:16 +0200

    Remove unnecessary comment.

--- copl/com.elster.jupiter.bpm.rest/src/main/java/com/elster/jupiter/bpm/rest/impl/BpmResource.java
+++ copl/com.elster.jupiter.bpm.rest/src/main/java/com/elster/jupiter/bpm/rest/impl/BpmResource.java
@@ -728,7 +728,6 @@ public class BpmResource {
         } catch (JSONException e) {
             throw new WebApplicationException(Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(this.errorInvalidMessage).build());
         } catch (RuntimeException e) {
-//            throw new WebApplicationException(Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(String.format(this.errorNotFoundMessage, e.getMessage())).build());
         }
         if(obj != null) {
             taskContentInfos = new TaskContentInfos(obj);
