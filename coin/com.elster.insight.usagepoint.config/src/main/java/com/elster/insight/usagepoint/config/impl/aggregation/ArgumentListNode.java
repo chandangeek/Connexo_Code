09a238165365ff3071e9d25aba32d825137faa68
igh <isabelle.gheysens@elster.com>
2016-02-05 08:38:26 +0100

    CXO-301 - Add an aggregated output readingtype by API

--- coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/aggregation/ArgumentListNode.java
+++ coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/aggregation/ArgumentListNode.java
@@ -1,14 +1,13 @@
 package com.elster.insight.usagepoint.config.impl.aggregation;
 
+import java.util.List;
+
 /**
  * Created by igh on 4/02/2016.
  */
 public class ArgumentListNode implements ExpressionNode {
 
-    private ExpressionNode leftExpression;
-    private ArgumentListNode argumentList;
-    //used for the right node if it contains 1 element (we don't use an ArgumentListNode then, would be too much overhead)
-    private ExpressionNode rightExpression;
+    private List<ExpressionNode> expression;
 
     @Override
     public void accept(Visitor visitor) {
