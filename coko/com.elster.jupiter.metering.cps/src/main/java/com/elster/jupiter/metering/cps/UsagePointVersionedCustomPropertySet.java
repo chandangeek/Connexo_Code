63e47908d3f789a1f9f3557973866fa729f7e6ca
Anton Fomchenko <antfom@mera.ru>
2016-02-05 17:14:32 +0300

    CXO-132 initial commit

---
+++ coko/com.elster.jupiter.metering.cps/src/main/java/com/elster/jupiter/metering/cps/UsagePointVersionedCustomPropertySet.java
@@ -0,0 +1,205 @@
+package com.elster.jupiter.metering.cps;
+
+import com.elster.jupiter.cps.CustomPropertySet;
+import com.elster.jupiter.cps.EditPrivilege;
+import com.elster.jupiter.cps.PersistenceSupport;
+import com.elster.jupiter.cps.ViewPrivilege;
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.orm.Column;
+import com.elster.jupiter.orm.Table;
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.PropertySpecService;
+import com.google.inject.Module;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Component(name = "com.elster.jupiter.metering.cps.UsagePointVersionedCustomPropertySet", service = CustomPropertySet.class, immediate = true)
+@SuppressWarnings("unused")
+public class UsagePointVersionedCustomPropertySet implements CustomPropertySet<UsagePoint, UsagePointVersionedDomainExtension> {
+
+    public static final String TABLE_NAME = "RVK_CPS_USAGEPOINT_VER";
+    public static final String FK_CPS_DEVICE_ONE = "FK_CPS_USAGEPOINT_VER";
+
+    public volatile PropertySpecService propertySpecService;
+    public volatile MeteringService meteringService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    public void setMeteringService(MeteringService meteringService) {
+        this.meteringService = meteringService;
+    }
+
+    @Reference
+    public void setPropertySpecService(PropertySpecService propertySpecService) {
+        this.propertySpecService = propertySpecService;
+    }
+
+
+    public UsagePointVersionedCustomPropertySet() {
+        super();
+    }
+
+    @Inject
+    public UsagePointVersionedCustomPropertySet(PropertySpecService propertySpecService, MeteringService meteringService) {
+        this();
+        this.setPropertySpecService(propertySpecService);
+        this.setMeteringService(meteringService);
+    }
+
+    @Activate
+    public void activate() {
+        System.out.println(TABLE_NAME);
+    }
+
+    @Override
+    public String getName() {
+        return UsagePointVersionedCustomPropertySet.class.getSimpleName();
+    }
+
+    @Override
+    public Class<UsagePoint> getDomainClass() {
+        return UsagePoint.class;
+    }
+
+    @Override
+    public PersistenceSupport<UsagePoint, UsagePointVersionedDomainExtension> getPersistenceSupport() {
+        return new UsagePointVerPeristenceSupport();
+    }
+
+    @Override
+    public boolean isRequired() {
+        return false;
+    }
+
+    @Override
+    public boolean isVersioned() {
+        return true;
+    }
+
+    @Override
+    public Set<ViewPrivilege> defaultViewPrivileges() {
+        return EnumSet.allOf(ViewPrivilege.class);
+    }
+
+    @Override
+    public Set<EditPrivilege> defaultEditPrivileges() {
+        return EnumSet.allOf(EditPrivilege.class);
+    }
+
+    @Override
+    public List<PropertySpec> getPropertySpecs() {
+        return Arrays.asList(
+                this.propertySpecService
+                        .bigDecimalSpec()
+                        .named(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_NUMBER.javaName(), UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_NUMBER.javaName())
+                        .describedAs("kw")
+                        .setDefaultValue(BigDecimal.ZERO)
+                        .markRequired()
+                        .finish(),
+                this.propertySpecService
+                        .stringSpec()
+                        .named(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_STRING.javaName(), UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_STRING.javaName())
+                        .describedAs("infoString")
+                        .setDefaultValue("description")
+                        .finish(),
+                this.propertySpecService
+                        .bigDecimalSpec()
+                        .named(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_NUMBER.javaName(), UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_NUMBER.javaName())
+                        .describedAs("A")
+                        .addValues(BigDecimal.valueOf(7L), BigDecimal.valueOf(77L), BigDecimal.valueOf(777L))
+                        .setDefaultValue(BigDecimal.valueOf(77L))
+                        .finish(),
+                this.propertySpecService
+                        .stringSpec()
+                        .named(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_STRING.javaName(), UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_STRING.javaName())
+                        .describedAs("infoEnumString")
+                        .addValues("alfa", "beta", "gamma")
+                        .setDefaultValue("gamma")
+                        .finish(),
+                this.propertySpecService
+                        .booleanSpec()
+                        .named(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_BOOLEAN.javaName(), UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_BOOLEAN.javaName())
+                        .describedAs("flag")
+                        .setDefaultValue(false)
+                        .finish());
+    }
+
+    private static class UsagePointVerPeristenceSupport implements PersistenceSupport<UsagePoint, UsagePointVersionedDomainExtension> {
+        @Override
+        public String componentName() {
+            return "CPM3";
+        }
+
+        @Override
+        public String tableName() {
+            return TABLE_NAME;
+        }
+
+        @Override
+        public String domainFieldName() {
+            return UsagePointVersionedDomainExtension.FieldNames.DOMAIN.javaName();
+        }
+
+        @Override
+        public String domainForeignKeyName() {
+            return FK_CPS_DEVICE_ONE;
+        }
+
+        @Override
+        public Class<UsagePointVersionedDomainExtension> persistenceClass() {
+            return UsagePointVersionedDomainExtension.class;
+        }
+
+        @Override
+        public Optional<Module> module() {
+            return Optional.empty();
+        }
+
+        @Override
+        public List<Column> addCustomPropertyPrimaryKeyColumnsTo(Table table) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public void addCustomPropertyColumnsTo(Table table, List<Column> customPrimaryKeyColumns) {
+            table
+                    .column(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_NUMBER.databaseName())
+                    .number()
+                    .map(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_NUMBER.javaName())
+                    .notNull()
+                    .add();
+            table
+                    .column(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_STRING.databaseName())
+                    .varChar()
+                    .map(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_STRING.javaName())
+                    .notNull()
+                    .add();
+            table
+                    .column(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_NUMBER.databaseName())
+                    .number()
+                    .map(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_NUMBER.javaName())
+                    .add();
+            table
+                    .column(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_STRING.databaseName())
+                    .varChar()
+                    .map(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_ENUM_STRING.javaName())
+                    .add();
+            table
+                    .column(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_BOOLEAN.databaseName())
+                    .bool()
+                    .map(UsagePointVersionedDomainExtension.FieldNames.TEST_ATTRIBUTE_BOOLEAN.javaName())
+                    .add();
+        }
+    }
+}
---
+++ coko/com.elster.jupiter.metering.cps/src/main/java/com/elster/jupiter/metering/cps/impl/MeteringCustomPropertySetsDemoInstaller.java
@@ -0,0 +1,92 @@
+package com.elster.jupiter.metering.cps.impl;
+
+import com.elster.jupiter.cps.CustomPropertySetService;
+import com.elster.jupiter.cps.RegisteredCustomPropertySet;
+import com.elster.jupiter.cps.impl.ServerCustomPropertySetService;
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.ServiceCategory;
+import com.elster.jupiter.metering.ServiceKind;
+import com.elster.jupiter.metering.cps.UsagePointOneCustomPropertySet;
+import com.elster.jupiter.metering.cps.UsagePointOneDomainExtension;
+import com.elster.jupiter.metering.cps.UsagePointTwoDomainExtension;
+import com.elster.jupiter.metering.cps.UsagePointVersionedDomainExtension;
+import com.elster.jupiter.nls.TranslationKeyProvider;
+import com.elster.jupiter.orm.UnderlyingSQLFailedException;
+import com.elster.jupiter.orm.callback.InstallService;
+import com.elster.jupiter.transaction.CommitException;
+import com.elster.jupiter.transaction.TransactionContext;
+import com.elster.jupiter.transaction.TransactionService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component(name = "com.elster.jupiter.metering.cps", service = {InstallService.class}, property = "name=CPSM")
+public class MeteringCustomPropertySetsDemoInstaller implements InstallService {
+
+    private volatile MeteringService meteringService;
+    private volatile CustomPropertySetService customPropertySetService;
+    private volatile TransactionService transactionService;
+
+    private ServiceCategory electricity;
+    private ServiceCategory gas;
+    private ServiceCategory water;
+    private ServiceCategory internet;
+
+    @Reference
+    public void setCustomPropertySetService(CustomPropertySetService customPropertySetService) {
+        this.customPropertySetService = customPropertySetService;
+    }
+
+    @Reference
+    public void setMeteringService(MeteringService meteringService) {
+        this.meteringService = meteringService;
+    }
+
+    @Reference
+    public void setTransactionService(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @Override
+    public void install() {
+
+        electricity = meteringService.getServiceCategory(ServiceKind.ELECTRICITY).get();
+        customPropertySetService.findActiveCustomPropertySet(UsagePointOneDomainExtension.class.getName()).ifPresent(this::addElecttricitySets);
+        customPropertySetService.findActiveCustomPropertySet(UsagePointTwoDomainExtension.class.getName()).ifPresent(this::addElecttricitySets);
+        customPropertySetService.findActiveCustomPropertySet(UsagePointVersionedDomainExtension.class.getName()).ifPresent(this::addElecttricitySets);
+
+        gas = meteringService.getServiceCategory(ServiceKind.GAS).get();
+        customPropertySetService.findActiveCustomPropertySet(UsagePointOneDomainExtension.class.getName()).ifPresent(this::addGasSets);
+        customPropertySetService.findActiveCustomPropertySet(UsagePointVersionedDomainExtension.class.getName()).ifPresent(this::addGasSets);
+
+        water = meteringService.getServiceCategory(ServiceKind.WATER).get();
+        customPropertySetService.findActiveCustomPropertySet(UsagePointOneDomainExtension.class.getName()).ifPresent(this::addWaterSets);
+        customPropertySetService.findActiveCustomPropertySet(UsagePointTwoDomainExtension.class.getName()).ifPresent(this::addWaterSets);
+
+        internet = meteringService.getServiceCategory(ServiceKind.INTERNET).get();
+        customPropertySetService.findActiveCustomPropertySet(UsagePointTwoDomainExtension.class.getName()).ifPresent(this::addInternetSets);
+    }
+
+    private void addElecttricitySets(RegisteredCustomPropertySet registeredCustomPropertySet) {
+        electricity.addCustomPropertySet(registeredCustomPropertySet);
+    }
+
+    private void addGasSets(RegisteredCustomPropertySet registeredCustomPropertySet) {
+        gas.addCustomPropertySet(registeredCustomPropertySet);
+    }
+
+    private void addWaterSets(RegisteredCustomPropertySet registeredCustomPropertySet) {
+        water.addCustomPropertySet(registeredCustomPropertySet);
+    }
+
+    private void addInternetSets(RegisteredCustomPropertySet registeredCustomPropertySet) {
+        internet.addCustomPropertySet(registeredCustomPropertySet);
+    }
+
+    @Override
+    public List<String> getPrerequisiteModules() {
+        return Arrays.asList("CPS", "MTR");
+    }
+}
