80c5a282cf45cfd7d53a97dd854681fb3eb022a6
Sentyurin Sergey <ssentyur@mera.ru>
2016-02-05 16:11:00 +0300

    CXO-187 View service categories

--- coin/com.elster.insight.app/src/main/web/js/insight/app/Application.js
+++ coin/com.elster.insight.app/src/main/web/js/insight/app/Application.js
@@ -1,6 +1,10 @@
 Ext.define('InsightApp.Application', {
     extend: 'Ext.app.Application',
 
+    requires:[
+        'Imt.privileges.ServiceCategory'
+    ],
+
     controllers: [
         'InsightApp.controller.Main'
     ],
