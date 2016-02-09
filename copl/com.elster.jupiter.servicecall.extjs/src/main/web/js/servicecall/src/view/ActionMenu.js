ffb443901cd0d2e0f3dd851cfade9f64aa7c1d56
TVN <thomas.vandebuerie@elster.com>
2016-02-09 16:53:03 +0100

    CXO-441 - Service call overview in MultiSense

--- copl/com.elster.jupiter.servicecall.extjs/src/main/web/js/servicecall/src/view/ActionMenu.js
+++ copl/com.elster.jupiter.servicecall.extjs/src/main/web/js/servicecall/src/view/ActionMenu.js
@@ -4,30 +4,56 @@ Ext.define('Scs.view.ActionMenu', {
     plain: true,
     border: false,
     shadow: false,
+    record: null,
     items: [
         {
             itemId: 'cancel-scs',
             text: Uni.I18n.translate('general.cancel', 'SCS', 'Cancel'),
             //privileges: Apr.privileges.AppServer.admin,
-            action: 'cancel'
+            action: 'cancel',
+            visible: function() {
+                //you can access this.record here! -> check if the cancel option is possible. Same for other options.
+                return false;
+            }
         },
         {
             itemId: 'resume-scs',
             text: Uni.I18n.translate('general.resume', 'SCS', 'Resume'),
             //privileges: Apr.privileges.AppServer.admin,
-            action: 'resume'
+            action: 'resume',
+            visible: function() {
+                return true;
+            }
         },
         {
             itemId: 'pause-scs',
             text: Uni.I18n.translate('general.pause', 'SCS', 'Pause'),
             //privileges: Apr.privileges.AppServer.admin,
-            action: 'pause'
+            action: 'pause',
+            visible: function() {
+                return false;
+            }
         },
         {
             itemId: 'retry-scs',
             text: Uni.I18n.translate('general.retry', 'SCS', 'Retry'),
             //privileges: Apr.privileges.AppServer.admin,
-            action: 'retry'
+            action: 'retry',
+            visible: function() {
+                return true;
+            }
         }
-    ]
+    ],
+    listeners: {
+        beforeshow: function() {
+            var me = this;
+            me.items.each(function(item){
+                if (item.visible === undefined) {
+                    item.show();
+                } else {
+                    item.visible.call(me) ?  item.show() : item.hide(); //hier nog privileges in de check?
+                }
+            })
+        }
+    }
 });
