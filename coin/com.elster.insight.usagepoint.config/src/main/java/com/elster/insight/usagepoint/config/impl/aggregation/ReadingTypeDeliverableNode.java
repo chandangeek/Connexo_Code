81a9f1e69aa5dccd292a521dda295a0229fefd25
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-02-09 12:54:10 +0100

    CXO-161: Fix NPE due to uninitialized Reference

--- coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/aggregation/ReadingTypeDeliverableNode.java
+++ coin/com.elster.insight.usagepoint.config/src/main/java/com/elster/insight/usagepoint/config/impl/aggregation/ReadingTypeDeliverableNode.java
@@ -1,7 +1,9 @@
 package com.elster.insight.usagepoint.config.impl.aggregation;
 
-import com.elster.insight.usagepoint.config.ReadingTypeDeliverable;
 import com.elster.jupiter.orm.associations.Reference;
+import com.elster.jupiter.orm.associations.ValueReference;
+
+import com.elster.insight.usagepoint.config.ReadingTypeDeliverable;
 
 /**
  * Created by igh on 4/02/2016.
@@ -10,7 +12,7 @@ public class ReadingTypeDeliverableNode extends AbstractNode {
 
     static final String TYPE_IDENTIFIER = "DEL";
 
-    private Reference<ReadingTypeDeliverable> readingTypeDeliverable;
+    private Reference<ReadingTypeDeliverable> readingTypeDeliverable = ValueReference.absent();
 
     public ReadingTypeDeliverableNode(ReadingTypeDeliverable readingTypeDeliverable) {
         super();
