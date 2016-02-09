4c430d06d68f0ff8281aa445797cdd329c3f5416
TVN <thomas.vandebuerie@elster.com>
2016-02-09 10:54:34 +0100

    CXO-438	 - View service call types

--- copl/com.elster.jupiter.servicecalltype.extjs/src/main/web/js/servicecalltype/src/store/ServiceCallTypes.js
+++ copl/com.elster.jupiter.servicecalltype.extjs/src/main/web/js/servicecalltype/src/store/ServiceCallTypes.js
@@ -1,5 +1,6 @@
 Ext.define('Sct.store.ServiceCallTypes', {
     extend: 'Ext.data.Store',
+    //model: 'Sct.model.ServiceCallType',
     autoLoad: false,
     /*proxy: {
         type: 'rest',
@@ -21,6 +22,22 @@ Ext.define('Sct.store.ServiceCallTypes', {
         {name: 'id', type: 'int'}
     ],
     data: [
-        {type: 'SAP', versionName: '15', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '2'}
+        {type: 'SAP', versionName: '15', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '2'},
+        {type: 'SAP', versionName: '17', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '3'},
+        {type: 'SAP', versionName: '18', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '4'},
+        {type: 'SAP', versionName: '19', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '5'},
+        {type: 'SAP', versionName: '20', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '6'},
+        {type: 'SAP', versionName: '21', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '7'},
+        {type: 'SAP', versionName: '22', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '8'},
+        {type: 'SAP', versionName: '23', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '9'},
+        {type: 'SAP', versionName: '24', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '10'},
+        {type: 'SAP', versionName: '25', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '11'},
+        {type: 'SAP', versionName: '26', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '12'},
+        {type: 'SAP', versionName: '27', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '13'},
+        {type: 'SAP', versionName: '28', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '14'},
+        {type: 'SAP', versionName: '29', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '15'},
+        {type: 'SAP', versionName: '30', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '16'},
+        {type: 'SAP', versionName: '31', status: 'active', loglevel: 'Warning', lifecycle: 'default', version: '204', id: '17'}
+
     ]
 });
