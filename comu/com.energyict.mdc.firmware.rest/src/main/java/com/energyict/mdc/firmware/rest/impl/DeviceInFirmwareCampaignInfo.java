8a7704883addd5f39bd3873121d1d514f2f4d0ef
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-02-11 14:20:28 +0100

    CXO-484: Moved IdWithNameInfo to rest.util

--- comu/com.energyict.mdc.firmware.rest/src/main/java/com/energyict/mdc/firmware/rest/impl/DeviceInFirmwareCampaignInfo.java
+++ comu/com.energyict.mdc.firmware.rest/src/main/java/com/energyict/mdc/firmware/rest/impl/DeviceInFirmwareCampaignInfo.java
@@ -1,13 +1,14 @@
 package com.energyict.mdc.firmware.rest.impl;
 
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.rest.util.IdWithNameInfo;
 import com.energyict.mdc.firmware.DeviceInFirmwareCampaign;
 
 import java.time.Instant;
 
 public class DeviceInFirmwareCampaignInfo {
     public String mrid;
-    public com.energyict.mdc.common.rest.IdWithNameInfo status;
+    public IdWithNameInfo status;
     public Instant startedOn;
     public Instant finishedOn;
 
@@ -17,7 +18,7 @@ public class DeviceInFirmwareCampaignInfo {
     public DeviceInFirmwareCampaignInfo(DeviceInFirmwareCampaign device, Thesaurus thesaurus) {
         this.mrid = device.getDevice().getmRID();
         if (device.getStatus() != null) {
-            this.status = new com.energyict.mdc.common.rest.IdWithNameInfo();
+            this.status = new IdWithNameInfo();
             this.status.id = device.getStatus().key();
             this.status.name = thesaurus.getString(MessageSeeds.Keys.FIRMWARE_MANAGEMENT_DEVICE_STATUS_PREFIX + device.getStatus().key(), device.getStatus().key());
         }
