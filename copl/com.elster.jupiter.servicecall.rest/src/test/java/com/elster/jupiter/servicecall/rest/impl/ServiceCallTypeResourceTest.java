a063d207f8e0996627be8c758ee41ab79feed64b
TVN <thomas.vandebuerie@elster.com>
2016-02-11 15:24:13 +0100

    CXO-97

--- copl/com.elster.jupiter.servicecall.rest/src/test/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallTypeResourceTest.java
+++ copl/com.elster.jupiter.servicecall.rest/src/test/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallTypeResourceTest.java
@@ -1,6 +1,7 @@
 package com.elster.jupiter.servicecall.rest.impl;
 
 import com.elster.jupiter.domain.util.Finder;
+import com.elster.jupiter.servicecall.ServiceCallLifeCycle;
 import com.elster.jupiter.servicecall.ServiceCallType;
 import com.jayway.jsonpath.JsonModel;
 import org.junit.Test;
@@ -8,8 +9,10 @@ import org.junit.Test;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,6 +28,7 @@ public class ServiceCallTypeResourceTest extends ServiceCallApplicationTest {
         when(serviceCallType.getName()).thenReturn("Mbus 1");
         Finder<ServiceCallType> serviceCallTypeFinder = mockFinder(Collections.singletonList(serviceCallType));
         when(serviceCallService.getServiceCallTypes()).thenReturn(serviceCallTypeFinder);
+        when(serviceCallType.getServiceCallLifeCycle()).thenReturn(Optional.empty());
 
         Response response = target("/servicecalltypes").request().get();
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
