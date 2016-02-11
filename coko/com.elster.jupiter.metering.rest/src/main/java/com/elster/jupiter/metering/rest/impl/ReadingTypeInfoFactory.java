a0dd5a6bb9051bdd89cccf63ed2865538b3cb30f
Anton Fomchenko <antfom@mera.ru>
2016-02-11 16:01:24 +0300

    CXO-125 add unit symbol to reading type unit display value

--- coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/ReadingTypeInfoFactory.java
+++ coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/ReadingTypeInfoFactory.java
@@ -4,6 +4,7 @@ import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.metering.rest.ReadingTypeInfo;
 import com.elster.jupiter.metering.rest.ReadingTypeInfos;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.units.Unit;
 
 import javax.inject.Inject;
 import java.util.Locale;
@@ -27,6 +28,8 @@ public class ReadingTypeInfoFactory {
         info.measurementKind = thesaurus.getString("readingType.measurementKind."+readingType.getMeasurementKind().name(),readingType.getMeasurementKind().getDescription());
         info.phases = thesaurus.getString("readingType.phase."+readingType.getPhases().name(),readingType.getPhases().getBaseDescription());
         info.unit = thesaurus.getString("readingType.unit."+readingType.getUnit().name()+".name",readingType.getUnit().getUnit().getName());
+        info.unit = thesaurus.getString("readingType.unit." + readingType.getUnit().name() + ".name", readingType.getUnit().getUnit().getName()).concat(
+                !readingType.getUnit().getUnit().equals(Unit.UNITLESS) ? (" (" + thesaurus.getString("readingType.unit." + readingType.getUnit().name(), readingType.getUnit().getUnit().getSymbol()) + ")") : "");
         info.currency = thesaurus.getString("readingType.currency."+readingType.getCurrency().getCurrencyCode(),readingType.getCurrency().getDisplayName(Locale.ENGLISH));
         return info;
     }
