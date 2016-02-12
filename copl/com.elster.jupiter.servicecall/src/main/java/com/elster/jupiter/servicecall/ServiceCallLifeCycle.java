da76c9e77d6ebf49a8c32341e1a1b1953bd9f78c
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-02-12 09:36:07 +0100

    CXO-97: LifeCycle now extends HasId and HasName

--- copl/com.elster.jupiter.servicecall/src/main/java/com/elster/jupiter/servicecall/ServiceCallLifeCycle.java
+++ copl/com.elster.jupiter.servicecall/src/main/java/com/elster/jupiter/servicecall/ServiceCallLifeCycle.java
@@ -1,10 +1,10 @@
 package com.elster.jupiter.servicecall;
 
+import com.elster.jupiter.util.HasId;
+import com.elster.jupiter.util.HasName;
+
 /**
  * This is the wrapper class for FSM in service call bundle
  */
-public interface ServiceCallLifeCycle {
-    long getId();
-
-    String getName();
+public interface ServiceCallLifeCycle extends HasId, HasName {
 }
