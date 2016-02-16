99f261d96adef447382c925bb6518e4252206877
govanni <govanni.naessens@elster.com>
2016-02-16 09:54:13 +0100

    feature/CXO-401 removed the validation of at least one protocoltask per ComTask

--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/MessageSeeds.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/MessageSeeds.java
@@ -23,7 +23,6 @@ public enum MessageSeeds implements MessageSeed {
     TIMEDURATION_IS_NULL(8, Keys.TIMEDURATION_MUST_BE_POSITIVE, "Field must denote non-0 duration"),
     SIZE_TOO_LONG(9, Keys.SIZE_TOO_LONG, "Field exceeds max size of {max} characters"),
     DUPLICATE_COMTASK_NAME(10, Keys.DUPLICATE_COMTASK_NAME, "A ComTask by this name already exists"),
-    PROTOCOL_TASK_REQUIRED(11, Keys.COMTASK_WITHOUT_PROTOCOLTASK, "You have to add at least one action or you have to select at least one command category"),
     FIELD_SIZE_INCORRECT(12, Keys.FIELD_TOO_LONG, "Field size should be exceed {max} characters"),
     DUPLICATE_PROTOCOL_TASK_TYPE_IN_COMTASK(13, Keys.DUPLICATE_PROTOCOL_TASK_TYPE_IN_COM_TASK, "ComTask contains multiple ProtocolTasks of the same type"),
     VETO_LOG_BOOK_TYPE_DELETION(14, Keys.VETO_LOG_BOOK_TYPE_DELETION, "Log book type ''{0}'' is still in use by the following communication task(s): ''{1}''"),
@@ -83,7 +82,6 @@ public enum MessageSeeds implements MessageSeed {
         public static final String TIMEDURATION_MUST_BE_POSITIVE = TaskService.COMPONENT_NAME+".TimeDurationMustBePositive";
         public static final String SIZE_TOO_LONG = TaskService.COMPONENT_NAME+".MaxSizeExceeded";
         public static final String DUPLICATE_COMTASK_NAME = TaskService.COMPONENT_NAME + ".comTask.name.duplicated";
-        public static final String COMTASK_WITHOUT_PROTOCOLTASK = TaskService.COMPONENT_NAME + ".comTask.requiresProtocolTask";
         public static final String DUPLICATE_PROTOCOL_TASK_TYPE_IN_COM_TASK = TaskService.COMPONENT_NAME + ".duplicateProtocolTaskInComTask";
         public static final String CLOCK_TASK_TYPE_SET_CLOCK = TaskService.COMPONENT_NAME + ".clockTaskType.setClock";
         public static final String CLOCK_TASK_TYPE_FORCE_CLOCK = TaskService.COMPONENT_NAME + ".clockTaskType.forceClock";
