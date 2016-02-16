b9fba261ae98e5d7c08d2b96a0ceca7d9c337af4
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-02-16 15:47:27 +0100

    CXO-398: Updated doc

--- comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/DeviceConfigurationResource.java
+++ comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/DeviceConfigurationResource.java
@@ -56,13 +56,15 @@ public class DeviceConfigurationResource {
      * @param deviceTypeId Id of the devuce type
      * @param id Id of the device configuration
      * @param uriInfo uriInfo
+     * @param fields fields
      * @return Uniquely identified device configuration
      */
     @GET @Transactional
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     @Path("/{deviceConfigId}")
     @RolesAllowed({com.energyict.mdc.multisense.api.security.Privileges.Constants.PUBLIC_REST_API})
-    public DeviceConfigurationInfo getDeviceConfiguration(@PathParam("deviceTypeId") long deviceTypeId, @PathParam("deviceConfigId") long id, @BeanParam FieldSelection fields, @Context UriInfo uriInfo) {
+    public DeviceConfigurationInfo getDeviceConfiguration(@PathParam("deviceTypeId") long deviceTypeId, @PathParam("deviceConfigId") long id,
+                                                          @BeanParam FieldSelection fields, @Context UriInfo uriInfo) {
         DeviceConfigurationInfo deviceConfigurationInfo = deviceConfigurationService.
                 findDeviceType(deviceTypeId)
                 .orElseThrow(exceptionFactory.newExceptionSupplier(Response.Status.NOT_FOUND, MessageSeeds.NO_SUCH_DEVICE_TYPE)).
@@ -84,13 +86,15 @@ public class DeviceConfigurationResource {
      * @param deviceTypeId Id of the devuce type
      * @param uriInfo uriInfo
      * @param queryParameters queryParameters
+     * @param fields fields
      * @return a sorted, pageable list of elements. Only fields mentioned in field-param will be provided, or all fields if no
      * field-param was provided. The list will be sorted according to db order.
      */
     @GET @Transactional
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     @RolesAllowed({com.energyict.mdc.multisense.api.security.Privileges.Constants.PUBLIC_REST_API})
-    public PagedInfoList<DeviceConfigurationInfo> getHypermediaDeviceConfigurations(@PathParam("deviceTypeId") long deviceTypeId, @BeanParam JsonQueryParameters queryParameters,
+    public PagedInfoList<DeviceConfigurationInfo> getHypermediaDeviceConfigurations(@PathParam("deviceTypeId") long deviceTypeId,
+                                                                                    @BeanParam JsonQueryParameters queryParameters,
                                                            @BeanParam FieldSelection fields,@Context UriInfo uriInfo) {
         List<DeviceConfiguration> allDeviceConfigurations = deviceConfigurationService.
                 findDeviceType(deviceTypeId)
