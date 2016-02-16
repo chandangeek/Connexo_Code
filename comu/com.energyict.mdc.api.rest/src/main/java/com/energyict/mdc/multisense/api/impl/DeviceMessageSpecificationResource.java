19c7b883493f73466834595f2a43183ea99f5c8b
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-02-16 10:08:35 +0100

    CXO-398: Added device message specification doc

--- comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/DeviceMessageSpecificationResource.java
+++ comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/DeviceMessageSpecificationResource.java
@@ -87,7 +87,7 @@ public class DeviceMessageSpecificationResource {
      * Any DeviceMessageSpec that is created through the ComServer
      * API will by default be a non-standard DeviceMessage.
      * Note that non standard message can still be part
-     * of standard {@link DeviceMessageCategory DeviceMessageCategories}.
+     * of standard device message category.
      *
      * @summary Get a set of device message specifications
      *
