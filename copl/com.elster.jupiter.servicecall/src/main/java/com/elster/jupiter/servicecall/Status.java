662b91778402769d0c000209e417c7dff06af8f5
TVN <thomas.vandebuerie@elster.com>
2016-02-17 14:26:49 +0100

    CXO-438 - Status is now correctly displayed with a translation (changes in FE & BE)

--- copl/com.elster.jupiter.servicecall/src/main/java/com/elster/jupiter/servicecall/Status.java
+++ copl/com.elster.jupiter.servicecall/src/main/java/com/elster/jupiter/servicecall/Status.java
@@ -1,9 +1,41 @@
 package com.elster.jupiter.servicecall;
 
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.TranslationKey;
+
 /**
  * Created by bvn on 2/10/16.
  */
-public enum Status {
-    ACTIVE,
-    DEPRECATED
+public enum Status implements TranslationKey {
+    ACTIVE("Active"),
+    DEPRECATED("Deprecated");
+
+    private String name;
+
+    Status(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName(Thesaurus thesaurus) {
+        return thesaurus.getString(getTranslationKey(this), this.getDefaultFormat());
+    }
+
+    public static String getTranslationKey(Status status){
+        return "servicecalltype.status." + status.name().toLowerCase();
+    }
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getDefaultFormat() {
+        return toString();
+    }
+
 }
