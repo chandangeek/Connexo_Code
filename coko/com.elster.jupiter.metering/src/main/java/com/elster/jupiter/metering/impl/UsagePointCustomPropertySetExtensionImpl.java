d93a865fae7b29f9a2960b659354b7a08ded8a9a
Anton Suslennikov <antsusl@mera.ru>
2016-02-18 17:46:07 +0300

    Add time-sliced versions

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/UsagePointCustomPropertySetExtensionImpl.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/UsagePointCustomPropertySetExtensionImpl.java
@@ -9,6 +9,7 @@ import com.elster.jupiter.metering.UsagePointCustomPropertySetExtension;
 import com.elster.jupiter.metering.UsagePointCustomPropertySetValuesManageException;
 import com.elster.jupiter.metering.config.MetrologyConfiguration;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.time.RangeInstantComparator;
 import com.google.common.collect.Range;
 
 import javax.inject.Inject;
@@ -16,7 +17,6 @@ import java.time.Clock;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -195,22 +195,9 @@ public class UsagePointCustomPropertySetExtensionImpl implements UsagePointCusto
                 .stream()
                 .map(CustomPropertySetValues::getEffectiveRange)
                 .distinct()
-                .sorted(new RangeComparator())
+                .sorted(new RangeInstantComparator())
                 .collect(Collectors.toList());
         Collections.reverse(versionIntervals);
         return versionIntervals;
-    }
-
-    static class RangeComparator implements Comparator<Range<Instant>> {
-
-        @Override
-        public int compare(Range<Instant> firstRange, Range<Instant> secondRange) {
-            long firstLowerEndpoint = firstRange.hasLowerBound() ? firstRange.lowerEndpoint().toEpochMilli() : 0;
-            long firstUpperEndpoint = firstRange.hasUpperBound() ? firstRange.upperEndpoint().toEpochMilli() : Long.MAX_VALUE;
-            long secondLowerEndpoint = secondRange.hasLowerBound() ? secondRange.lowerEndpoint().toEpochMilli() : 0;
-            long secondUpperEndpoint = secondRange.hasUpperBound() ? secondRange.upperEndpoint().toEpochMilli() : Long.MAX_VALUE;
-
-            return Long.compare(firstLowerEndpoint, secondLowerEndpoint) != 0 ? Long.compare(firstLowerEndpoint, secondLowerEndpoint) : Long.compare(firstUpperEndpoint, secondUpperEndpoint);
-        }
     }
 }
