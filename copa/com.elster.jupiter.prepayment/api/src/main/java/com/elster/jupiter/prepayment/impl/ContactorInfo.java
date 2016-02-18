1071db17b7976eb79d629f321213d4e570f8f778
sva <stijn.vanhoorelbeke@elster.com>
2016-02-18 13:19:24 +0100

    CXO-124 Add load limitation support to communication protocol: A1440 + Added load limitation registers and messages to the ICE1107 A1440 protocol + Refactored the prepayment application, so that based on the given ContactorInfo the necessary contactor and load limitation messages are scheduled

--- copa/com.elster.jupiter.prepayment/api/src/main/java/com/elster/jupiter/prepayment/impl/ContactorInfo.java
+++ copa/com.elster.jupiter.prepayment/api/src/main/java/com/elster/jupiter/prepayment/impl/ContactorInfo.java
@@ -1,15 +1,18 @@
 package com.elster.jupiter.prepayment.impl;
 
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.math.BigDecimal;
 import java.time.Instant;
 
 /**
  * Created by bvn on 9/18/15.
  */
 public class ContactorInfo {
+
     public Status status;
-    public Integer loadLimit;
     public Instant activationDate;
+    public BigDecimal loadLimit;
     public Integer loadTolerance;
+    public Integer[] tariffs;
+    public String readingType;
     public String callback;
 }
