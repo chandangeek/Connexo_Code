ca9660b377472ba6c4cb77455fb56f628fb754ad
Geert <geert.delmeiren@elster.com>
2016-02-22 15:59:54 +0100

    CXO-598 - No 'Load profile', 'Registers' and 'Logbooks' shown in combobox when adding an action to a communication task

--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/MessageSeeds.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/MessageSeeds.java
@@ -16,21 +16,17 @@ public enum MessageSeeds implements MessageSeed {
     SET_CLOCK(1, Keys.CLOCK_TASK_TYPE_SET_CLOCK, "Set the clock"),
     FORCE_CLOCK(2, Keys.CLOCK_TASK_TYPE_FORCE_CLOCK, "Force the clock"),
     SYNC_CLOCK(3, Keys.CLOCK_TASK_TYPE_SYNCHRONIZE_CLOCK, "Synchronize the clock"),
-    SHOULD_BE_AT_LEAST(4, Keys.VALUE_TOO_SMALL, "Minimal acceptable value is {min}, was {value}"),
     CAN_NOT_BE_EMPTY(5, Keys.CAN_NOT_BE_EMPTY, "This field is required"),
     MIN_ABOVE_MAX(6, Keys.MIN_MUST_BE_BELOW_MAX, "Invalid range: minimum value exceeds maximum value"),
     MIN_EQUALS_MAX(7, Keys.MIN_EQUALS_MAX, "Invalid range: minimum value equals maximum value "),
     TIMEDURATION_IS_NULL(8, Keys.TIMEDURATION_MUST_BE_POSITIVE, "Field must denote non-0 duration"),
-    SIZE_TOO_LONG(9, Keys.SIZE_TOO_LONG, "Field exceeds max size of {max} characters"),
     DUPLICATE_COMTASK_NAME(10, Keys.DUPLICATE_COMTASK_NAME, "A ComTask by this name already exists"),
-    FIELD_SIZE_INCORRECT(12, Keys.FIELD_TOO_LONG, "Field size should be exceed {max} characters"),
     DUPLICATE_PROTOCOL_TASK_TYPE_IN_COMTASK(13, Keys.DUPLICATE_PROTOCOL_TASK_TYPE_IN_COM_TASK, "ComTask contains multiple ProtocolTasks of the same type"),
     VETO_LOG_BOOK_TYPE_DELETION(14, Keys.VETO_LOG_BOOK_TYPE_DELETION, "Log book type ''{0}'' is still in use by the following communication task(s): ''{1}''"),
     VETO_LOAD_PROFILE_TYPE_DELETION(15, Keys.VETO_LOAD_PROFILE_TYPE_DELETION, "Load profile type ''{0}'' is still in use by the following communication task(s): ''{1}''"),
     ONLY_ONE_COMTASK_WITH_FIRMWARE_ALLOWED(16, Keys.ONLY_ONE_COMTASK_WITH_FIRMWARE_ALLOWED, "Only one comtask with the firmware protocol task allowed"),
     ONLY_ONE_PROTOCOLTASK_WHEN_FIRMWARE_UPGRADE(17, Keys.ONLY_ONE_PROTOCOLTASK_WHEN_FIRMWARE_UPGRADE, "Only one protocol task is allowed when defining a firmware upgrade comtask"),
-    VETO_REGISTER_GROUP_DELETION(18, Keys.VETO_REGISTER_GROUP_DELETION, "Register group ''{0}'' is still in use by the following communication task(s): ''{1}''"),
-    VALUE_NOT_IN_RANGE(999, Keys.VALUE_NOT_IN_RANGE, "{value} not in range {min} to {max}");
+    VETO_REGISTER_GROUP_DELETION(18, Keys.VETO_REGISTER_GROUP_DELETION, "Register group ''{0}'' is still in use by the following communication task(s): ''{1}''");
 
     private final int number;
     private final String key;
