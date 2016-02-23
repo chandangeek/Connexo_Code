15ea76316bfb86fe959d72a25929359e2a54659e
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-02-23 09:31:01 +0100

    CXO-428 - Create a service call

--- copl/com.elster.jupiter.servicecall.rest/src/main/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallTypeInfo.java
+++ copl/com.elster.jupiter.servicecall.rest/src/main/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallTypeInfo.java
@@ -3,15 +3,11 @@ package com.elster.jupiter.servicecall.rest.impl;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.rest.util.IdWithDisplayValueInfo;
 import com.elster.jupiter.rest.util.IdWithNameInfo;
-import com.elster.jupiter.servicecall.LogLevel;
 import com.elster.jupiter.servicecall.ServiceCallLifeCycle;
 import com.elster.jupiter.servicecall.ServiceCallType;
-import com.elster.jupiter.servicecall.Status;
-import sun.rmi.runtime.Log;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class ServiceCallTypeInfo {
     public long id;
@@ -33,9 +29,8 @@ public class ServiceCallTypeInfo {
         this.versionName = serviceCallType.getVersionName();
         this.status = new IdWithDisplayValueInfo<>(serviceCallType.getStatus().name(), serviceCallType.getStatus().getDisplayName(thesaurus));
         this.logLevel = new IdWithDisplayValueInfo<>(serviceCallType.getLogLevel().name(), serviceCallType.getLogLevel().getDisplayName(thesaurus));
-        Optional<ServiceCallLifeCycle> serviceCallLifeCycle = serviceCallType.getServiceCallLifeCycle();
-        this.serviceCallLifeCycle = serviceCallLifeCycle.isPresent() ?
-                new IdWithNameInfo(serviceCallLifeCycle.get().getId(), serviceCallLifeCycle.get().getName()) : null;
+        ServiceCallLifeCycle serviceCallLifeCycle = serviceCallType.getServiceCallLifeCycle();
+        this.serviceCallLifeCycle = new IdWithNameInfo(serviceCallLifeCycle.getId(), serviceCallLifeCycle.getName());
         customPropertySets = new ArrayList<>();
         serviceCallType.getCustomPropertySets().stream()
                 .forEach(cps -> this.customPropertySets.add(new ServiceCallTypeCustomPropertySetInfo(cps)));
