fd6504744bf594d3507bb98f4479648ec4eb9921
Albert Versin <albertv@rimroe.elster-group.com>
2016-02-25 14:26:33 +0200

    Fix for CXO-618 Tasks grid displays tasks of inactive processes.

--- copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/src/store/task/TasksFilterProcesses.js
+++ copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/src/store/task/TasksFilterProcesses.js
@@ -4,7 +4,7 @@ Ext.define('Bpm.store.task.TasksFilterProcesses', {
 
     proxy: {
         type: 'rest',
-        url: '/api/bpm/runtime/processes',
+        url: '/api/bpm/runtime/avaiableactiveprocesses',
         timeout: 240000,
         reader: {
             type: 'json',
