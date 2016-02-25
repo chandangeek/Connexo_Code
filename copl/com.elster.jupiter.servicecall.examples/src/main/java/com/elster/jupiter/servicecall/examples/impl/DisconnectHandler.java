6558cb3b0da98c97f2ca760e2ac5f2f7515e06ea
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-02-25 16:16:05 +0100

    CXO-647: Unknown service call handler can not be registered anymore

---
+++ copl/com.elster.jupiter.servicecall.examples/src/main/java/com/elster/jupiter/servicecall/examples/impl/DisconnectHandler.java
@@ -0,0 +1,32 @@
+package com.elster.jupiter.servicecall.examples.impl;
+
+import com.elster.jupiter.servicecall.DefaultState;
+import com.elster.jupiter.servicecall.ServiceCallHandler;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Example handler taking care of a disconnect
+ * Created by bvn on 2/18/16.
+ */
+@Component(name = "com.elster.jupiter.servicecall.example.disconnect.handler",
+        service = ServiceCallHandler.class,
+        immediate = true,
+        property = "name=DisconnectHandler1")
+public class DisconnectHandler implements ServiceCallHandler {
+
+    public DisconnectHandler() {
+    }
+
+    @Override
+    public void onEntry(DefaultState state) {
+        System.out.println("Now entering state " + state.getKey());
+    }
+
+    @Activate
+    public void activate() {
+        System.err.println("Activating disconnect handler");
+    }
+
+}
