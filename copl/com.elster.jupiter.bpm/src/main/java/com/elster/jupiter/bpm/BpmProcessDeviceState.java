5dfe46539827e1b6a2dbeb25d06b4c614fa98c36
Dragos Sinca <(dragos.sinca@elster.com)>
2016-02-26 11:43:09 +0200

    Deprecated old interface

---
+++ copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/BpmProcessDeviceState.java
@@ -0,0 +1,31 @@
+package com.elster.jupiter.bpm;
+
+
+import aQute.bnd.annotation.ProviderType;
+
+@ProviderType
+@Deprecated
+public interface BpmProcessDeviceState {
+
+    long getProcessId();
+
+    void persist();
+
+    void delete();
+
+    long getDeviceStateId();
+
+    void setDeviceStateId(long deviceStateId);
+
+    long getDeviceLifeCycleId();
+
+    void setDeviceLifeCycleId(long deviceLifeCycleId);
+
+    String getName();
+
+    void setName(String name);
+
+    String getDeviceState();
+
+    void setDeviceState(String deviceState);
+}
