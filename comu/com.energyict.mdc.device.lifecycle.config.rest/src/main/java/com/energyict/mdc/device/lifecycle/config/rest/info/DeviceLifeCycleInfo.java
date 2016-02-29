2c58730de3f5cb9cc01ec1c54f34d479dcfecb4e
govanni <govanni.naessens@elster.com>
2016-02-29 15:01:34 +0100

    feature/CXO-595 Finished Datalogger Slave deviceType

--- comu/com.energyict.mdc.device.lifecycle.config.rest/src/main/java/com/energyict/mdc/device/lifecycle/config/rest/info/DeviceLifeCycleInfo.java
+++ comu/com.energyict.mdc.device.lifecycle.config.rest/src/main/java/com/energyict/mdc/device/lifecycle/config/rest/info/DeviceLifeCycleInfo.java
@@ -1,9 +1,7 @@
 package com.energyict.mdc.device.lifecycle.config.rest.info;
 
 import com.elster.jupiter.rest.util.IdWithNameInfo;
-import com.energyict.mdc.device.lifecycle.config.AuthorizedTransitionAction;
 import com.energyict.mdc.device.lifecycle.config.DeviceLifeCycle;
-import com.energyict.mdc.device.lifecycle.config.MicroCategory;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -18,7 +16,6 @@ public class DeviceLifeCycleInfo {
     public Integer actionsCount;
     public List<IdWithNameInfo> deviceTypes;
     public boolean obsolete;
-    public boolean containsCommunicationActions;
 
     public DeviceLifeCycleInfo() {
     }
@@ -30,28 +27,5 @@ public class DeviceLifeCycleInfo {
         this.actionsCount = deviceLifeCycle.getAuthorizedActions().size();
         this.version = deviceLifeCycle.getVersion();
         this.obsolete = deviceLifeCycle.isObsolete();
-        this.containsCommunicationActions = deviceLifecycleContainsCommunicationRelatedActions(deviceLifeCycle) ||
-                deviceLifecycleContainsCommunicationRelatedChecks(deviceLifeCycle);
-    }
-
-    private boolean deviceLifecycleContainsCommunicationRelatedChecks(DeviceLifeCycle deviceLifeCycle) {
-        return deviceLifeCycle.getAuthorizedActions()
-                .stream()
-                .filter(authorizedAction -> authorizedAction instanceof AuthorizedTransitionAction)
-                .flatMap(authorizedAction -> ((AuthorizedTransitionAction) authorizedAction).getChecks()
-                        .stream())
-                .filter(microCheck -> microCheck.getCategory().equals(MicroCategory.COMMUNICATION))
-                .findAny()
-                .isPresent();
-    }
-
-    private boolean deviceLifecycleContainsCommunicationRelatedActions(DeviceLifeCycle deviceLifeCycle) {
-        return deviceLifeCycle.getAuthorizedActions()
-                .stream()
-                .filter(authorizedAction -> authorizedAction instanceof AuthorizedTransitionAction)
-                .flatMap(authorizedAction1 -> ((AuthorizedTransitionAction) authorizedAction1).getActions().stream())
-                .filter(microAction -> microAction.getCategory().equals(MicroCategory.COMMUNICATION))
-                .findAny()
-                .isPresent();
     }
 }
