cee34f1bd53fc0a62e237213ff18ec7d520f0a67
David Nitescu <david.nitescu@elster.com>
2016-03-01 15:53:31 +0200

    resource renamed

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/property/view/property/SelectionGrid.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/property/view/property/SelectionGrid.js
@@ -6,7 +6,7 @@ Ext.define('Uni.property.view.property.SelectionGrid', {
 
     ignoreFields: ['id', 'lifeCycleId'],
     availableFields: [
-        {id: 'deviceStates.lifeCycleName', value: Uni.I18n.translate('process.deviceCycleName', 'UNI', 'Device cycle name')},
+        {id: 'deviceStates.lifeCycleName', value: Uni.I18n.translate('process.deviceCycleName', 'UNI', 'Device life cycle')},
         {id: 'deviceStates.name', value: Uni.I18n.translate('process.deviceState', 'UNI', 'Device state')},
         {id: 'issueReasons.name', value: Uni.I18n.translate('process.issueReasons', 'UNI', 'Issue reason')}
     ],
