1943d1207206c260c53253e51f22663ad0eed4cc
igh <isabelle.gheysens@elster.com>
2016-03-01 10:57:25 +0100

    CXO-480 - Reading type name not displayed correctly on export log history

--- coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/DefaultItemDataSelector.java
+++ coko/com.elster.jupiter.export/src/main/java/com/elster/jupiter/export/impl/DefaultItemDataSelector.java
@@ -285,7 +285,7 @@ class DefaultItemDataSelector implements ItemDataSelector {
         List<? extends BaseReadingRecord> readings = new ArrayList<>(item.getReadingContainer().getReadingsUpdatedSince(updateInterval, item.getReadingType(), since));
 
         String mrid = item.getReadingContainer().getMeter(occurrence.getTriggerTime()).map(Meter::getMRID).orElse("");
-        String itemDescription = mrid + ":" + item.getReadingType().getAliasName();
+        String itemDescription = mrid + ":" + item.getReadingType().getFullAliasName();
 
         Optional<RelativePeriod> updateWindow = item.getSelector().getStrategy().getUpdateWindow();
         if (updateWindow.isPresent()) {
