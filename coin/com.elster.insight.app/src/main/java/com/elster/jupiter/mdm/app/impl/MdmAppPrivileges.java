ca092eb8967cc695e37ac61291f934486d6035bb
Maksim Barinov <mbarinov@mera.ru>
2016-03-04 13:04:58 +0300

    Added privileges

--- coin/com.elster.insight.app/src/main/java/com/elster/jupiter/mdm/app/impl/MdmAppPrivileges.java
+++ coin/com.elster.insight.app/src/main/java/com/elster/jupiter/mdm/app/impl/MdmAppPrivileges.java
@@ -24,7 +24,18 @@ class MdmAppPrivileges {
                 com.elster.jupiter.metering.security.Privileges.Constants.VIEW_METROLOGY_CONFIGURATION,
 
                 //service category
-                com.elster.jupiter.metering.security.Privileges.Constants.VIEW_SERVICECATEGORY
+                com.elster.jupiter.metering.security.Privileges.Constants.VIEW_SERVICECATEGORY,
+
+                //com.elster.jupiter.bpm.security
+                com.elster.jupiter.bpm.security.Privileges.Constants.EXECUTE_TASK,
+                com.elster.jupiter.bpm.security.Privileges.Constants.VIEW_TASK,
+                com.elster.jupiter.bpm.security.Privileges.Constants.ASSIGN_TASK,
+                com.elster.jupiter.bpm.security.Privileges.Constants.VIEW_BPM,
+                com.elster.jupiter.bpm.security.Privileges.Constants.ADMINISTRATE_BPM,
+                com.elster.jupiter.bpm.security.Privileges.Constants.EXECUTE_PROCESSES_LVL_1,
+                com.elster.jupiter.bpm.security.Privileges.Constants.EXECUTE_PROCESSES_LVL_2,
+                com.elster.jupiter.bpm.security.Privileges.Constants.EXECUTE_PROCESSES_LVL_3,
+                com.elster.jupiter.bpm.security.Privileges.Constants.EXECUTE_PROCESSES_LVL_4
         );
     }
 }
