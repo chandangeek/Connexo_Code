d970b8ef62fbf079d37ad3b72c5e161d65e6f9a3
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-03-09 16:21:40 +0100

    CXO-441 - Added some REST testing

--- copl/com.elster.jupiter.servicecall.rest/src/test/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallResourceTest.java
+++ copl/com.elster.jupiter.servicecall.rest/src/test/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallResourceTest.java
@@ -23,7 +23,7 @@ public class ServiceCallResourceTest extends ServiceCallApplicationTest {
 
     @Test
     public void getAllServiceCalls() throws Exception {
-        mockServiceCall(1L);
+        mockSetup();
         Response response = target("/servicecalls").request().get();
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
         JsonModel jsonModel = JsonModel.model((InputStream) response.getEntity());
@@ -32,21 +32,67 @@ public class ServiceCallResourceTest extends ServiceCallApplicationTest {
         assertThat(jsonModel.<String>get("serviceCalls[0].type")).isEqualTo("Mbus 1");
     }
 
+    @Test
+    public void getChildren() throws Exception {
+        mockSetup();
+        Response response = target("/servicecalls/1/children").request().get();
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((InputStream) response.getEntity());
+        assertThat(jsonModel.<Integer>get("total")).isEqualTo(1);
+        assertThat(jsonModel.<Integer>get("serviceCalls[0].id")).isEqualTo(2);
+    }
+
+    @Test
+    public void getServiceCall() throws Exception {
+        mockSetup();
+        Response response = target("/servicecalls/1").request().get();
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((InputStream) response.getEntity());
+        assertThat(jsonModel.<Integer>get("id")).isEqualTo(1);
+    }
+
+    @Test
+    public void getServiceCallWithParent() throws Exception {
+        mockSetup();
+        Response response = target("/servicecalls/2").request().get();
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((InputStream) response.getEntity());
+        assertThat(jsonModel.<Integer>get("id")).isEqualTo(2);
+        assertThat(jsonModel.<Integer>get("parents[0].id")).isEqualTo(1);
+    }
+
+    private void mockSetup() {
+        ServiceCall serviceCall = mockServiceCall(1L);
+        ServiceCall child = mockServiceCall(2L);
+        when(child.getParent()).thenReturn(Optional.of(serviceCall));
+        ServiceCallFinder finder = mock(ServiceCallFinder.class);
+        ServiceCallFinder childrenFinder = mock(ServiceCallFinder.class);
+        ServiceCallFinder childrenOfChildFinder = mock(ServiceCallFinder.class);
+        when(finder.find()).thenReturn(Collections.singletonList(serviceCall));
+        when(serviceCallService.getServiceCall(1)).thenReturn(Optional.of(serviceCall));
+        when(serviceCallService.getServiceCallFinder()).thenReturn(finder);
+        when(serviceCall.getChildrenFinder()).thenReturn(childrenFinder);
+        when(serviceCall.getChildrenFinder().find()).thenReturn(Collections.singletonList(child));
+        when(child.getChildrenFinder()).thenReturn(childrenOfChildFinder);
+        when(child.getChildrenFinder().find()).thenReturn(Collections.emptyList());
+        when(serviceCallService.getServiceCall(2)).thenReturn(Optional.of(child));
+    }
+
     private ServiceCall mockServiceCall(long id) {
         ServiceCall serviceCall = mock(ServiceCall.class);
         ServiceCallType serviceCallType = mock(ServiceCallType.class);
+        when(serviceCall.getId()).thenReturn(id);
+        when(serviceCall.getType()).thenReturn(serviceCallType);
         ServiceCallLifeCycle serviceCallLifeCycle = mock(ServiceCallLifeCycle.class);
         when(serviceCallLifeCycle.getId()).thenReturn(1L);
         when(serviceCallLifeCycle.getName()).thenReturn("default");
         when(serviceCallType.getName()).thenReturn("Mbus 1");
-        ServiceCallFinder finder = mock(ServiceCallFinder.class);
-        when(finder.find()).thenReturn(Collections.singletonList(serviceCall));
-        when(serviceCallService.getServiceCallFinder()).thenReturn(finder);
-        when(serviceCall.getId()).thenReturn(id);
-        when(serviceCall.getType()).thenReturn(serviceCallType);
         when(serviceCall.getCreationTime()).thenReturn(Instant.EPOCH);
         when(serviceCall.getLastModificationTime()).thenReturn(Instant.EPOCH);
         when(serviceCall.getExternalReference()).thenReturn(Optional.empty());
+        when(serviceCall.getLastCompletedTime()).thenReturn(Optional.empty());
+        when(serviceCall.getOrigin()).thenReturn(Optional.empty());
+        when(serviceCall.getParent()).thenReturn(Optional.empty());
 
         return serviceCall;
     }
