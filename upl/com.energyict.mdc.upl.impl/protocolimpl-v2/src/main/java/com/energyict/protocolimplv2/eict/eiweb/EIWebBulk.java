f6abd94d042c8284da900bffde832ac0fb9287a5
khe <kristof.hennebel@elster.com>
2016-03-10 17:29:29 +0100

    EISERVER-5374 - EIWeb - java.lang.NumberFormatException: For input string: "2147483648"
    LP intervals: now also supporting unsigned integers (4 bytes) that are higher than 2147483647.

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/eiweb/EIWebBulk.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/eiweb/EIWebBulk.java
@@ -56,7 +56,7 @@ public class EIWebBulk implements ServletBasedInboundDeviceProtocol {
 
     @Override
     public String getVersion() {
-        return "$Date: 2015-12-17 16:52:58 +0200 (Thu, 17 Dec 2015)$";
+        return "$Date: 2016-03-10 17:29:29 +0100 (Thu, 10 Mar 2016)$";
     }
 
     @Override
