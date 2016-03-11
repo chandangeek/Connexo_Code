beb085bffa603a2c9231771f55ab3f389d6ffa0d
Kirill Koshelev <kirkosh@mera.ru>
2016-03-11 17:38:05 +0300

    CXO-659 / Navigation fix

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/controller/history/Router.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/controller/history/Router.js
@@ -261,12 +261,13 @@ Ext.define('Uni.controller.history.Router', {
                         } else if (config.dynamicPrivilegeStores){
                             Uni.DynamicPrivileges.loadPage(config.dynamicPrivilegeStores, config.dynamicPrivilege, applyAction, me);
                         } else if (config.hideIfAppInstalled){
+                            //Ex.: If Insight installed, Usage points are not available in Multisense
                             if(Uni.store.Apps.storeLoaded){
-                                Uni.store.Apps.getAppUrl(config.checkApp) ? crossroads.parse("/error/notfound") : applyAction();
+                                Uni.store.Apps.checkApp(config.hideIfAppInstalled) ? crossroads.parse("/error/notfound") : applyAction();
                             } else {
                                 Uni.store.Apps.load({
                                     callback: function () {
-                                        Uni.store.Apps.getAppUrl(config.hideIfAppInstalled) ? crossroads.parse("/error/notfound") : applyAction();
+                                        Uni.store.Apps.checkApp(config.hideIfAppInstalled) ? crossroads.parse("/error/notfound") : applyAction();
                                     }
                                 });
                             }
