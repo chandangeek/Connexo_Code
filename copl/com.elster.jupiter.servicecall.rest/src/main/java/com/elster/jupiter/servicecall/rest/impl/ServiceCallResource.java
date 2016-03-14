e975342e1bf2ac39a3d754bc954a7efe64d0bd39
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-03-14 10:32:31 +0100

    CXO-441 - changing how service calls are found with remarks from review

--- copl/com.elster.jupiter.servicecall.rest/src/main/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallResource.java
+++ copl/com.elster.jupiter.servicecall.rest/src/main/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallResource.java
@@ -1,16 +1,15 @@
 package com.elster.jupiter.servicecall.rest.impl;
 
 
+import com.elster.jupiter.domain.util.Finder;
 import com.elster.jupiter.rest.util.ExceptionFactory;
 import com.elster.jupiter.rest.util.JsonQueryFilter;
 import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.elster.jupiter.rest.util.PagedInfoList;
 import com.elster.jupiter.servicecall.ServiceCall;
-import com.elster.jupiter.servicecall.ServiceCallFinder;
+import com.elster.jupiter.servicecall.ServiceCallFilter;
 import com.elster.jupiter.servicecall.ServiceCallService;
 import com.elster.jupiter.servicecall.security.Privileges;
-
-import com.google.common.collect.Range;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
@@ -22,7 +21,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,11 +44,8 @@ public class ServiceCallResource {
     @RolesAllowed(Privileges.Constants.VIEW_SERVICE_CALLS)
     public PagedInfoList getAllServiceCalls(@BeanParam JsonQueryParameters queryParameters, @BeanParam JsonQueryFilter filter) {
         List<ServiceCallInfo> serviceCallInfos = new ArrayList<>();
-        ServiceCallFinder serviceCallFinder = serviceCallService.getServiceCallFinder();
-        applyFilterToFinder(filter, serviceCallFinder);
-        queryParameters.getLimit().ifPresent(limit -> serviceCallFinder.setLimit(limit + 1));
-        queryParameters.getStart().ifPresent(serviceCallFinder::setStart);
-        List<ServiceCall> serviceCalls = serviceCallFinder.find();
+        Finder<ServiceCall> serviceCallFinder = serviceCallService.getServiceCallFinder(convertToFilter(filter));
+        List<ServiceCall> serviceCalls = serviceCallFinder.from(queryParameters).find();
 
         serviceCalls.stream()
                 .forEach(serviceCall -> serviceCallInfos.add(serviceCallInfoFactory.summarized(serviceCall)));
@@ -58,28 +53,29 @@ public class ServiceCallResource {
         return PagedInfoList.fromPagedList("serviceCalls", serviceCallInfos, queryParameters);
     }
 
-    private void applyFilterToFinder(JsonQueryFilter filter, ServiceCallFinder serviceCallFinder) {
+    private ServiceCallFilter convertToFilter(JsonQueryFilter filter) {
+        ServiceCallFilter serviceCallFilter = serviceCallService.newServiceCallFilter();
         if (filter.hasProperty("name")) {
-            serviceCallFinder.setReference(filter.getString("name"));
+            serviceCallFilter.setReference(filter.getString("name"));
         }
         if (filter.hasProperty("type")) {
-            serviceCallFinder.setType(filter.getStringList("type"));
+            serviceCallFilter.setTypes(filter.getStringList("type"));
         }
         if(filter.hasProperty("status")) {
-            serviceCallFinder.setState(filter.getStringList("status"));
+            serviceCallFilter.setStates(filter.getStringList("status"));
         }
         if (filter.hasProperty("receivedDateFrom")) {
-            serviceCallFinder.withCreationTimeIn(Range.closed(filter.getInstant("receivedDateFrom"),
-                    filter.hasProperty("receivedDateTo") ? filter.getInstant("receivedDateTo") : Instant.now()));
+            serviceCallFilter.setReceivedDateFrom(filter.getInstant("receivedDateFrom"));
         } else if (filter.hasProperty("receivedDateTo")) {
-            serviceCallFinder.withCreationTimeIn(Range.closed(Instant.EPOCH, filter.getInstant("receivedDateTo")));
+            serviceCallFilter.setReceivedDateTo(filter.getInstant("receivedDateTo"));
         }
         if (filter.hasProperty("modificationDateFrom")) {
-            serviceCallFinder.withModTimeIn(Range.closed(filter.getInstant("modificationDateFrom"),
-                    filter.hasProperty("modificationDateTo") ? filter.getInstant("modificationDateTo") : Instant.now()));
+            serviceCallFilter.setModificationDateFrom(filter.getInstant("modificationDateFrom"));
         } else if (filter.hasProperty("modificationDateTo")) {
-            serviceCallFinder.withModTimeIn(Range.closed(Instant.EPOCH, filter.getInstant("modificationDateTo")));
+            serviceCallFilter.setModificationDateTo(filter.getInstant("modificationDateTo"));
         }
+
+        return serviceCallFilter;
     }
 
     @GET
@@ -100,11 +96,8 @@ public class ServiceCallResource {
         ServiceCall parent = serviceCallService.getServiceCall(id)
                 .orElseThrow(exceptionFactory.newExceptionSupplier(MessageSeeds.NO_SUCH_SERVICE_CALL));
         List<ServiceCallInfo> serviceCallInfos = new ArrayList<>();
-        ServiceCallFinder serviceCallFinder = parent.findChildren();
-        applyFilterToFinder(filter, serviceCallFinder);
-        queryParameters.getLimit().ifPresent(limit -> serviceCallFinder.setLimit(limit + 1));
-        queryParameters.getStart().ifPresent(serviceCallFinder::setStart);
-        List<ServiceCall> serviceCalls = serviceCallFinder.find();
+
+        List<ServiceCall> serviceCalls = parent.findChildren(convertToFilter(filter)).from(queryParameters).find();
 
         serviceCalls.stream()
                 .forEach(serviceCall -> serviceCallInfos.add(serviceCallInfoFactory.summarized(serviceCall)));
--- copl/com.elster.jupiter.servicecall.rest/src/test/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallResourceTest.java
+++ copl/com.elster.jupiter.servicecall.rest/src/test/java/com/elster/jupiter/servicecall/rest/impl/ServiceCallResourceTest.java
@@ -1,8 +1,11 @@
 package com.elster.jupiter.servicecall.rest.impl;
 
+import com.elster.jupiter.domain.util.DefaultFinder;
+import com.elster.jupiter.domain.util.Finder;
+import com.elster.jupiter.domain.util.QueryParameters;
 import com.elster.jupiter.servicecall.DefaultState;
 import com.elster.jupiter.servicecall.ServiceCall;
-import com.elster.jupiter.servicecall.ServiceCallFinder;
+import com.elster.jupiter.servicecall.ServiceCallFilter;
 import com.elster.jupiter.servicecall.ServiceCallLifeCycle;
 import com.elster.jupiter.servicecall.ServiceCallType;
 
@@ -11,12 +14,19 @@ import com.jayway.jsonpath.JsonModel;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
 
+import org.mockito.Mockito;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -28,7 +38,7 @@ public class ServiceCallResourceTest extends ServiceCallApplicationTest {
         Response response = target("/servicecalls").request().get();
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
         JsonModel jsonModel = JsonModel.model((InputStream) response.getEntity());
-        assertThat(jsonModel.<Integer>get("total")).isEqualTo(1);
+        assertThat(jsonModel.<Integer>get("total")).isEqualTo(2);
         assertThat(jsonModel.<Integer>get("serviceCalls[0].id")).isEqualTo(1);
         assertThat(jsonModel.<String>get("serviceCalls[0].type")).isEqualTo("Mbus 1");
     }
@@ -73,16 +83,17 @@ public class ServiceCallResourceTest extends ServiceCallApplicationTest {
         ServiceCall serviceCall = mockServiceCall(1L);
         ServiceCall child = mockServiceCall(2L);
         when(child.getParent()).thenReturn(Optional.of(serviceCall));
-        ServiceCallFinder finder = mock(ServiceCallFinder.class);
-        ServiceCallFinder childrenFinder = mock(ServiceCallFinder.class);
-        ServiceCallFinder childrenOfChildFinder = mock(ServiceCallFinder.class);
-        when(finder.find()).thenReturn(Collections.singletonList(serviceCall));
+        List<ServiceCall> list = new ArrayList<>();
+        list.add(serviceCall);
+        list.add(child);
+
+        Finder<ServiceCall> finder = mockFinder(Collections.unmodifiableList(list));
+        Finder<ServiceCall> childrenFinder = mockFinder(Collections.singletonList(child));
+        Finder<ServiceCall> childrenOfChildFinder = mockFinder(Collections.emptyList());
         when(serviceCallService.getServiceCall(1)).thenReturn(Optional.of(serviceCall));
-        when(serviceCallService.getServiceCallFinder()).thenReturn(finder);
-        when(serviceCall.findChildren()).thenReturn(childrenFinder);
-        when(serviceCall.findChildren().find()).thenReturn(Collections.singletonList(child));
-        when(child.findChildren()).thenReturn(childrenOfChildFinder);
-        when(child.findChildren().find()).thenReturn(Collections.emptyList());
+        when(serviceCallService.getServiceCallFinder(any(ServiceCallFilter.class))).thenReturn(finder);
+        when(serviceCall.findChildren(any(ServiceCallFilter.class))).thenReturn(childrenFinder);
+        when(child.findChildren(any(ServiceCallFilter.class))).thenReturn(childrenOfChildFinder);
         when(serviceCallService.getServiceCall(2)).thenReturn(Optional.of(child));
     }
 
@@ -103,5 +114,16 @@ public class ServiceCallResourceTest extends ServiceCallApplicationTest {
         when(serviceCall.getParent()).thenReturn(Optional.empty());
         when(serviceCall.getState()).thenReturn(DefaultState.CREATED);
         return serviceCall;
+    }
+
+    <T> Finder<T> mockFinder(List<T> list) {
+        Finder<T> finder = mock(Finder.class);
+
+        when(finder.paged(anyInt(), anyInt())).thenReturn(finder);
+        when(finder.sorted(anyString(), any(Boolean.class))).thenReturn(finder);
+        when(finder.from(any(QueryParameters.class))).thenReturn(finder);
+        when(finder.find()).thenReturn(list);
+        when(finder.stream()).thenReturn(list.stream());
+        return finder;
     }
 }
