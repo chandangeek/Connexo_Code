cf7a4acbda66c89d06d002235d0f4d3b2af41afe
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-03-14 14:46:28 +0100

    CXO-439 - Service call landing page in MultiSense - Adding of link to Device/UsagePoint

---
+++ copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/ReferenceInfo.java
@@ -0,0 +1,11 @@
+package com.elster.jupiter.rest.whiteboard;
+
+public class ReferenceInfo {
+    public String type;
+    public String key;
+
+    public ReferenceInfo(String type, String key) {
+        this.type = type;
+        this.key = key;
+    }
+}
