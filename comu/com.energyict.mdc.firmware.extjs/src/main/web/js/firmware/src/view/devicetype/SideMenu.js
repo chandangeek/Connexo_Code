83fb135cf59f2b4383872248a8e8182acc6149e5
Geert <geert.delmeiren@elster.com>
2016-03-14 15:56:45 +0100

    CXO-846 - 'Communication' section should disappear on the device configuration details of a 'Datalogger slave device type'

--- comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/view/devicetype/SideMenu.js
+++ comu/com.energyict.mdc.firmware.extjs/src/main/web/js/firmware/src/view/devicetype/SideMenu.js
@@ -2,23 +2,26 @@ Ext.define('Fwc.view.devicetype.SideMenu', {
     override: 'Mdc.view.setup.devicetype.SideMenu',
 
     initComponent: function () {
-        this.callParent(arguments);
-        this.addMenuItems([{
-            title: Uni.I18n.translate('general.firmware', 'FWC', 'Firmware'),
-            items: [
-                {
-                    text: Uni.I18n.translate('general.firmwareManagementOptions', 'FWC', 'Firmware management options'),
-                    itemId: 'firmwareoptionsLink',
-                    href: '#/administration/devicetypes/' + this.deviceTypeId + '/firmware/options'
-                },
-                {
-                    text: Uni.I18n.translate('general.firmwareVersions', 'FWC', 'Firmware versions'),
-                    itemId: 'firmwareversionsLink',
-                    href: '#/administration/devicetypes/' + this.deviceTypeId + '/firmware/versions'
-                }
-            ]
-        }]);
-        this.checkNavigation(Ext.util.History.getToken());
+        var me = this;
+        me.callParent(arguments);
+        me.executeIfNoDataLoggerSlave(function() {
+            me.addMenuItems([{
+                title: Uni.I18n.translate('general.firmware', 'FWC', 'Firmware'),
+                items: [
+                    {
+                        text: Uni.I18n.translate('general.firmwareManagementOptions', 'FWC', 'Firmware management options'),
+                        itemId: 'firmwareoptionsLink',
+                        href: '#/administration/devicetypes/' + me.deviceTypeId + '/firmware/options'
+                    },
+                    {
+                        text: Uni.I18n.translate('general.firmwareVersions', 'FWC', 'Firmware versions'),
+                        itemId: 'firmwareversionsLink',
+                        href: '#/administration/devicetypes/' + me.deviceTypeId + '/firmware/versions'
+                    }
+                ]
+            }]);
+        });
+        me.checkNavigation(Ext.util.History.getToken());
     }
 });
 
