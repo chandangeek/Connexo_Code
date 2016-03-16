93b8c1081330874cce899a446fd8ff46dd395af1
Pasquien Dorny <pasquien.dorny@elster.com>
2016-03-16 13:02:39 +0100

    CXO-400: [G6] Can't search for firmware version on a device

---
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/FirmwareVersionNameSearchableProperty.java
@@ -0,0 +1,109 @@
+package com.energyict.mdc.device.data.impl.search;
+
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.TranslationKey;
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.search.*;
+import com.elster.jupiter.util.conditions.Condition;
+import com.elster.jupiter.util.sql.SqlBuilder;
+import com.elster.jupiter.util.sql.SqlFragment;
+import com.energyict.mdc.dynamic.PropertySpecService;
+import com.energyict.mdc.tasks.ComTask;
+
+import javax.inject.Inject;
+import java.time.Instant;
+import java.util.*;
+
+public class FirmwareVersionNameSearchableProperty extends AbstractSearchableDeviceProperty {
+    static final String PROPERTY_NAME =  DeviceTypeSearchableProperty.PROPERTY_NAME + ".firmware.version.name";
+
+    private final PropertySpecService propertySpecService;
+    private SearchDomain searchDomain;
+    private SearchablePropertyGroup group;
+
+    @Inject
+    public FirmwareVersionNameSearchableProperty(PropertySpecService propertySpecService, Thesaurus thesaurus) {
+        super(thesaurus);
+        this.propertySpecService = propertySpecService;
+    }
+
+    FirmwareVersionNameSearchableProperty init(SearchDomain searchDomain, SearchablePropertyGroup parentGroup) {
+        this.searchDomain = searchDomain;
+        this.group = parentGroup;
+        return this;
+    }
+
+    @Override
+    protected boolean valueCompatibleForDisplay(Object value) {
+        return value instanceof ComTask;
+    }
+
+    @Override
+    protected String toDisplayAfterValidation(Object value) {
+        return ((ComTask) value).getName();
+    }
+
+    @Override
+    public void appendJoinClauses(JoinClauseBuilder builder) {
+    }
+
+    @Override
+    public SqlFragment toSqlFragment(Condition condition, Instant now) {
+        SqlBuilder sqlBuilder = new SqlBuilder();
+        sqlBuilder.append(JoinClauseBuilder.Aliases.DEVICE + ".DEVICETYPE IN ");
+        sqlBuilder.openBracket();
+        sqlBuilder.append("select DEVICETYPE from FWC_FIRMWAREVERSION firmwareVersion where ");
+        sqlBuilder.add(toSqlFragment("FIRMWAREVERSION", condition, now));
+        sqlBuilder.closeBracket();
+        return sqlBuilder;
+    }
+
+    @Override
+    public SearchDomain getDomain() {
+        return this.searchDomain;
+    }
+
+    @Override
+    public boolean affectsAvailableDomainProperties() {
+        return false;
+    }
+
+    @Override
+    public Optional<SearchablePropertyGroup> getGroup() {
+        return Optional.of(this.group);
+    }
+
+    @Override
+    public PropertySpec getSpecification() {
+        return this.propertySpecService
+                .stringSpec()
+                .named(PROPERTY_NAME, getNameTranslationKey())
+                .fromThesaurus(this.getThesaurus())
+                .finish();
+    }
+
+    @Override
+    public Visibility getVisibility() {
+        return Visibility.REMOVABLE;
+    }
+
+    @Override
+    public SelectionMode getSelectionMode() {
+        return SelectionMode.SINGLE;
+    }
+
+    @Override
+    protected TranslationKey getNameTranslationKey() {
+        return PropertyTranslationKeys.FIRMWARE_VERSION_VERSION;
+    }
+
+    @Override
+    public List<SearchableProperty> getConstraints() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void refreshWithConstrictions(List<SearchablePropertyConstriction> constrictions) {
+        // no refresh
+    }
+}
--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/PropertyTranslationKeys.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/PropertyTranslationKeys.java
@@ -87,7 +87,10 @@ public enum PropertyTranslationKeys implements TranslationKey {
     CONNECTION_TASK_STATUS_INACTIVE("device.connection.status.INACTIVE", "Inactive"),
 
     CONNECTION_TASK_DIRECTION_INBOUND("device.connection.direction.inbound", "Inbound"),
-    CONNECTION_TASK_DIRECTION_OUTBOUND("device.connection.direction.outbound", "Outbound")
+    CONNECTION_TASK_DIRECTION_OUTBOUND("device.connection.direction.outbound", "Outbound"),
+
+    FIRMWARE_VERSION("device.firmware.version","Firmware Version") ,
+    FIRMWARE_VERSION_VERSION("device.firmware.version.version", "Version")
     ;
 
     private String key;
