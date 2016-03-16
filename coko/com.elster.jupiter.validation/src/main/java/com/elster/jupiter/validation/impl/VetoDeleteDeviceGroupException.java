56ec53e21d8832ca6f87c5c4850d34c7790bbab0
varuchin <draumar52@gmail.com>
2016-03-16 18:40:54 +0300

    Added new MessageSed and exception

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/VetoDeleteDeviceGroupException.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/VetoDeleteDeviceGroupException.java
@@ -1,4 +1,13 @@
 package com.elster.jupiter.validation.impl;
 
-public class VetoDeleteDeviceGroupException {
+import com.elster.jupiter.metering.groups.EndDeviceGroup;
+import com.elster.jupiter.nls.LocalizedException;
+import com.elster.jupiter.nls.Thesaurus;
+
+public class VetoDeleteDeviceGroupException extends LocalizedException {
+
+    public VetoDeleteDeviceGroupException(Thesaurus thesaurus, EndDeviceGroup deviceGroup) {
+        super(thesaurus, MessageSeeds.VETO_DEVICEGROUP_DELETION, deviceGroup.getName());
+    }
+
 }
