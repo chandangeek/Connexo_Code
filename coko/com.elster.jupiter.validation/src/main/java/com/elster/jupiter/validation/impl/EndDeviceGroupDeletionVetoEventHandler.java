50fbfd3597403e61d15cbb19ac6ad1a6d57ba194
varuchin <draumar52@gmail.com>
2016-03-17 12:25:47 +0300

    Added new eventhandler for situation when device group which is being deleted has a validation task.

---
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/EndDeviceGroupDeletionVetoEventHandler.java
@@ -0,0 +1,57 @@
+package com.elster.jupiter.validation.impl;
+
+import com.elster.jupiter.events.LocalEvent;
+import com.elster.jupiter.events.TopicHandler;
+import com.elster.jupiter.metering.groups.EndDeviceGroup;
+import com.elster.jupiter.metering.groups.EndDeviceGroupEventData;
+import com.elster.jupiter.metering.groups.EventType;
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.validation.ValidationService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.inject.Inject;
+
+@Component(name = "com.elster.jupiter.validation.impl.delete.queryenddevicegroup.eventhandler", service = TopicHandler.class, immediate = true)
+public class EndDeviceGroupDeletionVetoEventHandler implements TopicHandler {
+
+    private volatile ValidationService validationService;
+    private volatile Thesaurus thesaurus;
+    public final static String COMPONENT_NAME = "VAL";
+
+    public EndDeviceGroupDeletionVetoEventHandler() {
+    }
+
+    @Inject
+    public EndDeviceGroupDeletionVetoEventHandler(ValidationService validationService, Thesaurus thesaurus) {
+        setValidationService(validationService);
+        this.thesaurus = thesaurus;
+    }
+
+    @Reference
+    public void setValidationService(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+    @Reference
+    public void setNlsService(NlsService nlsService) {
+        this.thesaurus = nlsService.getThesaurus(COMPONENT_NAME, Layer.SERVICE);
+    }
+
+    @Override
+    public void handle(LocalEvent localEvent) {
+        EndDeviceGroupEventData eventSource = (EndDeviceGroupEventData) localEvent.getSource();
+        EndDeviceGroup endDeviceGroup = eventSource.getEndDeviceGroup();
+        if (validationService.findValidationTask(endDeviceGroup.getId()).isPresent()) {
+            throw new VetoDeleteDeviceGroupException(thesaurus, endDeviceGroup);
+        }
+    }
+
+    @Override
+    public String getTopicMatcher() {
+        return EventType.ENDDEVICEGROUP_VALIDATE_DELETED.topic();
+    }
+}
---
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/VetoDeleteDeviceGroupException.java
@@ -0,0 +1,13 @@
+package com.elster.jupiter.validation.impl;
+
+import com.elster.jupiter.metering.groups.EndDeviceGroup;
+import com.elster.jupiter.nls.LocalizedException;
+import com.elster.jupiter.nls.Thesaurus;
+
+public class VetoDeleteDeviceGroupException extends LocalizedException {
+
+    public VetoDeleteDeviceGroupException(Thesaurus thesaurus, EndDeviceGroup deviceGroup) {
+        super(thesaurus, MessageSeeds.VETO_DEVICEGROUP_DELETION, deviceGroup.getName());
+    }
+
+}
