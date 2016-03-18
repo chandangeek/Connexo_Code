c3a31992bfc7f05aad87731eb84b3d4f4f118986
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-03-18 08:54:44 +0100

    CXO-445 - Move of servicecallfilter

--- copl/com.elster.jupiter.servicecall.rest/src/main/java/com/elster/jupiter/servicecall/rest/ServiceCallInfoFactory.java
+++ copl/com.elster.jupiter.servicecall.rest/src/main/java/com/elster/jupiter/servicecall/rest/ServiceCallInfoFactory.java
@@ -1,7 +1,9 @@
 package com.elster.jupiter.servicecall.rest;
 
+import com.elster.jupiter.rest.util.JsonQueryFilter;
 import com.elster.jupiter.servicecall.DefaultState;
 import com.elster.jupiter.servicecall.ServiceCall;
+import com.elster.jupiter.servicecall.ServiceCallFilter;
 
 import java.util.Map;
 
@@ -10,4 +12,6 @@ public interface ServiceCallInfoFactory {
     ServiceCallInfo detailed(ServiceCall serviceCall, Map<DefaultState, Long> childrenInformation);
 
     ServiceCallInfo summarized(ServiceCall serviceCall);
+
+    ServiceCallFilter convertToServiceCallFilter(JsonQueryFilter filter);
 }
