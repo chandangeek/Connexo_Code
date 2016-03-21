107f29597f0d0977d23edb1cd17dd7b33afc78fa
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-03-21 10:34:09 +0100

    CXO-441 - Small fix

--- copl/com.elster.jupiter.servicecall.extjs/src/main/web/js/servicecall/src/view/PreviewForm.js
+++ copl/com.elster.jupiter.servicecall.extjs/src/main/web/js/servicecall/src/view/PreviewForm.js
@@ -113,16 +113,7 @@ Ext.define('Scs.view.PreviewForm', {
                         xtype: 'displayfield',
                         fieldLabel: Uni.I18n.translate('general.status', 'SCS', 'Status'),
                         name: 'state',
-                        hidden: !me.detailed,
-                        renderer: function(value) {
-                            if(value.displayValue) {
-                                return value.displayValue;
-                            } else {
-                                return '-';
-                            }
-                        }
-
-
+                        hidden: !me.detailed
                     },
                     {
                         xtype: 'displayfield',
