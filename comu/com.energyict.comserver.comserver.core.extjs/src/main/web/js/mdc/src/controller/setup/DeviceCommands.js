beb5c2d888f0d029f7ab86cd5e53069e215d7cfb
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-03-21 14:06:27 +0100

    CXO-910: Added check for trackingCategory value ON FRONTEND  !!!!

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceCommands.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceCommands.js
@@ -391,7 +391,7 @@ Ext.define("Mdc.controller.setup.DeviceCommands", {
             releaseDate && record.set('releaseDate', releaseDate);
             messageSpecification && record.set('messageSpecification', messageSpecification);
             record.set('status', null);
-            if (commandForm.getValues().trackingCategory === "") {
+            if (commandForm.getValues().trackingCategory === "" || commandForm.getValues().trackingCategory === undefined) {
                 record.set('trackingCategory', null);
             }
             record.endEdit();
