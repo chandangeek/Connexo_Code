e7f6966f07668fa6474d2466682df8ce92ebc12e
Anton Suslennikov <antsusl@mera.ru>
2016-03-24 15:59:53 +0300

    Fix after merge

--- copl/com.elster.jupiter.domain.util/src/test/java/com/elster/jupiter/nls/impl/ValidationTest.java
+++ copl/com.elster.jupiter.domain.util/src/test/java/com/elster/jupiter/nls/impl/ValidationTest.java
@@ -53,24 +53,24 @@ public class ValidationTest {
     @BeforeClass
     public static void setUp() {
         injector = Guice.createInjector(
-                    new MockModule(),
-        			inMemoryBootstrapModule,
-        			new OrmModule(),
-        			new UtilModule(),
-        			new ThreadSecurityModule(),
-        			new PubSubModule(),
-        			new TransactionModule(printSql),
-                    new NlsModule()
-                );
-        try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext() ) {
-        	injector.getInstance(NlsService.class);
-        	ctx.commit();
+                new MockModule(),
+                inMemoryBootstrapModule,
+                new OrmModule(),
+                new UtilModule(),
+                new ThreadSecurityModule(),
+                new PubSubModule(),
+                new TransactionModule(printSql),
+                new NlsModule()
+        );
+        try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext()) {
+            injector.getInstance(NlsService.class);
+            ctx.commit();
         }
     }
 
     @AfterClass
     public static void tearDown() {
-    	inMemoryBootstrapModule.deactivate();
+        inMemoryBootstrapModule.deactivate();
     }
 
     private NlsService getNlsService() {
@@ -82,40 +82,40 @@ public class ValidationTest {
     }
 
     @Test
-    public void testCrud()  {
-    	NlsService nlsService = getNlsService();
+    public void testCrud() {
+        NlsService nlsService = getNlsService();
         try (TransactionContext context = getTransactionService().getContext()) {
-        	Thesaurus thesaurus = nlsService.getThesaurus("DUM", Layer.DOMAIN);
-        	thesaurus.addTranslations(getTranslations());
-         	context.commit();
+            Thesaurus thesaurus = nlsService.getThesaurus("DUM", Layer.DOMAIN);
+            thesaurus.addTranslations(getTranslations());
+            context.commit();
         }
         ThreadPrincipalService threadPrincipalService = injector.getInstance(ThreadPrincipalService.class);
-        threadPrincipalService.set(threadPrincipalService.getPrincipal(),"module","action",Locale.FRANCE);
+        threadPrincipalService.set(threadPrincipalService.getPrincipal(), "module", "action", Locale.FRANCE);
         Validator validator = ((NlsServiceImpl) nlsService).getDataModel().getValidatorFactory().getValidator();
         for (ConstraintViolation<?> violation : validator.validate(new Bean())) {
-    		String message = nlsService.interpolate(violation);
-    		if (violation.getConstraintDescriptor().getAnnotation().annotationType().equals(NotNull.class)) {
-    			assertThat(message).isEqualTo("vide (ne peut pas \u00eatre nul)");
-    		} else {
-    			assertThat(message).isEqualTo("svp ne laissez pas vide (ne peut pas \u00eatre nul), valeur minimal: 10");
-    		}
-    	}
+            String message = nlsService.interpolate(violation);
+            if (violation.getConstraintDescriptor().getAnnotation().annotationType().equals(NotNull.class)) {
+                assertThat(message).isEqualTo("vide (ne peut pas \u00eatre nul)");
+            } else {
+                assertThat(message).isEqualTo("svp ne laissez pas vide (ne peut pas \u00eatre nul), valeur minimal: 10");
+            }
+        }
     }
 
     private List<Translation> getTranslations() {
-    	List<Translation> result = new ArrayList<>();
-    	SimpleNlsKey nlsKey = SimpleNlsKey.key("DUM", Layer.DOMAIN, "empty");
-    	result.add(SimpleTranslation.translation(nlsKey,Locale.FRANCE,"vide ({javax.validation.constraints.NotNull.message})"));
-    	nlsKey = SimpleNlsKey.key("DUM", Layer.DOMAIN, "min.size");
-    	result.add(SimpleTranslation.translation(nlsKey,Locale.FRANCE,"svp ne laissez pas {DUM.empty}, valeur minimal: {min}"));
-    	return result;
+        List<Translation> result = new ArrayList<>();
+        SimpleNlsKey nlsKey = SimpleNlsKey.key("DUM", Layer.DOMAIN, "empty");
+        result.add(SimpleTranslation.translation(nlsKey, Locale.FRANCE, "vide ({javax.validation.constraints.NotNull.message})"));
+        nlsKey = SimpleNlsKey.key("DUM", Layer.DOMAIN, "min.size");
+        result.add(SimpleTranslation.translation(nlsKey, Locale.FRANCE, "svp ne laissez pas {DUM.empty}, valeur minimal: {min}"));
+        return result;
     }
 
     public class Bean {
-    	@NotNull(message="{DUM.empty}")
-    	private String name;
-    	@Size(message="{DUM.min.size}", min=10 )
-    	private String description = "N/A";
+        @NotNull(message = "{DUM.empty}")
+        private String name;
+        @Size(message = "{DUM.min.size}", min = 10)
+        private String description = "N/A";
     }
 
     private static class MockModule extends AbstractModule {
