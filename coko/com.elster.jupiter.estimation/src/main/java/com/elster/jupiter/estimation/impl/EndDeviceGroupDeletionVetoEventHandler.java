8280b7c56afe99d2f974e463f4a95da9c399835d
Valery Ruchin <varuchin@mera.ru>
2016-03-30 13:59:10 +0300

    Added validation

--- coko/com.elster.jupiter.estimation/src/main/java/com/elster/jupiter/estimation/impl/EndDeviceGroupDeletionVetoEventHandler.java
+++ coko/com.elster.jupiter.estimation/src/main/java/com/elster/jupiter/estimation/impl/EndDeviceGroupDeletionVetoEventHandler.java
@@ -9,6 +9,7 @@ import com.elster.jupiter.metering.groups.EventType;
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
