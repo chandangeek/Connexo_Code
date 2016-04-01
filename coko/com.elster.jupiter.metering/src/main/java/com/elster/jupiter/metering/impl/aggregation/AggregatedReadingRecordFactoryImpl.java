d331f6b001f903336ae17e2bf9742ee160dafb8c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-04-01 15:46:08 +0200

    First stab at supporting monthly aggregation with meter activation in the middle of the month

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/aggregation/AggregatedReadingRecordFactoryImpl.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/aggregation/AggregatedReadingRecordFactoryImpl.java
@@ -6,7 +6,7 @@ import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import javax.inject.Inject;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +22,7 @@ import java.util.function.BiFunction;
 public class AggregatedReadingRecordFactoryImpl implements AggregatedReadingRecordFactory {
 
     private final DataModel dataModel;
-    private final Map<Timestamp, AggregatedReadingRecord> records = new TreeMap<>();    // Keep the keys sorted
+    private final Map<Instant, AggregatedReadingRecord> records = new TreeMap<>();    // Keep the keys sorted
 
     @Inject
     public AggregatedReadingRecordFactoryImpl(DataModel dataModel) {
@@ -33,8 +33,9 @@ public class AggregatedReadingRecordFactoryImpl implements AggregatedReadingReco
     public List<AggregatedReadingRecord> consume(ResultSet resultSet) {
         try {
             while (resultSet.next()) {
-                Timestamp localDate = resultSet.getTimestamp(3);
-                this.records.compute(localDate, this.createOrUpdate(resultSet));
+                this.records.compute(
+                        Instant.ofEpochMilli(resultSet.getLong(4)),
+                        this.createOrUpdate(resultSet));
             }
             return new ArrayList<>(this.records.values());
         } catch (SQLException e) {
@@ -42,8 +43,8 @@ public class AggregatedReadingRecordFactoryImpl implements AggregatedReadingReco
         }
     }
 
-    private BiFunction<? super Timestamp, ? super AggregatedReadingRecord, ? extends AggregatedReadingRecord> createOrUpdate(final ResultSet resultSet) {
-        return (localDate, readingRecord) -> readingRecord == null ? this.createFrom(resultSet) : readingRecord.addFrom(resultSet);
+    private BiFunction<? super Instant, ? super AggregatedReadingRecord, ? extends AggregatedReadingRecord> createOrUpdate(final ResultSet resultSet) {
+        return (timestamp, readingRecord) -> readingRecord == null ? this.createFrom(resultSet) : readingRecord.addFrom(resultSet);
     }
 
     private AggregatedReadingRecord createFrom(ResultSet resultSet) {
