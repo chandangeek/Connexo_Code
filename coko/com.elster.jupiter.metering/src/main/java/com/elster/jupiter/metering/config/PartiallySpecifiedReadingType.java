d4f7942d72dacdaca4a10067a4b34cd3ae1a7167
Anton Fomchenko <antfom@mera.ru>
2016-04-04 13:12:16 +0300

    CXO-648 reading type template display

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/config/PartiallySpecifiedReadingType.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/config/PartiallySpecifiedReadingType.java
@@ -2,6 +2,9 @@ package com.elster.jupiter.metering.config;
 
 import aQute.bnd.annotation.ProviderType;
 
+import java.util.List;
+import java.util.Optional;
+
 @ProviderType
 public interface PartiallySpecifiedReadingType extends ReadingTypeRequirement {
 
@@ -26,4 +29,30 @@ public interface PartiallySpecifiedReadingType extends ReadingTypeRequirement {
      * @return A source template
      */
     ReadingTypeTemplate getReadingTypeTemplate();
+
+
+    /**
+     * @return A string value based on attributes
+     */
+    String getStringDescription();
+
+    /**
+     * @return A time value
+     */
+    Optional<String> getTranslatedTimeValue();
+
+    /**
+     * @return A list of translated unit values without multipliers
+     */
+    List<String> getTranslatedUnitValues();
+
+    /**
+     * @param attributeName attribute unique id
+     * @return An Optional object with translated value
+     */
+    Optional<String> getTranslatedAttributeValue(ReadingTypeTemplateAttributeName attributeName);
+
+
+
+
 }
--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/config/PartiallySpecifiedReadingTypeImpl.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/config/PartiallySpecifiedReadingTypeImpl.java
@@ -9,6 +9,8 @@ import com.elster.jupiter.metering.config.PartiallySpecifiedReadingType;
 import com.elster.jupiter.metering.config.ReadingTypeTemplate;
 import com.elster.jupiter.metering.config.ReadingTypeTemplateAttribute;
 import com.elster.jupiter.metering.config.ReadingTypeTemplateAttributeName;
+import com.elster.jupiter.nls.NlsKey;
+import com.elster.jupiter.nls.TranslationKey;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.associations.IsPresent;
 import com.elster.jupiter.orm.associations.Reference;
@@ -18,12 +20,16 @@ import com.elster.jupiter.util.units.Dimension;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PartiallySpecifiedReadingTypeImpl extends ReadingTypeRequirementImpl implements PartiallySpecifiedReadingType {
     public static final String TYPE_IDENTIFIER = "PRT";
@@ -52,6 +58,95 @@ public class PartiallySpecifiedReadingTypeImpl extends ReadingTypeRequirementImp
     @Override
     public ReadingTypeTemplate getReadingTypeTemplate() {
         return this.readingTypeTemplate.get();
+    }
+
+    @Override
+    public String getStringDescription() {
+        List <String> valueElements = new ArrayList<>();
+        getTranslatedTimeValue().ifPresent(v -> valueElements.add("["+v+"]"));
+        getTranslatedAttributeValue(ReadingTypeTemplateAttributeName.ACCUMULATION).ifPresent(valueElements::add);
+        getTranslatedAttributeValue(ReadingTypeTemplateAttributeName.FLOW_DIRECTION).ifPresent(valueElements::add);
+        getTranslatedAttributeValue(ReadingTypeTemplateAttributeName.COMMODITY).ifPresent(valueElements::add);
+        valueElements.add(this.getName());
+        getTranslatedUnitWithMultiplierValueString().ifPresent(valueElements::add);
+        getTranslatedAttributeValue(ReadingTypeTemplateAttributeName.PHASE).ifPresent(valueElements::add);
+        getCodedValue(ReadingTypeTemplateAttributeName.TIME_OF_USE, "ToU").ifPresent(valueElements::add);
+        getCodedValue(ReadingTypeTemplateAttributeName.CRITICAL_PEAK_PERIOD, "CPP").ifPresent(valueElements::add);
+        getCodedValue(ReadingTypeTemplateAttributeName.CONSUMPTION_TIER, "Tier").ifPresent(valueElements::add);
+        return String.join(" ", valueElements);
+    }
+
+
+    public Optional<String> getTranslatedAttributeValue(ReadingTypeTemplateAttributeName attributeName){
+        List<Integer> possibleValues = this.getReadingTypeTemplate().getAttribute(attributeName).getPossibleValues();
+        Optional<Integer> overrideCode = overriddenAttributes.stream().filter(a -> a.getName().equals(attributeName)).findFirst().map(PartiallySpecifiedReadingTypeAttributeValueImpl::getCode);
+        if (overrideCode.isPresent()){
+            return Optional.ofNullable(translate(attributeName.getDefinition(),overrideCode.get()));
+        } else if (possibleValues.size()==1){
+            return Optional.ofNullable(translate(attributeName.getDefinition(),possibleValues.get(0)));
+        } else {
+            return attributeName.getDefinition().canBeWildcard() ? Optional.of("*") : Optional.empty();
+        }
+    }
+
+    private Optional<String> getCodedValue(ReadingTypeTemplateAttributeName attributeName, String prefix){
+        List<Integer> possibleValues = this.getReadingTypeTemplate().getAttribute(attributeName).getPossibleValues();
+        Optional<Integer> overrideCode = overriddenAttributes.stream().filter(a -> a.getName().equals(attributeName)).findFirst().map(PartiallySpecifiedReadingTypeAttributeValueImpl::getCode);
+        if (overrideCode.isPresent()){
+            return Optional.of(prefix + overrideCode.get());
+        } else if (possibleValues.size()==1){
+            return Optional.of(prefix + possibleValues.get(0));
+        } else {
+            return attributeName.getDefinition().canBeWildcard() ? Optional.of("*") : Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<String> getTranslatedTimeValue(){
+        Optional<Integer> macroPeriodCode = this.getReadingTypeTemplate().getAttribute(ReadingTypeTemplateAttributeName.MACRO_PERIOD).getPossibleValues().stream().findFirst();
+        Optional<Integer> timeCode = this.getReadingTypeTemplate().getAttribute(ReadingTypeTemplateAttributeName.TIME).getPossibleValues().stream().findFirst();
+        Optional<PartiallySpecifiedReadingTypeAttributeValueImpl> overrideCode = overriddenAttributes.stream()
+                .filter(a -> a.getName().equals(ReadingTypeTemplateAttributeName.MACRO_PERIOD) || a.getName().equals(ReadingTypeTemplateAttributeName.TIME))
+                .findFirst();
+        if (overrideCode.isPresent()){
+            return Optional.ofNullable(translate(overrideCode.get().getName().getDefinition(),overrideCode.get().getCode()));
+        } else if (macroPeriodCode.isPresent()){
+            return Optional.ofNullable(translate(ReadingTypeTemplateAttributeName.MACRO_PERIOD.getDefinition(),macroPeriodCode.get()));
+        } else if (timeCode.isPresent()){
+            return Optional.ofNullable(translate(ReadingTypeTemplateAttributeName.TIME.getDefinition(),timeCode.get()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<String> getTranslatedUnitValues(){
+        String multiplier = getTranslatedAttributeValue(ReadingTypeTemplateAttributeName.METRIC_MULTIPLIER).orElse("");
+        Optional<Integer> overrideUnitCode = overriddenAttributes.stream()
+                .filter(a -> a.getName().equals(ReadingTypeTemplateAttributeName.UNIT_OF_MEASURE))
+                .findFirst().map(PartiallySpecifiedReadingTypeAttributeValueImpl::getCode);
+        if (overrideUnitCode.isPresent()){
+            return Collections.singletonList(translate(ReadingTypeTemplateAttributeName.UNIT_OF_MEASURE.getDefinition(),overrideUnitCode.get()));
+        } else {
+            return this.getReadingTypeTemplate()
+                    .getAttribute(ReadingTypeTemplateAttributeName.UNIT_OF_MEASURE)
+                    .getPossibleValues().stream().map(e -> translate(ReadingTypeTemplateAttributeName.UNIT_OF_MEASURE.getDefinition(),e)).collect(Collectors.toList());
+
+        }
+    }
+
+    private Optional<String> getTranslatedUnitWithMultiplierValueString(){
+        String multiplier = getTranslatedAttributeValue(ReadingTypeTemplateAttributeName.METRIC_MULTIPLIER).orElse("");
+        List<String> units = getTranslatedUnitValues().stream().map(e -> multiplier + e).collect(Collectors.toList());
+        if(!units.isEmpty()){
+            return Optional.of("(" + String.join(", ", units) + ")");
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private <T> String translate(ReadingTypeTemplateAttributeName.ReadingTypeAttribute<T> definition, Integer code){
+        return this.getMetrologyConfigurationService().getThesaurus().getFormat(definition.getTranslationProvider().apply(definition.getCodeToValueConverter().apply(code))).format();
     }
 
     @Override
