279565fcfcfb334a37a77a32cb8f9e76d5fcf3ae
Pasquien Dorny <pasquien.dorny@elster.com>
2016-04-04 15:28:05 +0200

    CXO-845: Use time boundaries in firmware campaign execution CXO-928: View firmware history on device

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceFirmwareHistoryInfoFactory.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceFirmwareHistoryInfoFactory.java
@@ -2,52 +2,52 @@ package com.energyict.mdc.device.data.rest.impl;
 
 import com.elster.jupiter.nls.Thesaurus;
 import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.firmware.DeviceFirmwareHistory;
+import com.energyict.mdc.firmware.DeviceFirmwareVersionHistoryRecord;
+import com.energyict.mdc.firmware.FirmwareService;
 
 import javax.inject.Inject;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DeviceFirmwareHistoryInfoFactory {
 
+    private final FirmwareService firmwareService;
     private final Thesaurus thesaurus;
 
     @Inject
-    public DeviceFirmwareHistoryInfoFactory(Thesaurus thesaurus) {
+    public DeviceFirmwareHistoryInfoFactory(Thesaurus thesaurus, FirmwareService firmwareService) {
         this.thesaurus = thesaurus;
+        this.firmwareService = firmwareService;
     }
 
     public DeviceFirmwareHistoryInfos createDeviceFirmwareHistoryInfos(Device device) {
-        DeviceFirmwareHistoryInfos infos = new DeviceFirmwareHistoryInfos();
-
-        // Hard coded fake data for now
-        // To do: real data for this specific device
-        // (be sure to order the data DESCENDING, the most recent activation date first)
-        DeviceFirmwareHistoryInfo info1 = new DeviceFirmwareHistoryInfo();
-        info1.firmwareType = "Meter firmware";
-        info1.firmwareVersion = "NTA-Sim_V_2.1.0";
-        info1.activationDate = Instant.now();
-        infos.deviceFirmwareHistoryInfos.add(info1);
-
-        DeviceFirmwareHistoryInfo info2 = new DeviceFirmwareHistoryInfo();
-        info2.firmwareType = "Communication firmware";
-        info2.firmwareVersion = "NTA-Sim_V_1.0.0";
-        info2.activationDate = Instant.now().minusSeconds(3600);
-        infos.deviceFirmwareHistoryInfos.add(info2);
-
-        infos.total = infos.deviceFirmwareHistoryInfos.size();
-        return infos;
+        return new DeviceFirmwareHistoryInfos(firmwareService.getFirmwareHistory(device));
     }
-
 
     static class DeviceFirmwareHistoryInfos {
         public int total = 0;
         public List<DeviceFirmwareHistoryInfo> deviceFirmwareHistoryInfos = new ArrayList<>();
+
+        DeviceFirmwareHistoryInfos(DeviceFirmwareHistory history){
+            List<DeviceFirmwareVersionHistoryRecord> allHistory = history.history();
+            this.total = allHistory.size();
+            deviceFirmwareHistoryInfos = allHistory.stream().map(DeviceFirmwareHistoryInfo::new).collect(Collectors.toList());
+        }
     }
 
     static class DeviceFirmwareHistoryInfo {
         public String firmwareVersion;
         public String firmwareType;
         public Instant activationDate;
+
+        DeviceFirmwareHistoryInfo(DeviceFirmwareVersionHistoryRecord historyRecord){
+            this.firmwareVersion = historyRecord.getFirmwareVersion().getFirmwareVersion();
+            this.firmwareType = historyRecord.getFirmwareVersion().getFirmwareType().getType();
+            this.activationDate = historyRecord.getInterval().getStart();
+        }
     }
+
 }
