72e82c2c1f42dbcc17a219f5e8b53bc425c53bd8
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-04-05 16:32:43 +0200

    Add support for monthly aggregation with meter activation in the middle of the month

---
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/aggregation/CalculatedMetrologyContractDataImpl.java
@@ -0,0 +1,120 @@
+package com.elster.jupiter.metering.impl.aggregation;
+
+import com.elster.jupiter.metering.BaseReadingRecord;
+import com.elster.jupiter.metering.MeterActivation;
+import com.elster.jupiter.metering.ReadingType;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.metering.aggregation.CalculatedMetrologyContractData;
+import com.elster.jupiter.metering.config.MetrologyContract;
+import com.elster.jupiter.metering.config.ReadingTypeDeliverable;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Provides an implementation for the {@link CalculatedMetrologyContractData} interface.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2016-04-04 (10:24)
+ */
+public class CalculatedMetrologyContractDataImpl implements CalculatedMetrologyContractData {
+
+    private final UsagePoint usagePoint;
+    private final MetrologyContract contract;
+    private final Map<ReadingType, List<CalculatedReadingRecord>> calculatedReadingRecords;
+
+    public CalculatedMetrologyContractDataImpl(UsagePoint usagePoint, MetrologyContract contract, Map<ReadingType, List<CalculatedReadingRecord>> calculatedReadingRecords) {
+        this.usagePoint = usagePoint;
+        this.contract = contract;
+        this.injectUsagePoint(calculatedReadingRecords);
+        this.calculatedReadingRecords = this.mergeMeterActivations(calculatedReadingRecords);
+    }
+
+    protected void injectUsagePoint(Map<ReadingType, List<CalculatedReadingRecord>> calculatedReadingRecords) {
+        calculatedReadingRecords
+                .values()
+                .stream()
+                .flatMap(List::stream)
+                .forEach(this::injectUsagePoint);
+    }
+
+    private void injectUsagePoint(CalculatedReadingRecord calculatedReadingRecord) {
+        calculatedReadingRecord.setUsagePoint(this.usagePoint);
+    }
+
+    /**
+     * Merges multiple {@link CalculatedReadingRecord}s that relate to different
+     * {@link com.elster.jupiter.metering.MeterActivation}s within the same
+     * aggregation period. As an example, when a different meter was activated
+     * on a usage point in the middle of a month and aggregation is requested
+     * for one month then the two meter activations result in two different
+     * CalculatedReadingRecords because of the way the SQL is structured.
+     * These two CalculatedReadingRecords need to be merged to one and the same.
+     *
+     * @param calculatedReadingRecords The List of CalculatedReadingRecord organized by {@link ReadingType}
+     * @return The List of CalculatedReadingRecord organized by ReadingType spanning only periods that relate to the aggregation specified in the ReadingType
+     */
+    private Map<ReadingType, List<CalculatedReadingRecord>> mergeMeterActivations(Map<ReadingType, List<CalculatedReadingRecord>> calculatedReadingRecords) {
+        Map<ReadingType, List<CalculatedReadingRecord>> merged = new HashMap<>();
+        calculatedReadingRecords.entrySet().stream().forEach(readingTypeAndRecords -> this.mergeMeterActivations(readingTypeAndRecords, merged));
+        return merged;
+    }
+
+    private void mergeMeterActivations(Map.Entry<ReadingType, List<CalculatedReadingRecord>> readingTypeAndRecords, Map<ReadingType, List<CalculatedReadingRecord>> merged) {
+        merged.put(readingTypeAndRecords.getKey(), this.merge(readingTypeAndRecords.getKey(), readingTypeAndRecords.getValue()));
+    }
+
+    private List<CalculatedReadingRecord> merge(ReadingType readingType, List<CalculatedReadingRecord> readingRecords) {
+        Map<Instant, CalculatedReadingRecord> merged = new TreeMap<>(); // Keeps the keys sorted
+        IntervalLength intervalLength = IntervalLength.from(readingType);
+        readingRecords
+                .stream()
+                .forEach(record -> this.merge(record, intervalLength, merged));
+        return new ArrayList<>(merged.values());
+    }
+
+    private void merge(CalculatedReadingRecord record,  IntervalLength intervalLength, Map<Instant,CalculatedReadingRecord> merged) {
+        ZoneId zone = this.getUsagePoint()
+                            .getMeterActivation(record.getTimeStamp())
+                            .map(MeterActivation::getZoneId)
+                            .orElseGet(this.usagePoint::getZoneId);
+        final Instant endOfInterval;
+        Instant endOfIntervalCandidate = intervalLength.truncate(record.getTimeStamp(), zone);
+        if (!endOfIntervalCandidate.equals(record.getTimeStamp())) {
+            // Timestamp was not aligned with interval
+            endOfInterval = intervalLength.addTo(endOfIntervalCandidate, zone);
+        } else {
+            endOfInterval = endOfIntervalCandidate;
+        }
+        merged.compute(
+                endOfInterval,
+                (timestamp, existingRecord) -> existingRecord == null ? record : CalculatedReadingRecord.merge(existingRecord, record, endOfInterval));
+    }
+
+    @Override
+    public UsagePoint getUsagePoint() {
+        return this.usagePoint;
+    }
+
+    @Override
+    public MetrologyContract getMetrologyContract() {
+        return this.contract;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.calculatedReadingRecords.isEmpty();
+    }
+
+    @Override
+    public List<? extends BaseReadingRecord> getCalculatedDataFor(ReadingTypeDeliverable deliverable) {
+        return this.calculatedReadingRecords.getOrDefault(deliverable.getReadingType(), Collections.emptyList());
+    }
+
+}
---
+++ coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/aggregation/IntervalLengthAdditionTest.java
@@ -0,0 +1,199 @@
+package com.elster.jupiter.metering.impl.aggregation;
+
+import java.time.Instant;
+import java.time.ZoneId;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests the {@link IntervalLength#addTo(Instant, ZoneId)} method.
+ */
+public class IntervalLengthAdditionTest {
+
+    @Test
+    public void testOneMinute() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE1.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(960L));
+    }
+
+    @Test
+    public void testTwoMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE2.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1020L));
+    }
+
+    @Test
+    public void testThreeMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE3.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1080L));
+    }
+
+    @Test
+    public void testFourMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE4.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1140L));
+    }
+
+    @Test
+    public void testFiveMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE5.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1200L));
+    }
+
+    @Test
+    public void testSixMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE6.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1260L));
+    }
+
+    @Test
+    public void testTenMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE10.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1500L));
+    }
+
+    @Test
+    public void testTwelveMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE12.addTo(Instant.ofEpochSecond(900L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(1620L));
+    }
+
+    @Test
+    public void testFifteenMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE15.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(87300L));
+    }
+
+    @Test
+    public void testThirtyMinutes() {
+        // Business method
+        Instant instant = IntervalLength.MINUTE30.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(88200L));
+    }
+
+    @Test
+    public void testOneHour() {
+        // Business method
+        Instant instant = IntervalLength.HOUR1.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(86400L + 3600L));
+    }
+
+    @Test
+    public void testTwoHours() {
+        // Business method
+        Instant instant = IntervalLength.HOUR2.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(86400L + 7200L));
+    }
+
+    @Test
+    public void testThreeHours() {
+        // Business method
+        Instant instant = IntervalLength.HOUR3.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(86400L + 10800L));
+    }
+
+    @Test
+    public void testFourHours() {
+        // Business method
+        Instant instant = IntervalLength.HOUR4.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(86400L + 14400L));
+    }
+
+    @Test
+    public void testSixHours() {
+        // Business method
+        Instant instant = IntervalLength.HOUR6.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(86400L + 21600L));
+    }
+
+    @Test
+    public void testTwelveHours() {
+        // Business method
+        Instant instant = IntervalLength.HOUR12.addTo(Instant.ofEpochSecond(86400L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(129600L));
+    }
+
+    @Test
+    public void testOneDay() {
+        // Business method
+        Instant instant = IntervalLength.DAY1.addTo(Instant.ofEpochSecond(172800L), testZoneId());
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochSecond(259200L));
+    }
+
+    @Test
+    public void testOneWeek() {
+        // Business method
+        Instant instant = IntervalLength.WEEK1.addTo(Instant.ofEpochMilli(1459807200000L), testZoneId());  // In Europe/Brussels: 2016-04-05 00:00:00
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochMilli(1460412000000L));  // In Europe/Brussels: 2016-04-12 00:00:00
+    }
+
+    @Test
+    public void testOneMonth() {
+        // Business method
+        Instant instant = IntervalLength.MONTH1.addTo(Instant.ofEpochMilli(1459807200000L), testZoneId());  // In Europe/Brussels: 2016-04-05 00:00:00
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochMilli(1462399200000L));  // In Europe/Brussels: 2016-05-05 00:00:00
+    }
+
+    @Test
+    public void testOneYear() {
+        // Business method
+        Instant instant = IntervalLength.YEAR1.addTo(Instant.ofEpochMilli(1459807200000L), testZoneId());  // In Europe/Brussels: 2016-04-05 00:00:00
+
+        // Asserts
+        assertThat(instant).isEqualTo(Instant.ofEpochMilli(1491343200000L));  // In Europe/Brussels: 2017-04-05 00:00:00
+    }
+
+    protected ZoneId testZoneId() {
+        return ZoneId.of("Europe/Brussels");
+    }
+
+}
