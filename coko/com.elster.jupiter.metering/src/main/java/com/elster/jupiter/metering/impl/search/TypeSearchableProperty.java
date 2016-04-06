d2f207a8fdf218da0e4dfffad5fb6666842f374c
Valery Ruchin <varuchin@mera.ru>
2016-04-06 19:58:59 +0300

    Added some removables criteria

---
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/search/TypeSearchableProperty.java
@@ -0,0 +1,199 @@
+package com.elster.jupiter.metering.impl.search;
+
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.properties.AbstractValueFactory;
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.PropertySpecService;
+import com.elster.jupiter.search.SearchDomain;
+import com.elster.jupiter.search.SearchableProperty;
+import com.elster.jupiter.search.SearchablePropertyConstriction;
+import com.elster.jupiter.search.SearchablePropertyGroup;
+import com.elster.jupiter.util.conditions.Comparison;
+import com.elster.jupiter.util.conditions.Condition;
+import com.elster.jupiter.util.conditions.Where;
+
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class TypeSearchableProperty implements SearchableUsagePointProperty {
+
+    private final SearchDomain domain;
+    private final PropertySpecService propertySpecService;
+    private final Thesaurus thesaurus;
+    private static final String FIELDNAME = "type";
+
+    public TypeSearchableProperty(SearchDomain domain, PropertySpecService propertySpecService, Thesaurus thesaurus) {
+        super();
+        this.domain = domain;
+        this.propertySpecService = propertySpecService;
+        this.thesaurus = thesaurus;
+    }
+
+    @Override
+    public SearchDomain getDomain() {
+        return domain;
+    }
+
+    @Override
+    public boolean affectsAvailableDomainProperties() {
+        return false;
+    }
+
+    @Override
+    public Optional<SearchablePropertyGroup> getGroup() {
+        return Optional.empty();
+    }
+
+    @Override
+    public SearchableProperty.Visibility getVisibility() {
+        return SearchableProperty.Visibility.REMOVABLE;
+    }
+
+    @Override
+    public SearchableProperty.SelectionMode getSelectionMode() {
+        return SearchableProperty.SelectionMode.SINGLE;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return PropertyTranslationKeys.USAGEPOINT_TYPE.getDisplayName(this.thesaurus);
+    }
+
+    @Override
+    public String toDisplay(Object value) {
+        if (!this.valueCompatibleForDisplay(value)) {
+            throw new IllegalArgumentException("Value not compatible with domain");
+        }
+        return this.toDisplayAfterValidation(value);
+    }
+
+    private String toDisplayAfterValidation(Object value) {
+        UsagePointTypes usagePointTypes = (UsagePointTypes) value;
+        return usagePointTypes.getValue();
+    }
+
+    private boolean valueCompatibleForDisplay(Object value) {
+        return value instanceof Enum;
+    }
+
+    @Override
+    public PropertySpec getSpecification() {
+        return propertySpecService
+                .specForValuesOf(new TypeValueFactory())
+                .named(FIELDNAME, PropertyTranslationKeys.USAGEPOINT_TYPE)
+                .fromThesaurus(this.thesaurus)
+                .addValues(UsagePointTypes.values())
+                .markExhaustive()
+                .finish();
+    }
+
+    @Override
+    public List<SearchableProperty> getConstraints() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void refreshWithConstrictions(List<SearchablePropertyConstriction> constrictions) {
+        if (!constrictions.isEmpty()) {
+            throw new IllegalArgumentException("No constraint to refresh");
+        }
+    }
+
+    @Override
+    public Condition toCondition(Condition specification) {
+        return Arrays.asList(((Comparison) specification).getValues()).stream()
+                .map(UsagePointTypes.class::cast).map(result -> {
+                    Condition condition = null;
+                    if (result.getValue().equalsIgnoreCase("Measured SDP")) {
+                        condition = Where.where("isVirtual")
+                                .isEqualTo(true)
+                                .and(Where.where("isSdp").isEqualTo(true));
+                    }
+                    if (result.getValue().equalsIgnoreCase("Measured non-SDP")) {
+                        condition = Where.where("isVirtual")
+                                .isEqualTo(true)
+                                .and(Where.where("isSdp").isEqualTo(false));
+                    }
+                    if (result.getValue().equalsIgnoreCase("Unmeasured SDP")) {
+                        condition = Where.where("isVirtual")
+                                .isEqualTo(false)
+                                .and(Where.where("isSdp").isEqualTo(true));
+                    }
+                    if (result.getValue().equalsIgnoreCase("Unmeasured non-SDP")) {
+                        condition = Where.where("isVirtual")
+                                .isEqualTo(false)
+                                .and(Where.where("isSdp").isEqualTo(false));
+                    }
+                    return condition;
+                }).findAny().orElseThrow(null);
+    }
+
+    private enum UsagePointTypes {
+        MEASURED_SDP {
+            @Override
+            public String getValue() {
+                return "Measured SDP";
+            }
+        },
+        UNMEASURED_SDP {
+            @Override
+            public String getValue() {
+                return "Unmeasured SDP";
+            }
+        },
+        MEASURED_NON_SDP {
+            @Override
+            public String getValue() {
+                return "Measured non-SDP";
+            }
+        },
+        UNMEASURED_NON_SDP {
+            @Override
+            public String getValue() {
+                return "Unmeasured non-SDP";
+            }
+        };
+
+        public abstract String getValue();
+    }
+
+    static class TypeValueFactory extends AbstractValueFactory<Enum> {
+
+        @Override
+        protected int getJdbcType() {
+            return Types.VARCHAR;
+        }
+
+        @Override
+        public Enum fromStringValue(String stringValue) {
+            return Stream.of(UsagePointTypes.values())
+                    .filter(enumValue -> stringValue.equalsIgnoreCase(enumValue.name()))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+
+        @Override
+        public String toStringValue(Enum object) {
+            return String.valueOf(object);
+        }
+
+        @Override
+        public Class<Enum> getValueType() {
+            return Enum.class;
+        }
+
+        @Override
+        public Enum valueFromDatabase(Object object) {
+            return (Enum) object;
+        }
+
+        @Override
+        public Object valueToDatabase(Enum object) {
+            return object;
+        }
+    }
+}
--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/search/UsagePointSearchDomain.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/search/UsagePointSearchDomain.java
@@ -4,6 +4,7 @@ import com.elster.jupiter.domain.util.DefaultFinder;
 import com.elster.jupiter.domain.util.Finder;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.metering.UsagePointDetail;
 import com.elster.jupiter.metering.config.MetrologyConfigurationService;
 import com.elster.jupiter.metering.impl.ServerMeteringService;
 import com.elster.jupiter.metering.impl.config.UsagePointMetrologyConfiguration;
@@ -112,6 +113,10 @@ public class UsagePointSearchDomain implements SearchDomain {
                 new NameSearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
                 new ServiceCategorySearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
                 new ConnectionStateSearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
+                new PhaseCodeSearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
+                new ReadRouteSearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
+                new TypeSearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
+                new ServicePrioritySearchableProperty(this, this.propertySpecService, this.meteringService.getThesaurus()),
                 new MetrologyConfigurationSearchableProperty(this, this.propertySpecService, this.metrologyConfigurationService, this.meteringService
                         .getThesaurus())
         ));
@@ -172,7 +177,7 @@ public class UsagePointSearchDomain implements SearchDomain {
 
         private UsagePointFinder(Condition condition) {
             this.finder = DefaultFinder
-                    .of(UsagePoint.class, condition, meteringService.getDataModel(), UsagePointMetrologyConfiguration.class)
+                    .of(UsagePoint.class, condition, meteringService.getDataModel(), UsagePointMetrologyConfiguration.class, UsagePointDetail.class)
                     .defaultSortColumn("mRID");
         }
 
