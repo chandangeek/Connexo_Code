339c0d14c8f7fa7e050b060141d19ddad6551873
H165720 <albertv@rimroe.elster-group.com>
2016-04-07 14:56:22 +0300

    Support for using default LogManager.

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/UsersCreatedMessageHandlerFactory.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/UsersCreatedMessageHandlerFactory.java
@@ -3,16 +3,14 @@ package com.elster.jupiter.users.impl;
 import com.elster.jupiter.messaging.subscriber.MessageHandler;
 import com.elster.jupiter.messaging.subscriber.MessageHandlerFactory;
 import com.elster.jupiter.util.json.JsonService;
+
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
-import java.io.IOException;
-import java.time.Clock;
-import java.util.Arrays;
-import java.util.logging.*;
+import java.util.logging.Logger;
 
 @Component(name = "com.elster.jupiter.users.messagehandlerlauncher", service = MessageHandlerFactory.class, property = {"subscriber=UsrQueueSubsc", "destination=UsrQueueDest"}, immediate = true)
 public class UsersCreatedMessageHandlerFactory implements MessageHandlerFactory{
@@ -26,7 +24,6 @@ public class UsersCreatedMessageHandlerFactory implements MessageHandlerFactory{
     private Logger userLogin = Logger.getLogger(USER_LOG);
     private Logger tokenRenewal = Logger.getLogger(TOKEN_RENEWAL);
     private volatile JsonService jsonService;
-    private volatile Clock clock;
 
     @Override
     public MessageHandler newMessageHandler() {
@@ -35,37 +32,10 @@ public class UsersCreatedMessageHandlerFactory implements MessageHandlerFactory{
 
     @Activate
     public void activate(BundleContext context) {
-        LogManager manager = LogManager.getLogManager();
-        userLogin = manager.getProperty(USER_LOG + ".handler") != null ? setFileHandler(manager, manager.getProperty(USER_LOG + ".handler"), userLogin) : userLogin;
-        tokenRenewal = manager.getProperty(TOKEN_RENEWAL + ".handler") != null ? setFileHandler(manager, manager.getProperty(TOKEN_RENEWAL + ".handler"), tokenRenewal) : tokenRenewal;
     }
 
     @Deactivate
     public void deactivate() {
-        Arrays.stream(tokenRenewal.getHandlers()).forEach(Handler::close);
-        Arrays.stream(userLogin.getHandlers()).forEach(Handler::close);
-    }
-
-    private Logger setFileHandler(LogManager manager, String handler, Logger logger){
-        String pattern = manager.getProperty(handler + ".pattern");
-        if(pattern != null){
-            try {
-                String limit = manager.getProperty(handler + ".limit") != null ? manager.getProperty(handler + ".limit") : "0";
-                String count = manager.getProperty(handler + ".count") != null ? manager.getProperty(handler + ".count") : "1";
-                FileHandler fileHandler = new FileHandler(pattern, Integer.valueOf(limit), Integer.valueOf(count), true);
-                Formatter f = new SingleLineFormatter();
-                fileHandler.setFormatter(f);
-                logger.addHandler(fileHandler);
-            } catch (IOException e) {
-                logger.log(Level.SEVERE , e.getMessage(), e);
-            }
-        }
-        return logger;
-    }
-
-    @Reference
-    public void setClock(Clock clock){
-        this.clock = clock;
     }
 
     @Reference
