82331f593415adc6d005fd38a4553474b564ea52
David Nitescu <david.nitescu@elster.com>
2016-04-08 14:30:50 +0300

    add geoCoordinates

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/model/DeviceAttribute.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/model/DeviceAttribute.js
@@ -33,10 +33,14 @@ Ext.define('Mdc.model.DeviceAttribute', {
             }
         },
         {
+            name: 'geoCoordinates',
+            type: 'auto'
+        },
+        {
             name: 'location',
             type: 'auto',
             convert: function (value, record) {
-                return value.displayValue;
+                return value.displayValue != ""? value.displayValue : record.get('geoCoordinates').displayValue;
             }
         }
     ],
