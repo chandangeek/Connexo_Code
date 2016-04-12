30e4c915e8e550d9e19dc334992361e1ec2fc147
David Nitescu <david.nitescu@elster.com>
2016-04-12 18:44:14 +0300

    tooltip and default column added

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/service/Search.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/service/Search.js
@@ -7,15 +7,23 @@ Ext.define('Mdc.service.Search', {
 
         if (column && column.dataIndex === 'mRID') {
             if (me.searchDomain.getId() === 'com.energyict.mdc.device.data.Device') {
-                column.renderer = function (value) {
+                column.renderer = function (value, metaData, record) {
                     var url = me.router.getRoute('devices/device').buildUrl({mRID: encodeURIComponent(value)});
+                    metaData.tdAttr = 'data-qtip="' + value + '"';
                     return '<a href="{0}">{1}</a>'.replace('{0}', url).replace('{1}', Ext.String.htmlEncode(value));
                 }
             } else if (me.searchDomain.getId() === 'com.elster.jupiter.metering.UsagePoint') {
                 column.renderer = function (value, metaData, record) {
                     var url = me.router.getRoute('usagepoints/usagepoint').buildUrl({usagePointId: record.get('id')});
+                    metaData.tdAttr = 'data-qtip="' + value + '"';
                     return '<a href="{0}">{1}</a>'.replace('{0}', url).replace('{1}', Ext.String.htmlEncode(value));
                 }
+            }
+        }
+        else {
+            column.renderer = function (value, metaData, record) {
+                metaData.tdAttr = 'data-qtip="' + value + '"';
+                return value;
             }
         }
 
