b662a6c83d5486c66c23540af5deea263a952885
David Nitescu <david.nitescu@elster.com>
2016-04-13 11:27:33 +0300

    CXO-1243 - tooltip fix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/service/Search.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/service/Search.js
@@ -9,21 +9,22 @@ Ext.define('Mdc.service.Search', {
             if (me.searchDomain.getId() === 'com.energyict.mdc.device.data.Device') {
                 column.renderer = function (value, metaData, record) {
                     var url = me.router.getRoute('devices/device').buildUrl({mRID: encodeURIComponent(value)});
-                    metaData.tdAttr = 'data-qtip="' + value + '"';
+                    metaData.tdAttr = 'data-qtip="' + Ext.String.htmlEncode(Ext.String.htmlEncode(value)) + '"';
                     return '<a href="{0}">{1}</a>'.replace('{0}', url).replace('{1}', Ext.String.htmlEncode(value));
                 }
             } else if (me.searchDomain.getId() === 'com.elster.jupiter.metering.UsagePoint') {
                 column.renderer = function (value, metaData, record) {
                     var url = me.router.getRoute('usagepoints/usagepoint').buildUrl({usagePointId: record.get('id')});
-                    metaData.tdAttr = 'data-qtip="' + value + '"';
+                    metaData.tdAttr = 'data-qtip="' + Ext.String.htmlEncode(Ext.String.htmlEncode(value)) + '"';
                     return '<a href="{0}">{1}</a>'.replace('{0}', url).replace('{1}', Ext.String.htmlEncode(value));
                 }
             }
         }
         else {
             column.renderer = function (value, metaData, record) {
-                metaData.tdAttr = 'data-qtip="' + Ext.String.htmlEncode(value) + '"';
-                return value;
+                // stupid solution to resolve encoding in tooltip
+                metaData.tdAttr = 'data-qtip="' + Ext.String.htmlEncode(Ext.String.htmlEncode(value)) + '"';
+                return Ext.String.htmlEncode(value);
             }
         }
 
