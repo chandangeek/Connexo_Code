17a934fbf02ff7d62bc0b67f1e2675cf46d45980
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-04-19 13:18:10 +0200

    VersionedDataModel

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/Holder.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/Holder.java
@@ -1,14 +1,11 @@
 package com.elster.jupiter.util;
 
+import java.util.function.Supplier;
+
 /**
  * Holds a reference to a variable of type T
  * @param <T>
  */
-public interface Holder<T> {
-
-    /**
-     * @return the instance currently held.
-     */
-    T get();
+public interface Holder<T> extends Supplier<T> {
 
 }
