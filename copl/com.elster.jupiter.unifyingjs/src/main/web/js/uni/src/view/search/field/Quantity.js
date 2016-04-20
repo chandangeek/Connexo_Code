26d9d7e07cb4307524e32913f7c40a07e6fb7d6e
Sentyurin Sergey <ssentyur@mera.ru>
2016-04-20 11:21:55 +0300

    CXO-939+

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/Quantity.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/search/field/Quantity.js
@@ -6,49 +6,6 @@ Ext.define('Uni.view.search.field.Quantity', {
     alias: 'widget.uni-search-criteria-quantity',
     minWidth: 400,
 
-    //setValue: function (value) {
-    //    var me = this,
-    //        timeUnit = null;
-    //
-    //    Ext.Array.each(value, function (record) {
-    //        var values = [];
-    //
-    //        Ext.Array.each(record.get('criteria'), function (criteriaValue) {
-    //            var valueToArr = criteriaValue.split(':');
-    //
-    //            values.push(valueToArr[0]);
-    //            timeUnit = ['0', valueToArr[1], valueToArr[2]].join(':');
-    //        });
-    //
-    //        record.set('criteria', values);
-    //    });
-    //
-    //    me.getUnitField().setValue(timeUnit);
-    //    me.callParent(arguments);
-    //},
-    //
-    //getValue: function () {
-    //    var me = this,
-    //        value = this.callParent(arguments);
-    //
-    //    return value ? value.map(function (v) {
-    //        var criteria = v.get('criteria'),
-    //            unitFieldValue = me.getUnitField().getValue(),
-    //            unit;
-    //
-    //        if (Ext.isString(unitFieldValue)) {
-    //            unit = unitFieldValue.split(':');
-    //
-    //            if (unit.length === 3) {
-    //                v.set('criteria', _.map(Ext.isArray(criteria) ? criteria : [criteria], function (item) {
-    //                    return [item, unit[1], unit[2]].join(':');
-    //                }));
-    //            }
-    //        }
-    //        return v;
-    //    }) : null;
-    //},
-
     createCriteriaLine: function(config) {
         var me = this,
             store = Ext.create('Ext.data.Store', {
