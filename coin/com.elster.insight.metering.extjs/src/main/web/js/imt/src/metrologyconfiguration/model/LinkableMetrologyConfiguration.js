33ae8e4b1b05a54b1ae8b271aa0b79612ec45972
akuryuk <akuryuk@mera.ru>
2016-04-20 11:28:40 +0300

    [CXO-945] Link metrology configuration to a usage point without activated meters + [CXO-1315] Cuncurrent changes + Design update changes

---
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/metrologyconfiguration/model/LinkableMetrologyConfiguration.js
@@ -0,0 +1,23 @@
+Ext.define('Imt.metrologyconfiguration.model.LinkableMetrologyConfiguration', {
+    extend: 'Uni.model.Version',
+    fields: [
+        {name: 'id', type: 'number', useNull: true},
+        {name: 'name', type: 'string'}
+    ],
+    associations: [
+        {name: 'customPropertySets', type: 'hasMany', model: 'Imt.customattributesonvaluesobjects.model.AttributeSetOnObject', associationKey: 'customPropertySets', foreignKey: 'customPropertySets'}
+    ],
+    proxy: {
+        type: 'rest',
+        urlTpl: '/api/udr/usagepoints/{mRID}/metrologyconfiguration',
+        timeout: 240000,
+        reader: {
+            type: 'json',
+            root: 'metrologyConfigurations'
+        },
+        setUrl: function (mRID) {
+            this.url = this.urlTpl.replace('{mRID}', encodeURIComponent(mRID));
+        }
+    }
+
+});
