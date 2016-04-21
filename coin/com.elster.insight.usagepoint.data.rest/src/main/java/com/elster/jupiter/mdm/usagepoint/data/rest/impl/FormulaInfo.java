7abd87db6283b23e2db7ee963b13c80f945108be
Kirill Koshelev <kirkosh@mera.ru>
2016-04-21 17:49:53 +0300

    add files

---
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/FormulaInfo.java
@@ -0,0 +1,18 @@
+package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FormulaInfo {
+    public String description;
+    public List<ReadingTypeRequirementsInfo> readingTypeRequirements;
+    public List<CustomPropertiesInfo> customProperties;
+
+    public FormulaInfo() {
+    }
+}
