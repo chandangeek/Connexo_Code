9133b30d1b79bece95de1bfa91d96bd16607be74
igh <isabelle.gheysens@elster.com>
2016-04-21 15:58:05 +0200

    CXO-1008 - View time of use calendars

--- coko/com.elster.jupiter.calendar/src/test/java/com/elster/jupiter/calendar/impl/CalendarCrudTest.java
+++ coko/com.elster.jupiter.calendar/src/test/java/com/elster/jupiter/calendar/impl/CalendarCrudTest.java
@@ -1,5 +1,6 @@
 package com.elster.jupiter.calendar.impl;
 
+import com.elster.jupiter.calendar.Calendar;
 import com.elster.jupiter.calendar.CalendarService;
 import com.elster.jupiter.devtools.persistence.test.rules.Transactional;
 import com.elster.jupiter.devtools.persistence.test.rules.TransactionalRule;
@@ -10,7 +11,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.validation.constraints.AssertTrue;
 import java.time.Year;
 import java.util.TimeZone;
 
@@ -44,7 +47,14 @@ public class CalendarCrudTest {
     // formula = Requirement
     public void testRequirementNodeCrud() {
         CalendarService service = getCalendarService();
-        service.newCalendar("test", TimeZone.getTimeZone("Europe/Brussels"), Year.of(2010)).add();
+        Calendar calendar = service.newCalendar("test", TimeZone.getTimeZone("Europe/Brussels"), Year.of(2010))
+                .description("Description remains to be completed :-)")
+                .mRID("Sample-TOU-rates")
+                .add();
+        assertThat(calendar.getName()).isEqualTo("test");
+        assertThat(calendar.getDescription()).isEqualTo("Description remains to be completed :-)");
+        assertThat(calendar.getTimeZone()).isEqualTo(TimeZone.getTimeZone("Europe/Brussels"));
+        assertThat(calendar.getStartYear()).isEqualTo(Year.of(2010));
 
     }
 
