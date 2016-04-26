647ecad3f1c420fa5f3126c4d0014eb95cce684e
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-04-26 11:15:36 +0200

    CXO-169 - small fix

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/calendar/TimeOfUsePreview.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/calendar/TimeOfUsePreview.js
@@ -85,7 +85,7 @@ Ext.define('Uni.view.calendar.TimeOfUsePreview', {
 
     calculateDate: function (month, day) {
         var date = new Date();
-        date.setMonth(month);
+        date.setMonth(month - 1);
         date.setDate(day);
 
         return Ext.util.Format.date(date, 'j F')
