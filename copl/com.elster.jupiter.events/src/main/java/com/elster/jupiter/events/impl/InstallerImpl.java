8489eaf97aa253af5be76fe90b6823d22cdb54db
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-04-26 13:56:41 +0200

    CXO-1174 - Convert installers to our current framework events

--- copl/com.elster.jupiter.events/src/main/java/com/elster/jupiter/events/impl/InstallerImpl.java
+++ copl/com.elster.jupiter.events/src/main/java/com/elster/jupiter/events/impl/InstallerImpl.java
@@ -2,30 +2,37 @@ package com.elster.jupiter.events.impl;
 
 import com.elster.jupiter.messaging.DestinationSpec;
 import com.elster.jupiter.messaging.MessageService;
+import com.elster.jupiter.messaging.QueueTableSpec;
 import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.Version;
+import com.elster.jupiter.upgrade.FullInstaller;
+
+import javax.inject.Inject;
 
 import static com.elster.jupiter.events.EventService.JUPITER_EVENTS;
 
-public class InstallerImpl {
+public class InstallerImpl implements FullInstaller {
 
     private static final int RETRY_DELAY = 60;
 
     private final DataModel dataModel;
     private final MessageService messageService;
 
+    @Inject
     public InstallerImpl(DataModel dataModel, MessageService messageService) {
         this.dataModel = dataModel;
         this.messageService = messageService;
     }
 
-    public void install() {
-        try {
-            dataModel.install(true, true);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        DestinationSpec destinationSpec = messageService.getQueueTableSpec("MSG_RAWTOPICTABLE").get().createDestinationSpec(JUPITER_EVENTS, RETRY_DELAY);
+    @Override
+    public void install(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(dataModel, Version.latest());
+        DestinationSpec destinationSpec = getRawTopicTableSpec().createDestinationSpec(JUPITER_EVENTS, RETRY_DELAY);
         destinationSpec.activate();
+    }
+
+    private QueueTableSpec getRawTopicTableSpec() {
+        return messageService.getQueueTableSpec("MSG_RAWTOPICTABLE").get();
     }
 }
