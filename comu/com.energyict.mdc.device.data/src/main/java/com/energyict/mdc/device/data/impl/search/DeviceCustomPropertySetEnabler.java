34309b3ec0c3dd50aaa21c26aec8ccb83f7f8e9a
Anton Suslennikov <antsusl@mera.ru>
2016-04-27 12:23:37 +0300

    Fix for CPS Searchable property constraints

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/DeviceCustomPropertySetEnabler.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/DeviceCustomPropertySetEnabler.java
@@ -2,6 +2,7 @@ package com.energyict.mdc.device.data.impl.search;
 
 import com.elster.jupiter.cps.CustomPropertySet;
 import com.elster.jupiter.cps.CustomPropertySetSearchEnabler;
+import com.elster.jupiter.search.SearchableProperty;
 import com.elster.jupiter.search.SearchablePropertyConstriction;
 import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.data.Device;
@@ -9,6 +10,7 @@ import com.energyict.mdc.device.data.Device;
 import org.osgi.service.component.annotations.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component(name = "com.energyict.mdc.device.data.impl.search.DeviceCustomPropertySetEnabler", service = CustomPropertySetSearchEnabler.class, immediate = true)
 public class DeviceCustomPropertySetEnabler implements CustomPropertySetSearchEnabler {
@@ -28,5 +30,14 @@ public class DeviceCustomPropertySetEnabler implements CustomPropertySetSearchEn
                 .flatMap(dt -> dt.getCustomPropertySets().stream())
                 .map(rcps -> rcps.getCustomPropertySet().getId())
                 .anyMatch(id -> id.equals(customPropertySet.getId()));
+    }
+
+    @Override
+    public List<SearchableProperty> getConstrainingProperties(CustomPropertySet customPropertySet, List<SearchablePropertyConstriction> constrictions) {
+        return constrictions
+                .stream()
+                .filter(constriction -> constriction.getConstrainingProperty().hasName(DeviceTypeSearchableProperty.PROPERTY_NAME))
+                .map(SearchablePropertyConstriction::getConstrainingProperty)
+                .collect(Collectors.toList());
     }
 }
