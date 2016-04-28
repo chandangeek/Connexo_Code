5640872426870bc0ebc1b067c7c017c08e904c24
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-04-28 11:43:46 +0200

    CXO-1174 - Convert installers to our current framework fsm

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/UserServiceImpl.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/UserServiceImpl.java
@@ -123,6 +123,7 @@ public class UserServiceImpl implements UserService, MessageSeedProvider, Transl
         setDataVaultService(dataVaultService);
         setThreadPrincipalService(threadPrincipalService);
         setUpgradeService(upgradeService);
+        privilegesProviders.add(this);
         activate(bundleContext);
     }
 
