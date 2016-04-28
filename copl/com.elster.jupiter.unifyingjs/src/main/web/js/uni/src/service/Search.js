c2ec5ebbb3c24da102f1da09e5ed8144da0b95a6
Sentyurin Sergey <ssentyur@mera.ru>
2016-04-28 15:10:43 +0300

    default columns for search + change mapping for UP type

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/service/Search.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/service/Search.js
@@ -107,7 +107,7 @@ Ext.define('Uni.service.Search', {
 
     defaultColumns: {
         'com.energyict.mdc.device.data.Device': ['id', 'mRID', 'serialNumber', 'deviceTypeName', 'deviceConfigurationName', 'state.name', 'location'],
-        'com.elster.jupiter.metering.UsagePoint': ['mRID', 'displayServiceCategory', 'displayConnectionState', 'openIssues', 'location']
+        'com.elster.jupiter.metering.UsagePoint': ['mRID', 'displayServiceCategory', 'displayMetrologyConfiguration']
     },
 
     getDomain: function() {
