4032316e460d9ff78f241708c26c18aa2d2d3ab5
Dragos Iancu <dragos.iancu@elster.com>
2016-05-04 15:42:26 +0300

    CXO-1137 - updated completion options

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/CompletionOptions.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/CompletionOptions.java
@@ -5,4 +5,6 @@ import com.elster.jupiter.messaging.DestinationSpec;
 public interface CompletionOptions {
 
     void whenFinishedSend(String message, DestinationSpec destinationSpec);
+    CompletionOptions setDestination(DestinationSpec destination);
+    CompletionOptions setMessage(String message);
 }
