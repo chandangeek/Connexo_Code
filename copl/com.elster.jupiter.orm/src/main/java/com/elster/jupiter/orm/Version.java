abc62f1bce2bf0e773508a695b958099208e0afd
Matthieu Deroo <matthieu.deroo@elster.com>
2016-05-04 17:14:30 +0200

    CXO-1159 - Implemented equals and hashcode methods on Version

--- copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/Version.java
+++ copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/Version.java
@@ -3,6 +3,7 @@ package com.elster.jupiter.orm;
 import org.flywaydb.core.api.MigrationVersion;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 
@@ -44,6 +45,23 @@ public final class Version implements Comparable<Version> {
 
     public static Version earliest() {
         return EARLIEST;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Version version = (Version) o;
+        return Objects.equals(migrationVersion, version.migrationVersion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(migrationVersion);
     }
 }
     
