61c4ea5d0780e4fdfb74d00d2405ce494313d892
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-04 13:08:23 +0200

    CXO-1174 - Convert installers to our current framework usagepoint importer

---
+++ coko/com.elster.jupiter.metering.imports/src/main/java/com/elster/jupiter/metering/imports/impl/Installer.java
@@ -0,0 +1,32 @@
+package com.elster.jupiter.metering.imports.impl;
+
+import com.elster.jupiter.messaging.DestinationSpec;
+import com.elster.jupiter.messaging.MessageService;
+import com.elster.jupiter.messaging.QueueTableSpec;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.upgrade.FullInstaller;
+import com.elster.jupiter.util.exception.ExceptionCatcher;
+
+final class Installer implements FullInstaller {
+
+    private final MessageService messageService;
+
+    Installer(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @Override
+    public void install(DataModelUpgrader dataModelUpgrader) {
+        if (!messageService.getDestinationSpec(UsagePointFileImporterMessageHandler.DESTINATION_NAME).isPresent()) {
+            ExceptionCatcher.executing(
+                    () -> {
+                        QueueTableSpec queueTableSpec = messageService.getQueueTableSpec("MSG_RAWQUEUETABLE").get();
+                        DestinationSpec destinationSpec = queueTableSpec.createDestinationSpec(UsagePointFileImporterMessageHandler.DESTINATION_NAME, 60);
+                        destinationSpec.save();
+                        destinationSpec.activate();
+                        destinationSpec.subscribe(UsagePointFileImporterMessageHandler.SUBSCRIBER_NAME);
+                    }
+            ).andHandleExceptionsWith(Throwable::printStackTrace).execute();
+        }
+    }
+}
