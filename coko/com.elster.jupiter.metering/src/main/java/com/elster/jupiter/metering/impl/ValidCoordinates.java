01c6ed4cf1a4f8b40d61a15c9367b8ad6e0f954a
David Nitescu <david.nitescu@elster.com>
2016-05-05 14:23:50 +0300

    add/update location for usage point

---
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/ValidCoordinates.java
@@ -0,0 +1,29 @@
+package com.elster.jupiter.metering.impl;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by david on 4/28/2016.
+ */
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {ValidCoordinatesValidator.class})
+public @interface ValidCoordinates {
+
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
