822b57ad15d563f4e09c2cc25647316ffb1fa2cb
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-05-06 08:42:19 +0200

    CXO-475: added EndPointConfiguration persistence

--- coko/com.elster.jupiter.metering.cim/src/main/java/com/elster/jupiter/metering/cim/soap/impl/GetMeterReadingsEndpointProvider.java
+++ coko/com.elster.jupiter.metering.cim/src/main/java/com/elster/jupiter/metering/cim/soap/impl/GetMeterReadingsEndpointProvider.java
@@ -3,13 +3,15 @@ package com.elster.jupiter.metering.cim.soap.impl;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.groups.MeteringGroupsService;
 import com.elster.jupiter.soap.whiteboard.EndPointProvider;
-import java.time.Clock;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
-@Component(name="com.elster.jupiter.metering.cim.soap", service = { EndPointProvider.class }, immediate = true, property = {"alias=/cim"})
+import java.time.Clock;
+
+@Component(name = "com.elster.jupiter.metering.cim.soap", service = {EndPointProvider.class}, immediate = true, property = {"name=cim"})
 public class GetMeterReadingsEndpointProvider implements EndPointProvider {
 
     private volatile MeteringService meteringService;
