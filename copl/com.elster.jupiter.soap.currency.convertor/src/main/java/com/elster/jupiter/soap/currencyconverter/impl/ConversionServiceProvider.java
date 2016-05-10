442449cb2c02c1e93d2d9ad8db03d38a86a1babf
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-05-10 10:44:01 +0200

    CXO-475: split EndPointProvider into Inbound/Outbound

--- copl/com.elster.jupiter.soap.currency.convertor/src/main/java/com/elster/jupiter/soap/currencyconverter/impl/ConversionServiceProvider.java
+++ copl/com.elster.jupiter.soap.currency.convertor/src/main/java/com/elster/jupiter/soap/currencyconverter/impl/ConversionServiceProvider.java
@@ -1,6 +1,5 @@
 package com.elster.jupiter.soap.currencyconverter.impl;
 
-import com.elster.jupiter.soap.whiteboard.InboundEndPointProvider;
 import com.elster.jupiter.soap.whiteboard.OutboundEndPointProvider;
 
 import org.osgi.service.component.annotations.Component;
@@ -8,7 +7,10 @@ import org.osgi.service.component.annotations.Component;
 /**
  * Created by bvn on 5/9/16.
  */
-@Component(name = "com.elster.jupiter.soap.currency.converter.provider", service = {InboundEndPointProvider.class}, immediate = true, property = {"name=xe"})
+@Component(name = "com.elster.jupiter.soap.currency.converter.provider",
+        service = {OutboundEndPointProvider.class},
+        immediate = true,
+        property = {"name=xe"})
 public class ConversionServiceProvider implements OutboundEndPointProvider {
     @Override
     public Object get() {
