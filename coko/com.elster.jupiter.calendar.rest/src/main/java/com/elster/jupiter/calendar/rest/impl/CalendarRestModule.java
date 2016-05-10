31d1c8fffda87bc6c4be06ca4b09fc1506c7ad4e
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-05-10 16:46:19 +0200

    CXO-1011 - Adding rest classes and methods, fixing failing tests

---
+++ coko/com.elster.jupiter.calendar.rest/src/main/java/com/elster/jupiter/calendar/rest/impl/CalendarRestModule.java
@@ -0,0 +1,16 @@
+package com.elster.jupiter.calendar.rest.impl;
+
+import com.elster.jupiter.calendar.rest.CalendarInfoFactory;
+import com.elster.jupiter.nls.NlsService;
+
+import com.google.inject.AbstractModule;
+
+public class CalendarRestModule extends AbstractModule{
+
+    @Override
+    protected void configure() {
+        requireBinding(NlsService.class);
+
+        bind(CalendarInfoFactory.class).to(CalendarInfoFactoryImpl.class);
+    }
+}
