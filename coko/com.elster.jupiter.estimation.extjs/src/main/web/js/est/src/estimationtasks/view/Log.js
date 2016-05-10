3a18ff671f1f69e3233a05f26e91ee19666b5e19
geert <geert.delmeiren@elster.com>
2016-05-10 15:34:35 +0200

    CXO-1642 - Change empty grid on logging pages to an information message

--- coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationtasks/view/Log.js
+++ coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationtasks/view/Log.js
@@ -2,7 +2,7 @@ Ext.define('Est.estimationtasks.view.Log', {
     extend: 'Uni.view.container.ContentContainer',
     alias: 'widget.estimationtasks-log-setup',
     requires: [
-        'Uni.view.notifications.NoItemsFoundPanel',
+        'Uni.util.FormEmptyMessage',
         'Est.estimationtasks.view.LogGrid',
         'Est.estimationtasks.view.LogPreview',
         'Est.estimationtasks.view.LogSideMenu'
@@ -44,15 +44,12 @@ Ext.define('Est.estimationtasks.view.Log', {
                         xtype: 'estimationtasks-log-grid'
                     },
                     emptyComponent: {
-                        xtype: 'no-items-found-panel',
-                        title: Uni.I18n.translate('estimationtasks.log.empty.title', 'EST', 'No logs found'),
-                        reasons: [
-                            Ext.String.format(
-                                Uni.I18n.translate('estimationtasks.log.empty.list.item1', 'EST', "Estimation task '{0}' started on {1} did not create any logs."),
-                                me.task.get('name'),
-                                me.runStartedOn
-                            )
-                        ]
+                        xtype: 'uni-form-empty-message',
+                        text: Ext.String.format(
+                            Uni.I18n.translate('estimationtasks.log.empty.list.item1', 'EST', "Estimation task '{0}' started on {1} did not create any logs."),
+                            me.task.get('name'),
+                            me.runStartedOn
+                        )
                     }
                 }
             ]
