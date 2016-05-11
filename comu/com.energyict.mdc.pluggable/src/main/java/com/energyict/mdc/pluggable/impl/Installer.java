195293ef085811c1db5bb5c586fab5a38d00cbeb
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-11 15:22:27 +0200

    CXO-1174 - Convert installers and features to our current framework - Pulse mdc pluggable

--- comu/com.energyict.mdc.pluggable/src/main/java/com/energyict/mdc/pluggable/impl/Installer.java
+++ comu/com.energyict.mdc.pluggable/src/main/java/com/energyict/mdc/pluggable/impl/Installer.java
@@ -2,6 +2,9 @@ package com.energyict.mdc.pluggable.impl;
 
 import com.elster.jupiter.events.EventService;
 import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.Version;
+import com.elster.jupiter.upgrade.FullInstaller;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,7 +15,7 @@ import java.util.logging.Logger;
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2013-12-23 (15:08)
  */
-public class Installer {
+public class Installer implements FullInstaller {
 
     private final Logger logger = Logger.getLogger(Installer.class.getName());
 
@@ -25,12 +28,11 @@ public class Installer {
         this.eventService = eventService;
     }
 
-    public void install(boolean executeDdl, boolean createMasterData) {
+    @Override
+    public void install(DataModelUpgrader dataModelUpgrader) {
         try {
-            this.dataModel.install(executeDdl, true);
-            if (createMasterData) {
-                this.createMasterData();
-            }
+            dataModelUpgrader.upgrade(dataModel, Version.latest());
+            this.createMasterData();
         }
         catch (Exception e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
--- comu/com.energyict.mdc.pluggable/src/main/java/com/energyict/mdc/pluggable/impl/PluggableServiceImpl.java
+++ comu/com.energyict.mdc.pluggable/src/main/java/com/energyict/mdc/pluggable/impl/PluggableServiceImpl.java
@@ -1,9 +1,5 @@
 package com.energyict.mdc.pluggable.impl;
 
-import com.energyict.mdc.pluggable.PluggableClass;
-import com.energyict.mdc.pluggable.PluggableClassType;
-import com.energyict.mdc.pluggable.PluggableService;
-
 import com.elster.jupiter.domain.util.DefaultFinder;
 import com.elster.jupiter.domain.util.Finder;
 import com.elster.jupiter.events.EventService;
@@ -13,9 +9,14 @@ import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.OrmService;
-import com.elster.jupiter.orm.callback.InstallService;
+import com.elster.jupiter.upgrade.InstallIdentifier;
+import com.elster.jupiter.upgrade.UpgradeService;
 import com.elster.jupiter.util.conditions.Where;
 import com.elster.jupiter.util.exception.MessageSeed;
+import com.energyict.mdc.pluggable.PluggableClass;
+import com.energyict.mdc.pluggable.PluggableClassType;
+import com.energyict.mdc.pluggable.PluggableService;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import org.osgi.service.component.annotations.Activate;
@@ -25,6 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 import javax.inject.Inject;
 import java.time.Clock;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,27 +36,28 @@ import java.util.Optional;
  * @author Rudi Vankeirsbilck (rudi)
  * @since 2013-12-20 (17:49)
  */
-@Component(name = "com.energyict.mdc.pluggable", service = {PluggableService.class, InstallService.class, MessageSeedProvider.class}, property = "name=" + PluggableService.COMPONENTNAME)
-public class PluggableServiceImpl implements PluggableService, InstallService, MessageSeedProvider {
+@Component(name = "com.energyict.mdc.pluggable", service = {PluggableService.class, MessageSeedProvider.class}, property = "name=" + PluggableService.COMPONENTNAME)
+public class PluggableServiceImpl implements PluggableService, MessageSeedProvider {
 
     private volatile DataModel dataModel;
     private volatile EventService eventService;
     private volatile Clock clock;
     private volatile Thesaurus thesaurus;
+    private volatile UpgradeService upgradeService;
 
     public PluggableServiceImpl() {
         super();
     }
 
     @Inject
-    public PluggableServiceImpl(OrmService ormService, EventService eventService, NlsService nlsService, Clock clock) {
+    public PluggableServiceImpl(OrmService ormService, EventService eventService, NlsService nlsService, Clock clock, UpgradeService upgradeService) {
         this();
-        this.setOrmService(ormService);
-        this.setEventService(eventService);
-        this.setNlsService(nlsService);
-        this.setClock(clock);
-        this.activate();
-        this.install();
+        setOrmService(ormService);
+        setEventService(eventService);
+        setNlsService(nlsService);
+        setClock(clock);
+        setUpgradeService(upgradeService);
+        activate();
     }
 
     @Override
@@ -135,6 +138,11 @@ public class PluggableServiceImpl implements PluggableService, InstallService, M
         this.thesaurus = nlsService.getThesaurus(COMPONENTNAME, Layer.DOMAIN);
     }
 
+    @Reference
+    public void setUpgradeService(UpgradeService upgradeService) {
+        this.upgradeService = upgradeService;
+    }
+
     private Module getModule() {
         return new AbstractModule() {
             @Override
@@ -150,16 +158,8 @@ public class PluggableServiceImpl implements PluggableService, InstallService, M
     @Activate
     public void activate() {
         this.dataModel.register(this.getModule());
-    }
 
-    @Override
-    public void install() {
-        new Installer(this.dataModel, this.eventService).install(true, true);
-    }
-
-    @Override
-    public List<String> getPrerequisiteModules() {
-        return Arrays.asList("ORM", "EVT", "NLS");
+        upgradeService.register(InstallIdentifier.identifier(PluggableService.COMPONENTNAME), dataModel, Installer.class, Collections.emptyMap());
     }
 
 }
