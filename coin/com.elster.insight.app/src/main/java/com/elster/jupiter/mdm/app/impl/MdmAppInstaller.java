fbd806e3ba70df696092f5dce7152ca287171342
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-13 14:38:05 +0200

    CXO-1174 - Convert installers and features to our current framework - Pulse merge master into CXO-1174

--- coin/com.elster.insight.app/src/main/java/com/elster/jupiter/mdm/app/impl/MdmAppInstaller.java
+++ coin/com.elster.insight.app/src/main/java/com/elster/jupiter/mdm/app/impl/MdmAppInstaller.java
@@ -91,7 +91,7 @@ public class MdmAppInstaller {
                     //service category
                     com.elster.jupiter.metering.security.Privileges.Constants.VIEW_SERVICECATEGORY,
 
-                    com.elster.jupiter.servicecall.security.Privileges.Constants.CHANGE_SERVICE_CALL_STATE
+                    com.elster.jupiter.servicecall.security.Privileges.Constants.CHANGE_SERVICE_CALL_STATE,
 
                     //Relative periods
                     com.elster.jupiter.time.security.Privileges.Constants.VIEW_RELATIVE_PERIOD
