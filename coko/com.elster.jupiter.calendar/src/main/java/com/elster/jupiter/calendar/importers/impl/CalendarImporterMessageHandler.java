c128c393fd2f084b09918c2d2b38764169c58408
igh <isabelle.gheysens@elster.com>
2016-05-13 14:55:56 +0200

    CXO-1007	- Import TOU calendar

--- coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/importers/impl/CalendarImporterMessageHandler.java
+++ coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/importers/impl/CalendarImporterMessageHandler.java
@@ -44,7 +44,6 @@ public class CalendarImporterMessageHandler implements MessageHandlerFactory, Tr
 
     @Activate
     public void activate() {
-        System.out.println("test");
     }
 
     @Inject
