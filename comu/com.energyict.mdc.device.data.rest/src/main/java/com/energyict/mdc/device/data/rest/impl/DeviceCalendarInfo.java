f7394a7ad9561b90ac9d09ce2252c8601516ad0b
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-05-17 16:53:23 +0200

    CXO-1011 - Info of active calendar is shown correctly. Added new tablespecs to link passive calendar to comtask execution

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceCalendarInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceCalendarInfo.java
@@ -6,6 +6,7 @@ import com.elster.jupiter.calendar.rest.CalendarInfoFactory;
 import com.energyict.mdc.device.data.ActiveEffectiveCalendar;
 import com.energyict.mdc.device.data.PassiveEffectiveCalendar;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -13,19 +14,25 @@ import java.util.stream.Collectors;
 
 public class DeviceCalendarInfo {
 
-    public List<String> passiveCalendars;
+    public List<PassiveCalendarInfo> passiveCalendars;
     public CalendarInfo activeCalendar;
     private CalendarInfoFactory calendarInfoFactory;
+    public long lastVerified;
 
     public DeviceCalendarInfo(Optional<ActiveEffectiveCalendar> activeCalendar, List<PassiveEffectiveCalendar> passiveCalendars, CalendarInfoFactory calendarInfoFactory) {
         this.calendarInfoFactory = calendarInfoFactory;
         if(activeCalendar.isPresent()) {
-            this.activeCalendar = this.calendarInfoFactory.detailedFromCalendar(activeCalendar.get().getAllowedCalendar().getCalendar().get());
+            if(activeCalendar.get().getAllowedCalendar().getCalendar().isPresent()) {
+                this.activeCalendar = this.calendarInfoFactory.detailedFromCalendar(activeCalendar.get().getAllowedCalendar().getCalendar().get());
+            } else {
+                this.activeCalendar =  this.calendarInfoFactory.nameOnly(activeCalendar.get().getAllowedCalendar().getName());
+            }
+            this.lastVerified = activeCalendar.get().getLastVerifiedDate().toEpochMilli();
         }
         if(passiveCalendars.size() > 0) {
             this.passiveCalendars = new ArrayList<>();
             this.passiveCalendars = passiveCalendars.stream()
-                    .map(PEC -> PEC.getAllowedCalendar().getName())
+                    .map(PEC -> new PassiveCalendarInfo(PEC.getAllowedCalendar().getName(), PEC.getAllowedCalendar().isGhost()))
                     .collect(Collectors.toList());
         }
     }
---
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/PassiveCalendarInfo.java
@@ -0,0 +1,11 @@
+package com.energyict.mdc.device.data.rest.impl;
+
+public class PassiveCalendarInfo {
+    public String name;
+    public boolean ghost;
+
+    public PassiveCalendarInfo(String name, boolean ghost) {
+        this.name = name;
+        this.ghost = ghost;
+    }
+}
