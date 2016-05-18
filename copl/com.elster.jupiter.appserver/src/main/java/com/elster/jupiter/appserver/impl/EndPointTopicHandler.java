2cfba5c9fa37e4eabf6b15c530fcf4f10c2a541f
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-05-18 14:29:55 +0200

    CXO-475: activate/deactivate now done using events

--- copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/impl/EndPointTopicHandler.java
+++ copl/com.elster.jupiter.appserver/src/main/java/com/elster/jupiter/appserver/impl/EndPointTopicHandler.java
@@ -1,38 +1,46 @@
 package com.elster.jupiter.appserver.impl;
 
+import com.elster.jupiter.appserver.AppServerCommand;
 import com.elster.jupiter.appserver.AppService;
+import com.elster.jupiter.appserver.Command;
 import com.elster.jupiter.events.LocalEvent;
 import com.elster.jupiter.events.TopicHandler;
 import com.elster.jupiter.messaging.MessageService;
-import com.elster.jupiter.soap.whiteboard.EndPointConfiguration;
 import com.elster.jupiter.soap.whiteboard.EventType;
 import com.elster.jupiter.util.json.JsonService;
 
 import org.osgi.service.component.annotations.Component;
-
-import javax.inject.Inject;
+import org.osgi.service.component.annotations.Reference;
 
 /**
+ * Changed to and EndPointConfiguration are propagated to this handler through PubSub, and from this handler onwards
+ * re-published using the MessageService.
  * Created by bvn on 5/6/16.
  */
 @Component(name = "com.elster.jupiter.webservices.endpoint.eventhandler", service = TopicHandler.class, immediate = true)
 public class EndPointTopicHandler implements TopicHandler {
 
-    private final MessageService messageService;
-    private final JsonService jsonService;
+    private MessageService messageService;
+    private JsonService jsonService;
 
-    @Inject
-    public EndPointTopicHandler(MessageService messageService, JsonService jsonService) {
+    public EndPointTopicHandler() {
+    }
+
+    @Reference
+    public void setMessageService(MessageService messageService) {
         this.messageService = messageService;
+    }
+
+    @Reference
+    public void setJsonService(JsonService jsonService) {
         this.jsonService = jsonService;
     }
 
     @Override
     public void handle(LocalEvent localEvent) {
-        EndPointConfiguration source = (EndPointConfiguration) localEvent.getSource();
-
         messageService.getDestinationSpec(AppService.ALL_SERVERS)
-                .ifPresent(destination -> destination.message(jsonService.serialize(source)).send());
+                .ifPresent(destination -> destination.message(jsonService.serialize(new AppServerCommand(Command.CONFIG_CHANGED)))
+                        .send());
     }
 
     @Override
