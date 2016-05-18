9c24329d6674903b352d6477ab60d995014c1b4d
Kirill Koshelev <kirkosh@mera.ru>
2016-05-18 14:58:39 +0300

    [CXO-1653]-translation fix

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/metrologyconfiguration/view/CustomAttributeSets.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/metrologyconfiguration/view/CustomAttributeSets.js
@@ -28,7 +28,7 @@ Ext.define('Imt.metrologyconfiguration.view.CustomAttributeSets', {
             items: [
                 {
                     xtype: 'uni-form-empty-message',
-                    text: Uni.I18n.translate('Imt.metrologyconfiguration.error.active', 'IMT', 'You cannot edit custom attribute set because the metrology configuration is active'),
+                    text: Uni.I18n.translate('Imt.metrologyconfiguration.error.cannotEditCAS', 'IMT', "You can't manage custom attribute sets because the metrology configuration is active"),
                     hidden: !isActive
                 },
                 {
