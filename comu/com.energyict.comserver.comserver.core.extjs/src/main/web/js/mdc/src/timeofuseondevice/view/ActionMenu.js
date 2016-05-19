d314c8fe084c5ce1b23ba7e78f6c593e61ec24f5
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-05-19 12:24:16 +0200

    CXO-1011

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/timeofuseondevice/view/ActionMenu.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/timeofuseondevice/view/ActionMenu.js
@@ -6,34 +6,34 @@ Ext.define('Mdc.timeofuseondevice.view.ActionMenu', {
     shadow: false,
     record: null,
     items: [
-        {
-            itemId: 'activate-calendar-tou',
-            text: Uni.I18n.translate('timeofuse.activatePassiveCalendar', 'MDC', 'Activate passive calendar'),
-            privileges: Mdc.privileges.DeviceCommands.executeCommands,
-            action: 'activatecalendar',
-            dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.activatePassive
-        },
-        {
-            itemId: 'clear-tariff-tou',
-            text: Uni.I18n.translate('timeofuse.clearPassiveCalendar', 'MDC', 'Clear passive calendar'),
-            privileges: Mdc.privileges.DeviceCommands.executeCommands,
-            action: 'cleartariff',
-            dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.clearAndDisable
-        },
-        {
-            itemId: 'send-calendar-tou',
-            text: Uni.I18n.translate('timeofuse.sendCalendar', 'MDC', 'Send calendar'),
-            privileges: Mdc.privileges.DeviceCommands.executeCommands,
-            action: 'sendcalendar',
-            dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.sendCalendar
-        },
-        {
-            itemId: 'verify-calendars-tou',
-            text: Uni.I18n.translate('timeofuse.verifyCalendars', 'MDC', 'Verify calendars'),
-            privileges: Mdc.privileges.DeviceCommands.executeCommands,
-            action: 'verifycalendars',
-            dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.verifyCalendar
-        },
+        //{
+        //    itemId: 'activate-calendar-tou',
+        //    text: Uni.I18n.translate('timeofuse.activatePassiveCalendar', 'MDC', 'Activate passive calendar'),
+        //    privileges: Mdc.privileges.DeviceCommands.executeCommands,
+        //    action: 'activatecalendar',
+        //    dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.activatePassive
+        //},
+        //{
+        //    itemId: 'clear-tariff-tou',
+        //    text: Uni.I18n.translate('timeofuse.clearPassiveCalendar', 'MDC', 'Clear passive calendar'),
+        //    privileges: Mdc.privileges.DeviceCommands.executeCommands,
+        //    action: 'cleartariff',
+        //    dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.clearAndDisable
+        //},
+        //{
+        //    itemId: 'send-calendar-tou',
+        //    text: Uni.I18n.translate('timeofuse.sendCalendar', 'MDC', 'Send calendar'),
+        //    privileges: Mdc.privileges.DeviceCommands.executeCommands,
+        //    action: 'sendcalendar',
+        //    dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.sendCalendar
+        //},
+        //{
+        //    itemId: 'verify-calendars-tou',
+        //    text: Uni.I18n.translate('timeofuse.verifyCalendars', 'MDC', 'Verify calendars'),
+        //    privileges: Mdc.privileges.DeviceCommands.executeCommands,
+        //    action: 'verifycalendars',
+        //    dynamicPrivilege: Mdc.dynamicprivileges.DeviceState.verifyCalendar
+        //},
         {
             itemId: 'view-preview-tou',
             text: Uni.I18n.translate('timeofuse.viewPreview', 'MDC', 'View preview'),
