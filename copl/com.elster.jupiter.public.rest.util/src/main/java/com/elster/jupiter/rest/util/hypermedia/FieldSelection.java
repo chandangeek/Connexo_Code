6d9ab2f8db778a86db5fee334758a95def89a64b
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-05-19 13:08:17 +0200

    CXO-917: Moving Usage point resource to COPL

---
+++ copl/com.elster.jupiter.public.rest.util/src/main/java/com/elster/jupiter/rest/util/hypermedia/FieldSelection.java
@@ -0,0 +1,23 @@
+package com.elster.jupiter.rest.util.hypermedia;
+
+import javax.ws.rs.QueryParam;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Converts CSV string (as received from client) to list of individual field names
+ */
+public class FieldSelection {
+    /**
+     * @summary Comma separated list of fields that will be added to the response, if absent, all fields will be added.
+     * Get a list of all available field with PROPFIND method on this resource.
+     */
+    private
+    @QueryParam("fields")
+    String fields;
+
+    public List<String> getFields() {
+        return fields != null ? Collections.unmodifiableList(Arrays.asList(fields.split(","))) : Collections.emptyList();
+    }
+}
---
+++ copl/com.elster.jupiter.public.rest.util/src/main/java/com/elster/jupiter/rest/util/hypermedia/PropertyCopier.java
@@ -0,0 +1,13 @@
+package com.elster.jupiter.rest.util.hypermedia;
+
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Functional interface used to copy a single property from a domain object to an info object.
+ * UriInfo is passed along should there be a need to create links, without it, Supplier could have been used instead of PropertyCopier
+ * Created by bvn on 5/13/15.
+ */
+@FunctionalInterface
+public interface PropertyCopier<D, S> {
+    public void copy(D d, S s, UriInfo uriInfo);
+}
