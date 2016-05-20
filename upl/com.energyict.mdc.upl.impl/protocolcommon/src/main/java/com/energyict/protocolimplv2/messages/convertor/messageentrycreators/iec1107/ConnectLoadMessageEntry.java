68017dca316484701f4697ca1979e23292002e66
Claudiu Isac <claudiu.isac@elster.com>
2016-05-20 13:44:16 +0300

    COMMUNICATION-1605 - AS220 IEC1107 protocol messages for connect / disconnect are inversed

--- upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/messages/convertor/messageentrycreators/iec1107/ConnectLoadMessageEntry.java
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimplv2/messages/convertor/messageentrycreators/iec1107/ConnectLoadMessageEntry.java
@@ -6,7 +6,7 @@ import com.energyict.protocol.messaging.*;
 import com.energyict.protocolimplv2.messages.convertor.MessageEntryCreator;
 
 /**
- * Creates a MessageEntry based on the "CONTACTOR_OPEN" xml tag with no additional parameters
+ * Creates a MessageEntry based on the "CONTACTOR_CLOSE" xml tag with no additional parameters
  * This is used by the IEC1107 protocols (as1440, as220)
  * <p/>
  * Copyrights EnergyICT
@@ -20,7 +20,7 @@ public class ConnectLoadMessageEntry implements MessageEntryCreator {
 
     @Override
     public MessageEntry createMessageEntry(Messaging messagingProtocol, OfflineDeviceMessage offlineDeviceMessage) {
-        MessageTag messageTag = new MessageTag("CONTACTOR_OPEN");
+        MessageTag messageTag = new MessageTag("CONTACTOR_CLOSE");
         messageTag.add(new MessageValue(" "));
         return new MessageEntry(messagingProtocol.writeTag(messageTag), offlineDeviceMessage.getTrackingId());
     }
