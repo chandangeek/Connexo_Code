ab5c7e302abf4a21a92f877bea16d934c51222da
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-20 10:42:42 +0200

    CXO-1174 - Convert installers and features to our current framework - Pulse Make clean install work

--- copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/impl/BasicAuthentication.java
+++ copl/com.elster.jupiter.http.whiteboard/src/main/java/com/elster/jupiter/http/whiteboard/impl/BasicAuthentication.java
@@ -157,7 +157,7 @@ public class BasicAuthentication implements HttpAuthenticationService {
         tokenRefreshMaxCount = getIntParameter(TOKEN_REFRESH_MAX_COUNT, context, 100);
         tokenExpTime = getIntParameter(TOKEN_EXPIRATION_TIME, context, 300);
         installDir = context.getProperty("install.dir");
-        upgradeService.register(InstallIdentifier.identifier("HTTP"), dataModel, Installer.class, Collections.emptyMap());
+        upgradeService.register(InstallIdentifier.identifier("HTP"), dataModel, Installer.class, Collections.emptyMap());
         initSecurityTokenImpl();
     }
 
