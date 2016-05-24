b7cec909ebc52ae4cdfd6cafcd753b6cc08916ec
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-05-24 15:55:30 +0200

    CXO-1551 - dynamic capabilities of protocols

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/DeviceProtocolPluggableClass.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/DeviceProtocolPluggableClass.java
@@ -2,6 +2,10 @@ package com.energyict.mdc.protocol.api;
 
 import com.energyict.mdc.common.TypedProperties;
 import com.energyict.mdc.pluggable.PluggableClass;
+import com.energyict.mdc.protocol.api.messaging.DeviceMessageId;
+
+import java.util.EnumSet;
+import java.util.Set;
 
 /**
  * Models a {@link DeviceProtocol} that was registered in the HeadEnd as a {@link PluggableClass}.
@@ -24,4 +28,17 @@ public interface DeviceProtocolPluggableClass extends PluggableClass {
 
     public TypedProperties getProperties ();
 
+    default boolean supportsFileManagement() {
+        Set<DeviceMessageId> fileMessages = EnumSet.of(
+                DeviceMessageId.GENERAL_WRITE_FULL_CONFIGURATION,
+                DeviceMessageId.CONFIGURATION_CHANGE_UPLOAD_METER_SCHEME,
+                DeviceMessageId.CONFIGURATION_CHANGE_UPLOAD_SWITCH_POINT_CLOCK_SETTINGS,
+                DeviceMessageId.CONFIGURATION_CHANGE_UPLOAD_SWITCH_POINT_CLOCK_UPDATE_SETTINGS,
+                DeviceMessageId.ADVANCED_TEST_USERFILE_CONFIG
+        );
+        return this.getDeviceProtocol()
+                .getSupportedMessages()
+                .stream()
+                .anyMatch(fileMessages::contains);
+    }
 }
