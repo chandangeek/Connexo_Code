d60cdeb0c5a478846c70470fb4c79ec416942c77
geert <geert.delmeiren@elster.com>
2016-05-24 13:12:08 +0200

    CXO-1538 - Empty grid message relating to filters is not consistent

--- copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/config.rb
+++ copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/config.rb
@@ -1,2 +1,2 @@
 require '..\..\..\..\..\..\..\com.elster.jupiter.extjs\src\main\web\js\ext\packages\ext-theme-base\sass\utils.rb'
-Compass.add_project_configuration('..\sass\config.rb')
+require '..\sass\config.rb'
