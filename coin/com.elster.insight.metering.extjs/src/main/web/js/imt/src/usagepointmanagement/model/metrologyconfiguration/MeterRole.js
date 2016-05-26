1711144c9de570c8c92b2376909d62d7a849abe4
Kirill Koshelev <kirkosh@mera.ru>
2016-05-26 17:51:16 +0300

    model fix

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointmanagement/model/metrologyconfiguration/MeterRole.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointmanagement/model/metrologyconfiguration/MeterRole.js
@@ -1,11 +1,7 @@
 Ext.define('Imt.usagepointmanagement.model.metrologyconfiguration.MeterRole', {
     extend: 'Ext.data.Model',
     fields: [
-        'id', 'name', 'required',
-        {
-            name: 'meter',
-            type: 'auto'
-        },
+        'id', 'name', 'required', 'mRID',
         {
             name: 'activationTime',
             type: 'date',
