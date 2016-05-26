7cf3a0aecf7fa1195923f43cfa6a8d04fe8155a1
Sentyurin Sergey <ssentyur@mera.ru>
2016-05-26 13:19:33 +0300

    CXO-1724 [BE] Edit metrology configuration

--- coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/MetrologyConfigurationResource.java
+++ coko/com.elster.jupiter.metering.rest/src/main/java/com/elster/jupiter/metering/rest/impl/MetrologyConfigurationResource.java
@@ -31,11 +31,14 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -130,6 +133,23 @@ public class MetrologyConfigurationResource {
         });
 
         return Response.status(Response.Status.CREATED).entity(metrologyConfigurationInfoFactory.asInfo(metrologyConfiguration)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed({Privileges.Constants.ADMINISTER_METROLOGY_CONFIGURATION})
+    @Transactional
+    public Response updateMetrologyConfiguration(@PathParam("id") long id, MetrologyConfigurationInfo info, @Context SecurityContext securityContext) {
+        UsagePointMetrologyConfiguration metrologyConfiguration = resourceHelper.findAndLockMetrologyConfiguration(info);
+
+        metrologyConfiguration.startUpdate()
+                .setName(info.name)
+                .setDescription(info.description)
+                .complete();
+
+        return Response.ok().entity(metrologyConfigurationInfoFactory.asInfo(metrologyConfiguration)).build();
     }
 
     @DELETE
