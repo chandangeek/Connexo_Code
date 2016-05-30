c25d6ff2f177b6d8836d42bfa5f3d4adbfdf0831
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-30 11:34:43 +0200

    CXO-1159 Streamline use of logging

--- coko/com.elster.jupiter.ids/src/main/java/com/elster/jupiter/ids/impl/InstallerImpl.java
+++ coko/com.elster.jupiter.ids/src/main/java/com/elster/jupiter/ids/impl/InstallerImpl.java
@@ -13,6 +13,7 @@ import javax.inject.Inject;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class InstallerImpl implements FullInstaller {
@@ -31,27 +32,29 @@ public class InstallerImpl implements FullInstaller {
     private static final int DEFAULT_SLOT_COUNT = 8;
 
     @Override
-    public void install(DataModelUpgrader dataModelUpgrader) {
+    public void install(DataModelUpgrader dataModelUpgrader, Logger logger) {
         dataModelUpgrader.upgrade(dataModel, Version.latest());
-        createMasterData();
+        createMasterData(logger);
     }
 
-    private void createMasterData() {
-        createVaults();
-        createRecordSpecs();
+    private void createMasterData(Logger logger) {
+        createVaults(logger);
+        createRecordSpecs(logger);
     }
 
-    private void createVaults() {
+    private void createVaults(Logger logger) {
         Vault newVault = idsService.createVault(IdsService.COMPONENTNAME, 1, "Regular TimeSeries Default ", DEFAULT_SLOT_COUNT, 0, true);
         Instant start = Instant.now(clock).truncatedTo(ChronoUnit.DAYS);
         newVault.activate(start);
         newVault.extendTo(start.plus(360, ChronoUnit.DAYS), Logger.getLogger(getClass().getPackage().getName()));
+        logger.log(Level.INFO, "Created Vault \"Regular TimeSeries Default \", with slot count " + DEFAULT_SLOT_COUNT + ".");
     }
 
-    private void createRecordSpecs() {
+    private void createRecordSpecs(Logger logger) {
         RecordSpecBuilder builder = idsService.createRecordSpec(IdsService.COMPONENTNAME, 1, "Simple");
         builder.addFieldSpec("value", FieldType.NUMBER);
         builder.create();
+        logger.log(Level.INFO, "Created \"Simple\" Record Spec.");
     }
 
 }
