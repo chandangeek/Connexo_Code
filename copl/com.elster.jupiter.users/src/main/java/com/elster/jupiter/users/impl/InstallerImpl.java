5da91e3b4e896990df81cf8c22a8b011bafb1406
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-30 11:34:39 +0200

    CXO-1159 Streamline use of logging

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/InstallerImpl.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/InstallerImpl.java
@@ -8,6 +8,7 @@ import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.FullInstaller;
 import com.elster.jupiter.users.FormatKey;
+import com.elster.jupiter.users.Group;
 import com.elster.jupiter.users.UserPreferencesService;
 import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.users.security.Privileges;
@@ -18,14 +19,12 @@ import javax.inject.Inject;
 import java.lang.reflect.Field;
 import java.util.Locale;
 import java.util.UUID;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static com.elster.jupiter.util.Checks.is;
 
 public class InstallerImpl implements FullInstaller {
     private static final int DEFAULT_RETRY_DELAY_IN_SECONDS = 60;
-    private final Logger logger = Logger.getLogger(InstallerImpl.class.getName());
 
     private final DataModel dataModel;
     private final UserServiceImpl userService;
@@ -41,49 +40,65 @@ public class InstallerImpl implements FullInstaller {
     }
 
     @Override
-    public void install(DataModelUpgrader dataModelUpgrader) {
-        try {
-            createUSRQueue(messageService);
-            dataModelUpgrader.upgrade(dataModel, Version.latest());
-        } catch (Exception e) {
-            this.logger.log(Level.SEVERE, e.getMessage(), e);
-        }
-        userService.installPrivileges();
-        addDefaults(bundleContext != null ? bundleContext.getProperty("admin.password") : null);
+    public void install(DataModelUpgrader dataModelUpgrader, Logger logger) {
+        doTry(
+                "Create User Queue",
+                () -> createUserQueue(messageService),
+                logger);
+        dataModelUpgrader.upgrade(dataModel, Version.latest());
+        doTry(
+                "Install User privileges.",
+                () -> userService.installPrivileges(),
+                logger
+        );
+        addDefaults(bundleContext != null ? bundleContext.getProperty("admin.password") : null, logger);
 
     }
 
-    private void createUSRQueue(MessageService messageService) {
-        try {
-            QueueTableSpec defaultQueueTableSpec = messageService.getQueueTableSpec("MSG_RAWQUEUETABLE").get();
-            DestinationSpec destinationSpec = defaultQueueTableSpec.createDestinationSpec(UserService.USR_QUEUE_DEST, DEFAULT_RETRY_DELAY_IN_SECONDS);
-            destinationSpec.activate();
-            destinationSpec.subscribe(UserService.USR_QUEUE_SUBSC);
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, e.getMessage(), e);
+    private void createUserQueue(MessageService messageService) {
+        QueueTableSpec defaultQueueTableSpec = messageService.getQueueTableSpec("MSG_RAWQUEUETABLE").get();
+        DestinationSpec destinationSpec = defaultQueueTableSpec.createDestinationSpec(UserService.USR_QUEUE_DEST, DEFAULT_RETRY_DELAY_IN_SECONDS);
+        destinationSpec.activate();
+        destinationSpec.subscribe(UserService.USR_QUEUE_SUBSC);
+    }
+
+    public void addDefaults(String adminPassword, Logger logger) {
+        createMasterData(adminPassword, logger);
+        doTry(
+                "Create user preferences.",
+                () -> createUserPreferences(userService.getUserPreferencesService()),
+                logger
+        );
+    }
+
+    private void createMasterData(String adminPassword, Logger logger) {
+        InternalDirectoryImpl directory = (InternalDirectoryImpl) userService.findUserDirectory(userService.getRealm())
+                .orElseGet(this::createDirectory);
+        GroupImpl administrators = (GroupImpl) userService.findGroup(UserService.DEFAULT_ADMIN_ROLE)
+                .orElseGet(() -> createAdministratorGroup(logger));
+
+        doTry(
+                "Grant system administrator privileges to " + UserService.DEFAULT_ADMIN_ROLE,
+                () -> grantSystemAdministratorPrivileges(administrators),
+                logger
+        );
+
+        if (!userService.findUser("admin").isPresent()) {
+            doTry(
+                    "Create administrator user.",
+                    () -> createAdministratorUser(directory, new GroupImpl[]{administrators}, adminPassword),
+                    logger
+            );
         }
     }
 
-    public void addDefaults(String adminPassword) {
-        createMasterData(adminPassword);
-        createUserPreferences(userService.getUserPreferencesService());
-    }
+    private Group createAdministratorGroup(Logger logger) {
 
-    private void createMasterData(String adminPassword) {
-        try {
-            InternalDirectoryImpl directory = (InternalDirectoryImpl) userService.findUserDirectory(userService.getRealm())
-                    .orElseGet(this::createDirectory);
-            GroupImpl administrators = (GroupImpl) userService.findGroup(UserService.DEFAULT_ADMIN_ROLE)
-                    .orElseGet(() -> userService.createGroup(UserService.DEFAULT_ADMIN_ROLE, UserService.DEFAULT_ADMIN_ROLE_DESCRIPTION));
-
-            grantSystemAdministratorPrivileges(administrators);
-
-            if (!userService.findUser("admin").isPresent()) {
-                createAdministratorUser(directory, new GroupImpl[]{administrators}, adminPassword);
-            }
-        } catch (Exception e) {
-            this.logger.log(Level.SEVERE, e.getMessage(), e);
-        }
+        return doTry(
+                "Create " + UserService.DEFAULT_ADMIN_ROLE + " user group.",
+                () -> userService.createGroup(UserService.DEFAULT_ADMIN_ROLE, UserService.DEFAULT_ADMIN_ROLE_DESCRIPTION),
+                logger
+        );
     }
 
     private InternalDirectoryImpl createDirectory() {
@@ -118,38 +133,27 @@ public class InstallerImpl implements FullInstaller {
 
     private void createUserPreferences(UserPreferencesService userPrefsService) {
         //en (UK)
-        tryCatchBlock(() -> {
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.SHORT_DATE, "dd MMM ''yy", "d M \'y", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.LONG_DATE, "EEE dd MMM ''yy", "D d M \'y", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.SHORT_TIME, "HH:mm", "H:i", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.LONG_TIME, "HH:mm:ss", "H:i:s", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DATETIME_SEPARATOR, "SPACE", "-", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DATETIME_ORDER, "TD", "DT", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DECIMAL_PRECISION, "2", "2", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DECIMAL_SEPARATOR, ".", ".", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.THOUSANDS_SEPARATOR, ",", ",", true);
-            userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.CURRENCY, "\u00A3 {0}", "\u00A3 {0}", true);
-        });
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.SHORT_DATE, "dd MMM ''yy", "d M \'y", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.LONG_DATE, "EEE dd MMM ''yy", "D d M \'y", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.SHORT_TIME, "HH:mm", "H:i", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.LONG_TIME, "HH:mm:ss", "H:i:s", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DATETIME_SEPARATOR, "SPACE", "-", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DATETIME_ORDER, "TD", "DT", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DECIMAL_PRECISION, "2", "2", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.DECIMAL_SEPARATOR, ".", ".", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.THOUSANDS_SEPARATOR, ",", ",", true);
+        userPrefsService.createUserPreference(Locale.ENGLISH, FormatKey.CURRENCY, "\u00A3 {0}", "\u00A3 {0}", true);
         //en (US)
-        tryCatchBlock(() -> {
-            userPrefsService.createUserPreference(Locale.US, FormatKey.SHORT_DATE, "MMM-dd-''yy", "M-d-\'y", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.LONG_DATE, "EEE, MMM-dd-''yy", "D, M-d-\'y", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.SHORT_TIME, "hh:mm a", "h:i a", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.LONG_TIME, "hh:mm:ss a", "h:i:s a", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.DATETIME_SEPARATOR, "SPACE", "-", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.DATETIME_ORDER, "TD", "DT", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.DECIMAL_PRECISION, "2", "2", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.DECIMAL_SEPARATOR, ".", ".", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.THOUSANDS_SEPARATOR, ",", ",", true);
-            userPrefsService.createUserPreference(Locale.US, FormatKey.CURRENCY, "{0} $", "{0} $", true);
-        });
+        userPrefsService.createUserPreference(Locale.US, FormatKey.SHORT_DATE, "MMM-dd-''yy", "M-d-\'y", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.LONG_DATE, "EEE, MMM-dd-''yy", "D, M-d-\'y", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.SHORT_TIME, "hh:mm a", "h:i a", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.LONG_TIME, "hh:mm:ss a", "h:i:s a", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.DATETIME_SEPARATOR, "SPACE", "-", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.DATETIME_ORDER, "TD", "DT", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.DECIMAL_PRECISION, "2", "2", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.DECIMAL_SEPARATOR, ".", ".", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.THOUSANDS_SEPARATOR, ",", ",", true);
+        userPrefsService.createUserPreference(Locale.US, FormatKey.CURRENCY, "{0} $", "{0} $", true);
     }
 
-    private void tryCatchBlock(Runnable block) {
-        try {
-            block.run();
-        } catch (Exception e) {
-            this.logger.log(Level.SEVERE, e.getMessage(), e);
-        }
-    }
 }
