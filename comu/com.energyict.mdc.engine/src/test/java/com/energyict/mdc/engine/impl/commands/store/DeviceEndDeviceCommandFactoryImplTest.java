7062c815a543ece5ab1035ec10edaaafbbc8c40c
Dragos Iancu <dragos.iancu@elster.com>
2016-05-30 13:07:32 +0300

    CXO-1137 - updated unit tests and enddevicecontroltypes.csv

--- comu/com.energyict.mdc.engine/src/test/java/com/energyict/mdc/engine/impl/commands/store/DeviceEndDeviceCommandFactoryImplTest.java
+++ comu/com.energyict.mdc.engine/src/test/java/com/energyict/mdc/engine/impl/commands/store/DeviceEndDeviceCommandFactoryImplTest.java
@@ -29,7 +29,7 @@ import static org.mockito.Mockito.when;
  * @since 2012-08-23 (09:02)
  */
 @RunWith(MockitoJUnitRunner.class)
-public class DeviceCommandFactoryImplTest {
+public class DeviceEndDeviceCommandFactoryImplTest {
 
     @Mock
     private DeviceCommand.ServiceProvider serviceProvider;
