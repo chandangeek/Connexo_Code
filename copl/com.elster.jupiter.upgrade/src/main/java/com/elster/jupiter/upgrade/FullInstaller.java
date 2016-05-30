5b62ae9c49be5428e8d4a39f7b53fe9be7f56f13
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-30 11:34:41 +0200

    CXO-1159 Streamline use of logging

--- copl/com.elster.jupiter.upgrade/src/main/java/com/elster/jupiter/upgrade/FullInstaller.java
+++ copl/com.elster.jupiter.upgrade/src/main/java/com/elster/jupiter/upgrade/FullInstaller.java
@@ -4,6 +4,8 @@ import com.elster.jupiter.orm.DataModelUpgrader;
 
 import aQute.bnd.annotation.ConsumerType;
 
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ConsumerType
@@ -11,4 +13,27 @@ public interface FullInstaller {
 
     void install(DataModelUpgrader dataModelUpgrader, Logger logger);
 
+    default void doTry(String description, Runnable runnable, Logger logger) {
+        try {
+            runnable.run();
+            logger.log(Level.SEVERE, "Success : " + description);
+        } catch (RuntimeException e) {
+            logger.log(Level.SEVERE, "Failed  : " + description, e);
+            throw e;
+        }
+    }
+
+    default <T> T doTry(String description, Callable<T> callable, Logger logger) {
+        try {
+            T value = callable.call();
+            logger.log(Level.SEVERE, "Success : " + description);
+            return value;
+        } catch (RuntimeException e) {
+            logger.log(Level.SEVERE, "Failed  : " + description, e);
+            throw e;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed  : " + description, e);
+            throw new RuntimeException(e);
+        }
+    }
 }
