9c72069335b78f4731d9cbbb19fc63dc3bb080fe
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-05-30 11:34:35 +0200

    CXO-1159 Streamline use of logging

--- coko/com.elster.jupiter.parties/src/main/java/com/elster/jupiter/parties/impl/Installer.java
+++ coko/com.elster.jupiter.parties/src/main/java/com/elster/jupiter/parties/impl/Installer.java
@@ -7,45 +7,28 @@ import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.FullInstaller;
 
 import javax.inject.Inject;
+import java.util.logging.Logger;
 
 public class Installer implements FullInstaller {
-	
-	final private DataModel dataModel;
-	final private EventService eventService;
+
+    final private DataModel dataModel;
+    final private EventService eventService;
 
     @Inject
-	Installer(DataModel dataModel,EventService eventService) {
-		this.dataModel = dataModel;
-		this.eventService = eventService;
-	}
+    Installer(DataModel dataModel, EventService eventService) {
+        this.dataModel = dataModel;
+        this.eventService = eventService;
+    }
 
     @Override
-    public void install(DataModelUpgrader dataModelUpgrader) {
-        try {
-            dataModelUpgrader.upgrade(dataModel, Version.latest());
-            createMasterData();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
-        createEventTypes();
+    public void install(DataModelUpgrader dataModelUpgrader, Logger logger) {
+        dataModelUpgrader.upgrade(dataModel, Version.latest());
+        doTry(
+                "Create event types for PRT",
+                this::createEventTypes,
+                logger
+        );
     }
-
-    public void install(boolean executeDdl , boolean updateOrm , boolean createMasterData) {
-        try {
-            dataModel.install(executeDdl, updateOrm);
-            if (createMasterData) {
-                createMasterData();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
-        createEventTypes();
-    }
-	
-	private void createMasterData() {
-	}
 
     private void createEventTypes() {
         for (EventType eventType : EventType.values()) {
