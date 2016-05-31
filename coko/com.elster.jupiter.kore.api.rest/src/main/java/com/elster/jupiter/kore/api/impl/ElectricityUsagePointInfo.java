9ae986eac79cdf1472479d3a6b05c98793a9317c
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-05-31 09:46:58 +0200

    CXO-1834: Renamed from platform-stuff to kore

---
+++ coko/com.elster.jupiter.kore.api.rest/src/main/java/com/elster/jupiter/kore/api/impl/ElectricityUsagePointInfo.java
@@ -0,0 +1,46 @@
+package com.elster.jupiter.kore.api.impl;
+
+import com.elster.jupiter.cbo.PhaseCode;
+import com.elster.jupiter.metering.ServiceKind;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.metering.UsagePointDetailBuilder;
+import com.elster.jupiter.util.YesNoAnswer;
+import com.elster.jupiter.util.units.Quantity;
+
+import java.time.Instant;
+
+/**
+ * Created by bvn on 4/11/16.
+ */
+public class ElectricityUsagePointInfo extends UsagePointInfo {
+    public Quantity nominalServiceVoltage;
+    public PhaseCode phaseCode;
+    public Quantity ratedCurrent;
+    public Quantity ratedPower;
+    public Quantity estimatedLoad;
+    public YesNoAnswer limiter;
+    public String loadLimiterType;
+    public Quantity loadLimit;
+    public YesNoAnswer interruptible;
+
+    @Override
+    public UsagePointDetailBuilder createDetail(UsagePoint usagePoint, Instant instant) {
+        return usagePoint.newElectricityDetailBuilder(instant)
+                .withCollar(collar)
+                .withGrounded(grounded)
+                .withNominalServiceVoltage(nominalServiceVoltage)
+                .withPhaseCode(phaseCode)
+                .withRatedCurrent(ratedCurrent)
+                .withRatedPower(ratedPower)
+                .withEstimatedLoad(estimatedLoad)
+                .withLimiter(limiter)
+                .withLoadLimiterType(loadLimiterType)
+                .withLoadLimit(loadLimit)
+                .withInterruptible(interruptible);
+    }
+
+    @Override
+    ServiceKind getServiceKind() {
+        return ServiceKind.ELECTRICITY;
+    }
+}
---
+++ coko/com.elster.jupiter.kore.api.rest/src/main/java/com/elster/jupiter/kore/api/impl/MetrologyConfigurationInfo.java
@@ -0,0 +1,17 @@
+package com.elster.jupiter.kore.api.impl;
+
+import com.elster.jupiter.rest.util.hypermedia.LinkInfo;
+
+import java.time.Instant;
+
+/**
+ * Created by bvn on 4/15/16.
+ */
+public class MetrologyConfigurationInfo extends LinkInfo<Long> {
+    public String name;
+    public Boolean active;
+    public String userName;
+
+    public Instant createTime;
+    public Instant modTime;
+}
