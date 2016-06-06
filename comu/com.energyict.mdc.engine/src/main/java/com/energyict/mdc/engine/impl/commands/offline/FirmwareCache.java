ba9800f516331d735471068d80271eb760679cc2
khe <kristof.hennebel@elster.com>
2016-06-06 14:14:23 +0200

    CXO-982 - Add FirmwareCachingMechanism to communication server / protocols
    Now creating sub folders for every comserver that runs on the same machine

--- comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/commands/offline/FirmwareCache.java
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/commands/offline/FirmwareCache.java
@@ -1,5 +1,6 @@
 package com.energyict.mdc.engine.impl.commands.offline;
 
+import com.energyict.mdc.engine.config.HostName;
 import com.energyict.mdc.firmware.FirmwareVersion;
 
 import java.io.*;
@@ -14,21 +15,35 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class FirmwareCache {
 
-    private static final String TEMP_DIR = "java.io.tmpdir";
+    private static final String TEMP_DIR = System.getProperty("java.io.tmpdir");
+    private static final String FILE_SEPARATOR = System.getProperty("file.separator");
 
     /**
      * We lock the critical section where we write the firmware file, making sure that we don't corrupt it.
      */
     private static final Lock firmwareFileLock = new ReentrantLock();
 
-
+    /**
+     * Find or create the proper temp file for the given FirmwareVersion, in the sub-folder with the host name of the ComServer.
+     */
     public static File findOrCreateTempFile(final FirmwareVersion firmwareVersion) {
 
         firmwareFileLock.lock();
         try {
 
-            final File tempFile = new File(System.getProperty(TEMP_DIR), "firmware-" + firmwareVersion.getId() + "-" + firmwareVersion.getModTime().getEpochSecond() + ".tmp");
+            String path = TEMP_DIR + FILE_SEPARATOR + HostName.getCurrent() + FILE_SEPARATOR;
 
+            //First find or create the sub folder with the name of this ComServer
+            File folder = new File(path);
+            if (!folder.exists() || !folder.isDirectory()) {
+                boolean mkdir = folder.mkdir();
+                if (!mkdir) {
+                    throw new IllegalStateException("Could not create folder [" + folder + "] : mkdir() returns false !");
+                }
+            }
+
+            //Now find or create the temp file representing the given FirmwareVersion
+            final File tempFile = new File(path, "firmware-" + firmwareVersion.getId() + "-" + firmwareVersion.getModTime().getEpochSecond() + ".tmp");
             if (!tempFile.exists()) {
 
                 final boolean created = tempFile.createNewFile();
