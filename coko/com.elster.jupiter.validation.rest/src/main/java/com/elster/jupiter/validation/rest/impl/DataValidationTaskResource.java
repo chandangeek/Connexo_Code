6bd8f7712334a25c70cf7128fe4454d648bc24e5
Yan R <yaratay@mera.ru>
2016-06-06 12:04:48 +0300

    CXO-1456 [BE] Validation: Split up flags per system CXO-1461 [BE] Suspect reading quality change + code refactoring

--- coko/com.elster.jupiter.validation.rest/src/main/java/com/elster/jupiter/validation/rest/impl/DataValidationTaskResource.java
+++ coko/com.elster.jupiter.validation.rest/src/main/java/com/elster/jupiter/validation/rest/impl/DataValidationTaskResource.java
@@ -54,8 +54,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static com.elster.jupiter.util.conditions.Where.where;
-
 @Path("/validationtasks")
 public class DataValidationTaskResource {
 
