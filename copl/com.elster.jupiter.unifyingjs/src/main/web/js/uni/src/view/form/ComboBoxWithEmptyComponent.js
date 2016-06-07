65711dbd80d57e176d3f51ced845b285d786399f
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-06-07 17:09:51 +0200

    CXO-1012 - Special field working temporary, applyconfig workaround

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/form/ComboBoxWithEmptyComponent.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/form/ComboBoxWithEmptyComponent.js
@@ -15,16 +15,13 @@ Ext.define('Uni.view.form.ComboBoxWithEmptyComponent', {
     extend: 'Ext.form.FieldContainer',
     alias: 'widget.comboboxwithemptycomponent',
 
-    mixins: {
-        bindable: 'Ext.util.Bindable'
-    },
-
     config: {
         noObjectsText: Uni.I18n.translate('general.noObjectsDefinedYet', 'UNI', 'No objects defined yet'),
         store: null,
         name: null,
         displayField: null,
         valueField: null,
+        allowBlank: false,
         emptyText: Uni.I18n.translate('general.selectAnObject', 'UNI', 'Select an object...'),
     },
 
@@ -33,24 +30,37 @@ Ext.define('Uni.view.form.ComboBoxWithEmptyComponent', {
             storeObject,
             combo;
         me.initConfig(me.config);
-        storeObject =  Ext.data.StoreManager.lookup(me.getStore());
-        me.config.store = storeObject;
+        storeObject = Ext.data.StoreManager.lookup(me.getStore());
         storeObject.load({
             callback: function (record, operation, success) {
                 me.removeAll();
                 if (success && storeObject.count() > 0) {
-                    combo = Ext.create('Ext.form.ComboBox');
-                    Ext.apply(combo, me.config);
-                    combo.setWidth(me.config.width - me.getLabelWidth());
+                    combo = Ext.create('Ext.form.ComboBox', {
+                        queryMode: 'local',
+                        store: storeObject,
+                        allowBlank: me.allowBlank,
+                        width: me.config.width - me.getLabelWidth(),
+                        displayField: me.config.displayField,
+                        valueField: me.config.valueField,
+                        emptyText: me.config.emptyText
+                    }),
+                    //me.config.store = storeObject;
+                    //Ext.apply(combo, me.config);
+                    //combo.setWidth(me.config.width - me.getLabelWidth());
                     me.add(combo);
                 } else {
                     me.add({
                         xtype: 'displayfield',
+                        itemId: 'noObjectsFoundField',
                         fieldLabel: '',
                         value: me.getNoObjectsText(),
                         fieldStyle: {
                             color: '#EB5642'
+                        },
+                        validate: function() {
+                            return me.config.allowBlank;
                         }
+
                     })
                 }
 
