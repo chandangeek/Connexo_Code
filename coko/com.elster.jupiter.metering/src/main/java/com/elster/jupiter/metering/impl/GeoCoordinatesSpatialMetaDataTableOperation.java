10475331d634e00c99589599e446672e4e98f578
iancud <dragos.iancu@elster.com>
2016-06-14 16:12:19 +0300

    CXO-1850 - corrected merge issues

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/GeoCoordinatesSpatialMetaDataTableOperation.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/GeoCoordinatesSpatialMetaDataTableOperation.java
@@ -4,11 +4,15 @@ import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.LiteralSql;
 import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.util.sql.SqlBuilder;
+import oracle.jdbc.proxy.annotation.Pre;
 
 import javax.inject.Inject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @LiteralSql
 public final class GeoCoordinatesSpatialMetaDataTableOperation {
@@ -21,36 +25,30 @@ public final class GeoCoordinatesSpatialMetaDataTableOperation {
     }
 
     public void execute() {
+        List<PreparedStatement> statements = new ArrayList<>();
         try (Connection conn = dataModel.getConnection(false);
              PreparedStatement dropOldMetaData = buildStatement(conn, dropSpatialMetaDataSql());
-             PreparedStatement setMetData = buildStatement(conn, setSpatialMetaDataSql())) {
+             PreparedStatement setMetDataforEndDevice = buildStatement(conn, setSpatialMetaDataSql(TableSpecs.MTR_ENDDEVICE));
+             PreparedStatement setMetDataforUsagePoint = buildStatement(conn, setSpatialMetaDataSql(TableSpecs.MTR_USAGEPOINT))) {
             dropOldMetaData.execute();
-            setMetData.execute();
+            setMetDataforEndDevice.execute();
+            setMetDataforUsagePoint.execute();
         } catch (SQLException sqlEx) {
             throw new UnderlyingSQLFailedException(sqlEx);
         }
     }
 
 
-    private SqlBuilder setSpatialMetaDataSql() {
+    private SqlBuilder setSpatialMetaDataSql(TableSpecs table) {
         SqlBuilder builder = new SqlBuilder();
         builder.append("INSERT INTO mdsys.user_sdo_geom_metadata VALUES ('"
-                + TableSpecs.MTR_ENDDEVICE.name()
+                + table.name()
                 + "', '"
                 + "GEOCOORDINATES"
                 + "', "
                 + "MDSYS.SDO_DIM_ARRAY ("
                 + "SDO_DIM_ELEMENT ('LONGITUDE',-180,180,1),"
-                + "SDO_DIM_ELEMENT ('LATITUDE',-90,90,1)),8307);"
-        );
-        builder.append("INSERT INTO mdsys.user_sdo_geom_metadata VALUES ('"
-                + TableSpecs.MTR_USAGEPOINT.name()
-                + "', '"
-                + "GEOCOORDINATES"
-                + "', "
-                + "MDSYS.SDO_DIM_ARRAY ("
-                + "SDO_DIM_ELEMENT ('LONGITUDE',-180,180,1),"
-                + "SDO_DIM_ELEMENT ('LATITUDE',-90,90,1)),8307);"
+                + "SDO_DIM_ELEMENT ('LATITUDE',-90,90,1)),8307)"
         );
         return builder;
     }
@@ -60,7 +58,7 @@ public final class GeoCoordinatesSpatialMetaDataTableOperation {
         SqlBuilder builder = new SqlBuilder();
         builder.append("DELETE FROM mdsys.user_sdo_geom_metadata where table_name IN ('"
                 + TableSpecs.MTR_ENDDEVICE.name()
-                +"', '"
+                + "', '"
                 + TableSpecs.MTR_USAGEPOINT.name()
                 + "') and column_name = '"
                 + "GEOCOORDINATES'"
