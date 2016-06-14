a94e8920b0a0ae1bcbd0df356d4d3544b4eaf71e
H165720 <albertv@rimroe.elster-group.com>
2016-06-14 12:03:38 +0300

    Revert "CXO-1137 - added completion message info object"
    This reverts commit 607671000eadbe8ae69f62001d7585b1f33259bd.

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/ami/CompletionOptionsImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/ami/CompletionOptionsImpl.java
@@ -1,24 +1,23 @@
 package com.energyict.mdc.device.data.impl.ami;
 
 import com.elster.jupiter.messaging.DestinationSpec;
-import com.elster.jupiter.metering.ami.CompletionMessageInfo;
 import com.elster.jupiter.metering.ami.CompletionOptions;
 import com.elster.jupiter.servicecall.ServiceCall;
 
 public class CompletionOptionsImpl implements CompletionOptions {
 
-    private volatile CompletionMessageInfo message;
+    private volatile String message;
     private volatile DestinationSpec destination;
     private volatile ServiceCall serviceCall;
 
-    public CompletionOptionsImpl(CompletionMessageInfo message, DestinationSpec destination, ServiceCall serviceCall) {
+    public CompletionOptionsImpl(String message, DestinationSpec destination, ServiceCall serviceCall) {
         this.message = message;
         this.destination = destination;
         this.serviceCall = serviceCall;
     }
 
     @Override
-    public void whenFinishedSend(CompletionMessageInfo message, DestinationSpec destinationSpec) {
+    public void whenFinishedSend(String message, DestinationSpec destinationSpec) {
         // save the msg and the name of the destination spec to the custom prop set of the child servicecall
         // moved all service call related code to demo bundle under ami_scsexamples
     /*    this.serviceCall.findChildren()
@@ -31,6 +30,19 @@ public class CompletionOptionsImpl implements CompletionOptions {
                     this.serviceCall.update(extension);
                 });
     */
-        destinationSpec.message(message.toString()).send();
+        destinationSpec.message(message).send();
+    }
+
+
+    @Override
+    public CompletionOptionsImpl setDestination(DestinationSpec destination) {
+        this.destination = destination;
+        return this;
+    }
+
+    @Override
+    public CompletionOptionsImpl setMessage(String message) {
+        this.message = message;
+        return this;
     }
 }
