d2706d1631ff765dd79553d35c0914fa9337affa
H165720 <albertv@rimroe.elster-group.com>
2016-06-14 12:39:03 +0300

    CXO-1611 Remove data validation kpi support from mdc.

---
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/kpi/DataCollectionKpiCalculator.java
@@ -0,0 +1,13 @@
+package com.energyict.mdc.device.data.impl.kpi;
+
+/**
+ * Calculates and stores the scores for a {@link DataCollectionKpiImpl}.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2014-10-15 (17:13)
+ */
+public interface DataCollectionKpiCalculator {
+
+    public void calculateAndStore();
+
+}
---
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/kpi/DataCollectionKpiCalculatorHandler.java
@@ -0,0 +1,52 @@
+package com.energyict.mdc.device.data.impl.kpi;
+
+import com.energyict.mdc.device.data.kpi.DataCollectionKpiService;
+import com.energyict.mdc.device.data.tasks.CommunicationTaskReportService;
+import com.energyict.mdc.device.data.tasks.ConnectionTaskReportService;
+
+import com.elster.jupiter.tasks.TaskExecutor;
+import com.elster.jupiter.tasks.TaskOccurrence;
+
+/**
+ * Calculates the scores for a {@link DataCollectionKpiImpl}.
+ *
+ * @author Rudi Vankeirsbilck (rudi)
+ * @since 2014-10-07 (16:50)
+ */
+public class DataCollectionKpiCalculatorHandler implements TaskExecutor {
+
+
+    private final DataCollectionKpiService dataCollectionKpiService;
+    private final ConnectionTaskReportService connectionTaskReportService;
+    private final CommunicationTaskReportService communicationTaskReportService;
+
+    public DataCollectionKpiCalculatorHandler(DataCollectionKpiService dataCollectionKpiService, ConnectionTaskReportService connectionTaskReportService, CommunicationTaskReportService communicationTaskReportService) {
+        super();
+        this.dataCollectionKpiService = dataCollectionKpiService;
+        this.connectionTaskReportService = connectionTaskReportService;
+        this.communicationTaskReportService = communicationTaskReportService;
+    }
+
+    @Override
+    public void execute(TaskOccurrence taskOccurrence) {
+        KpiType.calculatorForRecurrentPayload(taskOccurrence, new ServiceProvider()).calculateAndStore();
+    }
+
+    private class ServiceProvider implements KpiType.ServiceProvider {
+        @Override
+        public DataCollectionKpiService dataCollectionKpiService() {
+            return dataCollectionKpiService;
+        }
+
+        @Override
+        public ConnectionTaskReportService connectionTaskReportService() {
+            return connectionTaskReportService;
+        }
+
+        @Override
+        public CommunicationTaskReportService communicationTaskService() {
+            return communicationTaskReportService;
+        }
+    }
+
+}
