f33db11945399b5a8fefd0ec663eae624dfe7ac2
geert <geert.delmeiren@elster.com>
2016-06-14 17:27:36 +0200

    CXO-1333 - Replace Interval state validator with a new Reading quality validator Make the reading qualities required

--- coko/com.elster.jupiter.validators/src/main/java/com/elster/jupiter/validators/impl/ReadingQualitiesValidator.java
+++ coko/com.elster.jupiter.validators/src/main/java/com/elster/jupiter/validators/impl/ReadingQualitiesValidator.java
@@ -123,6 +123,7 @@ class ReadingQualitiesValidator extends AbstractValidator {
                         .specForValuesOf(new ReadingQualityValueFactory())
                         .named(READING_QUALITIES, TranslationKeys.READING_QUALITIES_VALIDATOR)
                         .fromThesaurus(this.getThesaurus())
+                        .markRequired()
                         .addValues(getPossibleReadingQualityPropertyValues())
                         .markMultiValued()
                         .finish());
