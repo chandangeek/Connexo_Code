00cec1c363a48f79573c06ff8313ffc850e4ca82
Mihai Andreescu <mihai.andreescu@ro.elster.com>
2016-06-15 16:50:22 +0300

    .

--- comu/com.energyict.mdc.issue.datavalidation/src/main/java/com/energyict/mdc/issue/datavalidation/impl/Installer.java
+++ comu/com.energyict.mdc.issue.datavalidation/src/main/java/com/energyict/mdc/issue/datavalidation/impl/Installer.java
@@ -1,10 +1,10 @@
 package com.energyict.mdc.issue.datavalidation.impl;
 
 import com.elster.jupiter.events.EventService;
-//import com.elster.jupiter.issue.share.entity.CreationRuleActionPhase;
+import com.elster.jupiter.issue.share.entity.CreationRuleActionPhase;
 import com.elster.jupiter.issue.share.entity.IssueType;
-//import com.elster.jupiter.issue.share.service.IssueActionService;
 import com.elster.jupiter.issue.share.service.IssueService;
+import com.elster.jupiter.issue.share.service.IssueActionService;
 import com.elster.jupiter.messaging.DestinationSpec;
 import com.elster.jupiter.messaging.MessageService;
 import com.elster.jupiter.orm.DataModel;
@@ -12,10 +12,11 @@ import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.FullInstaller;
 import com.energyict.mdc.issue.datavalidation.IssueDataValidationService;
-//import com.energyict.mdc.issue.datavalidation.impl.actions.CloseIssueAction;
+import com.energyict.mdc.issue.datavalidation.impl.actions.CloseIssueAction;
 import com.energyict.mdc.issue.datavalidation.impl.event.DataValidationEventDescription;
 import com.energyict.mdc.issue.datavalidation.impl.event.DataValidationEventHandlerFactory;
-//import com.energyict.mdc.issue.datavalidation.impl.DataValidationActionsFactory;
+import com.energyict.mdc.issue.datavalidation.impl.DataValidationActionsFactory;
+
 
 import com.google.inject.Inject;
 
@@ -24,29 +25,28 @@ import java.util.logging.Logger;
 import static com.elster.jupiter.messaging.DestinationSpec.whereCorrelationId;
 
 class Installer implements FullInstaller {
-    private static final Logger LOG = Logger.getLogger("DataValidationIssueInstaller");
 
     private final IssueService issueService;
-    //private final IssueActionService issueActionService;
+    private final IssueActionService issueActionService;
     private final DataModel dataModel;
     private final EventService eventService;
     private final MessageService messageService;
     private IssueType issueType;
 
     @Inject
-    Installer(DataModel dataModel, IssueService issueService, EventService eventService, MessageService messageService) {
+    Installer(DataModel dataModel, IssueService issueService,IssueActionService issueActionService, EventService eventService, MessageService messageService) {
         this.dataModel = dataModel;
         this.issueService = issueService;
-        //this.issueActionService = issueActionService;
+        this.issueActionService = issueActionService;
         this.eventService = eventService;
         this.messageService = messageService;
-        this.issueType = setSupportedIssueType();
+        //this.issueType = setSupportedIssueType();
+
     }
 
     @Override
     public void install(DataModelUpgrader dataModelUpgrader, Logger logger) {
         dataModelUpgrader.upgrade(dataModel, Version.latest());
-
         doTry(
                 "Create issue view operation",
                 () -> new CreateIssueViewOperation(dataModel).execute(),
@@ -73,9 +73,8 @@ class Installer implements FullInstaller {
         IssueType type = issueService.createIssueType(IssueDataValidationService.ISSUE_TYPE_NAME, TranslationKeys.DATA_VALIDATION_ISSUE_TYPE);
         issueService.createReason(IssueDataValidationService.DATA_VALIDATION_ISSUE_REASON, type,
                 TranslationKeys.DATA_VALIDATION_ISSUE_REASON, TranslationKeys.DATA_VALIDATION_ISSUE_REASON_DESCRIPTION);
-        //issueActionService.createActionType(DataValidationActionsFactory.ID, CloseIssueAction.class.getName(), this.issueType, CreationRuleActionPhase.OVERDUE);
+        issueActionService.createActionType(DataValidationActionsFactory.ID, RetryEstimationAction.class.getName(), setSupportedIssueType(), CreationRuleActionPhase.OVERDUE);
     }
-
 
     private void publishEvents() {
         for (DataValidationEventDescription eventDescription : DataValidationEventDescription.values()) {
