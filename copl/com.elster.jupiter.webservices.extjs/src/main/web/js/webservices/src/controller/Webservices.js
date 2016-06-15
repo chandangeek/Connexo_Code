7767b4349165e1030fca106eb6ba800941e9f9ad
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-06-15 13:39:19 +0200

    CXO-1635

--- copl/com.elster.jupiter.webservices.extjs/src/main/web/js/webservices/src/controller/Webservices.js
+++ copl/com.elster.jupiter.webservices.extjs/src/main/web/js/webservices/src/controller/Webservices.js
@@ -2,7 +2,8 @@ Ext.define('Wss.controller.Webservices', {
     extend: 'Ext.app.Controller',
 
     views: [
-        'Wss.view.Setup'
+        'Wss.view.Setup',
+        'Uni.view.window.Confirmation'
     ],
     stores: [
         'Wss.store.Endpoints'
@@ -23,6 +24,9 @@ Ext.define('Wss.controller.Webservices', {
             'webservices-setup webservices-grid': {
                 select: this.showPreview
             },
+            'webservices-action-menu': {
+                click: this.chooseAction
+            }
         });
     },
 
@@ -44,5 +48,37 @@ Ext.define('Wss.controller.Webservices', {
         form.loadRecord(record);
         preview.setTitle(Ext.String.htmlEncode(record.get('name')));
         preview.down('webservices-action-menu').record = record;
-    }
+    },
+
+    chooseAction: function (menu, item) {
+        var me = this;
+
+        switch (item.action) {
+            case 'remove':
+                me.removeEndpoint(menu.record);
+                break;
+        }
+    },
+
+    removeEndpoint: function (record) {
+        var me = this,
+            confirmationWindow = Ext.create('Uni.view.window.Confirmation'),
+            store = me.getStore('Wss.store.Endpoints');
+
+        confirmationWindow.show(
+            {
+                title: Uni.I18n.translate('general.removeX', 'CAL', "Remove '{0}'?", [record.get('name')]),
+                msg: Uni.I18n.translate('webservices.remove.msg', 'WSS', 'This webservice endpoint will be removed and no longer be available.'),
+                fn: function (state) {
+                    if (state === 'confirm') {
+                        record.destroy({
+                            success: function () {
+                                me.getApplication().fireEvent('acknowledge', Uni.I18n.translate('webservices.remove.success.msg', 'WSS', 'Webservice endpoint removed'));
+                                store.load();
+                            }
+                        });
+                    }
+                }
+            });
+    },
 });
