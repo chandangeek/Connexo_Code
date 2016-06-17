2c3cd0987c36a7099e448fd3849c2dae0c74fd47
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-06-17 16:22:56 +0200

    Sonar

--- copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/PrivilegeInfo.java
+++ copl/com.elster.jupiter.users.rest/src/main/java/com/elster/jupiter/users/rest/PrivilegeInfo.java
@@ -2,11 +2,14 @@ package com.elster.jupiter.users.rest;
 
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.users.Privilege;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PrivilegeInfo {
-    
+
     public String name;
     public String translatedName;
     public String applicationName;
@@ -18,24 +21,31 @@ public class PrivilegeInfo {
         privilegeInfo.translatedApplicationName = thesaurus.getStringBeyondComponent(applicationName, applicationName);
         return privilegeInfo;
     }
+
+    public PrivilegeInfo() {
+    }
+
     public PrivilegeInfo(Thesaurus thesaurus, Privilege privilege) {
+        this();
         this.name = privilege.getName();
         this.translatedName = thesaurus.getStringBeyondComponent(this.name, this.name);
     }
 
     public PrivilegeInfo(Thesaurus thesaurus, String applicationName, Privilege privilege) {
+        this(thesaurus, privilege);
         this.applicationName = applicationName;
         this.translatedApplicationName = thesaurus.getStringBeyondComponent(applicationName, applicationName);
-        this.name = privilege.getName();
-        this.translatedName = thesaurus.getStringBeyondComponent(this.name, this.name);
-    }
-
-    public PrivilegeInfo() {
     }
 
     public String getApplicationName(){
         return applicationName;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, applicationName);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -50,4 +60,5 @@ public class PrivilegeInfo {
         return (name.equals(privilegeInfo.name)) && (applicationName.equals(privilegeInfo.applicationName)) ;
 
     }
+
 }
