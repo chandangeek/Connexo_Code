8f77890acc8c55ebc3750bba3646640a7a8754af
dvy <dieter.vyncke@elster.com>
2016-06-17 14:52:47 +0200

    Add of webservice endpoint

---
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/IdWithLocalizedValueInfo.java
@@ -0,0 +1,16 @@
+package com.elster.jupiter.rest.util;
+
+import com.elster.jupiter.util.HasId;
+import com.elster.jupiter.util.HasName;
+
+public class IdWithLocalizedValueInfo<T> {
+    public T id;
+    public String localizedValue;
+
+    public IdWithLocalizedValueInfo(){}
+
+    public IdWithLocalizedValueInfo(T id, String localizedValue) {
+        this.id = id;
+        this.localizedValue = localizedValue;
+    }
+}
