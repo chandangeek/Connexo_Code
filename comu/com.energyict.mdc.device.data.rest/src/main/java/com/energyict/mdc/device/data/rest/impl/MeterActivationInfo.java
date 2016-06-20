2ed8068b9954408f5bfad811f97ec7ec8952ffd3
Maksim Barinov <mbarinov@mera.ru>
2016-06-20 10:52:52 +0300

    CXO-1993

---
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/MeterActivationInfo.java
@@ -0,0 +1,38 @@
+package com.energyict.mdc.device.data.rest.impl;
+
+import com.elster.jupiter.metering.MeterActivation;
+import com.elster.jupiter.rest.util.IdWithNameInfo;
+import com.energyict.mdc.device.data.Device;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class MeterActivationInfo {
+
+    public long id;
+    public Long start;
+    public Long end;
+    public long version;
+    public boolean active;
+    public IdWithNameInfo deviceConfiguration;
+    public IdWithNameInfo usagePoint;
+    public Long multiplier;
+
+    public MeterActivationInfo() {
+    }
+
+    public MeterActivationInfo(MeterActivation meterActivation, Device device) {
+        this.id = meterActivation.getId();
+        if (meterActivation.getStart() != null) {
+            this.start = meterActivation.getStart().toEpochMilli();
+            this.multiplier = device.getMultiplierAt(meterActivation.getStart()).isPresent() ? device.getMultiplierAt(meterActivation.getStart()).get().longValue() : null;
+        }
+        this.end = meterActivation.getEnd() == null ? null : meterActivation.getEnd().toEpochMilli();
+        this.version = meterActivation.getVersion();
+        this.active = meterActivation.isCurrent();
+        this.deviceConfiguration = new IdWithNameInfo(device.getDeviceConfiguration().getId(), device.getDeviceConfiguration().getName());
+        this.usagePoint = meterActivation.getUsagePoint().isPresent() ?
+                new IdWithNameInfo(meterActivation.getUsagePoint().get().getId(), meterActivation.getUsagePoint().get().getName()) : null;
+    }
+}
+
