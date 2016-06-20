a6686028ccb7b59ef2936bbfdfd6509c2dc62783
Anton Suslennikov <antsusl@mera.ru>
2016-06-20 09:52:47 +0300

    CXO-1431 Fix missed usages

--- coko/com.elster.jupiter.cbo/src/test/java/com/elster/jupiter/cbo/QualityCodeSystemTest.java
+++ coko/com.elster.jupiter.cbo/src/test/java/com/elster/jupiter/cbo/QualityCodeSystemTest.java
@@ -14,13 +14,4 @@ public class QualityCodeSystemTest {
         assertThat(QualityCodeSystem.of("")).isEqualTo(QualityCodeSystem.NOTAPPLICABLE);
         assertThat(QualityCodeSystem.of("BOOTLEG")).isEqualTo(QualityCodeSystem.OTHER);
     }
-
-    @Test
-    public void testQualityCodeSystemOfApplication() {
-        assertThat(QualityCodeSystem.ofApplication("MDC")).isEqualTo(QualityCodeSystem.MDC);
-        assertThat(QualityCodeSystem.ofApplication("INS")).isEqualTo(QualityCodeSystem.MDM);
-        assertThat(QualityCodeSystem.ofApplication(null)).isEqualTo(QualityCodeSystem.NOTAPPLICABLE);
-        assertThat(QualityCodeSystem.ofApplication("")).isEqualTo(QualityCodeSystem.NOTAPPLICABLE);
-        assertThat(QualityCodeSystem.ofApplication("BOOTLEG")).isEqualTo(QualityCodeSystem.OTHER);
-    }
 }
