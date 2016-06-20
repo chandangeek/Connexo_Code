eaf9c9cbf3212062c29f19eb97f5c973cc7811b3
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-06-20 16:30:32 +0200

    Sonar: Resources should be closed

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/MessageSeeds.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/MessageSeeds.java
@@ -9,18 +9,18 @@ import java.util.logging.Logger;
 
 public enum MessageSeeds implements MessageSeed {
     NO_DEFAULT_REALM(1001, "domain.no.default", "No default domain found."),
-    FAIL_ACTIVATE_USER(1012, "fail.activate.user", "Fail to activate user."),
     NO_REALM_FOUND(1002, "domain.not.found", "No domain found with the name {0}."),
-    NO_REALMID_FOUND(1007, "domain.id.not.found", "No domain found with the id {0}."),
-    NO_LDAP_FOUND(1011, "ldap.not.found", "Connection to LDAP failed."),
     FIELD_CAN_NOT_BE_EMPTY(1003, Keys.FIELD_CAN_NOT_BE_EMPTY, "This field is required"),
     FIELD_SIZE_BETWEEN_1_AND_80(1004, Keys.FIELD_SIZE_BETWEEN_1_AND_80, "Field text length should be between 1 and 80 symbols"),
-    FIELD_SIZE_BETWEEN_1_AND_128(1010, Keys.FIELD_SIZE_BETWEEN_1_AND_128, "Field text length should be between 1 and 128 symbols"),
-    FIELD_SIZE_BETWEEN_1_AND_4000(1009, Keys.FIELD_SIZE_BETWEEN_1_AND_4000, "Field text length should be between 1 and 4000 symbols"),
     ONLY_ONE_DEFAULT_KEY_PER_LOCALE_ALLOWED(1005, Keys.ONLY_ONE_DEFAULT_KEY_PER_LOCALE_ALLOWED, "Only one default key per locale is allowed"),
     DUPLICATE_GROUP_NAME(1006, Keys.DUPLICATE_GROUP_NAME, "Duplicate name"),
+    NO_REALMID_FOUND(1007, "domain.id.not.found", "No domain found with the id {0}."),
     DUPLICATE_USER_DIRECTORY(1008, Keys.DUPLICATE_USER_DIRECTORY, "Duplicate name"),
-    UNDERLYING_IO_EXCEPTION(1009, Keys.UNDERLYING_IO_EXCEPTION, "Underlying IO Exception");
+    FIELD_SIZE_BETWEEN_1_AND_4000(1009, Keys.FIELD_SIZE_BETWEEN_1_AND_4000, "Field text length should be between 1 and 4000 symbols"),
+    FIELD_SIZE_BETWEEN_1_AND_128(1010, Keys.FIELD_SIZE_BETWEEN_1_AND_128, "Field text length should be between 1 and 128 symbols"),
+    NO_LDAP_FOUND(1011, "ldap.not.found", "Connection to LDAP failed."),
+    FAIL_ACTIVATE_USER(1012, "fail.activate.user", "Fail to activate user."),
+    UNDERLYING_IO_EXCEPTION(1013, Keys.UNDERLYING_IO_EXCEPTION, "Underlying IO Exception");
     ;
 
     private final int number;
