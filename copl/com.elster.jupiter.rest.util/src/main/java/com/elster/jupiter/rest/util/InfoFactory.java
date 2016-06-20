b75b910dc50396a21fc199d4965dd8b8bb3cd647
Bruno Ballekens <bruno.ballekens@elster.com>
2016-06-20 22:42:43 +0200

    CXO-1782: Search is too slow

--- copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/InfoFactory.java
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/InfoFactory.java
@@ -1,6 +1,7 @@
 package com.elster.jupiter.rest.util;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Allows creation of an Info object based on a domain object. This interface is required to register factories to be used by dynamic search.
@@ -12,12 +13,16 @@ public interface InfoFactory<T> {
      * @param domainObject The domain object (DeviceImpl, ComServerImpl, FirmwareVersion, ....
      * @return Some Info object
      */
-    public Object from(T domainObject);
+    Object from(T domainObject);
+
+    default List<Object> from(List<T> domainObjects) {
+        return domainObjects.stream().map(this::from).collect(Collectors.toList());
+    }
 
     List<PropertyDescriptionInfo> modelStructure();
 
     /**
      * Base-class any domain object for this factory should obey IS-A relation to.
      */
-    public Class<T> getDomainClass();
+    Class<T> getDomainClass();
 }
