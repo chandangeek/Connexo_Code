3bf37980c27d6c2a51c5f3ab55517589dc8e2911
Aura Vrancea <aura.vrancea@elster.com>
2016-06-23 18:48:07 +0300

    COMMUNICATION-1614 - New protocol development Schneider PM5561 ModBus meter

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/schneider/powerlogic/profile/ProfileBuilder.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/modbus/schneider/powerlogic/profile/ProfileBuilder.java
@@ -3,6 +3,7 @@ package com.energyict.protocolimpl.modbus.schneider.powerlogic.profile;
 import com.energyict.obis.ObisCode;
 import com.energyict.protocol.IntervalData;
 import com.energyict.protocol.ProfileData;
+import com.energyict.protocolimpl.modbus.core.AbstractRegister;
 import com.energyict.protocolimpl.modbus.core.functioncode.ReadGeneralReferenceRequest;
 import com.energyict.protocolimpl.modbus.schneider.powerlogic.PM5561;
 import com.energyict.protocolimpl.modbus.schneider.powerlogic.PM5561RegisterFactory;
@@ -18,10 +19,15 @@ public class ProfileBuilder {
 
     private static final int SECONDS_PER_MINUTE = 60;
     private static final int REGULAR_NUMBER_OF_PROFILE_RECORDS_PER_PROFILE_BLOCK = 1;
-    private static final int REGULAR_NUMBER_OF_CHANNELS = 10;
+    private static final int REGULAR_NUMBER_OF_CHANNELS = 9;
     private PM5561 protocol;
     private ProfileBlock lastProfileBlock;
     private double ratio = 1;
+    BigDecimal loadProfileStatus = null;
+    BigDecimal numberOfRecords = null;
+    BigDecimal referenceNo = null;
+    BigDecimal lastRecord = null;
+    AbstractRegister loadProfileRecordItem1 = null;
 
     public ProfileBuilder(PM5561 protocol) {
         this.protocol = protocol;
@@ -45,12 +51,19 @@ public class ProfileBuilder {
 
         profileDataBlocks.add(getLastProfileBlock());
         ProfileBlock profileBlock = getLastProfileBlock();
-        while (from.before(profileBlock.getOldestProfileRecordDate()) &&
-                profileBlock.getProfileRecords().getProfileRecords().size() == REGULAR_NUMBER_OF_PROFILE_RECORDS_PER_PROFILE_BLOCK) { // If a ProfileRecord contains less entries, then it was the last/oldest one available > you should stop readout
-            BigDecimal referenceNo  = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_FIRST_RECORD).value();
-            ReadGeneralReferenceRequest readGeneralReferenceRequest = protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_RECORD_ITEM1).getReadGeneralReferenceRequest(referenceNo.longValue());
-            BigDecimal numberOfRecords = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_NUMBER_OF_RECORDS).value();
-            profileBlock = new ProfileBlock(readGeneralReferenceRequest.getValues(), numberOfRecords.intValue());
+        if(numberOfRecords == null) {
+            numberOfRecords = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_NUMBER_OF_RECORDS).value();
+        }
+        if(referenceNo == null) {
+            referenceNo = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_FIRST_RECORD).value();
+        }
+        if(loadProfileRecordItem1 == null) {
+            loadProfileRecordItem1 = protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_RECORD_ITEM1);
+        }
+        int index = 0;
+        while (from.before(profileBlock.getOldestProfileRecordDate()) && index < 50) {
+            ReadGeneralReferenceRequest readGeneralReferenceRequest = loadProfileRecordItem1.getReadGeneralReferenceRequest(referenceNo.longValue() + index++);
+            profileBlock = new ProfileBlock(readGeneralReferenceRequest.getValues(), REGULAR_NUMBER_OF_CHANNELS);
             profileDataBlocks.add(profileBlock);
         }
 
@@ -74,7 +87,9 @@ public class ProfileBuilder {
 
     private ProfileBlock getLastProfileBlock() throws IOException {
         if (this.lastProfileBlock == null) {
-            BigDecimal lastRecord = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_LAST_RECORD).value();
+            if(lastRecord == null) {
+                lastRecord = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_LAST_RECORD).value();
+            }
             ReadGeneralReferenceRequest readGeneralReferenceRequest = protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_RECORD_ITEM1).getReadGeneralReferenceRequest(lastRecord.longValue());
             this.lastProfileBlock = new ProfileBlock(readGeneralReferenceRequest.getValues(), REGULAR_NUMBER_OF_CHANNELS);
         }
@@ -99,7 +114,9 @@ public class ProfileBuilder {
     }
 
     public boolean isSupported() throws IOException {
-        BigDecimal loadProfileStatus = (BigDecimal)protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_STATUS).value();
+        if(loadProfileStatus == null) {
+            loadProfileStatus = (BigDecimal) protocol.getRegisterFactory().findRegister(PM5561RegisterFactory.LOAD_PROFILE_STATUS).value();
+        }
         return loadProfileStatus.intValue() == 1;
     }
 
