d219c123f9c18b356a7094c75cb482dde4bd10f4
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-06-24 09:17:07 +0200

    CXO-1173 - Check for differences

---
+++ comu/com.energyict.mdc.masterdata/src/main/java/com/energyict/mdc/masterdata/impl/MdcChecklist.java
@@ -0,0 +1,46 @@
+package com.energyict.mdc.masterdata.impl;
+
+import com.elster.jupiter.upgrade.InstallIdentifier;
+import com.elster.jupiter.upgrade.UpgradeCheckList;
+
+import com.google.common.collect.ImmutableSet;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Set;
+
+@Component(name = "com.energyict.mdc.checklist", service = {UpgradeCheckList.class}, property = {"com.elster.jupiter.checklist=MultiSense"})
+public class MdcChecklist implements UpgradeCheckList {
+    @Override
+    public String application() {
+        return "MultiSense";
+    }
+
+    @Override
+    public Set<InstallIdentifier> componentsToInstall() {
+        return ImmutableSet.<InstallIdentifier>builder()
+                .add(
+                        InstallIdentifier.identifier(application(), "MDS"),
+                        InstallIdentifier.identifier(application(), "DLD"),
+                        InstallIdentifier.identifier(application(), "PPC"),
+                        InstallIdentifier.identifier(application(), "MDC"),
+                        InstallIdentifier.identifier(application(), "CTS"),
+                        InstallIdentifier.identifier(application(), "SCH"),
+                        InstallIdentifier.identifier(application(), "DTC"),
+                        InstallIdentifier.identifier(application(), "DDC"),
+                        InstallIdentifier.identifier(application(), "IDV"),
+                        InstallIdentifier.identifier(application(), "FAV"),
+                        InstallIdentifier.identifier(application(), "DTL"),
+                        InstallIdentifier.identifier(application(), "IDC"),
+                        InstallIdentifier.identifier(application(), "FWC"),
+                        InstallIdentifier.identifier(application(), "PR1"),
+                        InstallIdentifier.identifier(application(), "DDI"),
+                        InstallIdentifier.identifier(application(), "MRI"),
+                        InstallIdentifier.identifier(application(), "CES"),
+                        InstallIdentifier.identifier(application(), "CSM"),
+                        InstallIdentifier.identifier(application(), "MDA"),
+                        InstallIdentifier.identifier(application(), "FLI"),
+                        InstallIdentifier.identifier(application(), "CPC"),
+                        InstallIdentifier.identifier(application(), "DSI")
+                ).build();
+    }
+}
