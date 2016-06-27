326c79794423651f5c8920a1ac173bcc05eeee2e
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-06-27 16:17:05 +0200

    CXO-1637: revert to working code

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/OutboundEndPoint.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/OutboundEndPoint.java
@@ -4,19 +4,12 @@ import com.elster.jupiter.soap.whiteboard.cxf.EndPointConfiguration;
 import com.elster.jupiter.soap.whiteboard.cxf.OutboundEndPointConfiguration;
 import com.elster.jupiter.soap.whiteboard.cxf.OutboundEndPointProvider;
 
-import org.apache.cxf.annotations.SchemaValidation;
-import org.apache.cxf.feature.validation.SchemaValidationFeature;
-import org.apache.cxf.transport.common.gzip.GZIPFeature;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 import javax.inject.Inject;
 import javax.xml.ws.Service;
 import javax.xml.ws.WebServiceFeature;
-import javax.xml.ws.spi.Provider;
-import javax.xml.ws.spi.ServiceDelegate;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,14 +20,12 @@ public final class OutboundEndPoint implements ManagedEndpoint {
     private OutboundEndPointProvider endPointProvider;
     private EndPointConfiguration endPointConfiguration;
     private final BundleContext bundleContext;
-    private final javax.inject.Provider<AccessLogFeature> accessLogFeatureProvider;
 
     private ServiceRegistration<?> serviceRegistration;
 
     @Inject
-    public OutboundEndPoint(BundleContext bundleContext, javax.inject.Provider<AccessLogFeature> accessLogFeatureProvider) {
+    public OutboundEndPoint(BundleContext bundleContext) {
         this.bundleContext = bundleContext;
-        this.accessLogFeatureProvider = accessLogFeatureProvider;
     }
 
     OutboundEndPoint init(OutboundEndPointProvider endPointProvider, OutboundEndPointConfiguration endPointConfiguration) {
@@ -48,28 +39,12 @@ public final class OutboundEndPoint implements ManagedEndpoint {
         if (this.isPublished()) {
             throw new IllegalStateException("Service already published");
         }
-        try {
-            List<WebServiceFeature> features = new ArrayList<>();
-            if (endPointConfiguration.isHttpCompression()) {
-                features.add(new GZIPFeature());
-            }
-            if (endPointConfiguration.isSchemaValidation()) {
-                features.add(new SchemaValidationFeature(operationInfo -> SchemaValidation.SchemaValidationType.IN));
-            }
-            features.add(accessLogFeatureProvider.get().init(endPointConfiguration));
-            Service providedService = endPointProvider.get();
-            ServiceDelegate serviceDelegate = Provider.provider()
-                    .createServiceDelegate(new URL(endPointConfiguration.getUrl()),
-                            providedService.getServiceName(),
-                            providedService.getClass(),
-                            features.toArray(new WebServiceFeature[features.size()]));
-            serviceRegistration = bundleContext.registerService(
-                    endPointProvider.getService(),
-                    serviceDelegate.getPort(endPointProvider.getService()),
-                    null);
-        } catch (MalformedURLException e) {
-            endPointConfiguration.log("Failed to publish endpoint", e);
-        }
+        List<WebServiceFeature> features = new ArrayList<>();
+        Service providedService = endPointProvider.get();
+        serviceRegistration = bundleContext.registerService(
+                endPointProvider.getService(),
+                providedService.getPort(endPointProvider.getService(), features.toArray(new WebServiceFeature[features.size()])),
+                null);
     }
 
     @Override
