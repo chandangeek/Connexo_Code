269f138db44e57363d8e683ef1c9bf40355caed9
Claudiu Isac <claudiu.isac@elster.com>
2016-06-28 16:33:48 +0300

    COMMUNICATION-1634 - Beacon protocol should create DC schedules according to Beacon timezone

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtu3/beacon3100/messages/syncobjects/CronTabStyleConverter.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtu3/beacon3100/messages/syncobjects/CronTabStyleConverter.java
@@ -6,6 +6,9 @@ import com.energyict.mdc.tasks.NextExecutionSpecs;
 import com.energyict.protocol.exceptions.DeviceConfigurationException;
 import com.energyict.protocolimplv2.MdcManager;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 /**
  * Copyrights EnergyICT
  *
@@ -14,7 +17,7 @@ import com.energyict.protocolimplv2.MdcManager;
  */
 public class CronTabStyleConverter {
 
-    public static String convert(NextExecutionSpecs nextExecutionSpecs) {
+    public static String convert(NextExecutionSpecs nextExecutionSpecs, TimeZone timeZone, TimeZone localTimezone) {
         if (nextExecutionSpecs == null) {
             return "";
         } else if (nextExecutionSpecs.getTemporalExpression() != null) {
@@ -22,13 +25,32 @@ public class CronTabStyleConverter {
             final TimeDuration every = nextExecutionSpecs.getTemporalExpression().getEvery();
             final NextExecutionSpecsShadow.Offset offset = nextExecutionSpecs.getShadow().getOffsetInDaysHoursMinutes();
 
+            Calendar localTime = Calendar.getInstance();
+            localTime.setTimeZone(localTimezone);
+            if(every.getTimeUnitCode() == TimeDuration.MONTHS){
+                localTime.set(Calendar.DAY_OF_MONTH, offset.getDays() + 1); //+1 because EiServer has 0-based days
+            } else if(every.getTimeUnitCode() == TimeDuration.WEEKS){
+                localTime.set(Calendar.DAY_OF_WEEK, offset.getDays() + 1);
+            }
+            localTime.set(Calendar.HOUR_OF_DAY, offset.getHours());
+            localTime.set(Calendar.MINUTE, offset.getMinutes());
+
+            //convert localTime to beaconTime
+            Calendar beaconCalendar = Calendar.getInstance();
+            beaconCalendar.setTimeZone(timeZone);
+            beaconCalendar.setTimeInMillis(localTime.getTimeInMillis());
+            int dayOfMonth = beaconCalendar.get(Calendar.DAY_OF_MONTH);
+            int dayOfWeek = beaconCalendar.get(Calendar.DAY_OF_WEEK);
+            int hours = beaconCalendar.get(Calendar.HOUR_OF_DAY);
+            int minutes = beaconCalendar.get(Calendar.MINUTE);
+
             switch (every.getTimeUnitCode()) {
                 case TimeDuration.MONTHS:
-                    return "0 " + offset.getMinutes() + " " + offset.getHours() + " " + getDayOfMonth(offset) + " */" + every.getCount() + " *";  //E.g. '0 0 6 16 */3 *' means 'every 3 months, on day 16, at 06:00:00'
+                    return "0 " + minutes + " " + hours + " " + dayOfMonth + " */" + every.getCount() + " *";  //E.g. '0 0 6 16 */3 *' means 'every 3 months, on day 16, at 06:00:00'
                 case TimeDuration.WEEKS:
-                    return "0 " + offset.getMinutes() + " " + offset.getHours() + " */" + (every.getCount() * 7) + " * " + getDayOfWeek(offset);  //E.g. '0 0 6 */14 * 1' means 'every 2 weeks, on monday, at 06:00:00'
+                    return "0 " + minutes + " " + hours + " */" + (every.getCount() * 7) + " * " + dayOfWeek;  //E.g. '0 0 6 */14 * 1' means 'every 2 weeks, on monday, at 06:00:00'
                 case TimeDuration.DAYS:
-                    return "0 " + offset.getMinutes() + " " + offset.getHours() + " */" + every.getCount() + " * *";  //E.g. '0 0 6 */3 * *' means 'every 3 days, at 06:00:00'
+                    return "0 " + minutes + " " + hours + " */" + every.getCount() + " * *";  //E.g. '0 0 6 */3 * *' means 'every 3 days, at 06:00:00'
                 case TimeDuration.HOURS:
                     return "0 " + offset.getMinutes() + " */" + every.getCount() + " * * *";  //E.g. '0 3 */2 * * *' means 'every 2 hours, at 00:03:00'
                 case TimeDuration.MINUTES:
@@ -43,18 +65,4 @@ public class CronTabStyleConverter {
         }
     }
 
-    /**
-     * EIServer day is 0 based, return 1-based
-     */
-    private static int getDayOfMonth(NextExecutionSpecsShadow.Offset offset) {
-        return offset.getDays() + 1;
-    }
-
-    /**
-     * offset.getDays: 0 = monday, 1 = tuesday,... 6 = sunday
-     * returns: 1 for monday, 2 for tuesday, ... 7 for sunday
-     */
-    private static int getDayOfWeek(NextExecutionSpecsShadow.Offset offset) {
-        return offset.getDays() + 1;
-    }
 }
