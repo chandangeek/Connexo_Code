e1e52b522c93c34db73727143f1da38e73625b60
sva <stijn.vanhoorelbeke@elster.com>
2016-06-30 17:20:32 +0200

    CXO-2086 OptimisticLockException when comserver updates state of already revoked device command + Added front-ent js code to show error window in case the DeviceMessage could not be revoked

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/MessageSeeds.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/MessageSeeds.java
@@ -148,7 +148,7 @@ public enum MessageSeeds implements MessageSeed {
     NO_SUCH_END_DEVICE_CONTROL_TYPE(2122, "NoSuchEndDeviceControlType", "No end device control type with MRID {0}"),
     NO_SUCH_DEVICE_MESSAGE_SPEC(2123, "NoSuchDeviceMessageSpec", "No such device message spec: {0}"),
     NO_STATUS_INFORMATION_COMTASK(2124, Keys.NO_STATUS_INFORMATION_COMTASK, "A comtask to read out the status information could not be located"),
-    DEVICE_MESSAGE_REVOKE_PICKED_UP_BY_COMSERVER(2125, Keys.DEVICE_MESSAGE_REVOKE_PICKED_UP_BY_COMSERVER, "A comserver is currently executing this command, therefore it cannot be revoked"),
+    DEVICE_MESSAGE_REVOKE_PICKED_UP_BY_COMSERVER(2125, Keys.DEVICE_MESSAGE_REVOKE_PICKED_UP_BY_COMSERVER, "A communication server is currently executing this command, therefore it cannot be revoked"),
     ;
 
     private final int number;
