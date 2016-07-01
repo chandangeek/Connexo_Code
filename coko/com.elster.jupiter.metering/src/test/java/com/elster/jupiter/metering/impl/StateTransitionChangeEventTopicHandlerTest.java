ea9f45c4811464a96d162c9a1694b9965c96af5f
akuryuk <akuryuk@mera.ru>
2016-07-01 16:42:42 +0300

    [CXO-2243] StateTransitionChangeEventTopicHandler queries EndDevices on MRID ignoring obsolete date Roll back changes of CXO-2040 HotFix

--- coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/StateTransitionChangeEventTopicHandlerTest.java
+++ coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/StateTransitionChangeEventTopicHandlerTest.java
@@ -20,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
