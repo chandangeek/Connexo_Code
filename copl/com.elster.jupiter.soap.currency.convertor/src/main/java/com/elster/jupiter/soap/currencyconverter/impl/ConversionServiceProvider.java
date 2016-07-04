e84b9f37926118c6d943d4e558ab40c4bcdc8d65
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-07-04 14:24:48 +0200

    CXO-1931: Renamed SOAP specific classes to *Soap*

--- copl/com.elster.jupiter.soap.currency.convertor/src/main/java/com/elster/jupiter/soap/currencyconverter/impl/ConversionServiceProvider.java
+++ copl/com.elster.jupiter.soap.currency.convertor/src/main/java/com/elster/jupiter/soap/currencyconverter/impl/ConversionServiceProvider.java
@@ -1,7 +1,7 @@
 package com.elster.jupiter.soap.currencyconverter.impl;
 
 import com.elster.jupiter.soap.currencyconverter.CurrencyConvertorSoap;
-import com.elster.jupiter.soap.whiteboard.cxf.OutboundEndPointProvider;
+import com.elster.jupiter.soap.whiteboard.cxf.OutboundSoapEndPointProvider;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -11,10 +11,10 @@ import javax.xml.ws.Service;
  * Created by bvn on 5/9/16.
  */
 @Component(name = "com.elster.jupiter.soap.currency.converter.provider",
-        service = {OutboundEndPointProvider.class},
+        service = {OutboundSoapEndPointProvider.class},
         immediate = true,
         property = {"name=xe"})
-public class ConversionServiceProvider implements OutboundEndPointProvider {
+public class ConversionServiceProvider implements OutboundSoapEndPointProvider {
 
     @Override
     public Service get() {
