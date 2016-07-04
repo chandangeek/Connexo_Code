df19cde29d673baa75bc9ee9b5abecfa7ff9c7f3
Sentyurin Sergey <ssentyur@mera.ru>
2016-07-04 13:56:13 +0300

    CXO-2037; CXO-2038: move functionality to mdc

---
+++ comu/com.energyict.mdc.usagepoint.data.rest/src/main/java/com.energyict.mdc.usagepoint.data.rest/impl/UsagePointDeviceChannelInfo.java
@@ -0,0 +1,10 @@
+package com.energyict.mdc.usagepoint.data.rest.impl;
+
+import com.elster.jupiter.rest.util.IdWithNameInfo;
+
+public class UsagePointDeviceChannelInfo {
+    public Long from;
+    public Long until;
+    public String mRID;
+    public IdWithNameInfo channel;
+}
