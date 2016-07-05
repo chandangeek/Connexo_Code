c38cb40f35ea834636f9120715958ed91f658b76
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-07-05 15:17:21 +0200

    CXO-1931: Added outbound REST demo bundle

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/OutboundRestEndPointProvider.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/OutboundRestEndPointProvider.java
@@ -1,13 +1,13 @@
 package com.elster.jupiter.soap.whiteboard.cxf;
 
-import javax.xml.ws.Service;
+import javax.ws.rs.client.WebTarget;
 
 /**
  * Provider allows whiteboard to pick up deployed components that implement this interface.
  * Allows registering Outbound REST services.
  */
-public interface OutboundRestEndPointProvider extends EndPointProvider {
-    Service get();
+public interface OutboundRestEndPointProvider<S> extends EndPointProvider {
+    S get(WebTarget client);
 
-    Class getService();
+    Class<S> getService();
 }
