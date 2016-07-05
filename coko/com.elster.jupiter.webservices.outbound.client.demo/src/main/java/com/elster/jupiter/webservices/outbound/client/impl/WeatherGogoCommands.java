78878c993aeb492fffe337de1240c4161509fed8
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-07-05 15:17:21 +0200

    CXO-1931: Added outbound REST demo bundle

---
+++ coko/com.elster.jupiter.webservices.outbound.client.demo/src/main/java/com/elster/jupiter/webservices/outbound/client/impl/WeatherGogoCommands.java
@@ -0,0 +1,37 @@
+package com.elster.jupiter.webservices.outbound.client.impl;
+
+import com.elster.jupiter.webservices.outbound.rest.WeatherInfo;
+import com.elster.jupiter.webservices.outbound.rest.WeatherService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * Created by bvn on 5/9/16.
+ */
+@Component(name = "com.elster.jupiter.soap.currency.converter.client.gogo",
+        service = WeatherGogoCommands.class,
+        property = {"osgi.command.scope=wea",
+                "osgi.command.function=weather"},
+        immediate = true)
+public class WeatherGogoCommands {
+
+    private volatile WeatherService weatherService;
+
+    public void weather() {
+        System.out.println("Get the current weather for a certain city");
+        System.out.println("usage: weather <city>");
+        System.out.println("   where <city> denotes a city name, e.g. London, Brussels");
+        System.out.println("example: wea.weather London");
+    }
+
+    public void weather(String city) {
+        WeatherInfo result = weatherService.getWeather(city);
+        System.out.println("The temperature at " + city + " is currently " + result.main.temp + "K with " + result.clouds.all + "% cloud covarage");
+    }
+
+    @Reference
+    public void setWeatherService(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+}
