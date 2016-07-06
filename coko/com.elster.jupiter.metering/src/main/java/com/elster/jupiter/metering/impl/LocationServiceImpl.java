eb1bf026bb094ef8cf5d8df01f6b0bdc67ec992c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-07-06 14:02:04 +0200

    Cleanup

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/LocationServiceImpl.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/LocationServiceImpl.java
@@ -2,12 +2,9 @@ package com.elster.jupiter.metering.impl;
 
 import com.elster.jupiter.metering.Location;
 import com.elster.jupiter.metering.LocationService;
-import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.OrmService;
 
-import com.google.inject.AbstractModule;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -17,32 +14,30 @@ import java.util.Optional;
 @Component(name = "com.elster.jupiter.location",
         service = {LocationService.class},
         property = "name=" + LocationService.COMPONENT_NAME)
+@SuppressWarnings("unused")
 public class LocationServiceImpl implements LocationService {
 
     private volatile DataModel dataModel;
 
+    // For OSGi purposes
     public LocationServiceImpl() {
     }
 
+    // For testing purposes
     @Inject
     public LocationServiceImpl(OrmService ormService) {
+        this();
         this.setOrmService(ormService);
     }
 
-
     @Reference
     public final void setOrmService(OrmService ormService) {
-        //dataModel = ormService.newDataModel(MeteringService.COMPONENTNAME, "LocationService");
-        ormService.getDataModel("MTR").ifPresent(found ->
-                this.dataModel = found);
-    }
-
-    @Activate
-    public final void activate() {
+        ormService.getDataModel("MTR").ifPresent(found -> this.dataModel = found);
     }
 
     @Override
     public Optional<Location> findLocationById(long id) {
         return dataModel.mapper(Location.class).getOptional(id);
     }
+
 }
