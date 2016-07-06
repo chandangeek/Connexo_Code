27ad49eb1ef24dff02808894c8ba4a59b2fd5347
sva <stijn.vanhoorelbeke@elster.com>
2016-07-06 12:22:03 +0200

    CXO-1104 Use service call framework in Redknee rest api + Refactored code according to review JP-736 + ServiceCallTypes are now created upfront (when activating the component, instead of adhoc when needed) + EndDeviceCommand has been split up in abstract EndDeviceCommandImpl and an implementation for each command type

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/CommandFactory.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/CommandFactory.java
@@ -2,19 +2,22 @@ package com.elster.jupiter.metering.ami;
 
 import com.elster.jupiter.metering.EndDevice;
 import com.elster.jupiter.metering.EndDeviceControlType;
+import com.elster.jupiter.util.units.Quantity;
+
+import java.time.Instant;
 
 public interface CommandFactory {
 
-    EndDeviceCommand createCommand(EndDevice endDevice, EndDeviceControlType endDeviceControlType);
+    EndDeviceCommand createCommand(EndDevice endDevice, EndDeviceControlType endDeviceControlType) throws UnsupportedCommandException;
 
-    EndDeviceCommand createArmCommand(EndDevice endDevice, boolean armForOpen);
+    EndDeviceCommand createArmCommand(EndDevice endDevice, boolean armForOpen, Instant activationDate) throws UnsupportedCommandException;
 
-    EndDeviceCommand createConnectCommand(EndDevice endDevice);
+    EndDeviceCommand createConnectCommand(EndDevice endDevice, Instant activationDate) throws UnsupportedCommandException;
 
-    EndDeviceCommand createDisconnectCommand(EndDevice endDevice);
+    EndDeviceCommand createDisconnectCommand(EndDevice endDevice, Instant activationDate) throws UnsupportedCommandException;
 
-    EndDeviceCommand createEnableLoadLimitCommand(EndDevice endDevice);
+    EndDeviceCommand createEnableLoadLimitCommand(EndDevice endDevice, Quantity quantity) throws UnsupportedCommandException;
 
-    EndDeviceCommand createDisableLoadLimitCommand(EndDevice endDevice);
+    EndDeviceCommand createDisableLoadLimitCommand(EndDevice endDevice) throws UnsupportedCommandException;
 
 }
---
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/UnsupportedCommandException.java
@@ -0,0 +1,12 @@
+package com.elster.jupiter.metering.ami;
+
+import com.elster.jupiter.metering.MessageSeeds;
+import com.elster.jupiter.nls.LocalizedException;
+import com.elster.jupiter.nls.Thesaurus;
+
+public final class UnsupportedCommandException extends LocalizedException {
+
+    public UnsupportedCommandException(Thesaurus thesaurus, String command, String mRID) {
+        super(thesaurus, MessageSeeds.UNSUPPORTED_COMMAND, command, mRID);
+    }
+}
