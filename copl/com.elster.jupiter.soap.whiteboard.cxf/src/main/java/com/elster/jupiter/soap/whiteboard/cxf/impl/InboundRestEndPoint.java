ba4d326720b4f5b6612de5748f1fd1e7bf2fe385
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-07-07 08:52:42 +0200

    CXO-1676: Expose inbound endpoint through HttpService

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/InboundRestEndPoint.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/InboundRestEndPoint.java
@@ -1,11 +1,26 @@
 package com.elster.jupiter.soap.whiteboard.cxf.impl;
 
+import com.elster.jupiter.rest.util.BinderProvider;
+import com.elster.jupiter.rest.util.TransactionWrapper;
 import com.elster.jupiter.soap.whiteboard.cxf.InboundEndPointConfiguration;
 import com.elster.jupiter.soap.whiteboard.cxf.InboundRestEndPointProvider;
+import com.elster.jupiter.transaction.TransactionService;
+import com.elster.jupiter.util.osgi.ContextClassLoaderResource;
+
+import com.fasterxml.jackson.jaxrs.base.JsonMappingExceptionMapper;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.message.GZipEncoder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.core.Application;
+import java.util.Objects;
 
 /**
  * This endpoint manager knows how to set up and tear down an inbound REST endpoint. Features are added as configured on the endpoint configuration.
@@ -15,12 +30,19 @@ public final class InboundRestEndPoint implements ManagedEndpoint {
     private InboundRestEndPointProvider endPointProvider;
     private InboundEndPointConfiguration endPointConfiguration;
     private final String logDirectory;
+    private final TransactionService transactionService;
+    private final HttpService httpService;
 
     private Application application;
+    private HttpContext httpContext;
+
+    private String alias;
 
     @Inject
-    public InboundRestEndPoint(@Named("LogDirectory") String logDirectory) {
+    public InboundRestEndPoint(@Named("LogDirectory") String logDirectory, TransactionService transactionService, HttpService httpService) {
         this.logDirectory = logDirectory;
+        this.transactionService = transactionService;
+        this.httpService = httpService;
     }
 
     InboundRestEndPoint init(InboundRestEndPointProvider endPointProvider, InboundEndPointConfiguration endPointConfiguration) {
@@ -34,12 +56,34 @@ public final class InboundRestEndPoint implements ManagedEndpoint {
         if (this.isPublished()) {
             throw new IllegalStateException("Service already published");
         }
+        alias = endPointConfiguration.getUrl();
+        application = endPointProvider.get();
+        ResourceConfig secureConfig = ResourceConfig.forApplication(Objects.requireNonNull(application));
+        secureConfig.register(ObjectMapperProvider.class);
+        secureConfig.register(JacksonFeature.class);
+        secureConfig.register(RolesAllowedDynamicFeature.class);
+        secureConfig.register(JsonMappingExceptionMapper.class);
+        secureConfig.register(new TransactionWrapper(transactionService));
+//        secureConfig.register(urlRewriteFilter);
+        if (application instanceof BinderProvider) {
+            secureConfig.register(((BinderProvider) application).getBinder());
+        }
+        EncodingFilter.enableFor(secureConfig, GZipEncoder.class); // TODO deflate also
+        try (ContextClassLoaderResource ctx = ContextClassLoaderResource.of(application.getClass())) {
+            ServletContainer container = new ServletContainer(secureConfig);
+//            HttpServlet wrapper = new EventServletWrapper(new ServletWrapper(container, threadPrincipalService), this);
+            httpService.registerServlet(alias, container, null, null);
+        } catch (Exception e) {
+            endPointConfiguration.log("Error while registering " + alias + ": " + e.getMessage(), e);
+        }
+
     }
 
     @Override
     public void stop() {
         if (this.isPublished()) {
-//            application.stop();
+            httpService.unregister(alias);
+            alias = null;
             application = null;
         } else {
             throw new IllegalStateException("Service already stopped");
