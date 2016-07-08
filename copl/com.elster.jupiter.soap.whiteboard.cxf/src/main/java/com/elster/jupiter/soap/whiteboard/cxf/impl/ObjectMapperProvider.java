68eeb523f799729cbe0764c3d055b45f2ed2b6a4
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-07-08 08:04:37 +0200

    CXO-1676: Simplified ObjectMapper

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/ObjectMapperProvider.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/ObjectMapperProvider.java
@@ -1,23 +1,13 @@
 package com.elster.jupiter.soap.whiteboard.cxf.impl;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.AnnotationIntrospector;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
 import javax.ws.rs.ext.ContextResolver;
-import java.io.IOException;
-import java.time.Instant;
 
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
 
@@ -30,41 +20,10 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
         AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         mapper.setAnnotationIntrospector(pair);
-        mapper.registerModule(new SimpleModule("Instant", new Version(1, 0, 0, null, "com.elster.jupiter", "soap.whiteboard"))
-                .addSerializer(new InstantSerializer())
-                .addDeserializer(Instant.class, new InstantDeserializer()));
     }
 
     @Override
     public ObjectMapper getContext(Class<?> arg0) {
         return mapper;
-    }
-
-    private static class InstantSerializer extends StdSerializer<Instant> {
-
-        protected InstantSerializer() {
-            super(Instant.class);
-        }
-
-        @Override
-        public void serialize(Instant instant, JsonGenerator generator, SerializerProvider provider) throws
-                IOException {
-            generator.writeNumber(instant.toEpochMilli());
-        }
-
-    }
-
-    private static class InstantDeserializer extends StdDeserializer<Instant> {
-
-        private static final long serialVersionUID = 1L;
-
-        protected InstantDeserializer() {
-            super(Instant.class);
-        }
-
-        @Override
-        public Instant deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-            return Instant.ofEpochMilli(parser.getLongValue());
-        }
     }
 }
