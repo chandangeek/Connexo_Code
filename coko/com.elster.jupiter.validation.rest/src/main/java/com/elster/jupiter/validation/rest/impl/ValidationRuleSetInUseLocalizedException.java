a64dc026524c2f6feb8dfabf2685184012eb7a7f
Maksim Barinov <mbarinov@mera.ru>
2016-07-11 12:12:20 +0300

    CXO-2292

---
+++ coko/com.elster.jupiter.validation.rest/src/main/java/com/elster/jupiter/validation/rest/impl/ValidationRuleSetInUseLocalizedException.java
@@ -0,0 +1,12 @@
+package com.elster.jupiter.validation.rest.impl;
+
+import com.elster.jupiter.nls.LocalizedException;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.validation.ValidationRuleSet;
+
+public class ValidationRuleSetInUseLocalizedException extends LocalizedException {
+
+    public ValidationRuleSetInUseLocalizedException(Thesaurus thesaurus, ValidationRuleSet validationRuleSet) {
+        super(thesaurus, MessageSeeds.RULE_SET_IN_USE, validationRuleSet.getName());
+    }
+}
