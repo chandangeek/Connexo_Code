9e2c4f496bed3944578f14717f82cc2734d381b8
Sentyurin Sergey <ssentyur@mera.ru>
2016-07-12 13:49:29 +0300

    fix merge

--- comu/com.energyict.mdc.usagepoint.data.rest/src/main/java/com.energyict.mdc.usagepoint.data.rest/impl/UsagePointResource.java
+++ comu/com.energyict.mdc.usagepoint.data.rest/src/main/java/com.energyict.mdc.usagepoint.data.rest/impl/UsagePointResource.java
@@ -9,8 +9,8 @@ import com.elster.jupiter.rest.util.JsonQueryFilter;
 import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.elster.jupiter.rest.util.PagedInfoList;
 import com.elster.jupiter.rest.util.Transactional;
-import com.elster.jupiter.util.streams.Functions;
 import com.elster.jupiter.util.Ranges;
+import com.elster.jupiter.util.streams.Functions;
 import com.energyict.mdc.common.services.ListPager;
 
 import com.google.common.collect.Range;
