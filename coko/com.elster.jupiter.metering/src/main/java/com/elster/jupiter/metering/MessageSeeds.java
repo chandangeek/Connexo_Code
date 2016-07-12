5e17897432e98f0a647b8a6f51618fe25a12de0f
Sentyurin Sergey <ssentyur@mera.ru>
2016-07-12 12:43:01 +0300

    CXO-2042: draft version

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/MessageSeeds.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/MessageSeeds.java
@@ -30,6 +30,7 @@ public enum MessageSeeds implements MessageSeed {
     INVALID_UNIT(2016, Constants.INVALID_UNIT, "Invalid unit", Level.SEVERE),
     DUPLICATE_USAGEPOINT(3001, Constants.DUPLICATE_USAGEPOINT, "MRID must be unique", Level.SEVERE),
     NO_USAGE_POINT_FOR_MRID(2, Constants.NO_USAGE_POINT_FOR_MRID, "No usage point with MRID {0}"),
+    NO_CHANNEL_WITH_ID(2, Constants.NO_CHANNEL_WITH_ID, "No channel with id {0}"),
 
     REQUIRED(4001, Constants.REQUIRED, "This field is required"),
     FAIL_MANAGE_CPS_ON_ACTIVE_METROLOGY_CONFIGURATION(4002, Constants.FAIL_MANAGE_CPS_ON_ACTIVE_METROLOGY_CONFIGURATION, "You cannot manage custom attribute sets because metrology configuration is active."),
@@ -157,6 +158,7 @@ public enum MessageSeeds implements MessageSeed {
         public static final String INVALID_MULTIPLIER = "invalidMultiplier";
         public static final String INVALID_UNIT = "invalidUnit";
         public static final String NO_USAGE_POINT_FOR_MRID = "NoUsagePointForMRID";
+        public static final String NO_CHANNEL_WITH_ID = "no.channel.for.id";
         public static final String NO_SUCH_LOCATION = "no.such.location";
         public static final String DUPLICATE_LOCATION_ENTRY = "duplicate.address.entry";
         public static final String FAILED_TO_DEACTIVATE_METROLOGY_CONFIGURATION = "failed.to.deactivate.metrology.configuration";
