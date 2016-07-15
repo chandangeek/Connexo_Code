a9c64b642667f320c5e7c654074143232e513148
Sentyurin Sergey <ssentyur@mera.ru>
2016-07-15 15:20:30 +0300

    CXO-2042: add flow unit

---
+++ coko/com.elster.jupiter.cbo/src/main/java/com/elster/jupiter/cbo/ReadingTypeUnitConversion.java
@@ -0,0 +1,53 @@
+package com.elster.jupiter.cbo;
+
+import java.util.EnumSet;
+
+public class ReadingTypeUnitConversion {
+    private enum FlowUnit {
+        CUBICMETERPERHOUR("CUBICMETERPERHOUR"),
+        NORMALCUBICMETERPERHOUR("NORMALCUBICMETERPERHOUR"),
+        JOULEPERHOUR("JOULEPERHOUR"),
+        PASCALPERHOUR("PASCALPERHOUR"),
+        VOLTSQUARE("VOLTSQUARE"),
+        QUANTITYPOWER("QUANTITYPOWER"),
+        AMPERESQUARE("AMPERESQUARE"),
+        WATT("WATT"),
+        VOLTAMPERE("VOLTAMPERE"),
+        VOLTAMPEREREACTIVE("VOLTAMPEREREACTIVE"),
+        PERHOUR("PERHOUR"),
+        KILOGRAMPERHOUR("KILOGRAMPERHOUR"),
+        TONPERHOUR("TONPERHOUR"),
+        LITERPERHOUR("LITERPERHOUR"),
+        GALLONPERHOUR("GALLONPERHOUR"),
+        US_GALLONPERHOUR("US_GALLONPERHOUR"),
+        IMP_GALLONPERHOUR("IMP_GALLONPERHOUR"),
+        CUBICINCHPERHOUR("CUBICINCHPERHOUR"),
+        CUBICYARDPERHOUR("CUBICYARDPERHOUR"),
+        ACREFEETPERHOUR("ACREFEETPERHOUR");
+
+        private String name;
+
+        private FlowUnit(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public ReadingTypeUnitConversion() {
+
+    }
+
+    public static boolean isFlowUnit(ReadingTypeUnit readingTypeUnit) {
+        EnumSet<FlowUnit> flowUnits = EnumSet.allOf(FlowUnit.class);
+        for (FlowUnit flowUnit : flowUnits) {
+            if (flowUnit.getName().equals(readingTypeUnit.name())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
