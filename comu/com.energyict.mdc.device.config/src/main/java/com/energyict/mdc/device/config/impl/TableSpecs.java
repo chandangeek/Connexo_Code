b312712a39851d4d73ffd1bed9913b32767f3368
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-07-19 10:09:38 +0200

    Proper fix for CXO-2399 since ORM uses that of the primary key constraint and suffixes it with _JRNL I need to make sure that the length of the name of the primary key <= 25

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/TableSpecs.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/TableSpecs.java
@@ -373,7 +373,7 @@ public enum TableSpecs {
             Column deviceConfiguration = table.column("DEVICECONFIGURATION").number().notNull().add();
             table.column("DEVICEPROTOCOLDIALECT").varChar().notNull().map("protocolDialectName").add();
             Column nameColumn = table.column("NAME").varChar().notNull().map("name").add();
-            table.setJournalTableName("DTC_DIALECTCONFIGPROPSJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_DIALECT_CONFIG_PROPSJRNL").since(version(10, 2));
             table.addAuditColumns();
             table
                 .foreignKey("FK_DTC_DIALECTCONFPROPS_CONFIG")
@@ -399,14 +399,14 @@ public enum TableSpecs {
             table.addAuditColumns();
             table.column("VALUE").varChar(4000).notNull().map("value").add();
             table
-                .foreignKey("FK_DTC_CONFPROPSATTR_PROPS")
+                .foreignKey("DTC_DIALECTCONFIGPROPSATTRJRNL")
                 .on(id)
                 .references(DTC_DIALECTCONFIGPROPERTIES.name())
                 .map("properties")
                 .composition()
                 .reverseMap("propertyList")
                 .add();
-            table.primaryKey("PK_DTC_DIALECTCONFIGPROPSATTR").on(id, name).add();
+            table.primaryKey("PK_DTC_DIALCTCFGPROPSATTR").on(id, name).add();
         }
     },
 
@@ -418,7 +418,7 @@ public enum TableSpecs {
             Column deviceConfiguration = table.column("DEVICECONFIGURATION").number().notNull().add();
             Column name = table.column("NAME").varChar().notNull().map("name").add();
             table.column("VALUE").varChar().notNull().map("value").add();
-            table.setJournalTableName("DTC_PRTCLCONFPROPSATTRJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_PRTCLCONFIGPROPSATTRJRNL").since(version(10, 2));
             table.addAuditColumns();
             table.foreignKey("FK_DTC_PROTCONFPROPSATTR_CONF")
                     .on(deviceConfiguration)
@@ -427,7 +427,7 @@ public enum TableSpecs {
                     .composition()
                     .reverseMap("protocolProperties")
                     .add();
-            table.primaryKey("PK_DTC_PROTOCOLCONFPROPSATTR").on(deviceConfiguration, name).add();
+            table.primaryKey("PK_DTC_PRTCLCONFPROPSATTR").on(deviceConfiguration, name).add();
         }
     },
 
@@ -451,7 +451,7 @@ public enum TableSpecs {
             table.column("RESCHEDULERETRYDELAY").number().conversion(NUMBER2INT).map("rescheduleRetryDelay.count").add();
             Column comportpool = table.column("COMPORTPOOL").number().add();
             table.column("RESCHEDULERETRYDELAYCODE").number().conversion(NUMBER2INT).map("rescheduleRetryDelay.timeUnitCode").add();
-            table.setJournalTableName("DTC_PARTIALCONNTASKJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_PARTIALCONNECTIONTASKJRNL").since(version(10, 2));
             table.addAuditColumns();
             table.primaryKey("PK_DTC_PARTIALCONNTASK").on(id).add();
             table.foreignKey("FK_DTC_PARTIALCT_PLUGGABLE")
@@ -493,7 +493,7 @@ public enum TableSpecs {
             Column partialconnectiontask = table.column("PARTIALCONNECTIONTASK").number().notNull().add();
             Column name = table.column("NAME").varChar().notNull().map("name").add();
             table.column("VALUE").varChar().notNull().map("value").add();
-            table.setJournalTableName("DTC_PARTIALCONNTASKPROPSJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_PARTIAL_CONNTASK_PROPSJRNL").since(version(10, 2));
             table.addAuditColumns();
             table.foreignKey("FK_DTC_PARTIALCTPROPS_TASK")
                     .on(partialconnectiontask)
@@ -502,7 +502,7 @@ public enum TableSpecs {
                     .reverseMap("properties")
                     .composition()
                     .add();
-            table.primaryKey("PK_DTC_PARTIALCONTASKPROPS").on(partialconnectiontask, name).add();
+            table.primaryKey("PK_DTC_PARTIALCONTSKPROPS").on(partialconnectiontask, name).add();
         }
     },
 
@@ -543,7 +543,7 @@ public enum TableSpecs {
                     .composition()
                     .map("deviceMessageEnablement")
                     .add();
-            table.primaryKey("PK_DTC_MESENABLEUSERACTION").on(useraction, deviceMessageEnablement).add();
+            table.primaryKey("PK_DTC_MSGENABLEUSRACTION").on(useraction, deviceMessageEnablement).add();
         }
     },
 
@@ -569,7 +569,7 @@ public enum TableSpecs {
                     .reverseMap(DeviceConfigurationImpl.Fields.SECURITY_PROPERTY_SETS.fieldName())
                     .composition()
                     .add();
-            table.primaryKey("PK_DTC_SECURITYPROPERTYSET").on(id).add();
+            table.primaryKey("PK_DTC_SECURITYPROPSET").on(id).add();
             table.unique("UK_DTC_SECPROPSET_NAME").on(deviceConfiguration, name).add();
         }
     },
@@ -581,7 +581,7 @@ public enum TableSpecs {
             table.map(SecurityPropertySetImpl.UserActionRecord.class);
             Column useraction = table.column("USERACTION").number().conversion(NUMBER2ENUM).notNull().map("userAction").add();
             Column securitypropertyset = table.column("SECURITYPROPERTYSET").number().notNull().add();
-            table.setJournalTableName("DTC_SECPROPSETUSRACTIONJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_SEC_PROP_SET_USRACTIONJRNL").since(version(10, 2));
             table.addAuditColumns();
             table.foreignKey("FK_DTC_SECPROPSETUSRACT_SPS")
                     .on(securitypropertyset)
@@ -590,7 +590,7 @@ public enum TableSpecs {
                     .composition()
                     .map("set")
                     .add();
-            table.primaryKey("PK_DTC_SECPROPSETUSERACTION").on(useraction, securitypropertyset).add();
+            table.primaryKey("PK_DTC_SECPROPSETUSRACTN").on(useraction, securitypropertyset).add();
         }
     },
 
@@ -664,7 +664,7 @@ public enum TableSpecs {
                             .conversion(NUMBER2LONG)
                             .map("deviceConfigurationId")
                             .add();
-            table.setJournalTableName("DTC_DEVCFGVALRULESETUSEJRNL");
+            table.setJournalTableName("DTC_DEVCFGVALRULESETUSAGEJRNL");
             table.addAuditColumns();
 
             table.primaryKey("DTC_PK_SETCONFIGUSAGE").on(validationRuleSetIdColumn, deviceConfigurationIdColumn).add();
@@ -878,7 +878,7 @@ public enum TableSpecs {
             Column deviceType = table.column("DEVICETYPE").number().notNull().add();
             Column calendar = table.column("CALENDAR").number().add();
             table.column("NAME").varChar().map(AllowedCalendarImpl.Fields.NAME.fieldName()).add();
-            table.setJournalTableName("DTC_DEVICETYPECALUSAGEJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_DEVICETYPE_CAL_USAGEJRNL").since(version(10, 2));
             table.addAuditColumns();
             table.primaryKey("PK_DTC_CALUSAGE").on(id).add();
             table.foreignKey("FK_DTC_CALDEVICETYPE")
@@ -902,7 +902,7 @@ public enum TableSpecs {
             table.map(TimeOfUseOptionsImpl.class);
             Column deviceTypeColumn = table.column("DEVICETYPE").number().notNull().add();
             table.column("OPTIONS").number().map(TimeOfUseOptionsImpl.Fields.OPTION_BITS.fieldName()).conversion(NUMBER2LONG).notNull().add();
-            table.setJournalTableName("DTC_TOU_MNGMNTOPTIONSJRNL").since(version(10, 2));
+            table.setJournalTableName("DTC_TOU_MANAGEMENTOPTIONSJRNL").since(version(10, 2));
             table.addAuditColumns( );
             table.primaryKey("DTC_PK_TIMEOFUSEOPTIONS").on(deviceTypeColumn).add();
             table.foreignKey("DTC_TOUOPTIONS_FK_DEVICETYPE")
