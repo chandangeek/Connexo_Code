9201c0f9706ca71c09d8f02708383b61d10ebfb5
Anton Fomchenko <antfom@mera.ru>
2016-07-21 10:06:19 +0300

    CXO-141 integration with CompletionOptions

--- coko/com.elster.jupiter.kore.api.rest/src/main/java/com/elster/jupiter/kore/api/impl/servicecall/UsagePointCommandMessageHandler.java
+++ coko/com.elster.jupiter.kore.api.rest/src/main/java/com/elster/jupiter/kore/api/impl/servicecall/UsagePointCommandMessageHandler.java
@@ -2,6 +2,7 @@ package com.elster.jupiter.kore.api.impl.servicecall;
 
 import com.elster.jupiter.messaging.Message;
 import com.elster.jupiter.messaging.subscriber.MessageHandler;
+import com.elster.jupiter.metering.ami.CompletionMessageInfo;
 import com.elster.jupiter.servicecall.DefaultState;
 import com.elster.jupiter.servicecall.ServiceCall;
 import com.elster.jupiter.servicecall.ServiceCallService;
@@ -9,9 +10,11 @@ import com.elster.jupiter.util.json.JsonService;
 
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class UsagePointCommandMessageHandler implements MessageHandler {
 
+    private Logger logger = Logger.getLogger(UsagePointCommandMessageHandler.class.getName());
     private final ServiceCallService serviceCallService;
     private final JsonService jsonService;
 
@@ -23,7 +26,17 @@ public class UsagePointCommandMessageHandler implements MessageHandler {
     @Override
     public void process(Message message) {
         Map<?, ?> map = jsonService.deserialize(message.getPayload(), Map.class);
-        ServiceCall serviceCall = serviceCallService.getServiceCall((Integer)map.get("id")).get();
+        try {
+            if (map.get("correlationId") != null && map.get("completionMessageStatus") != null) {
+                serviceCallService.getServiceCall(Integer.valueOf(map.get("correlationId").toString()))
+                        .ifPresent(serviceCall -> process(serviceCall, map.get("completionMessageStatus").toString()));
+            }
+        } catch (NumberFormatException e) {
+            this.logger.fine(() -> "Unable to parse service call id '" + map.get("correlationId"));
+        }
+    }
+
+    private void process(ServiceCall serviceCall, String completionMessageStatus) {
         UsagePointCommandDomainExtension extension = serviceCall.getExtension(UsagePointCommandDomainExtension.class)
                 .orElseThrow(() -> new IllegalStateException("Unable to get domain extension for service call"));
 
@@ -31,7 +44,7 @@ public class UsagePointCommandMessageHandler implements MessageHandler {
         BigDecimal failedCommands = extension.getActualNumberOfFailedCommands();
         BigDecimal expectedCommands = extension.getExpectedNumberOfCommands();
 
-        if((Boolean) map.get("success")){
+        if (CompletionMessageInfo.CompletionMessageStatus.SUCCESS.name().equals(completionMessageStatus)) {
             successfulCommands = successfulCommands.add(BigDecimal.ONE);
             extension.setActualNumberOfSuccessfulCommands(successfulCommands);
         } else {
@@ -41,11 +54,11 @@ public class UsagePointCommandMessageHandler implements MessageHandler {
         serviceCall.update(extension);
 
         if (extension.getExpectedNumberOfCommands().compareTo(successfulCommands.add(failedCommands)) <= 0) {
-            if (successfulCommands.compareTo(expectedCommands) >= 0) {
+            if (successfulCommands.compareTo(expectedCommands) >= 0 && serviceCall.canTransitionTo(DefaultState.SUCCESSFUL)) {
                 serviceCall.requestTransition(DefaultState.SUCCESSFUL);
-            } else if (failedCommands.compareTo(expectedCommands) >= 0) {
+            } else if (failedCommands.compareTo(expectedCommands) >= 0 && serviceCall.canTransitionTo(DefaultState.FAILED)) {
                 serviceCall.requestTransition(DefaultState.FAILED);
-            } else {
+            } else if (serviceCall.canTransitionTo(DefaultState.PARTIAL_SUCCESS)) {
                 serviceCall.requestTransition(DefaultState.PARTIAL_SUCCESS);
             }
         }
