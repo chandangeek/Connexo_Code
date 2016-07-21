f1b9dc20560c87ebad6a8b96a22723e59cca2c62
Maksim Barinov <mbarinov@mera.ru>
2016-07-21 15:33:31 +0300

    CXO-1881

---
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/PurposeActionsMenu.js
@@ -0,0 +1,16 @@
+Ext.define('Imt.purpose.view.PurposeActionsMenu', {
+    extend: 'Ext.menu.Menu',
+    alias: 'widget.purpose-actions-menu',
+    plain: true,
+    border: false,
+    shadow: false,
+
+    items: [
+        {
+            text: Uni.I18n.translate('general.validateNow', 'IMT', 'Validate now'),
+            action: 'validateNow',
+            itemId: 'validate-now'
+        }
+    ]
+});
+
--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointmanagement/model/Purpose.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointmanagement/model/Purpose.js
@@ -1,11 +1,19 @@
 Ext.define('Imt.usagepointmanagement.model.Purpose', {
-    extend: 'Ext.data.Model',
+    extend: 'Uni.model.Version',
     requires: [],
     fields: [
         {name: 'id', type: 'int'},
         {name: 'name', type: 'string'},
         {name: 'required', type: 'boolean', useNull: true},
         {name: 'active', type: 'boolean', useNull: true},
-        {name: 'status', type: 'auto', useNull: true}
-    ]
+        {name: 'status', type: 'auto', useNull: true},
+        {name: 'lastChecked', type: 'auto', useNull: true}
+    ],
+    proxy: {
+        type: 'rest',
+        url: '/api/udr/usagepoints/{mRID}/purposes',
+        reader: {
+            type: 'json'            
+        }        
+    }
 });
