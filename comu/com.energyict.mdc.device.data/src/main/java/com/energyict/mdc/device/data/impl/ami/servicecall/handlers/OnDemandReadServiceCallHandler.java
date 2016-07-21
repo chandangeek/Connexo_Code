fa939d45f2c8900aa5270b9c76f738ebaf2bceb7
Anton Fomchenko <antfom@mera.ru>
2016-07-21 10:07:44 +0300

    CXO-141 integration with CompletionOptions

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/ami/servicecall/handlers/OnDemandReadServiceCallHandler.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/ami/servicecall/handlers/OnDemandReadServiceCallHandler.java
@@ -1,6 +1,8 @@
 package com.energyict.mdc.device.data.impl.ami.servicecall.handlers;
 
+import com.elster.jupiter.messaging.DestinationSpec;
 import com.elster.jupiter.messaging.MessageService;
+import com.elster.jupiter.metering.ami.CompletionMessageInfo;
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
@@ -10,6 +12,7 @@ import com.elster.jupiter.servicecall.ServiceCall;
 import com.elster.jupiter.servicecall.ServiceCallHandler;
 import com.energyict.mdc.device.data.DeviceDataServices;
 import com.energyict.mdc.device.data.DeviceService;
+import com.energyict.mdc.device.data.ami.CompletionOptionsCallBack;
 import com.energyict.mdc.device.data.impl.ami.servicecall.CommandCustomPropertySet;
 import com.energyict.mdc.device.data.impl.ami.servicecall.CommandServiceCallDomainExtension;
 import com.energyict.mdc.device.data.impl.ami.servicecall.CompletionOptionsCustomPropertySet;
@@ -18,6 +21,7 @@ import com.energyict.mdc.device.data.impl.ami.servicecall.CompletionOptionsServi
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import java.text.MessageFormat;
 import java.util.Optional;
 
 @Component(name = "com.energyict.servicecall.ami.on.demand.read.handler",
@@ -29,34 +33,20 @@ public class OnDemandReadServiceCallHandler implements ServiceCallHandler {
     public static final String VERSION = "v1.0";
     public static final String SERVICE_CALL_HANDLER_NAME = "OnDemandReadServiceCallHandler";
 
-    private volatile MessageService messageService;
-    private volatile Thesaurus thesaurus;
-    private volatile DeviceService deviceService;
+    private volatile CompletionOptionsCallBack completionOptionsCallBack;
 
     public OnDemandReadServiceCallHandler() {
         super();
     }
 
     // Constructor only to be used in JUnit tests
-    public OnDemandReadServiceCallHandler(MessageService messageService, DeviceService deviceService, Thesaurus thesaurus) {
-        this.messageService = messageService;
-        this.deviceService = deviceService;
-        this.thesaurus = thesaurus;
+    public OnDemandReadServiceCallHandler(CompletionOptionsCallBack completionOptionsCallBack) {
+        this.completionOptionsCallBack = completionOptionsCallBack;
     }
 
     @Reference
-    public void setMessageService(MessageService messageService) {
-        this.messageService = messageService;
-    }
-
-    @Reference
-    public void setDeviceService(DeviceService deviceService) {
-        this.deviceService = deviceService;
-    }
-
-    @Reference
-    public void setNlsService(NlsService nlsService) {
-        this.thesaurus = nlsService.getThesaurus(DeviceDataServices.COMPONENT_NAME, Layer.DOMAIN);
+    public void setCompletionOptionsCallBack(CompletionOptionsCallBack completionOptionsCallBack) {
+        this.completionOptionsCallBack = completionOptionsCallBack;
     }
 
     @Override
@@ -64,25 +54,16 @@ public class OnDemandReadServiceCallHandler implements ServiceCallHandler {
         serviceCall.log(LogLevel.FINE, "Now entering state " + newState.getDefaultFormat());
         switch (newState) {
             case SUCCESSFUL:
-                sendFinishedMessageToDestinationSpec(serviceCall);
+                completionOptionsCallBack.sendFinishedMessageToDestinationSpec(serviceCall);
                 break;
             case FAILED:
-                sendFinishedMessageToDestinationSpec(serviceCall);
-                break;
             case PARTIAL_SUCCESS:
-                sendFinishedMessageToDestinationSpec(serviceCall);
+            case CANCELLED:
+                completionOptionsCallBack.sendFinishedMessageToDestinationSpec(serviceCall, CompletionMessageInfo.CompletionMessageStatus.FAILURE, CompletionMessageInfo.FailureReason.ONE_OR_MORE_DEVICE_COMMANDS_FAILED);
                 break;
             default:
                 // No specific action required for these states
                 break;
-        }
-    }
-
-    protected void sendFinishedMessageToDestinationSpec(ServiceCall serviceCall) {
-        Optional<CompletionOptionsServiceCallDomainExtension> extension = serviceCall.getExtensionFor(new CompletionOptionsCustomPropertySet());
-        if (extension.isPresent()) {
-            CompletionOptionsServiceCallDomainExtension domainExtension = extension.get();
-            messageService.getDestinationSpec(domainExtension.getDestinationSpec()).ifPresent(destinationSpec -> destinationSpec.message(domainExtension.getDestinationMessage()).send());
         }
     }
 }
