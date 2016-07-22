2775718511839a15a74c6eb718f776f211e0c621
Anton Fomchenko <antfom@mera.ru>
2016-07-22 14:56:37 +0300

    CXO-2421

---
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ReadingTypeDeliverablesInfo.java
@@ -0,0 +1,25 @@
+package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
+
+import com.elster.jupiter.mdm.usagepoint.config.rest.impl.FormulaInfo;
+import com.elster.jupiter.metering.config.ReadingTypeDeliverable;
+import com.elster.jupiter.metering.rest.ReadingTypeInfo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ReadingTypeDeliverablesInfo {
+    public long id;
+    public String name;
+    public ReadingTypeInfo readingType;
+
+    public static ReadingTypeDeliverablesInfo asInfo(ReadingTypeDeliverable readingTypeDeliverable) {
+        ReadingTypeDeliverablesInfo info = new ReadingTypeDeliverablesInfo();
+        info.id = readingTypeDeliverable.getId();
+        info.name = readingTypeDeliverable.getName();
+        info.readingType = readingTypeDeliverable.getReadingType() != null ? new ReadingTypeInfo(readingTypeDeliverable.getReadingType()) : null;
+        return info;
+    }
+}
