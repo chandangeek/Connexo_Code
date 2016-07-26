89a1f80aaa5349301c0196446857ee85a8f4e320
geert <geert.delmeiren@elster.com>
2016-07-26 17:18:19 +0200

    CXO-2364 - All messages must end with a punctuation mark

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/timeofuseondevice/view/TimeOfUsePlannedOnForm.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/timeofuseondevice/view/TimeOfUsePlannedOnForm.js
@@ -55,9 +55,9 @@ Ext.define('Mdc.timeofuseondevice.view.TimeOfUsePlannedOnForm', {
         if (!willBePickedUpByPlannedComtask || !willBePickedUpByComtask) {
             me.down('#willNotBePickedUpMessage').show();
             if (!willBePickedUpByPlannedComtask && willBePickedUpByComtask) {
-                me.down('#willNotBePickedUpMessage').setText(Uni.I18n.translate('deviceCommands.view.willNotBePickedUpByPlannedComTask', 'MDC', 'This command is part of a communication task that is not planned and will not be picked up'));
+                me.down('#willNotBePickedUpMessage').setText(Uni.I18n.translate('deviceCommand.willNotBePickedUpByPlannedComTask', 'MDC', 'This command is part of a communication task that is not planned and will not be picked up.'));
             } else {
-                me.down('#willNotBePickedUpMessage').setText(Uni.I18n.translate('deviceCommands.view.willBePickedUpByComTask', 'MDC', 'This command is not part of a communication task on this device'));
+                me.down('#willNotBePickedUpMessage').setText(Uni.I18n.translate('deviceCommand.willBePickedUpByComTask', 'MDC', 'This command is not part of a communication task on this device.'));
             }
         }
     }
