27d2250271b3b37a73b17c4615a7c640d5ee34c7
Yan R <yaratay@mera.ru>
2016-07-26 15:42:48 +0300

    Revert "CXO-2430 Links to output data page are not taken filters into account" No way to apply filter on period in the link => no need in sending it in response
    This reverts commit cd220122535ce2311a6d672a398c8c4af92f62bd.

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ChannelDataValidationSummaryInfo.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ChannelDataValidationSummaryInfo.java
@@ -1,22 +1,17 @@
 package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
 
-import java.time.Instant;
 import java.util.List;
 
 public class ChannelDataValidationSummaryInfo {
     public long id;
     public String name;
-    public Instant intervalStart;
-    public Instant intervalEnd;
     public int total;
     public List<ChannelDataValidationSummaryFlagInfo> statistics;
 
-    ChannelDataValidationSummaryInfo(long id, String name, Instant intervalStart, Instant intervalEnd, int total,
+    ChannelDataValidationSummaryInfo(long id, String name, int total,
                                      List<ChannelDataValidationSummaryFlagInfo> statistics) {
         this.id = id;
         this.name = name;
-        this.intervalStart = intervalStart;
-        this.intervalEnd = intervalEnd;
         this.total = total;
         this.statistics = statistics;
     }
--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ChannelDataValidationSummaryInfoFactory.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ChannelDataValidationSummaryInfoFactory.java
@@ -5,10 +5,7 @@ import com.elster.jupiter.mdm.usagepoint.data.ChannelDataValidationSummaryFlag;
 import com.elster.jupiter.metering.config.ReadingTypeDeliverable;
 import com.elster.jupiter.nls.Thesaurus;
 
-import com.google.common.collect.Range;
-
 import javax.inject.Inject;
-import java.time.Instant;
 import java.util.stream.Collectors;
 
 public class ChannelDataValidationSummaryInfoFactory {
@@ -21,13 +18,8 @@ public class ChannelDataValidationSummaryInfoFactory {
 
     ChannelDataValidationSummaryInfo from(ReadingTypeDeliverable deliverable,
                                           ChannelDataValidationSummary summary) {
-        Range<Instant> interval = summary.getTargetInterval();
         return new ChannelDataValidationSummaryInfo(deliverable.getId(),
                 deliverable.getName(),
-                interval.hasLowerBound() ? interval.lowerEndpoint() : null, // should be present normally,
-                                                                            // at least channels container start
-                interval.hasUpperBound() ? interval.upperEndpoint() : null, // should be present normally,
-                                                                            // at most "now"
                 summary.getSum(),
                 summary.getValues().entrySet().stream()
                         .map(flagEntry -> {
