f53612ee9095a179855871c49208c94c31b4ddca
Sentyurin Sergey <ssentyur@mera.ru>
2016-07-26 10:35:59 +0300

    merge master

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/Main.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/Main.js
@@ -97,7 +97,6 @@ Ext.define('Mdc.controller.Main', {
         'Mdc.controller.setup.DeviceTransitionExecute',
         'Mdc.controller.setup.DeviceAttributes',
         'Mdc.usagepointmanagement.controller.UsagePoint',
-        'Mdc.usagepointmanagement.controller.History',
         'Mdc.usagepointmanagement.controller.ViewChannelsList',
         'Mdc.controller.setup.DeviceConflictingMapping',
         'Mdc.devicetypecustomattributes.controller.AttributeSets',
