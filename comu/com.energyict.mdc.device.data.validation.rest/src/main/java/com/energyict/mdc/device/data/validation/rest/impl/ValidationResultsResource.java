127e6b0b7585f4d0b3692bf986fcb1056bae5f4d
iancud <dragos.iancu@elster.com>
2016-07-27 20:00:02 +0300

    CXO-1611 - kpi members - validators show

--- comu/com.energyict.mdc.device.data.validation.rest/src/main/java/com/energyict/mdc/device/data/validation/rest/impl/ValidationResultsResource.java
+++ comu/com.energyict.mdc.device.data.validation.rest/src/main/java/com/energyict/mdc/device/data/validation/rest/impl/ValidationResultsResource.java
@@ -23,7 +23,9 @@ import javax.ws.rs.core.UriInfo;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,6 +33,39 @@ import java.util.stream.Stream;
 public class ValidationResultsResource {
 
     private final DeviceDataValidationService deviceDataValidationService;
+
+    private enum Validator {
+        THRESHOLDVALIDATOR("thresholdViolation"),
+        MISSINGVALUESVALIDATOR("checkMissing"),
+        READINGQUALITIESVALIDATOR("intervalState"),
+        REGISTERINCREASEVALIDATOR("registerIncrease");
+
+        private final String elementAbbreviation;
+
+        public String getElementAbbreviation() {
+            return elementAbbreviation;
+        }
+
+        Validator(String elementName) {
+            this.elementAbbreviation = elementName;
+        }
+
+
+        private static final Map<String, Boolean> stringToEnum
+                = new HashMap<>();
+
+        static void refreshValidatorValues() {
+            Stream.of(values()).forEach(e -> stringToEnum.put(e.elementAbbreviation, false));
+        }
+
+        static {
+            refreshValidatorValues();
+        }
+
+        public static Boolean fromAbbreviation(String abbreviation) {
+            return stringToEnum.get(abbreviation);
+        }
+    }
 
     @Inject
     public ValidationResultsResource(DeviceDataValidationService deviceDataValidationService) {
@@ -43,9 +78,12 @@ public class ValidationResultsResource {
     @Path("/devicegroups")
     @RolesAllowed({Privileges.Constants.VIEW_VALIDATION_CONFIGURATION, Privileges.Constants.VALIDATE_MANUAL, Privileges.Constants.ADMINISTRATE_VALIDATION_CONFIGURATION, Privileges.Constants.FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE, Privileges.Constants.FINE_TUNE_VALIDATION_CONFIGURATION_ON_DEVICE_CONFIGURATION})
     public PagedInfoList getValidationResultsPerDeviceGroup(@Context UriInfo uriInfo, @BeanParam JsonQueryParameters queryParameters) throws JSONException {
+
         Instant from = null;
         Instant to = null;
         List<Long> deviceGroups = new ArrayList<>();
+        List<Long> amountOfSuspectsList = new ArrayList<>();
+
         JSONArray filters = new JSONArray(uriInfo.getQueryParameters().getFirst("filter"));
         for (int i = 0; i < filters.length(); i++) {
             if (filters.getJSONObject(i).get("property").equals("from")) {
@@ -56,16 +94,39 @@ public class ValidationResultsResource {
             }
             if (filters.getJSONObject(i).get("property").equals("deviceGroups")) {
                 List<String> groups = Stream.of(filters.getJSONObject(i).get("value")).map(Object::toString).collect(Collectors.toList());
-                String[] list = groups.get(0).substring(1, groups.get(0).length() - 1).split(",");
-                Arrays.asList(list).stream().forEach(value -> deviceGroups.add(Long.parseLong(value)));
+                Arrays.asList(groups.get(0).substring(1, groups.get(0).length() - 1).split(",")).stream().forEach(value -> deviceGroups.add(Long.parseLong(value)));
+            }
+            if (filters.getJSONObject(i).get("property").equals("amountOfSuspects")) {
+                String value = filters.getJSONObject(i).get("value").toString();
+                if (value.contains("==")) {
+                    amountOfSuspectsList.add(0L);
+                    amountOfSuspectsList.add(Long.parseLong(value.substring(value.lastIndexOf(":") + 1, value.lastIndexOf("}"))));
+                } else if (value.contains("BETWEEN")) {
+                    Arrays.asList(value.substring(1, value.length() - 1).split(",")).stream().forEach(amount -> amountOfSuspectsList.add(Long.parseLong(amount)));
+                }
+            }
+            if (filters.getJSONObject(i).get("property").equals("validator")) {
+                List<String> validatorList = Stream.of(filters.getJSONObject(i).get("value")).map(Object::toString).collect(Collectors.toList());
+                Arrays.asList(validatorList.get(0).substring(1, validatorList.get(0).length() - 1).replace("\"", "").split(",")).stream().forEach(value -> Validator.stringToEnum.put(value, true));
             }
         }
-        Range<Instant> range =  from != null && to != null ? Range.closed(from, to) : Range.all();
+
+        Range<Instant> range = from != null && to != null ? Range.closed(from, to) : Range.all();
         List<ValidationSummaryInfo> data = deviceGroups.stream().flatMap(groupId ->
                 deviceDataValidationService
                         .getValidationResultsOfDeviceGroup(groupId, range)
                         .stream()
+                        .filter(resultAmt -> amountOfSuspectsList.isEmpty() || (amountOfSuspectsList.get(0) <= resultAmt.getDeviceValidationKpiResults().getAmountOfSuspects()
+                                && resultAmt.getDeviceValidationKpiResults().getAmountOfSuspects() <= amountOfSuspectsList.get(1)))
+                        .filter(validator -> Validator.stringToEnum.entrySet().stream().allMatch(a -> a.getValue() == false) ||
+                                (validator.getDeviceValidationKpiResults().isThresholdValidator() && Validator.fromAbbreviation(Validator.THRESHOLDVALIDATOR.getElementAbbreviation()) ||
+                                        validator.getDeviceValidationKpiResults().isMissingValuesValidator() && Validator.fromAbbreviation(Validator.MISSINGVALUESVALIDATOR.getElementAbbreviation()) ||
+                                        validator.getDeviceValidationKpiResults()
+                                                .isReadingQualitiesValidator() && Validator.fromAbbreviation(Validator.READINGQUALITIESVALIDATOR.getElementAbbreviation()) ||
+                                        validator.getDeviceValidationKpiResults()
+                                                .isRegisterIncreaseValidator() && Validator.fromAbbreviation(Validator.REGISTERINCREASEVALIDATOR.getElementAbbreviation())))
                         .map(ValidationSummaryInfo::new)).distinct().collect(Collectors.toList());
+        Validator.refreshValidatorValues();
         return PagedInfoList.fromPagedList("summary", data, queryParameters);
     }
 
