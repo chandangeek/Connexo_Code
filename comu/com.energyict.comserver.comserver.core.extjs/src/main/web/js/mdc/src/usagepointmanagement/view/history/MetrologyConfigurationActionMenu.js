fd2dff6f553ebab4ebe4f3e83d742e462393c6ae
akuryuk <akuryuk@mera.ru>
2016-07-27 22:18:44 +0300

    CXO-1795 Tooltip Fix

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/usagepointmanagement/view/history/MetrologyConfigurationActionMenu.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/usagepointmanagement/view/history/MetrologyConfigurationActionMenu.js
@@ -26,27 +26,34 @@ Ext.define('Mdc.usagepointmanagement.view.history.MetrologyConfigurationActionMe
         me.callParent(arguments);
     },
 
-    setMenuItems: function (record) {
-        var me = this;
-        if (record) {
-            var edititem = me.down('#action-menu-item-mc-edit'),
-                removeItem = me.down('#action-menu-item-mc-remove');
-            if (record.get('editable')) {
-                removeItem.enable();
-                removeItem.clearTip();
-                edititem.enable();
-                edititem.clearTip();
-            } else {
-                if (record.get('current')) {
+    listeners: {
+        show: {
+            fn: function (menu) {
+                var me = menu,
+                    record = menu.record,
+                    edititem = me.down('#action-menu-item-mc-edit'),
+                    removeItem = me.down('#action-menu-item-mc-remove');
+                if (record.get('editable')) {
+                    removeItem.enable();
+                    removeItem.itemEl.dom.removeAttribute(removeItem.getTipAttr());
                     edititem.enable();
-                    edititem.clearTip();
+                    edititem.itemEl.dom.removeAttribute(edititem.getTipAttr());
                 } else {
-                    edititem.disable();
-                    edititem.setTooltip(Uni.I18n.translate('usagepoint.actionMenu.editQtip', 'MDC', 'Version in the past can\'t be modified'));
+                    if (record.get('current')) {
+                        edititem.enable();
+                        edititem.itemEl.dom.removeAttribute(edititem.getTipAttr());
+                    } else {
+                        edititem.disable();
+                        edititem.setTooltip(Uni.I18n.translate('usagepoint.actionMenu.editQtip', 'MDC', 'Version in the past can\'t be modified'));
+                    }
+                    removeItem.disable();
+                    removeItem.setTooltip(Uni.I18n.translate('usagepoint.actionMenu.removeQtip', 'MDC', 'Future version only can be removed'));
                 }
-                removeItem.disable();
-                removeItem.setTooltip(Uni.I18n.translate('usagepoint.actionMenu.removeQtip', 'MDC', 'Future version only can be removed'));
             }
         }
+    },
+
+    setMenuItems: function (record) {
+        this.record = record;
     }
 });
