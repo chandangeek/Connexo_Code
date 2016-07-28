6489942aa30a237ba093529fd908c8608d8b0d20
iancud <dragos.iancu@elster.com>
2016-07-28 10:25:18 +0300

    CXO-1611 - kpi members - exceptions clear

--- coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/kpi/DataValidationKpiMembers.java
+++ coko/com.elster.jupiter.validation/src/main/java/com/elster/jupiter/validation/impl/kpi/DataValidationKpiMembers.java
@@ -1,4 +1,5 @@
 package com.elster.jupiter.validation.impl.kpi;
+
 import com.elster.jupiter.kpi.KpiEntry;
 import com.elster.jupiter.kpi.KpiMember;
 import com.elster.jupiter.validation.kpi.DataValidationKpiScore;
@@ -8,6 +9,7 @@ import com.google.common.collect.Range;
 import java.math.BigDecimal;
 import java.security.Timestamp;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.EnumSet;
@@ -23,30 +25,47 @@ public class DataValidationKpiMembers {
     DataValidationKpiMembers(List<? extends KpiMember> kpiMembers) {
         super();
         for (KpiMember kpiMember : kpiMembers) {
-            this.kpiMembers.put(MonitoredDataValidationKpiMemberTypes.valueOf(kpiMember.getName().substring(0,kpiMember.getName().indexOf("_"))), kpiMember);
+            this.kpiMembers.put(MonitoredDataValidationKpiMemberTypes.valueOf(kpiMember.getName().substring(0, kpiMember.getName().indexOf("_"))), kpiMember);
         }
     }
 
     Optional<DataValidationKpiScore> getScores(Range<Instant> interval) {
 
-       Map<MonitoredDataValidationKpiMemberTypes, BigDecimal> suspectMap = Stream.of(MonitoredDataValidationKpiMemberTypes.values()).limit(3)
+        Map<MonitoredDataValidationKpiMemberTypes, BigDecimal> suspectMap = Stream.of(MonitoredDataValidationKpiMemberTypes.values()).limit(3)
                 .collect(Collectors.toMap(s -> s, s -> this.kpiMembers.get(s).getScores(interval).stream().map(KpiEntry::getScore)
                         .reduce(BigDecimal.ZERO, BigDecimal::add)));
-        Map<MonitoredDataValidationKpiMemberTypes, BigDecimal> dataValidationStatusMap = Stream.of(MonitoredDataValidationKpiMemberTypes.values()).filter(kpiMemberType -> kpiMemberType.equals(MonitoredDataValidationKpiMemberTypes.ALLDATAVALIDATED))
-                .collect(Collectors.toMap(s -> s, s -> this.kpiMembers.get(s).getScores(interval).stream().map(KpiEntry::getScore).allMatch(a -> a.longValue() == 1) ? BigDecimal.ONE : BigDecimal.ZERO));
+        Map<MonitoredDataValidationKpiMemberTypes, BigDecimal> dataValidationStatusMap = Stream.of(MonitoredDataValidationKpiMemberTypes.values())
+                .filter(kpiMemberType -> kpiMemberType.equals(MonitoredDataValidationKpiMemberTypes.ALLDATAVALIDATED))
+                .collect(Collectors.toMap(s -> s, s -> this.kpiMembers.get(s)
+                        .getScores(interval)
+                        .stream()
+                        .map(KpiEntry::getScore)
+                        .allMatch(a -> a.longValue() == 1) ? BigDecimal.ONE : BigDecimal.ZERO));
         Map<MonitoredDataValidationKpiMemberTypes, BigDecimal> validatorsMap = Stream.of(MonitoredDataValidationKpiMemberTypes.values()).skip(4)
-               .collect(Collectors.toMap(s -> s, s -> this.kpiMembers.get(s).getScores(interval).stream().map(el -> el.getScore()).parallel().anyMatch(a -> a.longValue() == 1) ? BigDecimal.ONE : BigDecimal.ZERO));
-        Instant timestamp = kpiMembers.entrySet().stream().filter(member -> member.getKey().equals(MonitoredDataValidationKpiMemberTypes.SUSPECT))
-                .map(member -> member.getValue().getScores(interval)).map(list -> list.get(0).getTimestamp()).max(Comparator.naturalOrder()).get();
+                .collect(Collectors.toMap(s -> s, s -> this.kpiMembers.get(s)
+                        .getScores(interval)
+                        .stream()
+                        .map(el -> el.getScore())
+                        .parallel()
+                        .anyMatch(a -> a.longValue() == 1) ? BigDecimal.ONE : BigDecimal.ZERO));
         suspectMap.putAll(dataValidationStatusMap);
         suspectMap.putAll(validatorsMap);
-        return newScore(timestamp, suspectMap);
+        if (suspectMap.entrySet().stream().allMatch(a -> a.getValue() == BigDecimal.ZERO)) {
+            return Optional.empty();
+        } else {
+            Instant timestamp = kpiMembers.entrySet().stream().filter(member -> member.getKey().equals(MonitoredDataValidationKpiMemberTypes.SUSPECT))
+                    .map(member -> member.getValue().getScores(interval))
+                    .flatMap(Collection::stream)
+                    .map(KpiEntry::getTimestamp)
+                    .max(Comparator.naturalOrder()).get();
+            return newScore(timestamp, suspectMap);
+        }
 
     }
 
     private Optional<DataValidationKpiScore> newScore(Instant timestamp, Map<MonitoredDataValidationKpiMemberTypes, BigDecimal> scores) {
         return Optional.of(new DataValidationKpiScoreImpl(
-                timestamp,scores
-                ));
+                timestamp, scores
+        ));
     }
 }
