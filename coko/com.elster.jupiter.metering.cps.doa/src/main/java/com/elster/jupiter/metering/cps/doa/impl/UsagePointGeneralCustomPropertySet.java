c5ef99744095462c100a324ad2e9c1e4aafde3fc
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-08-01 15:05:15 +0200

    CXO-2482: initial version of custom properties

---
+++ coko/com.elster.jupiter.metering.cps.doa/src/main/java/com/elster/jupiter/metering/cps/doa/impl/UsagePointGeneralCustomPropertySet.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2016 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.metering.cps.doa.impl;
+
+import com.elster.jupiter.cps.CustomPropertySet;
+import com.elster.jupiter.cps.EditPrivilege;
+import com.elster.jupiter.cps.PersistenceSupport;
+import com.elster.jupiter.cps.ViewPrivilege;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.orm.Column;
+import com.elster.jupiter.orm.Table;
+import com.elster.jupiter.properties.PropertySelectionMode;
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.PropertySpecService;
+
+import com.google.inject.Module;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Component(name = "com.elster.jupiter.metering.cps.doa.UsagePointGeneral",
+        service = CustomPropertySet.class,
+        property = "name=UsagePointGeneral",
+        immediate = true)
+@SuppressWarnings("unused")
+public class UsagePointGeneralCustomPropertySet implements CustomPropertySet<UsagePoint, UsagePointGeneralDomainExtension> {
+
+    private static final String TABLE_NAME = "DOA_USAGEPOINT_GENERAL";
+    private static final String FK_CPS_DEVICE_GENERAL = "FK_DOA_USAGEPOINT_GENERAL";
+    private static final String COMPONENT_NAME = "DOA";
+
+    private PropertySpecService propertySpecService;
+
+    @Reference
+    @SuppressWarnings("unused") // Called by OSGi framework
+    public void setPropertySpecService(PropertySpecService propertySpecService) {
+        this.propertySpecService = propertySpecService;
+    }
+
+    @Override
+    public String getId() {
+        return "com.elster.jupiter.metering.cps.doa.UsagePointGeneral";
+    }
+
+    @Override
+    public String getName() {
+        return "General";
+    }
+
+    @Override
+    public Class<UsagePoint> getDomainClass() {
+        return UsagePoint.class;
+    }
+
+    @Override
+    public PersistenceSupport<UsagePoint, UsagePointGeneralDomainExtension> getPersistenceSupport() {
+        return new UsagePointGeneralPersistenceSupport();
+    }
+
+    @Override
+    public boolean isRequired() {
+        return false;
+    }
+
+    @Override
+    public boolean isVersioned() {
+        return false;
+    }
+
+    @Override
+    public Set<ViewPrivilege> defaultViewPrivileges() {
+        return EnumSet.allOf(ViewPrivilege.class);
+    }
+
+    @Override
+    public Set<EditPrivilege> defaultEditPrivileges() {
+        return EnumSet.allOf(EditPrivilege.class);
+    }
+
+    @Override
+    public List<PropertySpec> getPropertySpecs() {
+        PropertySpec prepaySpec = propertySpecService
+                .booleanSpec()
+                .named(UsagePointGeneralDomainExtension.Fields.PREPAY.javaName(), "Prepay")
+                .describedAs("For audit trail testing purposes only")
+                .markRequired()
+                .finish();
+        PropertySpec marketCodeSectorSpec = propertySpecService
+                .stringSpec()
+                .named(UsagePointGeneralDomainExtension.Fields.MARKET_CODE_SECTOR.javaName(), "Market code sector")
+                .describedAs("For audit trail testing purposes only")
+                .addValues("Industrial & Commercial", "Domestic")
+                .markExhaustive(PropertySelectionMode.COMBOBOX)
+                .markRequired()
+                .finish();
+        PropertySpec meteringPointTypeSpec = propertySpecService
+                .stringSpec()
+                .named(UsagePointGeneralDomainExtension.Fields.METERING_POINT_TYPE.javaName(), "Metering point type")
+                .describedAs("For audit trail testing purposes only")
+                .addValues("Consumption", "Production", "Combined", "Exchange")
+                .markExhaustive(PropertySelectionMode.COMBOBOX)
+                .markRequired()
+                .finish();
+
+        return Arrays.asList(
+                prepaySpec,
+                marketCodeSectorSpec,
+                meteringPointTypeSpec);
+    }
+
+    private class UsagePointGeneralPersistenceSupport implements PersistenceSupport<UsagePoint, UsagePointGeneralDomainExtension> {
+
+        @Override
+        public String application() {
+            return COMPONENT_NAME;
+        }
+
+        @Override
+        public String componentName() {
+            return COMPONENT_NAME;
+        }
+
+        @Override
+        public String tableName() {
+            return TABLE_NAME;
+        }
+
+        @Override
+        public String domainFieldName() {
+            return UsagePointGeneralDomainExtension.Fields.DOMAIN.javaName();
+        }
+
+        @Override
+        public String domainForeignKeyName() {
+            return FK_CPS_DEVICE_GENERAL;
+        }
+
+        @Override
+        public Class<UsagePointGeneralDomainExtension> persistenceClass() {
+            return UsagePointGeneralDomainExtension.class;
+        }
+
+        @Override
+        public Optional<Module> module() {
+            return Optional.empty();
+        }
+
+        @Override
+        public List<Column> addCustomPropertyPrimaryKeyColumnsTo(Table table) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public void addCustomPropertyColumnsTo(Table table, List<Column> customPrimaryKeyColumns) {
+            table.column(UsagePointGeneralDomainExtension.Fields.PREPAY.databaseName())
+                    .bool()
+                    .map(UsagePointGeneralDomainExtension.Fields.PREPAY.javaName())
+                    .notNull()
+                    .add();
+            table.column(UsagePointGeneralDomainExtension.Fields.MARKET_CODE_SECTOR.databaseName())
+                    .varChar()
+                    .map(UsagePointGeneralDomainExtension.Fields.MARKET_CODE_SECTOR.javaName())
+                    .notNull()
+                    .add();
+            table.column(UsagePointGeneralDomainExtension.Fields.METERING_POINT_TYPE.databaseName())
+                    .varChar()
+                    .map(UsagePointGeneralDomainExtension.Fields.METERING_POINT_TYPE.javaName())
+                    .notNull()
+                    .add();
+        }
+
+        @Override
+        public String columnNameFor(PropertySpec propertySpec) {
+            return EnumSet
+                    .complementOf(EnumSet.of(UsagePointGeneralDomainExtension.Fields.DOMAIN))
+                    .stream()
+                    .filter(each -> each.javaName().equals(propertySpec.getName()))
+                    .findFirst()
+                    .map(UsagePointGeneralDomainExtension.Fields::databaseName)
+                    .orElseThrow(() -> new IllegalArgumentException("Unknown property spec: " + propertySpec.getName()));
+        }
+
+    }
+
+}
---
+++ coko/com.elster.jupiter.metering.cps.doa/src/main/java/com/elster/jupiter/metering/cps/doa/impl/UsagePointGeneralDomainExtension.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2016 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.metering.cps.doa.impl;
+
+import com.elster.jupiter.cps.CustomPropertySetValues;
+import com.elster.jupiter.cps.PersistentDomainExtension;
+import com.elster.jupiter.cps.RegisteredCustomPropertySet;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.orm.Table;
+import com.elster.jupiter.orm.associations.IsPresent;
+import com.elster.jupiter.orm.associations.Reference;
+import com.elster.jupiter.orm.associations.ValueReference;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+class UsagePointGeneralDomainExtension implements PersistentDomainExtension<UsagePoint> {
+    public enum Fields {
+        DOMAIN {
+            @Override
+            public String javaName() {
+                return "usagePoint";
+            }
+        },
+
+        PREPAY {
+            @Override
+            public String javaName() {
+                return "prepay";
+            }
+        },
+
+        MARKET_CODE_SECTOR {
+            @Override
+            public String javaName() {
+                return "marketCodeSector";
+            }
+        },
+
+        METERING_POINT_TYPE {
+            @Override
+            public String javaName() {
+                return "meteringPointType";
+            }
+        };
+
+        public abstract String javaName();
+
+        public String databaseName() {
+            return name();
+        }
+    }
+
+    private Reference<UsagePoint> usagePoint = ValueReference.absent();
+    @IsPresent
+    @SuppressWarnings("unused") // Managed by CustomPropertySetService
+    private Reference<RegisteredCustomPropertySet> registeredCustomPropertySet = Reference.empty();
+
+    @NotNull(message = "{CannotBeNull}")
+    private Boolean prepay;
+    @NotNull(message = "{CannotBeNull}")
+    @Size(max = Table.SHORT_DESCRIPTION_LENGTH, message = "{FieldTooLong}")
+    private String marketCodeSector;
+    @NotNull(message = "{CannotBeNull}")
+    @Size(max = Table.SHORT_DESCRIPTION_LENGTH, message = "{FieldTooLong}")
+    private String meteringPointType;
+
+    UsagePointGeneralDomainExtension() {
+        super();
+    }
+
+    private Boolean getPrepay() {
+        return prepay;
+    }
+
+    private void setPrepay(Boolean prepay) {
+        this.prepay = prepay;
+    }
+
+    private String getMarketCodeSector() {
+        return marketCodeSector;
+    }
+
+    private void setMarketCodeSector(String marketCodeSector) {
+        this.marketCodeSector = marketCodeSector;
+    }
+
+    private String getMeteringPointType() {
+        return meteringPointType;
+    }
+
+    private void setMeteringPointType(String meteringPointType) {
+        this.meteringPointType = meteringPointType;
+    }
+
+    @Override
+    public void copyFrom(UsagePoint domainInstance, CustomPropertySetValues propertyValues, Object... additionalPrimaryKeyValues) {
+        this.usagePoint.set(domainInstance);
+        this.setPrepay((Boolean) propertyValues.getProperty(Fields.PREPAY.javaName()));
+        this.setMarketCodeSector((String) propertyValues.getProperty(Fields.MARKET_CODE_SECTOR.javaName()));
+        this.setMeteringPointType((String) propertyValues.getProperty(Fields.METERING_POINT_TYPE.javaName()));
+    }
+
+    @Override
+    public void copyTo(CustomPropertySetValues propertySetValues, Object... additionalPrimaryKeyValues) {
+        propertySetValues.setProperty(Fields.PREPAY.javaName(), this.getPrepay());
+        propertySetValues.setProperty(Fields.MARKET_CODE_SECTOR.javaName(), this.getMarketCodeSector());
+        propertySetValues.setProperty(Fields.METERING_POINT_TYPE.javaName(), this.getMeteringPointType());
+    }
+
+    @Override
+    public void validateDelete() {
+        // No validation on delete required for now
+    }
+
+}
