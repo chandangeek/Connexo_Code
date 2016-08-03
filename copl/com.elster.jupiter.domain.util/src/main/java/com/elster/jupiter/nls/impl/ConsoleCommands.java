8aeb253c6ae7dae0e1b82ceeb343f51c65af3946
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-08-03 11:58:02 +0200

    Added command to purge all translations from the db

--- copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/nls/impl/ConsoleCommands.java
+++ copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/nls/impl/ConsoleCommands.java
@@ -3,6 +3,13 @@ package com.elster.jupiter.nls.impl;
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.orm.OrmService;
+import com.elster.jupiter.security.thread.ThreadPrincipalService;
+import com.elster.jupiter.transaction.TransactionContext;
+import com.elster.jupiter.transaction.TransactionService;
+import com.elster.jupiter.util.conditions.Condition;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -10,6 +17,7 @@ import org.osgi.service.component.annotations.Reference;
         "osgi.command.function=load",
         "osgi.command.function=listKeys",
         "osgi.command.function=translate",
+        "osgi.command.function=purgeTranslations",
         "osgi.command.function=translations"}, immediate = true)
 public class ConsoleCommands {
 
@@ -20,10 +28,22 @@ public class ConsoleCommands {
     private Thesaurus thesaurus;
     private String module;
     private Layer layer;
+    private DataModel dataModel;
+    private volatile TransactionService transactionService;
+    private volatile ThreadPrincipalService threadPrincipalService;
 
     public void load(String moduleName, String layerName) {
         layer = Layer.valueOf(layerName);
         thesaurus = nlsService.getThesaurus(moduleName, layer);
+    }
+
+    public void purgeTranslations() {
+        threadPrincipalService.set(() -> "Console");
+
+        try (TransactionContext context = transactionService.getContext()) {
+            dataModel.query(NlsKeyImpl.class, NlsEntry.class).select(Condition.TRUE).forEach(dataModel::remove);
+            context.commit();
+        }
     }
 
     public void listKeys() {
@@ -51,4 +71,20 @@ public class ConsoleCommands {
     public void setNlsService(NlsService nlsService) {
         this.nlsService = nlsService;
     }
+
+    @Reference
+    public void setOrmService(OrmService ormService) {
+        dataModel = ormService.getDataModel(NlsService.COMPONENTNAME).get();
+    }
+
+    @Reference
+    public void setTransactionService(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @Reference
+    public void setThreadPrincipalService(ThreadPrincipalService threadPrincipalService) {
+        this.threadPrincipalService = threadPrincipalService;
+    }
+
 }
