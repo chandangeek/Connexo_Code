e71b21955945462f5882faf794fb349584700107
Sentyurin Sergey <ssentyur@mera.ru>
2016-08-04 14:35:24 +0300

    CXO-2428 Estimation task paging broken

--- coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationtasks/view/Grid.js
+++ coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationtasks/view/Grid.js
@@ -51,6 +51,8 @@ Ext.define('Est.estimationtasks.view.Grid', {
                 displayMsg: Uni.I18n.translate('estimationtasks.pagingtoolbartop.displayMsg', 'EST', '{0} - {1} of {2} estimation tasks'),
                 displayMoreMsg: Uni.I18n.translate('estimationtasks.pagingtoolbartop.displayMoreMsg', 'EST', '{0} - {1} of more than {2} estimation tasks'),
                 emptyMsg: Uni.I18n.translate('estimationtasks.pagingtoolbartop.emptyMsg', 'EST', 'There are no estimation tasks to display'),
+                isFullTotalCount: true,
+                usesExactCount: true,
                 items: [
                     {
                         xtype: 'button',
