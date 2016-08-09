a1564fe3a828ed502a4e0390478d35ca4c4add8e
Valery Ruchin <varuchin@mera.ru>
2016-08-09 13:47:43 +0300

    Created valueFactory for Active/Inactive values for some property specs. Changed ValidationStatus and EstimationStatus searchable properties according to remarks (See comments on JIRA issue CXO-2490)

---
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/search/DeviceDataStatusValueFactory.java
@@ -0,0 +1,25 @@
+package com.energyict.mdc.device.data.impl.search;
+
+import com.energyict.mdc.device.data.impl.SearchHelperValueFactory;
+
+import java.util.Arrays;
+
+public class DeviceDataStatusValueFactory extends SearchHelperValueFactory<DeviceDataStatusSearchWrapper> {
+    DeviceDataStatusValueFactory() {
+        super(DeviceDataStatusSearchWrapper.class);
+    }
+
+    @Override
+    public DeviceDataStatusSearchWrapper fromStringValue(String stringValue) {
+        return Arrays.stream(DeviceDataStatusContainer.values())
+                .filter(deviceDataStatusContainer -> deviceDataStatusContainer.getId().equalsIgnoreCase(stringValue))
+                .map(DeviceDataStatusSearchWrapper::new)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public String toStringValue(DeviceDataStatusSearchWrapper deviceDataStatusSearchWrapper) {
+        return deviceDataStatusSearchWrapper.getId();
+    }
+}
