fdca37c0456cf9df41f38b53741082c946386310
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-08-11 10:29:19 +0200

    Fix CXO-470: Refactor SubscriberSpec creation API to avoid many overloaded methods supporting all the different (optional) parameters.

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/Installer.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/Installer.java
@@ -10,7 +10,6 @@ import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.servicecall.LogLevel;
-import com.elster.jupiter.servicecall.ServiceCall;
 import com.elster.jupiter.servicecall.ServiceCallService;
 import com.elster.jupiter.servicecall.ServiceCallType;
 import com.elster.jupiter.upgrade.FullInstaller;
@@ -43,9 +42,11 @@ import com.energyict.mdc.scheduling.SchedulingService;
 import javax.inject.Inject;
 import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static com.elster.jupiter.messaging.DestinationSpec.whereCorrelationId;
 
@@ -58,9 +59,9 @@ import static com.elster.jupiter.messaging.DestinationSpec.whereCorrelationId;
 public class Installer implements FullInstaller, PrivilegesProvider {
 
     public static final String COMSCHEDULE_RECALCULATOR_MESSAGING_NAME = "COMSCHED_RECALCULATOR";
-    public static final String COMSCHEDULE_RECALCULATOR_MESSAGING_DISPLAYNAME = "Recalculate communication schedules";
+    static final String COMSCHEDULE_RECALCULATOR_MESSAGING_DISPLAYNAME = "Recalculate communication schedules";
     public static final String COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_NAME = "COMSCHED_BATCH_OBSOLETE";
-    public static final String COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_DISPLAYNAME = "Handle obsolete communication schedules";
+    static final String COMSCHEDULE_BACKGROUND_OBSOLETION_MESSAGING_DISPLAYNAME = "Handle obsolete communication schedules";
     private static final int DEFAULT_RETRY_DELAY_IN_SECONDS = 60;
 
     private final DataModel dataModel;
@@ -132,7 +133,8 @@ public class Installer implements FullInstaller, PrivilegesProvider {
                         .getKey(), Arrays.asList(Privileges.Constants.IMPORT_INVENTORY_MANAGEMENT, Privileges.Constants.REVOKE_INVENTORY_MANAGEMENT)),
                 this.userService.createModuleResourceWithPrivileges(DeviceDataServices.COMPONENT_NAME, Privileges.RESOURCE_DATA_COLLECTION_KPI.getKey(), Privileges.RESOURCE_DATA_COLLECTION_KPI_DESCRIPTION
                         .getKey(), Arrays.asList(Privileges.Constants.ADMINISTER_DATA_COLLECTION_KPI, Privileges.Constants.VIEW_DATA_COLLECTION_KPI)),
-                this.userService.createModuleResourceWithPrivileges(DeviceDataServices.COMPONENT_NAME, Privileges.RESOURCE_DEVICES.getKey(), Privileges.RESOURCE_DEVICES_DESCRIPTION.getKey(), Arrays.asList(Privileges.Constants.ADMINISTER_DEVICE_TIME_SLICED_CPS))
+                this.userService.createModuleResourceWithPrivileges(DeviceDataServices.COMPONENT_NAME, Privileges.RESOURCE_DEVICES.getKey(), Privileges.RESOURCE_DEVICES_DESCRIPTION.getKey(), Collections
+                        .singletonList(Privileges.Constants.ADMINISTER_DEVICE_TIME_SLICED_CPS))
         );
 
     }
@@ -141,24 +143,21 @@ public class Installer implements FullInstaller, PrivilegesProvider {
         Optional<DestinationSpec> destinationSpec = this.messageService.getDestinationSpec(EventService.JUPITER_EVENTS);
         if (destinationSpec.isPresent()) {
             DestinationSpec jupiterEvents = destinationSpec.get();
-            Arrays.asList(
+            Stream.of(
                     Pair.of(ComTaskEnablementConnectionMessageHandlerFactory.SUBSCRIBER_NAME, whereCorrelationId().like("com/energyict/mdc/device/config/comtaskenablement/%")),
                     Pair.of(ComTaskEnablementPriorityMessageHandlerFactory.SUBSCRIBER_NAME, whereCorrelationId().isEqualTo("com/energyict/mdc/device/config/comtaskenablement/PRIORITY_UPDATED")),
                     Pair.of(ComTaskEnablementStatusMessageHandlerFactory.SUBSCRIBER_NAME, whereCorrelationId().like("com/energyict/mdc/device/config/comtaskenablement/%")),
-                    Pair.of(MeterReadingEventHandlerFactory.SUBSCRIBER_NAME, whereCorrelationId().isEqualTo("com/elster/jupiter/metering/meterreading/CREATED").or(whereCorrelationId().isEqualTo("com/energyict/mdc/connectiontask/COMPLETION")))
-            )
-                    .stream()
-                    .
-                            filter(subscriber -> !jupiterEvents.getSubscribers()
+                    Pair.of(MeterReadingEventHandlerFactory.SUBSCRIBER_NAME, whereCorrelationId().isEqualTo("com/elster/jupiter/metering/meterreading/CREATED")
+                            .or(whereCorrelationId().isEqualTo("com/energyict/mdc/connectiontask/COMPLETION"))))
+                .filter(subscriber -> !jupiterEvents.getSubscribers()
                                     .stream()
                                     .anyMatch(s -> s.getName().equals(subscriber.getFirst())))
-                    .
-                            forEach(subscriber -> this.doSubscriber(jupiterEvents, subscriber));
+                .forEach(subscriber -> this.doSubscriber(jupiterEvents, subscriber));
         }
     }
 
     private void doSubscriber(DestinationSpec jupiterEvents, Pair<String, Condition> subscriber) {
-        jupiterEvents.subscribe(subscriber.getFirst(), subscriber.getLast());
+        jupiterEvents.subscribe(subscriber.getFirst()).with(subscriber.getLast()).create();
     }
 
     private void createMessageHandlers() {
@@ -188,7 +187,7 @@ public class Installer implements FullInstaller, PrivilegesProvider {
         if (!destinationSpecOptional.isPresent()) {
             DestinationSpec queue = defaultQueueTableSpec.createDestinationSpec(destinationName, DEFAULT_RETRY_DELAY_IN_SECONDS);
             queue.activate();
-            queue.subscribe(subscriberName);
+            queue.subscribe(subscriberName).create();
         } else {
             boolean notSubscribedYet = !destinationSpecOptional.get()
                     .getSubscribers()
@@ -196,7 +195,7 @@ public class Installer implements FullInstaller, PrivilegesProvider {
                     .anyMatch(spec -> spec.getName().equals(subscriberName));
             if (notSubscribedYet) {
                 destinationSpecOptional.get().activate();
-                destinationSpecOptional.get().subscribe(subscriberName);
+                destinationSpecOptional.get().subscribe(subscriberName).create();
             }
         }
     }
