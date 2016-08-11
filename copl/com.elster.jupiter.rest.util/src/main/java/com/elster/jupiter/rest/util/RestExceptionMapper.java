e6f045df7badfd73d99e8e6ae3baeb7d41aec0b4
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2016-08-11 15:28:41 +0200

    CXO-2541: Changed status code for LocalizedExceptions from 400 to 422

--- copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/RestExceptionMapper.java
+++ copl/com.elster.jupiter.rest.util/src/main/java/com/elster/jupiter/rest/util/RestExceptionMapper.java
@@ -1,28 +1,25 @@
 package com.elster.jupiter.rest.util;
 
-import com.elster.jupiter.nls.LocalizedException;
-
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 
-public class LocalizedExceptionMapper implements ExceptionMapper<LocalizedException>{
+public class RestExceptionMapper implements ExceptionMapper<ExceptionFactory.RestException> {
 
     private final Provider<ConstraintViolationInfo> infoProvider;
 
     @Inject
-    public LocalizedExceptionMapper(Provider<ConstraintViolationInfo> infoProvider) {
+    public RestExceptionMapper(Provider<ConstraintViolationInfo> infoProvider) {
         this.infoProvider = infoProvider;
     }
 
     @Override
-    public Response toResponse(LocalizedException exception) {
+    public Response toResponse(ExceptionFactory.RestException exception) {
         ConstraintViolationInfo constraintViolationInfo = infoProvider.get();
         constraintViolationInfo.from(exception);
-
-        return Response.status(StatusCode.UNPROCESSABLE_ENTITY.getStatusCode()).entity(constraintViolationInfo).build();
+        return Response.status(exception.getStatus()).entity(constraintViolationInfo).build();
     }
 
 }
