3aca2983aa1fe5165477c382eb43610f7bbc8857
Anton Suslennikov <antsusl@mera.ru>
2016-08-24 15:20:50 +0300

    CXO-2735 Upgrade from 10.1 to 10.2: EventTypes of 10.2 are not installed

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/InstallerImpl.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/InstallerImpl.java
@@ -17,18 +17,11 @@ import com.elster.jupiter.metering.EventType;
 import com.elster.jupiter.metering.KnownAmrSystem;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.MultiplierType;
-import com.elster.jupiter.metering.ServiceCategory;
 import com.elster.jupiter.metering.ServiceKind;
-import com.elster.jupiter.metering.config.DefaultMeterRole;
-import com.elster.jupiter.metering.config.DefaultMetrologyPurpose;
-import com.elster.jupiter.metering.config.MeterRole;
-import com.elster.jupiter.metering.impl.config.MetrologyConfigurationServiceImpl;
-import com.elster.jupiter.metering.impl.config.ReadingTypeTemplateInstaller;
 import com.elster.jupiter.metering.impl.upgraders.GasDayOptionsCreator;
 import com.elster.jupiter.metering.security.Privileges;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
-import com.elster.jupiter.orm.SqlDialect;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.parties.PartyService;
 import com.elster.jupiter.upgrade.FullInstaller;
@@ -78,11 +71,11 @@ public class InstallerImpl implements FullInstaller, PrivilegesProvider {
     private final String[] requiredReadingTypes;
     private final Clock clock;
     private final DataModel dataModel;
-    private final MetrologyConfigurationServiceImpl metrologyConfigurationService;
     private final BundleContext bundleContext;
+    private final InstallerV10_2Impl installerV10_2;
 
     @Inject
-    public InstallerImpl(BundleContext bundleContext, DataModel dataModel, ServerMeteringService meteringService, IdsService idsService, PartyService partyService, UserService userService, EventService eventService, MessageService messageService, Clock clock, MetrologyConfigurationServiceImpl metrologyConfigurationService, MeteringDataModelServiceImpl meteringDataModelService) {
+    public InstallerImpl(BundleContext bundleContext, DataModel dataModel, ServerMeteringService meteringService, IdsService idsService, PartyService partyService, UserService userService, EventService eventService, MessageService messageService, Clock clock, MeteringDataModelServiceImpl meteringDataModelService, InstallerV10_2Impl installerV10_2) {
         this.bundleContext = bundleContext;
         this.dataModel = dataModel;
         this.meteringService = meteringService;
@@ -91,7 +84,7 @@ public class InstallerImpl implements FullInstaller, PrivilegesProvider {
         this.userService = userService;
         this.eventService = eventService;
         this.messageService = messageService;
-        this.metrologyConfigurationService = metrologyConfigurationService;
+        this.installerV10_2 = installerV10_2;
         this.createAllReadingTypes = meteringDataModelService.isCreateAllReadingTypes();
         this.requiredReadingTypes = meteringDataModelService.getRequiredReadingTypes();
         this.clock = clock;
@@ -156,26 +149,7 @@ public class InstallerImpl implements FullInstaller, PrivilegesProvider {
                 this::createQueues,
                 logger
         );
-        doTry(
-                "Create SQL Aggregation Components",
-                this::createSqlAggregationComponents,
-                logger
-        );
-        doTry(
-                "Create Meter Roles",
-                this::createMeterRoles,
-                logger
-        );
-        doTry(
-                "Create Metrology Purposes",
-                this::createMetrologyPurposes,
-                logger
-        );
-        doTry(
-                "Create Reading Type templates",
-                this::createReadingTypeTemplates,
-                logger
-        );
+        installerV10_2.install(dataModelUpgrader, logger);
         doTry(
                 "Create Location Member table",
                 () -> new CreateLocationMemberTableOperation(dataModel, meteringService.getLocationTemplate()).execute(),
@@ -408,48 +382,6 @@ public class InstallerImpl implements FullInstaller, PrivilegesProvider {
         DestinationSpec destinationSpec = defaultQueueTableSpec.createDestinationSpec(queueDestination, DEFAULT_RETRY_DELAY_IN_SECONDS);
         destinationSpec.activate();
         destinationSpec.subscribe(queueSubscriber).create();
-    }
-
-    private void createSqlAggregationComponents() {
-        if (!SqlDialect.H2.equals(this.meteringService.getDataModel().getSqlDialect())) {
-            SqlExecuteFromResourceFile
-                    .executeAll(
-                            this.meteringService.getDataModel(),
-                            "type.Flags_Array.sql",
-                            "function.aggFlags.sql");
-        }
-    }
-
-
-    private void createMeterRoles() {
-        metrologyConfigurationService.newMeterRole(DefaultMeterRole.DEFAULT.getNlsKey());//available to all service categories, so no need to attach
-        MeterRole production = metrologyConfigurationService.newMeterRole(DefaultMeterRole.PRODUCTION.getNlsKey());
-        MeterRole consumption = metrologyConfigurationService.newMeterRole(DefaultMeterRole.CONSUMPTION.getNlsKey());
-        MeterRole main = metrologyConfigurationService.newMeterRole(DefaultMeterRole.MAIN.getNlsKey());
-        MeterRole check = metrologyConfigurationService.newMeterRole(DefaultMeterRole.CHECK.getNlsKey());
-        MeterRole peakConsumption = metrologyConfigurationService.newMeterRole(DefaultMeterRole.PEAK_CONSUMPTION.getNlsKey());
-        MeterRole offPeakConsumption = metrologyConfigurationService.newMeterRole(DefaultMeterRole.OFF_PEAK_CONSUMPTION.getNlsKey());
-
-        attachMeterRolesToServiceCategory(meteringService.getServiceCategory(ServiceKind.ELECTRICITY).get(), production, consumption, main, check);
-        attachMeterRolesToServiceCategory(meteringService.getServiceCategory(ServiceKind.GAS).get(), consumption, main, check);
-        attachMeterRolesToServiceCategory(meteringService.getServiceCategory(ServiceKind.WATER).get(), consumption, main, check, peakConsumption, offPeakConsumption);
-        attachMeterRolesToServiceCategory(meteringService.getServiceCategory(ServiceKind.HEAT).get(), consumption, main, check);
-    }
-
-    private void attachMeterRolesToServiceCategory(ServiceCategory serviceCategory, MeterRole... meterRoles) {
-        for (MeterRole meterRole : meterRoles) {
-            serviceCategory.addMeterRole(meterRole);
-        }
-    }
-
-    private void createMetrologyPurposes() {
-        metrologyConfigurationService.createMetrologyPurpose(DefaultMetrologyPurpose.BILLING);
-        metrologyConfigurationService.createMetrologyPurpose(DefaultMetrologyPurpose.INFORMATION);
-        metrologyConfigurationService.createMetrologyPurpose(DefaultMetrologyPurpose.VOLTAGE_MONITORING);
-    }
-
-    private void createReadingTypeTemplates() {
-        new ReadingTypeTemplateInstaller(metrologyConfigurationService).install();
     }
 
     private void createEndDeviceControlTypes(Logger logger) {
