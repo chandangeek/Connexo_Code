5d3ded97b23217b28a7cf3ed781596c264a15ce1
Maksim Barinov <mbarinov@mera.ru>
2016-08-25 13:12:19 +0300

    CXO-460

--- copl/com.elster.jupiter.cps.rest/src/test/java/com/elster/jupiter/cps/rest/impl/CustomPropertySetApplicationJerseyTest.java
+++ copl/com.elster.jupiter.cps.rest/src/test/java/com/elster/jupiter/cps/rest/impl/CustomPropertySetApplicationJerseyTest.java
@@ -2,21 +2,26 @@ package com.elster.jupiter.cps.rest.impl;
 
 import com.elster.jupiter.cps.CustomPropertySetService;
 import com.elster.jupiter.devtools.rest.FelixRestApplicationJerseyTest;
-import org.mockito.Mock;
+import com.elster.jupiter.properties.PropertyValueInfoService;
 
 import javax.ws.rs.core.Application;
 import java.time.Clock;
+
+import org.mockito.Mock;
 
 public class CustomPropertySetApplicationJerseyTest extends FelixRestApplicationJerseyTest {
 
     @Mock
     CustomPropertySetService customPropertySetService;
     @Mock
+    PropertyValueInfoService propertyValueInfoService;
+    @Mock
     Clock clock;
 
     @Override
     protected Application getApplication() {
         CustomPropertySetApplication application = new CustomPropertySetApplication();
+        application.setPropertyValueInfoService(propertyValueInfoService);
         application.setNlsService(nlsService);
         application.setTransactionService(transactionService);
         application.setCustomPropertySetService(customPropertySetService);
