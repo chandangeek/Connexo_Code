5c70addfd3b1eb9d561e9eb4e7be62ae77404374
khe <kristof.hennebel@elster.com>
2016-08-29 14:24:40 +0200

    CXO-2121 - Provide the parallel scheduling implementation CXO-2816 - mdc.device.data UpgraderV10_2 is not used
    Added SQL queries in the upgraders of mdc.device.data and  mdc.device.config to change the existing simultaneousconnections values from 0 to 1

--- comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/UpgraderV10_2.java
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/impl/UpgraderV10_2.java
@@ -2,10 +2,15 @@ package com.energyict.mdc.device.config.impl;
 
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.Upgrader;
 
 import javax.inject.Inject;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 class UpgraderV10_2 implements Upgrader {
 
@@ -19,5 +24,21 @@ class UpgraderV10_2 implements Upgrader {
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
         dataModelUpgrader.upgrade(dataModel, Version.version(10, 2));
+
+        List<String> sql = new ArrayList<>();
+        sql.add("UPDATE DTC_PARTIALCONNECTIONTASK SET simultaneousconnections=1 where simultaneousconnections = 0");
+        dataModel.useConnectionRequiringTransaction(connection -> {
+            try (Statement statement = connection.createStatement()) {
+                sql.forEach(sqlCommand -> execute(statement, sqlCommand));
+            }
+        });
+    }
+
+    private void execute(Statement statement, String sql) {
+        try {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new UnderlyingSQLFailedException(e);
+        }
     }
 }
