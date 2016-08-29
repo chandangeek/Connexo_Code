2c8b5653ee882676ab06e740f9fe61f108d3853e
Anton Suslennikov <antsusl@mera.ru>
2016-08-29 16:25:26 +0300

    CXO-2771 After upgrade from 10.1 to 10.2 RelativePeriodCategoryUsage doesn't have audit columns and jrnl table

---
+++ copl/com.elster.jupiter.upgrade/src/main/java/com/elster/jupiter/upgrade/V10_2SimpleUpgrader.java
@@ -0,0 +1,26 @@
+package com.elster.jupiter.upgrade;
+
+import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.Version;
+
+import com.google.common.collect.ImmutableMap;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class V10_2SimpleUpgrader implements Upgrader {
+    public static final Version VERSION = Version.version(10, 2);
+    public static final Map<Version, Class<? extends Upgrader>> V10_2_UPGRADER = ImmutableMap.of(VERSION, V10_2SimpleUpgrader.class);
+    private final DataModel dataModel;
+
+    @Inject
+    public V10_2SimpleUpgrader(DataModel dataModel) {
+        this.dataModel = dataModel;
+    }
+
+    @Override
+    public void migrate(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(dataModel, VERSION);
+    }
+}
