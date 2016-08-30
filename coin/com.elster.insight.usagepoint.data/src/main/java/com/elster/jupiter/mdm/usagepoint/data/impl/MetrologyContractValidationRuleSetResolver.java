57d0183374fe8d2c8db4b26c86060ec2ab7eee7b
Anton Fomchenko <antfom@mera.ru>
2016-08-30 10:59:31 +0300

    CXO-2804 restore ruleSet collector for multisense

--- coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractValidationRuleSetResolver.java
+++ coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractValidationRuleSetResolver.java
@@ -1,6 +1,9 @@
 package com.elster.jupiter.mdm.usagepoint.data.impl;
 
 import com.elster.jupiter.mdm.usagepoint.config.UsagePointConfigurationService;
+import com.elster.jupiter.metering.config.EffectiveMetrologyConfigurationOnUsagePoint;
+import com.elster.jupiter.metering.config.UsagePointMetrologyConfiguration;
+import com.elster.jupiter.util.streams.DecoratedStream;
 import com.elster.jupiter.validation.ValidationContext;
 import com.elster.jupiter.validation.ValidationRuleSet;
 import com.elster.jupiter.validation.ValidationRuleSetResolver;
@@ -8,8 +11,11 @@ import com.elster.jupiter.validation.ValidationRuleSetResolver;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component(name = "com.elster.jupiter.mdm.usagepoint.data.MetrologyContractValidationRuleSetResolver", service = ValidationRuleSetResolver.class, immediate = true)
 public class MetrologyContractValidationRuleSetResolver implements ValidationRuleSetResolver {
@@ -24,6 +30,16 @@ public class MetrologyContractValidationRuleSetResolver implements ValidationRul
     public List<ValidationRuleSet> resolve(ValidationContext validationContext) {
         if (validationContext.getMetrologyContract().isPresent()) {
             return this.usagePointConfigurationService.getValidationRuleSets(validationContext.getMetrologyContract().get());
+        }
+        if (validationContext.getChannelsContainer().getUsagePoint().isPresent()) {
+            Optional<UsagePointMetrologyConfiguration> metrologyConfiguration = validationContext.getChannelsContainer().getUsagePoint().get().getEffectiveMetrologyConfiguration(Instant.now())
+                    .map(EffectiveMetrologyConfigurationOnUsagePoint::getMetrologyConfiguration);
+            if (metrologyConfiguration.isPresent()){
+                return DecoratedStream.decorate(metrologyConfiguration.get().getContracts().stream())
+                        .flatMap(contract -> this.usagePointConfigurationService.getValidationRuleSets(contract).stream())
+                        .distinct(ValidationRuleSet::getId)
+                        .collect(Collectors.toList());
+            }
         }
         return Collections.emptyList();
     }
