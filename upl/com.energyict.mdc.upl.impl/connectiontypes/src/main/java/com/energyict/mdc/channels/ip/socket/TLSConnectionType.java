8cad04d0655877f41ccd43056cfa4e29a41447d2
khe <kristof.hennebel@elster.com>
2016-09-02 17:03:06 +0200

    COMMUNICATION-1611 - EVN requires DLMS-over-TLS support for RTU3 gateway/DC readout
    Made the preferred cipher suite(s) configurable, using property "PreferredCipherSuites". It's a comma-separated list, it has no default since the supported cipher suites rely on your java version.

--- upl/com.energyict.mdc.upl.impl/connectiontypes/src/main/java/com/energyict/mdc/channels/ip/socket/TLSConnectionType.java
+++ upl/com.energyict.mdc.upl.impl/connectiontypes/src/main/java/com/energyict/mdc/channels/ip/socket/TLSConnectionType.java
@@ -12,6 +12,8 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -24,37 +26,58 @@ public class TLSConnectionType extends OutboundTcpIpConnectionType {
 
     private static final String TLS_VERSION_PROPERTY_NAME = "TLSVersion";
     private static final String TLS_DEFAULT_VERSION = "TLSv1.2";
+    private static final String PREFERRED_CIPHER_SUITES_PROPERTY_NAME = "PreferredCipherSuites";
+    private static final String SEPARATOR = ",";
 
+    /**
+     * The version of the TLS protocol to be used.
+     * Defaults to TLSv1.2
+     */
     private PropertySpec tlsVersionPropertySpec() {
         return PropertySpecFactory.stringPropertySpec(TLS_VERSION_PROPERTY_NAME, TLS_DEFAULT_VERSION);
+    }
+
+    /**
+     * A comma-separated list of cipher suites that are preferred by the client (ComServer)
+     */
+    private PropertySpec preferredCipheringSuitesPropertySpec() {
+        return PropertySpecFactory.stringPropertySpec(PREFERRED_CIPHER_SUITES_PROPERTY_NAME);
     }
 
     @Override
     public List<PropertySpec> getOptionalProperties() {
         List<PropertySpec> optionalProperties = super.getOptionalProperties();
         optionalProperties.add(tlsVersionPropertySpec());
+        optionalProperties.add(preferredCipheringSuitesPropertySpec());
         return optionalProperties;
     }
 
-    protected String tlsVersionPropertyValue() {
+    private String getTLSVersionPropertyValue() {
         return (String) this.getProperty(TLS_VERSION_PROPERTY_NAME, TLS_DEFAULT_VERSION);
+    }
+
+    private String getPreferredCipherSuitesPropertyValue() {
+        return (String) this.getProperty(PREFERRED_CIPHER_SUITES_PROPERTY_NAME);
     }
 
     @Override
     public PropertySpec getPropertySpec(String name) {
-        if (name.equals(TLS_VERSION_PROPERTY_NAME)) {
-            return tlsVersionPropertySpec();
-        } else {
-            return super.getPropertySpec(name);
+        switch (name) {
+            case TLS_VERSION_PROPERTY_NAME:
+                return tlsVersionPropertySpec();
+            case PREFERRED_CIPHER_SUITES_PROPERTY_NAME:
+                return preferredCipheringSuitesPropertySpec();
+            default:
+                return super.getPropertySpec(name);
         }
     }
 
     @Override
     protected ServerLoggableComChannel newTcpIpConnection(String host, int port, int timeOut) throws ConnectionException {
-        System.setProperty("jdk.tls.client.protocols", tlsVersionPropertyValue());
+        System.setProperty("jdk.tls.client.protocols", getTLSVersionPropertyValue());
 
         try {
-            final SSLContext sslContext = SSLContext.getInstance(tlsVersionPropertyValue());
+            final SSLContext sslContext = SSLContext.getInstance(getTLSVersionPropertyValue());
 
             /* Accept everything */
             final TrustManager[] trustManagers = new TrustManager[]{getAcceptEverythingTrustManager()};
@@ -69,11 +92,34 @@ public class TLSConnectionType extends OutboundTcpIpConnectionType {
             SSLSocket socket = (SSLSocket) socketFactory.createSocket();
             socket.setNeedClientAuth(false);
             socket.setWantClientAuth(false);
-            socket.connect(new InetSocketAddress(host, port));
+            handlePreferredCipherSuites(socket);
+            socket.connect(new InetSocketAddress(host, port), timeOut);
 
             return new SocketComChannel(socket);
         } catch (NoSuchAlgorithmException | KeyManagementException | IOException e) {
             throw new ConnectionException(e);
+        }
+    }
+
+    private void handlePreferredCipherSuites(SSLSocket socket) throws ConnectionException {
+        String preferredCipherSuitesPropertyValue = getPreferredCipherSuitesPropertyValue();
+        if (preferredCipherSuitesPropertyValue != null) {
+
+            List<String> enabledCipherSuites = new ArrayList<>(Arrays.asList(socket.getEnabledCipherSuites()));
+
+            //Put the configured preferredCipherSuites in front of the 'enabled' list, if they are supported by this java version.
+            String[] preferredCipherSuites = preferredCipherSuitesPropertyValue.split(SEPARATOR);
+            for (int index = 0; index < preferredCipherSuites.length; index++) {
+                String preferredCipherSuite = preferredCipherSuites[index];
+                if (enabledCipherSuites.contains(preferredCipherSuite)) {
+                    enabledCipherSuites.remove(preferredCipherSuite);
+                    enabledCipherSuites.add(index, preferredCipherSuite);
+                } else {
+                    throw new ConnectionException("preferredCipherSuiteIsNotSupportedByJavaVersion", preferredCipherSuite);
+                }
+            }
+
+            socket.setEnabledCipherSuites(enabledCipherSuites.toArray(new String[enabledCipherSuites.size()]));
         }
     }
 
