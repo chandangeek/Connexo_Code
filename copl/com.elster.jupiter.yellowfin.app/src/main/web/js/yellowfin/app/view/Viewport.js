c06f33078afedc3349d3aba16c906fec326c295c
geert <geert.delmeiren@elster.com>
2016-09-05 12:00:01 +0200

    Removed all javascript/extjs code from yellowfin.app

--- copl/com.elster.jupiter.yellowfin.app/src/main/web/js/yellowfin/app/view/Viewport.js
+++
@@ -1,3 +0,0 @@
-Ext.define('YellowfinApp.view.Viewport', {
-    extend: 'Uni.view.Viewport'
-});
--- copl/com.elster.jupiter.yellowfin.app/src/main/web/js/yellowfin/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\jupiterrepo\jupiter-all\com.elster.jupiter.masterjs\src\main\web\js\app.js
