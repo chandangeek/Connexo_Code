ade0c8b4dcfc2a495f2b674aae2e1e2662de9ff2
Sentyurin Sergey <ssentyur@mera.ru>
2016-09-21 14:58:59 +0300

    CXO-3048 When on the "Edit meters" page, pressing "Save" brings you to another page than pressing "Cancel"

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointsetup/view/ActivateMeters.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointsetup/view/ActivateMeters.js
@@ -1,7 +1,6 @@
 Ext.define('Imt.usagepointsetup.view.ActivateMeters', {
     extend: 'Uni.view.container.ContentContainer',
     alias: 'widget.usagePointActivateMeters',
-    itemId: 'usage-point-activate-meters',
     router: null,
     usagePoint: null,
     meterRoles: null,
