78ee87b34117a1891bb13ecf8bf8b9ab53668c5f
Lobanov Dmitry <dlobanov@mera.ru>
2016-09-21 18:10:15 +0300

    CXO-2802 Change API of Device, DeviceService, fixed tests in mdc.device.data

--- comu/com.energyict.mdc.device.topology/src/main/java/com/energyict/mdc/device/topology/impl/TopologyServiceImpl.java
+++ comu/com.energyict.mdc.device.topology/src/main/java/com/energyict/mdc/device/topology/impl/TopologyServiceImpl.java
@@ -414,7 +414,7 @@ public class TopologyServiceImpl implements ServerTopologyService, MessageSeedPr
         Condition condition = where(PhysicalGatewayReferenceImpl.Field.GATEWAY.fieldName()).isEqualTo(dataLogger)
                 .and(where("interval.start").in(Range.closed(linkingDate.toEpochMilli(), linkingDate.toEpochMilli())));
         Optional<DataLoggerReference> duplicateReference = this.dataModel.mapper(DataLoggerReference.class).select(condition).stream().collect(Collectors.toList())
-                .stream().filter(reference -> reference.getOrigin().getmRID().equals(slave.getmRID()) && reference.getRange().lowerEndpoint().equals(linkingDate)).findAny();
+                .stream().filter(reference -> reference.getOrigin().getName().equals(slave.getName()) && reference.getRange().lowerEndpoint().equals(linkingDate)).findAny();
         if (duplicateReference.isPresent()) {
             throw DataLoggerLinkException.slaveWasPreviouslyLinkedAtSameTimeStamp(thesaurus, slave, dataLogger, linkingDate);
         }
@@ -1006,7 +1006,7 @@ public class TopologyServiceImpl implements ServerTopologyService, MessageSeedPr
         if (addressInformations.isEmpty()) {
             return Optional.empty();
         } else if (addressInformations.size() > 1) {
-            throw new IllegalStateException("Expecting at most 1 effective G3DeviceAddressInformation entity for device with mRID " + device.getmRID());
+            throw new IllegalStateException("Expecting at most 1 effective G3DeviceAddressInformation entity for device with name " + device.getName());
         } else {
             return Optional.of(addressInformations.get(0));
         }
