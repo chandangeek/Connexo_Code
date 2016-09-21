32cb30b526069743b77acc931c661192b6ba0d19
Lobanov Dmitry <dlobanov@mera.ru>
2016-09-21 18:10:15 +0300

    CXO-2802 Change API of Device, DeviceService, fixed tests in mdc.device.data

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/SchedulingResource.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/impl/SchedulingResource.java
@@ -4,9 +4,9 @@ import com.elster.jupiter.nls.LocalizedFieldValidationException;
 import com.elster.jupiter.rest.util.JsonQueryFilter;
 import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.elster.jupiter.rest.util.PagedInfoList;
+import com.elster.jupiter.rest.util.Transactional;
 import com.elster.jupiter.time.TemporalExpression;
 import com.energyict.mdc.common.rest.IdListBuilder;
-import com.elster.jupiter.rest.util.Transactional;
 import com.energyict.mdc.common.services.ListPager;
 import com.energyict.mdc.device.config.ComTaskEnablement;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
@@ -91,7 +91,7 @@ public class SchedulingResource {
 
     private void filterAvailableSchedulesOnly(String mrid, List<ComSchedule> comSchedules) {
         deviceService
-            .findByUniqueMrid(mrid)
+                .findDeviceByMrid(mrid)
             .ifPresent(device -> {
                 List<ComTaskExecution> comTaskExecutions = device.getComTaskExecutions();
                 List<ComTaskEnablement> comTaskEnablements = device.getDeviceConfiguration().getComTaskEnablements();
