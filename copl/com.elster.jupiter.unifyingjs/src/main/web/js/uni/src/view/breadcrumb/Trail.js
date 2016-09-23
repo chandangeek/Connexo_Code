3333a0859709c678835f0918bf9343374c1fc4bd
Artem Puchenkin <apuchenkin@gmail.com>
2016-09-23 15:35:01 +0300

    [CXO-3049] Navigating to a usage point starting from many search results takes too long (without "loading..." indication)

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/breadcrumb/Trail.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/breadcrumb/Trail.js
@@ -29,7 +29,7 @@ Ext.define('Uni.view.breadcrumb.Trail', {
         me.addBreadcrumbItem(item);
 
         if (me.rendered) {
-            Ext.resumeLayouts(isHidden !== me.hidden);
+            Ext.resumeLayouts(true);
         }
     },
 
