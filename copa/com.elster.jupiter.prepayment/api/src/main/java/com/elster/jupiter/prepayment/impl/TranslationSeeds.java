688dcc67df13f714d046a6501db018d7e67fe5d9
geert <geert.delmeiren@elster.com>
2016-09-23 10:48:58 +0200

    CXO-3073 - The 10.2 message services don't have a translated name yet  4 of the 5 messages done

--- copa/com.elster.jupiter.prepayment/api/src/main/java/com/elster/jupiter/prepayment/impl/TranslationSeeds.java
+++ copa/com.elster.jupiter.prepayment/api/src/main/java/com/elster/jupiter/prepayment/impl/TranslationSeeds.java
@@ -14,7 +14,8 @@ public enum TranslationSeeds implements TranslationKey {
     AS_SOON_AS_POSSIBLE("AsSoonAsPossible", "As soon as possible"),
     CALL_BACK_URL("callback", "Callback URL"),
     PROVIDED_RESPONSE("providedResponse", "Provided response"),
-    BREAKER_STATUS("breakerStatus", "Breaker status")
+    BREAKER_STATUS("breakerStatus", "Breaker status"),
+    COMPLETION_OPTIONS_MESSAGE_HANDLER_DISPLAYNAME(CompletionOptionsMessageHandlerFactory.COMPLETION_OPTIONS_TASK_SUBSCRIBER, CompletionOptionsMessageHandlerFactory.COMPLETION_OPTIONS_TASK_SUBSCRIBER_DISPLAYNAME)
     ;
     private String key;
     private final String defaultFormat;
