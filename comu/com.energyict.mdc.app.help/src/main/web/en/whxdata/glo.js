97613dbdf3f1ba11aa31f0e405c9035d00bbf016
Lobanov Dmitry <dlobanov@mera.ru>
2016-09-23 11:33:26 +0300

    Update for Connexo 10.2 preparation

--- comu/com.energyict.mdc.app.help/src/main/web/en/whxdata/glo.js
+++ comu/com.energyict.mdc.app.help/src/main/web/en/whxdata/glo.js
@@ -1,1 +1,1 @@
-gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><glossary>  <chunkinfo url=\"gdata1.js\" first=\"authentication\" last=\"state\" num=\"43\" />  <chunkinfo url=\"gdata2.js\" first=\"static device group\" last=\"validator\" num=\"5\" />  </glossary>";
+gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><glossary>  <chunkinfo url=\"gdata1.js\" first=\"authentication\" last=\"register types\" num=\"41\" />  <chunkinfo url=\"gdata2.js\" first=\"release date\" last=\"validator\" num=\"9\" />  </glossary>";
---
+++ comu/com.energyict.mdc.app.help/src/main/web/en/whxdata/toc61.js
@@ -0,0 +1,1 @@
+gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><data  src=\"toc.js\"  name=\"Data import\" url=\"000017_EN_Connexo_1.0_User_guide/000017_UMNL_ImportData/Data_import.htm\"><book  src=\"toc62.js\"  name=\"Data import\" url=\"000017_EN_Connexo_1.0_User_guide/000017_UMNL_ImportData/Data_import1.htm\" /></data>";
