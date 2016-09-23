99c1cb471679827e2a0e03faa6c64c1307cf58c8
Yan R <yaratay@mera.ru>
2016-09-23 18:04:10 +0300

    CXO-2908 Bad sorting & pagination of usage points quick update after review by Bruno Ballekens & Rudi Vankeirsbilck

--- comu/com.energyict.mdc.firmware/src/main/java/com/energyict/mdc/firmware/impl/search/FirmwareVersionDeviceSearchDomainExtension.java
+++ comu/com.energyict.mdc.firmware/src/main/java/com/energyict/mdc/firmware/impl/search/FirmwareVersionDeviceSearchDomainExtension.java
@@ -7,7 +7,6 @@ import com.elster.jupiter.search.SearchableProperty;
 import com.elster.jupiter.search.SearchablePropertyCondition;
 import com.elster.jupiter.search.SearchablePropertyConstriction;
 import com.elster.jupiter.util.conditions.Condition;
-import com.elster.jupiter.util.conditions.Order;
 import com.elster.jupiter.util.sql.SqlFragment;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.DeviceFields;
@@ -83,6 +82,6 @@ public class FirmwareVersionDeviceSearchDomainExtension implements SearchDomainE
     public SqlFragment asFragment(List<SearchablePropertyCondition> conditions) {
         return this.firmwareService.getDataModel().query(ActivatedFirmwareVersion.class, FirmwareVersion.class)
                 .asFragment(conditions.stream().reduce(Condition.TRUE, (ormCondition, searchCondition) -> ormCondition.and(searchCondition.getCondition()), Condition::and),
-                        new String[]{"device"}, Order.NOORDER);
+                        "device");
     }
 }
