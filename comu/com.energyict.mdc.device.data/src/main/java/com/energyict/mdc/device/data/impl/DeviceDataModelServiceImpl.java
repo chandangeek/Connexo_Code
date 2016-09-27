860adc160496f569eb49b331d1215b72f271f038
govanni <govanni.naessens@elster.com>
2016-09-27 11:46:07 +0200

    CXO-3052 - CXO-3053 Added a lockservice to the DeviceConfiguration bundle so it is possible to lock a DeviceType from within the DeviceData bundle. Locking was the key here to fix both bugs

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceDataModelServiceImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceDataModelServiceImpl.java
@@ -22,7 +22,6 @@ import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.LiteralSql;
 import com.elster.jupiter.orm.OrmService;
 import com.elster.jupiter.orm.UnderlyingSQLFailedException;
-import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
 import com.elster.jupiter.servicecall.ServiceCallService;
 import com.elster.jupiter.transaction.TransactionService;
@@ -35,6 +34,7 @@ import com.elster.jupiter.util.json.JsonService;
 import com.elster.jupiter.util.sql.SqlBuilder;
 import com.elster.jupiter.validation.ValidationService;
 import com.energyict.mdc.device.config.DeviceConfigurationService;
+import com.energyict.mdc.device.config.LockService;
 import com.energyict.mdc.device.data.BatchService;
 import com.energyict.mdc.device.data.DeviceDataServices;
 import com.energyict.mdc.device.data.DeviceMessageService;
@@ -148,6 +148,7 @@ public class DeviceDataModelServiceImpl implements DeviceDataModelService, Trans
     private volatile MetrologyConfigurationService metrologyConfigurationService;
     private volatile ServiceCallService serviceCallService;
     private volatile ThreadPrincipalService threadPrincipalService;
+    private volatile LockService lockService;
 
     private ServerConnectionTaskService connectionTaskService;
     private ConnectionTaskReportService connectionTaskReportService;
@@ -180,7 +181,7 @@ public class DeviceDataModelServiceImpl implements DeviceDataModelService, Trans
             SecurityPropertyService securityPropertyService, UserService userService, DeviceMessageSpecificationService deviceMessageSpecificationService, MeteringGroupsService meteringGroupsService,
             QueryService queryService, TaskService mdcTaskService, MasterDataService masterDataService,
             TransactionService transactionService, JsonService jsonService, com.energyict.mdc.issues.IssueService mdcIssueService, MdcReadingTypeUtilService mdcReadingTypeUtilService,
-            UpgradeService upgradeService, MetrologyConfigurationService metrologyConfigurationService, ServiceCallService serviceCallService, ThreadPrincipalService threadPrincipalService) {
+            UpgradeService upgradeService, MetrologyConfigurationService metrologyConfigurationService, ServiceCallService serviceCallService, ThreadPrincipalService threadPrincipalService, LockService lockService) {
         this();
         setOrmService(ormService);
         setEventService(eventService);
@@ -215,6 +216,7 @@ public class DeviceDataModelServiceImpl implements DeviceDataModelService, Trans
         setMetrologyConfigurationService(metrologyConfigurationService);
         setServiceCallService(serviceCallService);
         setThreadPrincipalService(threadPrincipalService);
+        setLockService(lockService);
         activate(bundleContext);
     }
 
@@ -507,6 +509,11 @@ public class DeviceDataModelServiceImpl implements DeviceDataModelService, Trans
         this.metrologyConfigurationService = metrologyConfigurationService;
     }
 
+    @Reference
+    public void setLockService(LockService lockService){
+        this.lockService = lockService;
+    }
+
     private Module getModule() {
         return new AbstractModule() {
             @Override
@@ -560,6 +567,7 @@ public class DeviceDataModelServiceImpl implements DeviceDataModelService, Trans
                 bind(UserPreferencesService.class).toInstance(userService.getUserPreferencesService());
                 bind(ThreadPrincipalService.class).toInstance(threadPrincipalService);
                 bind(DeviceMessageService.class).toInstance(deviceMessageService);
+                bind(LockService.class).toInstance(lockService);
             }
         };
     }
--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceImpl.java
@@ -83,6 +83,7 @@ import com.energyict.mdc.device.config.DeviceConfigurationService;
 import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.config.GatewayType;
 import com.energyict.mdc.device.config.LoadProfileSpec;
+import com.energyict.mdc.device.config.LockService;
 import com.energyict.mdc.device.config.LogBookSpec;
 import com.energyict.mdc.device.config.NumericalRegisterSpec;
 import com.energyict.mdc.device.config.PartialConnectionInitiationTask;
@@ -253,6 +254,7 @@ public class DeviceImpl implements Device, ServerDeviceForConfigChange, ServerDe
     private final MeteringGroupsService meteringGroupsService;
     private final CustomPropertySetService customPropertySetService;
     private final ServerDeviceService deviceService;
+    private final LockService lockService;
 
     private final MdcReadingTypeUtilService readingTypeUtilService;
     private final ThreadPrincipalService threadPrincipalService;
@@ -353,7 +355,9 @@ public class DeviceImpl implements Device, ServerDeviceForConfigChange, ServerDe
             MdcReadingTypeUtilService readingTypeUtilService,
             ThreadPrincipalService threadPrincipalService,
             UserPreferencesService userPreferencesService,
-            DeviceConfigurationService deviceConfigurationService, ServerDeviceService deviceService) {
+            DeviceConfigurationService deviceConfigurationService,
+            ServerDeviceService deviceService,
+            LockService lockService) {
         this.dataModel = dataModel;
         this.eventService = eventService;
         this.issueService = issueService;
@@ -375,6 +379,7 @@ public class DeviceImpl implements Device, ServerDeviceForConfigChange, ServerDe
         this.userPreferencesService = userPreferencesService;
         this.deviceConfigurationService = deviceConfigurationService;
         this.deviceService = deviceService;
+        this.lockService = lockService;
         // Helper to get activation info... from 'Kore'
         this.koreHelper = new SyncDeviceWithKoreForInfo(this, this.deviceService, this.readingTypeUtilService, clock, this.eventService);
         this.koreHelper.syncWithKore(this);
@@ -398,6 +403,18 @@ public class DeviceImpl implements Device, ServerDeviceForConfigChange, ServerDe
 
     ValidationService getValidationService() {
         return validationService;
+    }
+
+    ServerDeviceService getDeviceService() {
+        return deviceService;
+    }
+
+    DeviceConfigurationService getDeviceConfigurationService() {
+        return deviceConfigurationService;
+    }
+
+    LockService getLockService(){
+        return lockService;
     }
 
     private void setDeviceTypeFromDeviceConfiguration() {
