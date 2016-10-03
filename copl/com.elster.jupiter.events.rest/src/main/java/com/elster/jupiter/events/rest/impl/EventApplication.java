705748b3592aeb4dda7ac105735edc44453c7fa3
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-10-03 16:27:03 +0200

    CXO-3135: Avoid creating a Thesaurus you don't need

--- copl/com.elster.jupiter.events.rest/src/main/java/com/elster/jupiter/events/rest/impl/EventApplication.java
+++ copl/com.elster.jupiter.events.rest/src/main/java/com/elster/jupiter/events/rest/impl/EventApplication.java
@@ -1,11 +1,9 @@
 package com.elster.jupiter.events.rest.impl;
 
 import com.elster.jupiter.events.EventService;
-import com.elster.jupiter.nls.Layer;
-import com.elster.jupiter.nls.NlsService;
-import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.rest.util.RestQueryService;
 import com.elster.jupiter.transaction.TransactionService;
+
 import com.google.common.collect.ImmutableSet;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.osgi.service.component.annotations.Component;
@@ -15,14 +13,12 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-@Component(name = "com.elster.jupiter.event.rest", service = Application.class, immediate = true, property = {"alias=/evt", "app=SYS", "name=" + EventApplication.COMPONENT_NAME})
+@Component(name = "com.elster.jupiter.event.rest", service = Application.class, immediate = true, property = {"alias=/evt", "app=SYS", "name=" + EventService.COMPONENTNAME})
 public class EventApplication extends Application {
-    public static final String COMPONENT_NAME = "EVT";
 
     private volatile EventService eventService;
     private volatile TransactionService transactionService;
     private volatile RestQueryService restQueryService;
-    private volatile Thesaurus thesaurus;
 
     public Set<Class<?>> getClasses() {
         return ImmutableSet.of(EventTypeResource.class);
@@ -43,11 +39,6 @@ public class EventApplication extends Application {
         this.restQueryService = restQueryService;
     }
 
-    @Reference
-    public void setNlsService(NlsService nlsService) {
-        this.thesaurus = nlsService.getThesaurus(COMPONENT_NAME, Layer.REST);
-    }
-
     @Override
     public Set<Object> getSingletons() {
         Set<Object> singletons = new HashSet<>();
@@ -62,7 +53,6 @@ public class EventApplication extends Application {
             bind(restQueryService).to(RestQueryService.class);
             bind(transactionService).to(TransactionService.class);
             bind(eventService).to(EventService.class);
-            bind(thesaurus).to(Thesaurus.class);
         }
     }
 }
