94d79cd1be054c40b328a04ce2b74245d976df48
Artem Puchenkin <apuchenkin@gmail.com>
2016-10-04 15:56:47 +0300

    [CXO-2802] According to CIM MRID should be an UUID

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceConnectionMethods.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceConnectionMethods.js
@@ -295,7 +295,7 @@ Ext.define('Mdc.controller.setup.DeviceConnectionMethods', {
                 connectionMethodsStore.load({
                     params:{
                         available: true,
-                        deviceId: deviceId
+                        deviceId: device.getId()
                     },
                     callback: function (records) {
                         if(connectionMethodsStore.count() > 0) {
