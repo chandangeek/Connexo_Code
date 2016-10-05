090ab6503df956535ef078d6bb46c94cf9b1589c
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-10-05 16:22:49 +0200

    CXO-3135: Avoid usage of Thesaurus#getString(String, String) as much as possible

--- copl/com.elster.jupiter.search.rest/src/main/java/com/elster/jupiter/search/rest/MessageSeeds.java
+++ copl/com.elster.jupiter.search.rest/src/main/java/com/elster/jupiter/search/rest/MessageSeeds.java
@@ -1,12 +1,9 @@
 package com.elster.jupiter.search.rest;
 
-import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.search.SearchService;
 import com.elster.jupiter.util.exception.MessageSeed;
 
-import java.text.MessageFormat;
 import java.util.logging.Level;
-
 
 public enum MessageSeeds implements MessageSeed {
 
@@ -50,24 +47,4 @@ public enum MessageSeeds implements MessageSeed {
         return Level.SEVERE;
     }
 
-    public String getFormated(Object... args){
-        return MessageFormat.format(this.getDefaultFormat(), args);
-    }
-
-    public static String getString(MessageSeed messageSeed, Thesaurus thesaurus, Object... args){
-        String text = thesaurus.getString(messageSeed.getKey(), messageSeed.getDefaultFormat());
-        return MessageFormat.format(text, args);
-    }
-
-
-    public static MessageSeeds getByKey(String key) {
-        if (key != null) {
-            for (MessageSeeds column : MessageSeeds.values()) {
-                if (column.getKey().equals(key)) {
-                    return column;
-                }
-            }
-        }
-        return null;
-    }
 }
