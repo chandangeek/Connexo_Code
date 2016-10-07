fadb07a7e2b905dd351bfd9267240b83b2d027dc
Lobanov Dmitry <dlobanov@mera.ru>
2016-10-07 15:16:47 +0300

    CXO-2802 issues

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/DeviceResource.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/DeviceResource.java
@@ -76,8 +76,7 @@ public class DeviceResource {
         String dbSearchText = (searchText != null && !searchText.isEmpty()) ? ("*" + searchText + "*") : "*";
         MeterFilter filter = new MeterFilter();
         filter.setName(dbSearchText);
-        filter.addState("dlc.default.removed");
-        filter.addState("dlc.default.decommissioned");
+        filter.setExcludedStates("dlc.default.decommissioned", "dlc.default.removed");
         Finder<Meter> finder = meteringService.findMeters(filter);
         return Response.ok().entity(toMeterInfos(params.getStart().isPresent()
                 && params.getLimit().isPresent() ? finder.from(params).find() : finder.paged(start, limit).find(), start, limit)).build();
