fd0e6575d1e86ccc2202c3f2aff9f723a1499235
Albert Versin <albert.versin@elster.com>
2016-10-14 13:58:31 +0300

    CXO-3115 Added support for migration from 10.1

--- comu/com.energyict.mdc.device.lifecycle.config/src/main/java/com/energyict/mdc/device/lifecycle/config/impl/UpgraderV10_2.java
+++ comu/com.energyict.mdc.device.lifecycle.config/src/main/java/com/energyict/mdc/device/lifecycle/config/impl/UpgraderV10_2.java
@@ -24,6 +24,7 @@ public class UpgraderV10_2 implements Upgrader {
     public void migrate(DataModelUpgrader dataModelUpgrader) {
         List<String> sql = new ArrayList<>();
         sql.add("UPDATE DLD_AUTHORIZED_ACTION SET DISCRIMINATOR = 0 WHERE DISCRIMINATOR = 1");
+        sql.add("UPDATE DLD_DEVICE_LIFE_CYCLE SET MAXFUTUREEFFTIMESHIFTVALUE = 1, MAXPASTEFFTIMESHIFTVALUE = 30 WHERE FSM IN (SELECT ID FROM FSM_FINITE_STATE_MACHINE WHERE NAME = 'dlc.standard.device.life.cycle')");
         dataModel.useConnectionRequiringTransaction(connection -> {
             try (Statement statement = connection.createStatement()) {
                 sql.forEach(sqlCommand -> execute(statement, sqlCommand));
