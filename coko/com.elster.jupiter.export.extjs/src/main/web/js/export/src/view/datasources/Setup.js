4ecfc33dad953dd79520e9e39ac9a6e7770e9129
Sentyurin Sergey <ssentyur@mera.ru>
2016-10-20 14:43:24 +0300

    CXO-3645 [FE] View data sources of usage point readings data export task

--- coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/datasources/Setup.js
+++ coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/datasources/Setup.js
@@ -40,10 +40,16 @@ Ext.define('Dxp.view.datasources.Setup', {
                     emptyComponent: {
                         xtype: 'no-items-found-panel',
                         title: Uni.I18n.translate('dataSources.empty.title', 'DES', 'No data sources found'),
-                        reasons: [
-                            Uni.I18n.translate('dataSources.empty.list.item1', 'DES', 'The data export task has never run.'),
-                            Uni.I18n.translate('dataSources.empty.list.item2', 'DES', 'The device group does not contain any devices.'),
-                            Uni.I18n.translate('dataSources.empty.list.item3', 'DES', 'None of the devices in the group has a data source matching one of the selected reading types.')
+                        reasons: Uni.util.Application.getAppName() === 'MultiSense'
+                            ? [
+                            Uni.I18n.translate('dataSources.device.empty.list.item1', 'DES', 'The data export task has never run.'),
+                            Uni.I18n.translate('dataSources.device.empty.list.item2', 'DES', 'The device group does not contain any devices.'),
+                            Uni.I18n.translate('dataSources.device.empty.list.item3', 'DES', 'None of the devices in the group has a data source matching one of the selected reading types.')
+                        ]
+                            : [
+                            Uni.I18n.translate('dataSources.usagePoint.empty.list.item1', 'DES', 'The data export task has never run.'),
+                            Uni.I18n.translate('dataSources.usagePoint.empty.list.item2', 'DES', 'The usage point group does not contain any usage points.'),
+                            Uni.I18n.translate('dataSources.usagePoint.empty.list.item3', 'DES', 'None of the usage points in the group has data source matching one of the selected reading types.')
                         ]
                     }
                 }
