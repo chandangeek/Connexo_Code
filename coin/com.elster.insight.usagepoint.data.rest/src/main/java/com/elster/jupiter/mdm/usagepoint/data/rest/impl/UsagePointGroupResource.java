de20a5b0c8b284b8494db01fd0175bbfc42f1711
Yan R <yaratay@mera.ru>
2016-10-28 16:57:55 +0300

    CXO-3552: [BE] View usage point groups

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/UsagePointGroupResource.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/UsagePointGroupResource.java
@@ -1,12 +1,31 @@
 package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
 
+import com.elster.jupiter.domain.util.Query;
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.groups.MeteringGroupsService;
+import com.elster.jupiter.metering.groups.QueryUsagePointGroup;
+import com.elster.jupiter.metering.groups.UsagePointGroup;
 import com.elster.jupiter.rest.util.ExceptionFactory;
+import com.elster.jupiter.rest.util.JsonQueryFilter;
+import com.elster.jupiter.rest.util.JsonQueryParameters;
+import com.elster.jupiter.rest.util.PagedInfoList;
+import com.elster.jupiter.rest.util.Transactional;
 import com.elster.jupiter.search.SearchService;
+import com.elster.jupiter.util.conditions.Condition;
+import com.elster.jupiter.util.conditions.Order;
 
 import javax.inject.Inject;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Optional;
+
+import static com.elster.jupiter.util.conditions.Where.where;
 
 @Path("/usagepointgroups")
 public class UsagePointGroupResource {
@@ -27,6 +46,42 @@ public class UsagePointGroupResource {
         this.exceptionFactory = exceptionFactory;
         this.usagePointGroupInfoFactory = usagePointGroupInfoFactory;
         this.resourceHelper = resourceHelper;
+    }
+
+    @GET
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @Consumes(MediaType.APPLICATION_JSON)
+    // not protected by privileges yet because a combo-box containing all the groups needs to be shown when creating an export task
+    public PagedInfoList getUsagePointGroups(@QueryParam("type") String typeName, @BeanParam JsonQueryFilter filter, @BeanParam JsonQueryParameters queryParameters) {
+        Query<UsagePointGroup> query = getUsagePointGroupQueryByType(typeName);
+        Condition condition = buildCondition(filter);
+        Order order = Order.ascending("upper(name)");
+        List<UsagePointGroup> usagePointGroups;
+        Optional<Integer> start = queryParameters.getStart();
+        Optional<Integer> limit = queryParameters.getLimit();
+        if (start.isPresent() && limit.isPresent()) {
+            int from = start.get() + 1;
+            usagePointGroups = query.select(condition, from, from + limit.get(), order);
+        } else {
+            usagePointGroups = query.select(condition, order);
+        }
+        List<UsagePointGroupInfo> usagePointGroupInfoList = usagePointGroupInfoFactory.from(usagePointGroups);
+        return PagedInfoList.fromPagedList("usagepointgroups", usagePointGroupInfoList, queryParameters);
+    }
+
+    private Query<UsagePointGroup> getUsagePointGroupQueryByType(@QueryParam("type") String typeName) {
+        if (QueryUsagePointGroup.class.getSimpleName().equalsIgnoreCase(typeName)) {
+            return meteringGroupsService.getQueryUsagePointGroupQuery();
+        } else {
+            return meteringGroupsService.getUsagePointGroupQuery();
+        }
+    }
+
+    private Condition buildCondition(JsonQueryFilter filter) {
+        return filter.hasProperty("name") ?
+                where("name").isEqualTo(filter.getString("name")) :
+                Condition.TRUE;
     }
 
 }
