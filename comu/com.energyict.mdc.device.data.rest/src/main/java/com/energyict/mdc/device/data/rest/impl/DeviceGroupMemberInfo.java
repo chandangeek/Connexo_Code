db26c44fca782ac63b3967d2adae905dfb556d69
Yan R <yaratay@mera.ru>
2016-10-28 16:20:29 +0300

    CXO-3305: Manage usage point Rest resource for UsagePoint groups

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceGroupMemberInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceGroupMemberInfo.java
@@ -1,7 +1,5 @@
 package com.energyict.mdc.device.data.rest.impl;
 
-import com.elster.jupiter.metering.AmrSystem;
-import com.elster.jupiter.metering.groups.EndDeviceGroup;
 import com.energyict.mdc.device.data.Device;
 
 import java.util.List;
@@ -15,20 +13,6 @@ public class DeviceGroupMemberInfo {
     public String deviceTypeName;
     public String deviceConfigurationName;
     
-    public static DeviceGroupMemberInfo from(Device device, EndDeviceGroup endDeviceGroup) {
-        DeviceGroupMemberInfo deviceInfo = new DeviceGroupMemberInfo();
-        deviceInfo.id = device.getId();
-        deviceInfo.mRID = device.getmRID();
-        deviceInfo.serialNumber = device.getSerialNumber();
-        deviceInfo.deviceTypeName = device.getDeviceType().getName();
-        deviceInfo.deviceConfigurationName = device.getDeviceConfiguration().getName();
-        return deviceInfo;
-    }
-    
-    public static List<DeviceGroupMemberInfo> from(List<Device> devices, EndDeviceGroup endDeviceGroup, AmrSystem amrSystem) {
-        return devices.stream().map((d) -> from(d, endDeviceGroup)).collect(Collectors.toList());
-    }
-
     public static DeviceGroupMemberInfo from(Device device) {
         DeviceGroupMemberInfo deviceInfo = new DeviceGroupMemberInfo();
         deviceInfo.id = device.getId();
@@ -40,6 +24,6 @@ public class DeviceGroupMemberInfo {
     }
     
     public static List<DeviceGroupMemberInfo> from(List<Device> devices) {
-        return devices.stream().map((d) -> from(d)).collect(Collectors.toList());
+        return devices.stream().map(DeviceGroupMemberInfo::from).collect(Collectors.toList());
     }
 }
