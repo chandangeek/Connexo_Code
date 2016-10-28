33fa2187ee128238bb0edf62f549d8db207fff94
Anton Fomchenko <antfom@mera.ru>
2016-10-28 14:43:42 +0300

    CXO-3309, CXO-3841 count active rules

--- coin/com.elster.insight.usagepoint.config.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/config/rest/impl/EstimationRuleSetInfo.java
+++ coin/com.elster.insight.usagepoint.config.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/config/rest/impl/EstimationRuleSetInfo.java
@@ -1,24 +1,24 @@
 package com.elster.jupiter.mdm.usagepoint.config.rest.impl;
 
+import com.elster.jupiter.estimation.EstimationRule;
 import com.elster.jupiter.estimation.EstimationRuleSet;
-import com.elster.jupiter.validation.ValidationRuleSet;
-import com.elster.jupiter.validation.ValidationRuleSetVersion;
-import com.elster.jupiter.validation.ValidationVersionStatus;
-import com.elster.jupiter.validation.rest.ValidationRuleSetVersionInfo;
 
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.Optional;
 
 @XmlRootElement
 public class EstimationRuleSetInfo {
     public long id;
     public String name;
     public String description;
+    public long activeRules;
+    public long inactiveRules;
 
     public EstimationRuleSetInfo(EstimationRuleSet estimationRuleSet) {
         id = estimationRuleSet.getId();
         name = estimationRuleSet.getName();
         description = estimationRuleSet.getDescription();
+        activeRules = estimationRuleSet.getRules().stream().filter(EstimationRule::isActive).count();
+        inactiveRules = estimationRuleSet.getRules().size() - activeRules;
     }
 
     public EstimationRuleSetInfo() {
