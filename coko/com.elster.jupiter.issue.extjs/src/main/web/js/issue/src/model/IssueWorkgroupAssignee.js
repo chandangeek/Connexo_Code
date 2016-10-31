a0043ea4a0afe31abf1a73838c11f10065603247
Mihai Andreescu <mihai.andreescu@elster.com>
2016-10-31 15:10:45 +0200

    merge

---
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/model/IssueWorkgroupAssignee.js
@@ -0,0 +1,13 @@
+Ext.define('Isu.model.IssueWorkgroupAssignee', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {
+            name: 'id',
+            type: 'int'
+        },
+        {
+            name: 'name',
+            type: 'auto'
+        }
+    ]
+});
