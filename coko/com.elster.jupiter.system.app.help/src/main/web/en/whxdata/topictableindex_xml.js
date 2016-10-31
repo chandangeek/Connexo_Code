f06ef4f328db0b40660c88c90041c6b4e813338c
Lobanov Dmitry <dlobanov@mera.ru>
2016-10-31 11:30:01 +0300

    Update for Connexo 10.2 release

--- coko/com.elster.jupiter.system.app.help/src/main/web/en/whxdata/topictableindex_xml.js
+++ coko/com.elster.jupiter.system.app.help/src/main/web/en/whxdata/topictableindex_xml.js
@@ -1,1 +1,1 @@
-gXMLBuffer="<?xml version=\"1.0\" encoding=\"UTF-8\"?><cki><ck fm=\"01 Cover Page\" to=\"Manage service call custom attribute sets\" num=\"43\"/><ck fm=\"Process management\" to=\"Legal notices\" num=\"4\"/></cki>";
+gXMLBuffer="<?xml version=\"1.0\" encoding=\"UTF-8\"?><cki><ck fm=\"01 Cover Page\" to=\"Configure relative periods\" num=\"42\"/><ck fm=\"Reading types\" to=\"Legal notices\" num=\"19\"/></cki>";
