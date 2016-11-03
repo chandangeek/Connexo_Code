a936f74f9b358f48484f204dd1ea8b237f07708b
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-11-03 15:52:10 +0100

    CXO-2951: Continue converting com.energyict.protocolimpl.base.AbstractProtocol hierachy to upl meter protocol interface: CM10

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/CM10.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/cm10/CM10.java
@@ -1,7 +1,7 @@
 package com.energyict.protocolimpl.cm10;
 
-import com.energyict.mdc.upl.properties.InvalidPropertyException;
-import com.energyict.mdc.upl.properties.MissingPropertyException;
+import com.energyict.mdc.upl.properties.PropertySpec;
+import com.energyict.mdc.upl.properties.PropertyValidationException;
 
 import com.energyict.dialer.core.HalfDuplexController;
 import com.energyict.obis.ObisCode;
@@ -12,20 +12,20 @@ import com.energyict.protocol.RegisterValue;
 import com.energyict.protocolimpl.base.AbstractProtocol;
 import com.energyict.protocolimpl.base.Encryptor;
 import com.energyict.protocolimpl.base.ProtocolConnection;
+import com.energyict.protocolimpl.properties.UPLPropertySpecFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
 public class CM10 extends AbstractProtocol {
 
-    static final String IS_C10_METER = "CM_10_meter";
+    private static final String IS_C10_METER = "CM_10_meter";
     private static final int MAX_CLOCK_DEVIATION = 59;  // max 59 sec deviation
 
     private CM10Connection cm10Connection = null;
@@ -44,62 +44,70 @@ public class CM10 extends AbstractProtocol {
 
     private boolean isCM10Meter;
 
+    @Override
     public ProfileData getProfileData(Date from, Date to, boolean includeEvents) throws IOException {
         return getCM10Profile().getProfileData(from, to, includeEvents);
     }
 
+    @Override
     public ProfileData getProfileData(Date lastReading, boolean includeEvents) throws IOException {
     	Calendar cal=Calendar.getInstance(getTimeZone());
 		return getProfileData(lastReading, cal.getTime(), includeEvents);
     }
 
+    @Override
     public int getProfileInterval() throws IOException {
 		return 60 * getFullPersonalityTable().getIntervalInMinutes();
     }
 
-    public int getOutstationId() {
+    int getOutstationId() {
     	return this.outstationID;
     }
 
+    @Override
     public RegisterInfo translateRegister(ObisCode obisCode) throws IOException {
         return ObisCodeMapper.getRegisterInfo(obisCode);
     }
 
+    @Override
     public RegisterValue readRegister(ObisCode obisCode) throws IOException {
         return obisCodeMapper.getRegisterValue(obisCode);
     }
 
+    @Override
     public int getNumberOfChannels() throws IOException {
         return getStatusTable().getNumberOfChannels();
     }
 
-
+    @Override
 	protected void doConnect() throws IOException {
 		ProtocolUtils.delayProtocol(delayAfterConnect);
 	}
 
-    public void setProperties(Properties properties) throws InvalidPropertyException, MissingPropertyException {
-        try {
-            this.outstationID = Integer.parseInt(properties.getProperty("SerialNumber"));
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("The node address field has not been filled in");
-        }
+    @Override
+    public List<PropertySpec> getPropertySpecs() {
+        List<PropertySpec> propertySpecs = new ArrayList<>(super.getPropertySpecs());
+        propertySpecs.add(UPLPropertySpecFactory.integer("DelayAfterConnect", false));
+        propertySpecs.add(UPLPropertySpecFactory.integer(IS_C10_METER, false));
+        return propertySpecs;
+    }
 
+    @Override
+    protected boolean serialNumberIsRequired() {
+        return true;
+    }
+
+    @Override
+    public void setProperties(Properties properties) throws PropertyValidationException {
+        super.setProperties(properties);
+        this.outstationID = Integer.parseInt(properties.getProperty("SerialNumber"));
         setInfoTypeTimeoutProperty(Integer.parseInt(properties.getProperty(PROP_TIMEOUT, "5000").trim()));
         setInfoTypeProtocolRetriesProperty(Integer.parseInt(properties.getProperty(PROP_RETRIES, "3").trim()));
         this.delayAfterConnect = Integer.parseInt(properties.getProperty("DelayAfterConnect", "1000"));
-        this.isCM10Meter = !"0".equals(properties.getProperty("CM_10_meter"));
+        this.isCM10Meter = !"0".equals(properties.getProperty(IS_C10_METER));
     }
 
-    public List<String> getOptionalKeys() {
-		return Arrays.asList(
-		            "Timeout",
-		            "Retries",
-		            "DelayAfterConnect",
-		            IS_C10_METER);
-	}
-
-	public PowerFailDetailsTable getPowerFailDetailsTable() throws IOException {
+	PowerFailDetailsTable getPowerFailDetailsTable() throws IOException {
 		if (powerFailDetailsTable == null) {
 			getLogger().info("read power fail details");
 			Response response = commandFactory.getReadPowerFailDetailsCommand().invoke();
@@ -109,7 +117,7 @@ public class CM10 extends AbstractProtocol {
 		return powerFailDetailsTable;
 	}
 
-	public CurrentDialReadingsTable getCurrentDialReadingsTable() throws IOException {
+	CurrentDialReadingsTable getCurrentDialReadingsTable() throws IOException {
 		if (currentDialReadingsTable == null) {
 			getLogger().info("read current dial readings");
 			Response response = commandFactory.getReadCurrentDialReadingsCommand().invoke();
@@ -119,7 +127,7 @@ public class CM10 extends AbstractProtocol {
 		return currentDialReadingsTable;
 	}
 
-	public FullPersonalityTable getFullPersonalityTable() throws IOException {
+	FullPersonalityTable getFullPersonalityTable() throws IOException {
 		if (fullPersonalityTable == null) {
 			getLogger().info("read full personality table");
 			Response response = commandFactory.getReadFullPersonalityTableCommand().invoke();
@@ -129,7 +137,7 @@ public class CM10 extends AbstractProtocol {
 		return fullPersonalityTable;
 	}
 
-	public StatusTable getStatusTable() throws IOException {
+	StatusTable getStatusTable() throws IOException {
 		if (statusTable == null) {
 			getLogger().info("read status");
 			Response response = commandFactory.getReadStatusCommand().invoke();
@@ -139,7 +147,7 @@ public class CM10 extends AbstractProtocol {
 		return statusTable;
 	}
 
-	public TimeTable getTimeTable() throws IOException {
+	private TimeTable getTimeTable() throws IOException {
 		getLogger().info("read meter time");
 		Response response = commandFactory.getReadTimeCommand().invoke();
 		TimeTable timeTable = new TimeTable(this);
@@ -147,11 +155,8 @@ public class CM10 extends AbstractProtocol {
 		return timeTable;
 	}
 
+    @Override
 	protected void doDisconnect() throws IOException {
-	}
-
-	protected List<String> doGetOptionalKeys() {
-		return Collections.emptyList();
 	}
 
 	public RegisterFactory getRegisterFactory() throws IOException {
@@ -162,6 +167,7 @@ public class CM10 extends AbstractProtocol {
         return registerFactory;
     }
 
+    @Override
     protected ProtocolConnection doInit(InputStream inputStream, OutputStream outputStream, int timeoutProperty, int protocolRetriesProperty, int forcedDelay, int echoCancelling,
                                         int protocolCompatible, Encryptor encryptor, HalfDuplexController halfDuplexController) throws IOException {
         this.cm10Connection = new CM10Connection(
@@ -179,14 +185,11 @@ public class CM10 extends AbstractProtocol {
         return this.cm10Connection;
     }
 
-    public CM10Profile getCM10Profile() {
+    private CM10Profile getCM10Profile() {
         return cm10Profile;
     }
 
-    protected void doValidateProperties(Properties properties) throws MissingPropertyException, InvalidPropertyException {
-
-    }
-
+    @Override
     public String getFirmwareVersion() throws IOException {
 		if (!isCM10Meter) {
 			return "CM32 (firmware version not available)";
@@ -203,22 +206,21 @@ public class CM10 extends AbstractProtocol {
 		}
 	}
 
+    @Override
     public String getProtocolVersion() {
 		return "$Date: 2015-11-26 15:23:39 +0200 (Thu, 26 Nov 2015)$";
 	}
 
+    @Override
 	public Date getTime() throws IOException {
 		return getTimeTable().getTime();
 	}
 
-    /**
-     * Set time is only possible on commissioning or after loading a new personality table
-     * Use only trimmer.
-     * The value sent to the meter is added on the RTC value in the meter
-     *
-     * @throws IOException
-     */
+    @Override
     public void setTime() throws IOException {
+        /* Set time is only possible on commissioning or after loading a new personality table
+         * Use only trimmer.
+         * The value sent to the meter is added on the RTC value in the meter */
         byte result;
         Calendar systemTimeCal = Calendar.getInstance(getTimeZone());
         Calendar meterTimeCal = Calendar.getInstance(getTimeZone());
@@ -242,7 +244,7 @@ public class CM10 extends AbstractProtocol {
         return commandFactory;
     }
 
-    protected  final CM10Connection getCM10Connection() {
+    final CM10Connection getCM10Connection() {
         return cm10Connection;
     }
 
