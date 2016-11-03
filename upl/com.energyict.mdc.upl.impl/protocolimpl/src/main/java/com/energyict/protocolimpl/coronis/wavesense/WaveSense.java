5a53e5bcc28ac256c8b6b9e5df541d5607e2a162
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-11-03 10:04:31 +0100

    CXO-2951: Continue converting com.energyict.protocolimpl.base.AbstractProtocol hierachy to upl meter protocol interface: WaveSense

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/coronis/wavesense/WaveSense.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/coronis/wavesense/WaveSense.java
@@ -1,8 +1,7 @@
 package com.energyict.protocolimpl.coronis.wavesense;
 
-import com.energyict.mdc.upl.UnsupportedException;
-import com.energyict.mdc.upl.properties.InvalidPropertyException;
-import com.energyict.mdc.upl.properties.MissingPropertyException;
+import com.energyict.mdc.upl.properties.PropertySpec;
+import com.energyict.mdc.upl.properties.PropertyValidationException;
 
 import com.energyict.dialer.core.HalfDuplexController;
 import com.energyict.obis.ObisCode;
@@ -28,6 +27,7 @@ import com.energyict.protocolimpl.coronis.wavesense.core.AlarmFrameParser;
 import com.energyict.protocolimpl.coronis.wavesense.core.ObisCodeMapper;
 import com.energyict.protocolimpl.coronis.wavesense.core.parameter.ParameterFactory;
 import com.energyict.protocolimpl.coronis.wavesense.core.radiocommand.RadioCommandFactory;
+import com.energyict.protocolimpl.properties.UPLPropertySpecFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,6 +39,9 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Properties;
 
+import static com.energyict.mdc.upl.MeterProtocol.Property.CORRECTTIME;
+import static com.energyict.mdc.upl.MeterProtocol.Property.TIMEOUT;
+
 public class WaveSense extends AbstractProtocol implements MessageProtocol, ProtocolLink, EventMapper {
 
     private boolean verifyProfileInterval = false;
@@ -47,7 +50,6 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
     private RadioCommandFactory radioCommandFactory;
     private ParameterFactory parameterFactory = null;
     private WaveSenseMessages waveSenseMessages = new WaveSenseMessages(this);
-
 
     private int correctTime;
     private ObisCode loadProfileObisCode;
@@ -67,7 +69,7 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
         return parameterFactory;
     }
 
-    final public RadioCommandFactory getRadioCommandFactory() {
+    public final RadioCommandFactory getRadioCommandFactory() {
         return radioCommandFactory;
     }
 
@@ -78,14 +80,14 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
         }
     }
 
-    public int getNumberOfChannels() throws UnsupportedException, IOException {
+    @Override
+    public int getNumberOfChannels() throws IOException {
         return WAVESENSE_NUMBER_OF_CHANNELS;
     }
 
     @Override
     protected void doDisconnect() throws IOException {
     }
-
 
     @Override
     protected ProtocolConnection doInit(InputStream inputStream,
@@ -104,10 +106,19 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
     }
 
     @Override
-    protected void doValidateProperties(Properties properties) throws MissingPropertyException, InvalidPropertyException {
-        setInfoTypeTimeoutProperty(Integer.parseInt(properties.getProperty("Timeout", "40000").trim()));
+    public List<PropertySpec> getPropertySpecs() {
+        List<PropertySpec> propertySpecs = new ArrayList<>(super.getPropertySpecs());
+        propertySpecs.add(UPLPropertySpecFactory.integer("verifyProfileInterval", false));
+        propertySpecs.add(UPLPropertySpecFactory.string("LoadProfileObisCode", false));
+        return propertySpecs;
+    }
+
+    @Override
+    public void setProperties(Properties properties) throws PropertyValidationException {
+        super.setProperties(properties);
+        setInfoTypeTimeoutProperty(Integer.parseInt(properties.getProperty(TIMEOUT.getName(), "40000").trim()));
         setLoadProfileObisCode(ObisCode.fromString(properties.getProperty("LoadProfileObisCode", "0.0.99.1.0.255")));
-        correctTime = Integer.parseInt(properties.getProperty(com.energyict.mdc.upl.MeterProtocol.Property.CORRECTTIME.getName(), "0"));
+        correctTime = Integer.parseInt(properties.getProperty(CORRECTTIME.getName(), "0"));
         verifyProfileInterval = Integer.parseInt(properties.getProperty("verifyProfileInterval", "1")) == 1;
     }
 
@@ -148,11 +159,8 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
         getRadioCommandFactory().writeTimeDateRTC(cal.getTime());
     }
 
-    final public void writeSamplingRate() throws IOException {
-        getParameterFactory().writeSamplingPeriod(getProfileInterval());
-    }
-
-    public int getProfileInterval() throws UnsupportedException, IOException {
+    @Override
+    public int getProfileInterval() throws IOException {
         if (isVerifyProfileInterval()) {
             return getParameterFactory().getProfileIntervalInSeconds();
         } else {
@@ -160,7 +168,8 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
         }
     }
 
-    public ProfileData getProfileData(Date lastReading, Date toDate, boolean includeEvents) throws IOException, UnsupportedException {
+    @Override
+    public ProfileData getProfileData(Date lastReading, Date toDate, boolean includeEvents) throws IOException {
         try {
             return profileDataReader.getProfileData(lastReading, toDate, includeEvents);
         }
@@ -170,8 +179,9 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
         }
     }
 
+    @Override
     public RegisterInfo translateRegister(ObisCode obisCode) throws IOException {
-        return obisCodeMapper.getRegisterInfo(obisCode);
+        return ObisCodeMapper.getRegisterInfo(obisCode);
     }
 
     @Override
@@ -179,26 +189,32 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
         return obisCodeMapper.getRegisterValue(obisCode);
     }
 
+    @Override
     public void applyMessages(List messageEntries) throws IOException {
         waveSenseMessages.applyMessages(messageEntries);
     }
 
+    @Override
     public MessageResult queryMessage(MessageEntry messageEntry) throws IOException {
         return waveSenseMessages.queryMessage(messageEntry);
     }
 
+    @Override
     public List getMessageCategories() {
         return waveSenseMessages.getMessageCategories();
     }
 
+    @Override
     public String writeMessage(Message msg) {
         return waveSenseMessages.writeMessage(msg);
     }
 
+    @Override
     public String writeTag(MessageTag tag) {
         return waveSenseMessages.writeTag(tag);
     }
 
+    @Override
     public String writeValue(MessageValue value) {
         return waveSenseMessages.writeValue(value);
     }
@@ -212,21 +228,16 @@ public class WaveSense extends AbstractProtocol implements MessageProtocol, Prot
     }
 
     @Override
-    protected List doGetOptionalKeys() {
-        List result = new ArrayList();
-        result.add("verifyProfileInterval");
-        result.add("LoadProfileObisCode");
-        return result;
-    }
-
     public void setHalfDuplexController(HalfDuplexController halfDuplexController) {
         // absorb
     }
 
+    @Override
     public WaveFlowConnect getWaveFlowConnect() {
         return waveSenseConnect;
     }
 
+    @Override
     public List map2MeterEvent(String event) throws IOException {
         List statusAndEvents = new ArrayList();
         AlarmFrameParser alarmFrame = new AlarmFrameParser(this);
