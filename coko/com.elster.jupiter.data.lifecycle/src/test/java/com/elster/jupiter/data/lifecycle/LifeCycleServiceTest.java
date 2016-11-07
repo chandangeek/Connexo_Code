e44eccaefda8a2ce2235c6c2c2f44b0a8ce3ff52
Anton Suslennikov <antsusl@mera.ru>
2016-11-07 17:05:17 +0300

    CXO-3311 Add metering dependency

--- coko/com.elster.jupiter.data.lifecycle/src/test/java/com/elster/jupiter/data/lifecycle/LifeCycleServiceTest.java
+++ coko/com.elster.jupiter.data.lifecycle/src/test/java/com/elster/jupiter/data/lifecycle/LifeCycleServiceTest.java
@@ -27,6 +27,7 @@ import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.upgrade.UpgradeService;
 import com.elster.jupiter.upgrade.impl.UpgradeModule;
+import com.elster.jupiter.usagepoint.lifecycle.config.impl.UsagePointLifeCycleConfigurationModule;
 import com.elster.jupiter.users.impl.UserModule;
 import com.elster.jupiter.util.UtilModule;
 
@@ -53,83 +54,90 @@ import static org.mockito.Mockito.mock;
 @RunWith(MockitoJUnitRunner.class)
 public class LifeCycleServiceTest {
 
-	private InMemoryBootstrapModule inMemoryBootstrapModule = new InMemoryBootstrapModule();
-	private LifeCycleService lifeCycleService;
-	private Injector injector;
+    private InMemoryBootstrapModule inMemoryBootstrapModule = new InMemoryBootstrapModule();
+    private LifeCycleService lifeCycleService;
+    private Injector injector;
 
-	private static class MockModule extends AbstractModule {
-		@Override
+    private static class MockModule extends AbstractModule {
+        @Override
         protected void configure() {
             bind(BundleContext.class).toInstance(mock(BundleContext.class));
             bind(EventAdmin.class).toInstance(mock(EventAdmin.class));
-			bind(SearchService.class).toInstance(mock(SearchService.class));
-			bind(LicenseService.class).toInstance(mock(LicenseService.class));
-			bind(UpgradeService.class).toInstance(UpgradeModule.FakeUpgradeService.getInstance());
-		}
+            bind(SearchService.class).toInstance(mock(SearchService.class));
+            bind(LicenseService.class).toInstance(mock(LicenseService.class));
+            bind(UpgradeService.class).toInstance(UpgradeModule.FakeUpgradeService.getInstance());
+        }
     }
 
     private static final boolean printSql = false;
 
-	@Before
-	public void setUp() throws SQLException {
-		injector = Guice.createInjector(
-				new MockModule(),
-    			inMemoryBootstrapModule,
-    			new OrmModule(),
-				new DataVaultModule(),
-    			new DomainUtilModule(),
-    			new NlsModule(),
-    			new UserModule(),
-    			new UtilModule(),
-    			new ThreadSecurityModule(),
-    			new PubSubModule(),
-    			new TransactionModule(printSql),
-    			new InMemoryMessagingModule(),
-    			new TaskModule(),
-    			new IdsModule(),
-    			new EventsModule(),
-    			new PartyModule(),
+    @Before
+    public void setUp() throws SQLException {
+        injector = Guice.createInjector(
+                new MockModule(),
+                inMemoryBootstrapModule,
+                new OrmModule(),
+                new DataVaultModule(),
+                new DomainUtilModule(),
+                new NlsModule(),
+                new UserModule(),
+                new UtilModule(),
+                new ThreadSecurityModule(),
+                new PubSubModule(),
+                new TransactionModule(printSql),
+                new InMemoryMessagingModule(),
+                new TaskModule(),
+                new IdsModule(),
+                new EventsModule(),
+                new PartyModule(),
                 new FiniteStateMachineModule(),
-    			new MeteringModule(),
-				new BasicPropertiesModule(),
-				new TimeModule(),
-				new DataLifeCycleModule(),
-				new CustomPropertySetsModule()
-		);
-		try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext() ) {
-			injector.getInstance(CustomPropertySetService.class);
-			injector.getInstance(FiniteStateMachineService.class);
-			lifeCycleService = injector.getInstance(LifeCycleService.class);
-			ctx.commit();
-		}
-	}
-
-	@After
-	public void tearDown() throws SQLException {
-    	inMemoryBootstrapModule.deactivate();
+                new UsagePointLifeCycleConfigurationModule(),
+                new MeteringModule(),
+                new BasicPropertiesModule(),
+                new TimeModule(),
+                new DataLifeCycleModule(),
+                new CustomPropertySetsModule()
+        );
+        try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext()) {
+            injector.getInstance(CustomPropertySetService.class);
+            injector.getInstance(FiniteStateMachineService.class);
+            lifeCycleService = injector.getInstance(LifeCycleService.class);
+            ctx.commit();
+        }
     }
 
-	@Test
-    public void test()  {
-		Instant afterInstall = Instant.now();
-		List<LifeCycleCategory> categories = lifeCycleService.getCategories();
-		assertThat(categories).hasSize(LifeCycleCategoryKind.values().length);
-		for (LifeCycleCategory category : categories) {
-			assertThat(categories.indexOf(category)).isEqualTo(	category.getKind().ordinal());
-		}
-		lifeCycleService.getTask();
-		injector.getInstance(TransactionService.class).execute(() -> { lifeCycleService.runNow(); return null; });
-		Period period = categories.get(0).getRetention();
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-		injector.getInstance(TransactionService.class).execute(() -> { categories.get(0).setRetentionDays(100); return null;});
-		assertThat(lifeCycleService.getCategories().get(0).getRetention()).isEqualTo(Period.ofDays(100));
-		assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).get(0).getRetention()).isEqualTo(period);
-		assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).get(0).getKind()).isEqualTo(lifeCycleService.getCategories().get(0).getKind());
-		assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).size()).isEqualTo(lifeCycleService.getCategories().size());
-	}
+    @After
+    public void tearDown() throws SQLException {
+        inMemoryBootstrapModule.deactivate();
+    }
+
+    @Test
+    public void test() {
+        Instant afterInstall = Instant.now();
+        List<LifeCycleCategory> categories = lifeCycleService.getCategories();
+        assertThat(categories).hasSize(LifeCycleCategoryKind.values().length);
+        for (LifeCycleCategory category : categories) {
+            assertThat(categories.indexOf(category)).isEqualTo(category.getKind().ordinal());
+        }
+        lifeCycleService.getTask();
+        injector.getInstance(TransactionService.class).execute(() -> {
+            lifeCycleService.runNow();
+            return null;
+        });
+        Period period = categories.get(0).getRetention();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        injector.getInstance(TransactionService.class).execute(() -> {
+            categories.get(0).setRetentionDays(100);
+            return null;
+        });
+        assertThat(lifeCycleService.getCategories().get(0).getRetention()).isEqualTo(Period.ofDays(100));
+        assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).get(0).getRetention()).isEqualTo(period);
+        assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).get(0).getKind()).isEqualTo(lifeCycleService.getCategories().get(0).getKind());
+        assertThat(lifeCycleService.getCategoriesAsOf(afterInstall).size()).isEqualTo(lifeCycleService.getCategories().size());
+    }
 
 }
