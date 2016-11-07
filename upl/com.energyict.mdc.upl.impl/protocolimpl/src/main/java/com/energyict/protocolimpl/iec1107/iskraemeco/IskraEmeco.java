b023267bf8cc0c08c0900967f0a6c5cba44052ea
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-11-07 10:56:07 +0100

    CXO-2951: Convert IskraEmeco to to upl meter protocol interface

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/iskraemeco/IskraEmeco.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/iskraemeco/IskraEmeco.java
@@ -10,10 +10,10 @@
  */
 package com.energyict.protocolimpl.iec1107.iskraemeco;
 
-import com.energyict.mdc.upl.NoSuchRegisterException;
 import com.energyict.mdc.upl.UnsupportedException;
 import com.energyict.mdc.upl.properties.InvalidPropertyException;
 import com.energyict.mdc.upl.properties.MissingPropertyException;
+import com.energyict.mdc.upl.properties.PropertySpec;
 
 import com.energyict.cbo.NestedIOException;
 import com.energyict.cbo.Quantity;
@@ -39,21 +39,30 @@ import com.energyict.protocolimpl.iec1107.ChannelMap;
 import com.energyict.protocolimpl.iec1107.FlagIEC1107Connection;
 import com.energyict.protocolimpl.iec1107.FlagIEC1107ConnectionException;
 import com.energyict.protocolimpl.iec1107.ProtocolLink;
+import com.energyict.protocolimpl.properties.UPLPropertySpecFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TimeZone;
 import java.util.logging.Logger;
+
+import static com.energyict.mdc.upl.MeterProtocol.Property.ADDRESS;
+import static com.energyict.mdc.upl.MeterProtocol.Property.NODEID;
+import static com.energyict.mdc.upl.MeterProtocol.Property.PASSWORD;
+import static com.energyict.mdc.upl.MeterProtocol.Property.PROFILEINTERVAL;
+import static com.energyict.mdc.upl.MeterProtocol.Property.RETRIES;
+import static com.energyict.mdc.upl.MeterProtocol.Property.ROUNDTRIPCORRECTION;
+import static com.energyict.mdc.upl.MeterProtocol.Property.SECURITYLEVEL;
+import static com.energyict.mdc.upl.MeterProtocol.Property.SERIALNUMBER;
+import static com.energyict.mdc.upl.MeterProtocol.Property.TIMEOUT;
 
 /**
  * @author Koenraad Vanderschaeve
@@ -93,35 +102,32 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
     private TimeZone timeZone;
     private Logger logger;
 
-    int readCurrentDay;
+    private int readCurrentDay;
 
-    FlagIEC1107Connection flagIEC1107Connection = null;
-    IskraEmecoRegistry iskraEmecoRegistry = null;
-    IskraEmecoProfile iskraEmecoProfile = null;
-    RegisterConfig regs = new EDPRegisterConfig(); // we should use an infotype property to determine the registerset
+    private FlagIEC1107Connection flagIEC1107Connection = null;
+    private IskraEmecoRegistry iskraEmecoRegistry = null;
+    private IskraEmecoProfile iskraEmecoProfile = null;
+    private RegisterConfig regs = new EDPRegisterConfig(); // we should use an infotype property to determine the registerset
 
-    byte[] dataReadout = null;
+    private byte[] dataReadout = null;
 
     private boolean software7E1;
 
-    /**
-     * Creates a new instance of ABBA1500, empty constructor
-     */
-    public IskraEmeco() {
-    } // public IskraEmeco()
-
+    @Override
     public ProfileData getProfileData(boolean includeEvents) throws IOException {
         Calendar fromCalendar = ProtocolUtils.getCalendar(timeZone);
         fromCalendar.add(Calendar.YEAR, -10);
         return doGetProfileData(fromCalendar, ProtocolUtils.getCalendar(timeZone), includeEvents);
     }
 
+    @Override
     public ProfileData getProfileData(Date lastReading, boolean includeEvents) throws IOException {
         Calendar fromCalendar = ProtocolUtils.getCleanCalendar(timeZone);
         fromCalendar.setTime(lastReading);
         return doGetProfileData(fromCalendar, ProtocolUtils.getCalendar(timeZone), includeEvents);
     }
 
+    @Override
     public ProfileData getProfileData(Date from, Date to, boolean includeEvents) throws IOException {
         Calendar fromCalendar = ProtocolUtils.getCleanCalendar(timeZone);
         fromCalendar.setTime(from);
@@ -144,6 +150,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
                 false, isReadCurrentDay());
     }
 
+    @Override
     public Quantity getMeterReading(String name) throws IOException {
         try {
             return (Quantity) getIskraEmecoRegistry().getRegister(name);
@@ -152,6 +159,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         }
     }
 
+    @Override
     public Quantity getMeterReading(int channelId) throws IOException {
         String[] ISKRAEMECO_METERREADINGS = null;
         try {
@@ -166,30 +174,19 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         }
     }
 
-    /**
-     * This method sets the time/date in the remote meter equal to the system time/date of the machine where this object resides.
-     *
-     * @throws IOException
-     */
+    @Override
     public void setTime() throws IOException {
-        Calendar calendar = null;
-        calendar = ProtocolUtils.getCalendar(timeZone);
+        Calendar calendar = ProtocolUtils.getCalendar(timeZone);
         calendar.add(Calendar.MILLISECOND, iRoundtripCorrection);
         Date date = calendar.getTime();
         getIskraEmecoRegistry().setRegister("TimeDateWrite", date);
-        //getIskraEmecoRegistry().setRegister("0.9.2",date);
-    } // public void setTime() throws IOException
+    }
 
+    @Override
     public Date getTime() throws IOException {
         Date date = (Date) getIskraEmecoRegistry().getRegister("TimeDateReadOnly");
         return new Date(date.getTime() - iRoundtripCorrection);
     }
-
-    public byte getLastProtocolState() {
-        return -1;
-    }
-
-    /************************************** MeterProtocol implementation ***************************************/
 
     @Override
     public String getSerialNumber() {
@@ -200,131 +197,80 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         }
     }
 
-    /**
-     * this implementation calls <code> validateProperties </code>
-     * and assigns the argument to the properties field
-     *
-     * @param properties <br>
-     * @throws MissingPropertyException <br>
-     * @throws InvalidPropertyException <br>
-     */
-    public void setProperties(Properties properties) throws MissingPropertyException, InvalidPropertyException {
-        validateProperties(properties);
+    @Override
+    public List<PropertySpec> getPropertySpecs() {
+        return Arrays.asList(
+                UPLPropertySpecFactory.string(ADDRESS.getName(), false),
+                UPLPropertySpecFactory.string(PASSWORD.getName(), false),
+                UPLPropertySpecFactory.string(SERIALNUMBER.getName(), false),
+                UPLPropertySpecFactory.integer(TIMEOUT.getName(), false),
+                UPLPropertySpecFactory.integer(RETRIES.getName(), false),
+                UPLPropertySpecFactory.integer(ROUNDTRIPCORRECTION.getName(), false),
+                UPLPropertySpecFactory.integer(SECURITYLEVEL.getName(), false),
+                UPLPropertySpecFactory.string(NODEID.getName(), false),
+                UPLPropertySpecFactory.integer("EchoCancelling", false),
+                UPLPropertySpecFactory.integer("IEC1107Compatible", false),
+                UPLPropertySpecFactory.integer(PROFILEINTERVAL.getName(), false),
+                UPLPropertySpecFactory.integer("RequestHeader", false),
+                UPLPropertySpecFactory.string("ChannelMap", false),
+                UPLPropertySpecFactory.integer("ExtendedLogging", false),
+                UPLPropertySpecFactory.integer("ReadCurrentDay", false),
+                UPLPropertySpecFactory.string("Software7E1", false));
     }
 
-    /**
-     * <p>validates the properties.</p><p>
-     * The default implementation checks that all required parameters are present.
-     * </p>
-     *
-     * @param properties <br>
-     * @throws MissingPropertyException <br>
-     * @throws InvalidPropertyException <br>
-     */
-    private void validateProperties(Properties properties) throws MissingPropertyException, InvalidPropertyException {
+    @Override
+    public void setProperties(Properties properties) throws MissingPropertyException, InvalidPropertyException {
         try {
-            Iterator iterator = getRequiredKeys().iterator();
-            while (iterator.hasNext()) {
-                String key = (String) iterator.next();
-                if (properties.getProperty(key) == null) {
-                    throw new MissingPropertyException(key + " key missing");
-                }
-            }
-            strID = properties.getProperty(com.energyict.mdc.upl.MeterProtocol.Property.ADDRESS.getName());
-            strPassword = properties.getProperty(com.energyict.mdc.upl.MeterProtocol.Property.PASSWORD.getName());
-            serialNumber = properties.getProperty(com.energyict.mdc.upl.MeterProtocol.Property.SERIALNUMBER.getName());
-            iIEC1107TimeoutProperty = Integer.parseInt(properties.getProperty("Timeout", "20000").trim());
-            iProtocolRetriesProperty = Integer.parseInt(properties.getProperty("Retries", "5").trim());
-            iRoundtripCorrection = Integer.parseInt(properties.getProperty("RoundtripCorrection", "0").trim());
-            iSecurityLevel = Integer.parseInt(properties.getProperty("SecurityLevel", "1").trim());
-            nodeId = properties.getProperty(com.energyict.mdc.upl.MeterProtocol.Property.NODEID.getName(), "");
+            strID = properties.getProperty(ADDRESS.getName());
+            strPassword = properties.getProperty(PASSWORD.getName());
+            serialNumber = properties.getProperty(SERIALNUMBER.getName());
+            iIEC1107TimeoutProperty = Integer.parseInt(properties.getProperty(TIMEOUT.getName(), "20000").trim());
+            iProtocolRetriesProperty = Integer.parseInt(properties.getProperty(RETRIES.getName(), "5").trim());
+            iRoundtripCorrection = Integer.parseInt(properties.getProperty(ROUNDTRIPCORRECTION.getName(), "0").trim());
+            iSecurityLevel = Integer.parseInt(properties.getProperty(SECURITYLEVEL.getName(), "1").trim());
+            nodeId = properties.getProperty(NODEID.getName(), "");
             iEchoCancelling = Integer.parseInt(properties.getProperty("EchoCancelling", "0").trim());
             iIEC1107Compatible = Integer.parseInt(properties.getProperty("IEC1107Compatible", "1").trim());
-            iProfileInterval = Integer.parseInt(properties.getProperty("ProfileInterval", "3600").trim());
+            iProfileInterval = Integer.parseInt(properties.getProperty(PROFILEINTERVAL.getName(), "3600").trim());
             channelMap = new ChannelMap(properties.getProperty("ChannelMap", "1.5:5.5:8.5"));
             extendedLogging = Integer.parseInt(properties.getProperty("ExtendedLogging", "0").trim());
             readCurrentDay = Integer.parseInt(properties.getProperty("ReadCurrentDay", "0"));
             this.software7E1 = !"0".equalsIgnoreCase(properties.getProperty("Software7E1", "0"));
         } catch (NumberFormatException e) {
-            throw new InvalidPropertyException("DukePower, validateProperties, NumberFormatException, " + e.getMessage());
+            throw new InvalidPropertyException(e, this.getClass().getSimpleName() + ": validation of properties failed before");
         }
-
     }
 
-    /**
-     * this implementation throws UnsupportedException. Subclasses may override
-     *
-     * @param name <br>
-     * @return the register value
-     * @throws IOException             <br>
-     * @throws UnsupportedException    <br>
-     * @throws NoSuchRegisterException <br>
-     */
+    @Override
     public String getRegister(String name) throws IOException {
         return ProtocolUtils.obj2String(getIskraEmecoRegistry().getRegister(name));
     }
 
-    /**
-     * this implementation throws UnsupportedException. Subclasses may override
-     *
-     * @param name  <br>
-     * @param value <br>
-     * @throws IOException             <br>
-     * @throws NoSuchRegisterException <br>
-     * @throws UnsupportedException    <br>
-     */
+    @Override
     public void setRegister(String name, String value) throws IOException {
         getIskraEmecoRegistry().setRegister(name, value);
     }
 
-    /**
-     * this implementation throws UnsupportedException. Subclasses may override
-     *
-     * @throws IOException          <br>
-     * @throws UnsupportedException <br>
-     */
+    @Override
     public void initializeDevice() throws IOException {
         throw new UnsupportedException();
     }
 
-    public List<String> getRequiredKeys() {
-        return Collections.emptyList();
-    }
-
-    public List<String> getOptionalKeys() {
-        return Arrays.asList(
-                    "Timeout",
-                    "Retries",
-                    "SecurityLevel",
-                    "EchoCancelling",
-                    "IEC1107Compatible",
-                    "ChannelMap",
-                    "ExtendedLogging",
-                    "ReadCurrentDay",
-                    "Software7E1");
-    }
-
-    /** Protocol Version **/
+    @Override
     public String getProtocolVersion() {
         return "$Date: 2015-11-26 15:24:27 +0200 (Thu, 26 Nov 2015)$";
     }
 
+    @Override
     public String getFirmwareVersion() throws IOException {
         try {
             return ((String) getIskraEmecoRegistry().getRegister("software revision number"));
         } catch (IOException e) {
             throw new IOException("IskraEmeco, getFirmwareVersion, " + e.getMessage());
         }
-    } // public String getFirmwareVersion()
+    }
 
-    /**
-     * initializes the receiver
-     *
-     * @param inputStream  <br>
-     * @param outputStream <br>
-     * @param timeZone     <br>
-     * @param logger       <br>
-     */
+    @Override
     public void init(InputStream inputStream, OutputStream outputStream, TimeZone timeZone, Logger logger) {
         this.timeZone = timeZone;
         this.logger = logger;
@@ -337,6 +283,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         }
     }
 
+    @Override
     public void connect() throws IOException {
         try {
             flagIEC1107Connection.connectMAC(strID, strPassword, iSecurityLevel, nodeId);
@@ -353,6 +300,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         return regs.getRegisterInfo();
     }
 
+    @Override
     public void disconnect() throws NestedIOException {
         try {
             flagIEC1107Connection.disconnectMAC();
@@ -361,10 +309,12 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         }
     }
 
+    @Override
     public int getNumberOfChannels() throws IOException {
         return channelMap.getNrOfChannels();
     }
 
+    @Override
     public int getProfileInterval() throws IOException {
         Object obj = getIskraEmecoRegistry().getRegister("Profile Interval");
         if (obj == null) {
@@ -382,54 +332,47 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         return iskraEmecoProfile;
     }
 
-
-    // Implementation of interface ProtocolLink
+    @Override
     public FlagIEC1107Connection getFlagIEC1107Connection() {
         return flagIEC1107Connection;
     }
 
+    @Override
     public TimeZone getTimeZone() {
         return timeZone;
     }
 
+    @Override
     public boolean isIEC1107Compatible() {
         return (iIEC1107Compatible == 1);
     }
 
+    @Override
     public String getPassword() {
         return strPassword;
     }
 
+    @Override
     public byte[] getDataReadout() {
         return dataReadout;
     }
 
-    public Object getCache() {
-        return null;
-    }
-
-    public Object fetchCache(int rtuid) throws java.sql.SQLException, com.energyict.cbo.BusinessException {
-        return null;
-    }
-
-    public void setCache(Object cacheObject) {
-    }
-
-    public void updateCache(int rtuid, Object cacheObject) throws java.sql.SQLException, com.energyict.cbo.BusinessException {
-    }
-
+    @Override
     public ProtocolChannelMap getProtocolChannelMap() {
         return null;
     }
 
+    @Override
     public ChannelMap getChannelMap() {
         return channelMap;
     }
 
+    @Override
     public void enableHHUSignOn(SerialCommunicationChannel commChannel) throws ConnectionException {
         enableHHUSignOn(commChannel, false);
     }
 
+    @Override
     public void enableHHUSignOn(SerialCommunicationChannel commChannel, boolean datareadout) throws ConnectionException {
         HHUSignOn hhuSignOn =
                 new IEC1107HHUConnection(commChannel, iIEC1107TimeoutProperty, iProtocolRetriesProperty, 300, iEchoCancelling);
@@ -439,6 +382,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         getFlagIEC1107Connection().setHHUSignOn(hhuSignOn);
     }
 
+    @Override
     public byte[] getHHUDataReadout() {
         setDataReadout(getFlagIEC1107Connection().getHhuSignOn().getDataReadout());
         return getDataReadout();
@@ -448,7 +392,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         this.dataReadout = dataReadout;
     }
 
-
+    @Override
     public void release() throws IOException {
     }
 
@@ -499,6 +443,7 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         EXCEPTION_INFO_MAP.put("ER53", "Non R5 variable");
     }
 
+    @Override
     public String getExceptionInfo(String id) {
         String exceptionInfo = EXCEPTION_INFO_MAP.get(id);
         if (exceptionInfo != null) {
@@ -508,33 +453,33 @@ public class IskraEmeco extends PluggableMeterProtocol implements ProtocolLink, 
         }
     }
 
+    @Override
     public Logger getLogger() {
         return logger;
     }
 
-    /**
-     * ****************************************************************************************
-     * R e g i s t e r P r o t o c o l  i n t e r f a c e
-     * *****************************************************************************************
-     */
+    @Override
     public RegisterInfo translateRegister(ObisCode obisCode) throws IOException {
         return ObisCodeMapper.getRegisterInfo(obisCode);
     }
 
+    @Override
     public RegisterValue readRegister(ObisCode obisCode) throws IOException {
         ObisCodeMapper ocm = new ObisCodeMapper(getIskraEmecoRegistry(), getTimeZone(), regs);
         return ocm.getRegisterValue(obisCode);
     }
 
+    @Override
     public int getNrOfRetries() {
         return iProtocolRetriesProperty;
     }
 
+    @Override
     public boolean isRequestHeader() {
         return false;
     }
 
-    public boolean isReadCurrentDay() {
+    private boolean isReadCurrentDay() {
         return readCurrentDay == 1;
     }
 
