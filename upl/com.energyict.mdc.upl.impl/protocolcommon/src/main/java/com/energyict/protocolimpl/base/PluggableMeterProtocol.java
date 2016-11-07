e32daf0113bdb3bf95dbd2b5e8faaa345c412281
Rudi Vankeirsbilck <rudi.vankeirsbilck@elster.com>
2016-11-07 16:23:16 +0100

    CXO-2951: Properly adapt UPL classes with the UPLMeterProtocolAdapter in PluggableClassImpl to fool eiServer into believing it is dealing with a MeterProtocol as defined by the mdw MeterProtocol interface.

--- upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimpl/base/PluggableMeterProtocol.java
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/protocolimpl/base/PluggableMeterProtocol.java
@@ -3,8 +3,7 @@ package com.energyict.protocolimpl.base;
 import com.energyict.mdc.upl.MeterProtocol;
 
 /**
- * Abstract Class to map certain <i>new</i> functionality for existing protocols.
- * Introduced after we made MeterProtocol {@link com.energyict.mdw.core.Pluggable}
+ * Serves as the root for all class that intend to implement the {@link MeterProtocol} interface.
  *
  * Copyrights EnergyICT
  * Date: 3/07/12
