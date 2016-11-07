ef6f79be6b8c9fe12eaf3a3e72e18c3de5c6b094
Anton Suslennikov <antsusl@mera.ru>
2016-11-07 17:05:15 +0300

    CXO-3311 Add metering dependency

--- comu/com.energyict.mdc.device.data.importers/src/test/java/com/energyict/mdc/device/data/importers/impl/InMemoryIntegrationPersistence.java
+++ comu/com.energyict.mdc.device.data.importers/src/test/java/com/energyict/mdc/device/data/importers/impl/InMemoryIntegrationPersistence.java
@@ -38,6 +38,7 @@ import com.elster.jupiter.transaction.TransactionService;
 import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.upgrade.UpgradeService;
 import com.elster.jupiter.upgrade.impl.UpgradeModule;
+import com.elster.jupiter.usagepoint.lifecycle.config.impl.UsagePointLifeCycleConfigurationModule;
 import com.elster.jupiter.users.impl.UserModule;
 import com.elster.jupiter.util.UtilModule;
 import com.elster.jupiter.util.exception.MessageSeed;
@@ -100,6 +101,7 @@ public class InMemoryIntegrationPersistence {
                 new ServiceCallModule(),
                 new CustomPropertySetsModule(),
                 new IdsModule(),
+                new UsagePointLifeCycleConfigurationModule(),
                 new MeteringModule("0.0.0.9.1.1.12.0.0.0.0.1.0.0.0.0.72.0", "11.0.0.9.1.2.12.0.0.0.0.1.0.0.0.0.72.0"),
                 new PartyModule(),
                 new EventsModule(),
