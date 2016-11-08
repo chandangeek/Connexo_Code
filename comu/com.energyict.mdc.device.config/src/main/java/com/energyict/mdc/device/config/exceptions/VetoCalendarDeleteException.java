213672c503a37bee40ae04d8a0cdf4acfbc402be
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-11-08 11:04:55 +0100

    CXO-3362 - Remove calendar

---
+++ comu/com.energyict.mdc.device.config/src/main/java/com/energyict/mdc/device/config/exceptions/VetoCalendarDeleteException.java
@@ -0,0 +1,13 @@
+package com.energyict.mdc.device.config.exceptions;
+
+import com.elster.jupiter.calendar.Calendar;
+import com.elster.jupiter.nls.LocalizedException;
+import com.elster.jupiter.nls.Thesaurus;
+import com.energyict.mdc.device.config.impl.MessageSeeds;
+
+public class VetoCalendarDeleteException extends LocalizedException {
+
+    public VetoCalendarDeleteException(Thesaurus thesaurus, Calendar calendar) {
+        super(thesaurus, MessageSeeds.VETO_CALENDAR_DELETION, calendar.getName());
+    }
+}
