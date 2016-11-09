38a1a8d60d12efcc738b4fba49e491f8042a01ba
Maksim Barinov <mbarinov@mera.ru>
2016-11-09 17:55:55 +0300

    CXO-3311

--- coko/com.elster.jupiter.usagepoint.lifecycle.rest/src/main/java/com/elster/jupiter/usagepoint/lifecycle/rest/UsagePointLifeCycleInfo.java
+++ coko/com.elster.jupiter.usagepoint.lifecycle.rest/src/main/java/com/elster/jupiter/usagepoint/lifecycle/rest/UsagePointLifeCycleInfo.java
@@ -1,5 +1,8 @@
 package com.elster.jupiter.usagepoint.lifecycle.rest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UsagePointLifeCycleInfo {
     public long id;
     public String name;
