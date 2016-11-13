838e4b1c8b91db9cc732c003f3ed091914668b0e
Yan R <yaratay@mera.ru>
2016-11-13 19:19:46 +0300

    CXO-3540: [BE] Add usage point group unit tests for adding enumerated group + validation

--- comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/DeviceGroupResourceTest.java
+++ comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/DeviceGroupResourceTest.java
@@ -4,7 +4,11 @@ import com.elster.jupiter.devtools.ExtjsFilter;
 import com.elster.jupiter.devtools.tests.FakeBuilder;
 import com.elster.jupiter.domain.util.Finder;
 import com.elster.jupiter.domain.util.Query;
+import com.elster.jupiter.domain.util.VerboseConstraintViolationException;
+import com.elster.jupiter.metering.AmrSystem;
 import com.elster.jupiter.metering.EndDevice;
+import com.elster.jupiter.metering.KnownAmrSystem;
+import com.elster.jupiter.metering.Meter;
 import com.elster.jupiter.metering.groups.EndDeviceGroup;
 import com.elster.jupiter.metering.groups.EnumeratedEndDeviceGroup;
 import com.elster.jupiter.metering.groups.GroupBuilder;
@@ -12,13 +16,20 @@ import com.elster.jupiter.metering.groups.QueryEndDeviceGroup;
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.LocalizedException;
 import com.elster.jupiter.nls.impl.NlsModule;
+import com.elster.jupiter.properties.StringFactory;
 import com.elster.jupiter.rest.util.StatusCode;
+import com.elster.jupiter.search.SearchBuilder;
 import com.elster.jupiter.search.SearchDomain;
 import com.elster.jupiter.search.SearchableProperty;
+import com.elster.jupiter.search.SearchablePropertyCondition;
 import com.elster.jupiter.search.SearchablePropertyOperator;
 import com.elster.jupiter.search.SearchablePropertyValue;
+import com.elster.jupiter.search.impl.SearchBuilderImpl;
+import com.elster.jupiter.search.impl.SearchMonitor;
 import com.elster.jupiter.util.conditions.Comparison;
 import com.elster.jupiter.util.conditions.Condition;
+import com.elster.jupiter.util.conditions.Operator;
+import com.elster.jupiter.util.time.ExecutionTimer;
 import com.energyict.mdc.device.config.DeviceConfiguration;
 import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.data.Device;
@@ -37,24 +48,30 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.junit.Test;
+import org.mockito.Answers;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 
 import static com.elster.jupiter.util.conditions.Where.where;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.anyVararg;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -63,12 +80,30 @@ import static org.mockito.Mockito.when;
  */
 public class DeviceGroupResourceTest extends DeviceDataRestApplicationJerseyTest {
 
+    private static final String ALL_FILTER = JsonModel.model(ImmutableList.of(ImmutableMap.of(
+            "property", "name",
+            "value", ImmutableList.of(
+                    ImmutableMap.of(
+                            "operator", "==",
+                            "criteria", "*"
+                    )
+            )))).toJson();
     @Mock
     private EndDeviceGroup endDeviceGroup;
     @Mock
+    private Device device;
+    @Mock
+    private Meter meter;
+    @Mock
+    private AmrSystem amrSystem;
+    @Mock
     private Finder<Device> finder;
     @Mock
+    private SearchBuilder<Device> searchBuilder;
+    @Mock
     private Query<EndDeviceGroup> endDeviceGroupQuery;
+    @Captor
+    private ArgumentCaptor<List<SearchablePropertyCondition>> searchablePropertyConditionsCaptor;
 
     @Override
     protected void setupTranslations() {
@@ -192,14 +227,7 @@ public class DeviceGroupResourceTest extends DeviceDataRestApplicationJerseyTest
         DeviceGroupInfo info = new DeviceGroupInfo();
         info.name = "NewQueryGroup";
         info.dynamic = true;
-        info.filter = JsonModel.model(ImmutableList.of(ImmutableMap.of(
-                "property", "name",
-                "value", ImmutableList.of(
-                        ImmutableMap.of(
-                                "operator", "==",
-                                "criteria", "*"
-                        )
-                )))).toJson();
+        info.filter = ALL_FILTER;
 
         String response = target("/devicegroups").request().post(Entity.json(info), String.class);
 
@@ -215,9 +243,218 @@ public class DeviceGroupResourceTest extends DeviceDataRestApplicationJerseyTest
         verify(builder).setSearchDomain(searchDomain);
         verify(builder).setQueryProviderName(anyString());
         verify(builder).setLabel("MDC");
+        verify(builder).create();
 
         JsonModel jsonModel = JsonModel.model(response);
         assertThat(jsonModel.<Integer>get("$.id")).isEqualTo(133);
+    }
+
+    @Test
+    public void testCreateEnumeratedEndDeviceGroupFromFilter() throws Exception {
+        SearchDomain searchDomain = mock(SearchDomain.class);
+        when(searchDomain.getId()).thenReturn(Device.class.getName());
+        when(searchService.findDomain(Device.class.getName())).thenReturn(Optional.of(searchDomain));
+        SearchMonitor dummyMonitor = mock(SearchMonitor.class);
+        ExecutionTimer timer = mock(ExecutionTimer.class);
+        doAnswer(invocation -> ((Callable)invocation.getArguments()[0]).call()).when(timer).time(any(Callable.class));
+        when(dummyMonitor.searchTimer(searchDomain)).thenReturn(timer);
+
+        when(searchService.search(Device.class)).thenReturn(new SearchBuilderImpl<>(searchDomain, dummyMonitor));
+        when(device.getId()).thenReturn(31L);
+        when(meteringService.findAmrSystem(KnownAmrSystem.MDC.getId())).thenReturn(Optional.of(amrSystem));
+        when(amrSystem.findMeter("31")).thenReturn(Optional.of(meter));
+        SearchableProperty nameSearchableProperty = mock(SearchableProperty.class, Answers.RETURNS_DEEP_STUBS.get());
+        when(nameSearchableProperty.getName()).thenReturn("name");
+        when(nameSearchableProperty.getSelectionMode()).thenReturn(SearchableProperty.SelectionMode.SINGLE);
+        when(nameSearchableProperty.getSpecification().getValueFactory()).thenReturn(new StringFactory());
+        when(searchDomain.getPropertiesValues(any())).thenAnswer(invocationOnMock -> Collections.singletonList(
+                ((Function<SearchableProperty, SearchablePropertyValue>)invocationOnMock.getArguments()[0])
+                        .apply(nameSearchableProperty)));
+        doReturn(finder).when(searchDomain).finderFor(anyListOf(SearchablePropertyCondition.class));
+        when(finder.find()).thenReturn(Collections.singletonList(device));
+        when(finder.stream()).thenReturn(Stream.of(device));
+
+        EnumeratedEndDeviceGroup group = mock(EnumeratedEndDeviceGroup.class);
+        when(group.getId()).thenReturn(133L);
+        GroupBuilder.EnumeratedGroupBuilder<EndDevice, EnumeratedEndDeviceGroup> builder = FakeBuilder
+                .initBuilderStub(group, GroupBuilder.EnumeratedGroupBuilder.class);
+        doReturn(builder).when(meteringGroupService).createEnumeratedEndDeviceGroup(anyVararg());
+
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.name = "NewEnumGroup";
+        info.dynamic = false;
+        info.filter = ALL_FILTER;
+
+        String response = target("/devicegroups").request().post(Entity.json(info), String.class);
+
+        verify(searchDomain).finderFor(searchablePropertyConditionsCaptor.capture());
+        List<SearchablePropertyCondition> conditions = searchablePropertyConditionsCaptor.getValue();
+        assertThat(conditions).hasSize(1);
+        SearchablePropertyCondition searchablePropertyCondition = conditions.get(0);
+        assertThat(searchablePropertyCondition.getProperty()).isEqualTo(nameSearchableProperty);
+        Condition condition = searchablePropertyCondition.getCondition();
+        assertThat(condition).isInstanceOf(Comparison.class);
+        assertThat(((Comparison) condition).getOperator()).isEqualTo(Operator.LIKEIGNORECASE);
+        assertThat(((Comparison) condition).getValues()).containsExactly("%");
+
+        verify(meteringGroupService).createEnumeratedEndDeviceGroup(meter);
+        verify(builder).setName(info.name);
+        verify(builder).setMRID("MDC:" + info.name);
+        verify(builder).setLabel("MDC");
+        verify(builder).create();
+
+        JsonModel jsonModel = JsonModel.model(response);
+        assertThat(jsonModel.<Integer>get("$.id")).isEqualTo(133);
+    }
+
+    @Test
+    public void testCreateEnumeratedEndDeviceGroupFromList() throws Exception {
+        when(device.getId()).thenReturn(31L);
+        when(meteringService.findAmrSystem(KnownAmrSystem.MDC.getId())).thenReturn(Optional.of(amrSystem));
+        when(amrSystem.findMeter("31")).thenReturn(Optional.of(meter));
+
+        EnumeratedEndDeviceGroup group = mock(EnumeratedEndDeviceGroup.class);
+        when(group.getId()).thenReturn(133L);
+        GroupBuilder.EnumeratedGroupBuilder<EndDevice, EnumeratedEndDeviceGroup> builder = FakeBuilder
+                .initBuilderStub(group, GroupBuilder.EnumeratedGroupBuilder.class);
+        doReturn(builder).when(meteringGroupService).createEnumeratedEndDeviceGroup(anyVararg());
+
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.name = "NewEnumGroup";
+        info.dynamic = false;
+        info.filter = ALL_FILTER;
+        info.devices = Collections.singletonList(31L);
+
+        String response = target("/devicegroups").request().post(Entity.json(info), String.class);
+
+        verify(meteringGroupService).createEnumeratedEndDeviceGroup(meter);
+        verify(builder).setName(info.name);
+        verify(builder).setMRID("MDC:" + info.name);
+        verify(builder).setLabel("MDC");
+        verify(builder).create();
+
+        JsonModel jsonModel = JsonModel.model(response);
+        assertThat(jsonModel.<Integer>get("$.id")).isEqualTo(133);
+    }
+
+    @Test
+    public void testValidateQueryEndDeviceGroupCreation() throws Exception {
+        SearchDomain searchDomain = mock(SearchDomain.class);
+        when(searchDomain.getId()).thenReturn(Device.class.getName());
+        when(searchService.findDomain(Device.class.getName())).thenReturn(Optional.of(searchDomain));
+
+        QueryEndDeviceGroup group = mock(QueryEndDeviceGroup.class);
+        GroupBuilder.QueryGroupBuilder<EndDevice, QueryEndDeviceGroup> builder = FakeBuilder
+                .initBuilderStub(group, GroupBuilder.QueryGroupBuilder.class);
+        doReturn(builder).when(meteringGroupService).createQueryEndDeviceGroup(anyVararg());
+
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.name = "NewQueryGroup";
+        info.dynamic = true;
+
+        Response response = target("/devicegroups").queryParam("validate", true).request().post(Entity.json(info));
+
+        verify(meteringGroupService).createQueryEndDeviceGroup(anyVararg());
+        verify(builder).setName(info.name);
+        verify(builder).setMRID("MDC:" + info.name);
+        verify(builder).setLabel("MDC");
+        verify(builder).setSearchDomain(searchDomain);
+        verify(builder).setQueryProviderName(anyString());
+        verify(builder).validate();
+        verify(builder, never()).create();
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.ACCEPTED.getStatusCode());
+    }
+
+    @Test
+    public void testValidateEnumeratedEndDeviceGroupCreation() throws Exception {
+        EnumeratedEndDeviceGroup group = mock(EnumeratedEndDeviceGroup.class);
+        GroupBuilder.EnumeratedGroupBuilder<EndDevice, EnumeratedEndDeviceGroup> builder = FakeBuilder
+                .initBuilderStub(group, GroupBuilder.EnumeratedGroupBuilder.class);
+        doReturn(builder).when(meteringGroupService).createEnumeratedEndDeviceGroup(anyVararg());
+
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.name = "NewEnumeratedGroup";
+        info.dynamic = false;
+
+        Response response = target("/devicegroups").queryParam("validate", true).request().post(Entity.json(info));
+
+        verify(meteringGroupService).createEnumeratedEndDeviceGroup(anyVararg());
+        verify(builder).setName(info.name);
+        verify(builder).setMRID("MDC:" + info.name);
+        verify(builder).setLabel("MDC");
+        verify(builder).validate();
+        verify(builder, never()).create();
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.ACCEPTED.getStatusCode());
+    }
+
+    @Test
+    public void testValidationFailsNoName() throws IOException {
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.dynamic = true;
+
+        Response response = target("/devicegroups").queryParam("validate", true).request().post(Entity.json(info));
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((ByteArrayInputStream)response.getEntity());
+        assertThat(jsonModel.<Boolean>get("$.success")).isFalse();
+        assertThat(jsonModel.<List<String>>get("$.errors[*].id")).containsExactly("name");
+        assertThat(jsonModel.<List<String>>get("$.errors[*].msg")).containsExactly("This field is required");
+    }
+
+    @Test
+    public void testValidationFailsEmptyName() throws IOException {
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.dynamic = true;
+        info.name = "  ";
+
+        Response response = target("/devicegroups").queryParam("validate", true).request().post(Entity.json(info));
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((ByteArrayInputStream)response.getEntity());
+        assertThat(jsonModel.<Boolean>get("$.success")).isFalse();
+        assertThat(jsonModel.<List<String>>get("$.errors[*].id")).containsExactly("name");
+        assertThat(jsonModel.<List<String>>get("$.errors[*].msg")).containsExactly("This field is required");
+    }
+
+    @Test
+    public void testValidationFailsNoSearchDomain() throws IOException {
+        when(searchService.findDomain(Device.class.getName())).thenReturn(Optional.empty());
+
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.dynamic = true;
+        info.name = "NewGroup";
+
+        Response response = target("/devicegroups").queryParam("validate", true).request().post(Entity.json(info));
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((ByteArrayInputStream)response.getEntity());
+        assertThat(jsonModel.<Boolean>get("$.success")).isFalse();
+        assertThat(jsonModel.<String>get("$.message")).isEqualTo("Device search domain is not registered");
+    }
+
+    @Test
+    public void testValidationFails() throws IOException {
+        SearchDomain searchDomain = mock(SearchDomain.class);
+        when(searchDomain.getId()).thenReturn(Device.class.getName());
+        when(searchService.findDomain(Device.class.getName())).thenReturn(Optional.of(searchDomain));
+
+        QueryEndDeviceGroup group = mock(QueryEndDeviceGroup.class);
+        GroupBuilder.QueryGroupBuilder<EndDevice, QueryEndDeviceGroup> builder = FakeBuilder
+                .initBuilderStub(group, GroupBuilder.QueryGroupBuilder.class);
+        doReturn(builder).when(meteringGroupService).createQueryEndDeviceGroup(anyVararg());
+        doThrow(new VerboseConstraintViolationException(Collections.emptySet())).when(builder).validate();
+
+        DeviceGroupInfo info = new DeviceGroupInfo();
+        info.dynamic = true;
+        info.name = "NewGroup";
+
+        Response response = target("/devicegroups").queryParam("validate", true).request().post(Entity.json(info));
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+        JsonModel jsonModel = JsonModel.model((ByteArrayInputStream)response.getEntity());
+        assertThat(jsonModel.<Boolean>get("$.success")).isFalse();
     }
 
     @Test
