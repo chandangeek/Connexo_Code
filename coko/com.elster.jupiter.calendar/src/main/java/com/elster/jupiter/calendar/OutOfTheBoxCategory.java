ce1be35da69490ee8e8d8ebdaafdfc38c90a19f0
dvy <dieter.vyncke@elster.com>
2016-11-14 11:27:30 +0100

    CXO-3275 / CXO-3379 / CXO-3362

--- coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/OutOfTheBoxCategory.java
+++ coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/OutOfTheBoxCategory.java
@@ -17,6 +17,7 @@ public enum OutOfTheBoxCategory {
     }
 
     public String getDisplayName(Thesaurus thesaurus) {
-        return thesaurus.getString(CategoryImpl.CALENDAR_CATEGORY_KEY_PREFIX + this.name().toLowerCase(), getDefaultDisplayName());
+        return thesaurus.getString("calendar.category." + this.name().toLowerCase(), getDefaultDisplayName());
+
     }
 }
