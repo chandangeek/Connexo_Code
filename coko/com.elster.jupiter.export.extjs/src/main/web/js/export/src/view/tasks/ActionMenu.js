a9c130e3ae51ebf56ce0c146d2369ea76f3663c5
geert <geert.delmeiren@elster.com>
2016-11-17 08:43:04 +0100

    CXO-899 - Apply order to action menu

--- coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/tasks/ActionMenu.js
+++ coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/tasks/ActionMenu.js
@@ -1,39 +1,44 @@
 Ext.define('Dxp.view.tasks.ActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.dxp-tasks-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            itemId: 'edit-task',
-            text: Uni.I18n.translate('general.edit', 'DES', 'Edit'),
-            privileges: Dxp.privileges.DataExport.update,
-            action: 'editExportTask'
-        },
-        {
-            itemId: 'remove-task',
-            text: Uni.I18n.translate('general.remove', 'DES', 'Remove'),
-            privileges: Dxp.privileges.DataExport.admin,
-            action: 'removeTask'
-        },
-        {
-            itemId: 'view-log',
-            text: Uni.I18n.translate('general.viewLog', 'DES', 'View log'),
-            action: 'viewLog',
-            hidden: true
-        },
-        {
-            itemId: 'view-history',
-            text: Uni.I18n.translate('general.viewHistory', 'DES', 'View history'),
-            action: 'viewHistory'
-        },
-        {
-            itemId: 'run',
-            text: Uni.I18n.translate('general.run', 'DES', 'Run'),
-            action: 'run',
-            hidden: true
-        }
-    ]
-});
 
+    initComponent: function() {
+        this.items = [
+            {
+                itemId: 'run',
+                text: Uni.I18n.translate('general.run', 'DES', 'Run'),
+                action: 'run',
+                hidden: true,
+                section: this.SECTION_ACTION
+            },
+            {
+                itemId: 'edit-task',
+                text: Uni.I18n.translate('general.edit', 'DES', 'Edit'),
+                privileges: Dxp.privileges.DataExport.update,
+                action: 'editExportTask',
+                section: this.SECTION_EDIT
+            },
+            {
+                itemId: 'view-log',
+                text: Uni.I18n.translate('general.viewLog', 'DES', 'View log'),
+                action: 'viewLog',
+                hidden: true,
+                section: this.SECTION_VIEW
+            },
+            {
+                itemId: 'view-history',
+                text: Uni.I18n.translate('general.viewHistory', 'DES', 'View history'),
+                action: 'viewHistory',
+                section: this.SECTION_VIEW
+            },
+            {
+                itemId: 'remove-task',
+                text: Uni.I18n.translate('general.remove', 'DES', 'Remove'),
+                privileges: Dxp.privileges.DataExport.admin,
+                action: 'removeTask',
+                section: this.SECTION_REMOVE
+            }
+        ];
+        this.callParent(arguments);
+    }
+});
