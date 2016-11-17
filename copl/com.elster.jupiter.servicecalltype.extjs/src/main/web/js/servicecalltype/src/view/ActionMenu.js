b317cb963ed88899f82144661ab790bda6a99a89
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-11-17 13:51:21 +0100

    CXO-889

--- copl/com.elster.jupiter.servicecalltype.extjs/src/main/web/js/servicecalltype/src/view/ActionMenu.js
+++ copl/com.elster.jupiter.servicecalltype.extjs/src/main/web/js/servicecalltype/src/view/ActionMenu.js
@@ -1,15 +1,16 @@
 Ext.define('Sct.view.ActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.sct-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            itemId: 'change-log-level-sct',
-            text: Uni.I18n.translate('general.changeLogLevel', 'SCT', 'Change log level'),
-            privileges: Sct.privileges.ServiceCallType.admin,
-            action: 'changeLogLevel'
-        }
-    ]
+    initComponent: function () {
+        this.items = [
+            {
+                itemId: 'change-log-level-sct',
+                text: Uni.I18n.translate('general.changeLogLevel', 'SCT', 'Change log level'),
+                privileges: Sct.privileges.ServiceCallType.admin,
+                action: 'changeLogLevel',
+                section: this.SECTION_ACTION
+            }
+        ];
+        this.callParent(arguments);
+    }
 });
