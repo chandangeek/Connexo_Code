9f1621106d99b9e814ebcb051d83fe5967c1fc63
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-11-17 10:06:10 +0100

    CXO-3484 - Link event set to a metrology configuration

--- coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/impl/UpgraderV10_3.java
+++ coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/impl/UpgraderV10_3.java
@@ -3,10 +3,18 @@ package com.elster.jupiter.calendar.impl;
 import com.elster.jupiter.calendar.OutOfTheBoxCategory;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.Upgrader;
 
 import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.elster.jupiter.util.streams.Currying.perform;
 
 class UpgraderV10_3 implements Upgrader {
 
@@ -19,9 +27,27 @@ class UpgraderV10_3 implements Upgrader {
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
+        try (
+                Connection connection = dataModel.getConnection(true);
+                Statement statement = connection.createStatement();
+        ) {
+           introduceEventSetWithExistingCalendarsSQL()
+                   .forEach(perform(this::execute).on(statement));
+        } catch (SQLException e) {
+            throw new UnderlyingSQLFailedException(e);
+        }
+
         dataModelUpgrader.upgrade(dataModel, Version.version(10, 3));
 
         createNewCategories();
+    }
+
+    void execute(Statement statement, String sql) {
+        try {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new UnderlyingSQLFailedException(e);
+        }
     }
 
     private void createNewCategories() {
@@ -32,4 +58,24 @@ class UpgraderV10_3 implements Upgrader {
         }
     }
 
+
+    private List<String> introduceEventSetWithExistingCalendarsSQL() {
+        return Arrays.asList(
+                "CREATE TABLE CAL_EVENTSET (\"ID\" NUMBER, \"NAME\" VARCHAR2(80 CHAR))",
+                "CREATE UNIQUE INDEX CAL_PK_EVENTSET ON CAL_EVENTSET (\"ID\")",
+                "ALTER TABLE CAL_EVENTSET ADD CONSTRAINT CAL_PK_EVENTSET PRIMARY KEY (\"ID\")",
+                "ALTER TABLE CAL_EVENTSET MODIFY (\"NAME\" NOT NULL ENABLE)",
+                "ALTER TABLE CAL_EVENTSET MODIFY (\"ID\" NOT NULL ENABLE)",
+                "CREATE SEQUENCE  \"CAL_EVENTSETID\"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 1000 NOORDER  NOCYCLE",
+                "ALTER TABLE CAL_EVENT ADD EVENTSET NUMBER",
+                "INSERT INTO CAL_EVENTSET (ID, NAME) select CAL_EVENTSETID.nextVal, NAME from CAL_CALENDAR",
+                "UPDATE CAL_EVENT e SET EVENTSET = (select s.id from CAL_EVENTSET s where s.name = (select c.name from CAL_CALENDAR c where c.id = e.CALENDAR))",
+                "ALTER TABLE CAL_EVENT MODIFY EVENTSET NUMBER NOT NULL",
+                "ALTER TABLE CAL_EVENT ADD CONSTRAINT CAL_EVENT_TO_SET FOREIGN KEY (EVENTSET) REFERENCES CAL_EVENTSET (ID)",
+                "ALTER TABLE CAL_EVENT DROP COLUMN CALENDAR",
+                "ALTER TABLE CAL_CALENDAR ADD EVENTSET NUMBER",
+                "UPDATE CAL_CALENDAR c SET EVENTSET = (select ID from CAL_EVENTSET s where s.name = c.name)",
+                "ALTER TABLE CAL_CALENDAR ADD CONSTRAINT CAL_CAL_TO_EVENTSET FOREIGN KEY (EVENTSET) REFERENCES CAL_EVENTSET (ID)"
+        );
+    }
 }
