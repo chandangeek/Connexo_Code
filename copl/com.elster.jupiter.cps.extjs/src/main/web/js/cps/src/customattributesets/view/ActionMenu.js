8fee13a179fcdf13bfc073cfeb4f5acb268b7eee
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-11-17 13:51:25 +0100

    CXO-889

--- copl/com.elster.jupiter.cps.extjs/src/main/web/js/cps/src/customattributesets/view/ActionMenu.js
+++ copl/com.elster.jupiter.cps.extjs/src/main/web/js/cps/src/customattributesets/view/ActionMenu.js
@@ -1,13 +1,15 @@
 Ext.define('Cps.customattributesets.view.ActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.custom-attribute-sets-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            text: Uni.I18n.translate('customattributesets.editlevels', 'CPS', 'Edit levels'),
-            itemId: 'custom-attribute-sets-edit-levels'
-        }
-    ]
+    initComponent: function () {
+        this.items =
+            [
+                {
+                    text: Uni.I18n.translate('customattributesets.editlevels', 'CPS', 'Edit levels'),
+                    itemId: 'custom-attribute-sets-edit-levels',
+                    section: this.SECTION_EDIT
+                }
+            ];
+        this.callParent(arguments);
+    }
 });
