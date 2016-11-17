fd4d325ca33f369c98bac328043940eb58cd42d7
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-11-17 14:10:06 +0100

    CXO-889

--- copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/src/view/task/TaskActionMenu.js
+++ copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/src/view/task/TaskActionMenu.js
@@ -1,29 +1,33 @@
 Ext.define('Bpm.view.task.TaskActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.bpm-task-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            itemId: 'menu-edit-task',
-            text: Uni.I18n.translate('bpm.menu.editAttributes', 'BPM', 'Edit attributes'),
-            action: 'editTask',
-            privileges: Bpm.privileges.BpmManagement.assign
-        },
-        {
-            itemId: 'menu-perform-task',
-            text: Uni.I18n.translate('bpm.menu.start', 'BPM', 'Start'),
-            action: 'performTask',
-            privileges: Bpm.privileges.BpmManagement.execute
-        },
-        {
-            itemId: 'menu-complete-task',
-            text: Uni.I18n.translate('bpm.menu.complete', 'BPM', 'Complete'),
-            action: 'completeTask',
-            privileges: Bpm.privileges.BpmManagement.execute
-        }
-    ]
+    initComponent: function() {
+
+        this.items = [
+            {
+                itemId: 'menu-perform-task',
+                text: Uni.I18n.translate('bpm.menu.start', 'BPM', 'Start'),
+                action: 'performTask',
+                privileges: Bpm.privileges.BpmManagement.execute,
+                section: this.SECTION_ACTION
+            },
+            {
+                itemId: 'menu-complete-task',
+                text: Uni.I18n.translate('bpm.menu.complete', 'BPM', 'Complete'),
+                action: 'completeTask',
+                privileges: Bpm.privileges.BpmManagement.execute,
+                section: this.SECTION_ACTION
+            },
+            {
+                itemId: 'menu-edit-task',
+                text: Uni.I18n.translate('bpm.menu.editAttributes', 'BPM', 'Edit attributes'),
+                action: 'editTask',
+                privileges: Bpm.privileges.BpmManagement.assign,
+                section: this.SECTION_EDIT
+            }
+        ];
+        this.callParent(arguments);
+    }
 });
 
 
