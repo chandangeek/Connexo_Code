48497947c9010bac3a159b6151da09937728c6e3
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-11-17 13:51:26 +0100

    CXO-889

--- copl/com.elster.jupiter.time.extjs/src/main/web/js/time/src/view/relativeperiod/ActionMenu.js
+++ copl/com.elster.jupiter.time.extjs/src/main/web/js/time/src/view/relativeperiod/ActionMenu.js
@@ -1,21 +1,23 @@
 Ext.define('Tme.view.relativeperiod.ActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.relative-periods-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            itemId: 'remove-period',
-            text: Uni.I18n.translate('general.remove', 'TME', 'Remove'),
-            privileges : Tme.privileges.Period.admin,
-            action: 'removePeriod'
-        },
-        {
-            itemId: 'view-details',
-            text: Uni.I18n.translate('general.viewDetails', 'TME', 'View details'),
-            action: 'viewDetails'
-        }
-    ]
+    initComponent: function() {
+        this.items = [
+            {
+                itemId: 'view-details',
+                text: Uni.I18n.translate('general.viewDetails', 'TME', 'View details'),
+                action: 'viewDetails',
+                section: this.SECTION_VIEW
+            },
+            {
+                itemId: 'remove-period',
+                text: Uni.I18n.translate('general.remove', 'TME', 'Remove'),
+                privileges : Tme.privileges.Period.admin,
+                action: 'removePeriod',
+                section: this.SECTION_REMOVE
+            }
+        ];
+        this.callParent(arguments);
+    }
 });
 
