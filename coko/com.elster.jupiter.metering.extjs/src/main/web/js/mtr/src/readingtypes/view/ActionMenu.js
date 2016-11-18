db0cdb30070ac28d57d35945416e37e4148e94e2
geert <geert.delmeiren@elster.com>
2016-11-18 16:23:45 +0100

    CXO-899 - Apply order to action menu

--- coko/com.elster.jupiter.metering.extjs/src/main/web/js/mtr/src/readingtypes/view/ActionMenu.js
+++ coko/com.elster.jupiter.metering.extjs/src/main/web/js/mtr/src/readingtypes/view/ActionMenu.js
@@ -1,38 +1,43 @@
 Ext.define('Mtr.readingtypes.view.ActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.reading-types-action-menu',
-    shadow: false,
-    border: false,
-    plain: true,
-    items: [
-        {
-            itemId: 'reading-types-sorting-menu-activate',
-            text: Uni.I18n.translate('readingtypesmanagment.activate', 'MTR', 'Activate'),
-            action: 'activate'
-        },
-        {
-            itemId: 'reading-types-sorting-menu-deactivate',
-            text: Uni.I18n.translate('readingtypesmanagment.deactivate', 'MTR', 'Deactivate'),
-            action: 'deactivate'
-        },
-        {
-            itemId: 'reading-types-action-menu-edit',
-            text: Uni.I18n.translate('readingtypesmanagment.edit', 'MTR', 'Edit'),
-            action: 'edit'
-        }
-    ],
+    initComponent: function() {
+        this.items = [
+            {
+                itemId: 'reading-types-sorting-menu-activate',
+                text: Uni.I18n.translate('readingtypesmanagment.activate', 'MTR', 'Activate'),
+                action: 'activate',
+                section: this.SECTION_ACTION
+            },
+            {
+                itemId: 'reading-types-sorting-menu-deactivate',
+                text: Uni.I18n.translate('readingtypesmanagment.deactivate', 'MTR', 'Deactivate'),
+                action: 'deactivate',
+                section: this.SECTION_ACTION
+            },
+            {
+                itemId: 'reading-types-action-menu-edit',
+                text: Uni.I18n.translate('readingtypesmanagment.edit', 'MTR', 'Edit'),
+                action: 'edit',
+                section: this.SECTION_EDIT
+            }
+        ];
+        this.callParent(arguments);
+    },
 
-    beforeShow: function () {
+    listeners: {
+        beforeshow: function(menu) {
+            var activateMenuItem = menu.down('#reading-types-sorting-menu-activate'),
+                deactivateMenuItem = menu.down('#reading-types-sorting-menu-deactivate'),
+                active = menu.record.get('active');
 
-        var activate = this.down('#reading-types-sorting-menu-activate'),
-            deactivate = this.down('#reading-types-sorting-menu-deactivate'),
-            active = this.record.get('active');
-        if (active) {
-            deactivate.show();
-            activate.hide();
-        } else {
-            activate.show();
-            deactivate.hide();
+            if (active) {
+                deactivateMenuItem.show();
+                activateMenuItem.hide();
+            } else {
+                activateMenuItem.show();
+                deactivateMenuItem.hide();
+            }
         }
     }
 });
