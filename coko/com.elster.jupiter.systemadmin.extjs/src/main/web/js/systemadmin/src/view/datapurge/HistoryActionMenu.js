5c255e476c312890b1cd6b4952463fe41827667d
geert <geert.delmeiren@elster.com>
2016-11-18 16:23:46 +0100

    CXO-899 - Apply order to action menu

--- coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/datapurge/HistoryActionMenu.js
+++ coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/datapurge/HistoryActionMenu.js
@@ -1,27 +1,26 @@
 Ext.define('Sam.view.datapurge.HistoryActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.data-purge-history-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    defaultAlign: 'tr-br?',
     router: null,
+    initComponent: function() {
+        this.items = [
+            {
+                text: Uni.I18n.translate('datapurge.history.viewlog', 'SAM', 'View log'),
+                itemId: 'sam-purge-history-view-log',
+                href: me.router.getRoute(me.router.currentRoute + '/log').buildUrl({historyId: me.record.getId()}),
+                section: this.SECTION_VIEW
+            }
+        ];
+        this.callParent(arguments);
+    },
 
     listeners: {
-        show: {
-            fn: function () {
-                var me = this;
-                Ext.suspendLayouts();
-                me.removeAll();
-                if (me.record && me.router) {
-                    me.add(
-                        {
-                            text: Uni.I18n.translate('datapurge.history.viewlog', 'SAM', 'View log'),
-                            href: me.router.getRoute(me.router.currentRoute + '/log').buildUrl({historyId: me.record.getId()})
-                        }
-                    );
-                }
-                Ext.resumeLayouts(true);
+        beforeshow: function(menu) {
+            var viewLogMenuItem = menu.down('#sam-purge-history-view-log');
+            if (menu.record && menu.router) {
+                viewLogMenuItem.show();
+            } else {
+                viewLogMenuItem.hide();
             }
         }
     }
