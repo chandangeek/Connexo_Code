65e21ea99e07f0bc0a6c922666d7c073cbe004df
geert <geert.delmeiren@elster.com>
2016-11-18 16:23:44 +0100

    CXO-899 - Apply order to action menu

--- comu/com.energyict.mdc.bpm.extjs/src/main/web/js/dbp/src/processes/view/ProcessActionMenu.js
+++ comu/com.energyict.mdc.bpm.extjs/src/main/web/js/dbp/src/processes/view/ProcessActionMenu.js
@@ -1,29 +1,32 @@
 Ext.define('Dbp.processes.view.ProcessActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.dbp-process-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            itemId: 'menu-edit-process',
-            text: Uni.I18n.translate('dbp.menu.editProcess', 'DBP', 'Edit process'),
-            action: 'editProcess',
-            privileges: Dbp.privileges.DeviceProcesses.administrateProcesses
-        },
-        {
-            itemId: 'menu-activate-process',
-            text: Uni.I18n.translate('dbp.menu.activateProcess', 'DBP', 'Activate'),
-            action: 'activateProcess',
-            privileges: Dbp.privileges.DeviceProcesses.administrateProcesses
-        },
-        {
-            itemId: 'menu-deactivate-process',
-            text: Uni.I18n.translate('dbp.menu.deactivateProcess', 'DBP', 'Deactivate'),
-            action: 'deactivateProcess',
-            privileges: Dbp.privileges.DeviceProcesses.administrateProcesses
-        }
-    ]
+    initComponent: function() {
+        this.items = [
+            {
+                itemId: 'menu-edit-process',
+                text: Uni.I18n.translate('dbp.menu.editProcess', 'DBP', 'Edit process'),
+                action: 'editProcess',
+                privileges: Dbp.privileges.DeviceProcesses.administrateProcesses,
+                section: this.SECTION_EDIT
+            },
+            {
+                itemId: 'menu-activate-process',
+                text: Uni.I18n.translate('dbp.menu.activateProcess', 'DBP', 'Activate'),
+                action: 'activateProcess',
+                privileges: Dbp.privileges.DeviceProcesses.administrateProcesses,
+                section: this.SECTION_ACTION
+            },
+            {
+                itemId: 'menu-deactivate-process',
+                text: Uni.I18n.translate('dbp.menu.deactivateProcess', 'DBP', 'Deactivate'),
+                action: 'deactivateProcess',
+                privileges: Dbp.privileges.DeviceProcesses.administrateProcesses,
+                section: this.SECTION_ACTION
+            }
+        ];
+        this.callParent(arguments);
+    }
 });
 
 
