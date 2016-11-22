49aee1c9a40f4f30465ef114a8427f3dcd81aeab
dvy <dieter.vyncke@elster.com>
2016-11-22 07:53:24 +0100

    fix translation error

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointmanagement/view/bulk/Step3.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointmanagement/view/bulk/Step3.js
@@ -32,7 +32,7 @@ Ext.define('Imt.usagepointmanagement.view.bulk.Step3', {
                     itemId: 'step3-errors',
                     xtype: 'uni-form-error-message',
                     hidden: true,
-                    text: Uni.I18n.translate('searchItems.bulk.devicesError', 'IMT', 'It is required to select a calendar to go to the next step.')
+                    text: Uni.I18n.translate('searchItems.bulk.calendarError', 'IMT', 'It is required to select a calendar to go to the next step.')
                 }
             ]
         },
