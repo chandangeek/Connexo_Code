9abdf415433a43009e1aebc73c8082a76290f0fc
geert <geert.delmeiren@elster.com>
2016-11-22 13:44:57 +0100

    CXO-4325 - Cannot access data purge history page Back to what it previously was. Only kept the extention of ActionsMenu.

--- coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/datapurge/HistoryActionMenu.js
+++ coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/datapurge/HistoryActionMenu.js
@@ -2,26 +2,22 @@ Ext.define('Sam.view.datapurge.HistoryActionMenu', {
     extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.data-purge-history-action-menu',
     router: null,
-    initComponent: function() {
-        var me = this;
-        me.items = [
-            {
-                text: Uni.I18n.translate('datapurge.history.viewlog', 'SAM', 'View log'),
-                itemId: 'sam-purge-history-view-log',
-                href: me.router.getRoute(me.router.currentRoute + '/log').buildUrl({historyId: me.record.getId()}),
-                section: this.SECTION_VIEW
-            }
-        ];
-        me.callParent(arguments);
-    },
 
     listeners: {
-        beforeshow: function(menu) {
-            var viewLogMenuItem = menu.down('#sam-purge-history-view-log');
-            if (menu.record && menu.router) {
-                viewLogMenuItem.show();
-            } else {
-                viewLogMenuItem.hide();
+        show: {
+            fn: function () {
+                var me = this;
+                Ext.suspendLayouts();
+                me.removeAll();
+                if (me.record && me.router) {
+                    me.add(
+                        {
+                            text: Uni.I18n.translate('datapurge.history.viewlog', 'SAM', 'View log'),
+                            href: me.router.getRoute(me.router.currentRoute + '/log').buildUrl({historyId: me.record.getId()})
+                        }
+                    );
+                }
+                Ext.resumeLayouts(true);
             }
         }
     }
