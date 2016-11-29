5da41e5bab6f72d31a12162a4a2f1b025e50bc65
Yan R <yaratay@mera.ru>
2016-11-29 17:15:14 +0300

    CXO-4479: [BE] SearchDomainExtension fails to fetch columns if DB is upgraded includes as well fix for SD alias

--- coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/impl/SimpleEndDeviceQueryProvider.java
+++ coko/com.elster.jupiter.metering.groups/src/main/java/com/elster/jupiter/metering/groups/impl/SimpleEndDeviceQueryProvider.java
@@ -3,17 +3,33 @@ package com.elster.jupiter.metering.groups.impl;
 import com.elster.jupiter.metering.EndDevice;
 import com.elster.jupiter.metering.groups.MeteringGroupsService;
 import com.elster.jupiter.metering.groups.spi.QueryProvider;
+import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.search.SearchService;
 
 import org.osgi.service.component.annotations.Component;
 
-@Component(
-        name = "com.elster.jupiter.metering.groups.impl.SimpleEndDeviceQueryProvider",
+import javax.inject.Inject;
+
+@Component(name = "com.elster.jupiter.metering.groups.impl.SimpleEndDeviceQueryProvider",
         service = {QueryProvider.class},
         property = "name=" + MeteringGroupsService.COMPONENTNAME,
         immediate = true)
 public class SimpleEndDeviceQueryProvider extends SimpleQueryProvider<EndDevice> implements QueryProvider<EndDevice> {
 
     public static final String SIMPLE_END_DEVICE_QUERY_PROVIDER = SimpleEndDeviceQueryProvider.class.getName();
+
+    // For OSGI
+    public SimpleEndDeviceQueryProvider() {
+        super(EndDevice.class);
+    }
+
+    // For testing
+    @Inject
+    public SimpleEndDeviceQueryProvider(SearchService searchService, NlsService nlsService) {
+        this();
+        setSearchService(searchService);
+        setNlsService(nlsService);
+    }
 
     @Override
     public String getName() {
