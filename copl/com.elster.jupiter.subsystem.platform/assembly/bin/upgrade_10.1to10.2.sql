c15303782445fc8cc46a6672b5243059493b4b83
Bruno Ballekens <bruno.ballekens@elster.com>
2016-11-30 09:55:02 +0100

    CXO-3693: Upgrade ConnexoDemo 10.1 to 10.2 fails

--- copl/com.elster.jupiter.subsystem.platform/assembly/bin/upgrade_10.1to10.2.sql
+++ copl/com.elster.jupiter.subsystem.platform/assembly/bin/upgrade_10.1to10.2.sql
@@ -1,3 +1,4 @@
 UPDATE FSM_EVENT_TYPE SET CONTEXT = 'System' WHERE USERNAME = 'Fsm Install' and CONTEXT is null;
 UPDATE FSM_EVENT_TYPE SET CONTEXT = 'DLD' WHERE USERNAME = 'Jupiter Installer' and CONTEXT is null;
+UPDATE MTR_METERACTIVATION SET meterrole='meter.role.meter.role.default' where meterrole is null;
 COMMIT;
