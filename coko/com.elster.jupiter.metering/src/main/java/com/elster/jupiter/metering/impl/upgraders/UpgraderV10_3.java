838d0ea98edfcf0e5ef151123ec38b353a2b9dc3
Anton Suslennikov <antsusl@mera.ru>
2016-12-05 17:00:04 +0300

    CXO-3311 Added upgrader to 10.3

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/upgraders/UpgraderV10_3.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/upgraders/UpgraderV10_3.java
@@ -2,19 +2,17 @@ package com.elster.jupiter.metering.impl.upgraders;
 
 import com.elster.jupiter.events.EventService;
 import com.elster.jupiter.metering.EventType;
-import com.elster.jupiter.metering.impl.DefaultTranslationKey;
-import com.elster.jupiter.metering.impl.MeteringDataModelService;
-import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.impl.UsagePointImpl;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
-import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.Upgrader;
+import com.elster.jupiter.usagepoint.lifecycle.config.UsagePointLifeCycleConfigurationService;
+import com.elster.jupiter.usagepoint.lifecycle.config.UsagePointState;
+import com.elster.jupiter.util.conditions.Condition;
 
 import javax.inject.Inject;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.EnumSet;
 
 import static com.elster.jupiter.orm.Version.version;
@@ -24,21 +22,38 @@ public class UpgraderV10_3 implements Upgrader {
     private static final Version VERSION = version(10, 3);
     private final DataModel dataModel;
     private final EventService eventService;
+    private final MeteringService meteringService;
+    private final UsagePointLifeCycleConfigurationService lifeCycleConfigurationService;
 
     @Inject
-    public UpgraderV10_3(DataModel dataModel, EventService eventService) {
+    public UpgraderV10_3(DataModel dataModel, EventService eventService, MeteringService meteringService, UsagePointLifeCycleConfigurationService lifeCycleConfigurationService) {
         this.dataModel = dataModel;
         this.eventService = eventService;
+        this.meteringService = meteringService;
+        this.lifeCycleConfigurationService = lifeCycleConfigurationService;
     }
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(dataModel, VERSION);
         installNewEventTypes();
+        setInitialStateForUsagePoints();
     }
 
     private void installNewEventTypes() {
         EnumSet.of(EventType.METROLOGY_CONTRACT_DELETED)
                 .forEach(eventType -> eventType.install(eventService));
+    }
+
+    private void setInitialStateForUsagePoints() {
+        UsagePointState initialState = this.lifeCycleConfigurationService.getDefaultLifeCycle().getStates()
+                .stream()
+                .filter(UsagePointState::isInitial)
+                .findFirst()
+                .get();
+        this.meteringService.getUsagePointQuery().select(Condition.TRUE)
+                .stream()
+                .forEach(usagePoint -> ((UsagePointImpl) usagePoint).setState(initialState, usagePoint.getInstallationTime()));
     }
 }
 
