9b920f9d8f4bbc2de276472461b43fbfa781d3ec
khe <kristof.hennebel@elster.com>
2016-12-06 17:16:41 +0100

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
    First stab at replacing the DeviceProtocol interfaces from mdc.protocols.api by the UPL interfaces.
    Also, the protocols pom now simply includes the 9.1 protocol jars and will not have any specific protocol classes anymore.

--- comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/commands/CreateA3DeviceCommand.java
+++ comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/commands/CreateA3DeviceCommand.java
@@ -12,7 +12,7 @@ import com.elster.jupiter.demo.impl.templates.OutboundTCPComPortPoolTpl;
 import com.elster.jupiter.demo.impl.templates.RegisterTypeTpl;
 import com.elster.jupiter.time.TemporalExpression;
 import com.elster.jupiter.time.TimeDuration;
-import com.energyict.mdc.common.ObisCode;
+import com.energyict.obis.ObisCode;
 import com.energyict.mdc.common.Password;
 import com.energyict.mdc.common.TypedProperties;
 import com.energyict.mdc.device.config.ComTaskEnablement;
