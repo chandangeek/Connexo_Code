8011da7576970af228e62958cebc185fb258d0f9
khe <kristof.hennebel@elster.com>
2016-12-06 17:16:49 +0100

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
    First stab at replacing the DeviceProtocol interfaces from mdc.protocols.api by the UPL interfaces.
    Also, the protocols pom now simply includes the 9.1 protocol jars and will not have any specific protocol classes anymore.

--- comu/com.energyict.mdc.device.config.rest/src/test/java/com/energyict/mdc/device/configuration/rest/impl/BaseLoadProfileTest.java
+++ comu/com.energyict.mdc.device.config.rest/src/test/java/com/energyict/mdc/device/configuration/rest/impl/BaseLoadProfileTest.java
@@ -4,7 +4,7 @@ import com.elster.jupiter.cps.RegisteredCustomPropertySet;
 import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.properties.PropertySpec;
 import com.elster.jupiter.time.TimeDuration;
-import com.energyict.mdc.common.ObisCode;
+import com.energyict.obis.ObisCode;
 import com.energyict.mdc.device.config.DeviceConfiguration;
 import com.energyict.mdc.device.config.DeviceType;
 import com.energyict.mdc.device.config.LoadProfileSpec;
