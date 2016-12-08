b6b32fa5895bf137a1f6d62d4f7f2dc6d3bf06db
Maksim Barinov <mbarinov@mera.ru>
2016-12-08 12:13:45 +0300

    CXO-3311

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointlifecycle/view/ActionMenu.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/usagepointlifecycle/view/ActionMenu.js
@@ -1,27 +1,35 @@
 Ext.define('Imt.usagepointlifecycle.view.ActionMenu', {
-    extend: 'Ext.menu.Menu',
+    extend: 'Uni.view.menu.ActionsMenu',
     alias: 'widget.usagepoint-life-cycles-action-menu',
     xtype: 'usagepoint-life-cycles-action-menu',
-    plain: true,
-    border: false,
-    shadow: false,
-    items: [
-        {
-            text: Uni.I18n.translate('general.clone', 'IMT', 'Clone'),
-            action: 'clone'
-        },
-        {
-            text: Uni.I18n.translate('general.edit', 'IMT', 'Edit'),
-            action: 'edit'
-        },
-        {
-            text: Uni.I18n.translate('general.menu.remove', 'IMT', 'Remove'),
-            action: 'remove'
-        },        
-        {
-            itemId: 'set-as-default',
-            text: Uni.I18n.translate('general.setAsDefault', 'IMT', 'Set as default'),
-            action: 'setAsDefault'
-        }
-    ]
+    initComponent: function () {
+        var me = this;
+        me.items = [
+            {
+                text: Uni.I18n.translate('general.clone', 'IMT', 'Clone'),
+                itemId: 'clone-action',
+                action: 'edit',
+                section: me.SECTION_ACTION
+            },
+            {
+                text: Uni.I18n.translate('general.edit', 'IMT', 'Edit'),
+                itemId: 'edit-action',
+                action: 'edit',
+                section: me.SECTION_EDIT
+            },
+            {
+                text: Uni.I18n.translate('general.menu.remove', 'IMT', 'Remove'),
+                itemId: 'remove-action',
+                action: 'remove',
+                section: me.SECTION_REMOVE
+            },
+            {
+                itemId: 'set-as-default',
+                text: Uni.I18n.translate('general.setAsDefault', 'IMT', 'Set as default'),
+                action: 'setAsDefault',
+                section: me.SECTION_ACTION
+            }
+        ];
+        me.callParent();
+    }
 });
