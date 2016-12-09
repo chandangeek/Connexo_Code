0059c2b5a663e428d6823ec68ab029c15251ab58
Albert Versin <albert.versin@elster.com>
2016-12-09 08:56:22 +0200

    Removed unnecessary methods from DeviceAlarm interface.

--- comu/com.energyict.mdc.device.alarms/src/main/java/com/energyict/mdc/device/alarms/entity/DeviceAlarm.java
+++ comu/com.energyict.mdc.device.alarms/src/main/java/com/energyict/mdc/device/alarms/entity/DeviceAlarm.java
@@ -1,22 +1,13 @@
 package com.energyict.mdc.device.alarms.entity;
 
 import com.elster.jupiter.issue.share.entity.Issue;
-import com.elster.jupiter.metering.events.EndDeviceEventRecord;
-import com.elster.jupiter.metering.readings.EndDeviceEvent;
-
 import com.energyict.mdc.device.alarms.event.DeviceAlarmRelatedEvent;
 
-import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 
 public interface DeviceAlarm extends Issue {
 
     List<DeviceAlarmRelatedEvent> getDeviceAlarmRelatedEvents();
-
-    List<EndDeviceEvent> getEndDeviceRelatedEvents();
-
-    EndDeviceEvent getCurrentEvent();
 
     Boolean getClearedStatus();
 
--- comu/com.energyict.mdc.device.alarms/src/main/java/com/energyict/mdc/device/alarms/impl/records/DeviceAlarmImpl.java
+++ comu/com.energyict.mdc.device.alarms/src/main/java/com/energyict/mdc/device/alarms/impl/records/DeviceAlarmImpl.java
@@ -9,7 +9,6 @@ import com.elster.jupiter.issue.share.entity.IssueReason;
 import com.elster.jupiter.issue.share.entity.IssueStatus;
 import com.elster.jupiter.metering.EndDevice;
 import com.elster.jupiter.metering.UsagePoint;
-import com.elster.jupiter.metering.readings.EndDeviceEvent;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.associations.Reference;
 import com.elster.jupiter.orm.associations.ValueReference;
@@ -19,12 +18,9 @@ import com.energyict.mdc.device.alarms.event.DeviceAlarmRelatedEvent;
 
 import javax.inject.Inject;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class DeviceAlarmImpl implements DeviceAlarm {
 
@@ -160,20 +156,8 @@ public class DeviceAlarmImpl implements DeviceAlarm {
     }
 
     @Override
-    public List<EndDeviceEvent> getEndDeviceRelatedEvents() {
-        return deviceAlarmRelatedEvents.stream()
-                .map(DeviceAlarmRelatedEvent::getEventRecord)
-                .collect(Collectors.toList());
-    }
-
-    @Override
     public List<DeviceAlarmRelatedEvent> getDeviceAlarmRelatedEvents(){
         return deviceAlarmRelatedEvents;
-    }
-
-    @Override
-    public EndDeviceEvent getCurrentEvent() {
-        return Collections.max(getEndDeviceRelatedEvents(), Comparator.comparing(EndDeviceEvent::getCreatedDateTime));
     }
 
     @Override
