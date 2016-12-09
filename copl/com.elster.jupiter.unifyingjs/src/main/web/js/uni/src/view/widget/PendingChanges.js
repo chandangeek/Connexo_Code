cfefebe4d5a4125ce15d1923a275df042c4e2a2d
geert <geert.delmeiren@elster.com>
2016-12-09 11:19:18 +0100

    CXO-4588 - View pending changes

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/widget/PendingChanges.js
@@ -0,0 +1,68 @@
+Ext.define('Uni.view.widget.PendingChanges', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.pendingChangesPanel',
+
+    requires: [
+        'Uni.view.grid.PendingChangesGrid',
+        'Uni.util.FormInfoMessage'
+    ],
+    store: 'Uni.store.PendingChanges',
+
+    title: Uni.I18n.translate('general.pendingChanges', 'UNI', 'Pending changes'),
+    ui: 'large',
+    acceptRejectAllowed: true,
+
+    initComponent: function() {
+        var me = this;
+        me.items = [
+            {
+                xtype: 'uni-form-info-message',
+                itemId: 'uni-pendingChangesPnl-info-msg',
+                text: Uni.I18n.translate('general.noPendingChanges', 'UNI', 'No pending changes found.'),
+                hidden: true
+            },
+            {
+                xtype: 'pendingChangesGrid',
+                itemId: 'uni-pendingChangesPnl-changes-grid',
+                store: me.store
+            },
+            {
+                xtype: 'container',
+                itemId: 'uni-pendingChangesPnl-button-container',
+                hidden: !me.acceptRejectAllowed,
+                defaults: {
+                    xtype: 'button'
+                },
+                items: [
+                    {
+                        text: Uni.I18n.translate('general.accept', 'MDC', 'Accept'),
+                        itemId: 'uni-pendingChangesPnl-accept'
+                    },
+                    {
+                        text: Uni.I18n.translate('general.reject', 'MDC', 'Reject'),
+                        itemId: 'uni-pendingChangesPnl-reject'
+                    }
+                ]
+            }
+        ];
+        me.callParent(arguments);
+    },
+
+    listeners: {
+        afterrender: function(panel) {
+            var grid = panel.down('#uni-pendingChangesPnl-changes-grid'),
+                message = panel.down('#uni-pendingChangesPnl-info-msg'),
+                buttonContainer = panel.down('#uni-pendingChangesPnl-button-container'),
+                changesCount = panel.store.getCount();
+
+            grid.setVisible(changesCount > 0);
+            if (changesCount === 0) {
+                message.setText(Uni.I18n.translate('general.noPendingChanges', 'UNI', 'No pending changes found.'));
+                message.show();
+            } else {
+                message.hide();
+            }
+            buttonContainer.setVisible(panel.acceptRejectAllowed && changesCount > 0);
+        }
+    }
+});
