fd3b0d0ca01c9bd0fd1631fcade13a150e0f1641
Claudiu Isac <claudiu.isac@elster.com>
2016-12-12 12:12:20 +0200

    COMMUNICATION-1890 - [EVN] Beacon: Support for APN_SETUP

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtu3/beacon3100/registers/Beacon3100G3RegisterMapper.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtu3/beacon3100/registers/Beacon3100G3RegisterMapper.java
@@ -1,8 +1,13 @@
 package com.energyict.protocolimplv2.eict.rtu3.beacon3100.registers;
 
 import com.energyict.dlms.cosem.CosemObjectFactory;
+import com.energyict.obis.ObisCode;
+import com.energyict.protocolimpl.dlms.g3.registers.G3Mapping;
 import com.energyict.protocolimpl.dlms.g3.registers.G3RegisterMapper;
+import com.energyict.protocolimpl.dlms.g3.registers.MultiAPNConfigMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TimeZone;
 import java.util.logging.Logger;
 
@@ -13,6 +18,8 @@ import java.util.logging.Logger;
  * @since 2/09/2015 - 9:16
  */
 public class Beacon3100G3RegisterMapper extends G3RegisterMapper {
+
+    public static final ObisCode MULTI_APN_CONFIG = ObisCode.fromString("0.128.25.3.0.255");
 
     /**
      * G3 register mapping, used to read data from the meter as a register value
@@ -34,5 +41,12 @@ public class Beacon3100G3RegisterMapper extends G3RegisterMapper {
         this.mappings.addAll(getGprsModemSetupRegistering());
         this.mappings.addAll(getPPPSetupRegistering());
         this.mappings.addAll(getMetrologyRegistering());
+        this.mappings.addAll(getMultiAPNConfigurationMappings());
+    }
+
+    private final List<G3Mapping> getMultiAPNConfigurationMappings() {
+        final List<G3Mapping> apnConfigsMappings = new ArrayList<G3Mapping>();
+        apnConfigsMappings.add(new MultiAPNConfigMapping(MULTI_APN_CONFIG));
+        return apnConfigsMappings;
     }
 }
