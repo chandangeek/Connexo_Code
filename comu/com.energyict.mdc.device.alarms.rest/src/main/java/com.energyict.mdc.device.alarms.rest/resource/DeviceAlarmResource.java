a27b0276d4baf59584cb1a9bcf22886ef0a47c99
Albert Versin <albert.versin@elster.com>
2016-12-13 14:04:02 +0200

    Add post comment rest for alarms.

--- comu/com.energyict.mdc.device.alarms.rest/src/main/java/com.energyict.mdc.device.alarms.rest/resource/DeviceAlarmResource.java
+++ comu/com.energyict.mdc.device.alarms.rest/src/main/java/com.energyict.mdc.device.alarms.rest/resource/DeviceAlarmResource.java
@@ -1,11 +1,17 @@
 package com.energyict.mdc.device.alarms.rest.resource;
 
 import com.elster.jupiter.domain.util.Finder;
+import com.elster.jupiter.domain.util.Query;
+import com.elster.jupiter.issue.rest.request.CreateCommentRequest;
 import com.elster.jupiter.issue.rest.resource.StandardParametersBean;
+import com.elster.jupiter.issue.rest.response.IssueCommentInfo;
+import com.elster.jupiter.issue.share.entity.IssueComment;
 import com.elster.jupiter.rest.util.JsonQueryFilter;
 import com.elster.jupiter.rest.util.JsonQueryParameters;
 import com.elster.jupiter.rest.util.PagedInfoList;
 import com.elster.jupiter.rest.util.Transactional;
+import com.elster.jupiter.users.User;
+import com.elster.jupiter.util.conditions.Condition;
 import com.elster.jupiter.util.conditions.Order;
 import com.energyict.mdc.device.alarms.DeviceAlarmFilter;
 import com.energyict.mdc.device.alarms.DeviceAlarmService;
@@ -17,17 +23,23 @@ import com.energyict.mdc.device.alarms.security.Privileges;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static com.elster.jupiter.util.conditions.Where.where;
 
 
 @Path("/alarms")
@@ -69,6 +81,38 @@ public class DeviceAlarmResource{
         Optional<? extends DeviceAlarm> deviceAlarm = deviceAlarmService.findAlarm(id);
         return deviceAlarm.map(i -> Response.ok().entity(deviceAlarmInfoFactory.asInfo(i)).build())
                 .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+    }
+
+    @GET
+    @Transactional
+    @Path("/{id}/comments")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed({Privileges.Constants.VIEW_ALARM, Privileges.Constants.ASSIGN_ALARM, Privileges.Constants.CLOSE_ALARM, Privileges.Constants.COMMENT_ALARM, Privileges.Constants.ACTION_ALARM})
+    public PagedInfoList getComments(@PathParam("id") long id, @BeanParam JsonQueryParameters queryParameters) {
+//        DeviceAlarm deviceAlarm = deviceAlarmService.findAlarm(id).orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+//        return PagedInfoList.fromCompleteList("comments", getAlarmComments(deviceAlarm), queryParameters);
+        return null;
+    }
+
+    @POST
+    @Transactional
+    @Path("/{id}/comments")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed(Privileges.Constants.COMMENT_ALARM)
+    public Response postComment(@PathParam("id") long id, CreateCommentRequest request, @Context SecurityContext securityContext) {
+        DeviceAlarm deviceAlarm = deviceAlarmService.findAlarm(id).orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+        User author = (User) securityContext.getUserPrincipal();
+        IssueComment comment = deviceAlarm.addComment(request.getComment(), author)
+                .orElseThrow(() -> new WebApplicationException(Response.Status.BAD_REQUEST));
+        return Response.ok(new IssueCommentInfo(comment)).status(Response.Status.CREATED).build();
+    }
+
+    public List<IssueCommentInfo> getAlarmComments(DeviceAlarm deviceAlarm) {
+        Condition condition = where("issueId").isEqualTo(deviceAlarm.getId());
+        Query<IssueComment> query = deviceAlarmService.query(IssueComment.class, User.class);
+        List<IssueComment> commentsList = query.select(condition, Order.ascending("createTime"));
+        return commentsList.stream().map(IssueCommentInfo::new).collect(Collectors.toList());
     }
 
     private void validateMandatory(StandardParametersBean params, String... mandatoryParameters) {
