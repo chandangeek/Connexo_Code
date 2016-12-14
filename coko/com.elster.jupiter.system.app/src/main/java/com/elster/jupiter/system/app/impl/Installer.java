380dc3a971d3ca521bd3fbd8073dcdd248a630d7
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-12-14 10:50:03 +0100

    CXO-4577 - Out of the box 'Installer role'

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/impl/Installer.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/impl/Installer.java
@@ -36,5 +36,17 @@ final class Installer implements FullInstaller, Upgrader {
         String[] adminPrivileges = getAdminPrivileges();
         userService.grantGroupWithPrivilege(UserService.DEFAULT_ADMIN_ROLE, SysAppService.APPLICATION_KEY, adminPrivileges);
         userService.grantGroupWithPrivilege(UserService.BATCH_EXECUTOR_ROLE, SysAppService.APPLICATION_KEY, adminPrivileges);
+        userService.grantGroupWithPrivilege(UserService.DEFAULT_INSTALLER_ROLE, SysAppService.APPLICATION_KEY, installerPrivileges());
+    }
+
+    private String[] installerPrivileges() {
+        return new String[]{
+                //license
+                com.elster.jupiter.license.security.Privileges.Constants.VIEW_LICENSE,
+                com.elster.jupiter.license.security.Privileges.Constants.UPLOAD_LICENSE,
+                //users
+                com.elster.jupiter.users.security.Privileges.Constants.ADMINISTRATE_USER_ROLE,
+                com.elster.jupiter.users.security.Privileges.Constants.VIEW_USER_ROLE
+        };
     }
 }
