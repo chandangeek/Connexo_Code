e1c4743a6dd60991720ef9d6c1c2718846b7480d
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-12-15 13:34:31 +0100

    CXO-4586 - Out of the box 'Dual control administrator' role

--- copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/src/privileges/Users.js
+++ copl/com.elster.jupiter.users.extjs/src/main/web/js/usr/src/privileges/Users.js
@@ -8,8 +8,8 @@ Ext.define('Usr.privileges.Users', {
         'Uni.Auth'
     ],
     singleton: true,
-    view : ['privilege.administrate.userAndRole', 'privilege.view.userAndRole'],
-    admin : ['privilege.administrate.userAndRole'],
+    view : ['privilege.administrate.userAndRole', 'privilege.view.userAndRole', 'dualcontrol.grant.approval'],
+    admin : ['privilege.administrate.userAndRole', 'dualcontrol.grant.approval'],
     all: function() {
         return Ext.Array.merge(Usr.privileges.Users.view,
             Usr.privileges.Users.admin);
