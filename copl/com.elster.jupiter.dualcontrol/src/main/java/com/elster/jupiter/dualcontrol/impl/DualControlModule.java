c5cfa6d69c862b6df68487a81437a3fbd413c8b2
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-12-15 15:34:38 +0100

    CXO-4586 - Out of the box 'Dual control administrator' role

--- copl/com.elster.jupiter.dualcontrol/src/main/java/com/elster/jupiter/dualcontrol/impl/DualControlModule.java
+++ copl/com.elster.jupiter.dualcontrol/src/main/java/com/elster/jupiter/dualcontrol/impl/DualControlModule.java
@@ -3,11 +3,12 @@ package com.elster.jupiter.dualcontrol.impl;
 import com.elster.jupiter.dualcontrol.DualControlService;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 public class DualControlModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(DualControlService.class).to(DualControlServiceImpl.class);
+        bind(DualControlService.class).to(DualControlServiceImpl.class).in(Scopes.SINGLETON);
 
     }
 }
