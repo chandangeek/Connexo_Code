32b6d35bb090634067b100b01a8940f080107930
Kirill Koshelev <kirkosh@mera.ru>
2016-12-15 19:16:49 +0300

    [CXO-4504] - Edit order of estimation rule sets

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/metrologyconfiguration/view/estimation/PurposeWithRuleSetsGrid.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/metrologyconfiguration/view/estimation/PurposeWithRuleSetsGrid.js
@@ -5,11 +5,16 @@ Ext.define('Imt.metrologyconfiguration.view.estimation.PurposeWithRuleSetsGrid',
     router: null,
     metrologyConfig: null,
     requires: [
-        'Uni.grid.column.RemoveAction'
+        'Uni.grid.column.RemoveAction',
+        'Uni.grid.plugin.DragDropForGridWithGroups'
     ],
 
     initComponent: function () {
-        var me = this;
+        var me = this,
+            buttons,
+            rulesetsCounter = 0,
+            currentMetrologyContractId = 0;
+
 
         me.features = [{
             ftype: 'grouping',
@@ -63,6 +68,69 @@ Ext.define('Imt.metrologyconfiguration.view.estimation.PurposeWithRuleSetsGrid',
                 flex: 1
             },
             {
+                header: Uni.I18n.translate('general.order', 'IMT', 'Order'),
+                renderer: function (value, metaData, record, rowIndex) {
+                    if(currentMetrologyContractId != record.get('metrologyContractId')){
+                        rulesetsCounter = rowIndex;
+                        currentMetrologyContractId = record.get('metrologyContractId');
+                    }
+                    return ++rowIndex - rulesetsCounter;
+                }
+            }
+        ];
+
+        if (me.editOrder) {
+            me.viewConfig = {
+                plugins: {
+                    ptype: 'gridviewwithgroupsdragdrop',
+                    groupByHiddenField: 'hiddenGroupId',
+                    dragText: '&nbsp;'
+                },
+                listeners: {
+                    drop: {
+                        fn: function () {
+                            me.getView().refresh();
+                        }
+                    }
+                }
+            };
+            me.selModel = {
+                mode: 'MULTI'
+            };
+            me.columns.push(
+                {
+                    header: Uni.I18n.translate('general.ordering', 'IMT', 'Ordering'),
+                    align: 'center',
+                    renderer: function () {
+                        return '<span class="icon-stack3"></span>';
+                    }
+                },
+                //Hidden column is needed for gridviewwithgroupsdragdrop plugin (hidden: true will remove this column from html code)
+                {
+                    dataIndex: 'hiddenGroupId',
+                    width: 1
+                }
+            );
+
+            buttons = [
+                {
+                    xtype: 'button',
+                    itemId: 'btn-save-estimation-rules-order',
+                    text: Uni.I18n.translate('general.saveOrder', 'IMT', 'Save order'),
+                    action: 'saveEstimationRuleSetsOrder',
+                    privileges: Imt.privileges.MetrologyConfig.adminEstimation
+                },
+                {
+                    xtype: 'button',
+                    itemId: 'btn-undo-estimation-rules-order',
+                    text: Uni.I18n.translate('general.undo', 'IMT', 'Undo'),
+                    action: 'undoEstimationRuleSetsOrder',
+                    href: me.router.getRoute(me.router.currentRoute).buildUrl(me.router.arguments, null),
+                    privileges: Imt.privileges.MetrologyConfig.adminEstimation
+                }
+            ]
+        } else {
+            me.columns.push({
                 xtype: 'uni-actioncolumn-remove',
                 itemId: 'remove-rule-set-from-purpose-column',
                 hidden: me.metrologyConfig.get('status').id == 'deprecated',
@@ -70,8 +138,27 @@ Ext.define('Imt.metrologyconfiguration.view.estimation.PurposeWithRuleSetsGrid',
                 handler: function (grid, rowIndex, colIndex, item, e, record) {
                     this.fireEvent('removeRuleSetFromPurpose', record);
                 }
-            }
-        ];
+            });
+
+            buttons = [
+                {
+                    xtype: 'button',
+                    itemId: 'btn-edit-order-estimation-rules',
+                    text: Uni.I18n.translate('estimation.general.editOrder', 'IMT', 'Edit order'),
+                    action: 'editOrderEstimationRules',
+                    href: me.router.getRoute('administration/metrologyconfiguration/view/estimation').buildUrl(me.router.arguments, {editOrder: true}),
+                    privileges: Imt.privileges.MetrologyConfig.adminEstimation
+                },
+                {
+                    xtype: 'button',
+                    itemId: 'metrology-config-add-estimation-rule-set-btn',
+                    text: Uni.I18n.translate('estimation.addRuleSet', 'IMT', 'Add estimation rule set'),
+                    privileges: Imt.privileges.MetrologyConfig.adminEstimation,
+                    action: 'addEstimationRuleSet',
+                    href: me.router.getRoute('administration/metrologyconfiguration/view/estimation/add').buildUrl()
+                }
+            ]
+        }
 
         me.dockedItems = [
             {
@@ -82,16 +169,7 @@ Ext.define('Imt.metrologyconfiguration.view.estimation.PurposeWithRuleSetsGrid',
                 displayMsg: Uni.I18n.translate('metrologyConfiguration.estimation.ruleSetsCount', 'IMT', '{0} estimation rule set(s)', me.getStore().totalCount),
                 noBottomPaging: true,
                 usesExactCount: true,
-                items: [
-                    {
-                        xtype: 'button',
-                        itemId: 'metrology-config-add-estimation-rule-set-btn',
-                        text: Uni.I18n.translate('estimation.addRuleSet', 'IMT', 'Add estimation rule set'),
-                        privileges: Imt.privileges.MetrologyConfig.adminEstimation,
-                        action: 'addEstimationRuleSet',
-                        href: me.router.getRoute('administration/metrologyconfiguration/view/estimation/add').buildUrl()
-                    }
-                ]
+                items: buttons
             }
         ];
 
