c58558bc5694b45d81c60b54338f07ad3030cd81
Tom De Greyt <Tom.DeGreyt@elster.com>
2016-12-20 13:35:42 +0100

    CXO-4580 - Add CRC check to ORM layer

--- copl/com.elster.jupiter.datavault/src/main/java/com/elster/jupiter/datavault/DataVault.java
+++ copl/com.elster.jupiter.datavault/src/main/java/com/elster/jupiter/datavault/DataVault.java
@@ -1,5 +1,7 @@
 package com.elster.jupiter.datavault;
 
+import com.elster.jupiter.orm.Encrypter;
+
 import java.io.OutputStream;
 import java.io.Serializable;
 
@@ -8,7 +10,7 @@ import java.io.Serializable;
  *
  * @since 9/6/12 3:39 PM
  */
-public interface DataVault extends Serializable {
+public interface DataVault extends Serializable, Encrypter {
     String encrypt(byte[] decrypted);
     byte[] decrypt(String encrypted);
     void createVault(OutputStream stream);
