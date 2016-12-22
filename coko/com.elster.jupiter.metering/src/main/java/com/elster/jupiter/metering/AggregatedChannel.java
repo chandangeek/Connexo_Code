a47aad9a059e215e0752098b568bb8efbcdd73c0
Anton Fomchenko <antfom@mera.ru>
2016-12-22 17:59:46 +0300

    CXO-3319 remarks from JP-950

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/AggregatedChannel.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/AggregatedChannel.java
@@ -1,10 +1,12 @@
 package com.elster.jupiter.metering;
 
+import aQute.bnd.annotation.ProviderType;
 import com.google.common.collect.Range;
 
 import java.time.Instant;
 import java.util.List;
 
+@ProviderType
 public interface AggregatedChannel extends Channel {
 
     List<IntervalReadingRecord> getPersistedIntervalReadings(Range<Instant> interval);
