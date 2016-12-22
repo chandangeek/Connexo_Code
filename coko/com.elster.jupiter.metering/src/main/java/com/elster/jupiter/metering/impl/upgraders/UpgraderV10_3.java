5f4d04bb9db2ebeea5bc2e16f58b7f335b4f8a5c
Anton Fomchenko <ailescendres@gmail.com>
2016-12-22 23:10:35 +0300

    CXO-4006 upgrade effective comtract to 10.3

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/upgraders/UpgraderV10_3.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/upgraders/UpgraderV10_3.java
@@ -5,11 +5,15 @@ import com.elster.jupiter.metering.EventType;
 import com.elster.jupiter.metering.impl.ServerMeteringService;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.time.TimeService;
 import com.elster.jupiter.upgrade.Upgrader;
 
 import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.EnumSet;
 
 import static com.elster.jupiter.orm.Version.version;
@@ -38,10 +42,26 @@ public class UpgraderV10_3 implements Upgrader {
         dataModelUpgrader.upgrade(dataModel, VERSION);
         installNewEventTypes();
         GasDayRelativePeriodCreator.createAll(this.meteringService, this.timeService);
+        try (Connection connection = this.dataModel.getConnection(true)) {
+            this.upgradeEffectiveContracts(connection);
+        } catch (SQLException e) {
+            throw new UnderlyingSQLFailedException(e);
+        }
     }
 
     private void installNewEventTypes() {
         EnumSet.of(EventType.METROLOGY_CONTRACT_DELETED)
                 .forEach(eventType -> eventType.install(eventService));
+    }
+
+    private void upgradeEffectiveContracts(Connection connection) {
+        String[] sqlStatements = { "update MTR_EFFECTIVE_CONTRACT set CHANNELS_CONTAINER = (select MTR_CHANNEL_CONTAINER.ID from MTR_CHANNEL_CONTAINER where MTR_CHANNEL_CONTAINER.EFFECTIVE_CONTRACT = MTR_EFFECTIVE_CONTRACT.ID)"};
+        for (String sqlStatement : sqlStatements) {
+            try (PreparedStatement statement = connection.prepareStatement(sqlStatement)) {
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                throw new UnderlyingSQLFailedException(e);
+            }
+        }
     }
 }
