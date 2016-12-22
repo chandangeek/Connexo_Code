ebe92b888fbe309e675a08599b53949f92dd1bc9
Anton Fomchenko <antfom@mera.ru>
2016-12-22 17:58:42 +0300

    CXO-3319 remarks from JP-950

--- coko/com.elster.jupiter.estimation/src/main/java/com/elster/jupiter/estimation/EstimationResolver.java
+++ coko/com.elster.jupiter.estimation/src/main/java/com/elster/jupiter/estimation/EstimationResolver.java
@@ -4,8 +4,11 @@ import com.elster.jupiter.metering.ChannelsContainer;
 import com.elster.jupiter.metering.Meter;
 import com.elster.jupiter.metering.MeterActivation;
 
+import aQute.bnd.annotation.ConsumerType;
+
 import java.util.List;
 
+@ConsumerType
 public interface EstimationResolver {
 
     boolean isEstimationActive(Meter meter);
