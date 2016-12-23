9814987abdcdc566b4f2b2426d5e01a156e3558b
Yan R <yaratay@mera.ru>
2016-12-23 14:40:13 +0300

    CXO-4898: Connection state of existing usage points must be updated after introduction of life cycle + small update on upgraders

--- comu/com.energyict.mdc.engine.config/src/main/java/com/energyict/mdc/engine/config/impl/UpgraderV10_2.java
+++ comu/com.energyict.mdc.engine.config/src/main/java/com/energyict/mdc/engine/config/impl/UpgraderV10_2.java
@@ -2,12 +2,9 @@ package com.energyict.mdc.engine.config.impl;
 
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
-import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 import com.elster.jupiter.upgrade.Upgrader;
 
 import javax.inject.Inject;
-
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,13 +31,5 @@ public class UpgraderV10_2 implements Upgrader {
                 sql.forEach(sqlCommand -> execute(statement, sqlCommand));
             }
         });
-    }
-
-    private void execute(Statement statement, String sql) {
-        try {
-            statement.execute(sql);
-        } catch (SQLException e) {
-            throw new UnderlyingSQLFailedException(e);
-        }
     }
 }
