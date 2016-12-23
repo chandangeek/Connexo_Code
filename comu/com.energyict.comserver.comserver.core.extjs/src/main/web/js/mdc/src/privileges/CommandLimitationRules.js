e9c8c1e95c5cfa2e5a8aa839320314ac3eb175dd
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2016-12-23 15:21:09 +0100

    Approve/reject pending changes role

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/privileges/CommandLimitationRules.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/privileges/CommandLimitationRules.js
@@ -3,16 +3,19 @@ Ext.define('Mdc.privileges.CommandLimitationRules', {
         'Uni.Auth'
     ],
     singleton: true,
-    view : ['privilege.administrate.commandLimitationRule', 'privilege.view.commandLimitationRule'],
+    view : ['privilege.administrate.commandLimitationRule', 'privilege.view.commandLimitationRule', 'privilege.approve.commandLimitationRule'],
     admin: ['privilege.administrate.commandLimitationRule'],
+    approval: ['privilege.approve.commandLimitationRule'],
     all: function() {
-        return Ext.Array.merge(Mdc.privileges.CommandLimitationRules.view, Mdc.privileges.CommandLimitationRules.admin);
+        return Ext.Array.merge(Mdc.privileges.CommandLimitationRules.view, Mdc.privileges.CommandLimitationRules.admin, Mdc.privileges.CommandLimitationRules.approval);
     },
     canView: function(){
         return Uni.Auth.checkPrivileges(Mdc.privileges.CommandLimitationRules.view );
     },
+    canAdmin: function() {
+        return Uni.Auth.checkPrivileges(Mdc.privileges.CommandLimitationRules.admin);
+    },
     canApproveReject: function() {
-        // TODO: later on, an extra Approve/Reject privilege should be used here
-        return Uni.Auth.checkPrivileges(Mdc.privileges.CommandLimitationRules.admin );
+        return Uni.Auth.checkPrivileges(Mdc.privileges.CommandLimitationRules.approval );
     }
 });
