5ec9388cf7109f795778610ae695ffae98104a54
Yan R <yaratay@mera.ru>
2016-12-23 14:40:12 +0300

    CXO-4898: Connection state of existing usage points must be updated after introduction of life cycle + small update on upgraders

--- copl/com.elster.jupiter.upgrade/src/main/java/com/elster/jupiter/upgrade/Upgrader.java
+++ copl/com.elster.jupiter.upgrade/src/main/java/com/elster/jupiter/upgrade/Upgrader.java
@@ -1,11 +1,28 @@
 package com.elster.jupiter.upgrade;
 
 import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.UnderlyingSQLFailedException;
 
 import aQute.bnd.annotation.ConsumerType;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
 
 @ConsumerType
 public interface Upgrader {
 
     void migrate(DataModelUpgrader dataModelUpgrader);
+
+    default void execute(Statement statement, String sql) {
+        try {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            String name = this.getClass().getName();
+            Logger.getLogger(name)
+                    .severe("Exception during upgrade by " + name + System.lineSeparator() +
+                            "Failed statement: " + sql);
+            throw new UnderlyingSQLFailedException(e);
+        }
+    }
 }
