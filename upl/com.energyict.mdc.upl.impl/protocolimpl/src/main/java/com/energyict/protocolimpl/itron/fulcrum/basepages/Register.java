d38d6bcfa9f7857f1e07b18baeff2769fd40b16b
rudi <rudi.vankeirsbilck@honeywell.com>
2016-12-29 13:37:02 +0100

    CXO-2951: Fix compile errors

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/itron/fulcrum/basepages/Register.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/itron/fulcrum/basepages/Register.java
@@ -10,56 +10,58 @@
 
 package com.energyict.protocolimpl.itron.fulcrum.basepages;
 
-import com.energyict.cbo.*;
-import com.energyict.obis.*;
-import com.energyict.protocol.*;
-import java.io.*;
-import java.math.*;
-import java.util.*;
+import com.energyict.cbo.Unit;
+import com.energyict.obis.ObisCode;
+import com.energyict.protocol.ProtocolUtils;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  *
  * @author Koen
  */
 public class Register {
-    
+
     private int address;
     private int format;
     private ObisCode obisCode;
     private Unit unit;
     private String description;
     private int selfReadSet=-1;
-    
+
     public static final int FORMAT_IEEE_32BIT_FP=0;
     public static final int FORMAT_IEEE_32BIT_FP_WITH_5BYTE_TIMESTAMP=1;
     public static final int FORMAT_INTEGER_16BIT=2;
-    
+
     public Register(Register register, int address, int billingPoint, int selfReadSet) {
         this.setAddress(address);
         this.setFormat(register.getFormat());
         this.setObisCode(new ObisCode(register.getObisCode().getA(),register.getObisCode().getB(),register.getObisCode().getC(),register.getObisCode().getD(),register.getObisCode().getE(),billingPoint));
         this.setDescription(register.getDescription());
-        this.setUnit(register.getUnit());   
+        this.setUnit(register.getUnit());
         this.setSelfReadSet(selfReadSet);
     }
-    
-    
 
-    
+
+
+
     /** Creates a new instance of Register */
     public Register(int address, ObisCode obisCode) {
-        this(address, FORMAT_IEEE_32BIT_FP, obisCode, obisCode.getDescription());
+        this(address, FORMAT_IEEE_32BIT_FP, obisCode, obisCode.toString());
     }
     public Register(int address, ObisCode obisCode, String description) {
         this(address, FORMAT_IEEE_32BIT_FP, obisCode, description);
     }
     public Register(int address, int format, ObisCode obisCode) {
-        this(address, format, obisCode, obisCode.getDescription());
+        this(address, format, obisCode, obisCode.toString());
     }
     public Register(int address, int format, ObisCode obisCode, String description) {
         this(address, format, obisCode, description, obisCode.getUnitElectricity(0));
     }
-            
+
     public Register(int address, int format, ObisCode obisCode, String description, Unit unit) {
         this.setAddress(address);
         this.setFormat(format);
@@ -70,11 +72,9 @@ public class Register {
 
     public String toString() {
         // Generated code by ToStringBuilder
-        StringBuffer strBuff = new StringBuffer();
-        strBuff.append("Register: 0x"+Integer.toHexString(getAddress())+", "+getDescription()+", "+getObisCode()+"\n");
-        return strBuff.toString();
-    }     
-    
+        return ("Register: 0x" + Integer.toHexString(getAddress()) + ", " + getDescription() + ", " + getObisCode() + "\n");
+    }
+
     public int getLength() throws IOException {
         switch(getFormat()) {
             case FORMAT_IEEE_32BIT_FP: {
@@ -89,7 +89,7 @@ public class Register {
         }
         throw new IOException("Register, getLength(), error invalid format "+getFormat());
     }
-    
+
     public BigDecimal getValue(byte[]data) throws IOException {
         switch(getFormat()) {
             case FORMAT_IEEE_32BIT_FP: {
@@ -104,7 +104,7 @@ public class Register {
         }
         throw new IOException("Register, getValue(...), error invalid format "+getFormat());
     }
-    
+
     public Date getTimestamp(byte[]data, TimeZone timeZone) throws IOException {
         switch(getFormat()) {
             case FORMAT_IEEE_32BIT_FP: {
@@ -119,8 +119,8 @@ public class Register {
         }
         throw new IOException("Register, getTimestamp(...), error invalid format "+getFormat());
     }
-    
-    
+
+
     public int getAddress() {
         return address;
     }
@@ -172,5 +172,5 @@ public class Register {
     public boolean isSelfReadRegister() {
         return selfReadSet != -1;
     }
-    
+
 }
