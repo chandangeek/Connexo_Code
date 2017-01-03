3e026545256b85a1d53a7ddd1d3f4195a34555cc
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-01-03 16:50:56 +0100

    CXO-4933 & CXO-4934

--- coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/impl/Installer.java
+++ coko/com.elster.jupiter.system.app/src/main/java/com/elster/jupiter/system/app/impl/Installer.java
@@ -29,14 +29,17 @@ final class Installer implements FullInstaller, Upgrader {
     }
 
     private String[] getAdminPrivileges() {
-        return SysAppPrivileges.getApplicationPrivileges().stream().toArray(String[]::new);
+        return SysAppPrivileges.getApplicationPrivileges().stream()
+                .filter(name -> !name.equals(com.elster.jupiter.dualcontrol.Privileges.Constants.GRANT_APPROVAL))
+                .toArray(String[]::new);
     }
 
     private void grantPrivileges() {
-        String[] adminPrivileges = adminPrivileges();
-        userService.grantGroupWithPrivilege(UserService.DEFAULT_ADMIN_ROLE, SysAppService.APPLICATION_KEY, adminPrivileges);
+        String[] adminPrivileges = userAdminPrivileges();
+        userService.grantGroupWithPrivilege(UserService.USER_ADMIN_ROLE, SysAppService.APPLICATION_KEY, adminPrivileges);
         userService.grantGroupWithPrivilege(UserService.BATCH_EXECUTOR_ROLE, SysAppService.APPLICATION_KEY, adminPrivileges);
         userService.grantGroupWithPrivilege(UserService.DEFAULT_INSTALLER_ROLE, SysAppService.APPLICATION_KEY, installerPrivileges());
+        userService.grantGroupWithPrivilege(UserService.SYSTEM_ADMIN_ROLE, SysAppService.APPLICATION_KEY, getAdminPrivileges());
     }
 
     private String[] installerPrivileges() {
@@ -50,11 +53,12 @@ final class Installer implements FullInstaller, Upgrader {
         };
     }
 
-    private String[] adminPrivileges() {
+    private String[] userAdminPrivileges() {
         return new String[]{
                 //users
                 com.elster.jupiter.users.security.Privileges.Constants.ADMINISTRATE_USER_ROLE,
                 com.elster.jupiter.users.security.Privileges.Constants.VIEW_USER_ROLE,
         };
+
     }
 }
