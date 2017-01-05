395a1c116e2fcee7085b20a094ae37f25f52df5a
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-01-05 11:25:12 +0100

    CXO-4589

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/command/impl/CommandRuleImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/command/impl/CommandRuleImpl.java
@@ -242,7 +242,6 @@ public class CommandRuleImpl implements CommandRule, UnderDualControl<CommandRul
     }
 
     public void save() {
-        this.numberOfCommands = commands.size();
         if (this.getId() > 0) {
             doUpdate();
         } else {
@@ -336,6 +335,8 @@ public class CommandRuleImpl implements CommandRule, UnderDualControl<CommandRul
             weekLimit = commandRulePendingUpdate.getWeekLimit();
             monthLimit = commandRulePendingUpdate.getMonthLimit();
             active = commandRulePendingUpdate.isActive();
+            this.numberOfCommands = commandRulePendingUpdate.getCommands().stream().count();
+            this.save();
             this.commands.clear();
             commandRulePendingUpdate.getCommands()
                     .stream()
