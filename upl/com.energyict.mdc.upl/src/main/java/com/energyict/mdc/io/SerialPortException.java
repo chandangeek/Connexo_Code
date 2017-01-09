117a6d3b807f702bffcec3ee939ef1b14edf4dcb
rudi <rudi.vankeirsbilck@honeywell.com>
2017-01-09 14:32:51 +0100

    CXO-2951: Fix connection type compile errors

--- upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/io/SerialPortException.java
+++ upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/io/SerialPortException.java
@@ -9,9 +9,13 @@ package com.energyict.mdc.io;
 public class SerialPortException extends RuntimeException {
 
     private final Type type;
+    private final Object[] messageArguments;
 
     public enum Type {
-        SERIAL_PORT_LIBRARY_EXCEPTION;
+        SERIAL_PORT_LIBRARY_EXCEPTION,
+        SERIAL_PORT_CONFIGURATION_MISMATCH,
+        SERIAL_PORT_DOES_NOT_EXIST,
+        SERIAL_PORT_IS_IN_USE;
     }
 
     /**
@@ -24,17 +28,55 @@ public class SerialPortException extends RuntimeException {
         return new SerialPortException(Type.SERIAL_PORT_LIBRARY_EXCEPTION, cause);
     }
 
+    /**
+     * Creates a {@link SerialPortException} indicating that the value of some configuration property is not as expected
+     *
+     * @param propertyName   the name of the property
+     * @param invalidValue the invalid value of the property
+     * @return the newly crated exception
+     */
+    public static SerialPortException configurationMisMatch(String propertyName, String invalidValue) {
+        return new SerialPortException(Type.SERIAL_PORT_CONFIGURATION_MISMATCH, propertyName, invalidValue);
+    }
+
+    /**
+     * Creates a {@link SerialPortException} indicating that the SerialPort with the given name does not exist.
+     *
+     * @param name the name of the ComPort that does not exist
+     * @return the newly create exception
+     */
+    public static SerialPortException serialPortDoesNotExist(String name) {
+        return new SerialPortException(Type.SERIAL_PORT_DOES_NOT_EXIST, name);
+    }
+
+    /**
+     * Creates a {@link SerialPortException} indicating that the SerialPort with the given name is used by another process.
+     *
+     * @param name the name of the ComPort that is used by another process
+     * @param currentOwner the process that currently uses the port
+     * @return the newly created exception
+     */
+    public static SerialPortException serialPortIsInUse(String name, String currentOwner) {
+        return new SerialPortException(Type.SERIAL_PORT_IS_IN_USE, name, currentOwner);
+    }
+
     public Type getType() {
         return type;
     }
 
-    private SerialPortException(Type type) {
-        this.type = type;
+    public Object[] getMessageArguments() {
+        return messageArguments;
     }
 
-    private SerialPortException(Type type, Throwable t) {
+    private SerialPortException(Type type, Object... messageArguments) {
+        this.type = type;
+        this.messageArguments = messageArguments;
+    }
+
+    private SerialPortException(Type type, Throwable t, Object... messageArguments) {
         super(t);
         this.type = type;
+        this.messageArguments = messageArguments;
     }
 
 }
