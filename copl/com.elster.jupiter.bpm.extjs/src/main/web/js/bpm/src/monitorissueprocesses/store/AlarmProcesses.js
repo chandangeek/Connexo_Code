d94ca06275477849eb0f462d9591e7a6baef5625
Mihai Andreescu <mihai.andreescu@elster.com>
2017-01-11 13:09:14 +0200

    correction for processes on alarms

---
+++ copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/src/monitorissueprocesses/store/AlarmProcesses.js
@@ -0,0 +1,20 @@
+Ext.define('Bpm.monitorissueprocesses.store.AlarmProcesses', {
+    extend: 'Ext.data.Store',
+    model: 'Bpm.monitorissueprocesses.model.IssueProcess',
+    autoLoad: false,
+    proxy: 'memory',
+
+    proxy: {
+        type: 'rest',
+        urlTpl: '/api/idc/issues/{alarmId}/processes?variableid=alarmId&variablevalue={Id}',
+        timeout: 240000,
+        reader: {
+            type: 'json',
+            root: 'processes'
+        },
+        setUrl: function (alarmId) {
+            this.url = this.urlTpl.replace('{alarmId}', alarmId)
+                .replace('{Id}', alarmId);
+        }
+    }
+});
