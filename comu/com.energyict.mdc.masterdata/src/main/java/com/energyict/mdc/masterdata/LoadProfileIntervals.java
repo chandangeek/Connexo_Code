18b68b9d01e24f639aecbe8ddfe66190c8a68922
pdo <pasquien.dorny@elster.com>
2017-01-16 13:52:00 +0100

    CXO-4942: Add load profile type - no "1 minute interval" available

--- comu/com.energyict.mdc.masterdata/src/main/java/com/energyict/mdc/masterdata/LoadProfileIntervals.java
+++ comu/com.energyict.mdc.masterdata/src/main/java/com/energyict/mdc/masterdata/LoadProfileIntervals.java
@@ -2,6 +2,9 @@ package com.energyict.mdc.masterdata;
 
 import com.elster.jupiter.time.TimeDuration;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public enum LoadProfileIntervals {
 
     MINUTE_ONE("MINUTE", new TimeDuration(1, TimeDuration.TimeUnit.MINUTES)),
@@ -12,14 +15,14 @@ public enum LoadProfileIntervals {
     MINUTES_FIFTEEN("MINUTES", new TimeDuration(15, TimeDuration.TimeUnit.MINUTES)),
     MINUTES_TWENTY("MINUTES", new TimeDuration(20, TimeDuration.TimeUnit.MINUTES)),
     MINUTES_THIRTY("MINUTES", new TimeDuration(30, TimeDuration.TimeUnit.MINUTES)),
-    HOURS_ONE("HOUR", new TimeDuration(1, TimeDuration.TimeUnit.HOURS)),
+    HOUR_ONE("HOUR", new TimeDuration(1, TimeDuration.TimeUnit.HOURS)),
     HOURS_TWO("HOURS", new TimeDuration(2, TimeDuration.TimeUnit.HOURS)),
     HOURS_THREE("HOURS", new TimeDuration(3, TimeDuration.TimeUnit.HOURS)),
     HOURS_FOUR("HOURS", new TimeDuration(4, TimeDuration.TimeUnit.HOURS)),
     HOURS_SIX("HOURS", new TimeDuration(6, TimeDuration.TimeUnit.HOURS)),
     HOURS_TWELVE("HOURS", new TimeDuration(12, TimeDuration.TimeUnit.HOURS)),
-    DAYS_ONE("DAY", new TimeDuration(1, TimeDuration.TimeUnit.DAYS)),
-    MONTHS_ONE("MONTHS", new TimeDuration(1, TimeDuration.TimeUnit.MONTHS));
+    DAY_ONE("DAY", new TimeDuration(1, TimeDuration.TimeUnit.DAYS)),
+    MONTH_ONE("MONTH", new TimeDuration(1, TimeDuration.TimeUnit.MONTHS));
 
     private final TimeDuration timeDuration;
     private final String unitName;
@@ -35,6 +38,11 @@ public enum LoadProfileIntervals {
 
     public String unitName(){
         return unitName;
+    }
+
+    public static Optional<LoadProfileIntervals> fromTimeDuration(TimeDuration duration){
+        return Stream.of(values()).filter((i) -> i.getTimeDuration().equals(duration)).findFirst();
+
     }
 
 }
--- comu/com.energyict.mdc.masterdata/src/test/java/com/energyict/mdc/masterdata/LoadProfileIntervalsTest.java
+++ comu/com.energyict.mdc.masterdata/src/test/java/com/energyict/mdc/masterdata/LoadProfileIntervalsTest.java
@@ -32,9 +32,9 @@ public class LoadProfileIntervalsTest {
     @Test
     public void testHaveThemAll(){
         assertThat(LoadProfileIntervals.values().length).isEqualTo(expected.length);
-        assertThat(LoadProfileIntervals.DAYS_ONE.getTimeDuration()).isEqualTo(expected[14]);      //
+        assertThat(LoadProfileIntervals.DAY_ONE.getTimeDuration()).isEqualTo(expected[14]);      //
         assertThat(LoadProfileIntervals.HOURS_FOUR.getTimeDuration()).isEqualTo(expected[11]);    //
-        assertThat(LoadProfileIntervals.HOURS_ONE.getTimeDuration()).isEqualTo(expected[8]);      //
+        assertThat(LoadProfileIntervals.HOUR_ONE.getTimeDuration()).isEqualTo(expected[8]);      //
         assertThat(LoadProfileIntervals.HOURS_SIX.getTimeDuration()).isEqualTo(expected[12]);     //
         assertThat(LoadProfileIntervals.HOURS_THREE.getTimeDuration()).isEqualTo(expected[10]);   //
         assertThat(LoadProfileIntervals.HOURS_TWELVE.getTimeDuration()).isEqualTo(expected[13]);  //
@@ -47,6 +47,29 @@ public class LoadProfileIntervalsTest {
         assertThat(LoadProfileIntervals.MINUTES_THREE.getTimeDuration()).isEqualTo(expected[2]);  //
         assertThat(LoadProfileIntervals.MINUTES_TWENTY.getTimeDuration()).isEqualTo(expected[6]); //
         assertThat(LoadProfileIntervals.MINUTES_TWO.getTimeDuration()).isEqualTo(expected[1]);    //
-        assertThat(LoadProfileIntervals.MONTHS_ONE.getTimeDuration()).isEqualTo(expected[15]);    //
+        assertThat(LoadProfileIntervals.MONTH_ONE.getTimeDuration()).isEqualTo(expected[15]);    //
+    }
+
+    @Test
+    public void fromTimeDuration(){
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[0]).get()).isEqualTo(LoadProfileIntervals.MINUTE_ONE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[1]).get()).isEqualTo(LoadProfileIntervals.MINUTES_TWO);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[2]).get()).isEqualTo(LoadProfileIntervals.MINUTES_THREE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[3]).get()).isEqualTo(LoadProfileIntervals.MINUTES_FIVE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[4]).get()).isEqualTo(LoadProfileIntervals.MINUTES_TEN);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[5]).get()).isEqualTo(LoadProfileIntervals.MINUTES_FIFTEEN);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[6]).get()).isEqualTo(LoadProfileIntervals.MINUTES_TWENTY);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[7]).get()).isEqualTo(LoadProfileIntervals.MINUTES_THIRTY);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[8]).get()).isEqualTo(LoadProfileIntervals.HOUR_ONE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[9]).get()).isEqualTo(LoadProfileIntervals.HOURS_TWO);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[10]).get()).isEqualTo(LoadProfileIntervals.HOURS_THREE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[11]).get()).isEqualTo(LoadProfileIntervals.HOURS_FOUR);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[12]).get()).isEqualTo(LoadProfileIntervals.HOURS_SIX);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[13]).get()).isEqualTo(LoadProfileIntervals.HOURS_TWELVE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[14]).get()).isEqualTo(LoadProfileIntervals.DAY_ONE);
+        assertThat(LoadProfileIntervals.fromTimeDuration(expected[15]).get()).isEqualTo(LoadProfileIntervals.MONTH_ONE);
+
+        assertThat(LoadProfileIntervals.fromTimeDuration(TimeDuration.minutes(4)).isPresent()).isFalse();
+        assertThat(LoadProfileIntervals.fromTimeDuration(TimeDuration.hours(5)).isPresent()).isFalse();
     }
 }
