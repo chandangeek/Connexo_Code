9c0cdd0617f5ec210962c60031418b0c4897d2b6
Sentyurin Sergey <ssentyur@mera.ru>
2017-01-16 12:36:30 +0300

    CXO-4778/CXO-5008

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/OutputPreview.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/OutputPreview.js
@@ -40,17 +40,20 @@ Ext.define('Imt.purpose.view.OutputPreview', {
             {
                 fieldLabel: Uni.I18n.translate('general.suspects', 'INS', 'Suspects'),
                 name: 'suspects',
-                itemId: 'suspects-field'
+                itemId: 'suspects-field',
+                htmlEncode: false
             },
             {
                 fieldLabel: Uni.I18n.translate('general.informatives', 'INS', 'Informatives'),
                 name: 'informatives',
-                itemId: 'informatives-field'
+                itemId: 'informatives-field',
+                htmlEncode: false
             },
             {
                 fieldLabel: Uni.I18n.translate('general.estimates', 'INS', 'Estimates'),
                 name: 'estimates',
-                itemId: 'estimates-field'
+                itemId: 'estimates-field',
+                htmlEncode: false
             }
         ]
     },
@@ -63,11 +66,69 @@ Ext.define('Imt.purpose.view.OutputPreview', {
         me.setTitle(record.get('name'));
         me.removeAll();
         me.add(Ext.apply(me.chartConfig, {
-            output: record,
+            output: record.getSummary(),
             purpose: me.purpose,
             router: me.router
         }));
         attributesForm = me.add(me.attributesConfig);
+        attributesForm.getForm().setValues(me.prepareAttributesData(record.get('validationInfo')));
         Ext.resumeLayouts(true);
+    },
+
+    prepareAttributesData: function (validationInfo) {
+        var me = this,
+            data = {
+                allDataValidated: validationInfo.allDataValidated
+                    ? Uni.I18n.translate('general.yes', 'INS', 'Yes')
+                    : Uni.I18n.translate('general.no', 'INS', 'No')
+            };
+
+        if (!Ext.isEmpty(validationInfo.lastChecked)) {
+            data.lastChecked = Uni.DateTime.formatDateTimeLong(new Date(validationInfo.lastChecked));
+        }
+
+        data.suspects = me.prepareRulesInfo(validationInfo.suspectReason);
+        data.informatives = me.prepareRulesInfo(validationInfo.informativeReason);
+        data.estimates = me.prepareRulesInfo(validationInfo.estimateReason, true);
+
+        return data;
+    },
+
+    prepareRulesInfo: function (data, isEstimation) {
+        var me = this,
+            result;
+
+        if (!Ext.isEmpty(data)) {
+            result = '';
+            Ext.Array.each(data, function (item, index) {
+                var rule = item.key,
+                    url;
+
+                if (index > 0) {
+                    result += '<br>';
+                }
+
+                if (rule.deleted) {
+                    result += rule.displayName + ' - ' + item.value;
+                } else {
+                    if (isEstimation) {
+                        url = me.router.getRoute('administration/estimationrulesets/estimationruleset/rules/rule').buildUrl({
+                            ruleSetId: rule.ruleSet.id,
+                            ruleId: rule.id
+                        });
+                    } else {
+                        url = me.router.getRoute('administration/rulesets/overview/versions/overview/rules/overview').buildUrl({
+                            ruleSetId: rule.ruleSetVersion.ruleSet.id,
+                            versionId: rule.ruleSetVersion.id,
+                            ruleId: rule.id
+                        });
+                    }
+
+                    result += '<a href="' + url + '">' + rule.displayName + '</a>' + ' - ' + item.value;
+                }
+            });
+        }
+
+        return result;
     }
 });
