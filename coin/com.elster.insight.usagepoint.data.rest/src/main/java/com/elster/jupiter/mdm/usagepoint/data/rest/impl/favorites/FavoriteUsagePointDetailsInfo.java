3f6dc63aca1f2ce277abc4365294050570a09e47
David Nitescu <david.nitescu@elster.com>
2017-01-17 12:09:19 +0200

    CXO-3976 - Back end for visualizing favorite usage points widget on dashboard CXO-3982 - Back end for visualizing favorite usage point groups widget on dashboard

---
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/favorites/FavoriteUsagePointDetailsInfo.java
@@ -0,0 +1,64 @@
+package com.elster.jupiter.mdm.usagepoint.data.rest.impl.favorites;
+
+import com.elster.jupiter.mdm.usagepoint.data.favorites.FavoriteUsagePoint;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.metering.UsagePointTypeInfo;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.rest.util.VersionInfo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.Instant;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FavoriteUsagePointDetailsInfo {
+
+    public String name;
+    public String displayServiceCategory;
+    public String displayMetrologyConfiguration;
+    public String displayType;
+    public String displayConnectionState;
+    public Long creationDate;
+    public Instant flaggedDate;
+    public String comment;
+    public boolean favorite;
+    public VersionInfo<Long> parent;
+
+    public FavoriteUsagePointDetailsInfo() {
+    }
+
+    public FavoriteUsagePointDetailsInfo(FavoriteUsagePoint favoriteUsagePoint, Thesaurus thesaurus) {
+        UsagePoint usagePoint = favoriteUsagePoint.getUsagePoint();
+        name = usagePoint.getName();
+        displayServiceCategory = usagePoint.getServiceCategory().getDisplayName();
+        displayMetrologyConfiguration = usagePoint.getCurrentEffectiveMetrologyConfiguration().map(mc -> mc.getMetrologyConfiguration().getName()).orElse(null);
+        displayType = this.getUsagePointDisplayType(usagePoint, thesaurus);
+        usagePoint.getCurrentConnectionState().ifPresent(connectionState -> {
+            displayConnectionState = connectionState.getName();
+        });
+        creationDate = usagePoint.getCreateDate().toEpochMilli();
+        comment = favoriteUsagePoint.getComment();
+        flaggedDate = favoriteUsagePoint.getCreationDate();
+        favorite = true;
+        parent = new VersionInfo<>();
+        parent.id = usagePoint.getId();
+        parent.version = usagePoint.getVersion();
+
+    }
+
+    private String getUsagePointDisplayType(UsagePoint usagePoint, Thesaurus thesaurus) {
+        if (usagePoint.isSdp() && usagePoint.isVirtual()) {
+            return UsagePointTypeInfo.UsagePointType.UNMEASURED_SDP.getDisplayName(thesaurus);
+        }
+        if (!usagePoint.isSdp() && usagePoint.isVirtual()) {
+            return UsagePointTypeInfo.UsagePointType.UNMEASURED_NON_SDP.getDisplayName(thesaurus);
+        }
+        if (usagePoint.isSdp() && !usagePoint.isVirtual()) {
+            return UsagePointTypeInfo.UsagePointType.MEASURED_SDP.getDisplayName(thesaurus);
+        }
+        if (!usagePoint.isSdp() && !usagePoint.isVirtual()) {
+            return UsagePointTypeInfo.UsagePointType.MEASURED_NON_SDP.getDisplayName(thesaurus);
+        }
+        return null;
+    }
+}
--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/favorites/FavoritesResource.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/favorites/FavoritesResource.java
@@ -9,26 +9,36 @@ import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.UsagePoint;
 import com.elster.jupiter.metering.groups.MeteringGroupsService;
 import com.elster.jupiter.metering.groups.UsagePointGroup;
+import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.rest.util.ConcurrentModificationExceptionFactory;
+import com.elster.jupiter.rest.util.JsonQueryParameters;
+import com.elster.jupiter.rest.util.PagedInfoList;
 import com.elster.jupiter.rest.util.Transactional;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/favorites")
 public class FavoritesResource {
     private static final String USAGE_POINT_GROUPS_RESOURCE = "/usagepointgroups";
     private static final String USAGE_POINTS_RESOURCE = "/usagepoints";
     private final ResourceHelper resourceHelper;
+    private final Thesaurus thesaurus;
     private final MeteringService meteringService;
     private final MeteringGroupsService meteringGroupsService;
     private final FavoritesService favoritesService;
@@ -36,11 +46,13 @@ public class FavoritesResource {
 
     @Inject
     public FavoritesResource(ResourceHelper resourceHelper,
+                             Thesaurus thesaurus,
                              MeteringService meteringService,
                              MeteringGroupsService meteringGroupsService,
                              FavoritesService favoritesService,
                              ConcurrentModificationExceptionFactory conflictFactory) {
         this.resourceHelper = resourceHelper;
+        this.thesaurus = thesaurus;
         this.meteringService = meteringService;
         this.meteringGroupsService = meteringGroupsService;
         this.favoritesService = favoritesService;
@@ -122,6 +134,79 @@ public class FavoritesResource {
                     .ifPresent(favoritesService::removeFromFavorites);
             return new FavoriteUsagePointInfo(usagePoint);
         }
+    }
+
+    @GET
+    @Transactional
+    @Path(USAGE_POINTS_RESOURCE)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed({com.elster.jupiter.metering.security.Privileges.Constants.VIEW_ANY_USAGEPOINT,
+            com.elster.jupiter.metering.security.Privileges.Constants.VIEW_OWN_USAGEPOINT,
+            com.elster.jupiter.metering.security.Privileges.Constants.ADMINISTER_ANY_USAGEPOINT,
+            com.elster.jupiter.metering.security.Privileges.Constants.ADMINISTER_OWN_USAGEPOINT})
+    public Response getFavoriteUsagePoints(@BeanParam JsonQueryParameters queryParameters) {
+        List<FavoriteUsagePointDetailsInfo> infos = favoritesService.getFavoriteUsagePoints().stream()
+                .map(up -> new FavoriteUsagePointDetailsInfo(up, thesaurus))
+                .sorted((u1, u2) -> u2.flaggedDate.compareTo(u1.flaggedDate))//descending order
+                .collect(Collectors.toList());
+        return Response.ok(PagedInfoList.fromPagedList("usagePoints", infos, queryParameters)).build();
+    }
+
+    @GET
+    @Transactional
+    @Path(USAGE_POINT_GROUPS_RESOURCE)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed({com.elster.jupiter.mdm.usagepoint.data.security.Privileges.Constants.ADMINISTER_USAGE_POINT_GROUP,
+            com.elster.jupiter.mdm.usagepoint.data.security.Privileges.Constants.ADMINISTER_USAGE_POINT_ENUMERATED_GROUP,
+            com.elster.jupiter.mdm.usagepoint.data.security.Privileges.Constants.VIEW_USAGE_POINT_GROUP_DETAIL})
+    public Response getFavoriteUsagePointGroups(@QueryParam("includeAllGroups") boolean includeAllGroups, @BeanParam JsonQueryParameters queryParameters) {
+        List<FavoriteUsagePointGroup> favoriteUsagePointGroups = favoritesService.getFavoriteUsagePointGroups();
+        List<FavoriteUsagePointGroupDetailsInfo> infos = new ArrayList<>();
+        if (includeAllGroups) {
+            infos = meteringGroupsService.findUsagePointGroups().stream()
+                    .map(upg -> new FavoriteUsagePointGroupDetailsInfo(upg, favoriteUsagePointGroups))
+                    .sorted((g1, g2) -> g1.name.compareToIgnoreCase(g2.name))
+                    .collect(Collectors.toList());
+        } else {
+            infos = favoriteUsagePointGroups.stream()
+                    .map(FavoriteUsagePointGroupDetailsInfo::new)
+                    .sorted((g1, g2) -> g2.flaggedDate.compareTo(g1.flaggedDate))//descending order
+                    .collect(Collectors.toList());
+        }
+        return Response.ok(PagedInfoList.fromPagedList("usagePointGroups", infos, queryParameters)).build();
+    }
+
+    @PUT
+    @Transactional
+    @Path(USAGE_POINT_GROUPS_RESOURCE)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed({com.elster.jupiter.mdm.usagepoint.data.security.Privileges.Constants.ADMINISTER_USAGE_POINT_GROUP,
+            com.elster.jupiter.mdm.usagepoint.data.security.Privileges.Constants.ADMINISTER_USAGE_POINT_ENUMERATED_GROUP,
+            com.elster.jupiter.mdm.usagepoint.data.security.Privileges.Constants.VIEW_USAGE_POINT_GROUP_DETAIL})
+    public Response updateFavoriteUsagePointGroups(FavoriteUsagePointGroupDetailsInfo.FavoriteUsagePointGroups usagePointGroups, @BeanParam JsonQueryParameters queryParameters) {
+        List<FavoriteUsagePointGroup> favoriteUsagePointGroups = favoritesService.getFavoriteUsagePointGroups();
+        List<FavoriteUsagePointGroupDetailsInfo> savedFavoriteUsagePointGroups = meteringGroupsService.findUsagePointGroups().stream()
+                .map(upg -> new FavoriteUsagePointGroupDetailsInfo(upg, favoriteUsagePointGroups))
+                .collect(Collectors.toList());
+        List<FavoriteUsagePointGroupDetailsInfo> toUpdateFavoriteUsagePointGroups = usagePointGroups.favoriteUsagePointGroups.stream()
+                .filter(fav -> (savedFavoriteUsagePointGroups.stream()
+                        .filter(savedFav -> {
+                            return (savedFav.getId().compareTo(fav.getId())) == 0 && (savedFav.getFavorite() == fav.getFavorite());
+                        })
+                        .count()) < 1)
+                .collect(Collectors.toList());
+
+        toUpdateFavoriteUsagePointGroups.stream().forEach(fav -> {
+            FavoriteUsagePointGroupInfo info = fav.toFavoriteUsagePointGroupInfo();
+            UsagePointGroup usagePointGroup = lockUsagePointGroupOrThrowException(info, !fav.favorite);
+            if (fav.favorite) {
+                favoritesService.markFavorite(usagePointGroup);
+            } else {
+                favoritesService.findFavoriteUsagePointGroup(usagePointGroup)
+                        .ifPresent(favoritesService::removeFromFavorites);
+            }
+        });
+        return Response.ok().build();
     }
 
     private UsagePointGroup lockUsagePointGroupOrThrowException(FavoriteUsagePointGroupInfo info, boolean remove) {
