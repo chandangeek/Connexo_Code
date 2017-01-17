9a43653e71e288c6a426c5385ea55af019053287
rudi <rudi.vankeirsbilck@honeywell.com>
2017-01-17 12:00:30 +0100

    CXO-2951: Provide an implementation for the UPL ObjectMapper service

--- upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/ObjectMapperService.java
+++ upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/ObjectMapperService.java
@@ -9,5 +9,19 @@ import org.codehaus.jackson.map.ObjectMapper;
  * @since 2017-01-05 (16:18)
  */
 public interface ObjectMapperService {
+    /**
+     * Getter for a new {@link ObjectMapper} that is configured with specific
+     * {@link org.codehaus.jackson.map.AnnotationIntrospector}s
+     * for both serialization and deserialization.
+     * Following introspectors are set:
+     * <ul>
+     * <li>for serialization, a {@link org.codehaus.jackson.xc.JaxbAnnotationIntrospector} is set,
+     * thus serialization will be done according to Jaxb annotations (e.g. @XmlAttribute, @XmlElement, ...);</li>
+     * <li>for deserialization, a {@link org.codehaus.jackson.map.introspect.JacksonAnnotationIntrospector} is set,
+     * thus deserialization is according to Jackson annotations (e.g. @JsonTypeInfo, which is useful for deserialization of polymorphic types);
+     * </ul>
+     *
+     * @return The new ObjectMapper
+     */
     ObjectMapper newJacksonMapper();
 }
