06ab129991729ffee041cb2d9263cb793a892dec
Anton Fomchenko <ailescendres@gmail.com>
2017-01-22 19:50:14 +0300

    fix unit tests compilation

--- coin/com.elster.insight.usagepoint.data.rest/src/test/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/UsagePointResourceTest.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/test/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/UsagePointResourceTest.java
@@ -81,6 +81,7 @@ import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
