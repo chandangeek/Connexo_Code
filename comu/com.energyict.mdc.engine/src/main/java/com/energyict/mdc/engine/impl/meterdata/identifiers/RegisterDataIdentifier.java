d9d2883a9d3813b495873417cbfe2df723c80b38
rudi <rudi.vankeirsbilck@honeywell.com>
2017-01-23 13:27:38 +0100

    CXO-2951: Remove obsolete code in RegisterIdentifier implementation classes

--- comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/meterdata/identifiers/RegisterDataIdentifier.java
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/meterdata/identifiers/RegisterDataIdentifier.java
@@ -1,13 +1,10 @@
 package com.energyict.mdc.engine.impl.meterdata.identifiers;
 
-import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.Register;
 import com.energyict.mdc.upl.meterdata.identifiers.DeviceIdentifier;
 import com.energyict.mdc.upl.meterdata.identifiers.RegisterIdentifier;
 
 import com.energyict.obis.ObisCode;
-
-import java.util.List;
 
 /**
  * Implementation of a {@link RegisterIdentifier} that uniquely identifies an {@link com.energyict.mdc.upl.meterdata.Register} based on the ObisCode
@@ -33,24 +30,6 @@ public class RegisterDataIdentifier implements RegisterIdentifier {
     }
 
     @Override
-    public Register findRegister() {
-        if (this.register == null) {
-            List<Register> registers = ((Device) this.deviceIdentifier.findDevice()).getRegisters();        //Downcast to the Connexo Device
-            for (Register register : registers) {
-                // first need to check the DeviceObisCode
-                if (register.getDeviceObisCode() != null && register.getDeviceObisCode().equals(deviceRegisterObisCode)) {
-                    this.register = register;
-                    break;
-                } else if (register.getRegisterTypeObisCode().equals(registerObisCode)) {
-                    this.register = register;
-                    break;
-                }
-            }
-        }
-        return this.register;
-    }
-
-    @Override
     public ObisCode getRegisterObisCode() {
         return this.registerObisCode;
     }
@@ -63,11 +42,6 @@ public class RegisterDataIdentifier implements RegisterIdentifier {
     @Override
     public String toString() {
         return "register having OBIS code " + this.registerObisCode;
-    }
-
-    @Override
-    public DeviceIdentifier getDeviceIdentifier() {
-        return deviceIdentifier;
     }
 
     private class Introspector implements com.energyict.mdc.upl.meterdata.identifiers.Introspector {
