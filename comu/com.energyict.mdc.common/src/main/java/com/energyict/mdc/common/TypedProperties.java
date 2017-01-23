b1e6824bcf8bca5f716f72d9075adda758bbafda
khe <kristof.hennebel@elster.com>
2017-01-23 10:04:08 +0100

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/TypedProperties.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/TypedProperties.java
@@ -74,7 +74,7 @@ public class TypedProperties implements com.energyict.mdc.upl.properties.TypedPr
             result.setAllProperties(otherTypedProperties);
             return result;
         } else {
-            throw new IllegalArgumentException("Expected instance of " + this.getClass().getName());
+            throw new IllegalArgumentException("Expected instance of " + TypedProperties.class.getName());
         }
     }
 
