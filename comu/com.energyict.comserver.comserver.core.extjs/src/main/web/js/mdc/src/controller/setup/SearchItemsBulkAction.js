65b9f1c3cf75d66b3017f2ef422402538a798be5
geert <geert.delmeiren@elster.com>
2017-01-24 09:55:44 +0100

    CXO-5124 - Issue when performing a bulk action (on devices as well as on usage points)

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/SearchItemsBulkAction.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/SearchItemsBulkAction.js
@@ -11,7 +11,8 @@ Ext.define('Mdc.controller.setup.SearchItemsBulkAction', {
     ],
     requires: [
         'Uni.view.window.Wizard',
-        'Uni.data.reader.JsonBuffered'
+        'Uni.data.reader.JsonBuffered',
+        'Uni.view.window.Confirmation'
     ],
     stores: [
         'Mdc.store.CommunicationSchedulesWithoutPaging',
@@ -137,45 +138,68 @@ Ext.define('Mdc.controller.setup.SearchItemsBulkAction', {
         var me = this,
             search = me.getController('Mdc.controller.Search'),
             searchResults = Ext.getStore('Uni.store.search.Results'),
-            widget;
+            widget,
+            goOnWithTheCurrentSearchResults = function() {
+                if (!searchResults.getCount()) {
+                    this.goBack();
+                } else {
+                    me.devices = null;
+                    me.allDevices = false;
+                    me.schedules = null;
+                    me.operation = null;
+                    me.configData = null;
+                    me.shedulesUnchecked = false;
 
-        searchResults.on('load', function() {
-            if (!searchResults.getCount()) {
-                this.goBack();
-            } else {
-                me.devices = null;
-                me.allDevices = false;
-                me.schedules = null;
-                me.operation = null;
-                me.configData = null;
-                me.shedulesUnchecked = false;
+                    var store = Ext.create('Ext.data.Store', {
+                        buffered: true,
+                        pageSize: 100,
+                        remoteFilter: true,
+                        model: searchResults.model,
+                        filters: searchResults.filters.getRange(),
+                        proxy: searchResults.getProxy()
+                    });
 
-                var store = Ext.create('Ext.data.Store', {
-                    buffered: true,
-                    pageSize: 100,
-                    remoteFilter: true,
-                    model: searchResults.model,
-                    filters: searchResults.filters.getRange(),
-                    proxy: searchResults.getProxy()
-                });
+                    // we replace reader to buffered due to our store is buffered
+                    store.getProxy().setReader(Ext.create('Uni.data.reader.JsonBuffered', store.getProxy().getReader()));
 
-                // we replace reader to buffered due to our store is buffered
-                store.getProxy().setReader(Ext.create('Uni.data.reader.JsonBuffered', store.getProxy().getReader()));
+                    widget = Ext.widget('searchitems-bulk-browse', {
+                        deviceStore: store
+                    });
+                    me.getApplication().fireEvent('changecontentevent', widget);
+                    widget.setLoading();
+                    store.load(function() {
+                        widget.setLoading(false);
+                    });
+                    me.getStore('Mdc.store.CommunicationSchedulesWithoutPaging').load();
+                }
+            };
 
-                widget = Ext.widget('searchitems-bulk-browse', {
-                    deviceStore: store
-                });
-                me.getApplication().fireEvent('changecontentevent', widget);
-                widget.setLoading();
-                store.load(function() {
-                    widget.setLoading(false);
-                });
-                me.getStore('Mdc.store.CommunicationSchedulesWithoutPaging').load();
-            }
-        }, me, {single:true});
-
-        // In case the user forgot to press "Search" on the search page, we need to ensure that the state of the search criteria match the search results:
-        search.service.applyFilters();
+        if (search.service.changedFiltersNotYetApplied) {
+            var confirmationWindow = Ext.create('Uni.view.window.Confirmation', {
+                confirmText: Uni.I18n.translate('general.apply', 'MDC', 'Apply'),
+                secondConfirmText: Uni.I18n.translate('general.dontApply', 'MDC', "Don't apply"),
+                green: true,
+                confirmation: function (button) {
+                    confirmationWindow.close();
+                    if (button.action === 'confirm') { // (Re)apply the criteria first
+                        searchResults.on('load', function() {
+                            goOnWithTheCurrentSearchResults();
+                        }, me, {single:true});
+                        search.service.applyFilters();
+                    } else if (button.action === 'confirm2') { // Don't (re)apply the cirteria
+                        goOnWithTheCurrentSearchResults();
+                        search.service.rollbackCriteriaChanges();
+                    }
+                }
+            });
+            confirmationWindow.show({
+                title: Uni.I18n.translate('general.performBulkAction', 'MDC', 'Perform bulk action?'),
+                msg: Uni.I18n.translate('general.unconfirmedSearchCriteria', 'MDC',
+                    "Some search criteria haven't been applied. Do you want to apply them?")
+            });
+        } else {
+            goOnWithTheCurrentSearchResults();
+        }
     },
 
     enableNextButton: function() {
