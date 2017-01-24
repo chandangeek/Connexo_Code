f44bfcb1271c8b993c41f5d09dc6f0639e2b4d7e
rudi <rudi.vankeirsbilck@honeywell.com>
2017-01-24 12:09:33 +0100

    CXO-2951: Fix issues after merge master into branch

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/device/offline/OfflineDevice.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/device/offline/OfflineDevice.java
@@ -1,17 +1,10 @@
 package com.energyict.mdc.protocol.api.device.offline;
 
 import com.elster.jupiter.orm.MacException;
-import com.energyict.mdc.common.Offline;
-import com.energyict.mdc.common.TypedProperties;
-import com.energyict.mdc.protocol.api.DeviceProtocolCache;
 import com.energyict.mdc.protocol.api.DeviceProtocolPluggableClass;
 import com.energyict.mdc.upl.offline.Offline;
-import com.energyict.mdc.protocol.api.device.data.identifiers.DeviceIdentifier;
-import com.energyict.mdc.protocol.api.device.messages.DeviceMessageStatus;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.TimeZone;
 
 /**
  * Represents an Offline version of a physical device which should contain all
