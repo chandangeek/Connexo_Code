5723b8d54416c652f00353d1874839e9918a7f8e
rudi <rudi.vankeirsbilck@honeywell.com>
2017-01-26 13:34:32 +0100

    Cleanup

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/sdksample/SDKBreakerDialectCustomPropertySet.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/sdksample/SDKBreakerDialectCustomPropertySet.java
@@ -5,6 +5,7 @@ import com.elster.jupiter.cps.PersistenceSupport;
 import com.elster.jupiter.nls.Thesaurus;
 import com.energyict.mdc.protocol.api.DeviceProtocolDialectPropertyProvider;
 import com.energyict.mdc.upl.DeviceProtocolDialect;
+
 import com.energyict.protocolimplv2.common.AbstractDialectCustomPropertySet;
 import test.com.energyict.protocolimplv2.sdksample.SDKBreakerTaskProtocolDialectProperties;
 
@@ -19,7 +20,7 @@ import javax.inject.Inject;
 class SDKBreakerDialectCustomPropertySet extends AbstractDialectCustomPropertySet implements CustomPropertySet<DeviceProtocolDialectPropertyProvider, SDKBreakerDialectProperties> {
 
     @Inject
-    public SDKBreakerDialectCustomPropertySet(Thesaurus thesaurus) {
+    SDKBreakerDialectCustomPropertySet(Thesaurus thesaurus) {
         super(thesaurus);
     }
 
