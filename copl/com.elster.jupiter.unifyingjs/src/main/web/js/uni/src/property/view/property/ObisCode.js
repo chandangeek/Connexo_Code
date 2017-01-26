313b59d1e853c172601ee25a6a2ee2a842b54e00
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-01-26 11:47:25 +0100

    CXO-5169

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/property/view/property/ObisCode.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/property/view/property/ObisCode.js
@@ -1,19 +1,53 @@
 Ext.define('Uni.property.view.property.ObisCode', {
     extend: 'Uni.property.view.property.ObisCodeCombo',
 
+    requires: [
+        'Uni.form.field.Obis'
+    ],
+
     getNormalCmp: function () {
         var me = this;
         return {
-            xtype: 'textfield',
+            xtype: 'obis-field',
             name: this.getName(),
             itemId: me.key,
             width: me.width,
             msgTarget: 'under',
             readOnly: me.isReadOnly,
+            fieldLabel: undefined,
             allowBlank: me.allowBlank,
             inputType: me.inputType,
-            blankText: me.blankText
+            blankText: me.blankText,
+            listeners: {
+                blur: {
+                    fn: me.checkValidObisCode,
+                    scope: me
+                }
+            }
         }
+    },
+
+    checkValidObisCode: function () {
+        return true;
+        //var me = this,
+        //    field = me.getField(),
+        //    obisCode = field.getValue(),
+        //    split,
+        //    valid = true;
+        //
+        //split = obisCode.split('.');
+        //
+        //if(split.length !== 6) {
+        //   valid = false;
+        //} else {
+        //    Ext.each(split, function(obisPart) {
+        //            valid = !!(!Ext.isEmpty(obisPart) && !isNaN(obisPart) && obisPart >= 0 && obisPart <= 255 && valid);
+        //    });
+        //}
+        //
+        //if(valid === false) {
+        //    me.getField().setValue(null);
+        //}
     },
 
     getField: function () {
