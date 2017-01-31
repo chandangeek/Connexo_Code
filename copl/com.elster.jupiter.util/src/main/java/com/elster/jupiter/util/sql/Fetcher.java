97a119002d6ef90ac77619146bc411b7a8d77d06
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:36 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/sql/Fetcher.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/sql/Fetcher.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.util.sql;
 
 public interface Fetcher<T> extends Iterable<T> , AutoCloseable {
