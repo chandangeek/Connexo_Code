137c8a158403f93ad38b7321fbd3bc2e75cafc7e
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:08:59 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.system.app/src/main/web/js/system/resources/js/moment/lang/zh-tw.js
+++ coko/com.elster.jupiter.system.app/src/main/web/js/system/resources/js/moment/lang/zh-tw.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 // moment.js language configuration
 // language : traditional chinese (zh-tw)
 // author : Ben : https://github.com/ben-lin
