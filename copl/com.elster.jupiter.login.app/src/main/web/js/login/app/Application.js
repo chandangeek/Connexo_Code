4c7e91955a5fbf9694763d75e8d6b789622eebea
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:17:00 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.login.app/src/main/web/js/login/app/Application.js
+++ copl/com.elster.jupiter.login.app/src/main/web/js/login/app/Application.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 Ext.define('Login.Application', {
     name: 'Login',
     extend: 'Ext.app.Application',
--- copl/com.elster.jupiter.login.app/src/main/web/js/login/app/controller/Base64.js
+++ copl/com.elster.jupiter.login.app/src/main/web/js/login/app/controller/Base64.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 Ext.define('Login.controller.Base64', {
     extend: 'Ext.app.Controller',
 
