a7bdb501f9bc081ca4d1c20a358e6bb6c0d058c0
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:09:18 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.webservices.outbound.rest.demo/src/main/java/com/elster/jupiter/webservices/outbound/rest/impl/WeatherEndpointProvider.java
+++ coko/com.elster.jupiter.webservices.outbound.rest.demo/src/main/java/com/elster/jupiter/webservices/outbound/rest/impl/WeatherEndpointProvider.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.webservices.outbound.rest.impl;
 
 import com.elster.jupiter.soap.whiteboard.cxf.OutboundRestEndPointProvider;
