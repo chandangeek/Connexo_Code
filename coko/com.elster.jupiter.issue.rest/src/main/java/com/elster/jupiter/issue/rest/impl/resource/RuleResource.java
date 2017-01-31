ec10a7dcec6c1407034bc50f923aafa8e1677718
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:08:21 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.issue.rest/src/main/java/com/elster/jupiter/issue/rest/impl/resource/RuleResource.java
+++ coko/com.elster.jupiter.issue.rest/src/main/java/com/elster/jupiter/issue/rest/impl/resource/RuleResource.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.issue.rest.impl.resource;
 
 import static com.elster.jupiter.issue.rest.request.RequestHelper.ISSUE_TYPE;
