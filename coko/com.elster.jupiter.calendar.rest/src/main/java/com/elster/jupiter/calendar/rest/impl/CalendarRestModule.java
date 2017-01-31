12bdb8aa49f3df77c1f46230e149f03fe0aaa5d7
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:53 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.calendar.rest/src/main/java/com/elster/jupiter/calendar/rest/impl/CalendarRestModule.java
+++ coko/com.elster.jupiter.calendar.rest/src/main/java/com/elster/jupiter/calendar/rest/impl/CalendarRestModule.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.calendar.rest.impl;
 
 import com.elster.jupiter.calendar.rest.CalendarInfoFactory;
