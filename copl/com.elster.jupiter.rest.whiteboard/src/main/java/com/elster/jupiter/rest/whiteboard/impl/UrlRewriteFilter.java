879d73fbda656b396fa6c84a6c011abbff39142d
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:17:16 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/UrlRewriteFilter.java
+++ copl/com.elster.jupiter.rest.whiteboard/src/main/java/com/elster/jupiter/rest/whiteboard/impl/UrlRewriteFilter.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.rest.whiteboard.impl;
 
 import javax.ws.rs.container.ContainerRequestContext;
