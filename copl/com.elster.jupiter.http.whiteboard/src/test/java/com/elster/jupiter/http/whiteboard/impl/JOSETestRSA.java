3263c074b8fd36c59dfb67a560d7620a71ae3a83
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:16:56 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.http.whiteboard/src/test/java/com/elster/jupiter/http/whiteboard/impl/JOSETestRSA.java
+++ copl/com.elster.jupiter.http.whiteboard/src/test/java/com/elster/jupiter/http/whiteboard/impl/JOSETestRSA.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.http.whiteboard.impl;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
