32e66f24285cabdb41a02e144fab555346980166
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:09:32 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.cps/src/main/java/com/elster/jupiter/cps/impl/TableSpecs.java
+++ copl/com.elster.jupiter.cps/src/main/java/com/elster/jupiter/cps/impl/TableSpecs.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.cps.impl;
 
 import com.elster.jupiter.cps.RegisteredCustomPropertySet;
--- copl/com.elster.jupiter.cps/src/test/java/com/elster/jupiter/cps/CustomPropertySetValuesEqualsTest.java
+++ copl/com.elster.jupiter.cps/src/test/java/com/elster/jupiter/cps/CustomPropertySetValuesEqualsTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.cps;
 
 import com.elster.jupiter.devtools.tests.EqualsContractTest;
