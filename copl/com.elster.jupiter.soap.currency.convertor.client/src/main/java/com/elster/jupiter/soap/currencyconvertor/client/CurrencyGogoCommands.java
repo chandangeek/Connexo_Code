46cecdde1f5c741236c767ace33c16e04dd5d792
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:17:25 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.soap.currency.convertor.client/src/main/java/com/elster/jupiter/soap/currencyconvertor/client/CurrencyGogoCommands.java
+++ copl/com.elster.jupiter.soap.currency.convertor.client/src/main/java/com/elster/jupiter/soap/currencyconvertor/client/CurrencyGogoCommands.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.soap.currencyconvertor.client;
 
 import com.elster.jupiter.soap.currencyconverter.Currency;
