cb83b1dd4b5bfc479d17649c6159b75d72ae2378
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:09:25 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.bootstrap.h2/src/main/java/com/elster/jupiter/bootstrap/h2/impl/ProviderResolverService.java
+++ copl/com.elster.jupiter.bootstrap.h2/src/main/java/com/elster/jupiter/bootstrap/h2/impl/ProviderResolverService.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.bootstrap.h2.impl;
 
 import com.google.common.collect.ImmutableList;
