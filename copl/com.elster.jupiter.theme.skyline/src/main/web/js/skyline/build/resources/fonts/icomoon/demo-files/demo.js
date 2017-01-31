023689f19e63687617a326ffdab355ef11398404
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:09 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/resources/fonts/icomoon/demo-files/demo.js
+++ copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/resources/fonts/icomoon/demo-files/demo.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 if (!('boxShadow' in document.body.style)) {
     document.body.setAttribute('class', 'noBoxShadow');
 }
--- copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/resources/js/tinyscrollbar-2.1.4/lib/jquery.tinyscrollbar.js
+++ copl/com.elster.jupiter.theme.skyline/src/main/web/js/skyline/build/resources/js/tinyscrollbar-2.1.4/lib/jquery.tinyscrollbar.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 ;(function (factory)
 {
     if (typeof define === 'function' && define.amd)
