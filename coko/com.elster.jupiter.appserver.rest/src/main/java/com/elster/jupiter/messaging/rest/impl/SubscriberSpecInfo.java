8c00c8f38a4725e93cd14fddc65bd5f7677ea025
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:48 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.appserver.rest/src/main/java/com/elster/jupiter/messaging/rest/impl/SubscriberSpecInfo.java
+++ coko/com.elster.jupiter.appserver.rest/src/main/java/com/elster/jupiter/messaging/rest/impl/SubscriberSpecInfo.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.messaging.rest.impl;
 
 import com.elster.jupiter.appserver.AppService;
