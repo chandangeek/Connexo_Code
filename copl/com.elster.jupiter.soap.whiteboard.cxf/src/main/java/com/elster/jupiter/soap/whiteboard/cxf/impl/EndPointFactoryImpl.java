6ba1f66572c2984936cc0e04eb083cc2a36744c0
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:17:26 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/EndPointFactoryImpl.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/EndPointFactoryImpl.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.soap.whiteboard.cxf.impl;
 
 import com.elster.jupiter.soap.whiteboard.cxf.EndPointProvider;
--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/soap/TracingFeature.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/soap/TracingFeature.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.soap.whiteboard.cxf.impl.soap;
 
 import org.apache.cxf.Bus;
