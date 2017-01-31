2356ca2e6fbcf2de28b319b4d81a1b54001295d4
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:46 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.yellowfin.installer/src/main/java/com/elster/jupiter/install/reports/OpenReports.java
+++ copl/com.elster.jupiter.yellowfin.installer/src/main/java/com/elster/jupiter/install/reports/OpenReports.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.install.reports;
 
 import com.hof.mi.web.service.AdministrationServiceRequest;
