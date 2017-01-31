ee4c79e3945af49119d1e2d75ac2f399be564301
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:08:51 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.metering.imports/src/main/java/com/elster/jupiter/metering/imports/impl/parsers/LiteralStringParser.java
+++ coko/com.elster.jupiter.metering.imports/src/main/java/com/elster/jupiter/metering/imports/impl/parsers/LiteralStringParser.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.metering.imports.impl.parsers;
 
 import com.elster.jupiter.metering.imports.impl.FieldParser;
