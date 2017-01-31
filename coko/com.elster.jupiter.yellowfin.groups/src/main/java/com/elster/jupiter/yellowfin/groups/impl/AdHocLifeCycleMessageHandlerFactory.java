504cbda625eee11879d04b881e3ad4368a19981c
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:09:20 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.yellowfin.groups/src/main/java/com/elster/jupiter/yellowfin/groups/impl/AdHocLifeCycleMessageHandlerFactory.java
+++ coko/com.elster.jupiter.yellowfin.groups/src/main/java/com/elster/jupiter/yellowfin/groups/impl/AdHocLifeCycleMessageHandlerFactory.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.yellowfin.groups.impl;
 
 import com.elster.jupiter.messaging.subscriber.MessageHandler;
