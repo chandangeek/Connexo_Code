284c12d716d1a68efeb365f80b50c9dd2c5dcb12
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:20 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/form/Label.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/form/Label.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 Ext.define('Uni.override.form.Label', {
     override: 'Ext.form.Label',
     cls: 'x-form-item-label'
