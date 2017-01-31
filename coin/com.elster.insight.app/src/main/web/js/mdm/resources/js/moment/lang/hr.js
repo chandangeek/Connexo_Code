fe4976303685a90c15db678d0494311868441a10
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 17:00:10 +0100

    Apply Copyright statement

--- coin/com.elster.insight.app/src/main/web/js/mdm/resources/js/moment/lang/hr.js
+++ coin/com.elster.insight.app/src/main/web/js/mdm/resources/js/moment/lang/hr.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 // moment.js language configuration
 // language : hrvatski (hr)
 // author : Bojan Marković : https://github.com/bmarkovic
