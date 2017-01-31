4316bd6294a27bc167373336b3353868228ab841
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:09:21 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/ReportInfos.java
+++ coko/com.elster.jupiter.yellowfin.rest/src/main/java/com/elster/jupiter/yellowfin/rest/impl/ReportInfos.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.yellowfin.rest.impl;
 
 import com.elster.jupiter.yellowfin.YellowfinReportInfo;
