f80ecf1bc957f40c8951a0e91c5a835aa7d322c4
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:08:03 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/TranslationKeyProvider.java
+++ coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/TranslationKeyProvider.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.estimators.impl;
 
 import com.elster.jupiter.estimation.EstimationService;
