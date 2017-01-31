245f6f61bcb90daf6d173099e94eaa82aa434f7a
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:08:46 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.metering.cim/src/main/java/com/elster/jupiter/metering/cim/impl/Marshaller.java
+++ coko/com.elster.jupiter.metering.cim/src/main/java/com/elster/jupiter/metering/cim/impl/Marshaller.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.metering.cim.impl;
 
 import ch.iec.tc57._2011.meterreadings.MeterReadings;
--- coko/com.elster.jupiter.metering.cim/src/main/java/com/elster/jupiter/metering/cim/soap/impl/GetMeterReadingsPortImpl.java
+++ coko/com.elster.jupiter.metering.cim/src/main/java/com/elster/jupiter/metering/cim/soap/impl/GetMeterReadingsPortImpl.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.metering.cim.soap.impl;
 
 import ch.iec.tc57._2011.getmeterreadings.FaultMessage;
