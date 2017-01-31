b1a4da9f5b5ed9f150a3c2fdf288757bbaa837fa
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:58 +0100

    Apply Copyright statement

--- coko/com.elster.jupiter.data.lifecycle/src/main/java/com/elster/jupiter/data/lifecycle/impl/LifeCycleTaskExecutor.java
+++ coko/com.elster.jupiter.data.lifecycle/src/main/java/com/elster/jupiter/data/lifecycle/impl/LifeCycleTaskExecutor.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.data.lifecycle.impl;
 
 import java.util.logging.Logger;
--- coko/com.elster.jupiter.data.lifecycle/src/main/java/com/elster/jupiter/data/lifecycle/impl/TranslationKeys.java
+++ coko/com.elster.jupiter.data.lifecycle/src/main/java/com/elster/jupiter/data/lifecycle/impl/TranslationKeys.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.data.lifecycle.impl;
 
 import com.elster.jupiter.nls.TranslationKey;
