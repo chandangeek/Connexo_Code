8d2119cf11695a39d4bc2b2c9e96ff6825a4b507
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 12:07:41 +0100

    Apply Copyright statement

--- copl/com.elster.jupiter.webservices.rest/src/test/java/com.elster.jupiter.webservices.rest.impl/EndPointConfigurationResourceTest.java
+++ copl/com.elster.jupiter.webservices.rest/src/test/java/com.elster.jupiter.webservices.rest.impl/EndPointConfigurationResourceTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.webservices.rest.impl;
 
 import com.elster.jupiter.devtools.tests.FakeBuilder;
