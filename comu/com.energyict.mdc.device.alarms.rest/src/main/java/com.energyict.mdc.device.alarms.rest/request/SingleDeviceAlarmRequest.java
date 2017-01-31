9e333febf300ea8883a06e22729440075392dd12
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 16:35:58 +0100

    Apply Copyright statement

--- comu/com.energyict.mdc.device.alarms.rest/src/main/java/com.energyict.mdc.device.alarms.rest/request/SingleDeviceAlarmRequest.java
+++ comu/com.energyict.mdc.device.alarms.rest/src/main/java/com.energyict.mdc.device.alarms.rest/request/SingleDeviceAlarmRequest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.energyict.mdc.device.alarms.rest.request;
 
 import com.elster.jupiter.issue.rest.request.EntityReference;
