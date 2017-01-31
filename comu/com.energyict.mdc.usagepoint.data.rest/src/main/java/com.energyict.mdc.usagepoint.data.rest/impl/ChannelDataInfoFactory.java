34617a61789e81225328a4ca18e8831e2b6cd094
Bruno Ballekens <bruno.ballekens@elster.com>
2017-01-31 16:35:54 +0100

    Apply Copyright statement

--- comu/com.energyict.mdc.usagepoint.data.rest/src/main/java/com.energyict.mdc.usagepoint.data.rest/impl/ChannelDataInfoFactory.java
+++ comu/com.energyict.mdc.usagepoint.data.rest/src/main/java/com.energyict.mdc.usagepoint.data.rest/impl/ChannelDataInfoFactory.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.energyict.mdc.usagepoint.data.rest.impl;
 
 import com.elster.jupiter.validation.ValidationAction;
