d4f47bea47f3de9eb744aeedae8726e1faf77097
rudi <rudi.vankeirsbilck@honeywell.com>
2017-02-01 13:22:03 +0100

    Fix CXO-3711

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/MacroPeriodAdapter.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/rest/MacroPeriodAdapter.java
@@ -17,5 +17,6 @@ public class MacroPeriodAdapter extends MapBasedXmlAdapter<MacroPeriod> {
         register("Seasonal", MacroPeriod.SEASONAL);
         register("Specified period", MacroPeriod.SPECIFIEDPERIOD);
         register("Weekly", MacroPeriod.WEEKLYS);
+        register("Yearly", MacroPeriod.YEARLY);
     }
 }
