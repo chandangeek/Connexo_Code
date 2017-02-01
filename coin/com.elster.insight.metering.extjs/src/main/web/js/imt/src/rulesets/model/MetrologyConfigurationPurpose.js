5b1cbe8f82332816334ead2445455538dc79c306
Sentyurin Sergey <ssentyur@mera.ru>
2017-02-01 12:50:51 +0300

    CXO-5248/CXO-5291

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/rulesets/model/MetrologyConfigurationPurpose.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/rulesets/model/MetrologyConfigurationPurpose.js
@@ -1,7 +1,8 @@
 Ext.define('Imt.rulesets.model.MetrologyConfigurationPurpose', {
     extend: 'Ext.data.Model',
-    fields: ['isActive', 'metrologyConfigurationInfo', 'purpose', 'outputs', 'metrologyContractId', 'metrologyContractInfo'],
-    idProperty: 'metrologyContractId',
+    fields: ['isActive', 'metrologyConfigurationInfo', 'purpose', 'outputs', 'metrologyContractInfo',
+        {name: 'id', mapping: 'metrologyContractInfo.id'}
+    ],
     proxy: {
         type: 'rest',
         url: '/api/ucr/validationrulesets/{ruleSetId}/purposes',
