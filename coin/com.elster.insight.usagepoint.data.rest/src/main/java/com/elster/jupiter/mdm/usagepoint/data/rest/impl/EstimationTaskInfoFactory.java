efc85054caa717baef8dd7424f4126d2288ede39
Maksim Barinov <mbarinov@mera.ru>
2017-02-01 14:07:53 +0300

    Merged master

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/EstimationTaskInfoFactory.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/EstimationTaskInfoFactory.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
 
 import com.elster.jupiter.estimation.EstimationTask;
