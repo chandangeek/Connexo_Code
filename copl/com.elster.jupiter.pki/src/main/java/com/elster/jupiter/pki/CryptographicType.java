842e3a73b8ac19092c8c713a8757037b9179d436
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2017-02-02 09:33:41 +0100

    CXO-3606: Added requiresDuration to CryptoType

--- copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/CryptographicType.java
+++ copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/CryptographicType.java
@@ -4,9 +4,19 @@ package com.elster.jupiter.pki;
  * Created by bvn on 1/18/17.
  */
 public enum CryptographicType {
-    Certificate,
-    CertificateWithPrivateKey,
-    SymmetricKey,
-    Passphrase,
-    AsymmetricKey
+    Certificate(false),
+    CertificateWithPrivateKey(false),
+    SymmetricKey(true),
+    Passphrase(true),
+    AsymmetricKey(true);
+
+    CryptographicType(boolean durationMandatory) {
+        this.durationMandatory = durationMandatory;
+    }
+
+    private boolean durationMandatory;
+
+    public boolean requiresDuration() {
+        return durationMandatory;
+    };
 }
