b9bab76a0146d93cc1a867c2bb7b2d2a2983e737
Valery Ruchin <varuchin@mera.ru>
2017-02-02 16:02:39 +0300

    Added opportunity to remove import service history

--- coin/com.elster.jupiter.mdm.test.api.rest/src/main/java/com/elster/jupiter/autotests/rest/impl/UsagePointTestResource.java
+++ coin/com.elster.jupiter.mdm.test.api.rest/src/main/java/com/elster/jupiter/autotests/rest/impl/UsagePointTestResource.java
@@ -2,26 +2,32 @@ package com.elster.jupiter.autotests.rest.impl;
 
 import com.elster.jupiter.metering.MeteringService;
 import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.orm.OrmService;
 import com.elster.jupiter.rest.util.Transactional;
 
 import javax.inject.Inject;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 @Path("/usagepoints")
 public class UsagePointTestResource {
 
     private final MeteringService meteringService;
+    private final OrmService ormService;
 
     @Inject
-    public UsagePointTestResource(MeteringService meteringService) {
+    public UsagePointTestResource(MeteringService meteringService, OrmService ormService) {
         this.meteringService = meteringService;
+        this.ormService = ormService;
     }
 
     @DELETE
@@ -35,6 +41,19 @@ public class UsagePointTestResource {
             return Response.status(Response.Status.OK).build();
         } else {
             return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @PUT
+    @Path("/clearHistory/")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @Transactional
+    public Response clearImportHistory() throws SQLException {
+        try (Connection connection = ormService.getDataModel("FIS").get().getConnection(true)) {
+            try (Statement statement = connection.createStatement()) {
+                statement.execute("DELETE FROM FIM_FILE_IMPORT_OCCURRENCE");
+            }
+            return Response.status(Response.Status.OK).build();
         }
     }
 }
