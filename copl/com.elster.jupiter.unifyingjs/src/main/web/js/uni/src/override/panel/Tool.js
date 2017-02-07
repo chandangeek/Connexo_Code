dd7052d91ecf0c3f5575ebfde4f7981aa8ea39db
Sentyurin Sergey <ssentyur@mera.ru>
2017-02-07 13:17:58 +0300

    CXO-5500/CXO-5501

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/override/panel/Tool.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+Ext.define('Uni.override.panel.Tool', {
+    override: 'Ext.panel.Tool',
+
+    renderTpl: [
+        '<div id="{id}-toolEl" src="{blank}" class="{baseCls}-img {baseCls}-{type}' +
+        '{childElCls}" role="presentation"></div>'
+    ]
+});
