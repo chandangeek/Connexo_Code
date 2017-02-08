56d2ed25386645204de6ef2c433b1ae0353ddbf9
Anton Fomchenko <antfom@mera.ru>
2017-02-08 18:13:29 +0300

    CXO-4183 refactoring

--- copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/csvimport/exceptions/ProcessorException.java
+++ copl/com.elster.jupiter.fileimport/src/main/java/com/elster/jupiter/fileimport/csvimport/exceptions/ProcessorException.java
@@ -7,7 +7,18 @@ package com.elster.jupiter.fileimport.csvimport.exceptions;
 import com.elster.jupiter.util.exception.MessageSeed;
 
 public class ProcessorException extends ImportException {
+    private boolean stopImport = false;
+
     public ProcessorException(MessageSeed message, Object... args) {
         super(message, args);
+    }
+
+    public ProcessorException andStopImport() {
+        stopImport = true;
+        return this;
+    }
+
+    public boolean shouldStopImport() {
+        return stopImport;
     }
 }
