4c3a42fda960ac4d024d2737f1e2e92183f28ba6
Dragos Sinca <dragos.sinca@honeywell.com>
2017-02-10 13:50:23 +0200

    Added Connexo base URL + basic authentication support when no token is provided

--- copa/com.elster.jupiter.sso.flow/src/main/java/com/elster/partners/flow/components/workitems/ConnexoRESTWorkItemHandler.java
+++ copa/com.elster.jupiter.sso.flow/src/main/java/com/elster/partners/flow/components/workitems/ConnexoRESTWorkItemHandler.java
@@ -12,23 +12,66 @@ import org.jbpm.process.workitem.rest.RESTWorkItemHandler;
 import org.uberfire.commons.services.cdi.Veto;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Base64;
 import java.util.Map;
 
 @Veto
 public class ConnexoRESTWorkItemHandler extends RESTWorkItemHandler {
     private String token;
+    private String user;
+    private String password;
+    private URI connexoURI;
 
     @Override
     protected HttpResponse doRequestWithAuthorization(HttpClient httpclient, RequestBuilder requestBuilder, Map<String, Object> params, AuthenticationType type) {
-        if (token == null) {
-            token = System.getProperty("com.elster.jupiter.token");
+        loadConfiguration();
+
+        URI configuredURI = requestBuilder.getUri();
+        try {
+            requestBuilder.setUri(new URI(connexoURI.toString() + "/" + configuredURI.toString()));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
+
         HttpUriRequest request = requestBuilder.build();
-        request.addHeader("Authorization", "Bearer " + this.token);
+        if (this.token != null && !this.token.isEmpty()) {
+            request.addHeader("Authorization", "Bearer " + this.token);
+        } else {
+            if ((this.user != null && !this.user.isEmpty()) &&
+                    (this.password != null && !this.password.isEmpty())) {
+                request.addHeader("Authorization", "Basic " + Base64.getEncoder()
+                        .encodeToString((this.user + ":" + this.password).getBytes()));
+            }
+        }
+
         try {
             return httpclient.execute(request);
         } catch (IOException e) {
             throw new RuntimeException("Could not execute request on Connexo REST API!", e);
+        }
+    }
+
+    private void loadConfiguration() {
+        if (token == null) {
+            token = System.getProperty("com.elster.jupiter.token");
+        }
+
+        if (connexoURI == null) {
+            try {
+                connexoURI = new URI(System.getProperty("com.elster.jupiter.url"));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (user == null) {
+            this.user = System.getProperty("com.elster.jupiter.user");
+        }
+
+        if (password == null) {
+            this.password = System.getProperty("com.elster.jupiter.password");
         }
     }
 }
