b828e78907aa1d97c4971e6811ddedfaf507385c
khe <kristof.hennebel@elster.com>
2017-02-13 16:14:35 +0100

    CXO-2951 - Universal protocols part 1
    Added value types to UPL

---
+++ upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/properties/LinePoint.java
@@ -0,0 +1,10 @@
+package com.energyict.mdc.upl.properties;
+
+/**
+ * Copyrights EnergyICT
+ *
+ * @author khe
+ * @since 13/02/2017 - 15:50
+ */
+public interface LinePoint {
+}
---
+++ upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/properties/ProtocolChannelMap.java
@@ -0,0 +1,10 @@
+package com.energyict.mdc.upl.properties;
+
+/**
+ * Copyrights EnergyICT
+ *
+ * @author khe
+ * @since 13/02/2017 - 15:53
+ */
+public interface ProtocolChannelMap {
+}
