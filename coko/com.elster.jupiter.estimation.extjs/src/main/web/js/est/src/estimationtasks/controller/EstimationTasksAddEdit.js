169d3f2ca3dd39a3d411713ec07e4178c0e0416f
geert <geert.delmeiren@elster.com>
2017-02-13 16:18:33 +0100

    CXO-5648 - Uncaught TypeError in browser console when navigating directly to add estimation task page

--- coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationtasks/controller/EstimationTasksAddEdit.js
+++ coko/com.elster.jupiter.estimation.extjs/src/main/web/js/est/src/estimationtasks/controller/EstimationTasksAddEdit.js
@@ -1,7 +1,10 @@
 Ext.define('Est.estimationtasks.controller.EstimationTasksAddEdit', {
     extend: 'Ext.app.Controller',
 
-    requires: ['Uni.util.Application'],
+    requires: [
+        'Uni.util.Application',
+        'Uni.util.LogLevel'
+    ],
 
     stores: [
         'Est.estimationtasks.store.DeviceGroups',
@@ -27,6 +30,7 @@ Ext.define('Est.estimationtasks.controller.EstimationTasksAddEdit', {
     taskId: null,
 
     init: function () {
+        Uni.util.LogLevel.loadLogLevels();
         this.control({
             'estimationtasks-addedit #add-button': {
                 click: this.createEstimationTask
