40c5e1ef88dce12701467c8516237be26580788d
tvn <thomas.vandebuerie@honeywell.com>
2017-02-14 17:06:06 +0100

    CXO-5388 - code formatting

--- copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/KeyAccessorType.java
+++ copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/KeyAccessorType.java
@@ -1,6 +1,5 @@
 package com.elster.jupiter.pki;
 
-import com.elster.jupiter.domain.util.Save;
 import com.elster.jupiter.time.TimeDuration;
 import com.elster.jupiter.util.HasId;
 import com.elster.jupiter.util.HasName;
@@ -13,7 +12,7 @@ import java.util.Optional;
  * KeyAccessor links a KeyType to a DeviceType. By configuring KeyTypes on a DeviceType, the user identifies which
  * keys/certificates are required or make sense for devices of this type, the user assigns a function to the key/certificate.
  * Key requirement is based on which keys the underlying protocol actually needs.
- *
+ * <p>
  * The KeyAccessorType also has a name. This name will identify a key for a certain purpose. E.g. the Shipment file will
  * map keys to KeyAccessorType by name, that is, the keys from the shipment file will be labelled, the label matches the
  * name of a KeyAccessor.
@@ -47,6 +46,7 @@ public interface KeyAccessorType extends HasId, HasName {
     /**
      * Keytype identifies main parameters for the stored value (certificate or key), such as curve, algorithm,
      * bitlength, ...
+     *
      * @return The KeyType associated with this KeyAccessor
      */
     KeyType getKeyType();
@@ -54,6 +54,7 @@ public interface KeyAccessorType extends HasId, HasName {
     /**
      * KeyEncryptionMethod describes how the key will be stored. KeyEncryptionMethods are registered through whiteboard
      * on the KeyService
+     *
      * @return The KeyEncryptionMethod as a name
      */
     String getKeyEncryptionMethod();
@@ -62,14 +63,19 @@ public interface KeyAccessorType extends HasId, HasName {
 
     interface Builder {
         Builder description(String description);
+
         Builder duration(TimeDuration duration);
+
         KeyAccessorType add();
     }
 
     interface Updater {
         Updater name(String name);
+
         Updater description(String description);
+
         Updater duration(TimeDuration duration);
+
         KeyAccessorType complete();
     }
 }
