32565947516517b5041340f6fb440bf1ed3424e5
dvy <dieter.vyncke@elster.com>
2017-02-15 15:45:47 +0100

    cleanup

--- comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/DataLoggerReferenceImplTest.java
+++ comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/DataLoggerReferenceImplTest.java
@@ -860,7 +860,7 @@ public class DataLoggerReferenceImplTest extends PersistenceIntegrationTest {
         registerMapping.put(slaveRegister3, dataLoggerR3);
 
         dataLoggerR2.startEditingData()
-                .editReading(ReadingImpl.of(dataLoggerR2.getReadingType().getMRID(), new BigDecimal(0), readingsDataLoggerR2.get(readingsDataLoggerR2.size()-2).getTimeStamp()))
+                .editReading(ReadingImpl.of(dataLoggerR2.getReadingType().getMRID(), new BigDecimal(0), readingsDataLoggerR2.get(readingsDataLoggerR2.size()-2).getTimeStamp()),readingsDataLoggerR2.get(readingsDataLoggerR2.size()-2).getTimeStamp())
                 .complete();
 
         inMemoryPersistence.getTopologyService().setDataLogger(slave, dataLogger, startLink, channelMapping, registerMapping);
