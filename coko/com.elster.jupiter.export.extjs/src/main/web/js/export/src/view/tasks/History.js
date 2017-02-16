7796a13da2768646b4109ffb8f9fd3b93ae81aa9
Kirill Koshelev <kirkosh@mera.ru>
2017-02-16 15:04:05 +0300

    [CXO-3345] - Export/Import UX improvements (CXO-5698)

--- coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/tasks/History.js
+++ coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/view/tasks/History.js
@@ -79,12 +79,9 @@ Ext.define('Dxp.view.tasks.History', {
                         router: me.router
                     },
                     emptyComponent: {
-                        xtype: 'no-items-found-panel',
-                        title: Uni.I18n.translate('exportTasksHistory.empty.title', 'DES', 'No export history found'),
-                        reasons: [
-                            Uni.I18n.translate('exportTasksHistory.empty.list.item1', 'DES', 'There is no history available for this export task.'),
-                            Uni.I18n.translate('dataExportTasksHistory.empty.list.item2', 'DES', 'No history items comply with the filter.')
-                        ],
+                        xtype: 'uni-form-empty-message',
+                        doAutoSize: false,
+                        text: Uni.I18n.translate('exportTasksHistory.empty.titlex', 'DES', 'There is no history available for this export task.'),
                         margin: '16 0 0 0'
                     },
                     previewComponent: {
