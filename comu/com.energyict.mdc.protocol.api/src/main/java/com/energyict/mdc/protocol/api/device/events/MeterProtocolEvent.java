182076541726733b104f01f694268cb5788dafed
govanni <govanni.naessens@elster.com>
2017-02-17 11:26:18 +0100

    CXO 5389   Update the ComServer storage engine to store the event details
    Updated the MeterDataFactory so we can properly delegate the necessary information to Pulse
    Updated ComServerDOA to fix the storage of the lastlogbookdate
    Updated the SDK protocol so this issue can be tested
    Updated teh NTA protocol so the disconnect control logbook contains additional information regarding the disconnect events
    Updated the MeterEvent/MeterProtocolEvents object so it contains the additional information

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/device/events/MeterProtocolEvent.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/device/events/MeterProtocolEvent.java
@@ -8,6 +8,8 @@ import com.elster.jupiter.metering.events.EndDeviceEventType;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MeterProtocolEvent implements Serializable, Comparable {
 
@@ -40,17 +42,21 @@ public class MeterProtocolEvent implements Serializable, Comparable {
      * Identifies the (sequential) ID of the event in the particular logbook of the Device.
      */
     private final int deviceEventId;
+    /**
+     * Keeps track of additional information regarding this meterprotocolevent
+     */
+    private Map<String, String> additionalInformation = new HashMap<>();
 
     /**
      * Create a new MeterProtocolEvent with the given arguments
      *
-     * @param time            the time the Event was recorded (this is not the time the event was read from the device)
-     * @param eiCode          the EIServer code of this event
-     * @param protocolCode    the event code we received from the device
-     * @param eventType       the CIM {@link EndDeviceEventType}
-     * @param message         the text that may clarify the event
+     * @param time the time the Event was recorded (this is not the time the event was read from the device)
+     * @param eiCode the EIServer code of this event
+     * @param protocolCode the event code we received from the device
+     * @param eventType the CIM {@link EndDeviceEventType}
+     * @param message the text that may clarify the event
      * @param meterEventLogId identifies the <i>LogBook</i> ID of the device (CIM logbook id) <i>(don't confuse with our own LogBook.getId())</i>
-     * @param deviceEventId   identifies the (sequential) ID of the event in the particular logbook of the Device.
+     * @param deviceEventId identifies the (sequential) ID of the event in the particular logbook of the Device.
      */
     public MeterProtocolEvent(Date time, int eiCode, int protocolCode, EndDeviceEventType eventType, String message, int meterEventLogId, int deviceEventId) {
         this.time = time;
@@ -92,5 +98,25 @@ public class MeterProtocolEvent implements Serializable, Comparable {
 
     public int getDeviceEventId() {
         return deviceEventId;
+    }
+
+    /**
+     * Provides a list of additional information regarding this meterevent.
+     * It will provide a list of key-value pairs which are provided by the meterprotocol
+     *
+     * @return list of key-value pairs, giving more information about this event.
+     */
+    public Map<String, String> getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    /**
+     * Adds additional information to this event
+     *
+     * @param key the key of the info
+     * @param value the value of the info
+     */
+    public void addAdditionalInformation(String key, String value) {
+        this.additionalInformation.put(key, value);
     }
 }
