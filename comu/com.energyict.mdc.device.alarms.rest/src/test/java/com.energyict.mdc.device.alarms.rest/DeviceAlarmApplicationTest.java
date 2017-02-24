494382eb3084c5e85edd33fc5b084b70fdbec7c0
Albert Versin <albert.versin@elster.com>
2017-02-24 12:25:59 +0200

    Added unit tests for alarm creation rule and fixed minor issues.

--- comu/com.energyict.mdc.device.alarms.rest/src/test/java/com.energyict.mdc.device.alarms.rest/DeviceAlarmApplicationTest.java
+++ comu/com.energyict.mdc.device.alarms.rest/src/test/java/com.energyict.mdc.device.alarms.rest/DeviceAlarmApplicationTest.java
@@ -11,12 +11,19 @@ import com.elster.jupiter.cbo.EndDeviceSubDomain;
 import com.elster.jupiter.cbo.EndDeviceType;
 import com.elster.jupiter.devtools.rest.FelixRestApplicationJerseyTest;
 import com.elster.jupiter.issue.rest.TranslationKeys;
+import com.elster.jupiter.issue.share.CreationRuleTemplate;
+import com.elster.jupiter.issue.share.IssueAction;
 import com.elster.jupiter.issue.share.IssueGroupFilter;
+import com.elster.jupiter.issue.share.entity.CreationRule;
+import com.elster.jupiter.issue.share.entity.DueInType;
+import com.elster.jupiter.issue.share.entity.IssueActionType;
 import com.elster.jupiter.issue.share.entity.IssueAssignee;
 import com.elster.jupiter.issue.share.entity.IssueReason;
 import com.elster.jupiter.issue.share.entity.IssueStatus;
 import com.elster.jupiter.issue.share.entity.IssueType;
 import com.elster.jupiter.issue.share.service.IssueActionService;
+import com.elster.jupiter.issue.share.service.IssueAssignmentService;
+import com.elster.jupiter.issue.share.service.IssueCreationService;
 import com.elster.jupiter.issue.share.service.IssueService;
 import com.elster.jupiter.metering.AmrSystem;
 import com.elster.jupiter.metering.KnownAmrSystem;
@@ -29,7 +36,12 @@ import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsMessageFormat;
 import com.elster.jupiter.nls.NlsService;
 import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.properties.PropertySpec;
+import com.elster.jupiter.properties.StringFactory;
+import com.elster.jupiter.properties.rest.PropertyInfo;
+import com.elster.jupiter.properties.rest.PropertyTypeInfo;
 import com.elster.jupiter.properties.rest.PropertyValueInfoService;
+import com.elster.jupiter.properties.rest.SimplePropertyType;
 import com.elster.jupiter.users.User;
 import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.users.WorkGroup;
@@ -95,6 +107,10 @@ public class DeviceAlarmApplicationTest extends FelixRestApplicationJerseyTest {
     @Mock
     IssueActionService issueActionService;
     @Mock
+    IssueCreationService issueCreationService;
+    @Mock
+    IssueAssignmentService issueAssignmentService;
+    @Mock
     static SecurityContext securityContext;
 
     @Provider
@@ -122,6 +138,8 @@ public class DeviceAlarmApplicationTest extends FelixRestApplicationJerseyTest {
         when(nlsService.getThesaurus(DeviceAlarmService.COMPONENT_NAME, Layer.REST)).thenReturn(thesaurus);
         when(nlsService.getThesaurus(DeviceAlarmService.COMPONENT_NAME, Layer.DOMAIN)).thenReturn(thesaurus);
         when(issueService.getIssueActionService()).thenReturn(issueActionService);
+        when(issueService.getIssueCreationService()).thenReturn(issueCreationService);
+        when(issueService.getIssueAssignmentService()).thenReturn(issueAssignmentService);
 
         deviceAlarmApplication.setTransactionService(transactionService);
         deviceAlarmApplication.setDeviceService(deviceService);
@@ -133,7 +151,6 @@ public class DeviceAlarmApplicationTest extends FelixRestApplicationJerseyTest {
         deviceAlarmApplication.setNlsService(nlsService);
         deviceAlarmApplication.setBpmService(bpmService);
         deviceAlarmApplication.setPropertyValueInfoService(propertyValueInfoService);
-
         return deviceAlarmApplication;
     }
 
@@ -148,6 +165,93 @@ public class DeviceAlarmApplicationTest extends FelixRestApplicationJerseyTest {
     protected void mockTranslation(DeviceAlarmTranslationKeys translationKey) {
         NlsMessageFormat nlsMessageFormat = this.mockNlsMessageFormat(translationKey.getDefaultFormat());
         when(thesaurus.getFormat(translationKey)).thenReturn(nlsMessageFormat);
+    }
+
+    protected CreationRuleTemplate getDefaultCreationRuleTemplate() {
+        IssueType issueType = mock(IssueType.class);
+        when(issueType.getKey()).thenReturn("devicealarm");
+        when(issueType.getName()).thenReturn("Device akarm");
+        when(issueType.getPrefix()).thenReturn("DAL");
+        return mockCreationRuleTemplate("0-1-2", "Description", issueType, mockPropertySpecs());
+    }
+
+    protected List<PropertySpec> mockPropertySpecs() {
+        PropertySpec propertySpec = mock(PropertySpec.class);
+        when(propertySpec.getName()).thenReturn("property");
+        when(propertySpec.getValueFactory()).thenReturn(new StringFactory());
+        when(propertySpec.isRequired()).thenReturn(true);
+        return Collections.singletonList(propertySpec);
+    }
+
+    protected IssueActionType getDefaultIssueActionType(){
+        IssueType issueType = getDefaultIssueType();
+        return mockIssueActionType(1, "send", issueType);
+    }
+
+    protected IssueActionType mockIssueActionType(long id, String name, IssueType issueType){
+        IssueActionType type = mock(IssueActionType.class);
+        IssueAction action = mockIssueAction(name);
+        when(type.getId()).thenReturn(id);
+        when(type.createIssueAction()).thenReturn(Optional.of(action));
+        when(type.getIssueType()).thenReturn(issueType);
+        return type;
+    }
+
+    protected IssueAction mockIssueAction(String name){
+        IssueAction action = mock(IssueAction.class);
+        when(action.getDisplayName()).thenReturn(name);
+        List<PropertySpec> propertySpec = mockPropertySpecs();
+        when(action.getPropertySpecs()).thenReturn(propertySpec);
+        return action;
+    }
+
+    protected IssueType getDefaultIssueType(){
+        return mockIssueType("devicealarm", "Device alarm");
+    }
+
+    protected CreationRuleTemplate mockCreationRuleTemplate(String name, String description, IssueType issueType, List<PropertySpec> properties){
+        CreationRuleTemplate template = mock(CreationRuleTemplate.class);
+        when(template.getName()).thenReturn(name);
+        when(template.getDisplayName()).thenReturn("Display Name: " + name);
+        when(template.getDescription()).thenReturn(description);
+        when(template.getIssueType()).thenReturn(issueType);
+        when(template.getPropertySpecs()).thenReturn(properties);
+        PropertyTypeInfo propertyTypeInfo = new PropertyTypeInfo();
+        propertyTypeInfo.simplePropertyType = SimplePropertyType.TEXTAREA;
+        PropertyInfo propertyInfo = new PropertyInfo("property", "property", null, propertyTypeInfo, false);
+        if (properties != null && !properties.isEmpty()) {
+            when(propertyValueInfoService.getPropertyInfos(template.getPropertySpecs())).thenReturn(Collections.singletonList(propertyInfo));
+        }
+        return template;
+    }
+
+    protected IssueType mockIssueType(String key, String name){
+        IssueType issueType = mock(IssueType.class);
+        when(issueType.getKey()).thenReturn(key);
+        when(issueType.getName()).thenReturn(name);
+        when(issueType.getPrefix()).thenReturn(name+key);
+        return issueType;
+    }
+
+    protected CreationRule mockCreationRule(long id, String name) {
+        Instant instant = Instant.now();
+        IssueReason reason = getDefaultReason();
+        CreationRuleTemplate template = getDefaultCreationRuleTemplate();
+        CreationRule rule = mock(CreationRule.class);
+        when(rule.getId()).thenReturn(id);
+        when(rule.getName()).thenReturn(name);
+        when(rule.getComment()).thenReturn("comment");
+        when(rule.getReason()).thenReturn(reason);
+        when(rule.getDueInType()).thenReturn(DueInType.DAY);
+        when(rule.getDueInValue()).thenReturn(5L);
+        when(rule.getActions()).thenReturn(Collections.emptyList());
+        when(rule.getProperties()).thenReturn(Collections.emptyMap());
+        when(rule.getTemplate()).thenReturn(template);
+        when(rule.getModTime()).thenReturn(instant);
+        when(rule.getCreateTime()).thenReturn(instant);
+        when(rule.getPriority()).thenReturn(com.elster.jupiter.issue.share.Priority.DEFAULT);
+        when(rule.getVersion()).thenReturn(2L);
+        return rule;
     }
 
     protected void mockTranslation(TranslationKeys translationKey) {
