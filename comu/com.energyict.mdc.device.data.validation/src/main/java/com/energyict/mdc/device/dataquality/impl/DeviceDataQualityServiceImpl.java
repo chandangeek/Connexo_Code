caeddb672f8b2a331d6c521a4e21fbf4ea45a4bc
Lobanov Dmitry <dlobanov@mera.ru>
2017-03-03 10:32:16 +0300

    CXO-5329 'Data quality' page improvements

--- comu/com.energyict.mdc.device.data.validation/src/main/java/com/energyict/mdc/device/dataquality/impl/DeviceDataQualityServiceImpl.java
+++ comu/com.energyict.mdc.device.data.validation/src/main/java/com/energyict/mdc/device/dataquality/impl/DeviceDataQualityServiceImpl.java
@@ -13,6 +13,7 @@ import com.elster.jupiter.validation.ValidationService;
 import com.energyict.mdc.device.dataquality.DataQualityOverviews;
 import com.energyict.mdc.device.dataquality.DeviceDataQualityService;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -23,16 +24,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Component(
-        name = "com.energyict.mdc.device.data.quality",
+        name = "com.energyict.mdc.device.dataquality",
         service = {DeviceDataQualityService.class},
         property = "name=" + DeviceDataQualityService.COMPONENT_NAME,
         immediate = true
 )
 public class DeviceDataQualityServiceImpl implements DeviceDataQualityService {
 
-    private volatile DataModel dataQualityKpiDataModel;
+    private volatile OrmService ormService;
     private volatile ValidationService validationService;
     private volatile EstimationService estimationService;
+    private volatile DataModel dataModel;
 
     // For OSGi purposes
     public DeviceDataQualityServiceImpl() {
@@ -45,11 +47,12 @@ public class DeviceDataQualityServiceImpl implements DeviceDataQualityService {
         this.setOrmService(ormService);
         this.setValidationService(validationService);
         this.setEstimationService(estimationService);
+        this.activate();
     }
 
     @Reference
     public void setOrmService(OrmService ormService) {
-        this.dataQualityKpiDataModel = ormService.getDataModel(DataQualityKpiService.COMPONENTNAME).orElseThrow(IllegalStateException::new);
+        this.ormService = ormService;
     }
 
     @Reference
@@ -62,6 +65,16 @@ public class DeviceDataQualityServiceImpl implements DeviceDataQualityService {
         this.estimationService = estimationService;
     }
 
+    @Reference
+    public void setDataQualityKpiService(DataQualityKpiService dataQualityKpiService) {
+        // just a dependency
+    }
+
+    @Activate
+    public void activate() {
+        this.dataModel = ormService.getDataModel(DataQualityKpiService.COMPONENTNAME).orElseThrow(IllegalStateException::new);
+    }
+
     @Override
     public DataQualityOverviewBuilder forAllDevices() {
         return new DataQualityOverviewBuilderImpl(this, this.validationService, this.estimationService);
@@ -69,7 +82,7 @@ public class DeviceDataQualityServiceImpl implements DeviceDataQualityService {
 
     DataQualityOverviews queryWith(DataQualityOverviewSpecificationImpl specification) {
         DataQualityOverviewSqlBuilder sqlBuilder = new DataQualityOverviewSqlBuilder(specification);
-        try (Connection connection = this.dataQualityKpiDataModel.getConnection(true);
+        try (Connection connection = this.dataModel.getConnection(true);
              PreparedStatement statement = sqlBuilder.prepare(connection)) {
             return this.execute(statement, specification);
         } catch (SQLException e) {
