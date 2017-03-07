da846d639af25d18a925c750b671e119ba1f1efe
geert <geert.delmeiren@elster.com>
2017-03-07 11:01:18 +0100

    CXO-5312 - View 'Security accessors' on device type

--- copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/CryptographicType.java
+++ copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/CryptographicType.java
@@ -4,19 +4,29 @@ package com.elster.jupiter.pki;
  * Created by bvn on 1/18/17.
  */
 public enum CryptographicType {
-    Certificate(false),
-    CertificateWithPrivateKey(false),
-    SymmetricKey(true),
-    Passphrase(true),
-    AsymmetricKey(true);
+    Certificate(false, MetaType.CERTIFICATE),
+    CertificateWithPrivateKey(false, MetaType.CERTIFICATE),
+    SymmetricKey(true, MetaType.KEY),
+    Passphrase(true, MetaType.KEY),
+    AsymmetricKey(false, MetaType.KEY);
 
-    CryptographicType(boolean durationMandatory) {
+    CryptographicType(boolean durationMandatory, MetaType metaType) {
         this.durationMandatory = durationMandatory;
+        this.metaType = metaType;
     }
 
     private boolean durationMandatory;
+    private MetaType metaType;
 
     public boolean requiresDuration() {
         return durationMandatory;
     };
+
+    public boolean isKey() {
+        return MetaType.KEY.equals(metaType);
+    }
+
+    enum MetaType {
+        KEY, CERTIFICATE
+    }
 }
