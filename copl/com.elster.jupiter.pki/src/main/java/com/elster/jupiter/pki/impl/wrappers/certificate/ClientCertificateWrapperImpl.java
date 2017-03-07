33f9690ef04b3cf65be2fc222ac7aedfe568814e
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2017-03-07 10:20:02 +0100

    CXO-5397: Fixing issue in RefAny from ClientCertificate to PrivateKey

--- copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/impl/wrappers/certificate/ClientCertificateWrapperImpl.java
+++ copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/impl/wrappers/certificate/ClientCertificateWrapperImpl.java
@@ -6,6 +6,7 @@ package com.elster.jupiter.pki.impl.wrappers.certificate;
 
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.orm.associations.RefAny;
 import com.elster.jupiter.orm.associations.Reference;
 import com.elster.jupiter.orm.associations.ValueReference;
 import com.elster.jupiter.pki.ClientCertificateWrapper;
@@ -16,36 +17,39 @@ import com.elster.jupiter.properties.PropertySpecService;
 import javax.inject.Inject;
 
 public class ClientCertificateWrapperImpl extends RequestableCertificateWrapperImpl implements ClientCertificateWrapper {
-    private Reference<PrivateKeyWrapper> privateKeyReference = ValueReference.absent();
+    private final DataModel dataModel;
+
+    private RefAny privateKeyReference;
     private Reference<KeyType> keyTypeReference = ValueReference.absent();
 
     @Inject
     public ClientCertificateWrapperImpl(DataModel dataModel, Thesaurus thesaurus, PropertySpecService propertySpecService) {
         super(dataModel, thesaurus, propertySpecService);
+        this.dataModel = dataModel;
     }
 
     /**
      * Initializer for Client certificate wrapper
      *
-     * @param alias
+     * @param alias The alias for this client certificate
      * @param privateKeyWrapper The Wrapper that will contain the private key for this client certificate
      * @param keyType The type for this certificate.
      * @return The updated ClientCertificateWrapper
      */
     public ClientCertificateWrapperImpl init(String alias, PrivateKeyWrapper privateKeyWrapper, KeyType keyType) {
         this.setAlias(alias);
-        this.privateKeyReference.set(privateKeyWrapper);
+        this.privateKeyReference = dataModel.asRefAny(privateKeyWrapper);
         this.keyTypeReference.set(keyType);
         return this;
     }
 
     @Override
     public PrivateKeyWrapper getPrivateKeyWrapper() {
-        return privateKeyReference.get();
+        return (PrivateKeyWrapper) privateKeyReference.get();
     }
 
     public void setPrivateKeyWrapper(PrivateKeyWrapper privateKeyWrapper) {
-        this.privateKeyReference.set(privateKeyWrapper);
+        this.privateKeyReference = dataModel.asRefAny(privateKeyWrapper);
         this.save();
     }
 
