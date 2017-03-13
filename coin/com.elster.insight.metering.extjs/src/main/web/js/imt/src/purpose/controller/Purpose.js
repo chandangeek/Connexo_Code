27bc590ff3c8c0f7c00ab3a96e6ea72b12c92a92
MaxIlyushen <ilyushen@mera.ru>
2017-03-13 23:39:42 +0300

    [CXO-5883]message-text-not-consistent-across-all-usages

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/controller/Purpose.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/controller/Purpose.js
@@ -356,7 +356,7 @@ Ext.define('Imt.purpose.controller.Purpose', {
             duration: 60000,
             fn: Ext.bind(me.onTooLongOperation, me, [purpose, confWindow, {
                 title: Uni.I18n.translate('purpose.dataValidation.timeout.title1', 'IMT', 'Data validation takes longer than expected'),
-                msg: Uni.I18n.translate('purpose.dataValidation.timeout.msg1', 'IMT', 'Data validation takes longer than expected. Data validation will continue in the background.')
+                msg: Uni.I18n.translate('purpose.dataValidation.timeout.message', 'IMT', 'Data validation takes longer than expected and will continue in the background.')
             }])
         });
 
@@ -406,7 +406,7 @@ Ext.define('Imt.purpose.controller.Purpose', {
             duration: 60000,
             fn: Ext.bind(me.onTooLongOperation, me, [purpose, confirmationWindow, {
                 title: Uni.I18n.translate('purpose.dataEstimation.timeout.title1', 'IMT', 'Data estimation takes longer than expected'),
-                msg: Uni.I18n.translate('purpose.dataEstimation.timeout.msg1', 'IMT', 'Data estimation takes longer than expected. Data estimation will continue in the background.')
+                msg: Uni.I18n.translate('purpose.dataValidation.timeout.message', 'IMT', 'Data validation takes longer than expected and will continue in the background.')
             }])
         });
 
