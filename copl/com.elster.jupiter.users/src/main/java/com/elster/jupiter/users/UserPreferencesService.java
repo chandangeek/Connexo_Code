b3d3ef81b6cff3cb8caa3f5ed60428c6457a351b
Valery Ruchin <varuchin@mera.ru>
2017-03-20 18:12:25 +0300

    [CXO-5786] [CXO-5792]

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/UserPreferencesService.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/UserPreferencesService.java
@@ -4,6 +4,8 @@
 
 package com.elster.jupiter.users;
 
+import java.security.Principal;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -11,7 +13,7 @@ import java.util.Optional;
 public interface UserPreferencesService {
     
     UserPreference createUserPreference(Locale locale, PreferenceType key, String formatBE, String formatFE, boolean isDefault);
-    
+
     List<Locale> getSupportedLocales();
     
     List<UserPreference> getPreferences(User user);
@@ -19,4 +21,13 @@ public interface UserPreferencesService {
     Optional<UserPreference> getPreferenceByKey(User user, PreferenceType format);
     
     Optional<UserPreference> getPreferenceByKey(Locale locale, PreferenceType format);
+
+    /***
+     * This method is used to receive date time formatter for user and selected preference types.
+     * Note that there cannot be more than 2 preference types
+     * @param principal
+     * @param preferenceTypes
+     * @return
+     */
+    DateTimeFormatter getDateTimeFormatter(Principal principal, PreferenceType... preferenceTypes);
 }
--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/UserPreferencesServiceImpl.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/UserPreferencesServiceImpl.java
@@ -4,13 +4,6 @@
 
 package com.elster.jupiter.users.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import com.elster.jupiter.domain.util.Save;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.users.PreferenceType;
@@ -18,8 +11,19 @@ import com.elster.jupiter.users.User;
 import com.elster.jupiter.users.UserPreference;
 import com.elster.jupiter.users.UserPreferencesService;
 
+import java.security.Principal;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class UserPreferencesServiceImpl implements UserPreferencesService {
-    
+
     public final DataModel dataModel;
 
     public UserPreferencesServiceImpl(DataModel dataModel) {
@@ -63,5 +67,64 @@ public class UserPreferencesServiceImpl implements UserPreferencesService {
         return dataModel.mapper(UserPreference.class).getUnique(
                 new String[] { "locale", "key", "isDefault" },
                 new Object[] { locale.toLanguageTag(), key, true });
+    }
+
+    @Override
+    public DateTimeFormatter getDateTimeFormatter(Principal principal, PreferenceType... preferenceTypes) {
+        // Construct the default backend date format:
+
+        String dateTimeFormat;
+        dateTimeFormat = "HH:mm:ss EEE dd MMM ''yy";
+        Locale locale = Locale.ENGLISH;
+        if (principal instanceof User) {
+            User user = (User) principal;
+            Optional<UserPreference> dateFormatPref = Stream.of(preferenceTypes)
+                    .filter(EnumSet.of(PreferenceType.SHORT_DATE, PreferenceType.LONG_DATE)::contains)
+                    .distinct()
+                    .reduce((p1, p2) -> {
+                        throw new IllegalArgumentException("More than two preference types are defined.");
+                    })
+                    .flatMap(timeFormat -> getPreferenceByKey(user, timeFormat));
+            Optional<UserPreference> timeFormatPref = Stream.of(preferenceTypes)
+                    .filter(EnumSet.of(PreferenceType.SHORT_TIME, PreferenceType.LONG_TIME)::contains)
+                    .distinct()
+                    .reduce((p1, p2) -> {
+                        throw new IllegalArgumentException("More than two preference types are defined.");
+                    })
+                    .flatMap(timeFormat -> getPreferenceByKey(user, timeFormat));
+            Optional<UserPreference> orderFormatPref = getPreferenceByKey(user, PreferenceType.DATETIME_ORDER);
+            Optional<UserPreference> separatorFormatPref = getPreferenceByKey(user, PreferenceType.DATETIME_SEPARATOR);
+            if (dateFormatPref.isPresent() && timeFormatPref.isPresent() && orderFormatPref.isPresent() && separatorFormatPref.isPresent()) {
+                dateTimeFormat = getDateTimeFormat(
+                        dateFormatPref.get().getFormat(),
+                        timeFormatPref.get().getFormat(),
+                        orderFormatPref.get().getFormat(),
+                        separatorFormatPref.get().getFormat()
+                );
+            }
+            locale = user.getLocale().orElse(locale);
+        }
+        return DateTimeFormatter.ofPattern(dateTimeFormat, locale);
+    }
+
+    private String getDateTimeFormat(String dateFormat, String timeFormat, String dateTimeOrderFormat, String separatorFormat) {
+        StringBuilder dateTimeFormatBuilder = new StringBuilder();
+
+        if (dateTimeOrderFormat.startsWith("T")) {
+            dateTimeFormatBuilder.append(timeFormat);
+        } else {
+            dateTimeFormatBuilder.append(dateFormat);
+        }
+        if ("SPACE".equals(separatorFormat)) {
+            dateTimeFormatBuilder.append(" ");
+        } else {
+            dateTimeFormatBuilder.append(" " + separatorFormat.trim() + " ");
+        }
+        if (dateTimeOrderFormat.startsWith("T")) {
+            dateTimeFormatBuilder.append(dateFormat);
+        } else {
+            dateTimeFormatBuilder.append(timeFormat);
+        }
+        return dateTimeFormatBuilder.toString();
     }
 }
