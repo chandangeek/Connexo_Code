a370ed1f4166844564470fe1597e18a06ff4b5c2
Lobanov Dmitry <dlobanov@mera.ru>
2017-03-20 11:54:37 +0300

    CXO-5336 apply review remarks

--- coko/com.elster.jupiter.dataquality/src/main/java/com/elster/jupiter/dataquality/impl/calc/DeviceDataQualityKpiSqlBuilder.java
+++ coko/com.elster.jupiter.dataquality/src/main/java/com/elster/jupiter/dataquality/impl/calc/DeviceDataQualityKpiSqlBuilder.java
@@ -4,105 +4,32 @@
 
 package com.elster.jupiter.dataquality.impl.calc;
 
-import com.elster.jupiter.cbo.QualityCodeIndex;
 import com.elster.jupiter.cbo.QualityCodeSystem;
-import com.elster.jupiter.metering.ReadingQualityType;
 import com.elster.jupiter.metering.groups.EndDeviceGroup;
 import com.elster.jupiter.orm.LiteralSql;
-import com.elster.jupiter.util.sql.SqlBuilder;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.time.Instant;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @LiteralSql
-public class DeviceDataQualityKpiSqlBuilder implements DataQualityKpiSqlBuilder {
+class DeviceDataQualityKpiSqlBuilder extends DataQualityKpiSqlBuilder {
 
     private final EndDeviceGroup deviceGroup;
-    private Instant startTime;
-    private Instant endTime;
-
-    private SqlBuilder sqlBuilder;
 
     DeviceDataQualityKpiSqlBuilder(EndDeviceGroup deviceGroup) {
+        super();
         this.deviceGroup = deviceGroup;
-        this.sqlBuilder = new SqlBuilder();
     }
 
     @Override
-    public DeviceDataQualityKpiSqlBuilder init(Instant start, Instant end) {
-        this.startTime = start;
-        this.endTime = end;
-        return this;
+    QualityCodeSystem getQualityCodeSystem() {
+        return QualityCodeSystem.MDC;
     }
 
     @Override
-    public PreparedStatement prepare(Connection connection) throws SQLException {
-        return buildSql().prepare(connection);
+    void appendChannelsSubQuery() {
+        super.sqlBuilder.append("SELECT id FROM MTR_CHANNEL WHERE channel_container IN (");
+        super.sqlBuilder.append("    SELECT id FROM MTR_CHANNEL_CONTAINER WHERE meter_activation IN (");
+        super.sqlBuilder.append("        SELECT id FROM MTR_METERACTIVATION WHERE meterid IN (");
+        super.sqlBuilder.add(this.deviceGroup.toSubQuery("id").toFragment());
+        super.sqlBuilder.append(")))");
     }
 
-    private SqlBuilder buildSql() {
-        this.appendAllReadingQualitiesWithClause();
-        this.appendActualQuery();
-        return this.sqlBuilder;
-    }
-
-    private void appendAllReadingQualitiesWithClause() {
-        sqlBuilder.append("WITH allReadingQualities (channelid, readingtimestamp, type, notsuspect) AS (");
-        sqlBuilder.append(" SELECT q.channelid, utc2date(q.readingtimestamp, t.timezonename), q.type, CASE WHEN q2.channelid IS NULL THEN 'Y' ELSE 'N' END");
-        sqlBuilder.append(" FROM MTR_READINGQUALITY q LEFT JOIN MTR_READINGQUALITY q2");
-        sqlBuilder.append(" ON  q.readingtimestamp = q2.readingtimestamp");
-        sqlBuilder.append(" AND q.channelid = q2.channelid");
-        sqlBuilder.append(" AND q2.type = " + toSql(ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.SUSPECT)));
-        sqlBuilder.append(" AND q2.actual = 'Y'");
-        sqlBuilder.append(" , IDS_TIMESERIES t");
-        sqlBuilder.append(" WHERE EXISTS (SELECT id FROM MTR_CHANNEL c WHERE q.channelid  = c.id AND c.timeseriesid = t.id)");
-        sqlBuilder.append(" AND q.channelid IN (");
-        this.appendChannelsSubQuery();
-        sqlBuilder.append(" )");
-        sqlBuilder.append(" AND (q.type IN (");
-        this.appendReadingQualityTypes();
-        sqlBuilder.append(" ) OR q.type LIKE '2.6.%'");
-        sqlBuilder.append("   OR q.type LIKE '2.8.%'");
-        sqlBuilder.append(" ) AND q.actual = 'Y'");
-        sqlBuilder.append("   AND q.readingtimestamp > ");
-        sqlBuilder.addLong(this.startTime.toEpochMilli());
-        sqlBuilder.append("   AND q.readingtimestamp <= ");
-        sqlBuilder.addLong(this.endTime.toEpochMilli());
-        sqlBuilder.append(")");
-    }
-
-    private void appendChannelsSubQuery() {
-        sqlBuilder.append("SELECT id FROM MTR_CHANNEL WHERE channel_container IN (");
-        sqlBuilder.append("    SELECT id FROM MTR_CHANNEL_CONTAINER WHERE meter_activation IN (");
-        sqlBuilder.append("        SELECT id FROM MTR_METERACTIVATION WHERE meterid IN (");
-        sqlBuilder.add(this.deviceGroup.toSubQuery("ID").toFragment());
-        sqlBuilder.append(")))");
-    }
-
-    private void appendReadingQualityTypes() {
-        String fixedReadingQualityTypeCodes = Stream.of(
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.SUSPECT),
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.KNOWNMISSINGREAD),
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.EDITGENERIC),
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.ADDED),
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.REJECTED),
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.ESTIMATEGENERIC),
-                ReadingQualityType.of(QualityCodeSystem.MDC, QualityCodeIndex.ACCEPTED))
-                .map(this::toSql)
-                .collect(Collectors.joining(", "));
-        sqlBuilder.append(fixedReadingQualityTypeCodes);
-    }
-
-    private String toSql(ReadingQualityType readingQualityType) {
-        return "'" + readingQualityType.getCode() + "'";
-    }
-
-    private void appendActualQuery() {
-        sqlBuilder.append("SELECT channelid, TRUNC(readingtimestamp, 'DDD'), type, notsuspect, COUNT(*) FROM allReadingQualities");
-        sqlBuilder.append(" GROUP BY channelid, TRUNC(readingtimestamp, 'DDD'), type, notsuspect");
-    }
 }
--- coko/com.elster.jupiter.dataquality/src/main/java/com/elster/jupiter/dataquality/impl/calc/UsagePointDataQualityKpiSqlBuilder.java
+++ coko/com.elster.jupiter.dataquality/src/main/java/com/elster/jupiter/dataquality/impl/calc/UsagePointDataQualityKpiSqlBuilder.java
@@ -4,113 +4,38 @@
 
 package com.elster.jupiter.dataquality.impl.calc;
 
-import com.elster.jupiter.cbo.QualityCodeIndex;
 import com.elster.jupiter.cbo.QualityCodeSystem;
-import com.elster.jupiter.metering.ReadingQualityType;
 import com.elster.jupiter.metering.config.MetrologyPurpose;
 import com.elster.jupiter.metering.groups.UsagePointGroup;
 import com.elster.jupiter.orm.LiteralSql;
-import com.elster.jupiter.util.sql.SqlBuilder;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.time.Instant;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @LiteralSql
-public class UsagePointDataQualityKpiSqlBuilder implements DataQualityKpiSqlBuilder {
+class UsagePointDataQualityKpiSqlBuilder extends DataQualityKpiSqlBuilder {
 
     private final UsagePointGroup usagePointGroup;
     private final MetrologyPurpose metrologyPurpose;
 
-    private Instant startTime;
-    private Instant endTime;
-
-    private SqlBuilder sqlBuilder;
-
     UsagePointDataQualityKpiSqlBuilder(UsagePointGroup usagePointGroup, MetrologyPurpose metrologyPurpose) {
+        super();
         this.usagePointGroup = usagePointGroup;
         this.metrologyPurpose = metrologyPurpose;
-        this.sqlBuilder = new SqlBuilder();
     }
 
     @Override
-    public UsagePointDataQualityKpiSqlBuilder init(Instant start, Instant end) {
-        this.startTime = start;
-        this.endTime = end;
-        return this;
+    QualityCodeSystem getQualityCodeSystem() {
+        return QualityCodeSystem.MDM;
     }
 
     @Override
-    public PreparedStatement prepare(Connection connection) throws SQLException {
-        return buildSql().prepare(connection);
-    }
-
-    private SqlBuilder buildSql() {
-        this.appendAllReadingQualitiesWithClause();
-        this.appendActualQuery();
-        return this.sqlBuilder;
-    }
-
-    private void appendAllReadingQualitiesWithClause() {
-        sqlBuilder.append("WITH allReadingQualities (channelid, readingtimestamp, type, notsuspect) AS (");
-        sqlBuilder.append(" SELECT q.channelid, utc2date(q.readingtimestamp, t.timezonename), q.type, CASE WHEN q2.channelid IS NULL THEN 'Y' ELSE 'N' END");
-        sqlBuilder.append(" FROM MTR_READINGQUALITY q LEFT JOIN MTR_READINGQUALITY q2");
-        sqlBuilder.append(" ON  q.readingtimestamp = q2.readingtimestamp");
-        sqlBuilder.append(" AND q.channelid = q2.channelid");
-        sqlBuilder.append(" AND q2.type = " + toSql(ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.SUSPECT)));
-        sqlBuilder.append(" AND q2.actual = 'Y'");
-        sqlBuilder.append(" , IDS_TIMESERIES t");
-        sqlBuilder.append(" WHERE EXISTS (SELECT id FROM MTR_CHANNEL c WHERE q.channelid  = c.id AND c.timeseriesid = t.id)");
-        sqlBuilder.append(" AND q.channelid IN (");
-        this.appendChannelsSubQuery();
-        sqlBuilder.append(" )");
-        sqlBuilder.append(" AND (q.type IN (");
-        this.appendReadingQualityTypes();
-        sqlBuilder.append(" ) OR q.type LIKE '3.6.%'");
-        sqlBuilder.append("   OR q.type LIKE '3.8.%'");
-        sqlBuilder.append(" ) AND q.actual = 'Y'");
-        sqlBuilder.append("   AND q.readingtimestamp > ");
-        sqlBuilder.addLong(this.startTime.toEpochMilli());
-        sqlBuilder.append("   AND q.readingtimestamp <= ");
-        sqlBuilder.addLong(this.endTime.toEpochMilli());
-        sqlBuilder.append(")");
-    }
-
-    private void appendChannelsSubQuery() {
-        sqlBuilder.append("SELECT ch.id FROM MTR_CHANNEL ch");
-        sqlBuilder.append("    JOIN MTR_EFFECTIVE_CONTRACT efc ON efc.channels_container = ch.channel_container");
-        sqlBuilder.append("    JOIN MTR_METROLOGY_CONTRACT mc ON mc.id = efc.metrology_contract");
-        sqlBuilder.append("    JOIN MTR_USAGEPOINTMTRCONFIG upmc ON upmc.id = efc.effective_conf");
-        sqlBuilder.append("    WHERE mc.metrology_purpose = ");
-        sqlBuilder.addLong(metrologyPurpose.getId());
-        sqlBuilder.append("      AND upmc.usagepoint IN (");
-        sqlBuilder.add(this.usagePointGroup.toSubQuery("ID").toFragment());
-        sqlBuilder.append(")");
-    }
-
-    private void appendReadingQualityTypes() {
-        String fixedReadingQualityTypeCodes = Stream.of(
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.SUSPECT),
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.KNOWNMISSINGREAD),
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.EDITGENERIC),
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.ADDED),
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.REJECTED),
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.ESTIMATEGENERIC),
-                ReadingQualityType.of(QualityCodeSystem.MDM, QualityCodeIndex.ACCEPTED))
-                .map(this::toSql)
-                .collect(Collectors.joining(", "));
-        sqlBuilder.append(fixedReadingQualityTypeCodes);
-    }
-
-    private String toSql(ReadingQualityType readingQualityType) {
-        return "'" + readingQualityType.getCode() + "'";
-    }
-
-    private void appendActualQuery() {
-        sqlBuilder.append("SELECT channelid, TRUNC(readingtimestamp, 'DDD'), type, notsuspect, COUNT(*) FROM allReadingQualities");
-        sqlBuilder.append(" GROUP BY channelid, TRUNC(readingtimestamp, 'DDD'), type, notsuspect");
+    void appendChannelsSubQuery() {
+        super.sqlBuilder.append("SELECT ch.id FROM MTR_CHANNEL ch");
+        super.sqlBuilder.append("    JOIN MTR_EFFECTIVE_CONTRACT efc ON efc.channels_container = ch.channel_container");
+        super.sqlBuilder.append("    JOIN MTR_METROLOGY_CONTRACT mc ON mc.id = efc.metrology_contract");
+        super.sqlBuilder.append("    JOIN MTR_USAGEPOINTMTRCONFIG upmc ON upmc.id = efc.effective_conf");
+        super.sqlBuilder.append("    WHERE mc.metrology_purpose = ");
+        super.sqlBuilder.addLong(this.metrologyPurpose.getId());
+        super.sqlBuilder.append("      AND upmc.usagepoint IN (");
+        super.sqlBuilder.add(this.usagePointGroup.toSubQuery("id").toFragment());
+        super.sqlBuilder.append(")");
     }
 }
