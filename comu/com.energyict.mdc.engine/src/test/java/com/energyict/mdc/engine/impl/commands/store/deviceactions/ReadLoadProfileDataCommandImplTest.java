6affa4900cee870aae86923cbbf7af2191742cf4
rudi <rudi.vankeirsbilck@honeywell.com>
2017-03-21 15:22:47 +0100

    Fix unit test failures

--- comu/com.energyict.mdc.engine/src/test/java/com/energyict/mdc/engine/impl/commands/store/deviceactions/ReadLoadProfileDataCommandImplTest.java
+++ comu/com.energyict.mdc.engine/src/test/java/com/energyict/mdc/engine/impl/commands/store/deviceactions/ReadLoadProfileDataCommandImplTest.java
@@ -1,7 +1,5 @@
 package com.energyict.mdc.engine.impl.commands.store.deviceactions;
 
-import com.energyict.cbo.BaseUnit;
-import com.energyict.cbo.Unit;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.impl.identifiers.DeviceIdentifierByMRID;
 import com.energyict.mdc.device.data.impl.identifiers.LoadProfileIdentifierById;
@@ -19,19 +17,23 @@ import com.energyict.mdc.protocol.api.DeviceProtocol;
 import com.energyict.mdc.protocol.api.device.offline.OfflineDevice;
 import com.energyict.mdc.tasks.LoadProfilesTask;
 import com.energyict.mdc.upl.meterdata.CollectedLoadProfile;
+
+import com.energyict.cbo.BaseUnit;
+import com.energyict.cbo.Unit;
 import com.energyict.obis.ObisCode;
 import com.energyict.protocol.ChannelInfo;
 import com.energyict.protocol.IntervalData;
 import com.energyict.protocol.LoadProfileReader;
-import org.junit.Test;
-import org.mockito.Matchers;
-import org.mockito.Mock;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,7 +43,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * Tests for the ReadLoadProfileDataCommandImpl component
+ * Tests for the ReadLoadProfileDataCommandImpl component.
  *
  * @author gna
  * @since 31/05/12 - 11:12
@@ -68,11 +70,11 @@ public class ReadLoadProfileDataCommandImplTest extends CommonCommandImplTests {
     @Test
     public void doExecuteTest() {
         DeviceProtocol deviceProtocol = mock(DeviceProtocol.class);
-        when(deviceProtocol.getLoadProfileData(Matchers.<List<LoadProfileReader>>any())).thenReturn(Arrays.<CollectedLoadProfile>asList(createDeviceCollectedLoadProfile()));
+        when(deviceProtocol.getLoadProfileData(Matchers.<List<LoadProfileReader>>any())).thenReturn(Collections.singletonList(createDeviceCollectedLoadProfile()));
         LoadProfilesTask loadProfilesTask = mock(LoadProfilesTask.class);
         LoadProfileType loadProfileType = mock(LoadProfileType.class);
         when(loadProfileType.getObisCode()).thenReturn(ObisCode.fromString("1.1.1.1.1.1"));
-        when(loadProfilesTask.getLoadProfileTypes()).thenReturn(Arrays.asList(loadProfileType));
+        when(loadProfilesTask.getLoadProfileTypes()).thenReturn(Collections.singletonList(loadProfileType));
         ExecutionContext executionContext = newTestExecutionContext();
         GroupedDeviceCommand groupedDeviceCommand = createGroupedDeviceCommand(offlineDevice, deviceProtocol);
         ComTaskExecution comTaskExecution = mock(ComTaskExecution.class);
@@ -89,6 +91,6 @@ public class ReadLoadProfileDataCommandImplTest extends CommonCommandImplTests {
         assertEquals("There should be 1 collected data object in the list", 1, loadProfileCommand.getCollectedData().size());
         assertTrue("The collected data should be CollectedLoadProfile", loadProfileCommand.getCollectedData().get(0) instanceof CollectedLoadProfile);
         assertEquals("Should have 10 intervals", 10, ((CollectedLoadProfile) loadProfileCommand.getCollectedData().get(0)).getCollectedIntervalData().size());
-        assertThat(journalMessage).startsWith(ComCommandDescriptionTitle.ReadLoadProfileDataCommandImpl.getDescription() + " {collectedProfiles: (0.0.99.98.0.255 - Supported - channels: CHN1, CHN2 - dataPeriod: [");
+        assertThat(journalMessage).matches(ComCommandDescriptionTitle.ReadLoadProfileDataCommandImpl.getDescription() + " \\{collectedProfiles: \\(.* - Supported - channels: CHN1, CHN2 - dataPeriod: \\[.*\\]\\)\\}");
     }
 }
