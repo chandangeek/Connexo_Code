d56b02036fbe12ff06fc2b023872e7a2deef32b5
dragos iancu <dragos-florin.iancu@honeywell.com>
2017-03-22 13:27:40 +0200

    CXO-3343 -  translation fixes

---
+++ copl/com.elster.jupiter.yellowfin/src/main/java/com/elster/jupiter/yellowfin/impl/MessageSeeds.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.yellowfin.impl;
+
+import com.elster.jupiter.nls.NlsMessageFormat;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.util.exception.MessageSeed;
+import com.elster.jupiter.yellowfin.YellowfinService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public enum MessageSeeds implements MessageSeed {
+
+    FACTS_NOT_AVAILABLE(1001, Keys.FACTS_UNAVAILABLE, "Connexo Facts is not available.");
+
+    private final int number;
+    private final String key;
+    private final String defaultFormat;
+    private final Level level;
+
+    MessageSeeds(int number, String key, String defaultFormat) {
+        this(number, key, defaultFormat, Level.SEVERE);
+    }
+
+    MessageSeeds(int number, String key, String defaultFormat, Level level) {
+        this.number = number;
+        this.key = key;
+        this.defaultFormat = defaultFormat;
+        this.level = level;
+    }
+
+    @Override
+    public String getModule() {
+        return YellowfinService.COMPONENTNAME;
+    }
+
+    @Override
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String getDefaultFormat() {
+        return defaultFormat;
+    }
+
+    @Override
+    public Level getLevel() {
+        return level;
+    }
+
+    public void log(Logger logger, Thesaurus thesaurus, Object... args) {
+        NlsMessageFormat format = thesaurus.getFormat(this);
+        logger.log(getLevel(), format.format(args));
+    }
+
+    public void log(Logger logger, Thesaurus thesaurus, Throwable t, Object... args) {
+        NlsMessageFormat format = thesaurus.getFormat(this);
+        logger.log(getLevel(), format.format(args), t);
+    }
+
+    public enum Keys {
+        ;
+        public static final String FACTS_UNAVAILABLE = "error.facts.unavailable";
+    }
+}
