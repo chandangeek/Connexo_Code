f14783b4682fce178f91e48d89d19d1ed49f9468
Bruno Ballekens <bruno.ballekens@elster.com>
2017-03-22 15:05:45 +0100

    Javadoc generation

--- copl/com.elster.jupiter.messaging/src/main/java/com/elster/jupiter/messaging/subscriber/MessageHandlerFactory.java
+++ copl/com.elster.jupiter.messaging/src/main/java/com/elster/jupiter/messaging/subscriber/MessageHandlerFactory.java
@@ -4,7 +4,23 @@
 
 package com.elster.jupiter.messaging.subscriber;
 
+import aQute.bnd.annotation.ConsumerType;
+
+/**
+ * Factory interface to create MessageHandlers.
+ * Implementation must register the factory in the OSGi container with following properties:
+ * <ul>
+ *     <li>destination: name of the queue/destination on which MessageHandler will be listening</li>
+ *     <li>subscriber: name of the subscriber on that destination on which MessageHandler will be listening</li>
+ * </ul>
+ * Remark: the destination and the subscriber must be created by an installer
+ */
+@ConsumerType
 public interface MessageHandlerFactory {
 
+    /**
+     * Create a new message handler
+     * @return new MessageHandler
+     */
     MessageHandler newMessageHandler();
 }
