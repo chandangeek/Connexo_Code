1e6a24fa36d05384b03b7f7b91b002bf147734d8
Yan R <yaratay@mera.ru>
2017-03-23 18:00:49 +0300

    CXO-6088/CXO-6158: [BE] Replace 'Number of consecutive suspects' with 'Maximum period' on the existing estimators

---
+++ copl/com.elster.jupiter.time.rest/src/main/java/com/elster/jupiter/time/rest/TimeDurationInfo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.time.rest;
+
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.time.TimeDuration;
+import com.elster.jupiter.time.impl.TimeDurationUnitTranslationKeys;
+
+public class TimeDurationInfo {
+    public long count;
+    public String timeUnit = TimeDuration.TimeUnit.SECONDS.getDescription();
+    public String localizedTimeUnit = TimeDuration.TimeUnit.SECONDS.getDescription();
+    public int asSeconds;
+
+    public TimeDurationInfo() {
+    }
+
+    public TimeDurationInfo(String bogus) {
+        // Constructor to allow ExtJS empty string TimeDurations
+    }
+
+    public TimeDurationInfo(TimeDuration timeDuration) {
+        this.count = timeDuration.getCount();
+        this.timeUnit = TimeDuration.getTimeUnitDescription(timeDuration.getTimeUnitCode());
+        this.localizedTimeUnit = TimeDuration.getTimeUnitDescription(timeDuration.getTimeUnitCode());
+        this.asSeconds = timeDuration.getSeconds();
+    }
+
+    public TimeDurationInfo(TimeDuration timeDuration, Thesaurus thesaurus) {
+        this.count = timeDuration.getCount();
+        this.timeUnit = TimeDuration.getTimeUnitDescription(timeDuration.getTimeUnitCode());
+        this.localizedTimeUnit = thesaurus.getString(
+                TimeDurationUnitTranslationKeys.Constants.DESCRIPTION_TRANSLATION_KEY_PREFIX
+                        + TimeDuration.getTimeUnitDescription(timeDuration.getTimeUnitCode()),
+                TimeDuration.getTimeUnitDescription(timeDuration.getTimeUnitCode()));
+        this.asSeconds = timeDuration.getSeconds();
+    }
+
+    public TimeDurationInfo(long seconds) {
+        this.count = seconds;
+        this.timeUnit = TimeDuration.TimeUnit.SECONDS.getDescription();
+        this.localizedTimeUnit = TimeDuration.TimeUnit.SECONDS.getDescription();
+        this.asSeconds = (int) seconds;
+    }
+
+    public static TimeDurationInfo of(TimeDuration timeDurationOrNull) {
+        return timeDurationOrNull == null ? null : new TimeDurationInfo(timeDurationOrNull);
+    }
+
+    public TimeDuration asTimeDuration() {
+        return new TimeDuration(this.count + " " + this.timeUnit);
+    }
+
+}
