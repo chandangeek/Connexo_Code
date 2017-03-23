295e75d7e863a851fbb7f1473513a06ec4269b99
geert <geert.delmeiren@elster.com>
2017-03-23 12:39:22 +0100

    CXO-6213 - View certificates (in the certificate store)

--- copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/view/CertificateFilter.js
+++ copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/view/CertificateFilter.js
@@ -14,24 +14,7 @@ Ext.define('Pkj.view.CertificateFilter', {
                 type: 'text',
                 dataIndex: 'alias',
                 emptyText: Uni.I18n.translate('general.alias', 'PKJ', 'Alias')
-            },
-            //{
-            //    type: 'combobox',
-            //    dataIndex: 'queue',
-            //    emptyText: Uni.I18n.translate('general.queue', 'APR', 'Queue'),
-            //    multiSelect: true,
-            //    displayField: 'queue',
-            //    valueField: 'queue',
-            //    store: 'Apr.store.Queues'
-            //    //   hidden: !me.includeServiceCombo
-            //},
-            //{
-            //    type: 'interval',
-            //    dataIndex: 'startedBetween',
-            //    dataIndexFrom: 'startedOnFrom',
-            //    dataIndexTo: 'startedOnTo',
-            //    text: Uni.I18n.translate('validationtask.historyFilter.startedBetween', 'APR', 'Started between')
-            //}
+            }
         ];
 
         me.callParent(arguments);
