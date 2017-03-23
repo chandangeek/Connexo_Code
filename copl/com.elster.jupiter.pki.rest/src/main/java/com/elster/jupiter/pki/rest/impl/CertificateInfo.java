3aecd9734e09ea8c1ce27002cc5bccf0bd9ea476
geert <geert.delmeiren@elster.com>
2017-03-23 12:37:48 +0100

    CXO-6213 - View certificates (in the certificate store)

--- copl/com.elster.jupiter.pki.rest/src/main/java/com/elster/jupiter/pki/rest/impl/CertificateInfo.java
+++ copl/com.elster.jupiter.pki.rest/src/main/java/com/elster/jupiter/pki/rest/impl/CertificateInfo.java
@@ -4,11 +4,24 @@
 
 package com.elster.jupiter.pki.rest.impl;
 
+import com.elster.jupiter.pki.ClientCertificateWrapper;
+
 import java.time.Instant;
 
 public class CertificateInfo {
 
     public String alias;
     public Instant expirationDate;
+    public String type;
+    public String issuer;
+    public String subject;
+    public String status;
 
+    CertificateInfo() {
+    }
+
+    CertificateInfo(ClientCertificateWrapper certificateWrapper) {
+        this.alias = certificateWrapper.getAlias();
+        this.expirationDate = certificateWrapper.getExpirationTime().orElse(null);
+    }
 }
