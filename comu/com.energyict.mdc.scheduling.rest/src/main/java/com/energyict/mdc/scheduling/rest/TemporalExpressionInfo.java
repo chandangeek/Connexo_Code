51d0dbbb8e779c4beabb9455b4b928ad9e363126
Yan R <yaratay@mera.ru>
2017-03-23 18:01:45 +0300

    CXO-6088/CXO-6158: [BE] Replace 'Number of consecutive suspects' with 'Maximum period' on the existing estimators

--- comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/TemporalExpressionInfo.java
+++ comu/com.energyict.mdc.scheduling.rest/src/main/java/com/energyict/mdc/scheduling/rest/TemporalExpressionInfo.java
@@ -7,7 +7,7 @@ package com.energyict.mdc.scheduling.rest;
 import com.elster.jupiter.nls.LocalizedFieldValidationException;
 import com.elster.jupiter.time.TemporalExpression;
 import com.elster.jupiter.time.TimeDuration;
-import com.energyict.mdc.common.rest.TimeDurationInfo;
+import com.elster.jupiter.time.rest.TimeDurationInfo;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
