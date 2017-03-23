69ab65a04fb9787e357168606e21f81d12121314
Albert Versin <albert.versin@elster.com>
2017-03-23 14:36:16 +0200

    CXO-6105 CXO-6106 CXO-6107 Re-factor the support for configuring processes on device states & implement support for configuring processes on UP states.

---
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/FsmUsagePointProviderImpl.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.metering.impl;
+
+import com.elster.jupiter.fsm.FsmUsagePointProvider;
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.UsagePoint;
+import com.elster.jupiter.metering.UsagePointConnectionState;
+import com.elster.jupiter.metering.config.EffectiveMetrologyConfigurationOnUsagePoint;
+import com.elster.jupiter.properties.HasIdAndName;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Component(
+        name = "com.elster.jupiter.metering.fsmprovider",
+        service = {FsmUsagePointProvider.class},
+        property = {"name=FSM_PROVIDER"}, immediate = true)
+public class FsmUsagePointProviderImpl implements FsmUsagePointProvider {
+
+    private volatile MeteringService meteringService;
+    private final static String CONNECTION_STATES = "connectionStates";
+    private final static String METROLOGY_CONFIG = "metrologyConfigurations";
+
+    public FsmUsagePointProviderImpl(){
+    }
+
+    @Inject
+    public FsmUsagePointProviderImpl(MeteringService meteringService){
+        this.meteringService = meteringService;
+    }
+
+    @Reference
+    public void setMeteringService(MeteringService meteringService) {
+        this.meteringService = meteringService;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean areProcessPropertiesAvailableForUP(Map<String, Object> processProperties, long id) {
+        boolean result= false;
+        UsagePoint usagePoint = meteringService.findUsagePointById(id).orElse(null);
+        if(usagePoint != null){
+            Optional<UsagePointConnectionState> currentConnectionState = usagePoint.getCurrentConnectionState();
+            Optional<EffectiveMetrologyConfigurationOnUsagePoint> metrologyConfiguration = usagePoint
+                    .getCurrentEffectiveMetrologyConfiguration();
+            if(currentConnectionState.isPresent() && metrologyConfiguration.isPresent()){
+                boolean connectionCheck = List.class.isInstance(processProperties.get(CONNECTION_STATES)) && ((List<Object>) processProperties
+                        .get(CONNECTION_STATES))
+                        .stream()
+                        .filter(HasIdAndName.class::isInstance)
+                        .anyMatch(v -> ((HasIdAndName) v).getId()
+                                .toString()
+                                .equals(currentConnectionState.get().getConnectionState().getId()));
+                boolean metrologyConfigurationCheck = List.class.isInstance(processProperties.get(METROLOGY_CONFIG)) && ((List<Object>) processProperties
+                        .get(METROLOGY_CONFIG))
+                        .stream()
+                        .filter(HasIdAndName.class::isInstance)
+                        .anyMatch(v -> ((HasIdAndName) v).getId()
+                                .toString()
+                                .equals(String.valueOf(metrologyConfiguration.get().getMetrologyConfiguration().getId())));
+                result = connectionCheck && metrologyConfigurationCheck;
+            }
+        }
+        return result;
+    }
+}
