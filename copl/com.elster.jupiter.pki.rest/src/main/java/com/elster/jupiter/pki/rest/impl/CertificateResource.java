950e29e6b0b3633308b518aed672e33ebcc19efd
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2017-03-23 13:52:05 +0100

    CXO-3603: CertificateResource adding

---
+++ copl/com.elster.jupiter.pki.rest/src/main/java/com/elster/jupiter/pki/rest/impl/CertificateResource.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 by Honeywell Inc. All rights reserved.
+ */
+
+package com.elster.jupiter.pki.rest.impl;
+
+import javax.ws.rs.Path;
+
+@Path("/certificates")
+public class CertificateResource {
+
+//    @GET
+
+}
