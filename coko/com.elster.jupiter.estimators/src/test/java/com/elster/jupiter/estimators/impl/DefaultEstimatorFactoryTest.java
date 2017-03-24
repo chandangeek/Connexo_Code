4129dacf57346115c110911c002445e1c0924458
Yan R <yaratay@mera.ru>
2017-03-24 10:21:44 +0300

    CXO-6088/CXO-6158: [BE] Replace 'Number of consecutive suspects' with 'Maximum period' on the existing estimators Testfix

--- coko/com.elster.jupiter.estimators/src/test/java/com/elster/jupiter/estimators/impl/DefaultEstimatorFactoryTest.java
+++ coko/com.elster.jupiter.estimators/src/test/java/com/elster/jupiter/estimators/impl/DefaultEstimatorFactoryTest.java
@@ -4,19 +4,23 @@
 
 package com.elster.jupiter.estimators.impl;
 
-import com.elster.jupiter.kpi.KpiService;
+import com.elster.jupiter.estimation.EstimationService;
 import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.impl.NlsModule;
 import com.elster.jupiter.properties.PropertySpecService;
 import com.elster.jupiter.time.TimeService;
 import com.elster.jupiter.validation.ValidationService;
 
-import org.junit.*;
-import org.junit.runner.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static com.elster.jupiter.devtools.tests.assertions.JupiterAssertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultEstimatorFactoryTest {
@@ -26,22 +30,33 @@ public class DefaultEstimatorFactoryTest {
     @Mock
     private PropertySpecService propertySpecService;
     @Mock
-    private KpiService kpiService;
-    @Mock
     private ValidationService validationService;
     @Mock
     private MeteringService meteringService;
     @Mock
     private TimeService timeService;
 
-    @Test
-    public void testCreatePowerGapFill() {
-
-        DefaultEstimatorFactory defaultEstimatorFactory = new DefaultEstimatorFactory(nlsService, propertySpecService, validationService, meteringService, timeService);
-
-        assertThat(defaultEstimatorFactory.available()).contains(DefaultEstimatorFactory.POWER_GAP_FILL_ESTIMATOR);
-
-        assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.POWER_GAP_FILL_ESTIMATOR)).isInstanceOf(PowerGapFill.class);
+    @Before
+    public void setUp() {
+        when(nlsService.getThesaurus(MessageSeeds.COMPONENT_NAME, Layer.DOMAIN)).thenReturn(NlsModule.FakeThesaurus.INSTANCE);
+        when(nlsService.getThesaurus(EstimationService.COMPONENTNAME, Layer.DOMAIN)).thenReturn(NlsModule.FakeThesaurus.INSTANCE);
     }
 
+    @Test
+    public void testCreatePowerGapFill() {
+        DefaultEstimatorFactory defaultEstimatorFactory = new DefaultEstimatorFactory(nlsService, propertySpecService, validationService, meteringService, timeService);
+
+        assertThat(defaultEstimatorFactory.available()).containsOnly(
+                DefaultEstimatorFactory.POWER_GAP_FILL_ESTIMATOR,
+                DefaultEstimatorFactory.AVG_WITH_SAMPLES_ESTIMATOR,
+                DefaultEstimatorFactory.EQUAL_DISTRIBUTION_ESTIMATOR,
+                DefaultEstimatorFactory.LINEAR_INTERPOLATION_ESTIMATOR,
+                DefaultEstimatorFactory.VALUE_FILL_ESTIMATOR);
+
+        assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.POWER_GAP_FILL_ESTIMATOR)).isInstanceOf(PowerGapFill.class);
+        assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.AVG_WITH_SAMPLES_ESTIMATOR)).isInstanceOf(AverageWithSamplesEstimator.class);
+        assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.EQUAL_DISTRIBUTION_ESTIMATOR)).isInstanceOf(EqualDistribution.class);
+        assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.LINEAR_INTERPOLATION_ESTIMATOR)).isInstanceOf(LinearInterpolation.class);
+        assertThat(defaultEstimatorFactory.createTemplate(DefaultEstimatorFactory.VALUE_FILL_ESTIMATOR)).isInstanceOf(ValueFillEstimator.class);
+    }
 }
