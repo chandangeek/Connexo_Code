835304dab8c88067a08c1c7c01a69685ac040291
khe <kristof.hennebel@elster.com>
2017-03-28 15:26:15 +0200

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
     - Fixed some bugs and JUnit tests in MDC
     - Removed some failing tests for identifiers, they need to be created again properly

--- comu/com.energyict.mdc.tasks/src/test/java/com/energyict/mdc/tasks/MockModule.java
+++ comu/com.energyict.mdc.tasks/src/test/java/com/energyict/mdc/tasks/MockModule.java
@@ -5,24 +5,35 @@ import com.elster.jupiter.search.SearchService;
 import com.elster.jupiter.time.TimeService;
 import com.elster.jupiter.upgrade.UpgradeService;
 import com.elster.jupiter.upgrade.impl.UpgradeModule;
-
+import com.energyict.mdc.protocol.api.device.messages.DeviceMessageSpecificationService;
 import com.google.inject.AbstractModule;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
 
+import java.util.Optional;
+
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MockModule extends AbstractModule {
 
     private BundleContext bundleContext;
     private EventAdmin eventAdmin;
     private TimeService timeService;
+    private DeviceMessageSpecificationService deviceMessageSpecificationService;
 
     public MockModule(BundleContext bundleContext) {
         super();
         this.bundleContext = bundleContext;
         this.eventAdmin = mock(EventAdmin.class);
         this.timeService = mock(TimeService.class);
+        this.deviceMessageSpecificationService = mock(DeviceMessageSpecificationService.class);
+
+        when(deviceMessageSpecificationService.findCategoryById(anyInt())).thenAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+            return Optional.of(DeviceMessageTestCategories.values()[((int) args[0])]);
+        });
     }
 
     @Override
@@ -30,6 +41,7 @@ public class MockModule extends AbstractModule {
         bind(BundleContext.class).toInstance(bundleContext);
         bind(EventAdmin.class).toInstance(eventAdmin);
         bind(TimeService.class).toInstance(this.timeService);
+        bind(DeviceMessageSpecificationService.class).toInstance(this.deviceMessageSpecificationService);
         bind(SearchService.class).toInstance(mock(SearchService.class));
         bind(LicenseService.class).toInstance(mock(LicenseService.class));
         bind(UpgradeService.class).toInstance(UpgradeModule.FakeUpgradeService.getInstance());
