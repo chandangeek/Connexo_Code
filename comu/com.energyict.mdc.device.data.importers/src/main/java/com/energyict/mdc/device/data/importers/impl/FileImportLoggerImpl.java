f2fe05a37db7c03e5a625e3bc362ab4b3f9468da
khe <kristof.hennebel@elster.com>
2017-03-30 15:55:18 +0200

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
     - Fixed some compilation issues due to merge
     - Removed the 'Copyright EnergyICT' which I had accidently re-introduced while merging.

--- comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/FileImportLoggerImpl.java
+++ comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/FileImportLoggerImpl.java
@@ -8,7 +8,6 @@ import com.elster.jupiter.fileimport.FileImportOccurrence;
 import com.elster.jupiter.fileimport.csvimport.exceptions.ImportException;
 import com.elster.jupiter.nls.TranslationKey;
 import com.elster.jupiter.util.exception.MessageSeed;
-import com.energyict.mdc.device.data.importers.impl.exceptions.ImportException;
 import com.energyict.mdc.upl.issue.Warning;
 
 public abstract class FileImportLoggerImpl<T extends FileImportRecord> implements FileImportLogger<T> {
