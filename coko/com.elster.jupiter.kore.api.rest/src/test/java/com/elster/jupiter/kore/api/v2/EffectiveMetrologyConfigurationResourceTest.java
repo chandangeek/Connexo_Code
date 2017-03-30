5237c9ee22517c5217dac7218c0f6a0bef310c2a
Anton Fomchenko <antfom@mera.ru>
2017-03-30 15:19:29 +0300

    CXO-5581 unit tests

--- coko/com.elster.jupiter.kore.api.rest/src/test/java/com/elster/jupiter/kore/api/v2/EffectiveMetrologyConfigurationResourceTest.java
+++ coko/com.elster.jupiter.kore.api.rest/src/test/java/com/elster/jupiter/kore/api/v2/EffectiveMetrologyConfigurationResourceTest.java
@@ -8,14 +8,18 @@ import com.elster.jupiter.metering.ServiceKind;
 import com.elster.jupiter.metering.UsagePoint;
 import com.elster.jupiter.metering.config.EffectiveMetrologyConfigurationOnUsagePoint;
 import com.elster.jupiter.metering.config.UsagePointMetrologyConfiguration;
+import com.elster.jupiter.rest.api.util.v1.hypermedia.LinkInfo;
 import com.elster.jupiter.rest.api.util.v1.hypermedia.Relation;
+import com.elster.jupiter.rest.util.IntervalInfo;
 import com.elster.jupiter.util.time.Interval;
 
 import com.google.common.collect.Range;
 import com.jayway.jsonpath.JsonModel;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
+import java.net.URI;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +30,7 @@ import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -34,17 +39,21 @@ public class EffectiveMetrologyConfigurationResourceTest extends PlatformPublicA
     @Mock
     EffectiveMetrologyConfigurationOnUsagePoint effectiveMetrologyConfiguration;
 
+    UsagePoint usagePoint;
+    UsagePointMetrologyConfiguration metrologyConfiguration;
+
     @Override
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        UsagePointMetrologyConfiguration metrologyConfiguration = mockMetrologyConfiguration(41, "metro", 1);
-        UsagePoint usagePoint = mockUsagePoint(MRID, 1, ServiceKind.ELECTRICITY);
+        metrologyConfiguration = mockMetrologyConfiguration(41, "metro", 1);
+        usagePoint = mockUsagePoint(MRID, 1, ServiceKind.ELECTRICITY);
         when(effectiveMetrologyConfiguration.getMetrologyConfiguration()).thenReturn(metrologyConfiguration);
         when(effectiveMetrologyConfiguration.getUsagePoint()).thenReturn(usagePoint);
         when(effectiveMetrologyConfiguration.getRange()).thenReturn(Range.atLeast(Instant.ofEpochMilli(1468933329000L)));
         when(effectiveMetrologyConfiguration.getInterval()).thenReturn(Interval.of(Range.atLeast(Instant.ofEpochMilli(1468933329000L))));
         when(usagePoint.getEffectiveMetrologyConfiguration(any(Instant.class))).thenReturn(Optional.of(effectiveMetrologyConfiguration));
+        when(meteringService.findAndLockUsagePointByIdAndVersion(41,1)).thenReturn(Optional.of(usagePoint));
     }
 
     @Test
@@ -86,5 +95,48 @@ public class EffectiveMetrologyConfigurationResourceTest extends PlatformPublicA
         JsonModel model = JsonModel.model((InputStream) response.getEntity());
         assertThat(model.<List>get("$")).hasSize(6);
         assertThat(model.<List<String>>get("$")).containsOnly("id", "link", "interval", "metrologyConfiguration", "purposes", "usagePoint");
+    }
+
+    @Test
+    public void testCreateEffectiveMetrologyConfiguration() throws Exception {
+        EffectiveMetrologyConfigurationInfo info = new EffectiveMetrologyConfigurationInfo();
+        info.id = 123456L;
+        info.usagePoint = new LinkInfo();
+        info.usagePoint.id = 123L;
+        info.usagePoint.version = 1L;
+        info.metrologyConfiguration = new LinkInfo<>();
+        info.metrologyConfiguration.id = 41L;
+        info.metrologyConfiguration.version = 1L;
+
+        // Business method
+        Response response = target("/usagepoints/" + MRID + "/metrologyconfigurations").request().post(Entity.json(info));
+
+        // Asserts
+        assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+        assertThat(response.getLocation()).isEqualTo(new URI("http://localhost:9998/usagepoints/" + MRID + "/metrologyconfigurations/" + info.id));
+
+        verify(usagePoint).apply(metrologyConfiguration, Instant.ofEpochMilli(info.id));
+    }
+
+    @Test
+    public void testCloseEffectiveMetrologyConfiguration() throws Exception {
+        EffectiveMetrologyConfigurationInfo info = new EffectiveMetrologyConfigurationInfo();
+        info.id = 123456L;
+        info.usagePoint = new LinkInfo();
+        info.usagePoint.id = 123L;
+        info.usagePoint.version = 1L;
+        info.metrologyConfiguration = new LinkInfo<>();
+        info.metrologyConfiguration.id = 41L;
+        info.metrologyConfiguration.version = 1L;
+        info.interval = new IntervalInfo();
+        info.interval.end = 1234567L;
+
+        // Business method
+        Response response = target("/usagepoints/" + MRID + "/metrologyconfigurations/" + info.id).request().put(Entity.json(info));
+
+        // Asserts
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+
+        verify(effectiveMetrologyConfiguration).close(Instant.ofEpochMilli(info.interval.end));
     }
 }
