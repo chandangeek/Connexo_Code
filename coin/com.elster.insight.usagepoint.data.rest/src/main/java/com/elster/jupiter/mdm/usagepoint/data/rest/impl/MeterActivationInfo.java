59a9b7fe9a1ea01a3264a29fc8d07b276caac18a
Valery Ruchin <varuchin@mera.ru>
2017-03-31 17:01:06 +0300

    [CXO-5602] [CXO-5600]

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/MeterActivationInfo.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/MeterActivationInfo.java
@@ -7,14 +7,11 @@ package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
 import com.elster.jupiter.metering.MeterActivation;
 
 import javax.xml.bind.annotation.XmlRootElement;
-import java.time.Instant;
 
 @XmlRootElement
 public class MeterActivationInfo {
 
     public long id;
-    public Instant start;
-    public Instant end;
     public long version;
     public MeterInfo meter;
     public MeterRoleInfo meterRole;
@@ -25,8 +22,6 @@ public class MeterActivationInfo {
 
     public MeterActivationInfo(MeterActivation meterActivation, boolean includeMeterInfo) {
         this.id = meterActivation.getId();
-        this.start = meterActivation.getStart() == null ? null : meterActivation.getStart();
-        this.end = meterActivation.getEnd() == null ? null : meterActivation.getEnd();
         this.version = meterActivation.getVersion();
         meterActivation.getMeter().ifPresent(m -> {
             this.meter = includeMeterInfo ? new MeterInfo(m) : new MeterInfo();
