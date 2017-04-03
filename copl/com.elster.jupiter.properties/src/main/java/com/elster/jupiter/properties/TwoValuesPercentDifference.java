8908d3fccd626dc766bc52c1e220de9150272c06
Denis Antonov <dantonov@mera.ru>
2017-04-03 18:16:59 +0300

    fix property representation. CXO-5751

--- copl/com.elster.jupiter.properties/src/main/java/com/elster/jupiter/properties/TwoValuesPercentDifference.java
+++ copl/com.elster.jupiter.properties/src/main/java/com/elster/jupiter/properties/TwoValuesPercentDifference.java
@@ -4,14 +4,20 @@
 
 package com.elster.jupiter.properties;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by dantonov on 28.03.2017.
  */
+@XmlRootElement
 public class TwoValuesPercentDifference extends TwoValuesDifference {
 
     public TwoValuesPercentDifference() {
         super(Type.percent);
     }
 
+    @JsonProperty(value = "value")
     public Double percent;
 }
