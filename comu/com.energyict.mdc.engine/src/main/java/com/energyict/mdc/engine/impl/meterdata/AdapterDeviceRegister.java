f6a7233fb08c7c22ccfe76eaee959048026cbd31
rudi <rudi.vankeirsbilck@honeywell.com>
2017-04-03 12:04:48 +0200

    Provide implementation for meter and smart meter protocol adapter + replace protocol-api specific classes with the same upl alternatives

--- comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/meterdata/AdapterDeviceRegister.java
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/meterdata/AdapterDeviceRegister.java
@@ -4,17 +4,18 @@
 
 package com.energyict.mdc.engine.impl.meterdata;
 
-import com.energyict.cbo.Quantity;
 import com.energyict.mdc.upl.meterdata.identifiers.RegisterIdentifier;
 
-import java.time.Instant;
+import com.energyict.cbo.Quantity;
+
+import java.util.Date;
 
 /**
  * Implementation of a Register, collected from a Device, <b>only</b> used by the ProtocolAdapters to map
- * older {@link com.energyict.mdc.protocol.api.device.data.RegisterValue}s to a {@link DeviceRegister}.
+ * older {@link com.energyict.protocol.RegisterValue}s to a {@link DeviceRegister}.
  * <p>
  * If data is collected, then a proper collected data <b>AND</b> {@link #readTime} should be set by
- * {@link #setCollectedData(Quantity, String)} and {@link #setReadTime(Instant)}.
+ * {@link #setCollectedData(Quantity, String)} and {@link #setReadTime(Date)}.
  * <p>
  * If no data could be collected, then a proper {@link com.energyict.mdc.upl.issue.Issue} and {@link com.energyict.mdc.upl.meterdata.ResultType}
  * should be returned by calling the {@link #setFailureInformation(com.energyict.mdc.upl.meterdata.ResultType, com.energyict.mdc.upl.issue.Issue)}.
