bd0572fad5908d9a94e1475125c013aa6b5bb868
Yan R <yaratay@mera.ru>
2017-04-05 18:16:27 +0300

    CXO-5873/CXO-5986: Validation/estimation of data from historical/future metrology configurations

--- copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/nls/impl/NlsModule.java
+++ copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/nls/impl/NlsModule.java
@@ -59,7 +59,6 @@ public class NlsModule extends AbstractModule {
             return false;
         }
 
-
         @Override
         public Thesaurus join(Thesaurus thesaurus) {
             return this;
