692973dcdddf597da58b591998688bfd21345aa1
Yan R <yaratay@mera.ru>
2017-04-05 18:16:32 +0300

    CXO-5873/CXO-5986: Validation/estimation of data from historical/future metrology configurations

--- copl/com.elster.jupiter.transaction/src/main/java/com/elster/jupiter/transaction/impl/TransactionModule.java
+++ copl/com.elster.jupiter.transaction/src/main/java/com/elster/jupiter/transaction/impl/TransactionModule.java
@@ -7,24 +7,30 @@ package com.elster.jupiter.transaction.impl;
 import com.elster.jupiter.bootstrap.BootstrapService;
 import com.elster.jupiter.pubsub.Publisher;
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
+import com.elster.jupiter.transaction.Transaction;
+import com.elster.jupiter.transaction.TransactionBuilder;
+import com.elster.jupiter.transaction.TransactionContext;
+import com.elster.jupiter.transaction.TransactionEvent;
 import com.elster.jupiter.transaction.TransactionService;
+import com.elster.jupiter.util.time.StopWatch;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 import com.google.inject.name.Names;
 
 import javax.sql.DataSource;
 
-public class TransactionModule extends AbstractModule  {
-	
-	private final boolean printSql;
-	
-	public TransactionModule() {
-		this(false);
-	}
-	
-	public TransactionModule(boolean printSql) {
-		this.printSql = printSql;
-	}
+public class TransactionModule extends AbstractModule {
+
+    private final boolean printSql;
+
+    public TransactionModule() {
+        this(false);
+    }
+
+    public TransactionModule(boolean printSql) {
+        this.printSql = printSql;
+    }
 
     @Override
     protected void configure() {
@@ -36,4 +42,50 @@ public class TransactionModule extends AbstractModule  {
         bind(DataSource.class).to(TransactionalDataSource.class).in(Scopes.SINGLETON);
     }
 
+    public enum FakeTransactionService implements TransactionService {
+        INSTANCE;
+
+        @Override
+        public <T> T execute(Transaction<T> transaction) {
+            return transaction.perform();
+        }
+
+        @Override
+        public TransactionEvent run(Runnable transaction) {
+            transaction.run();
+            return new TransactionEvent(true, new StopWatch(false), 0, 0);
+        }
+
+        @Override
+        public TransactionContext getContext() {
+            return new FakeTransactionContext();
+        }
+
+        @Override
+        public TransactionBuilder builder() {
+            return null;
+        }
+
+        @Override
+        public boolean isInTransaction() {
+            return false;
+        }
+
+        private class FakeTransactionContext implements TransactionContext {
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public void commit() {
+
+            }
+
+            @Override
+            public TransactionEvent getStats() {
+                return new TransactionEvent(false, new StopWatch(false), 0, 0);
+            }
+        }
+    }
 }
