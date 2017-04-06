ab2801879c8a485d057c796287f8f3911e7d91aa
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2017-04-06 14:37:33 +0200

    CXO-3603: Added PUT method for SecurityAccessors on keys

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/pki/SymmetricKeyAccessorImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/pki/SymmetricKeyAccessorImpl.java
@@ -48,6 +48,9 @@ public class SymmetricKeyAccessorImpl extends AbstractKeyAccessorImpl<SymmetricK
 
     @Override
     public Optional<SymmetricKeyWrapper> getTempValue() {
+        if (tempSymmetricKeyWrapperReference==null) {
+            return Optional.empty();
+        }
         return (Optional<SymmetricKeyWrapper>) tempSymmetricKeyWrapperReference.getOptional();
     }
 
