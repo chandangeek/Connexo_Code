4f1572c62c4d3952e37cf879ebb905893069b8a4
Dragos Sinca <dragos.sinca@honeywell.com>
2017-04-14 07:43:05 +0300

    Fixed unit tests

--- comu/com.energyict.mdc.scheduling/src/test/java/com/energyict/mdc/scheduling/model/impl/InMemoryPersistence.java
+++ comu/com.energyict.mdc.scheduling/src/test/java/com/energyict/mdc/scheduling/model/impl/InMemoryPersistence.java
@@ -5,6 +5,7 @@
 package com.energyict.mdc.scheduling.model.impl;
 
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
+import com.elster.jupiter.bpm.impl.BpmModule;
 import com.elster.jupiter.calendar.impl.CalendarModule;
 import com.elster.jupiter.cps.CustomPropertySetService;
 import com.elster.jupiter.cps.impl.CustomPropertySetsModule;
@@ -100,6 +101,7 @@ public class InMemoryPersistence {
                 new DomainUtilModule(),
                 new PartyModule(),
                 new UserModule(),
+                new BpmModule(),
                 new IdsModule(),
                 new FiniteStateMachineModule(),
                 new UsagePointLifeCycleConfigurationModule(),
