644f9858c98b85b1fa2d451779fcf298be3dd24b
pdo <pasquien.dorny@elster.com>
2017-04-14 13:45:30 +0200

    CXO-5359: Failing tests due to merging master in this branch

--- comu/com.energyict.mdc.device.lifecycle.config.rest/src/test/java/com/energyict/mdc/device/lifecycle/config/rest/impl/resource/InMemoryPersistence.java
+++ comu/com.energyict.mdc.device.lifecycle.config.rest/src/test/java/com/energyict/mdc/device/lifecycle/config/rest/impl/resource/InMemoryPersistence.java
@@ -58,6 +58,8 @@ import com.energyict.mdc.device.lifecycle.config.DeviceLifeCycleConfigurationSer
 import com.energyict.mdc.device.lifecycle.config.impl.DeviceLifeCycleConfigurationModule;
 import com.energyict.mdc.device.lifecycle.impl.DeviceLifeCycleModule;
 import com.energyict.mdc.device.topology.TopologyService;
+import com.energyict.mdc.device.topology.impl.TopologyModule;
+import com.energyict.mdc.device.topology.multielement.MultiElementDeviceService;
 import com.energyict.mdc.engine.config.EngineConfigurationService;
 import com.energyict.mdc.protocol.api.device.messages.DeviceMessageSpecificationService;
 import com.energyict.mdc.protocol.pluggable.ProtocolPluggableService;
@@ -115,6 +117,7 @@ public class InMemoryPersistence {
     private IssueService issueService;
     private PropertySpecService propertySpecService;
     private TopologyService topologyService;
+    private MultiElementDeviceService multiElementDeviceService;
     private DataVaultService dataVaultService;
     private CustomPropertySetService customPropertySetService;
     private LicenseService licenseService;
@@ -212,6 +215,7 @@ public class InMemoryPersistence {
         this.issueService = mock(IssueService.class);
         this.propertySpecService = mock(PropertySpecService.class);
         this.topologyService = mock(TopologyService.class);
+        this.multiElementDeviceService = mock(MultiElementDeviceService.class);
         this.dataVaultService = mock(DataVaultService.class);
         this.licenseService = mock(LicenseService.class);
         this.searchService = mock(SearchService.class);
@@ -249,6 +253,7 @@ public class InMemoryPersistence {
             bind(IssueService.class).toInstance(issueService);
             bind(PropertySpecService.class).toInstance(propertySpecService);
             bind(TopologyService.class).toInstance(topologyService);
+            bind(MultiElementDeviceService.class).toInstance(multiElementDeviceService);
             bind(DataVaultService.class).toInstance(dataVaultService);
             bind(SearchService.class).toInstance(searchService);
             bind(TimeService.class).toInstance(timeService);
