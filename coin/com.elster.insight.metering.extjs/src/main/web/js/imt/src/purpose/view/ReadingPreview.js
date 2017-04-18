77fe4c49dcf51b4e53d7a428c7127dbd0050f7fe
Kirill Koshelev <kirkosh@mera.ru>
2017-04-18 14:33:53 +0300

    [CXO-6370] - correct values

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/ReadingPreview.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/ReadingPreview.js
@@ -139,7 +139,7 @@ Ext.define('Imt.purpose.view.ReadingPreview', {
         var icon;
 
         icon = '<span class="icon-flag5" style="margin-left:10px; color:#33CC33;" data-qtip="'
-            + Uni.I18n.translate('reading.estimated', 'IMT', 'Estimated in {0} on {1} at {2}', [
+            + Uni.I18n.translate('reading.estimatedWithTime', 'IMT', 'Estimated in {0} on {1} at {2}', [
                 estimatedByRule.application.name,
                 Uni.DateTime.formatDateLong(new Date(estimatedByRule.when)),
                 Uni.DateTime.formatTimeLong(new Date(estimatedByRule.when))
