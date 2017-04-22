c57bbe0dee1ac87ed681657ffc64e097c963842e
Valery Ruchin <varuchin@mera.ru>
2017-04-22 18:37:59 +0300

    [CXO-6346][CXO-6289][CXO-6370] Apply estimation comments during estimation of data; Manage estimation comment while editing channel data

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ValueCorrectionInfo.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/ValueCorrectionInfo.java
@@ -7,7 +7,6 @@ package com.elster.jupiter.mdm.usagepoint.data.rest.impl;
 import com.elster.jupiter.mdm.common.rest.IntervalInfo;
 import com.elster.jupiter.rest.util.BigDecimalFunction;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
@@ -16,7 +15,7 @@ import java.util.List;
 public class ValueCorrectionInfo {
     @JsonProperty("amount")
     public BigDecimal amount;
-    @JsonIgnore(true)
+    @JsonProperty("commentId")
     public long commentId;
     @JsonProperty("intervals")
     public List<IntervalInfo> intervals;
