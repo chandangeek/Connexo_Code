696deabc4caf2d3161de01625a90afe22d4c16a5
Anton Fomchenko <antfom@mera.ru>
2017-04-24 16:22:54 +0300

    CXO-6491 Specify list of usage point life cycle states on link between metrology configuration and validation rule sets

---
+++ coin/com.elster.insight.usagepoint.config.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/config/rest/impl/MetrologyContractRuleSetUsageInfo.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.mdm.usagepoint.config.rest.impl;
+
+import com.elster.jupiter.usagepoint.lifecycle.rest.UsagePointLifeCycleStateInfo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MetrologyContractRuleSetUsageInfo {
+    public long total;
+    public List<LinkableMetrologyContractInfo> contracts;
+}
