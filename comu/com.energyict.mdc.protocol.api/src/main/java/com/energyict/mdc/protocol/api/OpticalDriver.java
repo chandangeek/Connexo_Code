4298c32f5f13d9c05454b005442cbc5ff1dd07a5
khe <kristof.hennebel@elster.com>
2017-04-24 15:24:26 +0200

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
    Further refactored mdc.protocols.api module. Removing/replacing every class that has an UPL 'alternative'

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/OpticalDriver.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
- */
-
-package com.energyict.mdc.protocol.api;
-
-public interface OpticalDriver {
-}
--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/legacy/SmartMeterProtocol.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/legacy/SmartMeterProtocol.java
@@ -4,147 +4,17 @@
 
 package com.energyict.mdc.protocol.api.legacy;
 
-import com.energyict.mdc.protocol.api.InvalidPropertyException;
-import com.energyict.mdc.protocol.api.MissingPropertyException;
-import com.energyict.mdc.protocol.api.UnsupportedException;
 import com.energyict.mdc.protocol.api.legacy.dynamic.Pluggable;
-import com.energyict.mdc.upl.DeviceDescriptionSupport;
 import com.energyict.mdc.upl.meterdata.BreakerStatus;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Date;
 import java.util.Optional;
-import java.util.TimeZone;
-import java.util.logging.Logger;
 
 /**
  * SmartMeterProtocol is an extension to the standard {@link MeterProtocol} interface.
  * The basic idea is to do more bulk request and adjust our framework to the current smarter meter market.
  */
-public interface SmartMeterProtocol extends Pluggable, MultipleLoadProfileSupport, BulkRegisterProtocol, MeterProtocolEventSupport, CachingProtocol, DeviceDescriptionSupport {
-
-    /**
-     * The string typically used for the device address property
-     */
-    String ADDRESS = "DeviceId";
-
-    /**
-     * The string used for the protocol password property
-     */
-    String PASSWORD = "Password";
-
-    /**
-     * The string used for the protocol ProfileInterval property
-     */
-    String PROFILEINTERVAL = "ProfileInterval";
-
-    /**
-     * The string used for the serialNumber property
-     */
-    String SERIALNUMBER = "SerialNumber";
-
-    /**
-     * The string used for the nodeId property
-     */
-    String NODEID = "NodeAddress";
-
-    /**
-     * The string used for the roundtripCorrection property.
-     * This property is used by the getTime() and setTime() method
-     * to correct the communication roundtrip.
-     */
-    String ROUNDTRIPCORR = "RoundtripCorrection";
-
-    /**
-     * The string used for the correctTime property.
-     * The property is used only by the protocoltester software.
-     */
-    String CORRECTTIME = "CorrectTime";
-
-    /**
-     * The string used for the protocol classname property
-     */
-    String PROTOCOL = "ProtocolReader";
-
-    /**
-     * Defines the number of retries a certain communicationRequest may be retried
-     */
-    String RETRIES = "Retries";
-
-    /**
-     * <p>
-     * Validates the protocol specific properties.
-     * The implementer has to specify which keys are mandatory,
-     * </p><p>
-     *
-     * @throws InvalidPropertyException if a property value is not compatible with the device type
-     * @throws MissingPropertyException if a required property is not present
-     */
-    void validateProperties() throws InvalidPropertyException, MissingPropertyException;
-
-    /**
-     * <p>
-     * Initializes the MeterProtocol.
-     * </p><p>
-     * Implementers should save the arguments for future use.
-     * </p><p>
-     * All times exchanged between the data collection system and a MeterProtocol are java.util.Date ,
-     * expressed in milliseconds since 1/1/1970 in UTC. The implementer has
-     * to convert the device times to UTC. </p><p>
-     * The timeZone argument is the timezone that is configured in the collecting system
-     * for the device. If the device knows its own timezone, this argument can be ignored </p><p>
-     * Implementers can use the argument to convert from device
-     * time format to java.util.Date, e.g.</p>
-     * <PRE>
-     * Calendar deviceCalendar = Calendar.getInstance(timeZone);
-     * deviceCalendar.clear();
-     * deviceCalendar.set(year, month - 1 , day , hour , minute , second);
-     * java.util.Date deviceDate = deviceCalendar.getTime();
-     * </PRE>
-     * <p>
-     * The last argument is used to inform the data collection system of problems and/or progress.
-     * Messages with level INFO or above are logged to the collection system's
-     * logbook. Messages with level below INFO are only displayed in diagnostic mode
-     * </p>
-     *
-     * @param inputStream  byte stream to read data from the device
-     * @param outputStream byte stream to send data to the device
-     * @param timeZone     the device's timezone
-     * @param logger       used to provide feedback to the collection system
-     * @throws IOException Thrown when an exception happens
-     */
-    void init(InputStream inputStream, OutputStream outputStream, TimeZone timeZone, Logger logger) throws IOException;
-
-    /**
-     * <p>
-     * Sets up the logical connection with the device.
-     * </p><p>
-     * As the physical connection has already been setup by the collection system,
-     * it is up to the implementer to decide if any additional implementation is needed
-     * </p>
-     *
-     * @throws IOException <br>
-     */
-    void connect() throws IOException;
-
-    /**
-     * Terminates the logical connection with the device.
-     * The implementer should not close the inputStream and outputStream. This
-     * is the responsibility of the collection system
-     *
-     * @throws IOException thrown in case of an exception
-     */
-    void disconnect() throws IOException;
-
-    /**
-     * Get the firmware version of the meter
-     *
-     * @return the version of the meter firmware
-     * @throws IOException Thrown in case of an exception
-     */
-    String getFirmwareVersion() throws IOException;
+public interface SmartMeterProtocol extends Pluggable, com.energyict.mdc.upl.SmartMeterProtocol {
 
     /**
      * Gets the current status of the breaker <br/>
@@ -181,48 +51,4 @@ public interface SmartMeterProtocol extends Pluggable, MultipleLoadProfileSuppor
     default Optional<String> getPassiveCalendarName() throws IOException {
         return Optional.empty();
     }
-
-    /**
-     * Get the SerialNumber of the device
-     *
-     * @return the serialNumber of the device
-     * @throws IOException thrown in case of an exception
-     */
-    String getMeterSerialNumber() throws IOException;
-
-    /**
-     * @return the current device time
-     * @throws IOException <br>
-     */
-    Date getTime() throws IOException;
-
-    /**
-     * <p>
-     * sets the device time to the current system time.
-     * </p>
-     *
-     * @param newMeterTime the time to set in the meter
-     * @throws IOException Thrown in case of an exception
-     */
-    void setTime(Date newMeterTime) throws IOException;
-
-    /**
-     * <p>
-     * Initializes the device, typically clearing all profile data
-     * </p>
-     *
-     * @throws IOException          <br>
-     * @throws UnsupportedException if the device does not support this operation
-     */
-    void initializeDevice() throws IOException;
-
-    /**
-     * This method is called by the collection software before the disconnect()
-     * and can be used to free resources that cannot be freed in the disconnect()
-     * method.
-     *
-     * @throws IOException Thrown in case of an exception
-     */
-    void release() throws IOException;
-
 }
