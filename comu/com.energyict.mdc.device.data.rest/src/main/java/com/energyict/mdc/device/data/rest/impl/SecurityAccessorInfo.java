ca4da35cd4deebcc3dc57a354d6ede4a01fb79e6
Bart Vansegbroeck <bart.vansegbroeck@elster.com>
2017-04-25 11:13:40 +0200

    CXO-5839: Added PassphraseWrapper

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/SecurityAccessorInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/SecurityAccessorInfo.java
@@ -19,6 +19,8 @@ public class SecurityAccessorInfo {
     public Instant lastReadDate;
     public Instant modificationDate;
     public String status;
+    public Boolean hasTempValue;
+    public Boolean hasCompleteTempValue;
     public List<PropertyInfo> currentProperties;
     public List<PropertyInfo> tempProperties;
 }
