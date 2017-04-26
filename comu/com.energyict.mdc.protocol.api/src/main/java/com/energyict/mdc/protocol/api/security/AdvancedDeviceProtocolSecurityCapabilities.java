20e2629f410f6eb73bebd141c133195c6ebda87c
khe <kristof.hennebel@elster.com>
2017-04-26 10:53:09 +0200

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
     - Added adapters for the advanced security levels. These should be used by the CXO business code before using an UPL advanced security level. Adaption is available on the ProtocolPluggableService

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/security/AdvancedDeviceProtocolSecurityCapabilities.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/security/AdvancedDeviceProtocolSecurityCapabilities.java
@@ -4,25 +4,6 @@
 
 package com.energyict.mdc.protocol.api.security;
 
-import java.util.List;
-
-public interface AdvancedDeviceProtocolSecurityCapabilities extends DeviceProtocolSecurityCapabilities {
-
-    /**
-     * A list of all supported security suites
-     */
-    List<SecuritySuite> getSecuritySuites();
-
-    /**
-     * A list of all supported security levels for requests.
-     * Note that not all these levels are supported in all security suites.
-     */
-    List<RequestSecurityLevel> getRequestSecurityLevels();
-
-    /**
-     * A list of all supported security levels for responses
-     * Note that not all these levels are supported in all security suites.
-     */
-    List<ResponseSecurityLevel> getResponseSecurityLevels();
+public interface AdvancedDeviceProtocolSecurityCapabilities extends DeviceProtocolSecurityCapabilities, com.energyict.mdc.upl.security.AdvancedDeviceProtocolSecurityCapabilities {
 
 }
