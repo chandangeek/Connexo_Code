babab264c1ea8c49c56d5b3db31ef63098c101f8
Denis Antonov <dantonov@mera.ru>
2017-04-27 11:49:10 +0300

    refactor reading type property (with factory and converter) to be used in estimators and validators. CXO-6185, CXO-6187

--- coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/ReferenceSubstitutionEstimator.java
+++ coko/com.elster.jupiter.estimators/src/main/java/com/elster/jupiter/estimators/impl/ReferenceSubstitutionEstimator.java
@@ -5,13 +5,18 @@
 package com.elster.jupiter.estimators.impl;
 
 import com.elster.jupiter.estimation.EstimationPropertyDefinitionLevel;
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.ReadingTypeValueFactory;
+import com.elster.jupiter.metering.UsagePoint;
 import com.elster.jupiter.metering.config.MetrologyConfigurationService;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.nls.TranslationKey;
+import com.elster.jupiter.properties.PropertySelectionMode;
 import com.elster.jupiter.properties.PropertySpec;
 import com.elster.jupiter.properties.PropertySpecService;
 import com.elster.jupiter.validation.ValidationService;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -22,12 +27,19 @@ import java.util.Map;
  */
 public class ReferenceSubstitutionEstimator extends AbstractMainCheckEstimator {
 
-    ReferenceSubstitutionEstimator(Thesaurus thesaurus, MetrologyConfigurationService metrologyConfigurationService, ValidationService validationService, PropertySpecService propertySpecService) {
+    static final String CHECK_USAGE_POINT = TranslationKeys.CHECK_USAGE_POINT.getKey();
+    static final String CHECK_READING_TYPE = TranslationKeys.CHECK_READING_TYPE.getKey();
+
+    private MeteringService meteringService;
+
+    ReferenceSubstitutionEstimator(Thesaurus thesaurus, MetrologyConfigurationService metrologyConfigurationService, ValidationService validationService, PropertySpecService propertySpecService, MeteringService meteringService) {
         super(thesaurus, metrologyConfigurationService, validationService, propertySpecService);
+        this.meteringService = meteringService;
     }
 
-    ReferenceSubstitutionEstimator(Thesaurus thesaurus, MetrologyConfigurationService metrologyConfigurationService, ValidationService validationService, PropertySpecService propertySpecService, Map<String, Object> properties) {
+    ReferenceSubstitutionEstimator(Thesaurus thesaurus, MetrologyConfigurationService metrologyConfigurationService, ValidationService validationService, PropertySpecService propertySpecService, MeteringService meteringService, Map<String, Object> properties) {
         super(thesaurus, metrologyConfigurationService, validationService, propertySpecService, properties);
+        this.meteringService = meteringService;
     }
 
     @Override
@@ -48,6 +60,32 @@ public class ReferenceSubstitutionEstimator extends AbstractMainCheckEstimator {
     }
 
     @Override
+    public List<PropertySpec> getPropertySpecs() {
+        return Arrays.asList(buildCheckPurposePropertySpec(),buildReferenceUsagePointPropertySpec(),buildReferenceReadingTypePropertySpec());
+    }
+
+    private PropertySpec buildReferenceUsagePointPropertySpec() {
+        return getPropertySpecService()
+                .referenceSpec(UsagePoint.class)
+                .named(TranslationKeys.CHECK_USAGE_POINT)
+                .describedAs(TranslationKeys.CHECK_USAGE_POINT_DESCRIPTION)
+                .fromThesaurus(this.getThesaurus())
+                .markRequired()
+                .markExhaustive(PropertySelectionMode.COMBOBOX)
+                .finish();
+    }
+
+    private PropertySpec buildReferenceReadingTypePropertySpec() {
+        return getPropertySpecService()
+                .specForValuesOf(new ReadingTypeValueFactory(meteringService, ReadingTypeValueFactory.Mode.ONLY_REGULAR))
+                .named(TranslationKeys.CHECK_READING_TYPE)
+                .describedAs(TranslationKeys.CHECK_READING_TYPE_DESCRIPTION)
+                .fromThesaurus(getThesaurus())
+                .markRequired()
+                .finish();
+    }
+
+    @Override
     public String getDefaultFormat() {
         return TranslationKeys.ESTIMATOR_NAME.getDefaultFormat();
     }
@@ -55,8 +93,10 @@ public class ReferenceSubstitutionEstimator extends AbstractMainCheckEstimator {
     public enum TranslationKeys implements TranslationKey {
         ESTIMATOR_NAME(ReferenceSubstitutionEstimator.class.getName(), "Reference substitution"),
 
-        CHECK_PURPOSE("maincheck.purpose", "Check purpose"),
-        CHECK_PURPOSE_DESCRIPTION("maincheck.purpose.description", "Check purpose");
+        CHECK_USAGE_POINT("reference.check.usagepoint", "Check usage point"),
+        CHECK_USAGE_POINT_DESCRIPTION("reference.check.usagepoint.description", "Check usage point"),
+        CHECK_READING_TYPE("reference.check.readingtype", "Check reading type"),
+        CHECK_READING_TYPE_DESCRIPTION("reference.check.readingtype.description", "Check reading type");
 
         private final String key;
         private final String defaultFormat;
