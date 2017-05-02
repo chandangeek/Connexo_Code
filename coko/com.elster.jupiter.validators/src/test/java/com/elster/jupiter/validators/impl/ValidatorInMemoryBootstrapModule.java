52a28cec03479d44f7a11d8f37be34850ffe1820
Lobanov Dmitry <dlobanov@mera.ru>
2017-05-02 14:36:53 +0300

    Add missing dependencies

--- coko/com.elster.jupiter.validators/src/test/java/com/elster/jupiter/validators/impl/ValidatorInMemoryBootstrapModule.java
+++ coko/com.elster.jupiter.validators/src/test/java/com/elster/jupiter/validators/impl/ValidatorInMemoryBootstrapModule.java
@@ -26,6 +26,7 @@ import com.elster.jupiter.pubsub.impl.PubSubModule;
 import com.elster.jupiter.search.impl.SearchModule;
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
 import com.elster.jupiter.security.thread.impl.ThreadSecurityModule;
+import com.elster.jupiter.tasks.impl.TaskModule;
 import com.elster.jupiter.time.impl.TimeModule;
 import com.elster.jupiter.transaction.TransactionContext;
 import com.elster.jupiter.transaction.TransactionService;
@@ -90,7 +91,8 @@ public class ValidatorInMemoryBootstrapModule {
                 new CalendarModule(),
                 new SearchModule(),
                 new CustomPropertySetsModule(),
-                new UsagePointLifeCycleConfigurationModule()
+                new UsagePointLifeCycleConfigurationModule(),
+                new TaskModule()
         );
         try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext()) {
             injector.getInstance(ThreadPrincipalService.class);
