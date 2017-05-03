f3870e244c8893aaaf697a6abae43914c50f9097
Anton Fomchenko <antfom@mera.ru>
2017-05-03 18:17:37 +0300

    CXO-6492 validate data taking into account usage point life cycle states

--- coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractValidationRuleSetResolver.java
+++ coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractValidationRuleSetResolver.java
@@ -5,46 +5,99 @@
 package com.elster.jupiter.mdm.usagepoint.data.impl;
 
 import com.elster.jupiter.mdm.usagepoint.config.UsagePointConfigurationService;
+import com.elster.jupiter.metering.UsagePoint;
 import com.elster.jupiter.metering.config.EffectiveMetrologyConfigurationOnUsagePoint;
+import com.elster.jupiter.metering.config.MetrologyContract;
 import com.elster.jupiter.metering.config.UsagePointMetrologyConfiguration;
-import com.elster.jupiter.util.streams.DecoratedStream;
+import com.elster.jupiter.usagepoint.lifecycle.UsagePointLifeCycleService;
+import com.elster.jupiter.usagepoint.lifecycle.UsagePointStateChangeRequest;
 import com.elster.jupiter.validation.ValidationContext;
 import com.elster.jupiter.validation.ValidationRuleSet;
 import com.elster.jupiter.validation.ValidationRuleSetResolver;
 
+import com.google.common.collect.Range;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component(name = "com.elster.jupiter.mdm.usagepoint.data.MetrologyContractValidationRuleSetResolver", service = ValidationRuleSetResolver.class, immediate = true)
 public class MetrologyContractValidationRuleSetResolver implements ValidationRuleSetResolver {
     private volatile UsagePointConfigurationService usagePointConfigurationService;
+    private volatile UsagePointLifeCycleService usagePointLifeCycleService;
 
     @Reference
     public void setUsagePointConfigurationService(UsagePointConfigurationService usagePointConfigurationService) {
         this.usagePointConfigurationService = usagePointConfigurationService;
     }
 
+    @Reference
+    public void setUsagePointLifeCycleService(UsagePointLifeCycleService usagePointLifeCycleService) {
+        this.usagePointLifeCycleService = usagePointLifeCycleService;
+    }
+
     @Override
-    public List<ValidationRuleSet> resolve(ValidationContext validationContext) {
-        if (validationContext.getMetrologyContract().isPresent()) {
-            return this.usagePointConfigurationService.getValidationRuleSets(validationContext.getMetrologyContract().get());
+    public Map<ValidationRuleSet, List<Range<Instant>>> resolve(ValidationContext validationContext) {
+        Map<ValidationRuleSet, List<Range<Instant>>> result = new HashMap<>();
+
+        if (validationContext.getMetrologyContract().isPresent() && validationContext.getUsagePoint().isPresent()) {
+            return getRuleSets(validationContext.getUsagePoint().get(), Collections.singletonList(validationContext.getMetrologyContract().get()));
         }
         if (validationContext.getChannelsContainer().getUsagePoint().isPresent()) {
-            Optional<UsagePointMetrologyConfiguration> metrologyConfiguration = validationContext.getChannelsContainer().getUsagePoint().get().getCurrentEffectiveMetrologyConfiguration()
+            Optional<UsagePointMetrologyConfiguration> metrologyConfiguration = validationContext.getChannelsContainer()
+                    .getUsagePoint()
+                    .get()
+                    .getCurrentEffectiveMetrologyConfiguration()
                     .map(EffectiveMetrologyConfigurationOnUsagePoint::getMetrologyConfiguration);
-            if (metrologyConfiguration.isPresent()){
-                return DecoratedStream.decorate(metrologyConfiguration.get().getContracts().stream())
-                        .flatMap(contract -> this.usagePointConfigurationService.getValidationRuleSets(contract).stream())
-                        .distinct(ValidationRuleSet::getId)
-                        .collect(Collectors.toList());
+            if (metrologyConfiguration.isPresent()) {
+                return getRuleSets(validationContext.getChannelsContainer().getUsagePoint().get(), metrologyConfiguration.get().getContracts());
             }
         }
-        return Collections.emptyList();
+        return result;
+    }
+
+    private Map<ValidationRuleSet, List<Range<Instant>>> getRuleSets(UsagePoint usagePoint, List<MetrologyContract> metrologyContracts) {
+        Map<ValidationRuleSet, List<Range<Instant>>> result = new HashMap<>();
+        List<Instant> stateChanges = getUsagePointStateChangeRequests(usagePoint).stream()
+                .map(UsagePointStateChangeRequest::getTransitionTime)
+                .sorted(Instant::compareTo)
+                .collect(Collectors.toList());
+        for (int i = 0; i < stateChanges.size(); i++) {
+            Range<Instant> range;
+            if (i + 1 < stateChanges.size()) {
+                range = Range.openClosed(stateChanges.get(i), stateChanges.get(i + 1));
+            } else {
+                range = Range.atLeast(stateChanges.get(i));
+            }
+            metrologyContracts.stream().forEach(metrologyContract ->
+                    this.usagePointConfigurationService.getValidationRuleSets(metrologyContract,
+                            usagePoint.getState(range.lowerEndpoint()))
+                            .stream()
+                            .forEach(e -> {
+                                if (result.containsKey(e)) {
+                                    result.get(e).add(range);
+                                } else {
+                                    result.put(e, new ArrayList<>(Collections.singletonList(range)));
+                                }
+                            })
+            );
+        }
+        return result;
+    }
+
+    private List<UsagePointStateChangeRequest> getUsagePointStateChangeRequests(UsagePoint usagePoint) {
+        return usagePointLifeCycleService.getHistory(usagePoint)
+                .stream()
+                .filter(usagePointStateChangeRequest -> usagePointStateChangeRequest.getStatus().equals(UsagePointStateChangeRequest.Status.COMPLETED))
+                .sorted((a, b) -> a.getTransitionTime().compareTo(b.getTransitionTime()))
+                .collect(Collectors.toList());
     }
 
     @Override
--- coin/com.elster.insight.usagepoint.data/src/test/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractValidationRuleSetResolverTest.java
+++ coin/com.elster.insight.usagepoint.data/src/test/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractValidationRuleSetResolverTest.java
@@ -4,14 +4,23 @@
 
 package com.elster.jupiter.mdm.usagepoint.data.impl;
 
+import com.elster.jupiter.fsm.State;
 import com.elster.jupiter.mdm.usagepoint.config.UsagePointConfigurationService;
 import com.elster.jupiter.metering.ChannelsContainer;
+import com.elster.jupiter.metering.UsagePoint;
 import com.elster.jupiter.metering.config.MetrologyContract;
+import com.elster.jupiter.usagepoint.lifecycle.UsagePointLifeCycleService;
+import com.elster.jupiter.usagepoint.lifecycle.UsagePointStateChangeRequest;
 import com.elster.jupiter.validation.ValidationContext;
 import com.elster.jupiter.validation.ValidationRuleSet;
 
+import com.google.common.collect.Range;
+
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -26,12 +35,21 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class MetrologyContractValidationRuleSetResolverTest {
 
+
+    private final Instant stateTransition1 = Instant.ofEpochMilli(1491228423000L);
+    private final Instant stateTransition2 = Instant.ofEpochMilli(1492697223000L);
+    private final Instant stateTransition3 = Instant.ofEpochMilli(1494425223000L);
+
     @Mock
     private UsagePointConfigurationService usagePointConfigurationService;
+
+    @Mock
+    private UsagePointLifeCycleService usagePointLifeCycleService;
 
     private MetrologyContractValidationRuleSetResolver testInstance() {
         MetrologyContractValidationRuleSetResolver ruleSetResolver = new MetrologyContractValidationRuleSetResolver();
         ruleSetResolver.setUsagePointConfigurationService(usagePointConfigurationService);
+        ruleSetResolver.setUsagePointLifeCycleService(usagePointLifeCycleService);
         return ruleSetResolver;
     }
 
@@ -42,7 +60,7 @@ public class MetrologyContractValidationRuleSetResolverTest {
         when(validationContext.getChannelsContainer()).thenReturn(channelsContainer);
         when(validationContext.getMetrologyContract()).thenReturn(Optional.empty());
         when(validationContext.getChannelsContainer().getUsagePoint()).thenReturn(Optional.empty());
-        List<ValidationRuleSet> validationRuleSets = testInstance().resolve(validationContext);
+        Map<ValidationRuleSet, List<Range<Instant>>> validationRuleSets = testInstance().resolve(validationContext);
 
         assertThat(validationRuleSets).isEmpty();
     }
@@ -51,15 +69,45 @@ public class MetrologyContractValidationRuleSetResolverTest {
     public void testCorrectRuleSetsForMetrologyContractChannelsContainer() {
         MetrologyContract metrologyContract = mock(MetrologyContract.class);
         ChannelsContainer channelsContainer = mock(ChannelsContainer.class);
+        UsagePoint usagePoint = mock(UsagePoint.class);
+        UsagePointStateChangeRequest stateChangeRequest1 = mock(UsagePointStateChangeRequest.class);
+        when(stateChangeRequest1.getStatus()).thenReturn(UsagePointStateChangeRequest.Status.COMPLETED);
+        when(stateChangeRequest1.getTransitionTime()).thenReturn(stateTransition1);
+        UsagePointStateChangeRequest stateChangeRequest2 = mock(UsagePointStateChangeRequest.class);
+        when(stateChangeRequest2.getStatus()).thenReturn(UsagePointStateChangeRequest.Status.COMPLETED);
+        when(stateChangeRequest2.getTransitionTime()).thenReturn(stateTransition2);
+        UsagePointStateChangeRequest stateChangeRequest3 = mock(UsagePointStateChangeRequest.class);
+        when(stateChangeRequest3.getStatus()).thenReturn(UsagePointStateChangeRequest.Status.SCHEDULED);
+        when(stateChangeRequest3.getTransitionTime()).thenReturn(stateTransition3);
+        when(usagePointLifeCycleService.getHistory(usagePoint)).thenReturn(Arrays.asList(stateChangeRequest1, stateChangeRequest2));
+        State state1 = mock(State.class);
+        when(usagePoint.getState(stateTransition1)).thenReturn(state1);
+        State state2 = mock(State.class);
+        when(usagePoint.getState(stateTransition2)).thenReturn(state2);
+        State state3 = mock(State.class);
+        when(usagePoint.getState(stateTransition3)).thenReturn(state3);
         ValidationContext validationContext = mock(ValidationContext.class);
         when(validationContext.getChannelsContainer()).thenReturn(channelsContainer);
         when(validationContext.getMetrologyContract()).thenReturn(Optional.of(metrologyContract));
+        when(validationContext.getUsagePoint()).thenReturn(Optional.of(usagePoint));
 
-        List<ValidationRuleSet> validationRuleSets = Collections.singletonList(mock(ValidationRuleSet.class));
-        when(usagePointConfigurationService.getValidationRuleSets(metrologyContract)).thenReturn(validationRuleSets);
+        ValidationRuleSet validationRuleSet1 = mock(ValidationRuleSet.class);
 
-        List<ValidationRuleSet> resolvedValidationRuleSets = testInstance().resolve(validationContext);
+        ValidationRuleSet validationRuleSet2 = mock(ValidationRuleSet.class);
+
+
+        List<ValidationRuleSet> validationRuleSets1 = Collections.singletonList(validationRuleSet1);
+        when(usagePointConfigurationService.getValidationRuleSets(metrologyContract, state1)).thenReturn(validationRuleSets1);
+
+        List<ValidationRuleSet> validationRuleSets2 = Collections.singletonList(validationRuleSet2);
+        when(usagePointConfigurationService.getValidationRuleSets(metrologyContract, state2)).thenReturn(validationRuleSets2);
+
+        Map<ValidationRuleSet, List<Range<Instant>>> resolvedValidationRuleSets = testInstance().resolve(validationContext);
 
         assertThat(resolvedValidationRuleSets).isNotEmpty();
+        assertThat(resolvedValidationRuleSets).hasSize(2);
+        assertThat(resolvedValidationRuleSets).containsKeys(validationRuleSet1, validationRuleSet2);
+        assertThat(resolvedValidationRuleSets.get(validationRuleSet1)).containsExactlyElementsOf(Collections.singletonList(Range.openClosed(stateTransition1, stateTransition2)));
+        assertThat(resolvedValidationRuleSets.get(validationRuleSet2)).containsExactlyElementsOf(Collections.singletonList(Range.atLeast(stateTransition2)));
     }
 }
