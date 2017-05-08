35c9369f4b47205b1c8184c2dc1d44daadad4f62
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-05-08 15:24:53 +0200

    CXO-6678 Security set - Add 'attributes' Added client support to all  SecuritySupport implementations.

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/security/DeviceProtocolSecurityCapabilities.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/security/DeviceProtocolSecurityCapabilities.java
@@ -4,12 +4,7 @@
 
 package com.energyict.mdc.protocol.api.security;
 
-import com.elster.jupiter.cps.CustomPropertySet;
-import com.elster.jupiter.cps.PersistentDomainExtension;
 import com.energyict.mdc.protocol.api.DeviceProtocol;
-import com.energyict.mdc.upl.meterdata.Device;
-
-import java.util.Optional;
 
 /**
  * Provides functionality to expose a {@link DeviceProtocol DeviceProtocol}
@@ -21,33 +16,4 @@ import java.util.Optional;
  */
 public interface DeviceProtocolSecurityCapabilities extends com.energyict.mdc.upl.security.DeviceProtocolSecurityCapabilities {
 
-    /**
-     * Returns the {@link CustomPropertySet} that provides the storage area
-     * for the properties of a {@link Device} that has these security capabilities
-     * or an empty Optional if there are no properties.
-     * In that case, {@link #getSecurityProperties()} should return
-     * an empty collection as well for consistency.
-     * <p>
-     * Note that none of the properties should be 'required'
-     * because it is possible that the communication
-     * expert configures the Device in such a way
-     * that not all of the properties are actually needed.
-     * As an example, say that the security capabilities
-     * involve the following set of properties:
-     * <ul>
-     * <li>clientId</li>
-     * <li>password</li>
-     * <li>authentication key</li>
-     * <li>encryption key</li>
-     * </ul>
-     * When the communication expert configures the Device
-     * to always use a clientId and an authentication key
-     * then the password and the encryption key are never used
-     * and can therefore never be required.
-     *
-     * @return The CustomPropertySet
-     */
-    default Optional<CustomPropertySet<Device, ? extends PersistentDomainExtension<Device>>> getCustomPropertySet() {
-        return Optional.empty(); // TODO: remove all usages
-    }
 }
