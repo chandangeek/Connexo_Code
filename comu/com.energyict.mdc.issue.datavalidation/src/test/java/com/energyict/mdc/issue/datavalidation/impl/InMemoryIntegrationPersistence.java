bb08d4921aced8f1b039ba0e69888a281496f93c
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-05-08 16:32:18 +0200

    merging issues

--- comu/com.energyict.mdc.issue.datavalidation/src/test/java/com/energyict/mdc/issue/datavalidation/impl/InMemoryIntegrationPersistence.java
+++ comu/com.energyict.mdc.issue.datavalidation/src/test/java/com/energyict/mdc/issue/datavalidation/impl/InMemoryIntegrationPersistence.java
@@ -160,8 +160,7 @@ public class InMemoryIntegrationPersistence {
                 StateTransitionTriggerEventTopicHandler stateTransitionTriggerEventTopicHandler = new StateTransitionTriggerEventTopicHandler(
                         this.injector.getInstance(EventService.class),
                         this.injector.getInstance(BpmService.class),
-                        this.injector.getInstance(StateTransitionPropertiesProvider.class),
-                        this.injector.getInstance(HttpAuthenticationService.class));
+                        this.injector.getInstance(StateTransitionPropertiesProvider.class));
                 ((EventServiceImpl) this.injector.getInstance(EventService.class)).addTopicHandler(stateTransitionTriggerEventTopicHandler);
                 com.elster.jupiter.metering.impl.StateTransitionChangeEventTopicHandler meteringTopicHandler =
                         new com.elster.jupiter.metering.impl.StateTransitionChangeEventTopicHandler(Clock.systemDefaultZone(),
