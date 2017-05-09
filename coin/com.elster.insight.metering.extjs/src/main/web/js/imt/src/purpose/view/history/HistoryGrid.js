434e21fe7fd51f499cb75251404dff6f56917c12
Valery Ruchin <varuchin@mera.ru>
2017-05-09 20:37:06 +0300

    [CXO-6380]

--- coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/history/HistoryGrid.js
+++ coin/com.elster.insight.metering.extjs/src/main/web/js/imt/src/purpose/view/history/HistoryGrid.js
@@ -82,7 +82,7 @@ Ext.define('Imt.purpose.view.history.HistoryGrid', {
         }
         if (!Ext.isEmpty(estimatedByRule)) {
             icon = '<span class="icon-flag5" style="margin-left:10px; position:absolute; color:#33CC33;" data-qtip="'
-                + Uni.I18n.translate('reading.estimated', 'IMT', 'Estimated in {0} at {1}', [
+                + Uni.I18n.translate('reading.estimated', 'IMT', 'Estimated on {0} at {1}', [
                     Uni.DateTime.formatDateLong(new Date(estimatedByRule.when)),
                     Uni.DateTime.formatTimeLong(new Date(estimatedByRule.when))
                 ], false) + '"></span>';
