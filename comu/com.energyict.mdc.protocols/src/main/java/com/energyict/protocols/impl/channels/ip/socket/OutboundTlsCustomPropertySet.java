cc99df4252e57cd423b31c98e9cd6b5b4002eb4d
khe <kristof.hennebel@elster.com>
2017-05-10 10:21:36 +0200

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
     - Rolled back the new X509Service
     This was a port from EISErver 9.1 to support persisted privatekeys and certificates but should be replaced by the implementation in the security feature (CXO-6659)

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocols/impl/channels/ip/socket/OutboundTlsCustomPropertySet.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocols/impl/channels/ip/socket/OutboundTlsCustomPropertySet.java
@@ -8,14 +8,12 @@ import com.elster.jupiter.cps.CustomPropertySet;
 import com.elster.jupiter.cps.PersistenceSupport;
 import com.elster.jupiter.nls.Thesaurus;
 import com.energyict.mdc.protocol.api.ConnectionProvider;
-import com.energyict.mdc.upl.crypto.KeyStoreService;
-import com.energyict.mdc.upl.crypto.X509Service;
 import com.energyict.mdc.upl.io.ConnectionType;
 import com.energyict.mdc.upl.nls.NlsService;
 import com.energyict.protocols.impl.channels.AbstractConnectionTypeCustomPropertySet;
 import com.energyict.protocols.impl.channels.CustomPropertySetTranslationKeys;
 import com.energyict.protocols.impl.channels.ip.OutboundIpConnectionProperties;
-import com.energyict.protocols.impl.channels.ip.OutboundIpConnectionPropertiesPersistenceSupport;
+import com.energyict.protocols.impl.channels.ip.OutboundTlsConnectionPropertiesPersistenceSupport;
 
 import javax.inject.Inject;
 
@@ -23,23 +21,19 @@ import javax.inject.Inject;
  * Provides an implementation for the {@link CustomPropertySet} interface
  * for the {@link com.energyict.mdc.channels.ip.socket.TLSConnectionType}.
  */
-public class OutboundTlsCustomPropertySet  extends AbstractConnectionTypeCustomPropertySet implements CustomPropertySet<ConnectionProvider, OutboundIpConnectionProperties> {
+public class OutboundTlsCustomPropertySet extends AbstractConnectionTypeCustomPropertySet implements CustomPropertySet<ConnectionProvider, OutboundIpConnectionProperties> {
 
     private final NlsService nlsService;
-    private final X509Service x509Service;
-    private final KeyStoreService keyStoreService;
 
     @Inject
-    public OutboundTlsCustomPropertySet(Thesaurus thesaurus, com.energyict.mdc.upl.properties.PropertySpecService propertySpecService, NlsService nlsService, X509Service x509Service, KeyStoreService keyStoreService) {
+    public OutboundTlsCustomPropertySet(Thesaurus thesaurus, com.energyict.mdc.upl.properties.PropertySpecService propertySpecService, NlsService nlsService) {
         super(thesaurus, propertySpecService);
         this.nlsService = nlsService;
-        this.x509Service = x509Service;
-        this.keyStoreService = keyStoreService;
     }
 
     @Override
     public ConnectionType getConnectionTypeSupport() {
-        return new com.energyict.mdc.channels.ip.socket.TLSConnectionType(propertySpecService, nlsService, x509Service, keyStoreService);
+        return new com.energyict.mdc.channels.ip.socket.TLSConnectionType(propertySpecService, nlsService, null, null);  //TODO adjust TLSConnectionType to use PKI stuff instead!
     }
 
     @Override
@@ -49,6 +43,6 @@ public class OutboundTlsCustomPropertySet  extends AbstractConnectionTypeCustomP
 
     @Override
     public PersistenceSupport<ConnectionProvider, OutboundIpConnectionProperties> getPersistenceSupport() {
-        return new OutboundIpConnectionPropertiesPersistenceSupport();
+        return new OutboundTlsConnectionPropertiesPersistenceSupport();
     }
 }
