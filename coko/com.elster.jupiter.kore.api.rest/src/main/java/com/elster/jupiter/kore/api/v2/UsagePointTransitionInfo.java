12f36aea2ba921545a836f843e2b122460520182
Anton Fomchenko <antfom@mera.ru>
2017-05-17 16:17:09 +0300

    CXO-6653, CXO-6655 fix transition default parameter

--- coko/com.elster.jupiter.kore.api.rest/src/main/java/com/elster/jupiter/kore/api/v2/UsagePointTransitionInfo.java
+++ coko/com.elster.jupiter.kore.api.rest/src/main/java/com/elster/jupiter/kore/api/v2/UsagePointTransitionInfo.java
@@ -11,5 +11,5 @@ import java.time.Instant;
 public class UsagePointTransitionInfo extends LinkInfo<Long> {
     public String name;
     public Instant effectiveTimestamp;
-    public Boolean transitionNow = true;
+    public Boolean transitionNow = false;
 }
