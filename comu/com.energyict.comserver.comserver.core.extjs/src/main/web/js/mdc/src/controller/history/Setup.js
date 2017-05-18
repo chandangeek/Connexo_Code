c480cbb5bc9ec948af8eacde58ece380e472811a
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-05-18 10:58:15 +0200

    CXO-6826 fix, faulty merge happened

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/history/Setup.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/history/Setup.js
@@ -1556,7 +1556,14 @@ Ext.define('Mdc.controller.history.Setup', {
                                                                 route: '{securitySettingId}/edit',
                                                                 privileges: Mdc.privileges.DeviceType.admin,
                                                                 controller: 'Mdc.controller.setup.SecuritySettings',
-                                                                action: 'showSecuritySettingsEditView'
+                                                                action: 'showSecuritySettingsEditView',
+                                                                callback: function (route) {
+                                                                    this.getApplication().on('loadSecuritySetting', function (record) {
+                                                                        route.setTitle(Uni.I18n.translate('general.editx', 'MDC', "Edit '{0}'", record.get('name'), false));
+                                                                        return true;
+                                                                    }, {single: true});
+                                                                    return this;
+                                                                }
                                                             }
                                                         }
                                                     },
