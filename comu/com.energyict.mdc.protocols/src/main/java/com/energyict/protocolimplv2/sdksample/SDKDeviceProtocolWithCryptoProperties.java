c86f65d6bd31b7fe471ec343f5f970b86c419e10
khe <kristof.hennebel@elster.com>
2017-05-31 16:18:28 +0200

    CXO-2951 - Integrate existing DeviceProtocol classes in Connexo code base
     - Renamed DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_LATER to FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_IMMEDIATE, because all the protocol impls for this message simply activate the FW immediately.

--- comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/sdksample/SDKDeviceProtocolWithCryptoProperties.java
+++ comu/com.energyict.mdc.protocols/src/main/java/com/energyict/protocolimplv2/sdksample/SDKDeviceProtocolWithCryptoProperties.java
@@ -255,7 +255,7 @@
 //                DeviceMessageId.CONTACTOR_CLOSE,
 //                DeviceMessageId.CONTACTOR_OPEN,
 //                DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_AND_ACTIVATE_DATE,
-//                DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_LATER,
+//                DeviceMessageId.FIRMWARE_UPGRADE_WITH_USER_FILE_ACTIVATE_IMMEDIATE,
 //                DeviceMessageId.FIRMWARE_UPGRADE_ACTIVATE,
 //                DeviceMessageId.FIRMWARE_UPGRADE_URL_AND_ACTIVATE_DATE);
 //    }
