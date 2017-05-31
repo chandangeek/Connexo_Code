d89a21be6f3fdbf5e79fbfdd556a07bdf53bb797
geert <geert.delmeiren@elster.com>
2017-05-31 11:14:36 +0200

    CXO-6712 - Subject order should be fixed (same on form and details) and correct

--- copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/view/AddCSRForm.js
+++ copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/view/AddCSRForm.js
@@ -76,89 +76,42 @@ Ext.define('Pkj.view.AddCSRForm', {
                 forceSelection: true
             },
             {
-                xtype: 'displayfield',
-                fieldLabel: Uni.I18n.translate('general.subject', 'PKJ', 'Subject'),
-                itemId: 'pkj-csr-add-form-subject',
-                renderer: function(value) {
-                    if (Ext.isEmpty(value)) {
-                        return Uni.I18n.translate('general.csrAttribute.subject.emptyText', 'PKJ', 'Fill in attributes below to preview subject');
-                    }
-                    return Ext.String.htmlEncode(value);
-                }
-            },
-            {
                 xtype: 'textfield',
                 fieldLabel: Uni.I18n.translate('general.csrAttribute.commonName', 'PKJ', 'Common name (CN)'),
                 required: true,
                 name: 'CN',
                 itemId: 'pkj-csr-add-form-common-name',
-                allowBlank: false,
-                listeners: {
-                    change: {
-                        fn: me.onFieldChange,
-                        scope: me
-                    }
-                }
+                allowBlank: false
             },
             {
                 xtype: 'textfield',
                 fieldLabel: Uni.I18n.translate('general.csrAttribute.organisationalUnit', 'PKJ', 'Organisational unit (OU)'),
                 name: 'OU',
-                itemId: 'pkj-csr-add-form-organisational-unit',
-                listeners: {
-                    change: {
-                        fn: me.onFieldChange,
-                        scope: me
-                    }
-                }
+                itemId: 'pkj-csr-add-form-organisational-unit'
             },
             {
                 xtype: 'textfield',
                 fieldLabel: Uni.I18n.translate('general.csrAttribute.organisation', 'PKJ', 'Organisation (O)'),
                 name: 'O',
-                itemId: 'pkj-csr-add-form-organisation',
-                listeners: {
-                    change: {
-                        fn: me.onFieldChange,
-                        scope: me
-                    }
-                }
+                itemId: 'pkj-csr-add-form-organisation'
             },
             {
                 xtype: 'textfield',
                 fieldLabel: Uni.I18n.translate('general.csrAttribute.locality', 'PKJ', 'Locality (L)'),
                 name: 'L',
-                itemId: 'pkj-csr-add-form-locality',
-                listeners: {
-                    change: {
-                        fn: me.onFieldChange,
-                        scope: me
-                    }
-                }
+                itemId: 'pkj-csr-add-form-locality'
             },
             {
                 xtype: 'textfield',
                 fieldLabel: Uni.I18n.translate('general.csrAttribute.stateOrProvince', 'PKJ', 'State or province (ST)'),
                 name: 'ST',
-                itemId: 'pkj-csr-add-form-state',
-                listeners: {
-                    change: {
-                        fn: me.onFieldChange,
-                        scope: me
-                    }
-                }
+                itemId: 'pkj-csr-add-form-state'
             },
             {
                 xtype: 'textfield',
                 fieldLabel: Uni.I18n.translate('general.csrAttribute.country', 'PKJ', 'Country (C)'),
                 name: 'C',
-                itemId: 'pkj-csr-add-form-country',
-                listeners: {
-                    change: {
-                        fn: me.onFieldChange,
-                        scope: me
-                    }
-                }
+                itemId: 'pkj-csr-add-form-country'
             },
             {
                 xtype: 'fieldcontainer',
@@ -188,45 +141,5 @@ Ext.define('Pkj.view.AddCSRForm', {
             }
         ];
         me.callParent(arguments);
-    },
-
-    onFieldChange: function(field) {
-        this.down('#pkj-csr-add-form-subject').setValue(this.constructSubject());
-    },
-
-    constructSubject: function() {
-        var me = this,
-            cnField = me.down('#pkj-csr-add-form-common-name'),
-            ouField = me.down('#pkj-csr-add-form-organisational-unit'),
-            oField = me.down('#pkj-csr-add-form-organisation'),
-            lField = me.down('#pkj-csr-add-form-locality'),
-            sField = me.down('#pkj-csr-add-form-state'),
-            cField = me.down('#pkj-csr-add-form-country'),
-            result ='';
-
-        if (!Ext.isEmpty(cnField.getValue())) {
-            result += 'CN=' + cnField.getValue();
-        }
-        if (!Ext.isEmpty(ouField.getValue())) {
-            if (result.length>0) { result += ', '; }
-            result += 'OU=' + ouField.getValue();
-        }
-        if (!Ext.isEmpty(oField.getValue())) {
-            if (result.length>0) { result += ', '; }
-            result += 'O=' + oField.getValue();
-        }
-        if (!Ext.isEmpty(lField.getValue())) {
-            if (result.length>0) { result += ', '; }
-            result += 'L=' + lField.getValue();
-        }
-        if (!Ext.isEmpty(sField.getValue())) {
-            if (result.length>0) { result += ', '; }
-            result += 'ST=' + sField.getValue();
-        }
-        if (!Ext.isEmpty(cField.getValue())) {
-            if (result.length>0) { result += ', '; }
-            result += 'C=' + cField.getValue();
-        }
-        return result;
     }
 });
