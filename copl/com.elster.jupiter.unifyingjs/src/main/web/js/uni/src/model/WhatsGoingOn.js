5ad537deac490c06aa971944e3beced53a479c1d
MaxIlyushen <ilyushen@mera.ru>
2017-06-05 16:42:48 +0300

    [CXO-7211]status-of-process-didn't-shown-on-tooltip

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/model/WhatsGoingOn.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/model/WhatsGoingOn.js
@@ -20,7 +20,26 @@ Ext.define('Uni.model.WhatsGoingOn', {
         'isMyWorkGroup',
         'userAssigneeIsCurrentUser',
         'reference',
-        'status',
+        {
+            name: 'status',
+            type: 'string',
+            mapping: function (record) {
+                switch (record.status) {
+                    case 0:
+                        return Uni.I18n.translate('bpm.status.pending', 'BPM', 'Pending');
+                    case 1:
+                        return Uni.I18n.translate('bpm.status.active', 'BPM', 'Active');
+                    case 2:
+                        return Uni.I18n.translate('bpm.status.completed', 'BPM', 'Completed');
+                    case 3:
+                        return Uni.I18n.translate('bpm.status.cancelled', 'BPM', 'Cancelled');
+                    case 4:
+                        return Uni.I18n.translate('bpm.status.ongoing', 'BPM', 'Ongoing');
+                    default :
+                        return record.status;
+                }
+            }
+        },
         {
             name: 'internalId',
             mapping: function(record){
