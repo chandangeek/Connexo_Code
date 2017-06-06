8da3fc1d8c35e29db23208b4cf15ebdec8d5b5aa
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-06-06 13:54:55 +0200

    CXO-7226

--- coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/MeterActivationImplIT.java
+++ coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/MeterActivationImplIT.java
@@ -10,6 +10,7 @@ import com.elster.jupiter.devtools.persistence.test.rules.ExpectedConstraintViol
 import com.elster.jupiter.devtools.persistence.test.rules.ExpectedConstraintViolationRule;
 import com.elster.jupiter.devtools.persistence.test.rules.Transactional;
 import com.elster.jupiter.devtools.persistence.test.rules.TransactionalRule;
+import com.elster.jupiter.domain.util.VerboseConstraintViolationException;
 import com.elster.jupiter.fsm.FiniteStateMachine;
 import com.elster.jupiter.fsm.FiniteStateMachineBuilder;
 import com.elster.jupiter.fsm.FiniteStateMachineService;
@@ -704,7 +705,7 @@ public class MeterActivationImplIT {
     @Test
     @Transactional
     public void testIncorrectStartTimeOfMeterAndMetrologyConfig() {
-        expectedException.expect(UsagePointManagementException.class);
+        expectedException.expect(VerboseConstraintViolationException.class);
 
         ServerMeteringService meteringService = inMemoryBootstrapModule.getMeteringService();
         AmrSystem system = meteringService.findAmrSystem(KnownAmrSystem.MDC.getId()).get();
