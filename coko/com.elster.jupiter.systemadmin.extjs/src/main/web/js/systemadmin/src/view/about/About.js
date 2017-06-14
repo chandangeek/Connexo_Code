71edcb05f6d6b772b0b99f80fe8d72b73e1d1fd2
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-06-14 11:04:10 +0200

    change about page

--- coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/about/About.js
+++ coko/com.elster.jupiter.systemadmin.extjs/src/main/web/js/systemadmin/src/view/about/About.js
@@ -16,7 +16,7 @@ Ext.define('Sam.view.about.About', {
             ui: 'large',
             bbar: {
                 xtype: 'panel',
-                title: Uni.I18n.translate('general.connexoVersion.10.2', 'SAM', 'Connexo version 10.2'),
+                title: Uni.I18n.translate('general.connexoVersion.10.3', 'SAM', 'Connexo version 10.3'),
                 ui: 'medium',
                 padding: 0,
                 items: [
