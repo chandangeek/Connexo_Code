5ab78e8336f9ed893830403fb82b9dc49a93a8d2
Thomas Vandebuerie <thomas.vandebuerie@elster.com>
2017-06-15 10:37:51 +0200

    CXO-7158

--- coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/impl/TranslationKeys.java
+++ coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/impl/TranslationKeys.java
@@ -12,7 +12,7 @@ import com.elster.jupiter.nls.TranslationKey;
  * Created by igh on 18/04/2016.
  */
 public enum TranslationKeys implements TranslationKey {
-    CALENDAR_IMPORTER(CalendarImporterFactory.NAME, "Calendar importer"),
+    CALENDAR_IMPORTER(CalendarImporterFactory.NAME, "Calendar importer [STD]"),
     CALENDAR_IMPORTED_SUCCESSFULLY("calendar.import.summary.succeeded", "Finished successfully."),
     CALENDAR_IMPORT_FAILED("calendar.import.summary.failed", "Failed to complete, no calendar has been processed."),
     CALENDAR_IMPORTER_MESSAGE_HANDLER_DISPLAYNAME(CalendarImporterMessageHandler.SUBSCRIBER_NAME, "Handle calendar import"),
