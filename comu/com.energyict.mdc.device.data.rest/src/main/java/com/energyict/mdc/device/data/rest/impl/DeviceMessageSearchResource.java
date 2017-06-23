203e0af3f66e83ec3194fde692cf55839cd0fc19
Bart Vansegbroeck <bart.vansegbroeck@honeywell.com>
2017-06-23 09:22:48 +0200

    CXO-7365: Cleaner code

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceMessageSearchResource.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/rest/impl/DeviceMessageSearchResource.java
@@ -25,7 +25,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
