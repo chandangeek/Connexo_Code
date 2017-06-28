154df913239412832163e567c4b216941287ea21
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-06-28 14:10:10 +0200

    CXO-7146 - Communication functions + Extended DeviceProtocol interface with DeviceProtocolConnectionFunctionSupport interface + Added the provided/consumable connection functions for Beacon 3100 and AM540 protocols + Added ConnectionFunction field to PartialConnectionTask and ComTaskEnablement objects + Adapted extjs front-end for connection method/communication task configuration

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/DeviceProtocolPluggableClass.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/DeviceProtocolPluggableClass.java
@@ -4,18 +4,19 @@
 
 package com.energyict.mdc.protocol.api;
 
-import com.energyict.mdc.upl.TypedProperties;
 import com.energyict.mdc.pluggable.PluggableClass;
 import com.energyict.mdc.protocol.api.messaging.DeviceMessageId;
+import com.energyict.mdc.upl.TypedProperties;
 import com.energyict.mdc.upl.messages.DeviceMessageSpec;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * Models a {@link DeviceProtocol} that was registered in the HeadEnd as a {@link PluggableClass}.
  * <p>
- *
+ * <p>
  * Date: 3/07/12
  * Time: 8:58
  */
@@ -32,6 +33,24 @@ public interface DeviceProtocolPluggableClass extends PluggableClass {
     DeviceProtocol getDeviceProtocol();
 
     TypedProperties getProperties();
+
+    /**
+     * Provide the list of possible {@link ConnectionFunction}s that are provided by the {@link DeviceProtocol}
+     * and therefore can be used as {@link ConnectionFunction} on connections of corresponding device(s).<br/>
+     * In case no specific connection function(s) are supported, an empty list should be returned.
+     *
+     * @return the possible ConnectionFunctions that are provided
+     */
+    List<ConnectionFunction> getProvidedConnectionFunctions();
+
+    /**
+     * Provide the list of possible {@link ConnectionFunction}s that can be consumed by this {@link DeviceProtocol}
+     * and therefore  can be used as {@link ConnectionFunction} on communication tasks of corresponding device(s).<br/>
+     * In case no specific connection function(s) are supported, an empty list should be returned.<br/>
+     *
+     * @return the possible ConnectionFunctions that can be consumed
+     */
+    List<ConnectionFunction> getConsumableConnectionFunctions();
 
     default boolean supportsFileManagement() {
         Set<Long> fileMessages = DeviceMessageId.fileManagementRelated().stream().map(DeviceMessageId::dbValue).collect(Collectors.toSet());
