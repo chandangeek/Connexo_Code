7d4085e2504f21aafb2d5d06b1625f0f01372dc6
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-07-13 17:45:38 +0200

    Small bugfixes to protocols (not related to any JIRA ticket)

--- upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/license/LicensedProtocolRule.java
+++ upl/com.energyict.mdc.upl.impl/protocolcommon/src/main/java/com/energyict/license/LicensedProtocolRule.java
@@ -41,9 +41,7 @@ public enum LicensedProtocolRule implements LicensedProtocol {
     SIEMENS_7ED62(13, "com.energyict.protocolimpl.siemens7ED62.Siemens7ED62"),
     SDC(14, "com.energyict.protocolimpl.iec1107.sdc.Sdc"),
     QUANTUM_SCHLUMBERGER(15, "com.energyict.protocolimpl.itron.quantum.Quantum"),
-
     SDK_DEVICE_PROTOCOL_WITH_ALL_PROPERTIES(16, "test.com.energyict.protocolimplv2.sdksample.SDKDeviceProtocolTestWithAllProperties", FamilyRule.TEST),
-
     DATASTAR(17, "com.energyict.protocolimpl.itron.datastar.Datastar"),
     VECTRON(18, "com.energyict.protocolimpl.itron.vectron.Vectron"),
     EZ7(19, "com.energyict.protocolimpl.emon.ez7.EZ7"),
@@ -334,11 +332,10 @@ public enum LicensedProtocolRule implements LicensedProtocol {
     T210D_MBUS_DEVICE(285, "com.energyict.protocolimplv2.dlms.idis.sagemcom.T210D.MBusDevice"),
     MK10_V2(286, "com.energyict.protocolimplv2.edmi.mk10.MK10", FamilyRule.EDMI),
     MK6_V2(287, "com.energyict.protocolimplv2.edmi.mk6.MK6"),
-
-/*
-    CRYPTO_T210D(288, "com.sagemcom.protocolimplv2.dlms.idis.T210D.CryptoT210D"),
-*/
-
+//    CRYPTO_T210D(288, "com.sagemcom.protocolimplv2.dlms.idis.T210D.CryptoT210D"),
+    AM132 (289, "com.energyict.protocolimplv2.dlms.idis.am132.AM132"),
+//    CRYPTO_AM132(290, "com.evn.protocolimplv2.dlms.idis.am132.CryptoAM132"),
+    MINI_MAX(291, "com.elster.us.protocolimplv2.mercury.minimax.MiniMax"),
 
     // Deprecated
     FERRANTI(10001, "com.energyict.protocolimpl.iec1107.ferranti.Ferranti"),
