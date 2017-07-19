1248790d37bc15059437e33f912430221f0426a5
Aura Vrancea <aura.vrancea@elster.com>
2017-07-19 13:55:38 +0300

    CXO-7410 - Set alarms priority in bulk

--- comu/com.energyict.mdc.device.alarms.rest/src/main/java/com.energyict.mdc.device.alarms.rest/resource/DeviceAlarmResource.java
+++ comu/com.energyict.mdc.device.alarms.rest/src/main/java/com.energyict.mdc.device.alarms.rest/resource/DeviceAlarmResource.java
@@ -402,13 +402,10 @@ public class DeviceAlarmResource extends BaseAlarmResource{
     private ActionInfo doBulkSetPriority(SetPriorityAlarmRequest request, Function<ActionInfo, List<? extends Issue>> issueProvider) {
         ActionInfo response = new ActionInfo();
         for (Issue issue : issueProvider.apply(response)) {
-            if (issue.getStatus().isHistorical()) {
-                response.addFail(getThesaurus().getFormat(DeviceAlarmTranslationKeys.ALARM_ALREADY_CLOSED).format(), issue.getId(), issue.getTitle());
-            } else {
-                issue.setPriority(Priority.fromStringValue(request.priority));
-                issue.update();
-                response.addSuccess(issue.getId());
-            }
+            issue.setPriority(Priority.fromStringValue(request.priority));
+            issue.update();
+            response.addSuccess(issue.getId());
+
         }
 
         return response;
