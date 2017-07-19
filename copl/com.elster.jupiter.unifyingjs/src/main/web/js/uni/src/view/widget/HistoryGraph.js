98b97650fda099d4abf78a985cf22e3635e8243d
David Nitescu <david.nitescu@honeywell.com>
2017-07-19 15:36:14 +0300

    CXO-7580 - Issues per priority - fixed color and legend

--- copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/widget/HistoryGraph.js
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/view/widget/HistoryGraph.js
@@ -106,6 +106,9 @@ Ext.define('Uni.view.widget.HistoryGraph', {
         var me = this, storeFields;
 
         if ((me.fields == null) || (me.fields.length == 0)) {
+            if (me.sortGrophFields) {
+                data.fields = me.sortGrophFields(data.fields);
+            }
             me.fields = data.fields;
             storeFields = data.fields.slice();
             storeFields.push('date');
