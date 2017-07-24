751be180a57664bbd079da30903e69f8b629a845
Bart Vansegbroeck <bart.vansegbroeck@honeywell.com>
2017-07-24 15:44:28 +0200

    CXO-7595: Adding shipment validator

--- coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/impl/importers/XmlValidationFailed.java
+++ coko/com.elster.jupiter.calendar/src/main/java/com/elster/jupiter/calendar/impl/importers/XmlValidationFailed.java
@@ -5,7 +5,6 @@
 package com.elster.jupiter.calendar.impl.importers;
 
 import com.elster.jupiter.calendar.impl.MessageSeeds;
-import com.elster.jupiter.fileimport.FileImportOccurrence;
 import com.elster.jupiter.nls.LocalizedException;
 import com.elster.jupiter.nls.Thesaurus;
 
