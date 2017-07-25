c0a70da86473914918e141fbaef2e789bae768dd
Bart Vansegbroeck <bart.vansegbroeck@honeywell.com>
2017-07-25 09:47:18 +0200

    Cleaned up unused imports

--- comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/MdcPropertyUtils.java
+++ comu/com.energyict.mdc.pluggable.rest/src/main/java/com/energyict/mdc/pluggable/rest/MdcPropertyUtils.java
@@ -4,46 +4,16 @@
 
 package com.energyict.mdc.pluggable.rest;
 
-import com.elster.jupiter.calendar.Calendar;
-import com.elster.jupiter.properties.BoundedBigDecimalPropertySpec;
-import com.elster.jupiter.properties.PropertySelectionMode;
 import com.elster.jupiter.properties.PropertySpec;
-import com.elster.jupiter.properties.PropertySpecPossibleValues;
-import com.elster.jupiter.properties.rest.NumberValidationRules;
-import com.elster.jupiter.properties.rest.PredefinedPropertyValuesInfo;
 import com.elster.jupiter.properties.rest.PropertyInfo;
-import com.elster.jupiter.properties.rest.PropertyType;
-import com.elster.jupiter.properties.rest.PropertyTypeInfo;
-import com.elster.jupiter.properties.rest.PropertyValidationRule;
-import com.elster.jupiter.properties.rest.PropertyValueConverter;
-import com.elster.jupiter.properties.rest.PropertyValueInfo;
-import com.elster.jupiter.properties.rest.PropertyValueInfoService;
-import com.elster.jupiter.properties.rest.PropertyValuesResourceInfo;
 import com.energyict.mdc.device.data.Device;
-import com.energyict.mdc.masterdata.LoadProfileType;
-import com.energyict.mdc.pluggable.rest.impl.CalendarResource;
-import com.energyict.mdc.pluggable.rest.impl.LoadProfileTypeResource;
-import com.energyict.mdc.pluggable.rest.impl.properties.SimplePropertyType;
-import com.energyict.mdc.protocol.api.timezones.TimeZoneInUse;
 import com.energyict.mdc.upl.TypedProperties;
-import com.energyict.mdc.upl.properties.DeviceGroup;
-import com.elster.jupiter.metering.groups.MeteringGroupsService;
 
 import aQute.bnd.annotation.ProviderType;
 
-import javax.inject.Inject;
 import javax.ws.rs.core.UriInfo;
-import java.math.BigDecimal;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-
-import static com.energyict.mdc.pluggable.rest.MdcPropertyUtils.PrivilegePresence.WITHOUT_PRIVILEGES;
-import static com.energyict.mdc.pluggable.rest.MdcPropertyUtils.ValueVisibility.HIDE_VALUES;
-import static com.energyict.mdc.pluggable.rest.MdcPropertyUtils.ValueVisibility.SHOW_VALUES;
 
 /**
  * Serves as a utility class to create proper PropertyInfo objects for a set of Properties
