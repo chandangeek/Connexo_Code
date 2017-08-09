6f2bdbd35a0e72d5e9d5eddee84d522c67daf3bc
Aura Vrancea <aura.vrancea@elster.com>
2017-08-09 16:46:55 +0300

    CXO-7655 - Front-end for view export schedule on demand

---
+++ coko/com.elster.jupiter.export.extjs/src/main/web/js/export/src/store/ExportTasks.js
@@ -0,0 +1,19 @@
+/*
+ *  Copyright text to : Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+Ext.define('Dxp.store.DataExportTasks', {
+    extend: 'Ext.data.Store',
+    model: 'Dxp.model.DataExportTask',
+    autoLoad: false,
+
+    proxy: {
+        type: 'rest',
+        url: '/api/export/dataexporttask',
+        timeout: 240000,
+        reader: {
+            type: 'json',
+            root: 'dataExportTasks'
+        }
+    }
+});
