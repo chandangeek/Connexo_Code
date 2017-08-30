40ec50f99a5c626a721c24baf33ccc439d1864c4
David Nitescu <david.nitescu@elster.com>
2017-08-30 11:36:14 +0300

    initial version

---
+++ coko/com.elster.jupiter.webservices.outbound.rest.issue.actions/src/main/java/com/elster/jupiter/webservices/outbound/rest/issue/actions/impl/IssueLoggingClientImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.webservices.outbound.rest.issue.actions.impl;
+import com.elster.jupiter.issue.share.IssueWebServiceClient;
+import com.elster.jupiter.issue.share.entity.Issue;
+import com.elster.jupiter.soap.whiteboard.cxf.EndPointConfiguration;
+
+import javax.ws.rs.client.WebTarget;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class IssueLoggingClientImpl implements IssueWebServiceClient {
+
+    private static final Logger LOGGER = Logger.getLogger(IssueLoggingClientImpl.class.getName());
+    private volatile WebTarget target;
+
+    public IssueLoggingClientImpl(WebTarget target) {
+        this.target = target;
+    }
+
+    @Override
+    public String getWebServiceName(){
+        return "Issue logging";
+    }
+
+    @Override
+    public boolean call(Issue issue, EndPointConfiguration endPointConfiguration)
+    {
+        try {
+
+            StringBuilder comment = new StringBuilder();
+            comment.append("Issue generated: ");
+            comment.append("for " + issue.getDevice().getName() + " device; ");
+            comment.append("issue created on " + issue.getCreateDateTime().toString());
+            LOGGER.log(Level.INFO, comment.toString());
+        } catch (RuntimeException e) {
+            return false;
+        }
+        return true;
+    }
+}
