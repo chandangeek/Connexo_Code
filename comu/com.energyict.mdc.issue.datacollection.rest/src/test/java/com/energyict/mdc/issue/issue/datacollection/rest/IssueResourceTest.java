7218a26f0f7e62e4e9c04fee9c2e10050225421f
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-08-30 17:13:48 +0200

    CXO-7146 Communication functions Fixed failing unit tests

--- comu/com.energyict.mdc.issue.datacollection.rest/src/test/java/com/energyict/mdc/issue/issue/datacollection/rest/IssueResourceTest.java
+++ comu/com.energyict.mdc.issue.datacollection.rest/src/test/java/com/energyict/mdc/issue/issue/datacollection/rest/IssueResourceTest.java
@@ -6,14 +6,9 @@ package com.energyict.mdc.issue.issue.datacollection.rest;
 
 import com.elster.jupiter.bpm.BpmProcessDefinition;
 import com.elster.jupiter.bpm.BpmServer;
-import com.elster.jupiter.issue.rest.request.CloseIssueRequest;
-import com.elster.jupiter.issue.rest.request.EntityReference;
-import com.elster.jupiter.issue.share.entity.IssueStatus;
 import com.energyict.mdc.issue.datacollection.entity.IssueDataCollection;
-import com.energyict.mdc.issue.datacollection.entity.OpenIssueDataCollection;
 import com.energyict.mdc.issue.datacollection.rest.IssueProcessInfos;
 
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
 import java.util.Map;
@@ -133,25 +128,6 @@ public class IssueResourceTest extends IssueDataCollectionApplicationJerseyTest 
         assertThat(issueProcessInfos.processes.get(2).openTasks.get(0).deploymentId).isEqualTo("com.elster.test:1.0");
         assertThat(issueProcessInfos.processes.get(2).openTasks.get(0).status).isEqualTo("Reserved");
         assertThat(issueProcessInfos.processes.get(2).openTasks.get(0).createdOn).isEqualTo("1456821941001");
-    }
-
-    @Test
-    public void testCloseAction() {
-        CloseIssueRequest request = new CloseIssueRequest();
-        EntityReference issueRef = new EntityReference();
-        issueRef.setId(1L);
-        request.issues = Arrays.asList(issueRef);
-        request.status = "resolved";
-        IssueStatus status = mockStatus("resolved", "Resolved", true);
-        when(issueService.findStatus("resolved")).thenReturn(Optional.of(status));
-        OpenIssueDataCollection issueDataCollection = mock(OpenIssueDataCollection.class);
-        when(issueDataCollectionService.findOpenIssue(1L)).thenReturn(Optional.of(issueDataCollection));
-        when(issueDataCollection.getStatus()).thenReturn(status);
-        
-
-        Entity<CloseIssueRequest> json = Entity.json(request);
-        Response response = target("issues/close").request().put(json);
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
     }
 
     private void assertDefaultIssueMap(Map<?, ?> issueMap) {
