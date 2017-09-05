ee7471915d78e93adf7006487ccc0ca9f98198c5
dragos iancu <dragos-florin.iancu@honeywell.com>
2017-09-05 08:56:25 +0300

    CXO-7650 - update translation for FE

--- comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/i18n/TranslationKeys.java
+++ comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/i18n/TranslationKeys.java
@@ -19,7 +19,7 @@ public enum TranslationKeys implements TranslationKey {
 
     PARAMETER_NAME_EVENT_TYPE(BasicDataCollectionRuleTemplate.EVENTTYPE, "Event"),
     PARAMETER_AUTO_RESOLUTION(BasicDataCollectionRuleTemplate.AUTORESOLUTION, "Auto resolution"),
-    PARAMETER_INCREASE_URGENCY(BasicDataCollectionRuleTemplate.INCREASEURGENCY, "Increase Urgency on issue recurrence"),
+    PARAMETER_INCREASE_URGENCY(BasicDataCollectionRuleTemplate.INCREASEURGENCY, "Increase urgency on recurrence (+1)"),
     PARAMETER_NAME_THRESHOLD(EventAggregationRuleTemplate.THRESHOLD, "Threshold (%)"),
     PARAMETER_NAME_EVENT_TYPE_FOR_AGGREGATION(EventAggregationRuleTemplate.EVENTTYPE, "Event"),
 
