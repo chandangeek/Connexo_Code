f61d5f1454e7f6e73c66cd8bbcc41eb91e6902cf
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-09-14 16:06:02 +0200

    CXO-7803 Support for Breaker/LoadLimit (messages & registers) for Redknee Support Re-added Connexo implementations (which were lost when UPL was introduced)

--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/legacy/MeterProtocol.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/legacy/MeterProtocol.java
@@ -5,10 +5,6 @@
 package com.energyict.mdc.protocol.api.legacy;
 
 import com.energyict.mdc.protocol.api.legacy.dynamic.Pluggable;
-import com.energyict.mdc.upl.meterdata.BreakerStatus;
-
-import java.io.IOException;
-import java.util.Optional;
 
 /**
  * Adds configuration support to the {@link com.energyict.mdc.upl.MeterProtocol} interface.
@@ -24,39 +20,4 @@ import java.util.Optional;
  */
 public interface MeterProtocol extends Pluggable, com.energyict.mdc.upl.cache.CachingProtocol, com.energyict.mdc.upl.MeterProtocol {
 
-    /**
-     * Gets the current status of the breaker<br/>
-     * Note: if the {@link MeterProtocol} doesn't support breaker functionality (e.g. the device is
-     * not equipped with a breaker), then {@link Optional#empty()} should be returned.
-     *
-     * @return the current status of the breaker
-     * @throws IOException Thrown in case of an exception
-     */
-    default Optional<BreakerStatus> getBreakerStatus() throws IOException {
-        return Optional.empty();
-    }
-
-    /**
-     * Gets the name of the active calendar that is currently configured on the device.
-     * Note: if the {@link MeterProtocol} doesn't support calendar functionality,
-     * then {@link Optional#empty()} should be returned.
-     *
-     * @return The name of the active calendar
-     * @throws IOException
-     */
-    default Optional<String> getActiveCalendarName() throws IOException {
-        return Optional.empty();
-    }
-
-    /**
-     * Gets the name of the passive calendar that is currently configured on the device.
-     * Note: if the {@link MeterProtocol} doesn't support calendar functionality,
-     * then {@link Optional#empty()} should be returned.
-     *
-     * @return The name of the passive calendar
-     * @throws IOException
-     */
-    default Optional<String> getPassiveCalendarName() throws IOException {
-        return Optional.empty();
-    }
 }
--- comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/legacy/SmartMeterProtocol.java
+++ comu/com.energyict.mdc.protocol.api/src/main/java/com/energyict/mdc/protocol/api/legacy/SmartMeterProtocol.java
@@ -5,10 +5,6 @@
 package com.energyict.mdc.protocol.api.legacy;
 
 import com.energyict.mdc.protocol.api.legacy.dynamic.Pluggable;
-import com.energyict.mdc.upl.meterdata.BreakerStatus;
-
-import java.io.IOException;
-import java.util.Optional;
 
 /**
  * SmartMeterProtocol is an extension to the standard {@link MeterProtocol} interface.
@@ -16,39 +12,4 @@ import java.util.Optional;
  */
 public interface SmartMeterProtocol extends Pluggable, com.energyict.mdc.upl.SmartMeterProtocol {
 
-    /**
-     * Gets the current status of the breaker <br/>
-     * Note: if the {@link MeterProtocol} doesn't support breaker functionality (e.g. the device is
-     * not equipped with a breaker), then {@link Optional#empty()} should be returned.
-     *
-     * @return the current status of the breaker
-     * @throws IOException Thrown in case of an exception
-     */
-    default Optional<BreakerStatus> getBreakerStatus() throws IOException {
-        return Optional.empty();
-    }
-
-    /**
-     * Gets the name of the active calendar that is currently configured on the device.
-     * Note: if the {@link MeterProtocol} doesn't support calendar functionality,
-     * then {@link Optional#empty()} should be returned.
-     *
-     * @return The name of the active calendar
-     * @throws IOException
-     */
-    default Optional<String> getActiveCalendarName() throws IOException {
-        return Optional.empty();
-    }
-
-    /**
-     * Gets the name of the passive calendar that is currently configured on the device.
-     * Note: if the {@link MeterProtocol} doesn't support calendar functionality,
-     * then {@link Optional#empty()} should be returned.
-     *
-     * @return The name of the passive calendar
-     * @throws IOException
-     */
-    default Optional<String> getPassiveCalendarName() throws IOException {
-        return Optional.empty();
-    }
 }
