7044aba70f17f7b095828a21aa9795283f3da266
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-09-26 12:22:33 +0200

    CXO-7839 Define CustomPropertySet for all DeviceProtocolDialects + Added CustomPropertySet & mappings for dialects of US MiniMax and Edmi MK10/MK6 protocols

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceImpl.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/DeviceImpl.java
@@ -451,7 +451,6 @@ public class DeviceImpl implements Device, ServerDeviceForConfigChange, ServerDe
     }
 
     void postSave() {
-        getDeviceService().findDevicesByConnectionTypeAndProperty(null, "host", "me");
         if (this.meter.isPresent() && !getName().equals(this.meter.get().getName())) {
             this.meter.get().setName(getName());
             this.meter.get().update();
