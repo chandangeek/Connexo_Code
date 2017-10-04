f0848fa0932ebd08b033d1c913fea6596109e857
Bart Vansegbroeck <bart.vansegbroeck@honeywell.com>
2017-10-04 15:02:17 +0200

    Fixed OSGi startup errors

--- comu/com.energyict.mdc.device.lifecycle.config/src/main/java/com/energyict/mdc/device/lifecycle/config/impl/DeviceLifeCycleImpl.java
+++ comu/com.energyict.mdc.device.lifecycle.config/src/main/java/com/energyict/mdc/device/lifecycle/config/impl/DeviceLifeCycleImpl.java
@@ -84,7 +84,7 @@ public class DeviceLifeCycleImpl implements DeviceLifeCycle {
     @NotNull(message = "{" + MessageSeeds.Keys.CAN_NOT_BE_EMPTY + "}", groups = { Save.Create.class, Save.Update.class })
     private TimeDuration maximumFutureEffectiveTimeShift = EffectiveTimeShift.FUTURE.defaultValue();
     @NotNull(message = "{" + MessageSeeds.Keys.CAN_NOT_BE_EMPTY + "}", groups = { Save.Create.class, Save.Update.class })
-    private TimeDuration maximumPastEffectiveTimeShift;
+    private TimeDuration maximumPastEffectiveTimeShift = EffectiveTimeShift.PAST.defaultValue();
     @Valid
     private List<AuthorizedAction> actions = new ArrayList<>();
     private List<AuthorizedActionImpl> updated = new ArrayList<>();
