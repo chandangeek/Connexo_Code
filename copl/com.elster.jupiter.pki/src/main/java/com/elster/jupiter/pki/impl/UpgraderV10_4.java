612795823157bb4924613d3cd16a63f07ac93b26
H257223 <peter.lowagie@honeywell.com>
2017-10-10 13:45:30 +0200

    save keyUsages + extendedKeyUsages for trust stores

--- copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/impl/UpgraderV10_4.java
+++ copl/com.elster.jupiter.pki/src/main/java/com/elster/jupiter/pki/impl/UpgraderV10_4.java
@@ -1,28 +1,18 @@
 package com.elster.jupiter.pki.impl;
 
-import com.elster.jupiter.domain.util.Finder;
 import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.orm.Version;
-import com.elster.jupiter.pki.CertificateWrapper;
-import com.elster.jupiter.pki.KeyUsage;
-import com.elster.jupiter.pki.ExtendedKeyUsage;
 import com.elster.jupiter.pki.PkiService;
 import com.elster.jupiter.pki.RequestableCertificateWrapper;
+import com.elster.jupiter.pki.TrustedCertificate;
 import com.elster.jupiter.pki.impl.wrappers.PkiLocalizedException;
-import com.elster.jupiter.pki.impl.wrappers.certificate.ClientCertificateWrapperImpl;
 import com.elster.jupiter.upgrade.Upgrader;
-import com.elster.jupiter.users.UserService;
 
 import javax.inject.Inject;
 import java.security.cert.CertificateParsingException;
-import java.security.cert.X509Certificate;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class UpgraderV10_4 implements Upgrader {
     private final DataModel dataModel;
@@ -35,13 +25,17 @@ public class UpgraderV10_4 implements Upgrader {
         this.dataModel = dataModel;
         this.pkiService = pkiService;
         this.thesaurus = thesaurus;
-
     }
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
         dataModelUpgrader.upgrade(dataModel, Version.version(10, 4));
 
+        updateCertificates();
+        updateTrustedCertificates();
+    }
+
+    private void updateCertificates() {
         pkiService.findAllCertificates().find().stream()
                 .map(x -> {
                     RequestableCertificateWrapper csr = (RequestableCertificateWrapper) x;
@@ -53,29 +47,33 @@ public class UpgraderV10_4 implements Upgrader {
                             csr.getCertificate().ifPresent(cert -> csr.setIssuer(cert.getIssuerDN().toString()));
                             csr.setKeyUsagesCsv(csr.stringifyKeyUsages(csr.getKeyUsages()));
                             csr.setExtendedKeyUsagesCsv(csr.stringifyExtendedKeyUsages(csr.getExtendedKeyUsages()));
+                            csr.save();
                         } catch (CertificateParsingException e) {
                             throw new PkiLocalizedException(thesaurus, MessageSeeds.COULD_NOT_READ_KEY_USAGES);
                         }
                     }
                     return x;
                 })
-                .forEach(certificate -> dataModel.update(certificate));
-
-        pkiService.getAllTrustStores().stream()
-                .map(x -> x.getCertificates())
-                .flatMap(y -> y.stream())
-                .forEach(z -> {
-                    X509Certificate cert = z.getCertificate().get();
-                    System.out.println(z.getCertificate().get().getSubjectDN());
-                    System.out.println(z.getCertificate().get().getIssuerDN());
-//                    z.getKeyUsages()
-//                    z.getExtendedKeyUsages()
-
-                });
-
-
-        // TODO get the damn trust storese
+                .collect(Collectors.toList());
     }
 
 
+    private void updateTrustedCertificates() {
+        pkiService.getAllTrustStores().stream()
+                .map(x -> {
+                    for (TrustedCertificate trustedCertificate : x.getCertificates()) {
+                        try {
+                            trustedCertificate.getCertificate().ifPresent(cert -> trustedCertificate.setSubject(cert.getSubjectDN().toString()));
+                            trustedCertificate.getCertificate().ifPresent(cert -> trustedCertificate.setIssuer(cert.getIssuerDN().toString()));
+                            trustedCertificate.setKeyUsagesCsv(trustedCertificate.stringifyKeyUsages(trustedCertificate.getKeyUsages()));
+                            trustedCertificate.setExtendedKeyUsagesCsv(trustedCertificate.stringifyExtendedKeyUsages(trustedCertificate.getExtendedKeyUsages()));
+                            trustedCertificate.save();
+                        } catch (CertificateParsingException e) {
+                            throw new PkiLocalizedException(thesaurus, MessageSeeds.COULD_NOT_READ_KEY_USAGES);
+                        }
+                    }
+                    return x;
+                })
+                .collect(Collectors.toList());
+    }
 }
