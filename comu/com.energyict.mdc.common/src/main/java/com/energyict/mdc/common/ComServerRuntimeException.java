3ce4855893a9b519c414fde27105cd552f9f5141
Stijn Vanhoorelbeke <stijn.vanhoorelbeke@honeywell.com>
2017-10-10 11:56:13 +0200

    CXO-7865 Refactor protocol-api and review how exceptions are handled in protocols / comserver engine # Refactored the protocol exception types: added ProtocolRuntimeException/CommunicationException/... to mdc.upl. - All protocol related exception types defined in protocol-api extend now from the mdc.upl variant - The mdc.upl variant can be used directly inside comserver engine / protocol pluggable # Refactored translation of ProtocolRuntimeException / ComServerRuntimeException, so these can be translated based on an injected Thesaurus

--- comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/ComServerRuntimeException.java
+++ comu/com.energyict.mdc.common/src/main/java/com/energyict/mdc/common/ComServerRuntimeException.java
@@ -6,8 +6,10 @@ package com.energyict.mdc.common;
 
 import com.elster.jupiter.nls.Layer;
 import com.elster.jupiter.nls.NlsService;
-import com.elster.jupiter.nls.Thesaurus;
 import com.elster.jupiter.util.exception.MessageSeed;
+
+import java.text.MessageFormat;
+import java.util.Optional;
 
 /**
  * Serves as the root for all exceptions that we want to deal with at runtime.
@@ -24,37 +26,50 @@ public abstract class ComServerRuntimeException extends ComServerExecutionExcept
     private Object[] messageArguments;
     private MessageSeed messageSeed;
 
+    private Optional<NlsService> nlsService = Optional.empty();
+
     /**
      * Constructs a new ComServerRuntimeException identified by the {@link MessageSeed}.
      *
      * @param messageSeed The MessageSeed
      * @param messageArguments A sequence of values for the arguments of the human readable description
-     *        that is associated with the MessageSeed
+     * that is associated with the MessageSeed
      */
-    public ComServerRuntimeException (MessageSeed messageSeed, Object... messageArguments) {
-        super();
+    public ComServerRuntimeException(MessageSeed messageSeed, Object... messageArguments) {
+        super(defaultFormattedMessage(messageSeed, messageArguments));
         this.messageSeed = messageSeed;
         this.messageArguments = messageArguments;
     }
 
     /**
      * Constructs a new ComServerRuntimeException identified by the {@link MessageSeed}.
-     * <p>
-     * The error message of the cause exception (cause) is added to the new error message.
-     * Both error messages are split by {@code ComServerRuntimeException.DEFAULT_CAUSED_BY_SEPARATOR_VALUE},
-     * but this value can be overridden by defining a
-     * value for the key {@code ComServerRuntimeException.CAUSED_BY_SEPARATOR}
-     * in one of your resource bundles.
+     *
+     * @param cause The actual cause of the exceptional situation
+     * @param messageSeed The MessageSeed
+     * that is associated with the MessageSeed
+     */
+    public ComServerRuntimeException(Throwable cause, MessageSeed messageSeed) {
+        super(defaultFormattedMessage(messageSeed, new Object[]{cause.getMessage()}), cause);
+        this.messageSeed = messageSeed;
+        this.messageArguments = new Object[0];
+    }
+
+    /**
+     * Constructs a new ComServerRuntimeException identified by the {@link MessageSeed}.
      *
      * @param cause The actual cause of the exceptional situation
      * @param messageSeed The MessageSeed
      * @param messageArguments A sequence of values for the arguments of the human readable description
-     *                         that is associated with the MessageSeed
+     * that is associated with the MessageSeed
      */
-    public ComServerRuntimeException (Throwable cause, MessageSeed messageSeed, Object... messageArguments) {
-        super(cause);
+    public ComServerRuntimeException(Throwable cause, MessageSeed messageSeed, Object... messageArguments) {
+        super(defaultFormattedMessage(messageSeed, messageArguments), cause);
         this.messageSeed = messageSeed;
         this.messageArguments = messageArguments;
+    }
+
+    private static String defaultFormattedMessage(MessageSeed message, Object[] messageArguments) {
+        return MessageFormat.format(message.getDefaultFormat(), messageArguments);
     }
 
     public MessageSeed getMessageSeed() {
@@ -62,12 +77,29 @@ public abstract class ComServerRuntimeException extends ComServerExecutionExcept
     }
 
     public Object[] getMessageArguments() {
-        return messageArguments;
+        return (messageArguments.length == 0 && getCause() != null) // If no message arguments, then use the message of the cause as argument
+                ? new Object[]{getCause().getMessage()}
+                : messageArguments;
     }
 
-    public String translated (NlsService nlsService) {
-        Thesaurus thesaurus = nlsService.getThesaurus(this.messageSeed.getModule(), Layer.DOMAIN);
-        return thesaurus.getFormat(messageSeed).format(this.messageArguments);
+    public void injectNlsService(NlsService nlsService) {
+        this.nlsService = Optional.of(nlsService);
+        if (getCause() instanceof ComServerRuntimeException) {
+            ((ComServerRuntimeException) getCause()).injectNlsService(nlsService);
+        }
     }
 
+    @Override
+    public String getMessage() {
+        return nlsService.isPresent()
+                ? nlsService.get().getThesaurus(getMessageSeed().getModule(), Layer.DOMAIN).getFormat(getMessageSeed()).format(getMessageArguments())
+                : super.getMessage();
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return nlsService.isPresent()
+                ? nlsService.get().getThesaurus(getMessageSeed().getModule(), Layer.DOMAIN).getFormat(getMessageSeed()).format(getMessageArguments())
+                : super.getLocalizedMessage();
+    }
 }
