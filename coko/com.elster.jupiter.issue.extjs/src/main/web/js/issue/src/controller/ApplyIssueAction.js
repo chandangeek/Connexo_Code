c025b0e92a3e86205b6e52f68a890129c8e97b84
David Nitescu <david.nitescu@honeywell.com>
2017-10-16 10:44:05 +0300

    CXO-7644 - Issue history on device - reported issue fixed

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/controller/ApplyIssueAction.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/controller/ApplyIssueAction.js
@@ -87,6 +87,7 @@ Ext.define('Isu.controller.ApplyIssueAction', {
                         if (form.title === 'Close issue' || form.title === 'Close alarm' || form.title === 'Notify user' || form.title === 'Assign issue' || form.title === 'Assign alarm' || form.title === "Snooze") {
                             form.down('#issue-action-apply') && form.down('#issue-action-apply').setText(form.title.split(' ')[0]);
                             form.down('#alarm-action-apply') && form.down('#alarm-action-apply').setText(form.title.split(' ')[0]);
+                            form.down('#device-issue-action-apply') && form.down('#device-issue-action-apply').setText(form.title.split(' ')[0]);
                         }
                     }
                 }
