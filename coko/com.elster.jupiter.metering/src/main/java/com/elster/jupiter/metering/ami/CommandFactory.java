18477b393bdcf222a035f3f0bb1a3f440c7ad7cf
Bart Vansegbroeck <bart.vansegbroeck@honeywell.com>
2017-10-24 15:41:31 +0200

    CXO-7686: Renamed KeyAccessor(Type) to SecurityAccessor(Type)

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/CommandFactory.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/ami/CommandFactory.java
@@ -7,7 +7,7 @@ package com.elster.jupiter.metering.ami;
 import aQute.bnd.annotation.ConsumerType;
 import com.elster.jupiter.metering.EndDevice;
 import com.elster.jupiter.metering.EndDeviceControlType;
-import com.elster.jupiter.pki.KeyAccessorType;
+import com.elster.jupiter.pki.SecurityAccessorType;
 import com.elster.jupiter.util.units.Quantity;
 
 import java.time.Instant;
@@ -27,6 +27,6 @@ public interface CommandFactory {
 
     EndDeviceCommand createDisableLoadLimitCommand(EndDevice endDevice) throws UnsupportedCommandException;
 
-    EndDeviceCommand createKeyRenewalCommand(EndDevice endDevice, KeyAccessorType keyAccessortype) throws UnsupportedCommandException;
+    EndDeviceCommand createKeyRenewalCommand(EndDevice endDevice, SecurityAccessorType securityAccessortype) throws UnsupportedCommandException;
 
 }
