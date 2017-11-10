773f850e0978a363d2fbeb0a5c2f098ff6a4be1c
Laurentia Timar <laurentia.timar@honeywell.com>
2017-11-10 19:47:00 +0200

    CXO-7969 - Importer failed when DST change

--- coko/com.elster.jupiter.metering.imports/src/main/java/com/elster/jupiter/metering/imports/impl/properties/TimeZonePropertySpec.java
+++ coko/com.elster.jupiter.metering.imports/src/main/java/com/elster/jupiter/metering/imports/impl/properties/TimeZonePropertySpec.java
@@ -16,7 +16,7 @@ import java.time.Clock;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class TimeZonePropertySpec extends BasicPropertySpec {
 
@@ -38,10 +38,7 @@ public class TimeZonePropertySpec extends BasicPropertySpec {
 
     public String getDefaultTimeZone(Clock clock) {
         TimeZone timeZone = TimeZone.getTimeZone(clock.getZone());
-        int rawOffset = timeZone.getOffset(clock.millis());
-        long hours = TimeUnit.MILLISECONDS.toHours(rawOffset);
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(rawOffset) - TimeUnit.HOURS.toMinutes(hours);
-        return String.format("GMT%s%02d:%02d", rawOffset >= 0 ? "+" : "-", hours, minutes);
+        return timeZone.getID();    // CXO-7969
     }
 
     @Override
@@ -51,15 +48,23 @@ public class TimeZonePropertySpec extends BasicPropertySpec {
             if (Checks.is(value).emptyOrOnlyWhiteSpace()) {
                 return false;
             }
-            try {
-                format.parse(value);
-            } catch (DateTimeParseException e) {
-                throw new InvalidValueException(thesaurus.getFormat(MessageSeeds.TIME_ZONE_IS_NOT_VALID).format(),
-                        MessageSeeds.TIME_ZONE_IS_NOT_VALID.getDefaultFormat(),
-                        getName());
-            }
-            return true;
+            return checkForKnowTimeZoneId(value) || checkAccordingToDateTimeFormatter(value); //lori
         }
         return false;
+    }
+
+    private boolean checkAccordingToDateTimeFormatter(String value) throws InvalidValueException {
+        try {
+            format.parse(value);
+        } catch (DateTimeParseException e) {
+            throw new InvalidValueException(thesaurus.getFormat(MessageSeeds.TIME_ZONE_IS_NOT_VALID).format(),
+                    MessageSeeds.TIME_ZONE_IS_NOT_VALID.getDefaultFormat(),
+                    getName());
+        }
+        return true;
+    }
+
+    private boolean checkForKnowTimeZoneId(String value) {
+        return Stream.of(TimeZone.getAvailableIDs()).filter(s -> s.equals(value)).findFirst().isPresent();
     }
 }
