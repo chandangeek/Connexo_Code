2d92b5cbb49df34978d42102277d18930804b04b
Yan R <yaratay@mera.ru>
2017-12-01 19:10:20 +0300

    Move current Security accessors CRUD from device type level (available 10.3) to a generic Administration level On device type level add / remove security accessors from the list of the ones available at system level

---
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/ShouldHaveUniqueName.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.util;
+
+public interface ShouldHaveUniqueName {
+    boolean hasUniqueName();
+}
---
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/UniqueNameValidator.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.util;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueNameValidator implements ConstraintValidator<UniqueName, ShouldHaveUniqueName> {
+    private String message;
+
+    @Override
+    public void initialize(UniqueName constraintAnnotation) {
+        message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(ShouldHaveUniqueName value, ConstraintValidatorContext context) {
+        if (!value.hasUniqueName()) { // delegate validation
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(message).addPropertyNode("name").addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+}
