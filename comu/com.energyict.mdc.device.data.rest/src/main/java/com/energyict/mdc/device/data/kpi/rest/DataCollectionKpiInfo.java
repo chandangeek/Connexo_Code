261e5703aad63bbc17ec8f1183398d1556b61edf
David Nitescu <david.nitescu@honeywell.com>
2017-12-05 14:31:51 +0200

    CXO-7991 - data collection kpi

--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/kpi/rest/DataCollectionKpiInfo.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/kpi/rest/DataCollectionKpiInfo.java
@@ -4,13 +4,13 @@
 
 package com.energyict.mdc.device.data.kpi.rest;
 
-import com.elster.jupiter.rest.util.IdWithNameInfo;
 import com.elster.jupiter.rest.util.LongIdWithNameInfo;
 import com.elster.jupiter.time.rest.TimeDurationInfo;
 import com.energyict.mdc.scheduling.rest.TemporalExpressionInfo;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
 
 public class DataCollectionKpiInfo {
 
@@ -21,7 +21,13 @@ public class DataCollectionKpiInfo {
     public BigDecimal connectionTarget;
     public BigDecimal communicationTarget;
     public Instant latestCalculationDate;
+    public Long connectionTaskId;
+    public Long communicationTaskId;
     public long version;
+    public List<TaskInfo> connectionNextRecurrentTasks;
+    public List<TaskInfo> communicationNextRecurrentTasks;
+    public List<TaskInfo> connectionPreviousRecurrentTasks;
+    public List<TaskInfo> communicationPreviousRecurrentTasks;
 }
 
 
--- comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/kpi/rest/DataCollectionKpiInfoFactory.java
+++ comu/com.energyict.mdc.device.data.rest/src/main/java/com/energyict/mdc/device/data/kpi/rest/DataCollectionKpiInfoFactory.java
@@ -4,11 +4,14 @@
 
 package com.energyict.mdc.device.data.kpi.rest;
 
-import com.elster.jupiter.rest.util.IdWithNameInfo;
 import com.elster.jupiter.rest.util.LongIdWithNameInfo;
+import com.elster.jupiter.tasks.RecurrentTask;
 import com.elster.jupiter.time.rest.TimeDurationInfo;
 import com.energyict.mdc.device.data.kpi.DataCollectionKpi;
 import com.energyict.mdc.scheduling.rest.TemporalExpressionInfo;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DataCollectionKpiInfoFactory {
 
@@ -22,7 +25,20 @@ public class DataCollectionKpiInfoFactory {
         kpi.getStaticCommunicationKpiTarget().ifPresent(target -> kpiInfo.communicationTarget = target);
         kpi.getStaticConnectionKpiTarget().ifPresent(target -> kpiInfo.connectionTarget = target);
         kpi.getLatestCalculation().ifPresent(target -> kpiInfo.latestCalculationDate = target);
+        kpi.communicationKpiTask().ifPresent(task -> {
+            kpiInfo.communicationTaskId = task.getId();
+            kpiInfo.communicationNextRecurrentTasks = constructTaskInfo(task.getNextRecurrentTasks());
+            kpiInfo.communicationPreviousRecurrentTasks = constructTaskInfo(task.getPrevRecurrentTasks());
+        });
+        kpi.connectionKpiTask().ifPresent(task -> {
+            kpiInfo.connectionTaskId = task.getId();
+            kpiInfo.connectionNextRecurrentTasks = constructTaskInfo(task.getNextRecurrentTasks());
+            kpiInfo.connectionPreviousRecurrentTasks = constructTaskInfo(task.getPrevRecurrentTasks());
+        });
         return kpiInfo;
     }
 
+    private List<TaskInfo> constructTaskInfo(List<RecurrentTask> recurrentTasks) {
+        return recurrentTasks.stream().map(recurrentTask -> TaskInfo.from(recurrentTask)).collect(Collectors.toList());
+    }
 }
