8ddbcd1a931356ec620fb837680de59487e6bd9d
Tatiana Buica <tatiana.buica@honeywell.com>
2017-12-29 09:32:40 +0200

    CXO-8109 added 'security accessors' for each property set

--- comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/templates/SecurityPropertySetTpl.java
+++ comu/com.elster.jupiter.demo/src/main/java/com/elster/jupiter/demo/impl/templates/SecurityPropertySetTpl.java
@@ -8,55 +8,65 @@ import com.elster.jupiter.demo.impl.builders.SecurityPropertySetBuilder;
 import com.energyict.mdc.device.config.SecurityPropertySet;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 public enum SecurityPropertySetTpl implements Template<SecurityPropertySet, SecurityPropertySetBuilder> {
-    NO_SECURITY("No security",
+    NO_SECURITY_DEFAULT("No security",
             1,
             -1,
             0,  //NO_AUTHENTICATION
-            0  //NO_ENCRYPTION
+            0,  //NO_ENCRYPTION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY, KeyAccessorTpl.AUTHENTICATION_KEY, KeyAccessorTpl.PASSWORD)
     ),
-    NO_SECURITY_ZERO_SUITE("No security",
+    NO_SECURITY_ZERO_SUITE_DEFAULT("No security",
             1,
             0, //SECURITY_SUITE_0
             0, //NO_AUTHENTICATION
-            0  //NO_ENCRYPTION
+            0,  //NO_ENCRYPTION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY, KeyAccessorTpl.AUTHENTICATION_KEY, KeyAccessorTpl.PASSWORD)
     ),
     HIGH_LEVEL("High level authentication (MD5) and encryption",
             1,
             -1,
             3,  //HIGH_LEVEL_MD5
-            3  //DATA_AUTHENTICATION_ENCRYPTION
+            3,  //DATA_AUTHENTICATION_ENCRYPTION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY, KeyAccessorTpl.AUTHENTICATION_KEY, KeyAccessorTpl.PASSWORD)
     ),
     HIGH_LEVEL_NO_ENCRYPTION_MD5("High level authentication (MD5) - No encryption",
             1,
             -1,
             3,  //HIGH_LEVEL_MD5
-            0  //NO_ENCRYPTION
+            0,  //NO_ENCRYPTION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY, KeyAccessorTpl.AUTHENTICATION_KEY, KeyAccessorTpl.PASSWORD)
     ),
     HIGH_LEVEL_NO_ENCRYPTION_GMAC("High level authentication - No encryption",
             1,
             -1,
             5,  //HIGH_LEVEL_GMAC
-            0   //NO_ENCRYPTION
+            0,   //NO_ENCRYPTION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY, KeyAccessorTpl.AUTHENTICATION_KEY, KeyAccessorTpl.PASSWORD)
     ),
     BEACON1_GMAC("Beacon1 GMAC 5:3",
             1,
             0,  //SECURITY_SUITE_0
             5,  //HIGH_LEVEL_GMAC
-            3   //MESSAGE_ENCRYPTION_AND_AUTHENTICATION
+            3,  //MESSAGE_ENCRYPTION_AND_AUTHENTICATION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY_BEACON1, KeyAccessorTpl.AUTHENTICATION_KEY_BEACON1)
     ),
     BEACON2_GMAC("Beacon2 GMAC 5:3",
             1,
             0,  //SECURITY_SUITE_0
             5,  //HIGH_LEVEL_GMAC
-            3   //MESSAGE_ENCRYPTION_AND_AUTHENTICATION
+            3,  //MESSAGE_ENCRYPTION_AND_AUTHENTICATION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY_BEACON2, KeyAccessorTpl.AUTHENTICATION_KEY_BEACON2)
     ),
     PASSWORD_ONLY("Password only",
             1,
             -1,
             1,  //LOW_LEVEL
-            0  //NO_ENCRYPTION
+            0,  //NO_ENCRYPTION
+            Arrays.asList(KeyAccessorTpl.ENCRYPTION_KEY, KeyAccessorTpl.AUTHENTICATION_KEY, KeyAccessorTpl.PASSWORD)
     ),
     ;
     private String name;
@@ -64,13 +74,15 @@ public enum SecurityPropertySetTpl implements Template<SecurityPropertySet, Secu
     private int suite;
     private int authLevel;
     private int encLevel;
+    private List<KeyAccessorTpl> keys;
 
-    SecurityPropertySetTpl(String name, int client, int suite, int authLevel, int encLevel) {
+    SecurityPropertySetTpl(String name, int client, int suite, int authLevel, int encLevel, List<KeyAccessorTpl> keys) {
         this.name = name;
         this.suite = suite;
         this.client = client;
         this.authLevel = authLevel;
         this.encLevel = encLevel;
+        this.keys = keys;
     }
 
     @Override
@@ -80,7 +92,7 @@ public enum SecurityPropertySetTpl implements Template<SecurityPropertySet, Secu
 
     @Override
     public SecurityPropertySetBuilder get(SecurityPropertySetBuilder builder) {
-        return builder.withClient(BigDecimal.valueOf(client)).withSuite(suite).withAuthLevel(authLevel).withEncLevel(encLevel).withName(name);
+        return builder.withClient(BigDecimal.valueOf(client)).withSuite(suite).withAuthLevel(authLevel).withEncLevel(encLevel).withName(name).withKeys(keys);
     }
 
     public String getName() {
