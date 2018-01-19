6eaf88e88764bc3212551da842803facb9f7873c
H251853 <andreea.gutu@honeywell.com>
2018-01-19 10:30:58 +0200

    CXO-8206: Graph shows registered devices in the past, till now.

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/registereddevices/model/RegisteredDevicesOnGateway.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/registereddevices/model/RegisteredDevicesOnGateway.js
@@ -4,8 +4,19 @@
 Ext.define('Mdc.registereddevices.model.RegisteredDevicesOnGateway', {
     extend: 'Uni.model.Version',
     fields: [
-        {name: 'timestamp', type: 'integer', useNull: true},
-        {name: 'registered', type: 'integer', useNull: true}
+        {
+            name: 'timestamp',
+            type: 'integer',
+            useNull: true
+        },
+        {
+            name: 'registered',
+            type: 'integer',
+            useNull: true,
+            convert: function (value, rec) {
+                return rec.get('timestamp') < new Date().getTime() ? value : 0;
+            }
+        }
     ],
     proxy: {
         type: 'rest',
