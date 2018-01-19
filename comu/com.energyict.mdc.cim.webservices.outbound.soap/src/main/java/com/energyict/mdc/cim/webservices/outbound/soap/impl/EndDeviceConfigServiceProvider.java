8a9b848cb8d506fd06b4a260475431d01cc477b3
Yan Polovenko <polovenk@mera.ru>
2018-01-19 12:06:54 +0300

    [CXO-8142] Added end point services

--- comu/com.energyict.mdc.cim.webservices.outbound.soap/src/main/java/com/energyict/mdc/cim/webservices/outbound/soap/impl/EndDeviceConfigServiceProvider.java
+++ comu/com.energyict.mdc.cim.webservices.outbound.soap/src/main/java/com/energyict/mdc/cim/webservices/outbound/soap/impl/EndDeviceConfigServiceProvider.java
@@ -9,17 +9,47 @@ import com.elster.jupiter.soap.whiteboard.cxf.EndPointConfiguration;
 import com.elster.jupiter.soap.whiteboard.cxf.OutboundSoapEndPointProvider;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
+import ch.iec.tc57._2011.meterconfigmessage.MeterConfigEventMessageType;
+import ch.iec.tc57._2011.meterconfigmessage.MeterConfigPayloadType;
+import ch.iec.tc57._2011.meterconfigmessage.MeterConfigResponseMessageType;
+import ch.iec.tc57._2011.replymeterconfig.FaultMessage;
+import ch.iec.tc57._2011.schema.message.HeaderType;
+import ch.iec.tc57._2011.schema.message.ReplyType;
 import ch.iec.tc57._2011.replymeterconfig.MeterConfigPort;
 import ch.iec.tc57._2011.replymeterconfig.ReplyMeterConfig;
 
 import javax.xml.ws.Service;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Component(name = "com.energyict.mdc.cim.webservices.outbound.soap.enddeviceconfig.provider",
         service = {StateTransitionWebServiceClient.class, OutboundSoapEndPointProvider.class},
         immediate = true,
         property = {"name=" + StateTransitionWebServiceClient.NAME})
 public class EndDeviceConfigServiceProvider implements StateTransitionWebServiceClient, OutboundSoapEndPointProvider {
+
+    private final ch.iec.tc57._2011.schema.message.ObjectFactory cimMessageObjectFactory = new ch.iec.tc57._2011.schema.message.ObjectFactory();
+    private final ch.iec.tc57._2011.meterconfigmessage.ObjectFactory meterConfigMessageObjectFactory = new ch.iec.tc57._2011.meterconfigmessage.ObjectFactory();
+
+    private final List<MeterConfigPort> stateMeterConfigPortServices = new ArrayList<>();
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addMeterConfigPortService(MeterConfigPort meterConfigPort) {
+        stateMeterConfigPortServices.add(meterConfigPort);
+    }
+
+    public void removeMeterConfigPortService(MeterConfigPort meterConfigPort) {
+        stateMeterConfigPortServices.remove(meterConfigPort);
+    }
+
+    public List<MeterConfigPort> getStateTransitionWebServiceClients() {
+        return Collections.unmodifiableList(this.stateMeterConfigPortServices);
+    }
 
     @Override
     public Service get() {
@@ -38,6 +68,25 @@ public class EndDeviceConfigServiceProvider implements StateTransitionWebService
 
     @Override
     public boolean call(long id, EndPointConfiguration endPointConfiguration) {
-        return false;
+        MeterConfigResponseMessageType meterConfigResponseMessageType = null;
+        try {
+            MeterConfigEventMessageType meterConfigEventMessageType = new MeterConfigEventMessageType();
+            HeaderType header = cimMessageObjectFactory.createHeaderType();
+            header.setNoun(getWebServiceName());
+            header.setVerb(HeaderType.Verb.CHANGED);
+            meterConfigEventMessageType.setHeader(header);
+
+            MeterConfigPayloadType meterConfigPayloadType = new MeterConfigPayloadType();
+            meterConfigEventMessageType.setPayload(meterConfigPayloadType);
+
+            ReplyType replyType = cimMessageObjectFactory.createReplyType();
+            replyType.setResult(ReplyType.Result.OK);
+            meterConfigEventMessageType.setReply(replyType);
+
+            meterConfigResponseMessageType = stateMeterConfigPortServices.get(0).createdMeterConfig(meterConfigEventMessageType);
+        } catch (FaultMessage faultMessage) {
+            faultMessage.printStackTrace();
+        }
+        return meterConfigResponseMessageType == null;
     }
 }
