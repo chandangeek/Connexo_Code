280426341ab12165651f18976ee1c56ee8dec53c
Yan Polovenko <polovenk@mera.ru>
2018-01-22 00:00:56 +0300

    [CXO-8142] Added meter config factory

--- comu/com.energyict.mdc.cim.webservices.outbound.soap/src/main/java/com/energyict/mdc/cim/webservices/outbound/soap/impl/EndDeviceConfigServiceProvider.java
+++ comu/com.energyict.mdc.cim.webservices.outbound.soap/src/main/java/com/energyict/mdc/cim/webservices/outbound/soap/impl/EndDeviceConfigServiceProvider.java
@@ -9,6 +9,9 @@ import com.elster.jupiter.soap.whiteboard.cxf.EndPointConfiguration;
 import com.elster.jupiter.soap.whiteboard.cxf.LogLevel;
 import com.elster.jupiter.soap.whiteboard.cxf.OutboundSoapEndPointProvider;
 
+import com.energyict.mdc.cim.webservices.outbound.soap.meterconfig.MeterConfigFactory;
+import com.energyict.mdc.device.data.Device;
+import com.energyict.mdc.device.data.DeviceService;
 import org.apache.cxf.jaxws.JaxWsClientProxy;
 import org.apache.cxf.message.Message;
 import org.osgi.service.component.annotations.Component;
@@ -16,6 +19,7 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 
+import ch.iec.tc57._2011.meterconfig.MeterConfig;
 import ch.iec.tc57._2011.meterconfigmessage.MeterConfigEventMessageType;
 import ch.iec.tc57._2011.meterconfigmessage.MeterConfigPayloadType;
 import ch.iec.tc57._2011.replymeterconfig.FaultMessage;
@@ -24,6 +28,7 @@ import ch.iec.tc57._2011.schema.message.ReplyType;
 import ch.iec.tc57._2011.replymeterconfig.MeterConfigPort;
 import ch.iec.tc57._2011.replymeterconfig.ReplyMeterConfig;
 
+import javax.inject.Inject;
 import javax.xml.ws.Service;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
@@ -38,8 +43,25 @@ public class EndDeviceConfigServiceProvider implements StateTransitionWebService
 
     private final ch.iec.tc57._2011.schema.message.ObjectFactory cimMessageObjectFactory = new ch.iec.tc57._2011.schema.message.ObjectFactory();
     private final ch.iec.tc57._2011.meterconfigmessage.ObjectFactory meterConfigMessageObjectFactory = new ch.iec.tc57._2011.meterconfigmessage.ObjectFactory();
-
     private final List<MeterConfigPort> stateMeterConfigPortServices = new ArrayList<>();
+    private final MeterConfigFactory meterConfigFactory = new MeterConfigFactory();
+
+    private volatile DeviceService deviceService;
+
+    public EndDeviceConfigServiceProvider() {
+        // for OSGI purposes
+    }
+
+    @Inject
+    public EndDeviceConfigServiceProvider(DeviceService deviceService) {
+        this();
+        setDeviceService(deviceService);
+    }
+
+    @Reference
+    public void setDeviceService(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
 
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
     public void addMeterConfigPortService(MeterConfigPort meterConfigPort) {
@@ -72,25 +94,12 @@ public class EndDeviceConfigServiceProvider implements StateTransitionWebService
     @Override
     public void call(long id, EndPointConfiguration endPointConfiguration) {
         try {
-            MeterConfigEventMessageType meterConfigEventMessageType = new MeterConfigEventMessageType();
-            HeaderType header = cimMessageObjectFactory.createHeaderType();
-            header.setNoun(getWebServiceName());
-            header.setVerb(HeaderType.Verb.CHANGED);
-            meterConfigEventMessageType.setHeader(header);
-
-            MeterConfigPayloadType meterConfigPayloadType = new MeterConfigPayloadType();
-            meterConfigEventMessageType.setPayload(meterConfigPayloadType);
-
-            ReplyType replyType = cimMessageObjectFactory.createReplyType();
-            replyType.setResult(ReplyType.Result.OK);
-            meterConfigEventMessageType.setReply(replyType);
-
             stateMeterConfigPortServices.stream()
-                    .filter(f -> getProxyFromMeterConfigPortService(f, endPointConfiguration.getUrl()))
+                    .filter(meterConfigPort -> getProxyFromMeterConfigPortService(meterConfigPort, endPointConfiguration.getUrl()))
                     .findFirst()
-                    .ifPresent(meterConfigPort -> {
+                    .ifPresent(meterConfigPortService -> {
                         try {
-                            meterConfigPort.createdMeterConfig(meterConfigEventMessageType);
+                            meterConfigPortService.changedMeterConfig(createResponseMessage(deviceService.findDeviceById(id).get()));
                         } catch (FaultMessage faultMessage) {
                             endPointConfiguration.log(LogLevel.SEVERE, "");
                         }
@@ -98,6 +107,30 @@ public class EndDeviceConfigServiceProvider implements StateTransitionWebService
         } catch (RuntimeException ex) {
             endPointConfiguration.log(LogLevel.SEVERE, ex.getMessage());
         }
+    }
+
+    private MeterConfigEventMessageType createResponseMessage(Device device) {
+        MeterConfigEventMessageType meterConfigEventMessageType = new MeterConfigEventMessageType();
+
+        // set header
+        HeaderType header = cimMessageObjectFactory.createHeaderType();
+        header.setNoun(getWebServiceName());
+        header.setVerb(HeaderType.Verb.CHANGED);
+        meterConfigEventMessageType.setHeader(header);
+
+        // set reply
+        ReplyType replyType = cimMessageObjectFactory.createReplyType();
+        replyType.setResult(ReplyType.Result.OK);
+        meterConfigEventMessageType.setReply(replyType);
+
+        // set payload
+        MeterConfigPayloadType payloadType = meterConfigMessageObjectFactory.createMeterConfigPayloadType();
+        meterConfigEventMessageType.setPayload(payloadType);
+        MeterConfig meterConfig = meterConfigFactory.asMeterConfig(device);
+        payloadType.setMeterConfig(meterConfig);
+        meterConfigEventMessageType.setPayload(payloadType);
+
+        return meterConfigEventMessageType;
     }
 
     private boolean getProxyFromMeterConfigPortService(MeterConfigPort meterConfigPort, String url) throws RuntimeException {
