505a82b241a7272427c4b81ee7734ef40c30acf1
Andrey Lelekov <alelekov@merann.ru>
2018-01-26 12:00:53 +0300

    CXO-8204_8205 update

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/KeyRenewalService.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/KeyRenewalService.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.energyict.mdc.device.data;
 
 import com.elster.jupiter.tasks.RecurrentTask;
--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/pki/tasks/keyrenewal/KeyRenewalTaskExecutor.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/pki/tasks/keyrenewal/KeyRenewalTaskExecutor.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.energyict.mdc.device.data.impl.pki.tasks.keyrenewal;
 
 import com.elster.jupiter.bpm.BpmProcessDefinition;
