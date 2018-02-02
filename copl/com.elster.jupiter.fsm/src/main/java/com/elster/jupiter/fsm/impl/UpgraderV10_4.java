136788a7ec142dbd9ab7451c29b05d24bffffbde
Yan R <yaratay@mera.ru>
2018-02-02 14:19:26 +0300

    CXO-8381: [Upgrade] Upgrade 10.3 -> 10.4 fails on ISU with ORA-00942: table or view does not exist

--- copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/impl/UpgraderV10_4.java
+++ copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/impl/UpgraderV10_4.java
@@ -5,21 +5,27 @@
 package com.elster.jupiter.fsm.impl;
 
 import com.elster.jupiter.events.EventService;
+import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.upgrade.Upgrader;
 
 import javax.inject.Inject;
 
+import static com.elster.jupiter.orm.Version.version;
+
 public class UpgraderV10_4 implements Upgrader {
+    private final DataModel dataModel;
     private final EventService eventService;
 
     @Inject
-    public UpgraderV10_4(EventService eventService) {
+    public UpgraderV10_4(DataModel dataModel, EventService eventService) {
+        this.dataModel = dataModel;
         this.eventService = eventService;
     }
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(dataModel, version(10, 4));
         createNewEventType();
     }
 
