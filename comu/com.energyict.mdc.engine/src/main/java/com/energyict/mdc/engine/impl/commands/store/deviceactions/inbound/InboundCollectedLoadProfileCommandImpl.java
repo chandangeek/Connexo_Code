76029eadf755dcd4f51886faa00c300f24830324
dragos iancu <dragos-florin.iancu@honeywell.com>
2018-02-12 12:43:56 +0200

    CONM-125 - datalogger

--- comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/commands/store/deviceactions/inbound/InboundCollectedLoadProfileCommandImpl.java
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/commands/store/deviceactions/inbound/InboundCollectedLoadProfileCommandImpl.java
@@ -66,7 +66,7 @@ public class InboundCollectedLoadProfileCommandImpl extends LoadProfileCommandIm
                     OfflineLoadProfile offlineLoadProfile = optionalOfflineLoadProfile.get();
                     int configuredNumberOfChannels = offlineLoadProfile.getAllOfflineChannels().size();
                     int receivedNumberOfChannels = collectedLoadProfile.getChannelInfo().size();
-                    if (configuredNumberOfChannels != receivedNumberOfChannels) {
+                    if (configuredNumberOfChannels != receivedNumberOfChannels && !offlineLoadProfile.isDataLoggerSlaveLoadProfile()) {
                         //We received a wrong number of channels
                         addIssue(
                                 getIssueService().newProblem(
