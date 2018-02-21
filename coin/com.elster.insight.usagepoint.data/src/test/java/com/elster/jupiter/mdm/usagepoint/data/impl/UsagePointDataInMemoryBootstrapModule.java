abd011928f3cec25321520bda4a07954b860bd75
Victor Gavrila <victor.gavrila@honeywell.com>
2018-02-21 14:52:33 +0200

    CXO-8519 - Fix unit tests coin

--- coin/com.elster.insight.usagepoint.data/src/test/java/com/elster/jupiter/mdm/usagepoint/data/impl/UsagePointDataInMemoryBootstrapModule.java
+++ coin/com.elster.insight.usagepoint.data/src/test/java/com/elster/jupiter/mdm/usagepoint/data/impl/UsagePointDataInMemoryBootstrapModule.java
@@ -37,6 +37,7 @@ import com.elster.jupiter.pubsub.impl.PubSubModule;
 import com.elster.jupiter.search.impl.SearchModule;
 import com.elster.jupiter.security.thread.ThreadPrincipalService;
 import com.elster.jupiter.security.thread.impl.ThreadSecurityModule;
+import com.elster.jupiter.soap.whiteboard.cxf.impl.WebServicesModule;
 import com.elster.jupiter.tasks.impl.TaskModule;
 import com.elster.jupiter.time.impl.TimeModule;
 import com.elster.jupiter.transaction.TransactionContext;
@@ -57,6 +58,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
+import org.osgi.service.http.HttpService;
 import org.osgi.service.log.LogService;
 
 import java.time.Clock;
@@ -107,7 +109,8 @@ public class UsagePointDataInMemoryBootstrapModule {
                 new UsagePointDataModule(),
                 new BasicPropertiesModule(),
                 new TimeModule(),
-                new SearchModule()
+                new SearchModule(),
+                new WebServicesModule()
         );
         try (TransactionContext ctx = injector.getInstance(TransactionService.class).getContext()) {
             injector.getInstance(ThreadPrincipalService.class);
@@ -184,6 +187,7 @@ public class UsagePointDataInMemoryBootstrapModule {
             bind(LicenseService.class).toInstance(mock(LicenseService.class));
             bind(LogService.class).toInstance(mock(LogService.class));
             bind(UpgradeService.class).toInstance(UpgradeModule.FakeUpgradeService.getInstance());
+            bind(HttpService.class).toInstance(mock(HttpService.class));
         }
     }
 }
