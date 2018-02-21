ac93d0bd81a2021f1baf5e814ebc5df2b811b2d4
Sergey Chashchukhin <sergeych@mera.ru>
2018-02-21 13:57:59 +0300

    CXO-8374 Revoke action for multiple certificates - FE
    correct timeout values

--- copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/store/Timeouts.js
+++ copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/store/Timeouts.js
@@ -9,12 +9,12 @@ Ext.define('Pkj.store.Timeouts', {
     extend: 'Ext.data.Store',
 
     fields: [
-        'value','displayValue'
+        'value', 'displayValue'
     ],
     data: [
-        {value: 500,displayValue: Uni.I18n.translate('HealthCategories.30sec', 'Pkj', '30 sec')},
-        {value: 1000,displayValue: Uni.I18n.translate('HealthCategories.1min', 'Pkj', '1 min')},
-        {value: 2000,displayValue: Uni.I18n.translate('HealthCategories.2min', 'Pkj', '2 min')},
-        {value: 5000,displayValue: Uni.I18n.translate('HealthCategories.5min', 'Pkj', '5 min')}
+        {value: 30000, displayValue: Uni.I18n.translate('HealthCategories.30sec', 'Pkj', '30 sec')},
+        {value: 60000, displayValue: Uni.I18n.translate('HealthCategories.1min', 'Pkj', '1 min')},
+        {value: 120000, displayValue: Uni.I18n.translate('HealthCategories.2min', 'Pkj', '2 min')},
+        {value: 300000, displayValue: Uni.I18n.translate('HealthCategories.5min', 'Pkj', '5 min')}
     ]
 });
