78d8a7b4a97817a3d9434edc689822119f4e86f1
MaxIlyushen <ilyushen@mera.ru>
2018-02-26 16:57:23 +0300

    [CXO-8406]revert

--- copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/view/CertificateActionMenu.js
+++ copl/com.elster.jupiter.pki.extjs/src/main/web/js/pkj/src/view/CertificateActionMenu.js
@@ -11,7 +11,40 @@ Ext.define('Pkj.view.CertificateActionMenu', {
         var me = this;
 
         me.items = [
-
+            {
+                text: Uni.I18n.translate('general.downloadCertificate', 'PKJ', 'Download certificate'),
+                itemId: 'pkj-download-certificate-menu-item',
+                privileges: Pkj.privileges.CertificateManagement.adminCertificates,
+                action: 'downloadCertificate',
+                visible: function(record) {
+                    return !Ext.isEmpty(record) && record.get('hasCertificate');
+                },
+                section: this.SECTION_ACTION
+            },
+            {
+                text: Uni.I18n.translate('general.downloadCSR', 'PKJ', 'Download CSR'),
+                itemId: 'pkj-download-csr-menu-item',
+                privileges: Pkj.privileges.CertificateManagement.adminCertificates,
+                action: 'downloadCSR',
+                visible: function(record) {
+                    return !Ext.isEmpty(record) && record.get('hasCSR');
+                },
+                section: this.SECTION_ACTION
+            },
+            {
+                text: Uni.I18n.translate('general.importCertificate', 'PKJ', 'Import certificate'),
+                itemId: 'pkj-import-certificate-menu-item',
+                privileges: Pkj.privileges.CertificateManagement.adminCertificates,
+                action: 'importCertificate',
+                section: this.SECTION_ACTION
+            },
+            {
+                text: Uni.I18n.translate('general.remove', 'PKJ', 'Remove'),
+                itemId: 'pkj-remove-certificate-menu-item',
+                privileges: Pkj.privileges.CertificateManagement.adminCertificates,
+                action: 'removeCertificate',
+                section: this.SECTION_REMOVE
+            },
             {
                 text: Uni.I18n.translate('general.obsolete', 'PKJ', 'Mark as obsolete'),
                 itemId: 'pkj-obsolete-certificate-menu-item',
