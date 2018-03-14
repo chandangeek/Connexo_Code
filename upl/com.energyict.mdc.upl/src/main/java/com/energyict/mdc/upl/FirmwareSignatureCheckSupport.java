620dd7714f9552a9af1dc4d5b1c681b958f70928
Claudiu Isac <claudiu.isac@elster.com>
2018-03-14 23:16:58 +0200

    CXO-8614 - Implement firmware signature check

---
+++ upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/FirmwareSignatureCheckSupport.java
@@ -0,0 +1,30 @@
+package com.energyict.mdc.upl;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SignatureException;
+
+/**
+ * Provides functionality for firmware signature check
+ * Connexo core can call these methods in order to do the check before the firmware ends up in the device.
+ * The actual check will be done by {@link DeviceProtocol}
+ * <p/>
+ *
+ * Created by H165680 on 3/13/2018.
+ */
+public interface FirmwareSignatureCheckSupport {
+
+    default boolean firmwareSignatureCheckSupported(){
+        return false;
+    }
+
+    default public boolean verifyFirmwareSignature(File firmwareFile, PublicKey pubKey) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, IOException {
+        //By default do not provide support for firmware signature check. Where needed overwrite the method and provide proper implementation for this method
+        throw new NotImplementedException();
+    }
+}
