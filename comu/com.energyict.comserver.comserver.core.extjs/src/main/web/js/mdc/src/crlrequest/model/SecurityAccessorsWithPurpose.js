55dc3b0a54f5c0d75cd986670797ed26af762576
MaxIlyushen <ilyushen@mera.ru>
2018-03-17 13:51:32 +0300

    [CXO-8350]crl-request-WIP

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/crlrequest/model/SecurityAccessorsWithPurpose.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/crlrequest/model/SecurityAccessorsWithPurpose.js
@@ -6,16 +6,8 @@ Ext.define('Mdc.crlrequest.model.SecurityAccessorsWithPurpose', {
     extend: 'Uni.model.Version',
 
     fields: [
-        {name: 'securityAccessorName', type: 'auto', defaultValue: null, persist:false},
-        {name: 'securityAccessor', type: 'auto', defaultValue: null},
-        {name: 'caName', type: 'string', defaultValue: null},
-        {name: 'timeDurationInfo', type: 'auto', defaultValue: null},
-        {name: 'nextRun', type: 'auto', defaultValue: new Date().getTime()}
+        {name: 'id', type: 'auto', defaultValue: null},
+        {name: 'name', type: 'auto', defaultValue: null}
 
-    ],
-
-    proxy: {
-        type: 'rest',
-        url: '/api/ddr/crlprops'
-    }
+    ]
 });
