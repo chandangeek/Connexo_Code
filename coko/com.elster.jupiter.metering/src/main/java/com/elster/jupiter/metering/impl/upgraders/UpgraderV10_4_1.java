8a4814c7860fa150b47489f97da67427252111b8
Anton Fomchenko <antfom@mera.ru>
2018-03-26 16:02:31 +0300

    CXO-8216 install new end device control types when upgrading

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/upgraders/UpgraderV10_4_1.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/upgraders/UpgraderV10_4_1.java
@@ -4,6 +4,9 @@
 
 package com.elster.jupiter.metering.impl.upgraders;
 
+import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.metering.impl.EndDeviceControlTypeInstallerUtil;
+import com.elster.jupiter.metering.impl.ServerMeteringService;
 import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.orm.Version;
@@ -13,13 +16,18 @@ import com.google.common.collect.ImmutableList;
 
 import javax.inject.Inject;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class UpgraderV10_4_1 implements Upgrader {
     private final DataModel dataModel;
+    private final ServerMeteringService meteringService;
+    private final Logger logger;
 
     @Inject
-    public UpgraderV10_4_1(DataModel dataModel) {
+    public UpgraderV10_4_1(DataModel dataModel, ServerMeteringService meteringService) {
         this.dataModel = dataModel;
+        this.meteringService = meteringService;
+        this.logger = Logger.getLogger(this.getClass().getName());
     }
 
     @Override
@@ -32,5 +40,6 @@ public class UpgraderV10_4_1 implements Upgrader {
                 ).forEach(command -> execute(statement, command));
             }
         });
+        new EndDeviceControlTypeInstallerUtil(meteringService).createEndDeviceControlTypes(logger);
     }
 }
