9cd6fe2201e8d4a2f123b7c10691d24b993e6b56
Andrey Lelekov <alelekov@merann.ru>
2018-04-06 16:03:53 +0300

    CRL request task

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/crlrequest/controller/TaskManagementCrlRequest.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/crlrequest/controller/TaskManagementCrlRequest.js
@@ -105,7 +105,9 @@ Ext.define('Mdc.crlrequest.controller.TaskManagementCrlRequest', {
                 },
                 failure: function (record, operation) {
                     if (operation.response.status == 400) {
-                        formErrorsPanel.show();
+                        formErrorsPanel.setVisible(false);
+                        Ext.Msg.alert(Uni.I18n.translate('crlRequest.caname', 'MDC', 'The CA name must be unique'),
+                            Uni.I18n.translate('crlRequest.task', 'MDC', 'CRL request task with such CA name already exist.'));
                         if (!Ext.isEmpty(operation.response.responseText)) {
                             var json = Ext.decode(operation.response.responseText, true);
                             if (json && json.errors) {
@@ -150,6 +152,20 @@ Ext.define('Mdc.crlrequest.controller.TaskManagementCrlRequest', {
                 recurrenceType.setValue(timeDurationInfo.timeUnit);
                 form.form.clearInvalid();
                 editOperationCompleteLoading.call(controller);
+            },
+            failure: function (record, operation) {
+                if (operation.response.status == 400) {
+                    formErrorsPanel.setVisible(false);
+                    Ext.Msg.alert(Uni.I18n.translate('crlRequest.caname', 'MDC', 'The CA name must be unique'),
+                        Uni.I18n.translate('crlRequest.task', 'MDC', 'CRL request task with such CA name already exist.'));
+
+                    if (!Ext.isEmpty(operation.response.responseText)) {
+                        var json = Ext.decode(operation.response.responseText, true);
+                        if (json && json.errors) {
+                            form.getForm().markInvalid(json.errors);
+                        }
+                    }
+                }
             }
         });
     },
