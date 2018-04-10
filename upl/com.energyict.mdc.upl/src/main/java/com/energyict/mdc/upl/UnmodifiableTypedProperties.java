a6c0b855feb670f789ff5e91407ddd1f6c4a2798
Claudiu Isac <claudiu.isac@honeywell.com>
2018-04-10 17:52:20 +0300

    CXO-8740 - Info on the fact, whether protocol/protocoldialect property is overridden or not, is incorrect

--- upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/UnmodifiableTypedProperties.java
+++ upl/com.energyict.mdc.upl/src/main/java/com/energyict/mdc/upl/UnmodifiableTypedProperties.java
@@ -4,6 +4,10 @@
 
 package com.energyict.mdc.upl;
 
+import java.math.BigDecimal;
+import java.util.Properties;
+import java.util.Set;
+
 /**
  * A subclass of TypedProperties which is unmodifiable.
  *
@@ -12,7 +16,10 @@ package com.energyict.mdc.upl;
  */
 public class UnmodifiableTypedProperties extends TypedProperties {
 
+    private final TypedProperties delegate;
+
     public UnmodifiableTypedProperties(TypedProperties delegate) {
+        this.delegate = delegate;
         super.setAllProperties(delegate);
     }
 
@@ -22,8 +29,133 @@ public class UnmodifiableTypedProperties extends TypedProperties {
     }
 
     @Override
+    public Object getProperty(String propertyName) {
+        return delegate.getProperty(propertyName);
+    }
+
+    @Override
+    public <T> T getTypedProperty(String propertyName) {
+        return delegate.getTypedProperty(propertyName);
+    }
+
+    @Override
+    public Object getProperty(String propertyName, Object defaultValue) {
+        return delegate.getProperty(propertyName, defaultValue);
+    }
+
+    @Override
+    public <T> T getTypedProperty(String propertyName, T defaultValue) {
+        return delegate.getTypedProperty(propertyName, defaultValue);
+    }
+
+    @Override
     public void removeProperty(String propertyName) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TypedProperties clone() {
+        return delegate.clone();
+    }
+
+    @Override
+    public int localSize() {
+        return delegate.localSize();
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public String getStringProperty(String propertyName) {
+        return delegate.getStringProperty(propertyName);
+    }
+
+    @Override
+    public BigDecimal getIntegerProperty(String propertyName) {
+        return delegate.getIntegerProperty(propertyName);
+    }
+
+    @Override
+    public BigDecimal getIntegerProperty(String key, BigDecimal defaultValue) {
+        return delegate.getIntegerProperty(key, defaultValue);
+    }
+
+    @Override
+    public Set<String> localPropertyNames() {
+        return delegate.localPropertyNames();
+    }
+
+    @Override
+    public Set<String> propertyNames() {
+        return delegate.propertyNames();
+    }
+
+    @Override
+    public boolean hasLocalValueFor(String propertyName) {
+        return delegate.hasLocalValueFor(propertyName);
+    }
+
+    @Override
+    public boolean isLocalValueFor(Object value, String propertyName) {
+        return delegate.isLocalValueFor(value, propertyName);
+    }
+
+    @Override
+    public boolean hasInheritedValueFor(String propertyName) {
+        return delegate.hasInheritedValueFor(propertyName);
+    }
+
+    @Override
+    public boolean isInheritedValueFor(Object value, String propertyName) {
+        return delegate.isInheritedValueFor(value, propertyName);
+    }
+
+    @Override
+    public boolean hasValueFor(String propertyName) {
+        return delegate.hasValueFor(propertyName);
+    }
+
+    @Override
+    public boolean isValueFor(Object value, String propertyName) {
+        return delegate.isValueFor(value, propertyName);
+    }
+
+    @Override
+    public Properties toStringProperties() {
+        return delegate.toStringProperties();
+    }
+
+    @Override
+    public String toString() {
+        return delegate.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return delegate.equals(other);
+    }
+
+    @Override
+    public com.energyict.mdc.upl.properties.TypedProperties getInheritedProperties() {
+        return delegate.getInheritedProperties();
+    }
+
+    @Override
+    public Object getLocalValue(String propertyName) {
+        return this.delegate.getLocalValue(propertyName);
+    }
+
+    @Override
+    public Object getInheritedValue(String propertyName) {
+        return this.delegate.getInheritedValue(propertyName);
     }
 
 }
