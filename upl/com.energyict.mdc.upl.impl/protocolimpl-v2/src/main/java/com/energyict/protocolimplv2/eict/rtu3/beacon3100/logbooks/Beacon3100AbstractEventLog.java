de0d94447ca539bcea12d2e659319abcdc6af146
H245796 <cristian.ile@honeywell.com>
2018-04-17 09:50:00 +0300

    CXO-8706 Added missing Beacon events, partially mapped events to CIM codes

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtu3/beacon3100/logbooks/Beacon3100AbstractEventLog.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/rtu3/beacon3100/logbooks/Beacon3100AbstractEventLog.java
@@ -5,7 +5,11 @@ import com.energyict.dlms.DataStructure;
 import com.energyict.protocol.MeterEvent;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TimeZone;
 
 /**
  * Copyrights EnergyICT
@@ -57,6 +61,13 @@ public abstract class Beacon3100AbstractEventLog {
         deviceToEIServerCodeMapping.put(0x00c70000, MeterEvent.PLC_G3_UPPERMAC_STARTED);
         deviceToEIServerCodeMapping.put(0x00c80000, MeterEvent.PLC_G3_JOIN_FAILED);
         deviceToEIServerCodeMapping.put(0x00c90000, MeterEvent.PLC_G3_AUTH_FAILURE);
+        //
+        deviceToEIServerCodeMapping.put(0x00ca0000, MeterEvent.PLC_G3_BLACKLIST);
+        deviceToEIServerCodeMapping.put(0x00cb0000, MeterEvent.PLC_G3_NODE_LINK_LOST);
+        deviceToEIServerCodeMapping.put(0x00cc0000, MeterEvent.PLC_G3_NODE_LINK_RECOVERED);
+        deviceToEIServerCodeMapping.put(0x00cd0000, MeterEvent.PLC_G3_PAN_ID);
+        deviceToEIServerCodeMapping.put(0x00ce0000, MeterEvent.PLC_G3_TOPOLOGY_UPDATE);
+        //
         deviceToEIServerCodeMapping.put(0x00200000, MeterEvent.DLMS_SERVER_SESSION_ACCEPTED);
         deviceToEIServerCodeMapping.put(0x00210000, MeterEvent.DLMS_SERVER_SESSION_FINISHED);
         deviceToEIServerCodeMapping.put(0x00220000, MeterEvent.DLMS_OTHER);
@@ -67,25 +78,56 @@ public abstract class Beacon3100AbstractEventLog {
         deviceToEIServerCodeMapping.put(0x00330000, MeterEvent.MODEM_CONNECTED);
         deviceToEIServerCodeMapping.put(0x00340000, MeterEvent.MODEM_DISCONNECTED);
         deviceToEIServerCodeMapping.put(0x00350000, MeterEvent.MODEM_WAKE_UP);
+        //
+        deviceToEIServerCodeMapping.put(0x00362600, MeterEvent.MODEM_NEW_SIM);
+        deviceToEIServerCodeMapping.put(0x00372600, MeterEvent.MODEM_NEW_EQUIPMENT);
+        //
         deviceToEIServerCodeMapping.put(0x00360000, MeterEvent.PROTOCOL_PRELIMINARY_TASK_COMPLETED);
         deviceToEIServerCodeMapping.put(0x00370000, MeterEvent.PROTOCOL_PRELIMINARY_TASK_FAILED);
         deviceToEIServerCodeMapping.put(0x00380000, MeterEvent.PROTOCOL_CONSECUTIVE_FAILURE);
+        //
+        deviceToEIServerCodeMapping.put(0x00390000, MeterEvent.CHECK_DATA_CONCENTRATOR_CONFIG);
+        //
         deviceToEIServerCodeMapping.put(0x00990000, MeterEvent.FIRMWARE_UPGRADE);
         deviceToEIServerCodeMapping.put(0x01000007, MeterEvent.FIRMWARE_MODIFIED);
         deviceToEIServerCodeMapping.put(0x01010000, MeterEvent.CPU_OVERLOAD);
         deviceToEIServerCodeMapping.put(0x01020000, MeterEvent.RAM_TOO_HIGH);
         deviceToEIServerCodeMapping.put(0x01030000, MeterEvent.DISK_USAGE_TOO_HIGH);
-        deviceToEIServerCodeMapping.put(0x01040000, MeterEvent.PACE_EXCEPTION);
+        deviceToEIServerCodeMapping.put(0x01040000, MeterEvent.PACE_EXCEPTION); // TODO SMART_CARD_EXCEPTION?
         deviceToEIServerCodeMapping.put(0x01050000, MeterEvent.SSH_LOGIN);
         deviceToEIServerCodeMapping.put(0x01060000, MeterEvent.FACTORY_RESET);
+        //
+        deviceToEIServerCodeMapping.put(0x01070000, MeterEvent.LINK_UP);
+        deviceToEIServerCodeMapping.put(0x01080000, MeterEvent.LINK_DOWN);
+        deviceToEIServerCodeMapping.put(0x01090000, MeterEvent.USB_ADD);
+        deviceToEIServerCodeMapping.put(0x010a0000, MeterEvent.USB_REMOVE);
+        deviceToEIServerCodeMapping.put(0x010b0000, MeterEvent.FILE_TRANSFER_COMPLETED);
+        deviceToEIServerCodeMapping.put(0x010c0000, MeterEvent.FILE_TRANSFER_FAILED);
+        deviceToEIServerCodeMapping.put(0x010d0000, MeterEvent.SCRIPT_EXECUTION_STARTED);
+        deviceToEIServerCodeMapping.put(0x010e0000, MeterEvent.SCRIPT_EXECUTION_COMPLETED);
+        deviceToEIServerCodeMapping.put(0x010f0000, MeterEvent.SCRIPT_EXECUTION_FAILED);
+        deviceToEIServerCodeMapping.put(0x01100000, MeterEvent.SCRIPT_EXECUTION_SCHEDULED);
+        deviceToEIServerCodeMapping.put(0x01110000, MeterEvent.SCRIPT_EXECUTION_DESCHEDULED);
+        //
         deviceToEIServerCodeMapping.put(0x02000000, MeterEvent.WEBPORTAL_LOGIN);
         deviceToEIServerCodeMapping.put(0x02010000, MeterEvent.WEBPORTAL_ACTION);
         deviceToEIServerCodeMapping.put(0x02020000, MeterEvent.WEBPORTAL_FAILED_LOGIN);
         deviceToEIServerCodeMapping.put(0x02030000, MeterEvent.WEBPORTAL_LOCKED_USER);
+        //
+        deviceToEIServerCodeMapping.put(0x02040000, MeterEvent.WEBPORTAL_CSRF_ATTACK);
+        //
         deviceToEIServerCodeMapping.put(0x03000000, MeterEvent.METER_MULTICAST_UPGRADE_START);
         deviceToEIServerCodeMapping.put(0x03010000, MeterEvent.METER_MULTICAST_UPGRADE_COMPLETED);
         deviceToEIServerCodeMapping.put(0x03020000, MeterEvent.METER_MULTICAST_UPGRADE_FAILED);
         deviceToEIServerCodeMapping.put(0x03030000, MeterEvent.METER_MULTICAST_UPGRADE_INFO);
+        //
+        deviceToEIServerCodeMapping.put(0x00005300, MeterEvent.SNMP_OTHER);
+        deviceToEIServerCodeMapping.put(0x00015300, MeterEvent.SNMP_INFO);
+        deviceToEIServerCodeMapping.put(0x00025300, MeterEvent.SNMP_UNSUPPORTED_VERSION);
+        deviceToEIServerCodeMapping.put(0x00035300, MeterEvent.SNMP_UNSUPPORTED_SEC_MODEL);
+        deviceToEIServerCodeMapping.put(0x00045300, MeterEvent.SNMP_INVALID_USER_NAME);
+        deviceToEIServerCodeMapping.put(0x00055300, MeterEvent.SNMP_INVALID_ENGINE_ID);
+        //
         deviceToEIServerCodeMapping.put(0x00006300, MeterEvent.GENERAL_SECURITY_ERROR);
         deviceToEIServerCodeMapping.put(0x00016300, MeterEvent.WRAP_KEY_ERROR);
         deviceToEIServerCodeMapping.put(0x04006300, MeterEvent.DLMS_AUTHENTICATION_LEVEL_UPDATED);
@@ -95,6 +137,20 @@ public abstract class Beacon3100AbstractEventLog {
         deviceToEIServerCodeMapping.put(0x04046300, MeterEvent.DLMS_ACCESS_VIOLATION);
         deviceToEIServerCodeMapping.put(0x04056300, MeterEvent.DLMS_AUTHENTICATION_FAILURE);
         deviceToEIServerCodeMapping.put(0x04066300, MeterEvent.DLMS_CIPHERING_ERROR);
+        //
+        deviceToEIServerCodeMapping.put(0x04076300, MeterEvent.SNMP_AUTHENTICATION_FAILURE);
+        deviceToEIServerCodeMapping.put(0x04086300, MeterEvent.SNMP_KEYS_UPDATED);
+        deviceToEIServerCodeMapping.put(0x04096300, MeterEvent.CRL_UPDATED);
+        deviceToEIServerCodeMapping.put(0x040a6300, MeterEvent.CRL_UPDATE_REJECTED);
+        deviceToEIServerCodeMapping.put(0x040b6300, MeterEvent.KEY_UPDATE_REQUEST);
+        deviceToEIServerCodeMapping.put(0x040c6300, MeterEvent.CRL_REMOVED);
+        deviceToEIServerCodeMapping.put(0x040d6300, MeterEvent.DOT1X_SUCCESS);
+        deviceToEIServerCodeMapping.put(0x040e6300, MeterEvent.DOT1X_FAILURE);
+        deviceToEIServerCodeMapping.put(0x040f6300, MeterEvent.REPLAY_ATTACK);
+        deviceToEIServerCodeMapping.put(0x04106300, MeterEvent.CERTIFICATE_ADDED);
+        deviceToEIServerCodeMapping.put(0x04116300, MeterEvent.CERTIFICATE_REMOVED);
+        deviceToEIServerCodeMapping.put(0x04126300, MeterEvent.CERTIFICATE_EXPIRED);
+        //
         deviceToEIServerCodeMapping.put(0x00100000, MeterEvent.UNKNOWN_REGISTER);
 
     }
