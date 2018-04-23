bbe265507f1d597924ef476b062ce0e199483fb7
David Nitescu <david.nitescu@honeywell.com>
2018-04-23 10:09:32 +0300

    CXO-8484 Selection of one of the available life cycles during usage point creation - fix change lifecycle

--- coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/UsagePointResource.java
+++ coin/com.elster.insight.usagepoint.data.rest/src/main/java/com/elster/jupiter/mdm/usagepoint/data/rest/impl/UsagePointResource.java
@@ -375,7 +375,7 @@ public class UsagePointResource {
         if (!meteringService.findUsagePointLifeCycle(newLifeCycle)
                 .getStates()
                 .stream()
-                .filter(state -> thesaurus.getString(state.getName(), state.getName()).equals(usagePoint.getState().getName()))
+                .filter(state -> thesaurus.getString(state.getName(), state.getName()).equals(thesaurus.getString(usagePoint.getState().getName(), usagePoint.getState().getName())))
                 .findAny()
                 .isPresent()) {
             return Response.status(Response.Status.BAD_REQUEST).entity(changeUsagePointLifeCycleInfo).build();
