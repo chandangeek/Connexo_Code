fda54b20785c0fb0522325d005e900fbf241ee9a
Ruben Eichelmann <Anton-Ruben.Eichelmann@honeywell.com>
2018-05-08 16:05:03 +0300

    [HSM] Receiving file as argument for init HSM

--- copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/impl/HsmConsoleService.java
+++ copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/impl/HsmConsoleService.java
@@ -6,11 +6,9 @@ package com.elster.jupiter.hsm.impl;
 
 import com.atos.worldline.jss.api.JSSRuntimeControl;
 import com.atos.worldline.jss.configuration.RawConfiguration;
-import com.atos.worldline.jss.configuration.RawConfigurationConverter;
 import org.osgi.service.component.annotations.Component;
 
 import java.io.File;
-import java.io.InputStream;
 
 @Component(name = "com.elster.jupiter.hsm.console", service = {HsmConsoleService.class}, property = {"name=" + "HSM" + ".console", "osgi.command.scope=jupiter", "osgi.command.function=initHsm"}, immediate = true)
 
@@ -19,13 +17,13 @@ public class HsmConsoleService {
 
     public void initHsm(String file) {
         try {
-            //InputStream is = this.getClass().getClassLoader().getResourceAsStream("hsm-runtime-configuration.json");
             File f = new File(file);
             RawConfiguration cfg = new HsmConfigLoader().load(f);
             JSSRuntimeControl.initialize();
             JSSRuntimeControl.newConfiguration(cfg) ;
-        } catch (Exception e){
-            System.out.println(e.getMessage());
+        } catch (Throwable e){
+            System.out.println(e);
+            throw(e);
         }
         System.out.println("JSS initialized");
     }
