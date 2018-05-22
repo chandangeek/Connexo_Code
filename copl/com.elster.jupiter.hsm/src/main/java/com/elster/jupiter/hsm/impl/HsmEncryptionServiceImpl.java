087d67f7ad9a771618525cf3856ab37fd7f0d79c
Ruben Eichelmann <Anton-Ruben.Eichelmann@honeywell.com>
2018-05-22 10:04:42 +0300

    [HSM] Using enc/dec request objects in service

--- copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/impl/HsmEncryptionServiceImpl.java
+++ copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/impl/HsmEncryptionServiceImpl.java
@@ -10,8 +10,6 @@ import com.elster.jupiter.hsm.model.EncryptBaseException;
 
 import com.atos.worldline.jss.api.FunctionFailedException;
 import com.atos.worldline.jss.api.basecrypto.Asymmetric;
-import com.atos.worldline.jss.api.basecrypto.ChainingMode;
-import com.atos.worldline.jss.api.basecrypto.PaddingAlgorithm;
 import com.atos.worldline.jss.api.basecrypto.Symmetric;
 import com.atos.worldline.jss.api.key.KeyLabel;
 import com.atos.worldline.jss.api.key.derivation.KeyDerivation;
@@ -27,25 +25,25 @@ public class HsmEncryptionServiceImpl implements HsmEncryptionService {
     public EncryptResponse encrypt(EncryptRequest eRequest) throws EncryptBaseException {
         this.hsmConfigService.checkInit();
         try {
-            return new EncryptResponse(getEncrypt(eRequest.getBytes(), new KeyLabel(eRequest.getKeyLabel()), eRequest.getType()));
+            return new EncryptResponse(getEncrypt(eRequest));
         } catch (FunctionFailedException e) {
             e.printStackTrace();
             throw new EncryptBaseException(e);
         }
     }
 
-    private byte[] getEncrypt(byte[] bytes, KeyLabel keyLabel, EncryptionType etype) throws FunctionFailedException {
-        if (EncryptionType.SYMMETRIC.equals(etype)) {
-            return Symmetric.encrypt(keyLabel, KeyDerivation.FIXED_KEY_ARRAY, bytes, null, PaddingAlgorithm.ANSI_X9_23, ChainingMode.CBC).getData();
+    private byte[] getEncrypt(EncryptRequest eReq) throws FunctionFailedException {
+        if (EncryptionType.SYMMETRIC.equals(eReq.getType())) {
+            return Symmetric.encrypt(new KeyLabel(eReq.getKeyLabel()), KeyDerivation.FIXED_KEY_ARRAY, eReq.getBytes(), null, Mapper.map(eReq.getPaddingAlgorithm()), Mapper.map(eReq.getChainingMode())).getData();
         }
-        return Asymmetric.encrypt(keyLabel, bytes, PaddingAlgorithm.ANSI_X9_23);
+        return Asymmetric.encrypt(new KeyLabel(eReq.getKeyLabel()), eReq.getBytes(), Mapper.map(eReq.getPaddingAlgorithm()));
     }
 
     @Override
     public DecryptResponse decrypt(DecryptRequest dRequest) throws EncryptBaseException {
         this.hsmConfigService.checkInit();
         try {
-            byte[] decrypt = getDecrypt(dRequest.getKeyLabel(), dRequest.getBytes(), dRequest.getType(), Mapper.map(dRequest.getPaddingAlgorithm()), Mapper.map(dRequest.getChainingMode()));
+            byte[] decrypt = getDecrypt(dRequest);
             return new DecryptResponse(decrypt);
         } catch (FunctionFailedException e) {
             e.printStackTrace();
@@ -54,11 +52,11 @@ public class HsmEncryptionServiceImpl implements HsmEncryptionService {
 
     }
 
-    private byte[] getDecrypt(String label, byte[] bytes, EncryptionType etype, PaddingAlgorithm pAlg, ChainingMode cMode) throws FunctionFailedException {
-        if (EncryptionType.SYMMETRIC.equals(etype)) {
-            return Symmetric.decrypt(new KeyLabel(label), KeyDerivation.FIXED_KEY_ARRAY, bytes, null, pAlg, cMode);
+    private byte[] getDecrypt(DecryptRequest dReq) throws FunctionFailedException {
+        if (EncryptionType.SYMMETRIC.equals(dReq.getType())) {
+            return Symmetric.decrypt(new KeyLabel(dReq.getKeyLabel()), KeyDerivation.FIXED_KEY_ARRAY, dReq.getBytes(), null, Mapper.map(dReq.getPaddingAlgorithm()), Mapper.map(dReq.getChainingMode()));
         }
-      return   Asymmetric.decrypt(new KeyLabel(label), bytes,  PaddingAlgorithm.ANSI_X9_23);
+      return   Asymmetric.decrypt(new KeyLabel(dReq.getKeyLabel()), dReq.getBytes(),  Mapper.map(dReq.getPaddingAlgorithm()));
     }
 
 
