8296045da923bebece45d1663ba00ddca733618d
Ruben Eichelmann <Anton-Ruben.Eichelmann@honeywell.com>
2018-05-22 14:48:17 +0300

    [HSM] Removing rubbish from test (even tough entire test is rubish)

--- copl/com.elster.jupiter.hsm/src/test/java/com/elster/jupiter/hsm/model/MessageTest.java
+++ copl/com.elster.jupiter.hsm/src/test/java/com/elster/jupiter/hsm/model/MessageTest.java
@@ -19,11 +19,6 @@ public class MessageTest {
         Assert.assertArrayEquals(testMsg.getBytes(charSet), msg.getBytes());
         Assert.assertEquals(charSet, msg.getCharSet());
         Assert.assertEquals(testMsg, msg.toString());
-
-        byte[] data =  {61, 16, 2, 116, -108, -42, 73, -92, 112, -103, -39, -126, 44, -125, -85, -80, 55, -43, 100, -113, -60, 9, -37, 105, -113, 40, -99, -18, -1, -86, 70, -120};
-        String s = new Message(data).toString();
-        Assert.assertEquals(s, "");
-
     }
 
 
