c0ae3f7e6bbfc09dba243760fcba71d3420d2845
Victor Gavrila <victor.gavrila@honeywell.com>
2018-05-22 11:32:46 +0300

    CXO-8634 - Estimation Configuration on Purpose - estimate active only

--- coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractEstimationRuleSetResolver.java
+++ coin/com.elster.insight.usagepoint.data/src/main/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractEstimationRuleSetResolver.java
@@ -6,6 +6,7 @@ package com.elster.jupiter.mdm.usagepoint.data.impl;
 
 import com.elster.jupiter.estimation.EstimationResolver;
 import com.elster.jupiter.estimation.EstimationRuleSet;
+import com.elster.jupiter.estimation.EstimationService;
 import com.elster.jupiter.estimation.Priority;
 import com.elster.jupiter.mdm.usagepoint.config.UsagePointConfigurationService;
 import com.elster.jupiter.metering.ChannelsContainer;
@@ -15,6 +16,7 @@ import com.elster.jupiter.metering.MetrologyContractChannelsContainer;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -22,10 +24,16 @@ import java.util.stream.Stream;
 @Component(name = "com.elster.jupiter.mdm.usagepoint.data.MetrologyContractEstimationRuleSetResolver", service = EstimationResolver.class, immediate = true)
 public class MetrologyContractEstimationRuleSetResolver implements EstimationResolver {
     private volatile UsagePointConfigurationService usagePointConfigurationService;
+    private volatile EstimationService estimationService;
 
     @Reference
     public void setUsagePointConfigurationService(UsagePointConfigurationService usagePointConfigurationService) {
         this.usagePointConfigurationService = usagePointConfigurationService;
+    }
+
+    @Reference
+    public void setEstimationService(EstimationService estimationService) {
+        this.estimationService = estimationService;
     }
 
     @Override
@@ -33,8 +41,20 @@ public class MetrologyContractEstimationRuleSetResolver implements EstimationRes
         return true;
     }
 
+
     @Override
     public List<EstimationRuleSet> resolve(ChannelsContainer channelsContainer) {
+        if (estimationService.isEstimationActive(channelsContainer)) {
+            List<EstimationRuleSet> activeRuleSets = estimationService.activeRuleSets(channelsContainer);
+            return this.getLinkedRuleSets(channelsContainer)
+                    .stream()
+                    .filter(activeRuleSets::contains)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    private List<EstimationRuleSet> getLinkedRuleSets(ChannelsContainer channelsContainer){
         return Stream.of(channelsContainer)
                 .filter(container -> container instanceof MetrologyContractChannelsContainer)
                 .map(MetrologyContractChannelsContainer.class::cast)
--- coin/com.elster.insight.usagepoint.data/src/test/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractEstimationRuleSetResolverTest.java
+++ coin/com.elster.insight.usagepoint.data/src/test/java/com/elster/jupiter/mdm/usagepoint/data/impl/MetrologyContractEstimationRuleSetResolverTest.java
@@ -5,14 +5,17 @@
 package com.elster.jupiter.mdm.usagepoint.data.impl;
 
 import com.elster.jupiter.estimation.EstimationRuleSet;
+import com.elster.jupiter.estimation.EstimationService;
 import com.elster.jupiter.mdm.usagepoint.config.UsagePointConfigurationService;
 import com.elster.jupiter.metering.ChannelsContainer;
 import com.elster.jupiter.metering.MetrologyContractChannelsContainer;
 import com.elster.jupiter.metering.config.MetrologyContract;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -27,15 +30,30 @@ public class MetrologyContractEstimationRuleSetResolverTest {
 
     @Mock
     private UsagePointConfigurationService usagePointConfigurationService;
+    @Mock
+    private EstimationService estimationService;
+    @Mock
+    private MetrologyContract metrologyContract;
+    @Mock
+    private MetrologyContractChannelsContainer channelsContainer;
+
+    @Before
+    public void setUp(){
+        when(channelsContainer.getMetrologyContract()).thenReturn(metrologyContract);
+        when(estimationService.isEstimationActive(channelsContainer)).thenReturn(true);
+        when(estimationService.activeRuleSets(channelsContainer)).thenReturn(Collections.emptyList());
+        when(usagePointConfigurationService.getEstimationRuleSets(metrologyContract)).thenReturn(Collections.emptyList());
+    }
 
     private MetrologyContractEstimationRuleSetResolver testInstance() {
         MetrologyContractEstimationRuleSetResolver ruleSetResolver = new MetrologyContractEstimationRuleSetResolver();
         ruleSetResolver.setUsagePointConfigurationService(usagePointConfigurationService);
+        ruleSetResolver.setEstimationService(estimationService);
         return ruleSetResolver;
     }
 
     @Test
-    public void testEmptyRuleSetsForMeterActivationChannelsContainer() {
+    public void whenMeterActivationChannels_thenEmptyRuleSets() {
         ChannelsContainer channelsContainer = mock(ChannelsContainer.class);
 
         List<EstimationRuleSet> estimationRuleSets = testInstance().resolve(channelsContainer);
@@ -44,11 +62,8 @@ public class MetrologyContractEstimationRuleSetResolverTest {
     }
 
     @Test
-    public void testEmptyRuleSetsForMetrologyContractChannelsContainer() {
-        MetrologyContract metrologyContract = mock(MetrologyContract.class);
-        MetrologyContractChannelsContainer channelsContainer = mock(MetrologyContractChannelsContainer.class);
-        when(channelsContainer.getMetrologyContract()).thenReturn(metrologyContract);
-        when(usagePointConfigurationService.getEstimationRuleSets(metrologyContract)).thenReturn(Collections.emptyList());
+    public void whenInactiveEstimation_thenEmptyRuleSets() {
+        when(estimationService.isEstimationActive(channelsContainer)).thenReturn(false);
 
         List<EstimationRuleSet> estimationRuleSets = testInstance().resolve(channelsContainer);
 
@@ -56,12 +71,18 @@ public class MetrologyContractEstimationRuleSetResolverTest {
     }
 
     @Test
-    public void testCorrectRuleSetsForMetrologyContractChannelsContainer() {
-        MetrologyContract metrologyContract = mock(MetrologyContract.class);
-        MetrologyContractChannelsContainer channelsContainer = mock(MetrologyContractChannelsContainer.class);
-        when(channelsContainer.getMetrologyContract()).thenReturn(metrologyContract);
+    public void whenInactiveRuleSets_thenEmptyRuleSets() {
+        List<EstimationRuleSet> estimationRuleSets = testInstance().resolve(channelsContainer);
+
+        assertThat(estimationRuleSets).isEmpty();
+    }
+
+
+    @Test
+    public void whenActiveEstimationAndRuleSets_thenReturnUniqueRuleSets() {
         EstimationRuleSet estimationRuleSet = mock(EstimationRuleSet.class);
         when(usagePointConfigurationService.getEstimationRuleSets(metrologyContract)).thenReturn(Collections.singletonList(estimationRuleSet));
+        when(estimationService.activeRuleSets(channelsContainer)).thenReturn(Arrays.asList(estimationRuleSet, estimationRuleSet));
 
         List<EstimationRuleSet> resolvedEstimationRuleSets = testInstance().resolve(channelsContainer);
 
