6174f89daee27cbc3e619ed94bca4708e8bc44f0
David Nitescu <david.nitescu@honeywell.com>
2018-08-01 13:59:17 +0300

    CONM-211 - Deactivate Application server with active webservices endpoints

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/WebServicesServiceImpl.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/WebServicesServiceImpl.java
@@ -174,7 +174,7 @@ public class WebServicesServiceImpl implements WebServicesService , BundleWaiter
                     .getName();
             logger.info(msg);
             endPointConfiguration.log(LogLevel.FINE, msg);
-            managedEndpoint.stop();
+            stopEndpoint(managedEndpoint);
         });
         endpoints.clear();
     }
@@ -353,5 +353,13 @@ public class WebServicesServiceImpl implements WebServicesService , BundleWaiter
             }
         }
         return new ArrayList<>();
+    }
+
+    private void stopEndpoint(ManagedEndpoint endpoint) {
+        try {
+            endpoint.stop();
+        } catch (Exception ex) {
+
+        }
     }
 }
