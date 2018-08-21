ea78db04d6f243a0028097b9dee5437d24bd851f
Ruben Eichelmann <Anton-Ruben.Eichelmann@honeywell.com>
2018-08-21 13:54:49 +0300

    [CXO-8585] binding proper service for HSM configuration

--- copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/impl/HsmPublicConfigurationImpl.java
+++ copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/impl/HsmPublicConfigurationImpl.java
@@ -1,7 +1,6 @@
 package com.elster.jupiter.hsm.impl;
 
 import com.elster.jupiter.hsm.HsmPublicConfiguration;
-import com.elster.jupiter.hsm.impl.config.HsmConfiguration;
 import com.elster.jupiter.hsm.impl.config.HsmLabelConfiguration;
 import com.elster.jupiter.hsm.model.HsmBaseException;
 
@@ -13,16 +12,16 @@ import java.util.Collection;
 @Component(name = "com.elster.jupiter.hsm.impl.HsmPublicConfigurationImpl", service = {HsmPublicConfiguration.class}, immediate = true, property = "name=" + HsmPublicConfiguration.COMPONENTNAME)
 public class HsmPublicConfigurationImpl implements HsmPublicConfiguration {
 
-    private volatile HsmConfiguration hsmConfiguration;
+    private volatile HsmConfigurationService hsmConfigurationService;
 
     @Override
     public Collection<HsmLabelConfiguration> labels() throws HsmBaseException {
-        return hsmConfiguration.getLabels();
+        return hsmConfigurationService.getHsmConfiguration().getLabels();
     }
 
     @Reference
-    public void setHsmConfiguration(HsmConfiguration hsmConfiguration){
-        this.hsmConfiguration = hsmConfiguration;
+    public void setHsmConfigurationService(HsmConfigurationService hsmConfigurationService){
+        this.hsmConfigurationService = hsmConfigurationService;
     }
 
 }
