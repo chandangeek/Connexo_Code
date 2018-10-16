52d3853ff28536f37fd5c8424f826c729a4fe63e
Cristian Ile <cristian.ile@honeywell.com>
2018-10-16 15:08:23 +0300

    CONM-547 added beacon modulation to connexo modulation logic, added new modulation types to connexo modulation

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/test/java/com/energyict/mdc/MockDeviceTopology.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/test/java/com/energyict/mdc/MockDeviceTopology.java
@@ -193,7 +193,7 @@ public class MockDeviceTopology implements CollectedTopology {
     public void addTopologyNeighbour(DeviceIdentifier neighbour, int modulationSchema, long toneMap, int modulation,
                                      int txGain, int txRes, int txCoeff, int lqi, int phaseDifferential, int tmrValidTime,
                                      int neighbourValidTime, String nodeAddress, int shortAddress, Date lastUpdate,
-                                     Date lastPathRequest, int state, long roundTrip, int linkCost) {
+                                     Date lastPathRequest, int state, long roundTrip, int linkCost, boolean isBeaconModulation) {
         topologyNeighbours.add(
                 new TopologyNeighbour(neighbour, modulationSchema, toneMap, modulation, txGain, txRes, txCoeff, lqi,
                         phaseDifferential, tmrValidTime, neighbourValidTime, nodeAddress, shortAddress, lastUpdate,
