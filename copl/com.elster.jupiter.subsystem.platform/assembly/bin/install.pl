9128b6a55e1f9cfb60079eb94c53ad75ccbf64e8
inester <inester@mera.ru>
2018-12-03 13:42:42 +0300

    CXO-9081 Set Facts header during installation. Update Flow and Facts headers after installation

--- copl/com.elster.jupiter.subsystem.platform/assembly/bin/install.pl
+++ copl/com.elster.jupiter.subsystem.platform/assembly/bin/install.pl
@@ -1560,7 +1560,7 @@ sub update_tomcat_apps_header {
     	my $FLOW_DIR="$TOMCAT_BASE/$TOMCAT_DIR/webapps/flow";
         #set system identifier in the header
 		if ("$SYSTEM_IDENTIFIER" ne "") {
-		    replace_in_file("$FLOW_DIR/org.kie.workbench.KIEWebapp/org.kie.workbench.KIEWebapp.connexo.js", "Connexo Flow.*", "Connexo Flow<span style=\"color:$SYSTEM_IDENTIFIER_COLOR;\"> - $SYSTEM_IDENTIFIER</span></span>");
+		    replace_in_file("$FLOW_DIR/org.kie.workbench.KIEWebapp/org.kie.workbench.KIEWebapp.connexo.js", "Connexo Flow.*</span>", "Connexo Flow<span style=\"color:$SYSTEM_IDENTIFIER_COLOR;\"> - $SYSTEM_IDENTIFIER</span></span>");
 		} else {
 		    replace_in_file("$FLOW_DIR/org.kie.workbench.KIEWebapp/org.kie.workbench.KIEWebapp.connexo.js","Connexo Flow.*</span>", "Connexo Flow</span>");
 		}
