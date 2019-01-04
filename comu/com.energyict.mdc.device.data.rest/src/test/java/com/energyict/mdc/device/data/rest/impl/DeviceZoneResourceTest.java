535512b58b7bace0023e31f3417ce234e8c97c1f
GLOBAL\H165696 <david.nitescu@honeywell.com>
2019-01-04 13:31:27 +0200

    CXO-9225 - REST API for View zones & Zones widget of a device - unit tests for adding and editing of end device zone

--- comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/DeviceZoneResourceTest.java
+++ comu/com.energyict.mdc.device.data.rest/src/test/java/com/energyict/mdc/device/data/rest/impl/DeviceZoneResourceTest.java
@@ -7,11 +7,13 @@ package com.energyict.mdc.device.data.rest.impl;
 import com.elster.jupiter.domain.util.Finder;
 import com.elster.jupiter.metering.EndDevice;
 import com.elster.jupiter.metering.zone.EndDeviceZone;
+import com.elster.jupiter.metering.zone.EndDeviceZoneBuilder;
 import com.elster.jupiter.metering.zone.Zone;
 import com.elster.jupiter.metering.zone.ZoneType;
 
 import com.jayway.jsonpath.JsonModel;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
@@ -19,6 +21,7 @@ import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -36,6 +39,12 @@ public class DeviceZoneResourceTest extends DeviceDataRestApplicationJerseyTest 
     private static final long END_DEVICE_ZONE_ID = 3L;
     private static final long NO_END_DEVICE_ZONE_ID = 33L;
 
+    private static final String APPLICATION = "nameOfApplication";
+    private static final long VERSION = 1L;
+
+    @Mock
+    private EndDeviceZoneBuilder endDeviceZoneBuilder;
+
     @Before
     public void setUp1() {
         EndDevice endDevice = mock(EndDevice.class);
@@ -48,6 +57,26 @@ public class DeviceZoneResourceTest extends DeviceDataRestApplicationJerseyTest 
 
         when(meteringZoneService.getEndDeviceZone(END_DEVICE_ZONE_ID)).thenReturn(Optional.of(endDeviceZone1));
         when(meteringZoneService.getEndDeviceZone(NO_END_DEVICE_ZONE_ID)).thenReturn(Optional.empty());
+
+        Zone zone = mockZone(ZONE_ID, ZONE_NAME, APPLICATION, VERSION, ZONE_TYPE_ID, ZONE_TYPE_NAME);
+        when(meteringZoneService.getZone(ZONE_ID)).thenReturn(Optional.of(zone));
+        when(meteringZoneService.newEndDeviceZoneBuilder()).thenReturn(endDeviceZoneBuilder);
+        when(endDeviceZoneBuilder.withEndDevice(endDevice)).thenReturn(endDeviceZoneBuilder);
+        when(endDeviceZoneBuilder.withZone(zone)).thenReturn(endDeviceZoneBuilder);
+        when(endDeviceZoneBuilder.create()).thenReturn(endDeviceZone1);
+    }
+
+    private Zone mockZone(Long zoneId, String zoneName, String application, long version, long zoneTypeId, String zoneTypeName) {
+        Zone zone = mock(Zone.class);
+        when(zone.getId()).thenReturn(zoneId);
+        when(zone.getName()).thenReturn(zoneName);
+        when(zone.getApplication()).thenReturn(application);
+        when(zone.getVersion()).thenReturn(version);
+        ZoneType zoneType = mock(ZoneType.class);
+        when(zoneType.getId()).thenReturn(zoneTypeId);
+        when(zoneType.getName()).thenReturn(zoneTypeName);
+        when(zone.getZoneType()).thenReturn(zoneType);
+        return zone;
     }
 
     private EndDeviceZone mockEndDeviceZone(long zoneTypeId, String zoneTypeName, long zoneId, String zoneName, long endDeviceZoneId) {
@@ -82,6 +111,22 @@ public class DeviceZoneResourceTest extends DeviceDataRestApplicationJerseyTest 
         JsonModel model = JsonModel.model((ByteArrayInputStream) response.getEntity());
         assertThat(model.<Boolean>get("$.success")).isFalse();
         assertThat(model.<String>get("$.error")).isEqualTo(MessageSeeds.NO_SUCH_DEVICE.getKey());
+    }
+
+    @Test
+    public void testAddZone() {
+        EndDeviceZoneInfo info = new EndDeviceZoneInfo(ZONE_TYPE_NAME, ZONE_NAME, END_DEVICE_ZONE_ID, ZONE_TYPE_ID, ZONE_ID);
+        Entity<EndDeviceZoneInfo> json = Entity.json(info);
+        Response response = target("/devices/" + END_DEVICE_NAME + "/zones").queryParam("name", END_DEVICE_NAME).request().post(json);
+        assertThat(response.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+    }
+
+    @Test
+    public void testEditZone() {
+        EndDeviceZoneInfo info = new EndDeviceZoneInfo(ZONE_TYPE_NAME, ZONE_NAME, END_DEVICE_ZONE_ID, ZONE_TYPE_ID, ZONE_ID);
+        Entity<EndDeviceZoneInfo> json = Entity.json(info);
+        Response response = target("/devices/" + END_DEVICE_NAME + "/zones/" + END_DEVICE_ZONE_ID).request().put(json);
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
     }
 
     @Test
