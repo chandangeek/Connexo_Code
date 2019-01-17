db5e4aa8a70ec45b2345f3dfaaa69b9b230578bf
Jozsef Szekrenyes <jozsef.szekrenyes@honeywell.com>
2019-01-17 09:25:49 +0200

    added test on MeteringZoneService's TranslationKeyProvider implementation removed unused imports

--- coko/com.elster.jupiter.metering.zone/src/test/java/com/elster/jupiter/metering/zone/impl/MeteringZoneImplIT.java
+++ coko/com.elster.jupiter.metering.zone/src/test/java/com/elster/jupiter/metering/zone/impl/MeteringZoneImplIT.java
@@ -4,67 +4,21 @@
 
 package com.elster.jupiter.metering.zone.impl;
 
-import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
-import com.elster.jupiter.bpm.impl.BpmModule;
-import com.elster.jupiter.calendar.impl.CalendarModule;
-import com.elster.jupiter.cps.impl.CustomPropertySetsModule;
-import com.elster.jupiter.datavault.impl.DataVaultModule;
 import com.elster.jupiter.devtools.persistence.test.rules.ExpectedConstraintViolation;
-import com.elster.jupiter.devtools.persistence.test.rules.ExpectedConstraintViolationRule;
 import com.elster.jupiter.devtools.persistence.test.rules.Transactional;
-import com.elster.jupiter.devtools.persistence.test.rules.TransactionalRule;
 import com.elster.jupiter.domain.util.Finder;
-import com.elster.jupiter.domain.util.impl.DomainUtilModule;
-import com.elster.jupiter.events.impl.EventsModule;
-import com.elster.jupiter.fsm.impl.FiniteStateMachineModule;
-import com.elster.jupiter.ids.impl.IdsModule;
-import com.elster.jupiter.license.LicenseService;
-import com.elster.jupiter.messaging.h2.impl.InMemoryMessagingModule;
-import com.elster.jupiter.metering.impl.MeteringModule;
 import com.elster.jupiter.metering.zone.MeteringZoneService;
 import com.elster.jupiter.metering.zone.Zone;
 import com.elster.jupiter.metering.zone.ZoneType;
-import com.elster.jupiter.nls.impl.NlsModule;
-import com.elster.jupiter.orm.impl.OrmModule;
-import com.elster.jupiter.parties.impl.PartyModule;
-import com.elster.jupiter.properties.impl.BasicPropertiesModule;
-import com.elster.jupiter.pubsub.impl.PubSubModule;
-import com.elster.jupiter.search.impl.SearchModule;
-import com.elster.jupiter.security.thread.impl.ThreadSecurityModule;
-import com.elster.jupiter.soap.whiteboard.cxf.impl.WebServicesModule;
-import com.elster.jupiter.tasks.impl.TaskModule;
-import com.elster.jupiter.time.impl.TimeModule;
-import com.elster.jupiter.transaction.TransactionService;
-import com.elster.jupiter.transaction.impl.TransactionModule;
-import com.elster.jupiter.upgrade.UpgradeService;
-import com.elster.jupiter.upgrade.impl.UpgradeModule;
-import com.elster.jupiter.usagepoint.lifecycle.config.impl.UsagePointLifeCycleConfigurationModule;
-import com.elster.jupiter.users.impl.UserModule;
-import com.elster.jupiter.util.UtilModule;
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.TranslationKeyProvider;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.http.HttpService;
-
-import java.util.Dictionary;
-
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MeteringZoneImplIT extends BaseZoneIT {
@@ -146,6 +100,14 @@ public class MeteringZoneImplIT extends BaseZoneIT {
                 .withName(null)
                 .withZoneType(zoneType)
                 .create();
+    }
+
+    @Test
+    @Transactional
+    public void testMeteringServiceProperties() {
+        assertThat(MeteringZoneService.COMPONENTNAME).isEqualTo(((TranslationKeyProvider) meteringZoneService).getComponentName());
+        assertThat(Layer.DOMAIN).isEqualTo(((TranslationKeyProvider) meteringZoneService).getLayer());
+        assertThat(((TranslationKeyProvider) meteringZoneService).getKeys().isEmpty()).isFalse();
     }
 
     private ZoneType createZoneType(MeteringZoneService meteringZoneService) {
