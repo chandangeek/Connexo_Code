ae9accfcf8e80168b668295ae2fca214f67689cd
Yan Polovenko <polovenk@mera.ru>
2019-01-23 14:59:27 +0300

    [CXO-9353] Added public API

--- comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/DeviceTypeInfoFactory.java
+++ comu/com.energyict.mdc.api.rest/src/main/java/com/energyict/mdc/multisense/api/impl/DeviceTypeInfoFactory.java
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
  */
-
 package com.energyict.mdc.multisense.api.impl;
 
+import com.elster.jupiter.cps.RegisteredCustomPropertySet;
 import com.elster.jupiter.rest.api.util.v1.hypermedia.LinkInfo;
 import com.elster.jupiter.rest.api.util.v1.hypermedia.PropertyCopier;
 import com.elster.jupiter.rest.api.util.v1.hypermedia.Relation;
@@ -20,19 +20,21 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
-/**
- * Created by bvn on 4/30/15.
- */
 public class DeviceTypeInfoFactory extends SelectableFieldFactory<DeviceTypeInfo, DeviceType> {
 
+    private final Provider<RegisteredCustomPropertySetInfoFactory> registeredCustomPropertySetInfoFactoryProvider;
     private final Provider<DeviceConfigurationInfoFactory> deviceConfigurationInfoFactoryProvider;
     private final Provider<DeviceMessageFileInfoFactory> deviceMessageFileInfoFactoryProvider;
 
     @Inject
-    public DeviceTypeInfoFactory(Provider<DeviceConfigurationInfoFactory> deviceConfigurationInfoFactory, Provider<DeviceMessageFileInfoFactory> deviceMessageFileInfoFactoryProvider) {
+    public DeviceTypeInfoFactory(Provider<RegisteredCustomPropertySetInfoFactory> registeredCustomPropertySetInfoFactoryProvider,
+                                 Provider<DeviceConfigurationInfoFactory> deviceConfigurationInfoFactory,
+                                 Provider<DeviceMessageFileInfoFactory> deviceMessageFileInfoFactoryProvider) {
+        this.registeredCustomPropertySetInfoFactoryProvider = registeredCustomPropertySetInfoFactoryProvider;
         this.deviceConfigurationInfoFactoryProvider = deviceConfigurationInfoFactory;
         this.deviceMessageFileInfoFactoryProvider = deviceMessageFileInfoFactoryProvider;
     }
@@ -45,7 +47,7 @@ public class DeviceTypeInfoFactory extends SelectableFieldFactory<DeviceTypeInfo
     }
 
     public List<LinkInfo> asLink(Collection<DeviceType> deviceTypes, Relation relation, UriInfo uriInfo) {
-        return deviceTypes.stream().map(i-> asLink(i, relation, uriInfo)).collect(toList());
+        return deviceTypes.stream().map(i -> asLink(i, relation, uriInfo)).collect(toList());
     }
 
     private Link link(DeviceType deviceType, Relation relation, UriInfo uriInfo) {
@@ -67,30 +69,32 @@ public class DeviceTypeInfoFactory extends SelectableFieldFactory<DeviceTypeInfo
         return deviceTypeInfo;
     }
 
-    protected Map<String, PropertyCopier<DeviceTypeInfo,DeviceType>> buildFieldMap() {
+    protected Map<String, PropertyCopier<DeviceTypeInfo, DeviceType>> buildFieldMap() {
         Map<String, PropertyCopier<DeviceTypeInfo, DeviceType>> map = new HashMap<>();
-        map.put("id", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.id = deviceType.getId();
-        });
-        map.put("version", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.version = deviceType.getVersion();
-        });
-        map.put("name", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.name = deviceType.getName();
-        });
-        map.put("link", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.link = link(deviceType, Relation.REF_SELF, uriInfo);
-        });
-        map.put("description", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.description = deviceType.getDescription();
-        });
-        map.put("deviceConfigurations", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.deviceConfigurations = deviceConfigurationInfoFactoryProvider.get().asLink(deviceType.getConfigurations(), Relation.REF_RELATION, uriInfo);
-        });
-        map.put("deviceMessageFiles", (deviceTypeInfo, deviceType, uriInfo) -> {
-            deviceTypeInfo.deviceMessageFiles = deviceMessageFileInfoFactoryProvider.get().asLink(deviceType.getDeviceMessageFiles(), Relation.REF_RELATION, uriInfo);
-        });
+        map.put("id", (deviceTypeInfo, deviceType, uriInfo) -> deviceTypeInfo.id = deviceType.getId());
+        map.put("version", (deviceTypeInfo, deviceType, uriInfo) -> deviceTypeInfo.version = deviceType.getVersion());
+        map.put("name", (deviceTypeInfo, deviceType, uriInfo) -> deviceTypeInfo.name = deviceType.getName());
+        map.put("link", (deviceTypeInfo, deviceType, uriInfo) -> deviceTypeInfo.link = link(deviceType, Relation.REF_SELF, uriInfo));
+        map.put("description", (deviceTypeInfo, deviceType, uriInfo) -> deviceTypeInfo.description = deviceType.getDescription());
+        map.put("customAttributeSets", (deviceTypeInfo, deviceType, uriInfo) ->
+                deviceTypeInfo.customAttributeSets = registeredCustomPropertySetInfoFactoryProvider.get()
+                        .asLink(getRegisteredCustomPropertySetTypeInfos(deviceType), Relation.REF_RELATION, uriInfo));
+        map.put("deviceConfigurations", (deviceTypeInfo, deviceType, uriInfo) ->
+                deviceTypeInfo.deviceConfigurations = deviceConfigurationInfoFactoryProvider.get()
+                        .asLink(deviceType.getConfigurations(), Relation.REF_RELATION, uriInfo));
+        map.put("deviceMessageFiles", (deviceTypeInfo, deviceType, uriInfo) ->
+                deviceTypeInfo.deviceMessageFiles = deviceMessageFileInfoFactoryProvider.get()
+                        .asLink(deviceType.getDeviceMessageFiles(), Relation.REF_RELATION, uriInfo));
         return map;
     }
 
+    public List<RegisteredCustomPropertySetTypeInfo> getRegisteredCustomPropertySetTypeInfos(DeviceType deviceType) {
+        return deviceType.getCustomPropertySets()
+                .stream()
+                .filter(RegisteredCustomPropertySet::isViewableByCurrentUser)
+                .filter(rcps -> !rcps.getCustomPropertySet().isVersioned())
+                .filter(rcps -> rcps.getCustomPropertySet().getDomainClass().equals(DeviceType.class))
+                .map(rcps -> RegisteredCustomPropertySetTypeInfo.builder().from(deviceType, rcps).build())
+                .collect(Collectors.toList());
+    }
 }
