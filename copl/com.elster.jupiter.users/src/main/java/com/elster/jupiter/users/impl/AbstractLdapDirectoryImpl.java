8c9a5bab30172348caa5b7b6421c68ccc9304d71
Alexandru Ganea <aganea@pentalog.fr>
2019-02-15 09:18:11 +0200

    Revert "Initial implementation for ActiveDirectory"
    This reverts commit 705ccfd31fdfd646fb01a337472f13f93924fbd5.

--- copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/AbstractLdapDirectoryImpl.java
+++ copl/com.elster.jupiter.users/src/main/java/com/elster/jupiter/users/impl/AbstractLdapDirectoryImpl.java
@@ -18,7 +18,6 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 import javax.validation.constraints.Size;
-
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -44,11 +43,7 @@ public abstract class AbstractLdapDirectoryImpl extends AbstractUserDirectoryImp
     @Size(max = Table.DESCRIPTION_LENGTH, groups = {Save.Create.class, Save.Update.class}, message = "{" + MessageSeeds.Keys.FIELD_SIZE_BETWEEN_1_AND_4000 + "}")
     private String baseUser;
     @Size(max = Table.DESCRIPTION_LENGTH, groups = {Save.Create.class, Save.Update.class}, message = "{" + MessageSeeds.Keys.FIELD_SIZE_BETWEEN_1_AND_4000 + "}")
-    // group which contains all groups as children so we can check them when checking which groups a specific user belongs to
     private String baseGroup;
-    @Size(max = Table.DESCRIPTION_LENGTH, groups = {Save.Create.class, Save.Update.class}, message = "{" + MessageSeeds.Keys.FIELD_SIZE_BETWEEN_1_AND_4000 + "}")
-    // group which contains users we are interested in
-    private String groupName;
     private Long trustStoreId;
     @Size(max = Table.DESCRIPTION_LENGTH, groups = {Save.Create.class, Save.Update.class}, message = "{" + MessageSeeds.Keys.FIELD_SIZE_BETWEEN_1_AND_4000 + "}")
     private String certificateAlias;
@@ -106,7 +101,7 @@ public abstract class AbstractLdapDirectoryImpl extends AbstractUserDirectoryImp
 
     @Override
     public void setSecurity(String security){
-        securityProtocol = security;
+        this.securityProtocol = security;
     }
 
     @Override
@@ -185,14 +180,6 @@ public abstract class AbstractLdapDirectoryImpl extends AbstractUserDirectoryImp
         userService.getTrustedKeyStoreForUserDirectory(this).ifPresent(sslSecurityProperties::setTrustedStore);
         userService.getKeyStoreForUserDirectory(this, null).ifPresent(sslSecurityProperties::setKeyStore);
         return sslSecurityProperties;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
     }
 
 }
