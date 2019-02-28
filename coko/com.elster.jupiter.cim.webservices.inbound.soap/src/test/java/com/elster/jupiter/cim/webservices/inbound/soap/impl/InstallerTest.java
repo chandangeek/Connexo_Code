c8926a04ba854c06460a32c6e601efe41c0fddcb
Alexandru Ganea <aganea@pentalog.fr>
2019-02-28 13:54:14 +0200

    CXO-9632 Added some unit tests

---
+++ coko/com.elster.jupiter.cim.webservices.inbound.soap/src/test/java/com/elster/jupiter/cim/webservices/inbound/soap/impl/InstallerTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.elster.jupiter.cim.webservices.inbound.soap.impl;
+
+import com.elster.jupiter.cim.webservices.inbound.soap.servicecall.ServiceCallCommands.ServiceCallTypes;
+import com.elster.jupiter.cps.CustomPropertySetService;
+import com.elster.jupiter.cps.RegisteredCustomPropertySet;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.servicecall.ServiceCallService;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InstallerTest {
+
+    @Mock
+    private CustomPropertySetService customPropertySetService;
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private ServiceCallService serviceCallService;
+    @Mock
+    private RegisteredCustomPropertySet registeredCustomPropertySet;
+    @Mock
+    private DataModelUpgrader dataModelUpgrader;
+    @Mock
+    private Logger logger;
+
+    @Test
+    public void testInstall() {
+        when(serviceCallService.findServiceCallType(anyString(), anyString())).thenReturn(Optional.empty());
+        Installer installer = new Installer(serviceCallService, customPropertySetService);
+        when(customPropertySetService.findActiveCustomPropertySet(anyString()))
+                .thenReturn(Optional.of(registeredCustomPropertySet));
+
+        installer.install(dataModelUpgrader, logger);
+
+        verify(serviceCallService, times(ServiceCallTypes.values().length)).createServiceCallType(anyString(),
+                anyString());
+    }
+}
