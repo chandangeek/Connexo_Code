ff5cd457bc5807569c3199c827f32c2d5a4572a0
diambartev <diambartev@P5MD-PORT266.pentalog.com>
2019-02-28 12:38:39 +0200

    CXO-9639 Create implementation of ReplyMasterDataLinkageConfigWebService which sends response by means of generated classes

---
+++ coko/com.elster.jupiter.cim.webservices.inbound.soap/src/main/java/com/elster/jupiter/cim/webservices/inbound/soap/FailedLinkageOperation.java
@@ -0,0 +1,24 @@
+package com.elster.jupiter.cim.webservices.inbound.soap;
+
+public class FailedLinkageOperation extends LinkageOperation {
+
+	private String errorCode;
+	private String errorMessage;
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
