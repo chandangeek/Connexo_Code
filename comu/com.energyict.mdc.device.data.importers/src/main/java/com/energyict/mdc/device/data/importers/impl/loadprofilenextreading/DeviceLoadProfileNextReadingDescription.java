0dc6cc00e09228693d0a38fa704bb9cf415720ed
Laurentia Timar <laurentia.timar@honeywell.com>
2019-03-07 11:20:45 +0200

    CXO-9596 - Data import - Support updating "Next Reading Block" on load profiles

---
+++ comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/loadprofilenextreading/DeviceLoadProfileNextReadingDescription.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.energyict.mdc.device.data.importers.impl.loadprofilenextreading;
+
+import com.elster.jupiter.fileimport.csvimport.FieldParser;
+import com.elster.jupiter.fileimport.csvimport.fields.CommonField;
+import com.elster.jupiter.fileimport.csvimport.fields.FileImportField;
+import com.energyict.mdc.device.data.importers.impl.FileImportDescription;
+import com.energyict.mdc.device.data.importers.impl.parsers.DateParser;
+import com.energyict.mdc.device.data.importers.impl.parsers.LiteralStringParser;
+import com.google.common.collect.ImmutableMap;
+
+import java.time.ZonedDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DeviceLoadProfileNextReadingDescription implements FileImportDescription<DeviceLoadProfileNextReadingRecord> {
+    private final LiteralStringParser stringParser;
+    private final DateParser dateParser;
+   // private final BigDecimalParser bigDecimalParser;
+
+    public DeviceLoadProfileNextReadingDescription(String dateFormat, String timeZone) {
+        stringParser = new LiteralStringParser();
+        this.dateParser = new DateParser(dateFormat, timeZone);
+      //  this.bigDecimalParser = new BigDecimalParser(numberFormat);
+    }
+
+    @Override
+    public DeviceLoadProfileNextReadingRecord getFileImportRecord() {
+        return new DeviceLoadProfileNextReadingRecord();
+    }
+
+    @Override
+    public Map<String, FileImportField<?>> getFields(DeviceLoadProfileNextReadingRecord record) {
+        Map<String, FileImportField<?>> fields = new LinkedHashMap<>();
+        // Device mRID or name
+        fields.put("deviceIdentifier", CommonField.withParser(stringParser)
+                .withName("Device identifier")
+                .withSetter(record::setDeviceIdentifier)
+                .markMandatory()
+                .build());
+        // Reading type mRID
+        fields.put("loadProfileOBIS", CommonField.withParser(stringParser)
+                .withName("Load profile OBIS")
+                .withSetter(record::setLoadProfileOBIS)
+                .markMandatory()
+             //   .markRepetitive()
+                .build());
+        // Next Reading block start date
+        fields.put("nextReadingBlockDateTime", CommonField.withParser(dateParser)
+                .withName("Reading date and time")
+                .withSetter(record::setLoadProfileNextReadingBlockDateTime)
+                .build());
+
+        return fields;
+    }
+
+    @Override
+    public Map<Class, FieldParser> getParsers() {
+        return ImmutableMap.of(
+                String.class, stringParser,
+                ZonedDateTime.class, dateParser
+        );
+    }}
