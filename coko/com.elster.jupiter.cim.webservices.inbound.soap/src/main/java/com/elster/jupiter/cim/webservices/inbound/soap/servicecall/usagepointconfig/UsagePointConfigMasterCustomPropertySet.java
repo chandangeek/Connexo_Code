609de6e626fb62879c0c9f007e126a36face3862
diambartev <diambartev@P5MD-PORT266.pentalog.com>
2019-03-07 10:00:05 +0200

    CXO-9624 Create CAS for service call types - reformatted

--- coko/com.elster.jupiter.cim.webservices.inbound.soap/src/main/java/com/elster/jupiter/cim/webservices/inbound/soap/servicecall/usagepointconfig/UsagePointConfigMasterCustomPropertySet.java
+++ coko/com.elster.jupiter.cim.webservices.inbound.soap/src/main/java/com/elster/jupiter/cim/webservices/inbound/soap/servicecall/usagepointconfig/UsagePointConfigMasterCustomPropertySet.java
@@ -31,165 +31,164 @@ import com.elster.jupiter.servicecall.ServiceCallService;
 import com.google.inject.Module;
 
 @Component(name = "com.elster.jupiter.cim.webservices.inbound.soap.UsagePointConfigMasterCustomPropertySet", service = CustomPropertySet.class, property = "name="
-		+ UsagePointConfigMasterCustomPropertySet.CUSTOM_PROPERTY_SET_NAME, immediate = true)
+        + UsagePointConfigMasterCustomPropertySet.CUSTOM_PROPERTY_SET_NAME, immediate = true)
 public class UsagePointConfigMasterCustomPropertySet
-		implements CustomPropertySet<ServiceCall, UsagePointConfigMasterDomainExtension> {
+        implements CustomPropertySet<ServiceCall, UsagePointConfigMasterDomainExtension> {
 
-	public static final String CUSTOM_PROPERTY_SET_NAME = "UsagePointConfigMasterCustomPropertySet";
+    public static final String CUSTOM_PROPERTY_SET_NAME = "UsagePointConfigMasterCustomPropertySet";
 
-	private volatile PropertySpecService propertySpecService;
-	private volatile Thesaurus thesaurus;
+    private volatile PropertySpecService propertySpecService;
+    private volatile Thesaurus thesaurus;
 
-	public UsagePointConfigMasterCustomPropertySet() {
-	}
+    public UsagePointConfigMasterCustomPropertySet() {
+    }
 
-	@Inject
-	public UsagePointConfigMasterCustomPropertySet(PropertySpecService propertySpecService,
-			CustomPropertySetService customPropertySetService, Thesaurus thesaurus) {
-		setPropertySpecService(propertySpecService);
-		setCustomPropertySetService(customPropertySetService);
-		this.thesaurus = thesaurus;
-	}
+    @Inject
+    public UsagePointConfigMasterCustomPropertySet(PropertySpecService propertySpecService,
+            CustomPropertySetService customPropertySetService, Thesaurus thesaurus) {
+        setPropertySpecService(propertySpecService);
+        setCustomPropertySetService(customPropertySetService);
+        this.thesaurus = thesaurus;
+    }
 
-	@Reference
-	public void setPropertySpecService(PropertySpecService propertySpecService) {
-		this.propertySpecService = propertySpecService;
-	}
+    @Reference
+    public void setPropertySpecService(PropertySpecService propertySpecService) {
+        this.propertySpecService = propertySpecService;
+    }
 
-	@Reference
-	public void setServiceCallService(ServiceCallService serviceCallService) {
-		// PATCH; required for proper startup; do not delete
-	}
+    @Reference
+    public void setServiceCallService(ServiceCallService serviceCallService) {
+        // PATCH; required for proper startup; do not delete
+    }
 
-	@Reference
-	public void setCustomPropertySetService(CustomPropertySetService customPropertySetService) {
-		customPropertySetService.addCustomPropertySet(this);
-	}
+    @Reference
+    public void setCustomPropertySetService(CustomPropertySetService customPropertySetService) {
+        customPropertySetService.addCustomPropertySet(this);
+    }
 
-	@Reference
-	public void setNlsService(NlsService nlsService) {
-		thesaurus = nlsService.getThesaurus(CIMInboundSoapEndpointsActivator.COMPONENT_NAME, Layer.SOAP);
-	}
+    @Reference
+    public void setNlsService(NlsService nlsService) {
+        thesaurus = nlsService.getThesaurus(CIMInboundSoapEndpointsActivator.COMPONENT_NAME, Layer.SOAP);
+    }
 
-	@Override
-	public String getName() {
-		return UsagePointConfigMasterCustomPropertySet.class.getSimpleName();
-	}
+    @Override
+    public String getName() {
+        return UsagePointConfigMasterCustomPropertySet.class.getSimpleName();
+    }
 
-	@Override
-	public Class<ServiceCall> getDomainClass() {
-		return ServiceCall.class;
-	}
+    @Override
+    public Class<ServiceCall> getDomainClass() {
+        return ServiceCall.class;
+    }
 
-	@Override
-	public String getDomainClassDisplayName() {
-		return thesaurus.getFormat(TranslationKeys.DOMAIN_NAME).format();
-	}
+    @Override
+    public String getDomainClassDisplayName() {
+        return thesaurus.getFormat(TranslationKeys.DOMAIN_NAME).format();
+    }
 
-	@Override
-	public PersistenceSupport<ServiceCall, UsagePointConfigMasterDomainExtension> getPersistenceSupport() {
-		return new UsagePointConfigMasterCustomPropertyPersistenceSupport();
-	}
+    @Override
+    public PersistenceSupport<ServiceCall, UsagePointConfigMasterDomainExtension> getPersistenceSupport() {
+        return new UsagePointConfigMasterCustomPropertyPersistenceSupport();
+    }
 
-	@Override
-	public boolean isRequired() {
-		return true;
-	}
+    @Override
+    public boolean isRequired() {
+        return true;
+    }
 
-	@Override
-	public boolean isVersioned() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean isVersioned() {
+        return false;
+    }
 
-	@Override
-	public Set<ViewPrivilege> defaultViewPrivileges() {
-		return Collections.emptySet();
-	}
+    @Override
+    public Set<ViewPrivilege> defaultViewPrivileges() {
+        return Collections.emptySet();
+    }
 
-	@Override
-	public Set<EditPrivilege> defaultEditPrivileges() {
-		return Collections.emptySet();
-	}
+    @Override
+    public Set<EditPrivilege> defaultEditPrivileges() {
+        return Collections.emptySet();
+    }
 
-	@Override
-	public List<PropertySpec> getPropertySpecs() {
-		return Arrays.asList(
-				propertySpecService.bigDecimalSpec()
-						.named(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_SUCCESS.javaName(),
-								TranslationKeys.CALLS_SUCCESS)
-						.describedAs(TranslationKeys.CALLS_SUCCESS).fromThesaurus(thesaurus).finish(),
-				propertySpecService.bigDecimalSpec()
-						.named(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_FAILED.javaName(),
-								TranslationKeys.CALLS_ERROR)
-						.describedAs(TranslationKeys.CALLS_ERROR).fromThesaurus(thesaurus).finish(),
-				propertySpecService.bigDecimalSpec()
-						.named(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_EXPECTED.javaName(),
-								TranslationKeys.CALLS_EXPECTED)
-						.describedAs(TranslationKeys.CALLS_EXPECTED).fromThesaurus(thesaurus).finish(),
-				propertySpecService.stringSpec()
-						.named(UsagePointConfigMasterDomainExtension.FieldNames.CALLBACK_URL.javaName(),
-								TranslationKeys.CALLBACK_URL)
-						.describedAs(TranslationKeys.CALLBACK_URL).fromThesaurus(thesaurus).finish());
-	}
+    @Override
+    public List<PropertySpec> getPropertySpecs() {
+        return Arrays.asList(
+                propertySpecService.bigDecimalSpec()
+                        .named(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_SUCCESS.javaName(),
+                                TranslationKeys.CALLS_SUCCESS)
+                        .describedAs(TranslationKeys.CALLS_SUCCESS).fromThesaurus(thesaurus).finish(),
+                propertySpecService.bigDecimalSpec()
+                        .named(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_FAILED.javaName(),
+                                TranslationKeys.CALLS_ERROR)
+                        .describedAs(TranslationKeys.CALLS_ERROR).fromThesaurus(thesaurus).finish(),
+                propertySpecService.bigDecimalSpec()
+                        .named(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_EXPECTED.javaName(),
+                                TranslationKeys.CALLS_EXPECTED)
+                        .describedAs(TranslationKeys.CALLS_EXPECTED).fromThesaurus(thesaurus).finish(),
+                propertySpecService.stringSpec()
+                        .named(UsagePointConfigMasterDomainExtension.FieldNames.CALLBACK_URL.javaName(),
+                                TranslationKeys.CALLBACK_URL)
+                        .describedAs(TranslationKeys.CALLBACK_URL).fromThesaurus(thesaurus).finish());
+    }
 
-	private class UsagePointConfigMasterCustomPropertyPersistenceSupport
-			implements PersistenceSupport<ServiceCall, UsagePointConfigMasterDomainExtension> {
+    private class UsagePointConfigMasterCustomPropertyPersistenceSupport
+            implements PersistenceSupport<ServiceCall, UsagePointConfigMasterDomainExtension> {
 
-		private static final String TABLE_NAME = "UCP_MSC_WS1";
-		private static final String FK = "FK_UCP_MSC_WS1";
+        private static final String TABLE_NAME = "UCP_MSC_WS1";
+        private static final String FK = "FK_UCP_MSC_WS1";
 
-		@Override
-		public String componentName() {
-			return "UCM";
-		}
+        @Override
+        public String componentName() {
+            return "UCM";
+        }
 
-		@Override
-		public String tableName() {
-			return TABLE_NAME;
-		}
+        @Override
+        public String tableName() {
+            return TABLE_NAME;
+        }
 
-		@Override
-		public String domainFieldName() {
-			return UsagePointConfigMasterDomainExtension.FieldNames.DOMAIN.javaName();
-		}
+        @Override
+        public String domainFieldName() {
+            return UsagePointConfigMasterDomainExtension.FieldNames.DOMAIN.javaName();
+        }
 
-		@Override
-		public String domainForeignKeyName() {
-			return FK;
-		}
+        @Override
+        public String domainForeignKeyName() {
+            return FK;
+        }
 
-		@Override
-		public Class<UsagePointConfigMasterDomainExtension> persistenceClass() {
-			return UsagePointConfigMasterDomainExtension.class;
-		}
+        @Override
+        public Class<UsagePointConfigMasterDomainExtension> persistenceClass() {
+            return UsagePointConfigMasterDomainExtension.class;
+        }
 
-		@Override
-		public Optional<Module> module() {
-			return Optional.empty();
-		}
+        @Override
+        public Optional<Module> module() {
+            return Optional.empty();
+        }
 
-		@Override
-		public List<Column> addCustomPropertyPrimaryKeyColumnsTo(Table table) {
-			return Collections.emptyList();
-		}
+        @Override
+        public List<Column> addCustomPropertyPrimaryKeyColumnsTo(Table table) {
+            return Collections.emptyList();
+        }
 
-		@Override
-		public void addCustomPropertyColumnsTo(Table table, List<Column> customPrimaryKeyColumns) {
-			table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_SUCCESS.databaseName()).number()
-					.map(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_SUCCESS.javaName()).notNull().add();
-			table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_FAILED.databaseName()).number()
-					.map(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_FAILED.javaName()).notNull().add();
-			table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_EXPECTED.databaseName()).number()
-					.map(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_EXPECTED.javaName()).notNull().add();
-			table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLBACK_URL.databaseName()).varChar()
-					.map(UsagePointConfigMasterDomainExtension.FieldNames.CALLBACK_URL.javaName()).notNull(false).add();
-		}
+        @Override
+        public void addCustomPropertyColumnsTo(Table table, List<Column> customPrimaryKeyColumns) {
+            table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_SUCCESS.databaseName()).number()
+                    .map(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_SUCCESS.javaName()).notNull().add();
+            table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_FAILED.databaseName()).number()
+                    .map(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_FAILED.javaName()).notNull().add();
+            table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_EXPECTED.databaseName()).number()
+                    .map(UsagePointConfigMasterDomainExtension.FieldNames.CALLS_EXPECTED.javaName()).notNull().add();
+            table.column(UsagePointConfigMasterDomainExtension.FieldNames.CALLBACK_URL.databaseName()).varChar()
+                    .map(UsagePointConfigMasterDomainExtension.FieldNames.CALLBACK_URL.javaName()).notNull(false).add();
+        }
 
-		@Override
-		public String application() {
-			return DataLinkageConfigChecklist.APPLICATION_NAME;
-		}
-	}
+        @Override
+        public String application() {
+            return DataLinkageConfigChecklist.APPLICATION_NAME;
+        }
+    }
 
 }
