9cec188933aec1f59be7f589fc69cc2683d12afd
diambartev <diambartev@P5MD-PORT266.pentalog.com>
2019-03-07 10:00:05 +0200

    CXO-9624 Create CAS for service call types - reformatted

--- coko/com.elster.jupiter.cim.webservices.inbound.soap/src/main/java/com/elster/jupiter/cim/webservices/inbound/soap/servicecall/usagepointconfig/UsagePointConfigDomainExtension.java
+++ coko/com.elster.jupiter.cim.webservices.inbound.soap/src/main/java/com/elster/jupiter/cim/webservices/inbound/soap/servicecall/usagepointconfig/UsagePointConfigDomainExtension.java
@@ -16,113 +16,115 @@ import com.elster.jupiter.orm.associations.Reference;
 import com.elster.jupiter.servicecall.ServiceCall;
 
 public class UsagePointConfigDomainExtension extends AbstractPersistentDomainExtension
-		implements PersistentDomainExtension<ServiceCall> {
+        implements PersistentDomainExtension<ServiceCall> {
 
-	public enum FieldNames {
-		DOMAIN("serviceCall", "serviceCall"), USAGE_POINT("usagePoint", "usagePoint"),
-		PARENT_SERVICE_CALL("parentServiceCallId", "parentServiceCallId"),
-		ERROR_MESSAGE("errorMessage", "errorMessage"), ERROR_CODE("errorCode", "errorCode"),
-		OPERATION("operation", "operation");
+    public enum FieldNames {
+        DOMAIN("serviceCall", "serviceCall"),
+        USAGE_POINT("usagePoint", "usagePoint"),
+        PARENT_SERVICE_CALL("parentServiceCallId", "parentServiceCallId"),
+        ERROR_MESSAGE("errorMessage", "errorMessage"),
+        ERROR_CODE("errorCode", "errorCode"),
+        OPERATION("operation", "operation");
 
-		FieldNames(String javaName, String databaseName) {
-			this.javaName = javaName;
-			this.databaseName = databaseName;
-		}
+        FieldNames(String javaName, String databaseName) {
+            this.javaName = javaName;
+            this.databaseName = databaseName;
+        }
 
-		private final String javaName;
-		private final String databaseName;
+        private final String javaName;
+        private final String databaseName;
 
-		public String javaName() {
-			return javaName;
-		}
+        public String javaName() {
+            return javaName;
+        }
 
-		public String databaseName() {
-			return databaseName;
-		}
-	}
+        public String databaseName() {
+            return databaseName;
+        }
+    }
 
-	private Reference<ServiceCall> serviceCall = Reference.empty();
+    private Reference<ServiceCall> serviceCall = Reference.empty();
 
-	@Size(max = Table.MAX_STRING_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
-			+ MessageSeeds.Keys.FIELD_TOO_LONG + "}")
-	private String usagePoint;
-	@NotNull(message = "{" + MessageSeeds.Keys.THIS_FIELD_IS_REQUIRED + "}")
-	private BigDecimal parentServiceCallId;
-	@Size(max = Table.MAX_STRING_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
-			+ MessageSeeds.Keys.FIELD_TOO_LONG + "}")
-	private String errorMessage;
-	@Size(max = Table.NAME_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
-			+ MessageSeeds.Keys.FIELD_TOO_LONG + "}")
-	private String errorCode;
-	@NotNull(message = "{" + MessageSeeds.Keys.THIS_FIELD_IS_REQUIRED + "}")
-	@Size(max = Table.NAME_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
-			+ MessageSeeds.Keys.FIELD_TOO_LONG + "}")
-	private String operation;
+    @Size(max = Table.MAX_STRING_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
+            + MessageSeeds.Keys.FIELD_TOO_LONG + "}")
+    private String usagePoint;
+    @NotNull(message = "{" + MessageSeeds.Keys.THIS_FIELD_IS_REQUIRED + "}")
+    private BigDecimal parentServiceCallId;
+    @Size(max = Table.MAX_STRING_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
+            + MessageSeeds.Keys.FIELD_TOO_LONG + "}")
+    private String errorMessage;
+    @Size(max = Table.NAME_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
+            + MessageSeeds.Keys.FIELD_TOO_LONG + "}")
+    private String errorCode;
+    @NotNull(message = "{" + MessageSeeds.Keys.THIS_FIELD_IS_REQUIRED + "}")
+    @Size(max = Table.NAME_LENGTH, groups = { Save.Create.class, Save.Update.class }, message = "{"
+            + MessageSeeds.Keys.FIELD_TOO_LONG + "}")
+    private String operation;
 
-	public String getUsagePoint() {
-		return usagePoint;
-	}
+    public String getUsagePoint() {
+        return usagePoint;
+    }
 
-	public void setUsagePoint(String usagePoint) {
-		this.usagePoint = usagePoint;
-	}
+    public void setUsagePoint(String usagePoint) {
+        this.usagePoint = usagePoint;
+    }
 
-	public BigDecimal getParentServiceCallId() {
-		return parentServiceCallId;
-	}
+    public BigDecimal getParentServiceCallId() {
+        return parentServiceCallId;
+    }
 
-	public void setParentServiceCallId(BigDecimal parentServiceCallId) {
-		this.parentServiceCallId = parentServiceCallId;
-	}
+    public void setParentServiceCallId(BigDecimal parentServiceCallId) {
+        this.parentServiceCallId = parentServiceCallId;
+    }
 
-	public String getErrorMessage() {
-		return errorMessage;
-	}
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 
-	public String getErrorCode() {
-		return errorCode;
-	}
+    public String getErrorCode() {
+        return errorCode;
+    }
 
-	public void setErrorCode(String errorCode) {
-		this.errorCode = errorCode;
-	}
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
 
-	public String getOperation() {
-		return operation;
-	}
+    public String getOperation() {
+        return operation;
+    }
 
-	public void setOperation(String operation) {
-		this.operation = operation;
-	}
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
 
-	@Override
-	public void copyFrom(ServiceCall serviceCall, CustomPropertySetValues propertyValues,
-			Object... additionalPrimaryKeyValues) {
-		this.serviceCall.set(serviceCall);
-		setUsagePoint((String) propertyValues.getProperty(FieldNames.USAGE_POINT.javaName));
-		setParentServiceCallId(new BigDecimal(
-				Optional.ofNullable(propertyValues.getProperty(FieldNames.PARENT_SERVICE_CALL.javaName()))
-						.orElse(BigDecimal.ZERO).toString()));
-		setErrorMessage((String) propertyValues.getProperty(FieldNames.ERROR_MESSAGE.javaName()));
-		setErrorCode((String) propertyValues.getProperty(FieldNames.ERROR_CODE.javaName()));
-		setOperation((String) propertyValues.getProperty(FieldNames.OPERATION.javaName()));
-	}
+    @Override
+    public void copyFrom(ServiceCall serviceCall, CustomPropertySetValues propertyValues,
+            Object... additionalPrimaryKeyValues) {
+        this.serviceCall.set(serviceCall);
+        setUsagePoint((String) propertyValues.getProperty(FieldNames.USAGE_POINT.javaName));
+        setParentServiceCallId(new BigDecimal(
+                Optional.ofNullable(propertyValues.getProperty(FieldNames.PARENT_SERVICE_CALL.javaName()))
+                        .orElse(BigDecimal.ZERO).toString()));
+        setErrorMessage((String) propertyValues.getProperty(FieldNames.ERROR_MESSAGE.javaName()));
+        setErrorCode((String) propertyValues.getProperty(FieldNames.ERROR_CODE.javaName()));
+        setOperation((String) propertyValues.getProperty(FieldNames.OPERATION.javaName()));
+    }
 
-	@Override
-	public void copyTo(CustomPropertySetValues propertySetValues, Object... additionalPrimaryKeyValues) {
-		propertySetValues.setProperty(FieldNames.USAGE_POINT.javaName(), getUsagePoint());
-		propertySetValues.setProperty(FieldNames.PARENT_SERVICE_CALL.javaName(), getParentServiceCallId());
-		propertySetValues.setProperty(FieldNames.ERROR_MESSAGE.javaName(), getErrorMessage());
-		propertySetValues.setProperty(FieldNames.ERROR_CODE.javaName(), getErrorCode());
-		propertySetValues.setProperty(FieldNames.OPERATION.javaName(), getOperation());
-	}
+    @Override
+    public void copyTo(CustomPropertySetValues propertySetValues, Object... additionalPrimaryKeyValues) {
+        propertySetValues.setProperty(FieldNames.USAGE_POINT.javaName(), getUsagePoint());
+        propertySetValues.setProperty(FieldNames.PARENT_SERVICE_CALL.javaName(), getParentServiceCallId());
+        propertySetValues.setProperty(FieldNames.ERROR_MESSAGE.javaName(), getErrorMessage());
+        propertySetValues.setProperty(FieldNames.ERROR_CODE.javaName(), getErrorCode());
+        propertySetValues.setProperty(FieldNames.OPERATION.javaName(), getOperation());
+    }
 
-	@Override
-	public void validateDelete() {
-	}
+    @Override
+    public void validateDelete() {
+    }
 
 }
