b5e3c1b129b9180c47b677bc56cf8c10b804f4be
diambartev <diambartev@P5MD-PORT266.pentalog.com>
2019-03-11 15:19:50 +0200

    CXO-9626 Create implementation of ReplyUsagePointConfigWebService - partial commit

--- coko/com.elster.jupiter.cim.webservices.outbound.soap/src/main/java/com/elster/jupiter/cim/webservices/outbound/soap/ReplyUsagePointConfigWebService.java
+++ coko/com.elster.jupiter.cim.webservices.outbound.soap/src/main/java/com/elster/jupiter/cim/webservices/outbound/soap/ReplyUsagePointConfigWebService.java
@@ -6,6 +6,7 @@ package com.elster.jupiter.cim.webservices.outbound.soap;
 import com.elster.jupiter.soap.whiteboard.cxf.EndPointConfiguration;
 
 import aQute.bnd.annotation.ConsumerType;
+import ch.iec.tc57._2011.usagepointconfig.UsagePoint;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -13,18 +14,21 @@ import java.util.List;
 @ConsumerType
 public interface ReplyUsagePointConfigWebService {
 
-	String NAME = "CIM ReplyUsagePointConfigConfig";
+    String NAME = "CIM ReplyUsagePointConfigConfig";
 
-	/**
-	 * Invoked by the service call when the async inbound WS is completed
-	 *
-	 * @param endPointConfiguration - the outbound end point
-	 * @param operation             - the operation that has been performed ("CREATE" or "CLOSE")
-	 * @param successfulLinkages    - the list of successfully created linkages between usagePoints and meters
-	 * @param failedLinkages        - the list of failed linkage creation attempts (between usagePoints and meters) with error message code and description
-	 * @param expectedNumberOfCalls - the expected number of child calls
-	 */
-	void call(EndPointConfiguration endPointConfiguration, String operation,
-			List<LinkageOperation> successfulLinkages, List<FailedLinkageOperation> failedLinkages,
-			BigDecimal expectedNumberOfCalls);
+    /**
+     * Invoked by the service call when the async inbound WS is completed
+     *
+     * @param endPointConfiguration
+     *            - the outbound end point
+     * @param operation
+     *            - the operation that has been performed ("CREATE" or "CLOSE")
+     * @param successfulLinkages
+     *            - the list of successfully created linkages between usagePoints and meters
+     * @param failedLinkages
+     *            - the list of failed linkage creation attempts (between usagePoints and meters) with error message code and description
+     * @param expectedNumberOfCalls
+     *            - the expected number of child calls
+     */
+    void call(EndPointConfiguration endPointConfiguration, String operation, List<UsagePoint> successList,List<FailedUsagePointOperation> failureList, BigDecimal expectedNumberOfCalls);
 }
