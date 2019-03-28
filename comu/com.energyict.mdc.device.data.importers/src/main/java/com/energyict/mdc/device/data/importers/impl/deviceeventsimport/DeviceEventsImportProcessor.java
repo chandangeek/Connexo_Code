7901178de760df67b4ef5cb73c1e32716a008b9c
Claudiu Isac <claudiu.isac@honeywell.com>
2019-03-28 15:59:05 +0200

    CXO-9691 - Implement device events importer Validate the reading date set on imported events: - check that reading date is after meter was activated - check that reading date is before meter was deactivated - check that reading date is after event date and if not set reading date to have same value as event date

--- comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/deviceeventsimport/DeviceEventsImportProcessor.java
+++ comu/com.energyict.mdc.device.data.importers/src/main/java/com/energyict/mdc/device/data/importers/impl/deviceeventsimport/DeviceEventsImportProcessor.java
@@ -1,8 +1,10 @@
 package com.energyict.mdc.device.data.importers.impl.deviceeventsimport;
 
 import com.elster.jupiter.fileimport.csvimport.exceptions.ProcessorException;
+import com.elster.jupiter.metering.MeterActivation;
 import com.elster.jupiter.metering.readings.beans.EndDeviceEventImpl;
 import com.elster.jupiter.metering.readings.beans.MeterReadingImpl;
+import com.elster.jupiter.util.time.DefaultDateTimeFormatters;
 import com.energyict.mdc.device.data.Device;
 import com.energyict.mdc.device.data.LogBook;
 import com.energyict.mdc.device.data.importers.impl.AbstractDeviceDataFileImportProcessor;
@@ -11,6 +13,8 @@ import com.energyict.mdc.device.data.importers.impl.FileImportLogger;
 import com.energyict.mdc.device.data.importers.impl.MessageSeeds;
 
 import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public class DeviceEventsImportProcessor extends AbstractDeviceDataFileImportProcessor<DeviceEventsImportRecord> {
@@ -39,7 +43,47 @@ public class DeviceEventsImportProcessor extends AbstractDeviceDataFileImportPro
         endDeviceEvent.setType(data.getDeviceCode());
         endDeviceEvent.setLogBookPosition(Integer.parseInt(data.getEventLogID()));
         meterReading.addEndDeviceEvent(endDeviceEvent);
-        device.store(meterReading, data.getReadingDate() != null ? data.getReadingDate().toInstant() : null);
+        device.store(meterReading, getValidReadingDate(data, eventTime));
+    }
+
+    private Instant getValidReadingDate(DeviceEventsImportRecord data, Instant eventTime) {
+        Instant readingDate;
+        if(data.getReadingDate() != null) {
+            validateReadingDate(device, data.getDateTime(), data.getReadingDate(), data.getLineNumber());
+            readingDate = data.getReadingDate().toInstant();
+            return eventTime.isBefore(readingDate) ? readingDate : eventTime;
+        }
+        return null;
+    }
+
+    private void validateReadingDate(Device device, ZonedDateTime eventDate, ZonedDateTime readingDate, long lineNumber) {
+        List<MeterActivation> meterActivations = device.getMeterActivationsMostRecentFirst();
+        if (!hasMeterActivationEffectiveAt(meterActivations, readingDate.toInstant())) {
+            MeterActivation firstMeterActivation = meterActivations.get(meterActivations.size() - 1);
+            if (firstMeterActivation.getRange().hasLowerBound() && !readingDate.toInstant().isAfter(firstMeterActivation.getStart())) {
+                throw new ProcessorException(MessageSeeds.READING_DATE_BEFORE_METER_ACTIVATION, lineNumber,
+                        DefaultDateTimeFormatters.shortDate().withShortTime().build().format(readingDate));
+            }
+            MeterActivation lastMeterActivation = meterActivations.get(0);
+            if (lastMeterActivation.getRange().hasUpperBound() && readingDate.toInstant().isAfter(lastMeterActivation.getEnd())) {
+                throw new ProcessorException(MessageSeeds.READING_DATE_AFTER_METER_ACTIVATION, lineNumber,
+                        DefaultDateTimeFormatters.shortDate().withShortTime().build().format(readingDate));
+            }
+        }
+        //reading date MUST be after event date
+        if(readingDate.isBefore(eventDate)) {
+            throw new ProcessorException(MessageSeeds.READING_DATE_BEFORE_EVENT_DATE, lineNumber,
+                    DefaultDateTimeFormatters.shortDate().withShortTime().build().format(readingDate),
+                    DefaultDateTimeFormatters.shortDate().withShortTime().build().format(eventDate));
+        }
+    }
+
+    private boolean hasMeterActivationEffectiveAt(List<MeterActivation> meterActivations, Instant timeStamp) {
+        return meterActivations.isEmpty() || getMeterActivationEffectiveAt(meterActivations, timeStamp).isPresent();
+    }
+
+    private Optional<MeterActivation> getMeterActivationEffectiveAt(List<MeterActivation> meterActivations, Instant timeStamp) {
+        return meterActivations.stream().filter(ma -> ma.getInterval().toOpenClosedRange().contains(timeStamp)).findFirst();
     }
 
     private Optional<LogBook> getLogBookByOBIS(Device device, String logbookOBIScode) {
