ae18dbcdc8c149888cde4ff770a8cdcc61710207
Laurentia Timar <laurentia.timar@honeywell.com>
2019-04-13 10:06:44 +0300

    CXO-9377 - Start process Device Lifecycle Issue (existing process) add start process in history 2

--- comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceHistory.js
+++ comu/com.energyict.comserver.comserver.core.extjs/src/main/web/js/mdc/src/controller/setup/DeviceHistory.js
@@ -284,7 +284,7 @@ Ext.define('Mdc.controller.setup.DeviceHistory', {
 
         if (store.getCount()) {
             var issueActualType = store.getById(parseInt(issueId)).get('issueType').uid;
-            if ((issueActualType === 'datacollection') || (issueActualType === 'datavalidation')) {
+            if ((issueActualType === 'datacollection') || (issueActualType === 'datavalidation') || (issueActualType === 'devicelifecycle')) {
                 me.getController('Mdc.controller.setup.IssueStartProcess').queryParams = {activeTab: 'issues'};
                 me.getController('Mdc.controller.setup.IssueStartProcess').showStartProcess(issueId);
             }
