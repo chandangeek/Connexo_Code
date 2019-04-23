5a14e75bcf6d9994a13a940fdb2c8460e343668f
GLOBAL\H165696 <david.nitescu@honeywell.com>
2019-04-23 10:29:15 +0300

    CXO-9979 - Audit trail: Versioned CAS changes are not audited

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/audit/deviceCustomPropertySet/AuditTrailDeviceCPSDecoder.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/audit/deviceCustomPropertySet/AuditTrailDeviceCPSDecoder.java
@@ -4,6 +4,7 @@
 
 package com.energyict.mdc.device.data.impl.audit.deviceCustomPropertySet;
 
+import com.elster.jupiter.audit.AuditDomainContextType;
 import com.elster.jupiter.audit.AuditLogChange;
 import com.elster.jupiter.cps.CustomPropertySetService;
 import com.elster.jupiter.cps.CustomPropertySetValues;
@@ -23,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class AuditTrailDeviceCPSDecoder extends AbstractCPSAuditDecoder {
 
@@ -41,7 +43,7 @@ public class AuditTrailDeviceCPSDecoder extends AbstractCPSAuditDecoder {
                 .filter(set -> set.getCustomPropertySet().isVersioned())
                 .ifPresent(set -> {
                     CustomPropertySetValues customPropertySetValues = getCustomPropertySetValues(getCustomPropertySet().get(),
-                            isContextObsolete() ? getAuditTrailReference().getModTimeEnd().minusMillis(1) : getAuditTrailReference().getModTimeEnd());
+                            isContextObsolete() ? getAuditTrailReference().getModTimeEnd() : getAuditTrailReference().getModTimeEnd());
                     if (customPropertySetValues.getEffectiveRange().hasLowerBound()) {
                         builder.put("startTime", customPropertySetValues.getEffectiveRange().lowerEndpoint());
                     }
@@ -53,6 +55,18 @@ public class AuditTrailDeviceCPSDecoder extends AbstractCPSAuditDecoder {
         return builder.build();
     }
 
+    public UnexpectedNumberOfUpdatesException.Operation getOperation(UnexpectedNumberOfUpdatesException.Operation operation, AuditDomainContextType context){
+        return getAuditTrailReference().getOperation().equals(UnexpectedNumberOfUpdatesException.Operation.INSERT) ?
+                UnexpectedNumberOfUpdatesException.Operation.INSERT : UnexpectedNumberOfUpdatesException.Operation.UPDATE;
+    }
+
+    @Override
+    public List<AuditLogChange> getAuditLogChanges() {
+        return getAuditLogChangesFromDevice().stream()
+                        .distinct()
+                        .collect(Collectors.toList());
+    }
+
     protected List<AuditLogChange> getAuditLogChangesFromDevice() {
         try {
             List<AuditLogChange> auditLogChanges = new ArrayList<>();
@@ -62,7 +76,8 @@ public class AuditTrailDeviceCPSDecoder extends AbstractCPSAuditDecoder {
                 return auditLogChanges;
             }
 
-            if (getAuditTrailReference().getOperation() == UnexpectedNumberOfUpdatesException.Operation.UPDATE) {
+            if ((getAuditTrailReference().getOperation() == UnexpectedNumberOfUpdatesException.Operation.UPDATE) ||
+                 (getAuditTrailReference().getOperation() == UnexpectedNumberOfUpdatesException.Operation.DELETE)){
                 CustomPropertySetValues toCustomPropertySetValues = getCustomPropertySetValues(registeredCustomPropertySet.get(),getAuditTrailReference().getModTimeEnd());
                 CustomPropertySetValues fromCustomPropertySetValues = getCustomPropertySetValues(registeredCustomPropertySet.get(), getAuditTrailReference().getModTimeStart()
                         .minusMillis(1));
