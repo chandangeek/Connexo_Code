fca94cd6964e99a0066d6e2692ce8c879fdceda1
GLOBAL\H165696 <david.nitescu@honeywell.com>
2019-04-24 15:06:38 +0300

    CXO-9813 - setPriority issue fixed: - display issue detail page - page refresh - change priority

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/controller/SetPriority.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/controller/SetPriority.js
@@ -35,7 +35,7 @@ Ext.define('Isu.controller.SetPriority', {
 
         var widget = Ext.widget('issue-set-priority', {
             router: router,
-            returnLink: router.getRoute(router.currentRoute.replace(fromDetails ? '/setpriority' : '/view/setpriority', '')).buildUrl({}, me.queryParams)
+            returnLink: router.getRoute(router.currentRoute.replace(fromDetails ? '/setpriority' : '/view/setpriority', '')).buildUrl({}, router.queryParams)
         });
         viewport.setLoading();
 
