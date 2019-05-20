fc0da3548292306a8f305c6b7b0cc7e113172c7d
ssafyannikov <ssafyannikov@avaya.com>
2019-05-20 15:43:27 +0300

    fix user combobox

--- coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/view/issues/ManuallyRuleItem.js
+++ coko/com.elster.jupiter.issue.extjs/src/main/web/js/issue/src/view/issues/ManuallyRuleItem.js
@@ -216,25 +216,27 @@ Ext.define('Isu.view.issues.ManuallyRuleItem', {
                   listeners: {
                      render: function () {
                         this.store.load();
+                     },
+                     change: function (combo, newValue) {
+                        this.up('#issue-manually-creation-rules-item').down('#mi-user-issue-assignee').fireEvent('workgroupChanged', newValue);
+                     },
+                     select: function (combo, newValue) {
+                        this.up('#issue-manually-creation-rules-item').down('#mi-user-issue-assignee').fireEvent('workgroupChanged', newValue[0].get('id'));
                      }
                   }
                },
                {
-                  xtype: 'combobox',
+                  xtype: 'issues-user-assignee-combo',
                   itemId: 'mi-user-issue-assignee',
                   fieldLabel: Uni.I18n.translate('general.user', 'ISU', 'User'),
                   name: 'assignToUserId',
+                  queryMode: 'local',
                   valueField: 'id',
                   displayField: 'name',
                   editable: false,
                   store: 'Isu.store.UserList',
                   emptyText: Uni.I18n.translate('general.unassigned', 'ISU', 'Unassigned'),
-                  msgTarget: 'under',
-                  listeners: {
-                     render: function () {
-                        this.store.load();
-                     }
-                  }
+                  msgTarget: 'under'
                },
                {
                     itemId: 'comment',
