49518322c7ebb6c70f7bc4252f18485191faa61c
Viyunov <rviyunov@merann.ru>
2019-05-31 15:35:49 +0300

    Compilation error fix.

---
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/ApplicationSpecific.java
@@ -0,0 +1,24 @@
+package com.elster.jupiter.soap.whiteboard.cxf;
+
+public interface ApplicationSpecific {
+
+    enum WebServiceApplicationName{
+        MULTISENSE("Multisense"),
+        INSIGHT("Insight"),
+        MULTISENSE_INSIGHT("Multisense/Insight");
+
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        WebServiceApplicationName(String name) {
+            this.name = name;
+        }
+    }
+
+    default  String getApplication(){
+        return "Application not specified";
+    };
+}
