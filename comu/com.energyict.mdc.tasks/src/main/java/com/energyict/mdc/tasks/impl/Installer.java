827406c7b58426ee5d934fe519c62f69479c3ba3
Alexandru Ganea <aganea@pentalog.com>
2019-06-10 09:46:52 +0300

    Revert "CXO-9869 Tested the installation works properly for clean DB"
    This reverts commit baf0d0e93f2dda6b85257da62fefcba30338cb9f.

--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/Installer.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/Installer.java
@@ -22,15 +22,12 @@ class Installer implements FullInstaller {
     private final DataModel dataModel;
     private final EventService eventService;
     private final TaskService taskService;
-    private final UpgraderV10_7 upgraderV10_7;
 
     @Inject
-    Installer(DataModel dataModel, EventService eventService, TaskService taskService,
-            UpgraderV10_7 upgraderV10_7) {
+    Installer(DataModel dataModel, EventService eventService, TaskService taskService) {
         this.dataModel = dataModel;
         this.eventService = eventService;
         this.taskService = taskService;
-        this.upgraderV10_7 = upgraderV10_7;
     }
 
     @Override
@@ -44,11 +41,6 @@ class Installer implements FullInstaller {
         doTry(
                 "Create Firmware Com Task",
                 this::createFirmwareComTaskIfNotPresentYet,
-                logger
-        );
-        doTry(
-                "Create Firmware Com Task",
-                () -> upgraderV10_7.migrate(dataModelUpgrader),
                 logger
         );
     }
--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/UpgraderV10_7.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/UpgraderV10_7.java
@@ -4,7 +4,9 @@
 
 package com.energyict.mdc.tasks.impl;
 
+import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.Version;
 import com.elster.jupiter.upgrade.Upgrader;
 import com.elster.jupiter.users.UserService;
 
@@ -12,17 +14,20 @@ import javax.inject.Inject;
 
 class UpgraderV10_7 implements Upgrader {
 
+    private final DataModel dataModel;
     private final UserService userService;
     private final PrivilegesProvider10_7 privilegesProvider10_7;
 
     @Inject
-    UpgraderV10_7(UserService userService, PrivilegesProvider10_7 privilegesProvider10_7) {
+    UpgraderV10_7(DataModel dataModel, UserService userService, PrivilegesProvider10_7 privilegesProvider10_7) {
+        this.dataModel = dataModel;
         this.userService = userService;
         this.privilegesProvider10_7 = privilegesProvider10_7;
     }
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(dataModel, Version.version(10, 7));
         userService.addModulePrivileges(privilegesProvider10_7);
     }
 
