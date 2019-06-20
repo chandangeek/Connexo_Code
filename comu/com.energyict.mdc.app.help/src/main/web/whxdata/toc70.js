d6d53c8335559d8219818387d9e7753865e38758
Dmitry Golovin <walterdive@gmail.com>
2019-06-20 17:23:49 +0300

    Cleared from the garbage that came from the integration branch

--- comu/com.energyict.mdc.app.help/src/main/web/whxdata/toc70.js
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
- */
-
-gXMLBuffer = "<?xml version=\"1.0\" encoding=\"utf-8\" ?><data  src=\"toc.js\"  name=\"Topology\" url=\"000017_EN_Connexo_1.0_User_guide/000017_UMNL_Topology/000017_UMNL_Topology.htm\"><book name=\"Communication topology\" url=\"000017_EN_Connexo_1.0_User_guide/000017_UMNL_Topology/Communication_topology.htm\" src=\"toc71.js\" /></data>";
