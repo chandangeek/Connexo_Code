e51ab11e5777facbfbe909f08187af90e4a2fd66
Elena Borunova <elena.borunova@mera.com>
2019-06-25 15:15:23 +0300

    Fix device.data tests

--- comu/com.energyict.mdc.device.data/src/test/java/com/energyict/mdc/device/data/impl/InMemoryPersistenceWithMockedDeviceProtocol.java
+++ comu/com.energyict.mdc.device.data/src/test/java/com/energyict/mdc/device/data/impl/InMemoryPersistenceWithMockedDeviceProtocol.java
@@ -4,6 +4,7 @@
 
 package com.energyict.mdc.device.data.impl;
 
+import com.elster.jupiter.appserver.AppService;
 import com.elster.jupiter.audit.AuditService;
 import com.elster.jupiter.audit.impl.AuditServiceModule;
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
@@ -353,6 +354,7 @@ public class InMemoryPersistenceWithMockedDeviceProtocol {
             bind(DeviceMessageSpecificationService.class).toInstance(mock(DeviceMessageSpecificationService.class));
             bind(HsmEnergyService.class).toInstance(mock(HsmEnergyService.class));;
             bind(HsmEncryptionService.class).toInstance(mock(HsmEncryptionService.class));;
+            bind(AppService.class).toInstance(mock(AppService.class));
         }
 
     }
