11bdc0fc08503e234701769359ea12b12d719bc9
Olga Nacharkina <onachark@mera.ru>
2019-06-25 22:03:58 +0300

    [CXO-10230] fix unit tests

--- comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/InMemoryPersistenceWithMockedDeviceProtocol.java
+++ comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/InMemoryPersistenceWithMockedDeviceProtocol.java
@@ -4,6 +4,7 @@
 
 package com.energyict.mdc.device.topology.impl;
 
+import com.elster.jupiter.appserver.AppService;
 import com.elster.jupiter.audit.AuditService;
 import com.elster.jupiter.audit.impl.AuditServiceModule;
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
@@ -627,6 +628,7 @@ public class InMemoryPersistenceWithMockedDeviceProtocol {
             bind(HttpService.class).toInstance(mock(HttpService.class));
             bind(HsmEnergyService.class).toInstance(mock(HsmEnergyService.class));
             bind(HsmEncryptionService.class).toInstance(mock(HsmEncryptionService.class));
+            bind(AppService.class).toInstance(mock(AppService.class));
         }
     }
 }
--- comu/com.energyict.mdc.issue.datacollection/src/test/java/com/energyict/mdc/issue/datacollection/BaseTest.java
+++ comu/com.energyict.mdc.issue.datacollection/src/test/java/com/energyict/mdc/issue/datacollection/BaseTest.java
@@ -4,6 +4,7 @@
 
 package com.energyict.mdc.issue.datacollection;
 
+import com.elster.jupiter.appserver.AppService;
 import com.elster.jupiter.audit.AuditService;
 import com.elster.jupiter.audit.impl.AuditServiceModule;
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
@@ -158,6 +159,7 @@ public abstract class BaseTest {
             bind(HttpService.class).toInstance(mock(HttpService.class));
             bind(HsmEnergyService.class).toInstance(mock(HsmEnergyService.class));
             bind(HsmEncryptionService.class).toInstance(mock(HsmEncryptionService.class));
+            bind(AppService.class).toInstance(mock(AppService.class));
         }
     }
 
