c31a82e35cfad792ba658e521daa5538eacf8f58
Iulian Maiorescu <iulian.maiorescu@honeywell.com>
2019-07-02 11:26:14 +0300

    CXO-10075 - SagemcomT/210 ESMR5.0 will increase the Frame counter on the Sagem device - cleanup for Crypto ESMR (cherry picked from commit ce74b74)

--- upl/com.energyict.mdc.upl.impl/cryptoprotocols/src/main/java/com/energyict/protocolimplv2/nta/esmr50/common/CryptoESMR50SecurityProvider.java
+++ upl/com.energyict.mdc.upl.impl/cryptoprotocols/src/main/java/com/energyict/protocolimplv2/nta/esmr50/common/CryptoESMR50SecurityProvider.java
@@ -1,21 +1,27 @@
-package com.energyict.protocolimplv2.nta.dsmr40.common;
+package com.energyict.protocolimplv2.nta.esmr50.common;
 
 import com.energyict.mdc.upl.properties.TypedProperties;
 
 import com.energyict.protocolimplv2.nta.dsmr23.common.CryptoDSMR23SecurityProvider;
 import com.energyict.protocolimplv2.nta.dsmr40.DSMR40RespondingFrameCounterHandler;
+import com.energyict.protocolimplv2.nta.dsmr40.common.CryptoDSMR40SecurityProvider;
 
 import java.util.Random;
 
-public class CryptoDSMR40SecurityProvider extends CryptoDSMR23SecurityProvider {
+public class CryptoESMR50SecurityProvider extends CryptoDSMR40SecurityProvider {
     /**
      * Create a new instance of NTASecurityProvider
      *
      * @param properties - contains the keys for the authentication/encryption
      * @param authenticationLevel
      */
-    public CryptoDSMR40SecurityProvider(TypedProperties properties, int authenticationLevel) {
+    public CryptoESMR50SecurityProvider(TypedProperties properties, int authenticationLevel) {
         super(properties, authenticationLevel);
         setRespondingFrameCounterHandling(new DSMR40RespondingFrameCounterHandler());
+    }
+
+    @Override
+    public long initializeFrameCounter() {
+        return 0;
     }
 }
