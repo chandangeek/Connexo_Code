b41a5411fbede298718dbf0d7cd9b4ea3c39d20a
Sergey Vorobiev <svorob@mera.ru>
2019-07-25 17:56:48 +0300

    autoresolution fix

--- coko/com.elster.jupiter.issue.servicecall/src/main/java/com/elster/jupiter/issue/servicecall/impl/ServiceCallIssueCreationRuleTemplate.java
+++ coko/com.elster.jupiter.issue.servicecall/src/main/java/com/elster/jupiter/issue/servicecall/impl/ServiceCallIssueCreationRuleTemplate.java
@@ -99,7 +99,7 @@ public class ServiceCallIssueCreationRuleTemplate implements CreationRuleTemplat
                 "end\n" +
                 "rule \"Auto-resolution section @{ruleId}\"\n" +
                 "when\n" +
-                "\tevent : ServiceCallStateChangedEvent(stateId == " + DefaultState.SUCCESSFUL.ordinal() + "), " +
+                "\tevent : ServiceCallStateChangedEvent(stateId == " + DefaultState.SUCCESSFUL.ordinal() + ", " +
                 " serviceCallTypeId in (@{" + TranslationKeys.SERVICE_CALL_TYPE.getKey() + "})," +
                 "@{" + AUTORESOLUTION + "} == 1" +
                 ")\n" +
