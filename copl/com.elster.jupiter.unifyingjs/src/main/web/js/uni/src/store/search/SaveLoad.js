de729a8c9442396f3d732bf79378c1595e5d0149
H291525 <venkatakrishna.alisetty@honeywell.com>
2019-08-13 17:09:23 +0530

    table domain

---
+++ copl/com.elster.jupiter.unifyingjs/src/main/web/js/uni/src/store/search/SaveLoad.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+/**
+ * @class Uni.store.search.SaveLoad
+ */
+Ext.define('Uni.store.search.SaveLoad', {
+    extend: 'Ext.data.Store',
+    storeId: 'Uni.store.search.SaveLoads',
+    alias: 'store.saveSearchStore',
+    autoLoad: true,
+   // remoteFilter: true,
+   // pageSize: 100,
+    fields: [
+        {type: 'string', name: 'name'},
+        {type: 'string', name: 'user'},
+        {type: 'string', name: 'criteria'},
+        {type: 'string', name: 'domainid'}
+    ],
+    sorters: [{
+        property: 'name',
+        direction: 'ASC'
+    }],
+    proxy: {
+        type: 'rest',
+        url: '../../api/jsr/search/searchCriteria',
+        pageParam: false,
+        startParam: false,
+        limitParam: false,
+        reader: {
+            type: 'json',
+            root: 'numberOfSearchResults'
+        }
+
+    }
+});
