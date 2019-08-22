77c844ebbc3937e9b9353a5477a55bb884873a4e
MERA_H245859 <vsukova@mera.ru>
2019-08-22 12:52:38 +0300

    [CONM-1084]

--- coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/StateTransitionChangeEventTopicHandler.java
+++ coko/com.elster.jupiter.metering/src/main/java/com/elster/jupiter/metering/impl/StateTransitionChangeEventTopicHandler.java
@@ -4,24 +4,40 @@
 
 package com.elster.jupiter.metering.impl;
 
+import com.elster.jupiter.cbo.DateTimeFormatGenerator;
 import com.elster.jupiter.domain.util.Query;
 import com.elster.jupiter.events.LocalEvent;
 import com.elster.jupiter.events.TopicHandler;
 import com.elster.jupiter.fsm.FiniteStateMachineService;
+import com.elster.jupiter.fsm.State;
+import com.elster.jupiter.fsm.StateTimeSlice;
 import com.elster.jupiter.fsm.StateTransitionChangeEvent;
 import com.elster.jupiter.metering.EndDevice;
 import com.elster.jupiter.metering.KnownAmrSystem;
 import com.elster.jupiter.metering.MeteringService;
+import com.elster.jupiter.nls.Layer;
+import com.elster.jupiter.nls.NlsService;
+import com.elster.jupiter.nls.Thesaurus;
+import com.elster.jupiter.nls.TranslationKey;
+import com.elster.jupiter.security.thread.ThreadPrincipalService;
+import com.elster.jupiter.users.UserService;
 import com.elster.jupiter.util.conditions.Condition;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.inject.Inject;
+import java.security.Principal;
 import java.time.Clock;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static com.elster.jupiter.util.conditions.Where.where;
 
@@ -40,6 +56,9 @@ public class StateTransitionChangeEventTopicHandler implements TopicHandler {
     private volatile Clock clock;
     private volatile FiniteStateMachineService stateMachineService;
     private volatile MeteringService meteringService;
+    private volatile ThreadPrincipalService threadPrincipalService;
+    private volatile Thesaurus thesaurus;
+    private volatile UserService userService;
 
     // For OSGi purposes
     public StateTransitionChangeEventTopicHandler() {
@@ -68,6 +87,21 @@ public class StateTransitionChangeEventTopicHandler implements TopicHandler {
     @Reference
     public void setMeteringService(MeteringService meteringService) {
         this.meteringService = meteringService;
+    }
+
+    @Reference
+    public void setThreadPrincipalService(ThreadPrincipalService threadPrincipalService) {
+        this.threadPrincipalService = threadPrincipalService;
+    }
+
+    @Reference
+    public final void setNlsService(NlsService nlsService) {
+        this.thesaurus = nlsService.getThesaurus(MeteringService.COMPONENTNAME, Layer.DOMAIN);
+    }
+
+    @Reference
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
@@ -100,7 +134,57 @@ public class StateTransitionChangeEventTopicHandler implements TopicHandler {
     }
 
     private void handle(StateTransitionChangeEvent event, ServerEndDevice endDevice) {
-        endDevice.changeState(event.getNewState(), this.effectiveTimestampFrom(event));
+        Instant effectiveTimestamp = this.effectiveTimestampFrom(event);
+        State newState = event.getNewState();
+
+        this.effectiveTimestampAfterLastStateChange(effectiveTimestamp, newState, endDevice);
+        endDevice.changeState(newState, effectiveTimestamp);
+    }
+
+    private void effectiveTimestampAfterLastStateChange(Instant effectiveTimestamp, State state, ServerEndDevice endDevice) {
+        Optional<Instant> lastStateChangeTimestamp = this.getLastStateChangeTimestamp(endDevice);
+
+        if(lastStateChangeTimestamp.isPresent()) {
+            if (lastStateChangeTimestamp.isPresent() && !effectiveTimestamp.isAfter(lastStateChangeTimestamp.get())) {
+                throw new StateTransitionChangeEventException.UnableToChangeDeviceStateDueToTimeNotAfterLastStateChangeException(thesaurus, getStateName(state), endDevice.getName(),
+                        getFormattedInstant(getLongDateFormatForCurrentUser(), effectiveTimestamp),
+                        getFormattedInstant(getLongDateFormatForCurrentUser(), lastStateChangeTimestamp.get()));
+            }
+        }
+    }
+
+    private Optional<Instant> getLastStateChangeTimestamp(ServerEndDevice endDevice) {
+        List<StateTimeSlice> stateTimeSlices = endDevice.getStateTimeline().get().getSlices();
+        return this.lastSlice(stateTimeSlices).map(lastSlice -> lastSlice.getPeriod().lowerEndpoint());
+    }
+
+    private Optional<StateTimeSlice> lastSlice(List<StateTimeSlice> stateTimeSlices) {
+        if (stateTimeSlices.isEmpty()) {
+            // MDC device always have at least one state
+            return Optional.empty();
+        } else {
+            return Optional.of(stateTimeSlices.get(stateTimeSlices.size() - 1));
+        }
+    }
+
+    private String getStateName(State state) {
+        return DefaultState
+                .from(state)
+                .map(ent->thesaurus.getFormat(ent).format())
+                .orElseGet(state::getName);
+    }
+
+    private String getFormattedInstant(DateTimeFormatter formatter, Instant time){
+        return formatter.format(LocalDateTime.ofInstant(time, ZoneId.systemDefault()));
+    }
+
+    private DateTimeFormatter getLongDateFormatForCurrentUser() {
+        Principal principal = threadPrincipalService.getPrincipal();
+        return DateTimeFormatGenerator.getDateFormatForUser(
+                DateTimeFormatGenerator.Mode.LONG,
+                DateTimeFormatGenerator.Mode.LONG,
+                this.userService.getUserPreferencesService(),
+                this.threadPrincipalService.getPrincipal());
     }
 
     private Instant effectiveTimestampFrom(StateTransitionChangeEvent event) {
@@ -109,6 +193,55 @@ public class StateTransitionChangeEventTopicHandler implements TopicHandler {
             effectiveTimestamp = this.clock.instant();
         }
         return effectiveTimestamp;
+    }
+
+    private enum DefaultState implements TranslationKey {
+
+        IN_STOCK("dlc.default.inStock", "In stock"),
+        COMMISSIONING("dlc.default.commissioning", "Commissioning"),
+        ACTIVE("dlc.default.active", "Active"),
+        INACTIVE("dlc.default.inactive", "Inactive"),
+        DECOMMISSIONED("dlc.default.decommissioned", "Decommissioned"),
+        REMOVED("dlc.default.removed", "Removed");
+
+        private final String key;
+        private final String defaultFormat;
+
+        DefaultState(String key, String defaultFormat) {
+            this.key = key;
+            this.defaultFormat = defaultFormat;
+        }
+
+        @Override
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public String getDefaultFormat() {
+            return defaultFormat;
+        }
+
+        /**
+         * Determines the DefaultState for the specified {@link State}.
+         * Will return <code>Optional.empty()</code> when the State
+         * is not standard or the symbolic name does not match
+         * one of the standards.
+         *
+         * @param state The State
+         * @return The DefaultState
+         */
+        public static Optional<DefaultState> from(State state) {
+            if (state != null && !state.isCustom()) {
+                String symbolicName = state.getName();
+                return Stream
+                        .of(DefaultState.values())
+                        .filter(d -> d.getKey().equals(symbolicName))
+                        .findFirst();
+            } else {
+                return Optional.empty();
+            }
+        }
     }
 
     @Override
