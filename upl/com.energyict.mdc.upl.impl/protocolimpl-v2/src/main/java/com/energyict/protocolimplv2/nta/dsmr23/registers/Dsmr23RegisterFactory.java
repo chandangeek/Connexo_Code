0a47ea88a09fabb37e55a118a3db0a00a6b4ad74
Iulian Maiorescu <iulian.maiorescu@honeywell.com>
2019-09-03 16:28:33 +0300

    COMMUNICATION-3031 - Ad Hoc Read Registers M-Bus Diagnostics Statistics G - no values available - refactored the composed register reading to check various nulls and invalid values (cherry picked from commit 8a7f30f)

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/nta/dsmr23/registers/Dsmr23RegisterFactory.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/nta/dsmr23/registers/Dsmr23RegisterFactory.java
@@ -8,6 +8,7 @@ import com.energyict.dlms.axrdencoding.AbstractDataType;
 import com.energyict.dlms.axrdencoding.BooleanObject;
 import com.energyict.dlms.axrdencoding.OctetString;
 import com.energyict.dlms.axrdencoding.TypeEnum;
+import com.energyict.dlms.axrdencoding.util.DateTime;
 import com.energyict.dlms.cosem.AssociationLN;
 import com.energyict.dlms.cosem.ComposedCosemObject;
 import com.energyict.dlms.cosem.DLMSClassId;
@@ -116,27 +117,7 @@ public class Dsmr23RegisterFactory implements DeviceRegisterSupport {
             RegisterValue rv = null;
             try {
                 if (this.composedRegisterMap.containsKey(register)) {
-                    ScalerUnit su = new ScalerUnit(registerComposedCosemObject.getAttribute(this.composedRegisterMap.get(register).getRegisterUnitAttribute()));
-                    if (su.getUnitCode() != 0) {
-                        Date eventTime = null;   //Optional capture time attribute
-                        DLMSAttribute registerCaptureTime = this.composedRegisterMap.get(register).getRegisterCaptureTime();
-                        if (registerCaptureTime != null) {
-                            AbstractDataType attribute = registerComposedCosemObject.getAttribute(registerCaptureTime);
-                            eventTime = attribute.getOctetString().getDateTime(protocol.getDlmsSession().getTimeZone()).getValue().getTime();
-                        }
-                        ComposedRegister composedRegister = this.composedRegisterMap.get(register);
-                        DLMSAttribute attr = composedRegister.getRegisterValueAttribute();
-                        AbstractDataType abstractDataType = registerComposedCosemObject.getAttribute(attr);
-                        if (abstractDataType.isOctetString()){
-                            String text = abstractDataType.getOctetString().stringValue();
-                            rv = new RegisterValue(register, null, eventTime, null, null, new Date(), 0, text );
-                        } else {
-                            rv = getRegisterValueForComposedRegister(register, eventTime, abstractDataType, su.getEisUnit() );
-
-                        }
-                    } else {
-                        this.protocol.journal(Level.WARNING, "Register with ObisCode " + register.getObisCode() + "[" + register.getSerialNumber() + "] does not provide a proper Unit.");
-                    }
+                    rv = readComposedRegister(registerComposedCosemObject, register);
                 } else if (this.registerMap.containsKey(register)) {
                     rv = convertCustomAbstractObjectsToRegisterValues(register, registerComposedCosemObject.getAttribute(this.registerMap.get(register)));
                 }
@@ -163,6 +144,90 @@ public class Dsmr23RegisterFactory implements DeviceRegisterSupport {
         }
         return collectedRegisters;
     }
+
+
+    /**
+     * Will try to read-out the scaler & unit from a register.
+     *
+     * Will check if the received unit has a valid DLMS code > 0. Default will be "undefined" = 255.
+     */
+    private Unit readComposedRegisterUnit(ComposedCosemObject registerComposedCosemObject, ComposedRegister composedRegister, OfflineRegister register) throws IOException {
+        DLMSAttribute unitAttribute = composedRegister.getRegisterUnitAttribute();
+        if (unitAttribute!=null) {
+            AbstractDataType unitAttributeValue = registerComposedCosemObject.getAttribute(unitAttribute);
+            if (unitAttributeValue.isStructure()) {
+                ScalerUnit su = new ScalerUnit(unitAttributeValue.getStructure());
+                if (su.getUnitCode()!=0) {
+                    return su.getEisUnit();
+                } else {
+                    this.protocol.journal(Level.WARNING, "Register with ObisCode " + register.getObisCode() + "[" + register.getSerialNumber() + "] does not provide a valid Unit (it is zero)");
+                }
+            } else {
+                this.protocol.journal(Level.WARNING, "Register with ObisCode " + register.getObisCode() + "[" + register.getSerialNumber() + "] does not provide a valid Unit, is not a Structure: "+unitAttributeValue.toString());
+            }
+        }
+
+        return Unit.getUndefined();
+    }
+
+    /**
+     * Will try to read-out the event time from a composed register.
+     *
+     * The read-out value needs to be a proper OctetString, else everything will be default to null (current time)
+     */
+    private Date readComposedRegisterEventTime(ComposedCosemObject registerComposedCosemObject, ComposedRegister composedRegister, OfflineRegister register) throws IOException {
+
+        DLMSAttribute registerCaptureTime = composedRegister.getRegisterCaptureTime();
+        if (registerCaptureTime != null) {
+            TimeZone protocolTimeZone = protocol.getDlmsSession().getTimeZone();
+            AbstractDataType attribute = registerComposedCosemObject.getAttribute(registerCaptureTime);
+            if (attribute!=null){
+                if (attribute.isOctetString()){
+                    DateTime dateTime = attribute.getOctetString().getDateTime(protocolTimeZone);
+                    return dateTime.getValue().getTime();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Will read a "composed" register, which holds more information than a simple value.
+     * This is valid for Extended Register or Demand Registers, etc.
+     * The register value will have:
+     *      - a value
+     *      - an unit (optional)
+     *      - an time-stamp (optional)
+     *
+     * @param registerComposedCosemObject
+     * @param register
+     * @return
+     * @throws IOException
+     */
+    private RegisterValue readComposedRegister(ComposedCosemObject registerComposedCosemObject, OfflineRegister register) throws IOException {
+        RegisterValue rv = null;
+
+        ComposedRegister composedRegister = this.composedRegisterMap.get(register);
+
+        Unit eisUnit = readComposedRegisterUnit(registerComposedCosemObject, composedRegister,  register);
+
+        Date eventTime = readComposedRegisterEventTime(registerComposedCosemObject, composedRegister, register);
+
+        DLMSAttribute attributeValue = composedRegister.getRegisterValueAttribute();
+        AbstractDataType abstractDataType = registerComposedCosemObject.getAttribute(attributeValue);
+
+        if (abstractDataType.isOctetString()){
+            String text = abstractDataType.getOctetString().stringValue();
+            rv = new RegisterValue (register, null, eventTime, null, null, new Date(), 0, text );
+        } else {
+            rv = getRegisterValueForComposedRegister(register, eventTime, abstractDataType, eisUnit);
+        }
+
+        return rv;
+    }
+
+
 
     /** Handle any special composed register
      *
@@ -356,7 +421,7 @@ public class Dsmr23RegisterFactory implements DeviceRegisterSupport {
                             this.registerMap.put(register, new DLMSAttribute(adjustToMbusOC(rObisCode), MbusClientAttributes.DEVICE_TYPE.getAttributeNumber(), DLMSClassId.MBUS_CLIENT.getClassId()));
                             dlmsAttributes.add(this.registerMap.get(register));
                         } else {
-                            protocol.journal(Level.WARNING, "Register with ObisCode " + rObisCode + " is not supported by the protocol and will not be ignored.");
+                            protocol.journal(Level.WARNING, "Register with ObisCode " + rObisCode + " is not supported by the protocol and will not be ignor.");
                         }
                     }
                 }
