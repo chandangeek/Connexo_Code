3e344318cff82010633de00a752041afe0a19cbb
Viyunov <rviyunov@merann.ru>
2019-09-11 15:30:31 +0300

    RelatedObjectInfo added to index.

---
+++ copl/com.elster.jupiter.webservices.rest/src/main/java/com.elster.jupiter.webservices.rest.impl/RelatedObjectInfo.java
@@ -0,0 +1,11 @@
+package com.elster.jupiter.webservices.rest.impl;
+
+public class RelatedObjectInfo {
+    long id;
+    String value;
+
+    public RelatedObjectInfo(long id, String value){
+        this.id = id;
+        this.value = value;
+    }
+}
