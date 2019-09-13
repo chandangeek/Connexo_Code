55cabd3e6c2d6a2463710b3d1229528a35ba8ba3
Dmitry Golovin <walterdive@gmail.com>
2019-09-13 10:17:49 +0300

    fixed message number

--- copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/MessageSeeds.java
+++ copl/com.elster.jupiter.fsm/src/main/java/com/elster/jupiter/fsm/MessageSeeds.java
@@ -20,7 +20,7 @@ public enum MessageSeeds implements MessageSeed {
     // Generic
     FIELD_TOO_LONG(100, Keys.FIELD_TOO_LONG, "Field must not exceed {max} characters"),
     CAN_NOT_BE_EMPTY(101, Keys.CAN_NOT_BE_EMPTY, "This field is required"),
-    END_POINT_CONFIG_IN_USE_BY_FSM(1002, Keys.END_POINT_CONFIG_IS_USE,"Web service end point ''{0}'' is in use by a device life cycle.", Level.SEVERE),
+    END_POINT_CONFIG_IN_USE_BY_FSM(102, Keys.END_POINT_CONFIG_IS_USE,"Web service end point ''{0}'' is in use by a device life cycle.", Level.SEVERE),
 
     // StateTransitionEventType
     UNIQUE_EVENT_TYPE_SYMBOL(200, Keys.UNIQUE_EVENT_TYPE_SYMBOL, "The symbolic representation of a state transition event type must be unique"),
