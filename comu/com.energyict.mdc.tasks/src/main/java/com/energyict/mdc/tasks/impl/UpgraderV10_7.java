893522d57f6258e42b0b9ca5177f9d78538372cb
GLOBAL\H165696 <david.nitescu@honeywell.com>
2019-09-16 09:49:54 +0300

    upgrade dataModel to 10.7 version

--- comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/UpgraderV10_7.java
+++ comu/com.energyict.mdc.tasks/src/main/java/com/energyict/mdc/tasks/impl/UpgraderV10_7.java
@@ -4,6 +4,7 @@
 
 package com.energyict.mdc.tasks.impl;
 
+import com.elster.jupiter.orm.DataModel;
 import com.elster.jupiter.orm.DataModelUpgrader;
 import com.elster.jupiter.upgrade.Upgrader;
 import com.elster.jupiter.users.UserService;
@@ -12,19 +13,24 @@ import com.energyict.mdc.common.tasks.security.Privileges;
 
 import javax.inject.Inject;
 
+import static com.elster.jupiter.orm.Version.version;
+
 class UpgraderV10_7 implements Upgrader {
 
+    private final DataModel dataModel;
     private final UserService userService;
     private final PrivilegesProvider10_7 privilegesProvider10_7;
 
     @Inject
-    UpgraderV10_7(UserService userService, PrivilegesProvider10_7 privilegesProvider10_7) {
+    UpgraderV10_7(DataModel dataModel, UserService userService, PrivilegesProvider10_7 privilegesProvider10_7) {
+        this.dataModel = dataModel;
         this.userService = userService;
         this.privilegesProvider10_7 = privilegesProvider10_7;
     }
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(dataModel, version(10, 7));
         userService.addModulePrivileges(privilegesProvider10_7);
         userService.grantGroupWithPrivilege(UserService.BATCH_EXECUTOR_ROLE, "MDC", getExecuteComTaskPrivileges());
     }
