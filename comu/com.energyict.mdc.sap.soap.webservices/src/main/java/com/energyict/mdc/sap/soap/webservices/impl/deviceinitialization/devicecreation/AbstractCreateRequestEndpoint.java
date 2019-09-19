42ab36eaae50169918aca0bb7109abd2ebff36c9
Igor Sedoykin <igor.sedoykin@mera.com>
2019-09-19 12:22:13 +0300

    [CXO-10829][CXO-10831][CXO-10857] - added copyrights for new files

--- comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/deviceinitialization/devicecreation/AbstractCreateRequestEndpoint.java
+++ comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/deviceinitialization/devicecreation/AbstractCreateRequestEndpoint.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) 2019 by Honeywell International Inc. All Rights Reserved
+ */
 package com.energyict.mdc.sap.soap.webservices.impl.deviceinitialization.devicecreation;
 
 import com.elster.jupiter.nls.Thesaurus;
