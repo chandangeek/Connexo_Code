6b267e624cc32d1df5496e4b27658865a9005e5c
H292055 <adriana.mizulescu@honeywell.com>
2019-09-25 16:35:04 +0300

    CXO-10899: remove upgrader for the moment, bundlewaiter is not enough

--- comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/install/DataCollectionRulesUpgrader.java
+++ comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/install/DataCollectionRulesUpgrader.java
@@ -10,13 +10,9 @@ import com.elster.jupiter.issue.share.entity.IssueType;
 import com.elster.jupiter.issue.share.service.IssueCreationService;
 import com.elster.jupiter.issue.share.service.IssueService;
 import com.elster.jupiter.orm.DataModelUpgrader;
-import com.elster.jupiter.properties.HasIdAndName;
-import com.elster.jupiter.time.DefaultRelativePeriodDefinition;
-import com.elster.jupiter.time.RelativePeriod;
 import com.elster.jupiter.time.TimeService;
 import com.elster.jupiter.upgrade.Upgrader;
 import com.elster.jupiter.util.conditions.Condition;
-import net.minidev.json.JSONObject;
 
 import javax.inject.Inject;
 
@@ -42,33 +38,12 @@ public class DataCollectionRulesUpgrader implements Upgrader {
                 .select(Condition.TRUE)
                 .stream()
                 .filter(rule -> rule.getTemplateImpl().equals("BasicDataCollectionRuleTemplate"))
-                .filter(rule -> rule.getCreationRuleProperties().stream().anyMatch(creationRuleProperty -> creationRuleProperty.getName().equals("BasicDataCollectionRuleTemplate.threshold")))
+                .filter(rule -> rule.getCreationRuleProperties().stream().noneMatch(creationRuleProperty -> creationRuleProperty.getName().equals("BasicDataCollectionRuleTemplate.threshold")))
                 .forEach(this::updateCreationRule);
     }
 
     private void updateCreationRule(final CreationRule creationRule) {
         final IssueCreationService.CreationRuleUpdater creationRuleUpdater = creationRule.startUpdate();
         creationRuleUpdater.complete();
-    }
-
-    private HasIdAndName getRelativePeriodWithCount(DefaultRelativePeriodDefinition relativePeriodDefinition) {
-        RelativePeriod relativePeriod = timeService.findRelativePeriodByName(relativePeriodDefinition.getPeriodName()).isPresent() ? timeService.findRelativePeriodByName(relativePeriodDefinition.getPeriodName())
-                .get() : timeService.getAllRelativePeriod();
-        String occurrenceCount = "1";
-
-        return new HasIdAndName() {
-            @Override
-            public String getId() {
-                return occurrenceCount + ":" + relativePeriod.getId();
-            }
-
-            @Override
-            public String getName() {
-                JSONObject jsonId = new JSONObject();
-                jsonId.put("occurrenceCount", occurrenceCount);
-                jsonId.put("relativePeriod", relativePeriod.getName());
-                return jsonId.toString();
-            }
-        };
     }
 }
