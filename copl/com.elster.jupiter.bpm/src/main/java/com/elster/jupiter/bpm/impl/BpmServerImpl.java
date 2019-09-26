4a927b44f6d88439ea187b4ef0d4a639c8585da1
Dmitry Golovin <walterdive@gmail.com>
2019-09-26 16:18:26 +0300

    fix bpmserver

--- copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/BpmServerImpl.java
+++ copl/com.elster.jupiter.bpm/src/main/java/com/elster/jupiter/bpm/impl/BpmServerImpl.java
@@ -24,7 +24,6 @@ import java.util.Base64;
 public class BpmServerImpl implements BpmServer {
 
     private static final String BPM_URL = "com.elster.jupiter.bpm.url";
-    private static final String BPM_EXTERNAL_URL = "com.elster.jupiter.bpm.externalurl";
     private static final String BPM_USER = "com.elster.jupiter.bpm.user";
     private static final String BPM_PASSWORD = "com.elster.jupiter.bpm.password";
 
@@ -52,9 +51,6 @@ public class BpmServerImpl implements BpmServer {
 
     private void setUrlFromContext(BundleContext context) {
         if (context != null) {
-            url = context.getProperty(BPM_EXTERNAL_URL);
-        }
-        if (url == null) {
             url = context.getProperty(BPM_URL);
         }
         if (url == null) {
