9a741703314331912b51993e148560acacb787fc
Iulian Dabuleanu <iulian.dabuleanu@honeywell.com>
2019-10-17 16:11:39 +0300

    CONM-810 Implement ComServerMobile in Connexo - Update the filemobileConnexo.bat file

--- subsystems/com.elster.jupiter.subsystem.offline/assembly/bin/mobileConnexo.bat
+++ subsystems/com.elster.jupiter.subsystem.offline/assembly/bin/mobileConnexo.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 set CLASSPATH="../bundles/*"
-set VM_OPTIONS=-Dsun.java2d.d3d=false -Dos.name="Windows XP" -Dservername=RO18LTC29G9H2-Mobile
-%JAVA_HOME%\bin\java %VM_OPTIONS% -cp %CLASSPATH% com.elster.jupiter.launcher.ConnexoLauncher
+set VM_OPTIONS=-Dsun.java2d.d3d=false -Dos.name="Windows XP" -Dservername=ConnexoMobile
+start %JAVA_HOME%\bin\java %VM_OPTIONS% -cp %CLASSPATH% com.elster.jupiter.launcher.ConnexoLauncher
 
