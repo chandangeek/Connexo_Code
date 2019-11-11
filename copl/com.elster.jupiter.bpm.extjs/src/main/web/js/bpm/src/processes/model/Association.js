1ab044be4bba6d55824a2aa69f0ffb80c0c65a78
ssafyannikov <ssafyannikov@avaya.com>
2019-11-11 16:20:29 +0300

    [CONM-955] replace using 'raw data' to standard api

---
+++ copl/com.elster.jupiter.bpm.extjs/src/main/web/js/bpm/src/processes/model/Association.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+Ext.define('Bpm.processes.model.Association', {
+    extend: 'Ext.data.Model',
+    requires: [
+        'Uni.property.model.Property',
+    ],
+    fields: [
+        {name: 'type', type: 'string'},
+        {name: 'name', type: 'string'}
+    ],
+    associations: [
+        {
+            type: 'hasMany',
+            name: 'properties',
+            model: 'Uni.property.model.Property',
+            associationKey: 'properties',
+            foreignKey: 'properties',
+            getTypeDiscriminator: function (node) {
+                return 'Uni.property.model.Property';
+            }
+        },
+    ]
+});
