cc123f4933ed3cfe8e5f0281c621fb16ec893dfa
Sergey Vorobiev <svorob@mera.ru>
2019-12-06 11:49:34 +0300

    CONM-580 loop fix

---
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/Upgrader_V10_7_1.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.energyict.mdc.engine.impl;
+
+import com.elster.jupiter.messaging.DestinationSpec;
+import com.elster.jupiter.messaging.MessageService;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.tasks.RecurrentTask;
+import com.elster.jupiter.tasks.TaskService;
+import com.elster.jupiter.upgrade.Upgrader;
+
+import javax.inject.Inject;
+
+public class Upgrader_V10_7_1 implements Upgrader {
+
+    public static final String COM_SERVER_ALIVE_TASK_DESTINATION = "ComServerStatusTopic";
+    private static final String COM_SERVER_STATUS_TASK_NAME = "ComServerAliveStatusTask";
+    private final MessageService messageService;
+    private final TaskService taskService;
+
+    @Inject
+    public Upgrader_V10_7_1(MessageService messageService, TaskService taskService) {
+        this.messageService = messageService;
+        this.taskService = taskService;
+    }
+
+    @Override
+    public void migrate(DataModelUpgrader dataModelUpgrader) {
+        messageService.getDestinationSpec(COM_SERVER_ALIVE_TASK_DESTINATION).ifPresent(DestinationSpec::delete);
+        taskService.getRecurrentTask(COM_SERVER_STATUS_TASK_NAME).ifPresent(RecurrentTask::delete);
+    }
+}
