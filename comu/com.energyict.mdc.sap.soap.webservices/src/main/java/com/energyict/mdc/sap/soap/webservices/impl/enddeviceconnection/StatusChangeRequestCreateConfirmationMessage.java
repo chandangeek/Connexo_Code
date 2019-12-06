f2d25f543f6067174460c404a934b52a903c66ac
Vladimir Rodionov <rodionov@mera.ru>
2019-12-06 13:46:16 +0300

    [CXO-11453] add Log elements to single and bulk connection status change requests

--- comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/enddeviceconnection/StatusChangeRequestCreateConfirmationMessage.java
+++ comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/enddeviceconnection/StatusChangeRequestCreateConfirmationMessage.java
@@ -7,7 +7,10 @@ import com.elster.jupiter.servicecall.DefaultState;
 import com.elster.jupiter.servicecall.ServiceCall;
 import com.energyict.mdc.common.device.data.Device;
 import com.energyict.mdc.sap.soap.webservices.SAPCustomPropertySets;
+import com.energyict.mdc.sap.soap.webservices.impl.MessageSeeds;
 import com.energyict.mdc.sap.soap.webservices.impl.ProcessingResultCode;
+import com.energyict.mdc.sap.soap.webservices.impl.WebServiceActivator;
+import com.energyict.mdc.sap.soap.webservices.impl.servicecall.ServiceCallHelper;
 import com.energyict.mdc.sap.soap.webservices.impl.servicecall.enddeviceconnection.ConnectionStatusChangeDomainExtension;
 import com.energyict.mdc.sap.soap.wsdl.webservices.smartmeterconnectionstatuschangerequestcreateconfirmation.BusinessDocumentMessageHeader;
 import com.energyict.mdc.sap.soap.wsdl.webservices.smartmeterconnectionstatuschangerequestcreateconfirmation.BusinessDocumentMessageID;
@@ -64,11 +67,21 @@ public class StatusChangeRequestCreateConfirmationMessage {
             confirmationMessage = OBJECT_FACTORY.createSmrtMtrUtilsConncnStsChgReqERPCrteConfMsg();
         }
 
-        public Builder from(ServiceCall parent, List<ServiceCall> childs, Instant now) {
+        public Builder from(ServiceCall parent, List<ServiceCall> children, Instant now) {
             ConnectionStatusChangeDomainExtension extension = parent.getExtension(ConnectionStatusChangeDomainExtension.class)
                     .orElseThrow(() -> new IllegalStateException("Unable to get domain extension for service call"));
             confirmationMessage.setMessageHeader(createHeader(extension.getRequestId(), extension.getUuid(), now));
-            confirmationMessage.setUtilitiesConnectionStatusChangeRequest(createBody(parent, childs));
+            confirmationMessage.setUtilitiesConnectionStatusChangeRequest(createBody(parent, children));
+            // parent state still not changed before sending confirmation, so check children states
+            if (ServiceCallHelper.hasAllChildrenInState(children, DefaultState.SUCCESSFUL)) {
+                confirmationMessage.setLog(createSuccessfulLog());
+            } else if (ServiceCallHelper.hasAllChildrenInState(children, DefaultState.CANCELLED)) {
+                confirmationMessage.setLog(createFailedLog(MessageSeeds.SERVICE_CALL_WAS_CANCELLED.getDefaultFormat()));
+            } else if (ServiceCallHelper.hasAnyChildState(children, DefaultState.SUCCESSFUL)) {
+                confirmationMessage.setLog(createPartiallySuccessfulLog());
+            } else {
+                confirmationMessage.setLog(createFailedLog());
+            }
             return this;
         }
 
@@ -164,8 +177,8 @@ public class StatusChangeRequestCreateConfirmationMessage {
             UtilitiesConnectionStatusChangeResultCode resultCode =
                     OBJECT_FACTORY.createUtilitiesConnectionStatusChangeResultCode();
             resultCode.setValue(serviceCall.getState().equals(DefaultState.SUCCESSFUL)
-                    ? ProcessingResultCode.SUCCESSFUL.getCode()
-                    : ProcessingResultCode.FAILED.getCode());
+                    ? ConnectionStatusProcessingResultCode.SUCCESSFUL.getCode()
+                    : ConnectionStatusProcessingResultCode.FAILED.getCode());
             deviceConnectionStatus.setUtilitiesDeviceConnectionStatusProcessingResultCode(resultCode);
 
             return deviceConnectionStatus;
@@ -196,6 +209,43 @@ public class StatusChangeRequestCreateConfirmationMessage {
             UUID messageUUID = OBJECT_FACTORY.createUUID();
             messageUUID.setValue(uuid);
             return messageUUID;
+        }
+
+        private Log createSuccessfulLog() {
+            Log log = OBJECT_FACTORY.createLog();
+            log.setBusinessDocumentProcessingResultCode(ProcessingResultCode.SUCCESSFUL.getCode());
+            return log;
+        }
+
+        private Log createFailedLog() {
+            Log log = OBJECT_FACTORY.createLog();
+            log.setBusinessDocumentProcessingResultCode(ProcessingResultCode.FAILED.getCode());
+            return log;
+        }
+
+        private Log createFailedLog(String message) {
+            Log log = OBJECT_FACTORY.createLog();
+            log.setBusinessDocumentProcessingResultCode(ProcessingResultCode.FAILED.getCode());
+            log.getItem().add(createLogItem(message));
+            return log;
+        }
+
+        private Log createPartiallySuccessfulLog() {
+            Log log = OBJECT_FACTORY.createLog();
+            log.setBusinessDocumentProcessingResultCode(ProcessingResultCode.PARTIALLY_SUCCESSFUL.getCode());
+            return log;
+        }
+
+        private LogItem createLogItem(String message) {
+            LogItemCategoryCode logItemCategoryCode = OBJECT_FACTORY.createLogItemCategoryCode();
+            logItemCategoryCode.setValue(WebServiceActivator.PROCESSING_ERROR_CATEGORY_CODE);
+
+            LogItem logItem = OBJECT_FACTORY.createLogItem();
+            logItem.setTypeID(UNSUCCESSFUL_PROCESSING_ERROR_TYPE_ID);
+            logItem.setCategoryCode(logItemCategoryCode);
+            logItem.setNote(message);
+
+            return logItem;
         }
 
         public StatusChangeRequestCreateConfirmationMessage build() {
