63ae3962919a947e1bb3fa3d48deedd3c879f624
Vladimir Rodionov <rodionov@mera.ru>
2019-12-12 17:05:46 +0300

    [CXO-11547] revert isValid method

--- comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/meterreadingdocument/MeterReadingDocumentResultCreateConfirmationRequestMessage.java
+++ comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/meterreadingdocument/MeterReadingDocumentResultCreateConfirmationRequestMessage.java
@@ -36,6 +36,10 @@ public class MeterReadingDocumentResultCreateConfirmationRequestMessage {
         return processingResultCodes;
     }
 
+    public boolean isValid() {
+        return id != null && uuid != null && !processingResultCodes.isEmpty();
+    }
+
     public boolean isBulk() {
         return bulk;
     }
