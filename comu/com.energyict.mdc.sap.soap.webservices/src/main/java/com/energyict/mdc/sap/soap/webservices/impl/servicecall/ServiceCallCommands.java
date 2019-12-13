a1576a2af87a39dcc8e251d2beee4b502668da02
Viyunov <rviyunov@merann.ru>
2019-12-13 14:02:06 +0300

    CXO-11521: Cleanup.

--- comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/servicecall/ServiceCallCommands.java
+++ comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/servicecall/ServiceCallCommands.java
@@ -54,7 +54,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static com.energyict.mdc.sap.soap.webservices.impl.WebServiceActivator.APPLICATION_NAME;
