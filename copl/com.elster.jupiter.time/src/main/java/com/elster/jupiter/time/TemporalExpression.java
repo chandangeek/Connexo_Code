2b5fe4421c1e86519e354d9df853f156d314075e
asmet <asmet@mera.ru>
2020-01-09 13:56:56 +0300

    review fix

--- copl/com.elster.jupiter.time/src/main/java/com/elster/jupiter/time/TemporalExpression.java
+++ copl/com.elster.jupiter.time/src/main/java/com/elster/jupiter/time/TemporalExpression.java
@@ -224,7 +224,7 @@ public final class TemporalExpression implements ScheduleExpression {
         }
         offset.addTo(base, true);
 
-        if (base.getTime().before(previous.getTime())) {
+        if ((every.getTimeUnit().equals(TimeDuration.TimeUnit.HOURS) && !base.getTime().after(previous.getTime())) || base.getTime().before(previous.getTime())) {
             every.addTo(base, false);
         }
 
