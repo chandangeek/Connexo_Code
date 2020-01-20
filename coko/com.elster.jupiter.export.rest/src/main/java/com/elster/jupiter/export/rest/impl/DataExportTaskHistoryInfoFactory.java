58f562df04862b7ce2da1a0859fad0a0598caf3a
Dmitry Golovin <walterdive@gmail.com>
2020-01-20 12:41:13 +0300

    [CONM-612] add status id to DataExportTask info

--- coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/DataExportTaskHistoryInfoFactory.java
+++ coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/DataExportTaskHistoryInfoFactory.java
@@ -209,12 +209,17 @@ public class DataExportTaskHistoryInfoFactory {
         if (DataExportStatus.BUSY.equals(dataExportStatus)) {
             info.statusPrefix = thesaurus.getFormat(TranslationKeys.SINCE).format(statusTranslation);
             info.statusDate = info.startedOn;
+            info.statusId = 0;
         } else if ((DataExportStatus.FAILED.equals(dataExportStatus)) || (DataExportStatus.SUCCESS.equals(dataExportStatus))) {
             info.statusPrefix = thesaurus.getFormat(TranslationKeys.ON).format(statusTranslation);
             info.statusDate = info.finishedOn;
+            info.statusId = 1;
         } else {
             info.statusPrefix = statusTranslation;
+            info.statusId = 2;
         }
+
+
     }
 
     private static Long calculateDuration(Instant startedOn, Instant finishedOn) {
