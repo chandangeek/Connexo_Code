fbbe029b3aae51b5bb35950d45c9a8b7c5070f34
Olga Nacharkina <onachark@mera.ru>
2020-01-23 20:55:00 +0300

    [CXO-11690] changed error message

--- comu/com.energyict.mdc.sap.soap.custom/src/main/java/com/energyict/mdc/sap/soap/custom/export/MessageSeeds.java
+++ comu/com.energyict.mdc.sap.soap.custom/src/main/java/com/energyict/mdc/sap/soap/custom/export/MessageSeeds.java
@@ -16,7 +16,7 @@ public enum MessageSeeds implements MessageSeed {
     SOME_DEVICES_HAVE_NONE_OF_THE_SELECTED_READINGTYPES(1027, "dataexport.device.mismatch", "Some devices of device group {0} don''t contain the selected reading type(s) that have to be exported.", Level.WARNING),
     EXPORT_PERIOD_COVERS_FUTURE(1029, "dataexport.exportwindow.overlapsfuture", "The export window {0} overlaps with the future. As a result the exported data is incomplete.", Level.WARNING),
     NO_DATA_TOEXPORT(1030, "dataexport.nodata", "There is no data to export.", Level.INFO),
-    ITEM_DOES_NOT_HAVE_CREATED_DATA_FOR_EXPORT_WINDOW(1052, "dataexport.item.created.nodata", "Item {0} doesn''t contain created data for the selected export window.", Level.WARNING),
+    ITEM_DOES_NOT_HAVE_CREATED_DATA_FOR_EXPORT_WINDOW(1052, "dataexport.item.created.nodata", "Item {0} doesn''t contain created data or profile id is not set for the selected export window.", Level.WARNING),
     ITEM_DOES_NOT_HAVE_CHANGED_DATA_FOR_UPDATE_WINDOW(1053, "dataexport.item.changed.nodata", "Item {0} doesn''t contain changed data for the selected update window.", Level.WARNING);
 
     private final int number;
