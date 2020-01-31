3f7c98a010eac84db8b5585c64082060e1048811
Dmitry Golovin <walterdive@gmail.com>
2020-01-31 12:07:55 +0300

    [CXO-11737] add new rest point

--- coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/DataExportTaskResource.java
+++ coko/com.elster.jupiter.export.rest/src/main/java/com/elster/jupiter/export/rest/impl/DataExportTaskResource.java
@@ -136,16 +136,23 @@ public class DataExportTaskResource {
     @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
     @RolesAllowed({Privileges.Constants.VIEW_DATA_EXPORT_TASK, Privileges.Constants.ADMINISTRATE_DATA_EXPORT_TASK, Privileges.Constants.UPDATE_DATA_EXPORT_TASK, Privileges.Constants.UPDATE_SCHEDULE_DATA_EXPORT_TASK, Privileges.Constants.RUN_DATA_EXPORT_TASK, Privileges.Constants.VIEW_HISTORY})
     public PagedInfoList getDataExportTasks(@BeanParam JsonQueryParameters queryParameters, @HeaderParam(X_CONNEXO_APPLICATION_NAME) String appCode) {
-        String applicationName = getApplicationNameFromCode(appCode);
-        ExportTaskFinder finder = dataExportService.findExportTasks().ofApplication(applicationName);
+        ExportTaskFinder finder = dataExportService.findExportTasks().ofApplication(getApplicationNameFromCode(appCode);
         finder.setStart(queryParameters.getStart().orElse(0));
-        queryParameters.getLimit().ifPresent(finder::setLimit);
-        List<DataExportTaskInfo> infos = finder.stream()
-                .map(dataExportTaskInfoFactory::asInfoWithMinimalHistory)
-                .sorted((dt1, dt2) -> dt1.name.compareToIgnoreCase(dt2.name))
-                .collect(Collectors.toList());
-        return PagedInfoList.fromPagedList("dataExportTasks", infos, queryParameters);
+        finder.setLimit(queryParameters.getLimit().orElse(0) + 1);
+        return PagedInfoList.fromPagedList("dataExportTasks", getListOfDataExportTasksInfo(finder), queryParameters);
     }
+
+    @GET
+    @Path("/fullListOfExportTasks")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @RolesAllowed({Privileges.Constants.VIEW_DATA_EXPORT_TASK, Privileges.Constants.ADMINISTRATE_DATA_EXPORT_TASK, Privileges.Constants.UPDATE_DATA_EXPORT_TASK, Privileges.Constants.UPDATE_SCHEDULE_DATA_EXPORT_TASK, Privileges.Constants.RUN_DATA_EXPORT_TASK, Privileges.Constants.VIEW_HISTORY})
+    public PagedInfoList getLimitExportTasks(@BeanParam JsonQueryParameters queryParameters, @HeaderParam(X_CONNEXO_APPLICATION_NAME) String appCode) {
+        ExportTaskFinder finder = dataExportService.findExportTasks().ofApplication(getApplicationNameFromCode(appCode));
+        finder.setStart(queryParameters.getStart().orElse(0));
+        finder.setLimit(Integer.MAX_VALUE);
+        return PagedInfoList.fromPagedList("dataExportTasks", getListOfDataExportTasksInfo(finder), queryParameters);
+    }
+
 
     @GET
     @Path("/usagepoint/{usagePointId}/{purposeId}")
@@ -928,4 +935,12 @@ public class DataExportTaskResource {
                 }
         ));
     }
+
+    private List<DataExportTaskInfo> getListOfDataExportTasksInfo(ExportTaskFinder finder ){
+        return finder.stream()
+                .map(dataExportTaskInfoFactory::asInfoWithMinimalHistory)
+                .sorted((dt1, dt2) -> dt1.name.compareToIgnoreCase(dt2.name))
+                .collect(Collectors.toList());
+    }
+
 }
