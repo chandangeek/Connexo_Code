96e886c0d4b6fb7c464065cf2e76d1b5f12b1edf
Vladimir Rodionov <rodionov@mera.ru>
2020-02-04 13:25:33 +0300

    [CXO-11747] rework

--- comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/task/UpdateSapExportTaskHandler.java
+++ comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/task/UpdateSapExportTaskHandler.java
@@ -4,13 +4,17 @@
 
 package com.energyict.mdc.sap.soap.webservices.impl.task;
 
+import com.elster.jupiter.cbo.IdentifiedObject;
 import com.elster.jupiter.export.DataExportService;
 import com.elster.jupiter.export.ExportTask;
 import com.elster.jupiter.export.ReadingTypeDataExportItem;
+import com.elster.jupiter.metering.Channel;
+import com.elster.jupiter.metering.ChannelsContainer;
 import com.elster.jupiter.metering.EndDevice;
+import com.elster.jupiter.metering.Meter;
+import com.elster.jupiter.metering.ReadingType;
 import com.elster.jupiter.metering.groups.EndDeviceGroup;
 import com.elster.jupiter.metering.groups.EnumeratedEndDeviceGroup;
-import com.elster.jupiter.metering.groups.EnumeratedGroup;
 import com.elster.jupiter.metering.groups.MeteringGroupsService;
 import com.elster.jupiter.tasks.TaskExecutor;
 import com.elster.jupiter.tasks.TaskOccurrence;
@@ -18,8 +22,11 @@ import com.elster.jupiter.util.time.Interval;
 import com.energyict.mdc.sap.soap.webservices.SAPCustomPropertySets;
 import com.energyict.mdc.sap.soap.webservices.impl.WebServiceActivator;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.energyict.mdc.sap.soap.webservices.impl.MeasurementTaskAssignmentChangeProcessor.DEFAULT_GROUP_NAME;
@@ -41,38 +48,51 @@ public class UpdateSapExportTaskHandler implements TaskExecutor {
     @Override
     public void execute(TaskOccurrence taskOccurrence) {
         Optional<EndDeviceGroup> endDeviceGroup = meteringGroupsService.findEndDeviceGroupByName(WebServiceActivator.getExportTaskDeviceGroupName().orElse(DEFAULT_GROUP_NAME));
+        Optional<? extends ExportTask> exportTask = dataExportService.getReadingTypeDataExportTaskByName(WebServiceActivator.getExportTaskName().orElse(DEFAULT_TASK_NAME));
         if (endDeviceGroup.isPresent()) {
-            ((EnumeratedEndDeviceGroup) endDeviceGroup.get()).getEntries().stream().filter(endDevice -> deviceCanBeRemoved(endDevice))
-                    .forEach(device -> ((EnumeratedEndDeviceGroup) endDeviceGroup.get()).remove(device));
+            if (exportTask.isPresent()) {
+                Map<IdentifiedObject, List<ReadingTypeDataExportItem>> exportItemsMap = exportTask.get().getReadingDataSelectorConfig().get().getExportItems().stream()
+                        .collect(Collectors.groupingBy(item -> item.getDomainObject()));
+                ((EnumeratedEndDeviceGroup) endDeviceGroup.get()).getEntries().stream()
+                        .filter(endDevice -> deviceCanBeRemoved(exportTask.get(), exportItemsMap, endDevice.getMember()))
+                        .forEach(device -> ((EnumeratedEndDeviceGroup) endDeviceGroup.get()).remove(device));
+            } else {
+                endDeviceGroup.get().delete();
+            }
         }
     }
 
-    private boolean deviceCanBeRemoved(EnumeratedGroup.Entry<EndDevice> endDeviceEntry) {
-        Optional<? extends ExportTask> exportTask = dataExportService.getReadingTypeDataExportTaskByName(WebServiceActivator.getExportTaskName().orElse(DEFAULT_TASK_NAME));
-        if (exportTask.isPresent()) {
-            boolean isSelectorConfigPresent = exportTask.get().getReadingDataSelectorConfig().isPresent();
-            if (isSelectorConfigPresent) {
-                List<? extends ReadingTypeDataExportItem> exportItems = exportTask.get().getReadingDataSelectorConfig().get().getExportItems().stream()
-                        .filter(item -> ((EndDevice) item.getDomainObject()).getId() == endDeviceEntry.getMember().getId())
-                        .collect(Collectors.toList());
-                if (exportItems.isEmpty()) {
-                    return false;
-                } else {
-                    return exportItems.stream().allMatch(item -> {
-                        Optional<Interval> lastProfileIdInterval = sapCustomPropertySets.getLastProfileIdIntervalForChannelOnDevice(Long.parseLong(endDeviceEntry.getMember().getAmrId()),
-                                item.getReadingType().getMRID());
-                        if (lastProfileIdInterval.isPresent()) {
-                            if (lastProfileIdInterval.get().getEnd() == null) {
-                                return false;
-                            }
-                            return item.getLastExportedPeriodEnd().isPresent() && item.getLastExportedPeriodEnd().get().isAfter(lastProfileIdInterval.get().getEnd());
-                        }
-                        return true;
-                    });
+    private boolean deviceCanBeRemoved(ExportTask exportTask, Map<IdentifiedObject, List<ReadingTypeDataExportItem>> exportItemsMap, EndDevice member) {
+        boolean isSelectorConfigPresent = exportTask.getReadingDataSelectorConfig().isPresent();
+        if (isSelectorConfigPresent) {
+            List<? extends ReadingTypeDataExportItem> exportItems = exportItemsMap.getOrDefault(member, Collections.emptyList());
+            if (exportItems.isEmpty()) {
+                if (member instanceof Meter) {
+                    Set<ReadingType> readingTypeSet = exportTask.getReadingDataSelectorConfig().get().getReadingTypes();
+                    return ((Meter) member).getChannelsContainers().stream()
+                            .map(ChannelsContainer::getChannels)
+                            .flatMap(List::stream)
+                            .map(Channel::getReadingTypes)
+                            .flatMap(List::stream)
+                            .noneMatch(readingTypeSet::contains);
                 }
+                return true;
             } else {
+                return exportItems.stream().allMatch(item -> isNothingToExportOnDataSource(item, member));
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isNothingToExportOnDataSource(ReadingTypeDataExportItem item, EndDevice member) {
+        Optional<Interval> lastProfileIdInterval = sapCustomPropertySets.getLastProfileIdIntervalForChannelOnDevice(Long.parseLong(member.getAmrId()),
+                item.getReadingType().getMRID());
+        if (lastProfileIdInterval.isPresent()) {
+            if (lastProfileIdInterval.get().getEnd() == null) {
                 return false;
             }
+            return item.getLastExportedPeriodEnd().isPresent() && item.getLastExportedPeriodEnd().get().isAfter(lastProfileIdInterval.get().getEnd());
         }
         return true;
     }
