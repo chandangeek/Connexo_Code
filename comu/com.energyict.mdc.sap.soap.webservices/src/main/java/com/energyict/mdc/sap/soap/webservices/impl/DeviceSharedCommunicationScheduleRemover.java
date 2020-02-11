b5f3766b0ef97aa0d19e625cc78c7da1473d27b3
Vladimir Rodionov <rodionov@mera.ru>
2020-02-11 17:33:37 +0300

    [CXO-11786] rework schedule remover

--- comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/DeviceSharedCommunicationScheduleRemover.java
+++ comu/com.energyict.mdc.sap.soap.webservices/src/main/java/com/energyict/mdc/sap/soap/webservices/impl/DeviceSharedCommunicationScheduleRemover.java
@@ -10,13 +10,12 @@ import com.elster.jupiter.nls.Thesaurus;
 import com.energyict.mdc.common.device.data.Device;
 import com.energyict.mdc.common.scheduling.ComSchedule;
 import com.energyict.mdc.device.data.DeviceService;
-import com.energyict.mdc.sap.soap.webservices.impl.custompropertyset.MessageSeeds;
-import com.energyict.mdc.scheduling.SchedulingService;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component(name = DeviceSharedCommunicationScheduleRemover.NAME,
@@ -26,17 +25,20 @@ public class DeviceSharedCommunicationScheduleRemover {
 
     public static final String NAME = "DeviceSharedCommunicationScheduleRemover";
     private volatile Thesaurus thesaurus;
-    private volatile SchedulingService schedulingService;
     private volatile DeviceService deviceService;
 
     public void remove(long deviceId) {
         Device device = lockDeviceOrThrowException(deviceId);
-        List<ComSchedule> schedules = schedulingService.getAllSchedules().stream()
-                .filter(s -> device.getComTaskExecutions().stream()
-                        .anyMatch(comTaskExecution -> s.getComTasks().contains(comTaskExecution.getComTask())))
+        List<ComSchedule> schedules = device.getComTaskExecutions().stream()
+                .filter(c -> c.usesSharedSchedule())
+                .map(c -> c.getComSchedule())
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .distinct()
                 .collect(Collectors.toList());
         for (ComSchedule schedule : schedules) {
             device.getComTaskExecutions().stream()
+                    .filter(comTaskExecution -> comTaskExecution.usesSharedSchedule())
                     .filter(comTaskExecution -> schedule.getComTasks().contains(comTaskExecution.getComTask()))
                     .forEach(comTaskExecution -> comTaskExecution.schedule(null));
         }
@@ -45,17 +47,12 @@ public class DeviceSharedCommunicationScheduleRemover {
 
     private Device lockDeviceOrThrowException(long deviceId) {
         return deviceService.findAndLockDeviceById(deviceId)
-                .orElseThrow(() -> new SAPWebServiceException(thesaurus, MessageSeeds.NO_SUCH_DEVICE, deviceId));
+                .orElseThrow(() -> new SAPWebServiceException(thesaurus, MessageSeeds.NO_DEVICE_FOUND, deviceId));
     }
 
     @Reference
     public void setDeviceService(DeviceService deviceService) {
         this.deviceService = deviceService;
-    }
-
-    @Reference
-    public void setSchedulingService(SchedulingService schedulingService) {
-        this.schedulingService = schedulingService;
     }
 
     @Reference
