d5c149dd092c0cb8a11448e03c4b351704787856
Sergey Vorobiev <svorob@mera.ru>
2020-02-21 14:55:37 +0300

    CXO-11836 upgrader rework. Fix bugs of CONM-1227

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/UpgraderV10_4_9.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/UpgraderV10_4_9.java
@@ -11,8 +11,6 @@ import javax.inject.Inject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Logger;
 
 public class UpgraderV10_4_9 implements Upgrader {
@@ -28,8 +26,8 @@ public class UpgraderV10_4_9 implements Upgrader {
 
     @Override
     public void migrate(DataModelUpgrader dataModelUpgrader) {
-        dataModel.useConnectionRequiringTransaction(this::updateCRLTable);
         dataModelUpgrader.upgrade(dataModel, Version.version(10, 4, 9));
+        dataModel.useConnectionRequiringTransaction(this::updateCRLTable);
     }
 
     private void updateCRLTable(Connection connection) {
@@ -37,19 +35,13 @@ public class UpgraderV10_4_9 implements Upgrader {
         if (definedCRLTasks != 0) {
             throw new RuntimeException("Cannot upgrade while CRL tasks defined and cannot be automatically upgraded. Please delete your CRL tasks and retry");
         }
-        final String dropConstraint = "ALTER TABLE" + TableSpecs.DDC_CRL_REQUEST_TASK_PROPS.name() + " DROP CONSTRAINT FK_DDC_CRL_SECURITYACCESSOR";
-        final String dropColumn = "ALTER TABLE" + TableSpecs.DDC_CRL_REQUEST_TASK_PROPS.name() + " DROP COLUMN SECURITY_ACCESSOR";
-
-        List<String> updateSql = Arrays.asList(dropConstraint, dropColumn);
-        updateSql.forEach(sqlStatement -> {
-            try (PreparedStatement statement = connection.prepareStatement(sqlStatement)) {
-                logger.info("Executing: " + sqlStatement);
-                statement.executeUpdate();
-            } catch (SQLException e) {
-                logger.warning("Couldn't perform upgrade for \n" + sqlStatement);
-                throw new UnderlyingSQLFailedException(e);
-            }
-        });
+        String sqlStatement = "ALTER TABLE " + TableSpecs.DDC_CRL_REQUEST_TASK_PROPS.name() + " DROP COLUMN SECURITY_ACCESSOR";
+        try (PreparedStatement statement = connection.prepareStatement(sqlStatement)) {
+            logger.info("Executing: " + sqlStatement);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            logger.warning("Couldn't perform upgrade for \n" + sqlStatement);
+            throw new UnderlyingSQLFailedException(e);
+        }
     }
-
 }
