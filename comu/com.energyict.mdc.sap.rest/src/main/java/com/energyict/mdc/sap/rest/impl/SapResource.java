d7f589881d29f872b41373ae2e2cef34eda645bb
ssafyannikov <ssafyannikov@avaya.com>
2020-03-10 18:16:58 +0300

    [CXO-11810] Fix wrong imports

--- comu/com.energyict.mdc.sap.rest/src/main/java/com/energyict/mdc/sap/rest/impl/SapResource.java
+++ comu/com.energyict.mdc.sap.rest/src/main/java/com/energyict/mdc/sap/rest/impl/SapResource.java
@@ -19,6 +19,13 @@ import com.energyict.mdc.sap.soap.webservices.security.Privileges;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.time.Clock;
