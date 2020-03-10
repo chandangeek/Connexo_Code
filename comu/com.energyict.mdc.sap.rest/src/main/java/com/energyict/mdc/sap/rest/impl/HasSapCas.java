e0f166dc76ac3684def8516699ce8f10fc329137
ssafyannikov <ssafyannikov@avaya.com>
2020-03-10 16:58:35 +0300

    [CXO-11810][FE] Some rest fixes

---
+++ comu/com.energyict.mdc.sap.rest/src/main/java/com/energyict/mdc/sap/rest/impl/HasSapCas.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 by Honeywell International Inc. All Rights Reserved
+ */
+
+package com.energyict.mdc.sap.rest.impl;
+
+public class HasSapCas {
+    public boolean exist;
+
+    public HasSapCas() {
+    }
+
+    public HasSapCas(boolean exist) {
+        this.exist = exist;
+    }
+}
