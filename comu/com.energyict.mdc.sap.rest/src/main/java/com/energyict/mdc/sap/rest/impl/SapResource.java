c2463104179a64b72a36829062e6c8350c4f6532
ssafyannikov <ssafyannikov@avaya.com>
2020-03-10 18:08:39 +0300

    Revert "[CXO-11810][FE] Fix wrong commit"
    This reverts commit 59c06b9c2bf97261f9d00eb3557e7ab439609402.

--- comu/com.energyict.mdc.sap.rest/src/main/java/com/energyict/mdc/sap/rest/impl/SapResource.java
+++ comu/com.energyict.mdc.sap.rest/src/main/java/com/energyict/mdc/sap/rest/impl/SapResource.java
@@ -19,7 +19,6 @@ import com.energyict.mdc.sap.soap.webservices.security.Privileges;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
-import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.time.Clock;
