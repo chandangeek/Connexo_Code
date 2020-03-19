b9e2920041426e64b2734db3442ffdc7a09af809
Iulian Dabuleanu <iulian.dabuleanu@honeywell.com>
2020-03-19 15:35:35 +0200

    COMMUNICATION-3330 New DLMS Protocols for ACUD - update dlms properties

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/acud/properties/AcudDlmsProperties.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/acud/properties/AcudDlmsProperties.java
@@ -1,8 +1,24 @@
 package com.energyict.protocolimplv2.dlms.acud.properties;
 
 
+import com.energyict.dlms.CipheringType;
+import com.energyict.dlms.aso.ConformanceBlock;
 import com.energyict.protocolimplv2.nta.dsmr23.DlmsProperties;
 
 public class AcudDlmsProperties extends DlmsProperties {
+
+    @Override
+    public byte[] getSystemIdentifier() {
+        return null;
+    }
+
+    @Override
+    public ConformanceBlock getConformanceBlock() {
+        ConformanceBlock conformanceBlock = super.getConformanceBlock();
+        conformanceBlock.setPriorityManagementSupported(false);
+        conformanceBlock.setAttribute0SupportedWithGet(false);
+        conformanceBlock.setEventNotification(false);
+        return conformanceBlock;
+    }
 
 }
