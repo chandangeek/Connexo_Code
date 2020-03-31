c987e831e048da8a8cffb589f45f59c299598d61
Iulian Dabuleanu <iulian.dabuleanu@honeywell.com>
2020-03-31 13:45:22 +0300

    COMMUNICATION-3330 New DLMS Protocols for ACUD  - Add addressing mode on 4 bytes config parameter

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/acud/properties/AcudConfigurationSupport.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/acud/properties/AcudConfigurationSupport.java
@@ -1,7 +1,14 @@
 package com.energyict.protocolimplv2.dlms.acud.properties;
 
+import com.energyict.dlms.common.DlmsProtocolProperties;
+import com.energyict.mdc.upl.properties.PropertySpec;
 import com.energyict.mdc.upl.properties.PropertySpecService;
+import com.energyict.protocolimpl.nls.PropertyTranslationKeys;
 import com.energyict.protocolimplv2.nta.dsmr23.DlmsConfigurationSupport;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AcudConfigurationSupport extends DlmsConfigurationSupport {
 
@@ -9,4 +16,14 @@ public class AcudConfigurationSupport extends DlmsConfigurationSupport {
         super(propertySpecService);
     }
 
+    @Override
+    public List<PropertySpec> getUPLPropertySpecs() {
+        List<PropertySpec> propertySpecs = new ArrayList<>(super.getUPLPropertySpecs());
+        propertySpecs.add(addressModePropertySpec());
+        return propertySpecs;
+    }
+
+    private PropertySpec addressModePropertySpec() {
+        return this.bigDecimalSpec(DlmsProtocolProperties.ADDRESSING_MODE, true, PropertyTranslationKeys.DLMS_ADDRESSING_MODE, new BigDecimal(4), new BigDecimal(2), new BigDecimal(4));
+    }
 }
