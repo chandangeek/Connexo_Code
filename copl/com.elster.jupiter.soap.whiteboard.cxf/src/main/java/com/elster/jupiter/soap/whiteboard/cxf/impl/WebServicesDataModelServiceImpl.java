2c03474a1903730d125bd53bff116c7faffe1299
Yan R <yaratay@mera.ru>
2020-04-13 19:02:01 +0300

    CXO-12046: Application server memory leak - avoid IllegalStateException in case web service call session hangs for > 30 min - hash maps -> concurrent hash maps - javadoc improved - some minor refactoring

--- copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/WebServicesDataModelServiceImpl.java
+++ copl/com.elster.jupiter.soap.whiteboard.cxf/src/main/java/com/elster/jupiter/soap/whiteboard/cxf/impl/WebServicesDataModelServiceImpl.java
@@ -67,8 +67,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.elster.jupiter.orm.Version.version;
-
 /**
  * Created by bvn on 5/4/16.
  */
@@ -215,8 +213,6 @@ public class WebServicesDataModelServiceImpl implements WebServicesDataModelServ
                 provider.getAttributeTranslations());
     }
 
-    ;
-
     public void removeAttributeTypes(WebServiceCallRelatedAttributeTypeProvider provider) {
         webServiceCallOccurrenceService.removeRelatedObjectTypes(provider.getAttributeTranslations());
     }
@@ -237,6 +233,7 @@ public class WebServicesDataModelServiceImpl implements WebServicesDataModelServ
                 bind(String.class).annotatedWith(Names.named("LogDirectory")).toInstance(logDirectory);
                 bind(ThreadPrincipalService.class).toInstance(threadPrincipalService);
                 bind(Clock.class).toInstance(clock);
+                bind(NlsService.class).toInstance(nlsService);
 
                 bind(WebServicesService.class).toInstance(webServicesService);
                 bind(WebServicesServiceImpl.class).toInstance(webServicesService);
@@ -259,8 +256,8 @@ public class WebServicesDataModelServiceImpl implements WebServicesDataModelServ
             logDirectory = logDirectory + File.separator;
         }
         endPointConfigurationService = new EndPointConfigurationServiceImpl(dataModel, eventService);
-        webServicesService = new WebServicesServiceImpl(dataModel, eventService, transactionService, clock, endPointConfigurationService);
-        webServiceCallOccurrenceService = new WebServiceCallOccurrenceServiceImpl(dataModel, endPointConfigurationService, nlsService);
+        webServiceCallOccurrenceService = new WebServiceCallOccurrenceServiceImpl(dataModel, nlsService);
+        webServicesService = new WebServicesServiceImpl(dataModel, eventService, transactionService, clock, endPointConfigurationService, webServiceCallOccurrenceService);
         this.dataModel.register(this.getModule(logDirectory));
         upgradeService.register(
                 InstallIdentifier.identifier("Pulse", WebServicesService.COMPONENT_NAME),
