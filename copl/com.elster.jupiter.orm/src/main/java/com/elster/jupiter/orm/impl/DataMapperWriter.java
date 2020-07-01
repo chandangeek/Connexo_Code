0153934554cecfa598f3788f35d91c075885d749
Jozsef Szekrenyes <jozsef.szekrenyes@honeywell.com>
2020-07-01 13:27:25 +0300

    CXO-12428 : Audit is started even if no audited column was updated

--- copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/impl/DataMapperWriter.java
+++ copl/com.elster.jupiter.orm/src/main/java/com/elster/jupiter/orm/impl/DataMapperWriter.java
@@ -228,7 +228,9 @@ public class DataMapperWriter<T> {
             journal(object, now);
         }
 
-        new AuditTrailDataWriter(dataMapper, object, now, UnexpectedNumberOfUpdatesException.Operation.UPDATE, columns.size() == 0).audit();
+        if (isAuditEnabled() && getTable().hasAudit() && doJournal(columns)) {
+            new AuditTrailDataWriter(dataMapper, object, now, UnexpectedNumberOfUpdatesException.Operation.UPDATE, columns.size() == 0).audit();
+        }
 
         prepare(object, true, now);
         ColumnImpl[] versionCountColumns = getTable().getVersionColumns();
@@ -285,6 +287,14 @@ public class DataMapperWriter<T> {
             pair.getFirst().setDomainValue(object, pair.getLast() + 1);
         }
         refresh(object, false);
+    }
+
+    private boolean isAuditEnabled() {
+        return getAuditEnabledProperty().toLowerCase().equals("true");
+    }
+
+    private String getAuditEnabledProperty() {
+        return Optional.ofNullable(getTable().getDataModel().getOrmService().getEnableAuditing()).orElse("false");
     }
 
     private boolean doJournal(List<ColumnImpl> columns) {
