391bd9ded060e03fc8a14fbd3047e0d6b423c1d6
Yan R <yan.rataushko@orioninc.com>
2020-07-02 20:08:06 +0300

    CXO-12064 some tests are checked, fixed, refactored

--- coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/ReadingTypeGeneratorForDataLoggerIT.java
+++ coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/ReadingTypeGeneratorForDataLoggerIT.java
@@ -6,12 +6,15 @@ package com.elster.jupiter.metering.impl;
 
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
 import com.elster.jupiter.bpm.impl.BpmModule;
+import com.elster.jupiter.calendar.impl.CalendarModule;
 import com.elster.jupiter.cps.CustomPropertySetService;
 import com.elster.jupiter.cps.impl.CustomPropertySetsModule;
+import com.elster.jupiter.datavault.impl.DataVaultModule;
 import com.elster.jupiter.domain.util.impl.DomainUtilModule;
 import com.elster.jupiter.events.impl.EventsModule;
 import com.elster.jupiter.fsm.FiniteStateMachineService;
 import com.elster.jupiter.fsm.impl.FiniteStateMachineModule;
+import com.elster.jupiter.http.whiteboard.TokenService;
 import com.elster.jupiter.ids.impl.IdsModule;
 import com.elster.jupiter.license.LicenseService;
 import com.elster.jupiter.messaging.h2.impl.InMemoryMessagingModule;
@@ -19,16 +22,18 @@ import com.elster.jupiter.nls.impl.NlsModule;
 import com.elster.jupiter.orm.h2.H2OrmModule;
 import com.elster.jupiter.parties.impl.PartyModule;
 import com.elster.jupiter.properties.PropertySpecService;
-import com.elster.jupiter.pubsub.Subscriber;
 import com.elster.jupiter.pubsub.impl.PubSubModule;
 import com.elster.jupiter.search.SearchService;
 import com.elster.jupiter.security.thread.impl.ThreadSecurityModule;
+import com.elster.jupiter.soap.whiteboard.cxf.impl.WebServicesModule;
+import com.elster.jupiter.tasks.impl.TaskModule;
+import com.elster.jupiter.time.impl.TimeModule;
 import com.elster.jupiter.transaction.TransactionService;
-import com.elster.jupiter.transaction.VoidTransaction;
 import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.upgrade.UpgradeService;
 import com.elster.jupiter.upgrade.impl.UpgradeModule;
-import com.elster.jupiter.users.UserService;
+import com.elster.jupiter.usagepoint.lifecycle.config.impl.UsagePointLifeCycleConfigurationModule;
+import com.elster.jupiter.users.impl.UserModule;
 import com.elster.jupiter.util.Pair;
 import com.elster.jupiter.util.UtilModule;
 
@@ -37,13 +42,13 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
+import org.osgi.service.http.HttpService;
 
 import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -53,33 +58,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
-@Ignore // install framework ro revise this
-public class ReadingTypeGeneratorForParametersTest {
+public class ReadingTypeGeneratorForDataLoggerIT {
     private Injector injector;
 
     @Mock
     private BundleContext bundleContext;
     @Mock
-    private UserService userService;
-    @Mock
     private EventAdmin eventAdmin;
-    @Mock
-    private Subscriber topicHandler;
 
     private InMemoryBootstrapModule inMemoryBootstrapModule = new InMemoryBootstrapModule();
 
-
     private class MockModule extends AbstractModule {
-
         @Override
         protected void configure() {
-            bind(UserService.class).toInstance(userService);
             bind(BundleContext.class).toInstance(bundleContext);
             bind(EventAdmin.class).toInstance(eventAdmin);
             bind(SearchService.class).toInstance(mock(SearchService.class));
             bind(PropertySpecService.class).toInstance(mock(PropertySpecService.class));
             bind(LicenseService.class).toInstance(mock(LicenseService.class));
             bind(UpgradeService.class).toInstance(UpgradeModule.FakeUpgradeService.getInstance());
+            bind(TokenService.class).toInstance(mock(TokenService.class));
+            bind(HttpService.class).toInstance(mock(HttpService.class));
         }
     }
 
@@ -102,13 +101,19 @@ public class ReadingTypeGeneratorForParametersTest {
                 new FiniteStateMachineModule(),
                 new TransactionModule(),
                 new NlsModule(),
-                new CustomPropertySetsModule()
+                new CustomPropertySetsModule(),
+                new CalendarModule(),
+                new WebServicesModule(),
+                new TimeModule(),
+                new UsagePointLifeCycleConfigurationModule(),
+                new TaskModule(),
+                new UserModule(),
+                new DataVaultModule()
         );
-        injector.getInstance(TransactionService.class).execute(() -> {
+        injector.getInstance(TransactionService.class).run(() -> {
             injector.getInstance(CustomPropertySetService.class);
             injector.getInstance(FiniteStateMachineService.class);
-            injector.getInstance(MeteringServiceImpl.class);
-            return null;
+            injector.getInstance(MeteringDataModelService.class);
         });
     }
 
@@ -119,24 +124,22 @@ public class ReadingTypeGeneratorForParametersTest {
 
     @Test
     public void generateTest() {
-        getTransactionService().execute(new VoidTransaction() {
-            @Override
-            protected void doPerform() {
-                assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(0).overridingErrorMessage("We should have started with 0 reading types");
+        getTransactionService().run(() -> {
+            assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(0)
+                    .overridingErrorMessage("We should have started with 0 reading types");
 
-                ReadingTypeGeneratorForParameters readingTypeGeneratorForParameters = new ReadingTypeGeneratorForParameters();
-                List<Pair<String, String>> readingTypes = readingTypeGeneratorForParameters.generateReadingTypes();
+            ReadingTypeGeneratorForDataLogger readingTypeGeneratorForDataLogger = new ReadingTypeGeneratorForDataLogger();
+            List<Pair<String, String>> readingTypes = readingTypeGeneratorForDataLogger.generateReadingTypes();
+            assertThat(readingTypes).hasSize(32);
+            getMeteringService().createAllReadingTypes(readingTypes);
 
-                getMeteringService().createAllReadingTypes(readingTypes);
-
-                assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(readingTypes.size()).overridingErrorMessage("Expected " + readingTypes.size() + " reading types");
-            }
+            assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(readingTypes.size())
+                    .overridingErrorMessage("Expected " + readingTypes.size() + " reading types");
         });
-
     }
 
-    private MeteringServiceImpl getMeteringService() {
-        return injector.getInstance(MeteringServiceImpl.class);
+    private ServerMeteringService getMeteringService() {
+        return injector.getInstance(ServerMeteringService.class);
     }
 
     private TransactionService getTransactionService() {
--- coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/ReadingTypeGeneratorForGasIT.java
+++ coko/com.elster.jupiter.metering/src/test/java/com/elster/jupiter/metering/impl/ReadingTypeGeneratorForGasIT.java
@@ -6,29 +6,34 @@ package com.elster.jupiter.metering.impl;
 
 import com.elster.jupiter.bootstrap.h2.impl.InMemoryBootstrapModule;
 import com.elster.jupiter.bpm.impl.BpmModule;
+import com.elster.jupiter.calendar.impl.CalendarModule;
 import com.elster.jupiter.cps.CustomPropertySetService;
 import com.elster.jupiter.cps.impl.CustomPropertySetsModule;
+import com.elster.jupiter.datavault.impl.DataVaultModule;
 import com.elster.jupiter.domain.util.impl.DomainUtilModule;
 import com.elster.jupiter.events.impl.EventsModule;
 import com.elster.jupiter.fsm.FiniteStateMachineService;
 import com.elster.jupiter.fsm.impl.FiniteStateMachineModule;
+import com.elster.jupiter.http.whiteboard.TokenService;
 import com.elster.jupiter.ids.impl.IdsModule;
 import com.elster.jupiter.license.LicenseService;
 import com.elster.jupiter.messaging.h2.impl.InMemoryMessagingModule;
 import com.elster.jupiter.nls.impl.NlsModule;
 import com.elster.jupiter.orm.h2.H2OrmModule;
 import com.elster.jupiter.parties.impl.PartyModule;
-import com.elster.jupiter.properties.PropertySpecService;
-import com.elster.jupiter.pubsub.Subscriber;
+import com.elster.jupiter.properties.impl.BasicPropertiesModule;
 import com.elster.jupiter.pubsub.impl.PubSubModule;
 import com.elster.jupiter.search.SearchService;
 import com.elster.jupiter.security.thread.impl.ThreadSecurityModule;
+import com.elster.jupiter.soap.whiteboard.cxf.impl.WebServicesModule;
+import com.elster.jupiter.tasks.impl.TaskModule;
+import com.elster.jupiter.time.impl.TimeModule;
 import com.elster.jupiter.transaction.TransactionService;
-import com.elster.jupiter.transaction.VoidTransaction;
 import com.elster.jupiter.transaction.impl.TransactionModule;
 import com.elster.jupiter.upgrade.UpgradeService;
 import com.elster.jupiter.upgrade.impl.UpgradeModule;
-import com.elster.jupiter.users.UserService;
+import com.elster.jupiter.usagepoint.lifecycle.config.impl.UsagePointLifeCycleConfigurationModule;
+import com.elster.jupiter.users.impl.UserModule;
 import com.elster.jupiter.util.Pair;
 import com.elster.jupiter.util.UtilModule;
 
@@ -37,13 +42,13 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.event.EventAdmin;
+import org.osgi.service.http.HttpService;
 
 import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -53,33 +58,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
-@Ignore // install framework ro revise this
-public class ReadingTypeGeneratorForWaterTest {
+public class ReadingTypeGeneratorForGasIT {
+
     private Injector injector;
 
     @Mock
     private BundleContext bundleContext;
     @Mock
-    private UserService userService;
-    @Mock
     private EventAdmin eventAdmin;
-    @Mock
-    private Subscriber topicHandler;
 
     private InMemoryBootstrapModule inMemoryBootstrapModule = new InMemoryBootstrapModule();
 
-
     private class MockModule extends AbstractModule {
-
         @Override
         protected void configure() {
-            bind(UserService.class).toInstance(userService);
             bind(BundleContext.class).toInstance(bundleContext);
             bind(EventAdmin.class).toInstance(eventAdmin);
             bind(SearchService.class).toInstance(mock(SearchService.class));
-            bind(PropertySpecService.class).toInstance(mock(PropertySpecService.class));
             bind(LicenseService.class).toInstance(mock(LicenseService.class));
             bind(UpgradeService.class).toInstance(UpgradeModule.FakeUpgradeService.getInstance());
+            bind(TokenService.class).toInstance(mock(TokenService.class));
+            bind(HttpService.class).toInstance(mock(HttpService.class));
         }
     }
 
@@ -89,6 +88,8 @@ public class ReadingTypeGeneratorForWaterTest {
                 new MockModule(),
                 inMemoryBootstrapModule,
                 new InMemoryMessagingModule(),
+                new TimeModule(),
+                new BasicPropertiesModule(),
                 new IdsModule(),
                 new MeteringModule(),
                 new PartyModule(),
@@ -102,13 +103,18 @@ public class ReadingTypeGeneratorForWaterTest {
                 new BpmModule(),
                 new FiniteStateMachineModule(),
                 new NlsModule(),
-                new CustomPropertySetsModule()
+                new CustomPropertySetsModule(),
+                new CalendarModule(),
+                new WebServicesModule(),
+                new TimeModule(),
+                new UsagePointLifeCycleConfigurationModule(),
+                new TaskModule(),
+                new UserModule(),
+                new DataVaultModule()
         );
-        injector.getInstance(TransactionService.class).execute(() -> {
+        injector.getInstance(TransactionService.class).run(() -> {
             injector.getInstance(CustomPropertySetService.class);
             injector.getInstance(FiniteStateMachineService.class);
-            injector.getInstance(MeteringServiceImpl.class);
-            return null;
         });
     }
 
@@ -119,24 +125,19 @@ public class ReadingTypeGeneratorForWaterTest {
 
     @Test
     public void generateTest() {
-        getTransactionService().execute(new VoidTransaction() {
-            @Override
-            protected void doPerform() {
-                assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(0).overridingErrorMessage("We should have started with 0 reading types");
+        getTransactionService().run(() -> {
+            assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(0).overridingErrorMessage("We should have started with 0 reading types");
 
-                ReadingTypeGeneratorForWater readingTypeGeneratorForWater = new ReadingTypeGeneratorForWater();
-                List<Pair<String, String>> readingTypes = readingTypeGeneratorForWater.generateReadingTypes();
-                getMeteringService().createAllReadingTypes(readingTypes);
+            ReadingTypeGeneratorForGas readingTypeGeneratorForGas = new ReadingTypeGeneratorForGas();
+            List<Pair<String, String>> readingTypes = readingTypeGeneratorForGas.generateReadingTypes();
+            getMeteringService().createAllReadingTypes(readingTypes);
 
-                assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(readingTypes.size()).overridingErrorMessage("Expected " + readingTypes.size() + " reading types");
-
-            }
+            assertThat(getMeteringService().getAvailableReadingTypes()).hasSize(readingTypes.size()).overridingErrorMessage("Expected " + readingTypes.size() + " reading types");
         });
-
     }
 
-    private MeteringServiceImpl getMeteringService() {
-        return injector.getInstance(MeteringServiceImpl.class);
+    private ServerMeteringService getMeteringService() {
+        return injector.getInstance(ServerMeteringService.class);
     }
 
     private TransactionService getTransactionService() {
