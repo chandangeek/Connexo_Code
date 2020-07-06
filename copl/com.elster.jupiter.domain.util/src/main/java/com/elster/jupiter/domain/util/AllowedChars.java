5bfe5e193db403818b14b4cdea4ff1cc1049b11b
E492165 <rudraiah.mr@honeywell.com>
2020-07-06 10:46:11 +0530

    CXO-1222-: RegEx change to support non mandatory Feilds.

--- copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/domain/util/AllowedChars.java
+++ copl/com.elster.jupiter.domain.util/src/main/java/com/elster/jupiter/domain/util/AllowedChars.java
@@ -20,7 +20,7 @@ public enum AllowedChars {
         public static final String ALPHABETS = "[a-zA-Z]";
         public static final String NUMBERS="[0-9]";
         public static final String ALPHABETS_AND_NUMBERS = "^[a-zA-Z0-9._@-]+$";
-        public static final String ALLOWED_CHARS_WITH_SPACE = "^[a-zA-Z0-9\\s._@-]+$";
-        public static final String ALLOWED_SPECIAL_CHARS = "^[a-zA-Z0-9\\s._*#$!(){}@-]+$";
+        public static final String ALLOWED_CHARS_WITH_SPACE = "^|[a-zA-Z0-9\\s._@-]+$";
+        public static final String ALLOWED_SPECIAL_CHARS = "^|[a-zA-Z0-9\\s.,?_*#$!(){}@-]+$";
     }
 }
