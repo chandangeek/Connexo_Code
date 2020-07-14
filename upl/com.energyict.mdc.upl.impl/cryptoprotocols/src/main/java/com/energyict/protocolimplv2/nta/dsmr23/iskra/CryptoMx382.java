13fa9e55671b4199bf0cb9b2ee53fdedb116fc57
Cristian Ile <cristian.ile@honeywell.com>
2020-07-14 14:07:52 +0200

    COMMUNICATION-3475 reset cached frame counter when changing EK, other bug fixes

--- upl/com.energyict.mdc.upl.impl/cryptoprotocols/src/main/java/com/energyict/protocolimplv2/nta/dsmr23/iskra/CryptoMx382.java
+++ upl/com.energyict.mdc.upl.impl/cryptoprotocols/src/main/java/com/energyict/protocolimplv2/nta/dsmr23/iskra/CryptoMx382.java
@@ -1,21 +1,19 @@
 package com.energyict.protocolimplv2.nta.dsmr23.iskra;
 
 import com.energyict.common.framework.CryptoDlmsSession;
-import com.energyict.dialer.connection.HHUSignOn;
-import com.energyict.dialer.connection.HHUSignOnV2;
 import com.energyict.dlms.protocolimplv2.DlmsSession;
 import com.energyict.mdc.protocol.ComChannel;
-import com.energyict.mdc.protocol.ComChannelType;
-import com.energyict.mdc.protocol.SerialPortComChannel;
 import com.energyict.mdc.upl.issue.IssueFactory;
-import com.energyict.mdc.upl.messages.legacy.*;
+import com.energyict.mdc.upl.messages.legacy.DeviceMessageFileExtractor;
+import com.energyict.mdc.upl.messages.legacy.KeyAccessorTypeExtractor;
+import com.energyict.mdc.upl.messages.legacy.LoadProfileExtractor;
+import com.energyict.mdc.upl.messages.legacy.NumberLookupExtractor;
+import com.energyict.mdc.upl.messages.legacy.TariffCalendarExtractor;
 import com.energyict.mdc.upl.meterdata.CollectedDataFactory;
 import com.energyict.mdc.upl.nls.NlsService;
-import com.energyict.mdc.upl.offline.OfflineDevice;
 import com.energyict.mdc.upl.properties.Converter;
 import com.energyict.mdc.upl.properties.HasDynamicProperties;
 import com.energyict.mdc.upl.properties.PropertySpecService;
-import com.energyict.protocolimplv2.hhusignon.IEC1107HHUSignOn;
 import com.energyict.protocolimplv2.nta.dsmr23.Iskra.Mx382;
 import com.energyict.protocolimplv2.nta.dsmr23.common.CryptoDSMR23Properties;
 import com.energyict.protocolimplv2.nta.dsmr23.messages.CryptoDSMR23MessageExecutor;
@@ -32,7 +30,7 @@ public class CryptoMx382 extends Mx382 {
 
     @Override
     public String getVersion() {
-        return "Crypto version: 2020-04-15";
+        return "Crypto version: 2020-07-14";
     }
 
     @Override
