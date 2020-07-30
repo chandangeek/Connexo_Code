7909098b7ff5410d033ede8523bd016828bea370
H165678 <dragos-florin.iancu@honeywell.com>
2020-07-30 10:03:02 +0300

    CXO-12218 - CP_HPS_Connexo_10.8_2020-05-18 - Sensitive Information Disclosure - as per PM and security architect request

--- coko/com.elster.jupiter.systemadmin.rest/src/main/java/com/elster/jupiter/systemadmin/rest/imp/response/SystemInfo.java
+++ coko/com.elster.jupiter.systemadmin.rest/src/main/java/com/elster/jupiter/systemadmin/rest/imp/response/SystemInfo.java
@@ -7,22 +7,14 @@ package com.elster.jupiter.systemadmin.rest.imp.response;
 import java.util.Map;
 
 public class SystemInfo {
-    public String jre;
-    public String jvm;
-    public String javaHome;
-    public String javaClassPath;
+
     public String osName;
     public String osArch;
     public String timeZone;
-    public int numberOfProcessors;
     public long totalMemory;
     public long freeMemory;
     public long usedMemory;
     public long lastStartedTime;
     public long serverUptime;
-    public String dbConnectionUrl;
-    public String dbUser;
-    public String dbMaxConnectionsNumber;
-    public String dbMaxStatementsPerRequest;
     public Map<String, String> environmentParameters;
 }
