390c4084b7f65d3441911caf5e8ef48039a0903e
Olga Nacharkina <onachark@mera.ru>
2020-08-26 12:25:53 +0300

    [CONM-1531] fix

--- copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/model/keys/HsmKeyType.java
+++ copl/com.elster.jupiter.hsm/src/main/java/com/elster/jupiter/hsm/model/keys/HsmKeyType.java
@@ -1,15 +1,8 @@
 package com.elster.jupiter.hsm.model.keys;
 
-import com.elster.jupiter.hsm.model.HsmBaseException;
-
-import com.atos.worldline.jss.api.custom.energy.ProtectedSessionKeyType;
-
 import java.util.Objects;
 
 public class HsmKeyType {
-
-    private static final int AES_KEY_LENGTH = 16;
-    private static final int AES256_KEY_LENGTH = 32;
 
     private final HsmJssKeyType hsmJssKeyType;
     private final String label;
@@ -17,7 +10,6 @@ public class HsmKeyType {
     private final SessionKeyCapability renewCapability;
     private final int keySize;
     private final boolean isReversible;
-
 
 
     public HsmKeyType(HsmJssKeyType hsmJssKeyType, String label, SessionKeyCapability importCapability, SessionKeyCapability renewCapability, int keySize, boolean isReversible) {
@@ -29,7 +21,9 @@ public class HsmKeyType {
         this.isReversible = isReversible;
     }
 
-    public HsmJssKeyType getHsmJssKeyType() {  return hsmJssKeyType; }
+    public HsmJssKeyType getHsmJssKeyType() {
+        return hsmJssKeyType;
+    }
 
     public String getLabel() {
         return label;
@@ -45,24 +39,6 @@ public class HsmKeyType {
 
     public int getKeySize() {
         return keySize;
-    }
-
-    public ProtectedSessionKeyType getSessionKeyType() throws HsmBaseException {
-        /**
-         * This might be wrong but with no specs this is all I could do :)
-         * Somehow it works E2E for 16 and 32 bytes. What we do for other sizes!? Hell knows!
-         */
-        if (HsmJssKeyType.AES.equals(this.hsmJssKeyType)) {
-            if (keySize == AES_KEY_LENGTH) {
-                return ProtectedSessionKeyType.AES;
-            }
-            if (keySize == AES256_KEY_LENGTH) {
-                return ProtectedSessionKeyType.AES_256;
-            }
-            throw new HsmBaseException("Could not determine session key type for key length (expected 16 or 32):" + keySize);
-        }
-        throw new HsmBaseException("Only AES device key accepted:" + keySize);
-
     }
 
     @Override
