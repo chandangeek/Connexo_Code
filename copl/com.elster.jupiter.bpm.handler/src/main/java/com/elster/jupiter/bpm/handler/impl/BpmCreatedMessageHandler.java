663c22d2de92705056c9ccd9ede3ae348da94aa1
H403324 <sanchita.rath@honeywell.com>
2020-10-05 11:22:26 +0530

    CXO-12734-Bug fix for running processes in Connexo UI

--- copl/com.elster.jupiter.bpm.handler/src/main/java/com/elster/jupiter/bpm/handler/impl/BpmCreatedMessageHandler.java
+++ copl/com.elster.jupiter.bpm.handler/src/main/java/com/elster/jupiter/bpm/handler/impl/BpmCreatedMessageHandler.java
@@ -9,6 +9,8 @@ import com.elster.jupiter.bpm.BpmServer;
 import com.elster.jupiter.messaging.Message;
 import com.elster.jupiter.messaging.subscriber.MessageHandler;
 import com.elster.jupiter.util.json.JsonService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -23,31 +25,18 @@ public class BpmCreatedMessageHandler implements MessageHandler {
         this.bpmRestClient = server;
     }
 
-
     @Override
     public void process(Message message) {
         BpmProcess bpmProcess =  jsonService.deserialize(message.getPayload(), BpmProcess.class);
-        String targetURL = "/services/rest/server/containers/"+bpmProcess.getDeploymentId()+"/processes/"+bpmProcess.getId()+"/instances"+getProcessParameters(bpmProcess.getParameters());
-        bpmRestClient.doPost(targetURL, null, bpmProcess.getAuth());
-    }
-
-    private String getProcessParameters(Map<String, Object> params) {
-        String result = "";
-        if (params != null && !params.isEmpty()) {
-            for (Map.Entry<String, Object> entry : params.entrySet()) {
-                String paramValue = "&";
-                try {
-                    if (entry.getValue() != null) {
-                        paramValue = URLEncoder.encode(entry.getValue().toString(), "UTF-8") + "&";
-                    }
-                } catch (UnsupportedEncodingException e) {
-                    throw new IllegalArgumentException(e);
-                }
-                result += "map_" + entry.getKey() + "=" + paramValue;
+        ObjectMapper mapper=new ObjectMapper();
+        String payload=null;
+        if(bpmProcess.getParameters()!=null) {
+            try {
+                payload = mapper.writeValueAsString(bpmProcess.getParameters());
+            } catch (JsonProcessingException e) {
             }
-            result = "?" + result.substring(0, result.length()-1);
         }
-        return result;
+        String targetURL = "/services/rest/server/containers/"+bpmProcess.getDeploymentId()+"/processes/"+bpmProcess.getId()+"/instances";
+        bpmRestClient.doPost(targetURL, payload, bpmProcess.getAuth());
     }
-
 }
