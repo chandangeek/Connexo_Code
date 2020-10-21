0f6501d20b8ea54003dc567a36228b406418f266
E492165 <rudraiah.mr@honeywell.com>
2020-10-21 16:55:34 +0530

    CXO-12702 Default Apache Folders removed during installation.

--- subsystems/com.elster.jupiter.subsystem.platform/assembly/bin/install.pl
+++ subsystems/com.elster.jupiter.subsystem.platform/assembly/bin/install.pl
@@ -626,6 +626,8 @@ sub install_tomcat {
         if (-e "$TOMCAT_BASE/slf4j-log4j12-1.7.5.jar") {
             copy("$TOMCAT_BASE/slf4j-log4j12-1.7.5.jar","$TOMCAT_BASE/tomcat/lib/slf4j-log4j12-1.7.5.jar");
         }
+        # Removing examples directory
+        if (-d "$TOMCAT_DIR/webapps/examples") { rmtree("$TOMCAT_DIR/webapps/examples"); }
 		chdir "$TOMCAT_DIR/bin";
 		replace_in_file("$TOMCAT_BASE/$TOMCAT_DIR/conf/server.xml","<Connector port=\"8009\" protocol=\"AJP/1.3\" redirectPort=\"8443\" />","<Connector port=\"$TOMCAT_AJP_PORT\" protocol=\"AJP/1.3\" redirectPort=\"8443\" />");
 		replace_in_file("$TOMCAT_BASE/$TOMCAT_DIR/conf/tomcat-users.xml","password=\"analyst\"","password=\"$TOMCAT_ADMIN_PASSWORD\"");
