1796c780615448f9e6981bd6ba03c5f8738302e2
Yan R <yan.rataushko@orioninc.com>
2021-02-15 12:45:28 +0300

    CONM-1751 update after review
    (cherry picked from commit bb134af17d7c28446b76353687adc54b0720601a)

--- comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/MessageSeedsTest.java
+++ comu/com.energyict.mdc.device.topology/src/test/java/com/energyict/mdc/device/topology/impl/MessageSeedsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 by Honeywell International Inc. All Rights Reserved
+ * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
  */
 
 package com.energyict.mdc.device.topology.impl;
