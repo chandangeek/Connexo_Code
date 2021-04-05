36e7c05f008541b7fb48acd8eea5b8342f4c4834
Sergey Vorobiev <sergey.vorobiev@orioninc.com>
2021-04-05 23:26:23 +0300

    CXO-13048 order rework

--- copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/conditions/Order.java
+++ copl/com.elster.jupiter.util/src/main/java/com/elster/jupiter/util/conditions/Order.java
@@ -7,8 +7,12 @@ package com.elster.jupiter.util.conditions;
 import com.elster.jupiter.util.MessageSeeds;
 import com.elster.jupiter.util.exception.NoFieldSpecifiedException;
 import com.elster.jupiter.util.exception.SqlInjectionException;
+import com.elster.jupiter.util.sql.SqlFragment;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 public final class Order {
@@ -17,12 +21,11 @@ public final class Order {
     private static final String COMMAND = "Order";
 
     public static final Order[] NOORDER = new Order[0];
-
+    private String rawSql;
     private String name;
     private final boolean ascending;
     private String function;
     private NullStrategy nullStrategy = NullStrategy.NONE;
-
 
     private Order(String name, boolean ascending) {
         String trimmedColumnName = name.trim();
@@ -55,6 +58,11 @@ public final class Order {
         return this;
     }
 
+    public Order applySqlString(String sql){
+        this.rawSql = sql;
+        return this;
+    }
+
     public Order toUpperCase() {
         return apply("upper");
     }
@@ -68,10 +76,13 @@ public final class Order {
     }
 
     private String getBaseClause(String resolvedField) {
-        if (function == null) {
-            return resolvedField + " " + ordering();
-        } else {
+        if (rawSql != null) {
+            return rawSql.replaceAll(name, resolvedField) + " " + ordering();
+        }
+        else if (function != null) {
             return function + "(" + resolvedField + ")" + " " + ordering();
+        } else {
+            return resolvedField + " " + ordering();
         }
     }
 
