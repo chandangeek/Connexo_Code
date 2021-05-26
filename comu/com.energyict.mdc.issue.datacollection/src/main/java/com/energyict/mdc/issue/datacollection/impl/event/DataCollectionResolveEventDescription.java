26d196199c3acdadff93270e9ef01f39ec157b7c
Yulia Alenkova <yulia.alenkova@mera.com>
2021-05-26 13:49:58 +0300

    CONM-2165 - fix for processing of the CONNECTION_LOST_AUTO_RESOLVE event

--- comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/event/DataCollectionResolveEventDescription.java
+++ comu/com.energyict.mdc.issue.datacollection/src/main/java/com/energyict/mdc/issue/datacollection/impl/event/DataCollectionResolveEventDescription.java
@@ -5,6 +5,8 @@
 package com.energyict.mdc.issue.datacollection.impl.event;
 
 import com.elster.jupiter.util.Checks;
+import com.energyict.mdc.common.tasks.history.ComSession;
+import com.energyict.mdc.issue.datacollection.event.ConnectionEvent;
 import com.energyict.mdc.issue.datacollection.event.ConnectionLostResolvedEvent;
 import com.energyict.mdc.issue.datacollection.event.DataCollectionEvent;
 import com.energyict.mdc.issue.datacollection.event.DeviceCommunicationFailureResolvedEvent;
@@ -27,13 +29,19 @@ public enum DataCollectionResolveEventDescription implements EventDescription {
     CONNECTION_LOST_AUTO_RESOLVE(
             "com/energyict/mdc/connectiontask/COMPLETION",
             ConnectionLostResolvedEvent.class) {
+        @Override
         public boolean validateEvent(Map<?, ?> map) {
-            if (super.validateEvent(map)) {
-                return isEmptyString(map, ModuleConstants.SKIPPED_TASK_IDS)
-                        && !isEmptyString(map, ModuleConstants.CONNECTION_TASK_ID);
-            }
-            return false;
+            return true;
         }
+
+        @Override
+        public boolean validateEvent(DataCollectionEvent dataCollectionEvent) {
+            return ((ConnectionEvent) dataCollectionEvent).getComSession()
+                    .map(ComSession::getSuccessIndicator)
+                    .filter(successIndicator -> !successIndicator.equals(ComSession.SuccessIndicator.Broken))
+                    .isPresent();
+        }
+
 
         @Override
         public String getUniqueKey() {
