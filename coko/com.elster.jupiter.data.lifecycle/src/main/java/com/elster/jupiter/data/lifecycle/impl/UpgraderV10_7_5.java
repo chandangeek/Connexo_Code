6446e0b9b8c63534e37cda0166b0a128d550b168
Olga Nacharkina <onachark@mera.ru>
2021-06-07 17:27:49 +0300

    [CXO-13802] fix propagation (update)

--- coko/com.elster.jupiter.data.lifecycle/src/main/java/com/elster/jupiter/data/lifecycle/impl/UpgraderV10_7_5.java
+++ coko/com.elster.jupiter.data.lifecycle/src/main/java/com/elster/jupiter/data/lifecycle/impl/UpgraderV10_7_5.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2021 by Honeywell International Inc. All Rights Reserved
+ */
+
 package com.elster.jupiter.data.lifecycle.impl;
 
 import com.elster.jupiter.upgrade.DummyUpgrader;
