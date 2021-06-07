a3b4911dbcd6945186adb5db457cd1bc1da0b7fb
Olga Nacharkina <onachark@mera.ru>
2021-06-07 17:04:05 +0300

    [CONM-2145] fix propagation

---
+++ comu/com.energyict.mdc.device.topology/src/main/java/com/energyict/mdc/device/topology/impl/UpgraderV10_4_21.java
@@ -0,0 +1,6 @@
+package com.energyict.mdc.device.topology.impl;
+
+import com.elster.jupiter.upgrade.DummyUpgrader;
+
+public class UpgraderV10_4_21 extends DummyUpgrader {
+}
---
+++ comu/com.energyict.mdc.device.topology/src/main/java/com/energyict/mdc/device/topology/impl/UpgraderV10_9_4.java
@@ -0,0 +1,22 @@
+package com.energyict.mdc.device.topology.impl;
+
+import com.elster.jupiter.orm.DataModel;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.orm.Version;
+import com.elster.jupiter.upgrade.Upgrader;
+
+import javax.inject.Inject;
+
+public class UpgraderV10_9_4 implements Upgrader {
+    private final DataModel dataModel;
+
+    @Inject
+    public UpgraderV10_9_4(DataModel dataModel) {
+        this.dataModel = dataModel;
+    }
+
+    @Override
+    public void migrate(DataModelUpgrader dataModelUpgrader) {
+        dataModelUpgrader.upgrade(this.dataModel, Version.version(10, 9, 4));
+    }
+}
