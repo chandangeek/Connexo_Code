8fae724ca93e73e46415fc6d42babb99ba833f15
Robert Mulyukov <robert.mulyukov@orioninc.com>
2021-07-22 17:45:22 +0300

    CONM-2250: fix of upgrader for this branch.

--- comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/UpgraderV10_8_15.java
+++ comu/com.energyict.mdc.device.data/src/main/java/com/energyict/mdc/device/data/impl/UpgraderV10_8_15.java
@@ -1,6 +1,23 @@
 package com.energyict.mdc.device.data.impl;
 
-import com.elster.jupiter.upgrade.DummyUpgrader;
+import com.elster.jupiter.orm.DataModelUpgrader;
+import com.elster.jupiter.upgrade.Upgrader;
 
-public class UpgraderV10_8_15 extends DummyUpgrader {
+import javax.inject.Inject;
+import java.util.logging.Logger;
+
+public class UpgraderV10_8_15 implements Upgrader {
+
+    private final InstallerV10_8_1Impl installerV10_8_1;
+    private final static Logger logger = Logger.getLogger(UpgraderV10_8_15.class.getName());
+
+    @Inject
+    UpgraderV10_8_15(InstallerV10_8_1Impl installerV10_8_1) {
+        this.installerV10_8_1 = installerV10_8_1;
+    }
+
+    @Override
+    public void migrate(DataModelUpgrader dataModelUpgrader) {
+        installerV10_8_1.createOrUpdateDashboardProcedures(logger);
+    }
 }
