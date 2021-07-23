dea8ef3d9bad5514f3560c59a4d01e3a29f7a32d
Dmitriy Borisov <dmitriy.borisov@orioninc.com>
2021-07-23 18:31:55 +0300

    COMMUNICATION-3733 - set HDLC connection as main for Sagemcom-CX2000-9 device

---
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/edp/EDPDlmsSession.java
@@ -0,0 +1,19 @@
+package com.energyict.protocolimplv2.edp;
+
+import com.energyict.dlms.protocolimplv2.DlmsSession;
+import com.energyict.dlms.protocolimplv2.DlmsSessionProperties;
+import com.energyict.dlms.protocolimplv2.connection.DlmsV2Connection;
+import com.energyict.dlms.protocolimplv2.connection.HDLCConnection;
+import com.energyict.mdc.protocol.ComChannel;
+
+public class EDPDlmsSession extends DlmsSession {
+
+    EDPDlmsSession(ComChannel comChannel, DlmsSessionProperties properties) {
+        super(comChannel, properties);
+    }
+
+    @Override
+    protected DlmsV2Connection defineTransportDLMSConnection() {
+        return new HDLCConnection(getComChannel(), getProperties());
+    }
+}
