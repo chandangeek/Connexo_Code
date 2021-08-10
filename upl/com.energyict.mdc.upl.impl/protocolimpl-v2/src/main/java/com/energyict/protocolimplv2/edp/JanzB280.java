56dc11c4e5052b3aca6c9d7232547938f0d1581f
Dmitriy Borisov <dmitriy.borisov@orioninc.com>
2021-08-10 10:45:42 +0300

    COMMUNICATION-3733 - prevent JANZ use HDLC connection

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/edp/JanzB280.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/edp/JanzB280.java
@@ -1,12 +1,16 @@
 package com.energyict.protocolimplv2.edp;
 
+import com.energyict.dlms.protocolimplv2.DlmsSession;
+import com.energyict.mdc.protocol.ComChannel;
 import com.energyict.mdc.upl.issue.IssueFactory;
 import com.energyict.mdc.upl.messages.legacy.DeviceMessageFileExtractor;
 import com.energyict.mdc.upl.messages.legacy.TariffCalendarExtractor;
 import com.energyict.mdc.upl.meterdata.CollectedDataFactory;
 import com.energyict.mdc.upl.nls.NlsService;
+import com.energyict.mdc.upl.offline.OfflineDevice;
 import com.energyict.mdc.upl.properties.Converter;
 import com.energyict.mdc.upl.properties.PropertySpecService;
+
 
 /**
  * Copyrights EnergyICT
@@ -18,6 +22,13 @@ public class JanzB280 extends CX20009 {
 
     public JanzB280(PropertySpecService propertySpecService, NlsService nlsService, Converter converter, CollectedDataFactory collectedDataFactory, IssueFactory issueFactory, TariffCalendarExtractor calendarExtractor, DeviceMessageFileExtractor messageFileExtractor) {
         super(propertySpecService, nlsService, converter, collectedDataFactory, issueFactory, calendarExtractor, messageFileExtractor);
+    }
+
+    @Override
+    public void init(OfflineDevice offlineDevice, ComChannel comChannel) {
+        this.offlineDevice = offlineDevice;
+        getDlmsSessionProperties().setSerialNumber(offlineDevice.getSerialNumber());
+        setDlmsSession(new DlmsSession(comChannel, getDlmsSessionProperties()));
     }
 
     @Override
