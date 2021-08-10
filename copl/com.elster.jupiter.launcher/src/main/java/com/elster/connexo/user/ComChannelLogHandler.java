a93fe851d0a73c085f5d2f5fb489f35af55621ab
Yulia Alenkova <yulia.alenkova@mera.com>
2021-08-10 10:33:11 +0300

    CONM-2156 - moving handler in the right place
    (cherry picked from commit 32a92e41ddef486e5349bc87c9a6155e3ec60881)
    
    # Conflicts:
    #	copl/com.elster.jupiter.launcher/src/main/java/com/elster/connexo/user/ComChannelLogHandler.java

--- copl/com.elster.jupiter.launcher/src/main/java/com/elster/connexo/user/ComChannelLogHandler.java
+++ copl/com.elster.jupiter.launcher/src/main/java/com/elster/connexo/user/ComChannelLogHandler.java
@@ -1,11 +1,8 @@
 /*
- * Copyright (c) 2017 by Honeywell International Inc. All Rights Reserved
+ * Copyright (c) 2021 by Honeywell International Inc. All Rights Reserved
  */
 
-package com.energyict.mdc.engine.impl.core;
-
-import com.energyict.mdc.common.tasks.history.ComSessionJournalEntry;
-import com.energyict.mdc.common.tasks.history.ComTaskExecutionMessageJournalEntry;
+package com.elster.connexo.user;
 
 import java.io.IOException;
 import java.text.MessageFormat;
@@ -15,16 +12,7 @@ import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-/**
- * Provides an implementation for the Handler class
- * that creates {@link ComSessionJournalEntry ComSessionJournalEntries}
- * or {@link ComTaskExecutionMessageJournalEntry ComTaskExecutionMessageJournalEntries}
- * each time bytes have been read or written to a {@link ComPortRelatedComChannel}.
- *
- * @author Rudi Vankeirsbilck (rudi)
- * @since 2013-03-14 (10:17)
- */
-class ComChannelLogHandler extends FileHandler {
+public class ComChannelLogHandler extends FileHandler {
 
     public ComChannelLogHandler() throws IOException, SecurityException {
         super();
