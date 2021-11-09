e0210b1e1996eb494d20c48a14951c5e65316f7d
Elena Borunova <elena.borunova@orioninc.com>
2021-11-09 17:46:51 +0300

    COMMUNICATION-3790 Change device event 21 meaning

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/idis/aec/events/AECStandardEventLog.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/idis/aec/events/AECStandardEventLog.java
@@ -21,7 +21,7 @@ public class AECStandardEventLog extends StandardEventLog {
                 meterEvents.add(new MeterEvent((Date) eventTimeStamp.clone(), MeterEvent.EXTERNAL_ALERT, eventId, "External alert detected"));
                 break;
             case 21:
-                meterEvents.add(new MeterEvent((Date) eventTimeStamp.clone(), MeterEvent.END_OF_NONPERIODIC_BILLING_INTERVAL, eventId, "End of non-periodic billing interval"));
+                meterEvents.add(new MeterEvent((Date) eventTimeStamp.clone(), MeterEvent.CONFIGURATION_PARAMETER_CHANGED, eventId, "Parameter modified"));
                 break;
             case 52:
                 meterEvents.add(new MeterEvent((Date) eventTimeStamp.clone(), MeterEvent.UNEXPECTED_CONSUMPTION, eventId, "Unexpected consumption"));
