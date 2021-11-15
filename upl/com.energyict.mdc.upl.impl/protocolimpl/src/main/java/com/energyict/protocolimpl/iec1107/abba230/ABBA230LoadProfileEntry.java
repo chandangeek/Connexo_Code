af8e51678878fd10dc6e8f81fd09498bb04fdbf2
Dmitriy Borisov <dmitriy.borisov@orioninc.com>
2021-11-15 13:11:52 +0300

    https://acsjira.honeywell.com/browse/COMMUNICATION-3822 - Connexo does not record Partial period flag for Channel data for AS230 meters
    + Refactor

--- upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/abba230/ABBA230LoadProfileEntry.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl/src/main/java/com/energyict/protocolimpl/iec1107/abba230/ABBA230LoadProfileEntry.java
@@ -1,5 +1,6 @@
 package com.energyict.protocolimpl.iec1107.abba230;
 
+import com.energyict.mdc.upl.ProtocolException;
 import com.energyict.protocolimpl.iec1107.abba1140.Calculate;
 import com.energyict.protocolimpl.utils.ProtocolUtils;
 
@@ -8,151 +9,172 @@ import java.io.IOException;
 import java.util.Calendar;
 import java.util.TimeZone;
 
+enum ParsingLPEntryStage
+{
+    StageType,
+    StageDate,
+    StageNewDayChangeConfValues;
+}
+
 /**
  * Copyrights EnergyICT
- * User: sva
- * Date: 3/01/12
- * Time: 15:35
+ * User: Dmitry Borisov
+ * Date: 12/11/21
+ * Time: 18:00
  */
-
 public class ABBA230LoadProfileEntry implements ABBA230ProfileEntry {
 
+    /** */
     ABBA230RegisterFactory registerFactory;
-    ByteArrayInputStream bai;
+    /** */
     int nrOfChannels;
-    boolean isValue = false;
+    /** Unix-epoch based date */
     long date;
-    int type;
+    /** Profile entry type if it is marker */
+    int profileEntryType;
+    /** Profile entry type if it is values */
+    int dataStatus; // ? if not marker store this
+    /** If interval type is not marker it's should contain values */
     double[] values = new double[4];
-    int status;
+    /** ??? */
     int integrationPeriod;
+    /** ??? */
     boolean isDst;
+    /** */
     byte[] channelMask;
+    /** */
     ProfileConfigRegister profileConfigRegister;
 
-    public void start( ABBA230RegisterFactory registerFactory, ByteArrayInputStream bai, int nrOfChannels)
-    throws IOException {
-
-        this.registerFactory = registerFactory;
-        this.bai = bai;
-        this.nrOfChannels = nrOfChannels;
-        this.values = new double[this.nrOfChannels];
-        date = 0;
-        init();
+    @Override
+    public void start( ABBA230RegisterFactory registerFactory, ByteArrayInputStream bai, int nrOfChannels) throws IOException {
+        this.registerFactory    = registerFactory;
+        this.nrOfChannels       = nrOfChannels;
+        this.values             = new double[this.nrOfChannels];
+        date                    = 0;
+        dataStatus              = -1;
+        profileEntryType        = -1;
+        init(bai);
     }
 
-    public void init()  throws IOException {
+    protected void readNewDayChangeConfValues(ByteArrayInputStream bai, int chMaskSize) throws IOException {
+        // 2 bytes Channel configuration
+        channelMask = new byte[chMaskSize];
+        bai.read(channelMask, 0, chMaskSize);
+
         // 1 byte
-        type = ProtocolUtils.getVal(bai);
+        int byte8               = ProtocolUtils.getVal(bai);
+        integrationPeriod       = registerFactory.getDataType().integrationPeriod.parse((byte) (byte8 & 0x0f));
+        isDst                   = (byte8 & 0x80) > 0;
+        profileConfigRegister   = new LoadProfileConfigRegister();
+        profileConfigRegister.loadConfig(registerFactory, channelMask);
+        nrOfChannels = profileConfigRegister.getNumberRegisters();
+    }
 
-        if( isMarker() ) {
-
-            if( isEndOfData() ) {
-                return;
-            }
-            // 4 bytes
-            date = (long)ProtocolUtils.getIntLE(bai)&0xFFFFFFFFL;
-
-            if (isNewDay() || isConfigurationChange()) {
-                // 2 bytes Channel configuration
-                channelMask = new byte[2];
-                channelMask[0] = (byte) bai.read();
-                channelMask[1] = (byte) bai.read();
-
-                // 1 byte
-                int byte8 = ProtocolUtils.getVal(bai);
-                integrationPeriod = registerFactory.getDataType().integrationPeriod.parse((byte) (byte8 & 0x0f));
-                isDst = (byte8 & 0x80) > 0;
-                profileConfigRegister = new LoadProfileConfigRegister();
-                profileConfigRegister.loadConfig(registerFactory, channelMask);
-                nrOfChannels = profileConfigRegister.getNumberRegisters();
-                return;
-            }
-
-        } else {
-            /** If it is not a marker, it is profile data.
-             * This means it starts with status */
-            status = type;
-            isValue = true;
-            for (int i=0;i<nrOfChannels;i++) {
-                long val = (int)Long.parseLong(Long.toHexString(ProtocolUtils.getLong(bai,3)));
-                values[i] = (val/10) * Calculate.exp(val%10);
-            }
+    protected void readValues( ByteArrayInputStream bai ) throws ProtocolException {
+        // If it is not a marker, it is profile data. This means it starts with status
+        dataStatus = profileEntryType;
+        for (int i = 0; i < nrOfChannels; i++) {
+            long val = (int)Long.parseLong(Long.toHexString(ProtocolUtils.getLong(bai,3))); // ???
+            values[i] = (val/10) * Calculate.exp(val%10);
         }
     }
 
-    public int getType() {
-        return type;
+    protected void finiteStateDataParser(ByteArrayInputStream bai, ParsingLPEntryStage stage ) throws IOException {
+        switch (stage) {
+            case StageType:
+                profileEntryType = ProtocolUtils.getVal(bai);
+                if(Marker.fromInt(profileEntryType) != null) {
+                    if (Marker.ENDOFDATA.getMarker() == profileEntryType) {
+                        return;
+                    }
+                    finiteStateDataParser(bai, ParsingLPEntryStage.StageDate); // after type should follow date.
+                }
+                else {
+                    readValues(bai);
+                }
+                break;
+            case StageDate:
+                date = (long) ProtocolUtils.getIntLE(bai) & 0xFFFFFFFFL;
+                finiteStateDataParser(bai, ParsingLPEntryStage.StageNewDayChangeConfValues); // wait for values after date
+                break;
+            case StageNewDayChangeConfValues:
+                if (Marker.NEWDAY.getMarker() == profileEntryType ||
+                    Marker.CONFIGURATIONCHANGE.getMarker() == profileEntryType) {
+                    readNewDayChangeConfValues(bai, 2);
+                }
+                break;
+        }
     }
 
+    protected void init(ByteArrayInputStream bai) throws IOException {
+        finiteStateDataParser(bai, ParsingLPEntryStage.StageType);
+    }
+
+    @Override
+    public int getProfileEntryType() {
+        return profileEntryType;
+    }
+
+    @Override
     public int getIntegrationPeriod() {
         return integrationPeriod;
     }
 
-    public byte[] getChannelmask() {
+    @Override
+    public byte[] getChannelMask() {
         return channelMask;
     }
 
+    @Override
     public ProfileConfigRegister getProfileConfig() {
        return profileConfigRegister;
     }
 
+    @Override
     public int getNumberOfChannels() {
         return nrOfChannels;
     }
 
+    @Override
     public boolean isDST() {
         return isDst;
     }
 
-    public int getStatus() {
-        return status;
+    @Override
+    public int getDataStatus() {
+        return dataStatus;
     }
 
+    @Override
     public double[] getValues() {
         return  values;
     }
 
+    @Override
     public long getTime() {
         return date;
     }
 
-
-    public boolean isMarker() {
-        return ((type == POWERUP) || (type == CONFIGURATIONCHANGE) || (type == POWERDOWN) ||
-                (type == NEWDAY) || (type == TIMECHANGE) || (type == DAYLIGHTSAVING) ||
-                (type == PROFILECLEARED) || (type == FORCEDENDOFDEMAND) || (type == ENDOFDATA));
-    }
-
-    boolean isNewDay() {
-        return type == NEWDAY;
-    }
-
-    boolean isConfigurationChange(){
-        return type == CONFIGURATIONCHANGE;
-    }
-
-    boolean isEndOfData(){
-        return type == ENDOFDATA;
-    }
-
+    @Override
     public String toString(TimeZone timeZone, boolean dst) {
         StringBuffer strBuff = new StringBuffer();
-        if (!isMarker()) {
-            strBuff.append("Demanddata:\n");
-            strBuff.append("   Status: 0x"+Integer.toHexString(status)+"\n");
-            for(int i=0;i<values.length;i++) {
-                strBuff.append("   Channel "+i+": "+values[i]+"\n");
+        Marker entry_marker = Marker.fromInt(profileEntryType);
+
+        if (null == entry_marker ) {
+            strBuff.append("Demand data:\n");
+            strBuff.append("   Status: 0x" + Integer.toHexString(dataStatus) + "\n");
+            for(int i = 0; i < values.length; i++) {
+                strBuff.append("   Channel " + i + ": " + values[i] + "\n");
             }
-        } else if(isEndOfData()) {
+        } else if(Marker.ENDOFDATA == entry_marker) {
             strBuff.append("End of data\n");
         } else {
-            Calendar cal = ProtocolUtils.getCalendar(timeZone,date);
-            strBuff.append("Marker: 0x"+Integer.toHexString(type)+" at "+cal.getTime()+" "+date+"\n");
-            if (isNewDay() || isConfigurationChange() ) {
-                strBuff.append("   ChannelMask: " + getChannelmask().toString());
-                strBuff.append("   IntegrationTime: "+Integer.toString(getIntegrationPeriod()));
-                strBuff.append("   dst: "+isDST());
+            Calendar cal = ProtocolUtils.getCalendar(timeZone, date);
+            strBuff.append("Marker: 0x" + Integer.toHexString(entry_marker.getMarker()) + " at " + cal.getTime() + " " + date + "\n");
+            if (Marker.NEWDAY == entry_marker || Marker.CONFIGURATIONCHANGE == entry_marker) {
+                strBuff.append("   ChannelMask: "       + getChannelMask().toString());
+                strBuff.append("   IntegrationTime: "   + getIntegrationPeriod());
+                strBuff.append("   dst: "               + isDST());
             }
         }
         return strBuff.toString();
