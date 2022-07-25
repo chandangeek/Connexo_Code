4fcc53218a10b8e3d09d3d281d3bd6c113e951cf
Yan R <yan.rataushko@orioninc.com>
2022-07-25 14:37:02 +0400

    CONM-2983 excluding device types that don't support firmware at all

--- comu/com.energyict.demo/src/main/java/com/elster/jupiter/demo/impl/commands/SetupFirmwareManagementCommand.java
+++ comu/com.energyict.demo/src/main/java/com/elster/jupiter/demo/impl/commands/SetupFirmwareManagementCommand.java
@@ -13,13 +13,15 @@ import com.energyict.mdc.firmware.FirmwareService;
 import com.energyict.mdc.firmware.FirmwareStatus;
 import com.energyict.mdc.firmware.FirmwareType;
 import com.energyict.mdc.firmware.FirmwareVersion;
+import com.energyict.mdc.upl.messages.ProtocolSupportedFirmwareOptions;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
+import java.util.Set;
 
-public class SetupFirmwareManagementCommand extends CommandWithTransaction{
+public class SetupFirmwareManagementCommand extends CommandWithTransaction {
 
     private final static String FIRMWARE_VERSION_V1 = "NTA-Sim_V_1.0.0";
     private final static String FIRMWARE_VERSION_V2 = "NTA-Sim_V_2.0.0";
@@ -41,17 +43,18 @@ public class SetupFirmwareManagementCommand extends CommandWithTransaction{
         deviceConfigurationService.findAllDeviceTypes().stream().forEach(this::setUpDeviceTypeForFirmwareManagement);
     }
 
-    public static boolean isExcluded(DeviceType deviceType){
+    public static boolean isExcluded(DeviceType deviceType) {
         return deviceType.getName().equals(LANDIS_GYR_ZMD_DEVICETYPE);
     }
 
-    private void setUpDeviceTypeForFirmwareManagement(DeviceType deviceType){
-        if (!isExcluded(deviceType)) {
-            try(InputStream v1inputStream = getClass().getClassLoader().getResourceAsStream(FIRMWARE_VERSION_V1+".firm");
-                InputStream v2inputStream = getClass().getClassLoader().getResourceAsStream(FIRMWARE_VERSION_V2+".firm")) {
+    private void setUpDeviceTypeForFirmwareManagement(DeviceType deviceType) {
+        Set<ProtocolSupportedFirmwareOptions> supportedFirmwareOptions = firmwareService.getSupportedFirmwareOptionsFor(deviceType);
+        if (!isExcluded(deviceType) && !supportedFirmwareOptions.isEmpty()) {
+            try (InputStream v1inputStream = getClass().getClassLoader().getResourceAsStream(FIRMWARE_VERSION_V1 + ".firm");
+                 InputStream v2inputStream = getClass().getClassLoader().getResourceAsStream(FIRMWARE_VERSION_V2 + ".firm")) {
                 FirmwareVersion v1 = firmwareService.getFirmwareVersionByVersionAndType(FIRMWARE_VERSION_V1, FirmwareType.METER, deviceType)
                         .orElseGet(() -> firmwareService.newFirmwareVersion(deviceType, FIRMWARE_VERSION_V1, FirmwareStatus.GHOST, FirmwareType.METER, IMAGE_IDENTIFIER).create());
-                setFirmwareBytes(v1,v1inputStream);
+                setFirmwareBytes(v1, v1inputStream);
 
                 FirmwareVersion v2 = firmwareService.getFirmwareVersionByVersionAndType(FIRMWARE_VERSION_V2, FirmwareType.METER, deviceType)
                         .orElseGet(() -> firmwareService.newFirmwareVersion(deviceType, FIRMWARE_VERSION_V2, FirmwareStatus.GHOST, FirmwareType.METER, IMAGE_IDENTIFIER).create());
@@ -61,23 +64,23 @@ public class SetupFirmwareManagementCommand extends CommandWithTransaction{
             }
             if (firmwareService.getAllowedFirmwareManagementOptionsFor(deviceType).isEmpty()) {
                 FirmwareManagementOptions options = firmwareService.newFirmwareManagementOptions(deviceType);
-                if(deviceType.getName().equals(ACTARIS_SL7000_DEVICETYPE)){
+                if (deviceType.getName().equals(ACTARIS_SL7000_DEVICETYPE)) {
                     options.activateFirmwareCheckWithStatuses(FirmwareCheckManagementOption.TARGET_FIRMWARE_STATUS_CHECK, Collections.singleton(FirmwareStatus.FINAL));
                 }
-                options.setOptions(firmwareService.getSupportedFirmwareOptionsFor(deviceType));
+                options.setOptions(supportedFirmwareOptions);
                 options.save();
             }
         }
     }
 
-    private void setFirmwareBytes(FirmwareVersion firmwareVersion, InputStream inputStream){
+    private void setFirmwareBytes(FirmwareVersion firmwareVersion, InputStream inputStream) {
         try {
             this.firmwareService.findAndLockFirmwareVersionByIdAndVersion(firmwareVersion.getId(), firmwareVersion.getVersion());
             firmwareVersion.setFirmwareFile(getBytes(inputStream));
             firmwareVersion.setFirmwareStatus(FirmwareStatus.FINAL);
             firmwareVersion.update();
-        }catch(IOException exception){
-           throw new UnableToCreate("FirmwareFile " + firmwareVersion.getFirmwareVersion() + ".firm could not be read");
+        } catch (IOException exception) {
+            throw new UnableToCreate("FirmwareFile " + firmwareVersion.getFirmwareVersion() + ".firm could not be read");
         }
     }
 
