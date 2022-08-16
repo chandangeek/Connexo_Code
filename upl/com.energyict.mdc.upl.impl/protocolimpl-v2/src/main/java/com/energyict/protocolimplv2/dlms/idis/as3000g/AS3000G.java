7915ef4d5dbde79a5e4d3088c5f217cf2462da26
H514454 <PETRUTA.BIANCA@Honeywell.com>
2022-08-16 15:08:37 +0300

    COMMUNICATION-3988 updated version

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/idis/as3000g/AS3000G.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/dlms/idis/as3000g/AS3000G.java
@@ -80,6 +80,6 @@ public class AS3000G extends AM540 {
 
     @Override
     public String getVersion() {
-        return "$Date: 2022-06-16 10:05:00 +0300 (Thu, 16 Jun 2022)$";
+        return "$Date: 2022-08-16 10:05:00 +0300 (Thu, 16 Jun 2022)$";
     }
 }
