7d151dad91d8ed0d0a047e8c4d66410ca3bdc2a7
H233293 <paul.vanminderhout2@honeywell.com>
2022-12-14 08:43:02 +0100

    CONM-2080_-_Fixed_Connexo_Core_Communication_tasks_with_only_Action_Commands_and_there_are no messages, Logoff is not executed, Framecounter is not stored, Connection Failed while Successfull

--- comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/commands/store/core/DeviceProtocolCommandCreator.java
+++ comu/com.energyict.mdc.engine/src/main/java/com/energyict/mdc/engine/impl/commands/store/core/DeviceProtocolCommandCreator.java
@@ -6,6 +6,8 @@ package com.energyict.mdc.engine.impl.commands.store.core;
 
 import com.energyict.mdc.common.tasks.ComTaskExecution;
 import com.energyict.mdc.common.tasks.ProtocolTask;
+import com.energyict.mdc.engine.impl.commands.collect.ComCommandTypes;
+import com.energyict.mdc.engine.impl.commands.store.deviceactions.NoopCommandImpl;
 import com.energyict.mdc.engine.impl.core.ComTaskExecutionConnectionSteps;
 import com.energyict.mdc.engine.impl.core.CommandCreator;
 import com.energyict.mdc.engine.impl.core.CommandFactory;
@@ -33,6 +35,14 @@ public class DeviceProtocolCommandCreator implements CommandCreator {
         if (!hasCommandsToExecute(groupedDeviceCommand, protocolTasks, comTaskExecution)) {
             // create an entry in the comTaskExecutionComCommands map for the comTaskExecution, without any command
             groupedDeviceCommand.getComTaskRoot(comTaskExecution);
+
+            if (groupedDeviceCommand.getCommands().containsKey(ComCommandTypes.LOGON)) {
+                groupedDeviceCommand.addCommand(new NoopCommandImpl(groupedDeviceCommand), comTaskExecution);
+                if (comTaskExecutionConnectionStep.isLogOffRequired()) {
+                    CommandFactory.createLogOffCommand(groupedDeviceCommand, comTaskExecution);
+                    doTearDown(groupedDeviceCommand, groupedDeviceCommand.getOfflineDevice(), comTaskExecution);
+                }
+            }
             return;
         }
         if (comTaskExecutionConnectionStep.isLogOnRequired()) {
@@ -57,10 +67,7 @@ public class DeviceProtocolCommandCreator implements CommandCreator {
     public static boolean hasCommandsToExecute(GroupedDeviceCommand groupedDeviceCommand, List<ProtocolTask> protocolTasks, ComTaskExecution comTaskExecution) {
         GroupedDeviceCommand clonedGroupedDeviceCommand = groupedDeviceCommand.clone();
         CommandFactory.createCommandsFromTask(clonedGroupedDeviceCommand, comTaskExecution, protocolTasks);
-        if (clonedGroupedDeviceCommand.getComTaskRoot(comTaskExecution).getCommands().isEmpty()) {
-            return false;
-        }
-        return true;
+        return !clonedGroupedDeviceCommand.getComTaskRoot(comTaskExecution).getCommands().isEmpty();
     }
 
     private void doSetupNewDevice(GroupedDeviceCommand groupedDeviceCommand, TypedProperties protocolDialectProperties, ComChannelPlaceHolder comChannel, OfflineDevice offlineDevice, DeviceProtocolSecurityPropertySet deviceProtocolSecurityPropertySet, ComTaskExecution comTaskExecution) {
