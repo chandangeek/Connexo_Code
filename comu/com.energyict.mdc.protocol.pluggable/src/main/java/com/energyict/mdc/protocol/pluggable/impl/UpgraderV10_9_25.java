03372007b601e9f12e706a1503fce66a8d524d06
Elena Borunova <elena.borunova@orioninc.com>
2023-03-28 11:59:40 +0300

    COMMUNICATION-4115 Apply code review remarks 2

--- comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/UpgraderV10_9_25.java
+++ comu/com.energyict.mdc.protocol.pluggable/src/main/java/com/energyict/mdc/protocol/pluggable/impl/UpgraderV10_9_25.java
@@ -16,7 +16,7 @@ import java.sql.Statement;
 import java.time.Clock;
 
 
-public class UpgraderV10_9_25  implements Upgrader {
+public class UpgraderV10_9_25 implements Upgrader {
     private final DataModel dataModel;
     private final Clock clock;
 
@@ -30,12 +30,8 @@ public class UpgraderV10_9_25  implements Upgrader {
     public void migrate(DataModelUpgrader dataModelUpgrader) {
         dataModelUpgrader.upgrade(dataModel, Version.version(10, 9, 25));
         long upgradeTime = clock.instant().toEpochMilli();
-        dataModel.useConnectionRequiringTransaction(connection -> {
-            String sql = "INSERT INTO CPC_PLUGGABLECLASS (ID, NAME, JAVACLASSNAME, PLUGGABLETYPE, VERSIONCOUNT, CREATETIME, MODTIME) " +
-                    "VALUES (1005, 'OutboundWebServiceConnectionType', 'com.energyict.mdc.channels.ip.socket.OutboundWebServiceConnectionType', 18, 1, " + upgradeTime + ", " + upgradeTime + ")";
-            try (Statement statement = connection.createStatement()) {
-                execute(statement, sql);
-            }
-        });
+        String sql = "INSERT INTO CPC_PLUGGABLECLASS (ID, NAME, JAVACLASSNAME, PLUGGABLETYPE, VERSIONCOUNT, CREATETIME, MODTIME) " +
+                "VALUES (1005, 'OutboundWebServiceConnectionType', 'com.energyict.mdc.channels.ip.socket.OutboundWebServiceConnectionType', 18, 1, " + upgradeTime + ", " + upgradeTime + ")";
+        execute(dataModel, sql);
     }
 }
