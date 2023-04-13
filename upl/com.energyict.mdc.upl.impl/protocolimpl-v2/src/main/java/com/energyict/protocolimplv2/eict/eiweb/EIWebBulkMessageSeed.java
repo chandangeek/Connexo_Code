a9c08c9834aa93ca9e3c8fe779c2188eaa4f3fe8
H233293 <paul.vanminderhout2@honeywell.com>
2023-04-13 10:23:29 +0200

    COMMUNICATION-4108 implement wait time for future data or reject when no wait time configured or exceeds maximum, maximum allowed wait time is 30 seconds, corrected Log and Exception messages

--- upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/eiweb/EIWebBulkMessageSeed.java
+++ upl/com.energyict.mdc.upl.impl/protocolimpl-v2/src/main/java/com/energyict/protocolimplv2/eict/eiweb/EIWebBulkMessageSeed.java
@@ -3,20 +3,20 @@ package com.energyict.protocolimplv2.eict.eiweb;
 public class EIWebBulkMessageSeed {
 
     public static String MAX_CONFIGURED_WAIT_TIME_X_EXCEEDS_THE_MAXIMUM_ALLOWED_Y =
-                            "The Configured Max Wait Time {0} ms exceeds the maximum allowed value of {1} ms, " +
+                            "The configured max wait time {0} ms exceeds the maximum allowed value of {1} ms, " +
                             "Rejecting Message";
     public static String DATA_IN_FUTURE_DIFFERENCE_X_NO_WAIT_TIME_CONFIGURED_REJECTING_MESSAGE =
-                            "Received LoadProfileData in the future, difference {0} ms no wait time configured on device, " +
+                            "Received load profile data in the future, difference {0} ms no wait time configured on device, " +
                             "Rejecting Message";
     public static String DATA_IN_FUTURE_WHICH_DIFFERENCE_X_EXCEED_MAXIMUM_CONFIGURED_Y =
-                            "Received LoadProfileData in the future, difference {0} ms which exceed the maximum " +
+                            "Received load profile data in the future, difference {0} ms which exceeds the maximum " +
                             "configured wait time of {1} ms ";
     public static String DATA_IN_FUTURE_CONFIGURED_DIFFERENCE_X_MAX_Y_START_WAITING =
-                            "Received LoadProfile Data in the future, difference {0} ms which is within the configured " +
+                            "Received load profile data in the future, difference {0} ms which is within the configured " +
                             "wait time of {1} ms, Start waiting";
 
     public static String DATA_IN_FUTURE_CONFIGURED_DIFFERENCE_X_MAX_Y_FINISHED_WAITING =
-                            "Received LoadProfile Data in the future, difference {0} ms which is within the configured " +
+                            "Received load profile data in the future, difference {0} ms which is within the configured " +
                             "wait time of {1} ms, Finished waiting";
 
 }
